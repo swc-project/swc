1535fffcbdb7a0d9788ab9061559608404e75482
강동윤 <kdy1@outlook.kr>
2017-12-27 11:50:36 +0900

    make rustdoc better

---
+++ scripts/rustdoc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eu
+
+crate_name() {
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
+
+    case $key in
+        --crate-name)
+        CRATE_NAME="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL[@]}" # restore positional parameters
+
+    echo "$CRATE_NAME"
+}
+
+cr=$(crate_name "$@")
+
+if [[ $cr == swc* ]]; then
+    # We use this instead of --document-private-items to
+    # make output simillar to usage from outside.
+    #
+    # e.g. this inlines self::stmt::*, and when we're using ecmascript::ast, 
+    #   we can't use ecmascript::ast::stmt because it's private.  
+    rustdoc --passes strip-hidden,unindent-comments,\
+collapse-docs,strip-priv-imports,propagate-doc-cfg $@
+else
+    rustdoc "$@"
+fi
+
