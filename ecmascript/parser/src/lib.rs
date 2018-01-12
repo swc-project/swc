97b99dbd21df19f3f104d1b55aa85400c73da983
강동윤 <kdy1@outlook.kr>
2018-01-12 15:14:55 +0900

    move parser to subdirectory

---
+++ ecmascript/parser/src/lib.rs
@@ -0,0 +1,36 @@
+#![feature(box_syntax)]
+#![feature(box_patterns)]
+#![feature(const_fn)]
+#![feature(specialization)]
+#![feature(never_type)]
+#![feature(nll)]
+#![feature(proc_macro)]
+#![feature(try_from)]
+#![feature(try_trait)]
+#![feature(trace_macros)]
+#![cfg_attr(test, feature(conservative_impl_trait))]
+#![deny(unreachable_patterns)]
+
+extern crate either;
+#[macro_use]
+extern crate failure;
+extern crate parser_macros;
+#[macro_use]
+extern crate slog;
+#[macro_use(js_word)]
+extern crate swc_atoms;
+extern crate swc_common;
+#[macro_use]
+extern crate swc_macros;
+#[cfg(test)]
+#[macro_use]
+extern crate testing;
+extern crate unicode_xid;
+pub extern crate swc_ecma_ast as ast;
+
+#[macro_use]
+mod macros;
+pub mod error;
+pub mod lexer;
+pub mod token;
+pub mod parser;
