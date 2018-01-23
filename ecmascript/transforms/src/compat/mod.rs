fc2746a7501bec1b8e12115ce77eb3a29f5c5c0e
강동윤 <kdy1@outlook.kr>
2018-01-23 15:27:22 +0900

    fixup! [WIP] object shorthand property

--- ecmascript/transforms/src/compat/mod.rs
+++ ecmascript/transforms/src/compat/mod.rs
@@ -1,3 +1,5 @@
 pub use self::exponentation::Exponentation;
+pub use self::shorthand::Shorthand;
 
 mod exponentation;
+mod shorthand;
---
+++ ecmascript/transforms/src/compat/shorthand.rs
@@ -0,0 +1,128 @@
+use swc_common::{FoldWith, Folder};
+use swc_ecma_ast::*;
+
+/// Compile ES2015 shorthand properties to ES5
+///
+///# Example
+///## In
+///
+/// ```js
+/// var o = { a, b, c };
+/// ```
+///
+///## Out
+///
+/// ```js
+/// var o = { a: a, b: b, c: c };
+/// ```
+///
+///## In
+///
+/// ```js
+/// var cat = {
+///   getName() {
+///     return name;
+///   }
+/// };
+/// ```
+///
+///## Out
+///```js
+/// var cat = {
+///   getName: function () {
+///     return name;
+///   }
+/// };
+/// ```
+///
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Shorthand;
+
+impl Folder<PropKind> for Shorthand {
+    fn fold(&mut self, prop: PropKind) -> PropKind {
+        let prop = prop.fold_children(self);
+
+        match prop {
+            PropKind::Shorthand(i) => PropKind::KeyValue {
+                key: PropName::Ident(i.clone()),
+                value: i.into(),
+            },
+            PropKind::Method { key, function } => PropKind::KeyValue {
+                key,
+                value: box Expr {
+                    span: function.span,
+                    node: ExprKind::Fn(FnExpr {
+                        ident: None,
+                        function,
+                    }),
+                },
+            },
+            _ => prop,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    test!(
+        Shorthand,
+        babel_method_plain,
+        "var obj = {
+  method() {
+    return 5 + 5;
+  }
+};",
+        "var obj = {
+  method: function () {
+    return 5 + 5;
+  }
+};"
+    );
+
+    test!(
+        Shorthand,
+        babel_comments,
+        "var A = 'a';
+var o = {
+  A // comment
+};",
+        "var A = 'a';
+var o = {
+  A: A // comment
+
+};"
+    );
+
+    test!(
+        Shorthand,
+        babel_mixed,
+        "var coords = { x, y, foo: 'bar' };",
+        "var coords = {
+  x: x,
+  y: y,
+  foo: 'bar'
+};"
+    );
+
+    test!(
+        Shorthand,
+        babel_multiple,
+        "var coords = { x, y };",
+        "var coords = {
+  x: x,
+  y: y
+};"
+    );
+
+    test!(
+        Shorthand,
+        babel_single,
+        "var coords = { x };",
+        "var coords = {
+  x: x
+};"
+    );
+
+}
