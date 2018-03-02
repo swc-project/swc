773ebdc0b00e20f058ee54482d47d5e878e5cc53
강동윤 <kdy1@outlook.kr>
2018-03-02 21:57:52 +0900

    SemiAware -> OmitTrailingSemi

---
+++ ecmascript/codegen/src/config.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub struct Config {
+    pub omit_trailing_semi: bool,
+}
--- ecmascript/codegen/src/lib.rs
+++ ecmascript/codegen/src/lib.rs
@@ -1,17 +1,20 @@
+#![feature(box_syntax)]
+
 pub extern crate sourcemap;
 extern crate swc_ecma_ast;
 
 use sourcemap::SourceMapBuilder;
 use std::io::{self, Write};
-use text_writer::{SemiAware, TextWriter};
+use text_writer::TextWriter;
 
+pub mod config;
 pub mod text_writer;
 
 pub type Result = io::Result<()>;
 
 pub struct Emitter {
     pub srcmap: SourceMapBuilder,
-    pub writer: SemiAware<Box<TextWriter>>,
+    pub writer: Box<TextWriter>,
 }
 
 impl Emitter {}
