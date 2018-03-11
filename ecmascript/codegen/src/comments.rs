5aaded7982f18778651c377a590f3db675fffc03
강동윤 <kdy1@outlook.kr>
2018-03-11 18:50:51 +0900

    [WIP] Basical emitter

---
+++ ecmascript/codegen/src/comments.rs
@@ -0,0 +1,50 @@
+use super::*;
+
+impl<'a> Emitter<'a> {
+    pub(super) fn emit_trailing_comments_of_pos(
+        &mut self,
+        pos: BytePos,
+        prefix_space: bool,
+    ) -> Result {
+        if !self.enable_comments {
+            return Ok(());
+        }
+        unimplemented!()
+    }
+    pub(super) fn emit_leading_comments_of_pos(&mut self, pos: BytePos) -> Result {
+        if !self.enable_comments {
+            return Ok(());
+        }
+        unimplemented!()
+    }
+
+    // /// (commentPos: number, commentEnd: number, kind: SyntaxKind,
+    // /// hasTrailingNewLine: boolean, rangePos: number)
+    // fn for_each_leading_comment_to_emit<F>(&mut self, pos: BytePos, cb: F)
+    // where
+    //     F: FnMut(&mut Self, BytePos, BytePos, SyntaxKind, bool, BytePos),
+    // {
+    // // Emit the leading comments only if the container's pos doesn't match
+    // because     // the container should take care of emitting these comments
+    //     if containerPos == -1 || pos != containerPos {
+    //         if hasDetachedComments(pos) {
+    //             forEachLeadingCommentWithoutDetachedComments(cb);
+    //         } else {
+    // forEachLeadingCommentRange(currentText, pos, cb, /* state */
+    // pos);         }
+    //     }
+    // }
+
+    // ///(commentPos: number, commentEnd: number, kind: SyntaxKind,
+    // /// hasTrailingNewLine: boolean)
+    // fn for_each_trailing_comment_to_emit<F>(&mut self, end: BytePos, cb: F)
+    // where
+    //     F: FnMut(&mut Self, BytePos, BytePos, SyntaxKind, bool),
+    // {
+    // // Emit the trailing comments only if the container's end doesn't match
+    // because     // the container should take care of emitting these comments
+    // if containerEnd == -1 || (end != containerEnd && end !=
+    // declarationListContainerEnd) { forEachTrailingCommentRange(currentText,
+    // end, cb);     }
+    // }
+}
--- ecmascript/codegen/src/config.rs
+++ ecmascript/codegen/src/config.rs
@@ -1,4 +1,17 @@
-#[derive(Debug)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Config {
     pub omit_trailing_semi: bool,
+    pub sourcemap: Option<SourceMapConfig>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
+pub enum SourceMapConfig {
+    File,
+    Inline,
+}
+
+impl Config {
+    pub fn is_sourcemap_enabled(&self) -> bool {
+        self.sourcemap.is_some()
+    }
 }
