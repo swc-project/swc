bd671d07440cb2f925bda51888efa09a3b79400f
강동윤 <kdy1@outlook.kr>
2018-10-31 13:40:04 +0900

    quoter for member expr / ident expr

--- ecmascript/transforms/src/lib.rs
+++ ecmascript/transforms/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(box_syntax)]
 #![feature(try_trait)]
 #![feature(specialization)]
+#![feature(trace_macros)]
 
 #[cfg(test)]
 #[macro_use]
@@ -11,14 +12,15 @@ pub extern crate swc_atoms;
 pub extern crate swc_common;
 #[macro_use]
 pub extern crate swc_ecma_ast;
-#[cfg(test)]
-extern crate swc_ecma_parser;
+pub extern crate swc_ecma_parser;
 #[cfg(test)]
 #[macro_use]
 extern crate testing;
 
 #[macro_use]
 mod macros;
+#[macro_use]
+mod quote;
 pub mod compat;
 pub mod scope;
 pub mod simplify;
---
+++ ecmascript/transforms/src/quote.rs
@@ -0,0 +1,77 @@
+#[macro_export]
+macro_rules! quote_ident {
+    ($span:expr, $s:expr) => {{
+        ::swc_ecma_ast::Ident::new($s.into(), span)
+    }};
+}
+
+#[macro_export]
+macro_rules! expr {
+    ($span:expr, null) => {{
+        use swc_ecma_ast::*;
+        Expr::Lit(Lit::Null(Null { span }))
+    }};
+
+    ($span:expr, undefined) => {{
+        box Expr::Ident(Ident::new(js_word!("undefined"), span))
+    }};
+}
+
+macro_rules! member_expr {
+    ($span:expr, $first:ident) => {{
+        use swc_ecma_ast::*;
+        box Expr::Ident(Ident::new($first.into(), span))
+    }};
+
+    ($span:expr, $first:ident . $($rest:tt)+) => {{
+        use swc_ecma_ast::*;
+        let obj = member_expr!(span, $first);
+
+        member_expr!(@EXT, $span, obj, $($rest)* );
+    }};
+
+    (@EXT, $span:expr, $obj:expr,  $first:ident . $($rest:tt)* ) => {{
+        use swc_ecma_ast::*;
+        let prop = member_expr!(span, $first);
+
+        member_expr!(@EXT, $span, Expr::Member(MemberExpr{
+            span: $span,
+            obj: $obj,
+            computed: false,
+            prop,
+        }), $($rest)*);
+    }};
+
+    (@EXT, $span:expr, $obj:expr,  $first:ident) => {{
+        use swc_ecma_ast::*;
+        let prop = member_expr!(span, $first);
+
+        box Expr::Member(MemberExpr{
+            span: $span,
+            obj: $obj,
+            computed: false,
+            prop,
+        })
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use swc_common::DUMMY_SP as span;
+    #[test]
+    fn quote_member_expr() {
+        assert_eq_ignore_span!(
+            member_expr!(span, Function.prototype.bind),
+            MemberExpr {
+                obj: MemberExpr {
+                    obj: member_expr!(span, Function),
+                    computed: false,
+                    prop: member_expr!(span, prototype),
+                },
+                computed: false,
+                prop: member_expr!(span, bind),
+            }
+        );
+    }
+}
