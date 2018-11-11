4cbc52f1a11f01b9617a3ac5fcfca4120f5fd795
강동윤 <kdy1@outlook.kr>
2018-11-11 21:50:13 +0900

    ast: add methods about reserved words

---
+++ ecmascript/ast/src/keywords.rs
@@ -0,0 +1,102 @@
+use crate::Ident;
+use swc_atoms::JsWord;
+
+pub trait IdentExt: AsRef<str> {
+    fn is_reserved_for_es3(&self) -> bool {
+        [
+            "abstract",
+            "boolean",
+            "break",
+            "byte",
+            "case",
+            "catch",
+            "char",
+            "class",
+            "const",
+            "continue",
+            "debugger",
+            "default",
+            "delete",
+            "do",
+            "double",
+            "else",
+            "enum",
+            "export",
+            "extends",
+            "false",
+            "final",
+            "finally",
+            "float",
+            "for",
+            "function",
+            "goto",
+            "if",
+            "implements",
+            "import",
+            "in",
+            "instanceof",
+            "int",
+            "interface",
+            "long",
+            "native",
+            "new",
+            "null",
+            "package",
+            "private",
+            "protected",
+            "public",
+            "return",
+            "short",
+            "static",
+            "super",
+            "switch",
+            "synchronized",
+            "this",
+            "throw",
+            "throws",
+            "transient",
+            "true",
+            "try",
+            "typeof",
+            "var",
+            "void",
+            "volatile",
+            "while",
+            "with",
+        ]
+            .contains(&self.as_ref())
+    }
+
+    fn is_reserved_only_for_es3(&self) -> bool {
+        [
+            "abstract",
+            "boolean",
+            "byte",
+            "char",
+            "double",
+            "enum",
+            "final",
+            "float",
+            "goto",
+            "implements",
+            "int",
+            "interface",
+            "long",
+            "native",
+            "package",
+            "private",
+            "protected",
+            "public",
+            "short",
+            "static",
+            "synchronized",
+            "throws",
+            "transient",
+            "volatile",
+        ]
+            .contains(&self.as_ref())
+    }
+}
+
+impl IdentExt for JsWord {}
+impl IdentExt for Ident {}
