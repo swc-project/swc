7e8c0651179c0e6073ab02730bf851566dc13785
강동윤 <kdy1@outlook.kr>
2018-11-27 11:56:09 +0900

    implement rest parameter (test: 9/27)

--- ecmascript/transforms/src/compat/es2015/parameters/mod.rs
+++ ecmascript/transforms/src/compat/es2015/parameters/mod.rs
@@ -1,5 +1,6 @@
 use ast::*;
-use swc_common::{Fold, FoldWith};
+use crate::util::ExprFactory;
+use swc_common::{Fold, FoldWith, Mark, Spanned, DUMMY_SP};
 
 #[cfg(test)]
 mod tests;
@@ -9,3 +10,149 @@ pub fn parameters() -> impl Fold<Module> {
 }
 
 struct Params;
+
+impl Fold<Function> for Params {
+    fn fold(&mut self, f: Function) -> Function {
+        let f = f.fold_children(self);
+
+        let mut params = vec![];
+        let mut decls = vec![];
+        let mut unpack_rest = None;
+
+        for (i, param) in f.params.into_iter().enumerate() {
+            let span = param.span();
+
+            match param {
+                Pat::Ident(..) => params.push(param),
+                Pat::Array(..) | Pat::Object(..) => {
+                    let mark = Mark::fresh(Mark::root());
+                    let binding = quote_ident!(span.apply_mark(mark), "param");
+
+                    params.push(Pat::Ident(binding.clone()));
+                    decls.push(VarDeclarator {
+                        span,
+                        name: param,
+                        init: Some(box Expr::Ident(binding)),
+                    })
+                }
+                Pat::Rest(RestPat { dot3_token, arg }) => {
+                    // Inject a for statement
+                    //
+                    // for(var _len = arguments.length, a1 = new Array(_len), _key = 0; _key <
+                    // _len; _key++){
+                    //      a1[_key] = arguments[_key];
+                    // }
+                    assert!(unpack_rest.is_none());
+                    let mark = Mark::fresh(Mark::root());
+                    let idx_ident = quote_ident!(span.apply_mark(mark), "_key");
+                    let len_ident = quote_ident!(span.apply_mark(mark), "_len");
+
+                    let arg = match *arg {
+                        Pat::Ident(ident) => ident,
+                        _ => unreachable!("unknown rest pattern `...{:?}`", arg),
+                    };
+
+                    unpack_rest = Some(Stmt::For(ForStmt {
+                        span,
+                        init: Some(VarDeclOrExpr::VarDecl(VarDecl {
+                            kind: VarDeclKind::Var,
+                            span,
+                            decls: vec![
+                                // _len = arguments.length
+                                VarDeclarator {
+                                    span,
+                                    name: Pat::Ident(len_ident.clone()),
+                                    init: Some(member_expr!(span, arguments.length)),
+                                },
+                                // a1 = new Array(_len)
+                                VarDeclarator {
+                                    span,
+                                    name: Pat::Ident(arg.clone()),
+                                    init: Some(box Expr::New(NewExpr {
+                                        span,
+                                        callee: box quote_ident!("Array").into(),
+                                        args: Some(vec![len_ident.clone().as_arg()]),
+                                    })),
+                                },
+                                // _key = 0
+                                VarDeclarator {
+                                    span,
+                                    name: Pat::Ident(idx_ident.clone().into()),
+                                    init: Some(box Expr::Lit(Lit::Num(Number {
+                                        span,
+                                        value: i as f64,
+                                    }))),
+                                },
+                            ],
+                        })),
+                        // `_key < _len`
+                        test: Some(box Expr::Bin(BinExpr {
+                            span,
+                            left: box idx_ident.clone().into(),
+                            op: op!("<"),
+                            right: box len_ident.into(),
+                        })),
+                        // _key++
+                        update: Some(box Expr::Update(UpdateExpr {
+                            span,
+                            op: op!("++"),
+                            prefix: false,
+                            arg: box idx_ident.clone().into(),
+                        })),
+                        body: box Stmt::Block(BlockStmt {
+                            span,
+                            stmts: vec![{
+                                let prop = box Expr::Ident(idx_ident.clone());
+                                // a1[_key] = arguments[_key];
+                                Stmt::Expr(box Expr::Assign(AssignExpr {
+                                    span,
+                                    left: PatOrExpr::Expr(
+                                        box MemberExpr {
+                                            span,
+                                            obj: ExprOrSuper::Expr(box Expr::Ident(arg)),
+                                            computed: true,
+                                            prop: prop.clone(),
+                                        }
+                                        .into(),
+                                    ),
+                                    op: op!("="),
+                                    right: box MemberExpr {
+                                        span,
+                                        obj: ExprOrSuper::Expr(
+                                            box quote_ident!(span, "arguments").into(),
+                                        ),
+                                        computed: true,
+                                        prop,
+                                    }
+                                    .into(),
+                                }))
+                            }],
+                        }),
+                    }))
+                }
+                _ => params.push(param),
+            }
+        }
+
+        let mut body = f.body;
+        let stmts = if decls.is_empty() {
+            None
+        } else {
+            Some(Stmt::Decl(Decl::Var(VarDecl {
+                span: DUMMY_SP,
+                kind: VarDeclKind::Let,
+                decls,
+            })))
+        }
+        .into_iter()
+        .chain(unpack_rest)
+        .chain(body.stmts)
+        .collect();
+
+        Function {
+            params,
+            body: BlockStmt { stmts, ..body },
+            ..f
+        }
+    }
+}
