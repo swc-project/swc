9d8f8e50fc1f78dd245bea977da9384b6f89c760
강동윤 <kdy1@outlook.kr>
2018-12-21 19:05:50 +0900

    [WIP] for_of statement

---
+++ ecmascript/transforms/src/compat/es2015/for_of/mod.rs
@@ -0,0 +1,312 @@
+use crate::util::ExprFactory;
+use ast::*;
+use swc_common::{Fold, FoldWith, Mark, Spanned, DUMMY_SP};
+
+#[cfg(test)]
+mod tests;
+
+pub fn for_of() -> impl Fold<Module> {
+    ForOf
+}
+
+struct ForOf;
+
+/// Real folder.
+#[derive(Default)]
+struct Actual {
+    ///```js
+    /// var _iteratorNormalCompletion = true;
+    /// var _didIteratorError = false;
+    /// var _iteratorError = undefined;
+    /// ```
+    top_level_vars: Vec<VarDeclarator>,
+}
+
+impl Fold<Stmt> for Actual {
+    fn fold(&mut self, stmt: Stmt) -> Stmt {
+        let stmt = stmt.fold_children(self);
+
+        match stmt {
+            Stmt::ForOf(ForOfStmt {
+                span,
+                await_token: None,
+                left,
+                right,
+                body,
+            }) => {
+                let var_span = left.span().apply_mark(Mark::fresh(Mark::root()));
+                // convert to normal for loop if rhs is array
+                // TODO: Type annotation
+                match *right {
+                    Expr::Array(_arr) => unimplemented!("Array optimization"),
+                    _ => {}
+                }
+
+                let mut body = match *body {
+                    Stmt::Block(block) => block,
+                    body => BlockStmt {
+                        span: DUMMY_SP,
+                        stmts: vec![body],
+                    },
+                };
+
+                let step = quote_ident!(var_span, "_step");
+                let step_value = box Expr::Member(MemberExpr {
+                    span: DUMMY_SP,
+                    obj: ExprOrSuper::Expr(box Expr::Ident(step.clone())),
+                    computed: false,
+                    prop: box Expr::Ident(quote_ident!("value")),
+                });
+                body.stmts.insert(
+                    0,
+                    match left {
+                        //TODO
+                        VarDeclOrPat::VarDecl(decl) => Stmt::Decl(Decl::Var(decl)),
+                        VarDeclOrPat::Pat(pat) => Stmt::Expr(box Expr::Assign(AssignExpr {
+                            span: DUMMY_SP,
+                            left: PatOrExpr::Pat(box pat),
+                            op: op!("="),
+                            right: step_value,
+                        })),
+                    },
+                );
+
+                let iterator = quote_ident!(var_span, "_iterator");
+                // `_iterator.return`
+                let iterator_return = box Expr::Member(MemberExpr {
+                    span: DUMMY_SP,
+                    obj: ExprOrSuper::Expr(box Expr::Ident(iterator.clone())),
+                    computed: false,
+                    prop: box Expr::Ident(quote_ident!("return")),
+                });
+
+                let normal_completion_ident =
+                    Ident::new("_iteratorNormalCompletion".into(), var_span);
+                self.top_level_vars.push(VarDeclarator {
+                    span: DUMMY_SP,
+                    name: Pat::Ident(normal_completion_ident.clone()),
+                    init: Some(box Expr::Lit(Lit::Bool(Bool {
+                        span: DUMMY_SP,
+                        value: true,
+                    }))),
+                });
+                let error_flag_ident = Ident::new("_didIteratorError".into(), var_span);
+                self.top_level_vars.push(VarDeclarator {
+                    span: DUMMY_SP,
+                    name: Pat::Ident(error_flag_ident.clone()),
+                    init: Some(box Expr::Lit(Lit::Bool(Bool {
+                        span: DUMMY_SP,
+                        value: false,
+                    }))),
+                });
+                let error_ident = Ident::new("_iteratorError".into(), var_span);
+                self.top_level_vars.push(VarDeclarator {
+                    span: DUMMY_SP,
+                    name: Pat::Ident(error_ident.clone()),
+                    init: Some(box Expr::Ident(Ident::new(js_word!("undefined"), DUMMY_SP))),
+                });
+
+                let for_stmt = ForStmt {
+                    span,
+                    init: Some(VarDeclOrExpr::VarDecl(VarDecl {
+                        span: DUMMY_SP,
+                        kind: VarDeclKind::Var,
+                        decls: vec![
+                            VarDeclarator {
+                                span: DUMMY_SP,
+                                name: Pat::Ident(iterator.clone()),
+                                init: Some(box Expr::Call(CallExpr {
+                                    span: DUMMY_SP,
+                                    callee: MemberExpr {
+                                        span: DUMMY_SP,
+                                        obj: ExprOrSuper::Expr(right),
+                                        computed: true,
+                                        prop: member_expr!(DUMMY_SP, Symbol.iterator),
+                                    }
+                                    .as_callee(),
+                                    args: vec![],
+                                })),
+                            },
+                            VarDeclarator {
+                                span: DUMMY_SP,
+                                name: Pat::Ident(step.clone()),
+                                init: None,
+                            },
+                        ],
+                    })),
+                    // !(_iteratorNormalCompletion = (_step = _iterator.next()).done)
+                    test: Some(box Expr::Unary(UnaryExpr {
+                        span: DUMMY_SP,
+                        op: op!("!"),
+                        arg: box Expr::Paren(ParenExpr {
+                            span: DUMMY_SP,
+                            expr: box Expr::Member(MemberExpr {
+                                span: DUMMY_SP,
+                                obj: ExprOrSuper::Expr(box Expr::Assign(AssignExpr {
+                                    span: DUMMY_SP,
+                                    left: PatOrExpr::Pat(box Pat::Ident(
+                                        normal_completion_ident.clone(),
+                                    )),
+                                    op: op!("="),
+                                    right: box Expr::Paren(ParenExpr {
+                                        span: DUMMY_SP,
+                                        expr: box Expr::Assign(AssignExpr {
+                                            span: DUMMY_SP,
+                                            left: PatOrExpr::Pat(box Pat::Ident(step.clone())),
+                                            op: op!("="),
+                                            // `_iterator.next()`
+                                            right: box Expr::Call(CallExpr {
+                                                span: DUMMY_SP,
+                                                // `_iterator.next`
+                                                callee: MemberExpr {
+                                                    span: DUMMY_SP,
+                                                    computed: false,
+                                                    obj: ExprOrSuper::Expr(box Expr::Ident(
+                                                        iterator.clone(),
+                                                    )),
+                                                    prop: member_expr!(DUMMY_SP, next),
+                                                }
+                                                .as_callee(),
+                                                args: vec![],
+                                            }),
+                                        }),
+                                    }),
+                                })),
+                                computed: false,
+                                prop: box Expr::Ident(quote_ident!("done")),
+                            }),
+                        }),
+                    })),
+
+                    // `_iteratorNormalCompletion = true`
+                    update: Some(box Expr::Assign(AssignExpr {
+                        span: DUMMY_SP,
+                        left: PatOrExpr::Pat(box Pat::Ident(normal_completion_ident.clone())),
+                        op: op!("="),
+                        right: box Expr::Lit(Lit::Bool(Bool {
+                            span: DUMMY_SP,
+                            value: true,
+                        })),
+                    })),
+                    body: box Stmt::Block(body),
+                };
+
+                Stmt::Try(TryStmt {
+                    span: DUMMY_SP,
+                    block: BlockStmt {
+                        span: DUMMY_SP,
+                        stmts: vec![Stmt::For(for_stmt)],
+                    },
+                    handler: Some(CatchClause {
+                        span: DUMMY_SP,
+                        param: Some(Pat::Ident(quote_ident!("err"))),
+                        // _didIteratorError = true;
+                        // _iteratorError = err;
+                        body: BlockStmt {
+                            span: DUMMY_SP,
+                            stmts: vec![
+                                // _didIteratorError = true;
+                                Stmt::Expr(box Expr::Assign(AssignExpr {
+                                    span: DUMMY_SP,
+                                    left: PatOrExpr::Pat(box Pat::Ident(error_flag_ident.clone())),
+                                    op: op!("="),
+                                    right: box Expr::Lit(Lit::Bool(Bool {
+                                        span: DUMMY_SP,
+                                        value: true,
+                                    })),
+                                })),
+                                // _iteratorError = err;
+                                Stmt::Expr(box Expr::Assign(AssignExpr {
+                                    span: DUMMY_SP,
+                                    left: PatOrExpr::Pat(box Pat::Ident(error_ident.clone())),
+                                    op: op!("="),
+                                    right: box Expr::Ident(quote_ident!("err")),
+                                })),
+                            ],
+                        },
+                    }),
+                    finalizer: Some(BlockStmt {
+                        span: DUMMY_SP,
+                        stmts: vec![
+                            //   try {
+                            //     if (!_iteratorNormalCompletion && _iterator.return != null) {
+                            //       _iterator.return();
+                            //     }
+                            //   } finally {
+                            //     if (_didIteratorError) {
+                            //       throw _iteratorError;
+                            //     }
+                            //   }
+                            Stmt::Try(TryStmt {
+                                span: DUMMY_SP,
+                                block: BlockStmt {
+                                    span: DUMMY_SP,
+                                    stmts: vec![
+                                        // if (!_iteratorNormalCompletion && _iterator.return !=
+                                        // null) {
+                                        //   _iterator.return();
+                                        // }
+                                        Stmt::If(IfStmt {
+                                            span: DUMMY_SP,
+                                            test: box Expr::Bin(BinExpr {
+                                                span: DUMMY_SP,
+                                                left: box Expr::Unary(UnaryExpr {
+                                                    span: DUMMY_SP,
+                                                    op: op!("!"),
+                                                    arg: box Expr::Ident(
+                                                        normal_completion_ident.clone(),
+                                                    ),
+                                                }),
+                                                op: op!("&&"),
+                                                right: box Expr::Bin(BinExpr {
+                                                    span: DUMMY_SP,
+                                                    left: iterator_return.clone(),
+                                                    op: op!("!="),
+                                                    right: box Expr::Lit(Lit::Null(Null {
+                                                        span: DUMMY_SP,
+                                                    })),
+                                                }),
+                                            }),
+                                            cons: box Stmt::Block(BlockStmt {
+                                                span: DUMMY_SP,
+                                                stmts: vec![Stmt::Expr(box Expr::Call(CallExpr {
+                                                    span: DUMMY_SP,
+                                                    callee: iterator_return.as_callee(),
+                                                    args: vec![],
+                                                }))],
+                                            }),
+                                            alt: None,
+                                        }),
+                                    ],
+                                },
+                                handler: None,
+                                finalizer: Some(BlockStmt {
+                                    span: DUMMY_SP,
+
+                                    stmts: vec![
+                                        // if (_didIteratorError) {
+                                        //   throw _iteratorError;
+                                        // }
+                                        Stmt::If(IfStmt {
+                                            span: DUMMY_SP,
+                                            test: box Expr::Ident(error_flag_ident),
+                                            cons: box Stmt::Block(BlockStmt {
+                                                span: DUMMY_SP,
+                                                stmts: vec![Stmt::Throw(ThrowStmt {
+                                                    span: DUMMY_SP,
+                                                    arg: box Expr::Ident(error_ident),
+                                                })],
+                                            }),
+                                            alt: None,
+                                        }),
+                                    ],
+                                }),
+                            }),
+                        ],
+                    }),
+                })
+            }
+            _ => stmt,
+        }
+    }
+}
---
+++ ecmascript/transforms/src/compat/es2015/for_of/tests.rs
@@ -0,0 +1,239 @@
+use super::*;
+
+test!(
+    ForOf,
+    spec_identifer,
+    r#"for (i of arr) {
+}"#,
+    r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    i = _step.value;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }"#
+);
+
+test!(ForOf, spec_ignore_cases, r#"for (var i of foo) {
+  switch (i) {
+    case 1:
+      break;
+  }
+}"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = foo[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+
+    switch (i) {
+      case 1:
+        break;
+    }
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}"#);
+
+test!(ForOf, spec_let, r#"for (let i of arr) {
+
+}"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    let i = _step.value;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }"#);
+
+test!(ForOf, spec_member_expr, r#"for (obj.prop of arr) {
+
+}"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    obj.prop = _step.value;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}"#);
+
+test!(ForOf, spec_multiple, r#"for (var i of arr) {
+
+}
+
+for (var i of numbers) {
+
+}
+"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = numbers[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var i = _step2.value;
+  }
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+      _iterator2.return();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
+    }
+  }
+}"#);
+
+test!(ForOf, spec_nested_label_for_of, r#"b: for (let c of d()) {
+  for (let e of f()) {
+    continue b;
+  }
+}"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  b: for (var _iterator = d()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    let c = _step.value;
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = f()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        let e = _step2.value;
+        continue b;
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}"#);
+
+test!(ForOf, spec_var, r#"for (var i of arr) {
+
+}"#, r#"var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return != null) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}"#);
