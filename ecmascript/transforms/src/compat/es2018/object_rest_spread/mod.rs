ec6ce297e57130d200659f47cfe0e7fc6c7a3cbd
강동윤 <kdy1@outlook.kr>
2018-12-24 22:19:01 +0900

    [WIP] Object rest pattern

--- ecmascript/transforms/src/compat/es2018/object_rest_spread/mod.rs
+++ ecmascript/transforms/src/compat/es2018/object_rest_spread/mod.rs
@@ -1,10 +1,13 @@
-use crate::{compat::helpers::Helpers, util::ExprFactory};
+use crate::{
+    compat::helpers::Helpers,
+    util::{ExprFactory, StmtLike},
+};
 use ast::*;
 use std::{
     mem,
     sync::{atomic::Ordering, Arc},
 };
-use swc_common::{Fold, FoldWith, Visit, VisitWith, DUMMY_SP};
+use swc_common::{Fold, FoldWith, Mark, Visit, VisitWith, DUMMY_SP};
 
 #[cfg(test)]
 mod tests;
@@ -20,6 +23,132 @@ pub fn object_rest_spread(helpers: Arc<Helpers>) -> impl Fold<Module> {
 
 struct ObjectRest {
     helpers: Arc<Helpers>,
+}
+
+struct RestFolder {
+    /// Injected before the original statement.
+    helpers: Arc<Helpers>,
+    vars: Vec<VarDeclarator>,
+    /// Injected after the original statement.
+    stmts: Vec<Stmt>,
+}
+
+/// Handles assign expression
+impl Fold<Expr> for RestFolder {
+    fn fold(&mut self, expr: Expr) -> Expr {
+        // fast path
+        if !contains_rest(&expr) {
+            return expr;
+        }
+
+        let expr = expr.fold_children(self);
+
+        match expr {
+            Expr::Assign(AssignExpr {
+                span,
+                left:
+                    PatOrExpr::Pat(box Pat::Object(ObjectPat {
+                        span: props_span,
+                        mut props,
+                    })),
+                op: op!("="),
+                right,
+            }) => {
+                match props.last() {
+                    Some(ObjectPatProp::Rest(..)) => {}
+                    _ => {
+                        return Expr::Assign(AssignExpr {
+                            span,
+                            left: PatOrExpr::Pat(box Pat::Object(ObjectPat {
+                                span: props_span,
+                                props,
+                            })),
+                            op: op!("="),
+                            right,
+                        });
+                    }
+                }
+                let last = match props.pop() {
+                    Some(ObjectPatProp::Rest(rest_pat)) => rest_pat,
+                    _ => unreachable!(),
+                };
+
+                let mark = Mark::fresh(Mark::root());
+                let mut var_ident = match *right {
+                    Expr::Ident(ref ident) => quote_ident!(format!("_{}", ident.sym)),
+                    _ => quote_ident!("_tmp"),
+                };
+                var_ident.span = var_ident.span.apply_mark(mark);
+
+                self.vars.push(VarDeclarator {
+                    span: DUMMY_SP,
+                    name: Pat::Ident(var_ident.clone()),
+                    init: Some(right),
+                });
+
+                let excluded_props = props
+                    .iter()
+                    .map(|prop| match prop {
+                        ObjectPatProp::KeyValue(KeyValuePatProp { key, .. }) => match key {
+                            PropName::Ident(ident) => Lit::Str(Str {
+                                span: ident.span,
+                                value: ident.sym.clone(),
+                                has_escape: false,
+                            })
+                            .as_arg(),
+                            PropName::Str(s) => Lit::Str(s.clone()).as_arg(),
+                            _ => {
+                                unimplemented!("numeric / computed property name with object rest")
+                            }
+                        },
+                        ObjectPatProp::Assign(AssignPatProp { key, .. }) => Lit::Str(Str {
+                            span: key.span,
+                            value: key.sym.clone(),
+                            has_escape: false,
+                        })
+                        .as_arg(),
+                        ObjectPatProp::Rest(..) => {
+                            unreachable!("invalid syntax (multiple rest element)")
+                        }
+                    })
+                    .map(Some)
+                    .collect();
+
+                self.helpers
+                    .object_without_properties
+                    .store(true, Ordering::Relaxed);
+                self.stmts.push(Stmt::Expr(box Expr::Assign(AssignExpr {
+                    span: DUMMY_SP,
+                    left: PatOrExpr::Pat(last.arg),
+                    op: op!("="),
+                    // we exclude properties using helper
+                    right: box Expr::Call(CallExpr {
+                        span: DUMMY_SP,
+                        callee: quote_ident!("_objectWithoutProperties").as_callee(),
+                        args: vec![
+                            var_ident.clone().as_arg(),
+                            ArrayLit {
+                                span: DUMMY_SP,
+                                elems: excluded_props,
+                            }
+                            .as_arg(),
+                        ],
+                    }),
+                })));
+
+                Expr::Assign(AssignExpr {
+                    span,
+                    left: PatOrExpr::Pat(box Pat::Object(ObjectPat {
+                        span: props_span,
+                        props,
+                    })),
+                    op: op!("="),
+                    right: box Expr::Ident(var_ident.clone()),
+                })
+            }
+            _ => expr,
+        }
+    }
 }
 
 struct RestVisitor {
@@ -39,6 +168,49 @@ where
     let mut v = RestVisitor { found: false };
     node.visit_with(&mut v);
     v.found
+}
+
+impl<T: StmtLike + VisitWith<RestVisitor>> Fold<Vec<T>> for ObjectRest
+where
+    Vec<T>: FoldWith<Self>,
+{
+    fn fold(&mut self, stmts: Vec<T>) -> Vec<T> {
+        if !contains_rest(&stmts) {
+            return stmts;
+        }
+        let stmts = stmts.fold_children(self);
+
+        let mut buf = vec![];
+
+        for stmt in stmts {
+            match stmt.try_into_stmt() {
+                Err(module_item) => buf.push(module_item),
+                Ok(stmt) => {
+                    let mut folder = RestFolder {
+                        helpers: self.helpers.clone(),
+                        vars: vec![],
+                        stmts: vec![],
+                    };
+                    let stmt = stmt.fold_with(&mut folder);
+
+                    // Add variable declaration
+                    // e.g. var ref
+                    if !folder.vars.is_empty() {
+                        buf.push(T::from_stmt(Stmt::Decl(Decl::Var(VarDecl {
+                            span: DUMMY_SP,
+                            kind: VarDeclKind::Var,
+                            decls: folder.vars,
+                        }))));
+                    }
+
+                    buf.push(T::from_stmt(stmt));
+                    buf.extend(folder.stmts.into_iter().map(T::from_stmt))
+                }
+            }
+        }
+
+        buf
+    }
 }
 
 struct ObjectSpread {
