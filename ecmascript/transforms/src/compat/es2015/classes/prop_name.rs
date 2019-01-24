11e3a823d230853449e1f5134fa22b20316461a9
강동윤 <kdy1@outlook.kr>
2019-01-24 09:56:41 +0900

    different variant for ident / str while handling property name

--- ecmascript/transforms/src/compat/es2015/classes/prop_name.rs
+++ ecmascript/transforms/src/compat/es2015/classes/prop_name.rs
@@ -5,6 +5,7 @@ use swc_common::{Span, Spanned};
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum HashKey {
+    Ident(JsWord),
     Str(JsWord),
     Num(OrderedFloat<f64>),
     /// Not for key merging
@@ -14,8 +15,8 @@ pub enum HashKey {
 impl From<&PropName> for HashKey {
     fn from(p: &PropName) -> Self {
         match *p {
-            PropName::Ident(Ident { sym: ref value, .. })
-            | PropName::Str(Str { ref value, .. }) => HashKey::Str(value.clone()),
+            PropName::Ident(Ident { ref sym, .. }) => HashKey::Ident(sym.clone()),
+            PropName::Str(Str { ref value, .. }) => HashKey::Str(value.clone()),
             PropName::Num(Number { value, .. }) => HashKey::Num(value.into()),
             PropName::Computed(ref expr) => HashKey::Computed(expr.span()),
         }
