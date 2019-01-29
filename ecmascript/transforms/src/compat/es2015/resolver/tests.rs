33639f6631332953e3de601ca65968573bc4ca93
강동윤 <kdy1@outlook.kr>
2019-01-29 14:59:50 +0900

    fixup! [WIP] resolver

--- ecmascript/transforms/src/compat/es2015/resolver/tests.rs
+++ ecmascript/transforms/src/compat/es2015/resolver/tests.rs
@@ -548,3 +548,55 @@ var Sub = function(_Foo) {
 }(Foo);
         "#
 );
+
+identical!(
+    regression_001,
+    "var sym = Symbol();
+
+class Foo {
+  [sym] () {
+    return 1;
+  }
+}
+
+class Bar extends Foo {
+  [sym] () {
+    return super[sym]() + 2;
+  }
+}
+
+var i = new Bar();
+
+expect(i[sym]()).toBe(3);"
+);
+
+identical!(
+    regression_002,
+    "var sym = Symbol();
+    var Foo = function() {
+        function Foo() {
+            _classCallCheck(this, Foo);
+        }
+        _createClass(Foo, [{
+                 key: sym, value: function() {
+                        return 1;
+                    } 
+            }]);
+        return Foo;
+    }();
+    var Bar = function(_Foo) {
+        _inherits(Bar, _Foo);
+        function Bar() {
+            _classCallCheck(this, Bar);
+            return _possibleConstructorReturn(this, _getPrototypeOf(Bar).apply(this, arguments));
+        }
+        _createClass(Bar, [{
+                 key: sym, value: function() {
+                        return _get(_getPrototypeOf(Bar.prototype), sym, this)() + 2;
+                    } 
+            }]);
+        return Bar;
+    }(Foo);
+    var i = new Bar();
+    expect(i[sym]()).toBe(3);"
+);
