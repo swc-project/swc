b99caacffc542137aab0ca2be21a5c2ba93b259b
강동윤 <kdy1@outlook.kr>
2019-02-04 23:00:09 +0900

    Remove amd & system_js pass

--- ecmascript/transforms/src/modules/mod.rs
+++ ecmascript/transforms/src/modules/mod.rs
@@ -1,7 +1,5 @@
-pub use self::{amd::amd, common_js::common_js, system_js::system_js, umd::umd};
+pub use self::{common_js::common_js, umd::umd};
 
-mod amd;
 mod common_js;
-mod system_js;
 mod umd;
 mod util;
