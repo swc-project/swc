93f47309cfdf76bc3b2796d80fb9655dc3c1a5e6
강동윤 <kdy1@outlook.kr>
2019-02-10 23:20:06 +0900

    Improve MoveMap

--- common/src/util/move_map.rs
+++ common/src/util/move_map.rs
@@ -15,6 +15,36 @@ pub trait MoveMap<T>: Sized {
 }
 
 impl<T> MoveMap<T> for Vec<T> {
+    /// This reduces binary size.
+    fn move_map<F>(mut self, mut f: F) -> Self
+    where
+        F: FnMut(T) -> T,
+    {
+        let mut read_i = 0;
+        let mut write_i = 0;
+        unsafe {
+            let old_len = self.len();
+            self.set_len(0); // make sure we just leak elements in case of panic
+
+            while read_i < old_len {
+                // move the read_i'th item out of the vector and map it
+                // to an iterator
+                let e = ptr::read(self.get_unchecked(read_i));
+                let e = f(e);
+                read_i += 1;
+
+                assert!(write_i < read_i);
+                ptr::write(self.get_unchecked_mut(write_i), e);
+                write_i += 1;
+            }
+
+            // write_i tracks the number of actually written new items.
+            self.set_len(write_i);
+        }
+
+        self
+    }
+
     fn move_flat_map<F, I>(mut self, mut f: F) -> Self
     where
         F: FnMut(T) -> I,
