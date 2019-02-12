8889f531de059cd2ed8cb3bf6bf964736e63e4ab
강동윤 <kdy1@outlook.kr>
2019-02-13 08:17:25 +0900

    Comment out `export import A = B`

--- ecmascript/transforms/src/typescript/mod.rs
+++ ecmascript/transforms/src/typescript/mod.rs
@@ -191,7 +191,7 @@ mod tests {
 }"
     );
 
-    to!(export_import, "export import A = B", "export { B as A }");
+    // to!(export_import, "export import A = B", "export { B as A }");
 
     to!(export_equals, "export = Foo", "export default Foo");
 }
