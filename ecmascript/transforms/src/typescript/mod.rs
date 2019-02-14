4cef38d6139edb0bb329a86c00aed1a125753faf
강동윤 <kdy1@outlook.kr>
2019-02-14 22:45:03 +0900

    Move tests to separate file

--- ecmascript/transforms/src/typescript/mod.rs
+++ ecmascript/transforms/src/typescript/mod.rs
@@ -9,6 +9,9 @@ use swc_common::{
     util::move_map::MoveMap, Fold, FoldWith, Spanned, SyntaxContext, VisitWith, DUMMY_SP,
 };
 
+#[cfg(test)]
+mod tests;
+
 /// Strips type annotations out.
 pub fn strip() -> impl Pass + Clone {
     Strip::default()
@@ -287,76 +290,4 @@ impl Fold<Expr> for Strip {
             _ => expr,
         }
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::strip;
-
-    macro_rules! to {
-        ($name:ident, $from:expr, $to:expr) => {
-            test!(
-                ::swc_ecma_parser::Syntax::Typescript(Default::default()),
-                |_| strip(),
-                $name,
-                $from,
-                $to
-            );
-        };
-    }
-
-    to!(
-        constructor_01,
-        "class Foo {
-    constructor(public readonly foo) {}
-}",
-        "class Foo {
-    constructor(foo) {
-        this.foo = foo;
-    }
-}"
-    );
-
-    to!(
-        constructor_02,
-        "class Foo {
-    constructor(readonly foo) {
-        this.bar = 1;
-    }
-}",
-        "class Foo {
-    constructor(foo) {
-        this.foo = foo;
-        this.bar = 1;
-    }
-}"
-    );
-
-    to!(export_import, "export import A = B", "export { B as A }");
-
-    to!(export_equals, "export = Foo", "export default Foo");
-
-    to!(
-        issue_196_01,
-        "export type Link = { key: string; text: string };",
-        ""
-    );
-
-    to!(
-        issue_196_02,
-        "type Link = { key: string; text: string };
-export { Link };",
-        ""
-    );
-
-    to!(
-        issue_196_03,
-        "type Link = { key: string; text: string };
-const Link = 'Boo';
-export { Link };",
-        "const Link = 'Boo';
-export { Link };"
-    );
-
-    // TODO: Test function / variable hoisting
 }
