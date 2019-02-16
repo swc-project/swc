53714bb60e3b9099bfdae3dc46a3c717594db6ea
강동윤 <kdy1@outlook.kr>
2019-02-16 14:16:42 +0900

    FIx #220

--- ecmascript/transforms/src/modules/import_analysis.rs
+++ ecmascript/transforms/src/modules/import_analysis.rs
@@ -3,6 +3,7 @@ use crate::util::State;
 use ast::*;
 use swc_common::{Fold, Visit, VisitWith};
 
+/// Inject required helpers methods **for** module transform passes.
 #[derive(Default, Clone)]
 pub struct ImportAnalyzer {
     scope: State<Scope>,
@@ -27,23 +28,36 @@ impl Fold<Module> for ImportAnalyzer {
     }
 }
 
-impl Visit<ModuleDecl> for ImportAnalyzer {
-    fn visit(&mut self, decl: &ModuleDecl) {
-        decl.visit_children(self);
+impl Visit<ExportAll> for ImportAnalyzer {
+    fn visit(&mut self, export: &ExportAll) {
+        self.scope
+            .value
+            .import_types
+            .entry(export.src.value.clone())
+            .and_modify(|v| *v = true);
+    }
+}
 
-        match *decl {
-            ModuleDecl::ExportAll(ref export) => {
-                self.scope
-                    .value
-                    .import_types
-                    .entry(export.src.value.clone())
-                    .and_modify(|v| *v = true);
+impl Visit<NamedExport> for ImportAnalyzer {
+    fn visit(&mut self, export: &NamedExport) {
+        for &NamedExportSpecifier {
+            ref orig,
+            ref exported,
+            ..
+        } in export.specifiers.iter().map(|e| match *e {
+            ExportSpecifier::Named(ref e) => e,
+            _ => unreachable!("export default from 'foo'; should be removed by previous pass"),
+        }) {
+            let is_import_default = orig.sym == js_word!("default");
+
+            if let Some(ref src) = export.src {
+                if is_import_default {
+                    self.scope
+                        .import_types
+                        .entry(src.value.clone())
+                        .or_insert(false);
+                }
             }
-            ModuleDecl::ExportDecl(..) => {}
-            ModuleDecl::ExportDefaultDecl(..) => {}
-            ModuleDecl::ExportDefaultExpr(..) => {}
-            ModuleDecl::ExportNamed(..) => {}
-            _ => {}
         }
     }
 }
