601f0feb146a20bc519f178fd54c93c720b843df
강동윤 <kdy1@outlook.kr>
2019-02-20 12:10:28 +0900

    Hoist functions in block scoped function pass.
    Fix #245.

--- ecmascript/transforms/src/compat/es2015/block_scoped_fn.rs
+++ ecmascript/transforms/src/compat/es2015/block_scoped_fn.rs
@@ -4,38 +4,70 @@ use swc_common::{Fold, FoldWith, Spanned, DUMMY_SP};
 #[derive(Clone, Copy)]
 pub struct BlockScopedFns;
 
-impl Fold<Stmt> for BlockScopedFns {
-    fn fold(&mut self, stmt: Stmt) -> Stmt {
-        // This is to preserve function Class()
-        if stmt.span().is_dummy() {
-            return stmt;
+impl Fold<Vec<Stmt>> for BlockScopedFns {
+    fn fold(&mut self, items: Vec<Stmt>) -> Vec<Stmt> {
+        let mut stmts = Vec::with_capacity(items.len());
+        let mut extra_stmts = Vec::with_capacity(items.len());
+
+        for stmt in items {
+            // This is to preserve function Class()
+            if stmt.span().is_dummy() {
+                extra_stmts.push(stmt)
+            } else {
+                match stmt {
+                    Stmt::Decl(Decl::Fn(decl)) => stmts.push(Stmt::Decl(Decl::Var(VarDecl {
+                        span: DUMMY_SP,
+                        kind: VarDeclKind::Let,
+                        decls: vec![VarDeclarator {
+                            span: DUMMY_SP,
+                            name: Pat::Ident(decl.ident.clone()),
+                            init: Some(box Expr::Fn(FnExpr {
+                                ident: Some(decl.ident),
+                                function: decl.function,
+                            })),
+                            definite: false,
+                        }],
+                        declare: false,
+                    }))),
+                    _ => extra_stmts.push(stmt.fold_children(self)),
+                }
+            }
         }
 
-        match stmt {
-            Stmt::Decl(Decl::Fn(decl)) => {
-                return Stmt::Decl(Decl::Var(VarDecl {
-                    span: DUMMY_SP,
-                    kind: VarDeclKind::Let,
-                    decls: vec![VarDeclarator {
-                        span: DUMMY_SP,
-                        name: Pat::Ident(decl.ident.clone()),
-                        init: Some(box Expr::Fn(FnExpr {
-                            ident: Some(decl.ident),
-                            function: decl.function,
-                        })),
-                        definite: false,
-                    }],
-                    declare: false,
-                }));
-            }
-            _ => stmt.fold_children(self),
-        }
+        stmts.append(&mut extra_stmts);
+
+        stmts
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    test!(
+        ::swc_ecma_parser::Syntax::default(),
+        |_| BlockScopedFns,
+        hoisting,
+        r#"
+{
+    function fn1() { fn2(); }
+
+    fn1();
+
+    function fn2() { }
+}
+"#,
+        r#"
+{
+    let fn1 = function fn1() {
+        fn2();
+    };
+    let fn2 = function fn2() {
+    };
+    fn1();
+}
+"#
+    );
 
     test!(
         ::swc_ecma_parser::Syntax::default(),
