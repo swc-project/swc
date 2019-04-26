71150af582fc27374033b2bf5b5b712e0ba97f1d
강동윤 <kdy1@outlook.kr>
2019-04-26 22:17:45 +0900

    fixup! Prevent hygiene from changing key in object pattern

--- ecmascript/transforms/src/hygiene/ops.rs
+++ ecmascript/transforms/src/hygiene/ops.rs
@@ -166,30 +166,6 @@ impl Fold<Expr> for VarFolder<'_, '_> {
     }
 }
 
-/// Preserve key of properties.
-impl<'a> Fold<AssignPatProp> for Operator<'a> {
-    fn fold(&mut self, p: AssignPatProp) -> AssignPatProp {
-        match p.value {
-            Some(value) => {
-                return AssignPatProp {
-                    value: Some(value.fold_children(self)),
-                    ..p
-                }
-            }
-            None => {}
-        }
-
-        match self.rename_ident(p.key.clone()) {
-            Ok(renamed) => AssignPatProp {
-                key: p.key,
-                value: Some(box Expr::Ident(renamed)),
-                ..p
-            },
-            Err(i) => p,
-        }
-    }
-}
-
 /// Preserves key
 impl<'a> Fold<Prop> for Operator<'a> {
     fn fold(&mut self, prop: Prop) -> Prop {
@@ -208,6 +184,16 @@ impl<'a> Fold<Prop> for Operator<'a> {
                 }
             }
             _ => prop.fold_children(self),
+        }
+    }
+}
+
+/// Preserve key in object properties.
+impl<'a> Fold<KeyValueProp> for Operator<'a> {
+    fn fold(&mut self, p: KeyValueProp) -> KeyValueProp {
+        KeyValueProp {
+            value: p.value.fold_with(self),
+            ..p
         }
     }
 }
