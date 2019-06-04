941700db323814b1dd0c8419b10f4965df754526
강동윤 <kdy1@outlook.kr>
2019-06-04 14:54:31 +0900

    remove_falst()

--- typescript/checker/src/analyzer/expr.rs
+++ typescript/checker/src/analyzer/expr.rs
@@ -1,4 +1,5 @@
 use super::Analyzer;
+use swc_common::Span;
 use swc_ecma_ast::*;
 
 impl Analyzer {
@@ -19,7 +20,101 @@ impl Analyzer {
                 lit: TsLit::Number(v),
             })),
 
+            Expr::TsAs(TsAsExpr { ref type_ann, .. }) => Some(*type_ann.clone()),
+            Expr::TsTypeCast(TsTypeCastExpr { ref type_ann, .. }) => {
+                Some(*type_ann.type_ann.clone())
+            }
+
+            Expr::TsNonNull(TsNonNullExpr { ref expr, .. }) => {
+                self.type_of(expr).map(|ty| ty.remove_falsy())
+            }
+
             _ => None,
         }
+    }
+}
+
+trait RemoveTypes {
+    /// Removes falsy values from `self`.
+    fn remove_falsy(self) -> TsType;
+}
+
+fn never_ty(span: Span) -> TsType {
+    TsType::TsKeywordType(TsKeywordType {
+        span,
+        kind: TsKeywordTypeKind::TsNeverKeyword,
+    })
+}
+
+impl RemoveTypes for TsType {
+    fn remove_falsy(self) -> TsType {
+        match self {
+            TsType::TsUnionOrIntersectionType(n) => n.remove_falsy().into(),
+            TsType::TsKeywordType(TsKeywordType { kind, span }) => match kind {
+                TsKeywordTypeKind::TsUndefinedKeyword | TsKeywordTypeKind::TsNullKeyword => {
+                    never_ty(span)
+                }
+                _ => self,
+            },
+            _ => self,
+        }
+    }
+}
+
+impl RemoveTypes for TsUnionOrIntersectionType {
+    fn remove_falsy(self) -> TsType {
+        match self {
+            TsUnionOrIntersectionType::TsIntersectionType(n) => n.remove_falsy().into(),
+            TsUnionOrIntersectionType::TsUnionType(n) => n.remove_falsy().into(),
+        }
+    }
+}
+
+impl RemoveTypes for TsIntersectionType {
+    fn remove_falsy(self) -> TsType {
+        let types = self
+            .types
+            .into_iter()
+            .map(|ty| ty.remove_falsy())
+            .map(Box::new)
+            .collect::<Vec<_>>();
+        if types.iter().any(|ty| is_never(&ty)) {
+            return TsType::TsKeywordType(TsKeywordType {
+                span: self.span,
+                kind: TsKeywordTypeKind::TsNeverKeyword,
+            });
+        }
+
+        TsType::TsUnionOrIntersectionType(TsIntersectionType { types, ..self }.into())
+    }
+}
+
+impl RemoveTypes for TsUnionType {
+    fn remove_falsy(self) -> TsType {
+        let types = self
+            .types
+            .into_iter()
+            .map(|ty| ty.remove_falsy())
+            .filter(|ty| !is_never(&ty))
+            .map(Box::new)
+            .collect();
+
+        TsType::TsUnionOrIntersectionType(TsUnionType { types, ..self }.into())
+    }
+}
+
+impl RemoveTypes for Box<TsType> {
+    fn remove_falsy(self) -> TsType {
+        (*self).remove_falsy()
+    }
+}
+
+fn is_never(ty: &TsType) -> bool {
+    match *ty {
+        TsType::TsKeywordType(TsKeywordType {
+            kind: TsKeywordTypeKind::TsNeverKeyword,
+            ..
+        }) => false,
+        _ => true,
     }
 }
