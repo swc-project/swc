6934591fa2060c2c1aefd09eb1473705abe6914c
강동윤 <kdy1@outlook.kr>
2019-06-05 22:00:10 +0900

    Fix resolver

--- typescript/checker/src/resolver.rs
+++ typescript/checker/src/resolver.rs
@@ -4,7 +4,7 @@ use swc_atoms::JsWord;
 
 ///
 pub trait Resolve: Send + Sync {
-    fn resolve(&self, base: PathBuf, src: &JsWord) -> Result<PathBuf, Error>;
+    fn resolve(&self, cur_file: PathBuf, src: &JsWord) -> Result<PathBuf, Error>;
 }
 
 pub struct Resolver {
@@ -21,8 +21,16 @@ impl Resolver {
 }
 
 impl Resolve for Resolver {
-    fn resolve(&self, base: PathBuf, src: &JsWord) -> Result<PathBuf, Error> {
+    fn resolve(&self, cur_file: PathBuf, src: &JsWord) -> Result<PathBuf, Error> {
         // TODO: Handle error gracefully.
+
+        let base = match cur_file.file_name() {
+            Some(..) => cur_file
+                .parent()
+                .expect("file_name() is not null, so there should be parent")
+                .to_path_buf(),
+            None => cur_file,
+        };
 
         let p = self
             .r
