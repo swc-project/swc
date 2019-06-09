cc7c016971b9012b6cb82e5a16cd2930425471c4
강동윤 <kdy1@outlook.kr>
2019-06-09 21:47:44 +0900

    Print source on unimpl

--- typescript/checker/src/analyzer/mod.rs
+++ typescript/checker/src/analyzer/mod.rs
@@ -322,9 +322,12 @@ impl Visit<VarDecl> for Analyzer<'_, '_> {
 impl Analyzer<'_, '_> {
     fn try_assign(&mut self, lhs: &PatOrExpr, ty: Cow<TsType>) {
         match *lhs {
-            PatOrExpr::Expr(ref expr) | PatOrExpr::Pat(box Pat::Expr(ref expr)) => {
-                unimplemented!("assign: {:?} = {:?}", expr, ty)
-            }
+            PatOrExpr::Expr(ref expr) | PatOrExpr::Pat(box Pat::Expr(ref expr)) => unimplemented!(
+                "assign: {:?} = {:?}\nFile: {}",
+                expr,
+                ty,
+                self.path.display()
+            ),
 
             PatOrExpr::Pat(ref pat) => {
                 // Update variable's type
