706eed9bcf8f58a9c99b9741d1e7fbd36690f665
강동윤 <kdy1997.dev@gmail.com>
2019-09-18 22:36:03 +0900

    parser: typescript const assertion

--- ecmascript/parser/src/parser/util.rs
+++ ecmascript/parser/src/parser/util.rs
@@ -220,6 +220,8 @@ pub(super) trait ExprExt {
             | Expr::TsAs(TsAsExpr { ref expr, .. }) => {
                 expr.is_valid_simple_assignment_target(strict)
             }
+
+            Expr::TsConstAssertion(..) => false,
         }
     }
 }
