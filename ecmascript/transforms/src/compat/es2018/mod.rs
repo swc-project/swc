25b5da47d600165970dc747ad35f1d9708eeb17d
강동윤 <kdy1997.dev@gmail.com>
2019-09-22 19:29:11 +0900

    Implement optional catch binding
    Closes #411.

--- ecmascript/transforms/src/compat/es2018/mod.rs
+++ ecmascript/transforms/src/compat/es2018/mod.rs
@@ -1,9 +1,12 @@
-pub use self::object_rest_spread::object_rest_spread;
-
+pub use self::{
+    object_rest_spread::object_rest_spread, optional_catch_binding::optional_catch_binding,
+};
 use crate::pass::Pass;
+use ast::Module;
 
 mod object_rest_spread;
+mod optional_catch_binding;
 
 pub fn es2018() -> impl Pass {
-    object_rest_spread()
+    chain_at!(Module, object_rest_spread(), optional_catch_binding())
 }
