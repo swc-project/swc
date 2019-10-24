71b1572946aea1ca3a478dde685adee634a9db7f
강동윤 <kdy1997.dev@gmail.com>
2019-10-24 21:04:49 +0900

    fmt

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@ use ecmascript::{
     ast::{Expr, Module, ModuleItem, Stmt},
     parser::{Parser, Session as ParseSess, SourceFileInput, Syntax},
     transforms::{
-        chain_at, const_modules,  modules,
+        chain_at, const_modules, modules,
         pass::{noop, Optional, Pass},
         proposals::{class_properties, decorators, export},
         react, resolver, simplifier, typescript, InlineGlobals,
