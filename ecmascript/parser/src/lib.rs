d7fa5d995aeb3253304c2c06781b3c57d7dd84e8
강동윤 <kdy1997.dev@gmail.com>
2019-11-06 12:38:50 +0900

    Done

--- ecmascript/parser/src/lib.rs
+++ ecmascript/parser/src/lib.rs
@@ -31,11 +31,11 @@
 //!     errors::{ColorConfig, Handler},
 //!     FileName, FilePathMapping, SourceMap,
 //! };
-//! use swc_ecma_parser::{Parser, Session, SourceFileInput, Syntax};
+//! use swc_ecma_parser::{lexer::Lexer, Parser, Session, SourceFileInput, Syntax};
 //!
 //! fn main() {
 //!     swc_common::GLOBALS.set(&swc_common::Globals::new(), || {
-//!         let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+//!         let cm: Arc<SourceMap> = Default::default();
 //!         let handler =
 //!             Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
 //!
@@ -46,17 +46,20 @@
 //!         //     .load_file(Path::new("test.js"))
 //!         //     .expect("failed to load test.js");
 //!
+//!
 //!         let fm = cm.new_source_file(
 //!             FileName::Custom("test.js".into()),
 //!             "function foo() {}".into(),
 //!         );
-//!
-//!         let mut parser = Parser::new(
+//!         let lexer = Lexer::new(
 //!             session,
 //!             Syntax::Es(Default::default()),
 //!             SourceFileInput::from(&*fm),
-//!             None, // Disable comments
+//!             None,
 //!         );
+//!
+//!         let mut parser = Parser::new(session, lexer);
+//!
 //!
 //!         let _module = parser
 //!             .parse_module()
