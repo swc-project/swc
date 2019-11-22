51ba429b52bb598c9643f40b5c6de45ee173bdba
강동윤 <kdy1997.dev@gmail.com>
2019-11-23 02:10:48 +0900

    Optimize

--- ecmascript/parser/src/parser/typescript.rs
+++ ecmascript/parser/src/parser/typescript.rs
@@ -2023,17 +2023,21 @@ impl<'a, I: Tokens> Parser<'a, I> {
         &mut self,
         start: BytePos,
     ) -> PResult<'a, Option<ArrowExpr>> {
-        let res = self.try_parse_ts(|p| {
-            let type_params = p.parse_ts_type_params()?;
-            // Don't use overloaded parseFunctionParams which would look for "<" again.
-            expect!('(');
-            let params = p.parse_formal_params()?;
-            expect!(')');
-            let return_type = p.try_parse_ts_type_or_type_predicate_ann()?;
-            expect!("=>");
+        let res = if is_one_of!('<', JSXTagStart) {
+            self.try_parse_ts(|p| {
+                let type_params = p.parse_ts_type_params()?;
+                // Don't use overloaded parseFunctionParams which would look for "<" again.
+                expect!('(');
+                let params = p.parse_formal_params()?;
+                expect!(')');
+                let return_type = p.try_parse_ts_type_or_type_predicate_ann()?;
+                expect!("=>");
 
-            Ok(Some((type_params, params, return_type)))
-        });
+                Ok(Some((type_params, params, return_type)))
+            })
+        } else {
+            None
+        };
 
         let (type_params, params, return_type) = match res {
             Some(v) => v,
