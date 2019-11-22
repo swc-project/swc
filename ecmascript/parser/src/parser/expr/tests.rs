c10699773b8effd695d4061c4a55cc1dbf929ac5
강동윤 <kdy1997.dev@gmail.com>
2019-11-23 01:42:08 +0900

    Add a benchmark of member expression

--- ecmascript/parser/src/parser/expr/tests.rs
+++ ecmascript/parser/src/parser/expr/tests.rs
@@ -411,7 +411,7 @@ fn issue_380() {
 }
 
 #[bench]
-fn bench_new_expr(b: &mut Bencher) {
+fn bench_ts_new_expr(b: &mut Bencher) {
     bench_parser(
         b,
         "new Foo()",
@@ -421,4 +421,33 @@ fn bench_new_expr(b: &mut Bencher) {
             Ok(())
         },
     );
+}
+
+#[bench]
+fn bench_es_new_expr(b: &mut Bencher) {
+    bench_parser(b, "new Foo()", Syntax::Es(Default::default()), |p| {
+        black_box(p.parse_expr()?);
+        Ok(())
+    });
+}
+
+#[bench]
+fn bench_ts_member_expr(b: &mut Bencher) {
+    bench_parser(
+        b,
+        "a.b.c.d.e.f",
+        Syntax::Typescript(Default::default()),
+        |p| {
+            black_box(p.parse_expr()?);
+            Ok(())
+        },
+    );
+}
+
+#[bench]
+fn bench_es_member_expr(b: &mut Bencher) {
+    bench_parser(b, "a.b.c.d.e.f", Syntax::Es(Default::default()), |p| {
+        black_box(p.parse_expr()?);
+        Ok(())
+    });
 }
