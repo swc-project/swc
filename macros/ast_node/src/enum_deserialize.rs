0982d3381c80ab3d619831eb576ce81373527fad
강동윤 <kdy1997.dev@gmail.com>
2019-11-30 00:58:32 +0900

    Check error

--- macros/ast_node/src/enum_deserialize.rs
+++ macros/ast_node/src/enum_deserialize.rs
@@ -87,7 +87,21 @@ pub fn expand(
                                 VariantFieldType: &field_type,
                             },
                             {
-                                if let Ok(v) = std::result::Result::map(
+                                //                                if let Ok(v) =
+                                // std::result::Result::map(
+                                //                                    <VariantFieldType as
+                                // serde::Deserialize>::deserialize(
+                                //
+                                // serde::private::de::ContentRefDeserializer::<D::Error>::new(
+                                //                                            &content,
+                                //                                        ),
+                                //                                    ),
+                                //                                    Enum::Variant,
+                                //                                ) {
+                                //                                    return Ok(v);
+                                //                                }
+
+                                match std::result::Result::map(
                                     <VariantFieldType as serde::Deserialize>::deserialize(
                                         serde::private::de::ContentRefDeserializer::<D::Error>::new(
                                             &content,
@@ -95,7 +109,8 @@ pub fn expand(
                                     ),
                                     Enum::Variant,
                                 ) {
-                                    return Ok(v);
+                                    Ok(v) => return Ok(v),
+                                    Err(err) => return Err(err),
                                 }
                             }
                         ))
