d0da951f084b75f5e394bf21e40d5ac0275826b9
강동윤 <kdy1997.dev@gmail.com>
2019-11-30 09:37:12 +0900

    Add some serialization test

---
+++ tests/serde.rs
@@ -0,0 +1,71 @@
+use std::path::Path;
+use swc::{
+    common::CM,
+    ecmascript::{
+        ast::Module,
+        parser::{lexer::Lexer, PResult, Parser, Session, Syntax},
+    },
+    SourceFileInput,
+};
+use testing::NormalizedOutput;
+
+fn with_parser<F, Ret>(file_name: &str, f: F) -> Result<Ret, NormalizedOutput>
+where
+    F: for<'a> FnOnce(&mut Parser<'a, Lexer<'a, SourceFileInput<'_>>>) -> PResult<'a, Ret>,
+{
+    let output = ::testing::run_test(false, |cm, handler| {
+        CM.set(&cm, || {
+            let fm = cm
+                .load_file(Path::new(file_name))
+                .unwrap_or_else(|e| panic!("failed to load {}: {}", file_name, e));
+
+            let res = f(&mut Parser::new(
+                Session { handler: &handler },
+                if file_name.ends_with(".ts") {
+                    Syntax::Typescript(Default::default())
+                } else {
+                    Syntax::default()
+                },
+                (&*fm).into(),
+                None,
+            ))
+            .map_err(|mut e| {
+                e.emit();
+            });
+
+            if handler.has_errors() {
+                return Err(());
+            }
+
+            res
+        })
+    });
+
+    output
+}
+
+#[test]
+fn color_js() {
+    with_parser("tests/serde/colors.js", |p| {
+        let m = p.parse_module()?;
+
+        let s = serde_json::to_string(&m).expect("failed to serialize");
+        let _: Module = serde_json::from_str(&s).expect("failed to deserialize");
+
+        Ok(())
+    })
+    .expect("failed");
+}
+
+#[test]
+fn color_ts() {
+    with_parser("tests/serde/colors.ts", |p| {
+        let m = p.parse_module()?;
+
+        let s = serde_json::to_string(&m).expect("failed to serialize");
+        let _: Module = serde_json::from_str(&s).expect("failed to deserialize");
+
+        Ok(())
+    })
+    .expect("failed");
+}
---
+++ tests/serde/colors.js
@@ -0,0 +1,69 @@
+'use strict';
+/**
+ * Extract red color out of a color integer:
+ *
+ * 0x00DEAD -> 0x00
+ *
+ * @param  {Number} color
+ * @return {Number}
+ */
+function red( color )
+{
+    let foo = 3.14;
+    return color >> 16;
+}
+/**
+ * Extract green out of a color integer:
+ *
+ * 0x00DEAD -> 0xDE
+ *
+ * @param  {Number} color
+ * @return {Number}
+ */
+function green( color )
+{
+    return ( color >> 8 ) & 0xFF;
+}
+/**
+ * Extract blue color out of a color integer:
+ *
+ * 0x00DEAD -> 0xAD
+ *
+ * @param  {Number} color
+ * @return {Number}
+ */
+function blue( color )
+{
+    return color & 0xFF;
+}
+/**
+ * Converts an integer containing a color such as 0x00DEAD to a hex
+ * string, such as '#00DEAD';
+ *
+ * @param  {Number} int
+ * @return {String}
+ */
+function intToHex( int )
+{
+    const mask = '#000000';
+    const hex = int.toString( 16 );
+    return mask.substring( 0, 7 - hex.length ) + hex;
+}
+/**
+ * Converts a hex string containing a color such as '#00DEAD' to
+ * an integer, such as 0x00DEAD;
+ *
+ * @param  {Number} num
+ * @return {String}
+ */
+function hexToInt( hex )
+{
+    return parseInt( hex.substring( 1 ), 16 );
+}
+module.exports = {
+    red,
+    green,
+    blue,
+    intToHex,
+    hexToInt,
+};
