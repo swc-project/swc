cb384ed088f81b1d58a4d42dac157e6983c0be5e
강동윤 <kdy1997.dev@gmail.com>
2019-12-03 12:41:26 +0900

    Fix del.py

--- ecmascript/transforms/scripts/del.py
+++ ecmascript/transforms/scripts/del.py
@@ -3,36 +3,22 @@
 
 from os import listdir, remove
 from os.path import isfile, join
+import shutil
 
-files = [f for f in listdir('./fixtures') if isfile(join('./fixtures', f))]
+def search(dir):
+    print('Searching ', dir)
+    shouldDelete = True;
+    for f in listdir(dir):
+        p = join(dir, f);
+        if isfile(p):
+            if f == 'options.json':
+                return False
+        else:
+            if not search(p):
+                shouldDelete = False
+    if shouldDelete:
+        shutil.rmtree(dir, ignore_errors=True)
+    return True
 
-fm = {}
 
-for f in files:
-    name = join('./fixtures', f)
-    test_name, file_type = f.rsplit('-', 1)
-    if file_type == 'input.mjs':
-        file_type = 'input.js'
-    if file_type == 'input.mjsz':
-        file_type = 'input.js'
-    if file_type == 'output.mjs':
-        file_type = 'output.js'
-    if file_type == 'output.mjsz':
-        file_type = 'output.js'
-
-    if file_type == 'exec.js':
-        test_name = '{}_exec'.format(test_name)
-
-    if not test_name in fm:
-        fm[test_name] = {}
-    with open(name, "r") as f:
-        fm[test_name][file_type] = f.read()
-
-for name in fm:
-    m = fm[name]
-    name = name.replace('-', '_')
-
-    if not 'options.json' in m:
-        for k in m.keys():
-            remove(m[k])
-        continue
+search('./fixtures')
