4eb6bdf4fd091148b2819bb43c741464259cb60d
강동윤 <kdy1997.dev@gmail.com>
2019-12-09 20:19:43 +0900

    Change message of iteration failure

--- ecmascript/transforms/src/compat/es2015/destructuring/tests.rs
+++ ecmascript/transforms/src/compat/es2015/destructuring/tests.rs
@@ -677,12 +677,12 @@ test_exec!(
 expect(
   () => {
     var [foo, bar] = undefined;
-  }).toThrow("Invalid attempt to destructure non-iterable instance");
+  }).toThrow();
 
 expect(
   () => {
     var foo = [ ...undefined ];
-  }).toThrow("Invalid attempt to spread non-iterable instance");
+  }).toThrow();
 
 "#
 );
