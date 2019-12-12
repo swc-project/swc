5076249c907f53235778d6b44d6b83d433b88829
강동윤 <kdy1997.dev@gmail.com>
2019-12-13 08:38:46 +0900

    fixup! Add a test for #503

---
+++ ecmascript/parser/examples/typescript.rs
@@ -0,0 +1,48 @@
+use swc_common;
+
+use std::sync::Arc;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    FileName, SourceMap,
+};
+use swc_ecma_parser::{lexer::Lexer, Capturing, Parser, Session, SourceFileInput, Syntax};
+
+fn main() {
+    swc_common::GLOBALS.set(&swc_common::Globals::new(), || {
+        let cm: Arc<SourceMap> = Default::default();
+        let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+
+        let session = Session { handler: &handler };
+
+        // Real usage
+        // let fm = cm
+        //     .load_file(Path::new("test.js"))
+        //     .expect("failed to load test.js");
+
+        let fm = cm.new_source_file(
+            FileName::Custom("test.js".into()),
+            "interface Foo {}".into(),
+        );
+
+        let lexer = Lexer::new(
+            session,
+            Syntax::Typescript(Default::default()),
+            Default::default(),
+            SourceFileInput::from(&*fm),
+            None,
+        );
+
+        let capturing = Capturing::new(lexer);
+
+        let mut parser = Parser::new_from(session, capturing);
+
+        let _module = parser
+            .parse_typescript_module()
+            .map_err(|mut e| {
+                e.emit();
+            })
+            .expect("Failed to parse module.");
+
+        println!("Tokens: {:?}", parser.input().take());
+    });
+}
