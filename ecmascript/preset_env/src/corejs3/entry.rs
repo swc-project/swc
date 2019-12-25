7b047abc0522897595331d213c680bfab9e19ac1
강동윤 <kdy1997.dev@gmail.com>
2019-12-25 23:44:22 +0900

    add

--- ecmascript/preset_env/src/corejs3/entry.rs
+++ ecmascript/preset_env/src/corejs3/entry.rs
@@ -1,6 +1,8 @@
 use fxhash::FxHashMap;
 use once_cell::sync::Lazy;
 use swc_atoms::JsWord;
+use swc_common::{Visit, VisitWith};
+use swc_ecma_ast::*;
 
 static ENTRIES: Lazy<FxHashMap<String, Vec<String>>> = Lazy::new(|| {
     serde_json::from_str(include_str!("entries.json"))
@@ -18,4 +20,23 @@ impl Entry {
             imports: Default::default(),
         }
     }
+
+    /// Add imports
+    fn add(&mut self, src: &str) {
+        if let Some(features) = ENTRIES.get(src) {
+            for f in &**features {
+                self.imports.push(format!("core-js/modules/{}", f).into());
+            }
+        }
+    }
 }
+
+impl Visit<ImportDecl> for Entry {
+    fn visit(&mut self, i: &ImportDecl) {
+        i.visit_children(self);
+
+        //        if is_corejs_src(&i.src.value) {}
+    }
+}
+
+//fn is_corejs_src(s: &str) -> bool {}
