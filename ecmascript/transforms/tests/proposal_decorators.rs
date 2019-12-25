823b114d2f9d1a748e078fadc7dbfb126469762b
강동윤 <kdy1997.dev@gmail.com>
2019-12-25 17:20:54 +0900

    Update a reference

--- ecmascript/transforms/tests/proposal_decorators.rs
+++ ecmascript/transforms/tests/proposal_decorators.rs
@@ -2846,17 +2846,16 @@ class B {
 }
 "#,
     r#"
-var _class, _class2;
-
-let A = (_class2 = class A {
-  foo() {}
-
-}, (_applyDecoratedDescriptor(_class2.prototype, "foo", [dec], Object.getOwnPropertyDescriptor(_class2.prototype, "foo"), _class2.prototype)), _class2);
-export { A as default };
-let B = (_class = class B {
-  foo() {}
-
-}, (_applyDecoratedDescriptor(_class.prototype, "foo", [dec], Object.getOwnPropertyDescriptor(_class.prototype, "foo"), _class.prototype)), _class);
+var _class, _class1;
+let A = ((_class = class A{
+     foo() {
+    }
+}) || _class, _applyDecoratedDescriptor(_class.prototype, 'foo', [dec], Object.getOwnPropertyDescriptor(_class.prototype, 'foo'), _class.prototype), _class);
+let B = ((_class1 = class B{
+     foo() {
+    }
+}) || _class1, _applyDecoratedDescriptor(_class1.prototype, 'foo', [dec], Object.getOwnPropertyDescriptor(_class1.prototype, 'foo'), _class1.prototype), _class1);
+export { A as default }
 
 "#
 );
