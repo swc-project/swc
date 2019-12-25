2d9f012600e2bac531173830b7bfbc51549eac3b
강동윤 <kdy1997.dev@gmail.com>
2019-12-25 21:47:23 +0900

    Update a ref

--- ecmascript/preset_env/tests/fixtures/corejs3/usage-destructuring-params/output.mjs
+++ ecmascript/preset_env/tests/fixtures/corejs3/usage-destructuring-params/output.mjs
@@ -2,6 +2,6 @@ import "core-js/modules/es.array.iterator";
 import "core-js/modules/es.string.iterator";
 import "core-js/modules/web.dom-collections.iterator";
 
-function a(_ref) {
-  var [b, c] = _ref;
+function a(param) {
+  var [b, c] = param;
 }
