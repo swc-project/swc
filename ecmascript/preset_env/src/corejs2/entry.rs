9747b505b4f1eb82277afded81f6294076fc1481
강동윤 <kdy1997.dev@gmail.com>
2019-12-27 00:16:20 +0900

    core-js@2: entry: default

--- ecmascript/preset_env/src/corejs2/entry.rs
+++ ecmascript/preset_env/src/corejs2/entry.rs
@@ -1,7 +1,9 @@
 use super::builtin::BUILTINS;
 use crate::{version::should_enable, Version, Versions};
 use fxhash::FxHashSet;
-use swc_atoms::JsWord;
+use swc_atoms::{js_word, JsWord};
+use swc_common::{Fold, FoldWith, DUMMY_SP};
+use swc_ecma_ast::*;
 
 #[derive(Debug)]
 pub struct Entry {
@@ -12,32 +14,82 @@ pub struct Entry {
 
 impl Entry {
     pub fn new(target: Versions) -> Self {
-        Entry {
+        let is_any_target = target.is_any_target();
+        let is_web_target = target.iter().any(|(k, v)| {
+            if k == "node" {
+                return false;
+            }
+
+            v.is_some()
+        });
+
+        let mut v = Entry {
             is_any_target: target.is_any_target(),
             target,
             imports: Default::default(),
+        };
+        if is_any_target || is_web_target {
+            v.imports.insert("core-js/modules/web.timers".into());
+            v.imports.insert("core-js/modules/web.immediate".into());
+            v.imports.insert("core-js/modules/web.dom.iterable".into());
         }
+
+        v
     }
 
     /// Add imports.
     /// Returns true if it's replaced.
-    fn add(&mut self, src: &str) -> bool {
+    fn add_all(&mut self, src: &str) -> bool {
         let Entry {
             is_any_target,
             target,
             ..
         } = self;
 
+        if src != "@swc/polyfill" {
+            return false;
+        }
+
         if !*is_any_target {
-            if let Some(feature) = BUILTINS.get(src) {
-                if !should_enable(*target, *feature, true) {
-                    return false;
-                }
+            for (feature, version) in BUILTINS.iter() {
+                self.add_inner(&feature, *version);
             }
         }
 
-        self.imports
-            .insert(format!("core-js/modules/{}", src).into());
         true
+    }
+
+    fn add(&mut self, feature: &str) {
+        if let Some(version) = BUILTINS.get(feature) {
+            self.add_inner(feature, *version);
+        }
+    }
+
+    fn add_inner(&mut self, feature: &str, version: Versions) {
+        if should_enable(self.target, version, true) {
+            self.imports
+                .insert(format!("core-js/modules/{}", feature).into());
+        }
+    }
+}
+
+impl Fold<ImportDecl> for Entry {
+    fn fold(&mut self, i: ImportDecl) -> ImportDecl {
+        let i: ImportDecl = i.fold_children(self);
+
+        let remove = i.specifiers.is_empty() && self.add_all(&i.src.value);
+
+        if remove {
+            ImportDecl {
+                src: Str {
+                    span: DUMMY_SP,
+                    value: js_word!(""),
+                    ..i.src
+                },
+                ..i
+            }
+        } else {
+            i
+        }
     }
 }
