20265bb3d8b584068967f94ad445ebc07b922540
강동윤 <kdy1997.dev@gmail.com>
2020-01-01 05:47:44 +0900

    Fix tests

--- ecmascript/codegen/tests/references/9681f5d844d7acd0.js
+++ ecmascript/codegen/tests/references/9681f5d844d7acd0.js
@@ -1,22 +1,16 @@
 (a)=>b
 ; // 1 args
-
 (a, b)=>c
 ; // n args
-
 ()=>b
 ; // 0 args
-
 (a)=>(b)=>c
 ; // func returns func returns func
-
 (a)=>((b)=>c
     )
 ; // So these parens are dropped
-
 ()=>(b, c)=>d
 ; // func returns func returns func
-
 (a)=>{
     return b;
 };
