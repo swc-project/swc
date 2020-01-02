7812b8a765dd4055fa246d8a28bc960b109f972c
강동윤 <kdy1997.dev@gmail.com>
2020-01-02 13:36:28 +0900

    Update a test reference

--- ecmascript/transforms/tests/es2015_regenerator.rs
+++ ecmascript/transforms/tests/es2015_regenerator.rs
@@ -240,13 +240,12 @@ var _marked = regeneratorRuntime.mark(foo);
 
 function foo(a, b, c) {
   return regeneratorRuntime.wrap(function foo$(_ctx) {
-    while (1) {
+    while (1) 
       switch (_ctx.prev = _ctx.next) {
         case 0:
         case "end":
           return _ctx.stop();
       }
-    }
   }, _marked);
 }
 "#
