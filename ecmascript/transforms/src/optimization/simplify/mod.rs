63714246a52d4a4e09bff947f46d0304ff9a4bc6
강동윤 <kdy1997.dev@gmail.com>
2020-02-07 21:04:14 +0900

    Add some tests

--- ecmascript/transforms/src/optimization/simplify/mod.rs
+++ ecmascript/transforms/src/optimization/simplify/mod.rs
@@ -2,13 +2,20 @@
 pub use self::{branch::dead_branch_remover, expr::expr_simplifier, inlining::inlining};
 use crate::pass::RepeatedJsPass;
 use swc_common::{chain, pass::Repeat};
+use swc_ecma_utils::Id;
 
 mod branch;
 mod expr;
 mod inlining;
 
+#[derive(Debug, Default)]
+pub struct Config {
+    /// If this is [None], all exports are treated as used.
+    pub used: Option<Vec<Id>>,
+}
+
 /// Performs simplify-expr, inlining, remove-dead-branch and dce until nothing
 /// changes.
-pub fn simplifier() -> impl RepeatedJsPass + 'static {
+pub fn simplifier(_: Config) -> impl RepeatedJsPass + 'static {
     Repeat::new(chain!(expr_simplifier(), inlining(), dead_branch_remover()))
 }
--- ecmascript/transforms/tests/optimization_simplify.rs
+++ ecmascript/transforms/tests/optimization_simplify.rs
@@ -13,7 +13,7 @@ macro_rules! to {
     ($name:ident, $src:expr, $expected:expr) => {
         test!(
             Default::default(),
-            |_| chain!(resolver(), simplifier()),
+            |_| chain!(resolver(), simplifier(Default::default())),
             $name,
             $src,
             $expected
@@ -36,4 +36,25 @@ if (a) {
     const b = 2;
 }
 "
+);
+
+optimized_out!(issue_607, "let a");
+
+to!(
+    multi_run,
+    "
+let b = 2;
+
+let a = 1;
+if (b) { // Removed by first run of remove_dead_branch
+    a = 2; // It becomes `flat assignment` to a on second run of inlining
+}
+
+let c;
+if (a) { // Removed by second run of remove_dead_branch
+    c = 3; // It becomes `flat assignment` to c on third run of inlining.
+}
+console.log(c); // Prevent optimizing out.
+",
+    "console.log(3)"
 );
