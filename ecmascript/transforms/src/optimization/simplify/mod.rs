62cf269497ace93e61f223e66e2713d2c7b82817
강동윤 <kdy1997.dev@gmail.com>
2020-02-07 20:32:36 +0900

    move

--- ecmascript/transforms/src/optimization/simplify/mod.rs
+++ ecmascript/transforms/src/optimization/simplify/mod.rs
@@ -1,19 +1,14 @@
 //! Ported from closure compiler.
-pub use self::branch::dead_branch_remover;
-use self::expr::expr_simplifier;
+pub use self::{branch::dead_branch_remover, expr::expr_simplifier, inlining::inlining};
 use crate::pass::RepeatedJsPass;
 use swc_common::{chain, pass::Repeat};
 
 mod branch;
 mod expr;
-pub mod inlining;
+mod inlining;
 
 /// Performs simplify-expr, inlining, remove-dead-branch and dce until nothing
 /// changes.
 pub fn simplifier() -> impl RepeatedJsPass + 'static {
-    Repeat::new(chain!(
-        expr_simplifier(),
-        inlining::inlining(),
-        dead_branch_remover()
-    ))
+    Repeat::new(chain!(expr_simplifier(), inlining(), dead_branch_remover()))
 }
