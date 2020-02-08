c4e0c682aa81eeffd9f8bfd36f616074c9c75408
강동윤 <kdy1997.dev@gmail.com>
2020-02-08 21:17:10 +0900

    Add a test for dce

---
+++ ecmascript/transforms/tests/optimization_simplify_dce.rs
@@ -0,0 +1,65 @@
+#![feature(box_syntax)]
+#![feature(test)]
+#![feature(box_patterns)]
+#![feature(specialization)]
+
+use swc_common::chain;
+use swc_ecma_transforms::{optimization::simplify::dce::dce, resolver};
+
+#[macro_use]
+mod common;
+
+macro_rules! to {
+    ($name:ident, $src:expr, $expected:expr) => {
+        test!(
+            Default::default(),
+            |_| chain!(resolver(), dce(Default::default())),
+            $name,
+            $src,
+            $expected
+        );
+    };
+}
+
+macro_rules! optimized_out {
+    ($name:ident, $src:expr) => {
+        to!($name, $src, "");
+    };
+}
+
+macro_rules! noop {
+    ($name:ident, $src:expr) => {
+        to!($name, $src, $src);
+    };
+}
+
+optimized_out!(
+    single_pass,
+    "
+const a = 1;
+
+if (a) {
+    const b = 2;
+}
+"
+);
+
+optimized_out!(issue_607, "let a");
+
+noop!(
+    noop_1,
+    "
+let b = 2;
+
+let a = 1;
+if (b) {
+    a = 2;
+}
+
+let c;
+if (a) {
+    c = 3;
+}
+console.log(c);
+"
+);
