48de18bea32d5c753f7f7fe334dc68c34ee56af7
강동윤 <kdy1997.dev@gmail.com>
2020-02-08 11:09:39 +0900

    Simple inlining of flat variables

--- ecmascript/transforms/src/optimization/simplify/inlining/mod.rs
+++ ecmascript/transforms/src/optimization/simplify/inlining/mod.rs
@@ -1,9 +1,12 @@
 use crate::{pass::RepeatedJsPass, scope::ScopeKind};
 use fxhash::FxHashMap;
-use std::{borrow::Cow, cell::Cell};
+use std::{
+    borrow::Cow,
+    cell::{Cell, RefCell},
+};
 use swc_common::{
     pass::{CompilerPass, Repeated},
-    Fold, FoldWith,
+    Fold, FoldWith, Visit, VisitWith,
 };
 use swc_ecma_ast::*;
 use swc_ecma_utils::{ident::IdentLike, Id, StmtLike};
@@ -59,6 +62,45 @@ struct Scope<'a> {
 }
 
 impl Scope<'_> {
+    /// True if the returned scope is self
+    fn scope_for(&self, id: &Id) -> Option<(&Scope, bool)> {
+        if let Some(..) = self.constants.get(id) {
+            return Some((self, true));
+        }
+
+        self.parent
+            .and_then(|parent| parent.scope_for(id))
+            .map(|(scope, _)| (scope, false))
+    }
+
+    pub fn add_read(&self, id: &Id) {
+        if let Some((scope, is_self)) = self.scope_for(id) {
+            if !is_self {
+                if let Some(var_info) = scope.bindings.get(id) {
+                    var_info.read_from_nested_scope.set(true);
+                }
+            }
+        }
+    }
+
+    pub fn add_write(&self, id: &Id) {
+        if let Some((scope, is_self)) = self.scope_for(id) {
+            if !is_self {
+                if let Some(var_info) = scope.bindings.get(id) {
+                    var_info.write_from_nested_scope.set(true)
+                }
+            }
+        }
+    }
+
+    pub fn find_binding(&self, id: &Id) -> Option<&VarInfo> {
+        if let Some(e) = self.bindings.get(id) {
+            return Some(e);
+        }
+
+        self.parent.and_then(|parent| parent.find_binding(id))
+    }
+
     pub fn find_constants(&self, id: &Id) -> Option<&Expr> {
         if let Some(e) = self.constants.get(id) {
             return Some(e);
@@ -73,7 +115,22 @@ struct VarInfo {
     kind: VarDeclKind,
     read_from_nested_scope: Cell<bool>,
     write_from_nested_scope: Cell<bool>,
+    value: RefCell<Option<Expr>>,
 }
+
+//struct IdentFinder<'a> {
+//    targets: &'a FxHashMap<Id, VarInfo>,
+//    found: &'a mut Vec<Id>,
+//}
+//
+//impl Visit<Ident> for IdentFinder<'_> {
+//    fn visit(&mut self, node: &Ident) {
+//        let id = node.to_id();
+//        if self.targets.contains_key(&id) {
+//            self.found.push(id)
+//        }
+//    }
+//}
 
 impl Inlining<'_> {
     fn fold_with_child<T>(&mut self, kind: ScopeKind, node: T) -> T
@@ -88,6 +145,7 @@ impl Inlining<'_> {
             scope: Scope {
                 parent: Some(&self.scope),
                 kind,
+                bindings: Default::default(),
                 constants: Default::default(),
             },
             var_decl_kind: VarDeclKind::Var,
@@ -113,16 +171,28 @@ impl Fold<VarDecl> for Inlining<'_> {
 
 impl Fold<VarDeclarator> for Inlining<'_> {
     fn fold(&mut self, node: VarDeclarator) -> VarDeclarator {
-        if self.var_decl_kind == VarDeclKind::Const && self.is_first_run {
-            match node.name {
-                Pat::Ident(ref name) => match &node.init {
-                    Some(box e @ Expr::Lit(..)) | Some(box e @ Expr::Ident(..)) => {
-                        self.scope.constants.insert(name.to_id(), e.clone());
+        match node.name {
+            Pat::Ident(ref name) => match &node.init {
+                Some(box e @ Expr::Lit(..)) | Some(box e @ Expr::Ident(..)) => {
+                    if self.var_decl_kind == VarDeclKind::Const {
+                        if self.is_first_run {
+                            self.scope.constants.insert(name.to_id(), e.clone());
+                        }
+                    } else {
+                        self.scope.bindings.insert(
+                            name.to_id(),
+                            VarInfo {
+                                kind: self.var_decl_kind,
+                                read_from_nested_scope: Cell::new(false),
+                                write_from_nested_scope: Cell::new(false),
+                                value: RefCell::new(Some(e.clone().into())),
+                            },
+                        );
                     }
-                    _ => {}
-                },
+                }
                 _ => {}
-            }
+            },
+            _ => {}
         }
 
         node.fold_children(self)
@@ -157,11 +227,44 @@ impl Fold<Expr> for Inlining<'_> {
         let node: Expr = node.fold_children(self);
 
         match node {
-            Expr::Ident(ref i) if self.is_first_run => {
-                if let Some(expr) = self.scope.find_constants(&i.to_id()) {
-                    self.changed = true;
-                    return expr.clone();
+            Expr::Ident(ref i) => {
+                let id = i.to_id();
+
+                if self.is_first_run {
+                    if let Some(expr) = self.scope.find_constants(&id) {
+                        self.changed = true;
+                        return expr.clone();
+                    }
                 }
+
+                if let Some(var) = self.scope.find_binding(&id) {
+                    if !var.write_from_nested_scope.get() {
+                        let expr = var.value.borrow();
+
+                        if let Some(expr) = &*expr {
+                            self.changed = true;
+                            return expr.clone();
+                        }
+                    }
+                }
+
+                self.scope.add_read(&i.to_id())
+            }
+
+            _ => {}
+        }
+
+        node
+    }
+}
+
+impl Fold<Pat> for Inlining<'_> {
+    fn fold(&mut self, node: Pat) -> Pat {
+        let node: Pat = node.fold_children(self);
+
+        match node {
+            Pat::Ident(ref i) => {
+                self.scope.add_write(&i.to_id());
             }
 
             _ => {}
