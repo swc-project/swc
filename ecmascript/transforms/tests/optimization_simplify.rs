5a54294729970daa211a0007699454538d962372
강동윤 <kdy1997.dev@gmail.com>
2020-02-13 10:18:39 +0900

    fmt

--- ecmascript/transforms/tests/optimization_simplify.rs
+++ ecmascript/transforms/tests/optimization_simplify.rs
@@ -77,7 +77,7 @@ console.log(c); // Prevent optimizing out.
 
 /** Check that removing blocks with 1 child works * * * * * * * * * * * * *
  ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- **   * * * * * * * * * * * * **/
+ **   * * * * * * * * * * * * * **/
 #[test]
 #[ignore] // TODO
 fn test_fold_one_child_blocks_integration() {
@@ -149,7 +149,7 @@ fn test_fold_one_child_blocks_string_compare() {
 
 /** Test a particularly hairy edge case. * * * * * * * * * * * * * * * * * *
  ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- **   * * * * * * * **/
+ **   * * * * * * * * **/
 #[test]
 fn test_necessary_dangling_else() {
     test(
@@ -160,7 +160,7 @@ fn test_necessary_dangling_else() {
 
 /** Try to minimize returns * * * * * * * * * * * * * * * * * * * * * * * *
  ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- **   * **/
+ **   * * **/
 #[test]
 #[ignore] // TODO
 fn test_fold_returns_integration() {
