bb2d3be96cd065471afb9b47781190af48a183b3
강동윤 <kdy1997.dev@gmail.com>
2020-02-22 13:15:19 +0900

    Add more tests for #686

--- ecmascript/transforms/src/compat/es2015/block_scoping.rs
+++ ecmascript/transforms/src/compat/es2015/block_scoping.rs
@@ -195,6 +195,7 @@ impl Fold<WhileStmt> for BlockScoping {
 impl Fold<ForStmt> for BlockScoping {
     fn fold(&mut self, node: ForStmt) -> ForStmt {
         let init = node.init.fold_with(self);
+
         let mut vars = find_vars(&init);
         let args = vars.clone();
 
@@ -673,5 +674,49 @@ foo();",
 }
 
 foo();"
+    );
+
+    test!(
+        ::swc_ecma_parser::Syntax::default(),
+        |_| block_scoping(),
+        issue_686,
+        "module.exports = function(values) {
+    var vars = [];
+    var elem = null, name, val;
+    for (var i = 0; i < this.elements.length; i++) {
+      elem = this.elements[i];
+      name = elem.name;
+      if (!name) continue;
+      val = values[name];
+      if (val == null) val = '';
+      switch (elem.type) {
+      case 'submit':
+        break;
+      case 'radio':
+      case 'checkbox':
+        elem.checked = val.some(function(str) {
+          return str.toString() == elem.value;
+        });
+        break;
+      case 'select-multiple':
+        elem.fill(val);
+        break;
+      case 'textarea':
+        elem.innerText = val;
+        break;
+      case 'hidden':
+        break;
+      default:
+        if (elem.fill) {
+          elem.fill(val);
+        } else {
+          elem.value = val;
+        }
+        break;
+      }
+    }
+    return vars;
+  };",
+        ""
     );
 }
