5a2f9249b27206af045dbddb9ea2a798161c7aa0
David Sherret <dsherret@users.noreply.github.com>
2020-03-09 21:38:56 -0400

    Fix class declaration with decorators start. (#711)

--- ecmascript/parser/src/parser/stmt.rs
+++ ecmascript/parser/src/parser/stmt.rs
@@ -107,8 +107,6 @@ impl<'a, I: Tokens> Parser<'a, I> {
         }
         trace_cur!(parse_stmt_internal);
 
-        let start = cur_pos!();
-
         if self.input.syntax().typescript() && is!("const") && peeked_is!("enum") {
             assert_and_bump!("const");
             assert_and_bump!("enum");
