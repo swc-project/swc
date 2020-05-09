b2e499d232e8e4ecbde441d4e47e361824627fd0
강동윤 <kdy1997.dev@gmail.com>
2020-05-09 18:22:29 +0900

    Move test

--- spack/lab/src/pageLoader.js
+++ spack/lab/src/pageLoader.js
@@ -1,7 +1,8 @@
 export default function pageLoader(platform = 'desktop', componentName) {
     switch (platform) {
         case "desktop":
-            return import(`./components/desktop/${componentName}`);
+            // return import(`./components/desktop/${componentName}`);
+            return import(`./${componentName}`);
 
         default:
             return import(`./components/mobile/${componentName}`);
