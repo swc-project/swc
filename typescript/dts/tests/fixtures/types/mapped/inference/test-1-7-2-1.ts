c2c1ae2cd4b0199d29336853c9550aa48977119e
강동윤 <kdy1997.dev@gmail.com>
2020-05-09 14:32:45 +0900

    Remove comments

--- typescript/dts/tests/fixtures/types/mapped/inference/test-1-7-2-1.ts
+++ typescript/dts/tests/fixtures/types/mapped/inference/test-1-7-2-1.ts
@@ -1,15 +1,8 @@
-// Repro from #12606
-
 type Func<T> = (...args: any[]) => T;
 type Spec<T> = {
     [P in keyof T]: Func<T[P]> | Spec<T[P]>;
 };
 
-/**
- * Given a spec object recursively mapping properties to functions, creates a function
- * producing an object of the same structure, by mapping each property to the result
- * of calling its associated function with the supplied arguments.
- */
 declare function applySpec<T>(obj: Spec<T>): (...args: any[]) => T;
 
 // Infers g1: (...args: any[]) => { sum: number, nested: { mul: string } }
