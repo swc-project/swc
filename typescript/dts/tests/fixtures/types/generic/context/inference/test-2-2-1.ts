928b6441f1a81027b9373a2003bc29405bd101a7
강동윤 <kdy1997.dev@gmail.com>
2020-05-18 21:35:46 +0900

    Add a test to ensure than type parameter with same name is handled correctly

---
+++ typescript/dts/tests/fixtures/types/generic/context/inference/test-2-2-1.ts
@@ -0,0 +1,11 @@
+type Box<T> = { value: T };
+
+declare function compose<A, B, C>(f: (a: A) => B, g: (b: B) => C): (a: A) => C;
+
+declare function list<T>(a: T): T[];
+
+declare function unlist<T>(a: T[]): T;
+
+declare function box<V>(x: V): Box<V>;
+
+const f11: <T>(x: T) => Box<T[]> = compose(list, box);
