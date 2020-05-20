b02f3f7f0bbdeb09169dbb219e56e7c6ee987c65
강동윤 <kdy1997.dev@gmail.com>
2020-05-20 16:21:44 +0900

    Split a test

---
+++ typescript/dts/tests/fixtures/types/generic/context/inference/test-3-3.ts
@@ -0,0 +1,7 @@
+type Box<T> = { value: T };
+
+declare function identity<T>(x: T): T;
+
+const arrayMap = <T, U>(f: (x: T) => U) => (a: T[]) => a.map(f);
+
+const f22: <A>(a: A[]) => A[] = arrayMap(identity);
