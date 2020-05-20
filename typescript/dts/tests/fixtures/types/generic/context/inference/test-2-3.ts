76e3da156b8e7980f0106429ce04a4e9a1deec9e
강동윤 <kdy1997.dev@gmail.com>
2020-05-20 16:19:50 +0900

    Fix test

--- typescript/dts/tests/fixtures/types/generic/context/inference/test-2-3.ts
+++ typescript/dts/tests/fixtures/types/generic/context/inference/test-2-3.ts
@@ -1,19 +1,9 @@
 type Box<T> = { value: T };
 
-declare function wrap<A, B>(f: (a: A) => B): (a: A) => B;
-
 declare function compose<A, B, C>(f: (a: A) => B, g: (b: B) => C): (a: A) => C;
 
-declare function list<T>(a: T): T[];
+declare function unlist<T>(a: T[]): T;
 
-declare function box<V>(x: V): Box<V>;
-
-declare function map<T, U>(a: T[], f: (x: T) => U): U[];
-
-declare function identity<T>(x: T): T;
-
-declare function zip<A, B>(a: A, b: B): [A, B];
-
-declare function flip<X, Y, Z>(f: (x: X, y: Y) => Z): (y: Y, x: X) => Z;
+declare function unbox<W>(x: Box<W>): W;
 
 const f12: <T>(x: Box<T[]>) => T = compose(a => unbox(a), b => unlist(b));
