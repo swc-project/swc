29d6207b59e5a375774144715920c49ca972706f
강동윤 <kdy1997.dev@gmail.com>
2020-05-21 18:34:39 +0900

    Add a test case for #788

--- ecmascript/transforms/src/resolver/tests.rs
+++ ecmascript/transforms/src/resolver/tests.rs
@@ -1105,3 +1105,14 @@ identical!(
     export default function reducer(state = initialState, action = {}) {
     }"
 );
+
+identical!(
+    issue_788_1,
+    "window.addEventListener('message', (e) => {
+    try {
+        console.log(e.data);
+    } catch(e) {
+        console.log(e);
+    }
+});"
+);
