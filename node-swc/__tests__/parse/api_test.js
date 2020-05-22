415bc039d313b6dd11c6f545387008dee7dfac8e
강동윤 <kdy1997.dev@gmail.com>
2020-05-22 15:44:34 +0900

    Fix tests

--- node-swc/__tests__/parse/api_test.js
+++ node-swc/__tests__/parse/api_test.js
@@ -20,12 +20,12 @@ it("can be emit code back asynchronously", async () => {
   const m = await swc.parse(`class Foo {}`);
   const out = await swc.print(m);
 
-  expect(out.code.trim().replace("\n", "")).toBe(`class Foo{}`);
+  expect(out.code.trim().replace("\n", "")).toBe(`class Foo {}`);
 });
 
 it("can be emit code back synchronously", async () => {
   const m = await swc.parseSync(`class Foo {}`);
   const out = swc.printSync(m);
 
-  expect(out.code.trim().replace("\n", "")).toBe(`class Foo{}`);
+  expect(out.code.trim().replace("\n", "")).toBe(`class Foo {}`);
 });
