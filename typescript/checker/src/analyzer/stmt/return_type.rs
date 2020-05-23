8a73c023ce79c0d0feca0d89c4766627da3835b3
강동윤 <kdy1997.dev@gmail.com>
2020-05-23 22:46:24 +0900

    WIP: return type

--- typescript/checker/src/analyzer/stmt/return_type.rs
+++ typescript/checker/src/analyzer/stmt/return_type.rs
@@ -90,6 +90,7 @@ where
 {
     fn visit_mut(&mut self, s: &mut ReturnStmt) {
         if let Some(ty) = s.arg.validate_with(self.analyzer) {
+            dbg!(&ty);
             self.types.push(ty)
         }
     }
