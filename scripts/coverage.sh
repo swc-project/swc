598c17900780ad3d664c656e849b7bd4db39e6ae
강동윤 <kdy1997.dev@gmail.com>
2020-05-23 20:15:41 +0900

    Correct coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
+set -eu
 
-cargo test --no-run
-cargo test
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
+export RUSTDOCFLAGS="-Cpanic=abort"
+
+cargo test --no-run --all --all-features --exclude node-swc wasm
+cargo test --all --all-features --exclude node-swc wasm
 
 zip -0 ccov.zip `find . \( -name "swc*.gc*" -o -name 'ast_node*.gc*' -o -name 'enum_kind*.gc*' -o -name 'string-enum*.gc*' -o -name 'from_variant*.gc*' \) -print`;
 
