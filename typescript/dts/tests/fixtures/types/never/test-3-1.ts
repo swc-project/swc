3b4b8a37eb7431e738e657cc7effcaa5d028fedc
강동윤 <kdy1997.dev@gmail.com>
2020-05-24 15:31:51 +0900

    Split test & rename `error`

--- typescript/dts/tests/fixtures/types/never/test-3-1.ts
+++ typescript/dts/tests/fixtures/types/never/test-3-1.ts
@@ -11,9 +11,3 @@ function move1(direction: "up" | "down") {
     }
     return error2("Should never get here");
 }
-
-function move2(direction: "up" | "down") {
-    return direction === "up" ? 1 :
-        direction === "down" ? -1 :
-            error2("Should never get here");
-}
--- typescript/dts/tests/fixtures/types/never/test-3-2.ts
+++ typescript/dts/tests/fixtures/types/never/test-3-2.ts
@@ -2,16 +2,6 @@ function error2(message: string): never {
     throw new Error(message);
 }
 
-function move1(direction: "up" | "down") {
-    switch (direction) {
-        case "up":
-            return 1;
-        case "down":
-            return -1;
-    }
-    return error2("Should never get here");
-}
-
 function move2(direction: "up" | "down") {
     return direction === "up" ? 1 :
         direction === "down" ? -1 :
