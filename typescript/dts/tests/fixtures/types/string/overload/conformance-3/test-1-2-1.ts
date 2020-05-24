8d7be2c6cc4eb929d208ca63e9d3312f3c924630
강동윤 <kdy1997.dev@gmail.com>
2020-05-25 00:10:02 +0900

    Split test

---
+++ typescript/dts/tests/fixtures/types/string/overload/conformance-3/test-1-2-1.ts
@@ -0,0 +1,32 @@
+// @declaration: true
+
+interface Base {
+    x: string;
+    y: number;
+}
+
+interface HelloOrWorld extends Base {
+    p1: boolean;
+}
+
+interface JustHello extends Base {
+    p2: boolean;
+}
+
+interface JustWorld extends Base {
+    p3: boolean;
+}
+
+let hello: "hello";
+let world: "world";
+let helloOrWorld: "hello" | "world";
+
+function g(p: string): Base;
+function g(p: "hello"): JustHello;
+function g(p: "hello" | "world"): HelloOrWorld;
+function g(p: "world"): JustWorld;
+function g(...args: any[]): any {
+    return undefined;
+}
+
+let gResult3 = g(helloOrWorld);
