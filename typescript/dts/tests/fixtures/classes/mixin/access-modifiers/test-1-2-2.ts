506e6af6d22510b1a64073f37e4f0b463b0dde34
강동윤 <kdy1997.dev@gmail.com>
2020-05-28 16:19:59 +0900

    Split more!

---
+++ typescript/dts/tests/fixtures/classes/mixin/access-modifiers/test-1-2-2.ts
@@ -0,0 +1,54 @@
+type Constructable = new (...args: any[]) => object;
+
+class Private {
+    constructor(...args: any[]) {
+    }
+
+    private p: string;
+}
+
+class Private2 {
+    constructor(...args: any[]) {
+    }
+
+    private p: string;
+}
+
+class Protected {
+    constructor(...args: any[]) {
+    }
+
+    protected p: string;
+    protected static s: string;
+}
+
+class Protected2 {
+    constructor(...args: any[]) {
+    }
+
+    protected p: string;
+    protected static s: string;
+}
+
+class Public {
+    constructor(...args: any[]) {
+    }
+
+    public p: string;
+    public static s: string;
+}
+
+class Public2 {
+    constructor(...args: any[]) {
+    }
+
+    public p: string;
+    public static s: string;
+}
+
+declare function Mix<T, U>(c1: T, c2: U): T & U;
+
+// Can't derive from type with inaccessible properties
+
+class C2 extends Mix(Private, Protected) {
+}
