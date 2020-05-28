12a46a298136081aaf68f44a24f0e94b54f582cb
강동윤 <kdy1997.dev@gmail.com>
2020-05-28 16:18:17 +0900

    Split tests

---
+++ typescript/dts/tests/fixtures/classes/mixin/access-modifiers/test-1-2.ts
@@ -0,0 +1,102 @@
+type Constructable = new (...args: any[]) => object;
+
+class Private {
+    constructor(...args: any[]) {
+    }
+
+    private p: string;
+}
+
+class Private2 {
+    constructor(...args: any[]) {
+    }
+
+    private p: string;
+}
+
+class Protected {
+    constructor(...args: any[]) {
+    }
+
+    protected p: string;
+    protected static s: string;
+}
+
+class Protected2 {
+    constructor(...args: any[]) {
+    }
+
+    protected p: string;
+    protected static s: string;
+}
+
+class Public {
+    constructor(...args: any[]) {
+    }
+
+    public p: string;
+    public static s: string;
+}
+
+class Public2 {
+    constructor(...args: any[]) {
+    }
+
+    public p: string;
+    public static s: string;
+}
+
+declare function Mix<T, U>(c1: T, c2: U): T & U;
+
+// Can't derive from type with inaccessible properties
+
+class C1 extends Mix(Private, Private2) {
+}
+
+class C2 extends Mix(Private, Protected) {
+}
+
+class C3 extends Mix(Private, Public) {
+}
+
+class C4 extends Mix(Protected, Protected2) {
+    f(c4: C4, c5: C5, c6: C6) {
+        c4.p;
+        c5.p;
+        c6.p;
+    }
+
+    static g() {
+        C4.s;
+        C5.s;
+        C6.s
+    }
+}
+
+class C5 extends Mix(Protected, Public) {
+    f(c4: C4, c5: C5, c6: C6) {
+        c4.p;  // Error, not in class deriving from Protected2
+        c5.p;
+        c6.p;
+    }
+
+    static g() {
+        C4.s;  // Error, not in class deriving from Protected2
+        C5.s;
+        C6.s
+    }
+}
+
+class C6 extends Mix(Public, Public2) {
+    f(c4: C4, c5: C5, c6: C6) {
+        c4.p;  // Error, not in class deriving from Protected2
+        c5.p;
+        c6.p;
+    }
+
+    static g() {
+        C4.s;  // Error, not in class deriving from Protected2
+        C5.s;
+        C6.s
+    }
+}
