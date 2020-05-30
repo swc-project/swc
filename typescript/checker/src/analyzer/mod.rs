a2e946c25e2c1f03ae3a9f37da29671af19bf0ef
강동윤 <kdy1997.dev@gmail.com>
2020-05-30 14:57:09 +0900

    Fix

--- typescript/checker/src/analyzer/mod.rs
+++ typescript/checker/src/analyzer/mod.rs
@@ -472,20 +472,18 @@ impl Validate<Vec<ModuleItem>> for Analyzer<'_, '_> {
 
             let mut new: Vec<Vec<ModuleItem>> = (0..items.len()).map(|_| vec![]).collect();
 
-            dbg!(&order);
-            for (i, &idx) in order.iter().enumerate() {
+            for &idx in &order {
                 items[idx].visit_mut_with(self);
 
-                new[i].extend(self.prepend_stmts.drain(..).map(ModuleItem::from));
+                new[idx].extend(self.prepend_stmts.drain(..).map(ModuleItem::from));
 
-                new[i].push(replace(
+                new[idx].push(replace(
                     &mut items[idx],
                     ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP })),
                 ));
 
-                new[i].extend(self.append_stmts.drain(..).map(ModuleItem::from));
+                new[idx].extend(self.append_stmts.drain(..).map(ModuleItem::from));
             }
-
             items.clear();
             items.extend(new.into_iter().flat_map(|v| v));
         }
