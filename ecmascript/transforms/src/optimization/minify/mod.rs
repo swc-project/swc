2936018f6c381001669fcd9d5f8ba66664cb5d6d
강동윤 <kdy1997.dev@gmail.com>
2020-06-08 01:57:19 +0900

    WIP: Minifier

--- ecmascript/transforms/src/optimization/minify/mod.rs
+++ ecmascript/transforms/src/optimization/minify/mod.rs
@@ -1,24 +1,63 @@
 use crate::pass::Pass;
-use swc_common::{pass::CompilerPass, Fold};
-use swc_ecma_ast::Ident;
+use fxhash::FxHashMap;
+use smallvec::alloc::borrow::Cow;
+use swc_atoms::JsWord;
+use swc_common::{pass::CompilerPass, Fold, FoldWith, Mark};
+use swc_ecma_ast::{Function, Ident};
+use swc_ecma_utils::Id;
 
 mod merge;
 mod rename;
 
-pub fn minifier() -> impl CompilerPass + Pass + 'static {
+pub fn minifier(global_mark: Mark) -> impl CompilerPass + Pass + 'static {
     Minifier {
-        scope: Default::default(),
+        global_mark,
+        scope: Scope {
+            parent: None,
+            ..Default::default()
+        },
     }
 }
 
 #[derive(Debug)]
 struct Minifier<'a> {
+    /// Identifiers marked with the marker are preserved.
+    global_mark: Mark,
     scope: Scope<'a>,
 }
 
 noop_fold_type!(Minifier<'_>);
 
+impl CompilerPass for Minifier<'_> {
+    fn name() -> Cow<'static, str> {
+        Cow::Borrowed("minifier")
+    }
+}
+
+impl Minifier<'_> {
+    fn fold_with_child<T>(&mut self, node: T) -> T
+    where
+        T: for<'any> FoldWith<Minifier<'any>>,
+    {
+        let mut child = Minifier {
+            global_mark: self.global_mark,
+            scope: Scope {
+                parent: Some(&self.scope),
+                ..Default::default()
+            },
+        };
+        node.fold_children(&mut child)
+    }
+}
+
 #[derive(Debug, Default)]
 struct Scope<'a> {
     parent: Option<&'a Scope<'a>>,
+    renamed: FxHashMap<Id, JsWord>,
+}
+
+impl Fold<Function> for Minifier<'_> {
+    fn fold(&mut self, f: Function) -> Function {
+        self.fold_with_child(f)
+    }
 }
