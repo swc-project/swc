b254bea68b41e4078a782cfcfcf22d9fdfa94ac0
강동윤 <kdy1997.dev@gmail.com>
2020-06-08 23:00:47 +0900

    Fix tests

--- node-swc/__tests__/spack/simple_test.js
+++ node-swc/__tests__/spack/simple_test.js
@@ -5,7 +5,9 @@ const path = require('path');
 it('should handle a simple case', async () => {
     const result = await swc.bundle({
         name: 'simple',
-        entry: path.join(__dirname, '../../tests/spack/simple/a.js'),
+        entry: {
+            simple: path.join(__dirname, '../../tests/spack/simple/a.js')
+        },
     });
 
     console.log(result)
