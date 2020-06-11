ae7d0f5979ccd708edbe789b3c6253f088ba8853
강동윤 <kdy1997.dev@gmail.com>
2020-06-11 15:56:47 +0900

    Add a test for #834

---
+++ tests/simple.rs
@@ -0,0 +1,42 @@
+use swc::{config::Options, Compiler};
+use swc_common::FileName;
+use testing::Tester;
+
+fn compile(src: &str, options: Options) -> String {
+    Tester::new()
+        .print_errors(|cm, handler| {
+            let c = Compiler::new(cm.clone(), handler);
+
+            let fm = cm.new_source_file(FileName::Real("input.js".into()), src.into());
+            let s = c.process_js_file(
+                fm,
+                &Options {
+                    is_module: true,
+                    ..options
+                },
+            );
+
+            match s {
+                Ok(v) => {
+                    if c.handler.has_errors() {
+                        Err(())
+                    } else {
+                        Ok(v.code.into())
+                    }
+                }
+                Err(err) => panic!("Error: {}", err),
+            }
+        })
+        .unwrap()
+}
+
+#[test]
+fn issue_834_1() {
+    compile(
+        "var foo =  2n + 7n;",
+        Options {
+            swcrc: false,
+            ..Default::default()
+        },
+    );
+}
