b0068325fb65b36b95f074ae1ccb2b885e9bbdc2
강동윤 <kdy1997.dev@gmail.com>
2020-06-12 13:13:48 +0900

    Add an integration test for #834

--- node-swc/__tests__/transform/issue834_test.js
+++ node-swc/__tests__/transform/issue834_test.js
@@ -1,9 +1,25 @@
 const swc = require('../../../');
 
-it('should perform dce', () => {
+it('should work without parser.sytax', () => {
     const out = swc.transformSync(`const someValue = "test" ?? "default value";`, {
             jsc: {
                 parser: {
+                    nullishCoalescing: true,
+                    numericSeparator: true
+                },
+            }
+        }
+    );
+    expect(out.map).toBeFalsy();
+    expect(out.code.trim()).toBe(`console.log('Foo');`);
+});
+
+
+it('should work with parser.syntax', () => {
+    const out = swc.transformSync(`const someValue = "test" ?? "default value";`, {
+            jsc: {
+                parser: {
+                    syntax: 'ecmascript',
                     nullishCoalescing: true,
                     numericSeparator: true
                 },
