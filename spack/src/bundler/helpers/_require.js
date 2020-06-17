b696dde4eb92763e6683f514f8c3d060270e0828
강동윤 <kdy1997.dev@gmail.com>
2020-06-17 23:10:51 +0900

    Inject __spack_require__

--- spack/src/bundler/helpers/_require.js
+++ spack/src/bundler/helpers/_require.js
@@ -1,3 +1,17 @@
 function __spack_require__(mod) {
+    var cache;
 
+    return function () {
+        if (cache) {
+            return cache;
+        }
+
+        var module = {
+            exports: {}
+        };
+
+        mod(module, module.exports);
+        cache = module.exports;
+        return cache;
+    }
 }
--- spack/tests/pass/node-modules/library/simple/output/entry.js
+++ spack/tests/pass/node-modules/library/simple/output/entry.js
@@ -1,4 +1,17 @@
-function __spack_require(mod) {
+function __spack_require__(mod) {
+    var cache;
+    return function() {
+        if (cache) {
+            return cache;
+        }
+        var module = {
+            exports: {
+            }
+        };
+        mod(module, module.exports);
+        cache = module.exports;
+        return cache;
+    };
 }
 var load = __spack_require__.bind(void 0, function(module1, exports) {
     var load1 = __spack_require__.bind(void 0, function(module1, exports1) /*!
