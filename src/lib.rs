e4b5082bcca52686391501156122634e3f76d836
강동윤 <kdy1997.dev@gmail.com>
2020-07-30 18:50:58 +0900

    Comments impl for the swc itself

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![deny(unused)]
+
 pub use sourcemap;
 pub use swc_atoms as atoms;
 pub use swc_common as common;
@@ -45,7 +47,7 @@ pub struct Compiler {
     /// CodeMap
     pub cm: Arc<SourceMap>,
     pub handler: Arc<Handler>,
-    comments: Comments,
+    comments: SwcComments,
 }
 
 #[derive(Debug, Serialize)]
@@ -481,10 +483,59 @@ fn load_swcrc(path: &Path) -> Result<Rc, Error> {
 
 type CommentMap = Arc<DashMap<BytePos, Vec<Comment>>>;
 
+/// Multi-threaded implementation of [Comments]
 #[derive(Clone)]
-pub struct MultiThreadComments {
+pub struct SwcComments {
     leading: CommentMap,
     trailing: CommentMap,
 }
 
-impl Comments for MultiThreadComments {}
+impl Comments for SwcComments {
+    fn add_leading(&self, pos: BytePos, cmt: Comment) {
+        self.leading.entry(pos).or_default().push(cmt);
+    }
+
+    fn add_leading_comments(&self, pos: BytePos, comments: Vec<Comment>) {
+        self.leading.entry(pos).or_default().extend(comments);
+    }
+
+    fn has_leading(&self, pos: BytePos) -> bool {
+        self.leading.contains_key(&pos)
+    }
+
+    fn move_leading(&self, from: BytePos, to: BytePos) {
+        let cmt = self.leading.remove(&from);
+
+        if let Some(cmt) = cmt {
+            self.leading.entry(to).or_default().extend(cmt.1);
+        }
+    }
+
+    fn take_leading(&self, pos: BytePos) -> Option<Vec<Comment>> {
+        self.leading.remove(&pos).map(|v| v.1)
+    }
+
+    fn add_trailing(&self, pos: BytePos, cmt: Comment) {
+        self.trailing.entry(pos).or_default().push(cmt)
+    }
+
+    fn add_trailing_comments(&self, pos: BytePos, comments: Vec<Comment>) {
+        self.trailing.entry(pos).or_default().extend(comments)
+    }
+
+    fn has_trailing(&self, pos: BytePos) -> bool {
+        self.trailing.contains_key(&pos)
+    }
+
+    fn move_trailing(&self, from: BytePos, to: BytePos) {
+        let cmt = self.trailing.remove(&from);
+
+        if let Some(cmt) = cmt {
+            self.trailing.entry(to).or_default().extend(cmt.1);
+        }
+    }
+
+    fn take_trailing(&self, pos: BytePos) -> Option<Vec<Comment>> {
+        self.trailing.remove(&pos).map(|v| v.1)
+    }
+}
