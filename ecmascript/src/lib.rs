9c7e4042ef6afa7d531488c6da5d42af01aff0ab
강동윤 <kdy1997.dev@gmail.com>
2020-07-31 18:04:05 +0900

    cargo features for swc_ecmascript

--- ecmascript/src/lib.rs
+++ ecmascript/src/lib.rs
@@ -1,6 +1,11 @@
 pub use swc_ecma_ast as ast;
+#[cfg(feature = "codegen")]
 pub use swc_ecma_codegen as codegen;
+#[cfg(feature = "parser")]
 pub use swc_ecma_parser as parser;
-pub use swc_ecma_preset_env as preset_env;
+#[cfg(feature = "transforms")]
 pub use swc_ecma_transforms as transforms;
+#[cfg(feature = "utils")]
+pub use swc_ecma_utils as utils;
+#[cfg(feature = "visit")]
 pub use swc_ecma_visit as visit;
