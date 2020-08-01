29da7fc03722377f5db1a1aafad81b03304df9c9
강동윤 <kdy1997.dev@gmail.com>
2020-08-01 16:36:10 +0900

    Script for auto-updating

---
+++ scripts/cron/update-coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+git checkout master
+changed=0
+git pull --dry-run | grep -q -v 'Already up-to-date.' && changed=1
+
+if [ $changed = '0' ] ; then 
+    echo 'Already up-to-date'
+    exit 0
+fi
+
+$DIR/../coverage.sh
