1ae45207535b1a28b0bdf8131dd84d44afe70d9b
강동윤 <kdy1997.dev@gmail.com>
2020-08-02 18:17:41 +0900

    Improve coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
 
 export CARGO_INCREMENTAL=0
@@ -9,7 +9,7 @@ export CARGO_TARGET_DIR=".COVERAGE_DIR"
 
 cargo test --all --all-features --exclude node-swc --exclude wasm --exclude spack
 
-zip -0 ccov.zip `find $CARGO_TARGET_DIR \( -name "swc*.gc*" -o -name 'ast_node*.gc*' -o -name 'enum_kind*.gc*' -o -name 'string-enum*.gc*' -o -name 'from_variant*.gc*' \) -print`;
+zip -0 ccov.zip `find $CARGO_TARGET_DIR \( -name "swc*.gc*" -o -name 'ast_node*.gc*' -o -name 'spack*.gc*' -o -name 'enum_kind*.gc*' -o -name 'string-enum*.gc*' -o -name 'from_variant*.gc*' \) -print`;
 
 grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info;
 
