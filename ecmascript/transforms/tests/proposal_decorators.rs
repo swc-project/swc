e26d9920fa9abfd11fd1cccc5377548446511787
강동윤 <kdy1997.dev@gmail.com>
2020-08-06 20:57:57 +0900

    fixup! Updage more references

--- ecmascript/transforms/tests/proposal_decorators.rs
+++ ecmascript/transforms/tests/proposal_decorators.rs
@@ -2497,6 +2497,7 @@ export default @dec class A {}
 @dec class B {}
 "#,
     r#"
+    var _class, _class1;
     export default _class = dec((_class = class A {
     }) || _class) || _class;
     let B = _class1 = dec((_class1 = class B {
