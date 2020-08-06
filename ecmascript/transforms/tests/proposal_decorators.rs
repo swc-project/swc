7e1ae93ba6a98c1d82d68422f2769d8711b2f9a1
강동윤 <kdy1997.dev@gmail.com>
2020-08-06 20:56:03 +0900

    fixup! Updage more references

--- ecmascript/transforms/tests/proposal_decorators.rs
+++ ecmascript/transforms/tests/proposal_decorators.rs
@@ -4735,26 +4735,26 @@ test!(
   }",
     r#"
     var _class, _dec, _dec1, _dec2;
-    var _dec3 = Reflect.metadata("design:paramtypes", [
-        typeof Generic === void 0 ? Object : Generic,
-        typeof Generic === void 0 ? Object : Generic
-    ]), _dec4 = Reflect.metadata("design:type", Function);
-    let MyClass = Decorate(_dec4(_dec3(((_class = class MyClass {
-        constructor(private generic: Generic<A>, generic2: Generic<A, B>){
-        }
-        method(generic: Inter<A>, generic2: InterGen<A, B>) {
-        }
-    }) || _class, _dec = function(target, target) {
-        return Arg()(target, key, 1);
-    }, _dec1 = Reflect.metadata("design:type", Function), _dec2 = Reflect.metadata("design:paramtypes", [
-        typeof Inter === void 0 ? Object : Inter,
-        typeof InterGen === void 0 ? Object : InterGen
-    ]), _applyDecoratedDescriptor(_class.prototype, "method", [
-        Run,
-        _dec,
-        _dec1,
-        _dec2
-    ], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype), _class))));
+var _dec3 = Reflect.metadata("design:paramtypes", [
+    typeof Generic === void 0 ? Object : Generic,
+    typeof Generic === void 0 ? Object : Generic
+]), _dec4 = Reflect.metadata("design:type", Function);
+let MyClass = _class = Decorate(_class = _dec4(_class = _dec3(((_class = class MyClass {
+    constructor(private generic: Generic<A>, generic2: Generic<A, B>){
+    }
+    method(generic: Inter<A>, generic2: InterGen<A, B>) {
+    }
+}) || _class, _dec = function(target, target) {
+    return Arg()(target, key, 1);
+}, _dec1 = Reflect.metadata("design:type", Function), _dec2 = Reflect.metadata("design:paramtypes", [
+    typeof Inter === void 0 ? Object : Inter,
+    typeof InterGen === void 0 ? Object : InterGen
+]), _applyDecoratedDescriptor(_class.prototype, "method", [
+    Run,
+    _dec,
+    _dec1,
+    _dec2
+], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype), _class)) || _class) || _class) || _class;
 "#
 );
 
