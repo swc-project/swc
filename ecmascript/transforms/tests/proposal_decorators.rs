f0235fe4b051aa31a4ca54a4e2be3ece468c0791
강동윤 <kdy1997.dev@gmail.com>
2020-08-06 16:44:55 +0900

    Udpate test

--- ecmascript/transforms/tests/proposal_decorators.rs
+++ ecmascript/transforms/tests/proposal_decorators.rs
@@ -4745,17 +4745,20 @@ test!(
     }),
     legacy_metadata_generics_1,
     "@Decorate
-class MyClass {}",
-    r##"let MyClass = (_dec = Reflect.metadata("design:type", Function), _dec2 = Reflect.metadata("design:paramtypes", [typeof Generic === "undefined" ? Object : Generic, typeof Generic === "undefined" ? Object : Generic]), _dec3 = function (target, key) {
-      return Arg()(target, key, 1);
-    }, _dec4 = Reflect.metadata("design:type", Function), _dec5 = Reflect.metadata("design:paramtypes", [typeof Inter === "undefined" ? Object : Inter, typeof InterGen === "undefined" ? Object : InterGen]), Decorate(_class = _dec(_class = _dec2(_class = (_class2 = class MyClass {
+class MyClass {
+  constructor(
+    private generic: Generic<A>,
+    generic2: Generic<A, B>
+  ) {}
+}",
+    r##"var _dec, _dec2, _class;
+
+    let MyClass = (_dec = Reflect.metadata("design:type", Function), _dec2 = Reflect.metadata("design:paramtypes", [typeof Generic === "undefined" ? Object : Generic, typeof Generic === "undefined" ? Object : Generic]), Decorate(_class = _dec(_class = _dec2(_class = class MyClass {
       constructor(generic, generic2) {
         this.generic = generic;
       }
     
-      method(generic, generic2) {}
-    
-    }, (_applyDecoratedDescriptor(_class2.prototype, "method", [Run, _dec3, _dec4, _dec5], Object.getOwnPropertyDescriptor(_class2.prototype, "method"), _class2.prototype)), _class2)) || _class) || _class) || _class);"##
+    }) || _class) || _class) || _class);"##
 );
 
 test!(
