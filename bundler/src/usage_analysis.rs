ba29ca5c11f7fa8b738296bf0bffea0ac95d1f4f
강동윤 <kdy1997.dev@gmail.com>
2020-08-07 17:02:59 +0900

    Depend on swc_ecma_transforms

--- bundler/src/usage_analysis.rs
+++ bundler/src/usage_analysis.rs
@@ -1,4 +1,4 @@
-use crate::{load::Load, resolve::Resolve, Bundler};
+use crate::{load::Load, module::Specifier, resolve::Resolve, Bundler};
 use std::{borrow::Cow, sync::Arc};
 use swc_common::SourceFile;
 use swc_ecma_ast::*;
@@ -21,35 +21,33 @@ where
         node: Module,
         used_exports: Option<&[Specifier]>,
     ) -> Module {
-        self.swc.run(|| {
-            let mut used = vec![];
+        let mut used = vec![];
 
-            if let Some(used_exports) = used_exports {
-                for export in used_exports {
-                    match export {
-                        Specifier::Specific { alias, local, .. } => {
-                            used.push(alias.as_ref().unwrap_or(local).to_id());
-                        }
-                        Specifier::Namespace { local } => {
-                            used.push(local.to_id());
-                        }
+        if let Some(used_exports) = used_exports {
+            for export in used_exports {
+                match export {
+                    Specifier::Specific { alias, local, .. } => {
+                        used.push(alias.as_ref().unwrap_or(local).to_id());
+                    }
+                    Specifier::Namespace { local } => {
+                        used.push(local.to_id());
                     }
                 }
             }
+        }
 
-            let used_mark = self.used_mark;
+        let used_mark = self.used_mark;
 
-            let mut v = dce::dce(dce::Config {
-                used: if used_exports.is_some() {
-                    Some(Cow::Owned(used))
-                } else {
-                    None
-                },
-                used_mark,
-            });
+        let mut v = dce::dce(dce::Config {
+            used: if used_exports.is_some() {
+                Some(Cow::Owned(used))
+            } else {
+                None
+            },
+            used_mark,
+        });
 
-            let node = node.fold_with(&mut v);
-            node
-        })
+        let node = node.fold_with(&mut v);
+        node
     }
 }
