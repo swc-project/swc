ec6a55f3665926b7c5352ab2a2639bd1d2f890c0
강동윤 <kdy1997.dev@gmail.com>
2020-08-07 14:38:17 +0900

    resolver

--- bundler/src/resolve.rs
+++ bundler/src/resolve.rs
@@ -4,3 +4,15 @@ use swc_common::FileName;
 pub trait Resolve {
     fn resolve(&self, base: &FileName, module_specifier: &str) -> Result<FileName, Error>;
 }
+
+impl<T: ?Sized + Resolve> Resolve for Box<T> {
+    fn resolve(&self, base: &FileName, module_specifier: &str) -> Result<FileName, Error> {
+        (**self).resolve(base, module_specifier)
+    }
+}
+
+impl<'a, T: ?Sized + Resolve> Resolve for &'a T {
+    fn resolve(&self, base: &FileName, module_specifier: &str) -> Result<FileName, Error> {
+        (**self).resolve(base, module_specifier)
+    }
+}
--- spack/src/resolve.rs
+++ spack/src/resolve.rs
@@ -1,24 +1,7 @@
 use anyhow::{Context, Error};
 use std::path::{Path, PathBuf};
 
-pub trait Resolve: Send + Sync {
-    ///
-    /// Returned filename will be hashed if possible and used to generate module
-    /// id.
-    fn resolve(&self, base: &Path, import: &str) -> Result<PathBuf, Error>;
-}
 
-impl<T: ?Sized + Resolve> Resolve for Box<T> {
-    fn resolve(&self, base: &Path, import: &str) -> Result<PathBuf, Error> {
-        T::resolve(self, base, import)
-    }
-}
-
-impl<'a, T: ?Sized + Resolve> Resolve for &'a T {
-    fn resolve(&self, base: &Path, import: &str) -> Result<PathBuf, Error> {
-        T::resolve(self, base, import)
-    }
-}
 
 pub struct NodeResolver;
 
