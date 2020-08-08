37c03a7af6cb898e043a105234474ce0e495492a
강동윤 <kdy1997.dev@gmail.com>
2020-08-08 14:25:18 +0900

    Fixing spack

--- spack/src/lib.rs
+++ spack/src/lib.rs
@@ -3,14 +3,7 @@
 #[cfg(test)]
 extern crate test;
 
-pub use self::{
-    bundler::{Bundle, BundleKind, Bundler},
-    id::{Id, ModuleId, QualifiedId},
-};
-
 mod bundler;
 pub mod config;
 mod debug;
 pub mod loaders;
-mod normalize;
-mod util;
