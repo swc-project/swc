78740db16bbe607b808b6016065dfed2435d26cf
강동윤 <kdy1997.dev@gmail.com>
2020-08-11 10:02:12 +0900

    Better hygiene printing

--- bundler/src/debug/mod.rs
+++ bundler/src/debug/mod.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::io::stdout;
+use std::io::{stdout, Write};
 use swc_common::{sync::Lrc, SourceMap};
 use swc_ecma_ast::{Ident, Module};
 use swc_ecma_codegen::{text_writer::JsWriter, Emitter};
@@ -9,12 +9,15 @@ use swc_ecma_visit::{Fold, FoldWith};
 pub(crate) fn print_hygiene(event: &str, cm: &Lrc<SourceMap>, t: &Module) {
     let module = t.clone().fold_with(&mut HygieneVisualizer);
 
-    println!("==================== @ {} ====================", event);
+    let stdout = stdout();
+    let mut w = stdout.lock();
+
+    writeln!(w, "==================== @ {} ====================", event).unwrap();
     Emitter {
         cfg: swc_ecma_codegen::Config { minify: false },
         cm: cm.clone(),
         comments: None,
-        wr: Box::new(JsWriter::new(cm.clone(), "\n", stdout().lock(), None)),
+        wr: Box::new(JsWriter::new(cm.clone(), "\n", w, None)),
         handlers: Box::new(Handlers),
     }
     .emit_module(&module)
