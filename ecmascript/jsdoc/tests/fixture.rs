33a60951d23dbc02bcd96f0d9ae8ae34e35e8134
강동윤 <kdy1997.dev@gmail.com>
2020-08-13 20:06:52 +0900

    WIP: test

--- ecmascript/jsdoc/tests/fixture.rs
+++ ecmascript/jsdoc/tests/fixture.rs
@@ -1,4 +1,8 @@
 #![feature(test)]
+
+extern crate test;
+
+use anyhow::Error;
 use dashmap::DashMap;
 use std::{env, path::PathBuf};
 use swc_common::{
@@ -6,12 +10,37 @@ use swc_common::{
     BytePos,
 };
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput};
+use test::{test_main, DynTestFn, ShouldPanic::No, TestDesc, TestDescAndFn, TestName, TestType};
 use walkdir::WalkDir;
 
-extern crate test;
+fn add_test<F: FnOnce() + Send + 'static>(
+    tests: &mut Vec<TestDescAndFn>,
+    name: String,
+    ignore: bool,
+    f: F,
+) {
+    tests.push(TestDescAndFn {
+        desc: TestDesc {
+            test_type: TestType::UnitTest,
+            name: TestName::DynTestName(name),
+            ignore,
+            should_panic: No,
+            allow_fail: false,
+        },
+        testfn: DynTestFn(Box::new(f)),
+    });
+}
 
 #[test]
+
 fn fixture() {
+    let args: Vec<_> = env::args().collect();
+    let mut tests = Vec::new();
+    add_fixture(&mut tests).unwrap();
+    test_main(&args, tests, Some(test::Options::new()));
+}
+
+fn add_fixture(tests: &mut Vec<TestDescAndFn>) -> Result<(), Error> {
     for entry in WalkDir::new(
         PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
             .join("tests")
@@ -23,42 +52,54 @@ fn fixture() {
         }
 
         if entry.path().to_string_lossy().ends_with(".js") {
-            testing::run_test2(true, |cm, handler| {
-                let comments = SwcComments::default();
+            add_test(
+                tests,
+                format!("jsdoc::fixture::{}", entry.path().display()),
+                false,
+                move || {
+                    testing::run_test2(true, |cm, handler| {
+                        let comments = SwcComments::default();
 
-                let fm = cm
-                    .load_file(entry.path())
-                    .expect("failed to load fixtue file");
+                        let fm = cm
+                            .load_file(entry.path())
+                            .expect("failed to load fixtue file");
 
-                let lexer = Lexer::new(
-                    Default::default(),
-                    Default::default(),
-                    StringInput::from(&*fm),
-                    Some(&comments),
-                );
-                let mut p = Parser::new_from(lexer);
-                p.parse_script().expect("should parse");
-                for err in p.take_errors() {
-                    err.into_diagnostic(&handler).emit();
-                }
-                if handler.has_errors() {
-                    return Err(());
-                }
+                        let lexer = Lexer::new(
+                            Default::default(),
+                            Default::default(),
+                            StringInput::from(&*fm),
+                            Some(&comments),
+                        );
+                        let mut p = Parser::new_from(lexer);
+                        p.parse_module().expect("should parse");
+                        for err in p.take_errors() {
+                            err.into_diagnostic(&handler).emit();
+                        }
+                        if handler.has_errors() {
+                            return Err(());
+                        }
 
-                for (_, comments) in comments.leading.into_iter() {
-                    dbg!();
-                    for cmt in &comments {
-                        println!("{}", cmt.text);
-                        let (i, parsed) = jsdoc::parse(cmt.into()).unwrap();
-                        println!("{:?}", parsed);
-                    }
-                }
+                        for (_, comments) in comments.leading.into_iter() {
+                            for cmt in &comments {
+                                println!(
+                                    "==================== {} ====================\n{}",
+                                    entry.path().display(),
+                                    cmt.text
+                                );
+                                let (i, parsed) = jsdoc::parse(cmt.into()).unwrap();
+                                println!("{:?}", parsed);
+                            }
+                        }
 
-                Ok(())
-            })
-            .unwrap();
+                        Ok(())
+                    })
+                    .unwrap();
+                },
+            );
         }
     }
+
+    Ok(())
 }
 
 type CommentMap = DashMap<BytePos, Vec<Comment>>;
