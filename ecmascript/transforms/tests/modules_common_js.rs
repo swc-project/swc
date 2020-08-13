bc57d868921c03adcfcb089c0d08199b627fc9f3
강동윤 <kdy1997.dev@gmail.com>
2020-08-13 23:54:32 +0900

    WIP: Test for #962

--- ecmascript/transforms/tests/modules_common_js.rs
+++ ecmascript/transforms/tests/modules_common_js.rs
@@ -4185,3 +4185,28 @@ function setup(url: string, obj: any) {
     return _url1;
 }"
 );
+
+test!(
+    syntax(),
+    |_| tr(Config {
+        ..Default::default()
+    }),
+    issue_962,
+    "import root from './_root.js';
+  import stubFalse from './stubFalse.js';
+    
+  var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
+  var freeModule = freeExports && typeof module == 'object' && module && !module.nodeType && \
+     module;
+    
+  var moduleExports = freeModule && freeModule.exports === freeExports;
+    
+  var Buffer = moduleExports ? root.Buffer : undefined;
+    
+  var nativeIsBuffer = Buffer ? Buffer.isBuffer : undefined;
+    
+  var isBuffer = nativeIsBuffer || stubFalse;
+    
+  export default isBuffer",
+    ""
+);
