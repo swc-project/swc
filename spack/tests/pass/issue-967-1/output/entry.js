076715467ae14ddc121926eea4724c89f6d73faf
강동윤 <kdy1997.dev@gmail.com>
2020-08-14 21:57:12 +0900

    It works if there's not recursive require

---
+++ spack/tests/pass/issue-967-1/output/entry.js
@@ -0,0 +1,27 @@
+function __spack_require__(mod) {
+    var cache;
+    return function() {
+        if (cache) {
+            return cache;
+        }
+        var module = {
+            exports: {
+            }
+        };
+        mod(module, module.exports);
+        cache = module.exports;
+        return cache;
+    };
+}
+var load = __spack_require__.bind(void 0, function(module, exports) {
+    console.log('c');
+});
+var load1 = __spack_require__.bind(void 0, function(module, exports) {
+    console.log('b');
+});
+var load2 = __spack_require__.bind(void 0, function(module, exports) {
+    console.log('a');
+});
+load2();
+load1();
+load();
