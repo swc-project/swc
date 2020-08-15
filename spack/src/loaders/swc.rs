ad4cd632417b855fcaad53ad7d425b9c6413068a
강동윤 <kdy1997.dev@gmail.com>
2020-08-15 20:00:14 +0900

    Do not tranfrom files from node_modules

--- spack/src/loaders/swc.rs
+++ spack/src/loaders/swc.rs
@@ -47,88 +47,114 @@ impl Load for SwcLoader {
 
         log::trace!("JsLoader.load: loaded");
 
-        let mut config = self.compiler.config_for_file(
-            &swc::config::Options {
-                config: {
-                    if let Some(c) = &self.options.config {
-                        Some(swc::config::Config {
-                            jsc: JscConfig {
-                                transform: {
-                                    if let Some(c) = &c.jsc.transform {
-                                        Some(TransformConfig {
-                                            react: c.react.clone(),
-                                            const_modules: c.const_modules.clone(),
-                                            optimizer: None,
-                                            legacy_decorator: c.legacy_decorator,
-                                            decorator_metadata: c.decorator_metadata,
-                                        })
-                                    } else {
-                                        None
-                                    }
+        let program = if fm.name.to_string().contains("node_modules") {
+            let program = self.compiler.parse_js(
+                fm.clone(),
+                JscTarget::Es2020,
+                Default::default(),
+                true,
+                true,
+            )?;
+            let program = helpers::HELPERS.set(&Helpers::new(true), || {
+                swc_ecma_utils::HANDLER.set(&self.compiler.handler, || {
+                    let program = program.fold_with(&mut InlineGlobals {
+                        envs: env_map(),
+                        globals: Default::default(),
+                    });
+                    let program = program.fold_with(&mut expr_simplifier());
+                    let program = program.fold_with(&mut dead_branch_remover());
+
+                    program
+                })
+            });
+
+            program
+        } else {
+            let mut config = self.compiler.config_for_file(
+                &swc::config::Options {
+                    config: {
+                        if let Some(c) = &self.options.config {
+                            Some(swc::config::Config {
+                                jsc: JscConfig {
+                                    transform: {
+                                        if let Some(c) = &c.jsc.transform {
+                                            Some(TransformConfig {
+                                                react: c.react.clone(),
+                                                const_modules: c.const_modules.clone(),
+                                                optimizer: None,
+                                                legacy_decorator: c.legacy_decorator,
+                                                decorator_metadata: c.decorator_metadata,
+                                            })
+                                        } else {
+                                            None
+                                        }
+                                    },
+                                    external_helpers: true,
+                                    ..c.jsc
                                 },
-                                external_helpers: true,
-                                ..c.jsc
-                            },
-                            module: None,
-                            minify: Some(false),
-                            ..c.clone()
-                        })
-                    } else {
-                        None
-                    }
+                                module: None,
+                                minify: Some(false),
+                                ..c.clone()
+                            })
+                        } else {
+                            None
+                        }
+                    },
+                    disable_hygiene: false,
+                    disable_fixer: true,
+                    global_mark: self.options.global_mark,
+                    cwd: self.options.cwd.clone(),
+                    caller: None,
+                    filename: String::new(),
+                    config_file: None,
+                    root: None,
+                    root_mode: Default::default(),
+                    swcrc: true,
+                    swcrc_roots: Default::default(),
+                    env_name: {
+                        let s = env::var("NODE_ENV").unwrap_or_else(|_| "development".into());
+                        s
+                    },
+                    input_source_map: InputSourceMap::Bool(false),
+                    source_maps: None,
+                    source_file_name: None,
+                    source_root: None,
+                    is_module: true,
                 },
-                disable_hygiene: false,
-                disable_fixer: true,
-                global_mark: self.options.global_mark,
-                cwd: self.options.cwd.clone(),
-                caller: None,
-                filename: String::new(),
-                config_file: None,
-                root: None,
-                root_mode: Default::default(),
-                swcrc: true,
-                swcrc_roots: Default::default(),
-                env_name: {
-                    let s = env::var("NODE_ENV").unwrap_or_else(|_| "development".into());
-                    s
-                },
-                input_source_map: InputSourceMap::Bool(false),
-                source_maps: None,
-                source_file_name: None,
-                source_root: None,
-                is_module: true,
-            },
-            &fm.name,
-        )?;
+                &fm.name,
+            )?;
 
-        log::trace!("JsLoader.load: loaded config");
+            log::trace!("JsLoader.load: loaded config");
 
-        // We run transform at this phase to strip out unused dependencies.
-        //
-        // Note that we don't apply compat transform at loading phase.
-        let program =
-            self.compiler
-                .parse_js(fm.clone(), JscTarget::Es2020, config.syntax, true, true)?;
+            // We run transform at this phase to strip out unused dependencies.
+            //
+            // Note that we don't apply compat transform at loading phase.
+            let program =
+                self.compiler
+                    .parse_js(fm.clone(), JscTarget::Es2020, config.syntax, true, true)?;
 
-        log::trace!("JsLoader.load: parsed");
+            log::trace!("JsLoader.load: parsed");
 
-        // Fold module
-        let program = helpers::HELPERS.set(&Helpers::new(true), || {
-            swc_ecma_utils::HANDLER.set(&self.compiler.handler, || {
-                let program = program.fold_with(&mut InlineGlobals {
-                    envs: env_map(),
-                    globals: Default::default(),
-                });
-                let program = program.fold_with(&mut expr_simplifier());
-                let program = program.fold_with(&mut dead_branch_remover());
+            // Fold module
+            let program = helpers::HELPERS.set(&Helpers::new(true), || {
+                swc_ecma_utils::HANDLER.set(&self.compiler.handler, || {
+                    let program = program.fold_with(&mut InlineGlobals {
+                        envs: env_map(),
+                        globals: Default::default(),
+                    });
+                    let program = program.fold_with(&mut expr_simplifier());
+                    let program = program.fold_with(&mut dead_branch_remover());
 
-                let program = program.fold_with(&mut config.pass);
+                    let program = program.fold_with(&mut config.pass);
 
-                program
-            })
-        });
+                    program
+                })
+            });
 
-        log::trace!("JsLoader.load: applied transforms");
+            log::trace!("JsLoader.load: applied transforms");
+
+            program
+        };
 
         match program {
             Program::Module(module) => Ok((fm, module)),
