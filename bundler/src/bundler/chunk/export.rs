9c35daf31bc854168ee95cd70fabe26a8d2910c0
강동윤 <kdy1997.dev@gmail.com>
2020-08-16 14:08:34 +0900

    Implement merging without hygiene

--- bundler/src/bundler/chunk/export.rs
+++ bundler/src/bundler/chunk/export.rs
@@ -1,6 +1,9 @@
+use super::merge::Unexporter;
 use crate::{bundler::load::TransformedModule, Bundler, Load, ModuleId, Resolve};
 use anyhow::{Context, Error};
-use swc_ecma_ast::Module;
+use std::mem::take;
+use swc_ecma_ast::*;
+use swc_ecma_visit::{FoldWith, VisitMut, VisitMutWith};
 
 impl<L, R> Bundler<'_, L, R>
 where
@@ -8,7 +11,7 @@ where
     R: Resolve,
 {
     pub(super) fn merge_reexports(
-        &mut self,
+        &self,
         entry: &mut Module,
         info: &TransformedModule,
         targets: &mut Vec<ModuleId>,
@@ -36,57 +39,88 @@ where
             // Tree-shaking
             dep = self.drop_unused(dep, Some(&specifiers));
 
-            if let Some(imports) = info
-                .exports
-                .reexports
-                .specifiers
-                .iter()
-                .find(|(s, _)| s.module_id == imported.id)
-                .map(|v| &v.1)
-            {
-                dep = dep.fold_with(&mut ExportRenamer {
-                    mark: imported.mark(),
-                    _exports: &imported.exports,
-                    imports: &imports,
-                    extras: vec![],
-                });
-            }
+            // if let Some(imports) = info
+            //     .exports
+            //     .reexports
+            //     .specifiers
+            //     .iter()
+            //     .find(|(s, _)| s.module_id == imported.id)
+            //     .map(|v| &v.1)
+            // {
+            //     dep = dep.fold_with(&mut ExportRenamer {
+            //         mark: imported.mark(),
+            //         _exports: &imported.exports,
+            //         imports: &imports,
+            //         extras: vec![],
+            //     });
+            // }
 
             dep = dep.fold_with(&mut Unexporter);
 
-            if !specifiers.is_empty() {
-                entry = entry.fold_with(&mut LocalMarker {
-                    mark: imported.mark(),
-                    specifiers: &specifiers,
-                    excluded: vec![],
-                });
+            //     entry = entry.fold_with(&mut LocalMarker {
+            //         mark: imported.mark(),
+            //         specifiers: &specifiers,
+            //         excluded: vec![],
+            //     });
 
-                // // Note: this does not handle `export default
-                // foo`
-                // dep = dep.fold_with(&mut LocalMarker {
-                //     mark: imported.mark(),
-                //     specifiers: &imported.exports.items,
-                // });
-            }
+            //     // // Note: this does not handle `export default
+            //     // foo`
+            //     // dep = dep.fold_with(&mut LocalMarker {
+            //     //     mark: imported.mark(),
+            //     //     specifiers: &imported.exports.items,
+            //     // });
+            // }
 
-            dep = dep.fold_with(&mut GlobalMarker {
-                used_mark: self.used_mark,
-                module_mark: imported.mark(),
-            });
+            // dep = dep.fold_with(&mut GlobalMarker {
+            //     used_mark: self.used_mark,
+            //     module_mark: imported.mark(),
+            // });
 
             // Replace import statement / require with module body
-            let mut injector = Es6ModuleInjector {
+            let mut injector = ExportInjector {
                 imported: dep.body.clone(),
                 src: src.src.clone(),
             };
             entry.body.visit_mut_with(&mut injector);
 
             // print_hygiene("entry:after:injection", &self.cm, &entry);
-            if injector.imported.is_empty() {
-                continue;
-            }
+            if injector.imported.is_empty() {}
         }
 
         Ok(())
+    }
+}
+
+struct ExportInjector {
+    imported: Vec<ModuleItem>,
+    src: Str,
+}
+
+impl VisitMut for ExportInjector {
+    fn visit_mut_module_items(&mut self, orig: &mut Vec<ModuleItem>) {
+        let items = take(orig);
+        let mut buf = Vec::with_capacity(self.imported.len() + items.len());
+
+        for item in items {
+            //
+            match item {
+                ModuleItem::ModuleDecl(ModuleDecl::ExportAll(export))
+                    if export.src.value == self.src.value =>
+                {
+                    buf.extend(take(&mut self.imported));
+                }
+
+                ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(NamedExport {
+                    src: Some(ref src),
+                    ..
+                })) if src.value == self.src.value => {
+                    buf.extend(take(&mut self.imported));
+                }
+
+                _ => buf.push(item),
+            }
+        }
+
+        *orig = buf;
     }
 }
