9bcdbe561d493b6ee64940ef33d2c4b0620c31e3
강동윤 <kdy1997.dev@gmail.com>
2020-08-17 14:31:53 +0900

    Update ref

---
+++ spack/tests/pass/reexport/recursive/output/entry.js
@@ -0,0 +1,12 @@
+const d = 'e';
+console.log('e');
+const d1 = d;
+console.log('d');
+const b = d1;
+console.log('c');
+const __default = b;
+console.log('b');
+const b1 = __default;
+console.log('a.js');
+export { b1 as b };
+console.log('entry');
