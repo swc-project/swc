9cdded95b71133c6894b8c5f78598bbb55b55cce
강동윤 <kdy1997.dev@gmail.com>
2020-08-20 16:13:16 +0900

    Add some tests for type resolver

--- ecmascript/transforms/src/typescript/resolver.rs
+++ ecmascript/transforms/src/typescript/resolver.rs
@@ -1,14 +1,25 @@
 use swc_common::Mark;
+use swc_ecma_ast::TsTypeRef;
 use swc_ecma_visit::{as_folder, Fold, VisitMut, VisitMutWith};
 
+#[cfg(test)]
+mod tests;
+
+/// `top_level_mark`  should match it provided to resolver.
 pub fn resolver(top_level_mark: Mark) -> impl Fold {
-    as_folder(Resolver { top_level_mark })
+    as_folder(Resolver {
+        _top_level_mark: top_level_mark,
+    })
 }
 
 struct Resolver {
-    top_level_mark: Mark,
+    _top_level_mark: Mark,
 }
 
 impl Resolver {}
 
-impl VisitMut for Resolver {}
+impl VisitMut for Resolver {
+    fn visit_mut_ts_type_ref(&mut self, r: &mut TsTypeRef) {
+        r.visit_mut_children_with(self);
+    }
+}
