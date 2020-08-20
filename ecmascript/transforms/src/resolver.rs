1d9642d08743e8c267d0c64275fd35bac5aa5402
강동윤 <kdy1997.dev@gmail.com>
2020-08-21 00:15:04 +0900

    fixup! TsConstructSignatureDecl

--- ecmascript/transforms/src/resolver.rs
+++ ecmascript/transforms/src/resolver.rs
@@ -352,7 +352,6 @@ impl<'a> Fold for Resolver<'a> {
 
     // WIP
 
-    // typed!(fold_ts_construct_signature_decl, TsConstructSignatureDecl);
     // typed!(fold_ts_constructor_type, TsConstructorType);
     // typed!(fold_ts_enum_decl, TsEnumDecl);
     // typed!(fold_ts_enum_member, TsEnumMember);
