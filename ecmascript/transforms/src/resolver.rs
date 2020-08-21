61adaebee8ffd056958fffdfe9bc00cb6428ead8
강동윤 <kdy1997.dev@gmail.com>
2020-08-21 10:38:10 +0900

    TsModuleBlock

--- ecmascript/transforms/src/resolver.rs
+++ ecmascript/transforms/src/resolver.rs
@@ -317,6 +317,7 @@ impl<'a> Fold for Resolver<'a> {
     typed!(fold_ts_type_element, TsTypeElement);
     typed!(fold_ts_module_decl, TsModuleDecl);
     typed!(fold_ts_signature_decl, TsSignatureDecl);
+    typed!(fold_ts_module_block, TsModuleBlock);
 
     fn fold_ts_tuple_element(&mut self, e: TsTupleElement) -> TsTupleElement {
         if !self.handle_types {
@@ -491,7 +492,6 @@ impl<'a> Fold for Resolver<'a> {
     // WIP
 
     typed!(fold_ts_import_equals_decl, TsImportEqualsDecl);
-    typed!(fold_ts_module_block, TsModuleBlock);
     typed!(fold_ts_namespace_body, TsNamespaceBody);
     typed!(fold_ts_namespace_decl, TsNamespaceDecl);
     typed!(fold_ts_namespace_export_decl, TsNamespaceExportDecl);
