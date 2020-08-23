8349dddc39faa3190cb12b6f1f5028ec40ed521c
강동윤 <kdy1997.dev@gmail.com>
2020-08-23 15:13:33 +0900

    Update ref

--- spack/tests/pass/issue-967-recursive-require/output/entry.js
+++ spack/tests/pass/issue-967-recursive-require/output/entry.js
@@ -14,33 +14,33 @@ function __spack_require__(mod) {
     };
 }
 var load = __spack_require__.bind(void 0, function(module, exports) {
-    module.exports = {
-        default: 'a-a-a'
-    };
-});
-var load1 = __spack_require__.bind(void 0, function(module, exports) {
-    console.log('a-b');
-    exports.default = 'ab';
-});
-var load2 = __spack_require__.bind(void 0, function(module, exports) {
-    module.exports = load();
-});
-var load3 = __spack_require__.bind(void 0, function(module, exports) {
-    console.log('b');
-});
-var load4 = __spack_require__.bind(void 0, function(module, exports) {
     console.log('c');
 });
-var load5 = __spack_require__.bind(void 0, function(module, exports) {
-    var aa = load2();
-    var bb = load1();
-    load3();
+var load1 = __spack_require__.bind(void 0, function(module, exports) {
+    var load2 = __spack_require__.bind(void 0, function(module, exports) {
+        console.log('a-b');
+        exports.default = 'ab';
+    });
+    var load3 = __spack_require__.bind(void 0, function(module, exports) {
+        var load4 = __spack_require__.bind(void 0, function(module, exports) {
+            module.exports = {
+                default: 'a-a-a'
+            };
+        });
+        module.exports = load4();
+    });
+    var load4 = __spack_require__.bind(void 0, function(module, exports) {
+        console.log('b');
+    });
+    var aa = load3();
+    var bb = load2();
+    load4();
     module.exports = {
         aa: aa,
         bb: bb
     };
 });
-load5();
-var b = load3();
-load4();
+load1();
+var b = require('./b');
+load();
 console.log(b);
