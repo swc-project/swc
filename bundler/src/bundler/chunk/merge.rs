312f08e9a65cf7e718a28262bf81e960a5d7ec46
강동윤 <kdy1997.dev@gmail.com>
2020-08-23 12:28:21 +0900

    Remove dbg!

--- bundler/src/bundler/chunk/merge.rs
+++ bundler/src/bundler/chunk/merge.rs
@@ -38,7 +38,6 @@ where
             // Handle circular imports
             if let Some(circular) = plan.entry_as_circular(info.id) {
                 log::info!("Circular dependency detected: ({})", info.fm.name);
-                dbg!(circular);
                 return Ok(self.merge_circular_modules(circular, entry));
             }
 
