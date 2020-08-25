13116bad9e1654cdfefb693407abb30c4b252403
강동윤 <kdy1997.dev@gmail.com>
2020-08-25 19:31:12 +0900

    More benchmarks

--- benches/typescript.rs
+++ benches/typescript.rs
@@ -34,50 +34,63 @@ fn bench_swc(b: &mut Bencher, opts: &Options) {
     });
 }
 
-#[bench]
-fn es2016_cjs(b: &mut Bencher) {
-    bench_swc(
-        b,
-        &Options {
-            config: Some(Config {
-                jsc: JscConfig {
-                    target: JscTarget::Es2016,
-                    syntax: Some(Syntax::Typescript(TsConfig {
+macro_rules! compat {
+    ($name:ident, $cjs_name:ident, $target:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            bench_swc(
+                b,
+                &Options {
+                    config: Some(Config {
+                        jsc: JscConfig {
+                            target: $target,
+                            syntax: Some(Syntax::Typescript(TsConfig {
+                                ..Default::default()
+                            })),
+                            ..Default::default()
+                        },
+                        module: None,
                         ..Default::default()
-                    })),
+                    }),
+                    swcrc: false,
+                    is_module: true,
                     ..Default::default()
                 },
-                module: Some(ModuleConfig::CommonJs(common_js::Config {
+            );
+        }
+
+        #[bench]
+        fn $cjs_name(b: &mut Bencher) {
+            bench_swc(
+                b,
+                &Options {
+                    config: Some(Config {
+                        jsc: JscConfig {
+                            target: $target,
+                            syntax: Some(Syntax::Typescript(TsConfig {
+                                ..Default::default()
+                            })),
+                            ..Default::default()
+                        },
+                        module: Some(ModuleConfig::CommonJs(common_js::Config {
+                            ..Default::default()
+                        })),
+                        ..Default::default()
+                    }),
+                    swcrc: false,
+                    is_module: true,
                     ..Default::default()
-                })),
-                ..Default::default()
-            }),
-            swcrc: false,
-            is_module: true,
-            ..Default::default()
-        },
-    );
+                },
+            );
+        }
+    };
 }
 
-#[bench]
-fn es2016(b: &mut Bencher) {
-    bench_swc(
-        b,
-        &Options {
-            config: Some(Config {
-                jsc: JscConfig {
-                    target: JscTarget::Es2016,
-                    syntax: Some(Syntax::Typescript(TsConfig {
-                        ..Default::default()
-                    })),
-                    ..Default::default()
-                },
-                module: None,
-                ..Default::default()
-            }),
-            swcrc: false,
-            is_module: true,
-            ..Default::default()
-        },
-    );
-}
+compat!(es3, es3_cjs, JscTarget::Es3);
+compat!(es5, es5_cjs, JscTarget::Es5);
+compat!(es2015, es2015_cjs, JscTarget::Es2015);
+compat!(es2016, es2016_cjs, JscTarget::Es2016);
+compat!(es2017, es2017_cjs, JscTarget::Es2017);
+compat!(es2018, es2018_cjs, JscTarget::Es2018);
+compat!(es2019, es2019_cjs, JscTarget::Es2019);
+compat!(es2020, es2020_cjs, JscTarget::Es2020);
