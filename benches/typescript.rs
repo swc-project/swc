3e6f70d1f197c8668575772519ceb3b673199286
강동윤 <kdy1997.dev@gmail.com>
2020-08-25 21:31:27 +0900

    Add a test for print

--- benches/typescript.rs
+++ benches/typescript.rs
@@ -3,14 +3,14 @@
 extern crate test;
 
 use std::{hint::black_box, sync::Arc};
-use swc::config::{Config, JscConfig, ModuleConfig, Options};
+use swc::config::{Config, JscConfig, Options, SourceMapsConfig};
 use swc_common::{
     errors::{ColorConfig, Handler},
     FileName, FilePathMapping, SourceMap,
 };
 use swc_ecma_ast::Program;
 use swc_ecma_parser::{JscTarget, Syntax, TsConfig};
-use swc_ecma_transforms::{compat, modules::common_js, typescript};
+use swc_ecma_transforms::{compat, typescript};
 use swc_ecma_visit::FoldWith;
 use test::Bencher;
 
@@ -100,7 +100,67 @@ fn base_tr_es2018(b: &mut Bencher) {
     b.iter(|| black_box(module.clone().fold_with(&mut compat::es2018())));
 }
 
-fn bench_swc(b: &mut Bencher, opts: &Options) {
+/// This benchmark exists to know exact execution time of each pass.
+#[bench]
+fn config_for_file(b: &mut Bencher) {
+    let c = mk();
+
+    b.iter(|| {
+        black_box(c.config_for_file(
+            &Options {
+                config: Some(Config {
+                    jsc: JscConfig {
+                        target: JscTarget::Es2016,
+                        syntax: Some(Syntax::Typescript(TsConfig {
+                            ..Default::default()
+                        })),
+                        ..Default::default()
+                    },
+                    module: None,
+                    ..Default::default()
+                }),
+                swcrc: false,
+                is_module: true,
+                ..Default::default()
+            },
+            &FileName::Real("rxjs/src/internal/observable/dom/AjaxObservable.ts".into()),
+        ))
+    });
+}
+
+fn bench_codegen(b: &mut Bencher, _target: JscTarget) {
+    let c = mk();
+    let module = as_es(&c);
+
+    //TODO: Use target
+
+    b.iter(|| {
+        black_box(
+            c.print(&module, SourceMapsConfig::Bool(false), None, false)
+                .unwrap(),
+        );
+    })
+}
+
+macro_rules! codegen {
+    ($name:ident, $target:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            bench_codegen(b, $target);
+        }
+    };
+}
+
+codegen!(codegen_es3, JscTarget::Es3);
+codegen!(codegen_es5, JscTarget::Es5);
+codegen!(codegen_es2015, JscTarget::Es2015);
+codegen!(codegen_es2016, JscTarget::Es2016);
+codegen!(codegen_es2017, JscTarget::Es2017);
+codegen!(codegen_es2018, JscTarget::Es2018);
+codegen!(codegen_es2019, JscTarget::Es2019);
+codegen!(codegen_es2020, JscTarget::Es2020);
+
+fn bench_full(b: &mut Bencher, opts: &Options) {
     let c = mk();
 
     b.iter(|| {
@@ -113,10 +173,10 @@ fn bench_swc(b: &mut Bencher, opts: &Options) {
 }
 
 macro_rules! compat {
-    ($name:ident, $cjs_name:ident, $target:expr) => {
+    ($name:ident, $target:expr) => {
         #[bench]
         fn $name(b: &mut Bencher) {
-            bench_swc(
+            bench_full(
                 b,
                 &Options {
                     config: Some(Config {
@@ -136,39 +196,14 @@ macro_rules! compat {
                 },
             );
         }
-
-        #[bench]
-        fn $cjs_name(b: &mut Bencher) {
-            bench_swc(
-                b,
-                &Options {
-                    config: Some(Config {
-                        jsc: JscConfig {
-                            target: $target,
-                            syntax: Some(Syntax::Typescript(TsConfig {
-                                ..Default::default()
-                            })),
-                            ..Default::default()
-                        },
-                        module: Some(ModuleConfig::CommonJs(common_js::Config {
-                            ..Default::default()
-                        })),
-                        ..Default::default()
-                    }),
-                    swcrc: false,
-                    is_module: true,
-                    ..Default::default()
-                },
-            );
-        }
     };
 }
 
-compat!(es3, es3_cjs, JscTarget::Es3);
-compat!(es5, es5_cjs, JscTarget::Es5);
-compat!(es2015, es2015_cjs, JscTarget::Es2015);
-compat!(es2016, es2016_cjs, JscTarget::Es2016);
-compat!(es2017, es2017_cjs, JscTarget::Es2017);
-compat!(es2018, es2018_cjs, JscTarget::Es2018);
-compat!(es2019, es2019_cjs, JscTarget::Es2019);
-compat!(es2020, es2020_cjs, JscTarget::Es2020);
+compat!(full_es3, JscTarget::Es3);
+compat!(full_es5, JscTarget::Es5);
+compat!(full_es2015, JscTarget::Es2015);
+compat!(full_es2016, JscTarget::Es2016);
+compat!(full_es2017, JscTarget::Es2017);
+compat!(full_es2018, JscTarget::Es2018);
+compat!(full_es2019, JscTarget::Es2019);
+compat!(full_es2020, JscTarget::Es2020);
