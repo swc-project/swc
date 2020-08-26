979994ed6d5f3450703aef272f2b9df5b360280e
강동윤 <kdy1997.dev@gmail.com>
2020-08-26 15:03:10 +0900

    Use method instead of macro

--- ecmascript/transforms/benches/compat.rs
+++ ecmascript/transforms/benches/compat.rs
@@ -6,42 +6,41 @@ use swc_common::{chain, FileName, Mark};
 use swc_ecma_ast::Module;
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
 use swc_ecma_transforms::{compat, helpers, typescript};
-use swc_ecma_visit::Fold;
+use swc_ecma_visit::{Fold, FoldWith};
 use test::Bencher;
 
 static SOURCE: &str = include_str!("assets/AjaxObservable.ts");
 
-/// Benchmark a folder
-macro_rules! tr {
-    ($b:expr, $tr:expr) => {
-        use swc_ecma_visit::FoldWith;
-        $b.bytes = SOURCE.len() as _;
+fn run<V>(b: &mut Bencher, tr: impl Fn() -> V)
+where
+    V: Fold,
+{
+    b.bytes = SOURCE.len() as _;
 
-        let _ = ::testing::run_test(false, |cm, _| {
-            let fm = cm.new_source_file(FileName::Anon, SOURCE.into());
-            let lexer = Lexer::new(
-                Syntax::Typescript(Default::default()),
-                Default::default(),
-                StringInput::from(&*fm),
-                None,
-            );
-            let mut parser = Parser::new_from(lexer);
-            let module = parser.parse_module().map_err(|_| ()).unwrap();
-            let module = module.fold_with(&mut typescript::strip());
+    let _ = ::testing::run_test(false, |cm, _| {
+        let fm = cm.new_source_file(FileName::Anon, SOURCE.into());
+        let lexer = Lexer::new(
+            Syntax::Typescript(Default::default()),
+            Default::default(),
+            StringInput::from(&*fm),
+            None,
+        );
+        let mut parser = Parser::new_from(lexer);
+        let module = parser.parse_module().map_err(|_| ()).unwrap();
+        let module = module.fold_with(&mut typescript::strip());
 
-            $b.iter(|| {
-                let module = module.clone();
+        b.iter(|| {
+            let module = module.clone();
 
-                let _ = helpers::HELPERS.set(&Default::default(), || {
-                    let mut tr = $tr();
+            let _ = helpers::HELPERS.set(&Default::default(), || {
+                let mut tr = tr();
 
-                    test::black_box(module.fold_with(&mut tr));
-                });
+                test::black_box(module.fold_with(&mut tr));
             });
-
-            Ok(())
         });
-    };
+
+        Ok(())
+    });
 }
 
 #[bench]
@@ -53,186 +52,185 @@ fn base(b: &mut Bencher) {
             m
         }
     }
-    tr!(b, || Noop);
+    run(b, || Noop);
 }
 
 #[bench]
 fn common_reserved_word(b: &mut Bencher) {
-    tr!(b, || compat::reserved_words::reserved_words());
+    run(b, || compat::reserved_words::reserved_words());
 }
 
 #[bench]
 fn es2020(b: &mut Bencher) {
-    tr!(b, || compat::es2020());
+    run(b, || compat::es2020());
 }
 
 #[bench]
 fn es2020_nullish_coalescing(b: &mut Bencher) {
-    tr!(b, || compat::es2020::nullish_coalescing());
+    run(b, || compat::es2020::nullish_coalescing());
 }
 
 #[bench]
 fn es2020_optional_chaining(b: &mut Bencher) {
-    tr!(b, || compat::es2020::optional_chaining());
+    run(b, || compat::es2020::optional_chaining());
 }
 
 #[bench]
 fn es2020_typescript_class_properties(b: &mut Bencher) {
-    tr!(b, || compat::es2020::typescript_class_properties());
+    run(b, || compat::es2020::typescript_class_properties());
 }
 
 #[bench]
 fn es2020_class_properties(b: &mut Bencher) {
-    tr!(b, || compat::es2020::class_properties());
+    run(b, || compat::es2020::class_properties());
 }
 
 #[bench]
 fn es2018(b: &mut Bencher) {
-    tr!(b, || compat::es2018());
+    run(b, || compat::es2018());
 }
 
 #[bench]
 fn es2018_object_rest_spread(b: &mut Bencher) {
-    tr!(b, || compat::es2018::object_rest_spread());
+    run(b, || compat::es2018::object_rest_spread());
 }
 
 #[bench]
 fn es2018_optional_catch_binding(b: &mut Bencher) {
-    tr!(b, || compat::es2018::optional_catch_binding());
+    run(b, || compat::es2018::optional_catch_binding());
 }
 
 #[bench]
 fn es2017(b: &mut Bencher) {
-    tr!(b, || compat::es2017());
+    run(b, || compat::es2017());
 }
 
 #[bench]
 fn es2017_async_to_generator(b: &mut Bencher) {
-    tr!(b, || compat::es2017::async_to_generator());
+    run(b, || compat::es2017::async_to_generator());
 }
 
 #[bench]
 fn es2016(b: &mut Bencher) {
-    tr!(b, || compat::es2016());
+    run(b, || compat::es2016());
 }
 
 #[bench]
 fn es2016_exponentation(b: &mut Bencher) {
-    tr!(b, || compat::es2016::exponentation());
+    run(b, || compat::es2016::exponentation());
 }
 
 #[bench]
 fn es2015(b: &mut Bencher) {
-    tr!(b, || compat::es2015(
-        Mark::fresh(Mark::root()),
-        Default::default()
-    ));
+    run(b, || {
+        compat::es2015(Mark::fresh(Mark::root()), Default::default())
+    });
 }
 
 #[bench]
 fn es2015_arrow(b: &mut Bencher) {
-    tr!(b, || compat::es2015::arrow());
+    run(b, || compat::es2015::arrow());
 }
 
 #[bench]
 fn es2015_block_scoped_fn(b: &mut Bencher) {
-    tr!(b, || compat::es2015::block_scoped_functions());
+    run(b, || compat::es2015::block_scoped_functions());
 }
 
 #[bench]
 fn es2015_block_scoping(b: &mut Bencher) {
-    tr!(b, || compat::es2015::block_scoping());
+    run(b, || compat::es2015::block_scoping());
 }
 
 #[bench]
 fn es2015_classes(b: &mut Bencher) {
-    tr!(b, || compat::es2015::classes());
+    run(b, || compat::es2015::classes());
 }
 
 #[bench]
 fn es2015_computed_props(b: &mut Bencher) {
-    tr!(b, compat::es2015::computed_properties);
+    run(b, compat::es2015::computed_properties);
 }
 
 #[bench]
 fn es2015_destructuring(b: &mut Bencher) {
-    tr!(b, || compat::es2015::destructuring(Default::default()));
+    run(b, || compat::es2015::destructuring(Default::default()));
 }
 
 #[bench]
 fn es2015_duplicate_keys(b: &mut Bencher) {
-    tr!(b, || compat::es2015::duplicate_keys());
+    run(b, || compat::es2015::duplicate_keys());
 }
 
 #[bench]
 fn es2015_parameters(b: &mut Bencher) {
-    tr!(b, || compat::es2015::parameters());
+    run(b, || compat::es2015::parameters());
 }
 
 #[bench]
 fn es2015_fn_name(b: &mut Bencher) {
-    tr!(b, || compat::es2015::function_name());
+    run(b, || compat::es2015::function_name());
 }
 
 #[bench]
 fn es2015_for_of(b: &mut Bencher) {
-    tr!(b, || compat::es2015::for_of(Default::default()));
+    run(b, || compat::es2015::for_of(Default::default()));
 }
 
 #[bench]
 fn es2015_instanceof(b: &mut Bencher) {
-    tr!(b, || compat::es2015::instance_of());
+    run(b, || compat::es2015::instance_of());
 }
 
 #[bench]
 fn es2015_shorthand_property(b: &mut Bencher) {
-    tr!(b, || compat::es2015::shorthand());
+    run(b, || compat::es2015::shorthand());
 }
 
 #[bench]
 fn es2015_spread(b: &mut Bencher) {
-    tr!(b, || compat::es2015::spread(
-        compat::es2015::spread::Config {
+    run(b, || {
+        compat::es2015::spread(compat::es2015::spread::Config {
             ..Default::default()
-        }
-    ));
+        })
+    });
 }
 
 #[bench]
 fn es2015_sticky_regex(b: &mut Bencher) {
-    tr!(b, || compat::es2015::sticky_regex());
+    run(b, || compat::es2015::sticky_regex());
 }
 
 #[bench]
 fn es2015_typeof_symbol(b: &mut Bencher) {
-    tr!(b, || compat::es2015::typeof_symbol());
+    run(b, || compat::es2015::typeof_symbol());
 }
 
 #[bench]
 fn es3(b: &mut Bencher) {
-    tr!(b, || compat::es3(Default::default()));
+    run(b, || compat::es3(Default::default()));
 }
 
 #[bench]
 fn full_es2016(b: &mut Bencher) {
-    tr!(b, || chain!(
-        compat::es2020(),
-        compat::es2018(),
-        compat::es2017(),
-        compat::es2016(),
-    ));
+    run(b, || {
+        chain!(
+            compat::es2020(),
+            compat::es2018(),
+            compat::es2017(),
+            compat::es2016(),
+        )
+    });
 }
 
 #[bench]
 fn full_es2017(b: &mut Bencher) {
-    tr!(b, || chain!(
-        compat::es2020(),
-        compat::es2018(),
-        compat::es2017(),
-    ));
+    run(b, || {
+        chain!(compat::es2020(), compat::es2018(), compat::es2017(),)
+    });
 }
 
 #[bench]
 fn full_es2018(b: &mut Bencher) {
-    tr!(b, || chain!(compat::es2020(), compat::es2018(),));
+    run(b, || chain!(compat::es2020(), compat::es2018(),));
 }
