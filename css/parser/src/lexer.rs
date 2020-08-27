b5085b89424f5ffd610b27f452c575ecb3c45604
강동윤 <kdy1997.dev@gmail.com>
2020-08-27 23:47:07 +0900

    lexer::parse_name

--- css/parser/src/lexer.rs
+++ css/parser/src/lexer.rs
@@ -1,6 +1,21 @@
 //! https://www.w3.org/TR/selectors-3/#lex
 
-use crate::{Input, PResult};
+use crate::{util::PResultExt, Input, PResult};
+use nom::bytes::complete::take_while1;
 use swc_css_ast::*;
 
-pub(crate) fn parse_ident(i: Input) -> PResult<Text> {}
+// pub(crate) fn parse_ident(i: Input) -> PResult<Text> {}
+
+pub(crate) fn parse_name(i: Input) -> PResult<Text> {
+    take_while1(is_name_char)(i).map_from()
+}
+
+/// `[_a-z0-9-]|{nonascii}|{escape}`
+fn is_name_char(c: char) -> bool {
+    match c {
+        '_' | 'a'..='z' | 'A'..='Z' | '0'..='9' | '-' => true,
+        // TODO: nonascii
+        // TODO: escape
+        _ => false,
+    }
+}
