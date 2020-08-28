dfd568cbc0ff1cabd9926c24ffd19e88dd31906f
강동윤 <kdy1997.dev@gmail.com>
2020-08-28 22:50:16 +0900

    prepublish script

---
+++ scripts/npm/prepublish.js
@@ -0,0 +1,26 @@
+const { execSync } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+const { version } = require('../../package.json')
+const platforms = require('./platforms')
+const updatePackageJson = require('./update-package')
+
+updatePackageJson(path.join(__dirname, 'package.json'), {
+    optionalDependencies: platforms.reduce((acc, cur) => {
+        acc[`@swc-node/core-${cur}`] = `^${version}`
+        return acc
+    }, {}),
+})
+
+for (const name of [...platforms, 'linux-musl']) {
+    const pkgDir = path.join(__dirname, 'npm', `core-${name}`)
+    const filename = `swc.${name}.node`
+    const bindingFile = fs.readFileSync(path.join(__dirname, `bindings-${name}`, filename))
+    fs.writeFileSync(path.join(pkgDir, filename), bindingFile)
+    execSync('npm publish', {
+        cwd: pkgDir,
+        env: process.env,
+        stdio: 'inherit',
+    })
+}
