ab3555f58ae921b0a221c1e99eadcde1316f1ba3
강동윤 <kdy1997.dev@gmail.com>
2020-08-28 18:55:10 +0900

    fixup! napi_serde: WIP

--- native/src/napi_serde/ser.rs
+++ native/src/napi_serde/ser.rs
@@ -63,74 +63,80 @@ impl<'env> Serializer for Ser<'env> {
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_int32(v as _)?.into_unknown()?)
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_int32(v as _)?.into_unknown()?)
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_int32(v)?.into_unknown()?)
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_int64(v)?.into_unknown()?)
     }
 
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_uint32(v as _)?.into_unknown()?)
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_uint32(v as _)?.into_unknown()?)
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_uint32(v as _)?.into_unknown()?)
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_int64(v as _)?.into_unknown()?)
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_double(v as _)?.into_unknown()?)
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_double(v as _)?.into_unknown()?)
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self
+            .env
+            .create_string_from_std(v.to_string())?
+            .into_unknown()?)
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.create_string(v)?.into_unknown()?)
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self
+            .env
+            .create_buffer_with_data(v.to_vec())?
+            .into_unknown()?)
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.get_null()?.into_unknown()?)
     }
 
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: serde::Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.get_null()?.into_unknown()?)
     }
 
     fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.env.get_null()?.into_unknown()?)
     }
 
     fn serialize_unit_variant(
@@ -139,7 +145,7 @@ impl<'env> Serializer for Ser<'env> {
         variant_index: u32,
         variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        self.serialize_str(variant)
     }
 
     fn serialize_newtype_struct<T: ?Sized>(
@@ -150,7 +156,7 @@ impl<'env> Serializer for Ser<'env> {
     where
         T: serde::Serialize,
     {
-        todo!()
+        value.serialize(self)
     }
 
     fn serialize_newtype_variant<T: ?Sized>(
