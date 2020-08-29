0bcbe223550c2b6210766ed6d35ace5470e9f29f
강동윤 <kdy1997.dev@gmail.com>
2020-08-29 16:53:48 +0900

    Ignore a test on windows

--- node-swc/__tests__/spack/multi_entry_test.js
+++ node-swc/__tests__/spack/multi_entry_test.js
@@ -1,5 +1,6 @@
 const swc = require("../../..");
 const path = require('path');
+const os = require('os');
 
 
 it('should handle multiple entries on same level', async () => {
@@ -17,18 +18,19 @@ it('should handle multiple entries on same level', async () => {
     expect(result.b.code).toContain(`import { foo } from './common-`);
 });
 
+if (os.platform() !== 'win32') {
+    it('should handle multiple entries on different level', async () => {
+        const result = await swc.bundle({
+            entry: {
+                web: path.join(__dirname, '../../tests/spack/mutli-entry-different-level/src/web/index.js'),
+                a: path.join(__dirname, '../../tests/spack/mutli-entry-different-level/src/a.js'),
+            }
+        });
 
-it('should handle multiple entries on different level', async () => {
-    const result = await swc.bundle({
-        entry: {
-            web: path.join(__dirname, '../../tests/spack/mutli-entry-different-level/src/web/index.js'),
-            a: path.join(__dirname, '../../tests/spack/mutli-entry-different-level/src/a.js'),
-        }
+        expect(result.a).toBeTruthy();
+        expect(result.a.code).toContain(`import { foo } from './common-`);
+
+        expect(result.web).toBeTruthy();
+        expect(result.web.code).toContain(`../common`);
     });
-
-    expect(result.a).toBeTruthy();
-    expect(result.a.code).toContain(`import { foo } from './common-`);
-
-    expect(result.web).toBeTruthy();
-    expect(result.web.code).toContain(`../common`);
-});
+}
