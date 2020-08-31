b0fde96e0264f407750eb140f9ea6f39fcc142e3
강동윤 <kdy1997.dev@gmail.com>
2020-08-31 16:21:59 +0900

    Doc

---
+++ bundler/src/bundler/computed_key.rs
@@ -0,0 +1,33 @@
+use super::load::TransformedModule;
+use crate::{Bundler, Load, Resolve};
+use anyhow::Error;
+use swc_ecma_ast::Module;
+
+impl<L, R> Bundler<'_, L, R>
+where
+    L: Load,
+    R: Resolve,
+{
+    //  Converts
+    ///
+    /// ```ts
+    /// export const arr = [1, 2, 3];
+    /// ```
+    ///
+    /// to
+    ///
+    /// ```ts
+    /// const _mod = (function(){
+    ///     const arr = [1, 2, 3];
+    ///     return {
+    ///         arr,
+    ///     };
+    /// })();
+    /// ```
+    pub(super) fn wrap_esm_as_a_var(
+        &self,
+        info: &TransformedModule,
+        module: Module,
+    ) -> Result<Module, Error> {
+    }
+}
