1b6f7951b27350643eb5e0e501217c9a9e589cfb
강동윤 <kdy1997.dev@gmail.com>
2020-09-02 00:13:04 +0900

    Update ref

---
+++ spack/tests/pass/cjs/mixed/output/entry.js
@@ -0,0 +1,23 @@
+function __spack_require__(mod) {
+    var cache;
+    return function() {
+        if (cache) {
+            return cache;
+        }
+        var module = {
+            exports: {
+            }
+        };
+        mod(module, module.exports);
+        cache = module.exports;
+        return cache;
+    };
+}
+var load = __spack_require__.bind(void 0, function(module, exports) {
+    var load1 = __spack_require__.bind(void 0, function(module1, exports1) {
+        module1.exports = 1;
+    });
+    module.exports = load1();
+});
+var { default: a  } = load();
+console.log(a);
