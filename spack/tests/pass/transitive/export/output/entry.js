6205eaf818d2173e862959f06eba5dd58d609e97
강동윤 <kdy1997.dev@gmail.com>
2020-09-03 20:21:04 +0900

    Update ref

--- spack/tests/pass/transitive/export/output/entry.js
+++ spack/tests/pass/transitive/export/output/entry.js
@@ -1,9 +1,8 @@
 const foo = 1;
 const bar = 1;
-const foo1 = foo, bar1 = bar;
+const baz = 1;
+const foo1 = foo, bar1 = bar, baz1 = baz;
 const foo2 = foo1, bar2 = bar1;
 export { foo2 as foo, bar2 as bar };
-const baz = 1;
-const baz1 = baz;
 const baz2 = baz1;
 export { baz2 as baz };
