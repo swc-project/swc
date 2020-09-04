8d434eecd0e1ab428271612ee6e5936aca803d44
강동윤 <kdy1997.dev@gmail.com>
2020-09-04 16:37:13 +0900

    Run inliner

--- bundler/src/bundler/optimize.rs
+++ bundler/src/bundler/optimize.rs
@@ -1,6 +1,6 @@
 use crate::{Bundler, Load, Resolve};
 use swc_ecma_ast::*;
-use swc_ecma_transforms::optimization::simplify::dce;
+use swc_ecma_transforms::optimization::simplify::{dce, inlining};
 use swc_ecma_visit::FoldWith;
 
 impl<L, R> Bundler<'_, L, R>
@@ -14,29 +14,12 @@ where
     /// other module.
     pub(super) fn optimize(&self, node: Module) -> Module {
         self.run(|| {
-            // let mut used = vec![];
-
-            // if let Some(used_exports) = used_exports {
-            //     for export in used_exports {
-            //         match export {
-            //             Specifier::Specific { alias, local, .. } => {
-            //                 used.push(alias.as_ref().unwrap_or(local).to_id());
-            //             }
-            //             Specifier::Namespace { local, .. } => {
-            //                 used.push(local.to_id());
-            //             }
-            //         }
-            //     }
-            // }
-
-            let used_mark = self.used_mark;
-
-            let mut v = dce::dce(dce::Config {
-                used: None,
-                used_mark,
-            });
-
-            let node = node.fold_with(&mut v);
+            let node = node
+                .fold_with(&mut dce::dce(dce::Config {
+                    used: None,
+                    used_mark: self.used_mark,
+                }))
+                .fold_with(&mut inlining::inlining(inlining::Config {}));
             node
         })
     }
