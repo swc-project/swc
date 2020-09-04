4658c53698f74db6593a9297e0f5cb230b5c1427
강동윤 <kdy1997.dev@gmail.com>
2020-09-04 20:36:23 +0900

    Update ref

--- spack/tests/pass/transitive/export-all-1/output/entry.js
+++ spack/tests/pass/transitive/export-all-1/output/entry.js
@@ -1,7 +1,7 @@
-const c = 3;
-export { c };
-const d = 4;
-export { d };
+const c1 = 3;
+export { c1 as c };
+const d1 = 4;
+export { d1 as d };
 export const b = 1;
-const e = 5;
-export { e };
+const e1 = 5;
+export { e1 as e };
