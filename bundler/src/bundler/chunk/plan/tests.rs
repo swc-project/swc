783ae80ef1aac1c86af796acf647afbaf4d33882
강동윤 <kdy1997.dev@gmail.com>
2020-09-07 16:57:03 +0900

    Add a test for cjs planning

--- bundler/src/bundler/chunk/plan/tests.rs
+++ bundler/src/bundler/chunk/plan/tests.rs
@@ -445,3 +445,46 @@ fn cjs_002() {
             Ok(())
         });
 }
+
+#[test]
+fn cjs_003() {
+    suite()
+        .file(
+            "main.js",
+            "
+                import './a';
+                import './b';
+                ",
+        )
+        .file(
+            "a.js",
+            "
+                module.exports = require('./common')
+                ",
+        )
+        .file(
+            "b.js",
+            "
+                module.exports = require('./common')
+                ",
+        )
+        .run(|t| {
+            let module = t
+                .bundler
+                .load_transformed(&FileName::Real("main.js".into()))?
+                .unwrap();
+            let mut entries = HashMap::default();
+            entries.insert("main.js".to_string(), module);
+
+            let p = t.bundler.determine_entries(entries)?;
+
+            dbg!(&p);
+
+            assert_eq!(p.circular.len(), 0);
+            assert_normal_transitive(t, &p, "main", &["a", "b"], &["common"]);
+            assert_normal(t, &p, "a", &["common"]);
+            assert_normal(t, &p, "b", &["common"]);
+
+            Ok(())
+        });
+}
