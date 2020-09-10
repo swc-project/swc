53591de5bbbf0a67038de54b1c876c7313aeb720
강동윤 <kdy1997.dev@gmail.com>
2020-09-10 17:16:38 +0900

    WIP: Fixing numbers

--- ecmascript/parser/src/lexer/number.rs
+++ ecmascript/parser/src/lexer/number.rs
@@ -42,56 +42,16 @@ impl<'a> Lexer<'a> {
                 }
 
                 let starts_with_zero = s.starts_with('0');
+                let mut iter = s.chars();
 
                 let val = if starts_with_dot {
                     // first char is '.'
                     0f64
                 } else {
                     // Use read_number_no_dot to support long numbers.
-                    let val = self.read_number_no_dot_as_str(10)?;
-
-                    if starts_with_zero {
-                        // TODO: I guess it would be okay if I don't use -ffast-math
-                        // (or something like that), but needs review.
-
-                        if val == 0.0f64 {
-                            // If only one zero is used, it's decimal.
-                            // And if multiple zero is used, it's octal.
-                            //
-                            // e.g. `0` is decimal (so it can be part of float)
-                            //
-                            // e.g. `000` is octal
-                            if 1 < s.len() {
-                                // `-1` is utf 8 length of `0`
-
-                                return self.make_legacy_octal(span, 0f64);
-                            }
-                        } else {
-                            // strict mode hates non-zero decimals starting with zero.
-                            // e.g. 08.1 is strict mode violation but
-                            // 0.1 is valid float.
-
-                            if val.fract() < 1e-10 {
-                                let d = digits(val.round() as u64, 10);
-
-                                // if it contains '8' or '9', it's decimal.
-                                if d.clone().any(|v| v == 8 || v == 9) {
-                                    if self.ctx.strict {
-                                        self.error_span(span, SyntaxError::LegacyDecimal)?
-                                    }
-                                } else {
-                                    // It's Legacy octal, and we should reinterpret value.
-                                    let val = u64::from_str_radix(&val.to_string(), 8)
-                                        .expect("Does this can really happen?");
-                                    let val = val
-                                        .to_string()
-                                        .parse()
-                                        .expect("failed to parse numeric value as f64");
-                                    return self.make_legacy_octal(span, val);
-                                }
-                            }
-                        }
-                    }
+                    let val = self.read_digits(&mut iter, 10, |total, radix, value| {
+                        f64::mul_add(total, radix as _, value as _)
+                    })?;
 
                     val
                 };
@@ -103,8 +63,8 @@ impl<'a> Lexer<'a> {
                 //  `0.a`, `08.a`, `102.a` are invalid.
                 //
                 // `.1.a`, `.1e-4.a` are valid,
-                if self.cur() == Some(itok!(".")) {
-                    self.bump(); // '.'
+                if iter.clone().next() == Some('.') {
+                    iter.next(); // '.'
 
                     if starts_with_dot {
                         // debug_assert!(self.cur().is_some());
@@ -113,7 +73,7 @@ impl<'a> Lexer<'a> {
 
                     let mut raw = Raw(Some(String::new()));
                     // Read numbers after dot
-                    let dec_val = self.read_int(10, 0, &mut raw)?;
+                    let dec_val = self.parse_int(&mut iter, 10, 0, &mut raw)?;
                     val = self.with_buf(|_, s| {
                         write!(s, "{}.", val).unwrap();
 
@@ -121,10 +81,7 @@ impl<'a> Lexer<'a> {
                             s.push_str(&raw.0.as_ref().unwrap());
                         }
 
-                        Ok(s.parse().expect(
-                            "failed to parse float using rust's
-                impl",
-                        ))
+                        Ok(s.parse().expect("failed to parse float using rust's impl"))
                     })?;
                 }
 
@@ -134,8 +91,12 @@ impl<'a> Lexer<'a> {
                 // 1e2 = 100
                 // 1e+2 = 100
                 // 1e-2 = 0.01
-                if self.input.eat(b'e') || self.input.eat(b'E') {
-                    let next = match self.input.cur_char() {
+                if match iter.clone().next() {
+                    Some('e') | Some('E') => true,
+                    _ => false,
+                } {
+                    iter.next();
+                    let next = match iter.clone().next() {
                         Some(next) => next,
                         None => {
                             let pos = self.cur_pos();
@@ -144,13 +105,15 @@ impl<'a> Lexer<'a> {
                     };
 
                     let positive = if next == '+' || next == '-' {
-                        self.bump(); // remove '+', '-'
+                        iter.next(); // '+' or '-'
                         next == '+'
                     } else {
                         true
                     };
 
-                    let exp = self.read_number_no_dot(10)?;
+                    let exp = self.read_digits(&mut iter, 10, |total, radix, value| {
+                        f64::mul_add(total, radix as _, value as _)
+                    })?;
                     let flag = if positive { '+' } else { '-' };
                     // TODO:
                     val = format!("{}e{}{}", val, flag, exp)
