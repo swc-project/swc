81301ed59056a35ec5288263f31d83b71fd4d1be
강동윤 <kdy1997.dev@gmail.com>
2020-09-11 14:35:19 +0900

    Fix

--- tests/projects.rs
+++ tests/projects.rs
@@ -329,21 +329,12 @@ fn issue_532() {
     assert_eq!(
         f,
         "\
-/*pre:1*/
-test();
-test(123/*post:3*/
-);
-test(/*pre:4*/
-123);
-test(/*pre:5*/
-123/*post:6*/
-);
-test(/*pre:7*/
-123,/*pre:8*/
-456);
-test(123/*post:9*/
-,456/*post:10*/
-);"
+/*pre:1*/test();
+test(123/*post:3*/);
+test(/*pre:4*/123);
+test(/*pre:5*/123/*post:6*/);
+test(/*pre:7*/123,/*pre:8*/456);
+test(123/*post:9*/,456/*post:10*/);"
     )
 }
 
