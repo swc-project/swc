95d32b9e73642a7975cd42ff2f824962a45ab68b
강동윤 <kdy1997.dev@gmail.com>
2020-09-11 19:15:01 +0900

    Update constructor test

--- ecmascript/transforms/src/resolver/tests.rs
+++ ecmascript/transforms/src/resolver/tests.rs
@@ -1518,7 +1518,23 @@ to_ts!(
         }
     }
     "#,
-    ""
+    r#"
+    class PartWriter {
+        constructor(private writer__2: Deno.Writer., readonly boundary__2: string, public headers__2: Headers, isFirstBoundary__2: boolean){
+            let buf__2 = "";
+            if (isFirstBoundary__2) {
+                buf__2 += `--${boundary__2}\r\n`;
+            } else {
+                buf__2 += `\r\n--${boundary__2}\r\n`;
+            }
+            for (const [key__2, value__2] of headers__2.entries()){
+                buf__2 += `${key__2}: ${value__2}\r\n`;
+            }
+            buf__2 += `\r\n`;
+            this.partHeader = buf__2;
+        }
+    }
+    "#
 );
 
 to_ts!(
