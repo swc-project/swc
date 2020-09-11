892b77dcce9d16b63b1bb538613e09bea372c4c7
강동윤 <kdy1997.dev@gmail.com>
2020-09-11 12:50:07 +0900

    Don't emit newline after block comment

--- ecmascript/codegen/src/comments.rs
+++ ecmascript/codegen/src/comments.rs
@@ -25,7 +25,6 @@ macro_rules! write_comments {
                     $e.wr.write_comment(cmt.span, "/*")?;
                     $e.wr.write_comment(cmt.span, &cmt.text)?;
                     $e.wr.write_comment(cmt.span, "*/")?;
-                    $e.wr.write_line()?;
                 }
             }
         }
