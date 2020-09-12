0e237c98f57dccb8cdf1caaa8e62ef81bf6c93e7
강동윤 <kdy1997.dev@gmail.com>
2020-09-12 23:27:49 +0900

    Update test

--- ecmascript/transforms/src/resolver/tests.rs
+++ ecmascript/transforms/src/resolver/tests.rs
@@ -1647,5 +1647,13 @@ to_ts!(
         },
       };
     "#,
-    r#""#
+    r#"
+    const directiveHandlers: DirectiveHandlers = {
+        TAG (state__2, _name__2, ...args__2: string[]): void {
+            const handle__2 = args__2[0];
+            if (!PATTERN_TAG_HANDLE.test(handle__2)) {
+            }
+        }
+    };
+    "#
 );
