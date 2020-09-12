cd8aac0dcd7f17e5a1983d231923efef2893d4c1
강동윤 <kdy1997.dev@gmail.com>
2020-09-12 20:24:00 +0900

    WIP

--- ecmascript/parser/raw_lexer/src/lib.rs
+++ ecmascript/parser/raw_lexer/src/lib.rs
@@ -12,8 +12,8 @@ pub struct Extras {
 /// Interal tokens for super-fast lexing.
 #[derive(Logos, Debug, Clone, Copy, PartialEq, Eq)]
 #[logos(extras = Extras)]
-#[logos(subpattern unicode4 = r"\\\\u[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]")]
-#[logos(subpattern unicode = r"\\\\u[{][0-9a-fA-F]+[}]")]
+#[logos(subpattern unicode4 = r"\\\\[u][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]")]
+#[logos(subpattern unicode = r"\\\\[u][{][0-9a-fA-F]+[}]")]
 #[logos(subpattern decimal = r"[0-9][_0-9]*")]
 #[logos(subpattern hex = r"[0-9a-fA-F][_0-9a-fA-F]*")]
 #[logos(subpattern octal = r"[0-7][_0-7]*")]
@@ -39,7 +39,7 @@ pub enum InternalToken {
 
     #[regex(r#"[a-zA-Z$_\p{XID_Start}][a-zA-Z0-9$_\p{XID_Continue}&unicode&unicode4]*"#)]
     #[regex(r#"&unicode[a-zA-Z0-9$_\p{XID_Continue}&unicode&unicode4)]*"#)]
-    #[regex(r#"(?&unicode4)[a-zA-Z0-9$_\p{XID_Continue}]|(?&unicode)|(?&unicode4)*"#)]
+    #[regex(r#"(\\\\[u][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])*"#)]
     Ident,
 
     #[token("await")]
