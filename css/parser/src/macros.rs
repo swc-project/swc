974698e58c3e6be575720940834ed7e0dedb81d7
강동윤 <kdy1997.dev@gmail.com>
2020-09-14 01:27:09 +0900

    Fix macro

--- css/parser/src/macros.rs
+++ css/parser/src/macros.rs
@@ -1,6 +1,12 @@
 macro_rules! tok {
     (";") => {
-        crate::token::Token::Semi;
+        crate::token::Token::Semi
+    };
+    ("{") => {
+        crate::token::Token::LBrace
+    };
+    ("}") => {
+        crate::token::Token::RBrace
     };
 }
 
@@ -8,10 +14,19 @@ macro_rules! expect {
     ($parser:expr, $t:tt) => {{
         use crate::error::{Error, ErrorKind};
 
-        match $parser.lexer.next() {
-            tok!($t) => true,
-            _ => Err(Error {
-                inner: Box::new(span!($parser), ErrorKind::Eof),
+        match $parser.lexer.cur() {
+            Some(tok!($t)) => true,
+            Some(other) => Err(Error {
+                inner: Box::new((
+                    $parser.i.span(),
+                    ErrorKind::Expected {
+                        expected: tok!($t),
+                        got: other,
+                    },
+                )),
+            }),
+            None => Err(Error {
+                inner: Box::new(($parser.i.span(), ErrorKind::Eof)),
             }),
         }
     }};
