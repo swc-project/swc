f733c20bec2302652ecd8d8a3a87390d831e7566
강동윤 <kdy1997.dev@gmail.com>
2020-09-14 01:42:59 +0900

    parse_decl_block

--- css/parser/src/parser/style_rule.rs
+++ css/parser/src/parser/style_rule.rs
@@ -5,4 +5,27 @@ impl Parser<'_> {
     pub(super) fn parse_style_rule(&mut self) -> PResult<StyleRule> {}
 
     fn parse_property(&mut self) -> PResult<Property> {}
+
+    pub(super) fn parse_decl_block(&mut self) -> PResult<DeclBlock> {
+        let start = self.i.cur_pos();
+        expect!(self, "{");
+        let mut properties = vec![];
+
+        while let Some(token) = self.i.cur() {
+            if token == tok!("}") {
+                break;
+            }
+
+            self.parse_property()?;
+        }
+
+        expect!(self, "}");
+
+        Ok(DeclBlock {
+            span: self.i.make_span(start),
+            properties,
+        })
+    }
+
+    fn parse_property(&mut self) -> PResult<Property> {}
 }
