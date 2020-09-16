bed545c1a74615210bbe115c82e34cbc8593fbd2
강동윤 <kdy1997.dev@gmail.com>
2020-09-16 20:59:12 +0900

    Add a test for deno

---
+++ spack/tests/pass/deno-001/input/http/http_status.ts
@@ -0,0 +1,8 @@
+// Copyright 2018-2020 the Deno authors. All rights reserved. MIT license.
+
+/** HTTP status codes */
+export enum Status {
+}
+
+export const STATUS_TEXT = new Map<Status, string>([
+]);
