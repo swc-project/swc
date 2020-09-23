6738dd413b72016ef39d70223fa2e32166ba2aad
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 01:03:03 +0900

    Simpler test

--- typescript/dts/tests/fixtures/types/mapped/.inference/test-1-1-2.ts
+++ typescript/dts/tests/fixtures/types/mapped/.inference/test-1-1-2.ts
@@ -10,36 +10,6 @@ type Boxified<T> = {
     [P in keyof T]: Box<T[P]>;
 }
 
-function box<T>(x: T): Box<T> {
-    return { value: x };
-}
-
-function unbox<T>(x: Box<T>): T {
-    return x.value;
-}
-
-function boxify<T>(obj: T): Boxified<T> {
-    let result = {} as Boxified<T>;
-    for (let k in obj) {
-        result[k] = box(obj[k]);
-    }
-    return result;
-}
-
-function unboxify<T>(obj: Boxified<T>): T {
-    let result = {} as T;
-    for (let k in obj) {
-        result[k] = unbox(obj[k]);
-    }
-    return result;
-}
-
-function assignBoxified<T>(obj: Boxified<T>, values: T) {
-    for (let k in values) {
-        obj[k].value = values[k];
-    }
-}
-
 declare let b: Boxified<{
     a: number,
     b: string,
