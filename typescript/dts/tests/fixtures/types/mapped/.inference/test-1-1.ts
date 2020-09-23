ffe9236a1e87b340e01108f273585d7d42d33227
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 02:21:26 +0900

    Simplify

--- typescript/dts/tests/fixtures/types/mapped/.inference/test-1-1.ts
+++ typescript/dts/tests/fixtures/types/mapped/.inference/test-1-1.ts
@@ -14,30 +14,12 @@ function box<T>(x: T): Box<T> {
     return { value: x };
 }
 
-function unbox<T>(x: Box<T>): T {
-    return x.value;
-}
-
 function boxify<T>(obj: T): Boxified<T> {
     let result = {} as Boxified<T>;
     for (let k in obj) {
         result[k] = box(obj[k]);
     }
     return result;
-}
-
-function unboxify<T>(obj: Boxified<T>): T {
-    let result = {} as T;
-    for (let k in obj) {
-        result[k] = unbox(obj[k]);
-    }
-    return result;
-}
-
-function assignBoxified<T>(obj: Boxified<T>, values: T) {
-    for (let k in values) {
-        obj[k].value = values[k];
-    }
 }
 
 let v = {
