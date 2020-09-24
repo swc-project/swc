43ddfe3d1014926686d0db731501ba695fb93c0c
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 20:29:39 +0900

    Split

---
+++ typescript/dts/tests/fixtures/key-of/indexed-access/test-6-9-2-3-1.ts
@@ -0,0 +1,10 @@
+
+
+
+function f52<T>(obj: { [x: string]: boolean }, k: Exclude<keyof T, symbol>, s: string, n: number) {
+    const x1 = obj[s];
+    const x2 = obj[n];
+    const x3 = obj[k];
+
+    return x1
+}
