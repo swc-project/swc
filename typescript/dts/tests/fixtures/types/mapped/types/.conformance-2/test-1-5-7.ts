5e5723b56d71f38f0ff2954f6c87cf77096383b6
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 14:37:20 +0900

    Split

---
+++ typescript/dts/tests/fixtures/types/mapped/types/.conformance-2/test-1-5-7.ts
@@ -0,0 +1,23 @@
+// @strictNullChecks: true
+// @declaration: true
+
+type Proxy<T> = {
+    get(): T;
+    set(value: T): void;
+}
+
+type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>;
+}
+
+interface Shape {
+    name: string;
+    width: number;
+    height: number;
+    location: Point;
+}
+
+declare var shape: Shape;
+declare const p: Proxify<Shape>
+let name = p.name.get();
+p.width.set(42);
