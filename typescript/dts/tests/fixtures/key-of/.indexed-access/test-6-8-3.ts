faa60eb38c3aeee7551d840c2116d38bbc4684e6
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 15:07:30 +0900

    Split

---
+++ typescript/dts/tests/fixtures/key-of/.indexed-access/test-6-8-3.ts
@@ -0,0 +1,55 @@
+declare class Shape {
+    name: string;
+    width: number;
+    height: number;
+    visible: boolean;
+}
+declare class TaggedShape extends Shape {
+    tag: string;
+}
+declare class Item {
+    name: string;
+    price: number;
+}
+declare class Options {
+    visible: "yes" | "no";
+}
+declare type Dictionary<T> = {
+    [x: string]: T;
+};
+declare type NumericallyIndexed<T> = {
+    [x: number]: T;
+};
+declare const enum E {
+    A = 0,
+    B = 1,
+    C = 2
+}
+declare type KeyOf<T> = keyof T;
+declare type NAME = "name";
+declare type WIDTH_OR_HEIGHT = "width" | "height";
+declare let cond: boolean;
+declare function getProperty<T, K extends keyof T>(obj: T, key: K): T[K];
+declare function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]): void;
+
+class Component<PropType> {
+    props: PropType;
+
+    getProperty<K extends keyof PropType>(key: K) {
+        return this.props[key];
+    }
+
+    setProperty<K extends keyof PropType>(key: K, value: PropType[K]) {
+        this.props[key] = value;
+    }
+}
+
+function pluck<T, K extends keyof T>(array: T[], key: K) {
+    return array.map(x => x[key]);
+}
+
+function f33<S extends Shape, K extends keyof S>(shape: S, key: K) {
+    let name = getProperty(shape, "name");
+    let prop = getProperty(shape, key);
+    return prop;
+}
