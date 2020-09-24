06fb9890b615a5a222261631cbfc1e56542c1581
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 15:03:28 +0900

    More split

---
+++ typescript/dts/tests/fixtures/key-of/.indexed-access/test-6-5.ts
@@ -0,0 +1,38 @@
+declare class Shape {
+    name: string;
+    width: number;
+    height: number;
+    visible: boolean;
+}
+declare class TaggedShape extends Shape {
+    tag: string;
+}
+declare class Item {
+    name: string;
+    price: number;
+}
+declare class Options {
+    visible: "yes" | "no";
+}
+declare type Dictionary<T> = {
+    [x: string]: T;
+};
+declare type NumericallyIndexed<T> = {
+    [x: number]: T;
+};
+declare const enum E {
+    A = 0,
+    B = 1,
+    C = 2
+}
+declare type KeyOf<T> = keyof T;
+declare type NAME = "name";
+declare type WIDTH_OR_HEIGHT = "width" | "height";
+declare let cond: boolean;
+declare function getProperty<T, K extends keyof T>(obj: T, key: K): T[K];
+declare function setProperty<T, K extends keyof T>(obj: T, key: K, value: T[K]): void;
+
+declare var foo: any, bar: any;
+let x = getProperty(foo, "x");  // any
+let y = getProperty(foo, "100");  // any
+let z = getProperty(foo, bar);  // any
