b5ad8615047f7410b8c1c048a22d06802649c491
강동윤 <kdy1997.dev@gmail.com>
2020-09-24 20:30:17 +0900

    Split a test

--- typescript/dts/tests/fixtures/key-of/.indexed-access/test-6-9-2-3-2.ts
+++ typescript/dts/tests/fixtures/key-of/.indexed-access/test-6-9-2-3-2.ts
@@ -2,9 +2,7 @@
 
 
 function f52<T>(obj: { [x: string]: boolean }, k: Exclude<keyof T, symbol>, s: string, n: number) {
-    const x1 = obj[s];
     const x2 = obj[n];
-    const x3 = obj[k];
 
-    return [x1, x2, x3]
+    return x2
 }
