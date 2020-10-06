dbca959453b28bbfcae56de92cf417873c2c3f69
강동윤 <kdy1997.dev@gmail.com>
2020-10-06 20:59:19 +0900

    Add a test for resolver

--- ecmascript/transforms/src/resolver/tests.rs
+++ ecmascript/transforms/src/resolver/tests.rs
@@ -1679,3 +1679,17 @@ to!(
     }
     "
 );
+
+to!(
+    issue_1140,
+    r#"
+    const categories = [{ key: "apple" }, { key: "banana" }, { key: "strawberry" }];
+
+    const item = "some item";
+
+    const catNames = categories.reduce((a, item) => {
+    return { ...a, [item.key.toString()]: item };
+    }, {});
+    "#,
+    ""
+);
