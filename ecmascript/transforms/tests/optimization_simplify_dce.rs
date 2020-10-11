ac78d4a8b56fb9d9dddd07e3a05ceedc8ac575d6
강동윤 <kdy1997.dev@gmail.com>
2020-10-11 21:15:10 +0900

    Update test ref

--- ecmascript/transforms/tests/optimization_simplify_dce.rs
+++ ecmascript/transforms/tests/optimization_simplify_dce.rs
@@ -756,6 +756,22 @@ test!(
     new A();
     ",
     "
-    
+    function d() {
+        let methods;
+        const promise = new Promise((resolve, reject)=>{
+            methods = {
+                resolve,
+                reject
+            };
+        });
+        return Object.assign(promise, methods);
+    }
+    class A {
+        s = d();
+        a() {
+            this.s.resolve();
+        }
+    }
+    new A();
     "
 );
