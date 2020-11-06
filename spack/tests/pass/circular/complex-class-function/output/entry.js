1c155580d80873dd664f07fd9f4107b3ea71599f
강동윤 <kdy1997.dev@gmail.com>
2020-11-06 15:24:25 +0900

    circular: Swap entry and dep

--- spack/tests/pass/circular/complex-class-function/output/entry.js
+++ spack/tests/pass/circular/complex-class-function/output/entry.js
@@ -1,11 +1,11 @@
-function getC() {
-    return C;
-}
-class C {
-}
 function a() {
     return new A();
 }
+function getC() {
+    return C;
+}
 class A extends getC() {
+}
+class C {
 }
 console.log(a, a());
