e4193202d60911a2cf361bb33888895e40221557
강동윤 <kdy1997.dev@gmail.com>
2020-11-10 22:02:09 +0900

    Update test references

--- bundler/tests/fixture/deno-8302-2/output/entry.js
+++ bundler/tests/fixture/deno-8302-2/output/entry.js
@@ -1,6 +1,6 @@
+class A {
+}
 const a = function() {
-    class A {
-    }
     console.log(b, A);
     console.log(c, A);
     console.log(a, A);
--- bundler/tests/fixture/deno-8302-3/output/entry.js
+++ bundler/tests/fixture/deno-8302-3/output/entry.js
@@ -1,11 +1,11 @@
+const lib = function() {
+    class A {
+    }
+    return {
+        A
+    };
+}();
 const a = function() {
-    const lib = function() {
-        class A {
-        }
-        return {
-            A
-        };
-    }();
     console.log(b, lib);
     console.log(c, lib);
     console.log(a, lib);
