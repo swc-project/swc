ef4ac45f24984420df4d9597888afcfdd267b13c
강동윤 <kdy1997.dev@gmail.com>
2020-11-12 20:06:27 +0900

    Update test refs

--- spack/tests/pass/import/namespace/name-conflict/in-entry/output/entry.js
+++ spack/tests/pass/import/namespace/name-conflict/in-entry/output/entry.js
@@ -2,7 +2,7 @@ function a() {
 }
 function foo() {
 }
-const a1 = a, foo1 = foo, a2 = a1, foo2 = foo1;
-function foo3() {
+const a1 = a, foo1 = foo;
+function foo2() {
 }
-console.log(foo3(), a2(), foo2());
+console.log(foo2(), a1(), foo1());
--- spack/tests/pass/import/namespace/simple/output/entry.js
+++ spack/tests/pass/import/namespace/simple/output/entry.js
@@ -2,5 +2,5 @@ function a() {
 }
 function foo() {
 }
-const a1 = a, foo1 = foo, a2 = a1, foo2 = foo1;
-console.log(a2(), foo2());
+const a1 = a, foo1 = foo;
+console.log(a1(), foo1());
