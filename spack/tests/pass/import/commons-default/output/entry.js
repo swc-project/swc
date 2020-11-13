5e676433942752f457495e352384c616e9a5e62a
강동윤 <kdy1997.dev@gmail.com>
2020-11-13 18:47:42 +0900

    Update test ref

--- spack/tests/pass/import/commons-default/output/entry.js
+++ spack/tests/pass/import/commons-default/output/entry.js
@@ -1,9 +1,14 @@
-class A extends Common {
+class Common {
 }
-const __default = A;
-const A1 = __default;
-class B extends Common {
+const __default = Common;
+const Common1 = __default;
+class A extends Common1 {
 }
-const __default1 = B;
-const B1 = __default1;
+const __default1 = A;
+const A1 = __default1;
+const Common2 = __default;
+class B extends Common2 {
+}
+const __default2 = B;
+const B1 = __default2;
 console.log(A1, B1);
