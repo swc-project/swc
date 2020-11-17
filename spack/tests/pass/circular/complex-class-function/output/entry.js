49f0525c9f55476b90073f10ccf1cd6be2d81a7c
강동윤 <kdy1997.dev@gmail.com>
2020-11-17 15:34:23 +0900

    Update test refs

--- spack/tests/pass/circular/complex-class-function/output/entry.js
+++ spack/tests/pass/circular/complex-class-function/output/entry.js
@@ -1,15 +1,15 @@
-function getC() {
-    return C;
-}
-class C {
-}
-const getC1 = getC;
-const getC2 = getC1;
 function a() {
     return new A();
 }
+const a1 = a;
+function getC() {
+    return C;
+}
+const getC1 = getC;
+const getC2 = getC1;
 class A extends getC2() {
 }
-const a1 = a;
+class C {
+}
 const a2 = a1;
 console.log(a2, a2());
--- spack/tests/pass/issue-1139/example-2/output/entry.js
+++ spack/tests/pass/issue-1139/example-2/output/entry.js
@@ -1,8 +1,10 @@
-function f2() {
-    console.log("f2");
-}
 function f1() {
     console.log("f1");
 }
-f1();
-f2();
+const f11 = f1;
+f11();
+function f2() {
+    console.log("f2");
+}
+const f21 = f2;
+f21();
