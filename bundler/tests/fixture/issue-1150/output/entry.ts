e138f1d3b41f0525fbea85ba8361e241d46988c3
강동윤 <kdy1997.dev@gmail.com>
2020-11-18 16:06:11 +0900

    Update test refs

--- bundler/tests/fixture/issue-1150/output/entry.ts
+++ bundler/tests/fixture/issue-1150/output/entry.ts
@@ -2,23 +2,25 @@ function a() {
     console.log("a");
 }
 const a1 = a;
-var O;
-(function(O1) {
-    O1[O1["A"] = 0] = "A";
-    O1[O1["B"] = 1] = "B";
-    O1[O1["C"] = 2] = "C";
-})(O || (O = {
+const a2 = a1;
+var O3;
+const O1 = O3;
+(function(O2) {
+    O2[O2["A"] = 0] = "A";
+    O2[O2["B"] = 1] = "B";
+    O2[O2["C"] = 2] = "C";
+})(O3 || (O3 = {
 }));
-const O1 = O;
-const defaultA = a1;
-export { O1 as O };
+const O2 = O1;
+const defaultA = a2;
+export { O2 as O };
 class A {
     #a;
     #c;
     constructor(o = {
     }){
-        const { a: a2 = defaultA , c ,  } = o;
-        this.#a = a2;
+        const { a: a3 = defaultA , c ,  } = o;
+        this.#a = a3;
         this.#c = c;
     }
     a() {
@@ -28,6 +30,6 @@ class A {
         console.log(this.#c);
     }
 }
-let a3 = new A();
-a3.a();
-a3.c();
+let a4 = new A();
+a4.a();
+a4.c();
