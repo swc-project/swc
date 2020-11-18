54c9d54c4b697bf8d56f3dc3353b98df1c2b54a7
강동윤 <kdy1997.dev@gmail.com>
2020-11-18 22:15:15 +0900

    Update test ref

--- spack/tests/pass/merge/name-conflict/simple/output/entry.js
+++ spack/tests/pass/merge/name-conflict/simple/output/entry.js
@@ -4,10 +4,10 @@ function foo() {
 function foo1() {
     return 2;
 }
-export { a2 as a, b2 as b };
-const a = foo();
-const a1 = a;
+const a3 = foo();
+const a1 = a3;
 const a2 = a1;
-const b = foo1();
-const b1 = b;
+const b3 = foo1();
+const b1 = b3;
 const b2 = b1;
+export { a2 as a, b2 as b };
