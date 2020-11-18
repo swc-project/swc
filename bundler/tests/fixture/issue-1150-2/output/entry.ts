3b6fa32111ce0dbe5877d500ab62ff4ea44144bb
강동윤 <kdy1997.dev@gmail.com>
2020-11-19 00:02:22 +0900

    Update test refs

--- bundler/tests/fixture/issue-1150-2/output/entry.ts
+++ bundler/tests/fixture/issue-1150-2/output/entry.ts
@@ -4,16 +4,13 @@ function a() {
 const a1 = a;
 const a2 = a1;
 var O3;
-const O1 = O3;
-(function(O2) {
-    O2[O2["A"] = 0] = "A";
-    O2[O2["B"] = 1] = "B";
-    O2[O2["C"] = 2] = "C";
+(function(O1) {
+    O1[O1["A"] = 0] = "A";
+    O1[O1["B"] = 1] = "B";
+    O1[O1["C"] = 2] = "C";
 })(O3 || (O3 = {
 }));
-const O2 = O1;
 const defaultA = a2;
-export { O2 as O };
 class A {
     #a;
     #c;
@@ -33,3 +30,6 @@ class A {
 let a4 = new A();
 a4.a();
 a4.c();
+const O1 = O3;
+const O2 = O1;
+export { O2 as O };
