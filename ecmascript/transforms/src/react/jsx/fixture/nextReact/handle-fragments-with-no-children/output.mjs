d6f096e9e78343bdfa98ac91c19b5ef3046b1f83
강동윤 <kdy1997.dev@gmail.com>
2020-11-25 18:46:25 +0900

    Update test refs

--- ecmascript/transforms/src/react/jsx/fixture/nextReact/handle-fragments-with-no-children/output.mjs
+++ ecmascript/transforms/src/react/jsx/fixture/nextReact/handle-fragments-with-no-children/output.mjs
@@ -1,4 +1,3 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Fragment as _Fragment } from "react/jsx-runtime";
-
-var x = /*#__PURE__*/_jsx(_Fragment, {});
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
+var x = _jsx(_Fragment, {
+});
