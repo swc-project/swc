c246ad086588f3983efe0ba6eedd5574f5cd3328
강동윤 <kdy1997.dev@gmail.com>
2020-11-30 18:45:06 +0900

    Fix test

--- ecmascript/transforms/src/compat/es2015/block_scoping.rs
+++ ecmascript/transforms/src/compat/es2015/block_scoping.rs
@@ -1474,7 +1474,6 @@ expect(foo()).toBe(false);
                                 }, _callee1);
                             }))));
                         case 2:
-                        case 3:
                         case "end":
                             return _ctx.stop();
                     }
