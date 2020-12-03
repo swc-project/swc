60096dfb79e33ebc8e10ed461db1c561ebbd2f56
강동윤 <kdy1997.dev@gmail.com>
2020-12-04 03:28:34 +0900

    Update test ref

--- ecmascript/transforms/tests/proposal_decorators.rs
+++ ecmascript/transforms/tests/proposal_decorators.rs
@@ -5152,6 +5152,31 @@ test!(
       return function (...args) {};
     }
     ",
-    "",
+    "
+    var _class, _descriptor;
+enum MyEnum {
+    x = \"xxx\",
+    y = \"yyy\"
+}
+var _dec = Decorator(), _dec1 = typeof Reflect !== \"undefined\" && typeof Reflect.metadata === \
+     \"function\" && Reflect.metadata(\"design:type\", String);
+let Xpto = ((_class = class Xpto {
+    constructor(){
+        _initializerDefineProperty(this, \"value\", _descriptor, this);
+    }
+}) || _class, _descriptor = _applyDecoratedDescriptor(_class.prototype, \"value\", [
+    _dec,
+    _dec1
+], {
+    configurable: true,
+    enumerable: true,
+    writable: true,
+    initializer: void 0
+}), _class);
+function Decorator() {
+    return function(...args) {
+    };
+}
+",
     ok_if_code_eq
 );
