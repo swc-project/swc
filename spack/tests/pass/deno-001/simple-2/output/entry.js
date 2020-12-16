0ee78b36588602cc783dbac6b5d33d6ad1fd7870
강동윤 <kdy1997.dev@gmail.com>
2020-12-16 16:56:36 +0900

    Update test refs

--- spack/tests/pass/deno-001/simple-2/output/entry.js
+++ spack/tests/pass/deno-001/simple-2/output/entry.js
@@ -18,19 +18,19 @@ class ServerRequest {
 }
 async function listenAndServe(addr, handler) {
 }
-const ServerRequest1 = ServerRequest;
-const listenAndServe1 = listenAndServe;
-const ServerRequest2 = ServerRequest1;
-console.log(ServerRequest2);
 async function writeResponse(w, r) {
 }
 async function readRequest(conn, bufr) {
 }
+const ServerRequest1 = ServerRequest;
+const listenAndServe1 = listenAndServe;
 const writeResponse1 = writeResponse;
 const readRequest1 = readRequest;
 const writeResponse2 = writeResponse1;
 const readRequest2 = readRequest1;
 console.log(deferred4, writeResponse2, readRequest2, MuxAsyncIterator2);
+const ServerRequest2 = ServerRequest1;
+console.log(ServerRequest2);
 const listenAndServe2 = listenAndServe1;
 listenAndServe2({
     port: 8080
