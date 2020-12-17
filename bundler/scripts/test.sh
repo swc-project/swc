6090d6aa09451723bb8e17dc1c06ed2617dcc5bf
강동윤 <kdy1997.dev@gmail.com>
2020-12-17 15:10:23 +0900

    Update test script

--- bundler/scripts/test.sh
+++ bundler/scripts/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 
+cargo test --no-run
+
+cargo test --lib
 cargo test --test fixture
 (cd ../spack && cargo test --test fixture)
 cargo test --test deno $@ -- --nocapture
