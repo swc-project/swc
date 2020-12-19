c30087a03a8d00b11a5008240a46f5c9ff3a03dc
강동윤 <kdy1997.dev@gmail.com>
2020-12-19 13:50:28 +0900

    Update test refs

--- bundler/tests/fixture/circular-imports/complex-export/output/entry.js
+++ bundler/tests/fixture/circular-imports/complex-export/output/entry.js
@@ -1,6 +1,6 @@
 const c2 = 'c';
 const a2 = 'a';
+const c1 = c2;
 const a1 = a2;
 export { a1 as a };
-const c1 = c2;
 export { c1 as c };
--- spack/tests/pass/alias/export-named-same-name/output/entry.js
+++ spack/tests/pass/alias/export-named-same-name/output/entry.js
@@ -1,8 +1,8 @@
 const foo = 1;
 const bar = 2;
 const bar1 = foo;
-const bar2 = bar1;
-console.log(bar2);
 const baz = bar;
+const bar2 = bar1;
 const baz1 = baz;
+console.log(bar2);
 console.log(baz1);
