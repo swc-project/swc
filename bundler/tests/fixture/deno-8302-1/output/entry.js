8a8f992dd005c866e112afb4abecb269231c9342
강동윤 <kdy1997.dev@gmail.com>
2020-12-19 16:43:05 +0900

    Update test refs

--- bundler/tests/fixture/deno-8302-1/output/entry.js
+++ bundler/tests/fixture/deno-8302-1/output/entry.js
@@ -1,16 +1,16 @@
 class A {
 }
-const c = 3;
-const b = 2;
-const a = 1;
 const A1 = A;
 const A2 = A1;
+const A3 = A1;
+const A4 = A1;
+const c = 3;
 const c1 = c;
 const c2 = c1;
-const A3 = A1;
+const b = 2;
 const b1 = b;
 const b2 = b1;
-const A4 = A1;
+const a = 1;
 const a1 = a;
 const a2 = a1;
 const a3 = a1;
