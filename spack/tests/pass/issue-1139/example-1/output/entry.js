4deea8ea879fe25411f8494ae060134197135100
강동윤 <kdy1997.dev@gmail.com>
2020-12-19 16:47:09 +0900

    Update test refs

--- spack/tests/pass/issue-1139/example-1/output/entry.js
+++ spack/tests/pass/issue-1139/example-1/output/entry.js
@@ -1,11 +1,11 @@
 function f2() {
     console.log("f2");
 }
+const f21 = f2;
 function f1() {
     console.log("f1");
 }
-const f21 = f2;
 const f11 = f1;
+export { f11 as f1 };
 f11();
 f21();
-export { f11 as f1 };
--- spack/tests/pass/issue-1139/example-2/output/entry.js
+++ spack/tests/pass/issue-1139/example-2/output/entry.js
@@ -1,10 +1,10 @@
 function f2() {
     console.log("f2");
 }
+const f21 = f2;
 function f1() {
     console.log("f1");
 }
-const f21 = f2;
 const f11 = f1;
 f11();
 f21();
