aeff7c95b4159200ab3fc2d3cb56c16f7a33aa88
강동윤 <kdy1997.dev@gmail.com>
2020-12-20 20:23:44 +0900

    Migrate typescript reference testing to `testing_macros`

--- ecmascript/parser/tests/typescript.rs
+++ ecmascript/parser/tests/typescript.rs
@@ -8,7 +8,7 @@ use std::{
     env,
     fs::File,
     io::{self, Read},
-    path::Path,
+    path::{Path, PathBuf},
 };
 use swc_ecma_ast::*;
 use swc_ecma_parser::{lexer::Lexer, JscTarget, PResult, Parser, StringInput, Syntax, TsConfig};
@@ -22,195 +22,126 @@ use walkdir::WalkDir;
 #[path = "common/mod.rs"]
 mod common;
 
-fn add_test<F: FnOnce() + Send + 'static>(
-    tests: &mut Vec<TestDescAndFn>,
-    name: String,
-    ignore: bool,
-    f: F,
-) {
+#[testing::fixture("typescript/**/*.ts")]
+#[testing::fixture("typescript/**/*.tsx")]
+fn spec(file: PathBuf) {
+    let file_name = file.display().to_string();
+
+    // Ignore some useless tests
+    let ignore = file_name.contains("tsc/es6/functionDeclarations/FunctionDeclaration7_es6")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings11_ES5")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings10_ES5")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings11_ES6")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings10_ES6")
+        || file_name.contains(
+            "tsc/types/objectTypeLiteral/propertySignatures/propertyNamesOfReservedWords",
+        )
+        || file_name.contains("unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates10_ES5")
+        || file_name.contains("unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates10_ES6")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates11_ES5")
+        || file_name
+            .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates11_ES6")
+        || file_name.contains("tsc/es6/variableDeclarations/VariableDeclaration6_es6")
+        || file_name.contains(
+            "tsc/parser/ecmascriptnext/numericSeparators/parser.numericSeparators.decimal",
+        );
+
+    // Useful only for error reporting
+    let ignore = ignore
+        || file_name.contains(
+            "tsc/types/objectTypeLiteral/callSignatures/callSignaturesWithParameterInitializers",
+        )
+        || file_name.contains("tsc/jsdoc/jsdocDisallowedInTypescript")
+        || file_name.contains("tsc/expressions/superCalls/errorSuperCalls")
+        || file_name.contains("tsc/types/rest/restElementMustBeLast");
+
+    // Postponed
+    let ignore = ignore
+        || file_name.contains("tsc/jsx/inline/inlineJsxFactoryDeclarationsx")
+        || file_name.contains("tsc/externalModules/typeOnly/importDefaultNamedType")
+        || file_name.contains("tsc/jsx/tsxAttributeResolution5x")
+        || file_name.contains("tsc/jsx/tsxErrorRecovery2x")
+        || file_name.contains("tsc/jsx/tsxErrorRecovery3x")
+        || file_name.contains("tsc/jsx/tsxErrorRecovery5x")
+        || file_name.contains("tsc/jsx/tsxReactEmitEntitiesx/input.tsx")
+        || file_name.contains("tsc/jsx/tsxTypeArgumentsJsxPreserveOutputx/input.tsx")
+        || file_name.contains(
+            "tsc/es7/exponentiationOperator/emitCompoundExponentiationAssignmentWithIndexingOnLHS3",
+        )
+        || file_name.contains("tsc/expressions/objectLiterals/objectLiteralGettersAndSetters")
+        || file_name.contains("tsc/types/rest/restElementMustBeLast")
+        || file_name.contains("tsc/jsx/unicodeEscapesInJsxtagsx/input.tsx")
+        || file_name.contains("tsc/es6/functionDeclarations/FunctionDeclaration6_es6");
+
     if ignore {
         return;
     }
-    tests.push(TestDescAndFn {
-        desc: TestDesc {
-            test_type: TestType::UnitTest,
-            name: TestName::DynTestName(name),
-            ignore,
-            should_panic: No,
-            allow_fail: false,
-        },
-        testfn: DynTestFn(Box::new(f)),
-    });
-}
 
-fn reference_tests(tests: &mut Vec<TestDescAndFn>, errors: bool) -> Result<(), io::Error> {
-    let root = {
-        let mut root = Path::new(env!("CARGO_MANIFEST_DIR")).to_path_buf();
-        root.push("tests");
-        root.push(if errors {
-            "typescript-errors"
-        } else {
-            "typescript"
-        });
-        root
-    };
+    {
+        // Drop to reduce memory usage.
+        //
+        // Because the test suite contains lots of test cases, it results in oom in
+        // github actions.
+        let input = {
+            let mut buf = String::new();
+            File::open(&file).unwrap().read_to_string(&mut buf).unwrap();
+            buf
+        };
 
-    eprintln!("Loading tests from {}", root.display());
-
-    let dir = root;
-
-    for entry in WalkDir::new(&dir).into_iter() {
-        let entry = entry?;
-        if entry.file_type().is_dir()
-            || !(entry.file_name().to_string_lossy().ends_with(".ts")
-                || entry.file_name().to_string_lossy().ends_with(".tsx"))
-        {
-            continue;
-        }
-        let file_name = entry
-            .path()
-            .strip_prefix(&dir)
-            .expect("failed to strip prefix")
-            .to_str()
-            .unwrap()
-            .to_string();
-
-        // Ignore some useless tests
-        let ignore = file_name.contains("tsc/es6/functionDeclarations/FunctionDeclaration7_es6")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings11_ES5")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings10_ES5")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings11_ES6")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInStrings10_ES6")
-            || file_name.contains(
-                "tsc/types/objectTypeLiteral/propertySignatures/propertyNamesOfReservedWords",
-            )
-            || file_name.contains("unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates10_ES5")
-            || file_name.contains("unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates10_ES6")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates11_ES5")
-            || file_name
-                .contains("tsc/es6/unicodeExtendedEscapes/unicodeExtendedEscapesInTemplates11_ES6")
-            || file_name.contains("tsc/es6/variableDeclarations/VariableDeclaration6_es6")
-            || file_name.contains(
-                "tsc/parser/ecmascriptnext/numericSeparators/parser.numericSeparators.decimal",
-            );
-
-        // Useful only for error reporting
-        let ignore = ignore
-            || file_name.contains(
-                "tsc/types/objectTypeLiteral/callSignatures/\
-                 callSignaturesWithParameterInitializers",
-            )
-            || file_name.contains("tsc/jsdoc/jsdocDisallowedInTypescript")
-            || file_name.contains("tsc/expressions/superCalls/errorSuperCalls")
-            || file_name.contains("tsc/types/rest/restElementMustBeLast");
-
-        // Postponed
-        let ignore = ignore
-            || file_name.contains("tsc/jsx/inline/inlineJsxFactoryDeclarationsx")
-            || file_name.contains("tsc/externalModules/typeOnly/importDefaultNamedType")
-            || file_name.contains("tsc/jsx/tsxAttributeResolution5x")
-            || file_name.contains("tsc/jsx/tsxErrorRecovery2x")
-            || file_name.contains("tsc/jsx/tsxErrorRecovery3x")
-            || file_name.contains("tsc/jsx/tsxErrorRecovery5x")
-            || file_name.contains("tsc/jsx/tsxReactEmitEntitiesx/input.tsx")
-            || file_name.contains("tsc/jsx/tsxTypeArgumentsJsxPreserveOutputx/input.tsx")
-            || file_name.contains(
-                "tsc/es7/exponentiationOperator/\
-                 emitCompoundExponentiationAssignmentWithIndexingOnLHS3",
-            )
-            || file_name.contains("tsc/expressions/objectLiterals/objectLiteralGettersAndSetters")
-            || file_name.contains("tsc/types/rest/restElementMustBeLast")
-            || file_name.contains("tsc/jsx/unicodeEscapesInJsxtagsx/input.tsx")
-            || file_name.contains("tsc/es6/functionDeclarations/FunctionDeclaration6_es6");
-
-        let dir = dir.clone();
-        let name = format!(
-            "typescript::{}::{}",
-            if errors { "error" } else { "reference" },
-            file_name
+        eprintln!(
+            "\n\n========== Running reference test {}\nSource:\n{}\n",
+            file_name, input
         );
-        add_test(tests, name, ignore, move || {
-            if env::var("CI").is_err() {
-                let input = {
-                    let mut buf = String::new();
-                    File::open(entry.path())
-                        .unwrap()
-                        .read_to_string(&mut buf)
-                        .unwrap();
-                    buf
-                };
-
-                eprintln!(
-                    "\n\n========== Running reference test {}\nSource:\n{}\n",
-                    file_name, input
-                );
-            }
-
-            let path = dir.join(&file_name);
-            if errors {
-                let module = with_parser(false, &path, !errors, |p| p.parse_typescript_module());
-
-                let err = module.expect_err("should fail, but parsed as");
-                if err
-                    .compare_to_file(format!("{}.stderr", path.display()))
-                    .is_err()
-                {
-                    panic!()
-                }
-            } else {
-                with_parser(is_backtrace_enabled(), &path, !errors, |p| {
-                    let program = p.parse_program()?.fold_with(&mut Normalizer {
-                        drop_span: false,
-                        is_test262: false,
-                    });
-
-                    let json = serde_json::to_string_pretty(&program)
-                        .expect("failed to serialize module as json");
-
-                    if StdErr::from(json.clone())
-                        .compare_to_file(format!("{}.json", path.display()))
-                        .is_err()
-                    {
-                        panic!()
-                    }
-
-                    let program = program.fold_with(&mut Normalizer {
-                        drop_span: true,
-                        is_test262: false,
-                    });
-
-                    let deser = match serde_json::from_str::<Program>(&json) {
-                        Ok(v) => v.fold_with(&mut Normalizer {
-                            drop_span: true,
-                            is_test262: false,
-                        }),
-                        Err(err) => {
-                            if err.to_string().contains("invalid type: null, expected f64") {
-                                return Ok(());
-                            }
-
-                            panic!(
-                                "failed to deserialize json back to module: {}\n{}",
-                                err, json
-                            )
-                        }
-                    };
-
-                    assert_eq!(program, deser, "JSON:\n{}", json);
-
-                    Ok(())
-                })
-                .unwrap();
-            }
-        });
     }
 
-    Ok(())
+    with_parser(is_backtrace_enabled(), &file, true, |p| {
+        let program = p.parse_program()?.fold_with(&mut Normalizer {
+            drop_span: false,
+            is_test262: false,
+        });
+
+        let json =
+            serde_json::to_string_pretty(&program).expect("failed to serialize module as json");
+
+        if StdErr::from(json.clone())
+            .compare_to_file(&format!("{}.json", file.display()))
+            .is_err()
+        {
+            panic!()
+        }
+
+        let program = program.fold_with(&mut Normalizer {
+            drop_span: true,
+            is_test262: false,
+        });
+
+        let deser = match serde_json::from_str::<Program>(&json) {
+            Ok(v) => v.fold_with(&mut Normalizer {
+                drop_span: true,
+                is_test262: false,
+            }),
+            Err(err) => {
+                if err.to_string().contains("invalid type: null, expected f64") {
+                    return Ok(());
+                }
+
+                panic!(
+                    "failed to deserialize json back to module: {}\n{}",
+                    err, json
+                )
+            }
+        };
+
+        assert_eq!(program, deser, "JSON:\n{}", json);
+
+        Ok(())
+    })
+    .unwrap();
 }
 
 fn with_parser<F, Ret>(
@@ -261,20 +192,32 @@ where
     output
 }
 
-#[test]
-fn references() {
-    let args: Vec<_> = env::args().collect();
-    let mut tests = Vec::new();
-    reference_tests(&mut tests, false).unwrap();
-    test_main(&args, tests, Some(Options::new()));
-}
+#[testing::fixture("typescript-errors/**/*.ts")]
+fn errors(file: PathBuf) {
+    let file_name = file.display().to_string();
 
-#[test]
-fn errors() {
-    let args: Vec<_> = env::args().collect();
-    let mut tests = Vec::new();
-    reference_tests(&mut tests, true).unwrap();
-    test_main(&args, tests, Some(Options::new()));
+    {
+        let input = {
+            let mut buf = String::new();
+            File::open(&file).unwrap().read_to_string(&mut buf).unwrap();
+            buf
+        };
+
+        eprintln!(
+            "\n\n========== Running reference test {}\nSource:\n{}\n",
+            file_name, input
+        );
+    }
+
+    let module = with_parser(false, &file, false, |p| p.parse_typescript_module());
+
+    let err = module.expect_err("should fail, but parsed as");
+    if err
+        .compare_to_file(format!("{}.stderr", file.display()))
+        .is_err()
+    {
+        panic!()
+    }
 }
 
 fn is_backtrace_enabled() -> bool {
