d43ba04a7153c6d382d7906d1d5e8da221424daf
강동윤 <kdy1997.dev@gmail.com>
2020-12-23 15:14:01 +0900

    Test for 8545  should be terminated normally

--- bundler/tests/deno-exec/deno-8545/entry.ts
+++ bundler/tests/deno-exec/deno-8545/entry.ts
@@ -15,4 +15,7 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log(`Now listening on http://0.0.0.0:3000`);
-await app.listen("0.0.0.0:3000");
+setTimeout(() => {
+    Deno.exit(0);
+}, 5000)
+await app.listen("0.0.0.0:58545");
