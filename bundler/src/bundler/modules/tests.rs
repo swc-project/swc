239158095d3d7f9d863d6d0e65d81fc523c2af8e
강동윤 <kdy1997.dev@gmail.com>
2020-12-25 14:22:01 +0900

    Improve sorting test

--- bundler/src/bundler/modules/tests.rs
+++ bundler/src/bundler/modules/tests.rs
@@ -1,30 +1,37 @@
 use crate::bundler::modules::Modules;
 use crate::bundler::tests::suite;
+use crate::debug::print_hygiene;
 use swc_ecma_ast::Module;
 use swc_ecma_utils::drop_span;
 
-fn assert_sorted(src: &str, res: &str) {
+fn assert_sorted(src: &[&str], res: &str) {
     suite().run(|t| {
-        let actual: Module = drop_span(t.parse(src));
-        let mut module = Modules::from(actual, t.bundler.injected_ctxt);
-        module.sort();
-        let actual: Module = module.into();
+        let mut modules = Modules::empty(t.bundler.injected_ctxt);
+
+        for src in src {
+            let actual: Module = drop_span(t.parse(src));
+            modules.push_all(Modules::from(actual, t.bundler.injected_ctxt));
+        }
+
+        modules.sort();
+        let actual: Module = modules.into();
 
         let expected = drop_span(t.parse(res));
 
-        assert_eq!(actual, expected);
+        if actual == expected {
+            return Ok(());
+        }
 
-        Ok(())
+        print_hygiene("actual", &t.cm, &actual);
+        print_hygiene("expected", &t.cm, &expected);
+        panic!()
     });
 }
 
 #[test]
 fn sort_001() {
     assert_sorted(
-        "
-        _9[0] = 133; 
-        const _9 = new ByteArray(32);
-        ",
+        &["_9[0] = 133;", "const _9 = new ByteArray(32);"],
         "
         const _9 = new ByteArray(32);
         _9[0] = 133; 
@@ -35,12 +42,14 @@ fn sort_001() {
 #[test]
 fn sort_002() {
     assert_sorted(
-        "
-        const mod = (function(){
-            const A = v;
-        }());
-        const v = 5;
-        ",
+        &[
+            "
+            const mod = (function(){
+                const A = v;
+            }());
+            ",
+            "const v = 5;",
+        ],
         "
         const v = 5;
         const mod = (function(){
@@ -53,11 +62,10 @@ fn sort_002() {
 #[test]
 fn sort_003() {
     assert_sorted(
-        "
-        class Constraint extends serialization.Serializable {
-        }
-        const serialization = {};
-        ",
+        &[
+            "class Constraint extends serialization.Serializable {}",
+            "const serialization = {};",
+        ],
         "
         const serialization = {};
         class Constraint extends serialization.Serializable {
@@ -69,10 +77,7 @@ fn sort_003() {
 #[test]
 fn sort_004() {
     assert_sorted(
-        "
-        use(global);
-        const global = getGlobal();
-        ",
+        &["use(global);", "const global = getGlobal();"],
         "
         const global = getGlobal();
         use(global);
