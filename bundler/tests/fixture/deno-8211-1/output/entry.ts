4c4f3da2dac480d51ea7177a82cbc0fcd65694f9
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 21:37:29 +0900

    Update test refs

--- bundler/tests/fixture/deno-8211-1/output/entry.ts
+++ bundler/tests/fixture/deno-8211-1/output/entry.ts
@@ -2,9 +2,9 @@ class Zone3 {
 }
 const __default = Zone3;
 const Zone1 = __default;
-const Zone2 = __default;
 class FixedOffsetZone2 extends Zone1 {
 }
+const Zone2 = __default;
 const __default1 = FixedOffsetZone2;
 const FixedOffsetZone1 = __default1;
 export { Zone2 as Zone, FixedOffsetZone1 as FixedOffsetZone };
