21b5285eee1e99e62db2f9bcbc0db06f2bb9c444
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 19:08:17 +0900

    sort/graph: Optimize for read

--- bundler/src/bundler/modules/sort/graph.rs
+++ bundler/src/bundler/modules/sort/graph.rs
@@ -4,11 +4,16 @@ use petgraph::graphmap::AllEdges;
 use petgraph::graphmap::DiGraphMap;
 use petgraph::Directed;
 use petgraph::EdgeDirection;
+use petgraph::EdgeDirection::Outgoing;
+use std::collections::HashSet;
 
 /// Used to debug petgraph.
 #[derive(Debug, Default)]
 pub(super) struct StmtDepGraph {
     inner: DiGraphMap<usize, Required>,
+    /// Read-optimized hashset which contains all direct dependencies and
+    /// transitive dependencies.
+    paths: HashSet<(usize, usize)>,
 }
 
 impl StmtDepGraph {
@@ -29,6 +34,22 @@ impl StmtDepGraph {
     pub fn add_edge(&mut self, a: usize, b: usize, required: Required) {
         println!("[Graph][Edge]{},{}", a, b);
         self.inner.add_edge(a, b, required);
+
+        self.insert_transitives(a, b);
+    }
+
+    fn insert_transitives(&mut self, from: usize, to: usize) {
+        if !self.paths.insert((from, to)) {
+            return;
+        }
+
+        for transitive in self
+            .inner
+            .neighbors_directed(to, Outgoing)
+            .collect::<Vec<_>>()
+        {
+            self.insert_transitives(from, transitive)
+        }
     }
 
     pub fn all_edges(&self) -> AllEdges<usize, Required, Directed> {
@@ -48,25 +69,26 @@ impl StmtDepGraph {
     }
 
     pub fn has_a_path(&self, from: usize, to: usize) -> bool {
-        if cfg!(debug_assertions) {
-            assert_eq!(self.inner.node_count(), self.inner.nodes().count());
-            assert_eq!(self.inner.edge_count(), self.inner.all_edges().count());
-            for (a, b, _) in self.inner.all_edges() {
-                assert!(self.inner.contains_node(a));
-                assert!(self.inner.contains_node(b));
-            }
-        }
+        // if cfg!(debug_assertions) {
+        //     assert_eq!(self.inner.node_count(), self.inner.nodes().count());
+        //     assert_eq!(self.inner.edge_count(), self.inner.all_edges().count());
+        //     for (a, b, _) in self.inner.all_edges() {
+        //         assert!(self.inner.contains_node(a));
+        //         assert!(self.inner.contains_node(b));
+        //     }
+        // }
 
-        eprintln!(
-            "[Graph]all_simple_paths: {} => {}; nodes = {}, edges = {}",
-            from,
-            to,
-            self.inner.node_count(),
-            self.inner.edge_count()
-        );
-        all_simple_paths::<Vec<_>, _>(&self.inner, from, to, 0, None)
-            .next()
-            .is_some()
+        // eprintln!(
+        //     "[Graph]all_simple_paths: {} => {}; nodes = {}, edges = {}",
+        //     from,
+        //     to,
+        //     self.inner.node_count(),
+        //     self.inner.edge_count()
+        // );
+        self.paths.contains(&(from, to))
+        // all_simple_paths::<Vec<_>, _>(&self.inner, from, to, 0, None)
+        //     .next()
+        //     .is_some()
     }
 
     pub fn all_simple_paths(&self, from: usize, to: usize) -> Vec<Vec<usize>> {
