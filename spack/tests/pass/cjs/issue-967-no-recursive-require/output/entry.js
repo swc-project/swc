81cf8d7b37a3bb7b4bfd814489fe975234e1d6a0
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 21:48:22 +0900

    Update test refs

--- spack/tests/pass/cjs/issue-967-no-recursive-require/output/entry.js
+++ spack/tests/pass/cjs/issue-967-no-recursive-require/output/entry.js
@@ -16,12 +16,12 @@ function __spack_require__(mod) {
 var load = __spack_require__.bind(void 0, function(module, exports) {
     console.log('a');
 });
+load();
 var load1 = __spack_require__.bind(void 0, function(module, exports) {
     console.log('b');
 });
+load1();
 var load2 = __spack_require__.bind(void 0, function(module, exports) {
     console.log('c');
 });
-load();
-load1();
 load2();
--- spack/tests/pass/cjs/issue-967-recursive-require/output/entry.js
+++ spack/tests/pass/cjs/issue-967-recursive-require/output/entry.js
@@ -18,30 +18,30 @@ var load = __spack_require__.bind(void 0, function(module, exports) {
     module.exports = 'b';
 });
 var load1 = __spack_require__.bind(void 0, function(module, exports) {
-    console.log('c');
-});
-var load2 = __spack_require__.bind(void 0, function(module, exports) {
-    var load3 = __spack_require__.bind(void 0, function(module1, exports1) {
-        var load4 = __spack_require__.bind(void 0, function(module2, exports2) {
+    var load2 = __spack_require__.bind(void 0, function(module1, exports1) {
+        var load3 = __spack_require__.bind(void 0, function(module2, exports2) {
             module2.exports = {
                 default: 'a-a-a'
             };
         });
-        module1.exports = load4();
+        module1.exports = load3();
     });
-    var load4 = __spack_require__.bind(void 0, function(module1, exports1) {
+    var load3 = __spack_require__.bind(void 0, function(module1, exports1) {
         console.log('a-b');
         exports1.default = 'ab';
     });
-    var aa = load3();
-    var bb = load4();
+    var aa = load2();
+    var bb = load3();
     load();
     module.exports = {
         aa: aa,
         bb: bb
     };
 });
-var b = load();
 load1();
-console.log(b);
+var b = load();
+var load2 = __spack_require__.bind(void 0, function(module, exports) {
+    console.log('c');
+});
 load2();
+console.log(b);
