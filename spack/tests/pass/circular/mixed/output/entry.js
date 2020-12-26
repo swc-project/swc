08c4b080f2dd4c34f42cb63333a6fd863e9b21b0
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 21:44:10 +0900

    Update test refs

--- spack/tests/pass/circular/mixed/output/entry.js
+++ spack/tests/pass/circular/mixed/output/entry.js
@@ -1,15 +1,15 @@
 console.log('c');
 class A {
     method() {
-        return new B2();
+        return new B3();
     }
 }
 const A1 = A;
 const A2 = A1;
 const A3 = A1;
-class B extends A2 {
+class B extends A3 {
 }
 const B1 = B;
 const B2 = B1;
+console.log(A2, B2);
 const B3 = B1;
-console.log(A3, B3);
