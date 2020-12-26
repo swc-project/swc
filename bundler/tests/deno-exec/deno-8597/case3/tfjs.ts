a2a33c8e5fde4b892fad8c7d47473c4e06daa155
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 20:58:19 +0900

    Smaller repro

---
+++ bundler/tests/deno-exec/deno-8597/case3/tfjs.ts
@@ -0,0 +1,5 @@
+import { version_core } from "https://cdn.skypack.dev//-/@tensorflow/tfjs-core@v2.6.0-JX8lR1C58ILDYMLTVvxc/dist=es2020/@tensorflow/tfjs-core.js";
+export * from "https://cdn.skypack.dev//-/@tensorflow/tfjs-core@v2.6.0-JX8lR1C58ILDYMLTVvxc/dist=es2020/@tensorflow/tfjs-core.js";
+export * from "https://cdn.skypack.dev//-/@tensorflow/tfjs-layers@v2.6.0-CoxcCXNxQxzq0ZkceiB7/dist=es2020/@tensorflow/tfjs-layers.js";
+export * from "https://cdn.skypack.dev//-/@tensorflow/tfjs-converter@v2.6.0-PVxqoUi9WvmXRY4ebp1A/dist=es2020/@tensorflow/tfjs-converter.js";
+
