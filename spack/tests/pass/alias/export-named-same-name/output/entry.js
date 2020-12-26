5b9db3922a189d8ef6c5f2eb57f528cd64aa1754
강동윤 <kdy1997.dev@gmail.com>
2020-12-26 21:40:38 +0900

    Update test refs

--- spack/tests/pass/alias/export-named-same-name/output/entry.js
+++ spack/tests/pass/alias/export-named-same-name/output/entry.js
@@ -1,8 +1,8 @@
 const foo = 1;
-const bar = foo;
-const bar1 = bar;
-const bar2 = 2;
-const baz = bar2;
+const bar = 2;
+const bar1 = foo;
+const bar2 = bar1;
+console.log(bar2);
+const baz = bar;
 const baz1 = baz;
-console.log(bar1);
 console.log(baz1);
