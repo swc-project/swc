439511a8cb731c7667906aad5a549c4793a8cca5
강동윤 <kdy1997.dev@gmail.com>
2021-01-02 15:24:48 +0900

    Implement preparation

--- ecmascript/optimizer/src/ast.rs
+++ ecmascript/optimizer/src/ast.rs
@@ -1,4 +1,20 @@
-use swc_ecma_ast::Module;
+use swc_ecma_ast::*;
+use swc_ecma_visit::noop_visit_mut_type;
+use swc_ecma_visit::VisitMut;
+use swc_ecma_visit::VisitMutWith;
 
 /// Make every statements `identifiable` by marking all spans.
-pub(crate) fn prepare(m: &mut Module) {}
+///
+/// This requires [swc_common::GLOBALS] to be set.
+pub(crate) fn prepare(m: &mut Module) {
+    m.visit_mut_with(&mut Preparer)
+}
+
+struct Preparer;
+
+impl VisitMut for Preparer {
+    noop_visit_mut_type!();
+
+    /// Noop. This is performance optimization.
+    fn visit_mut_lit(&mut self, _: &mut Lit) {}
+}
