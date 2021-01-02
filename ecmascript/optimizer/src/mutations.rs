df900b2bf11a6bf0f3e351ebfafbd8041c2c24fd
강동윤 <kdy1997.dev@gmail.com>
2021-01-02 15:30:34 +0900

    Implement mutation applier, which can drop unused statements

--- ecmascript/optimizer/src/mutations.rs
+++ ecmascript/optimizer/src/mutations.rs
@@ -1,6 +1,10 @@
 use fxhash::FxHashMap;
+use swc_common::Spanned;
 use swc_common::SyntaxContext;
 use swc_ecma_ast::*;
+use swc_ecma_visit::noop_visit_mut_type;
+use swc_ecma_visit::VisitMut;
+use swc_ecma_visit::VisitMutWith;
 
 #[derive(Debug, Default)]
 pub(crate) struct Mutations {
@@ -14,4 +18,49 @@ impl Mutations {
 #[derive(Debug, Default)]
 pub(crate) struct ModuleItemMut {
     pub drop: bool,
+}
+
+#[derive(Debug)]
+struct Operator<'a> {
+    m: &'a mut Mutations,
+}
+
+impl VisitMut for Operator<'_> {
+    noop_visit_mut_type!();
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        stmts.visit_mut_children_with(self);
+
+        stmts.retain(|stmt| {
+            match stmt {
+                Stmt::Empty(..) => return false,
+                _ => {}
+            }
+            if let Some(ModuleItemMut { drop }) = self.m.module_items.get(&stmt.span().ctxt) {
+                if drop {
+                    return false;
+                }
+            }
+
+            true
+        });
+    }
+
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        stmts.visit_mut_children_with(self);
+
+        stmts.retain(|stmt| {
+            match stmt {
+                ModuleItem::Stmt(Stmt::Empty(..)) => return false,
+                _ => {}
+            }
+            if let Some(ModuleItemMut { drop }) = self.m.module_items.get(&stmt.span().ctxt) {
+                if drop {
+                    return false;
+                }
+            }
+
+            true
+        });
+    }
 }
