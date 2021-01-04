917956776959e198b5689ab2fb4496e3f37bb7f1
강동윤 <kdy1997.dev@gmail.com>
2021-01-05 00:41:05 +0900

    More imports

--- ecmascript/transforms/react/src/jsx/tests.rs
+++ ecmascript/transforms/react/src/jsx/tests.rs
@@ -3,6 +3,8 @@ use crate::display_name;
 use swc_common::{chain, Mark};
 use swc_ecma_transforms_compat::es2015::arrow;
 use swc_ecma_transforms_compat::es2015::classes;
+use swc_ecma_transforms_compat::es3::property_literals;
+use swc_ecma_transforms_module::common_js::common_js;
 use swc_ecma_transforms_testing::test;
 use swc_ecma_transforms_testing::Tester;
 
