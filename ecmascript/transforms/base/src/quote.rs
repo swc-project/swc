d0dd6e4020bf19238d891415db2f7402b3a12e2f
강동윤 <kdy1997.dev@gmail.com>
2021-01-04 16:59:08 +0900

    Fix helper macros

--- ecmascript/transforms/base/src/quote.rs
+++ ecmascript/transforms/base/src/quote.rs
@@ -28,16 +28,18 @@ macro_rules! helper_expr {
     }};
 
     ($span:expr, $field_name:ident, $s:tt) => {{
+        use swc_ecma_utils::quote_ident;
         debug_assert!(
             !$s.starts_with("_"),
             "helper! macro should not invoked with '_' prefix"
         );
-        let mark = enable_helper!($field_name);
+        let mark = $crate::enable_helper!($field_name);
         let span = $span.apply_mark(mark);
-        let external = crate::helpers::HELPERS.with(|helper| helper.external());
+        let external = $crate::helpers::HELPERS.with(|helper| helper.external());
 
         if external {
-            quote_ident!(span, "swcHelpers").make_member(quote_ident!($span, external_name!($s)))
+            quote_ident!(span, "swcHelpers")
+                .make_member(quote_ident!($span, $crate::external_name!($s)))
         } else {
             Expr::from(quote_ident!(span, concat!('_', $s)))
         }
@@ -49,10 +51,10 @@ macro_rules! helper_expr {
 #[macro_export]
 macro_rules! helper {
     ($field_name:ident, $s:tt) => {{
-        helper!(::swc_common::DUMMY_SP, $field_name, $s)
+        $crate::helper!(::swc_common::DUMMY_SP, $field_name, $s)
     }};
 
     ($span:expr, $field_name:ident, $s:tt) => {{
-        helper_expr!($span, $field_name, $s).as_callee()
+        $crate::helper_expr!($span, $field_name, $s).as_callee()
     }};
 }
