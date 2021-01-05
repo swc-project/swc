4ac5244eabe4be3dc4d39c04f41a0f287b4da973
강동윤 <kdy1997.dev@gmail.com>
2021-01-05 13:23:31 +0900

    Fix

--- ecmascript/transforms/src/lib.rs
+++ ecmascript/transforms/src/lib.rs
@@ -11,10 +11,17 @@ pub use swc_ecma_transforms_base::hygiene;
 pub use swc_ecma_transforms_base::pass;
 pub use swc_ecma_transforms_base::perf;
 pub use swc_ecma_transforms_base::resolver;
+#[cfg(feature = "swc_ecma_transforms_compat")]
 pub use swc_ecma_transforms_compat as compat;
+#[cfg(feature = "swc_ecma_transforms_module")]
 pub use swc_ecma_transforms_module as modules;
+#[cfg(feature = "swc_ecma_transforms_optimization")]
 pub use swc_ecma_transforms_optimization as optimization;
+#[cfg(feature = "swc_ecma_transforms_optimization")]
 pub use swc_ecma_transforms_optimization::const_modules;
+#[cfg(feature = "swc_ecma_transforms_proposal")]
 pub use swc_ecma_transforms_proposal as proposals;
+#[cfg(feature = "swc_ecma_transforms_react")]
 pub use swc_ecma_transforms_react as react;
+#[cfg(feature = "swc_ecma_transforms_typescript")]
 pub use swc_ecma_transforms_typescript as typescript;
