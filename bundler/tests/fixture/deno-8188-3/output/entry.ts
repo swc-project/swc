186029b54398577901e4db235f0ee3ef5b455733
강동윤 <kdy1997.dev@gmail.com>
2021-01-06 17:48:38 +0900

    Update test refs

--- bundler/tests/fixture/deno-8188-3/output/entry.ts
+++ bundler/tests/fixture/deno-8188-3/output/entry.ts
@@ -1,7 +1,8 @@
 const a = 1;
 const a1 = a;
-const a2 = a1;
-const a3 = a;
-const b = a3;
+const b = a1;
 const b1 = b;
-console.log(a2, b1);
+const b2 = b1;
+const a2 = a1;
+const a3 = a2;
+console.log(a3, b2);
--- bundler/tests/fixture/deno-8211-1/output/entry.ts
+++ bundler/tests/fixture/deno-8211-1/output/entry.ts
@@ -1,10 +1,12 @@
 class Zone3 {
 }
 const __default = Zone3;
-const Zone1 = __default;
-class FixedOffsetZone2 extends Zone1 {
+const __default1 = __default;
+const Zone1 = __default1;
+const Zone2 = __default1;
+class FixedOffsetZone2 extends Zone2 {
 }
-const Zone2 = __default;
-const __default1 = FixedOffsetZone2;
-const FixedOffsetZone1 = __default1;
-export { Zone2 as Zone, FixedOffsetZone1 as FixedOffsetZone };
+const __default2 = FixedOffsetZone2;
+const __default3 = __default2;
+const FixedOffsetZone1 = __default3;
+export { Zone1 as Zone, FixedOffsetZone1 as FixedOffsetZone };
