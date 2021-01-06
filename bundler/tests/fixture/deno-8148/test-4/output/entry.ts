b85c8c801adc6fcb8edb049697853d10ed46f8b1
강동윤 <kdy1997.dev@gmail.com>
2021-01-06 17:07:08 +0900

    Update test refs

--- bundler/tests/fixture/deno-8148/test-4/output/entry.ts
+++ bundler/tests/fixture/deno-8148/test-4/output/entry.ts
@@ -3,12 +3,14 @@ const __default = {
     "b": "b",
     "c": "c"
 };
-const foo = __default;
+const __default1 = __default;
+const foo = __default1;
 console.log(foo);
-const __default1 = {
+const __default2 = {
     "a": "a1",
     "b": "b1",
     "c": "c1"
 };
-const foo1 = __default1;
+const __default3 = __default2;
+const foo1 = __default3;
 console.log(foo1);
