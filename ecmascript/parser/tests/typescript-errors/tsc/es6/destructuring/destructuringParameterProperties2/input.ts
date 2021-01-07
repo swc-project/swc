af3e71690930c7e95f37f9797817a2270ff1f329
강동윤 <kdy1997.dev@gmail.com>
2021-01-07 14:03:24 +0900

    More tests

---
+++ ecmascript/parser/tests/typescript-errors/tsc/es6/destructuring/destructuringParameterProperties2/input.ts
@@ -0,0 +1,8 @@
+﻿class C1 {
+    constructor(private k: number, private [a, b, c]: [number, string, boolean]) {
+        if ((b === undefined && c === undefined) || (this.b === undefined && this.c === undefined)) {
+            this.a = a || k;
+        }
+    }
+
+}
