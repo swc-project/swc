25838634ec2566cbc8b875edb7f54919403e7e3c
강동윤 <kdy1997.dev@gmail.com>
2021-01-07 14:17:02 +0900

    More

---
+++ ecmascript/parser/tests/typescript-errors/tsc/es6/destructuring/destructuringParameterProperties4/input.ts
@@ -0,0 +1,12 @@
+﻿// @target: es6
+
+class C1<T, U, V> {
+    constructor(private k: T, protected [a, b, c]: [T, U, V]) {
+        if ((b === undefined && c === undefined) || (this.b === undefined && this.c === undefined)) {
+            this.a = a || k;
+        }
+    }
+
+    pu
+
+}
---
+++ ecmascript/parser/tests/typescript-errors/tsc/es6/destructuring/destructuringParameterProperties5/input.ts
@@ -0,0 +1,9 @@
+﻿type ObjType1 = { x: number; y: string; z: boolean }
+type TupleType1 = [ObjType1, number, string]
+
+class C1 {
+    constructor(public [{ x1, x2, x3 }, y, z]: TupleType1) {
+        var foo: any = x1 || x2 || x3 || y || z;
+        var bar: any = this.x1 || this.x2 || this.x3 || this.y || this.z;
+    }
+}
