f6f05dc803e489fd76285cde496683de9511df71
강동윤 <kdy1997.dev@gmail.com>
2021-01-07 18:14:21 +0900

    Update test refs

--- spack/tests/pass/node-modules/library/simple/output/entry.js
+++ spack/tests/pass/node-modules/library/simple/output/entry.js
@@ -193,7 +193,6 @@ var load = __spack_require__.bind(void 0, function(module, exports) {
         };
     });
     module.exports = load1();
-    const progress = module.exports;
 });
 var { default: progress  } = load();
 console.log(progress);
