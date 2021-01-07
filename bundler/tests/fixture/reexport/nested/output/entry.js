8733bac7687d7cda744f8f224ec268366767a5a6
강동윤 <kdy1997.dev@gmail.com>
2021-01-07 16:12:16 +0900

    Update test ref

--- bundler/tests/fixture/reexport/nested/output/entry.js
+++ bundler/tests/fixture/reexport/nested/output/entry.js
@@ -1,8 +1,8 @@
 const a1 = 1;
 function foo1() {
 }
-export { foo1 as foo };
 class Class1 {
 }
-export { Class1 as Class };
 export { a1 as a };
+export { foo1 as foo };
+export { Class1 as Class };
