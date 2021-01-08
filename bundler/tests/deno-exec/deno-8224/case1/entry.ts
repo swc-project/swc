44c46e9e29b730b4dbc36f813334edc34217d48d
강동윤 <kdy1997.dev@gmail.com>
2021-01-08 18:16:23 +0900

    Add some tests

---
+++ bundler/tests/deno-exec/deno-8224/case1/entry.ts
@@ -0,0 +1,2 @@
+import * as nodelibs from 'https://dev.jspm.io/npm:@jspm/core@1.1.1/nodelibs/chunk-0c2d1322.js';
+console.log(nodelibs)
---
+++ bundler/tests/deno-exec/deno-8224/case2/entry.ts
@@ -0,0 +1,1 @@
+export * from 'https://dev.jspm.io/npm:@jspm/core@1.1.1/nodelibs/chunk-cffba9d4.js';
