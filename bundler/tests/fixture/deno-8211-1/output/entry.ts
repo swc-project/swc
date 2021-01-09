bed876e84ebff49f3328d3bc692ca5f3ba08e4fe
강동윤 <kdy1997.dev@gmail.com>
2021-01-09 17:53:03 +0900

    Update test refs

--- bundler/tests/fixture/deno-8211-1/output/entry.ts
+++ bundler/tests/fixture/deno-8211-1/output/entry.ts
@@ -1,12 +1,14 @@
-class Zone3 {
+class Zone {
 }
-const __default = Zone3;
+const __default = Zone;
 const __default1 = __default;
 const Zone1 = __default1;
-const Zone2 = __default1;
-class FixedOffsetZone2 extends Zone2 {
+const Zone2 = Zone1;
+const Zone3 = __default1;
+class FixedOffsetZone extends Zone3 {
 }
-const __default2 = FixedOffsetZone2;
+const __default2 = FixedOffsetZone;
 const __default3 = __default2;
 const FixedOffsetZone1 = __default3;
+const FixedOffsetZone2 = FixedOffsetZone1;
 export { Zone1 as Zone, FixedOffsetZone1 as FixedOffsetZone };
--- bundler/tests/fixture/deno-8211-2/output/entry.ts
+++ bundler/tests/fixture/deno-8211-2/output/entry.ts
@@ -1,21 +1,24 @@
-class Zone3 {
+class Zone {
 }
-const __default = Zone3;
+const __default = Zone;
 const __default1 = __default;
 const Zone1 = __default1;
-const Zone2 = __default1;
-class FixedOffsetZone3 extends Zone2 {
+const Zone2 = Zone1;
+const Zone3 = __default1;
+class FixedOffsetZone extends Zone3 {
 }
-const __default2 = FixedOffsetZone3;
+const __default2 = FixedOffsetZone;
 const __default3 = __default2;
 const FixedOffsetZone1 = __default3;
-const FixedOffsetZone2 = __default3;
-class Info2 {
+const FixedOffsetZone2 = FixedOffsetZone1;
+const FixedOffsetZone3 = __default3;
+class Info {
     use() {
-        console.log(FixedOffsetZone2);
+        console.log(FixedOffsetZone3);
     }
 }
-const __default4 = Info2;
+const __default4 = Info;
 const __default5 = __default4;
 const Info1 = __default5;
+const Info2 = Info1;
 export { Zone1 as Zone, Info1 as Info, FixedOffsetZone1 as FixedOffsetZone };
