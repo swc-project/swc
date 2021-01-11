bea54811887a47325b54e09fb0e5783c88f7f04c
강동윤 <kdy1997.dev@gmail.com>
2021-01-11 21:39:17 +0900

    Custom `Optional<T>`

--- ecmascript/minifier/src/pass/compress/mod.rs
+++ ecmascript/minifier/src/pass/compress/mod.rs
@@ -1,12 +1,13 @@
+use crate::util::Optional;
 use drop_console::drop_console;
+use std::borrow::Cow;
 use swc_common::chain;
-use swc_common::pass::Optional;
+use swc_common::pass::CompilerPass;
 use swc_common::pass::Repeat;
 use swc_common::pass::Repeated;
 use swc_ecma_transforms::optimization::simplify::expr_simplifier;
-use swc_ecma_transforms::pass::RepeatedJsPass;
+use swc_ecma_transforms::pass::JsPass;
 use swc_ecma_visit::as_folder;
-use swc_ecma_visit::Fold;
 use swc_ecma_visit::VisitMut;
 
 mod drop_console;
@@ -18,7 +19,7 @@ pub struct CompressorOptions {
     pub drop_console: bool,
 }
 
-pub fn compressor(options: &CompressorOptions) -> impl '_ + Fold {
+pub fn compressor(options: &CompressorOptions) -> impl '_ + JsPass {
     let console_remover = Optional {
         enabled: options.drop_console,
         visitor: drop_console(),
@@ -27,14 +28,36 @@ pub fn compressor(options: &CompressorOptions) -> impl '_ + Fold {
         enabled: options.expr,
         visitor: expr_simplifier(),
     };
-    let compressor = Compressor { options };
+    let compressor = Compressor {
+        options,
+        changed: false,
+    };
 
-    chain!(console_remover, expr_simplifier, as_folder(compressor))
+    let repeated_passes = Repeat::new(chain!(expr_simplifier, as_folder(compressor)));
+
+    chain!(console_remover, repeated_passes)
 }
 
 #[derive(Debug)]
 struct Compressor<'a> {
     options: &'a CompressorOptions,
+    changed: bool,
+}
+
+impl CompilerPass for Compressor<'_> {
+    fn name() -> Cow<'static, str> {
+        "compressor".into()
+    }
+}
+
+impl Repeated for Compressor<'_> {
+    fn changed(&self) -> bool {
+        self.changed
+    }
+
+    fn reset(&mut self) {
+        self.changed = false;
+    }
 }
 
 impl VisitMut for Compressor<'_> {}
--- ecmascript/minifier/src/util/mod.rs
+++ ecmascript/minifier/src/util/mod.rs
@@ -1,1 +1,60 @@
+use swc_common::pass::CompilerPass;
+use swc_common::pass::Repeated;
+use swc_ecma_ast::Module;
+use swc_ecma_visit::Fold;
+use swc_ecma_visit::FoldWith;
+
 pub(crate) mod base54;
+
+/// TODO(kdy1): Modify swc_visit.
+/// Actually we should implement `swc_visit::Repeated` for
+/// `swc_visit::Optional`. But I'm too lazy to bump versions.
+pub(crate) struct Optional<V> {
+    pub enabled: bool,
+    pub visitor: V,
+}
+
+impl<V> Repeated for Optional<V>
+where
+    V: Repeated,
+{
+    #[inline]
+    fn changed(&self) -> bool {
+        if self.enabled {
+            return false;
+        }
+
+        self.visitor.changed()
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+        if self.enabled {
+            return;
+        }
+
+        self.visitor.reset()
+    }
+}
+
+impl<V> CompilerPass for Optional<V>
+where
+    V: CompilerPass,
+{
+    fn name() -> std::borrow::Cow<'static, str> {
+        V::name()
+    }
+}
+
+impl<V> Fold for Optional<V>
+where
+    V: Fold,
+{
+    #[inline(always)]
+    fn fold_module(&mut self, module: Module) -> Module {
+        if !self.enabled {
+            return module;
+        }
+        module.fold_with(&mut self.visitor)
+    }
+}
