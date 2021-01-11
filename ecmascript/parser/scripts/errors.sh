7044a87da7f2fdf50f020a0330fe094e869d0c25
강동윤 <kdy1997.dev@gmail.com>
2021-01-12 02:23:43 +0900

    Make test script invoke correctness test first

--- ecmascript/parser/scripts/errors.sh
+++ ecmascript/parser/scripts/errors.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
+UPDATE=0 cargo test --test typescript
+
 export RUST_TEST_TIME_INTEGRATION=1000,3000
 # cargo test --test errors $@ -- -Z unstable-options --ensure-time
 cargo test --test errors $@
