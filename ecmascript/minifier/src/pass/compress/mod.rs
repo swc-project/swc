e789a6e8f5fd3d16f7e46d2f19f70e11911d45ff
강동윤 <kdy1997.dev@gmail.com>
2021-01-11 22:12:45 +0900

    Baical structure of compressor

--- ecmascript/minifier/src/pass/compress/mod.rs
+++ ecmascript/minifier/src/pass/compress/mod.rs
@@ -1,13 +1,16 @@
 use crate::util::Optional;
 use drop_console::drop_console;
+use swc_ecma_visit::VisitMutWith;
 use std::borrow::Cow;
 use swc_common::chain;
 use swc_common::pass::CompilerPass;
 use swc_common::pass::Repeat;
 use swc_common::pass::Repeated;
+use swc_ecma_ast::*;
 use swc_ecma_transforms::optimization::simplify::expr_simplifier;
 use swc_ecma_transforms::pass::JsPass;
 use swc_ecma_visit::as_folder;
+use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
 
 mod drop_console;
@@ -17,6 +20,7 @@ pub struct CompressorOptions {
     /// Should we simplify expressions?
     pub expr: bool,
     pub drop_console: bool,
+    pub reduce_vars: bool,
 }
 
 pub fn compressor(options: &CompressorOptions) -> impl '_ + JsPass {
@@ -24,24 +28,33 @@ pub fn compressor(options: &CompressorOptions) -> impl '_ + JsPass {
         enabled: options.drop_console,
         visitor: drop_console(),
     };
-    let expr_simplifier = Optional {
+    let expr_pass = Optional {
         enabled: options.expr,
         visitor: expr_simplifier(),
     };
     let compressor = Compressor {
         options,
+        pass: 0,
         changed: false,
     };
 
-    let repeated_passes = Repeat::new(chain!(expr_simplifier, as_folder(compressor)));
+    let repeated_passes = Repeat::new(chain!(expr_pass, as_folder(compressor)));
 
-    chain!(console_remover, repeated_passes)
+    chain!(
+        console_remover,
+        repeated_passes,
+        Optional {
+            enabled: options.expr,
+            visitor: expr_simplifier(),
+        }
+    )
 }
 
 #[derive(Debug)]
 struct Compressor<'a> {
     options: &'a CompressorOptions,
     changed: bool,
+    pass: usize,
 }
 
 impl CompilerPass for Compressor<'_> {
@@ -57,7 +70,18 @@ impl Repeated for Compressor<'_> {
 
     fn reset(&mut self) {
         self.changed = false;
+        self.pass += 1;
     }
 }
 
-impl VisitMut for Compressor<'_> {}
+impl VisitMut for Compressor<'_> {
+    noop_visit_mut_type!();
+
+    fn visit_mut_module(&mut self, n: &mut Module) {
+        if self.pass > 0 || self.options.reduce_vars {
+            // reset_opt_flags
+        }
+
+        n.visit_mut_children_with(self);
+    }
+}
