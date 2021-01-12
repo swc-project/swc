2fbaacc6b15764ca2bf01b9c7d36ac98e5817180
강동윤 <kdy1997.dev@gmail.com>
2021-01-12 15:03:01 +0900

    Typescript test is too slow

--- ecmascript/parser/scripts/errors.sh
+++ ecmascript/parser/scripts/errors.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-UPDATE=0 cargo test --test typescript
+# UPDATE=0 cargo test --test typescript
 
 export RUST_TEST_TIME_INTEGRATION=1000,3000
 # cargo test --test errors $@ -- -Z unstable-options --ensure-time
