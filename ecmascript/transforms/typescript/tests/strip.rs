4149cac87326a02b37939e21fd31cbeff5209e44
강동윤 <kdy1997.dev@gmail.com>
2021-01-12 19:35:20 +0900

    One more

--- ecmascript/transforms/typescript/tests/strip.rs
+++ ecmascript/transforms/typescript/tests/strip.rs
@@ -3383,3 +3383,30 @@ to!(
     })(util || (util = {}));
     "
 );
+
+to!(
+    namespace_002,
+    "
+    export namespace util {
+        const c = 3;
+        export function foo() {
+
+        }
+
+        function bar() {
+
+        }
+    }
+    ",
+    "
+    export var util;
+    (function (util) {
+        const c = 3;
+        function foo() {
+        }
+        util.foo = foo;
+        function bar() {
+        }
+    })(util || (util = {}));
+    "
+);
