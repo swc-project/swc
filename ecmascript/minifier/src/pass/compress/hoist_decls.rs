a627ec257c279da98bdecc5b80e8e0b2ff80339c
강동윤 <kdy1997.dev@gmail.com>
2021-01-13 16:56:51 +0900

    decl_hoister: Reduce vars

--- ecmascript/minifier/src/pass/compress/hoist_decls.rs
+++ ecmascript/minifier/src/pass/compress/hoist_decls.rs
@@ -1,5 +1,9 @@
+use std::mem::take;
+
+use swc_ecma_ast::*;
 use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
+use swc_ecma_visit::VisitMutWith;
 
 pub(super) fn decl_hoister() -> Hoister {
     Hoister {}
@@ -7,6 +11,58 @@ pub(super) fn decl_hoister() -> Hoister {
 
 pub(super) struct Hoister {}
 
+impl Hoister {}
+
 impl VisitMut for Hoister {
     noop_visit_mut_type!();
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        stmts.visit_mut_children_with(self);
+
+        // TODO: Hoist vars, ignoring side-effect-free items like fn decl.
+
+        // Merge var declarations fully, if possible.
+        if stmts.windows(2).any(|stmts| match (&stmts[0], &stmts[1]) {
+            (Stmt::Decl(Decl::Var(..)), Stmt::Decl(Decl::Var(..))) => true,
+            _ => false,
+        }) {
+            let orig = take(stmts);
+            let mut new = Vec::with_capacity(orig.len());
+
+            let mut var_decl: Option<VarDecl> = None;
+
+            for stmt in orig {
+                match stmt {
+                    Stmt::Decl(Decl::Var(below)) => {
+                        //
+                        match var_decl.take() {
+                            Some(mut upper) if upper.kind == below.kind => {
+                                upper.decls.extend(below.decls);
+                                var_decl = Some(upper);
+                            }
+                            _ => {
+                                new.extend(var_decl.take().map(Decl::Var).map(Stmt::Decl));
+                                var_decl = Some(below);
+                            }
+                        }
+                    }
+                    _ => {
+                        // If it's not a var decl,
+
+                        new.extend(var_decl.take().map(Decl::Var).map(Stmt::Decl));
+                        new.push(stmt);
+                    }
+                }
+            }
+
+            *stmts = new
+        }
+    }
+
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        // TODO: Hoist vars, ignoring side-effect-free items like fn decl.
+
+        // TODO: Merge vars
+        stmts.visit_mut_children_with(self);
+    }
 }
