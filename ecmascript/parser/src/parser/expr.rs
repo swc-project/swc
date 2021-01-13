f6cbead9611e646336ace761252276b1afd7c81d
강동윤 <kdy1997.dev@gmail.com>
2021-01-14 01:11:48 +0900

    fixup

--- ecmascript/parser/src/parser/expr.rs
+++ ecmascript/parser/src/parser/expr.rs
@@ -81,7 +81,7 @@ impl<'a, I: Tokens> Parser<I> {
         if self.errors_for_backtraing.is_none() {
             match cur!(self, true) {
                 Ok(token) => match token {
-                    Token::BinOp(BinOpToken::Add) | Token::BinOp(BinOpToken::Minus) => {}
+                    Token::BinOp(BinOpToken::Add) | Token::BinOp(BinOpToken::Sub) => {}
                     Token::AssignOp(..) | Token::BinOp(..) => {
                         self.emit_err(self.input.cur_span(), SyntaxError::ExpectedExpr);
                         bump!(self);
