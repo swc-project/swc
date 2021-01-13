00bff0550196c53d1133682cd7f866ddf363d536
강동윤 <kdy1997.dev@gmail.com>
2021-01-13 14:23:55 +0900

    Copy more tests

---
+++ ecmascript/minifier/tests/terser/compress/hoist_props/single_use/input.js
@@ -0,0 +1,6 @@
+var obj = {
+    bar: function () {
+        return 42;
+    },
+};
+console.log(obj.bar());
---
+++ ecmascript/minifier/tests/terser/compress/hoist_props/single_use/output.js
@@ -0,0 +1,5 @@
+console.log({
+    bar: function () {
+        return 42;
+    },
+}.bar());
