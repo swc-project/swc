56a2b81c0c1779ed1fb59cd42c48d9a4261a4444
강동윤 <kdy1997.dev@gmail.com>
2021-01-13 18:08:25 +0900

    WIP: Var reducer

--- ecmascript/minifier/src/pass/compress/reduce_vars.rs
+++ ecmascript/minifier/src/pass/compress/reduce_vars.rs
@@ -1,5 +1,6 @@
 use std::mem::take;
 use swc_ecma_ast::*;
+use swc_ecma_utils::StmtLike;
 use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
@@ -11,11 +12,25 @@ pub(super) fn var_reducer() -> impl VisitMut {
 
 struct Reducer {}
 
+impl Reducer {
+    fn handle_stmt_likes<T>(&mut self, stmts: &mut Vec<T>)
+    where
+        T: StmtLike,
+        Vec<T>: VisitMutWith<Self>,
+    {
+        stmts.visit_mut_children_with(self);
+    }
+}
+
 impl VisitMut for Reducer {
     noop_visit_mut_type!();
 
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        self.handle_stmt_likes(stmts);
+    }
+
     fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
-        stmts.visit_mut_children_with(self);
+        self.handle_stmt_likes(stmts);
 
         // Merge var declarations fully, if possible.
         if stmts.windows(2).any(|stmts| match (&stmts[0], &stmts[1]) {
