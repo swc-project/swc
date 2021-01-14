409a0f4e959adcc93aeba333784cdfefa0c75b4f
강동윤 <kdy1997.dev@gmail.com>
2021-01-14 16:48:35 +0900

    Our one is better

--- ecmascript/minifier/tests/terser/compress/reduce_vars/multi_def_2/output.js
+++ ecmascript/minifier/tests/terser/compress/reduce_vars/multi_def_2/output.js
@@ -1,6 +1,6 @@
 function f() {
     if (16 == code) var bitsLength = 2, bitsOffset = 3, what = len;
-    else if (17 == code) var bitsLength = 3, bitsOffset = 3, what = (len = 0);
-    else if (18 == code) var bitsLength = 7, bitsOffset = 11, what = (len = 0);
+    else if (17 == code) var bitsLength = 3, bitsOffset = 3, what = len = 0;
+    else if (18 == code) var bitsLength = 7, bitsOffset = 11, what = len = 0;
     var repeatLength = this.getBits(bitsLength) + bitsOffset;
 }
