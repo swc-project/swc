0d269764b9edaf34342f5bb3cff534a0689422c9
강동윤 <kdy1997.dev@gmail.com>
2021-01-14 19:18:00 +0900

    Improve debug util

--- ecmascript/minifier/src/debug.rs
+++ ecmascript/minifier/src/debug.rs
@@ -1,16 +1,29 @@
 use swc_common::sync::Lrc;
 use swc_common::SourceMap;
+use swc_ecma_ast::StrKind;
 use swc_ecma_codegen::text_writer::JsWriter;
 use swc_ecma_codegen::Emitter;
 use swc_ecma_utils::drop_span;
 use swc_ecma_utils::DropSpan;
+use swc_ecma_visit::noop_visit_mut_type;
+use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
+
+pub(crate) struct Normalizer;
+
+impl VisitMut for Normalizer {
+    noop_visit_mut_type!();
+    fn visit_mut_str_kind(&mut self, n: &mut StrKind) {
+        *n = Default::default();
+    }
+}
 
 pub(crate) fn dump<N>(node: &N) -> String
 where
-    N: swc_ecma_codegen::Node + Clone + VisitMutWith<DropSpan>,
+    N: swc_ecma_codegen::Node + Clone + VisitMutWith<DropSpan> + VisitMutWith<Normalizer>,
 {
-    let node = drop_span(node.clone());
+    let mut node = drop_span(node.clone());
+    node.visit_mut_with(&mut Normalizer);
     let mut buf = vec![];
     let cm = Lrc::new(SourceMap::default());
 
