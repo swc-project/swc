4787cc8ec7385210488fb9684f5395a1556725d5
강동윤 <kdy1997.dev@gmail.com>
2021-01-15 19:23:18 +0900

    WIP: Add a test

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -1824,3 +1824,17 @@ to!(
 
     "
 );
+
+to_ts!(
+    as_operator_ambiguity,
+    "
+    interface A<T> { x: T; }
+    interface B { m: string; }
+
+    // Make sure this is a type assertion to an array type, and not nested comparison operators.
+    var x: any;
+    var y = x as A<B>[];
+    var z = y[0].m; // z should be string
+    ",
+    ""
+);
