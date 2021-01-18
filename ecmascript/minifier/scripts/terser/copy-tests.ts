b676e9aed40c5ebefa9df29952bc01b96c320491
강동윤 <kdy1997.dev@gmail.com>
2021-01-18 20:54:08 +0900

    Let's use typescript to copy tests

---
+++ ecmascript/minifier/scripts/terser/copy-tests.ts
@@ -0,0 +1,4 @@
+import * as path from 'path';
+
+const terserTestDir = path.resolve(path.join(__dirname, '..', '..', '..', '..', 'vendor', 'terser'))
+console.log(`Copying tests from ${terserTestDir}`);
