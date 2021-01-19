6459fb2d7485947247a50450c59451efb759f52f
강동윤 <kdy1997.dev@gmail.com>
2021-01-19 14:24:37 +0900

    Split codes

--- ecmascript/minifier/src/compress/reduce_vars/mod.rs
+++ ecmascript/minifier/src/compress/reduce_vars/mod.rs
@@ -1,6 +1,5 @@
 use crate::util::usage::ScopeData;
 use crate::util::usage::UsageAnalyzer;
-use crate::util::ValueExt;
 use fxhash::FxHashMap;
 use retain_mut::RetainMut;
 use std::fmt::Write;
@@ -21,6 +20,8 @@ use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
 use swc_ecma_visit::VisitWith;
+
+mod ops;
 
 #[derive(Debug)]
 pub(super) struct ReducerConfig {
@@ -80,112 +81,6 @@ impl Reducer {
         }
 
         stmts.visit_mut_children_with(self);
-    }
-
-    fn optimize_lit_cmp(&mut self, n: &mut BinExpr) -> Option<Expr> {
-        match n.op {
-            op!("==") | op!("!=") => {
-                let l = n.left.as_pure_bool().opt()?;
-                let r = n.right.as_pure_bool().opt()?;
-
-                let value = if n.op == op!("==") { l == r } else { l != r };
-
-                self.changed = true;
-                return Some(Expr::Lit(Lit::Bool(Bool {
-                    span: n.span,
-                    value,
-                })));
-            }
-            _ => {}
-        }
-
-        None
-    }
-
-    /// This method converts `!1` to `0`.
-    fn optimize_expr_in_bool_ctx(&mut self, n: &mut Expr) {
-        match n {
-            Expr::Unary(UnaryExpr {
-                span,
-                op: op!("!"),
-                arg,
-            }) => {
-                self.optimize_expr_in_bool_ctx(&mut **arg);
-
-                match &**arg {
-                    Expr::Lit(Lit::Num(Number { value, .. })) => {
-                        self.changed = true;
-                        *n = Expr::Lit(Lit::Num(Number {
-                            span: *span,
-                            value: if *value == 0.0 { 1.0 } else { 0.0 },
-                        }))
-                    }
-                    _ => {}
-                }
-            }
-            _ => {}
-        }
-    }
-
-    /// This method does
-    ///
-    /// - `x *= 3` => `x = 3 * x`
-    /// - `x = 3 | x` `x |= 3`
-    /// - `x = 3 & x` => `x &= 3;`
-    /// - `x ^= 3` => `x = 3 ^ x`
-    fn compress_bin_assignment_to_right(&mut self, e: &mut AssignExpr) {
-        // TODO: Handle pure properties.
-        let lhs = match &e.left {
-            PatOrExpr::Expr(e) => match &**e {
-                Expr::Ident(i) => i,
-                _ => return,
-            },
-            PatOrExpr::Pat(p) => match &**p {
-                Pat::Ident(i) => i,
-                _ => return,
-            },
-        };
-
-        let (op, left) = match &mut *e.right {
-            Expr::Bin(BinExpr {
-                left, op, right, ..
-            }) => match &**right {
-                Expr::Ident(r) if lhs.sym == r.sym && lhs.span.ctxt == r.span.ctxt => {
-                    // We need this check because a function call like below can change value of
-                    // operand.
-                    //
-                    // x = g() * x;
-
-                    match &**left {
-                        Expr::This(..) | Expr::Ident(..) | Expr::Lit(..) => {}
-                        _ => return,
-                    }
-
-                    (op, left)
-                }
-                _ => return,
-            },
-            _ => return,
-        };
-
-        let op = match op {
-            BinaryOp::Mul => {
-                op!("*=")
-            }
-            BinaryOp::BitOr => {
-                op!("|=")
-            }
-            BinaryOp::BitXor => {
-                op!("^=")
-            }
-            BinaryOp::BitAnd => {
-                op!("&=")
-            }
-            _ => return,
-        };
-
-        e.op = op;
-        e.right = left.take();
     }
 
     /// `a = a + 1` => `a += 1`.
---
+++ ecmascript/minifier/src/compress/reduce_vars/ops.rs
@@ -0,0 +1,113 @@
+use super::Reducer;
+use crate::util::ValueExt;
+use swc_ecma_ast::*;
+use swc_ecma_transforms_base::ext::MapWithMut;
+use swc_ecma_utils::ExprExt;
+
+impl Reducer {
+    pub(super) fn optimize_lit_cmp(&mut self, n: &mut BinExpr) -> Option<Expr> {
+        match n.op {
+            op!("==") | op!("!=") => {
+                let l = n.left.as_pure_bool().opt()?;
+                let r = n.right.as_pure_bool().opt()?;
+
+                let value = if n.op == op!("==") { l == r } else { l != r };
+
+                self.changed = true;
+                return Some(Expr::Lit(Lit::Bool(Bool {
+                    span: n.span,
+                    value,
+                })));
+            }
+            _ => {}
+        }
+
+        None
+    }
+
+    /// This method converts `!1` to `0`.
+    pub(super) fn optimize_expr_in_bool_ctx(&mut self, n: &mut Expr) {
+        match n {
+            Expr::Unary(UnaryExpr {
+                span,
+                op: op!("!"),
+                arg,
+            }) => {
+                self.optimize_expr_in_bool_ctx(&mut **arg);
+
+                match &**arg {
+                    Expr::Lit(Lit::Num(Number { value, .. })) => {
+                        self.changed = true;
+                        *n = Expr::Lit(Lit::Num(Number {
+                            span: *span,
+                            value: if *value == 0.0 { 1.0 } else { 0.0 },
+                        }))
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+
+    /// This method does
+    ///
+    /// - `x *= 3` => `x = 3 * x`
+    /// - `x = 3 | x` `x |= 3`
+    /// - `x = 3 & x` => `x &= 3;`
+    /// - `x ^= 3` => `x = 3 ^ x`
+    pub(super) fn compress_bin_assignment_to_right(&mut self, e: &mut AssignExpr) {
+        // TODO: Handle pure properties.
+        let lhs = match &e.left {
+            PatOrExpr::Expr(e) => match &**e {
+                Expr::Ident(i) => i,
+                _ => return,
+            },
+            PatOrExpr::Pat(p) => match &**p {
+                Pat::Ident(i) => i,
+                _ => return,
+            },
+        };
+
+        let (op, left) = match &mut *e.right {
+            Expr::Bin(BinExpr {
+                left, op, right, ..
+            }) => match &**right {
+                Expr::Ident(r) if lhs.sym == r.sym && lhs.span.ctxt == r.span.ctxt => {
+                    // We need this check because a function call like below can change value of
+                    // operand.
+                    //
+                    // x = g() * x;
+
+                    match &**left {
+                        Expr::This(..) | Expr::Ident(..) | Expr::Lit(..) => {}
+                        _ => return,
+                    }
+
+                    (op, left)
+                }
+                _ => return,
+            },
+            _ => return,
+        };
+
+        let op = match op {
+            BinaryOp::Mul => {
+                op!("*=")
+            }
+            BinaryOp::BitOr => {
+                op!("|=")
+            }
+            BinaryOp::BitXor => {
+                op!("^=")
+            }
+            BinaryOp::BitAnd => {
+                op!("&=")
+            }
+            _ => return,
+        };
+
+        e.op = op;
+        e.right = left.take();
+    }
+}
