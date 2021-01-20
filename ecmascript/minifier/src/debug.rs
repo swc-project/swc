4e5d37e2facd2c1f35e04a6db0169507b2b3c5d0
강동윤 <kdy1997.dev@gmail.com>
2021-01-20 21:08:02 +0900

    debugger: Print hygiene

--- ecmascript/minifier/src/debug.rs
+++ ecmascript/minifier/src/debug.rs
@@ -1,5 +1,7 @@
 use swc_common::sync::Lrc;
 use swc_common::SourceMap;
+use swc_common::SyntaxContext;
+use swc_ecma_ast::Ident;
 use swc_ecma_ast::StrKind;
 use swc_ecma_codegen::text_writer::JsWriter;
 use swc_ecma_codegen::Emitter;
@@ -9,10 +11,16 @@ use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
 
-pub(crate) struct Normalizer;
+pub(crate) struct Debugger;
 
-impl VisitMut for Normalizer {
+impl VisitMut for Debugger {
     noop_visit_mut_type!();
+
+    fn visit_mut_ident(&mut self, n: &mut Ident) {
+        n.sym = format!("{}{:?}", n.sym, n.span.ctxt).into();
+        n.span.ctxt = SyntaxContext::empty();
+    }
+
     fn visit_mut_str_kind(&mut self, n: &mut StrKind) {
         *n = Default::default();
     }
@@ -20,10 +28,11 @@ impl VisitMut for Normalizer {
 
 pub(crate) fn dump<N>(node: &N) -> String
 where
-    N: swc_ecma_codegen::Node + Clone + VisitMutWith<DropSpan> + VisitMutWith<Normalizer>,
+    N: swc_ecma_codegen::Node + Clone + VisitMutWith<DropSpan> + VisitMutWith<Debugger>,
 {
-    let mut node = drop_span(node.clone());
-    node.visit_mut_with(&mut Normalizer);
+    let mut node = node.clone();
+    node.visit_mut_with(&mut Debugger);
+    node = drop_span(node);
     let mut buf = vec![];
     let cm = Lrc::new(SourceMap::default());
 
