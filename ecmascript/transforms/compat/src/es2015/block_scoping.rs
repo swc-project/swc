819042e3b7c92152d4a4adfdd6e10dfe716bf56b
강동윤 <kdy1997.dev@gmail.com>
2021-01-23 17:12:31 +0900

    Update test refs

--- ecmascript/transforms/compat/src/es2015/block_scoping.rs
+++ ecmascript/transforms/compat/src/es2015/block_scoping.rs
@@ -1556,29 +1556,23 @@ expect(foo()).toBe(false);
         "
         function combineOverlappingMatches(matches) {
             var hasOverlaps = false;
-          
-            var _loop = function _loop(i) {
-              var currentMatch = matches[i];
-              var overlap = matches.find(function (match) {
-                return match !== currentMatch && match.itemsType === currentMatch.itemsType;
-              });
-          
-              if (overlap) {
-                hasOverlaps = true;
-                matches.splice(i, 1);
-              }
-            };
-          
-            for (var i = matches.length - 1; i >= 0; i--) {
-              _loop(i);
+            for(var i = matches.length - 1; i >= 0; i--){
+                var currentMatch = matches[i];
+                var overlap = matches.find((match)=>{
+                    return match !== currentMatch && match.itemsType === currentMatch.itemsType;
+                });
+                if (overlap) {
+                    hasOverlaps = true;
+                    matches.splice(i, 1);
+                }
             }
-          
             if (hasOverlaps) {
-              combineOverlappingMatches(matches);
+                combineOverlappingMatches(matches);
             }
-          }
-          
-          combineOverlappingMatches([1]);
+        }
+        combineOverlappingMatches([
+            1
+        ]);
         "
     );
 }
