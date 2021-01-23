17274400aad577e68dfcdb21cde3039e2a63693b
강동윤 <kdy1997.dev@gmail.com>
2021-01-23 17:01:19 +0900

    Add a test for #1231

--- ecmascript/transforms/compat/src/es2015/block_scoping.rs
+++ ecmascript/transforms/compat/src/es2015/block_scoping.rs
@@ -1525,4 +1525,36 @@ expect(foo()).toBe(false);
         })
         "
     );
+
+    test!(
+        ::swc_ecma_parser::Syntax::default(),
+        |_| block_scoping(),
+        issue_1231_1,
+        "
+        function combineOverlappingMatches(matches) {
+            let hasOverlaps = false
+            
+            for (let i = matches.length - 1; i >= 0; i--) {
+                let currentMatch = matches[i]
+                let overlap = matches.find(match => {
+                    return match !== currentMatch && match.itemsType === currentMatch.itemsType
+                })
+                
+                if (overlap) {
+                    hasOverlaps = true
+                    matches.splice(i, 1)
+                }
+            }
+            
+            if (hasOverlaps) {
+                combineOverlappingMatches(matches)
+            }
+        }
+        
+        combineOverlappingMatches([1])
+        ",
+        "
+
+        "
+    );
 }
