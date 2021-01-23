bc7ccb8e93a5855cc908c24f5460fada8d7670b6
강동윤 <kdy1997.dev@gmail.com>
2021-01-23 17:07:18 +0900

    Update test refs

--- ecmascript/transforms/compat/src/es2015/block_scoping.rs
+++ ecmascript/transforms/compat/src/es2015/block_scoping.rs
@@ -1554,7 +1554,31 @@ expect(foo()).toBe(false);
         combineOverlappingMatches([1])
         ",
         "
-
+        function combineOverlappingMatches(matches) {
+            var hasOverlaps = false;
+          
+            var _loop = function _loop(i) {
+              var currentMatch = matches[i];
+              var overlap = matches.find(function (match) {
+                return match !== currentMatch && match.itemsType === currentMatch.itemsType;
+              });
+          
+              if (overlap) {
+                hasOverlaps = true;
+                matches.splice(i, 1);
+              }
+            };
+          
+            for (var i = matches.length - 1; i >= 0; i--) {
+              _loop(i);
+            }
+          
+            if (hasOverlaps) {
+              combineOverlappingMatches(matches);
+            }
+          }
+          
+          combineOverlappingMatches([1]);
         "
     );
 }
