08e719b022e3d0a1ebdd3ead5386e72dec1e7781
강동윤 <kdy1997.dev@gmail.com>
2021-01-25 14:42:35 +0900

    Update test refs

--- bundler/tests/fixture/issue-1150/output/entry.ts
+++ bundler/tests/fixture/issue-1150/output/entry.ts
@@ -1,3 +1,6 @@
+function a() {
+    console.log("a");
+}
 var O3;
 (function(O1) {
     O1[O1["A"] = 0] = "A";
@@ -5,12 +8,9 @@ var O3;
     O1[O1["C"] = 2] = "C";
 })(O3 || (O3 = {
 }));
-const O1 = O3;
-function a() {
-    console.log("a");
-}
 const a1 = a;
 const a2 = a1;
+const O1 = O3;
 const O2 = O1;
 export { O2 as O };
 const defaultA = a2;
