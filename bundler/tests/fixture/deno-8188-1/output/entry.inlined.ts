2e787328d0032d4bce26a9470bcfa672028e18e9
강동윤 <kdy1997.dev@gmail.com>
2021-01-25 14:08:08 +0900

    Update test refs

--- bundler/tests/fixture/deno-8188-1/output/entry.inlined.ts
+++ bundler/tests/fixture/deno-8188-1/output/entry.inlined.ts
@@ -1,3 +1,3 @@
-const b = 1 + 1;
 const c = 1 + 2;
+const b = 1 + 1;
 console.log(1, b, c);
--- bundler/tests/fixture/deno-8188-2/output/entry.inlined.ts
+++ bundler/tests/fixture/deno-8188-2/output/entry.inlined.ts
@@ -1,8 +1,8 @@
-const b = 1 + 1;
-const c = 1 + 2;
-console.log(1, b, c);
 const user1 = 1 + 1;
 console.log('user 1', user1);
+const b = 1 + 1;
 const user2 = user1 + 1 + b;
 console.log('user 2', user2);
+const c = 1 + 2;
+console.log(1, b, c);
 console.log(user1, user2);
