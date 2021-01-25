4c8c107d246872f6aba6d10cd204e00c488eda8f
강동윤 <kdy1997.dev@gmail.com>
2021-01-25 14:08:47 +0900

    Use prepend_all for circular deps.

--- bundler/src/bundler/chunk/circular.rs
+++ bundler/src/bundler/chunk/circular.rs
@@ -149,7 +149,7 @@ where
 
                 // print_hygiene("[circular] dep:init 2", &self.cm, &dep);
 
-                entry.push_all(dep);
+                entry.prepend_all(dep);
             }
 
             // print_hygiene("before circular sort", &self.cm, &entry.clone().into());
