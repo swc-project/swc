e36ac71142967123f1dc7f117f6b7d63a10dfa85
강동윤 <kdy1997.dev@gmail.com>
2021-01-26 21:28:53 +0900

    Fix ts_resolver

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -1996,6 +1996,29 @@ to_ts!(
     }
     ",
     "
-    
+    function foo<T__2, U__2 extends T__2>(x__2: T__2, y__2: U__2): T__2 {
+        x__2 = y__2;
+        return y__2;
+    }
+    function foo2<U__3 extends T__3, T__3>(x__3: T__3, y__3: U__3): T__3 {
+        x__3 = y__3;
+        return y__3;
+    }
+    var f = function<T__4, U__4 extends T__4>(x__4: T__4, y__4: U__4): T__4 {
+        x__4 = y__4;
+        return y__4;
+    };
+    var f2 = function<U__5 extends T__5, T__5>(x__5: T__5, y__5: U__5): T__5 {
+        x__5 = y__5;
+        return y__5;
+    };
+    var f3 = <T__6, U__6 extends T__6>(x__6: T__6, y__6: U__6)=>{
+        x__6 = y__6;
+        return y__6;
+    };
+    var f4 = <U__7 extends T__7, T__7>(x__7: T__7, y__7: U__7)=>{
+        x__7 = y__7;
+        return y__7;
+    };    
     "
 );
