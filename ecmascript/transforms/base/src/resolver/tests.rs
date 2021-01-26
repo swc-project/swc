f933abaee748cae3030455c35251c80a2b4940ed
강동윤 <kdy1997.dev@gmail.com>
2021-01-26 21:47:08 +0900

    Add one more test

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2022,3 +2022,51 @@ to_ts!(
     };    
     "
 );
+
+
+to_ts!(
+    generic_call_type_argument_inference,
+    "
+    class C<T, U> {
+        constructor(public t: T, public u: U) {
+        }
+    
+        foo(t: T, u: U) {
+            return t;
+        }
+    
+        foo2(t: T, u: U) {
+            return u;
+        }
+    
+        foo3<T>(t: T, u: U) {
+            return t;
+        }
+    
+        foo4<U>(t: T, u: U) {
+            return t;
+        }
+    
+        foo5<T,U>(t: T, u: U) {
+            return t;
+        }
+    
+        foo6<T, U>() {
+            var x: T;
+            return x;
+        }
+    
+        foo7<T, U>(u: U) {
+            var x: T;
+            return x;
+        }
+    
+        foo8<T, U>() {
+            var x: T;
+            return x;
+        }
+    }    
+    ",
+    "
+    "
+);
