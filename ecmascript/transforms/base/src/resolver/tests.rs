abc4935ba289a696d1ecebf80072e0747bed9a4e
강동윤 <kdy1997.dev@gmail.com>
2021-01-26 21:54:48 +0900

    Update test refs

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2076,24 +2076,24 @@ to_ts!(
         foo2(t__4: T, u__4: U) {
             return u__4;
         }
-        foo3<<T__5>>(t__5: T__5, u__5: U) {
+        foo3<T__5>(t__5: T__5, u__5: U) {
             return t__5;
         }
-        foo4<<U__6>>(t__6: T, u__6: U__6) {
+        foo4<U__6>(t__6: T, u__6: U__6) {
             return t__6;
         }
-        foo5<<T__7, U__7>>(t__7: T__7, u__7: U__7) {
+        foo5<T__7, U__7>(t__7: T__7, u__7: U__7) {
             return t__7;
         }
-        foo6<<T__8, U__8>>() {
+        foo6<T__8, U__8>() {
             var x__8: T__8;
             return x__8;
         }
-        foo7<<T__9, U__9>>(u__9: U__9) {
+        foo7<T__9, U__9>(u__9: U__9) {
             var x__9: T__9;
             return x__9;
         }
-        foo8<<T__10, U__10>>() {
+        foo8<T__10, U__10>() {
             var x__10: T__10;
             return x__10;
         }
