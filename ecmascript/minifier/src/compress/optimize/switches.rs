9baf442e725df90a9555b1c71eec1232a4f76752
강동윤 <kdy1997.dev@gmail.com>
2021-01-28 16:12:00 +0900

    Add conditions for aborting

--- ecmascript/minifier/src/compress/optimize/switches.rs
+++ ecmascript/minifier/src/compress/optimize/switches.rs
@@ -1,6 +1,21 @@
 use super::Optimizer;
 use swc_ecma_ast::*;
+use swc_ecma_utils::ExprExt;
 
+/// Methods related to option `switches`.
 impl Optimizer {
-    pub(super) fn optimize_const_switches(&mut self, s: &mut Stmt) {}
+    pub(super) fn optimize_const_switches(&mut self, s: &mut Stmt) {
+        if !self.options.switches {
+            return;
+        }
+
+        let stmt = match s {
+            Stmt::Switch(s) => s,
+            _ => return,
+        };
+
+        if stmt.discriminant.may_have_side_effects() {
+            return;
+        }
+    }
 }
