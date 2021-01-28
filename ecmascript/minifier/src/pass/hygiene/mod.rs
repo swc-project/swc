09e7158b392577d383bc424b249dd3b624d140f3
강동윤 <kdy1997.dev@gmail.com>
2021-01-28 19:46:12 +0900

    WIP

--- ecmascript/minifier/src/pass/hygiene/mod.rs
+++ ecmascript/minifier/src/pass/hygiene/mod.rs
@@ -1,6 +1,8 @@
 use self::scope::Scope;
+use swc_ecma_ast::*;
 use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
+use swc_ecma_visit::VisitMutWith;
 
 mod scope;
 
@@ -17,6 +19,18 @@ struct Optimizer<'a> {
     scope: Scope<'a>,
 }
 
+impl Optimizer<'_> {
+    fn handle_binding_ident(&mut self, i: &mut Ident) {}
+}
+
 impl VisitMut for Optimizer<'_> {
     noop_visit_mut_type!();
+
+    fn visit_mut_function(&mut self, n: &mut Function) {
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_fn_decl(&mut self, n: &mut FnDecl) {
+        self.handle_binding_ident(&mut n.ident);
+    }
 }
