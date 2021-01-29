48ab64b1dbf0506f38e5d32569121aa0843f3142
강동윤 <kdy1997.dev@gmail.com>
2021-01-29 19:39:23 +0900

    evaluate: `undefined` => `void 0`

--- ecmascript/minifier/src/compress/optimize/evaluate.rs
+++ ecmascript/minifier/src/compress/optimize/evaluate.rs
@@ -1,10 +1,27 @@
 use super::Optimizer;
+use swc_atoms::js_word;
 use swc_ecma_ast::*;
+use swc_ecma_utils::undefined;
 
 /// Methods related to the option `evaludate`.
 impl Optimizer {
     /// Evaludate expression if possible.
     ///
     /// This method call apppropriate methods for each ast types.
-    pub(super) fn evaluate(&mut self, e: &mut Expr) {}
+    pub(super) fn evaluate(&mut self, e: &mut Expr) {
+        match e {
+            Expr::Ident(Ident {
+                span,
+                sym: js_word!("undefined"),
+                ..
+            }) => {
+                log::trace!("evaluate: `undefined` -> `void 0`");
+                self.changed = true;
+                *e = *undefined(*span);
+                return;
+            }
+
+            _ => {}
+        }
+    }
 }
