ec4ba98a73fbb06e9b71397c831401327fcaafef
강동윤 <kdy1997.dev@gmail.com>
2021-01-29 19:45:55 +0900

    WIP: eval_numbers

--- ecmascript/minifier/src/compress/optimize/evaluate.rs
+++ ecmascript/minifier/src/compress/optimize/evaluate.rs
@@ -2,6 +2,8 @@ use super::Optimizer;
 use swc_atoms::js_word;
 use swc_ecma_ast::*;
 use swc_ecma_utils::undefined;
+use swc_ecma_utils::ExprExt;
+use swc_ecma_utils::Value::Known;
 
 /// Methods related to the option `evaludate`.
 impl Optimizer {
@@ -23,6 +25,30 @@ impl Optimizer {
                 self.changed = true;
                 *e = *undefined(*span);
                 return;
+            }
+
+            _ => {}
+        }
+
+        self.eval_numbers(e);
+    }
+
+    fn eval_numbers(&mut self, e: &mut Expr) {
+        match e {
+            Expr::Bin(bin @ BinExpr { op: op!("/"), .. }) => {
+                //
+                if bin.left.may_have_side_effects() {
+                    return;
+                }
+
+                let rn = bin.right.as_number();
+                if Known(0.0) == rn {
+                    // It's NaN
+                    self.changed = true;
+                    log::trace!("evaluate: `foo / 0` => `NaN`");
+                    *e = Expr::Ident(Ident::new(js_word!("NaN"), bin.span));
+                    return;
+                }
             }
 
             _ => {}
