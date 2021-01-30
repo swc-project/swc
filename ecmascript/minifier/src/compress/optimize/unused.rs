3a8d18676d7c53c7a7a3ead14d395153bb084a50
강동윤 <kdy1997.dev@gmail.com>
2021-01-30 15:22:00 +0900

    Drop assignments from child scope, too

--- ecmascript/minifier/src/compress/optimize/unused.rs
+++ ecmascript/minifier/src/compress/optimize/unused.rs
@@ -112,7 +112,7 @@ impl Optimizer {
                         .as_ref()
                         .and_then(|data| data.vars.get(&i.to_id()))
                     {
-                        if var.usage_count == 0 && var.is_fn_local {
+                        if var.usage_count == 0 {
                             log::trace!(
                                 "unused: Dropping assignment to var '{}{:?}', which is never used",
                                 i.sym,
