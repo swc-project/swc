afa2353e5e116b72be23871429895eedae2694a6
강동윤 <kdy1997.dev@gmail.com>
2021-02-01 16:07:18 +0900

    mangler: Preserve variable if initializer is function.

--- ecmascript/minifier/src/pass/mangle_names/preserver.rs
+++ ecmascript/minifier/src/pass/mangle_names/preserver.rs
@@ -16,6 +16,7 @@ where
     let mut v = Preserver {
         options,
         preserved: Default::default(),
+        should_preserve: false,
     };
     n.visit_with(&Invalid { span: DUMMY_SP }, &mut v);
     v.preserved
@@ -23,6 +24,7 @@ where
 pub(super) struct Preserver {
     options: MangleOptions,
     preserved: FxHashSet<Id>,
+    should_preserve: bool,
 }
 
 impl Visit for Preserver {
@@ -33,6 +35,55 @@ impl Visit for Preserver {
 
         if self.options.keep_fn_names {
             self.preserved.insert(n.ident.to_id());
+        }
+    }
+
+    fn visit_pat(&mut self, n: &Pat, _: &dyn Node) {
+        n.visit_children_with(self);
+
+        match n {
+            Pat::Ident(i) => {
+                if self.should_preserve {
+                    self.preserved.insert(i.to_id());
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_expr(&mut self, n: &Expr, _: &dyn Node) {
+        n.visit_children_with(self);
+
+        match n {
+            Expr::Ident(i) => {
+                if self.should_preserve {
+                    self.preserved.insert(i.to_id());
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_var_declarator(&mut self, n: &VarDeclarator, _: &dyn Node) {
+        n.visit_children_with(self);
+
+        if self.options.keep_fn_names {
+            match n.init.as_deref() {
+                Some(Expr::Fn(..)) | Some(Expr::Arrow(..)) => {
+                    let old = self.should_preserve;
+                    self.should_preserve = true;
+                    n.name.visit_with(n, self);
+                    self.should_preserve = old;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn visit_member_expr(&mut self, n: &MemberExpr, _: &dyn Node) {
+        n.obj.visit_with(n, self);
+        if n.computed {
+            n.prop.visit_with(n, self);
         }
     }
 }
