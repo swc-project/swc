e52aa383fe04de3aaaa7997f976edea2546c99c0
강동윤 <kdy1997.dev@gmail.com>
2021-02-01 17:47:49 +0900

    simplifier: Track modifications

--- ecmascript/transforms/optimization/src/simplify/expr/mod.rs
+++ ecmascript/transforms/optimization/src/simplify/expr/mod.rs
@@ -306,7 +306,7 @@ impl SimplifyExpr {
                     match idx {
                         Some(i) => {
                             let v = props.remove(i);
-
+                            self.changed = true;
                             preserve_effects(
                                 span,
                                 match v {
@@ -331,6 +331,14 @@ impl SimplifyExpr {
                             }))),
                             ..e
                         }),
+                        None => {
+                            self.changed = true;
+                            preserve_effects(
+                                span,
+                                *undefined(span),
+                                once(Box::new(Expr::Object(ObjectLit { props, span }))),
+                            )
+                        }
                     }
                 }
                 _ => Expr::Member(MemberExpr {
