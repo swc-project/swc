b50696328123d2b14b8f40b9b75cbb694cbea5f7
강동윤 <kdy1997.dev@gmail.com>
2021-02-01 15:51:46 +0900

    WIP: Name mangler

--- ecmascript/minifier/src/pass/mangle_names.rs
+++ ecmascript/minifier/src/pass/mangle_names.rs
@@ -1,11 +1,94 @@
 use super::compute_char_freq::CharFreqInfo;
+use crate::analyzer::analyze;
+use crate::analyzer::ScopeData;
+use crate::util::base54::base54;
+use fxhash::FxHashMap;
+use swc_atoms::JsWord;
+use swc_common::SyntaxContext;
+use swc_ecma_ast::*;
+use swc_ecma_utils::ident::IdentLike;
+use swc_ecma_utils::Id;
+use swc_ecma_visit::noop_visit_mut_type;
 use swc_ecma_visit::VisitMut;
+use swc_ecma_visit::VisitMutWith;
 
 pub fn name_mangler(_char_freq_info: CharFreqInfo) -> impl VisitMut {
-    Mangler {}
+    Mangler::default()
 }
 
-#[derive(Debug)]
-struct Mangler {}
+#[derive(Debug, Default)]
+struct Mangler {
+    n: usize,
+    renamed: FxHashMap<Id, JsWord>,
+    data: Option<ScopeData>,
+}
 
-impl VisitMut for Mangler {}
+impl Mangler {
+    fn rename(&mut self, i: &mut Ident) {
+        if let Some(var) = self.data.as_ref().unwrap().vars.get(&i.to_id()) {
+            if !var.declared {
+                return;
+            }
+        }
+
+        i.span.ctxt = SyntaxContext::empty();
+        if let Some(v) = self.renamed.get(&i.to_id()) {
+            i.sym = v.clone();
+            return;
+        }
+
+        let sym: JsWord = base54(self.n).into();
+        self.renamed.insert(i.to_id(), sym.clone());
+        i.sym = sym.clone();
+        self.n += 1;
+    }
+}
+
+impl VisitMut for Mangler {
+    noop_visit_mut_type!();
+
+    fn visit_mut_module(&mut self, n: &mut Module) {
+        let data = analyze(&*n);
+        self.data = Some(data);
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_script(&mut self, n: &mut Script) {
+        let data = analyze(&*n);
+        self.data = Some(data);
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_labeled_stmt(&mut self, n: &mut LabeledStmt) {
+        n.body.visit_mut_with(self);
+    }
+
+    fn visit_mut_expr(&mut self, e: &mut Expr) {
+        e.visit_mut_children_with(self);
+
+        match e {
+            Expr::Ident(i) => {
+                self.rename(i);
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_mut_pat(&mut self, n: &mut Pat) {
+        n.visit_mut_children_with(self);
+
+        match n {
+            Pat::Ident(i) => {
+                self.rename(i);
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_mut_member_expr(&mut self, n: &mut MemberExpr) {
+        n.obj.visit_mut_with(self);
+        if n.computed {
+            n.prop.visit_mut_with(self);
+        }
+    }
+}
--- ecmascript/minifier/src/util/base54.rs
+++ ecmascript/minifier/src/util/base54.rs
@@ -1,1 +1,19 @@
+const CHARS: &[u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+/// Note: This returns `a` for 0.
+pub(crate) fn base54(mut n: usize) -> String {
+    let mut ret = String::new();
+    let mut base = 54;
+
+    n += 1;
+
+    while n > 0 {
+        n -= 1;
+
+        ret.push(CHARS[n % base] as char);
+        n = n / base;
+        base = 64;
+    }
+
+    ret
+}
