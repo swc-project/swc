27611fc5280a6050011bd5cc380f18887df7a483
강동윤 <kdy1997.dev@gmail.com>
2021-02-01 12:00:54 +0900

    Add a test for denoland/deno#9350

---
+++ bundler/tests/deno-exec/deno-9350/full/input/helpers/mod.ts
@@ -0,0 +1,5 @@
+export * from "./ask_trivia";
+export * from "./generate_source";
+export * from "./get_user_input";
+export * from "./logger";
+export * from "./pick_trivia";
---
+++ bundler/tests/deno-exec/deno-9350/full/input/mod.ts
@@ -0,0 +1,14 @@
+import { parse } from "https://deno.land/std@0.85.0/std/flags/mod.ts";
+
+import { getNumericInput, printQuestion } from "./helpers/mod";
+import printStats from "./print_stats.ts";
+
+const args = parse(Deno.args);
+
+if (args.stats) {
+  printStats();
+} else {
+  printQuestion("How many times have you played");
+
+  console.log(await getNumericInput());
+}
