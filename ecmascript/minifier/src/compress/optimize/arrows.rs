9e4635ffb49247bfabce75bb5248c6be2d7207f1
강동윤 <kdy1997.dev@gmail.com>
2021-02-04 15:23:33 +0900

    impl optimize_arrow_method_prop

--- ecmascript/minifier/src/compress/optimize/arrows.rs
+++ ecmascript/minifier/src/compress/optimize/arrows.rs
@@ -1,11 +1,54 @@
 use super::Optimizer;
+use swc_common::Spanned;
 use swc_ecma_ast::*;
+use swc_ecma_transforms_base::ext::MapWithMut;
 
 /// Methods related to the option `arrows`.
 impl Optimizer {
     pub(super) fn optimize_arrow_method_prop(&mut self, p: &mut Prop) {
         if !self.options.unsafe_methods || self.options.ecma < 2015 {
             return;
+        }
+
+        match p {
+            Prop::KeyValue(kv) => {
+                //
+                match &mut *kv.value {
+                    Expr::Arrow(
+                        m
+                        @
+                        ArrowExpr {
+                            body: BlockStmtOrExpr::BlockStmt(..),
+                            ..
+                        },
+                    ) => {
+                        *p = Prop::Method(MethodProp {
+                            key: kv.key.take(),
+                            function: Function {
+                                params: m
+                                    .params
+                                    .take()
+                                    .into_iter()
+                                    .map(|pat| Param {
+                                        span: pat.span(),
+                                        decorators: Default::default(),
+                                        pat,
+                                    })
+                                    .collect(),
+                                decorators: Default::default(),
+                                span: m.span,
+                                body: m.body.take().block_stmt(),
+                                is_generator: m.is_generator,
+                                is_async: m.is_async,
+                                type_params: Default::default(),
+                                return_type: Default::default(),
+                            },
+                        });
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
         }
     }
 }
