0d5239c81782a0daa8c70449d571d3dca95b19f2
강동윤 <kdy1997.dev@gmail.com>
2021-02-16 17:13:09 +0900

    Remove debug code for sorter

--- bundler/src/bundler/modules/sort/chunk.rs
+++ bundler/src/bundler/modules/sort/chunk.rs
@@ -99,15 +99,15 @@ fn toposort_real_modules<'a>(
 
         let stmts = sort_stmts(injected_ctxt, stmts);
 
-        print_hygiene(
-            "after sort",
-            cm,
-            &Module {
-                span: DUMMY_SP,
-                body: stmts.clone(),
-                shebang: None,
-            },
-        );
+        // print_hygiene(
+        //     "after sort",
+        //     cm,
+        //     &Module {
+        //         span: DUMMY_SP,
+        //         body: stmts.clone(),
+        //         shebang: None,
+        //     },
+        // );
 
         chunks.push(Chunk { stmts })
     }
