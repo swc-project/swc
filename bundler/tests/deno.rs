f08822f9329d867f578c839b339f6be81d310039
강동윤 <kdy1997.dev@gmail.com>
2021-02-16 13:29:22 +0900

    It's not too slow anymore

--- bundler/tests/deno.rs
+++ bundler/tests/deno.rs
@@ -408,7 +408,6 @@ fn deno_8573() {
 
 /// Timeout is long because tfjs is so large.
 #[test]
-#[ignore = "Too slow"]
 fn deno_8597() {
     run(
         "https://cdn.skypack.dev/@tensorflow/tfjs@2.6.0",
