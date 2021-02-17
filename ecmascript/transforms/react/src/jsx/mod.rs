10ddaa3f754a4908b173631859c8c248aa464b19
강동윤 <kdy1997.dev@gmail.com>
2021-02-17 15:08:15 +0900

    Moved all codes

--- ecmascript/transforms/react/src/jsx/mod.rs
+++ ecmascript/transforms/react/src/jsx/mod.rs
@@ -27,7 +27,6 @@ use swc_ecma_visit::Fold;
 use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
 
-mod jsx2;
 #[cfg(test)]
 mod tests;
 
@@ -898,4 +897,91 @@ fn jsx_text_to_str(t: JsWord) -> JsWord {
     }
 
     buf.into()
+}
+
+fn jsx_attr_value_to_expr(v: JSXAttrValue) -> Option<Box<Expr>> {
+    Some(match v {
+        JSXAttrValue::Lit(lit) => Box::new(lit.into()),
+        JSXAttrValue::JSXExprContainer(e) => match e.expr {
+            JSXExpr::JSXEmptyExpr(_) => None?,
+            JSXExpr::Expr(e) => e,
+        },
+        JSXAttrValue::JSXElement(e) => Box::new(Expr::JSXElement(e)),
+        JSXAttrValue::JSXFragment(f) => Box::new(Expr::JSXFragment(f)),
+    })
+}
+
+fn is_static<T>(node: &T) -> bool
+where
+    T: VisitWith<StaticVisitor>,
+{
+    let mut v = StaticVisitor::default();
+    node.visit_with(&Invalid { span: DUMMY_SP }, &mut v);
+    !v.dynamic
+}
+
+#[derive(Default)]
+struct StaticVisitor {
+    dynamic: bool,
+}
+
+impl Visit for StaticVisitor {
+    noop_visit_type!();
+
+    fn visit_member_expr(&mut self, e: &MemberExpr, _: &dyn Node) {
+        e.obj.visit_with(e, self);
+        if e.computed {
+            e.prop.visit_with(e, self);
+        }
+    }
+
+    fn visit_expr(&mut self, e: &Expr, _: &dyn Node) {
+        e.visit_children_with(self);
+
+        match e {
+            Expr::Lit(..) | Expr::JSXElement(..) | Expr::JSXFragment(..) | Expr::Array(..) => {
+                return
+            }
+            _ => {
+                self.dynamic = true;
+            }
+        }
+    }
+
+    fn visit_ident(&mut self, i: &Ident, _: &dyn Node) {
+        if i.sym == js_word!("this") {
+            self.dynamic = true;
+        }
+    }
+}
+
+/// We want to use React.createElement, even in the case of
+/// jsx, for <div {...props} key={key} /> to distinguish it
+/// from <div key={key} {...props} />. This is an intermediary
+/// step while we deprecate key spread from props. Afterwards,
+/// we will stop using createElement in the transform.
+fn should_use_create_element(attrs: &[JSXAttrOrSpread]) -> bool {
+    let mut seen_prop_spread = false;
+    for attr in attrs {
+        if seen_prop_spread
+            && match attr {
+                JSXAttrOrSpread::JSXAttr(attr) => match &attr.name {
+                    JSXAttrName::Ident(i) => i.sym == js_word!("key"),
+                    JSXAttrName::JSXNamespacedName(_) => false,
+                },
+                _ => false,
+            }
+        {
+            return true;
+        }
+
+        match attr {
+            JSXAttrOrSpread::SpreadElement(_) => {
+                seen_prop_spread = true;
+            }
+            _ => {}
+        }
+    }
+
+    false
 }
