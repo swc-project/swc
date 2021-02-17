1f6fb23215d823b4a2fb7a2074205af793ede7a0
강동윤 <kdy1997.dev@gmail.com>
2021-02-17 15:43:33 +0900

    WIP

--- ecmascript/transforms/react/scripts/patch-options.ts
+++ ecmascript/transforms/react/scripts/patch-options.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-async function* walk(dir: string) {
+async function* walk(dir: string): AsyncGenerator<string> {
     for await (const d of await fs.promises.opendir(dir)) {
         const entry = path.join(dir, d.name);
         if (d.isDirectory()) yield* walk(entry);
@@ -12,6 +12,12 @@ async function* walk(dir: string) {
 // Then, use it with a simple async for loop
 async function main() {
     // TODO: Generalize path
-    for await (const p of walk('src/jsx/fixture'))
+    for await (const p of walk('src/jsx/fixture')) {
+        if (!p.endsWith('.json')) {
+            continue
+        }
         console.log(p)
+    }
 }
+
+main()
