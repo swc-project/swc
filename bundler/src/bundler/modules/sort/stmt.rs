7356697a052314ac65d06956c6ebdf10bfc0eedc
강동윤 <kdy1997.dev@gmail.com>
2021-02-17 13:33:18 +0900

    [TRY] Prevent infinite loop

--- bundler/src/bundler/modules/sort/stmt.rs
+++ bundler/src/bundler/modules/sort/stmt.rs
@@ -228,6 +228,10 @@ fn iter<'a>(
                             }
                         }
 
+                        if !moves.insert((idx, dep)) {
+                            continue;
+                        }
+
                         deps_to_push.push(dep);
                     }
 
