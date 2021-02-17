f655bccfa855fb0105c515759fffe455fb7d8bd1
강동윤 <kdy1997.dev@gmail.com>
2021-02-17 15:41:30 +0900

    WIP: Script to patch options.json

---
+++ ecmascript/transforms/react/scripts/patch-options.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npx tsc script/patch-options.ts
+node scripts/patch-options.js
---
+++ ecmascript/transforms/react/scripts/patch-options.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+async function* walk(dir: string) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
+
+// Then, use it with a simple async for loop
+async function main() {
+    // TODO: Generalize path
+    for await (const p of walk('src/jsx/fixture'))
+        console.log(p)
+}
