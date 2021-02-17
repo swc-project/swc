1a47c9fa5e7102dc8ea49007e9786d13379d91b9
강동윤 <kdy1997.dev@gmail.com>
2021-02-17 17:58:48 +0900

    Update test refs: Merge imports

--- ecmascript/transforms/react/src/jsx/fixture/react-automatic/should-have-correct-comma-in-nested-children/output.mjs
+++ ecmascript/transforms/react/src/jsx/fixture/react-automatic/should-have-correct-comma-in-nested-children/output.mjs
@@ -1,10 +1,9 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 
 var x = /*#__PURE__*/_jsxs("div", {
-  children: [/*#__PURE__*/_jsx("div", {
-    children: /*#__PURE__*/_jsx("br", {})
-  }), /*#__PURE__*/_jsxs(Component, {
-    children: [foo, /*#__PURE__*/_jsx("br", {}), bar]
-  }), /*#__PURE__*/_jsx("br", {})]
+    children: [/*#__PURE__*/_jsx("div", {
+        children: /*#__PURE__*/_jsx("br", {})
+    }), /*#__PURE__*/_jsxs(Component, {
+        children: [foo, /*#__PURE__*/_jsx("br", {}), bar]
+    }), /*#__PURE__*/_jsx("br", {})]
 });
