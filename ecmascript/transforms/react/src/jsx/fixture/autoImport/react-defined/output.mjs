53de7a2e4f03182e6bab3e0da4758b0042c490d0
강동윤 <kdy1997.dev@gmail.com>
2021-02-18 13:01:08 +0900

    Update test refs

--- ecmascript/transforms/react/src/jsx/fixture/autoImport/react-defined/output.mjs
+++ ecmascript/transforms/react/src/jsx/fixture/autoImport/react-defined/output.mjs
@@ -1,15 +1,21 @@
-import * as react from "react";
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { createElement as _createElement } from "react";
+import * as react from "react";
 var y = react.createElement("div", {
     foo: 1
 });
-
-var x = /*#__PURE__*/_jsxs("div", {
-    children: [/*#__PURE__*/_jsx("div", {}, "1"), /*#__PURE__*/_jsx("div", {
-        meow: "wolf"
-    }, "2"), /*#__PURE__*/_jsx("div", {}, "3"), /*#__PURE__*/_createElement("div", {
-        ...props,
-        key: "4"
-    })]
+var x = _jsxs("div", {
+    children: [
+        _jsx("div", {
+        }, "1"),
+        _jsx("div", {
+            meow: "wolf"
+        }, "2"),
+        _jsx("div", {
+        }, "3"),
+        _createElement("div", {
+            ...props,
+            key: "4"
+        })
+    ]
 });
