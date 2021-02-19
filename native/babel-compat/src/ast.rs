bd111615677848e374e7e247a2a984f671771658
강동윤 <kdy1997.dev@gmail.com>
2021-02-19 21:55:43 +0900

    Some work

--- native/babel-compat/src/ast.rs
+++ native/babel-compat/src/ast.rs
@@ -1,1 +1,66 @@
+use std::collections::HashMap;
 
+use ahash::RandomState;
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::Value;
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LineCol {
+    pub line: usize,
+    pub column: usize,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Loc {
+    pub start: LineCol,
+    pub end: LineCol,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BaseComment {
+    pub value: String,
+    pub start: usize,
+    pub end: usize,
+    pub loc: Loc,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub enum Comment {
+    #[serde(rename = "CommentBlock")]
+    Block(BaseComment),
+    #[serde(rename = "CommentLine")]
+    Line(BaseComment),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BaseNode {
+    #[serde(default)]
+    pub leading_comments: Vec<Comment>,
+    #[serde(default)]
+    pub inner_comments: Vec<Comment>,
+    #[serde(default)]
+    pub trailing_comments: Vec<Comment>,
+
+    #[serde(default)]
+    pub start: Option<usize>,
+    #[serde(default)]
+    pub end: Option<usize>,
+    #[serde(default)]
+    pub loc: Option<Loc>,
+
+    #[serde(default)]
+    pub extra: Option<HashMap<String, Value, RandomState>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ArrayExpression {
+    #[serde(flatten)]
+    pub base: BaseNode,
+}
