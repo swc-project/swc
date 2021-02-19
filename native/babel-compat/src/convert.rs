3c63d31ab0ec622f2f26949b5acc9bcbf6a86930
강동윤 <kdy1997.dev@gmail.com>
2021-02-19 21:55:43 +0900

    Some work

--- native/babel-compat/src/convert.rs
+++ native/babel-compat/src/convert.rs
@@ -1,10 +1,15 @@
-#[derive(Debug, Clone)]
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::sync::Arc;
+use swc_common::SourceMap;
+
+#[derive(Clone)]
 pub struct Context {
     pub cm: Arc<SourceMap>,
 }
 
 pub trait Babelify {
-    type Output;
+    type Output: Serialize + DeserializeOwned;
 
     fn babelify(self, ctx: &mut Context) -> Self::Output;
 }
