515f1a17150ebc405b445fadb5c4d980ef3e16e6
강동윤 <kdy1997.dev@gmail.com>
2021-02-19 14:07:02 +0900

    Update test refs

--- ecmascript/transforms/react/src/jsx/fixture/react-automatic/should-handle-attributed-elements/output.mjs
+++ ecmascript/transforms/react/src/jsx/fixture/react-automatic/should-handle-attributed-elements/output.mjs
@@ -1,11 +1,11 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 var HelloMessage = React.createClass({
-    displayName: "HelloMessage",
     render: function () {
         return /*#__PURE__*/_jsxs("div", {
             children: ["Hello ", this.props.name]
         });
-    }
+    },
+    displayName: "HelloMessage",
 });
 React.render( /*#__PURE__*/_jsx(HelloMessage, {
     name: /*#__PURE__*/_jsx("span", {
