283cf21bd3afbe594cc0c425894d048474e9566e
강동윤 <kdy1997.dev@gmail.com>
2021-02-20 18:16:48 +0900

    Update test refs

--- spack/tests/pass/export/all-2/output/entry.js
+++ spack/tests/pass/export/all-2/output/entry.js
@@ -1,11 +1,8 @@
 class Root {
 }
-const Root1 = Root;
-const Root2 = Root1;
-const Root3 = Root1;
-class A1 extends Root3 {
+class A1 extends Root {
 }
 export { A1 as A };
-class B1 extends Root2 {
+class B1 extends Root {
 }
 export { B1 as B };
--- spack/tests/pass/export/all-nested-1/output/entry.js
+++ spack/tests/pass/export/all-nested-1/output/entry.js
@@ -1,6 +1,6 @@
 const a1 = 1;
-export { a1 as a };
 const b1 = 2;
-export { b1 as b };
 const c1 = 3;
+export { a1 as a };
+export { b1 as b };
 export { c1 as c };
