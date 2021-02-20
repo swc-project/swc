5fb4528aada67c195bc4b612ef00febfa8d92b7b
강동윤 <kdy1997.dev@gmail.com>
2021-02-20 17:44:46 +0900

    Update test refs

--- bundler/tests/fixture/deno-9346/output/entry.ts
+++ bundler/tests/fixture/deno-9346/output/entry.ts
@@ -1,12 +1,8 @@
 function __default(message) {
     console.log(message);
 }
-const __default1 = __default;
-const __default2 = __default1;
-const log = __default2;
 cli();
 async function cli() {
-    log("It works!");
+    __default("It works!");
 }
-const __default3 = cli;
-export { __default3 as default };
+export { cli as default };
