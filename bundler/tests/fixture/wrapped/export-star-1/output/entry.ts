28370f10e64db42c2ba23be1559f9fbc826e30b3
강동윤 <kdy1997.dev@gmail.com>
2021-02-22 14:12:36 +0900

    Update test refs

--- bundler/tests/fixture/wrapped/export-star-1/output/entry.ts
+++ bundler/tests/fixture/wrapped/export-star-1/output/entry.ts
@@ -3,15 +3,11 @@ const [a, b, c] = [
     2,
     3
 ];
-const c1 = c;
-const a1 = a;
-const b1 = b;
 const mod = function() {
     return {
-        a: a1,
-        b: b1,
-        c: c1
+        a: a,
+        b: b,
+        c: c
     };
 }();
-const foo = mod;
-console.log(foo);
+console.log(mod);
