252382314092d078a88ed37aa543829198aa213f
강동윤 <kdy1997.dev@gmail.com>
2021-02-25 14:09:32 +0900

    Remove outdated codes

--- bundler/src/bundler/chunk/export.rs
+++ bundler/src/bundler/chunk/export.rs
@@ -238,67 +238,21 @@ impl Fold for DepUnexporter<'_> {
 
     fn fold_module_item(&mut self, item: ModuleItem) -> ModuleItem {
         match item {
-            ModuleItem::ModuleDecl(decl) => match decl {
-                ModuleDecl::ExportDecl(mut export) => {
-                    match &mut export.decl {
-                        Decl::Class(c) => {
-                            if self.is_exported(&c.ident.to_id()) {
-                                return ModuleItem::Stmt(Stmt::Decl(export.decl));
-                            }
-                        }
-                        Decl::Fn(f) => {
-                            if self.is_exported(&f.ident.to_id()) {
-                                return ModuleItem::Stmt(Stmt::Decl(export.decl));
-                            }
-                        }
-                        Decl::Var(..) => {
-                            if self.exports.is_empty() {
-                                return ModuleItem::Stmt(Stmt::Decl(export.decl));
-                            }
-                        }
-                        _ => {}
-                    }
-                    ModuleItem::Stmt(Stmt::Decl(export.decl))
-                }
-
-                ModuleDecl::ExportDefaultDecl(export) => match export.decl {
-                    DefaultDecl::Class(ClassExpr { ident: None, .. })
-                    | DefaultDecl::Fn(FnExpr { ident: None, .. }) => {
+            ModuleItem::ModuleDecl(decl) => {
+                match decl {
+                    // Empty statement
+                    ModuleDecl::ExportDecl(..)
+                    | ModuleDecl::ExportDefaultDecl(..)
+                    | ModuleDecl::ExportAll(..)
+                    | ModuleDecl::ExportDefaultExpr(..)
+                    | ModuleDecl::ExportNamed(..) => {
                         ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }))
                     }
-                    DefaultDecl::TsInterfaceDecl(decl) => {
-                        ModuleItem::Stmt(Stmt::Decl(Decl::TsInterface(decl)))
-                    }
+                    ModuleDecl::Import(..) => ModuleItem::ModuleDecl(decl),
 
-                    DefaultDecl::Class(ClassExpr {
-                        ident: Some(ident),
-                        class,
-                    }) => ModuleItem::Stmt(Stmt::Decl(Decl::Class(ClassDecl {
-                        declare: false,
-                        ident,
-                        class,
-                    }))),
-
-                    DefaultDecl::Fn(FnExpr {
-                        ident: Some(ident),
-                        function,
-                    }) => ModuleItem::Stmt(Stmt::Decl(Decl::Fn(FnDecl {
-                        declare: false,
-                        function,
-                        ident,
-                    }))),
-                },
-
-                // Empty statement
-                ModuleDecl::ExportAll(..)
-                | ModuleDecl::ExportDefaultExpr(..)
-                | ModuleDecl::ExportNamed(..) => {
-                    ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }))
+                    _ => unimplemented!("Unexported: {:?}", decl),
                 }
-                ModuleDecl::Import(..) => ModuleItem::ModuleDecl(decl),
-
-                _ => unimplemented!("Unexported: {:?}", decl),
-            },
+            }
 
             _ => item,
         }
