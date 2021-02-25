30ced34df74091f7dca9e057241c2c06d365bffc
강동윤 <kdy1997.dev@gmail.com>
2021-02-25 16:27:09 +0900

    ignored.txt

--- ecmascript/minifier/scripts/sort.sh
+++ ecmascript/minifier/scripts/sort.sh
@@ -2,3 +2,6 @@
 set -eu
 cat tests/golden.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/golden_sorted.txt
 mv tests/golden_sorted.txt tests/golden.txt
+
+cat tests/ignored.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/ignored_sorted.txt
+mv tests/ignored_sorted.txt tests/ignored.txt
