81bfde7f5c81a0485e1a62ecbe08b9d8d8c0b6a0
강동윤 <kdy1997.dev@gmail.com>
2021-02-26 17:16:31 +0900

    Update test refs

--- spack/tests/pass/deno-001/full/output/entry.js
+++ spack/tests/pass/deno-001/full/output/entry.js
@@ -476,63 +476,6 @@ class BufWriterSync extends AbstractBufBase {
         this.buf = new Uint8Array(size3);
     }
 }
-/** Generate longest proper prefix which is also suffix array. */ function createLPS(pat) {
-    const lps = new Uint8Array(pat.length);
-    lps[0] = 0;
-    let prefixEnd = 0;
-    let i = 1;
-    while(i < lps.length)if (pat[i] == pat[prefixEnd]) {
-        prefixEnd++;
-        lps[i] = prefixEnd;
-        i++;
-    } else if (prefixEnd === 0) {
-        lps[i] = 0;
-        i++;
-    } else prefixEnd = pat[prefixEnd - 1];
-    return lps;
-}
-async function* readDelim(reader, delim) {
-    // Avoid unicode problems
-    const delimLen = delim.length;
-    const delimLPS = createLPS(delim);
-    let inputBuffer = new Deno.Buffer();
-    const inspectArr = new Uint8Array(Math.max(1024, delimLen + 1));
-    // Modified KMP
-    let inspectIndex = 0;
-    let matchIndex = 0;
-    while(true){
-        const result = await reader.read(inspectArr);
-        if (result === null) {
-            // Yield last chunk.
-            yield inputBuffer.bytes();
-            return;
-        }
-        if (result < 0) // Discard all remaining and silently fail.
-        return;
-        const sliceRead = inspectArr.subarray(0, result);
-        await Deno.writeAll(inputBuffer, sliceRead);
-        let sliceToProcess = inputBuffer.bytes();
-        while(inspectIndex < sliceToProcess.length)if (sliceToProcess[inspectIndex] === delim[matchIndex]) {
-            inspectIndex++;
-            matchIndex++;
-            if (matchIndex === delimLen) {
-                // Full match
-                const matchEnd = inspectIndex - delimLen;
-                const readyBytes = sliceToProcess.subarray(0, matchEnd);
-                // Copy
-                const pendingBytes = sliceToProcess.slice(inspectIndex);
-                yield readyBytes;
-                // Reset match, different from KMP.
-                sliceToProcess = pendingBytes;
-                inspectIndex = 0;
-                matchIndex = 0;
-            }
-        } else if (matchIndex === 0) inspectIndex++;
-        else matchIndex = delimLPS[matchIndex - 1];
-        // Keep inspectIndex and matchIndex.
-        inputBuffer = new Deno.Buffer(sliceToProcess);
-    }
-}
 function deferred() {
     let methods;
     const promise = new Promise((resolve, reject)=>{
