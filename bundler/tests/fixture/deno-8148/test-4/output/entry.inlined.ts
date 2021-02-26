b658563aa8aaad185e3cd79ee466a838e053f23c
강동윤 <kdy1997.dev@gmail.com>
2021-02-26 15:46:43 +0900

    Update test refs

--- bundler/tests/fixture/deno-8148/test-4/output/entry.inlined.ts
+++ bundler/tests/fixture/deno-8148/test-4/output/entry.inlined.ts
@@ -1,15 +1,12 @@
 const __default = {
-    "hello": "world"
-};
-const __default1 = {
     "a": "a",
     "b": "b",
     "c": "c"
 };
-const __default2 = {
+const __default1 = {
     "a": "a1",
     "b": "b1",
     "c": "c1"
 };
+console.log(__default);
 console.log(__default1);
-console.log(__default2);
