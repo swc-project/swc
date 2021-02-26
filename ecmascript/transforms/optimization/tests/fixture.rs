77d89cee8eddd7b76ca01df1fa0d72c507dc7585
강동윤 <kdy1997.dev@gmail.com>
2021-02-26 17:03:12 +0900

    Test `changed` of dce

--- ecmascript/transforms/optimization/tests/fixture.rs
+++ ecmascript/transforms/optimization/tests/fixture.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use swc_common::pass::Repeat;
 use swc_ecma_parser::EsConfig;
 use swc_ecma_parser::Syntax;
 use swc_ecma_transforms_optimization::simplify::dce::dce;
@@ -14,6 +15,21 @@ fn dce_single_pass(input: PathBuf) {
             ..Default::default()
         }),
         &|_| dce(Default::default()),
+        &input,
+        &output,
+    );
+}
+
+#[testing::fixture("dce/**/input.js")]
+fn dce_repeated(input: PathBuf) {
+    let output = input.with_file_name("output.full.js");
+
+    test_fixture(
+        Syntax::Es(EsConfig {
+            dynamic_import: true,
+            ..Default::default()
+        }),
+        &|_| Repeat::new(dce(Default::default())),
         &input,
         &output,
     );
