6e046645588346b178ad7e374ce922bcbb4a3373
Devon Govett <devongovett@gmail.com>
2021-02-28 09:51:36 -0500

    Add async arrows in class bugfix

--- ecmascript/transforms/compat/src/bugfixes.rs
+++ ecmascript/transforms/compat/src/bugfixes.rs
@@ -1,9 +1,11 @@
+pub use self::async_arrows_in_class::async_arrows_in_class;
 pub use self::edge_default_param::edge_default_param;
 use swc_common::chain;
 use swc_ecma_visit::Fold;
 
+mod async_arrows_in_class;
 mod edge_default_param;
 
 pub fn bugfixes() -> impl Fold {
-  edge_default_param()
+  chain!(async_arrows_in_class(), edge_default_param())
 }
