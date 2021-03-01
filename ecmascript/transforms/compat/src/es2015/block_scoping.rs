76211498531daaadd1b2383a1d8e5997bd0792a2
강동윤 <kdy1997.dev@gmail.com>
2021-03-01 16:29:06 +0900

    Update test refs

--- ecmascript/transforms/compat/src/es2015/block_scoping.rs
+++ ecmascript/transforms/compat/src/es2015/block_scoping.rs
@@ -1639,7 +1639,37 @@ expect(foo()).toBe(false);
           }
         ",
         "
-        
+        export function test(items) {
+            var infoMap = new WeakMap();
+            for(var i = 0; i < items.length; i += 1){
+                var item = items[i];
+                var info = void 0;
+                switch(item.type){
+                    case 'branch1':
+                        info = item.method1();
+                        break;
+                    case 'branch2':
+                        info = item.method2();
+                        break;
+                    case 'branch3':
+                        info = item.method3(Object.fromEntries(item.subItems.map((t)=>[
+                                item.alias ?? t.name,
+                                getInfo(t)
+                            ]
+                        )));
+                        break;
+                    default:
+                        throw new Error('boom');
+                }
+                infoMap.set(item, info);
+            }
+            function getInfo(item) {
+                if (!infoMap.has(item)) {
+                    throw new Error('no info yet');
+                }
+                return infoMap.get(item);
+            }
+        }
         "
     );
 }
