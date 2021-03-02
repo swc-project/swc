e4bb1bb0007a825a4fae6d8c94d059b2498b01e5
강동윤 <kdy1997.dev@gmail.com>
2021-03-02 17:17:05 +0900

    Remove wrong test

--- bundler/tests/fixture/deno-9307/case1/gql-refinery/input/src/utils/upperSnakeCase.ts
+++
@@ -1,8 +0,0 @@
-import { R } from "../../deps.ts";
-import snakeCase from "https://raw.githubusercontent.com/lodash/lodash/master/snakeCase.js";
-
-// deno-lint-ignore no-explicit-any
-export const upperSnakeCase = (R as any).compose(
-  R.toUpper,
-  snakeCase,
-);
