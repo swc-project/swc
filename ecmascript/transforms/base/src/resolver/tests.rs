51f3fab28fd1aa88f59da9db0377f129cd142878
강동윤 <kdy1997.dev@gmail.com>
2021-03-03 14:04:26 +0900

    Update test refs

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2332,6 +2332,16 @@ to_ts!(
     }
     ",
     "
-    
+    export class X {
+        constructor(){
+        }
+        bad(target__2: number) {
+            const d__2 = 1;
+            const min__2 = 0;
+            const max__2 = 100;
+            console.log('x', `duration ${d__2} not in range - ${min__2} ≥ ${d__2} && ${max__2} ≥ \
+     ${d__2}`);
+        }
+    }
     "
 );
