699a968047b5231ce4895d37e7d7ca83fc02ec13
강동윤 <kdy1997.dev@gmail.com>
2021-03-03 16:39:26 +0900

    Update test refs

--- ecmascript/codegen/src/tests.rs
+++ ecmascript/codegen/src/tests.rs
@@ -516,7 +516,7 @@ Content-Disposition: form-data; name=\"payload\"\r
 Content-Type: text/plain\r
 \r
 CONTENT\r
---------------------------366796e1c748a2fb--`",
+--------------------------366796e1c748a2fb--`;",
     );
 }
 
