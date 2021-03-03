feea3b07c85971e7a376da30568053c078d4b14c
강동윤 <kdy1997.dev@gmail.com>
2021-03-03 19:49:40 +0900

    Update tets refs

--- ecmascript/transforms/module/tests/common_js.rs
+++ ecmascript/transforms/module/tests/common_js.rs
@@ -4569,6 +4569,14 @@ test!(
     console.log(secondElement)
     ",
     "
- 
+    'use strict';
+    var _necessary = require('necessary');
+    const { second  } = _necessary.arrayUtilities;
+    const elements = [
+        1,
+        2,
+        3
+    ], secondElement = second(elements);
+    console.log(secondElement);
     "
 );
