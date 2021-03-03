3cf26919b669ce18d12a1d9cbd5e1bbf5eacd65d
강동윤 <kdy1997.dev@gmail.com>
2021-03-03 14:03:09 +0900

    Add a test

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2316,3 +2316,22 @@ fn issue_1279_2() {
         },
     );
 }
+
+to_ts!(
+    deno_9650_1,
+    "
+    export class X {
+        constructor() {
+        }
+        bad(target :number) {
+            const d = 1;
+            const min = 0;
+            const max = 100;
+            console.log('x', `duration ${d} not in range - ${min} ≥ ${d} && ${max} ≥ ${d}`);
+        }
+    }
+    ",
+    "
+    
+    "
+);
