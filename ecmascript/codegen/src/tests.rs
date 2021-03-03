3215e7fb3d1fb6d55e927ebaca148abce63d5631
강동윤 <kdy1997.dev@gmail.com>
2021-03-03 16:35:36 +0900

    Update test refs

--- ecmascript/codegen/src/tests.rs
+++ ecmascript/codegen/src/tests.rs
@@ -511,7 +511,12 @@ fn deno_9620() {
     \r
     CONTENT\r
     --------------------------366796e1c748a2fb--`",
-        "",
+        "const content = `--------------------------366796e1c748a2fb\r
+Content-Disposition: form-data; name=\"payload\"\r
+Content-Type: text/plain\r
+\r
+CONTENT\r
+--------------------------366796e1c748a2fb--`",
     );
 }
 
