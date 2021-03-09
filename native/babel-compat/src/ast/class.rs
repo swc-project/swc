e56628e28010bfbb02bd9de6497a1c150450a25a
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-03-09 00:41:51 -0800

    WIP began splitting up AST into separate files

---
+++ native/babel-compat/src/ast/class.rs
@@ -0,0 +1,204 @@
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
+
+use crate::ast::{
+    common::{BaseNode, Identifier, IdOrQualifiedId, Access, Param, Decorator, PrivateName, TypeAnnotOrNoop, TypeParamDeclOrNoop},
+    expr::{Expression},
+    flow::{TypeParameterInstantiation},
+    object::{ObjectKey},
+    stmt::{BlockStatement},
+    typescript::{TSDeclareMethod, TSIndexSignature, TSExpressionWithTypeArguments},
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ClassMethodKind {
+    Get,
+    Set,
+    Method,
+    Constructor,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct ClassMethod {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    #[serde(default)]
+    pub kind: Option<ClassMethodKind>,
+    pub key: ObjectKey,
+    #[serde(default)]
+    pub params: Vec<Param>,
+    pub body: BlockStatement,
+    #[serde(default)]
+    pub computed: Option<bool>,
+    #[serde(default, rename = "static")]
+    pub is_static: Option<bool>,
+    #[serde(default)]
+    pub generator: Option<bool>,
+    #[serde(default, rename = "async")]
+    pub is_async: Option<bool>,
+    #[serde(default, rename = "abstract")]
+    pub is_abstract: Option<bool>,
+    #[serde(default)]
+    pub access: Option<Access>,
+    #[serde(default)]
+    pub accessibility: Option<Access>,
+    #[serde(default)]
+    pub decorators: Option<Vec<Decorator>>,
+    #[serde(default)]
+    pub optional: Option<bool>,
+    #[serde(default)]
+    pub return_type: Option<TypeAnnotOrNoop>,
+    #[serde(default)]
+    pub type_parameters: Option<TypeParamDeclOrNoop>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct ClassPrivateProperty {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    pub key: PrivateName,
+    #[serde(default)]
+    pub value: Option<Expression>,
+    #[serde(default)]
+    pub decorators: Option<Vec<Decorator>>,
+    #[serde(default, rename = "static")]
+    pub static_any: Value, // TODO: is this the right way to model any?
+    #[serde(default)]
+    pub type_annotation: Option<TypeAnnotOrNoop>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct ClassPrivateMethod {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    #[serde(default)]
+    pub kind: Option<ClassMethodKind>,
+    pub key: PrivateName,
+    #[serde(default)]
+    pub params: Vec<Param>,
+    pub body: BlockStatement,
+    #[serde(default, rename = "static")]
+    pub is_static: Option<bool>,
+    #[serde(default, rename = "abstract")]
+    pub is_abstract: Option<bool>,
+    #[serde(default)]
+    pub access: Option<Access>,
+    #[serde(default)]
+    pub accessibility: Option<Access>,
+    #[serde(default, rename = "async")]
+    pub is_async: Option<bool>,
+    #[serde(default)]
+    pub computed: Option<bool>,
+    #[serde(default)]
+    pub decorators: Option<Vec<Decorator>>,
+    #[serde(default)]
+    pub generator: Option<bool>,
+    #[serde(default)]
+    pub optional: Option<bool>,
+    #[serde(default)]
+    pub return_type: Option<TypeAnnotOrNoop>,
+    #[serde(default)]
+    pub type_parameters: Option<TypeParamDeclOrNoop>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct ClassProperty {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    pub key: ObjectKey,
+    #[serde(default)]
+    pub value: Option<Expression>,
+    #[serde(default)]
+    pub type_annotation: Option<TypeAnnotOrNoop>,
+    #[serde(default)]
+    pub decorators: Option<Vec<Decorator>>,
+    #[serde(default)]
+    pub computed: Option<bool>,
+    #[serde(default, rename = "static")]
+    pub is_static: Option<bool>,
+    #[serde(default, rename = "abstract")]
+    pub is_abstract: Option<bool>,
+    #[serde(default)]
+    pub accessibility: Option<Access>,
+    #[serde(default)]
+    pub declare: Option<bool>,
+    #[serde(default)]
+    pub definite: Option<bool>,
+    #[serde(default)]
+    pub optinoal: Option<bool>,
+    #[serde(default)]
+    pub readonly: Option<bool>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum ClassBodyEl {
+    #[serde(rename = "ClassMethod")]
+    Method(ClassMethod),
+    #[serde(rename = "ClassPrivateMethod")]
+    PrivateMethod(ClassPrivateMethod),
+    #[serde(rename = "ClassProperty")]
+    Prop(ClassProperty),
+    #[serde(rename = "ClassPrivateProperty")]
+    PrivateProp(ClassPrivateProperty),
+    #[serde(rename = "TSDeclareMethod")]
+    TSMethod(TSDeclareMethod),
+    #[serde(rename = "TSIndexSignature")]
+    TSIndex(TSIndexSignature),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub struct ClassBody {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    #[serde(default)]
+    pub body: Vec<ClassBodyEl>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct ClassImplements {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    pub id: Identifier,
+    #[serde(default)]
+    pub type_parameters: Option<TypeParameterInstantiation>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum ClassImpl {
+    #[serde(rename = "TSExpressionWithTypeArguments")]
+    TSExpr(TSExpressionWithTypeArguments),
+    #[serde(rename = "ClassImplements")]
+    Implements(ClassImplements),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub struct InterfaceExtends {
+    #[serde(flatten)]
+    pub base: BaseNode,
+    pub id: IdOrQualifiedId,
+    #[serde(default)]
+    pub type_parameters: Option<TypeParameterInstantiation>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub struct Super {
+    #[serde(flatten)]
+    pub base: BaseNode,
+}
