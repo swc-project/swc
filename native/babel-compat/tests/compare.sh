e1fb7ab5d878118bc3a138e990f4654d43dfc77a
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-03-19 00:30:53 -0700

    Changed gen script names to be more ergonimic to type

--- native/babel-compat/tests/compare.sh
+++ native/babel-compat/tests/compare.sh
@@ -3,4 +3,4 @@
 # Print a side by side comparison of Babel and SWC AST.
 SCRIPT_DIR=`dirname "$0"`
 
-paste <(node "$SCRIPT_DIR/genbabel.js" $1) <(node "$SCRIPT_DIR/genswc.js" $1) | column -s $'\t' -t
+paste <(node "$SCRIPT_DIR/babelgen.js" $1) <(node "$SCRIPT_DIR/swcgen.js" $1) | column -s $'\t' -t
