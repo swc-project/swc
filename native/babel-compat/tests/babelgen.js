413cc02e1aaaa93dca18bbf85f5d55743ae9e877
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-03-19 00:30:53 -0700

    Changed gen script names to be more ergonimic to type

--- native/babel-compat/tests/babelgen.js
+++ native/babel-compat/tests/babelgen.js
@@ -1,11 +1,11 @@
 // Helper for generating Babel fixture JSON.
-// USAGE: node genbabel.js path/to/input.js > path/to/output.json
+// USAGE: node babelgen.js path/to/input.js > path/to/output.json
 const {readFileSync} = require("fs");
 const {parse} = require("@babel/parser");
 
 const inputFile = process.argv[2];
 if (!inputFile) {
-    console.error("Missing input file. Hint: `node genbabel.js path/to/input.js`");
+    console.error("Missing input file. Hint: `node babelgen.js path/to/input.js`");
     process.exit(1);
 }
 
--- native/babel-compat/tests/swcgen.js
+++ native/babel-compat/tests/swcgen.js
@@ -1,11 +1,11 @@
 // Helper for generating SWC JSON.
-// USAGE: node genswc.js path/to/input.js > path/to/output.json
+// USAGE: node swcgen.js path/to/input.js > path/to/output.json
 const {readFileSync} = require("fs");
 const {parseSync} = require("../../../index.js");
 
 const inputFile = process.argv[2];
 if (!inputFile) {
-    console.error("Missing input file. Hint: `node genswc.js path/to/input.js`");
+    console.error("Missing input file. Hint: `node swcgen.js path/to/input.js`");
     process.exit(1);
 }
 
