c7e96936c55d03b7bf09ee867eac6b502224ae06
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-03-19 00:31:44 -0700

    Finished up converters for lit.rs Added converters for operators.rs

--- native/babel-compat/src/convert/lit.rs
+++ native/babel-compat/src/convert/lit.rs
@@ -1,22 +1,31 @@
 use super::Context;
-use crate::ast::lit::BooleanLiteral;
-use crate::ast::lit::Literal;
-use crate::ast::lit::StringLiteral;
+use crate::ast::lit::{
+    Literal, BooleanLiteral, StringLiteral, NullLiteral, NumericLiteral, BigIntLiteral,
+    RegExpLiteral,
+};
+use crate::ast::jsx::JSXText as BabelJSXText;
 use crate::convert::Babelify;
-use swc_ecma_ast::*;
+use swc_ecma_ast::{Lit, Str, Bool, Null, Number, BigInt, Regex, JSXText};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum LitOutput {
+    Lit(Literal),
+    JSX(BabelJSXText),
+}
 
 impl Babelify for Lit {
-    type Output = Literal;
+    type Output = LitOutput;
 
     fn babelify(self, ctx: &Context) -> Self::Output {
         match self {
-            Lit::Str(s) => Literal::String(s.babelify(ctx)),
-            Lit::Bool(b) => Literal::Boolean(b.babelify(ctx)),
-            Lit::Null(_) => {}
-            Lit::Num(_) => {}
-            Lit::BigInt(_) => {}
-            Lit::Regex(_) => {}
-            Lit::JSXText(_) => {}
+            Lit::Str(s) => LitOutput::Lit(Literal::String(s.babelify(ctx))),
+            Lit::Bool(b) => LitOutput::Lit(Literal::Boolean(b.babelify(ctx))),
+            Lit::Null(n) => LitOutput::Lit(Literal::Null(n.babelify(ctx))),
+            Lit::Num(n) => LitOutput::Lit(Literal::Numeric(n.babelify(ctx))),
+            Lit::BigInt(i) => LitOutput::Lit(Literal::BigInt(i.babelify(ctx))),
+            Lit::Regex(r) => LitOutput::Lit(Literal::RegExp(r.babelify(ctx))),
+            Lit::JSXText(t) => LitOutput::JSX(t.babelify(ctx)),
         }
     }
 }
@@ -27,7 +36,7 @@ impl Babelify for Str {
     fn babelify(self, ctx: &Context) -> Self::Output {
         StringLiteral {
             base: ctx.base(self.span),
-            value: self.value,
+            value: self.value.to_string(),
         }
     }
 }
@@ -39,6 +48,61 @@ impl Babelify for Bool {
         BooleanLiteral {
             base: ctx.base(self.span),
             value: self.value,
+        }
+    }
+}
+
+impl Babelify for Null {
+    type Output = NullLiteral;
+
+    fn babelify(self, ctx: &Context) -> Self::Output {
+        NullLiteral {
+            base: ctx.base(self.span),
+        }
+    }
+}
+
+impl Babelify for Number {
+    type Output = NumericLiteral;
+
+    fn babelify(self, ctx: &Context) -> Self::Output {
+        NumericLiteral {
+            base: ctx.base(self.span),
+            value: self.value,
+        }
+    }
+}
+
+impl Babelify for BigInt {
+    type Output = BigIntLiteral;
+
+    fn babelify(self, ctx: &Context) -> Self::Output {
+        BigIntLiteral {
+            base: ctx.base(self.span),
+            value: self.value.to_string(),
+        }
+    }
+}
+
+impl Babelify for Regex {
+    type Output = RegExpLiteral;
+
+    fn babelify(self, ctx: &Context) -> Self::Output {
+        RegExpLiteral {
+            base: ctx.base(self.span),
+            pattern: self.exp.to_string(),
+            flags: self.flags.to_string(),
+        }
+    }
+}
+
+impl Babelify for JSXText {
+    type Output = BabelJSXText;
+
+    fn babelify(self, ctx: &Context) -> Self::Output {
+        BabelJSXText {
+            base: ctx.base(self.span),
+            value: self.value.to_string(),
         }
     }
 }
---
+++ native/babel-compat/src/convert/operators.rs
@@ -0,0 +1,99 @@
+use super::Context;
+use crate::ast::expr::{BinaryExprOp, LogicalExprOp, UpdateExprOp, UnaryExprOp};
+use crate::convert::Babelify;
+use swc_ecma_ast::{BinaryOp, AssignOp, UpdateOp, UnaryOp};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BinaryOpOutput {
+    BinOp(BinaryExprOp),
+    LogicOp(LogicalExprOp),
+}
+
+impl Babelify for BinaryOp {
+    type Output = BinaryOpOutput;
+
+    fn babelify(self, _ctx: &Context) -> Self::Output {
+        match self {
+            BinaryOp::EqEq => BinaryOpOutput::BinOp(BinaryExprOp::Equal),
+            BinaryOp::NotEq => BinaryOpOutput::BinOp(BinaryExprOp::NotEqual),
+            BinaryOp::EqEqEq => BinaryOpOutput::BinOp(BinaryExprOp::StrictEqual),
+            BinaryOp::NotEqEq => BinaryOpOutput::BinOp(BinaryExprOp::StrictNotEqual),
+            BinaryOp::Lt => BinaryOpOutput::BinOp(BinaryExprOp::LessThan),
+            BinaryOp::LtEq => BinaryOpOutput::BinOp(BinaryExprOp::LessThanOrEqual),
+            BinaryOp::Gt => BinaryOpOutput::BinOp(BinaryExprOp::GreaterThan),
+            BinaryOp::GtEq => BinaryOpOutput::BinOp(BinaryExprOp::GreaterThanOrEqual),
+            BinaryOp::LShift => BinaryOpOutput::BinOp(BinaryExprOp::LeftShift),
+            BinaryOp::RShift => BinaryOpOutput::BinOp(BinaryExprOp::RightShift),
+            BinaryOp::ZeroFillRShift => BinaryOpOutput::BinOp(BinaryExprOp::UnsignedRightShift),
+            BinaryOp::Add => BinaryOpOutput::BinOp(BinaryExprOp::Addition),
+            BinaryOp::Sub => BinaryOpOutput::BinOp(BinaryExprOp::Subtraction),
+            BinaryOp::Mul => BinaryOpOutput::BinOp(BinaryExprOp::Multiplication),
+            BinaryOp::Div => BinaryOpOutput::BinOp(BinaryExprOp::Division),
+            BinaryOp::Mod => BinaryOpOutput::BinOp(BinaryExprOp::Remainder),
+            BinaryOp::BitOr => BinaryOpOutput::BinOp(BinaryExprOp::Or),
+            BinaryOp::BitXor => BinaryOpOutput::BinOp(BinaryExprOp::Xor),
+            BinaryOp::BitAnd => BinaryOpOutput::BinOp(BinaryExprOp::And),
+            BinaryOp::LogicalOr => BinaryOpOutput::LogicOp(LogicalExprOp::Or),
+            BinaryOp::LogicalAnd => BinaryOpOutput::LogicOp(LogicalExprOp::And),
+            BinaryOp::In => BinaryOpOutput::BinOp(BinaryExprOp::In),
+            BinaryOp::InstanceOf => BinaryOpOutput::BinOp(BinaryExprOp::Instanceof),
+            BinaryOp::Exp => BinaryOpOutput::BinOp(BinaryExprOp::Exponentiation),
+            BinaryOp::NullishCoalescing => BinaryOpOutput::LogicOp(LogicalExprOp::Nullish),
+        }
+    }
+}
+
+// Babel appears to just store all of these as a string. See AssignmentExpression.operator field.
+// NOTE: I'm unsure if this is the correct way to handle this case. - Dan
+impl Babelify for AssignOp {
+    type Output = String;
+    fn babelify(self, _ctx: &Context) -> Self::Output {
+        match self {
+            AssignOp::Assign => "=".into(),
+            AssignOp::AddAssign => "+=".into(),
+            AssignOp::SubAssign => "-=".into(),
+            AssignOp::MulAssign => "*=".into(),
+            AssignOp::DivAssign => "/=".into(),
+            AssignOp::ModAssign => "%=".into(),
+            AssignOp::LShiftAssign => "<<=".into(),
+            AssignOp::RShiftAssign => ">>=".into(),
+            AssignOp::ZeroFillRShiftAssign => ">>>=".into(),
+            AssignOp::BitOrAssign => "|=".into(),
+            AssignOp::BitXorAssign => "^=".into(),
+            AssignOp::BitAndAssign => "&=".into(),
+            AssignOp::ExpAssign => "**=".into(),
+            AssignOp::AndAssign => "&&=".into(),
+            AssignOp::OrAssign => "||=".into(),
+            AssignOp::NullishAssign => "??=".into(),
+        }
+    }
+}
+
+impl Babelify for UpdateOp {
+    type Output = UpdateExprOp;
+
+    fn babelify(self, _ctx: &Context) -> Self::Output {
+        match self {
+            UpdateOp::PlusPlus => UpdateExprOp::Increment,
+            UpdateOp::MinusMinus => UpdateExprOp::Decrement,
+        }
+    }
+}
+
+impl Babelify for UnaryOp {
+    type Output = UnaryExprOp;
+
+    fn babelify(self, _ctx: &Context) -> Self::Output {
+        match self {
+            UnaryOp::Minus => UnaryExprOp::Negation,
+            UnaryOp::Plus => UnaryExprOp::Plus,
+            UnaryOp::Bang => UnaryExprOp::LogicalNot,
+            UnaryOp::Tilde => UnaryExprOp::BitwiseNot,
+            UnaryOp::TypeOf => UnaryExprOp::Typeof,
+            UnaryOp::Void => UnaryExprOp::Void,
+            UnaryOp::Delete => UnaryExprOp::Delete,
+        }
+    }
+}
+
