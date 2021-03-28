a4b2b566deac3f5556644b494ad562744d008873
강동윤 <kdy1997.dev@gmail.com>
2021-03-28 22:23:57 +0900

    Remove duplicate

--- ecmascript/parser/tests/typescript/issue-1505/case2/index.tsx
+++
@@ -1,6 +0,0 @@
-const withTeamsForUser = <P extends InjectedTeamsProps>() =>
-  class extends Component<
-    Omit<P, keyof InjectedTeamsProps> & Partial<InjectedTeamsProps>
-  > {
-    static displayName = `x`;
-  };
