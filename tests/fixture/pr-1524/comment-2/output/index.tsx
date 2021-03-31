ff6ee6abf0a26e0301f86de590e5de702bd6f829
강동윤 <kdy1997.dev@gmail.com>
2021-03-31 14:02:54 +0900

    Update test refs

---
+++ tests/fixture/pr-1524/comment-2/output/index.tsx
@@ -0,0 +1,46 @@
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+function _iterableToArrayLimit(arr, i) {
+  var _arr = [];
+  var _n = true;
+  var _d = false;
+  var _e = undefined;
+  try {
+    for (
+      var _i = arr[Symbol.iterator](), _s;
+      !(_n = (_s = _i.next()).done);
+      _n = true
+    ) {
+      _arr.push(_s.value);
+      if (i && _arr.length === i) break;
+    }
+  } catch (err) {
+    _d = true;
+    _e = err;
+  } finally {
+    try {
+      if (!_n && _i["return"] != null) _i["return"]();
+    } finally {
+      if (_d) throw _e;
+    }
+  }
+  return _arr;
+}
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance");
+}
+function _slicedToArray(arr, i) {
+  return (
+    _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest()
+  );
+}
+function Bar() {
+  var ref = _slicedToArray(useState(0), 2),
+    foo = ref[0],
+    setFoo = ref[1];
+  React.useEffect(function () {
+    // @refresh reset
+  });
+  return React.createElement("h1", null, foo, " ");
+}
