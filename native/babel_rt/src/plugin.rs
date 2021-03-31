0064c00e2ba3c9ac8461444d5d444d14785dbafe
강동윤 <kdy1997.dev@gmail.com>
2021-03-31 18:10:27 +0900

    Blocked by https://github.com/napi-rs/napi-rs/issues/436

--- native/babel_rt/src/plugin.rs
+++ native/babel_rt/src/plugin.rs
@@ -1,8 +1,16 @@
+use napi::threadsafe_function::ThreadsafeFunction;
+use napi::threadsafe_function::ThreadsafeFunctionCallMode;
 use swc_babel_compat::ast::module::Program;
 
 /// One babel plugin.
-pub struct Plugin {}
+pub struct Plugin {
+    inner: ThreadsafeFunction<Program>,
+}
 
 impl Plugin {
-    pub fn invoke(&self, program: Program) -> Program {}
+    pub fn invoke(&self, program: Program) -> Program {
+        let status = self
+            .inner
+            .call(Ok(program), ThreadsafeFunctionCallMode::NonBlocking);
+    }
 }
