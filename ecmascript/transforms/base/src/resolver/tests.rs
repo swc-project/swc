53e5f0851f83fd26a7e604164c31aff7703a4c6f
강동윤 <kdy1997.dev@gmail.com>
2021-03-31 19:37:23 +0900

    Add a test for resolver

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2377,3 +2377,28 @@ to_ts!(
     }
     "
 );
+
+to_ts!(
+    tsc_computed_property_name_11,
+    "
+    var s: string;
+    var n: number;
+    var a: any;
+    var v = {
+        get [s]() { return 0; },
+        set [n](v) { },
+        get [s + s]() { return 0; },
+        set [s + n](v) { },
+        get [+s]() { return 0; },
+        set [\"\"](v) { },
+        get [0]() { return 0; },
+        set [a](v) { },
+        get [<any>true]() { return 0; },
+        set [`hello bye`](v) { },
+        get [`hello ${a} bye`]() { return 0; }
+    }
+    ",
+    "
+    
+    "
+);
