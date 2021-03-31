9c2b52d7eebba87cb371a284458b58fc3407f61f
강동윤 <kdy1997.dev@gmail.com>
2021-03-31 14:03:13 +0900

    Update test refs

--- tests/fixture/issue-1333/case2/output/index.js
+++ tests/fixture/issue-1333/case2/output/index.js
@@ -97,7 +97,8 @@ class Shard extends _utils.Emitter {
                 this._cleanupConnection();
                 try {
                     _classPrivateFieldGet(this, _ws2).close(code);
-                } catch  {
+                } catch  // no-op
+                {
                 }
                 if (emit) {
                     /**
