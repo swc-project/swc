258a1670df0a0ffd7445f907b2ef8abd2bdc0e82
강동윤 <kdy1997.dev@gmail.com>
2021-03-31 20:55:21 +0900

    Update test refs

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2399,6 +2399,21 @@ to_ts!(
     }
     ",
     "
-    
+    var s: string;
+    var n: number;
+    var a: any;
+    var v = {
+        get [s]() { return 0; },
+        set [n](v) { },
+        get [s + s]() { return 0; },
+        set [s + n](v__2) { },
+        get [+s]() { return 0; },
+        set [\"\"](v__2) { },
+        get [0]() { return 0; },
+        set [a](v__2) { },
+        get [<any>true]() { return 0; },
+        set [`hello bye`](v__2) { },
+        get [`hello ${a} bye`]() { return 0; }
+    }
     "
 );
