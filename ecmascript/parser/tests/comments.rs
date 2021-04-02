9e1d9ea0714fb6c2548d60d3d20d597625826207
강동윤 <kdy1997.dev@gmail.com>
2021-04-02 16:38:46 +0900

    Implement comment test suitre

--- ecmascript/parser/tests/comments.rs
+++ ecmascript/parser/tests/comments.rs
@@ -1,16 +1,21 @@
-use std::cell::RefCell;
 use std::path::PathBuf;
-use std::rc::Rc;
 use swc_common::comments::SingleThreadedComments;
+use swc_common::errors::DiagnosticBuilder;
+use swc_common::errors::Handler;
 use swc_common::input::SourceFileInput;
+use swc_common::Span;
+use swc_common::DUMMY_SP;
 use swc_ecma_ast::*;
 use swc_ecma_parser::lexer::Lexer;
 use swc_ecma_parser::EsConfig;
 use swc_ecma_parser::Parser;
 use swc_ecma_parser::Syntax;
 use swc_ecma_parser::TsConfig;
+use swc_ecma_visit::Node;
+use swc_ecma_visit::Visit;
+use swc_ecma_visit::VisitWith;
 use testing::fixture;
-use testing::run_test2;
+use testing::Tester;
 
 #[fixture("comments/**/input.js")]
 #[fixture("comments/**/input.ts")]
@@ -22,66 +27,128 @@ fn test(input: PathBuf) {
     let output_file_name = format!("output.{}", ext);
     let output_file = input.with_file_name(&output_file_name);
 
-    run_test2(false, |cm, handler| {
-        //
-        let fm = cm.load_file(&input).unwrap();
-        let syntax = match &*ext {
-            "js" => Syntax::Es(EsConfig {
-                jsx: false,
-                num_sep: true,
-                class_private_props: true,
-                class_private_methods: true,
-                class_props: true,
-                fn_bind: false,
-                decorators: true,
-                decorators_before_export: false,
-                export_default_from: true,
-                export_namespace_from: true,
-                dynamic_import: true,
-                nullish_coalescing: true,
-                optional_chaining: true,
-                import_meta: true,
-                top_level_await: true,
-                import_assertions: true,
-            }),
-            "ts" | "tsx" => Syntax::Typescript(TsConfig {
-                tsx: ext == "tsx",
-                decorators: true,
-                dynamic_import: true,
-                no_early_errors: true,
-                import_assertions: true,
-                ..Default::default()
-            }),
-            _ => {
-                unreachable!()
+    let output = Tester::new()
+        .print_errors(|cm, handler| -> Result<(), _> {
+            //
+            let fm = cm.load_file(&input).unwrap();
+            let syntax = match &*ext {
+                "js" => Syntax::Es(EsConfig {
+                    jsx: false,
+                    num_sep: true,
+                    class_private_props: true,
+                    class_private_methods: true,
+                    class_props: true,
+                    fn_bind: false,
+                    decorators: true,
+                    decorators_before_export: false,
+                    export_default_from: true,
+                    export_namespace_from: true,
+                    dynamic_import: true,
+                    nullish_coalescing: true,
+                    optional_chaining: true,
+                    import_meta: true,
+                    top_level_await: true,
+                    import_assertions: true,
+                }),
+                "ts" | "tsx" => Syntax::Typescript(TsConfig {
+                    tsx: ext == "tsx",
+                    decorators: true,
+                    dynamic_import: true,
+                    no_early_errors: true,
+                    import_assertions: true,
+                    ..Default::default()
+                }),
+                _ => {
+                    unreachable!()
+                }
+            };
+
+            let comments = SingleThreadedComments::default();
+
+            let lexer = Lexer::new(
+                syntax,
+                EsVersion::latest(),
+                SourceFileInput::from(&*fm),
+                Some(&comments),
+            );
+            let mut parser = Parser::new_from(lexer);
+
+            let module = parser.parse_module();
+            let module = match module {
+                Ok(v) => v,
+                Err(err) => {
+                    panic!("{:?}", err)
+                }
+            };
+
+            module.visit_with(
+                &Invalid { span: DUMMY_SP },
+                &mut CommentPrinter {
+                    handler: &handler,
+                    comments,
+                },
+            );
+
+            Err(())
+        })
+        .unwrap_err();
+
+    output.compare_to_file(&output_file).unwrap();
+}
+
+struct CommentPrinter<'a> {
+    handler: &'a Handler,
+    comments: SingleThreadedComments,
+}
+
+impl Visit for CommentPrinter<'_> {
+    fn visit_span(&mut self, n: &Span, _: &dyn Node) {
+        self.comments.with_leading(n.lo, |comments| {
+            for c in comments {
+                DiagnosticBuilder::new(
+                    &self.handler,
+                    swc_common::errors::Level::Note,
+                    "Leading (lo)",
+                )
+                .span_note(Span::new(n.lo, n.lo, Default::default()), &c.text)
+                .emit();
             }
-        };
+        });
 
-        let comments = SingleThreadedComments::default();
-
-        let lexer = Lexer::new(
-            syntax,
-            EsVersion::latest(),
-            SourceFileInput::from(&*fm),
-            Some(&comments),
-        );
-        let mut parser = Parser::new_from(lexer);
-
-        let module = parser.parse_module();
-        let module = match module {
-            Ok(v) => v,
-            Err(err) => {
-                err.into_diagnostic(&handler).emit();
-                return Err(());
+        self.comments.with_trailing(n.lo, |comments| {
+            for c in comments {
+                DiagnosticBuilder::new(
+                    &self.handler,
+                    swc_common::errors::Level::Note,
+                    "Trailing (lo)",
+                )
+                .span_note(Span::new(n.lo, n.lo, Default::default()), &c.text)
+                .emit();
             }
-        };
+        });
 
-        let (leading, trailing) = comments.take_all();
+        self.comments.with_leading(n.hi, |comments| {
+            for c in comments {
+                DiagnosticBuilder::new(
+                    &self.handler,
+                    swc_common::errors::Level::Note,
+                    "Leading (hi)",
+                )
+                .span_note(Span::new(n.hi, n.hi, Default::default()), &c.text)
+                .emit();
+            }
+        });
 
-        let leading = RefCell::into_inner(Rc::try_unwrap(leading).unwrap());
-        let trailing = RefCell::into_inner(Rc::try_unwrap(trailing).unwrap());
-
-        Ok(())
-    })
-    .unwrap()
+        self.comments.with_trailing(n.hi, |comments| {
+            for c in comments {
+                DiagnosticBuilder::new(
+                    &self.handler,
+                    swc_common::errors::Level::Note,
+                    "Trailing (hi)",
+                )
+                .span_note(Span::new(n.hi, n.hi, Default::default()), &c.text)
+                .emit();
+            }
+        });
+    }
 }
