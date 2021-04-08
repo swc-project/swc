dfe50d2b0b6b99509b0c4a083f2702a87e8fcb3d
강동윤 <kdy1997.dev@gmail.com>
2021-04-08 17:29:19 +0900

    #1549: Add more tests

--- ecmascript/transforms/compat/tests/es2015_template_literals.rs
+++ ecmascript/transforms/compat/tests/es2015_template_literals.rs
@@ -946,5 +946,13 @@ test!(
     |_| tr(Default::default()),
     issue_1549_1,
     "const a = `\r\n`;",
-    ""
+    "var a = \"\\r\\n\";"
+);
+
+test!(
+    syntax(),
+    |_| tr(Default::default()),
+    issue_1549_2,
+    "const a = \"\r\n\";",
+    "const a = \"\\r\\n\";"
 );
