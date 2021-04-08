63f37b978f1ceef3226d07af2ddf2d3b9f40c099
강동윤 <kdy1997.dev@gmail.com>
2021-04-08 18:51:13 +0900

    Add a test for #1532

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,4 +1,5 @@
 use super::Rc;
+use crate::Options;
 use serde_json;
 
 #[test]
@@ -9,4 +10,12 @@ fn object() {
 #[test]
 fn array() {
     let _: Rc = serde_json::from_str(include_str!("array.json")).expect("failed to parse");
+}
+
+#[test]
+fn issue_1532() {
+    let res = serde_json::from_str::<Options>(include_str!("issue-1532.json"));
+
+    let err = res.expect_err("should fail");
+    assert!(err.to_string().contains("unknown variant `esnext`"));
 }
