c50498b78b1a4229c140408c0485ce5271fe09fa
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-10 00:40:06 -0700

    Added working fixture test runner

--- native/babel-compat/tests/convert.rs
+++ native/babel-compat/tests/convert.rs
@@ -1,21 +1,116 @@
+#![feature(test)]
+extern crate test;
+
 use swc_babel_compat::ast::module::File;
 use swc_babel_compat::convert::{Context, Babelify};
 use swc::Compiler;
 use swc_common::{
-    FilePathMapping, SourceMap, FileName, SourceFile,
+    // FilePathMapping, SourceMap, FileName, SourceFile,
+    FilePathMapping, SourceMap, FileName,
     errors::{ColorConfig, Handler},
 };
-use anyhow::Result;
-use std::sync::Arc;
-use std::fs;
-
-#[cfg(test)]
+use anyhow::{Context as AnyhowContext, Error};
 use pretty_assertions::{assert_eq};
-// use pretty_assertions::{assert_eq, assert_ne};
+use walkdir::WalkDir;
+use test::{test_main, TestDesc, TestDescAndFn, DynTestFn, ShouldPanic, TestName, TestType};
+use std::{sync::Arc, fs, env, path::PathBuf};
 
 #[test]
+fn fixtures() {
+    let args: Vec<_> = env::args().collect();
+    let mut tests: Vec<TestDescAndFn> = Vec::new();
+    add_fixture_tests(&mut tests).unwrap();
+    test_main(&args, tests, Some(test::Options::new()));
+}
+
+fn add_fixture_tests(tests: &mut Vec<TestDescAndFn>) -> Result<(), Error> {
+    for entry in WalkDir::new(PathBuf::from(env::var("CARGO_MANIFEST_DIR")?).join("tests").join("fixtures")) {
+        let entry = entry
+            .with_context(|| "Failed to walk dir")?;
+        if !entry.file_type().is_dir() {
+            continue;
+        }
+        let input_file: PathBuf = entry.path().join("input.js");
+        if !input_file.is_file() {
+            continue;
+        }
+        let output_file: PathBuf = entry.path().join("output.json");
+        if !output_file.is_file() {
+            continue;
+        }
+
+        let input = fs::read_to_string(&input_file)
+            .with_context(|| format!("Failed to open file: {}", &input_file.to_string_lossy()))?;
+        let output = fs::read_to_string(&output_file)
+            .with_context(|| format!("Failed to open file: {}", &output_file.to_string_lossy()))?;
+
+        add_test(
+            tests,
+            format!("babel_compat::fixture::{}", entry.path().display()),
+            false,
+            move || {
+                run_test(input, output);
+            },
+        );
+    }
+
+    Ok(())
+}
+
+fn add_test<F: FnOnce() + Send + 'static>(
+    tests: &mut Vec<TestDescAndFn>,
+    name: String,
+    ignore: bool,
+    f: F,
+) {
+    tests.push(TestDescAndFn {
+        desc: TestDesc {
+            test_type: TestType::UnitTest,
+            name: TestName::DynTestName(name),
+            ignore,
+            should_panic: ShouldPanic::No,
+            allow_fail: false,
+        },
+        testfn: DynTestFn(Box::new(f)),
+    });
+}
+
+fn run_test(src: String, expected: String) {
+    let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+    let handler = Arc::new(Handler::with_tty_emitter(
+        ColorConfig::Always,
+        true,
+        false,
+        Some(cm.clone()),
+    ));
+    let compiler = Compiler::new(cm.clone(), handler);
+    let fm = compiler.cm.new_source_file(FileName::Anon, src);
+
+    let swc_ast = compiler.parse_js(
+        fm.clone(),
+        Default::default(),
+        Default::default(),
+        false,
+        true, // parse conmments
+    ).unwrap();
+
+    let ctx = Context {
+        fm: fm,
+        cm: cm,
+        comments: Arc::new(compiler.comments().clone()),
+    };
+    let ast = swc_ast.babelify(&ctx);
+
+    let expected_ast: File = serde_json::from_str(&expected).unwrap();
+
+    assert_eq!(expected_ast, ast);
+}
+
+/*
+#[test]
 pub fn t1() -> Result<()> {
-    let src = fs::read_to_string("x.js")?;
+    // let src = fs::read_to_string("x.js")?;
+    let src = fs::read_to_string("tests/fixture/simple/input.js")?;
 
     let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
     let handler = Arc::new(Handler::with_tty_emitter(
@@ -32,7 +127,7 @@ pub fn t1() -> Result<()> {
         Default::default(),
         Default::default(),
         false,
-        Default::default(),
+        true, // parse conmments
     )?;
 
     let ctx = Context {
@@ -42,35 +137,15 @@ pub fn t1() -> Result<()> {
     };
     let ast = swc_ast.babelify(&ctx);
 
-    let output = fs::read_to_string("x.json")?;
+    // let output = fs::read_to_string("x.json")?;
+    let output = fs::read_to_string("tests/fixture/simple/output.json")?;
     let expected_ast: File = serde_json::from_str(&output)?;
 
-    // assert_eq!(ast, expected_ast);
-    println!("FROM SWC\n\n{:#?}\n\nFROM BABEL\n\n{:#?}", ast, expected_ast);
+    assert_eq!(expected_ast, ast);
+    // println!("FROM SWC\n\n{:#?}\n\nFROM BABEL\n\n{:#?}", ast, expected_ast);
 
     Ok(())
 }
+*/
 
-// // #[cfg(feature = "babel_test")]
-// #[test]
-// pub fn t1() {
-//     let a = TestNode {
-//         base: BaseNode {
-//             leading_comments: Default::default(),
-//             inner_comments: Default::default(),
-//             trailing_comments: Default::default(),
-//             start: Some(0),
-//             end: Some(4),
-//             loc: Some(Loc {
-//                 start: LineCol { line: 0, column: 0 },
-//                 end: LineCol { line: 1, column: 4 },
-//             }),
-//             extra: Default::default(),
-//         },
-//         is_optional: true,
-//     };
-//     let mut b = a.clone();
-//     b.base.start = None;
-//     assert_eq!(a, b);
-// }
 
