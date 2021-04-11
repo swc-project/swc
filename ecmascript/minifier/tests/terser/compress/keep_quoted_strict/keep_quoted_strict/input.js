b14fd16acc8e5dea93be37a7ee9e87120f9f784b
Fábio Santos <fabiosantosart@gmail.com>
2021-04-11 21:15:01 +0100

    fix tests

--- ecmascript/minifier/tests/terser/compress/keep_quoted_strict/keep_quoted_strict/input.js
+++ ecmascript/minifier/tests/terser/compress/keep_quoted_strict/keep_quoted_strict/input.js
@@ -19,8 +19,8 @@ var b = {
     },
 };
 var c = {};
-Object.defineProperty(c, "propa", { value: 9 });
-Object.defineProperty(c, "propc", { value: 10 });
+Object.defineProperty(c, "propa", { "value": 9 });
+Object.defineProperty(c, "propc", { "value": 10 });
 console.log(a.propa, a.propb, a.propc, a["propc"], a.propd, a["propd"]);
 console.log(b["propa"], b["propb"], b.propc, b["propc"], b.propd, b["propd"]);
 console.log(c.propa, c["propc"]);
