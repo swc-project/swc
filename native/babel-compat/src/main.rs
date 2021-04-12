cdd28d65e66141d0f78b7261fd31ce161f8557e2
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-12 00:30:58 -0700

    More testing code

--- native/babel-compat/src/main.rs
+++ native/babel-compat/src/main.rs
@@ -1,97 +1,10 @@
 
-fn main() {}
+// fn main() {}
 
-
-/*
-mod ser_union;
-use ser_union::SerializeUnion;
-
-// use serde::{Serialize, Deserialize, Serializer};
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// struct A {}
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// struct B {}
-// #[derive(SerializeUnion)]
-// enum X {
-//     A(A),
-//     B(B),
-// }
-// fn main() {
-// }
-
-
-use serde::{Serialize, Deserialize, Serializer};
-// use serde::{Serialize, Deserialize};
-use serde_json::Result;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type")]
-struct ThingA {
-    value: usize,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type")]
-struct ThingB {
-    value: usize,
-}
-
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-#[derive(Debug, Clone, SerializeUnion, Deserialize)]
-// #[serde(untagged)]
-#[serde(tag = "type")]
-enum Thing {
-    ThingA(ThingA),
-    ThingB(ThingB),
-}
-
-// impl Serialize for Thing {
-//     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-//     where
-//         S: Serializer,
-//     {
-//         match self {
-//             Thing::ThingA(a) => serializer.serialize_some(&a),
-//             Thing::ThingB(b) => serializer.serialize_some(&b),
-//             // Thing::ThingA(a) => serializer.serialize_newtype_variant("", 0, "ThingA", &a),
-//             // Thing::ThingB(b) => serializer.serialize_newtype_variant("", 1, "", &b),
-//         }
-//     }
-// }
-
-fn main() -> Result<()> {
-
-
-    let a = ThingA { value: 0 };
-    println!("{}", serde_json::to_string(&a)?);
-
-    let json = r#"{"type": "ThingB", "value": 0}"#;
-    let thing: Thing = serde_json::from_str(json)?;
-    // println!("{:?}", thing);
-    println!("{}", serde_json::to_string(&thing)?);
-
-    // println!("{:-<80}", "");
-    // let thing_b = ThingB { value: 10 };
-    // println!("{}", serde_json::to_string(&thing_b)?);
-
-    // let json = r#"
-    // {
-    //     "type": "ThingB",
-    //     "value": 10
-    // }
-    // "#;
-    // let thing: Thing = serde_json::from_str(json)?;
-    // println!("{:?}", thing);
-    // println!("{}", serde_json::to_string(&thing)?);
-
-    Ok(())
-
-}
-*/
-
-/*
 mod ast;
 mod convert;
+mod ser_union;
+mod defaults;
 
 use convert::{Context, Babelify};
 use ast::module::File;
@@ -107,18 +20,14 @@ use std::sync::Arc;
 use std::fs;
 
 fn main() {
-    // let src = read_fixture_file("simple/input.js".to_owned()).unwrap();
-    let src = fs::read_to_string("x.js").unwrap();
-
-    let cm = get_source_map();
-    let handler = get_handler(&cm);
-    let compiler = Compiler::new(cm.clone(), handler);
-    let fm = compiler.cm.new_source_file(FileName::Anon, src);
-    // let comments = Arc::new(compiler.comments().clone());
+    let src = fs::read_to_string("tests/fixtures/simple/input.js").unwrap();
+    let (cm, compiler, fm) = init(src);
 
     let program = compile(&compiler, fm.clone()).unwrap();
 
     // println!("{:#?}", program);
+
+    // println!("{}", serde_json::to_string_pretty(&program).unwrap());
 
     let ctx = Context {
         fm: fm,
@@ -136,21 +45,18 @@ fn main() {
     // println!("{:#?}", expected);
 }
 
-// fn read_fixture_file(path: String) -> std::io::Result<String> {
-//     fs::read_to_string("tests/fixture/".to_owned() + &path)
-// }
-
-fn get_source_map() -> Arc<SourceMap> {
-    Arc::new(SourceMap::new(FilePathMapping::empty()))
-}
-
-fn get_handler(cm: &Arc<SourceMap>) -> Arc<Handler> {
-    Arc::new(Handler::with_tty_emitter(
+fn init(src: String) -> (Arc<SourceMap>, Compiler, Arc<SourceFile>) {
+    let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+    let handler = Arc::new(Handler::with_tty_emitter(
         ColorConfig::Always,
         true,
         false,
         Some(cm.clone()),
-    ))
+    ));
+    let compiler = Compiler::new(cm.clone(), handler);
+    let fm = compiler.cm.new_source_file(FileName::Anon, src);
+
+    (cm, compiler, fm)
 }
 
 fn compile(compiler: &Compiler, fm: Arc<SourceFile>) -> Result<Program> {
@@ -161,10 +67,5 @@ fn compile(compiler: &Compiler, fm: Arc<SourceFile>) -> Result<Program> {
         false,
         Default::default(),
     )
-    // match program {
-    //     Program::Module(module) => Ok(module),
-    //     _ => Err(anyhow!("Unexpected program: {:?}", program)),
-    // }
 }
-*/
 
