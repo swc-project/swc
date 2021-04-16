f7475d724132ece9271c87bca9675d8a801f4537
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-15 23:54:18 -0700

    Quick test for visitor pattern

--- native/babel-compat/src/main.rs
+++ native/babel-compat/src/main.rs
@@ -1,11 +1,11 @@
 
-fn main() {}
+#![feature(type_name_of_val)]
 
-/*
 mod ast;
 mod convert;
 mod ser_union;
 mod defaults;
+mod visit;
 
 use convert::{Context, Babelify};
 use ast::module::File;
@@ -19,6 +19,23 @@ use anyhow::{anyhow, Result};
 use serde_json::{json, Value};
 use std::sync::Arc;
 use std::fs;
+
+
+// ----------------------------------------------------------------------------
+// Visitor
+use visit::{Visit, Node, VisitWith};
+use ast::common::Identifier;
+
+struct IdCollector {
+    ids: Vec<Identifier>,
+}
+
+impl Visit for IdCollector {
+    fn visit_identifier(&mut self, id: &Identifier, _parent: &dyn Node) {
+        self.ids.push(id.clone());
+    }
+}
+// ----------------------------------------------------------------------------
 
 fn main() {
     let src = fs::read_to_string("tests/fixtures/simple/input.js").unwrap();
@@ -35,11 +52,15 @@ fn main() {
         cm: cm,
         comments: Arc::new(compiler.comments().clone()),
     };
-    let babel = program.babelify(&ctx);
-    // println!("{:#?}", babel);
+    let ast = program.babelify(&ctx);
+    println!("{:#?}", ast);
 
-    let json = serde_json::to_string_pretty(&babel).unwrap();
-    println!("{}", json);
+    let mut idcoll = IdCollector { ids: vec![] };
+    idcoll.visit_with(&mut ast);
+    println!("{:#?}", idcoll.ids);
+
+    // let json = serde_json::to_string_pretty(&ast).unwrap();
+    // println!("{}", json);
 
     // let output = read_fixture_file("simple/output.json".to_owned()).unwrap();
     // let expected: File = serde_json::from_str(&output).unwrap();
@@ -69,5 +90,6 @@ fn compile(compiler: &Compiler, fm: Arc<SourceFile>) -> Result<Program> {
         Default::default(),
     )
 }
-*/
 
+
+// fn main() {}
