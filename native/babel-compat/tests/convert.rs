f657ac7fb0565496b4d0bf227f71566240ea911e
Gregor Weber <gregorwbr@gmail.com>
2021-04-17 22:58:39 +0200

    Include TypeScript fixture when testing all fixtures

--- native/babel-compat/tests/convert.rs
+++ native/babel-compat/tests/convert.rs
@@ -1,97 +1,101 @@
 #![feature(test)]
 extern crate test;
 
-use swc_babel_ast::File;
-use swc_babel_compat::{Context, Babelify};
+use anyhow::{Context as AnyhowContext, Error};
+use pretty_assertions::assert_eq;
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use swc::Compiler;
+use swc_babel_ast::File;
+use swc_babel_compat::{Babelify, Context};
 use swc_common::{
-    // FilePathMapping, SourceMap, FileName, SourceFile,
-    FilePathMapping, SourceMap, FileName,
     errors::{ColorConfig, Handler},
+    FileName,
+    // FilePathMapping, SourceMap, FileName, SourceFile,
+    FilePathMapping,
+    SourceMap,
 };
 use swc_ecma_parser::Syntax;
-use anyhow::{Context as AnyhowContext, Error};
-use pretty_assertions::{assert_eq};
+use test::{test_main, DynTestFn, ShouldPanic, TestDesc, TestDescAndFn, TestName, TestType};
 use walkdir::WalkDir;
-use test::{test_main, TestDesc, TestDescAndFn, DynTestFn, ShouldPanic, TestName, TestType};
-use std::{sync::Arc, fs, env, path::{PathBuf, Path}};
-
-// #[test]
-fn fixtures() {
-    let args: Vec<_> = env::args().collect();
-    let mut tests: Vec<TestDescAndFn> = Vec::new();
-    add_fixture_tests(&mut tests).unwrap();
-    test_main(&args, tests, Some(test::Options::new()));
-}
 
 #[test]
-fn single_fixture() -> Result<(), Error> {
-    let input_file = "tests/fixtures/ts-function/input.ts";
-    let output_file = "tests/fixtures/ts-function/output.json";
+fn fixtures() -> Result<(), Error> {
+    let mut tests = vec![];
 
-    let input = fs::read_to_string(&input_file)
-        .with_context(|| format!("Failed to open file: {}", &input_file))?;
-    let output = fs::read_to_string(&output_file)
-        .with_context(|| format!("Failed to open file: {}", &output_file))?;
-    run_test(input, output);
-
-    Ok(())
-}
-
-fn add_fixture_tests(tests: &mut Vec<TestDescAndFn>) -> Result<(), Error> {
-    let cargo_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?).join("tests").join("fixtures");
-    for entry in WalkDir::new(&cargo_dir) {
-        let entry = entry
-            .with_context(|| "Failed to walk dir")?;
+    let fixtures_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
+        .join("tests")
+        .join("fixtures");
+    for entry in WalkDir::new(&fixtures_path).into_iter() {
+        let entry = entry.with_context(|| "Failed to walk dir")?;
         if !entry.file_type().is_dir() {
             continue;
         }
-        let input_file: PathBuf = entry.path().join("input.js");
-        if !input_file.is_file() {
-            continue;
-        }
-        let output_file: PathBuf = entry.path().join("output.json");
-        if !output_file.is_file() {
+
+        let js_path: PathBuf = entry.path().join("input.js");
+        let ts_path: PathBuf = entry.path().join("input.ts");
+        let output_path: PathBuf = entry.path().join("output.json");
+
+        let is_typescript = ts_path.is_file();
+
+        if (!js_path.is_file() && !ts_path.is_file()) || !output_path.is_file() {
             continue;
         }
 
-        let input = fs::read_to_string(&input_file)
-            .with_context(|| format!("Failed to open file: {}", &input_file.to_string_lossy()))?;
-        let output = fs::read_to_string(&output_file)
-            .with_context(|| format!("Failed to open file: {}", &output_file.to_string_lossy()))?;
+        let input = fs::read_to_string(if is_typescript { &ts_path } else { &js_path })
+            .with_context(|| format!("Failed to open file: {}", &js_path.to_string_lossy()))?;
+        let output = fs::read_to_string(&output_path)
+            .with_context(|| format!("Failed to open file: {}", &output_path.to_string_lossy()))?;
 
-        add_test(
-            tests,
-            format!("babel_compat::convert::{}", get_test_name(entry.path(), &cargo_dir)?),
-            false,
-            move || {
-                run_test(input, output);
+        tests.push(TestDescAndFn {
+            desc: TestDesc {
+                test_type: TestType::IntegrationTest,
+                name: TestName::DynTestName(format!(
+                    "babel_compat::convert::{}",
+                    get_test_name(entry.path(), &fixtures_path)?
+                )),
+                ignore: false,
+                should_panic: ShouldPanic::No,
+                allow_fail: false,
             },
-        );
+            testfn: DynTestFn(Box::new(move || {
+                let syntax = if is_typescript {
+                    Syntax::Typescript(Default::default())
+                } else {
+                    Syntax::default()
+                };
+                run_test(input, output, syntax);
+            })),
+        })
     }
+
+    test_main(
+        &env::args().collect::<Vec<_>>(),
+        tests,
+        Some(test::Options::new()),
+    );
 
     Ok(())
 }
 
-fn add_test<F: FnOnce() + Send + 'static>(
-    tests: &mut Vec<TestDescAndFn>,
-    name: String,
-    ignore: bool,
-    f: F,
-) {
-    tests.push(TestDescAndFn {
-        desc: TestDesc {
-            test_type: TestType::UnitTest,
-            name: TestName::DynTestName(name),
-            ignore,
-            should_panic: ShouldPanic::No,
-            allow_fail: false,
-        },
-        testfn: DynTestFn(Box::new(f)),
-    });
-}
+// #[test]
+// fn single_fixture() -> Result<(), Error> {
+//     let input_file = "tests/fixtures/ts-function/input.ts";
+//     let output_file = "tests/fixtures/ts-function/output.json";
+//
+//     let input = fs::read_to_string(&input_file)
+//         .with_context(|| format!("Failed to open file: {}", &input_file))?;
+//     let output = fs::read_to_string(&output_file)
+//         .with_context(|| format!("Failed to open file: {}", &output_file))?;
+//     run_test(input, output, Syntax::Typescript(Default::default()));
+//
+//     Ok(())
+// }
 
-fn run_test(src: String, expected: String) {
+fn run_test(src: String, expected: String, syntax: Syntax) {
     let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
     let handler = Arc::new(Handler::with_tty_emitter(
         ColorConfig::Always,
@@ -102,17 +106,19 @@ fn run_test(src: String, expected: String) {
     let compiler = Compiler::new(cm.clone(), handler);
     let fm = compiler.cm.new_source_file(FileName::Anon, src);
 
-    let swc_ast = compiler.parse_js(
-        fm.clone(),
-        Default::default(),
-        Syntax::Typescript(Default::default()),
-        false,
-        true, // parse conmments
-    ).unwrap();
+    let swc_ast = compiler
+        .parse_js(
+            fm.clone(),
+            Default::default(),
+            syntax,
+            false,
+            true, // parse conmments
+        )
+        .unwrap();
 
     let ctx = Context {
-        fm: fm,
-        cm: cm,
+        fm,
+        cm,
         comments: Arc::new(compiler.comments().clone()),
     };
     let ast = swc_ast.babelify(&ctx);
@@ -122,8 +128,8 @@ fn run_test(src: String, expected: String) {
     assert_eq!(expected_ast, ast);
 }
 
-fn get_test_name(path: &Path, cargo_dir: &Path) -> Result<String, Error> {
-    let s: String = path.strip_prefix(cargo_dir)?.to_string_lossy().into();
+fn get_test_name(path: &Path, fixture_path: &Path) -> Result<String, Error> {
+    let s: String = path.strip_prefix(fixture_path)?.to_string_lossy().into();
     Ok(s)
 }
 
@@ -168,5 +174,3 @@ pub fn t1() -> Result<()> {
     Ok(())
 }
 */
-
-
