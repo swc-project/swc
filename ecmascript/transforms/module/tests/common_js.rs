c3686b4857144da1f1f87d131231a364891c1462
강동윤 <kdy1997.dev@gmail.com>
2021-04-17 14:05:57 +0900

    More context is required for #1568

--- ecmascript/transforms/module/tests/common_js.rs
+++ ecmascript/transforms/module/tests/common_js.rs
@@ -4634,11 +4634,12 @@ test!(
     Object.defineProperty(exports, '__esModule', {
       value: true
     });
-    exports.default = get;
     
     function get(key) {
       console.log(key);
     }
+
+    exports.default = get;
     "
 );
 
