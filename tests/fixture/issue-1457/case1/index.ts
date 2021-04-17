43d44e44fdcb27e55b308ed5f223b06cb42c7cc0
강동윤 <kdy1997.dev@gmail.com>
2021-04-17 14:15:25 +0900

    Add a test for #1457

---
+++ tests/fixture/issue-1457/case1/index.ts
@@ -0,0 +1,21 @@
+import {
+    setGlobalOptions,
+    plugin,
+    buildSchema,
+    addModelToTypegoose,
+    deleteModel,
+} from '@typegoose/typegoose';
+import * as typegoose from '@typegoose/typegoose';
+// typegoose.mongoose.set('debug', true);
+setGlobalOptions({
+    options: {
+        allowMixed: typegoose.Severity.ALLOW,
+    },
+    schemaOptions: {
+        timestamps: {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+    },
+});
+import { schemas } from './schemas';
