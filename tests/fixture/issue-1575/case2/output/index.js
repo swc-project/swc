134e4f9fbe2ed2ea59d230eaf2937534011439dc
강동윤 <kdy1997.dev@gmail.com>
2021-04-17 12:53:40 +0900

    Update test refs

---
+++ tests/fixture/issue-1575/case2/output/index.js
@@ -0,0 +1,49 @@
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+    try {
+        var info = gen[key](arg);
+        var value = info.value;
+    } catch (error) {
+        reject(error);
+        return;
+    }
+    if (info.done) {
+        resolve(value);
+    } else {
+        Promise.resolve(value).then(_next, _throw);
+    }
+}
+function _asyncToGenerator(fn) {
+    return function() {
+        var self = this, args = arguments;
+        return new Promise(function(resolve, reject) {
+            var gen = fn.apply(self, args);
+            function _next(value) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+            }
+            function _throw(err) {
+                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+            }
+            _next(undefined);
+        });
+    };
+}
+var regeneratorRuntime = require("regenerator-runtime");
+var obj = {
+    foo: 5,
+    method: function() {
+        return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+            return regeneratorRuntime.wrap(function _callee$(_ctx) {
+                while(1)switch(_ctx.prev = _ctx.next){
+                    case 0:
+                        return _ctx.abrupt("return", this.foo);
+                    case 1:
+                    case "end":
+                        return _ctx.stop();
+                }
+            }, _callee, this);
+        })).apply(this);
+    }
+};
+obj.method().then(function(v) {
+    return console.log(v);
+});
