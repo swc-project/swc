76670c7ad94bac7a3451c135f7cd5d31760f5e38
강동윤 <kdy1997.dev@gmail.com>
2021-04-21 16:45:00 +0900

    Add a test

--- common/tests/ast_serde.rs
+++ common/tests/ast_serde.rs
@@ -1,6 +1,8 @@
+use serde_json::from_str;
 use swc_common::ast_serde;
 
 #[ast_serde]
+#[derive(Debug, PartialEq)]
 pub enum Ambiguous {
     #[tag("A")]
     A(A),
@@ -8,7 +10,15 @@ pub enum Ambiguous {
     B(B),
 }
 #[ast_serde("B")]
+#[derive(Debug, PartialEq)]
 pub struct A {}
 
 #[ast_serde("B")]
+#[derive(Debug, PartialEq)]
 pub struct B {}
+
+#[test]
+fn deserialize() {
+    assert_eq!(A {}, from_str(r#"{"type": "A"}"#).unwrap());
+    assert_eq!(B {}, from_str(r#"{"type": "B"}"#).unwrap());
+}
