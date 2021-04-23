c436ec50e268d1e582c8f30de0892197abd5c74c
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-23 00:52:36 -0700

    Added anonymous function test

--- native/babel-compat/tests/convert.rs
+++ native/babel-compat/tests/convert.rs
@@ -84,8 +84,8 @@ fn fixtures() -> Result<(), Error> {
 
 #[test]
 fn single_fixture() -> Result<(), Error> {
-    let input_file = "tests/fixtures/function-simple/input.js";
-    let output_file = "tests/fixtures/function-simple/output.json";
+    let input_file = "tests/fixtures/function-anonymous/input.js";
+    let output_file = "tests/fixtures/function-anonymous/output.json";
 
     let input = fs::read_to_string(&input_file)
         .with_context(|| format!("Failed to open file: {}", &input_file))?;
