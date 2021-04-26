94080d05c79f73da002d38a2ae1735ab697dcb28
강동윤 <kdy1997.dev@gmail.com>
2021-04-26 16:19:30 +0900

    Some work

--- bundler/src/algo/johnson.rs
+++ bundler/src/algo/johnson.rs
@@ -1,6 +1,8 @@
-use fxhash::FxHashMap;
-
 use crate::util::fast_graph::FastDiGraphMap;
+use crate::util::fast_graph::NodeTrait;
+use fxhash::FxHashMap;
+use petgraph::EdgeDirection::Outgoing;
+use std::hash::Hash;
 
 pub(crate) struct Johnson<N, E> {
     graph: FastDiGraphMap<N, E>,
@@ -9,4 +11,66 @@ pub(crate) struct Johnson<N, E> {
     circuits: Vec<Vec<N>>,
 }
 
-impl<N, E> Johnson<N, E> {}
+impl<N, E> Johnson<N, E>
+where
+    N: Copy + Eq + Hash + NodeTrait,
+{
+    fn unblock(&mut self, u: N) {
+        self.blocked.insert(u, false);
+
+        while let Some(vec) = self.blocked_nodes.get_mut(&u) {
+            if vec.is_empty() {
+                break;
+            }
+
+            let w = vec.remove(0);
+            self.unblock(w);
+        }
+    }
+
+    fn circuit(&mut self, graph: &FastDiGraphMap<N, E>, v: N, s: N, stack: &mut Vec<N>) -> bool {
+        if graph.node_count() == 0 {
+            return false;
+        }
+
+        let mut find = false;
+        stack.push(v);
+        self.blocked.insert(v, true);
+
+        for w in graph.neighbors_directed(v, Outgoing) {
+            if w == s {
+                stack.push(s);
+                self.circuits.push(stack.clone());
+                stack.pop();
+                find = true;
+            } else {
+                if !self.blocked.get(&w).unwrap() {
+                    if self.circuit(graph, w, s, stack) {
+                        find = true;
+                    }
+                }
+            }
+        }
+
+        if find {
+            self.unblock(v);
+        } else {
+            for w in graph.neighbors_directed(v, Outgoing) {
+                let vec = self.blocked_nodes.get_mut(&w).unwrap();
+                if !vec.contains(&v) {
+                    vec.push(v);
+                }
+            }
+        }
+
+        stack.pop();
+
+        find
+    }
+
+    fn least_vertex(graph: &FastDiGraphMap<N, E>) -> Option<N> {
+        graph.nodes().min()
+    }
+
+    fn sub_graph_from(g: &FastDiGraphMap<N, E>, i: N) -> FastDiGraphMap<N, E> {}
+}
