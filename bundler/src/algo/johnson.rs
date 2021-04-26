fd46bb6207632c60f78907b9a7b083a9fefafcd5
강동윤 <kdy1997.dev@gmail.com>
2021-04-26 16:05:44 +0900

    Add struct

--- bundler/src/algo/johnson.rs
+++ bundler/src/algo/johnson.rs
@@ -1,1 +1,12 @@
+use fxhash::FxHashMap;
 
+use crate::util::fast_graph::FastDiGraphMap;
+
+pub(crate) struct Johnson<N, E> {
+    graph: FastDiGraphMap<N, E>,
+    blocked: FxHashMap<N, bool>,
+    blocked_nodes: FxHashMap<N, Vec<N>>,
+    circuits: Vec<Vec<N>>,
+}
+
+impl<N, E> Johnson<N, E> {}
