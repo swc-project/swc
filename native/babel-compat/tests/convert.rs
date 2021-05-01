55aadee738850e1f497e83e15e4b716da33ae1d4
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-30 23:23:56 -0700

    Added a test for regex with flags

--- native/babel-compat/tests/convert.rs
+++ native/babel-compat/tests/convert.rs
@@ -104,8 +104,8 @@ fn fixtures() -> Result<(), Error> {
 
 #[test]
 fn single_fixture() -> Result<(), Error> {
-    let input_file = "tests/fixtures/regex-simple/input.js";
-    let output_file = "tests/fixtures/regex-simple/output.json";
+    let input_file = "tests/fixtures/regex-flags/input.js";
+    let output_file = "tests/fixtures/regex-flags/output.json";
 
     let input = fs::read_to_string(&input_file)
         .with_context(|| format!("Failed to open file: {}", &input_file))?;
