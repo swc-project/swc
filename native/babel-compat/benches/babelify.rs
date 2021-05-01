be46bde18d976d76644c635ece3cd5adf9014161
강동윤 <kdy1997.dev@gmail.com>
2021-05-01 18:49:01 +0900

    WIP: Benchmark for babelify

---
+++ native/babel-compat/benches/babelify.rs
@@ -0,0 +1,60 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::{hint::black_box, sync::Arc};
+use swc::config::{Config, JscConfig, Options, SourceMapsConfig};
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    FileName, FilePathMapping, SourceMap,
+};
+use swc_ecma_ast::Program;
+use swc_ecma_parser::{JscTarget, Syntax, TsConfig};
+use swc_ecma_transforms::{fixer, hygiene, resolver, typescript};
+use swc_ecma_visit::FoldWith;
+use test::Bencher;
+
+static SOURCE: &str = include_str!("assets/AjaxObservable.ts");
+
+fn mk() -> swc::Compiler {
+    let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+    let handler = Arc::new(Handler::with_tty_emitter(
+        ColorConfig::Always,
+        true,
+        false,
+        Some(cm.clone()),
+    ));
+
+    let c = swc::Compiler::new(cm.clone(), handler);
+
+    c
+}
+
+fn parse(c: &swc::Compiler) -> (Arc<SourceFile>, Program) {
+    let fm = c.cm.new_source_file(
+        FileName::Real("rxjs/src/internal/observable/dom/AjaxObservable.ts".into()),
+        SOURCE.to_string(),
+    );
+    c.parse_js(
+        fm,
+        JscTarget::Es5,
+        Syntax::Typescript(Default::default()),
+        true,
+        true,
+    )
+    .unwrap()
+}
+
+#[bench]
+fn babelify(b: &mut Bencher) {
+    let c = mk();
+    let module = parse(&c);
+
+    b.iter(|| {
+        let (fm, program) = module.clone();
+        let ctx = Context {};
+
+        let babel_ast = program.babelify();
+        black_box(babel_ast)
+    });
+}
