9a6ddd21b22355a817e4f183844e5f3df43912f8
Daniel Woznicki <daniel.woznicki@gmail.com>
2021-04-30 23:12:46 -0700

    Added a test for exponent op

--- native/babel-compat/tests/convert.rs
+++ native/babel-compat/tests/convert.rs
@@ -104,8 +104,8 @@ fn fixtures() -> Result<(), Error> {
 
 #[test]
 fn single_fixture() -> Result<(), Error> {
-    let input_file = "tests/fixtures/delete-operator/input.js";
-    let output_file = "tests/fixtures/delete-operator/output.json";
+    let input_file = "tests/fixtures/exponentiation-operator/input.js";
+    let output_file = "tests/fixtures/exponentiation-operator/output.json";
 
     let input = fs::read_to_string(&input_file)
         .with_context(|| format!("Failed to open file: {}", &input_file))?;
