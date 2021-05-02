058e18c1efdddd076b59b25bce0eb0e8c890947d
Fábio Santos <fabiosantosart@gmail.com>
2021-05-02 11:35:49 +0100

    fix sorting differences between Linux and Mac

--- ecmascript/minifier/scripts/sort.sh
+++ ecmascript/minifier/scripts/sort.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -eu
+
+# Fix sorting differences between Linux and Mac
+export LC_ALL=C
+
 cat tests/golden.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/golden_sorted.txt
 mv tests/golden_sorted.txt tests/golden.txt
 
