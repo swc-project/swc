152f4b287030b2ed89c224b65f5ea7905757d4ef
강동윤 <kdy1997.dev@gmail.com>
2021-05-03 20:18:56 +0900

    Add full benchmarks for babelify

--- native/babel-compat/benches/babelify.rs
+++ native/babel-compat/benches/babelify.rs
@@ -33,10 +33,10 @@ fn mk() -> swc::Compiler {
     c
 }
 
-fn parse(c: &swc::Compiler) -> (Arc<SourceFile>, Program) {
+fn parse(c: &swc::Compiler, src: &str) -> (Arc<SourceFile>, Program) {
     let fm = c.cm.new_source_file(
         FileName::Real("rxjs/src/internal/observable/dom/AjaxObservable.ts".into()),
-        SOURCE.to_string(),
+        src.to_string(),
     );
     let module = c
         .parse_js(
@@ -48,12 +48,6 @@ fn parse(c: &swc::Compiler) -> (Arc<SourceFile>, Program) {
         )
         .unwrap();
 
-    let module = c.run_transform(false, || {
-        module
-            .fold_with(&mut typescript::strip())
-            .fold_with(&mut es2020())
-    });
-
     (fm, module)
 }
 
@@ -62,12 +56,17 @@ fn babelify(b: &mut Bencher) {
     b.bytes = SOURCE.len() as _;
 
     let c = mk();
-    let module = parse(&c);
+    let (fm, module) = parse(&c, SOURCE);
+    let module = c.run_transform(false, || {
+        module
+            .fold_with(&mut typescript::strip())
+            .fold_with(&mut es2020())
+    });
 
     b.iter(|| {
-        let (fm, program) = module.clone();
+        let program = module.clone();
         let ctx = Context {
-            fm,
+            fm: fm.clone(),
             cm: c.cm.clone(),
             comments: Arc::new(c.comments().clone()),
         };
@@ -76,3 +75,66 @@ fn babelify(b: &mut Bencher) {
         black_box(babel_ast)
     });
 }
+
+fn parse_and_babelify(b: &mut Bencher, _name: &str, src: &str) {
+    b.bytes = src.len() as _;
+
+    let c = mk();
+
+    b.iter(|| {
+        let (fm, program) = parse(&c, src);
+        let ctx = Context {
+            fm,
+            cm: c.cm.clone(),
+            comments: Arc::new(c.comments().clone()),
+        };
+
+        let babel_ast = program.babelify(&ctx);
+        black_box(babel_ast);
+    });
+}
+
+/// https://esprima.org/test/compare.html
+macro_rules! src_to_babel_ast {
+    ($name:ident, $src:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            parse_and_babelify(b, stringify!($name), $src);
+        }
+    };
+}
+
+src_to_babel_ast!(
+    angular,
+    include_str!("../../../ecmascript/parser/benches/files/angular-1.2.5.js")
+);
+
+src_to_babel_ast!(
+    backbone,
+    include_str!("../../../ecmascript/parser/benches/files/backbone-1.1.0.js")
+);
+
+src_to_babel_ast!(
+    jquery,
+    include_str!("../../../ecmascript/parser/benches/files/jquery-1.9.1.js")
+);
+
+src_to_babel_ast!(
+    jquery_mobile,
+    include_str!("../../../ecmascript/parser/benches/files/jquery.mobile-1.4.2.js")
+);
+
+src_to_babel_ast!(
+    mootools,
+    include_str!("../../../ecmascript/parser/benches/files/mootools-1.4.5.js")
+);
+
+src_to_babel_ast!(
+    underscore,
+    include_str!("../../../ecmascript/parser/benches/files/underscore-1.5.2.js")
+);
+
+src_to_babel_ast!(
+    yui,
+    include_str!("../../../ecmascript/parser/benches/files/yui-3.12.0.js")
+);
