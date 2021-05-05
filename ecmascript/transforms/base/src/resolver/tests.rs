176c26c16fabae67dfc5b6afabc98707b36005d8
강동윤 <kdy1997.dev@gmail.com>
2021-05-06 00:56:08 +0900

    Add a test for the type checker

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2686,3 +2686,28 @@ to_ts!(
     type A = {}
     "#
 );
+
+to!(
+    ts_resolver_computed_property_names_11_es5,
+    r#"
+    // @target: es5
+    var s: string;
+    var n: number;
+    var a: any;
+    var v = {
+        get [s]() { return 0; },
+        set [n](v) { },
+        get [s + s]() { return 0; },
+        set [s + n](v) { },
+        get [+s]() { return 0; },
+        set [""](v) { },
+        get [0]() { return 0; },
+        set [a](v) { },
+        get [<any>true]() { return 0; },
+        set [`hello bye`](v) { },
+        get [`hello ${a} bye`]() { return 0; }
+    }
+    "#,
+    r#"
+    "#
+);
