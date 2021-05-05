578108449a951588f40cd0fcd504ac99403b6abf
Fábio Santos <fabiosantosart@gmail.com>
2021-05-05 21:47:29 +0100

    add a golden test

--- ecmascript/minifier/tests/terser/compress/properties/mangle_define_property_arg/output.js
+++ ecmascript/minifier/tests/terser/compress/properties/mangle_define_property_arg/output.js
@@ -1,7 +1,7 @@
 var some_prop = "propname";
 
 const object = {
-  o: some_prop,
+  a: some_prop,
 };
 
 const non_global_console = console;
@@ -9,6 +9,6 @@ const non_global_console = console;
 // .log gets preserved because of jsprops
 non_global_console.log(object);
 
-Object.defineProperty(object, "o", { value: 3 });
-Object.defineProperty(non_global_console, "l", { value: 3 });
-Object.defineProperty(console, "l", { value: 3 });
+Object.defineProperty(object, "a", { value: 3 });
+Object.defineProperty(non_global_console, "b", { value: 3 });
+Object.defineProperty(console, "b", { value: 3 });
