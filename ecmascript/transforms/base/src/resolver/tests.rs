6f5db243a2477a2a6418fc86d6a40f614110d5e3
강동윤 <kdy1997.dev@gmail.com>
2021-05-06 13:18:30 +0900

    Fix a human error

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2469,38 +2469,39 @@ to_ts!(
     }
     ",
     r#"
+    var s: string;
     var n: number;
-var a: any;
-var v = {
-    get [s] () {
-        return 0;
-    },
-    set [n] (v__2){
-    },
-    get [s + s] () {
-        return 0;
-    },
-    set [s + n] (v__3){
-    },
-    get [+s] () {
-        return 0;
-    },
-    set [""] (v__4){
-    },
-    get [0] () {
-        return 0;
-    },
-    set [a] (v__5){
-    },
-    get [<any>true] () {
-        return 0;
-    },
-    set [`hello bye`] (v__6){
-    },
-    get [`hello ${a} bye`] () {
-        return 0;
-    }
-};
+    var a: any;
+    var v = {
+        get [s] () {
+            return 0;
+        },
+        set [n] (v__2){
+        },
+        get [s + s] () {
+            return 0;
+        },
+        set [s + n] (v__3){
+        },
+        get [+s] () {
+            return 0;
+        },
+        set [""] (v__4){
+        },
+        get [0] () {
+            return 0;
+        },
+        set [a] (v__5){
+        },
+        get [<any>true] () {
+            return 0;
+        },
+        set [`hello bye`] (v__6){
+        },
+        get [`hello ${a} bye`] () {
+            return 0;
+        }
+    };
     "#
 );
 
