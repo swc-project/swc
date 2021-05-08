3fed15a81bf3a3de9d3755073930c6e7d70da235
강동윤 <kdy1997.dev@gmail.com>
2021-05-08 22:19:37 +0900

    Add Swcify.swcify

--- babel/compat/src/swcify/mod.rs
+++ babel/compat/src/swcify/mod.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::sync::Arc;
 use swc_common::FileName;
 use swc_common::SourceFile;
@@ -5,15 +6,27 @@ use swc_common::SourceMap;
 
 pub struct Context {
     pub(crate) cm: Arc<SourceMap>,
+    pub(crate) fm: Arc<SourceFile>,
 }
 
 impl Context {
     /// This accepts source string because the spans of an ast node of swc are
     /// stored as interned.
-    pub fn new(cm: Arc<SourceMap>, filename: &FileName, src: &str) -> Self {}
+    ///
+    /// This method allocate a new [SourceFile] in the given `cm`.
+    pub fn new(cm: Arc<SourceMap>, filename: FileName, src: String) -> Self {
+        let fm = cm.new_source_file(filename, src);
+        Self::new_without_alloc(cm, fm)
+    }
 
-    pub fn new_without_alloc(cm: Arc<SourceMap>, fm: Arc<SourceFile>) -> Self {}
+    pub fn new_without_alloc(cm: Arc<SourceMap>, fm: Arc<SourceFile>) -> Self {
+        Self { cm, fm }
+    }
 }
 
 /// Used to convert a babel ast node to
-pub trait Swcify {}
+pub trait Swcify {
+    type Output: Debug + Send + Sync;
+
+    fn swcify(self, ctx: &Context) -> Self::Output;
+}
