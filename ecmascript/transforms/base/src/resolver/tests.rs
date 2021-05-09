befc6beea39bab2aa97ca2825f759ba6b22766fe
강동윤 <kdy1997.dev@gmail.com>
2021-05-09 18:04:47 +0900

    Add a test

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2796,3 +2796,29 @@ to_ts!(
     }
     "
 );
+
+to_ts!(
+    ts_if_do_while_statements_01,
+    "
+    var f3 = <T, U extends T>(x: T, y: U) => {
+        function bar<V extends T, W extends U>() {
+            var g = <X extends W, Y extends V>(a: X, b: Y): T => {
+                x = y;
+                return y;
+            }
+        }
+    }
+
+    var f4 = <U extends T, T>(x: T, y: U) => {
+        function bar<V extends T, W extends U>() {
+            var g = <X extends W, Y extends V>(a: X, b: Y): T => {
+                x = y;
+                return y;
+            }
+        }
+    }
+    ",
+    "
+    
+    "
+);
