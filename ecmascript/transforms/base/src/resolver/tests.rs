741ce03abe24e7046c0c7e2812484e9ef5997516
강동윤 <kdy1997.dev@gmail.com>
2021-05-10 01:11:02 +0900

    Add a test for #1653

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2837,3 +2837,27 @@ to_ts!(
     };
     "
 );
+
+to_ts!(
+    issue_1653,
+    "
+    namespace X
+    {
+        export namespace Z
+        {
+            export const foo = 0
+        }
+    }
+
+    namespace Y
+    {
+        export namespace Z
+        {
+            export const bar = 1
+        }
+    }
+
+    ",
+    "
+    "
+);
