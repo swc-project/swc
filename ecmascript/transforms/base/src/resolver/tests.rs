5c96a63dbb7c1addc38924d3b2b20c0c8caf41ce
강동윤 <kdy1997.dev@gmail.com>
2021-05-10 01:36:39 +0900

    Update test refs

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2902,7 +2902,16 @@ fn issue_1653_2() {
             }
             ",
         "
-            
+            module X {
+                export module Z__2 {
+                    export const foo__3 = 0;
+                }
+            }
+            module Y {
+                export module Z__4 {
+                    export const bar__5 = 1;
+                }
+            }
             ",
     );
 }
