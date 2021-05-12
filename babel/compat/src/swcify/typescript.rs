6d8cd6ddc29f7a74562637bc57c8ae784f31f810
강동윤 <kdy1997.dev@gmail.com>
2021-05-12 21:03:27 +0900

    fixup

--- babel/compat/src/swcify/typescript.rs
+++ babel/compat/src/swcify/typescript.rs
@@ -1,11 +1,15 @@
+use super::Context;
 use crate::swcify::Swcify;
 use swc_babel_ast::FlowType;
+use swc_babel_ast::TSTypeAnnotation;
 use swc_babel_ast::TSTypeParameter;
 use swc_babel_ast::TSTypeParameterDeclaration;
+use swc_babel_ast::TypeAnnotOrNoop;
 use swc_babel_ast::TypeParamDeclOrNoop;
 use swc_babel_ast::TypeParameterInstantiation;
 use swc_ecma_ast::Ident;
 use swc_ecma_ast::TsType;
+use swc_ecma_ast::TsTypeAnn;
 use swc_ecma_ast::TsTypeParam;
 use swc_ecma_ast::TsTypeParamDecl;
 use swc_ecma_ast::TsTypeParamInstantiation;
@@ -13,7 +17,7 @@ use swc_ecma_ast::TsTypeParamInstantiation;
 impl Swcify for TypeParameterInstantiation {
     type Output = TsTypeParamInstantiation;
 
-    fn swcify(self, ctx: &super::Context) -> Self::Output {
+    fn swcify(self, ctx: &Context) -> Self::Output {
         TsTypeParamInstantiation {
             span: ctx.span(&self.base),
             params: self.params.swcify(ctx),
@@ -24,13 +28,13 @@ impl Swcify for TypeParameterInstantiation {
 impl Swcify for FlowType {
     type Output = Box<TsType>;
 
-    fn swcify(self, ctx: &super::Context) -> Self::Output {}
+    fn swcify(self, ctx: &Context) -> Self::Output {}
 }
 
 impl Swcify for TypeParamDeclOrNoop {
     type Output = Option<TsTypeParamDecl>;
 
-    fn swcify(self, ctx: &super::Context) -> Self::Output {
+    fn swcify(self, ctx: &Context) -> Self::Output {
         match self {
             TypeParamDeclOrNoop::Flow(_) => None,
             TypeParamDeclOrNoop::TS(v) => Some(v.swcify(ctx)),
@@ -42,7 +46,7 @@ impl Swcify for TypeParamDeclOrNoop {
 impl Swcify for TSTypeParameterDeclaration {
     type Output = TsTypeParamDecl;
 
-    fn swcify(self, ctx: &super::Context) -> Self::Output {
+    fn swcify(self, ctx: &Context) -> Self::Output {
         TsTypeParamDecl {
             span: ctx.span(&self.base),
             params: self.params.swcify(ctx),
@@ -53,13 +57,36 @@ impl Swcify for TSTypeParameterDeclaration {
 impl Swcify for TSTypeParameter {
     type Output = TsTypeParam;
 
-    fn swcify(self, ctx: &super::Context) -> Self::Output {
+    fn swcify(self, ctx: &Context) -> Self::Output {
         let span = ctx.span(&self.base);
         TsTypeParam {
             span,
             name: Ident::new(self.name, span),
             constraint: self.constraint.swcify(ctx),
             default: self.default.swcify(ctx),
+        }
+    }
+}
+
+impl Swcify for TypeAnnotOrNoop {
+    type Output = Option<TsTypeAnn>;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        match self {
+            TypeAnnotOrNoop::Flow(_) => None,
+            TypeAnnotOrNoop::TS(v) => Some(v.swcify(ctx)),
+            TypeAnnotOrNoop::Noop(_) => None,
+        }
+    }
+}
+
+impl Swcify for TSTypeAnnotation {
+    type Output = TsTypeAnn;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        TsTypeAnn {
+            span: ctx.span(&self.base),
+            type_ann: self.type_annotation.swcify(ctx),
         }
     }
 }
