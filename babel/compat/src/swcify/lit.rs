0b2807904dc10298b7da9db55a2cc64a500402c8
강동윤 <kdy1997.dev@gmail.com>
2021-05-12 13:02:20 +0900

    fixup

--- babel/compat/src/swcify/lit.rs
+++ babel/compat/src/swcify/lit.rs
@@ -1,16 +1,23 @@
 use super::Context;
 use crate::swcify::Swcify;
+use swc_babel_ast::BigIntLiteral;
 use swc_babel_ast::BooleanLiteral;
+use swc_babel_ast::DecimalLiteral;
 use swc_babel_ast::Literal;
 use swc_babel_ast::NullLiteral;
 use swc_babel_ast::NumberLiteral;
 use swc_babel_ast::NumericLiteral;
+use swc_babel_ast::RegExpLiteral;
 use swc_babel_ast::StringLiteral;
+use swc_babel_ast::TemplateLiteral;
+use swc_ecma_ast::BigInt;
 use swc_ecma_ast::Bool;
 use swc_ecma_ast::Lit;
 use swc_ecma_ast::Null;
 use swc_ecma_ast::Number;
+use swc_ecma_ast::Regex;
 use swc_ecma_ast::Str;
+use swc_ecma_ast::Tpl;
 
 impl Swcify for Literal {
     type Output = Lit;
@@ -22,7 +29,7 @@ impl Swcify for Literal {
             Literal::Null(v) => v.swcify(ctx).into(),
             Literal::Boolean(v) => v.swcify(ctx).into(),
             Literal::RegExp(v) => v.swcify(ctx).into(),
-            Literal::Template(v) => v.swcify(ctx).into(),
+            Literal::Template(v) => unreachable!(),
             Literal::BigInt(v) => v.swcify(ctx).into(),
             Literal::Decimal(v) => v.swcify(ctx).into(),
         }
@@ -81,6 +88,58 @@ impl Swcify for BooleanLiteral {
         Bool {
             span: ctx.span(&self.base),
             value: self.value,
+        }
+    }
+}
+
+impl Swcify for RegExpLiteral {
+    type Output = Regex;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Regex {
+            span: ctx.span(&self.base),
+            exp: self.pattern,
+            flags: self.flags,
+        }
+    }
+}
+
+impl Swcify for TemplateLiteral {
+    type Output = Tpl;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Tpl {
+            span: ctx.span(&self.base),
+            exprs: self.expressions.swcify(ctx),
+            quasis: self.quasis.swcify(ctx),
+        }
+    }
+}
+
+impl Swcify for BigIntLiteral {
+    type Output = BigInt;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        BigInt {
+            span: ctx.span(&self.base),
+            value: self
+                .value
+                .parse()
+                .expect("failed to parse the value of BigIntLiteral"),
+        }
+    }
+}
+
+impl Swcify for DecimalLiteral {
+    type Output = Number;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Number {
+            span: ctx.span(&self.base),
+            value: self
+                .value
+                .parse()
+                .expect("failed to parse the value of DecimalLiteral"),
         }
     }
 }
