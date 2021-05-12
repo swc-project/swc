f1e5d94d806d9583b2213ed7076d2492a91bad78
강동윤 <kdy1997.dev@gmail.com>
2021-05-12 12:56:43 +0900

    More work

--- babel/compat/src/swcify/lit.rs
+++ babel/compat/src/swcify/lit.rs
@@ -1,9 +1,14 @@
 use super::Context;
 use crate::swcify::Swcify;
+use swc_babel_ast::BooleanLiteral;
 use swc_babel_ast::Literal;
+use swc_babel_ast::NullLiteral;
 use swc_babel_ast::NumberLiteral;
+use swc_babel_ast::NumericLiteral;
 use swc_babel_ast::StringLiteral;
+use swc_ecma_ast::Bool;
 use swc_ecma_ast::Lit;
+use swc_ecma_ast::Null;
 use swc_ecma_ast::Number;
 use swc_ecma_ast::Str;
 
@@ -42,6 +47,38 @@ impl Swcify for NumberLiteral {
 
     fn swcify(self, ctx: &Context) -> Self::Output {
         Number {
+            span: ctx.span(&self.base),
+            value: self.value,
+        }
+    }
+}
+
+impl Swcify for NumericLiteral {
+    type Output = Number;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Number {
+            span: ctx.span(&self.base),
+            value: self.value,
+        }
+    }
+}
+
+impl Swcify for NullLiteral {
+    type Output = Null;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Null {
+            span: ctx.span(&self.base),
+        }
+    }
+}
+
+impl Swcify for BooleanLiteral {
+    type Output = Bool;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        Bool {
             span: ctx.span(&self.base),
             value: self.value,
         }
