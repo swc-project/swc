445dab712f84365014ff080422bc680b67416dbd
강동윤 <kdy1997.dev@gmail.com>
2021-05-13 18:02:46 +0900

    fixup

--- ecmascript/parser/scripts/copy-ts-tests.ts
+++ ecmascript/parser/scripts/copy-ts-tests.ts
@@ -73,11 +73,11 @@ async function check(f: string) {
 
 
     if (res) {
-        console.log('Creating', f, '->', passTestDir)
+        console.log('Creating', f, '->', passTestFile)
         await fs.promises.mkdir(passTestDir, { recursive: true })
         await fs.promises.rename(f, passTestFile);
     } else {
-        console.log('Creating', f, '->', errorTestDir)
+        console.log('Creating', f, '->', errorTestFile)
         await fs.promises.mkdir(errorTestDir, { recursive: true })
         await fs.promises.rename(f, errorTestFile);
     }
