6fe094f3536bdb98647c4307d8949a1c24a3d9e5
강동윤 <kdy1997.dev@gmail.com>
2021-05-15 12:03:51 +0900

    Add a test

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2929,3 +2929,33 @@ to_ts!(
     }
     "
 );
+
+to_ts!(
+    ts_local_types_4,
+    "
+    function f1() {
+        // Type parameters are in scope in parameters and return types
+        function f<T>(x: T): T {
+            return undefined;
+        }
+    }
+    
+    function f2() {
+        // Local types are not in scope in parameters and return types
+        function f(x: T): T {
+            interface T { }
+            return undefined;
+        }
+    }
+    
+    function f3() {
+        // Type parameters and top-level local types are in same declaration space
+        function f<T>() {
+            interface T { }
+            return undefined;
+        }
+    }
+    ",
+    "
+    "
+);
