d8b4429d5566d101510e55bfe2da612eb5e26574
강동윤 <kdy1997.dev@gmail.com>
2021-05-15 14:38:45 +0900

    Update test refs

--- ecmascript/transforms/base/src/resolver/tests.rs
+++ ecmascript/transforms/base/src/resolver/tests.rs
@@ -2957,5 +2957,24 @@ to_ts!(
     }
     ",
     "
+    function f1() {
+        function f__2<T__3>(x__3: T__3): T__3 {
+            return undefined;
+        }
+    }
+    function f2() {
+        function f__4(x__5: T__5): T__5 {
+            interface T__5 {
+            }
+            return undefined;
+        }
+    }
+    function f3() {
+        function f__6<T__7>() {
+            interface T__7 {
+            }
+            return undefined;
+        }
+    }
     "
 );
