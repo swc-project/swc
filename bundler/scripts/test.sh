571360db8aa84268b2f39f56f5760aa19b3d0898
강동윤 <kdy1997.dev@gmail.com>
2021-05-20 00:57:54 +0900

    Use master

--- bundler/scripts/test.sh
+++ bundler/scripts/test.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-if [ -z "$@" ]; then
-    cargo test --lib
-    cargo test --test fixture
-    (cd ../spack && cargo test --test fixture)
-fi
-
+cargo test --lib
+cargo test --test fixture
+(cd ../spack && cargo test --test fixture)
 cargo test --test deno $@ -- --nocapture
