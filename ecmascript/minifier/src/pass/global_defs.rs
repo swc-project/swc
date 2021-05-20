183baf5d0245af83a0fecf77055e4c47edc4e307
강동윤 <kdy1997.dev@gmail.com>
2021-05-20 16:12:29 +0900

    Improve global_defs

--- ecmascript/minifier/src/pass/global_defs.rs
+++ ecmascript/minifier/src/pass/global_defs.rs
@@ -7,11 +7,16 @@ use swc_ecma_visit::VisitMut;
 use swc_ecma_visit::VisitMutWith;
 
 pub fn globals_defs(defs: Vec<(Box<Expr>, Lit)>) -> impl VisitMut {
-    GlobalDefs { defs }
+    GlobalDefs {
+        defs,
+        ..Default::default()
+    }
 }
 
+#[derive(Default)]
 struct GlobalDefs {
     defs: Vec<(Box<Expr>, Lit)>,
+    in_lhs_of_assign: bool,
 }
 
 impl CompilerPass for GlobalDefs {
@@ -24,7 +29,20 @@ impl CompilerPass for GlobalDefs {
 impl VisitMut for GlobalDefs {
     noop_visit_mut_type!();
 
+    fn visit_mut_assign_expr(&mut self, n: &mut AssignExpr) {
+        let old = self.in_lhs_of_assign;
+        self.in_lhs_of_assign = true;
+        n.left.visit_mut_with(self);
+        self.in_lhs_of_assign = false;
+        n.right.visit_mut_with(self);
+        self.in_lhs_of_assign = old;
+    }
+
     fn visit_mut_expr(&mut self, n: &mut Expr) {
+        if self.in_lhs_of_assign {
+            return;
+        }
+
         if let Some((_, new)) = self
             .defs
             .iter()
@@ -36,4 +54,8 @@ impl VisitMut for GlobalDefs {
 
         n.visit_mut_children_with(self);
     }
+
+    /// Noop.
+    #[inline]
+    fn visit_mut_update_expr(&mut self, _: &mut UpdateExpr) {}
 }
