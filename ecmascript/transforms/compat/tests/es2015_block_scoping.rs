2e8996219565ba86c2985ad7305fc74a71bf5664
강동윤 <kdy1997.dev@gmail.com>
2021-05-21 23:03:55 +0900

    Add a test for #1462

--- ecmascript/transforms/compat/tests/es2015_block_scoping.rs
+++ ecmascript/transforms/compat/tests/es2015_block_scoping.rs
@@ -837,3 +837,31 @@ test!(
         }
         "
 );
+
+test!(
+    ::swc_ecma_parser::Syntax::default(),
+    |_| block_scoping(),
+    issue_1462_1,
+    "
+    export default function _objectSpread(target) {
+        for (var i = 1; i < arguments.length; i++) {
+            var source = arguments[i] != null ? arguments[i] : {};
+            var ownKeys = Object.keys(source);
+        
+            if (typeof Object.getOwnPropertySymbols === 'function') {
+            ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+                return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+            }));
+            }
+        
+            ownKeys.forEach(function (key) {
+            defineProperty(target, key, source[key]);
+            });
+        }
+        
+        return target;
+    }
+    ",
+    "
+    "
+);
