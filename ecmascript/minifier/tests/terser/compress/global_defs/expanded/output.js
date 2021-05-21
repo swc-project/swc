36db21862c9980cbbe4befaac55fdf2d4f0b8640
강동윤 <kdy1997.dev@gmail.com>
2021-05-21 15:18:03 +0900

    Update test refs

--- ecmascript/minifier/tests/terser/compress/global_defs/expanded/output.js
+++ ecmascript/minifier/tests/terser/compress/global_defs/expanded/output.js
@@ -2,10 +2,12 @@ function f(CONFIG) {
     return CONFIG.VALUE;
 }
 function g() {
-    var CONFIG = { VALUE: 1 };
+    var CONFIG = {
+        VALUE: 1
+    };
     return CONFIG.VALUE;
 }
 function h() {
     return 42;
 }
-if ([0][0]) console.debug("foo");
+if (0) console.debug("foo");
