5332a628e7f3c6546390fb5e61052957082ff757
강동윤 <kdy1997.dev@gmail.com>
2021-05-21 14:48:36 +0900

    WIP: Property inliner

--- ecmascript/minifier/src/compress/optimize/properties.rs
+++ ecmascript/minifier/src/compress/optimize/properties.rs
@@ -2,12 +2,60 @@ use crate::compress::optimize::Optimizer;
 use swc_atoms::js_word;
 use swc_common::SyntaxContext;
 use swc_ecma_ast::*;
+use swc_ecma_utils::prop_name_eq;
 
 impl Optimizer<'_> {
     /// Converts `{ a: 1 }.a` into `1`.
     pub(super) fn handle_property_access(&mut self, e: &mut Expr) {
         if !self.options.props {
             return;
+        }
+
+        let me = match e {
+            Expr::Member(m) => m,
+            _ => return,
+        };
+        if me.computed {
+            return;
+        }
+
+        let key = match &*me.prop {
+            Expr::Ident(prop) => prop,
+            _ => return,
+        };
+
+        let obj = match &mut me.obj {
+            ExprOrSuper::Expr(e) => &mut **e,
+            _ => return,
+        };
+
+        let obj = match obj {
+            Expr::Object(o) => o,
+            _ => return,
+        };
+
+        for prop in &obj.props {
+            match prop {
+                PropOrSpread::Spread(_) => {}
+                PropOrSpread::Prop(p) => match &**p {
+                    Prop::Shorthand(_) => {}
+                    Prop::KeyValue(p) => {
+                        if prop_name_eq(&p.key, &key.sym) {
+                            match &*p.value {
+                                Expr::Lit(..) => {}
+                                _ => return,
+                            }
+                            self.changed = true;
+                            *e = *p.value.clone();
+                            return;
+                        }
+                    }
+                    Prop::Assign(_) => {}
+                    Prop::Getter(_) => {}
+                    Prop::Setter(_) => {}
+                    Prop::Method(_) => {}
+                },
+            }
         }
     }
 
