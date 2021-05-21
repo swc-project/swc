573bedf64b041f9b5ca6777ed28d43de3d156374
강동윤 <kdy1997.dev@gmail.com>
2021-05-21 15:05:39 +0900

    properties: More check

--- ecmascript/minifier/src/compress/optimize/properties.rs
+++ ecmascript/minifier/src/compress/optimize/properties.rs
@@ -3,11 +3,16 @@ use swc_atoms::js_word;
 use swc_common::SyntaxContext;
 use swc_ecma_ast::*;
 use swc_ecma_utils::prop_name_eq;
+use swc_ecma_utils::ExprExt;
 
 impl Optimizer<'_> {
     /// Converts `{ a: 1 }.a` into `1`.
     pub(super) fn handle_property_access(&mut self, e: &mut Expr) {
         if !self.options.props {
+            return;
+        }
+
+        if self.ctx.is_update_arg {
             return;
         }
 
@@ -34,6 +39,40 @@ impl Optimizer<'_> {
             _ => return,
         };
 
+        let duplicate_prop = obj
+            .props
+            .iter()
+            .filter(|prop| match prop {
+                PropOrSpread::Spread(_) => false,
+                PropOrSpread::Prop(p) => match &**p {
+                    Prop::Shorthand(p) => p.sym == key.sym,
+                    Prop::KeyValue(p) => prop_name_eq(&p.key, &key.sym),
+                    Prop::Assign(p) => p.key.sym == key.sym,
+                    Prop::Getter(p) => prop_name_eq(&p.key, &key.sym),
+                    Prop::Setter(p) => prop_name_eq(&p.key, &key.sym),
+                    Prop::Method(p) => prop_name_eq(&p.key, &key.sym),
+                },
+            })
+            .count()
+            != 1;
+        if duplicate_prop {
+            return;
+        }
+
+        if obj.props.iter().any(|prop| match prop {
+            PropOrSpread::Spread(_) => false,
+            PropOrSpread::Prop(p) => match &**p {
+                Prop::Shorthand(..) => false,
+                Prop::KeyValue(p) => p.key.is_computed() || p.value.may_have_side_effects(),
+                Prop::Assign(p) => p.value.may_have_side_effects(),
+                Prop::Getter(p) => p.key.is_computed(),
+                Prop::Setter(p) => p.key.is_computed(),
+                Prop::Method(p) => p.key.is_computed(),
+            },
+        }) {
+            return;
+        }
+
         for prop in &obj.props {
             match prop {
                 PropOrSpread::Spread(_) => {}
@@ -41,10 +80,7 @@ impl Optimizer<'_> {
                     Prop::Shorthand(_) => {}
                     Prop::KeyValue(p) => {
                         if prop_name_eq(&p.key, &key.sym) {
-                            match &*p.value {
-                                Expr::Lit(..) => {}
-                                _ => return,
-                            }
+                            log::trace!("properties: Inlining a key-value property `{}`", key.sym);
                             self.changed = true;
                             *e = *p.value.clone();
                             return;
