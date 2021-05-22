a1c6feb3db1e7301d26ebfffdc79fdfb2132bc1a
강동윤 <kdy1997.dev@gmail.com>
2021-05-23 00:20:17 +0900

    Lift some `-`s.

--- ecmascript/minifier/src/compress/optimize/numbers.rs
+++ ecmascript/minifier/src/compress/optimize/numbers.rs
@@ -1,5 +1,6 @@
 use crate::compress::optimize::Optimizer;
 use swc_ecma_ast::*;
+use swc_ecma_transforms_base::ext::MapWithMut;
 
 impl Optimizer<'_> {
     pub(super) fn optimize_expr_in_num_ctx(&mut self, e: &mut Expr) {
@@ -16,7 +17,45 @@ impl Optimizer<'_> {
         }
     }
 
-    /// 
+    ///
     /// - `1 / -0` => `- 1 / 0`
-    pub(super) fn lift_minus(&mut self, e: &mut Expr) {}
+    pub(super) fn lift_minus(&mut self, e: &mut Expr) {
+        if !self.options.evaluate {
+            return;
+        }
+
+        match e {
+            Expr::Unary(unary) => match &mut *unary.arg {
+                Expr::Bin(arg) => match &mut *arg.right {
+                    Expr::Unary(UnaryExpr {
+                        op: op!(unary, "-"),
+                        arg: right_arg,
+                        ..
+                    }) => {
+                        if arg.op != op!("*") && arg.op != op!("/") {
+                            return;
+                        }
+
+                        self.changed = true;
+                        log::trace!("numbers: Lifting `-`");
+
+                        *e = Expr::Unary(UnaryExpr {
+                            span: unary.span,
+                            op: op!(unary, "-"),
+                            arg: Box::new(Expr::Bin(BinExpr {
+                                span: arg.span,
+                                op: arg.op,
+                                left: arg.left.take(),
+                                right: right_arg.take(),
+                            })),
+                        });
+                        return;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
+        }
+    }
 }
