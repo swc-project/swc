db06623c098be43d9011ea28aa8d59b3f22917ec
강동윤 <kdy1997.dev@gmail.com>
2021-05-22 23:53:57 +0900

    Convert string literals into numbers

--- ecmascript/minifier/src/compress/optimize/numbers.rs
+++ ecmascript/minifier/src/compress/optimize/numbers.rs
@@ -2,5 +2,17 @@ use crate::compress::optimize::Optimizer;
 use swc_ecma_ast::*;
 
 impl Optimizer<'_> {
-    pub(super) fn optimize_expr_in_num_ctx(&mut self, e: &mut Expr) {}
+    pub(super) fn optimize_expr_in_num_ctx(&mut self, e: &mut Expr) {
+        match e {
+            Expr::Lit(Lit::Str(Str { span, value, .. })) => {
+                let value = if value.is_empty() { 0.0 } else { 1.0 };
+
+                self.changed = true;
+                log::trace!("numbers: Converting a string literal to {:?}", value);
+                *e = Expr::Lit(Lit::Num(Number { span: *span, value }));
+                return;
+            }
+            _ => {}
+        }
+    }
 }
