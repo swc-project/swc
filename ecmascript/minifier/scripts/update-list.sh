f7ff62c292cfe5c7a4ad623b538dba9f67366721
강동윤 <kdy1997.dev@gmail.com>
2021-05-22 13:57:23 +0900

    Add a script to auto-update lists

---
+++ ecmascript/minifier/scripts/update-list.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# This script updates golden.txt and ignored.txt
+# 
+#
+# Note that even if a test is ignored, this script will invoke it.
+#
+set -eu
+
+
+# Clean the ignore list
+echo '' > tests/ignored.txt
+
+scripts/add-golden.sh
+scripts/ignore.sh ''
