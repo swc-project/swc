d4fe19212b72e5bdfa695358d57fbb7b4f8c9c00
강동윤 <kdy1997.dev@gmail.com>
2021-05-22 21:25:15 +0900

    rust.sh: Log

--- ecmascript/minifier/scripts/run.sh
+++ ecmascript/minifier/scripts/run.sh
@@ -11,10 +11,10 @@
 #
 set -eu
 
+export RUST_LOG=swc_ecma_minifier=trace
+
 if [ -z "$@" ]; then
     ./scripts/sort.sh
-
-    export RUST_LOG=swc_ecma_minifier=trace
 
     GOLDEN_ONLY=1 cargo test --test compress --all-features 
 fi
