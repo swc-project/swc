edbd6fbfed7e068e60de9219d82b19c2baf356e4
muji <muji@tmpfs.org>
2021-05-26 15:37:05 +0800

    Update is_core_module() with node@16 builtinModules.

--- spack/src/resolvers/mod.rs
+++ spack/src/resolvers/mod.rs
@@ -18,13 +18,31 @@ use std::{
 use swc_bundler::Resolve;
 use swc_common::FileName;
 
+// Run `node -p "require('module').builtinModules"`
 pub(crate) fn is_core_module(s: &str) -> bool {
     match s {
-        "assert" | "buffer" | "child_process" | "console" | "cluster" | "crypto" | "dgram"
-        | "dns" | "events" | "fs" | "http" | "http2" | "https" | "net" | "os" | "path"
-        | "perf_hooks" | "process" | "querystring" | "readline" | "repl" | "stream"
-        | "string_decoder" | "timers" | "tls" | "tty" | "url" | "util" | "v8" | "vm" | "wasi"
-        | "worker" | "zlib" => true,
+        "_http_agent" |          "_http_client" |         "_http_common" |
+        "_http_incoming" |       "_http_outgoing" |       "_http_server" |
+        "_stream_duplex" |       "_stream_passthrough" |  "_stream_readable" |
+        "_stream_transform" |    "_stream_wrap" |         "_stream_writable" |
+        "_tls_common" |          "_tls_wrap" |            "assert" |
+        "assert/strict" |        "async_hooks" |          "buffer" |
+        "child_process" |        "cluster" |              "console" |
+        "constants" |            "crypto" |               "dgram" |
+        "diagnostics_channel" |  "dns" |                  "dns/promises" |
+        "domain" |               "events" |               "fs" |
+        "fs/promises" |          "http" |                 "http2" |
+        "https" |                "inspector" |            "module" |
+        "net" |                  "os" |                   "path" |
+        "path/posix" |           "path/win32" |           "perf_hooks" |
+        "process" |              "punycode" |             "querystring" |
+        "readline" |             "repl" |                 "stream" |
+        "stream/promises" |      "string_decoder" |       "sys" |
+        "timers" |               "timers/promises" |      "tls" |
+        "trace_events" |         "tty" |                  "url" |
+        "util" |                 "util/types" |           "v8" |
+        "vm" |                   "wasi" |                 "worker_threads" |
+        "zlib" => true,
         _ => false,
     }
 }
