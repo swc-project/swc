4fafcbecc28fe1700068a77d484b4a6a0edd0ec3
강동윤 <kdy1997.dev@gmail.com>
2021-05-27 13:40:56 +0900

    Add output

--- ecmascript/minifier/tests/compress/exec/terser/template_string/evaluate_nested_templates/input.js
+++ ecmascript/minifier/tests/compress/exec/terser/template_string/evaluate_nested_templates/input.js
@@ -3,3 +3,7 @@
 var foo = `${`${`${`foo`}`}`}`;
 var bar = `before ${`innerBefore ${any} innerAfter`} after`;
 var baz = `1 ${2 + `3 ${any} 4` + 5} 6`;
+
+console.log(foo);
+console.log(bar);
+console.log(baz);
