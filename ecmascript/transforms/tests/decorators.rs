168f410dfec04f9e40c4738063c975918232291e
강동윤 <kdy1997.dev@gmail.com>
2021-05-30 19:37:39 +0900

    Update test refs

--- ecmascript/transforms/tests/decorators.rs
+++ ecmascript/transforms/tests/decorators.rs
@@ -5757,6 +5757,50 @@ test!(
     }
     ",
     "
-  
+    let SomeClass = _decorate([], function(_initialize) {
+          class SomeClass {
+              constructor(){
+                  _initialize(this);
+              }
+          }
+          return {
+              F: SomeClass,
+              d: [
+                  {
+                      kind: 'method',
+                      decorators: [
+                          dec
+                      ],
+                      key: 'someMethod',
+                      value: function someMethod() {
+                      }
+                  }
+              ]
+          };
+      });
+      let OtherClass = _decorate([], function(_initialize, _SomeClass) {
+          class OtherClass extends _SomeClass {
+              constructor(...args){
+                  super(...args);
+                  _initialize(this);
+              }
+          }
+          return {
+              F: OtherClass,
+              d: [
+                  {
+                      kind: 'method',
+                      decorators: [
+                          dec
+                      ],
+                      key: 'anotherMethod',
+                      value: function anotherMethod() {
+                          _get(_getPrototypeOf(OtherClass.prototype), 'someMethod', \
+     this).call(this);
+                      }
+                  }
+              ]
+          };
+      }, SomeClass);
     "
 );
