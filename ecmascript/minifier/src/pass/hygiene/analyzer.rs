9830dc57609659cdbee0f5bec0d58c0b59274a1f
강동윤 <kdy1997.dev@gmail.com>
2021-05-30 16:16:17 +0900

    Thinking...

--- ecmascript/minifier/src/pass/hygiene/analyzer.rs
+++ ecmascript/minifier/src/pass/hygiene/analyzer.rs
@@ -1,8 +1,9 @@
 use fxhash::{FxHashMap, FxHashSet};
+use std::mem::take;
 use swc_atoms::JsWord;
 use swc_ecma_ast::*;
 use swc_ecma_utils::Id;
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut};
+use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith, VisitWith};
 
 #[derive(Default)]
 pub(super) struct HygieneAnalyzer {
@@ -14,10 +15,38 @@ struct ScopeInfo {
     used: FxHashSet<Id>,
 }
 
+impl HygieneAnalyzer {
+    fn with_scope<F, R>(&mut self, op: F) -> R
+    where
+        F: FnOnce(&mut HygieneAnalyzer) -> R,
+    {
+        let parent = take(&mut self.cur);
+        let ret = op(self);
+        let child = take(&mut self.cur);
+
+        self.cur = parent;
+
+        // We remove hygiene info if child scope uses only one id with a respect to
+        // symbol.
+
+        //
+
+        // Done
+
+        self.cur.used.extend(child.used);
+
+        ret
+    }
+}
+
 impl VisitMut for HygieneAnalyzer {
     noop_visit_mut_type!();
 
     fn visit_mut_function(&mut self, n: &mut Function) {
         // We add
+
+        self.with_scope(|a| {
+            n.visit_mut_children_with(a);
+        });
     }
 }
