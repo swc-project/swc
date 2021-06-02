9c78dcf28326de505129942e14ed35fb5e11e926
강동윤 <kdy1997.dev@gmail.com>
2021-06-02 16:54:40 +0900

    Add a test

---
+++ ecmascript/transforms/base/tests/ts-resolver/minifier/1/output.ts
@@ -0,0 +1,11 @@
+function foo() {
+    try {
+        foo();
+    } catch (ex__2) {
+        let x__2 = 10;
+        const y__2 = 10;
+        class Zee__2 {
+        }
+    }
+    console.log(x, y, Zee);
+}
