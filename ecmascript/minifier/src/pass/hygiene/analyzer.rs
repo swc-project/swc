cbe5f0194f3318658fc8d24908ff9bffb8153566
강동윤 <kdy1997.dev@gmail.com>
2021-06-02 11:01:44 +0900

    Experiments

--- ecmascript/minifier/src/pass/hygiene/analyzer.rs
+++ ecmascript/minifier/src/pass/hygiene/analyzer.rs
@@ -1,6 +1,7 @@
 use fxhash::{FxHashMap, FxHashSet};
 use std::mem::take;
-use swc_common::SyntaxContext;
+use swc_atoms::JsWord;
+use swc_common::{Mark, SyntaxContext};
 use swc_ecma_ast::*;
 use swc_ecma_utils::Id;
 use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
@@ -8,15 +9,32 @@ use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 #[derive(Default)]
 pub(super) struct HygieneAnalyzer {
     cur: ScopeInfo,
+    top_level_mark: Mark,
     result: FxHashMap<Id, SyntaxContext>,
 }
 
 #[derive(Default)]
 struct ScopeInfo {
-    used: FxHashSet<Id>,
+    /// Symbols used within a scope.
+    symbols: FxHashMap<JsWord, Vec<SyntaxContext>>,
 }
 
 impl HygieneAnalyzer {
+    pub fn new(top_level_mark: Mark) -> Self {
+        Self {
+            top_level_mark,
+            ..Default::default()
+        }
+    }
+
+    fn report_usage(&mut self, i: &Ident) {
+        let v = self.cur.symbols.entry(i.sym.clone()).or_default();
+        if !v.contains(&i.span.ctxt) {
+            v.push(i.span.ctxt);
+        }
+    }
+
+    /// We search for duplicated symbols.
     fn with_scope<F, R>(&mut self, op: F) -> R
     where
         F: FnOnce(&mut HygieneAnalyzer) -> R,
@@ -34,14 +52,16 @@ impl HygieneAnalyzer {
 
         // Done
 
-        self.cur.used.extend(child.used);
-
         ret
     }
 }
 
 impl VisitMut for HygieneAnalyzer {
     noop_visit_mut_type!();
+
+    fn visit_mut_expr(&mut self, n: &mut Expr) {
+        n.visit_mut_children_with(self);
+    }
 
     fn visit_mut_block_stmt(&mut self, n: &mut BlockStmt) {
         self.with_scope(|a| {
