9048b01c9ac993a07f88aacc9cab1b9caa298010
강동윤 <kdy1997.dev@gmail.com>
2021-06-02 12:00:04 +0900

    Remove dbg

--- ecmascript/minifier/src/pass/hygiene/mod.rs
+++ ecmascript/minifier/src/pass/hygiene/mod.rs
@@ -84,9 +84,6 @@ impl VisitMut for Optimizer {
             None => return,
         };
 
-        dbg!(&self.scope().declared_symbols);
-        dbg!(&self.data);
-
         // If multiple variables with same name is declared, skip it.
         if let Some(decls) = self.scope().declared_symbols.get(&i.sym) {
             if decls.len() >= 2 {
