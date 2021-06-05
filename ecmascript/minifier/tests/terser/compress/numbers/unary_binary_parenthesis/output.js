a9cc7c819683b8a02284c5e356bf9e7b05d4284e
강동윤 <kdy1997.dev@gmail.com>
2021-06-05 21:40:58 +0900

    Update test refs

--- ecmascript/minifier/tests/terser/compress/numbers/unary_binary_parenthesis/output.js
+++ ecmascript/minifier/tests/terser/compress/numbers/unary_binary_parenthesis/output.js
@@ -13,6 +13,6 @@ var v = [
 ];
 v.forEach(function(x) {
     v.forEach(function(y) {
-        console.log(+(x * y), +x / y, +x % y, -(x * y), -x / y, -x % y);
+        console.log(+(x * y), +(x / y), +(x % y), -(x * y), -(x / y), -(x % y));
     });
 });
