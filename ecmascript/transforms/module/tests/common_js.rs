3839cfe191d883ee2d196eb989a42e9c019396c6
강동윤 <kdy1997.dev@gmail.com>
2021-06-06 21:35:28 +0900

    Update test refs

--- ecmascript/transforms/module/tests/common_js.rs
+++ ecmascript/transforms/module/tests/common_js.rs
@@ -4777,5 +4777,8 @@ test!(
     console.log('aFunc: ', aFunc(1,2));
     ",
     "
+    'use strict';
+    var _testlibrary = require('testlibrary');
+    console.log('aFunc: ', (0, _testlibrary).aFunc(1, 2));
     "
 );
