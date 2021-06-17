151215c6bb386d8369b2eb2372960874e7e8f90c
Andrew Bradley <cspotcode@gmail.com>
2021-06-17 10:06:30 -0400

    Add test

--- ecmascript/transforms/module/tests/common_js.rs
+++ ecmascript/transforms/module/tests/common_js.rs
@@ -4723,3 +4723,50 @@ test!(
     })();
     "
 );
+
+test!(
+    syntax(),
+    |_| tr(Default::default()),
+    issue_1786_1,
+    "
+    import Foo from 'foo';
+    export {Foo} from 'foo';
+    ",
+    "
+    'use strict';
+    Object.defineProperty(exports, '__esModule', {
+        value: true
+    });
+    var _foo = _interopRequireWildcard(require('foo'));
+    function _interopRequireWildcard(obj) {
+        if (obj && obj.__esModule) {
+            return obj;
+        } else {
+            var newObj = {
+            };
+            if (obj != null) {
+                for(var key in obj){
+                    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                        var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? \
+     Object.getOwnPropertyDescriptor(obj, key) : {
+                        };
+                        if (desc.get || desc.set) {
+                            Object.defineProperty(newObj, key, desc);
+                        } else {
+                            newObj[key] = obj[key];
+                        }
+                    }
+                }
+            }
+            newObj.default = obj;
+            return newObj;
+        }
+    }
+    Object.defineProperty(exports, 'Foo', {
+        enumerable: true,
+        get: function() {
+            return _foo.Foo;
+        }
+    });
+    "
+);
