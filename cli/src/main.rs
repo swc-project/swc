23a1ceb5b12641e5e6660a35f38547e67aa71d79
강동윤 <kdy1997.dev@gmail.com>
2021-06-23 01:27:39 +0900

    Add a cli to profile binary size

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,25 @@
+use std::{env::args, os, path::Path, sync::Arc};
+use swc::Compiler;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    SourceMap,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let cm = Arc::new(SourceMap::default());
+    let handler = Arc::new(Handler::with_tty_emitter(
+        ColorConfig::Always,
+        true,
+        false,
+        Some(cm.clone()),
+    ));
+    let compiler = Compiler::new(cm.clone(), handler);
+
+    let path = args().nth(1).unwrap();
+    let fm = cm.load_file(Path::new(&path)).expect("failed to load file");
+    let output = compiler
+        .process_js_file(fm, &Default::default())
+        .expect("failed to process");
+
+    println!("{}", output.code);
 }
