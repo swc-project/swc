2180cb501e120754900a42f39fbced80a101e83f
강동윤 <kdy1997.dev@gmail.com>
2021-06-26 12:22:02 +0900

    fixup

--- ecmascript/transforms/compat/tests/es2021_logcal_assignments.rs
+++ ecmascript/transforms/compat/tests/es2021_logcal_assignments.rs
@@ -1,10 +1,10 @@
 use swc_ecma_parser::{Syntax, TsConfig};
-use swc_ecma_transforms_compat::es2020;
+use swc_ecma_transforms_compat::es2021;
 use swc_ecma_transforms_testing::test;
 use swc_ecma_visit::Fold;
 
 fn tr() -> impl Fold {
-    es2020::operators()
+    es2021::logical_assignments()
 }
 
 fn syntax() -> Syntax {
