3a0d5c6655f60c60a5f1e51a85769c38acfa5f1a
강동윤 <kdy1997.dev@gmail.com>
2021-06-27 21:58:59 +0900

    More work

--- babel/compat/src/swcify/pat.rs
+++ babel/compat/src/swcify/pat.rs
@@ -5,7 +5,10 @@ use swc_babel_ast::AssignmentPattern;
 use swc_babel_ast::AssignmentPatternLeft;
 use swc_babel_ast::LVal;
 use swc_babel_ast::ObjectPattern;
+use swc_babel_ast::ObjectPatternProp;
+use swc_babel_ast::PatternLike;
 use swc_babel_ast::RestElement;
+use swc_common::Spanned;
 use swc_ecma_ast::*;
 
 impl Swcify for LVal {
@@ -45,7 +48,7 @@ impl Swcify for AssignmentPattern {
     fn swcify(self, ctx: &Context) -> Self::Output {
         AssignPat {
             span: ctx.span(&self.base),
-            left: self.left.swcify(ctx),
+            left: Box::new(self.left.swcify(ctx)),
             right: self.right.swcify(ctx),
             type_ann: None,
         }
@@ -68,17 +71,109 @@ impl Swcify for AssignmentPatternLeft {
 impl Swcify for ArrayPattern {
     type Output = ArrayPat;
 
-    fn swcify(self, ctx: &Context) -> Self::Output {}
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        ArrayPat {
+            span: ctx.span(&self.base),
+            elems: self.elements.swcify(ctx),
+            optional: false,
+            type_ann: None,
+        }
+    }
+}
+
+impl Swcify for PatternLike {
+    type Output = Pat;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        match self {
+            PatternLike::Id(v) => Pat::from(v.swcify(ctx)),
+            PatternLike::RestEl(v) => Pat::from(v.swcify(ctx)),
+            PatternLike::AssignmentPat(v) => Pat::from(v.swcify(ctx)),
+            PatternLike::ArrayPat(v) => Pat::from(v.swcify(ctx)),
+            PatternLike::ObjectPat(v) => Pat::from(v.swcify(ctx)),
+        }
+    }
 }
 
 impl Swcify for ObjectPattern {
     type Output = ObjectPat;
 
-    fn swcify(self, ctx: &Context) -> Self::Output {}
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        ObjectPat {
+            span: ctx.span(&self.base),
+            props: self.properties.swcify(ctx),
+            optional: false,
+            type_ann: None,
+        }
+    }
+}
+
+impl Swcify for ObjectPatternProp {
+    type Output = ObjectPatProp;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        match self {
+            ObjectPatternProp::Rest(v) => ObjectPatProp::Rest(v.swcify(ctx)),
+            ObjectPatternProp::Prop(prop) => {
+                if prop.shorthand {
+                    return ObjectPatProp::Assign(AssignPatProp {
+                        span: ctx.span(&prop.base),
+                        key: prop.key.swcify(ctx).expect_ident(),
+                        value: None,
+                    });
+                }
+
+                match prop.value {
+                    swc_babel_ast::ObjectPropVal::Pattern(v) => {
+                        ObjectPatProp::KeyValue(KeyValuePatProp {
+                            key: prop.key.swcify(ctx),
+                            value: Box::new(v.swcify(ctx)),
+                        })
+                    }
+                    swc_babel_ast::ObjectPropVal::Expr(v) => ObjectPatProp::Assign(AssignPatProp {
+                        span: ctx.span(&prop.base),
+                        key: prop.key.swcify(ctx).expect_ident(),
+                        value: Some(v.swcify(ctx)),
+                    }),
+                }
+            }
+        }
+    }
+}
+
+impl Swcify for swc_babel_ast::Pattern {
+    type Output = Pat;
+
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        match self {
+            swc_babel_ast::Pattern::Assignment(v) => Pat::from(v.swcify(ctx)),
+            swc_babel_ast::Pattern::Array(v) => Pat::from(v.swcify(ctx)),
+            swc_babel_ast::Pattern::Object(v) => Pat::from(v.swcify(ctx)),
+        }
+    }
 }
 
 impl Swcify for swc_babel_ast::Param {
     type Output = swc_ecma_ast::Param;
 
-    fn swcify(self, ctx: &Context) -> Self::Output {}
+    fn swcify(self, ctx: &Context) -> Self::Output {
+        match self {
+            swc_babel_ast::Param::Id(v) => todo!(),
+            swc_babel_ast::Param::Pat(v) => {
+                let pat = v.swcify(ctx);
+
+                swc_ecma_ast::Param {
+                    span: pat.span(),
+                    decorators: Default::default(),
+                    pat,
+                }
+            }
+            swc_babel_ast::Param::Rest(v) => swc_ecma_ast::Param {
+                span: ctx.span(&v.base),
+                decorators: v.decorators.swcify(ctx).unwrap_or_default(),
+                pat: Pat::from(v.swcify(ctx)),
+            },
+            swc_babel_ast::Param::TSProp(v) => todo!(),
+        }
+    }
 }
