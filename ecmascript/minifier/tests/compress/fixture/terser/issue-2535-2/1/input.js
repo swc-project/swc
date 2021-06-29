cf45599a72ac3e4e2eb324ae71cd413c0f44b45b
강동윤 <kdy1997.dev@gmail.com>
2021-06-29 12:33:18 +0900

    Split a test to make it debuggable

--- ecmascript/minifier/tests/compress/fixture/terser/issue-2535-2/1/input.js
+++ ecmascript/minifier/tests/compress/fixture/terser/issue-2535-2/1/input.js
@@ -1,2 +1,4 @@
-console.log(x() && true && y());
-console.log(y() && true && x());
+console.log(x() || true || y());
+console.log(y() || true || x());
+console.log((x() || true) && y());
+console.log((y() || true) && x());
