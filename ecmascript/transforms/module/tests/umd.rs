f8a3df8cc3236b6cb251e3d4400019dd8c9f2edf
Iron Lu <IronLu233@users.noreply.github.com>
2021-06-29 14:12:18 +0800

    fix(es/transforms/module): Fix for duplicate export (#1846)

--- ecmascript/transforms/module/tests/umd.rs
+++ ecmascript/transforms/module/tests/umd.rs
@@ -1779,6 +1779,7 @@ export * from "foo";
   });
   Object.keys(_foo).forEach(function (key) {
     if (key === "default" || key === "__esModule") return;
+    if (key in _exports && _exports[key] === _foo[key]) return;
     Object.defineProperty(_exports, key, {
       enumerable: true,
       get: function () {
