f6da6597eb610776453d0884d86ecaad60413c71
강동윤 <kdy1997.dev@gmail.com>
2021-06-30 14:56:52 +0900

    nocapture

--- ecmascript/minifier/scripts/project-run.sh
+++ ecmascript/minifier/scripts/project-run.sh
@@ -4,5 +4,5 @@ set -eu
 export RUST_LOG=swc_ecma_minifier=trace
 touch tests/compress.rs
 
-UPDATE=1 cargo test --test compress projects
-UPDATE=1 cargo test --test compress projects
+UPDATE=1 cargo test --test compress projects -- --nocapture
+UPDATE=1 cargo test --test compress projects -- --nocapture
