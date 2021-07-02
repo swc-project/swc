945c04aae08876c6388b21d4aadcdca5dd433f98
강동윤 <kdy1997.dev@gmail.com>
2021-07-02 16:26:38 +0900

    Don't use --nocapture

--- ecmascript/minifier/scripts/project-run.sh
+++ ecmascript/minifier/scripts/project-run.sh
@@ -9,7 +9,7 @@ export RUST_BACKTRACE=1
 export RUST_LOG=swc_ecma_minifier=trace
 touch tests/compress.rs
 
-UPDATE=1 cargo test --test compress projects -- --nocapture || true
+UPDATE=1 cargo test --test compress projects || true
 
 # Make it easier to compare
 prettier --write tests/projects/output
