96f8db0f6ac0a7e35db92ac8cfebf0b1998aa249
강동윤 <kdy1997.dev@gmail.com>
2021-07-02 20:39:27 +0900

    Better panic message

--- ecmascript/minifier/src/compress/mod.rs
+++ ecmascript/minifier/src/compress/mod.rs
@@ -137,7 +137,7 @@ impl VisitMut for Compressor<'_> {
 
         // Temporary
         if self.pass > 100 {
-            panic!("Infinite loop detected")
+            panic!("Infinite loop detected (current pass = {})", self.pass)
         }
 
         let start = if cfg!(feature = "debug") {
