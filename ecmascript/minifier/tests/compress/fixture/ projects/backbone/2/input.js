cda49e2aa4934c646644ce5ff1dcc79df4d25b99
강동윤 <kdy1997.dev@gmail.com>
2021-07-03 14:51:39 +0900

    fixup

--- ecmascript/minifier/tests/compress/fixture/ projects/backbone/2/input.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/2/input.js
@@ -1,11 +1,13 @@
-if (!eventsApi(this, "on", name, [callback, context]) || !callback)
-    return this;
-return (
-    this._events || (this._events = {}),
-    (this._events[name] || (this._events[name] = [])).push({
-        callback: callback,
-        context: context,
-        ctx: context || this,
-    }),
-    this
-);
+function foo() {
+    if (!eventsApi(this, "on", name, [callback, context]) || !callback)
+        return this;
+    return (
+        this._events || (this._events = {}),
+        (this._events[name] || (this._events[name] = [])).push({
+            callback: callback,
+            context: context,
+            ctx: context || this,
+        }),
+        this
+    );
+}
