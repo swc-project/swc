b91ecb8b5d77870e63202fbcca9270686e6000a9
강동윤 <kdy1997.dev@gmail.com>
2021-07-05 21:52:29 +0900

    Add type

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,3 +1,8 @@
+use std::{ffi::CString, os::raw::c_char};
+
+/// `(ast_json, options_json) -> transformed_ast_json`
+pub type LibFn = unsafe extern "C" fn(*const c_char, *const c_char) -> CString;
+
 #[macro_export]
 macro_rules! export_transform {
     ($name:path) => {};
