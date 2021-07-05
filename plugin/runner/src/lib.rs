206d97c0fdbfa6d825b3f789bce9fb234c0e18a2
강동윤 <kdy1997.dev@gmail.com>
2021-07-05 20:48:40 +0900

    Add invoke_plugin_at

--- plugin/runner/src/lib.rs
+++ plugin/runner/src/lib.rs
@@ -1,4 +1,22 @@
-use anyhow::Error;
+use anyhow::{Context, Error};
+use std::{ffi::CString, path::Path};
 use swc_ecma_ast::Program;
 
+type LibFn = unsafe extern "C" fn(*const c_char) -> CString;
+
 pub fn invoke_plugin(plugin_name: &str, a: &Program) -> Result<Program, Error> {}
+
+pub fn invoke_plugin_at(filename: &Path, a: &Program) -> Result<Program, Error> {
+    let lib = unsafe {
+        libloading::Library::new(&filename)
+            .with_context(|| format!("failed to load shared library at `{}`", filename.display()))?
+    };
+    let func = libloading::Symbol::<LibFn> = unsafe {
+        lib.get("swc_plugin").with_context(|| {
+            fomrat!(
+                "failed to get function `swc_plugin` from a shared library at `{}`",
+                filename.display()
+            )
+        })?
+    };
+}
