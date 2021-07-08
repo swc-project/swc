439a6bd3f044fb8e9e9db732f472b5fe6f033a79
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 11:59:56 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/backbone/2/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/2/output.js
@@ -1,9 +1,9 @@
 function foo() {
     return eventsApi(this, "on", name, [callback,
-        context]) && callback ? this : (this._events || (this._events = {
+        context]) && callback ? (this._events || (this._events = {
     }), (this._events[name] || (this._events[name] = [])).push({
         callback: callback,
         context: context,
         ctx: context || this
-    }), this);
+    }), this) : this;
 }
--- ecmascript/minifier/tests/compress/fixture/ projects/backbone/5/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/5/output.js
@@ -1,10 +1,11 @@
 var Events = {
-    on: function (name, callback, context) {
-        return eventsApi(this, "on", name, [callback, context]) && callback ? (
-            this._events || (this._events = {}),
-            (
-                this._events[name] || (this._events[name] = [])
-            ).push({ callback: callback, context: context, ctx: context || this }), this) :
-            this
+    on: function(name, callback, context) {
+        return eventsApi(this, 'on', name, [callback,
+            context]) && callback ? (this._events || (this._events = {
+        }), (this._events[name] || (this._events[name] = [])).push({
+            callback: callback,
+            context: context,
+            ctx: context || this
+        }), this) : this;
     }
 };
