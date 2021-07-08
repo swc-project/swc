a5030bb5b566ae5389a12ad7f1599f6c17cc31ae
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 22:50:19 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/9/input.js
@@ -0,0 +1,34 @@
+(function () {
+
+    // Initial Setup
+    // -------------
+
+    // Save a reference to the global object (`window` in the browser, `exports`
+    // on the server).
+    var root = this;
+
+    // Save the previous value of the `Backbone` variable, so that it can be
+    // restored later on, if `noConflict` is used.
+    var previousBackbone = root.Backbone;
+
+    // Create local references to array methods we'll want to use later.
+    var array = [];
+    var push = array.push;
+    var slice = array.slice;
+    var splice = array.splice;
+
+    // The top-level namespace. All public Backbone classes and modules will
+    // be attached to this. Exported for both the browser and the server.
+    var Backbone;
+    if (typeof exports !== 'undefined') {
+        Backbone = exports;
+    } else {
+        Backbone = root.Backbone = {};
+    }
+
+    // Current version of the library. Keep in sync with `package.json`.
+    Backbone.VERSION = '1.1.0';
+
+    // Require Underscore, if we're on the server, and it's not already present.
+    var _ = root._;
+})()
