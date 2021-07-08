023b8120f19b75553973b2f1fcfd337b906f06e7
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 13:13:52 +0900

    Fix jsx

--- ecmascript/transforms/react/src/jsx/fixture/issue-1799/case1/output.mjs
+++ ecmascript/transforms/react/src/jsx/fixture/issue-1799/case1/output.mjs
@@ -1,9 +1,9 @@
 import React from "react";
 export default function Foo() {
     return React.createElement("div", {
-        onClick: async (e)=>{
+        onClick: async (e) => {
             await doSomething();
         }
     });
-};
+}
 Foo.displayName = "Foo";
