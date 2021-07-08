4d8b68ff46e96af33ff7285a123f0fd4d2d5666d
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 22:59:16 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/11/output.js
@@ -0,0 +1,10 @@
+
+
+const obj = {
+    navigate: function (fragment, options) {
+        if (!History.started) return !1;
+        (options && !0 !== options) || (options = {
+            trigger: !!options,
+        });
+    }
+}
