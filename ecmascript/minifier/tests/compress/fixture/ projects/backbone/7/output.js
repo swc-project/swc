9b57b68cfcd589b1c0cfe722b28f2ebfc35d83f6
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 12:29:04 +0900

    Update tests refs

--- ecmascript/minifier/tests/compress/fixture/ projects/backbone/7/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/7/output.js
@@ -1,7 +1,6 @@
 function foo() {
-    var collection = this,
-        success = options.success;
-    return options.success = function (model, resp, options) {
-        options.wait && collection.add(model, options), success && success(model, resp, options)
-    }, model.save(null, options), model
+    var collection = this, success = options.success;
+    return options.success = function(model, resp, options) {
+        options.wait && collection.add(model, options), success && success(model, resp, options);
+    }, model.save(null, options), model;
 }
