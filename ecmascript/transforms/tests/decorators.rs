0b9ed6f1a05ce93cdacbbd5f36f6ed0332e9128e
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 20:23:40 +0900

    fixup

--- ecmascript/transforms/tests/decorators.rs
+++ ecmascript/transforms/tests/decorators.rs
@@ -4252,29 +4252,34 @@ test!(
   @foo() bar = '1';
   @foo() baz = '2';
 }",
-    "var _class, _descriptor, _descriptor1;
-var _dec = foo(), _dec1 = foo();
-let Example = ((_class = class Example{
-    constructor(){
-        _initializerDefineProperty(this, 'bar', _descriptor, this);
-        _initializerDefineProperty(this, 'baz', _descriptor1, this);
-    }
-}) || _class, _descriptor = _applyDecoratedDescriptor(_class.prototype, 'bar', [_dec], {
-    configurable: true,
-    enumerable: true,
-    writable: true,
-    initializer: function() {
-        return '1';
-    }
-}), _descriptor1 = _applyDecoratedDescriptor(_class.prototype, 'baz', [_dec1], {
-    configurable: true,
-    enumerable: true,
-    writable: true,
-    initializer: function() {
-        return '2';
-    }
-}), _class);
-"
+    "
+    var _class, _descriptor, _dec, _descriptor1, _dec1;
+    let Example = ((_class = class Example {
+        constructor(){
+            _initializerDefineProperty(this, 'bar', _descriptor, this);
+            _initializerDefineProperty(this, 'baz', _descriptor1, this);
+        }
+    }) || _class, _dec = foo(), _dec1 = foo(), _descriptor = \
+     _applyDecoratedDescriptor(_class.prototype, 'bar', [
+        _dec
+    ], {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        initializer: function() {
+            return '1';
+        }
+    }), _descriptor1 = _applyDecoratedDescriptor(_class.prototype, 'baz', [
+        _dec1
+    ], {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        initializer: function() {
+            return '2';
+        }
+    }), _class);
+    "
 );
 
 test!(
