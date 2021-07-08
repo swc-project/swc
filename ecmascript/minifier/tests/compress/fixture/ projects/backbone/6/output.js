c714c47e7405053fe1bbcd63ee86fef52c8ec16b
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 12:20:14 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/6/output.js
@@ -0,0 +1,16 @@
+function foo() {
+    return (
+        !error ||
+        (this.trigger(
+            "invalid",
+            this,
+            error,
+            _.extend(
+                options,
+                {
+                    validationError: error,
+                }
+            ),
+        ),
+            !1)
+}
