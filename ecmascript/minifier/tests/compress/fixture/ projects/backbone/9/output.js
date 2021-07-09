df96ffbaf7eb7ea033c9e363ff8509537a2a911d
강동윤 <kdy1997.dev@gmail.com>
2021-07-09 16:17:14 +0900

    Update test refs

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/9/output.js
@@ -0,0 +1,5 @@
+!function() {
+    var Backbone, root = this;
+    root.Backbone, Backbone = 'undefined' !== typeof exports ? exports : root.Backbone = {
+    }, Backbone.VERSION = '1.1.0', root._;
+}();
