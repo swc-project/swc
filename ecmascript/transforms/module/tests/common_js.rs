3bc3d224f1b867b8c30d15302b7afd27c9baf2fb
강동윤 <kdy1997.dev@gmail.com>
2021-07-10 19:48:03 +0900

    Update test refs

--- ecmascript/transforms/module/tests/common_js.rs
+++ ecmascript/transforms/module/tests/common_js.rs
@@ -614,14 +614,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _foo = require("foo");
-
 Object.defineProperty(exports, "foo", {
   enumerable: true,
   get: function () {
     return _foo.foo;
   }
 });
+
+var _foo = require("foo");
 "#
 );
 
