8de7a77bcca807406f90c7b44e1e44ad84f4a321
강동윤 <kdy1997.dev@gmail.com>
2021-07-12 09:47:39 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/12/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/12/output.js
@@ -1,11 +1,3 @@
-_.random = function (
-    min, max
-) {
-    return (
-        null == max && ((max = min), (min = 0)),
-        min + Math.floor(
-            Math.random(
-            ) * (max - min + 1)
-        )
-    );
-}
+_.random = function(min, max) {
+    return null == max && (max = min, min = 0), min + Math.floor(Math.random() * (max - min + 1));
+};
