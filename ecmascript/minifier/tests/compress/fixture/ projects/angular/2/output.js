210d98bdab97d2ae465f77d1474a257133d3ea95
강동윤 <kdy1997.dev@gmail.com>
2021-07-12 17:44:55 +0900

    Add tests

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/angular/2/output.js
@@ -0,0 +1,17 @@
+var h = destination.$$hashKey;
+for (var key in (forEach(
+    destination,
+    function (
+        value, key
+    ) {
+        delete destination[key];
+    }
+),
+    source))
+    destination[key] = copy(
+        source[key]
+    );
+setHashKey(
+    destination,
+    h
+);
