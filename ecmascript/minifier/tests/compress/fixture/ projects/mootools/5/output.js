bad3d6f85230a795cc3fabf56f453de4044b67da
강동윤 <kdy1997.dev@gmail.com>
2021-07-12 17:16:50 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/mootools/5/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/mootools/5/output.js
@@ -1,11 +1,5 @@
 export const obj = {
-    set: function (
-        key, value
-    ) {
-        return (
-            (this[key] && !this.hasOwnProperty(
-                key
-            )) || (this[key] = value), this
-        );
+    set: function(key, value) {
+        return this[key] && !this.hasOwnProperty(key) || (this[key] = value), this;
     }
-}
+};
