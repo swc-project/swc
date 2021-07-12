392c1419ae69db324ddb4aea84bc8472a042e7cc
강동윤 <kdy1997.dev@gmail.com>
2021-07-12 17:25:50 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/1/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/1/output.js
@@ -1,5 +1,5 @@
 _.contains = _.include = function(obj, target) {
     return null != obj && (nativeIndexOf && obj.indexOf === nativeIndexOf ? -1 != obj.indexOf(target) : any(obj, function(value) {
-        return target === value;
+        return value === target;
     }));
 };
