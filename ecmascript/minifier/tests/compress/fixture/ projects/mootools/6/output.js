83c7b7c72877c1ed5de6ea8a256ee50ed3d83e9b
강동윤 <kdy1997.dev@gmail.com>
2021-07-13 10:32:47 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/mootools/6/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/mootools/6/output.js
@@ -1,16 +1,19 @@
 export const obj = {
-    removeEvents: function (events) {
+    removeEvents: function(events) {
         var type;
-        if ("object" == typeOf(events)) {
-            for (type in events) this.removeEvent(type, events[type]); return this
+        if ('object' == typeOf(events)) {
+            for(type in events)this.removeEvent(type, events[type]);
+            return this;
         }
-        var attached = this.retrieve("events");
+        var attached = this.retrieve('events');
         if (!attached) return this;
-        if (events) attached[events] && (attached[events].keys.each((function (fn) {
-            this.removeEvent(events, fn)
-        }), this), delete attached[events]); else {
-            for (type in attached) this.removeEvents(type); this.eliminate("events")
+        if (events) attached[events] && (attached[events].keys.each(function(fn) {
+            this.removeEvent(events, fn);
+        }, this), delete attached[events]);
+        else {
+            for(type in attached)this.removeEvents(type);
+            this.eliminate('events');
         }
-        return this
+        return this;
     }
 };
