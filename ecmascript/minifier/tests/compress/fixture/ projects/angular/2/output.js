1540506f65810bd3526fc34bc99d9e56382fd9f5
강동윤 <kdy1997.dev@gmail.com>
2021-07-13 10:05:53 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/angular/2/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/angular/2/output.js
@@ -1,17 +1,4 @@
 var h = destination.$$hashKey;
-for (var key in (forEach(
-    destination,
-    function (
-        value, key
-    ) {
-        delete destination[key];
-    }
-),
-    source))
-    destination[key] = copy(
-        source[key]
-    );
-setHashKey(
-    destination,
-    h
-);
+for(var key in forEach(destination, function(value, key) {
+    delete destination[key];
+}), source)destination[key] = copy(source[key]);
