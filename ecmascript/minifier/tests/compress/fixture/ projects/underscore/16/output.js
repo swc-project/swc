089e89e5175ec7d4b46448bfe6acebf1061c6529
강동윤 <kdy1997.dev@gmail.com>
2021-07-13 10:15:38 +0900

    Update test refs (manually)

--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/16/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/16/output.js
@@ -1,43 +1,12 @@
 function foo() {
-    aStack.push(
-        a
-    ), bStack.push(
-        b
-    );
-    var size = 0,
-        result = !0;
-    if ("[object Array]" == className) {
-        if ((result = (size = a.length) == b.length))
-            for (; size-- && (result = eq(
-                a[size],
-                b[size],
-                aStack,
-                bStack
-            )););
+    aStack.push(a), bStack.push(b);
+    var size = 0, result = !0;
+    if ('[object Array]' == className) {
+        if (result = (size = a.length) == b.length) for (; size--;)if (!(result = eq(a[size], b[size], aStack, bStack))) break;
     } else {
-        for (var key in a)
-            if (
-                _.has(
-                    a,
-                    key
-                ) &&
-                (size++,
-                    !(result = _.has(
-                        b,
-                        key
-                    ) && eq(
-                        a[key],
-                        b[key],
-                        aStack,
-                        bStack
-                    )))
-            )
-                break;
+        for (var key in a) if (_.has(a, key) && (size++, !(result = _.has(b, key) && eq(a[key], b[key], aStack, bStack)))) break;
         if (result) {
-            for (key in b) if (_.has(
-                b,
-                key
-            ) && !size--) break;
+            for (key in b) if (_.has(b, key) && !size--) break;
             result = !size;
         }
     }
