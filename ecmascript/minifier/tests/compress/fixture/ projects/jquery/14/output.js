9cff5e69b1156dd7fa0e056a8b9d7a30927e6703
강동윤 <kdy1997.dev@gmail.com>
2021-07-14 16:43:14 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/jquery/14/output.js
@@ -0,0 +1,11 @@
+export const obj = {
+    preventDefault: function (
+    ) {
+        var e = this.originalEvent;
+        (this.isDefaultPrevented = returnTrue),
+            e && (e.preventDefault
+                ? e.preventDefault(
+                )
+                : (e.returnValue = !1));
+    }
+}
