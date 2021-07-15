3e56a845a90138243cd8e737c68460c14c411670
강동윤 <kdy1997.dev@gmail.com>
2021-07-15 13:47:51 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/jquery/15/output.js
@@ -0,0 +1,84 @@
+export const obj = {
+    position: function (
+    ) {
+        if (this[0]) {
+            var offsetParent,
+                offset,
+                parentOffset = {
+                    top: 0,
+                    left: 0,
+                },
+                elem = this[0];
+            return (
+                "fixed" === jQuery.css(
+                    elem,
+                    "position"
+                )
+                    ? (offset = elem.getBoundingClientRect(
+                    ))
+                    : ((offsetParent = this.offsetParent(
+                    )),
+                        (offset = this.offset(
+                        )),
+                        jQuery.nodeName(
+                            offsetParent[0],
+                            "html"
+                        ) ||
+                        (parentOffset = offsetParent.offset(
+                        )),
+                        (parentOffset.top += jQuery.css(
+                            offsetParent[0],
+                            "borderTopWidth",
+                            !0,
+                        )),
+                        (parentOffset.left += jQuery.css(
+                            offsetParent[0],
+                            "borderLeftWidth",
+                            !0,
+                        ))),
+                {
+                    top:
+                        offset.top -
+                        parentOffset.top -
+                        jQuery.css(
+                            elem,
+                            "marginTop",
+                            !0
+                        ),
+                    left:
+                        offset.left -
+                        parentOffset.left -
+                        jQuery.css(
+                            elem,
+                            "marginLeft",
+                            !0
+                        ),
+                }
+            );
+        }
+    },
+    offsetParent: function (
+    ) {
+        return this.map(
+            function (
+            ) {
+                for (
+                    var offsetParent = this.offsetParent || document.documentElement;
+                    offsetParent &&
+                    !jQuery.nodeName(
+                        offsetParent,
+                        "html"
+                    ) &&
+                    "static" === jQuery.css(
+                        offsetParent,
+                        "position"
+                    );
+
+                )
+                    offsetParent = offsetParent.offsetParent;
+                return offsetParent || document.documentElement;
+            }
+        );
+    },
+}
+}
