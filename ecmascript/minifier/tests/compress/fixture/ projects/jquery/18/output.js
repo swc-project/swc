550c0f8bcf9eaba04f48d0d612b05a454b7ef92c
강동윤 <kdy1997.dev@gmail.com>
2021-07-15 13:52:37 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/jquery/18/output.js
@@ -0,0 +1,19 @@
+export const obj = {
+    tweener: function (
+        props, callback
+    ) {
+        jQuery.isFunction(
+            props
+        )
+            ? ((callback = props), (props = ["*",]))
+            : (props = props.split(
+                " "
+            ));
+        for (var prop, index = 0, length = props.length; index < length; index++)
+            (prop = props[index]),
+                (tweeners[prop] = tweeners[prop] || []),
+                tweeners[prop].unshift(
+                    callback
+                );
+    }
+}
