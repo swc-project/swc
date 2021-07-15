0a84d850a3244502765e9bdfe575d5b86630ff45
강동윤 <kdy1997.dev@gmail.com>
2021-07-15 12:46:02 +0900

    Reorder statements

--- ecmascript/minifier/src/compress/optimize/fns.rs
+++ ecmascript/minifier/src/compress/optimize/fns.rs
@@ -1,13 +1,53 @@
-use swc_ecma_utils::{ModuleItemLike, StmtLike};
-
-use crate::util::MoudleItemExt;
-
 use super::Optimizer;
+use crate::util::{sort::is_sorted_by, MoudleItemExt};
+use std::cmp::Ordering;
+use swc_ecma_ast::*;
+use swc_ecma_transforms_base::ext::MapWithMut;
 
 impl Optimizer<'_> {
     pub(super) fn reorder_stmts<T>(&mut self, stmts: &mut Vec<T>)
     where
         T: MoudleItemExt,
     {
+        if is_sorted_by(stmts.iter(), |a, b| {
+            // Check for function declarations.
+
+            match (a.as_module_decl(), b.as_module_decl()) {
+                (Err(Stmt::Decl(Decl::Fn(..))), Err(Stmt::Decl(Decl::Fn(..)))) => {
+                    Some(Ordering::Less)
+                }
+                (_, Err(Stmt::Decl(Decl::Fn(..)))) => Some(Ordering::Greater),
+                _ => Some(Ordering::Less),
+            }
+        }) {
+            return;
+        }
+
+        self.changed = true;
+        log::trace!("fns: Reordering statements");
+        let mut fns = vec![];
+        let mut other = vec![];
+
+        for stmt in stmts.take() {
+            let stmt = stmt.into_module_item();
+
+            match stmt {
+                ModuleItem::Stmt(Stmt::Decl(Decl::Fn(..)))
+                | ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ExportDecl {
+                    decl: Decl::Fn(..),
+                    ..
+                })) => {
+                    fns.push(T::from_module_item(stmt));
+                }
+
+                _ => {
+                    other.push(T::from_module_item(stmt));
+                }
+            }
+        }
+
+        fns.extend(other);
+
+        *stmts = fns;
     }
 }
