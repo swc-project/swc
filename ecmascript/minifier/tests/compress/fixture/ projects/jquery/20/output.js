65bc98aa1d768f7076f5cf2449aa7e3f12132021
강동윤 <kdy1997.dev@gmail.com>
2021-07-15 13:58:01 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/jquery/20/output.js
@@ -0,0 +1,15 @@
+export const obj = {
+    setRequestHeader: function (
+        name, value
+    ) {
+        var lname = name.toLowerCase(
+        );
+        return (
+            state ||
+            ((name = requestHeadersNames[lname] =
+                requestHeadersNames[lname] || name),
+                (requestHeaders[name] = value)),
+            this
+        );
+    }
+}
