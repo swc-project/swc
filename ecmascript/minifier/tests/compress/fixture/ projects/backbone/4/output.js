c8ccd48d898ce1285a47fe62b5c618305f1447fe
강동윤 <kdy1997.dev@gmail.com>
2021-07-16 14:24:47 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/backbone/4/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/backbone/4/output.js
@@ -1,7 +1,8 @@
 function foo(attrs, options) {
     if (!options.validate || !this.validate) return !0;
-    var error = this.validationError = this.validate(attrs = _.extend({
-    }, this.attributes, attrs), options) || null;
+    attrs = _.extend({
+    }, this.attributes, attrs);
+    var error = this.validationError = this.validate(attrs, options) || null;
     return !error || (this.trigger("invalid", this, error, _.extend(options, {
         validationError: error
     })), !1);
