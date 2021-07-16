9a0c1d73c70a13778f8d7e2fa4c90229eeace03b
강동윤 <kdy1997.dev@gmail.com>
2021-07-16 13:42:42 +0900

    Move bool pass to post compress

--- ecmascript/minifier/src/compress/optimize/bools.rs
+++ ecmascript/minifier/src/compress/optimize/bools.rs
@@ -577,43 +577,6 @@ impl Optimizer<'_> {
                 }
             }
         }
-
-        match n {
-            Expr::Bin(BinExpr {
-                op: op @ op!("&&") | op @ op!("||"),
-                left,
-                right,
-                ..
-            }) => {
-                let new_op = if *op == op!("&&") {
-                    op!("||")
-                } else {
-                    op!("&&")
-                };
-                match &**left {
-                    Expr::Bin(BinExpr { op: l_op, .. }) if *l_op == new_op => match &mut **right {
-                        Expr::Unary(UnaryExpr {
-                            op: op!("!"), arg, ..
-                        }) => {
-                            self.changed = true;
-                            log::trace!(
-                                "bools: `(a {} !b)` => `(a {} b)` (in bool context)",
-                                *op,
-                                new_op
-                            );
-                            *op = new_op;
-                            *right = arg.take();
-                            return;
-                        }
-
-                        _ => {}
-                    },
-                    _ => {}
-                }
-            }
-
-            _ => {}
-        }
     }
 
     pub(super) fn compress_if_stmt_as_expr(&mut self, s: &mut Stmt) {
--- ecmascript/minifier/src/pass/postcompress.rs
+++ ecmascript/minifier/src/pass/postcompress.rs
@@ -1,5 +1,7 @@
 use crate::option::CompressOptions;
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut};
+use swc_ecma_ast::*;
+use swc_ecma_transforms_base::ext::MapWithMut;
+use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 
 pub fn postcompress_optimizer<'a>(options: &'a CompressOptions) -> impl 'a + VisitMut {
     PostcompressOptimizer { options }
@@ -11,4 +13,39 @@ struct PostcompressOptimizer<'a> {
 
 impl VisitMut for PostcompressOptimizer<'_> {
     noop_visit_mut_type!();
+
+    fn visit_mut_if_stmt(&mut self, s: &mut IfStmt) {
+        s.visit_mut_children_with(self);
+
+        match &mut *s.test {
+            Expr::Bin(BinExpr {
+                op: op @ op!("||") | op @ op!("&&"),
+                right,
+                ..
+            }) => match &mut **right {
+                Expr::Unary(UnaryExpr {
+                    op: op!("!"), arg, ..
+                }) => {
+                    let new_op = if *op == op!("&&") {
+                        op!("||")
+                    } else {
+                        op!("&&")
+                    };
+
+                    log::trace!(
+                        "bools: `(a {} !b)` => `(a {} b)` (in bool context)",
+                        *op,
+                        new_op
+                    );
+                    *op = new_op;
+                    *right = arg.take();
+                    return;
+                }
+
+                _ => {}
+            },
+
+            _ => {}
+        }
+    }
 }
