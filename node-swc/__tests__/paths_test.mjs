babcd93884af69ba70b7f7a130b84e45da020e21
강동윤 <kdy1997.dev@gmail.com>
2021-07-17 19:43:10 +0900

    Add a test

---
+++ node-swc/__tests__/paths_test.mjs
@@ -0,0 +1,27 @@
+import swc from "../..";
+
+it("should respect paths", async () => {
+    const { code } = await swc.transform(`
+    import foo from '@src/app';
+    console.log(foo)
+    `, {
+        jsc: {
+            parser: {
+                syntax: 'typescript',
+            },
+            target: 'es2021',
+            transform: {
+
+            },
+            paths: {
+                '@src/*': ['bar/*']
+            }
+        },
+        module: {
+            type: 'commonjs'
+        },
+    });
+
+    expect(code).toContain(``);
+    expect(code).toEqual(``)
+})
