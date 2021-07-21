9d39d3415830ac13feef254a6ada1384e56b92bd
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 13:44:20 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/12/output.js
@@ -0,0 +1,15 @@
+function getSourceInfoErrorAddendumForProps(
+    elementProps
+) {
+    return null != elementProps && void 0 !== (source = elementProps.__source)
+        ? "\n\nCheck your code at " +
+        source.fileName.replace(
+            /^.*[\\\/]/,
+            ""
+        ) +
+        ":" +
+        source.lineNumber +
+        "."
+        : "";
+    var source;
+}
