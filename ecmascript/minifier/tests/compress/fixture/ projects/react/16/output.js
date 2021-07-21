527f74a907f6173886b90d3c3954eaaa09018fee
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 18:50:14 +0900

    Update test refs (Our one is better)

--- ecmascript/minifier/tests/compress/fixture/ projects/react/16/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/16/output.js
@@ -1,25 +1,3 @@
-function advanceTimers(
-    currentTime
-) {
-    for (var timer = peek(
-        timerQueue
-    ); null !== timer;) {
-        if (null === timer.callback) pop(
-            timerQueue
-        );
-        else {
-            if (!(timer.startTime <= currentTime)) return;
-            pop(
-                timerQueue
-            ),
-                (timer.sortIndex = timer.expirationTime),
-                push(
-                    taskQueue,
-                    timer
-                );
-        }
-        timer = peek(
-            timerQueue
-        );
-    }
+function advanceTimers(currentTime) {
+    for(var timer = peek(timerQueue); null !== timer;)null === timer.callback ? pop(timerQueue) : timer.startTime <= currentTime && (pop(timerQueue), timer.sortIndex = timer.expirationTime, push(taskQueue, timer)), timer = peek(timerQueue);
 }
