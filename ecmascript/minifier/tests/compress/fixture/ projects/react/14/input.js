d4616028f732a40a06e021896bb5b06d087f53bd
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 13:46:16 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/14/input.js
@@ -0,0 +1,37 @@
+function validatePropTypes(element) {
+    {
+        var type = element.type;
+
+        if (type === null || type === undefined || typeof type === 'string') {
+            return;
+        }
+
+        var propTypes;
+
+        if (typeof type === 'function') {
+            propTypes = type.propTypes;
+        } else if (typeof type === 'object' && (type.$$typeof === REACT_FORWARD_REF_TYPE || // Note: Memo only checks outer props here.
+            // Inner props are checked in the reconciler.
+            type.$$typeof === REACT_MEMO_TYPE)) {
+            propTypes = type.propTypes;
+        } else {
+            return;
+        }
+
+        if (propTypes) {
+            // Intentionally inside to avoid triggering lazy initializers:
+            var name = getComponentName(type);
+            checkPropTypes(propTypes, element.props, 'prop', name, element);
+        } else if (type.PropTypes !== undefined && !propTypesMisspellWarningShown) {
+            propTypesMisspellWarningShown = true; // Intentionally inside to avoid triggering lazy initializers:
+
+            var _name = getComponentName(type);
+
+            error('Component %s declared `PropTypes` instead of `propTypes`. Did you misspell the property assignment?', _name || 'Unknown');
+        }
+
+        if (typeof type.getDefaultProps === 'function' && !type.getDefaultProps.isReactClassApproved) {
+            error('getDefaultProps is only used on classic React.createClass ' + 'definitions. Use a static property named `defaultProps` instead.');
+        }
+    }
+}
