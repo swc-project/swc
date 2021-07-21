67dbb7f26f991403a12780186f7cc303414ddbe6
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 13:43:18 +0900

    Add a test

---
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/11/output.js
@@ -0,0 +1,16 @@
+function setCurrentlyValidatingElement$1(
+    element
+) {
+    if (element) {
+        var owner = element._owner;
+        setExtraStackFrame(
+            describeUnknownElementTypeFrameInDEV(
+                element.type,
+                element._source,
+                owner ? owner.type : null,
+            ),
+        );
+    } else setExtraStackFrame(
+        null
+    );
+}
