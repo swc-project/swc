2bd56054dd1e28a335cf52cf88428ae6eee7552e
강동윤 <kdy1997.dev@gmail.com>
2021-07-22 15:00:12 +0900

    fixup

--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/input.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/input.js
@@ -1,5 +1,7 @@
-var idCounter = 0;
-_.uniqueId = function (prefix) {
-    var id = ++idCounter + '';
-    return prefix ? prefix + id : id;
-};
+(function () {
+    var idCounter = 0;
+    _.uniqueId = function (prefix) {
+        var id = ++idCounter + '';
+        return prefix ? prefix + id : id;
+    };
+})()
--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/output.js
@@ -1,7 +1,9 @@
-var idCounter = 0;
-(_.uniqueId = function (
-    prefix
-) {
-    var id = ++idCounter + "";
-    return prefix ? prefix + id : id;
-});
+(function () {
+    var idCounter = 0;
+    (_.uniqueId = function (
+        prefix
+    ) {
+        var id = ++idCounter + "";
+        return prefix ? prefix + id : id;
+    });
+})()
