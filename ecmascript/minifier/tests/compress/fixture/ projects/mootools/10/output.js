84824f950a46ca4437e197c6ad7b1ea0ddf395e2
강동윤 <kdy1997.dev@gmail.com>
2021-07-22 18:14:28 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/mootools/10/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/mootools/10/output.js
@@ -1,33 +1,9 @@
 export const exported = {
-    fireEvent: function (
-        type, args, delay
-    ) {
-        type = removeOn(
-            type
-        );
+    fireEvent: function(type, args, delay) {
+        type = removeOn(type);
         var events = this.$events[type];
-        return events
-            ? ((args = Array.from(
-                args
-            )),
-                events.each(
-                    function (
-                        fn
-                    ) {
-                        delay
-                            ? fn.delay(
-                                delay,
-                                this,
-                                args
-                            )
-                            : fn.apply(
-                                this,
-                                args
-                            );
-                    },
-                    this
-                ),
-                this)
-            : this;
+        return events && (args = Array.from(args), events.each(function(fn) {
+            delay ? fn.delay(delay, this, args) : fn.apply(this, args);
+        }, this)), this;
     }
-}
+};
