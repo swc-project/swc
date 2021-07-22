6c7f55d5172ff777f49c4b1b6f8d78b119de1b0a
강동윤 <kdy1997.dev@gmail.com>
2021-07-22 11:58:50 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/react/12/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/12/output.js
@@ -1,15 +1,4 @@
-function getSourceInfoErrorAddendumForProps(
-    elementProps
-) {
-    return null != elementProps && void 0 !== (source = elementProps.__source)
-        ? "\n\nCheck your code at " +
-        source.fileName.replace(
-            /^.*[\\\/]/,
-            ""
-        ) +
-        ":" +
-        source.lineNumber +
-        "."
-        : "";
+function getSourceInfoErrorAddendumForProps(elementProps) {
+    return null != elementProps && void 0 !== (source = elementProps.__source) ? '\n\nCheck your code at ' + source.fileName.replace(/^.*[\\\/]/, '') + ':' + source.lineNumber + '.' : '';
     var source;
 }
