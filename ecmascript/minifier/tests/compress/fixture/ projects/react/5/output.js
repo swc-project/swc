0133dbd595f6222de3c9e6c7e207794fcddef76c
강동윤 <kdy1997.dev@gmail.com>
2021-07-22 11:34:44 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/react/5/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/react/5/output.js
@@ -1,14 +1,7 @@
 var emptyObject = {
 };
-function Component(
-    props, context, updater
-) {
-    (this.props = props),
-        (this.context = context),
-        (this.refs = emptyObject),
-        (this.updater = updater || ReactNoopUpdateQueue);
+function Component(props, context, updater) {
+    this.props = props, this.context = context, this.refs = emptyObject, this.updater = updater || ReactNoopUpdateQueue;
 }
-(Object.freeze(
-    emptyObject
-), Component.prototype.isReactComponent = {
-})
+Object.freeze(emptyObject), Component.prototype.isReactComponent = {
+};
