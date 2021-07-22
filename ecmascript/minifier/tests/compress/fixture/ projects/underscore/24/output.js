3ff15056ab8bc1f785bcaa587ac60964b4db07cd
강동윤 <kdy1997.dev@gmail.com>
2021-07-22 18:34:31 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/underscore/24/output.js
@@ -1,9 +1,7 @@
-(function () {
+!function() {
     var idCounter = 0;
-    (_.uniqueId = function (
-        prefix
-    ) {
-        var id = ++idCounter + "";
+    _.uniqueId = function(prefix) {
+        var id = ++idCounter + '';
         return prefix ? prefix + id : id;
-    });
-})()
+    };
+}();
