ed8aed967598548858369b76d3696297083607d0
강동윤 <kdy1997.dev@gmail.com>
2021-07-25 22:16:14 +0900

    Optimize prop names

--- ecmascript/minifier/src/compress/optimize/misc.rs
+++ ecmascript/minifier/src/compress/optimize/misc.rs
@@ -1,7 +1,30 @@
-use swc_ecma_ast::PropName;
+use swc_ecma_ast::*;
+use unicode_xid::UnicodeXID;
 
 use super::Optimizer;
 
 impl Optimizer<'_> {
-    pub(super) fn optimize_prop_name(&mut self, name: &mut PropName) {}
+    pub(super) fn optimize_prop_name(&mut self, name: &mut PropName) {
+        match name {
+            PropName::Str(s) => {
+                if s.value.is_reserved() || s.value.is_reserved_in_es3() {
+                    return;
+                }
+
+                if s.value.starts_with(|c: char| c.is_xid_start())
+                    && s.value.chars().all(|c: char| c.is_xid_continue())
+                {
+                    self.changed = true;
+                    log::trace!("misc: Optimizing string property name");
+                    *name = PropName::Ident(Ident {
+                        span: s.span,
+                        sym: s.value.clone(),
+                        optional: false,
+                    });
+                    return;
+                }
+            }
+            _ => {}
+        }
+    }
 }
