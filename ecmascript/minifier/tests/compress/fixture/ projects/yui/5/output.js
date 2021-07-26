167a7495b1a662daca1c44a39a0d52c072c310f3
강동윤 <kdy1997.dev@gmail.com>
2021-07-26 12:48:32 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/yui/5/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/yui/5/output.js
@@ -1,11 +1,5 @@
 export const E = {
-    _continue: function (
-    ) {
-        !_queue.running &&
-            _queue.size(
-            ) > 0 &&
-            ((_queue.running = !0), _queue.next(
-            )(
-            ));
-    },
-}
+    _continue: function() {
+        !_queue.running && _queue.size() > 0 && (_queue.running = !0, _queue.next()());
+    }
+};
