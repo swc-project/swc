2e73259d58641eb7b4726d186329c5ec6de99db0
강동윤 <kdy1997.dev@gmail.com>
2021-07-26 16:50:26 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/ projects/mootools/4/output.js
+++ ecmascript/minifier/tests/compress/fixture/ projects/mootools/4/output.js
@@ -2,7 +2,7 @@ const obj = {
     flatten: function() {
         for(var array = [], i = 0, l = this.length; i < l; i++){
             var type = typeOf(this[i]);
-            'null' != type && (array = array.concat('array' == type || 'collection' == type || 'arguments' == type || instanceOf(this[i], Array) ? Array.flatten(this[i]) : this[i]));
+            "null" != type && (array = array.concat("array" == type || "collection" == type || "arguments" == type || instanceOf(this[i], Array) ? Array.flatten(this[i]) : this[i]));
         }
         return array;
     }
