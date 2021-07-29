1065f5d2c557464be66f0d34fb4ab42f6647a458
강동윤 <kdy1997.dev@gmail.com>
2021-07-29 21:02:06 +0900

    Fix scripts

--- ecmascript/minifier/scripts/add-golden.sh
+++ ecmascript/minifier/scripts/add-golden.sh
@@ -7,9 +7,9 @@ set -eu
 export SWC_RUN=0
 
 cargo test --test compress --all-features \
-  | grep 'terser__compress' \
+  | grep 'fixture_tests__terser__compress__' \
   | grep 'js .\.\. ok$' \
-  | sed -e 's!test fixture_terser__compress__!!' \
+  | sed -e 's!test fixture_tests__terser__compress__!!' \
   | sed -e 's! ... ok!!' \
   | sed -e 's!__!/!g' \
   | sed -e 's!_js!.js!' \
--- ecmascript/minifier/scripts/ignore.sh
+++ ecmascript/minifier/scripts/ignore.sh
@@ -4,9 +4,9 @@ set -eu
 export SWC_RUN=0
 
 cargo test --test compress --all-features ${1-''} \
-  | grep 'terser__compress' \
+  | grep 'fixture_tests__terser__compress__' \
   | grep 'js .\.\. FAILED$' \
-  | sed -e 's!test fixture_terser__compress__!!' \
+  | sed -e 's!test fixture_tests__terser__compress__!!' \
   | sed -e 's! ... FAILED!!' \
   | sed -e 's!__!/!g' \
   | sed -e 's!_js!.js!' \
