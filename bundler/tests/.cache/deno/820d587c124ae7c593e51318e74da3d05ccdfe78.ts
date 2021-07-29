6f403df0a958da0bfdbfb5783b1fa19c18bf3bac
nuintun <nuintun@qq.com>
2021-07-29 11:42:45 +0800

    fix(es/codegen): Remove extra space of import decl

--- bundler/tests/.cache/deno/820d587c124ae7c593e51318e74da3d05ccdfe78.ts
+++ bundler/tests/.cache/deno/820d587c124ae7c593e51318e74da3d05ccdfe78.ts
@@ -1,7 +1,7 @@
 // Loaded from https://deno.land/x/jszip/mod.ts
 
 
-import  _JSZip from "https://dev.jspm.io/jszip@3.5.0";
+import _JSZip from "https://dev.jspm.io/jszip@3.5.0";
 import { WalkOptions, walk } from "https://deno.land/std@0.85.0/fs/mod.ts";
 import { SEP, join } from "https://deno.land/std@0.85.0/path/mod.ts";
 import type {
