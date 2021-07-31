d1901626ae0c121329f89e1c51721741f54280b0
강동윤 <kdy1997.dev@gmail.com>
2021-07-31 13:48:31 +0900

    More tests

--- node-swc/__tests__/minify_test.mjs
+++ node-swc/__tests__/minify_test.mjs
@@ -1,10 +1,31 @@
 import swc from '../..';
 
-it("should minify properly", async () => {
+it("should compress", async () => {
     const { code } = await swc.minify(`
     import foo from '@src/app';
     console.log(foo)
     `);
 
     expect(code).toMatchInlineSnapshot(`"import foo from'@src/app';console.log(foo);"`);
+})
+
+it("should accept object", async () => {
+    const { code } = await swc.minify(`
+    import foo from '@src/app';
+    console.log(foo)
+    `, {});
+
+    expect(code).toMatchInlineSnapshot(`"import foo from'@src/app';console.log(foo);"`);
+})
+
+it("should mangle", async () => {
+    const { code } = await swc.minify(`
+    import foo from '@src/app';
+    console.log(foo)
+    `, {
+        compress: false,
+        mangle: {},
+    });
+
+    expect(code).toMatchInlineSnapshot(`""`);
 })
