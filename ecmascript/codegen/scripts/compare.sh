a9d975bb1f7308ac5d87367068267a4611dc74c2
강동윤 <kdy1997.dev@gmail.com>
2021-08-03 19:25:50 +0900

    Improve compare.sh

--- ecmascript/codegen/scripts/compare.sh
+++ ecmascript/codegen/scripts/compare.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-find tests/test262-min -name '*.js' | xargs -L 1 ./scripts/compare-file.sh
-find tests/fixture -name 'output.js' | xargs -L 1 ./scripts/compare-file.sh
+git status -u --porcelain \
+    | grep '?? ecmascript/codegen/tests' \
+    | sed -e 's!^?? ecmascript/codegen/!!' \
+    | xargs -L 1 ./scripts/compare-file.sh
