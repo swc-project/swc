ea247e0c0e0bba1076b29ea131146270611fd9cf
강동윤 <kdy1997.dev@gmail.com>
2021-08-03 21:21:28 +0900

    Update test refs

---
+++ ecmascript/codegen/tests/test262-min/5b9f113c3bdd0c49.js
@@ -0,0 +1,1 @@
+function a(){b();c=1;throw"d";if(c){e();var c;function b(){};(function(){var f;function e(){};})()}}
---
+++ ecmascript/codegen/tests/test262-min/8d8913ebd8403c6a.js
@@ -0,0 +1,1 @@
+a();b();for(;false;);a();b();for(c=1;false;);c=(a in b);for(;false;);c=(a in b);for(d=2;false;);
