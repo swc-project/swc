63a2e8bf5d1c32164fc67b8df529c2c8eec2144b
강동윤 <kdy1997.dev@gmail.com>
2021-08-04 14:01:02 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/alias-outside.6e8773c7/output.js
+++ ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/alias-outside.6e8773c7/output.js
@@ -1,10 +1,10 @@
 import {
     m,
 } from "../index.f66dda46.js";
-const value$1 = "it works",
-    value = "it works";
 function AliasOutside(
 ) {
     return m`<div><p>Inside: ${value}</p><p>Outside: ${value$1}</p></div>`;
 }
+const value$1 = "it works",
+    value = "it works";
 export default AliasOutside;
