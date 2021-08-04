28ab810acdeba0fca147fac02b4535716ba04367
강동윤 <kdy1997.dev@gmail.com>
2021-08-04 14:06:07 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/class-fields.43d5f69c/output.js
+++ ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/class-fields.43d5f69c/output.js
@@ -1,22 +1,15 @@
-import {
-    _, m,
-} from "../index.f66dda46.js";
+import { _, m } from "../index.f66dda46.js";
 class ClassFields extends _ {
     state = {
-        value: 1,
+        value: 1
     };
-    onClick = (
-    ) => {
-        this.setState(
-            (
-                prev
-            ) => ({
-                value: prev.value + 1,
+    onClick = ()=>{
+        this.setState((prev)=>({
+                value: prev.value + 1
             })
         );
     };
-    render(
-    ) {
+    render() {
         return m`<div><p> State: <span>${this.state.value}</span></p><button onClick=${this.onClick}>click me</button></div>`;
     }
 }
