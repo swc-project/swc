14018d475f962d9b191d88032b78b5a1bd739229
강동윤 <kdy1997.dev@gmail.com>
2021-08-04 14:24:06 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/json.5609c5fa/output.js
+++ ecmascript/minifier/tests/compress/fixture/projects/wmr/archive-1/chunks/json.5609c5fa/output.js
@@ -1,45 +1,14 @@
-import {
-    a as l, y, m,
-} from "../index.f66dda46.js";
+import { a as l, y, m } from "../index.f66dda46.js";
+function JSONView() {
+    const [fetched, setFetched, ] = l(null);
+    return y(()=>{
+        fetch("./pages/foo.json").then((r)=>r.json()
+        ).then((r)=>setFetched(r)
+        );
+    }, []), m`<div><p>import: ${JSON.stringify(json)}</p><p>fetch: ${JSON.stringify(fetched)}</p></div>`;
+}
 const json = {
     foo: 42,
-    bar: "bar",
+    bar: "bar"
 };
-function JSONView(
-) {
-    const [fetched, setFetched,] = l(
-        null
-    );
-    return (
-        y(
-            (
-            ) => {
-                fetch(
-                    "./pages/foo.json"
-                )
-                    .then(
-                        (
-                            r
-                        ) => r.json(
-                        )
-                    )
-                    .then(
-                        (
-                            r
-                        ) => setFetched(
-                            r
-                        )
-                    );
-            },
-            []
-        ),
-        m`<div><p>import: ${JSON.stringify(
-            json
-        )}</p><p>fetch: ${JSON.stringify(
-            fetched
-        )}</p></div>`
-    );
-}
-export {
-    JSONView,
-};
+export { JSONView };
