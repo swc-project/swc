1e82f7a04e855e21088fa8d9e1d9d9c9944f15c0
강동윤 <kdy1997.dev@gmail.com>
2021-08-06 15:18:35 +0900

    pure: Use same thread for a while

--- ecmascript/minifier/src/compress/pure/mod.rs
+++ ecmascript/minifier/src/compress/pure/mod.rs
@@ -43,7 +43,7 @@ impl Pure<'_> {
     where
         N: for<'aa> VisitMutWith<Pure<'aa>> + Send + Sync,
     {
-        if self.ctx.par_depth >= MAX_PAR_DEPTH * 2 {
+        if self.ctx.par_depth >= MAX_PAR_DEPTH * 2 || true {
             for node in nodes {
                 let mut v = Pure {
                     options: self.options,
