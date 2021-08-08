1096f4335f3a79811be023ba83d40f8532652929
강동윤 <kdy1997.dev@gmail.com>
2021-08-08 20:26:36 +0900

    Update test refs

---
+++ tests/fixture/issue-2022/no-opt/output/index.js
@@ -0,0 +1,5 @@
+export function example(param, param1, param2) {
+    var a = param === void 0 ? 'click' : param, b = param1 === void 0 ? {
+    } : param1, c = param2 === void 0 ? 1 : param2;
+    console.log(a, b, c);
+}
