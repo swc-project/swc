1337876c5db9e840d62c3f8ca0552a240d41e5d1
강동윤 <kdy1997.dev@gmail.com>
2021-08-09 14:31:55 +0900

    Update test refs

--- tests/fixture/issue-2027/case1/output/index.js
+++ tests/fixture/issue-2027/case1/output/index.js
@@ -1,6 +1,6 @@
-var _loop = function (key) {
-    controller[key] = function (c) {
-        for (var _len = arguments.length, d = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+var _loop = function(key) {
+    controller[key] = function(c) {
+        for(var _len = arguments.length, d = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++){
             d[_key - 1] = arguments[_key];
         }
         console.log(keys[key]);
@@ -12,4 +12,4 @@ var keys = {
 };
 var controller = {
 };
-for (var key in keys) _loop(key);
+for(var key in keys)_loop(key);
