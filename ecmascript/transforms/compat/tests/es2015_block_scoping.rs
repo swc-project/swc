fb966dc899081d46e2fe597a91e33871eb11c4db
강동윤 <kdy1997.dev@gmail.com>
2021-08-09 14:35:04 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/es2015_block_scoping.rs
+++ ecmascript/transforms/compat/tests/es2015_block_scoping.rs
@@ -638,72 +638,73 @@ test!(
     |_| block_scoping(),
     issue_1415_1,
     "
-        export function test(items) {
-            const infoMap = new WeakMap();
-            for (let i = 0; i < items.length; i += 1) {
-              const item = items[i];
-              let info;
-              switch (item.type) {
-                case 'branch1':
-                  info = item.method1();
-                  break;
-                case 'branch2':
-                  info = item.method2();
-                  break;
-                case 'branch3':
-                  info = item.method3(
-                    Object.fromEntries(
-                      item.subItems.map((t) => [item.alias ?? t.name, getInfo(t)])
-                    )
-                  );
-                  break;
-                default:
-                  throw new Error('boom');
-              }
-              infoMap.set(item, info); // important
+    export function test(items) {
+        const infoMap = new WeakMap();
+        for (let i = 0; i < items.length; i += 1) {
+            const item = items[i];
+            let info;
+            switch (item.type) {
+            case 'branch1':
+                info = item.method1();
+                break;
+            case 'branch2':
+                info = item.method2();
+                break;
+            case 'branch3':
+                info = item.method3(
+                Object.fromEntries(
+                    item.subItems.map((t) => [item.alias ?? t.name, getInfo(t)])
+                )
+                );
+                break;
+            default:
+                throw new Error('boom');
             }
-          
-            function getInfo(item) {
-              if (!infoMap.has(item)) {
-                throw new Error('no info yet');
-              }
-              return infoMap.get(item);
-            }
-          }
-        ",
-    "
-        export function test(items) {
-            var infoMap = new WeakMap();
-            for(var i = 0; i < items.length; i += 1){
-                var item = items[i];
-                var info = void 0;
-                switch(item.type){
-                    case 'branch1':
-                        info = item.method1();
-                        break;
-                    case 'branch2':
-                        info = item.method2();
-                        break;
-                    case 'branch3':
-                        info = item.method3(Object.fromEntries(item.subItems.map((t)=>[
-                                item.alias ?? t.name,
-                                getInfo(t)
-                            ]
-                        )));
-                        break;
-                    default:
-                        throw new Error('boom');
-                }
-                infoMap.set(item, info);
-            }
-            function getInfo(item) {
-                if (!infoMap.has(item)) {
-                    throw new Error('no info yet');
-                }
-                return infoMap.get(item);
-            }
+            infoMap.set(item, info); // important
         }
-        "
+        
+        function getInfo(item) {
+            if (!infoMap.has(item)) {
+            throw new Error('no info yet');
+            }
+            return infoMap.get(item);
+        }
+    }
+    ",
+    "
+    export function test(items) {
+        var _loop = function(i) {
+            var item = items[i];
+            var info = void 0;
+            switch(item.type){
+                case 'branch1':
+                    info = item.method1();
+                    break;
+                case 'branch2':
+                    info = item.method2();
+                    break;
+                case 'branch3':
+                    info = item.method3(Object.fromEntries(item.subItems.map((t)=>[
+                            item.alias ?? t.name,
+                            getInfo(t)
+                        ]
+                    )));
+                    break;
+                default:
+                    throw new Error('boom');
+            }
+            infoMap.set(item, info);
+        };
+        var infoMap = new WeakMap();
+        for(var i = 0; i < items.length; i += 1)_loop(i);
+        function getInfo(item) {
+            if (!infoMap.has(item)) {
+                throw new Error('no info yet');
+            }
+            return infoMap.get(item);
+        }
+    }
+    "
 );
 
 test!(
