20d4c1d6699efe0b4f7a27e117d532dc79398f5a
강동윤 <kdy1997.dev@gmail.com>
2021-08-09 15:24:36 +0900

    Remove wrong logic

--- ecmascript/transforms/compat/src/es2020/class_properties/used_name.rs
+++ ecmascript/transforms/compat/src/es2020/class_properties/used_name.rs
@@ -1,50 +1,7 @@
 use swc_atoms::JsWord;
-use swc_common::Mark;
 use swc_ecma_ast::*;
 use swc_ecma_visit::noop_visit_type;
-use swc_ecma_visit::{noop_fold_type, Fold, FoldWith, Node, Visit, VisitWith};
-
-/// Used to rename **binding** identifiers in constructor.
-pub(super) struct UsedNameRenamer<'a> {
-    pub mark: Mark,
-    pub used_names: &'a [JsWord],
-}
-
-impl<'a> Fold for UsedNameRenamer<'a> {
-    noop_fold_type!();
-
-    fn fold_expr(&mut self, e: Expr) -> Expr {
-        match e {
-            Expr::Ident(..) => e,
-            _ => e.fold_children_with(self),
-        }
-    }
-
-    fn fold_ident(&mut self, ident: Ident) -> Ident {
-        if self.used_names.contains(&ident.sym) {
-            return Ident {
-                span: ident.span.apply_mark(self.mark),
-                ..ident
-            };
-        }
-        ident
-    }
-
-    fn fold_member_expr(&mut self, e: MemberExpr) -> MemberExpr {
-        if e.computed {
-            MemberExpr {
-                obj: e.obj.fold_with(self),
-                prop: e.prop.fold_with(self),
-                ..e
-            }
-        } else {
-            MemberExpr {
-                obj: e.obj.fold_with(self),
-                ..e
-            }
-        }
-    }
-}
+use swc_ecma_visit::{Node, Visit, VisitWith};
 
 pub(super) struct UsedNameCollector<'a> {
     pub used_names: &'a mut Vec<JsWord>,
