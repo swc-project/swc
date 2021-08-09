4d2e45b131ca288fb533a0559ddf60c3d3ba4c64
강동윤 <kdy1997.dev@gmail.com>
2021-08-09 13:56:24 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/es2015_block_scoping.rs
+++ ecmascript/transforms/compat/tests/es2015_block_scoping.rs
@@ -902,5 +902,17 @@ test!(
       }
     ",
     "
+    var _loop = function(key) {
+        controller[key] = (c, ...d)=>{
+            console.log(keys[key]);
+        };
+    };
+    var keys = {
+        a: 1,
+        b: 2
+    };
+    var controller = {
+    };
+    for(var key in keys)_loop(key);
     "
 );
