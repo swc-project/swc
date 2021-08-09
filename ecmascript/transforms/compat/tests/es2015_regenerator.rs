52bef39e0644aa563b83b41943e8e9d804eb0d5e
강동윤 <kdy1997.dev@gmail.com>
2021-08-09 14:49:55 +0900

    Updat test refs

--- ecmascript/transforms/compat/tests/es2015_regenerator.rs
+++ ecmascript/transforms/compat/tests/es2015_regenerator.rs
@@ -1631,5 +1631,36 @@ test!(
     })();
     ",
     "
+    var regeneratorRuntime = require('regenerator-runtime');
+    _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        var sleep, result;
+        return regeneratorRuntime.wrap(function _callee$(_ctx) {
+            while(1)switch(_ctx.prev = _ctx.next){
+                case 0:
+                    sleep = function() {
+                        return new Promise(function(resolve) {
+                            return setTimeout(function() {
+                                return resolve(undefined);
+                            }, 500);
+                        });
+                    };
+                    _ctx.next = 3;
+                    return sleep();
+                case 3:
+                    _ctx.t0 = _ctx.sent;
+                    if (_ctx.t0) {
+                        _ctx.next = 6;
+                        break;
+                    }
+                    _ctx.t0 = 'fallback';
+                case 6:
+                    result = _ctx.t0;
+                    console.log(result);
+                case 8:
+                case 'end':
+                    return _ctx.stop();
+            }
+        }, _callee);
+    }))();
     "
 );
