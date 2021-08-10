0b310a0a2b8a155b310f55da68f796163666f0b0
강동윤 <kdy1997.dev@gmail.com>
2021-08-10 12:30:12 +0900

    fixup

---
+++ ecmascript/minifier/tests/compress/fixture/issues/2044/pass-10/output.js
@@ -0,0 +1,54 @@
+function createCommonjsModule(fn) {
+    return fn();
+}
+const isFile = (config)=>!0
+, pkgBrowserslist = {
+}, config = {
+};
+createCommonjsModule(function(module, exports) {
+    Object.defineProperty(exports, "__esModule", {
+        value: !0
+    });
+}), createCommonjsModule(function(module) {
+    module.exports = {
+        findConfig: function(from) {
+            return function(dir) {
+                if (1) throw new Error("");
+                if (pkgBrowserslist) throw new Error("");
+                if (1) throw new Error("");
+                return module.exports.findConfig(null);
+            };
+        }
+    };
+}), createCommonjsModule(function(module, exports) {
+    Object.defineProperty(exports, "__esModule", {
+        value: !0
+    });
+}), createCommonjsModule(function(module, exports) {
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+    _interopRequireDefault(), _interopRequireDefault();
+}), createCommonjsModule(function(module, exports) {
+    exports.default = null, module.exports = exports.default;
+}), createCommonjsModule(function(module, exports) {
+    exports.default = void 0;
+});
+var namespace$1 = createCommonjsModule(function(module, exports) {
+    exports.default = void 0, module.exports = exports.default;
+});
+createCommonjsModule(function(module, exports) {
+    exports.default = void 0, module.exports = exports.default;
+}), createCommonjsModule(function(module, exports) {
+    exports.default = void 0, exports.default = String;
+}), createCommonjsModule(function(module, exports) {
+    exports.default = void 0, exports.default = String;
+}), createCommonjsModule(function(module, exports) {
+    exports.__esModule = !0;
+}), createCommonjsModule(function(module, exports) {
+    exports.__esModule = !0;
+}), createCommonjsModule(function(module, exports) {
+    exports.__esModule = !0;
+});
