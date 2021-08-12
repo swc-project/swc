f68ed71bb5323023b9cdc22af05725be6758a464
강동윤 <kdy1997.dev@gmail.com>
2021-08-12 23:40:24 +0900

    Version

--- plugin/shared/src/lib.rs
+++ plugin/shared/src/lib.rs
@@ -10,6 +10,8 @@ use abi_stable::StableAbi;
 #[sabi(kind(Prefix(prefix_ref = "SwcPluginRef")))]
 #[sabi(missing_field(panic))]
 pub struct SwcPlugin {
+    pub get_js_ast_version: extern "C" fn() -> RString,
+
     #[sabi(last_prefix_field)]
     pub new_js_transform: extern "C" fn() -> JsTransformBox,
 }
@@ -24,6 +26,7 @@ impl RootModule for SwcPluginRef {
 
 #[sabi_trait]
 pub trait JsTransform {
+    /// Should return ast as json string,
     fn process(&mut self, config_str: RStr, ast_json: RStr) -> RResult<RString, RString>;
 }
 
