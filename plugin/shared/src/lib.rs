6ea3476175e81c513a86722a9ddac4a4d03194ec
강동윤 <kdy1997.dev@gmail.com>
2021-08-12 23:36:29 +0900

    Use abi_stable

--- plugin/shared/src/lib.rs
+++ plugin/shared/src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use abi_stable::library::RootModule;
+use abi_stable::package_version_strings;
+use abi_stable::sabi_trait;
+use abi_stable::sabi_types::VersionStrings;
+use abi_stable::std_types::{RBox, RResult, RStr, RString};
+use abi_stable::StableAbi;
+
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(kind(Prefix(prefix_ref = "SwcPluginRef")))]
+#[sabi(missing_field(panic))]
+pub struct SwcPlugin {
+    #[sabi(last_prefix_field)]
+    pub new_js_transform: extern "C" fn() -> JsTransformBox,
 }
+
+impl RootModule for SwcPluginRef {
+    abi_stable::declare_root_module_statics! {SwcPluginRef}
+
+    const BASE_NAME: &'static str = "swc_plugin";
+    const NAME: &'static str = "swc_plugin";
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
+}
+
+#[sabi_trait]
+pub trait JsTransform {
+    fn process(&mut self, config_str: RStr, ast_json: RStr) -> RResult<RString, RString>;
+}
+
+pub type JsTransformBox = JsTransform_TO<'static, RBox<()>>;
