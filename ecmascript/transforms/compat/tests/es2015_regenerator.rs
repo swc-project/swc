6c4607204663e309763119fe686a285adcdabf51
강동윤 <kdy1997.dev@gmail.com>
2021-08-12 10:05:24 +0900

    Add tests for #1918

--- ecmascript/transforms/compat/tests/es2015_regenerator.rs
+++ ecmascript/transforms/compat/tests/es2015_regenerator.rs
@@ -1664,3 +1664,42 @@ test!(
     }))();
     "
 );
+
+test_exec!(
+    Syntax::default(),
+    |_| {
+        let mark = Mark::fresh(Mark::root());
+        chain!(async_to_generator(), regenerator(mark))
+    },
+    issue_1918_1,
+    "
+    let resolve;
+    let promise = new Promise((r) => (resolve = r));
+    let iterable = {
+    [Symbol.asyncIterator]() {
+        return {
+        next() {
+            return promise;
+        },
+        };
+    },
+    };
+
+    (async () => {
+    for await (let value of iterable) {
+        console.log(value);
+    }
+    })();
+
+    (async function () {
+    resolve({ value: 0, done: false });
+    promise = new Promise((r) => (resolve = r));
+
+    await null;
+    resolve({ value: 1, done: false });
+    promise = new Promise((r) => (resolve = r));
+
+    resolve({ value: undefined, done: true });
+    })();
+    "
+);
--- ecmascript/transforms/compat/tests/es2017_async_to_generator.rs
+++ ecmascript/transforms/compat/tests/es2017_async_to_generator.rs
@@ -2641,3 +2641,39 @@ test_exec!(
     printValues()
     "
 );
+
+test_exec!(
+    Syntax::default(),
+    |_| async_to_generator(),
+    issue_1918_1,
+    "
+    let resolve;
+    let promise = new Promise((r) => (resolve = r));
+    let iterable = {
+    [Symbol.asyncIterator]() {
+        return {
+        next() {
+            return promise;
+        },
+        };
+    },
+    };
+
+    (async () => {
+    for await (let value of iterable) {
+        console.log(value);
+    }
+    })();
+
+    (async function () {
+    resolve({ value: 0, done: false });
+    promise = new Promise((r) => (resolve = r));
+
+    await null;
+    resolve({ value: 1, done: false });
+    promise = new Promise((r) => (resolve = r));
+
+    resolve({ value: undefined, done: true });
+    })();
+    "
+);
