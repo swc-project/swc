b9e887bf3881c6255464c760ded103e5457efee2
강동윤 <kdy1997.dev@gmail.com>
2021-08-12 19:02:12 +0900

    fixup

--- tests/fixture/issue-1918/es2016/input/index.js
+++ tests/fixture/issue-1918/es2016/input/index.js
@@ -1,32 +1,34 @@
-let counter = 0;
-let resolve;
-let promise = new Promise((r) => (resolve = r));
-let iterable = {
-    [Symbol.asyncIterator]() {
-        return {
-            next() {
-                return promise;
-            },
-        };
-    },
-};
+(async function () {
+    let counter = 0;
+    let resolve;
+    let promise = new Promise((r) => (resolve = r));
+    let iterable = {
+        [Symbol.asyncIterator]() {
+            return {
+                next() {
+                    return promise;
+                },
+            };
+        },
+    };
 
-const res = (async () => {
-    for await (let value of iterable) {
-        counter++;
-        console.log(value);
+    const res = (async () => {
+        for await (let value of iterable) {
+            counter++;
+            console.log(value);
+        }
+
+        expect(counter).toBe(2);
+    })();
+
+
+    for (let v of [0, 1]) {
+        await null;
+        let oldresolve = resolve;
+        promise = new Promise((r) => (resolve = r));
+        oldresolve({ value: v, done: false });
     }
+    resolve({ value: undefined, done: true });
 
-    expect(counter).toBe(2);
-})();
-
-
-for (let v of [0, 1]) {
-    await null;
-    let oldresolve = resolve;
-    promise = new Promise((r) => (resolve = r));
-    oldresolve({ value: v, done: false });
-}
-resolve({ value: undefined, done: true });
-
-await res;
+    await res;
+})()
