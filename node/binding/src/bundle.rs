8cdf246d1a61c5d2ae4bbf86543f0ab3ede410d2
강동윤 <kdy1997.dev@gmail.com>
2021-08-13 16:27:16 +0900

    Fix

--- node/binding/src/bundle.rs
+++ node/binding/src/bundle.rs
@@ -35,7 +35,7 @@ struct StaticConfigItem {
     #[serde(default)]
     working_dir: String,
     #[serde(flatten)]
-    config: spack::config::Config,
+    config: swc_node_bundler::config::Config,
 }
 
 struct BundleTask {
@@ -187,7 +187,7 @@ pub(crate) fn bundle(cx: CallContext) -> napi::Result<JsObject> {
 
     let static_items: StaticConfigItem = cx.get_deserialized(0)?;
 
-    let loader = Box::new(spack::loaders::swc::SwcLoader::new(
+    let loader = Box::new(swc_node_bundler::loaders::swc::SwcLoader::new(
         c.clone(),
         static_items
             .config
