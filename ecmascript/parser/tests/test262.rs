90649188bb04e5c705b8842c822dc12e0a0182fe
강동윤 <kdy1997.dev@gmail.com>
2021-08-14 14:42:08 +0900

    Let's preserve test262 tests

--- ecmascript/parser/tests/test262.rs
+++ ecmascript/parser/tests/test262.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 use common::Normalizer;
 use std::{
     env,
--- ecmascript/transforms/base/tests/fixer_test262.rs
+++ ecmascript/transforms/base/tests/fixer_test262.rs
@@ -1,3 +1,6 @@
+#![feature(test)]
+extern crate test;
+
 use std::{
     env,
     fs::{read_dir, File},
