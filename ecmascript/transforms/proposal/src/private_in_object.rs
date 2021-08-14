d9b0ae90bb9397d80e4804134a385b967f7542dd
강동윤 <kdy1997.dev@gmail.com>
2021-08-15 07:26:06 +0900

    Add fixtures

--- ecmascript/transforms/proposal/src/private_in_object.rs
+++ ecmascript/transforms/proposal/src/private_in_object.rs
@@ -1,1 +1,10 @@
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
 
+    #[testing::fixture("tests/private-in-object/**/input.js")]
+    fn fixture(input: PathBuf) {}
+
+    #[testing::fixture("tests/private-in-object/**/exec.js")]
+    fn exec(input: PathBuf) {}
+}
