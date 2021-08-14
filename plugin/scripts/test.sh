bdbae7f2b2632b5b10f67ccea737e8b17bd3ff4f
강동윤 <kdy1997.dev@gmail.com>
2021-08-14 16:02:24 +0900

    Improve test.sh

--- plugin/scripts/test.sh
+++ plugin/scripts/test.sh
@@ -2,4 +2,13 @@
 
 ## This script assumes `plugins` is in `$CDPATH`
 
+set -eu
+
 (cd plugins && cargo build && ls -al target/debug)
+
+PLUGINS_DIR=$(cd plugins && pwd)
+echo "Using plugins at $PLUGINS_DIR"
+
+export PATH=${PLUGINS_DIR}/target/debug/:$PATH
+
+cargo test
