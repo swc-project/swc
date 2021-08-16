f05657d3c31785f1bfd6aebfc8a83b0e351377b1
강동윤 <kdy1997.dev@gmail.com>
2021-08-16 18:49:55 +0900

    Support query

--- css/ast/src/at_rule/support.rs
+++ css/ast/src/at_rule/support.rs
@@ -1,5 +1,7 @@
 use swc_common::{ast_node, Span};
 
+use crate::Property;
+
 #[ast_node("SupportsRule")]
 pub struct SupportsRule {
     pub span: Span,
@@ -11,10 +13,33 @@ pub struct SupportsRule {
 pub enum SupportQuery {
     #[tag("NotSupportQuery")]
     Not(NotSupportQuery),
+
+    #[tag("AndSupportQuery")]
+    And(AndSupportQuery),
+
+    #[tag("OrSupportQuery")]
+    Or(OrSupportQuery),
+
+    #[tag("Property")]
+    Property(Property),
 }
 
 #[ast_node("NotSupportQuery")]
 pub struct NotSupportQuery {
     pub span: Span,
     pub query: Box<SupportQuery>,
+}
+
+#[ast_node("AndSupportQuery")]
+pub struct AndSupportQuery {
+    pub span: Span,
+    pub left: Box<SupportQuery>,
+    pub right: Box<SupportQuery>,
+}
+
+#[ast_node("OrSupportQuery")]
+pub struct OrSupportQuery {
+    pub span: Span,
+    pub left: Box<SupportQuery>,
+    pub right: Box<SupportQuery>,
 }
