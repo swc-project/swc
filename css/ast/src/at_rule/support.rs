8e30dd950205dae736cedbab5a3472e9fb58762c
강동윤 <kdy1997.dev@gmail.com>
2021-08-16 18:48:15 +0900

    WIP: Suport query

--- css/ast/src/at_rule/support.rs
+++ css/ast/src/at_rule/support.rs
@@ -3,4 +3,18 @@ use swc_common::{ast_node, Span};
 #[ast_node("SupportsRule")]
 pub struct SupportsRule {
     pub span: Span,
+
+    pub query: SupportQuery,
+}
+
+#[ast_node]
+pub enum SupportQuery {
+    #[tag("NotSupportQuery")]
+    Not(NotSupportQuery),
+}
+
+#[ast_node("NotSupportQuery")]
+pub struct NotSupportQuery {
+    pub span: Span,
+    pub query: Box<SupportQuery>,
 }
