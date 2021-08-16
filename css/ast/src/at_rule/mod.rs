62102e72eaa60e0476dae3bd782ff5dda69a8f8a
강동윤 <kdy1997.dev@gmail.com>
2021-08-16 15:06:45 +0900

    WIP: Adding @ rules

--- css/ast/src/at_rule/mod.rs
+++ css/ast/src/at_rule/mod.rs
@@ -13,21 +13,37 @@ pub enum AtRule {
 
     #[tag("FontFaceRule")]
     FontFace(FontFaceRule),
+
+    #[tag("KeyframesRule")]
+    Keyframes(KeyframesRule),
+
+    #[tag("MediaRule")]
+    Media(MediaRule),
 }
 
-#[ast_node]
+#[ast_node("CharsetRule")]
 pub struct CharsetRule {
     pub span: Span,
     pub charset: Str,
 }
 
-#[ast_node]
+#[ast_node("ImportRule")]
 pub struct ImportRule {
     pub span: Span,
     pub src: Str,
 }
 
-#[ast_node]
+#[ast_node("FontFaceRule")]
 pub struct FontFaceRule {
+    pub span: Span,
+}
+
+#[ast_node("KeyframesRule")]
+pub struct KeyframesRule {
+    pub span: Span,
+}
+
+#[ast_node("MediaRule")]
+pub struct MediaRule {
     pub span: Span,
 }
