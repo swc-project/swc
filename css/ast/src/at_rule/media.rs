b2d9d587b4b681862760787b6119f2ab0bbf3bb4
강동윤 <kdy1997.dev@gmail.com>
2021-08-16 19:14:50 +0900

    Media query

--- css/ast/src/at_rule/media.rs
+++ css/ast/src/at_rule/media.rs
@@ -1,6 +1,27 @@
 use swc_common::{ast_node, Span};
 
+use crate::{Property, Rule};
+
 #[ast_node("MediaRule")]
 pub struct MediaRule {
     pub span: Span,
+
+    pub query: Box<MediaQuery>,
+
+    pub rules: Vec<Rule>,
+}
+
+#[ast_node]
+pub enum MediaQuery {
+    #[tag("AndMediaQuery")]
+    And(AndMediaQuery),
+    #[tag("Property")]
+    Property(Property),
+}
+
+#[ast_node("AndMediaQuery")]
+pub struct AndMediaQuery {
+    pub span: Span,
+    pub left: Box<MediaQuery>,
+    pub right: Box<MediaQuery>,
 }
