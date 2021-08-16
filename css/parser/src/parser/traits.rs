32eed67c26c7a546d05cbb619516062609d96aab
강동윤 <kdy1997.dev@gmail.com>
2021-08-16 18:59:18 +0900

    impl Parse<T>

--- css/parser/src/parser/traits.rs
+++ css/parser/src/parser/traits.rs
@@ -5,6 +5,14 @@ pub(super) trait Parse<T> {
     fn parse(&mut self) -> PResult<T>;
 }
 
+impl<T, P> Parse<Box<T>> for P
+where
+    Self: Parse<T>,
+{
+    fn parse(&mut self) -> PResult<Box<T>> {
+        self.parse().map(Box::new)
+    }
+}
 pub(super) trait ParseDelmited<T>: Parse<T> {
     fn eat_delimiter(&mut self) -> PResult<bool>;
 }
