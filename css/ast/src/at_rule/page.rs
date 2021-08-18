8230febfd44faab3d987ac9934d08e325842c271
강동윤 <kdy1997.dev@gmail.com>
2021-08-18 17:27:19 +0900

    Page rule

--- css/ast/src/at_rule/page.rs
+++ css/ast/src/at_rule/page.rs
@@ -1,6 +1,21 @@
 use swc_common::{ast_node, Span};
 
+use crate::{DeclBlock, Text};
+
 #[ast_node("PageRule")]
 pub struct PageRule {
     pub span: Span,
+
+    pub prelude: Vec<PageSelector>,
+
+    pub block: DeclBlock,
+}
+
+#[ast_node]
+pub struct PageSelector {
+    pub span: Span,
+
+    pub ident: Option<Text>,
+
+    pub pseudo: Option<Text>,
 }
