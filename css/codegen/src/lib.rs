addb37e5d439ca148228426346e32df6f5959940
강동윤 <kdy1997.dev@gmail.com>
2021-08-20 19:35:45 +0900

    Fix emit_complex_selector

--- css/codegen/src/lib.rs
+++ css/codegen/src/lib.rs
@@ -646,7 +646,7 @@ where
 
     #[emitter]
     fn emit_complex_selector(&mut self, n: &ComplexSelector) -> Result {
-        self.emit_list(&n.selectors, ListFormat::NotDelimited)?;
+        self.emit_list(&n.selectors, ListFormat::SpaceDelimited)?;
     }
 
     #[emitter]
