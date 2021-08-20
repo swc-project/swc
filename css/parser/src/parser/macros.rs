09ca7a321732f50a1fb02fedf0777de3ccdcb3b9
강동윤 <kdy1997.dev@gmail.com>
2021-08-20 18:24:05 +0900

    Fix macros

--- css/parser/src/parser/macros.rs
+++ css/parser/src/parser/macros.rs
@@ -127,7 +127,6 @@ macro_rules! expect {
         }
 
         if !eat!($parser, $tt) {
-            dbg!();
             let span = $parser.input.cur_span()?;
             Err(crate::error::Error::new(
                 span,
