fa923a48b3ce1627436a24aee9ab3c257d23d812
강동윤 <kdy1997.dev@gmail.com>
2021-08-20 18:17:07 +0900

    DropSpan

--- css/codegen/tests/fixture.rs
+++ css/codegen/tests/fixture.rs
@@ -1,11 +1,12 @@
 use std::path::PathBuf;
-use swc_common::FileName;
+use swc_common::{FileName, Span};
 use swc_css_ast::Stylesheet;
 use swc_css_codegen::{
     writer::basic::{BasicCssWriter, BasicCssWriterConfig},
     CodeGenerator, CodegenConfig, Emit,
 };
 use swc_css_parser::{parse_file, parser::ParserConfig};
+use swc_css_visit::{VisitMut, VisitMutWith};
 
 #[testing::fixture("../parser/tests/fixture/**/input.css")]
 fn parse_again(input: PathBuf) {
@@ -14,7 +15,8 @@ fn parse_again(input: PathBuf) {
 
         eprintln!("==== ==== Input ==== ====\n{}\n", fm.src);
 
-        let stylesheet: Stylesheet = parse_file(&fm, ParserConfig { parse_values: true }).unwrap();
+        let mut stylesheet: Stylesheet =
+            parse_file(&fm, ParserConfig { parse_values: true }).unwrap();
 
         let mut css_str = String::new();
         {
@@ -27,11 +29,23 @@ fn parse_again(input: PathBuf) {
         eprintln!("==== ==== Codegen ==== ====\n{}\n", css_str);
 
         let new_fm = cm.new_source_file(FileName::Anon, css_str);
-        let parsed: Stylesheet = parse_file(&new_fm, ParserConfig { parse_values: true }).unwrap();
+        let mut parsed: Stylesheet =
+            parse_file(&new_fm, ParserConfig { parse_values: true }).unwrap();
+
+        stylesheet.visit_mut_with(&mut DropSpan);
+        parsed.visit_mut_with(&mut DropSpan);
 
         assert_eq!(stylesheet, parsed);
 
         Ok(())
     })
     .unwrap();
+}
+
+struct DropSpan;
+
+impl VisitMut for DropSpan {
+    fn visit_mut_span(&mut self, n: &mut Span) {
+        *n = Default::default()
+    }
 }
