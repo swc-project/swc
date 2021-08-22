d7d7dec6112afa718761fddd4a676a57e3384e70
강동윤 <kdy1997.dev@gmail.com>
2021-08-22 14:34:14 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/fixture/new-target/general/arrow/output.js
+++ ecmascript/transforms/compat/tests/fixture/new-target/general/arrow/output.js
@@ -1,18 +1,14 @@
 function Foo() {
-  var _newtarget = this instanceof Foo ? this.constructor : void 0;
-
-  const a = function () {
-    _newtarget;
-  };
-}
-
-class Bar {
-  constructor() {
-    var _newtarget2 = this.constructor;
-
-    const a = function () {
-      _newtarget2;
+    var _newtarget = this instanceof Foo ? this.constructor : void 0;
+    const a = ()=>{
+        _newtarget;
     };
-  }
-
+}
+class Bar {
+    constructor(){
+        var _newtarget = this.constructor;
+        const a = ()=>{
+            _newtarget;
+        };
+    }
 }
