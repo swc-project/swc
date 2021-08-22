1e55e251a83813344fafb57c891b0950f33ff079
강동윤 <kdy1997.dev@gmail.com>
2021-08-22 13:34:30 +0900

    Fix test suite

--- ecmascript/transforms/compat/tests/es2015_new_target.rs
+++ ecmascript/transforms/compat/tests/es2015_new_target.rs
@@ -1,11 +1,11 @@
 use std::{fs::read_to_string, path::PathBuf};
 use swc_ecma_transforms_compat::es2015::new_target::new_target;
-use swc_ecma_transforms_testing::{compare_stdout, test_fixture};
+use swc_ecma_transforms_testing::{exec_tr, test_fixture};
 
 #[testing::fixture("tests/fixture/new-target/**/exec.js")]
 fn exec(input: PathBuf) {
     let input = read_to_string(&input).unwrap();
-    compare_stdout(Default::default(), |_| new_target(), &input);
+    exec_tr("new-target", Default::default(), |_| new_target(), &input);
 }
 
 #[testing::fixture("tests/fixture/new-target/**/input.js")]
