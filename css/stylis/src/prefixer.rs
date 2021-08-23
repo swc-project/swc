8b64213fcdccfa0a124841a7fc906ee36cd187f7
강동윤 <kdy1997.dev@gmail.com>
2021-08-23 15:11:28 +0900

    Prepare

--- css/stylis/src/prefixer.rs
+++ css/stylis/src/prefixer.rs
@@ -1,9 +1,30 @@
-use swc_css_visit::VisitMut;
+use std::mem::take;
+
+use swc_css_ast::*;
+use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub fn prefixer() -> impl VisitMut {
-    Prefixer
+    Prefixer::default()
 }
 
-struct Prefixer;
+#[derive(Default)]
+struct Prefixer {
+    added: Vec<Property>,
+}
 
-impl VisitMut for Prefixer {}
+impl VisitMut for Prefixer {
+    fn visit_mut_properties(&mut self, props: &mut Vec<Property>) {
+        let mut new = vec![];
+        for mut n in take(props) {
+            n.visit_mut_with(self);
+            new.extend(self.added.drain(..));
+            new.push(n);
+        }
+
+        *props = new;
+    }
+
+    fn visit_mut_property(&mut self, n: &mut Property) {
+        n.visit_mut_children_with(self);
+    }
+}
