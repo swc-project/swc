8823d2b8737309f5fa1f9233a45f1ccc2754bcf4
강동윤 <kdy1997.dev@gmail.com>
2021-08-23 15:46:50 +0900

    More work

--- css/stylis/src/prefixer.rs
+++ css/stylis/src/prefixer.rs
@@ -58,42 +58,56 @@ impl VisitMut for Prefixer {
         }
 
         match &*n.name.value {
+            "writing-mode" => {
+                if n.values.len() == 1 {
+                    match &n.values[0] {
+                        Value::Text(Text { value, .. }) => match &**value {
+                            "none" => {
+                                same_content!("-webkit-writing-mode");
+                                same_content!("-ms-writing-mode");
+                            }
+
+                            _ => {}
+                        },
+
+                        _ => {}
+                    }
+                }
+            }
+
             "min-width" | "width" | "max-width" | "min-height" | "height" | "max-height"
             | "min-block-size" | "min-inline-size" => {
                 if n.values.len() == 1 {
                     match &n.values[0] {
-                        Value::Text(Text { value, .. }) => {
-                            //
-                            match &**value {
-                                "fit-content" => {
-                                    same_name!("-webkit-fit-content");
-                                    same_name!("-moz-fit-content");
-                                }
-
-                                "max-content" => {
-                                    same_name!("-webkit-max-content");
-                                    same_name!("-moz-max-content");
-                                }
-
-                                "min-content" => {
-                                    same_name!("-webkit-min-content");
-                                    same_name!("-moz-min-content");
-                                }
-
-                                "fill-available" => {
-                                    same_name!("-webkit-fill-available");
-                                    same_name!("-moz-available");
-                                }
-
-                                "stretch" => {
-                                    same_name!("-webkit-fill-available");
-                                    same_name!("-moz-available");
-                                    same_name!("fill-available");
-                                }
-
-                                _ => {}
+                        Value::Text(Text { value, .. }) => match &**value {
+                            "fit-content" => {
+                                same_name!("-webkit-fit-content");
+                                same_name!("-moz-fit-content");
                             }
-                        }
+
+                            "max-content" => {
+                                same_name!("-webkit-max-content");
+                                same_name!("-moz-max-content");
+                            }
+
+                            "min-content" => {
+                                same_name!("-webkit-min-content");
+                                same_name!("-moz-min-content");
+                            }
+
+                            "fill-available" => {
+                                same_name!("-webkit-fill-available");
+                                same_name!("-moz-available");
+                            }
+
+                            "stretch" => {
+                                same_name!("-webkit-fill-available");
+                                same_name!("-moz-available");
+                                same_name!("fill-available");
+                            }
+
+                            _ => {}
+                        },
                         _ => {}
                     }
                 }
