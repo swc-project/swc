e3b632eca77fb9ce2a4dd2a73f96f41825d30c22
강동윤 <kdy1997.dev@gmail.com>
2021-08-24 19:38:00 +0900

    Update test refs

--- ecmascript/transforms/proposal/tests/private-in-object/private/method/output.js
+++ ecmascript/transforms/proposal/tests/private-in-object/private/method/output.js
@@ -1,21 +1,19 @@
-var _foo = /*#__PURE__*/new WeakSet();
-
-let Foo = /*#__PURE__*/function () {
-  "use strict";
-
-  function Foo() {
-    babelHelpers.classCallCheck(this, Foo);
-
-    _foo.add(this);
-  }
-
-  babelHelpers.createClass(Foo, [{
-    key: "test",
-    value: function test(other) {
-      return _foo.has(other);
+var _foo = new WeakSet();
+let Foo = function() {
+    "use strict";
+    function Foo() {
+        _classCallCheck(this, Foo);
+        _foo.add(this);
     }
-  }]);
-  return Foo;
+    _createClass(Foo, [
+        {
+            key: "test",
+            value: function test(other) {
+                return _foo.has(other);
+            }
+        }
+    ]);
+    return Foo;
 }();
-
-function _foo2() {}
+function foo() {
+}
