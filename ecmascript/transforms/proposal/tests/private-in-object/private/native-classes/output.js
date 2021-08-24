9241c8a3813c6055888a3b6544c5d6ed0c9021af
강동윤 <kdy1997.dev@gmail.com>
2021-08-24 19:46:17 +0900

    Update test refs

--- ecmascript/transforms/proposal/tests/private-in-object/private/native-classes/output.js
+++ ecmascript/transforms/proposal/tests/private-in-object/private/native-classes/output.js
@@ -1,24 +1,19 @@
-var _bar = /*#__PURE__*/new WeakMap();
-
 class Foo {
-  constructor() {
-    _bar.set(this, {
-      writable: true,
-      value: "bar"
-    });
-  }
-
-  static test() {
-    return Foo === Foo;
-  }
-
-  test() {
-    return _bar.has(this);
-  }
-
+    static test() {
+        return Foo === Foo;
+    }
+    test() {
+        return _bar.has(this);
+    }
+    constructor(){
+        _bar.set(this, {
+            writable: true,
+            value: "bar"
+        });
+    }
 }
-
 var _foo = {
-  writable: true,
-  value: "foo"
+    writable: true,
+    value: "foo"
 };
+var _bar = new WeakMap();
