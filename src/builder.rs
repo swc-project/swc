5234cb47791e6c6b00f5abd8fb560af770bbee01
강동윤 <kdy1997.dev@gmail.com>
2021-08-24 23:31:55 +0900

    Add `jsc.experimental.optimizeHygiene`

--- src/builder.rs
+++ src/builder.rs
@@ -30,6 +30,7 @@ pub struct PassBuilder<'a, 'b, P: swc_ecma_visit::Fold> {
     target: JscTarget,
     loose: bool,
     hygiene: Option<hygiene::Config>,
+    optimize_hygiene: bool,
     fixer: bool,
     inject_helpers: bool,
     minify: Option<JsMinifyOptions>,
@@ -46,12 +47,13 @@ impl<'a, 'b, P: swc_ecma_visit::Fold> PassBuilder<'a, 'b, P> {
         PassBuilder {
             cm,
             handler,
+            env: None,
             pass,
-            target: JscTarget::Es5,
             global_mark,
+            target: JscTarget::Es5,
             loose,
             hygiene: Some(Default::default()),
-            env: None,
+            optimize_hygiene: false,
             fixer: true,
             inject_helpers: true,
             minify: None,
@@ -66,12 +68,13 @@ impl<'a, 'b, P: swc_ecma_visit::Fold> PassBuilder<'a, 'b, P> {
         PassBuilder {
             cm: self.cm,
             handler: self.handler,
+            env: self.env,
             pass,
+            global_mark: self.global_mark,
             target: self.target,
             loose: self.loose,
             hygiene: self.hygiene,
-            env: self.env,
-            global_mark: self.global_mark,
+            optimize_hygiene: self.optimize_hygiene,
             fixer: self.fixer,
             inject_helpers: self.inject_helpers,
             minify: self.minify,
@@ -99,6 +102,11 @@ impl<'a, 'b, P: swc_ecma_visit::Fold> PassBuilder<'a, 'b, P> {
     /// If you pass [None] to this method, the `hygiene` pass will be disabled.
     pub fn hygiene(mut self, config: Option<hygiene::Config>) -> Self {
         self.hygiene = config;
+        self
+    }
+
+    pub fn optimize_hygiene(mut self, enable: bool) -> Self {
+        self.optimize_hygiene = enable;
         self
     }
 
@@ -227,8 +235,10 @@ impl<'a, 'b, P: swc_ecma_visit::Fold> PassBuilder<'a, 'b, P> {
             }),
             Optional::new(
                 chain!(
-                    unique_scope(),
-                    hygiene_optimizer(self.global_mark),
+                    Optional::new(
+                        chain!(unique_scope(), hygiene_optimizer(self.global_mark)),
+                        self.optimize_hygiene
+                    ),
                     hygiene_with_config(self.hygiene.clone().unwrap_or_default())
                 ),
                 self.hygiene.is_some()
