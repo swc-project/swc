73da1ce8f5cd11e0cc99206d67e6f1a880774843
강동윤 <kdy1997.dev@gmail.com>
2021-08-24 15:58:12 +0900

    Update test refs

---
+++ node/bundler/tests/pass/issue-2124/named-export/output/entry.js
@@ -0,0 +1,36 @@
+function __spack_require__(mod) {
+    var cache;
+    if (cache) {
+        return cache;
+    }
+    var module = {
+        exports: {
+        }
+    };
+    mod(module, module.exports);
+    cache = module.exports;
+    return cache;
+}
+var load = __spack_require__.bind(void 0, function(module, exports) {
+    // This is a minimal reproduction from lodash@4.17.21
+    function lodash(value) {
+        console.log('lodash');
+    }
+    function memoize() {
+        console.log('memoize');
+    }
+    lodash.memoize = memoize;
+    // Either of these lines cause this module
+    // not to be included in the bundle. Member expression
+    // on `module` or `exports`.
+    module.exports = lodash;
+    exports.memoize = memoize; // NOTE: Indirection on `exports` will work
+     //const exporter = exports;
+     //exporter.memoize = memoize;
+});
+const _cjs_module_ = load(), memoize = _cjs_module_.memoize;
+// Import directly from `lodash` instead and the module code is
+// included in the bundle
+//import {memoize} from './lodash';
+const name = memoize();
+console.log(name);
