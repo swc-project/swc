19a12c1777eaed040a1914735150dbadf6e7200b
강동윤 <kdy1997.dev@gmail.com>
2021-08-25 12:34:22 +0900

    Update test refs

--- ecmascript/transforms/proposal/tests/private-in-object/to-native-fields/static-shadowed-binding/output.js
+++ ecmascript/transforms/proposal/tests/private-in-object/to-native-fields/static-shadowed-binding/output.js
@@ -1,12 +1,9 @@
-var _fooBrandCheck = /*#__PURE__*/new WeakSet();
-
+var _brand_check_foo = new WeakSet();
 class A {
-  static #foo = void _fooBrandCheck.add(this);
-
-  test() {
-    let A = function fn(A) {
-      return _fooBrandCheck.has(A);
-    };
-  }
-
+    static #foo = void _brand_check_foo.add(this);
+    test() {
+        let A = function fn(A) {
+            return _brand_check_foo.has(A);
+        };
+    }
 }
