ec1368dc4b973c9e14e305935f80d3b486658f79
강동윤 <kdy1997.dev@gmail.com>
2021-08-25 22:40:47 +0900

    init

--- plugin/cli/src/main.rs
+++ plugin/cli/src/main.rs
@@ -1,13 +1,16 @@
 use build::BuildCommand;
+use init::InitCommand;
 use publish::PublishCommand;
 use structopt::StructOpt;
 
 mod build;
+mod init;
 mod package;
 mod publish;
 
 #[derive(Debug, StructOpt)]
 enum Cmd {
+    Init(InitCommand),
     Build(BuildCommand),
     Package(BuildCommand),
     Publish(PublishCommand),
@@ -17,6 +20,7 @@ fn main() {
     let cmd = Cmd::from_args();
 
     match cmd {
+        Cmd::Init(_) => todo!(),
         Cmd::Build(_) => todo!(),
         Cmd::Package(_) => todo!(),
         Cmd::Publish(_) => todo!(),
