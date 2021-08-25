e0e7123a3ff1505e353186cc9bfc8dc1afc873cb
강동윤 <kdy1997.dev@gmail.com>
2021-08-25 09:49:37 +0900

    Use marker

--- ecmascript/transforms/proposal/src/private_in_object.rs
+++ ecmascript/transforms/proposal/src/private_in_object.rs
@@ -1,15 +1,20 @@
 use std::borrow::Cow;
-use swc_common::pass::CompilerPass;
+use swc_common::{pass::CompilerPass, Mark};
 use swc_ecma_ast::*;
 use swc_ecma_transforms_base::pass::JsPass;
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, VisitMut, VisitMutWith};
 
 /// https://github.com/tc39/proposal-private-fields-in-in
 pub fn private_in_object() -> impl JsPass {
-    as_folder(PrivateInObject {})
+    as_folder(PrivateInObject { mark: Mark::root() })
 }
 
-struct PrivateInObject {}
+struct PrivateInObject {
+    /// [Mark] for the current class.
+    ///
+    /// This is modified the class visitor.
+    mark: Mark,
+}
 
 impl CompilerPass for PrivateInObject {
     fn name() -> Cow<'static, str> {
@@ -19,6 +24,16 @@ impl CompilerPass for PrivateInObject {
 
 impl VisitMut for PrivateInObject {
     noop_visit_mut_type!();
+
+    fn visit_mut_class(&mut self, n: &mut Class) {
+        let old_mark = self.mark;
+
+        self.mark = Mark::fresh(Mark::root());
+
+        n.visit_mut_children_with(self);
+
+        self.mark = old_mark;
+    }
 
     fn visit_mut_member_expr(&mut self, e: &mut MemberExpr) {
         e.obj.visit_mut_with(self);
