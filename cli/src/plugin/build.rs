69d7c84f6e3c9c2079cf0e4aaac3aac2e3ae9d60
강동윤 <kdy1997.dev@gmail.com>
2021-08-26 16:06:37 +0900

    Get target directory

--- cli/src/plugin/build.rs
+++ cli/src/plugin/build.rs
@@ -1,6 +1,9 @@
 use anyhow::{bail, Context, Error};
+use std::env;
 use structopt::StructOpt;
 use tokio::process::Command;
+
+use crate::util::cargo::cargo_target_dir;
 
 /// Build your plugin using `cargo`.
 #[derive(Debug, StructOpt)]
@@ -13,6 +16,10 @@ pub struct BuildCommand {
 impl BuildCommand {
     pub async fn run(self) -> Result<(), Error> {
         run_cargo_build(self.release).await?;
+        let path = env::current_dir().context("failed to get current directory")?;
+
+        let target_dir = cargo_target_dir(&path).await?;
+        tracing::debug!("Cargo target directory: {}", target_dir.display());
 
         Ok(())
     }
