71ab8a673ab4f6ac05bdd7840e83150af0bcf757
강동윤 <kdy1997.dev@gmail.com>
2021-08-26 19:57:36 +0900

    More work for plugin testing

--- plugin/runner/tests/ex.rs
+++ plugin/runner/tests/ex.rs
@@ -1,5 +1,25 @@
-use std::path::Path;
+use std::{env, path::Path, process::Command};
 
-fn cargo_run(dir: &Path) {
-    
+fn build(crate_name: &str) {
+    let status = Command::new("cargo")
+        .arg("build")
+        .arg("-p")
+        .current_dir(
+            env::current_dir()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .join("examples")
+                .join(crate_name),
+        )
+        .arg(crate_name)
+        .status()
+        .expect("failed to run cargo build");
+
+    assert!(status.success());
+}
+
+#[test]
+fn drop_console() {
+    build("drop_console");
 }
