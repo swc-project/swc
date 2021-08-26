8346c712b0fafc4299d35a87b8a347749c84311e
강동윤 <kdy1997.dev@gmail.com>
2021-08-26 16:13:28 +0900

    Add command

--- cli/src/plugin/mod.rs
+++ cli/src/plugin/mod.rs
@@ -1,5 +1,6 @@
 use self::{
     build::BuildCommand, init::InitCommand, package::PackageCommand, publish::PublishCommand,
+    upgrade_deps::UpgradeDepsCommand,
 };
 use anyhow::Error;
 use structopt::StructOpt;
@@ -8,6 +9,7 @@ pub mod build;
 pub mod init;
 pub mod package;
 pub mod publish;
+pub mod upgrade_deps;
 
 /// Manages the plugin. Used for developing plugins.
 #[derive(Debug, StructOpt)]
@@ -16,6 +18,7 @@ pub enum PluginCommand {
     Build(BuildCommand),
     Package(PackageCommand),
     Publish(PublishCommand),
+    UpgradeDeps(UpgradeDepsCommand),
 }
 
 impl PluginCommand {
@@ -25,6 +28,7 @@ impl PluginCommand {
             PluginCommand::Build(cmd) => cmd.run().await,
             PluginCommand::Package(_) => todo!(),
             PluginCommand::Publish(_) => todo!(),
+            PluginCommand::UpgradeDeps(_) => todo!(),
         }
     }
 }
