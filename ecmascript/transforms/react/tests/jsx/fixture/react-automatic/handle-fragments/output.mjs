f566d31ead00a4ed64dd52d5ac2df43afadd0f75
강동윤 <kdy1997.dev@gmail.com>
2021-08-30 00:55:12 +0900

    Update test refs

--- ecmascript/transforms/react/tests/jsx/fixture/react-automatic/handle-fragments/output.mjs
+++ ecmascript/transforms/react/tests/jsx/fixture/react-automatic/handle-fragments/output.mjs
@@ -1,7 +1,5 @@
 import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
 var x = _jsx(_Fragment, {
-    children: [
-        _jsx("div", {
-        })
-    ]
+    children: _jsx("div", {
+    })
 });
