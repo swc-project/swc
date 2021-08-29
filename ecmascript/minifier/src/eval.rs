8373d3da0f7ea293d42e1cc74169a82e373e3549
강동윤 <kdy1997.dev@gmail.com>
2021-08-29 13:14:59 +0900

    Some work

---
+++ ecmascript/minifier/src/eval.rs
@@ -0,0 +1,100 @@
+use crate::mode::Mode;
+use fxhash::FxHashMap;
+use swc_ecma_ast::*;
+use swc_ecma_utils::ExprExt;
+
+pub struct Evaluator {
+    module: Module,
+    store: EvalStore,
+}
+
+#[derive(Default)]
+struct EvalStore {
+    pub caceh: FxHashMap<Box<Expr>, Lit>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum EvalResult {
+    Lit(Lit),
+    Undefined,
+}
+
+impl Mode for EvalStore {}
+
+impl Evaluator {
+    pub fn eval(&mut self, e: &Expr) -> Option<EvalResult> {
+        match e {
+            Expr::Seq(s) => return self.eval(s.exprs.last()?),
+
+            Expr::Lit(l @ Lit::Null(..))
+            | Expr::Lit(l @ Lit::Num(..) | l @ Lit::Str(..) | l @ Lit::BigInt(..)) => {
+                return Some(EvalResult::Lit(l.clone()))
+            }
+
+            Expr::Tpl(t) => {
+                return self.eval_quasis(&t);
+            }
+
+            Expr::TaggedTpl(t) => {
+                // Handle `String.raw`
+
+                match &*t.tag {
+                    Expr::Member(MemberExpr {
+                        span,
+                        obj: ExprOrSuper::Expr(tag_obj),
+                        prop,
+                        computed: false,
+                    }) if tag_obj.is_ident_ref_to("String".into())
+                        && prop.is_ident_ref_to("raw".into()) =>
+                    {
+                        return self.eval_quasis(&t.tpl);
+                    }
+
+                    _ => {}
+                }
+            }
+
+            Expr::Cond(c) => {
+                let test = self.eval(&c.test)?;
+
+                if is_truthy(&test)? {
+                    return self.eval(&c.cons);
+                } else {
+                    return self.eval(&c.alt);
+                }
+            }
+
+            // TypeCastExpression, ExpressionStatement etc
+            Expr::TsTypeAssertion(e) => {
+                return self.eval(&e.expr);
+            }
+
+            Expr::TsConstAssertion(e) => {
+                return self.eval(&e.expr);
+            }
+
+            // "foo".length
+            Expr::Member(MemberExpr {
+                span,
+                obj: ExprOrSuper::Expr(obj),
+                prop,
+                computed: false,
+                ..
+            }) if obj.is_lit() && prop.is_ident_ref_to("length".into()) => {}
+
+            _ => {}
+        }
+
+        match e {
+            Expr::Ident(i) => {}
+
+            _ => {}
+        }
+
+        None
+    }
+
+    fn eval_quasis(&mut self, q: &Tpl) -> Option<EvalResult> {}
+}
+
+fn is_truthy(lit: &EvalResult) -> Option<bool> {}
--- ecmascript/minifier/src/mode.rs
+++ ecmascript/minifier/src/mode.rs
@@ -3,7 +3,3 @@ pub trait Mode {}
 pub struct Minification;
 
 impl Mode for Minification {}
-
-pub struct Eval;
-
-impl Mode for Eval {}
