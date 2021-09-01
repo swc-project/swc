1fba136cf1bac60616fdc250d541b6cb3533069e
강동윤 <kdy1997.dev@gmail.com>
2021-09-01 17:30:48 +0900

    test.sh: Integration tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,6 @@ set -eu
 yarn run build:dev
 yarn run tsc
 yarn test
+
+swc --sync tests/integration/three-js/repo/ -d tests/integration/three-js/build/
+(cd tests/integration/three-js/build/test && qunit -r failonlyreporter unit/three.source.unit.js)
