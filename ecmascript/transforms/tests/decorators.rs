e19849cbc158977bd2df7c1d3b53b20c0b233838
강동윤 <kdy1997.dev@gmail.com>
2021-09-01 18:05:41 +0900

    Update test refs

--- ecmascript/transforms/tests/decorators.rs
+++ ecmascript/transforms/tests/decorators.rs
@@ -308,7 +308,7 @@ let Foo = _decorate([decorator], function (_initialize) {
     d: [{
       kind: "method",
       key: "method",
-      value: function method() {
+      value: function method1() {
         return method;
       }
     }]
