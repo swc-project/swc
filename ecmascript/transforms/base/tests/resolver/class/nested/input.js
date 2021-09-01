52d7c4f92d7845216d9182e9ddc782e58f3b1986
강동윤 <kdy1997.dev@gmail.com>
2021-09-01 14:37:39 +0900

    Move tests

---
+++ ecmascript/transforms/base/tests/resolver/class/nested/input.js
@@ -0,0 +1,20 @@
+var Outer = function (_Hello) {
+    _inherits(Outer, _Hello);
+    function Outer() {
+        _classCallCheck(this, Outer);
+        var _this = _possibleConstructorReturn(this, _getPrototypeOf(Outer).call(this));
+        var Inner = function () {
+            function Inner() {
+                _classCallCheck(this, Inner);
+            }
+            _createClass(Inner, [{
+                key: _get(_getPrototypeOf(Outer.prototype), 'toString', _assertThisInitialized(_this)).call(_this), value: function () {
+                    return 'hello';
+                }
+            }]);
+            return Inner;
+        }();
+        return _possibleConstructorReturn(_this, new Inner());
+    }
+    return Outer;
+}(Hello);
---
+++ ecmascript/transforms/base/tests/resolver/class/var/1/input.js
@@ -0,0 +1,10 @@
+var Foo = function (_Bar) {
+    _inherits(Foo, _Bar);
+    function Foo() {
+        var _this;
+        _classCallCheck(this, Foo);
+        Foo[_assertThisInitialized(_this)];
+        return _possibleConstructorReturn(_this);
+    }
+    return Foo;
+}(Bar);
