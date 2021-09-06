806106a654d751a9198e8bdb72959547f4803fba
강동윤 <kdy1997.dev@gmail.com>
2021-09-06 13:24:59 +0900

    UnwrapParen

--- ecmascript/minifier/src/compress/util.rs
+++ ecmascript/minifier/src/compress/util.rs
@@ -579,8 +579,26 @@ where
 #[cfg(test)]
 mod tests {
     use super::negate_cost;
-    use swc_common::{input::SourceFileInput, FileName};
+    use swc_common::{input::SourceFileInput, util::take::Take, FileName};
+    use swc_ecma_ast::*;
     use swc_ecma_parser::{lexer::Lexer, Parser};
+    use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
+
+    struct UnwrapParen;
+    impl VisitMut for UnwrapParen {
+        noop_visit_mut_type!();
+
+        fn visit_mut_expr(&mut self, e: &mut Expr) {
+            e.visit_mut_children_with(self);
+
+            match e {
+                Expr::Paren(p) => {
+                    *e = *p.expr.take();
+                }
+                _ => {}
+            }
+        }
+    }
 
     fn assert_negate_cost(s: &str, in_bool_ctx: bool, is_ret_val_ignored: bool, expected: isize) {
         testing::run_test2(false, |cm, _| {
@@ -595,9 +613,11 @@ mod tests {
 
             let mut parser = Parser::new_from(lexer);
 
-            let e = parser
+            let mut e = parser
                 .parse_expr()
                 .expect("failed to parse input as an expression");
+
+            e.visit_mut_with(&mut UnwrapParen);
 
             let actual = negate_cost(&e, in_bool_ctx, is_ret_val_ignored).unwrap();
 
