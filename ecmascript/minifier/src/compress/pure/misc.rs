85a270802e94c8f4118d4a9c8336d2e5f3bdb61e
강동윤 <kdy1997.dev@gmail.com>
2021-09-08 13:42:59 +0900

    Drop useless `return` tokens

--- ecmascript/minifier/src/compress/pure/misc.rs
+++ ecmascript/minifier/src/compress/pure/misc.rs
@@ -29,7 +29,67 @@ where
 
     /// Removes last return statement. This should be callled only if the return
     /// value of function is ignored.
-    fn drop_return_value(&mut self, stmts: &mut Vec<Stmt>) {}
+    ///
+    /// Returns true if something is modified.
+    fn drop_return_value(&mut self, stmts: &mut Vec<Stmt>) -> bool {
+        // TODO(kdy1): (maybe) run optimization again if it's removed.
+
+        if let Some(last) = stmts.last_mut() {
+            self.drop_return_value_of_stmt(last)
+        } else {
+            false
+        }
+    }
+
+    /// Returns true if something is modified.
+    fn drop_return_value_of_stmt(&mut self, s: &mut Stmt) -> bool {
+        match s {
+            Stmt::Block(s) => self.drop_return_value(&mut s.stmts),
+            Stmt::Return(ret) => {
+                let span = ret.span;
+                match ret.arg.take() {
+                    Some(arg) => {
+                        *s = Stmt::Expr(ExprStmt { span, expr: arg });
+                    }
+                    None => {
+                        *s = Stmt::Empty(EmptyStmt { span });
+                    }
+                }
+
+                true
+            }
+
+            Stmt::Labeled(s) => self.drop_return_value_of_stmt(&mut s.body),
+            Stmt::If(s) => {
+                let c = self.drop_return_value_of_stmt(&mut s.cons);
+                let a = s
+                    .alt
+                    .as_deref_mut()
+                    .map(|s| self.drop_return_value_of_stmt(s))
+                    .unwrap_or_default();
+
+                c || a
+            }
+
+            Stmt::Try(s) => {
+                let a = self.drop_return_value(&mut s.block.stmts);
+                let b = s
+                    .handler
+                    .as_mut()
+                    .map(|c| self.drop_return_value(&mut c.body.stmts))
+                    .unwrap_or_default();
+                let c = s
+                    .finalizer
+                    .as_mut()
+                    .map(|s| self.drop_return_value(&mut s.stmts))
+                    .unwrap_or_default();
+
+                a || b || c
+            }
+
+            _ => false,
+        }
+    }
 
     pub(super) fn ignore_return_value(&mut self, e: &mut Expr) {
         match e {
