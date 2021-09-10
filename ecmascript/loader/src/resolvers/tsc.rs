53dd06f2c74fe036c78459d8b5bc5bf0ca843c45
강동윤 <kdy1997.dev@gmail.com>
2021-09-10 17:03:35 +0900

    Fix tsc resolver

--- ecmascript/loader/src/resolvers/tsc.rs
+++ ecmascript/loader/src/resolvers/tsc.rs
@@ -151,7 +151,7 @@ where
                         });
 
                         if to.len() == 1 {
-                            return Ok(FileName::Real(PathBuf::from(replaced)));
+                            return Ok(FileName::Real(self.base_url.join(replaced)));
                         }
                     }
 
