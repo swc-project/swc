7b15edd1babc3bc22c16dd12eabe28051b1b6356
강동윤 <kdy1997.dev@gmail.com>
2021-09-10 20:18:37 +0900

    I hate windows

--- ecmascript/loader/src/resolvers/tsc.rs
+++ ecmascript/loader/src/resolvers/tsc.rs
@@ -135,7 +135,7 @@ where
                     );
                     let mut errors = vec![];
                     for target in to {
-                        let replaced = target.replace('*', capture.as_str());
+                        let mut replaced = target.replace('*', capture.as_str());
                         let rel = format!("./{}", replaced);
 
                         let res = self.inner.resolve(base, &rel).with_context(|| {
@@ -149,6 +149,14 @@ where
                             Ok(v) => return Ok(v),
                             Err(err) => err,
                         });
+
+                        if cfg!(target_os = "windows") {
+                            if replaced.starts_with("./") {
+                                replaced = replaced[2..].to_string();
+                            }
+                            replaced = replaced.replace('/', "\\");
+                        }
+
 
                         if to.len() == 1 {
                             return Ok(FileName::Real(self.base_url.join(replaced)));
