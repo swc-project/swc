1e2962d24fef835621410261bfa9a541e3606b9e
강동윤 <kdy1997.dev@gmail.com>
2021-09-13 16:08:18 +0900

    Update test refs

--- ecmascript/minifier/tests/compress/fixture/projects/next/extra/if_return/1/output.js
+++ ecmascript/minifier/tests/compress/fixture/projects/next/extra/if_return/1/output.js
@@ -1,21 +1,10 @@
-export function foo(
-) {
+export function foo() {
     var obj;
-    return state.loading || state.error
-        ? _react.default.createElement(
-            opts.loading,
-            {
-                isLoading: state.loading,
-                pastDelay: state.pastDelay,
-                timedOut: state.timedOut,
-                error: state.error,
-                retry: subscription.retry,
-            }
-        )
-        : state.loaded
-            ? _react.default.createElement(
-                (obj = state.loaded) && obj.__esModule ? obj.default : obj,
-                props
-            )
-            : null;
+    return state.loading || state.error ? _react.default.createElement(opts.loading, {
+        isLoading: state.loading,
+        pastDelay: state.pastDelay,
+        timedOut: state.timedOut,
+        error: state.error,
+        retry: subscription.retry
+    }) : state.loaded ? _react.default.createElement((obj = state.loaded) && obj.__esModule ? obj.default : obj, props) : null;
 }
