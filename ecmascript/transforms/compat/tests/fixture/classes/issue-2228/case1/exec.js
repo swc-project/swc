d5bf2dfaa734ccd0ee5e9fa0381d09e105a86962
강동윤 <kdy1997.dev@gmail.com>
2021-09-14 16:01:18 +0900

    Harden the test for #2228

--- ecmascript/transforms/compat/tests/fixture/classes/issue-2228/case1/exec.js
+++ ecmascript/transforms/compat/tests/fixture/classes/issue-2228/case1/exec.js
@@ -14,4 +14,9 @@ class Bar extends Foo {
 }
 
 const bar = new Bar();
+console.log(bar.data);
 console.log(bar.events)
+console.log(bar.events["abc: click"]());
+console.log(bar.data);
+bar.setData();
+console.log(bar.data);
