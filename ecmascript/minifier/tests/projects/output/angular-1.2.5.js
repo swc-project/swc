4d60ef6c288c42f2eccc5179534a7b9b7a4f680a
강동윤 <kdy1997.dev@gmail.com>
2021-09-14 11:18:12 +0900

    Update test refs

--- ecmascript/minifier/tests/projects/output/angular-1.2.5.js
+++ ecmascript/minifier/tests/projects/output/angular-1.2.5.js
@@ -1640,7 +1640,6 @@
                     var parts = [];
                     return (function(obj, iterator, context) {
                         for(var keys = sortedKeys(obj), i = 0; i < keys.length; i++)iterator.call(void 0, obj[keys[i]], keys[i]);
-                        return keys;
                     })(params, function(value, key) {
                         null === value || isUndefined(value) || (isArray(value) || (value = [
                             value
