e2d846556568dc43093ef61612230b7323416898
강동윤 <kdy1997.dev@gmail.com>
2021-09-16 18:26:02 +0900

    fix(es/codegen): Fix codegen of ` in synthesized template literals (#2260)
    swc_ecma_codegen:
     - Fix codegen of ` in synthesized template string literals.

--- ecmascript/codegen/src/lib.rs
+++ ecmascript/codegen/src/lib.rs
@@ -1371,8 +1371,15 @@ impl<'a> Emitter<'a> {
 
     #[emitter]
     fn emit_quasi(&mut self, node: &TplElement) -> Result {
-        self.wr
-            .write_str_lit(node.span, &unescape_tpl_lit(&node.raw.value))?;
+        let is_synthesized = match node.raw.kind {
+            StrKind::Synthesized => true,
+            _ => false,
+        };
+
+        self.wr.write_str_lit(
+            node.span,
+            &unescape_tpl_lit(&node.raw.value, is_synthesized),
+        )?;
         return Ok(());
     }
 
@@ -2749,7 +2756,7 @@ where
     }
 }
 
-fn unescape_tpl_lit(s: &str) -> String {
+fn unescape_tpl_lit(s: &str, is_synthesized: bool) -> String {
     fn read_escaped(
         radix: u32,
         len: Option<usize>,
@@ -2814,6 +2821,10 @@ fn unescape_tpl_lit(s: &str) -> String {
                 }
                 '\n' => {
                     result.push_str("\n");
+                }
+
+                '`' if is_synthesized => {
+                    result.push_str("\\`");
                 }
 
                 // TODO: Handle all escapes
