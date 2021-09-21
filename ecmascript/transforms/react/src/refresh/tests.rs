f01553ce75d99472ec0a6b827a91ee6518ec61d8
Donny <kdy1997.dev@gmail.com>
2021-09-21 20:54:34 +0900

    Update test refs

--- ecmascript/transforms/react/src/refresh/tests.rs
+++ ecmascript/transforms/react/src/refresh/tests.rs
@@ -860,22 +860,22 @@ test!(
     r#"
     var _s = $RefreshSig$(), _s1 = $RefreshSig$();
 
-    export const A = _s(React.memo(_c = _s(React.forwardRef(_c1 = _s((props, ref) => {
+    export const A = _s(React.memo(_c2 = _s(React.forwardRef(_c1 = _s((props, ref) => {
       _s();
     
       const [foo, setFoo] = useState(0);
       React.useEffect(() => {});
       return <h1 ref={ref}>{foo}</h1>;
     }, "useState{[foo, setFoo](0)}\nuseEffect{}")), "useState{[foo, setFoo](0)}\nuseEffect{}")), "useState{[foo, setFoo](0)}\nuseEffect{}");
-    _c2 = A;
-    export const B = _s1(React.memo(_c4 = _s1(React.forwardRef(_c3 = _s1(function (props, ref) {
+    _c3 = A;
+    export const B = _s1(React.memo(_c5 = _s1(React.forwardRef(_c4 = _s1(function (props, ref) {
       _s1();
     
       const [foo, setFoo] = useState(0);
       React.useEffect(() => {});
       return <h1 ref={ref}>{foo}</h1>;
     }, "useState{[foo, setFoo](0)}\nuseEffect{}")), "useState{[foo, setFoo](0)}\nuseEffect{}")), "useState{[foo, setFoo](0)}\nuseEffect{}");
-    _c5 = B;
+    _c6 = B;
 
     function hoc() {
       var _s = $RefreshSig$();
@@ -891,14 +891,14 @@ test!(
 
     export let C = hoc();
 
-    var _c1, _c, _c2, _c3, _c4, _c5;
+    var _c1, _c2, _c3, _c4, _c5, _c6;
 
     $RefreshReg$(_c1, "A$React.memo$React.forwardRef");
-    $RefreshReg$(_c, "A$React.memo");
-    $RefreshReg$(_c2, "A");
-    $RefreshReg$(_c3, "B$React.memo$React.forwardRef");
-    $RefreshReg$(_c4, "B$React.memo");
-    $RefreshReg$(_c5, "B");
+    $RefreshReg$(_c2, "A$React.memo");
+    $RefreshReg$(_c3, "A");
+    $RefreshReg$(_c4, "B$React.memo$React.forwardRef");
+    $RefreshReg$(_c5, "B$React.memo");
+    $RefreshReg$(_c6, "B");
 "#
 );
 
