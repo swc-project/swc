2d423c7a5f89cf1b709feb62ece5a56bf7605613
Donny <kdy1997.dev@gmail.com>
2021-09-21 23:02:54 +0900

    Add a test for wasm

--- wasm/__tests__/simple.js
+++ wasm/__tests__/simple.js
@@ -3,3 +3,30 @@ const swc = require("../pkg");
 it("should be loadable", function () {
   const output = swc.transformSync("class Foo {}", {});
 });
+
+
+it("should support 'paths' and 'baseUrl'", async () => {
+  const { code } = await swc.transform(`
+  import foo from '@src/app';
+  console.log(foo)
+  `, {
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+      },
+      target: 'es2021',
+      transform: {
+
+      },
+      baseUrl: __dirname,
+      paths: {
+        '@src/*': ['bar/*']
+      }
+    },
+    module: {
+      type: 'commonjs'
+    },
+  });
+
+  expect(code).toContain(`bar/app`);
+})
