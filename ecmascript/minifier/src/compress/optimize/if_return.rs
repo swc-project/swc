45274dc474df75d2d18c5618fc28fde2cd120bc1
Donny <kdy1997.dev@gmail.com>
2021-09-22 16:33:48 +0900

    Better logging

--- ecmascript/minifier/src/compress/optimize/if_return.rs
+++ ecmascript/minifier/src/compress/optimize/if_return.rs
@@ -448,6 +448,10 @@ where
                             span: DUMMY_SP,
                             expr: Box::new(cur),
                         }))
+                    } else {
+                        if cfg!(feature = "debug") {
+                            log::debug!("if_return: Ignoring return value");
+                        }
                     }
                 }
                 _ => {
