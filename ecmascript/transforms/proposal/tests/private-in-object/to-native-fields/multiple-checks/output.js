5f46aa4f92bb79e4904f3aa2d548cc210c80c2e8
Donny <kdy1997.dev@gmail.com>
2021-09-27 15:40:15 +0900

    Update test refs

--- ecmascript/transforms/proposal/tests/private-in-object/to-native-fields/multiple-checks/output.js
+++ ecmascript/transforms/proposal/tests/private-in-object/to-native-fields/multiple-checks/output.js
@@ -1,20 +1,15 @@
-var _xBrandCheck = /*#__PURE__*/new WeakSet();
-
-var _ABrandCheck = /*#__PURE__*/new WeakSet();
-
+var _brand_check_x = new WeakSet(), _brand_check_m = new WeakSet();
 class A {
-  #x = (_ABrandCheck.add(this), void _xBrandCheck.add(this));
-
-  #m() {}
-
-  test() {
-    _xBrandCheck.has(this);
-
-    _ABrandCheck.has(this);
-
-    _xBrandCheck.has(this);
-
-    _ABrandCheck.has(this);
-  }
-
+    #x = void _brand_check_x.add(this);
+     #m() {
+    }
+    test() {
+        _brand_check_x.has(this);
+        _brand_check_m.has(this);
+        _brand_check_x.has(this);
+        _brand_check_m.has(this);
+    }
+    constructor(){
+        _brand_check_m.add(this);
+    }
 }
