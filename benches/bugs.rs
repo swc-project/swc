3688143f5f035cdac99e409a99310cdbe3f33eb0
Donny <kdy1997.dev@gmail.com>
2021-09-28 19:33:02 +0900

    Move

--- benches/bugs.rs
+++ benches/bugs.rs
@@ -4,6 +4,7 @@
 extern crate test;
 
 use std::{hint::black_box, io::stderr, path::Path};
+use swc::config::Options;
 use swc_common::{errors::Handler, sync::Lrc, FilePathMapping, SourceMap};
 use test::Bencher;
 
@@ -22,11 +23,20 @@ fn bugs_1(b: &mut Bencher) {
     b.iter(|| {
         let handler = Handler::with_emitter_writer(Box::new(stderr()), Some(c.cm.clone()));
 
-        let fm = c.cm.load_file(Path::new("benches/bugs/1.tsx")).unwrap();
+        let fm =
+            c.cm.load_file(Path::new("benches/bugs/1/input.tsx"))
+                .unwrap();
 
         let result = {
-            c.process_js_file(fm.clone(), &handler, &Default::default())
-                .unwrap()
+            c.process_js_file(
+                fm.clone(),
+                &handler,
+                &Options {
+                    is_module: true,
+                    ..Default::default()
+                },
+            )
+            .unwrap()
         };
         black_box(result);
     });
