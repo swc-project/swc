c64f9bbdb536bbe1a23e25a8a97dd6ca418ed6c2
Donny <kdy1997.dev@gmail.com>
2021-09-28 19:25:47 +0900

    Add a benchmark

---
+++ benches/bugs.rs
@@ -0,0 +1,37 @@
+#![feature(test)]
+#![feature(bench_black_box)]
+
+extern crate test;
+
+use std::{
+    hint::black_box,
+    io::{self, stderr},
+    path::Path,
+};
+use swc_common::{errors::Handler, sync::Lrc, FilePathMapping, SourceMap};
+use test::Bencher;
+
+fn mk() -> swc::Compiler {
+    let cm = Lrc::new(SourceMap::new(FilePathMapping::empty()));
+
+    let c = swc::Compiler::new(cm.clone());
+
+    c
+}
+
+#[bench]
+fn bugs_1(b: &mut Bencher) {
+    let c = mk();
+
+    b.iter(|| {
+        let handler = Handler::with_emitter_writer(Box::new(stderr()), Some(c.cm.clone()));
+
+        let fm = c.cm.load_file(Path::new("assets/bugs/1.tsx")).unwrap();
+
+        let result = {
+            c.process_js_file(fm.clone(), &handler, &Default::default())
+                .unwrap()
+        };
+        black_box(result);
+    });
+}
