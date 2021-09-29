1ca93459f3ceaf571545c0f51907a34607d3272c
Donny <kdy1997.dev@gmail.com>
2021-09-29 16:57:21 +0900

    fmt

--- ecmascript/parser/benches/parser.rs
+++ ecmascript/parser/benches/parser.rs
@@ -1,7 +1,7 @@
 #![feature(test)]
 
-extern crate test;
 extern crate swc_node_base;
+extern crate test;
 
 use swc_common::{comments::SingleThreadedComments, FileName};
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
