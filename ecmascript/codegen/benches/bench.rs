6896c491ddc3b7eb3527156843d5eace5c11cd5c
Donny <kdy1997.dev@gmail.com>
2021-09-29 20:43:17 +0900

    Remove asset

--- ecmascript/codegen/benches/bench.rs
+++ ecmascript/codegen/benches/bench.rs
@@ -83,7 +83,6 @@ module.exports = {
 "#;
 
 const LARGE_PARTIAL_JS: &str = include_str!("large-partial.js");
-const THREE_BUNDLE: &str = include_str!("three-bundle.js");
 
 fn bench_emitter(b: &mut Bencher, s: &str) {
     b.bytes = s.len() as _;
@@ -137,9 +136,4 @@ fn emit_colors(b: &mut Bencher) {
 #[bench]
 fn emit_large(b: &mut Bencher) {
     bench_emitter(b, LARGE_PARTIAL_JS)
-}
-
-#[bench]
-fn three_bundle(b: &mut Bencher) {
-    bench_emitter(b, THREE_BUNDLE)
 }
