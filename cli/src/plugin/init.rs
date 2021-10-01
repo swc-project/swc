33bea34648cab137ec239f8cdd9a80c5c4f1de75
Donny <kdy1997.dev@gmail.com>
2021-10-02 00:08:59 +0900

    WIP: init

--- cli/src/plugin/init.rs
+++ cli/src/plugin/init.rs
@@ -1,13 +1,29 @@
-use anyhow::{bail, Error};
-use std::path::PathBuf;
+use anyhow::{bail, Context, Error};
+use std::process::Stdio;
 use structopt::StructOpt;
+use tokio::process::Command;
 
 /// Initializes a plugin project.
 #[derive(Debug, StructOpt)]
 pub struct InitCommand {}
 
 impl InitCommand {
-    pub async fn run(self) -> Result<PathBuf, Error> {
-        bail!("Not implemented yet")
+    pub async fn run(self) -> Result<(), Error> {
+        let mut c = Command::new("cargo");
+        c.arg("init").stderr(Stdio::inherit());
+
+        let status = c
+            .arg("--vcs")
+            .arg("git")
+            .arg("--lib")
+            .status()
+            .await
+            .with_context(|| format!("failed to run `cargo init`"))?;
+
+        if !status.success() {
+            bail!("failed to initialize a cargo project")
+        }
+
+        Ok(())
     }
 }
