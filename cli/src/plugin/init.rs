542d9ac2a46262d21588d362995a4ee9534c5ac5
Donny <kdy1997.dev@gmail.com>
2021-10-01 23:58:28 +0900

    init command

--- cli/src/plugin/init.rs
+++ cli/src/plugin/init.rs
@@ -1,4 +1,13 @@
+use anyhow::{bail, Error};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
+/// Initializes a plugin project.
 #[derive(Debug, StructOpt)]
 pub struct InitCommand {}
+
+impl InitCommand {
+    pub async fn run(self) -> Result<PathBuf, Error> {
+        bail!("Not implemented yet")
+    }
+}
--- cli/src/plugin/mod.rs
+++ cli/src/plugin/mod.rs
@@ -2,7 +2,7 @@ use self::{
     build::BuildCommand, init::InitCommand, package::PackageCommand, publish::PublishCommand,
     upgrade_deps::UpgradeDepsCommand,
 };
-use anyhow::Error;
+use anyhow::{Context, Error};
 use structopt::StructOpt;
 
 pub mod build;
@@ -24,11 +24,23 @@ pub enum PluginCommand {
 impl PluginCommand {
     pub async fn run(self) -> Result<(), Error> {
         match self {
-            PluginCommand::Init(_) => todo!(),
-            PluginCommand::Build(cmd) => cmd.run().await,
+            PluginCommand::Init(cmd) => {
+                cmd.run()
+                    .await
+                    .context("failed to initialize a plugin projecy")?;
+            }
+            PluginCommand::Build(cmd) => {
+                cmd.run()
+                    .await
+                    .context("failed to build a plugin project")?;
+            }
             PluginCommand::Package(_) => todo!(),
             PluginCommand::Publish(_) => todo!(),
-            PluginCommand::UpgradeDeps(cmd) => cmd.run().await,
+            PluginCommand::UpgradeDeps(cmd) => {
+                cmd.run().await.context("failed to upgrade dependencies")?;
+            }
         }
+
+        Ok(())
     }
 }
