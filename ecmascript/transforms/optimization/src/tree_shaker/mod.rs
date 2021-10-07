61b15efe9313b00ccf8d93de494cdd594084172e
Donny <kdy1997.dev@gmail.com>
2021-10-07 14:56:47 +0900

    More work for tree shaker

--- ecmascript/transforms/optimization/src/tree_shaker/mod.rs
+++ ecmascript/transforms/optimization/src/tree_shaker/mod.rs
@@ -1,7 +1,8 @@
+use retain_mut::RetainMut;
 use rustc_hash::FxHashSet;
 use swc_common::{pass::Repeated, Mark, DUMMY_SP};
 use swc_ecma_ast::*;
-use swc_ecma_utils::{ident::IdentLike, Id};
+use swc_ecma_utils::{ident::IdentLike, ExprExt, Id};
 use swc_ecma_visit::{
     as_folder, noop_visit_mut_type, noop_visit_type, Fold, Node, Visit, VisitMut, VisitMutWith,
     VisitWith,
@@ -41,6 +42,17 @@ struct Analyzer<'a> {
 impl Visit for Analyzer<'_> {
     noop_visit_type!();
 
+    fn visit_expr(&mut self, e: &Expr, _: &dyn Node) {
+        e.visit_children_with(self);
+
+        match e {
+            Expr::Ident(i) => {
+                self.data.used_names.insert(i.to_id());
+            }
+            _ => {}
+        }
+    }
+
     fn visit_pat(&mut self, p: &Pat, _: &dyn Node) {
         p.visit_children_with(self);
 
@@ -52,15 +64,8 @@ impl Visit for Analyzer<'_> {
         }
     }
 
-    fn visit_expr(&mut self, e: &Expr, _: &dyn Node) {
-        e.visit_children_with(self);
-
-        match e {
-            Expr::Ident(i) => {
-                self.data.used_names.insert(i.to_id());
-            }
-            _ => {}
-        }
+    fn visit_var_declarator(&mut self, v: &VarDeclarator, _: &dyn Node) {
+        v.init.visit_with(v, self);
     }
 }
 
@@ -88,5 +93,35 @@ impl VisitMut for TreeShaker {
         }
 
         m.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_var_declarator(&mut self, v: &mut VarDeclarator) {
+        v.visit_mut_children_with(self);
+
+        if let Some(init) = &v.init {
+            if !init.may_have_side_effects() {
+                match &v.name {
+                    Pat::Ident(i) => {
+                        if !self.data.used_names.contains(&i.id.to_id()) {
+                            v.init.take();
+                        }
+                    }
+
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn visit_mut_var_declarators(&mut self, v: &mut Vec<VarDeclarator>) {
+        v.visit_mut_children_with(self);
+
+        v.retain(|v| {
+            if v.name.is_invalid() {
+                return false;
+            }
+
+            true
+        });
     }
 }
