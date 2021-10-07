d518439d4258a37f605bcbbf5fe0f79fae78dfe1
Donny <kdy1997.dev@gmail.com>
2021-10-07 14:11:15 +0900

    Update test refs

--- tests/fixture/issue-1525/case1/output/index.tsx
+++ tests/fixture/issue-1525/case1/output/index.tsx
@@ -25,4 +25,4 @@ function _interopRequireWildcard(obj) {
 }
 class X extends _react.Component {
 }
-_react.default.render(document.getElementById("#root"), /*#__PURE__*/ React.createElement(X, null));
+_react.default.render(document.getElementById("#root"), /*#__PURE__*/ _react.default.createElement(X, null));
--- tests/fixture/issue-1799/case3/output/index.js
+++ tests/fixture/issue-1799/case3/output/index.js
@@ -6,7 +6,7 @@ exports.default = Foo;
 var swcHelpers = require("@swc/helpers");
 var _react = swcHelpers.interopRequireDefault(require("react"));
 function Foo() {
-    return(/*#__PURE__*/ React.createElement("div", {
+    return(/*#__PURE__*/ _react.default.createElement("div", {
         onClick: swcHelpers.asyncToGenerator(function*(e) {
             yield doSomething();
         })
