70292830d43e37d6ab80d145498edd193b59c9eb
Donny <kdy1997.dev@gmail.com>
2021-10-08 18:12:52 +0900

    Add a test

---
+++ tests/fixture/vercel/full/1/input/index.js
@@ -0,0 +1,19 @@
+export default function createStore(initialState) {
+    let state = initialState;
+    const listeners = [];
+    return {
+        getState() {
+            return state;
+        },
+        setState(nextState) {
+            state = nextState;
+            listeners.forEach(listener => listener());
+        },
+        subscribe(listener) {
+            listeners.push(listener);
+            return function unsubscribe() {
+                listeners.splice(listeners.indexOf(listener), 1);
+            };
+        },
+    };
+}
