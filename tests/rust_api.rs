822183393e14ebde7d29551af2ede3dfa9c066c2
Donny <kdy1997.dev@gmail.com>
2021-10-08 11:35:00 +0900

    Add one more test

--- tests/rust_api.rs
+++ tests/rust_api.rs
@@ -1,10 +1,12 @@
 use swc::{
-    config::{Config, JscConfig, ModuleConfig, Options},
+    config::{
+        Config, InputSourceMap, JscConfig, ModuleConfig, Options, RootMode, SourceMapsConfig,
+    },
     Compiler,
 };
 use swc_common::FileName;
 use swc_ecma_ast::*;
-use swc_ecma_parser::{EsConfig, Syntax};
+use swc_ecma_parser::{EsConfig, Syntax, TsConfig};
 use swc_ecma_transforms::{modules::common_js, pass::noop};
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, VisitMut};
 
@@ -58,7 +60,7 @@ fn test_visit_mut() {
 }
 
 #[test]
-fn shopify_check_filename() {
+fn shopify_1_check_filename() {
     testing::run_test2(false, |cm, handler| {
         let c = Compiler::new(cm.clone());
 
@@ -108,6 +110,112 @@ fn shopify_check_filename() {
         eprintln!("{}", res.code);
 
         assert!(res.code.contains("_react.default.createElement"));
+        Ok(())
+    })
+    .unwrap()
+}
+
+#[test]
+fn shopify_2_same_opt() {
+    testing::run_test2(false, |cm, handler| {
+        let c = Compiler::new(cm.clone());
+
+        let opts = Options {
+            config: Config {
+                env: None,
+                test: None,
+                exclude: None,
+                jsc: JscConfig {
+                    syntax: Some(Syntax::Typescript(TsConfig {
+                        tsx: true,
+                        decorators: false,
+                        dynamic_import: true,
+                        dts: false,
+                        no_early_errors: false,
+                        import_assertions: false,
+                    })),
+                    transform: None,
+                    external_helpers: false,
+                    target: Some(EsVersion::Es5),
+                    loose: false,
+                    keep_class_names: false,
+                    base_url: Default::default(),
+                    paths: Default::default(),
+                    minify: None,
+                    experimental: Default::default(),
+                },
+                module: None,
+                minify: false,
+                input_source_map: InputSourceMap::Bool(false),
+                source_maps: None,
+                inline_sources_content: false,
+            },
+            skip_helper_injection: false,
+            disable_hygiene: false,
+            disable_fixer: false,
+            global_mark: None,
+            cwd: "/Users/kdy1/projects/example-swcify".into(),
+            caller: None,
+            filename: "/Users/kdy1/projects/example-swcify/src/App/App.tsx".into(),
+            config_file: None,
+            root: None,
+            root_mode: RootMode::Root,
+            swcrc: true,
+            swcrc_roots: None,
+            env_name: "development".into(),
+            source_maps: Some(SourceMapsConfig::Bool(false)),
+            source_file_name: Some("/Users/kdy1/projects/example-swcify/src/App/App.tsx".into()),
+            source_root: None,
+            is_module: true,
+            output_path: None,
+        };
+
+        let fm = cm.new_source_file(
+            FileName::Real("/Users/kdy1/projects/example-swcify/src/App/App.tsx".into()),
+            "
+            import React from 'react';
+            import { useI18n } from '@shopify/react-i18n';
+            
+            export function App() {
+                const [i18n] = useI18n();
+                return <h1>{i18n.translate('foo')}</h1>
+            }
+            "
+            .into(),
+        );
+
+        let res = c.process_js_with_custom_pass(
+            fm,
+            &handler,
+            &Options {
+                config: Config {
+                    jsc: JscConfig {
+                        syntax: Some(Syntax::Es(EsConfig {
+                            jsx: true,
+                            ..Default::default()
+                        })),
+                        ..Default::default()
+                    },
+                    module: Some(ModuleConfig::CommonJs(common_js::Config {
+                        ..Default::default()
+                    })),
+                    ..Default::default()
+                },
+                is_module: true,
+                ..Default::default()
+            },
+            as_folder(NoopType),
+            noop(),
+        );
+
+        if res.is_err() {
+            return Err(());
+        }
+
+        let res = res.unwrap();
+        eprintln!("{}", res.code);
+
+        panic!("WTF?");
 
         Ok(())
     })
