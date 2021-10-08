9796a09a9e6dfc200b55548a82f9af04734bf5b2
Donny <kdy1997.dev@gmail.com>
2021-10-08 18:47:15 +0900

    Typo

--- tests/projects.rs
+++ tests/projects.rs
@@ -790,7 +790,7 @@ fn should_visit() {
 }
 
 #[testing::fixture("tests/fixture/**/input/")]
-#[testing::fixture("tests/vecel/**/input/")]
+#[testing::fixture("tests/vercel/**/input/")]
 fn tests(input_dir: PathBuf) {
     let output = input_dir.parent().unwrap().join("output");
 
