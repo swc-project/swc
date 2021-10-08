60bc616c4dc1418d28fbef3b32966f980114c841
Donny <kdy1997.dev@gmail.com>
2021-10-08 10:59:18 +0900

    Add a test

--- tests/rust_api.rs
+++ tests/rust_api.rs
@@ -4,7 +4,7 @@ use swc::{
 };
 use swc_common::FileName;
 use swc_ecma_ast::*;
-use swc_ecma_parser::Syntax;
+use swc_ecma_parser::{EsConfig, Syntax};
 use swc_ecma_transforms::pass::noop;
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, VisitMut};
 
@@ -51,6 +51,54 @@ fn test_visit_mut() {
         );
 
         assert_ne!(res.unwrap().code, "console.log(5 as const)");
+
+        Ok(())
+    })
+    .unwrap()
+}
+
+#[test]
+fn shopify_check_filename() {
+    testing::run_test2(false, |cm, handler| {
+        let c = Compiler::new(cm.clone());
+
+        let fm = cm.new_source_file(
+            FileName::Anon,
+            "
+            import React from 'react';
+            import { useI18n } from '@shopify/react-i18n';
+            
+            export function App() {
+                const notused = React.version;
+                const [i18n] = useI18n();
+                return <h1>{i18n.translate('foo')}</h1>
+            }
+            "
+            .into(),
+        );
+
+        let res = c.process_js_with_custom_pass(
+            fm,
+            &handler,
+            &Options {
+                config: Config {
+                    jsc: JscConfig {
+                        syntax: Some(Syntax::Es(EsConfig {
+                            jsx: true,
+                            ..Default::default()
+                        })),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+
+                ..Default::default()
+            },
+            as_folder(NoopType),
+            noop(),
+        );
+
+        assert_eq!(res.unwrap().code, "console.log(5 as const)");
 
         Ok(())
     })
