51d1bb5298af3ecfcaf3d68eedd959994fb7dc3c
Donny <kdy1997.dev@gmail.com>
2021-10-08 11:15:08 +0900

    Fix tests

--- tests/fixture/shopify/003-env/input/index.js
+++ tests/fixture/shopify/003-env/input/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useI18n } from "@shopify/react-i18n";
 
 export function App() {
-    const notused = React.version;
     const [i18n] = useI18n();
     return <h1>{i18n.translate("foo")}</h1>
 }
