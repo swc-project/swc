f23e1020f6cda8996544dbcde8aa692241227efc
Donny <kdy1997.dev@gmail.com>
2021-10-10 19:47:03 +0900

    Remove swd

--- dev/cli/scripts/invoke.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-cargo install --path . --debug
-export RUST_LOG=debug
-
-(cd plugins/packages/jest && swd $@)
--- dev/cli/src/plugin/package.rs
+++
@@ -1,8 +0,0 @@
-use super::build::BaseCargoCommand;
-use structopt::StructOpt;
-
-#[derive(Debug, StructOpt)]
-pub struct PackageCommand {
-    #[structopt(flatten)]
-    pub cargo: BaseCargoCommand,
-}
