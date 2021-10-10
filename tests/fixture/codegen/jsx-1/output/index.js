352efdf920b9b8e66aef7272b9a79e1bcb7efeb1
Donny <kdy1997.dev@gmail.com>
2021-10-10 17:05:16 +0900

    Update test refs

--- tests/fixture/codegen/jsx-1/output/index.js
+++ tests/fixture/codegen/jsx-1/output/index.js
@@ -1,14 +1,5 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports["default"] = void 0;
-
-var _default = /*#__PURE__*/React.createElement(A, {
+export default /*#__PURE__*/ React.createElement(A, {
     className: b,
     header: "C",
     subheader: "D E"
 });
-
-exports["default"] = _default;
