c79d102a4faf9521876eb9582ed51f6618f35948
Donny <kdy1997.dev@gmail.com>
2021-10-12 07:00:27 +0900

    Use analyzer

--- ecmascript/transforms/base/src/hygiene/analyzer.rs
+++ ecmascript/transforms/base/src/hygiene/analyzer.rs
@@ -1,23 +1,111 @@
+use crate::scope::ScopeKind;
 use std::cell::RefCell;
+use swc_atoms::JsWord;
 use swc_common::{collections::AHashMap, SyntaxContext};
-use swc_ecma_visit::{noop_visit_type, Visit};
+use swc_ecma_ast::*;
+use swc_ecma_utils::{ident::IdentLike, Id};
+use swc_ecma_visit::{noop_visit_type, Node, Visit, VisitWith};
 
+#[derive(Debug, Default)]
 pub struct Data {
+    /// Top level scope uses [SyntaxContext::empty].
     pub scopes: AHashMap<SyntaxContext, ScopeData>,
 }
 
-pub struct ScopeData {}
+#[derive(Debug, Default)]
+pub struct ScopeData {
+    pub kind: ScopeKind,
+
+    pub decls: RefCell<AHashMap<JsWord, Vec<SyntaxContext>>>,
+
+    pub usages: RefCell<AHashMap<JsWord, Vec<SyntaxContext>>>,
+}
 
 pub struct CurScope<'a> {
     pub parent: Option<&'a CurScope<'a>>,
+    pub scope_ctxt: SyntaxContext,
     pub data: ScopeData,
+}
+
+impl CurScope<'_> {
+    fn add_decl(&self, id: Id) {
+        {
+            let mut b = self.data.decls.borrow_mut();
+            let v = b.entry(id.0.clone()).or_default();
+            if !v.contains(&id.1) {
+                v.push(id.1);
+            }
+        }
+
+        match self.parent {
+            Some(v) => {
+                v.add_decl(id);
+            }
+            None => {}
+        }
+    }
+
+    fn add_usage(&self, id: Id) {
+        {
+            let mut b = self.data.usages.borrow_mut();
+            let v = b.entry(id.0.clone()).or_default();
+            if !v.contains(&id.1) {
+                v.push(id.1);
+            }
+        }
+
+        match self.parent {
+            Some(v) => {
+                v.add_usage(id);
+            }
+            None => {}
+        }
+    }
 }
 
 pub struct Analyzer<'a> {
     pub data: &'a mut Data,
     pub cur: CurScope<'a>,
+
+    pub is_pat_decl: bool,
 }
+
+impl Analyzer<'_> {}
 
 impl Visit for Analyzer<'_> {
     noop_visit_type!();
+
+    fn visit_expr(&mut self, e: &Expr, _: &dyn Node) {
+        e.visit_children_with(self);
+
+        match e {
+            Expr::Ident(i) => {
+                self.cur.add_usage(i.to_id());
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_member_expr(&mut self, e: &MemberExpr, _: &dyn Node) {
+        e.obj.visit_with(e, self);
+
+        if e.computed {
+            e.obj.visit_with(e, self);
+        }
+    }
+
+    fn visit_pat(&mut self, p: &Pat, _: &dyn Node) {
+        p.visit_children_with(self);
+
+        match p {
+            Pat::Ident(i) => {
+                if self.is_pat_decl {
+                    self.cur.add_decl(i.id.to_id());
+                } else {
+                    self.cur.add_usage(i.id.to_id());
+                }
+            }
+            _ => {}
+        }
+    }
 }
--- ecmascript/transforms/base/src/hygiene/hygiene3.rs
+++ ecmascript/transforms/base/src/hygiene/hygiene3.rs
@@ -1,9 +1,11 @@
 //! Third one
 
-use crate::hygiene::unique_scope::unique_scope;
-use swc_common::{chain, SyntaxContext};
+use crate::hygiene::{analyzer::CurScope, unique_scope::unique_scope};
+use swc_common::{chain, SyntaxContext, DUMMY_SP};
 use swc_ecma_ast::*;
-use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith, VisitWith};
+
+use super::analyzer::Analyzer;
 
 pub fn hygiene3() -> impl Fold + VisitMut {
     as_folder(chain!(unique_scope(), Renaming::default()))
@@ -15,7 +17,25 @@ struct Renaming {
 }
 
 impl Renaming {
-    fn analyze<N>(&mut self, n: &N) {}
+    fn analyze<N>(&mut self, n: &N)
+    where
+        for<'aa> N: VisitWith<Analyzer<'aa>>,
+    {
+        let mut data = Default::default();
+        let mut v = Analyzer {
+            data: &mut data,
+            cur: CurScope {
+                parent: None,
+                scope_ctxt: SyntaxContext::empty(),
+                data: Default::default(),
+            },
+            is_pat_decl: false,
+        };
+
+        dbg!(&data);
+
+        n.visit_with(&Invalid { span: DUMMY_SP }, &mut v);
+    }
 }
 
 impl VisitMut for Renaming {
