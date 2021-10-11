1d3f1153aea6e37d546d94fe7083541e22fd7d3e
Donny <kdy1997.dev@gmail.com>
2021-10-12 05:48:40 +0900

    More

--- ecmascript/transforms/base/src/hygiene/analyzer.rs
+++ ecmascript/transforms/base/src/hygiene/analyzer.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use swc_common::{collections::AHashMap, SyntaxContext};
+use swc_ecma_visit::{noop_visit_type, Visit};
 
 pub struct Data {
     pub scopes: AHashMap<SyntaxContext, ScopeData>,
@@ -14,4 +16,8 @@ pub struct CurScope<'a> {
 pub struct Analyzer<'a> {
     pub data: &'a mut Data,
     pub cur: CurScope<'a>,
+}
+
+impl Visit for Analyzer<'_> {
+    noop_visit_type!();
 }
