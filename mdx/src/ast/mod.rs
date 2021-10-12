aab5760419f1672e008ff6893c820d08abe323e6
Donny <kdy1997.dev@gmail.com>
2021-10-12 23:13:07 +0900

    Add ast

---
+++ mdx/src/ast/mod.rs
@@ -0,0 +1,65 @@
+use swc_common::{Span, Spanned};
+use swc_ecma_ast::*;
+
+#[derive(Debug, Clone, PartialEq, Spanned)]
+pub struct MdxFile {
+    pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BlockNode {
+    Es6(ModuleDecl),
+
+    JsxEl(JSXElement),
+    JsxFrag(JSXFragment),
+
+    Header(Vec<TextNode>, usize),
+    Paragraph(Vec<TextNode>),
+    Blockquote(Vec<BlockNode>),
+    CodeBlock(Option<String>, String),
+    OrderedList {
+        span: Span,
+        items: Vec<ListItem>,
+        list_type: String,
+    },
+    UnorderedList {
+        span: Span,
+        items: Vec<ListItem>,
+    },
+    Raw(Text),
+    Hr,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ListItem {
+    Simple(Vec<TextNode>),
+    Paragraph(Vec<BlockNode>),
+}
+
+#[derive(Debug, Clone, PartialEq, Spanned)]
+pub struct TextNode {
+    pub span: Span,
+    pub kind: TextNodeKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TextNodeKind {
+    Break,
+    Text(String),
+    Code(String),
+    Link(String, String, Option<String>),
+    Image(String, String, Option<String>),
+    Emphasis(Vec<TextNode>),
+    Strong(Vec<TextNode>),
+}
+
+#[derive(Debug, Clone, PartialEq, Spanned)]
+pub enum InlineNode {
+    Jsx(JSXElement),
+}
+
+#[derive(Debug, Clone, PartialEq, Spanned)]
+pub struct Text {
+    pub span: Span,
+    pub value: String,
+}
