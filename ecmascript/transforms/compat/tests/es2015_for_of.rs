f6ef33325362693adfae64ae3933038f62d83b83
Donny <kdy1997.dev@gmail.com>
2021-10-13 16:20:08 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/es2015_for_of.rs
+++ ecmascript/transforms/compat/tests/es2015_for_of.rs
@@ -206,49 +206,50 @@ test!(
     continue b;
   }
 }"#,
-    r#"var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-
-try {
-  b: for (var _iterator = d()[Symbol.iterator](), _step; !(_iteratorNormalCompletion =
-      (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    let c = _step.value;
+    "
     var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-
-    try {
-      for (var _iterator = f()[Symbol.iterator](), _step1; !(_iteratorNormalCompletion =
-          (_step1 = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        let e = _step1.value;
-        continue b;
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-          _iterator.return();
+try {
+    b: for(var _iterator = d()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = \
+     _iterator.next()).done); _iteratorNormalCompletion = true){
+        let c = _step.value;
+        var _iteratorNormalCompletion1 = true, _didIteratorError1 = false, _iteratorError1 = \
+     undefined;
+        try {
+            for(var _iterator1 = f()[Symbol.iterator](), _step1; !(_iteratorNormalCompletion1 = \
+     (_step1 = _iterator1.next()).done); _iteratorNormalCompletion1 = true){
+                let e = _step1.value;
+                continue b;
+            }
+        } catch (err) {
+            _didIteratorError1 = true;
+            _iteratorError1 = err;
+        } finally{
+            try {
+                if (!_iteratorNormalCompletion1 && _iterator1.return != null) {
+                    _iterator1.return();
+                }
+            } finally{
+                if (_didIteratorError1) {
+                    throw _iteratorError1;
+                }
+            }
         }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
     }
-  }
 } catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
-  try {
-    if (!_iteratorNormalCompletion && _iterator.return != null) {
-      _iterator.return();
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally{
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+            _iterator.return();
+        }
+    } finally{
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
     }
-  } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
-  }
-}"#,
+}
+    ",
     ok_if_code_eq
 );
 
