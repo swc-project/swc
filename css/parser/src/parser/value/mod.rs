9c8262e23af2b23c485dfbffd546b45bb55c530d
evilebottnawi <sheo13666q@gmail.com>
2021-10-15 19:01:54 +0300

    style(css/parser): fix

--- css/parser/src/parser/value/mod.rs
+++ css/parser/src/parser/value/mod.rs
@@ -383,14 +383,18 @@ where
                     // Unit
                     let value = Num { span, value };
                     match bump!(self) {
-                        Token::Ident { value: unit_value, raw: unit_raw, .. } => {
+                        Token::Ident {
+                            value: unit_value,
+                            raw: unit_raw,
+                            ..
+                        } => {
                             return Ok(Value::Unit(UnitValue {
                                 span: span!(self, span.lo),
                                 value,
                                 unit: Unit {
                                     span: unit_span,
                                     value: unit_value,
-                                    raw: unit_raw
+                                    raw: unit_raw,
                                 },
                             }));
                         }
