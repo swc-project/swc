5fafd4d39e7dd02069e0303b53f26917071dfd76
Donny <kdy1997.dev@gmail.com>
2021-10-16 19:22:13 +0900

    Expose the util

--- ecmascript/transforms/base/src/parallel/mod.rs
+++ ecmascript/transforms/base/src/parallel/mod.rs
@@ -1,1 +1,29 @@
-mod stateless;
+//! This module contains utilties to parallelize based on the cargo feature.
+
+use swc_ecma_visit::{VisitMut, VisitMutWith};
+
+/// This **may** visit nodes in parallel.
+pub fn visit_mut_par<N, V, F>(nodes: &mut Vec<N>, threshold: usize, visitor: F)
+where
+    N: VisitMutWith<V> + Send + Sync,
+    Vec<N>: VisitMutWith<V>,
+    F: Sync + Fn() -> V,
+    V: VisitMut,
+{
+    #[cfg(feature = "rayon")]
+    if nodes.len() >= threshold {
+        use rayon::prelude::*;
+
+        nodes
+            .into_par_iter()
+            .map(|node| {
+                let mut v = visitor();
+                node.visit_mut_with(&mut v);
+            })
+            .reduce(|| (), |_, _| {});
+
+        return;
+    }
+
+    nodes.visit_mut_with(&mut visitor())
+}
