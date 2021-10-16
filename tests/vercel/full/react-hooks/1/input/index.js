b088434862a6e09dee3848055c960c5943dc7862
Donny <kdy1997.dev@gmail.com>
2021-10-16 23:15:23 +0900

    Add a test

---
+++ tests/vercel/full/react-hooks/1/input/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import { useProject } from '@swr/use-project';
+import useTeam from '@swr/use-team';
+
+export default function MyComp() {
+    const { project: projectName } = useRouter().query;
+    const { data: projectInfo } = useProject(projectName);
+    const { teamSlug } = useTeam();
+    const projectId = projectInfo?.id;
+    let { data: branches } = useProjectBranches(projectId);
+
+    return (
+        <></>
+    );
+}
