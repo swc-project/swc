cec3bbb25a4f18ab1bd3e0c436ac4c147945b07a
Donny <kdy1997.dev@gmail.com>
2021-10-16 23:45:09 +0900

    I want cargo-expand

---
+++ ecmascript/transforms/base/tests/macros.rs
@@ -0,0 +1,14 @@
+use swc_ecma_ast::*;
+use swc_ecma_transforms_base::perf::Parallel;
+use swc_ecma_transforms_macros::parallel;
+use swc_ecma_visit::{noop_visit_mut_type, VisitMut};
+
+#[derive(Default, Clone, Copy)]
+struct Example;
+
+impl Parallel for Example {
+    fn merge(&mut self, _: Self) {}
+}
+
+#[parallel]
+impl VisitMut for Example {}
