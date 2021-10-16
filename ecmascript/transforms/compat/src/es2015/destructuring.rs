b13f42608ddb3ef7ad5e50cc6f0ca9a62097fb52
Donny <kdy1997.dev@gmail.com>
2021-10-17 00:18:27 +0900

    destructuring: Remove exponential check

--- ecmascript/transforms/compat/src/es2015/destructuring.rs
+++ ecmascript/transforms/compat/src/es2015/destructuring.rs
@@ -2,12 +2,12 @@ use serde::Deserialize;
 use std::iter;
 use swc_common::{Spanned, SyntaxContext, DUMMY_SP};
 use swc_ecma_ast::*;
-use swc_ecma_transforms_base::{helper, perf::Check};
+use swc_ecma_transforms_base::helper;
 use swc_ecma_utils::{
     alias_ident_for, alias_if_required, has_rest_pat, is_literal, private_ident, prop_name_to_expr,
     quote_ident, undefined, ExprFactory, StmtLike,
 };
-use swc_ecma_visit::{noop_fold_type, noop_visit_type, Fold, FoldWith, Node, Visit, VisitWith};
+use swc_ecma_visit::{noop_fold_type, Fold, FoldWith};
 
 /// `@babel/plugin-transform-destructuring`
 ///
@@ -876,14 +876,9 @@ impl Fold for AssignFolder {
 impl Destructuring {
     fn fold_stmt_like<T>(&mut self, stmts: Vec<T>) -> Vec<T>
     where
-        Vec<T>: FoldWith<Self> + VisitWith<DestructuringVisitor>,
-        T: StmtLike + VisitWith<DestructuringVisitor> + FoldWith<AssignFolder>,
+        Vec<T>: FoldWith<Self>,
+        T: StmtLike + FoldWith<AssignFolder>,
     {
-        // fast path
-        if !has_destructuring(&stmts) {
-            return stmts;
-        }
-
         let stmts = stmts.fold_children_with(self);
 
         let mut buf = Vec::with_capacity(stmts.len());
@@ -1092,37 +1087,5 @@ fn can_be_null(e: &Expr) -> bool {
         Expr::OptChain(ref e) => can_be_null(&e.expr),
 
         Expr::Invalid(..) => unreachable!(),
-    }
-}
-
-fn has_destructuring<N>(node: &N) -> bool
-where
-    N: VisitWith<DestructuringVisitor>,
-{
-    let mut v = DestructuringVisitor { found: false };
-    node.visit_with(&Invalid { span: DUMMY_SP } as _, &mut v);
-    v.found
-}
-
-#[derive(Default)]
-struct DestructuringVisitor {
-    found: bool,
-}
-
-impl Visit for DestructuringVisitor {
-    noop_visit_type!();
-
-    fn visit_pat(&mut self, node: &Pat, _: &dyn Node) {
-        node.visit_children_with(self);
-        match *node {
-            Pat::Ident(..) => {}
-            _ => self.found = true,
-        }
-    }
-}
-
-impl Check for DestructuringVisitor {
-    fn should_handle(&self) -> bool {
-        self.found
     }
 }
