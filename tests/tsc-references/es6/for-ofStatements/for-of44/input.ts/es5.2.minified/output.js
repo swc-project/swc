533847f0c745abae640815e87ea9811702254e33
Donny <kdy1997.dev@gmail.com>
2021-10-18 16:11:00 +0900

    Update test refs

--- tests/tsc-references/es6/for-ofStatements/for-of44/input.ts/es5.2.minified/output.js
+++ tests/tsc-references/es6/for-ofStatements/for-of44/input.ts/es5.2.minified/output.js
@@ -19,22 +19,22 @@ function _slicedToArray(arr, i) {
         throw new TypeError("Invalid attempt to destructure non-iterable instance");
     })();
 }
-var _iteratorNormalCompletion = !0, _didIteratorError = !1, _iteratorError = void 0;
+var array = [
+    [
+        0,
+        ""
+    ],
+    [
+        0,
+        !0
+    ],
+    [
+        1,
+        Symbol()
+    ]
+], _iteratorNormalCompletion = !0, _didIteratorError = !1, _iteratorError = void 0;
 try {
-    for(var _step, _iterator = [
-        [
-            0,
-            ""
-        ],
-        [
-            0,
-            !0
-        ],
-        [
-            1,
-            Symbol()
-        ]
-    ][Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = !0)var _value = _slicedToArray(_step.value, 2);
+    for(var _step, _iterator = array[Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = !0)var _value = _slicedToArray(_step.value, 2);
     _value[0], _value[1];
 } catch (err) {
     _didIteratorError = !0, _iteratorError = err;
--- tests/tsc-references/es6/for-ofStatements/for-of49/input.ts/es5.2.minified/output.js
+++ tests/tsc-references/es6/for-ofStatements/for-of49/input.ts/es5.2.minified/output.js
@@ -1,11 +1,11 @@
-var _iteratorNormalCompletion = !0, _didIteratorError = !1, _iteratorError = void 0;
+var map = new Map([
+    [
+        "",
+        !0
+    ]
+]), _iteratorNormalCompletion = !0, _didIteratorError = !1, _iteratorError = void 0;
 try {
-    for(var ref, _step, _iterator = new Map([
-        [
-            "",
-            !0
-        ]
-    ])[Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = !0)(ref = _step.value)[0], ref.slice(1)[0];
+    for(var ref, _step, _iterator = map[Symbol.iterator](); !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = !0)(ref = _step.value)[0], ref.slice(1)[0];
 } catch (err) {
     _didIteratorError = !0, _iteratorError = err;
 } finally{
