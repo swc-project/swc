bf6e6db1e4b399377b4d448a376a11accb160008
Donny <kdy1997.dev@gmail.com>
2021-10-19 17:52:50 +0900

    Update test refs

--- tests/fixture/options/globals/typeofs-declared/output/index.js
+++ tests/fixture/options/globals/typeofs-declared/output/index.js
@@ -1,3 +1,5 @@
-const window = 'foo';
-
-console.log(typeof window);
+var _typeof = function(obj) {
+    return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj;
+};
+var window = 'foo';
+console.log(typeof window === "undefined" ? "undefined" : _typeof(window));
