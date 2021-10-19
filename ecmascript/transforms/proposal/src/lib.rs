5485997d49d3019c5cb2a4d09374afd9fd215c99
sosukesuzuki <aosukeke@gmail.com>
2021-10-19 17:33:24 +0900

    Add transformer

--- ecmascript/transforms/proposal/src/lib.rs
+++ ecmascript/transforms/proposal/src/lib.rs
@@ -1,9 +1,11 @@
 pub use self::{
     decorators::decorators, export_default_from::export_default_from,
     import_assertions::import_assertions, private_in_object::private_in_object,
+    static_blocks::static_blocks,
 };
 
 pub mod decorators;
 mod export_default_from;
 mod import_assertions;
 pub mod private_in_object;
+pub mod static_blocks;
---
+++ ecmascript/transforms/proposal/src/static_blocks.rs
@@ -0,0 +1,94 @@
+use std::collections::HashSet;
+use swc_common::DUMMY_SP;
+use swc_ecma_ast::*;
+use swc_ecma_visit::{noop_fold_type, Fold};
+
+struct ClassStaticBlock;
+
+pub fn static_blocks() -> impl Fold {
+    ClassStaticBlock
+}
+
+impl ClassStaticBlock {
+    fn fold_class_for_static_block(&mut self, class: Class) -> Class {
+        let mut private_names = HashSet::new();
+        for member in &class.body {
+            if let ClassMember::PrivateProp(private_property) = member {
+                private_names.insert(private_property.key.id.sym.clone());
+            }
+        }
+        let mut members = vec![];
+        for member in class.body {
+            let new_member = match member {
+                ClassMember::StaticBlock(static_block) => {
+                    // TODO: Generate an id that does not duplicate with the id of private fields in
+                    // the class
+                    let static_block_private_id = { "_".to_string() };
+                    ClassMember::PrivateProp(
+                        self.fold_static_block(static_block, static_block_private_id),
+                    )
+                }
+                m => m,
+            };
+            members.push(new_member);
+        }
+        Class {
+            body: members,
+            ..class
+        }
+    }
+    fn fold_static_block(&mut self, static_block: StaticBlock, private_id: String) -> PrivateProp {
+        PrivateProp {
+            span: DUMMY_SP,
+            is_static: true,
+            is_abstract: false,
+            is_optional: false,
+            is_override: false,
+            readonly: false,
+            computed: false,
+            definite: false,
+            type_ann: None,
+            decorators: Vec::new(),
+            accessibility: None,
+            key: PrivateName {
+                span: DUMMY_SP,
+                id: Ident {
+                    span: DUMMY_SP,
+                    sym: private_id.into(),
+                    optional: false,
+                },
+            },
+            value: Some(Box::new(Expr::Call(CallExpr {
+                span: DUMMY_SP,
+                callee: ExprOrSuper::Expr(Box::new(Expr::Arrow(ArrowExpr {
+                    span: DUMMY_SP,
+                    params: Vec::new(),
+                    is_async: false,
+                    is_generator: false,
+                    type_params: None,
+                    return_type: None,
+                    body: BlockStmtOrExpr::BlockStmt(static_block.body),
+                }))),
+                args: Vec::new(),
+                type_args: None,
+            }))),
+        }
+    }
+}
+
+impl Fold for ClassStaticBlock {
+    noop_fold_type!();
+
+    fn fold_decl(&mut self, declaration: Decl) -> Decl {
+        match declaration {
+            Decl::Class(class_declaration) => {
+                let class = self.fold_class_for_static_block(class_declaration.class);
+                Decl::Class(ClassDecl {
+                    class,
+                    ..class_declaration
+                })
+            }
+            _ => declaration,
+        }
+    }
+}
