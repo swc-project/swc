7b1ff4709313f2b9deb99b04cb23e97ab741ca2f
Donny <kdy1997.dev@gmail.com>
2021-10-21 16:34:44 +0900

    Improve test script

--- bundler/scripts/minifier.sh
+++ bundler/scripts/minifier.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-export RUST_LOG=swc_ecma_minifier=trace
+export RUST_LOG=swc_ecma_minifier=debug
 export RUST_BACKTRACE=1
 
 cargo test --features swc_ecma_minifier/debug --test deno $1
