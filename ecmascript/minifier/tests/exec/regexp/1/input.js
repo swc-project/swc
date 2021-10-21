ee703acca60a598f583934e86c25cc4d2d2940bb
Donny <kdy1997.dev@gmail.com>
2021-10-21 17:42:16 +0900

    fixup

--- ecmascript/minifier/tests/exec/regexp/1/input.js
+++ ecmascript/minifier/tests/exec/regexp/1/input.js
@@ -1,5 +1,10 @@
 
 
-export function compile(attributePattern, flags) {
+function compile(attributePattern, flags) {
     return new RegExp(`(?:^|;)\\s*${attributePattern}\s*=\s*([^";\s][^;\s]*|"(?:[^"\\]|\\"?)+"?)`, flags);
+
 }
+
+console.log(compile("foo", "g"));
+console.log(compile("bar", "g"));
+console.log(compile("baz", "g"));
