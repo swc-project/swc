8147b833abd768b3fbc902545c86e4c6f959ab33
Donny <kdy1997.dev@gmail.com>
2021-10-21 22:47:24 +0900

    Update test refs

--- ecmascript/transforms/react/src/refresh/tests.rs
+++ ecmascript/transforms/react/src/refresh/tests.rs
@@ -1416,5 +1416,15 @@ test!(
 
     export const Comp = dynamic(() => import('../Content'), { ssr: false })
     ",
-    ""
+    "
+    import dynamic from 'next/dynamic';
+    export const Comp = dynamic(_c = ()=>import('../Content')
+    , {
+        ssr: false
+    });
+    _c1 = Comp;
+    var _c, _c1;
+    $RefreshReg$(_c, 'Comp$dynamic');
+    $RefreshReg$(_c1, 'Comp');
+"
 );
