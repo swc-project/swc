81b1fb38e41b75e98142d686cc9c5bc6dc85a25a
Donny <kdy1997.dev@gmail.com>
2021-10-22 16:50:20 +0900

    Change order

--- ecmascript/minifier/src/pass/mangle_names/mod.rs
+++ ecmascript/minifier/src/pass/mangle_names/mod.rs
@@ -240,27 +240,27 @@ impl VisitMut for Mangler<'_> {
     }
 
     fn visit_mut_fn_decl(&mut self, n: &mut FnDecl) {
+        self.rename_decl(&mut n.ident);
+
         self.with_scope(|v| {
             n.function.visit_mut_with(v);
         });
-
-        self.rename_decl(&mut n.ident);
     }
 
     fn visit_mut_fn_expr(&mut self, n: &mut FnExpr) {
         self.with_scope(|v| {
-            n.function.visit_mut_with(v);
-
             if let Some(i) = &mut n.ident {
                 v.rename_decl(i);
             }
+
+            n.function.visit_mut_with(v);
         })
     }
 
     fn visit_mut_function(&mut self, n: &mut Function) {
-        n.body.visit_mut_with(self);
-
         n.params.visit_mut_with(self);
+
+        n.body.visit_mut_with(self);
 
         n.decorators.visit_mut_with(self);
     }
@@ -438,11 +438,12 @@ impl VisitMut for Mangler<'_> {
 
     fn visit_mut_var_declarator(&mut self, n: &mut VarDeclarator) {
         let old = self.data.is_pat_decl;
-        self.data.is_pat_decl = true;
-        n.name.visit_mut_with(self);
 
         self.data.is_pat_decl = false;
         n.init.visit_mut_with(self);
+
+        self.data.is_pat_decl = true;
+        n.name.visit_mut_with(self);
 
         self.data.is_pat_decl = old;
     }
