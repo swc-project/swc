f2b434195b20563171d920cfba1fc5610758d98c
Donny <kdy1997.dev@gmail.com>
2021-10-22 20:11:40 +0900

    More work for analyzer

--- ecmascript/minifier/src/pass/mangle_names/analyzer.rs
+++ ecmascript/minifier/src/pass/mangle_names/analyzer.rs
@@ -1,1 +1,96 @@
-pub(super) struct Analyzer<'_> {}
+use swc_atoms::JsWord;
+use swc_common::collections::AHashMap;
+use swc_ecma_ast::*;
+use swc_ecma_utils::{ident::IdentLike, Id};
+use swc_ecma_visit::{noop_visit_type, Node, Visit, VisitWith};
+
+#[derive(Default)]
+pub(super) struct Analyzer<'a> {
+    rename: AHashMap<Id, JsWord>,
+    scope: Scope<'a>,
+
+    is_pat_decl: bool,
+}
+
+#[derive(Debug, Default)]
+pub(super) struct Scope<'a> {
+    parent: Option<&'a Scope<'a>>,
+}
+
+impl Analyzer<'_> {
+    fn add_decl(&mut self, id: Id) {}
+
+    fn add_usage(&mut self, id: Id) {}
+
+    fn with_scope<F>(&mut self, op: F)
+    where
+        F: for<'aa> FnOnce(&mut Analyzer<'aa>),
+    {
+    }
+}
+
+impl Visit for Analyzer<'_> {
+    noop_visit_type!();
+
+    fn visit_expr(&mut self, e: &Expr, _: &dyn Node) {
+        e.visit_children_with(self);
+
+        match e {
+            Expr::Ident(i) => self.add_usage(i.to_id()),
+            _ => {}
+        }
+    }
+
+    fn visit_fn_decl(&mut self, f: &FnDecl, _: &dyn Node) {
+        self.add_decl(f.ident.to_id());
+
+        self.with_scope(|v| {
+            f.function.visit_with(f, v);
+        })
+    }
+
+    fn visit_fn_expr(&mut self, f: &FnExpr, _: &dyn Node) {
+        self.with_scope(|v| {
+            if let Some(id) = &f.ident {
+                v.add_decl(id.to_id());
+            }
+
+            f.function.visit_with(f, v);
+        })
+    }
+
+    fn visit_member_expr(&mut self, e: &MemberExpr, _: &dyn Node) {
+        e.obj.visit_with(e, self);
+
+        if e.computed {
+            e.prop.visit_with(e, self);
+        }
+    }
+
+    fn visit_pat(&mut self, e: &Pat, _: &dyn Node) {
+        e.visit_children_with(self);
+
+        match e {
+            Pat::Ident(i) => {
+                if self.is_pat_decl {
+                    self.add_decl(i.to_id())
+                } else {
+                    self.add_usage(i.to_id())
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_var_declarator(&mut self, v: &VarDeclarator, _: &dyn Node) {
+        let old = self.is_pat_decl;
+
+        self.is_pat_decl = true;
+        v.name.visit_with(v, self);
+
+        self.is_pat_decl = false;
+        v.init.visit_with(v, self);
+
+        self.is_pat_decl = old;
+    }
+}
