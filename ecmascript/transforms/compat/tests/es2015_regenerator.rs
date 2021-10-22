2e8e788f29bee5e113dc2fe47dfbe60368a03357
sosukesuzuki <aosukeke@gmail.com>
2021-10-22 22:31:45 +0900

    cargo fmt

--- ecmascript/transforms/compat/tests/es2015_regenerator.rs
+++ ecmascript/transforms/compat/tests/es2015_regenerator.rs
@@ -2,7 +2,7 @@ use swc_common::{chain, comments::SingleThreadedComments, Mark};
 use swc_ecma_parser::Syntax;
 use swc_ecma_transforms_base::resolver::resolver;
 use swc_ecma_transforms_compat::{
-    es2015, es2015::regenerator, es2016, es2017, es2017::async_to_generator, es2018, es2021, es2022
+    es2015, es2015::regenerator, es2016, es2017, es2017::async_to_generator, es2018, es2021, es2022,
 };
 use swc_ecma_transforms_testing::{test, test_exec};
 use swc_ecma_visit::Fold;
