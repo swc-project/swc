751b8ff194dab8f704e4d1e035d43a03ef76c6d3
Donny <kdy1997.dev@gmail.com>
2021-10-22 13:31:34 +0900

    Add a test for #1912

---
+++ bundler/tests/deno-exec/issue-1912/case1/entry.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Spotify } from "https://framer.com/m/framer/Spotify.js@0.4.0";
+
+console.log(typeof React);
+console.log(typeof render);
+console.log(typeof Spotify);
