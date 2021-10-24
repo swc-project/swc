6d10d08ed4c0d2f44b010661000729aa868a3445
Donny <kdy1997.dev@gmail.com>
2021-10-24 17:37:03 +0900

    Seems good

--- ecmascript/minifier/src/pass/mangle_names/analyzer/scope.rs
+++ ecmascript/minifier/src/pass/mangle_names/analyzer/scope.rs
@@ -1,7 +1,12 @@
 use std::cell::RefCell;
 use swc_atoms::JsWord;
-use swc_common::collections::{AHashMap, AHashSet};
+use swc_common::{
+    collections::{AHashMap, AHashSet},
+    util::take::Take,
+};
 use swc_ecma_utils::Id;
+
+use crate::util::base54::incr_base54;
 
 #[derive(Debug, Default)]
 pub(crate) struct Scope {
@@ -14,8 +19,6 @@ pub(crate) struct Scope {
 pub struct ScopeData {
     decls: RefCell<AHashSet<Id>>,
 
-    n: usize,
-
     /// Usages in current scope.
     usages: RefCell<AHashSet<Id>>,
 
@@ -26,7 +29,7 @@ impl Scope {
     pub(super) fn add_decl(&mut self, id: &Id) {
         self.data.decls.get_mut().insert(id.clone());
         {
-            let mut b = self.data.queue.get_mut();
+            let b = self.data.queue.get_mut();
             if !b.contains(id) {
                 b.push(id.clone());
             }
@@ -37,5 +40,45 @@ impl Scope {
         self.data.usages.get_mut().insert(id.clone());
     }
 
-    pub(super) fn rename(&mut self, to: &mut AHashMap<Id, JsWord>) {}
+    pub(super) fn rename(&mut self, to: &mut AHashMap<Id, JsWord>) {
+        let mut n = 0;
+        for id in self.data.queue.get_mut().take() {
+            loop {
+                let (_, sym) = incr_base54(&mut n);
+
+                let sym: JsWord = sym.into();
+
+                if self.can_rename(&id, &sym, &to) {
+                    to.insert(id.clone(), sym.clone());
+                    break;
+                }
+            }
+        }
+
+        for child in self.children.iter_mut() {
+            child.rename(to);
+        }
+    }
+
+    fn can_rename(&self, id: &Id, symbol: &JsWord, renamed: &AHashMap<Id, JsWord>) -> bool {
+        for used_id in self.data.usages.borrow().iter() {
+            if *used_id == *id {
+                continue;
+            }
+
+            if let Some(renamed_id) = renamed.get(used_id) {
+                if renamed_id == symbol {
+                    return false;
+                }
+            }
+        }
+
+        for c in self.children.iter() {
+            if !c.can_rename(id, symbol, renamed) {
+                return false;
+            }
+        }
+
+        true
+    }
 }
