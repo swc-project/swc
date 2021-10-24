26a7d81b8f55af5615828c2e2876a064941e95c2
Donny <kdy1997.dev@gmail.com>
2021-10-24 15:24:04 +0900

    Add more tests

---
+++ ecmascript/transforms/compat/tests/fixture/async-to-generator/for-await/2/exec.js
@@ -0,0 +1,15 @@
+function test() {
+    const hasDataInReplicache = async (replicache) => {
+        for await (const _ of [1, 3, 5, 7, 9]) {
+            return true
+        }
+        return false
+    }
+
+    console.log(hasDataInReplicache())
+    console.log(hasDataInReplicache())
+
+}
+
+test()
+test()
