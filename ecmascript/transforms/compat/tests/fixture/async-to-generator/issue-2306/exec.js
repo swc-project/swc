03faaef7b9e42c8e624b1337d1b0b3f1343b747e
Donny <kdy1997.dev@gmail.com>
2021-10-25 13:21:47 +0900

    Add tests for #2306

---
+++ ecmascript/transforms/compat/tests/fixture/async-to-generator/issue-2306/exec.js
@@ -0,0 +1,15 @@
+async function* g1() {
+    yield "OK";
+    yield "OK";
+    yield "OK";
+}
+
+async function* g2() {
+    for await (const g of g1()) {
+        yield g;
+    }
+}
+
+for await (const g of g2()) {
+    console.log(g);
+}
