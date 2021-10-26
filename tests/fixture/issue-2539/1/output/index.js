b6c05f73cce1319b072ceab08252fb8b2e8e34b2
Donny <kdy1997.dev@gmail.com>
2021-10-26 16:05:03 +0900

    Update test refs

---
+++ tests/fixture/issue-2539/1/output/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = void 0;
+var _default = {
+    foo: {
+        func1 (index) {
+        },
+        func2 (index, index1) {
+        }
+    }
+};
+exports.default = _default;
