b7e29185f0ca62a17b089a375bda0ffaf191decc
Christopher Hlubek <christopher.hlubek@networkteam.com>
2021-10-27 23:00:22 +0200

    fix(es/transform/react): Multiple JSX pragmas

--- ecmascript/transforms/react/src/jsx/tests.rs
+++ ecmascript/transforms/react/src/jsx/tests.rs
@@ -1365,6 +1365,61 @@ exports.default = RandomComponent;
 "#
 );
 
+test!(
+    ::swc_ecma_parser::Syntax::Es(::swc_ecma_parser::EsConfig {
+        jsx: true,
+        ..Default::default()
+    }),
+    |t| tr(t, Default::default(), Mark::fresh(Mark::root())),
+    mdx_react_jsx,
+    r#"
+/* @jsxRuntime automatic @jsxImportSource react */
+import {Fragment as _Fragment, jsx as _jsx, jsxs as _jsxs} from 'react/jsx-runtime'
+
+export const Thing = () => _jsx(_Fragment, {children: 'World!'})
+
+function MDXContent(props = {}) {
+  const _components = Object.assign({h1: 'h1'}, props.components)
+  const {wrapper: MDXLayout} = _components
+  const _content = _jsx(_Fragment, {
+    children: _jsxs(_components.h1, {
+      children: ['Hello, ', _jsx(Thing, {})]
+    })
+  })
+  return MDXLayout
+    ? _jsx(MDXLayout, Object.assign({}, props, {children: _content}))
+    : _content
+}
+
+export default MDXContent
+"#,
+    r#"
+/* @jsxRuntime automatic @jsxImportSource react */
+import {Fragment as _Fragment, jsx as _jsx, jsxs as _jsxs} from 'react/jsx-runtime'
+
+export const Thing = function() {
+  return _jsx(_Fragment, {
+    children: "World!"
+  });
+};
+
+function MDXContent(props = {}) {
+  const _components = Object.assign({h1: 'h1'}, props.components)
+  const {wrapper: MDXLayout} = _components
+  const _content = _jsx(_Fragment, {
+    children: _jsxs(_components.h1, {
+      children: ['Hello, ', _jsx(Thing, {})]
+    })
+  })
+  return MDXLayout
+    ? _jsx(MDXLayout, Object.assign({}, props, {children: _content}))
+    : _content
+}
+
+export default MDXContent
+"#
+);
+
 #[testing::fixture("tests/jsx/fixture/**/input.js")]
 fn fixture(input: PathBuf) {
     let mut output = input.with_file_name("output.js");
