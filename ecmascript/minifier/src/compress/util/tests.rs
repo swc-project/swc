64d1b52a063018abc4abfb4f3bc6a37312303e2d
Donny <kdy1997.dev@gmail.com>
2021-10-31 15:00:16 +0900

    Add a test

--- ecmascript/minifier/src/compress/util/tests.rs
+++ ecmascript/minifier/src/compress/util/tests.rs
@@ -100,3 +100,21 @@ fn negate_cost_4() {
         2,
     );
 }
+
+#[test]
+fn negate_cost_5() {
+    assert_negate_cost(
+        "(capacity && codeResult && codeResult = codeResult, !list = config.blacklist && \
+         list.some(function(item) {
+        return Object.keys(item).every(function(key) {
+            return item[key] === codeResult[key];
+        });
+    }) && codeResult = codeResult, \"function\" != typeof filter = config.filter || \
+         filter(codeResult) || capacity--, result.codeResult = codeResult, capture && \
+         canvas.width = imageSize.x, canvas.height = imageSize.y, image_debug.a.drawImage(data, \
+         imageSize, ctx), result.frame = canvas.toDataURL(), results.push(result))",
+        true,
+        true,
+        1000,
+    );
+}
