9cb3bf96dc26fe80f76bbdb6d89b30368e673778
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-11-01 23:11:43 +0900

    fix(es/minifier): Fix bugs of the minifier (#2610)
    swc_ecma_minifier:
     - Increase max pass limit. (#2604, #2604)
    
    swc:
     - Change default value of `compress` and `mangle`. (#2591)

---
+++ ecmascript/minifier/tests/compress/fixture/issues/2605/1/input.js
@@ -0,0 +1,23 @@
+var a = function (state) {
+    return $6aPrw.atTheBeginningOfBlock(state) && $6aPrw.atTheEndOfBlock(state);
+};
+function b() {
+    findDeleteRange(state);
+}
+
+function findDeleteRange(state) {
+    if (!a(state) || $0204f19a3843e92f$export$7abbbf8335ec653(state)) {
+        return state;
+    } else {
+        return $04186525b01d7539$var$range();
+    }
+}
+
+function Foo() {
+    this.x = Foo;
+    this.insertFile = function () {
+        b();
+    };
+}
+
+console.log(cls);
