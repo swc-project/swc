0a5d74f8329db6929f137906ef617acf0325f185
Donny <kdy1997.dev@gmail.com>
2021-11-02 18:15:08 +0900

    Update test refs

---
+++ ecmascript/minifier/tests/compress/fixture/issues/react-countup/2/output.js
@@ -0,0 +1,15 @@
+export function formatNumber(t) {
+    var i, a, n, e;
+    i = Math.abs(t).toFixed(s.options.decimalPlaces);
+    var o = (i += "").split(".");
+    if (a = o[0], n = o.length > 1 ? s.options.decimal + o[1] : "", s.options.useGrouping) {
+        e = "";
+        for(var l = 0, h = a.length; l < h; ++l)0 !== l && l % 3 == 0 && (e = s.options.separator + e), e = a[h - l - 1] + e;
+        a = e;
+    }
+    return s.options.numerals && s.options.numerals.length && (a = a.replace(/[0-9]/g, function(t) {
+        return s.options.numerals[+t];
+    }), n = n.replace(/[0-9]/g, function(t) {
+        return s.options.numerals[+t];
+    })), (t < 0 ? "-" : "") + s.options.prefix + a + n + s.options.suffix;
+}
