e9d59c59dd9e73549a4ae2f343da357520775f4e
Donny <kdy1997.dev@gmail.com>
2021-11-02 20:53:12 +0900

    fixup

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -12,13 +12,7 @@
 //!
 //! Adds methods to generate web sourcemap.
 //!
-//!
-//! ## `dynamic`
-//!
-//! If this feature is enabled, `swc_common` will be built as a dynamic library
-//! (`cdylib`).
 #![deny(unused)]
-#![cfg_attr(feature = "dynamic", crate_type = "dylib")]
 
 pub use self::{
     eq::{EqIgnoreSpan, TypeEq},
