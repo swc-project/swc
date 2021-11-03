49d3d25b47d6ac752d5b015a2b152b278cc79be5
Donny <kdy1997.dev@gmail.com>
2021-11-03 12:32:20 +0900

    Update test refs

--- tests/fixture/issue-1718/case1/output/index.ts
+++ tests/fixture/issue-1718/case1/output/index.ts
@@ -35,70 +35,70 @@ function _scanUser() {
                 case 0:
                     _ctx1.next = 2;
                     return Promise.all(groups.map(_asyncToGenerator(regeneratorRuntime.mark(function _callee(param) {
-                        var users = param.users;
-                        var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, user;
+                        var users, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, user;
                         return regeneratorRuntime.wrap(function _callee$(_ctx) {
                             while(1)switch(_ctx.prev = _ctx.next){
                                 case 0:
+                                    users = param.users;
                                     _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-                                    _ctx.prev = 1;
+                                    _ctx.prev = 2;
                                     _iterator = users[Symbol.iterator]();
-                                case 3:
+                                case 4:
                                     if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-                                        _ctx.next = 11;
+                                        _ctx.next = 12;
                                         break;
                                     }
                                     user = _step.value;
                                     console.log('user', user);
-                                    _ctx.next = 8;
+                                    _ctx.next = 9;
                                     return new Promise(function(resolve) {
                                         return setTimeout(resolve, 30);
                                     });
-                                case 8:
+                                case 9:
                                     _iteratorNormalCompletion = true;
-                                    _ctx.next = 3;
+                                    _ctx.next = 4;
                                     break;
-                                case 11:
-                                    _ctx.next = 17;
+                                case 12:
+                                    _ctx.next = 18;
                                     break;
-                                case 13:
-                                    _ctx.prev = 13;
-                                    _ctx.t0 = _ctx["catch"](1);
+                                case 14:
+                                    _ctx.prev = 14;
+                                    _ctx.t0 = _ctx["catch"](2);
                                     _didIteratorError = true;
                                     _iteratorError = _ctx.t0;
-                                case 17:
-                                    _ctx.prev = 17;
+                                case 18:
                                     _ctx.prev = 18;
+                                    _ctx.prev = 19;
                                     if (!_iteratorNormalCompletion && _iterator.return != null) {
                                         _iterator.return();
                                     }
-                                case 20:
-                                    _ctx.prev = 20;
+                                case 21:
+                                    _ctx.prev = 21;
                                     if (!_didIteratorError) {
-                                        _ctx.next = 23;
+                                        _ctx.next = 24;
                                         break;
                                     }
                                     throw _iteratorError;
-                                case 23:
-                                    return _ctx.finish(20);
                                 case 24:
-                                    return _ctx.finish(17);
+                                    return _ctx.finish(21);
                                 case 25:
+                                    return _ctx.finish(18);
+                                case 26:
                                 case "end":
                                     return _ctx.stop();
                             }
                         }, _callee, null, [
                             [
-                                1,
-                                13,
-                                17,
-                                25
+                                2,
+                                14,
+                                18,
+                                26
                             ],
                             [
-                                18,
+                                19,
                                 ,
-                                20,
-                                24
+                                21,
+                                25
                             ]
                         ]);
                     }))));
