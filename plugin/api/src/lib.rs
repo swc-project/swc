b3310e46bf42545b716050ec671b8ce9af9687fd
Donny <kdy1997.dev@gmail.com>
2021-11-04 15:35:02 +0900

    Remove duplicate

--- plugin/api/src/lib.rs
+++ plugin/api/src/lib.rs
@@ -6,9 +6,6 @@ use abi_stable::{
     std_types::{RResult, RStr, RString},
     StableAbi,
 };
-use anyhow::Context;
-use serde::de::DeserializeOwned;
-use swc_ecma_ast::Program;
 
 #[repr(C)]
 #[derive(StableAbi)]
@@ -26,50 +23,4 @@ impl RootModule for SwcPluginRef {
     const BASE_NAME: &'static str = "swc_plugin";
     const NAME: &'static str = "swc_plugin";
     const VERSION_STRINGS: VersionStrings = package_version_strings!();
-}
-
-#[doc(hidden)]
-pub fn invoke_js_plugin<C, F>(
-    op: fn(C) -> F,
-    config_json: RStr,
-    ast_json: RString,
-) -> RResult<RString, RString>
-where
-    C: DeserializeOwned,
-    F: swc_ecma_visit::Fold,
-{
-    use swc_ecma_visit::FoldWith;
-
-    let config = serde_json::from_str(config_json.as_str())
-        .context("failed to deserialize config string as json");
-    let config: C = match config {
-        Ok(v) => v,
-        Err(err) => return RResult::RErr(format!("{:?}", err).into()),
-    };
-
-    let ast =
-        serde_json::from_str(ast_json.as_str()).context("failed to deserialize ast string as json");
-    let ast: Program = match ast {
-        Ok(v) => v,
-        Err(err) => return RResult::RErr(format!("{:?}", err).into()),
-    };
-
-    let mut tr = op(config);
-
-    let ast = ast.fold_with(&mut tr);
-
-    let res = match serde_json::to_string(&ast) {
-        Ok(v) => v,
-        Err(err) => {
-            return RResult::RErr(
-                format!(
-                    "failed to serialize swc_ecma_ast::Program as json: {:?}",
-                    err
-                )
-                .into(),
-            )
-        }
-    };
-
-    RResult::ROk(res.into())
 }
