b36feed614e5ef8cc55431fd585509b255bb5eeb
Donny <kdy1997.dev@gmail.com>
2021-11-04 12:19:11 +0900

    Move code

--- plugin/api/src/lib.rs
+++ plugin/api/src/lib.rs
@@ -1,9 +1,32 @@
 /// Reexported for convenience.
-use abi_stable::std_types::{RResult, RStr, RString};
+use abi_stable::{
+    library::RootModule,
+    package_version_strings,
+    sabi_types::VersionStrings,
+    std_types::{RResult, RStr, RString},
+    StableAbi,
+};
 use anyhow::Context;
 use serde::de::DeserializeOwned;
 use swc_ecma_ast::Program;
-pub use swc_plugin_api::*;
+
+#[repr(C)]
+#[derive(StableAbi)]
+#[sabi(kind(Prefix(prefix_ref = "SwcPluginRef")))]
+#[sabi(missing_field(panic))]
+pub struct SwcPlugin {
+    #[sabi(last_prefix_field)]
+    pub process_js:
+        Option<extern "C" fn(config_str: RStr, ast_json: RString) -> RResult<RString, RString>>,
+}
+
+impl RootModule for SwcPluginRef {
+    abi_stable::declare_root_module_statics! {SwcPluginRef}
+
+    const BASE_NAME: &'static str = "swc_plugin";
+    const NAME: &'static str = "swc_plugin";
+    const VERSION_STRINGS: VersionStrings = package_version_strings!();
+}
 
 #[doc(hidden)]
 pub fn invoke_js_plugin<C, F>(
@@ -49,28 +72,4 @@ where
     };
 
     RResult::ROk(res.into())
-}
-
-#[macro_export]
-macro_rules! define_js_plugin {
-    ($fn_name:ident) => {
-        #[abi_stable::export_root_module]
-        pub fn swc_library() -> $crate::SwcPluginRef {
-            extern "C" fn swc_js_plugin(
-                config_json: abi_stable::std_types::RStr,
-                ast_json: abi_stable::std_types::RString,
-            ) -> abi_stable::std_types::RResult<
-                abi_stable::std_types::RString,
-                abi_stable::std_types::RString,
-            > {
-                $crate::invoke_js_plugin($fn_name, config_json, ast_json)
-            }
-            use abi_stable::prefix_type::PrefixTypeTrait;
-
-            $crate::SwcPlugin {
-                process_js: Some(swc_js_plugin),
-            }
-            .leak_into_prefix()
-        }
-    };
 }
