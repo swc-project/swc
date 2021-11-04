71265866400adcda03238acd97da44f273cd82e5
Donny <kdy1997.dev@gmail.com>
2021-11-04 13:25:29 +0900

    Use bincode for diagnostics

--- common/src/plugin.rs
+++ common/src/plugin.rs
@@ -3,14 +3,20 @@
 //! We need to replace operations related to thread-local variables in
 //! `swc_common`.
 
-use crate::errors::{Diagnostic, DiagnosticBuilder, Emitter};
-use abi_stable::sabi_trait;
+use crate::errors::{DiagnosticBuilder, Emitter};
+use abi_stable::{sabi_trait, std_types::RVec};
+use anyhow::{Context, Error};
+use serde::{de::DeserializeOwned, Serialize};
+use std::any::type_name;
 
 /// DO NOT USE THIS. This is internal API.
 #[sabi_trait]
 pub trait Runtime {
     /// Emit a structured diagnostic.
-    fn emit(&mut self, db: &Diagnostic);
+    ///
+    /// - `db`: Serialized version of [Diagnostic] which is serialized using
+    ///   bincode.
+    fn emit(&self, db: RVec<u8>);
 }
 
 #[cfg(feature = "plugin-mode")]
@@ -25,7 +31,9 @@ struct PluginEmitter;
 #[cfg(feature = "plugin-mode")]
 impl Emitter for PluginEmitter {
     fn emit(&mut self, db: &DiagnosticBuilder<'_>) {
-        RT.with(|rt| rt.emit(db))
+        let bytes: RVec<_> = serialize_for_plugin(&db.diagnostic).unwrap().into();
+
+        RT.with(|rt| rt.emit(bytes))
     }
 }
 
@@ -41,4 +49,20 @@ where
         // We proxy error reporting to the core runtime.
         HANDLER.set(&handler, || op())
     })
+}
+
+pub fn serialize_for_plugin<T>(t: &T) -> Result<Vec<u8>, Error>
+where
+    T: Serialize,
+{
+    bincode::serialize(&t)
+        .with_context(|| format!("failed to serialize `{}` using bincode", type_name::<T>()))
+}
+
+pub fn deserialize_for_plugin<T>(bytes: &[u8]) -> Result<Vec<u8>, Error>
+where
+    T: DeserializeOwned,
+{
+    bincode::deserialize(bytes)
+        .with_context(|| format!("failed to deserialize `{}` using bincode", type_name::<T>()))
 }
