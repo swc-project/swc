4e12f9f6c4244374b728efc199d165339de351c1
evilebottnawi <sheo13666q@gmail.com>
2021-11-07 20:58:51 +0300

    refactor(css/ast): pseudo element and pseudo class

---
+++ css/parser/src/parser/selector/mod.rs
@@ -0,0 +1,592 @@
+use super::{input::ParserInput, PResult, Parser};
+use crate::{
+    error::{Error, ErrorKind},
+    Parse,
+};
+use swc_atoms::{js_word, JsWord};
+use swc_common::{BytePos, Span};
+use swc_css_ast::*;
+
+impl<I> Parser<I>
+where
+    I: ParserInput,
+{
+    pub(super) fn parse_selectors(&mut self) -> PResult<SelectorList> {
+        self.input.skip_ws()?;
+
+        let child = self.parse_complex_selector()?;
+        let mut children = vec![child];
+
+        loop {
+            self.input.skip_ws()?;
+
+            if !eat!(self, ",") {
+                break;
+            }
+
+            self.input.skip_ws()?;
+
+            let child = self.parse_complex_selector()?;
+
+            children.push(child);
+        }
+
+        let start_pos = match children.first() {
+            Some(ComplexSelector { span, .. }) => span.lo,
+            _ => {
+                unreachable!();
+            }
+        };
+        let last_pos = match children.last() {
+            Some(ComplexSelector { span, .. }) => span.hi,
+            _ => {
+                unreachable!();
+            }
+        };
+
+        Ok(SelectorList {
+            span: Span::new(start_pos, last_pos, Default::default()),
+            children,
+        })
+    }
+
+    fn parse_complex_selector(&mut self) -> PResult<ComplexSelector> {
+        let child = ComplexSelectorChildren::CompoundSelector(self.parse_compound_selector()?);
+        let mut children = vec![child];
+
+        loop {
+            let span = self.input.cur_span()?;
+
+            self.input.skip_ws()?;
+
+            if is_one_of!(self, EOF, ",", "{") {
+                break;
+            }
+
+            let mut combinator = self.parse_combinator()?;
+
+            if combinator.value == CombinatorValue::Descendant {
+                combinator.span = span;
+            } else {
+                self.input.skip_ws()?;
+            }
+
+            children.push(ComplexSelectorChildren::Combinator(combinator));
+
+            let child = self.parse_compound_selector()?;
+
+            children.push(ComplexSelectorChildren::CompoundSelector(child));
+        }
+
+        let start_pos = match children.first() {
+            Some(ComplexSelectorChildren::CompoundSelector(child)) => child.span.lo,
+            _ => {
+                unreachable!();
+            }
+        };
+        let last_pos = match children.last() {
+            Some(ComplexSelectorChildren::CompoundSelector(child)) => child.span.hi,
+            _ => {
+                unreachable!();
+            }
+        };
+
+        Ok(ComplexSelector {
+            span: Span::new(start_pos, last_pos, Default::default()),
+            children,
+        })
+    }
+
+    fn parse_combinator(&mut self) -> PResult<Combinator> {
+        let span = self.input.cur_span()?;
+
+        if eat!(self, ">") {
+            return Ok(Combinator {
+                span,
+                value: CombinatorValue::Child,
+            });
+        } else if eat!(self, "+") {
+            return Ok(Combinator {
+                span,
+                value: CombinatorValue::NextSibling,
+            });
+        } else if eat!(self, "~") {
+            return Ok(Combinator {
+                span,
+                value: CombinatorValue::LaterSibling,
+            });
+        }
+
+        return Ok(Combinator {
+            span,
+            value: CombinatorValue::Descendant,
+        });
+    }
+
+    fn parse_ns_prefix(&mut self) -> PResult<Option<Text>> {
+        let span = self.input.cur_span()?;
+
+        if is!(self, Ident) && peeked_is!(self, "|") {
+            let token = bump!(self);
+            let text = match token {
+                Token::Ident { value, raw } => Text { span, value, raw },
+                _ => {
+                    unreachable!()
+                }
+            };
+
+            bump!(self);
+
+            return Ok(Some(text));
+        } else if is!(self, "*") && peeked_is!(self, "|") {
+            bump!(self);
+            bump!(self);
+
+            let value: JsWord = "*".into();
+            let raw = value.clone();
+
+            return Ok(Some(Text { span, value, raw }));
+        } else if is!(self, "|") {
+            bump!(self);
+
+            return Ok(Some(Text {
+                span: Span::new(span.lo, span.lo, Default::default()),
+                value: js_word!(""),
+                raw: js_word!(""),
+            }));
+        }
+
+        Ok(None)
+    }
+
+    fn parse_wq_name(&mut self) -> PResult<(Option<Text>, Option<Text>)> {
+        let state = self.input.state();
+
+        if is!(self, Ident) && peeked_is!(self, "|")
+            || is!(self, "*") && peeked_is!(self, "|")
+            || is!(self, "|")
+        {
+            let prefix = self.parse_ns_prefix()?;
+
+            if is!(self, Ident) {
+                let span = self.input.cur_span()?;
+                let token = bump!(self);
+                let name = match token {
+                    Token::Ident { value, raw } => Text { span, value, raw },
+                    _ => {
+                        unreachable!()
+                    }
+                };
+
+                return Ok((prefix, Some(name)));
+            } else {
+                // TODO: implement `peeked_ahead_is` for perf
+                self.input.reset(&state);
+            }
+        }
+
+        if is!(self, Ident) {
+            let span = self.input.cur_span()?;
+            let token = bump!(self);
+            let name = match token {
+                Token::Ident { value, raw } => Text { span, value, raw },
+                _ => {
+                    unreachable!()
+                }
+            };
+
+            return Ok((None, Some(name)));
+        }
+
+        Ok((None, None))
+    }
+
+    // TODO: no span as argument
+    fn parse_type_selector(&mut self, span: Span) -> PResult<Option<TypeSelector>> {
+        let start_pos = span.lo;
+
+        let mut prefix = None;
+
+        if let Ok(result) = self.parse_ns_prefix() {
+            prefix = result;
+        }
+
+        if is!(self, Ident) {
+            let span = self.input.cur_span()?;
+            let token = bump!(self);
+            let name = match token {
+                Token::Ident { value, raw } => Text { span, value, raw },
+                _ => {
+                    unreachable!()
+                }
+            };
+
+            return Ok(Some(TypeSelector {
+                span: span!(self, start_pos),
+                prefix,
+                name,
+            }));
+        } else if is!(self, "*") {
+            bump!(self);
+
+            let value: JsWord = "*".into();
+            let raw = value.clone();
+            let name = Text { span, value, raw };
+
+            return Ok(Some(TypeSelector {
+                span: span!(self, start_pos),
+                prefix,
+                name,
+            }));
+        }
+
+        Ok(None)
+    }
+
+    fn parse_id_selector(&mut self) -> PResult<IdSelector> {
+        let span = self.input.cur_span()?;
+
+        let text = match bump!(self) {
+            Token::Hash { value, raw, .. } => Text { span, value, raw },
+            _ => {
+                unreachable!()
+            }
+        };
+
+        Ok(IdSelector {
+            span: span!(self, span.lo),
+            text,
+        })
+    }
+
+    fn parse_class_selector(&mut self) -> PResult<ClassSelector> {
+        let start_pos = self.input.cur_span()?.lo;
+
+        bump!(self);
+
+        let span = self.input.cur_span()?;
+
+        match cur!(self) {
+            Token::Ident { .. } => {}
+            _ => Err(Error::new(span, ErrorKind::ExpectedSelectorText))?,
+        }
+
+        let value = bump!(self);
+        let values = match value {
+            Token::Ident { value, raw } => (value, raw),
+            _ => unreachable!(),
+        };
+
+        Ok(ClassSelector {
+            span: span!(self, start_pos),
+            text: Text {
+                span,
+                value: values.0,
+                raw: values.1,
+            },
+        })
+    }
+
+    fn parse_attribute_selector(&mut self) -> PResult<AttrSelector> {
+        let start_pos = self.input.cur_span()?.lo;
+
+        expect!(self, "[");
+
+        self.input.skip_ws()?;
+
+        let prefix;
+        let name;
+        let mut attr_matcher = None;
+        let mut matcher_value = None;
+        let mut matcher_modifier = None;
+
+        if let Ok((p, Some(n))) = self.parse_wq_name() {
+            prefix = p;
+            name = n;
+        } else {
+            return Err(Error::new(
+                span!(self, start_pos),
+                ErrorKind::InvalidAttrSelectorName,
+            ));
+        }
+
+        self.input.skip_ws()?;
+
+        if !is!(self, "]") {
+            let span = self.input.cur_span()?;
+
+            attr_matcher = match cur!(self) {
+                tok!("~") | tok!("|") | tok!("^") | tok!("$") | tok!("*") => {
+                    let tok = bump!(self);
+
+                    expect!(self, "=");
+
+                    Some(match tok {
+                        tok!("~") => AttrSelectorMatcher::Tilde,
+                        tok!("|") => AttrSelectorMatcher::Bar,
+                        tok!("^") => AttrSelectorMatcher::Caret,
+                        tok!("$") => AttrSelectorMatcher::Dollar,
+                        tok!("*") => AttrSelectorMatcher::Asterisk,
+                        _ => {
+                            unreachable!()
+                        }
+                    })
+                }
+                tok!("=") => {
+                    let tok = bump!(self);
+
+                    Some(match tok {
+                        tok!("=") => AttrSelectorMatcher::Equals,
+                        _ => {
+                            unreachable!()
+                        }
+                    })
+                }
+                _ => Err(Error::new(span, ErrorKind::InvalidAttrSelectorMatcher))?,
+            };
+
+            self.input.skip_ws()?;
+
+            let span = self.input.cur_span()?;
+
+            matcher_value = match cur!(self) {
+                Token::Ident { .. } => {
+                    let value = bump!(self);
+                    let ident = match value {
+                        Token::Ident { value, raw } => (value, raw),
+                        _ => unreachable!(),
+                    };
+
+                    Some(AttrSelectorValue::Text(Text {
+                        span,
+                        value: ident.0,
+                        raw: ident.1,
+                    }))
+                }
+                Token::Str { .. } => {
+                    let value = bump!(self);
+                    let str = match value {
+                        Token::Str { value, raw } => (value, raw),
+                        _ => unreachable!(),
+                    };
+
+                    Some(AttrSelectorValue::Str(Str {
+                        span,
+                        value: str.0,
+                        raw: str.1,
+                    }))
+                }
+                _ => Err(Error::new(span, ErrorKind::InvalidAttrSelectorMatcherValue))?,
+            };
+
+            self.input.skip_ws()?;
+
+            if is!(self, Ident) {
+                let span = self.input.cur_span()?;
+
+                match self.input.cur()? {
+                    Some(Token::Ident { value, .. }) => {
+                        matcher_modifier = value.chars().next();
+
+                        bump!(self);
+                    }
+                    _ => Err(Error::new(span, ErrorKind::InvalidAttrSelectorModifier))?,
+                }
+            }
+
+            self.input.skip_ws()?;
+        }
+
+        expect!(self, "]");
+
+        Ok(AttrSelector {
+            span: span!(self, start_pos),
+            prefix,
+            name,
+            matcher: attr_matcher,
+            value: matcher_value,
+            modifier: matcher_modifier,
+        })
+    }
+
+    fn parse_pseudo_class_selector(&mut self) -> PResult<PseudoClassSelector> {
+        let span = self.input.cur_span()?;
+
+        expect!(self, ":"); // `:`
+
+        // TODO: should be removed, because invalid
+        self.input.skip_ws()?;
+
+        if is!(self, Function) {
+            let fn_span = self.input.cur_span()?;
+            let value = bump!(self);
+            let values = match value {
+                Token::Function { value, raw } => (value, raw),
+                _ => unreachable!(),
+            };
+
+            let args = self.parse_any_value(false)?;
+
+            expect!(self, ")");
+
+            return Ok(PseudoClassSelector {
+                span: span!(self, span.lo),
+                name: Text {
+                    span: Span::new(fn_span.lo, fn_span.hi - BytePos(1), Default::default()),
+                    value: values.0,
+                    raw: values.1,
+                },
+                args,
+            });
+        } else if is!(self, Ident) {
+            let ident_span = self.input.cur_span()?;
+            let value = bump!(self);
+            let values = match value {
+                Token::Ident { value, raw } => (value, raw),
+                _ => unreachable!(),
+            };
+
+            return Ok(PseudoClassSelector {
+                span: span!(self, span.lo),
+                name: Text {
+                    span: ident_span,
+                    value: values.0,
+                    raw: values.1,
+                },
+                args: Default::default(),
+            });
+        }
+
+        let span = self.input.cur_span()?;
+
+        return Err(Error::new(span, ErrorKind::InvalidSelector));
+    }
+
+    fn parse_pseudo_element_selector(&mut self) -> PResult<PseudoElementSelector> {
+        let span = self.input.cur_span()?;
+
+        expect!(self, ":"); // `:`
+
+        let pseudo_class = self.parse_pseudo_class_selector()?;
+
+        return Ok(PseudoElementSelector {
+            span: span!(self, span.lo),
+            name: pseudo_class.name,
+            args: pseudo_class.args,
+        });
+    }
+
+    fn parse_compound_selector(&mut self) -> PResult<CompoundSelector> {
+        let span = self.input.cur_span()?;
+        let start_pos = span.lo;
+
+        let mut nesting_selector = None;
+
+        // TODO: move under option, because it is draft
+        // This is an extension: https://drafts.csswg.org/css-nesting-1/
+        if eat!(self, "&") {
+            nesting_selector = Some(NestingSelector {
+                span: span!(self, start_pos),
+            });
+        }
+
+        let type_selector = self.parse_type_selector(span).unwrap();
+        let mut subclass_selectors = vec![];
+
+        'subclass_selectors: loop {
+            if is!(self, EOF) {
+                break;
+            }
+
+            match cur!(self) {
+                Token::Hash { is_id, .. } => {
+                    if !*is_id {
+                        break 'subclass_selectors;
+                    }
+
+                    subclass_selectors.push(self.parse_id_selector()?.into());
+                }
+
+                tok!(".") => {
+                    subclass_selectors.push(self.parse_class_selector()?.into());
+                }
+
+                tok!("[") => {
+                    let attr = self.parse_attribute_selector()?;
+
+                    subclass_selectors.push(attr.into());
+                }
+
+                tok!(":") => {
+                    if peeked_is!(self, ":") {
+                        while is!(self, ":") {
+                            let pseudo_element = self.parse_pseudo_element_selector()?;
+
+                            subclass_selectors.push(pseudo_element.into());
+                        }
+
+                        break 'subclass_selectors;
+                    }
+
+                    let pseudo_class = self.parse_pseudo_class_selector()?;
+
+                    subclass_selectors.push(pseudo_class.into());
+                }
+
+                Token::AtKeyword { .. } if self.ctx.allow_at_selector => {
+                    let values = match bump!(self) {
+                        Token::AtKeyword { value, raw } => (value, raw),
+                        _ => {
+                            unreachable!()
+                        }
+                    };
+
+                    subclass_selectors.push(SubclassSelector::At(AtSelector {
+                        span,
+                        text: Text {
+                            span,
+                            value: values.0,
+                            raw: values.1,
+                        },
+                    }));
+                    break 'subclass_selectors;
+                }
+
+                _ => {
+                    break 'subclass_selectors;
+                }
+            }
+        }
+
+        let span = span!(self, start_pos);
+
+        if nesting_selector.is_none() && type_selector.is_none() && subclass_selectors.len() == 0 {
+            return Err(Error::new(span, ErrorKind::InvalidSelector));
+        }
+
+        Ok(CompoundSelector {
+            span,
+            nesting_selector,
+            type_selector,
+            subclass_selectors,
+        })
+    }
+}
+
+impl<I> Parse<SelectorList> for Parser<I>
+where
+    I: ParserInput,
+{
+    fn parse(&mut self) -> PResult<SelectorList> {
+        self.parse_selectors()
+    }
+}
+
+impl<I> Parse<ComplexSelector> for Parser<I>
+where
+    I: ParserInput,
+{
+    fn parse(&mut self) -> PResult<ComplexSelector> {
+        self.parse_complex_selector()
+    }
+}
---
+++ css/visit/src/lib.rs
@@ -0,0 +1,493 @@
+use swc_atoms::JsWord;
+use swc_common::Span;
+use swc_css_ast::*;
+use swc_visit::define;
+
+/// Visitable nodes.
+pub trait Node {}
+
+impl<T: ?Sized> Node for T {}
+
+define!({
+    pub struct Text {
+        pub span: Span,
+        pub value: JsWord,
+        pub raw: JsWord,
+    }
+
+    pub struct Str {
+        pub span: Span,
+        pub value: JsWord,
+        pub raw: JsWord,
+    }
+
+    pub struct Num {
+        pub span: Span,
+        pub value: f64,
+        pub raw: JsWord,
+    }
+
+    pub struct Declaration {
+        pub span: Span,
+        pub property: Text,
+        pub value: Vec<Value>,
+        pub important: Option<Span>,
+    }
+
+    pub struct StyleRule {
+        pub span: Span,
+        pub selectors: SelectorList,
+        pub block: Block,
+    }
+
+    pub struct Block {
+        pub span: Span,
+        pub items: Vec<DeclarationBlockItem>,
+    }
+
+    pub enum DeclarationBlockItem {
+        Invalid(Tokens),
+        Declaration(Declaration),
+    }
+
+    pub struct Tokens {
+        pub span: Span,
+        pub tokens: Vec<TokenAndSpan>,
+    }
+
+    pub struct TokenAndSpan {
+        pub span: Span,
+        pub token: Token,
+    }
+
+    pub struct Unit {
+        pub span: Span,
+        pub value: JsWord,
+        pub raw: JsWord,
+    }
+
+    pub enum Value {
+        SquareBracketBlock(SquareBracketBlock),
+
+        RoundBracketBlock(RoundBracketBlock),
+
+        Unit(UnitValue),
+
+        Number(Num),
+
+        Percent(PercentValue),
+
+        Hash(HashValue),
+
+        Text(Text),
+
+        Str(Str),
+
+        Fn(FnValue),
+
+        Bin(BinValue),
+
+        Space(SpaceValues),
+
+        Comma(CommaValues),
+
+        Brace(BraceValue),
+
+        Lazy(Tokens),
+
+        AtText(AtTextValue),
+
+        Url(UrlValue),
+    }
+
+    pub struct SpaceValues {
+        pub span: Span,
+        pub values: Vec<Value>,
+    }
+
+    pub struct CommaValues {
+        pub span: Span,
+        pub values: Vec<Value>,
+    }
+
+    pub struct BinValue {
+        pub span: Span,
+
+        pub op: BinOp,
+
+        pub left: Box<Value>,
+
+        pub right: Box<Value>,
+    }
+
+    pub struct FnValue {
+        pub span: Span,
+
+        pub name: Text,
+
+        pub args: Vec<Value>,
+    }
+
+    pub struct RoundBracketBlock {
+        pub span: Span,
+        pub children: Option<Vec<Value>>,
+    }
+
+    pub struct SquareBracketBlock {
+        pub span: Span,
+        pub children: Option<Vec<Value>>,
+    }
+
+    pub struct HashValue {
+        pub span: Span,
+        pub value: JsWord,
+        pub raw: JsWord,
+    }
+
+    pub struct UnitValue {
+        pub span: Span,
+        pub value: Num,
+        pub unit: Unit,
+    }
+
+    pub struct PercentValue {
+        pub span: Span,
+        pub value: Num,
+    }
+
+    pub struct BraceValue {
+        pub span: Span,
+        pub value: Box<Value>,
+    }
+
+    pub struct AtTextValue {
+        pub span: Span,
+        pub name: Text,
+        pub block: Option<BraceValue>,
+    }
+
+    pub struct UrlValue {
+        pub span: Span,
+        pub url: JsWord,
+        pub raw: JsWord,
+    }
+
+    pub struct SelectorList {
+        pub span: Span,
+        pub children: Vec<ComplexSelector>,
+    }
+
+    pub struct ComplexSelector {
+        pub span: Span,
+        pub children: Vec<ComplexSelectorChildren>,
+    }
+
+    pub enum ComplexSelectorChildren {
+        CompoundSelector(CompoundSelector),
+        Combinator(Combinator),
+    }
+
+    pub struct CompoundSelector {
+        pub span: Span,
+        pub nesting_selector: Option<NestingSelector>,
+        pub type_selector: Option<TypeSelector>,
+        pub subclass_selectors: Vec<SubclassSelector>,
+    }
+
+    pub struct Combinator {
+        pub span: Span,
+        pub value: CombinatorValue,
+    }
+
+    pub struct TypeSelector {
+        pub span: Span,
+        pub prefix: Option<Text>,
+        pub name: Text,
+    }
+
+    pub struct NestingSelector {
+        pub span: Span,
+    }
+
+    pub enum SubclassSelector {
+        Id(IdSelector),
+
+        Class(ClassSelector),
+
+        Attr(AttrSelector),
+
+        PseudoClass(PseudoClassSelector),
+        
+         PseudoElement(PseudoElementSelector),
+
+        At(AtSelector),
+    }
+
+    pub enum AttrSelectorValue {
+        Str(Str),
+        Text(Text),
+    }
+
+    pub struct AttrSelector {
+        pub span: Span,
+        pub prefix: Option<Text>,
+        pub name: Text,
+        pub matcher: Option<AttrSelectorMatcher>,
+        pub value: Option<AttrSelectorValue>,
+        pub modifier: Option<char>,
+    }
+
+    pub struct PseudoClassSelector {
+        pub span: Span,
+        pub name: Text,
+        pub args: Tokens,
+    }
+
+    pub struct PseudoElementSelector {
+        pub span: Span,
+        pub name: Text,
+        pub args: Tokens,
+    }
+
+    pub struct UniversalSelector {
+        pub span: Span,
+    }
+
+    pub struct IdSelector {
+        pub span: Span,
+        pub text: Text,
+    }
+
+    pub struct ClassSelector {
+        pub span: Span,
+        pub text: Text,
+    }
+
+    pub struct TagSelector {
+        pub span: Span,
+        pub text: Text,
+    }
+
+    pub struct AtSelector {
+        pub span: Span,
+        pub text: Text,
+    }
+
+    pub struct Stylesheet {
+        pub span: Span,
+        pub rules: Vec<Rule>,
+    }
+
+    pub enum Rule {
+        Style(StyleRule),
+
+        Invalid(Tokens),
+
+        AtRule(AtRule),
+    }
+
+    pub struct Invalid {
+        pub span: Span,
+    }
+
+    pub enum AtRule {
+        Charset(CharsetRule),
+        Import(ImportRule),
+        FontFace(FontFaceRule),
+        Keyframes(KeyframesRule),
+        Media(MediaRule),
+        Supports(SupportsRule),
+        Page(PageRule),
+        Namespace(NamespaceRule),
+        Viewport(ViewportRule),
+        Document(DocumentRule),
+        Unknown(UnknownAtRule),
+    }
+
+    pub struct CharsetRule {
+        pub span: Span,
+        pub charset: Str,
+    }
+
+    pub enum ImportSource {
+        Fn(FnValue),
+        Url(UrlValue),
+        Str(Str),
+    }
+
+    pub struct ImportRule {
+        pub span: Span,
+        pub src: ImportSource,
+        pub condition: Option<MediaQuery>,
+    }
+
+    pub struct FontFaceRule {
+        pub span: Span,
+        pub block: Block,
+    }
+
+    pub enum NamespaceValue {
+        Url(UrlValue),
+        Str(Str),
+    }
+
+    pub struct NamespaceRule {
+        pub span: Span,
+        pub prefix: Text,
+        pub value: NamespaceValue,
+    }
+
+    pub struct ViewportRule {
+        pub span: Span,
+        pub block: Block,
+    }
+
+    pub struct UnknownAtRule {
+        pub span: Span,
+        pub name: Text,
+        pub tokens: Tokens,
+    }
+
+    pub struct DocumentRule {
+        pub span: Span,
+        pub selectors: Vec<FnValue>,
+        pub block: Vec<Rule>,
+    }
+
+    pub struct KeyframesRule {
+        pub span: Span,
+        pub id: Text,
+        pub blocks: Vec<KeyframeBlock>,
+    }
+
+    pub struct KeyframeBlock {
+        pub span: Span,
+        pub selector: Vec<KeyframeSelector>,
+        pub rule: KeyframeBlockRule,
+    }
+
+    pub enum KeyframeSelector {
+        Id(Text),
+        Percent(PercentValue),
+    }
+
+    pub enum KeyframeBlockRule {
+        Block(Box<Block>),
+        AtRule(Box<AtRule>),
+    }
+
+    pub struct MediaRule {
+        pub span: Span,
+
+        pub query: Box<MediaQuery>,
+
+        pub rules: Vec<Rule>,
+    }
+
+    pub enum MediaQuery {
+        Text(Text),
+        And(AndMediaQuery),
+        Or(OrMediaQuery),
+        Not(NotMediaQuery),
+        Only(OnlyMediaQuery),
+        Declaration(Declaration),
+        Comma(CommaMediaQuery),
+    }
+
+    pub struct AndMediaQuery {
+        pub span: Span,
+        pub left: Box<MediaQuery>,
+        pub right: Box<MediaQuery>,
+    }
+
+    pub struct OrMediaQuery {
+        pub span: Span,
+        pub left: Box<MediaQuery>,
+        pub right: Box<MediaQuery>,
+    }
+
+    pub struct NotMediaQuery {
+        pub span: Span,
+        pub query: Box<MediaQuery>,
+    }
+
+    pub struct OnlyMediaQuery {
+        pub span: Span,
+        pub query: Box<MediaQuery>,
+    }
+
+    pub struct CommaMediaQuery {
+        pub span: Span,
+        pub queries: Vec<MediaQuery>,
+    }
+
+    pub struct PageRule {
+        pub span: Span,
+
+        pub prelude: Vec<PageSelector>,
+
+        pub block: PageRuleBlock,
+    }
+
+    pub struct PageSelector {
+        pub span: Span,
+
+        pub ident: Option<Text>,
+
+        pub pseudo: Option<Text>,
+    }
+
+    pub struct PageRuleBlock {
+        pub span: Span,
+        pub items: Vec<PageRuleBlockItem>,
+    }
+
+    pub enum PageRuleBlockItem {
+        Declaration(Box<Declaration>),
+        Nested(Box<NestedPageRule>),
+    }
+
+    pub struct NestedPageRule {
+        pub span: Span,
+        pub prelude: SelectorList,
+        pub block: PageRuleBlock,
+    }
+
+    pub struct SupportsRule {
+        pub span: Span,
+
+        pub query: SupportQuery,
+
+        pub rules: Vec<Rule>,
+    }
+
+    pub enum SupportQuery {
+        Not(NotSupportQuery),
+        And(AndSupportQuery),
+        Or(OrSupportQuery),
+        Declaration(Declaration),
+        Paren(ParenSupportQuery),
+    }
+
+    pub struct NotSupportQuery {
+        pub span: Span,
+        pub query: Box<SupportQuery>,
+    }
+
+    pub struct AndSupportQuery {
+        pub span: Span,
+        pub left: Box<SupportQuery>,
+        pub right: Box<SupportQuery>,
+    }
+
+    pub struct OrSupportQuery {
+        pub span: Span,
+        pub left: Box<SupportQuery>,
+        pub right: Box<SupportQuery>,
+    }
+
+    pub struct ParenSupportQuery {
+        pub span: Span,
+        pub query: Box<SupportQuery>,
+    }
+});
