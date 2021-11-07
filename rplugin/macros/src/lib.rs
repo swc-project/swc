48a605d7ec36cca637deee34e5becba19c143f7f
Donny <kdy1997.dev@gmail.com>
2021-11-08 00:06:30 +0900

    Copy content

--- rplugin/macros/src/lib.rs
+++ rplugin/macros/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate proc_macro;
 
-use pmutil::Quote;
+use pmutil::{q, Quote, ToTokensExt};
+use swc_macros_common::call_site;
 use syn::{parse, Item, ItemImpl, ItemMod, ItemStruct, Lit, Path};
 
 #[proc_macro_attribute]
@@ -13,25 +14,32 @@ pub fn ast_for_plugin(
     let module: ItemMod = parse(module).expect("failed to parse input as a module");
     let content = module.content.expect("module should have content").1;
 
-    let mut q = Quote::new_call_site();
+    let mut mod_items = vec![];
 
-    for item in &content {
-        match item {
+    for item in content {
+        match &item {
             Item::Struct(s) => {
-                // q.push_tokens(&make_stable_struct(&s));
                 // q.push_tokens(&make_unstable_ast_impl_for_struct(&
                 // normal_crate_path, &s));
             }
+
+            Item::Enum(s) => {}
+
             _ => {
                 todo!("Support: {:?}", item)
             }
         }
+
+        mod_items.push(item);
     }
 
-    proc_macro2::TokenStream::from(q).into()
+    ItemMod {
+        content: Some((call_site(), mod_items)),
+        ..module
+    }
+    .dump()
+    .into()
 }
-
-// fn make_stable_struct(src: &ItemStruct) -> ItemStruct {}
 
 // fn make_unstable_ast_impl_for_struct(normal_crate_path: &Path, src:
 // &ItemStruct) -> ItemImpl {}
--- rplugin/tests/macro.rs
+++ rplugin/tests/macro.rs
@@ -9,7 +9,25 @@ pub enum Expr {
     Bin(BinExpr),
 }
 
-pub enum Lit {}
+pub enum Lit {
+    Number(usize),
+}
 
 #[rplugin::ast_for_plugin("crate")]
-mod plugin {}
+mod plugin {
+
+    pub struct BinExpr {
+        pub left: Box<Expr>,
+        pub op: String,
+        pub right: Box<Expr>,
+    }
+
+    pub enum Expr {
+        Lit(Lit),
+        Bin(BinExpr),
+    }
+
+    pub enum Lit {
+        Number(usize),
+    }
+}
