487eb039a042ad706d6e4bbd2fabc695f1413248
Donny <kdy1997.dev@gmail.com>
2021-11-08 00:09:35 +0900

    Add derive(StableAbi)

--- rplugin/macros/src/lib.rs
+++ rplugin/macros/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use pmutil::{q, Quote, ToTokensExt};
 use swc_macros_common::call_site;
-use syn::{parse, Item, ItemImpl, ItemMod, ItemStruct, Lit, Path};
+use syn::{parse, Attribute, Item, ItemImpl, ItemMod, ItemStruct, Lit, Path};
 
 #[proc_macro_attribute]
 pub fn ast_for_plugin(
@@ -16,14 +16,17 @@ pub fn ast_for_plugin(
 
     let mut mod_items = vec![];
 
-    for item in content {
-        match &item {
+    for mut item in content {
+        match &mut item {
             Item::Struct(s) => {
+                add_stable_abi(&mut s.attrs);
                 // q.push_tokens(&make_unstable_ast_impl_for_struct(&
                 // normal_crate_path, &s));
             }
 
-            Item::Enum(s) => {}
+            Item::Enum(s) => {
+                add_stable_abi(&mut s.attrs);
+            }
 
             _ => {
                 todo!("Support: {:?}", item)
@@ -43,3 +46,14 @@ pub fn ast_for_plugin(
 
 // fn make_unstable_ast_impl_for_struct(normal_crate_path: &Path, src:
 // &ItemStruct) -> ItemImpl {}
+
+fn add_stable_abi(attrs: &mut Vec<Attribute>) {
+    let s = q!({
+        #[repr(C)]
+        #[derive(abi_stable::StableAbi)]
+        struct Dummy;
+    })
+    .parse::<ItemStruct>();
+
+    attrs.extend(s.attrs)
+}
