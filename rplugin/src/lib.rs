88007f02668ac82008aa5f7e87ff04d0e13917c9
Donny <kdy1997.dev@gmail.com>
2021-11-07 21:44:53 +0900

    Doc

--- rplugin/src/lib.rs
+++ rplugin/src/lib.rs
@@ -1,3 +1,25 @@
 //! AST processing plugin system.
 //!
 //! Designed as a general crate, to support using it from other libraries too.
+//! This depends on the [abi_stable] crate.
+//!
+//! # Why new crate?
+//!
+//! I (kdy1) wants to pass the AST data to plugin without having to change the
+//! type of AST nodes.
+//!
+//! e.g. Using [RBox](abi_stable::std_types::RBox) instead of
+//! [Box](std::boxed::Box) is not an acceptable option, considering there are
+//! lots of users.
+//!
+//! Instead, we just use proc-macro to convert ast into something that can be
+//! passed directly to the plugin, using [StableAbi][abi_stable::StableAbi]. In
+//! plugin, we convert it back to normal AST before processing, so the plugin
+//! authors can use exactly same code as core transforms. Of course, this is
+//! slow, but it's only slow if it's compared to the changing type. It's much
+//! faster than serializing/deserializing.
+//!
+//! In short, we do
+//!
+//! `Normal AST -> Plugin AST -> Normal AST -> plugin -> Normal AST -> Plugin
+//! AST -> Normal AST`
