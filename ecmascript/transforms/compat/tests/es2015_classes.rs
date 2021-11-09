0d59c3e996740732d4c0c9c1d6e4b88e8fe42fb7
Donny <kdy1997.dev@gmail.com>
2021-11-09 22:44:43 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/es2015_classes.rs
+++ ecmascript/transforms/compat/tests/es2015_classes.rs
@@ -1227,11 +1227,11 @@ let Obj =
 function (Base) {
   'use strict';
   _inherits(Obj, Base);
-
+  var _super = _createSuper(Obj);
   function Obj() {
     _classCallCheck(this, Obj);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Obj).apply(this, arguments));
+    return _super.apply(this, arguments);
   }
 
   _createClass(Obj, [{
