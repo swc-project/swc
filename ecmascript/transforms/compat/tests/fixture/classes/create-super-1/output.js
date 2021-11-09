a10eac5bad0d171ad9b508ab0f975796a6128150
Donny <kdy1997.dev@gmail.com>
2021-11-09 20:26:53 +0900

    Update test refs using babel

---
+++ ecmascript/transforms/compat/tests/fixture/classes/create-super-1/output.js
@@ -0,0 +1,13 @@
+var Foo = /*#__PURE__*/function (_Array) {
+    _inherits(Foo, _Array);
+
+    var _super = _createSuper(Foo);
+
+    function Foo() {
+        _classCallCheck(this, Foo);
+
+        return _super.apply(this, arguments);
+    }
+
+    return Foo;
+}( /*#__PURE__*/_wrapNativeSuper(Array));
