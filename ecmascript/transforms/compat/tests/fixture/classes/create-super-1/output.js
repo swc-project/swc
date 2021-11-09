28ea0925b0514ffa1ac1621356e22354575d9e11
Donny <kdy1997.dev@gmail.com>
2021-11-09 21:31:41 +0900

    Update test refs

--- ecmascript/transforms/compat/tests/fixture/classes/create-super-1/output.js
+++ ecmascript/transforms/compat/tests/fixture/classes/create-super-1/output.js
@@ -1,13 +1,10 @@
-var Foo = /*#__PURE__*/function (_UnknownNativeClass) {
-    _inherits(Foo, _UnknownNativeClass);
-
+let Foo = function(UnknownNativeClass) {
+    "use strict";
+    _inherits(Foo, UnknownNativeClass);
     var _super = _createSuper(Foo);
-
     function Foo() {
         _classCallCheck(this, Foo);
-
         return _super.apply(this, arguments);
     }
-
     return Foo;
 }(UnknownNativeClass);
