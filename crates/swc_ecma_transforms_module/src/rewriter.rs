e81355d381f4f89270463867d6960732263271db
Donny <kdy1997.dev@gmail.com>
2021-11-11 16:30:31 +0900

    Support dynamic imports

--- crates/swc_ecma_transforms_module/src/rewriter.rs
+++ crates/swc_ecma_transforms_module/src/rewriter.rs
@@ -1,8 +1,9 @@
 use crate::path::ImportResolver;
 use anyhow::Context;
+use swc_atoms::js_word;
 use swc_common::FileName;
 use swc_ecma_ast::*;
-use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 /// Import rewriter, which rewrites imports as es modules.
 pub fn import_rewriter<R>(base: FileName, resolver: R) -> impl Fold + VisitMut
@@ -25,6 +26,36 @@ where
     R: ImportResolver,
 {
     noop_visit_mut_type!();
+
+    fn visit_mut_call_expr(&mut self, e: &mut CallExpr) {
+        e.visit_mut_children_with(self);
+
+        match &e.callee {
+            ExprOrSuper::Expr(callee) => match &**callee {
+                Expr::Ident(Ident {
+                    sym: js_word!("import"),
+                    ..
+                }) => {
+                    if let Some(ExprOrSpread { spread: None, expr }) = &mut e.args.get_mut(0) {
+                        match &mut **expr {
+                            Expr::Lit(Lit::Str(s)) => {
+                                let src = self
+                                    .resolver
+                                    .resolve_import(&self.base, &s.value)
+                                    .with_context(|| {
+                                        format!("failed to resolve import `{}`", s.value)
+                                    })
+                                    .unwrap();
+
+                                i.src.value = src;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            },
+        }
+    }
 
     fn visit_mut_import_decl(&mut self, i: &mut ImportDecl) {
         let src = self
