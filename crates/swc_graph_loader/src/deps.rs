e9eaabc20b23e3b79f9bf0a1f9ab09a652b889a4
Donny <kdy1997.dev@gmail.com>
2021-11-12 21:40:49 +0900

    I was doing something wrong

--- crates/swc_graph_loader/src/deps.rs
+++
@@ -1,8 +0,0 @@
-use swc_atoms::JsWord;
-
-pub trait DepAnalyzer<M> {
-    /// Analyze dependencies of the given module.
-    ///
-    /// It's allowed to return same value multiple time.
-    fn analyze_deps(&self, module: &M) -> Vec<JsWord>;
-}
