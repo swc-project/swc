b754084e98fc22dc2be7b2f10308f3599afab6d1
Donny <kdy1997.dev@gmail.com>
2021-11-12 22:02:32 +0900

    Copy some code

--- crates/swc_graph_analyzer/src/lib.rs
+++ crates/swc_graph_analyzer/src/lib.rs
@@ -1,15 +1,20 @@
-use std::{hash::Hash, marker::PhantomData};
+use ahash::AHashSet;
+use std::{fmt::Debug, hash::Hash, marker::PhantomData};
 
 pub trait DepGraph {
-    type ModuleId: Copy + Eq + Hash + Ord;
+    type ModuleId: Debug + Copy + Eq + Hash + Ord;
+
+    fn deps_of(&self, module_id: Self::ModuleId) -> Vec<Self::ModuleId>;
 }
 
 pub struct GraphAnalyzer<G>
 where
     G: DepGraph,
 {
+    /// `(src, dst)`
+    tracked: AHashSet<(G::ModuleId, G::ModuleId)>,
     dep_graph: G,
-    result: GraphResult<G>,
+    data: GraphResult<G>,
 }
 
 impl<G> GraphAnalyzer<G>
@@ -19,18 +24,62 @@ where
     pub fn new(dep_graph: G) -> Self {
         Self {
             dep_graph,
-            result: GraphResult {
+            data: GraphResult {
                 all: Default::default(),
                 cycles: Default::default(),
                 _marker: Default::default(),
             },
+            tracked: Default::default(),
         }
     }
 
-    pub fn load(&mut self, entry: G::ModuleId) {}
+    pub fn load(&mut self, entry: G::ModuleId) {
+        self.add_to_graph(entry, &mut vec![entry])
+    }
+
+    fn add_to_graph(&mut self, module_id: G::ModuleId, path: &mut Vec<G::ModuleId>) {
+        let visited = self.data.all.contains(&module_id);
+        // dbg!(visited);
+        // dbg!(&path);
+        let cycle_rpos = if visited {
+            path.iter().rposition(|v| *v == module_id)
+        } else {
+            None
+        };
+
+        if let Some(rpos) = cycle_rpos {
+            let cycle = path[rpos..].to_vec();
+            tracing::debug!("Found cycle: {:?}", cycle);
+            self.data.cycles.push(cycle);
+        }
+
+        let prev_last = *path.last().unwrap();
+        // Prevent infinite recursion.
+        if !self.tracked.insert((prev_last, module_id)) {
+            return;
+        }
+
+        path.push(module_id);
+
+        if !visited {
+            self.data.all.push(module_id);
+        }
+        self.data.graph.add_node(module_id);
+
+        for dep_module_id in self.dep_graph.deps_of(module_id) {
+            tracing::debug!("Dep: {:?} -> {:?}", module_id, dep_module_id);
+
+            self.data.graph.add_edge(module_id, dep_module_id, ());
+
+            self.add_to_graph(dep_module_id, path);
+        }
+
+        let res = path.pop();
+        debug_assert_eq!(res, Some(module_id));
+    }
 
     pub fn into_result(self) -> GraphResult<G> {
-        self.result
+        self.data
     }
 }
 
