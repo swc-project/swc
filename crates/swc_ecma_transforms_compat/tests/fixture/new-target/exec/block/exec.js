645de575ec5e6d137cf6e4cac1fa914de20b1ae3
Bojan Durdevic <bojan88@gmail.com>
2021-11-12 16:31:14 -0500

    Fix for new target hoisting

---
+++ crates/swc_ecma_transforms_compat/tests/fixture/new-target/exec/block/exec.js
@@ -0,0 +1,22 @@
+const targets = [];
+
+{
+  function foo() {
+    targets.push(new.target);
+  }
+  foo();
+}
+
+expect(targets[0]).toBeUndefined();
+
+{
+  class Foo {
+    constructor() {
+      targets.push(new.target);
+    }
+  }
+
+  new Foo();
+}
+
+expect(targets[1]).not.toBeUndefined();
