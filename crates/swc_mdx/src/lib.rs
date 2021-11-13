045ef9a6d40053f6fe413b654c663dd398493c11
Donny <kdy1997.dev@gmail.com>
2021-11-13 20:14:32 +0900

    Processing

--- crates/swc_mdx/src/lib.rs
+++ crates/swc_mdx/src/lib.rs
@@ -1,7 +1,8 @@
 use crate::{error::Error, parser::Parser};
-use swc_common::{input::Input, SourceFile};
-use swc_ecma_ast::Module;
+use swc_common::{input::Input, SourceFile, DUMMY_SP};
+use swc_ecma_ast::*;
 use swc_ecma_parser::StringInput;
+use swc_ecma_utils::private_ident;
 
 pub mod ast;
 pub mod error;
@@ -17,6 +18,50 @@ where
 
     let mut parser = Parser::new(input);
     let res = parser.parse()?;
+
+    let fn_name = private_ident!("MDXContent");
+
+    let props = private_ident!("props");
+    let props_param = Param {
+        span: DUMMY_SP,
+        decorators: Default::default(),
+        pat: Pat::Assign(AssignPat {
+            span: DUMMY_SP,
+            left: Box::new(Pat::Ident(props.clone().into())),
+            right: Box::new(Expr::Object(ObjectLit {
+                span: DUMMY_SP,
+                props: Default::default(),
+            })),
+            type_ann: Default::default(),
+        }),
+    };
+
+    let mut fn_body = vec![];
+
+    items.push(ModuleItem::Stmt(Stmt::Decl(Decl::Fn(FnDecl {
+        ident: fn_name.clone(),
+        declare: false,
+        function: Function {
+            params: vec![props_param],
+            decorators: Default::default(),
+            span: res.span,
+            body: Some(BlockStmt {
+                span: DUMMY_SP,
+                stmts: fn_body,
+            }),
+            is_generator: false,
+            is_async: false,
+            type_params: Default::default(),
+            return_type: Default::default(),
+        },
+    }))));
+
+    items.push(ModuleItem::ModuleDecl(ModuleDecl::ExportDefaultExpr(
+        ExportDefaultExpr {
+            span: DUMMY_SP,
+            expr: Box::new(Expr::Ident(fn_name)),
+        },
+    )));
 
     Ok(Module {
         span: Default::default(),
