cd40b91fcf3e919557949697c67477538a1e8246
Donny <kdy1997.dev@gmail.com>
2021-11-13 23:30:32 +0900

    Update test refs

--- crates/swc_mdx/tests/fixture/mdx/10/output.js
+++ crates/swc_mdx/tests/fixture/mdx/10/output.js
@@ -1,16 +1,14 @@
-/*@jsxRuntime automatic @jsxImportSource react*/
-function MDXContent(props = {}) {
-    const {
-        wrapper: MDXLayout
-    } = props.components || ({});
+function MDXContent(props = {
+}) {
+    const { wrapper: MDXLayout  } = props.components || {
+    };
     return MDXLayout ? <MDXLayout {...props}><_createMdxContent /></MDXLayout> : _createMdxContent();
-
     function _createMdxContent() {
         const _components = Object.assign({
-            p: "p",
-            em: "em"
+            em: "em",
+            p: "p"
         }, props.components);
-        return <_components.p><_components.em>{"a"}</_components.em></_components.p>;
+        return <_components.p ><_components.em >{"a"}</_components.em></_components.p>;
     }
 }
 export default MDXContent;
