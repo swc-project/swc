df676535d02f83013ee39379bf1d72957dc7f91c
Donny <kdy1997.dev@gmail.com>
2021-11-14 14:48:32 +0900

    Fix script

--- crates/swc_mdx/scripts/compile.mjs
+++ crates/swc_mdx/scripts/compile.mjs
@@ -1,4 +1,14 @@
 import { compile } from '@mdx-js/mdx'
+import * as path from 'path';
+import { promises as fs } from 'fs';
 
-const output = await compile({ path: process.argv[2] }, { jsx: true })
-console.log(output)
+const file = process.argv[2];
+const inputContent = fs.readFile(file, 'utf8');
+const testDir = path.dirname(file);
+const outputPath = path.join(testDir, 'output.js');
+
+console.log(`Processing ${file}`)
+const output = await compile({ path: file, values: inputContent }, { jsx: true })
+
+
+await fs.writeFile(outputPath, output.value)
