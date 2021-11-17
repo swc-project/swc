9b8ef742a27b193661b010181dc713c417814ae5
Donny <kdy1997.dev@gmail.com>
2021-11-17 16:15:05 +0900

    Script is done

--- crates/swc_ecma_minifier/scripts/next/evaluate.js
+++ crates/swc_ecma_minifier/scripts/next/evaluate.js
@@ -1,15 +1,21 @@
 const readline = require('readline');
+const fs = require('fs').promises;
+const path = require('path');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
 });
 
-rl.on('line', function (data) {
+rl.on('line', async (data) => {
     try {
-        const obj = eval(`(${data})`)
+        const { name, source } = eval(`(${data})`)
+        const targetPath = path.join(__dirname, '..', '..', 'tests', 'compress', 'fixture', 'issues', 'next', name);
 
-        console.log(obj)
+        await fs.mkdir(path.dirname(targetPath), { recursive: true });
+
+        await fs.writeFile(targetPath, source, 'utf8');
     } catch (e) {
         console.log(`Code: (${data})`)
         console.error(e);
