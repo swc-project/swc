e1b76654f59cc02133d394e5393664ba74cb8d34
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-11-17 16:18:34 +0900

    chore(es/minifier): Add a script to copy tests from a next.js app (#2778)

---
+++ crates/swc_ecma_minifier/scripts/next/evaluate.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const fs = require('fs').promises;
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', async (data) => {
+    try {
+        const { name, source } = eval(`(${data})`)
+        const targetPath = path.join(__dirname, '..', '..', 'tests', 'compress', 'fixture', 'issues', 'next', name);
+
+        await fs.mkdir(path.dirname(targetPath), { recursive: true });
+
+        await fs.writeFile(targetPath, source, 'utf8');
+    } catch (e) {
+        console.log(`Code: (${data})`)
+        console.error(e);
+    }
+})
+
