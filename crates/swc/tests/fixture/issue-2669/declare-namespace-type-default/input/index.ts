5b1d7739843fd0f2c82735db58d7067a0d109933
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-11-20 11:08:45 +0900

    test(issue-2669): fix tests

--- crates/swc/tests/fixture/issue-2669/declare-namespace-type-default/input/index.ts
+++ crates/swc/tests/fixture/issue-2669/declare-namespace-type-default/input/index.ts
@@ -1,4 +1,4 @@
 declare namespace TEST {
-  export const VALUE = "value";
+  export type TYPE = "ty" | "pe";
 }
 export default TEST;
--- crates/swc/tests/fixture/issue-2669/namespace-var-default/input/index.ts
+++ crates/swc/tests/fixture/issue-2669/namespace-var-default/input/index.ts
@@ -1,5 +1,4 @@
 namespace TEST {
   export const VALUE = "value";
-  export type TYPE = "ty" | "pe";
 }
 export default TEST;
