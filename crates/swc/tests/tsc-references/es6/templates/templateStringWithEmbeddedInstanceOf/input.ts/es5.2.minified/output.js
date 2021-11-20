1d4e13ebc4292e9ab23db3d985ac15ae12db1b51
Donny <kdy1997.dev@gmail.com>
2021-11-20 16:46:48 +0900

    Update test refs

--- crates/swc/tests/tsc-references/es6/templates/templateStringWithEmbeddedInstanceOf/input.ts/es5.2.minified/output.js
+++ crates/swc/tests/tsc-references/es6/templates/templateStringWithEmbeddedInstanceOf/input.ts/es5.2.minified/output.js
@@ -1,2 +1,2 @@
-var right;
-"abc".concat(null != (right = String) && "undefined" != typeof Symbol && !!right[Symbol.hasInstance] && right[Symbol.hasInstance]("hello"), "def");
+var left, right;
+"abc".concat((left = "hello", null != (right = String) && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? right[Symbol.hasInstance](left) : left instanceof right), "def");
