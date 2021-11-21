4f2290be4dc899353a84e12e2854303400c851dd
Donny <kdy1997.dev@gmail.com>
2021-11-21 16:08:14 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/type-alias/hoisting/1/output.ts
@@ -0,0 +1,11 @@
+module Foo {
+    export type ServerHandle__2 = HandleFunction__2 | http.Server;
+    export class IncomingMessage__2 extends http.IncomingMessage {
+        originalUrl__0?: http.IncomingMessage["url"] | undefined;
+    }
+    type NextFunction__2 = (err?: any) => void;
+    export type SimpleHandleFunction__2 = (req: IncomingMessage__2, res: http.ServerResponse) => void;
+    export type NextHandleFunction__2 = (req: IncomingMessage__2, res: http.ServerResponse, next: NextFunction__2) => void;
+    export type ErrorHandleFunction__2 = (err: any, req: IncomingMessage__2, res: http.ServerResponse, next: NextFunction__2) => void;
+    export type HandleFunction__2 = SimpleHandleFunction__2 | NextHandleFunction__2 | ErrorHandleFunction__2;
+}
