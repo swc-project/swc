2574d396040e99abd095714bc946cfc8582642a2
Donny <kdy1997.dev@gmail.com>
2021-11-21 16:07:57 +0900

    Make items not top-level

--- crates/swc_ecma_transforms_base/tests/ts-resolver/type-alias/hoisting/1/input.ts
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/type-alias/hoisting/1/input.ts
@@ -1,12 +1,14 @@
-export type ServerHandle = HandleFunction | http.Server;
+namespace Foo {
+    export type ServerHandle = HandleFunction | http.Server;
 
-export class IncomingMessage extends http.IncomingMessage {
-    originalUrl?: http.IncomingMessage["url"] | undefined;
+    export class IncomingMessage extends http.IncomingMessage {
+        originalUrl?: http.IncomingMessage["url"] | undefined;
+    }
+
+    type NextFunction = (err?: any) => void;
+
+    export type SimpleHandleFunction = (req: IncomingMessage, res: http.ServerResponse) => void;
+    export type NextHandleFunction = (req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
+    export type ErrorHandleFunction = (err: any, req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
+    export type HandleFunction = SimpleHandleFunction | NextHandleFunction | ErrorHandleFunction;
 }
-
-type NextFunction = (err?: any) => void;
-
-export type SimpleHandleFunction = (req: IncomingMessage, res: http.ServerResponse) => void;
-export type NextHandleFunction = (req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
-export type ErrorHandleFunction = (err: any, req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
-export type HandleFunction = SimpleHandleFunction | NextHandleFunction | ErrorHandleFunction;
