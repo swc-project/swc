6b13073876b73bcfb8af3dc90f68c134d1a3071e
Shinyaigeek <Philispaxil@gmail.com>
2021-11-21 17:20:50 +0900

    Refactor: use undefined util module

--- crates/swc_ecma_transforms_react/tests/integration/fixture/jsx-dev-transform/output.mjs
+++ crates/swc_ecma_transforms_react/tests/integration/fixture/jsx-dev-transform/output.mjs
@@ -1,24 +1,47 @@
-import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
-const App = _jsxDEV("div", {
-    children: [
-        _jsxDEV("div", {
-        }, undefined, false, {
-            fileName: "input.js",
-            lineNumber: 3,
-            columnNumber: 9
-        }, this),
-        _jsxDEV(_Fragment, {
-            children: _jsxDEV("div", {
-                children: "hoge"
-            }, undefined, false, {
-                fileName: "input.js",
-                lineNumber: 5,
-                columnNumber: 13
-            }, this)
-        })
-    ]
-}, undefined, true, {
-    fileName: "input.js",
-    lineNumber: 2,
-    columnNumber: 5
-}, this);
+import {
+    jsxDEV as _jsxDEV,
+    Fragment as _Fragment,
+} from "react/jsx-dev-runtime";
+const App = _jsxDEV(
+    "div",
+    {
+        children: [
+            _jsxDEV(
+                "div",
+                {},
+                void 0,
+                false,
+                {
+                    fileName: "input.js",
+                    lineNumber: 3,
+                    columnNumber: 9,
+                },
+                this
+            ),
+            _jsxDEV(_Fragment, {
+                children: _jsxDEV(
+                    "div",
+                    {
+                        children: "hoge",
+                    },
+                    void 0,
+                    false,
+                    {
+                        fileName: "input.js",
+                        lineNumber: 5,
+                        columnNumber: 13,
+                    },
+                    this
+                ),
+            }),
+        ],
+    },
+    void 0,
+    true,
+    {
+        fileName: "input.js",
+        lineNumber: 2,
+        columnNumber: 5,
+    },
+    this
+);
