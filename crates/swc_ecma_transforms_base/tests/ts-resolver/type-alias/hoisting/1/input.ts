2c65ea44366e4f622d636e153cba22b0c8dd8b4c
Donny <kdy1997.dev@gmail.com>
2021-11-21 16:05:57 +0900

    Add a test

---
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/type-alias/hoisting/1/input.ts
@@ -0,0 +1,12 @@
+export type ServerHandle = HandleFunction | http.Server;
+
+export class IncomingMessage extends http.IncomingMessage {
+    originalUrl?: http.IncomingMessage["url"] | undefined;
+}
+
+type NextFunction = (err?: any) => void;
+
+export type SimpleHandleFunction = (req: IncomingMessage, res: http.ServerResponse) => void;
+export type NextHandleFunction = (req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
+export type ErrorHandleFunction = (err: any, req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void;
+export type HandleFunction = SimpleHandleFunction | NextHandleFunction | ErrorHandleFunction;
