0eb621fb512292f850362f21e20fcde8f10541f4
Donny <kdy1997.dev@gmail.com>
2021-11-24 05:56:30 +0900

    Config

--- crates/swc_estree_macros/src/ast.rs
+++ crates/swc_estree_macros/src/ast.rs
@@ -1,3 +1,11 @@
 use syn::Item;
 
-pub(crate) fn process_module_item(item: Item) -> Vec<Item> {}
+pub struct Config {
+    pub flavors: Vec<Flavor>,
+}
+
+pub struct Flavor {
+    pub name: String,
+}
+
+pub(crate) fn process_module_item(config: &Config, item: Item) -> Vec<Item> {}
