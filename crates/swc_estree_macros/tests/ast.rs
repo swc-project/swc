8f4d98d5b1d8c9ccf1beacbab1caefea3d78b961
Donny <kdy1997.dev@gmail.com>
2021-11-24 07:44:35 +0900

    Harden test

--- crates/swc_estree_macros/tests/ast.rs
+++ crates/swc_estree_macros/tests/ast.rs
@@ -3,5 +3,14 @@ use swc_estree_macros::estree_ast;
 #[estree_ast(flavors(babel, acorn))]
 pub mod ast {
 
-    pub enum Expr {}
+    pub enum Expr {
+        #[flavor(babel)]
+        Lit(Lit),
+    }
+
+    #[flavor(babel)]
+    pub enum Lit {}
+
+    #[flavor(acorn)]
+    pub struct Lit {}
 }
