4c4fb3cb6df5f3fcf2d80f25a619b79e56807843
Donny <kdy1997.dev@gmail.com>
2021-11-24 05:49:21 +0900

    Some work for macro

--- crates/swc_estree_macros/src/lib.rs
+++ crates/swc_estree_macros/src/lib.rs
@@ -1,1 +1,26 @@
+use pmutil::{q, ToTokensExt};
+use syn::Item;
+
 extern crate proc_macro;
+
+#[proc_macro_attribute]
+pub fn define_ast(
+    args: proc_macro::TokenStream,
+    module_tokens: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let args = syn::parse_macro_input!(args as syn::AttributeArgs);
+    let module = syn::parse_macro_input!(module_tokens as syn::ItemMod);
+
+    let (_, content) = module
+        .content
+        .expect("#[define_ast] requires a module with content");
+
+    let mut q = q!({});
+    for item in content.into_iter().flat_map(process_module_item) {
+        q.push_tokens(&item);
+    }
+
+    q.dump().into()
+}
+
+fn process_module_item(item: Item) -> Vec<Item> {}
