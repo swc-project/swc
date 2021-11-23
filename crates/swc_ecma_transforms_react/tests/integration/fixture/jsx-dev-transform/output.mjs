0e831aed9fa3f1b2ccf2593859685b432ce3475f
Shinyaigeek <Philispaxil@gmail.com>
2021-11-23 19:26:42 +0900

    Chore: add some test for jsx transforms

--- crates/swc_ecma_transforms_react/tests/integration/fixture/jsx-dev-transform/output.mjs
+++ crates/swc_ecma_transforms_react/tests/integration/fixture/jsx-dev-transform/output.mjs
@@ -24,7 +24,7 @@ const App = _jsxDEV(
                     {
                         children: "hoge",
                     },
-                    void 0,
+                    1,
                     false,
                     {
                         fileName: "input.js",
---
+++ crates/swc_ecma_transforms_react/tests/integration/fixture/jsx-transform/output.mjs
@@ -0,0 +1,12 @@
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
+const App = _jsxs("div", {
+    children: [
+        _jsx("div", {
+        }),
+        _jsx(_Fragment, {
+            children: _jsx("div", {
+                children: "hoge"
+            }, 1)
+        })
+    ]
+});
