3071ee632e677f6f09f2a1454fd5280cd95931c5
OJ Kwon <kwon.ohjoong@gmail.com>
2021-11-24 12:02:24 -0800

    test(swc): update test cases

---
+++ crates/swc/tests/fixture/issue-2856/1/input/index.js
@@ -0,0 +1,5 @@
+import { sdx } from 'boo';
+import boo from 'boo';
+
+boo.some();
+sdx();
---
+++ crates/swc/tests/fixture/issue-2856/1/output/index.js
@@ -0,0 +1,27 @@
+"use strict";
+var _boo = _interopRequireWildcard(require("boo"));
+function _interopRequireWildcard(obj) {
+    if (obj && obj.__esModule) {
+        return obj;
+    } else {
+        var newObj = {
+        };
+        if (obj != null) {
+            for(var key in obj){
+                if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                    var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {
+                    };
+                    if (desc.get || desc.set) {
+                        Object.defineProperty(newObj, key, desc);
+                    } else {
+                        newObj[key] = obj[key];
+                    }
+                }
+            }
+        }
+        newObj.default = obj;
+        return newObj;
+    }
+}
+_boo.default.some();
+(0, _boo).sdx();
