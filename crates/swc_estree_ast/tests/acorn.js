84f314d26138f7ec230e644dae4a5f830c8c27ec
Donny <kdy1997.dev@gmail.com>
2021-11-24 14:12:54 +0900

    Add acorn.js

--- crates/swc_estree_ast/tests/acorn.js
+++ crates/swc_estree_ast/tests/acorn.js
@@ -1,1 +1,9 @@
 // File to parse input as an AST using acorn
+
+
+const acorn = require("acorn");
+const res = acorn.parse(process.argv[1], {
+    ecmaVersion: 2020,
+    ranges: true
+})
+console.log(res);
