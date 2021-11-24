3c5672adf65bbd79152838876283c6c69fb5f06a
Donny <kdy1997.dev@gmail.com>
2021-11-24 09:33:59 +0900

    fixup

--- crates/swc_estree_ast/src/comment.rs
+++ crates/swc_estree_ast/src/comment.rs
@@ -1,27 +1,29 @@
+use super::Loc;
+use serde::{Deserialize, Serialize};
+use swc_common::ast_serde;
 
+#[derive(Debug, Clone, PartialEq)]
+#[ast_serde("BaseComment")]
+pub struct BaseComment {
+    pub value: String,
+    pub start: usize,
+    pub end: usize,
+    pub loc: Loc,
+}
 
-    #[derive(Debug, Clone, PartialEq)]
-    #[ast_serde("BaseComment")]
-    pub struct BaseComment {
-        pub value: String,
-        pub start: usize,
-        pub end: usize,
-        pub loc: Loc,
-    }
+#[derive(Debug, Clone, PartialEq)]
+#[ast_serde]
+pub enum Comment {
+    #[tag("CommentBlock")]
+    Block(BaseComment),
+    #[tag("CommentLine")]
+    Line(BaseComment),
+}
 
-    #[derive(Debug, Clone, PartialEq)]
-    #[ast_serde]
-    pub enum Comment {
-        #[tag("CommentBlock")]
-        Block(BaseComment),
-        #[tag("CommentLine")]
-        Line(BaseComment),
-    }
-
-    #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-    #[serde(rename_all = "lowercase")]
-    pub enum CommentTypeShorthand {
-        Leading,
-        Inner,
-        Trailing,
-    }
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum CommentTypeShorthand {
+    Leading,
+    Inner,
+    Trailing,
+}
