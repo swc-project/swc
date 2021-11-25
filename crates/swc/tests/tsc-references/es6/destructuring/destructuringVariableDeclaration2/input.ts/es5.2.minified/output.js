a6398e9f26e3c266e0413747cf45c509af5b502d
RiESAEX <89718456+RiESAEX@users.noreply.github.com>
2021-11-25 14:51:59 +0800

    fix(es/transforms/compat): Fix `destructuring` (#2866)
    swc_ecma_transforms_compat:
     - Check length correctly. (Closes #2819, Closes #2821, Closes #2843)

--- crates/swc/tests/tsc-references/es6/destructuring/destructuringVariableDeclaration2/input.ts/es5.2.minified/output.js
+++ crates/swc/tests/tsc-references/es6/destructuring/destructuringVariableDeclaration2/input.ts/es5.2.minified/output.js
@@ -3,3 +3,16 @@ var ref = {
     a2: 1
 };
 ref.a1, ref.a2;
+var ref1 = [
+    1,
+    2,
+    {
+        c3: 4,
+        c5: 0
+    }
+], c1 = ref1[0], c2 = ref1[1], ref2 = ref1[2];
+ref2.c3, ref2.c5, ref1.slice(4), void 0 === _d1;
+var _d1 = [
+    1,
+    2
+]; // Error
