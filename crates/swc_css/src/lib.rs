aa2349761e72cc5cbccaa452d32d3d330edd5a3d
Donny <kdy1997.dev@gmail.com>
2021-11-26 23:24:01 +0900

    Fix `swc_css`

--- crates/swc_css/src/lib.rs
+++ crates/swc_css/src/lib.rs
@@ -1,5 +1,6 @@
 pub extern crate swc_css_ast as ast;
 pub extern crate swc_css_codegen as codegen;
+#[cfg(feature = "swc_css_minifier")]
 #[cfg_attr(docsrs, doc(cfg(feature = "minifier")))]
 pub extern crate swc_css_minifier as minifier;
 pub extern crate swc_css_parser as parser;
