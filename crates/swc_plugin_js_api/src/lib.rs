b37195be5a8e89871643ffff6c4f30dbbd53df5f
Donny <kdy1997.dev@gmail.com>
2021-11-27 21:34:11 +0900

    Remove json-related stuffs

--- crates/swc_plugin_js_api/src/lib.rs
+++ crates/swc_plugin_js_api/src/lib.rs
@@ -2,36 +2,9 @@ use abi_stable::{
     library::RootModule,
     package_version_strings,
     sabi_types::VersionStrings,
-    std_types::{RResult, RStr, RString, RVec},
+    std_types::{RResult, RStr, RString},
     StableAbi,
 };
-use anyhow::{Context, Error};
-use serde::{de::DeserializeOwned, Serialize};
-use std::any::type_name;
-
-pub fn serialize_ast<T>(t: &T) -> Result<Vec<u8>, Error>
-where
-    T: Serialize,
-{
-    serde_json::to_vec(t).with_context(|| {
-        format!(
-            "failed to serialize `{}` using serde_json",
-            type_name::<T>()
-        )
-    })
-}
-
-pub fn deserialize_ast<T>(bytes: &[u8]) -> Result<T, Error>
-where
-    T: DeserializeOwned,
-{
-    serde_json::from_slice(bytes).with_context(|| {
-        format!(
-            "failed to deserialize `{}` using serde_json",
-            type_name::<T>()
-        )
-    })
-}
 
 /// Don't use this directly.
 #[repr(C)]
@@ -44,8 +17,8 @@ pub struct SwcJsPlugin {
         extern "C" fn(
             rt: swc_common::plugin::Runtime,
             config_json: RStr,
-            ast: RVec<u8>,
-        ) -> RResult<RVec<u8>, RString>,
+            ast: swc_ecma_plugin_ast::Program,
+        ) -> RResult<swc_ecma_plugin_ast::Program, RString>,
     >,
 }
 
