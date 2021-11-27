c6cb790cd927b5a7fdd623954ea5c1f02a53261d
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-11-27 14:33:33 +0900

    feat(css/minifier): Initialize crate (#2884)

--- crates/swc_css/src/lib.rs
+++ crates/swc_css/src/lib.rs
@@ -1,5 +1,8 @@
 pub extern crate swc_css_ast as ast;
 pub extern crate swc_css_codegen as codegen;
+#[cfg(feature = "swc_css_minifier")]
+#[cfg_attr(docsrs, doc(cfg(feature = "minifier")))]
+pub extern crate swc_css_minifier as minifier;
 pub extern crate swc_css_parser as parser;
 pub extern crate swc_css_utils as utils;
 pub extern crate swc_css_visit as visit;
