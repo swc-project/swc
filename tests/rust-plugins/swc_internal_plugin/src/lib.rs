158be2ba38979c68bd936783801f648a50da1982
Donny <kdy1997.dev@gmail.com>
2021-11-28 15:59:52 +0900

    Integration tests is working

--- tests/rust-plugins/swc_internal_plugin/src/lib.rs
+++ tests/rust-plugins/swc_internal_plugin/src/lib.rs
@@ -1,11 +1,15 @@
+use serde::Deserialize;
 use swc_ecmascript::visit::Fold;
 use swc_plugin::define_js_plugin;
 
 define_js_plugin!(internal_test);
 
-fn internal_test(_: ()) -> impl Fold {
+fn internal_test(_: Config) -> impl Fold {
     InternalTest
 }
+
+#[derive(Debug, Deserialize)]
+struct Config {}
 
 struct InternalTest;
 
