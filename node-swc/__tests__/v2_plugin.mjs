691414edb26e4c15af60ff977bec692f7de54c4e
Donny <kdy1997.dev@gmail.com>
2021-11-28 16:00:50 +0900

    Test

--- node-swc/__tests__/v2_plugin.mjs
+++ node-swc/__tests__/v2_plugin.mjs
@@ -1,18 +1,18 @@
 import swc from '../..';
 
-it("should resolve", async () => {
-    const { code } = await swc.transform(`console.log("Should be removed")`, {
-        jsc: {
-            experimental: {
-                plugins: [
-                    ['internal-test', {}]
-                ]
-            }
-        }
-    });
+// it("should resolve", async () => {
+//     const { code } = await swc.transform(`console.log("Should be removed")`, {
+//         jsc: {
+//             experimental: {
+//                 plugins: [
+//                     ['internal-test', {}]
+//                 ]
+//             }
+//         }
+//     });
 
-    expect(code).toMatch(``)
-})
+//     expect(code).toMatch(``)
+// })
 
 it("should error if it's not resolved", async () => {
     await expect(swc.transform(`console.log("Should be removed")`, {
