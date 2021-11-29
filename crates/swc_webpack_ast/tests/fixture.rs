dfda10fabdd374af5401a154ad18c091dee61569
Donny <kdy1997.dev@gmail.com>
2021-11-29 23:23:16 +0900

    Test using bundler

--- crates/swc_webpack_ast/tests/fixture.rs
+++ crates/swc_webpack_ast/tests/fixture.rs
@@ -1,9 +1,10 @@
 use std::{fs, path::PathBuf};
-use swc_common::{chain, Mark};
+use swc_common::{chain, Mark, DUMMY_SP};
+use swc_ecma_ast::*;
 use swc_ecma_parser::{EsConfig, Parser, StringInput, Syntax};
 use swc_ecma_transforms_base::resolver::resolver_with_mark;
 use swc_ecma_transforms_testing::test_fixture;
-use swc_ecma_visit::as_folder;
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, VisitMut, VisitMutWith};
 use swc_webpack_ast::reducer::ast_reducer;
 
 #[testing::fixture("tests/fixture/**/input.js")]
@@ -21,6 +22,54 @@ fn fixture(input: PathBuf) {
             chain!(
                 resolver_with_mark(top_level_mark),
                 as_folder(ast_reducer(top_level_mark))
+            )
+        },
+        &input,
+        &output,
+    );
+}
+
+struct DropModuleItem;
+
+impl VisitMut for DropModuleItem {
+    noop_visit_mut_type!();
+
+    fn visit_mut_module_item(&mut self, item: &mut ModuleItem) {
+        if let ModuleItem::ModuleDecl(..) = item {
+            *item = ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }));
+            return;
+        }
+
+        item.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_module_items(&mut self, items: &mut Vec<ModuleItem>) {
+        items.visit_mut_children_with(self);
+
+        items.retain(|s| match s {
+            ModuleItem::Stmt(Stmt::Empty(..)) => false,
+            _ => true,
+        });
+    }
+}
+
+#[testing::fixture("../swc_bundler/tests/fixture/**/output/entry.js")]
+#[testing::fixture("../swc_bundler/tests/fixture/**/output/entry.ts")]
+fn fixture_bundler(input: PathBuf) {
+    let output = input.parent().unwrap().join("output___truncated.js");
+
+    test_fixture(
+        Syntax::Es(EsConfig {
+            jsx: true,
+            ..Default::default()
+        }),
+        &|_| {
+            let top_level_mark = Mark::fresh(Mark::root());
+
+            chain!(
+                resolver_with_mark(top_level_mark),
+                as_folder(ast_reducer(top_level_mark)),
+                as_folder(DropModuleItem)
             )
         },
         &input,
