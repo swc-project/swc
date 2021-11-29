dc91ad0a02ec010f45ef3bd1a10c5feb00b6aab4
Donny <kdy1997.dev@gmail.com>
2021-11-29 19:59:36 +0900

    Adjust parser option

--- crates/swc_estree_compat/scripts/test-acorn.js
+++ crates/swc_estree_compat/scripts/test-acorn.js
@@ -1,10 +1,18 @@
 // File to parse input as an AST using acorn
 
 
-const acorn = require("acorn");
-const res = acorn.parse(process.argv[1], {
-    ecmaVersion: 2020,
+
+const parserOptions = {
     ranges: true,
-    allowHashBang: true
-})
+    locations: true,
+    ecmaVersion: "latest",
+    sourceType: "module",
+    // https://github.com/tc39/proposal-hashbang
+    allowHashBang: true,
+    onInsertedSemicolon: pos => semicolons.add(pos),
+    sourceType: "module"
+};
+
+const acorn = require("acorn");
+const res = acorn.parse(process.argv[1], parserOptions)
 console.log(JSON.stringify(res, void 0, 2));
