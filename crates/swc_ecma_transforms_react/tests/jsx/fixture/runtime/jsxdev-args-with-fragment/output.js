b1716dfa8b4da7e9c1ba9e85d397ee38db82308d
Shinyaigeek <Philispaxil@gmail.com>
2021-12-01 01:09:33 +0900

    Chore: add test case for Fragment jsxDev argument

---
+++ crates/swc_ecma_transforms_react/tests/jsx/fixture/runtime/jsxdev-args-with-fragment/output.js
@@ -0,0 +1,13 @@
+import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
+var x = _jsxDEV(_Fragment, {
+    children: [
+        _jsxDEV("div", {
+            children: "hoge"
+        }, void 0, false, void 0, void 0),
+        _jsxDEV("div", {
+            children: "fuga"
+        }, void 0, false, void 0, void 0)
+    ]
+},
+void 0,
+true);
