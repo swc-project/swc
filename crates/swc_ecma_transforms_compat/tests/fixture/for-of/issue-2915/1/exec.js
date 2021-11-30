0de0816a1971e19acffc617d156c75363720fc5d
Donny <kdy1997.dev@gmail.com>
2021-11-30 12:53:46 +0900

    #2915 is not enough to debug

--- crates/swc_ecma_transforms_compat/tests/fixture/for-of/issue-2915/1/exec.js
+++ crates/swc_ecma_transforms_compat/tests/fixture/for-of/issue-2915/1/exec.js
@@ -1,21 +1,17 @@
+
 function foo(baz) {
-    console.log(baz);
     for (const g in baz) {
+        console.log(g);
+
         for (let j = 0; j < g.length; j++) {
-            const b = a[j];
-            const { k, f } = b;
-            const l = f.findIndex((x) => x === k);
-            const [x, y] = foo(a, b, c);
-            for (const a of foos) {
-                console.log(a);
-            }
+            console.log(j);
         }
     }
 }
 
 
 console.log(foo({
-    a: 1,
-    b: 2,
-    c: 3
+    a: [1],
+    b: [2, 2],
+    c: [3, 3, 3]
 }))
