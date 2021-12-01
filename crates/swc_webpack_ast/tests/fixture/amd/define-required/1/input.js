8fa0a89561f0b2c0ed802f04a835b90d841f81f8
Donny <kdy1997.dev@gmail.com>
2021-12-01 20:29:55 +0900

    Harden test

--- crates/swc_webpack_ast/tests/fixture/amd/define-required/1/input.js
+++ crates/swc_webpack_ast/tests/fixture/amd/define-required/1/input.js
@@ -2,5 +2,11 @@
 
 
 export function foo() {
-    define(["require"], function (r) { r("lodash") })
+    define(["require"], function (r) {
+        console.log('foo');
+        r("lodash")
+
+        'should be removed';
+        console.log('should be removed');
+    })
 }
