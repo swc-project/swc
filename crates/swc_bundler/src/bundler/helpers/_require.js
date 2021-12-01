24de5507ae5c5774a6a329f79519b37461ce5145
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-12-01 19:53:06 +0900

    fix(bundler): Use interop for common js dependencies (#2930)
    swc_bundler:
     - Update helpers to use interop for common js deps. (Closes #2789)

--- crates/swc_bundler/src/bundler/helpers/_require.js
+++ crates/swc_bundler/src/bundler/helpers/_require.js
@@ -1,4 +1,31 @@
 function __swcpack_require__(mod) {
+    // Ported from _interopRequireWildcard
+    function interop(obj) {
+        if (obj && obj.__esModule) {
+            return obj;
+        } else {
+            var newObj = {};
+
+            if (obj != null) {
+                for (var key in obj) {
+                    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                        var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {};
+
+                        if (desc.get || desc.set) {
+                            Object.defineProperty(newObj, key, desc);
+                        } else {
+                            newObj[key] = obj[key];
+                        }
+                    }
+                }
+            }
+
+            newObj.default = obj;
+            return newObj;
+        }
+    }
+
+
     var cache;
 
     if (cache) {
@@ -10,6 +37,6 @@ function __swcpack_require__(mod) {
     };
 
     mod(module, module.exports);
-    cache = module.exports;
+    cache = interop(module.exports);
     return cache;
 }
