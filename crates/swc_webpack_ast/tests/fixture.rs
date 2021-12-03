ba27a3562bab46c0c3a72393d2810e0540ecc0be
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-12-03 18:54:28 +0900

    fix(webpack/ast): Remove more string literals (#2957)

--- crates/swc_webpack_ast/tests/fixture.rs
+++ crates/swc_webpack_ast/tests/fixture.rs
@@ -1,10 +1,10 @@
 use std::{fs, path::PathBuf};
-use swc_common::{chain, Mark, DUMMY_SP};
+use swc_common::{chain, Mark};
 use swc_ecma_ast::*;
 use swc_ecma_parser::{EsConfig, Parser, StringInput, Syntax, TsConfig};
 use swc_ecma_transforms_base::resolver::resolver_with_mark;
 use swc_ecma_transforms_testing::test_fixture;
-use swc_ecma_visit::{as_folder, Node, Visit, VisitMutWith, VisitWith};
+use swc_ecma_visit::{as_folder, VisitMut, VisitMutWith};
 use swc_webpack_ast::reducer::ast_reducer;
 
 #[testing::fixture("tests/fixture/**/input.js")]
@@ -21,7 +21,8 @@ fn fixture(input: PathBuf) {
 
             chain!(
                 resolver_with_mark(top_level_mark),
-                as_folder(ast_reducer(top_level_mark))
+                as_folder(ast_reducer(top_level_mark)),
+                as_folder(AssertValid)
             )
         },
         &input,
@@ -31,13 +32,19 @@ fn fixture(input: PathBuf) {
 
 struct AssertValid;
 
-impl Visit for AssertValid {
-    fn visit_invalid(&mut self, i: &Invalid, _: &dyn Node) {
+impl VisitMut for AssertValid {
+    fn visit_mut_invalid(&mut self, i: &mut Invalid) {
         panic!("found invalid: {:?}", i)
     }
 
-    fn visit_empty_stmt(&mut self, i: &EmptyStmt, _: &dyn Node) {
+    fn visit_mut_empty_stmt(&mut self, i: &mut EmptyStmt) {
         panic!("found empty: {:?}", i)
+    }
+
+    fn visit_mut_ident(&mut self, i: &mut Ident) {
+        if i.span.is_dummy() {
+            panic!("found an identifier with dummy span: {:?}", i)
+        }
     }
 }
 
@@ -78,7 +85,7 @@ fn assert_no_invalid(input: PathBuf) {
 
         module.visit_mut_with(&mut pass);
         dbg!(&module);
-        module.visit_with(&Invalid { span: DUMMY_SP }, &mut AssertValid);
+        module.visit_mut_with(&mut AssertValid);
 
         Ok(())
     })
