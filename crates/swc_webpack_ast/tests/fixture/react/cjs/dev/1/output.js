37d24fa81541ce33bf829ce9326eb9e52c06e081
Donny <kdy1997.dev@gmail.com>
2021-12-06 20:48:33 +0900

    Update test refs

---
+++ crates/swc_webpack_ast/tests/fixture/react/cjs/dev/1/output.js
@@ -0,0 +1,46 @@
+if (process.env.NODE_ENV !== "production") {
+    require("object-assign");
+    exports.Fragment;
+    exports.StrictMode;
+    exports.Profiler;
+    exports.Suspense;
+    exports.Fragment;
+    exports.StrictMode;
+    exports.Profiler;
+    exports.Suspense;
+    switch(null){
+        case exports.Fragment:
+        case exports.Profiler:
+        case exports.StrictMode:
+        case exports.Suspense:
+    }
+    exports.Fragment, exports.Profiler, exports.StrictMode, exports.Suspense;
+    switch(null){
+        case exports.Suspense:
+    }
+    exports.Fragment;
+    exports.Children;
+    exports.Component;
+    exports.PureComponent;
+    exports.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+    exports.cloneElement;
+    exports.createContext;
+    exports.createElement;
+    exports.createFactory;
+    exports.createRef;
+    exports.forwardRef;
+    exports.isValidElement;
+    exports.lazy;
+    exports.memo;
+    exports.useCallback;
+    exports.useContext;
+    exports.useDebugValue;
+    exports.useEffect;
+    exports.useImperativeHandle;
+    exports.useLayoutEffect;
+    exports.useMemo;
+    exports.useReducer;
+    exports.useRef;
+    exports.useState;
+    exports.version;
+}
