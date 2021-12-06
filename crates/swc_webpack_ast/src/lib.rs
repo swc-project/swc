c4d66c84676a3ffb622e26fb8a090d461f2615f7
Donny <kdy1997.dev@gmail.com>
2021-12-06 20:14:36 +0900

    WIP: Debug mode

--- crates/swc_webpack_ast/src/lib.rs
+++ crates/swc_webpack_ast/src/lib.rs
@@ -1,7 +1,10 @@
+use std::path::Path;
+
 use crate::reducer::ast_reducer;
 use anyhow::{Context, Error};
-use swc_common::{sync::Lrc, Mark, SourceFile, SourceMap};
+use swc_common::{sync::Lrc, FilePathMapping, Globals, Mark, SourceFile, SourceMap, GLOBALS};
 use swc_ecma_ast::*;
+use swc_ecma_parser::{lexer::Lexer, EsConfig, Parser, StringInput, Syntax, TsConfig};
 use swc_ecma_transforms_base::resolver::resolver_with_mark;
 use swc_ecma_visit::VisitMutWith;
 use swc_estree_ast::flavor::Flavor;
@@ -9,6 +12,50 @@ use swc_estree_compat::babelify::Babelify;
 use swc_timer::timer;
 
 pub mod reducer;
+
+pub fn parse_file_as_webpack_ast(path: &Path) -> Result<String, Error> {
+    let globals = Globals::new();
+    let cm = Lrc::new(SourceMap::new(FilePathMapping::empty()));
+
+    let fm = cm
+        .load_file(&path)
+        .with_context(|| format!("failed to load file at `{}`", path.display()))?;
+
+    let syntax = match path.extension() {
+        Some(ext) => {
+            if ext == "tsx" {
+                Syntax::Typescript(TsConfig {
+                    tsx: true,
+                    no_early_errors: true,
+                    ..Default::default()
+                })
+            } else if ext == "ts" {
+                Syntax::Typescript(TsConfig {
+                    no_early_errors: true,
+                    ..Default::default()
+                })
+            } else {
+                Syntax::Es(EsConfig {
+                    jsx: true,
+                    ..Default::default()
+                })
+            }
+        }
+        None => Default::default(),
+    };
+
+    let module = {
+        let lexer = Lexer::new(syntax, EsVersion::latest(), StringInput::from(&*fm), None);
+        let mut parser = Parser::new_from(lexer);
+
+        parser.parse_module().map_err(|err| {
+            // err.into_diagnostic(handler).emit();
+            anyhow::anyhow!("failed to parse module")
+        })?
+    };
+
+    GLOBALS.set(&globals, || webpack_ast(cm.clone(), fm, module))
+}
 
 /// `n` is expected to be pure (`resolver` is not applied)
 pub fn webpack_ast(
