fdb1eec1c61c44416e2412f0c91f8bcd70a98844
Donny <kdy1997.dev@gmail.com>
2021-12-06 20:39:08 +0900

    Update test refs

--- crates/swc_webpack_ast/tests/fixture/react-dom/dev-cjs/output.js
+++ crates/swc_webpack_ast/tests/fixture/react-dom/dev-cjs/output.js
@@ -3,4 +3,15 @@ if (process.env.NODE_ENV !== "production") {
     require("object-assign");
     require("scheduler");
     require("scheduler/tracing");
+    exports.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+    exports.createPortal;
+    exports.findDOMNode;
+    exports.flushSync;
+    exports.hydrate;
+    exports.render;
+    exports.unmountComponentAtNode;
+    exports.unstable_batchedUpdates;
+    exports.unstable_createPortal;
+    exports.unstable_renderSubtreeIntoContainer;
+    exports.version;
 }
--- crates/swc_webpack_ast/tests/fixture/regenerator/output.js
+++ crates/swc_webpack_ast/tests/fixture/regenerator/output.js
@@ -1,1 +1,12 @@
 module, module.exports;
+exports.wrap;
+exports.isGeneratorFunction;
+exports.mark;
+exports.awrap;
+exports.AsyncIterator;
+exports.async, function() {
+    exports.isGeneratorFunction(null);
+};
+exports.keys;
+exports.values;
+exports;
