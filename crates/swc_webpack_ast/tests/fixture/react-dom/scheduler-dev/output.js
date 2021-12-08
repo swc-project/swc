7e850da7f7d4ebc04202805d50be7bd9e2ac1b01
Donny <kdy1997.dev@gmail.com>
2021-12-08 15:51:06 +0900

    Update test refs

---
+++ crates/swc_webpack_ast/tests/fixture/react-dom/scheduler-dev/output.js
@@ -0,0 +1,44 @@
+if (process.env.NODE_ENV !== "production") {
+    if (null) exports.unstable_now;
+    else exports.unstable_now;
+    if (null) {
+        (function() {
+            exports.unstable_now();
+        });
+        exports.unstable_shouldYield;
+        exports.unstable_forceFrameRate;
+    } else {
+        exports.unstable_shouldYield, function() {
+            exports.unstable_now();
+        };
+        exports.unstable_forceFrameRate;
+        (function() {
+            exports.unstable_now();
+        });
+        (function() {
+            (function() {
+                exports.unstable_now();
+            });
+        });
+    }
+    exports.unstable_now();
+    exports.unstable_shouldYield();
+    exports.unstable_now();
+    exports.unstable_now();
+    exports.unstable_IdlePriority;
+    exports.unstable_ImmediatePriority;
+    exports.unstable_LowPriority;
+    exports.unstable_NormalPriority;
+    exports.unstable_Profiling;
+    exports.unstable_UserBlockingPriority;
+    exports.unstable_cancelCallback;
+    exports.unstable_continueExecution;
+    exports.unstable_getCurrentPriorityLevel;
+    exports.unstable_getFirstCallbackNode;
+    exports.unstable_next;
+    exports.unstable_pauseExecution;
+    exports.unstable_requestPaint;
+    exports.unstable_runWithPriority;
+    exports.unstable_scheduleCallback;
+    exports.unstable_wrapCallback;
+}
