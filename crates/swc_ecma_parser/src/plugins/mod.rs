5eda78a5abcf4c608cb9e0a15d8015e4d0e809b3
Donny <kdy1997.dev@gmail.com>
2021-12-13 18:56:00 +0900

    Add `StripPlugin`

---
+++ crates/swc_ecma_parser/src/plugins/mod.rs
@@ -0,0 +1,3 @@
+#[cfg(feature = "typescript")]
+#[cfg_attr(docsrs, doc(cfg(feature = "typescript")))]
+pub mod typescript;
---
+++ crates/swc_ecma_parser/src/plugins/typescript/strip.rs
@@ -0,0 +1,5 @@
+use crate::plugin::TypeScriptPlugin;
+
+pub struct StripPlugin {}
+
+impl TypeScriptPlugin for StripPlugin {}
