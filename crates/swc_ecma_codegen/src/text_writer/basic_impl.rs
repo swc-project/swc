c415487bb91f466948c2279030f3643049ad395a
Donny/강동윤 <kdy1997.dev@gmail.com>
2021-12-13 09:21:22 +0900

    fix(es/codegen): Fix sourcemap bug related to multi-line comments (#3023)
    swc_ecma_codegen:
     - Compute line starts in multi-line comments.

--- crates/swc_ecma_codegen/src/text_writer/basic_impl.rs
+++ crates/swc_ecma_codegen/src/text_writer/basic_impl.rs
@@ -184,6 +184,13 @@ impl<'a, W: Write> WriteJs for JsWriter<'a, W> {
 
     fn write_comment(&mut self, span: Span, s: &str) -> Result {
         self.write(Some(span), s)?;
+        {
+            let line_start_of_s = compute_line_starts(s);
+            if line_start_of_s.len() > 1 {
+                self.line_count = self.line_count + line_start_of_s.len() - 1;
+                self.line_pos = s.len() - line_start_of_s.last().cloned().unwrap_or(0);
+            }
+        }
         Ok(())
     }
 
