537cca978c89c0a172df85a85ab24ed058afee8e
sosukesuzuki <aosukeke@gmail.com>
2021-12-14 08:53:32 +0900

    Add parse_module_export_name

--- crates/swc_ecma_parser/src/parser/ident.rs
+++ crates/swc_ecma_parser/src/parser/ident.rs
@@ -4,6 +4,12 @@ use crate::token::Keyword;
 use either::Either;
 use swc_atoms::js_word;
 
+#[derive(Clone)]
+pub enum ModuleExportName {
+    Ident(Ident),
+    Str(Str),
+}
+
 impl<'a, I: Tokens> Parser<I> {
     pub(super) fn parse_maybe_private_name(&mut self) -> PResult<Either<PrivateName, Ident>> {
         let is_private = is!(self, '#');
@@ -71,6 +77,18 @@ impl<'a, I: Tokens> Parser<I> {
         };
 
         Ok(Ident::new(w, span!(self, start)))
+    }
+
+    // https://tc39.es/ecma262/#prod-ModuleExportName
+    pub(super) fn parse_module_export_name(&mut self) -> PResult<ModuleExportName> {
+        let module_export_name = match cur!(self, false)? {
+            Token::Str { .. } => ModuleExportName::Str(match self.parse_lit()? {
+                Lit::Str(str_lit) => str_lit,
+                _ => unreachable!(""),
+            }),
+            _ => ModuleExportName::Ident(self.parse_ident_name()?),
+        };
+        Ok(module_export_name)
     }
 
     /// Identifier
