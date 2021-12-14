af587d0fda9ad37823f91c1767178a8ace88b57a
Donny <kdy1997.dev@gmail.com>
2021-12-14 14:47:23 +0900

    --no-run

--- crates/swc_webpack_ast/scripts/test-next.sh
+++ crates/swc_webpack_ast/scripts/test-next.sh
@@ -4,6 +4,7 @@ set -eu
 # This script tests if webpack ast is identical to webpack ast produced by acorn,
 # by running webpack via next.js
 
+cargo test --no-run
 UPDATE=1 cargo test -q
 
 (cd next.js/packages/next-swc && yarn build-native)
