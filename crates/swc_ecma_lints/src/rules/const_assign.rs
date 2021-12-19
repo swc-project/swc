d3fb6a513d7335b0a72d27114f2621f6c8cc85bd
Donny <kdy1997.dev@gmail.com>
2021-12-19 19:29:39 +0900

    Implement `const-assign`

---
+++ crates/swc_ecma_lints/src/rules/const_assign.rs
@@ -0,0 +1,107 @@
+use crate::rule::{default_visitor_rule, Rule};
+use std::sync::Arc;
+use swc_common::{collections::AHashSet, errors::HANDLER};
+use swc_ecma_ast::*;
+use swc_ecma_utils::ident::IdentLike;
+use swc_ecma_visit::{Visit, VisitWith};
+
+pub fn const_assign() -> Arc<dyn Rule> {
+    default_visitor_rule::<ConstAssign>()
+}
+
+#[derive(Debug, Default)]
+struct ConstAssign {
+    const_vars: AHashSet<Id>,
+
+    is_pat_decl: bool,
+}
+
+impl ConstAssign {
+    fn check(&mut self, id: &Ident) {
+        if !self.is_pat_decl {
+            return;
+        }
+
+        if self.const_vars.contains(&id.to_id()) {
+            HANDLER.with(|handler| {
+                handler
+                    .struct_span_err(
+                        id.span,
+                        "Cannot reassign to a variable declared with `const`",
+                    )
+                    .emit();
+            });
+        }
+    }
+}
+
+impl Visit for ConstAssign {
+    fn visit_assign_pat_prop(&mut self, p: &AssignPatProp) {
+        p.visit_children_with(self);
+
+        self.check(&p.key);
+    }
+
+    fn visit_pat(&mut self, p: &Pat) {
+        p.visit_children_with(self);
+
+        match p {
+            Pat::Ident(p) => {
+                self.check(&p.id);
+            }
+
+            _ => {}
+        }
+    }
+
+    fn visit_program(&mut self, program: &Program) {
+        program.visit_children_with(&mut Collector {
+            const_vars: &mut self.const_vars,
+            var_decl_kind: None,
+        });
+
+        program.visit_children_with(self);
+    }
+
+    fn visit_var_decl(&mut self, var_decl: &VarDecl) {
+        let old_is_pat_decl = self.is_pat_decl;
+        self.is_pat_decl = true;
+        var_decl.visit_children_with(self);
+        self.is_pat_decl = old_is_pat_decl;
+    }
+}
+
+struct Collector<'a> {
+    const_vars: &'a mut AHashSet<Id>,
+
+    var_decl_kind: Option<VarDeclKind>,
+}
+
+impl Visit for Collector<'_> {
+    fn visit_assign_pat_prop(&mut self, p: &AssignPatProp) {
+        p.visit_children_with(self);
+
+        if let Some(VarDeclKind::Const) = self.var_decl_kind {
+            self.const_vars.insert(p.key.to_id());
+        }
+    }
+
+    fn visit_pat(&mut self, p: &Pat) {
+        p.visit_children_with(self);
+
+        if let Some(VarDeclKind::Const) = self.var_decl_kind {
+            if let Pat::Ident(i) = p {
+                self.const_vars.insert(i.to_id());
+            }
+        }
+    }
+
+    fn visit_var_decl(&mut self, var_decl: &VarDecl) {
+        let old_var_decl_kind = self.var_decl_kind;
+        self.var_decl_kind = Some(var_decl.kind);
+
+        var_decl.visit_children_with(self);
+
+        self.var_decl_kind = old_var_decl_kind;
+    }
+}
--- crates/swc_ecma_lints/src/rules/mod.rs
+++ crates/swc_ecma_lints/src/rules/mod.rs
@@ -4,4 +4,6 @@ use std::sync::Arc;
 mod const_assign;
 mod duplicated_binding;
 
-pub fn all() -> Vec<Arc<dyn Rule>> {}
+pub fn all() -> Vec<Arc<dyn Rule>> {
+    vec![self::const_assign::const_assign()]
+}
