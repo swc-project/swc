703d6b0a405216fc3801ee71704327aacd57edfa
quentm74 <quentinmichel69110@gmail.com>
2021-12-20 14:55:41 +0100

    move feature check to build script

--- crates/node/build.rs
+++ crates/node/build.rs
@@ -1,5 +1,8 @@
 extern crate napi_build;
 
+#[cfg(all(not(feature = "swc_v1"), not(feature = "swc_v2")))]
+compile_error!("Please enable swc_v1 or swc_v2 feature");
+
 fn main() {
     napi_build::setup();
 }
