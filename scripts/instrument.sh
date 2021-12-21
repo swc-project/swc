a1ab9f0b39d39a372a2740ca4aa5d5e0fa875754
Donny <kdy1997.dev@gmail.com>
2021-12-21 16:06:17 +0900

    Fix instrument scripts

--- scripts/instrument.sh
+++ scripts/instrument.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo instruments --release -t time --bench typescript --features concurrent,tracing/release_max_level_info $@
+cargo profile instruments --release -t time --bench typescript --features concurrent,tracing/release_max_level_info $@
