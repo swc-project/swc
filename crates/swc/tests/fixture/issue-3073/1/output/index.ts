30baa7276346954efd148e03354e5ad68a663f5a
magic-akari <akari.ccino@gmail.com>
2021-12-21 10:29:57 +0800

    fix(ecma/transform): add test

---
+++ crates/swc/tests/fixture/issue-3073/1/output/index.ts
@@ -0,0 +1,8 @@
+var n;
+(function(n1) {
+    var ref;
+    ref = {
+        a: 1
+    }, n1.a = ref.a, ref;
+})(n || (n = {
+}));
---
+++ crates/swc/tests/fixture/issue-3073/2/output/index.ts
@@ -0,0 +1,9 @@
+var Foo;
+(function(Foo1) {
+    var ref;
+    ref = {
+        a: 1,
+        b: 2
+    }, Foo1.A = ref.a, Foo1.B = ref.b, ref;
+})(Foo || (Foo = {
+}));
