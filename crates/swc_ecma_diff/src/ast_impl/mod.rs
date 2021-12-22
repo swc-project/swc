9308ecd862b75f1f2eb357badc2c2b4b8d7eae8e
Donny <kdy1997.dev@gmail.com>
2021-12-22 18:12:37 +0900

    fixup: ModuleItme

--- crates/swc_ecma_diff/src/ast_impl/mod.rs
+++ crates/swc_ecma_diff/src/ast_impl/mod.rs
@@ -1,14 +1,30 @@
 //! Implementation of [crate::Diff] for types in [swc_ecma_ast].
 
-use crate::{Ctx, Diff, DiffResult};
-use swc_ecma_ast::Module;
+use crate::{Ctx, Diff, DiffResult, Difference, Node};
+use swc_ecma_ast::{Module, ModuleItem};
 
 mod expr;
+mod stmt;
 
 impl Diff for Module {
     fn diff(&mut self, other: &mut Self, ctx: &mut Ctx) -> DiffResult {
         ctx.diff_struct("Module", |ctx| {
             ctx.field("span", &mut self.span, &mut other.span);
+            ctx.field("body", &mut self.body, &mut other.body);
         })
+    }
+}
+
+impl Diff for ModuleItem {
+    fn diff(&mut self, other: &mut Self, ctx: &mut Ctx) -> DiffResult {
+        match (self, other) {
+            (ModuleItem::Stmt(l), ModuleItem::Stmt(r)) => l.diff(r, ctx),
+            (ModuleItem::ModuleDecl(l), ModuleItem::ModuleDecl(r)) => l.diff(r, ctx),
+            _ => DiffResult::Different(Difference {
+                path: ctx.path.clone(),
+                left: Node(format!("{:?}", self)),
+                right: Node(format!("{:?}", other)),
+            }),
+        }
     }
 }
