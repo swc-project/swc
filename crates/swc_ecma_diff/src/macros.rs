7e3786cf264603567d64c8678a873332474d3d8f
Donny <kdy1997.dev@gmail.com>
2021-12-22 18:22:07 +0900

    Small cleanup

--- crates/swc_ecma_diff/src/macros.rs
+++ crates/swc_ecma_diff/src/macros.rs
@@ -7,14 +7,12 @@ macro_rules! diff_enum {
     ) => {
         impl crate::Diff for $T {
             fn diff(&mut self, other: &mut Self, ctx: &mut crate::Ctx) -> crate::DiffResult {
-                use crate::Diff;
-
                 match (self, other) {
                     $(
                         (
                             $T::$Variant(l),
                             $T::$Variant(r),
-                        ) => l.diff(r, ctx),
+                        ) => crate::Diff::diff(l,r,ctx),
                     )*
                     _ => crate::DiffResult::Different(crate::Difference {
                         path: ctx.path.clone(),
