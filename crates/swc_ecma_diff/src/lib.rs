38ce06339544f11eaad7880c67a3bd289bf11d9b
Donny <kdy1997.dev@gmail.com>
2021-12-22 17:05:57 +0900

    Some types

--- crates/swc_ecma_diff/src/lib.rs
+++ crates/swc_ecma_diff/src/lib.rs
@@ -1,1 +1,15 @@
+use swc_atoms::JsWord;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct DiffResult {}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum PathComponent {
+    ObjProp { key: JsWord },
+    VecElem { index: usize },
+}
+
+pub trait Diff {
+    /// This may remove common node from `self` and `other`.
+    fn diff(&mut self, other: &mut Self) -> DiffResult;
+}
