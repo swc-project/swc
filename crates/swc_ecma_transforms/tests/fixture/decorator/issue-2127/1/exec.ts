99a871cac35b435f68735a07e3dd9bbcc62c360d
Donny <kdy1997.dev@gmail.com>
2021-12-23 16:27:43 +0900

    Add tests for #2127

---
+++ crates/swc_ecma_transforms/tests/fixture/decorator/issue-2127/1/exec.ts
@@ -0,0 +1,12 @@
+const returnValue = "asdasd"
+class TestClass {
+    public testProperty: Date;
+}
+
+Object.defineProperty(TestClass.prototype, "testProperty", {
+    get: function () { return returnValue },
+    enumerable: true,
+    configurable: true,
+});
+const instance = new TestClass()
+expect(instance.testProperty).toBe(returnValue)
---
+++ crates/swc_ecma_transforms/tests/fixture/decorator/issue-2127/2/exec.ts
@@ -0,0 +1,13 @@
+const returnValue = "asdasd"
+class TestClass2 {
+    @deco public testProperty: Date;
+}
+function deco(target: any, key: string) {
+    Object.defineProperty(target.constructor.prototype, key, {
+        get: function () { return returnValue },
+        enumerable: true,
+        configurable: true,
+    });
+}
+const instance = new TestClass2()
+expect(instance.testProperty).toBe(returnValue)
