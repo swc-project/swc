30c2688e5ef8cdc3a654992db63d8b1b58803c9b
Donny <kdy1997.dev@gmail.com>
2021-12-23 17:48:34 +0900

    More log

--- crates/swc_ecma_transforms/tests/fixture/decorator/issue-2127/2/exec.ts
+++ crates/swc_ecma_transforms/tests/fixture/decorator/issue-2127/2/exec.ts
@@ -3,11 +3,13 @@ class TestClass2 {
     @deco public testProperty: Date;
 }
 function deco(target: any, key: string) {
+    console.log(target, key);
     Object.defineProperty(target.constructor.prototype, key, {
         get: function () { return returnValue },
         enumerable: true,
         configurable: true,
     });
 }
+console.log(TestClass2.prototype.testProperty)
 const instance = new TestClass2()
 expect(instance.testProperty).toBe(returnValue)
