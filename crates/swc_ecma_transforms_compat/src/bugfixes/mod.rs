bede5162032232158df80f2b6f02107c7879ef18
sosukesuzuki <aosukeke@gmail.com>
2021-12-24 19:24:40 +0900

    renaming

--- crates/swc_ecma_transforms_compat/src/bugfixes/mod.rs
+++ crates/swc_ecma_transforms_compat/src/bugfixes/mod.rs
@@ -1,14 +1,15 @@
 pub use self::{
     async_arrows_in_class::async_arrows_in_class, edge_default_param::edge_default_param,
-    template_literal_caching::template_literal_caching, safari_id_destructuring_collision_in_function_expression::safari_id_destructuring_collision_in_function_expression
+    safari_id_destructuring_collision_in_function_expression::safari_id_destructuring_collision_in_function_expression,
+    template_literal_caching::template_literal_caching,
 };
 use swc_common::chain;
 use swc_ecma_visit::Fold;
 
 mod async_arrows_in_class;
 mod edge_default_param;
-mod template_literal_caching;
 mod safari_id_destructuring_collision_in_function_expression;
+mod template_literal_caching;
 
 pub fn bugfixes() -> impl Fold {
     chain!(
--- crates/swc_ecma_transforms_compat/src/bugfixes/safari_id_destructuring_collision_in_function_expression.rs
+++ crates/swc_ecma_transforms_compat/src/bugfixes/safari_id_destructuring_collision_in_function_expression.rs
@@ -1,7 +1,8 @@
-use swc_ecma_ast::*;
 use swc_atoms::JsWord;
-use swc_ecma_visit::{Fold, VisitMut, as_folder, noop_visit_mut_type, VisitMutWith};
-use swc_common::{collections::AHashSet};
+use swc_common::{collections::AHashMap, Span};
+use swc_ecma_ast::*;
+use swc_ecma_transforms_base::hygiene::rename;
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 pub fn safari_id_destructuring_collision_in_function_expression() -> impl Fold + VisitMut {
     as_folder(SafariIdDestructuringCollisionInFunctionExpression::default())
@@ -10,32 +11,34 @@ pub fn safari_id_destructuring_collision_in_function_expression() -> impl Fold +
 #[derive(Default, Clone)]
 struct SafariIdDestructuringCollisionInFunctionExpression {
     fn_expr_name: JsWord,
-    destructured_ids: AHashSet<JsWord>
+    destructured_id_span: Option<Span>,
 }
-
 
 impl VisitMut for SafariIdDestructuringCollisionInFunctionExpression {
     noop_visit_mut_type!();
 
     fn visit_mut_binding_ident(&mut self, i: &mut BindingIdent) {
-        self.destructured_ids.insert(i.id.sym.clone());
+        let sym = i.id.sym.clone();
+        if self.fn_expr_name.eq(&sym) {
+            self.destructured_id_span = Some(i.id.span);
+        }
     }
 
     fn visit_mut_param(&mut self, param: &mut Param) {
         param.visit_mut_children_with(self);
     }
 
-    fn visit_mut_block_stmt(&mut self, _n: &mut BlockStmt) {
-        if self.destructured_ids.contains(&self.fn_expr_name) {
-            dbg!("collision");
-        }
-    }
-
     fn visit_mut_fn_expr(&mut self, n: &mut FnExpr) {
         if let Some(ident) = &n.ident {
             self.fn_expr_name = ident.sym.clone();
             n.function.params.visit_mut_children_with(self);
-            n.function.body.visit_mut_children_with(self);
+            if let Some(id_span) = &self.destructured_id_span {
+                let mut rename_map = AHashMap::default();
+                let new_id: JsWord = { "hi".into() };
+                let id = (self.fn_expr_name.clone(), id_span.ctxt());
+                rename_map.insert(id, new_id);
+                n.function.visit_mut_children_with(&mut rename(&rename_map));
+            }
         }
     }
 }
@@ -49,7 +52,7 @@ mod tests {
         ::swc_ecma_parser::Syntax::default(),
         |_| safari_id_destructuring_collision_in_function_expression(),
         basic,
-        "(function a ([a]) {})",
+        "(function a ([a]) { console.log(a) })",
         "(function a ([a]) {})"
     );
 }
