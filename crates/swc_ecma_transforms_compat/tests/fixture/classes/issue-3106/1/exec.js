e5971f77d5861038f7dcd905a9aca72c84b1a035
magic-akari <akari.ccino@gmail.com>
2021-12-24 12:29:49 +0800

    fix(es/ast): Fix `Ident::verify_symbol` (#3108)

--- crates/swc_ecma_transforms_compat/tests/fixture/classes/issue-3106/1/exec.js
+++ crates/swc_ecma_transforms_compat/tests/fixture/classes/issue-3106/1/exec.js
@@ -6,5 +6,15 @@ class ValidationStrategy {
     static "string!"(value) {
         return 2;
     }
+
+    static "eval?"(value) {
+        return 3;
+    }
+
+    static "arguments!"(value) {
+        return 3;
+    }
 }
-console.log(ValidationStrategy.prototype['string!']);
+console.log(ValidationStrategy.prototype["string!"]);
+console.log(ValidationStrategy.prototype["eval?"]);
+console.log(ValidationStrategy.prototype["arguments!"]);
