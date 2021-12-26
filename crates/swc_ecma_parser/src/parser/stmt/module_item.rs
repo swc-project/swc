7ffc050455ab4fe6ecfd8a693518213d859c0013
Andreu Botella <abb@randomunok.com>
2021-12-26 22:10:37 +0100

    Revert the changes to the parser

--- crates/swc_ecma_parser/src/parser/stmt/module_item.rs
+++ crates/swc_ecma_parser/src/parser/stmt/module_item.rs
@@ -153,16 +153,8 @@ impl<'a, I: Tokens> Parser<I> {
         let _ = cur!(self, false);
         let asserts = if self.input.syntax().import_assertions()
             && !self.input.had_line_break_before_cur()
-            && is!(self, "assert")
+            && eat!(self, "assert")
         {
-            let valid = self.target() >= EsVersion::Es2022;
-
-            if !valid {
-                self.emit_err(self.input.cur_span(), SyntaxError::ImportAssertions);
-            }
-
-            eat!(self, "assert");
-
             match *self.parse_object::<Box<Expr>>()? {
                 Expr::Object(v) => Some(v),
                 _ => unreachable!(),
@@ -763,16 +755,8 @@ impl<'a, I: Tokens> Parser<I> {
         let _ = cur!(self, false);
         let asserts = if self.input.syntax().import_assertions()
             && !self.input.had_line_break_before_cur()
-            && is!(self, "assert")
+            && eat!(self, "assert")
         {
-            let valid = self.target() >= EsVersion::Es2022;
-
-            if !valid {
-                self.emit_err(self.input.cur_span(), SyntaxError::ImportAssertions);
-            }
-
-            eat!(self, "assert");
-
             match *self.parse_object::<Box<Expr>>()? {
                 Expr::Object(v) => Some(v),
                 _ => unreachable!(),
