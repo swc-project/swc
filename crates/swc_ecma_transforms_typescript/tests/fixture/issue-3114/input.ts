ee425bbd56143d7bed5c35054607b0d8faa10518
OJ Kwon <kwon.ohjoong@gmail.com>
2021-12-28 07:08:08 +0000

    test(es/transform/typescript): update test cases

---
+++ crates/swc_ecma_transforms_typescript/tests/fixture/issue-3114/input.ts
@@ -0,0 +1,7 @@
+import type { foo } from 'bar';
+import { type bar, bee } from 'baz';
+import { boo } from 'baz2';
+
+export { foo } from 'bar';
+export { bar } from 'port';
+export { boo } from 'some';
---
+++ crates/swc_ecma_transforms_typescript/tests/fixture/issue-3114/output.js
@@ -0,0 +1,4 @@
+import { boo } from 'baz2';
+export { foo } from 'bar';
+export { bar } from 'port';
+export { boo } from 'some';
