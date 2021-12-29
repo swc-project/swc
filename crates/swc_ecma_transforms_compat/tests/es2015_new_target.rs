a9877ce4b7187d7b42debf083ecb9a5fd45ada79
Austaras <austaras@outlook.com>
2021-12-29 18:15:53 +0800

    build: fix private repo download

--- crates/swc_ecma_transforms_compat/tests/es2015_new_target.rs
+++ crates/swc_ecma_transforms_compat/tests/es2015_new_target.rs
@@ -1,6 +1,6 @@
 use std::{fs::read_to_string, path::PathBuf};
 use swc_ecma_transforms_compat::es2015::new_target::new_target;
-use swc_ecma_transforms_testing::{exec_tr, test_fixture};
+use swc_ecma_transforms_testing::{exec_tr, test, test_fixture};
 
 #[testing::fixture("tests/fixture/new-target/**/exec.js")]
 fn exec(input: PathBuf) {
@@ -14,3 +14,16 @@ fn fixture(input: PathBuf) {
     let output = dir.join("output.js");
     test_fixture(Default::default(), &|_| new_target(), &input, &output);
 }
+
+test!(
+    ::swc_ecma_parser::Syntax::default(),
+    |_| new_target(),
+    edge_12,
+    r#"function foo() {
+        const a = () => new.target
+    }"#,
+    r#"function foo() {
+        var _newtarget = this instanceof foo ? this.constructor : void 0;
+        const a = () => _newtarget
+    }"#
+);
