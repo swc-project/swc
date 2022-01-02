f97c9727e641a22fd9f72d7ba40048e2bf52b526
Yuchao Liang <yuchao.dev@gmail.com>
2022-01-02 23:25:21 +0800

    fix(es/minifier): obj, num and str comparison

---
+++ crates/swc_ecma_minifier/tests/compress/fixture/issues/lit_comparisons/input.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 4;
+const c = "3";
+const d = "4";
+const e = {};
+const f = {};
+const g = true;
+const h = false;
+const j = null;
+console.log(a === b, c === d, e === f, g === h, h === j);
