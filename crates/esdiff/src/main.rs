a6af6272f383c53680b71abf0b54c86b83b328dc
Donny <kdy1997.dev@gmail.com>
2022-01-02 17:52:55 +0900

    More

--- crates/esdiff/src/main.rs
+++ crates/esdiff/src/main.rs
@@ -1,5 +1,8 @@
 use crate::minified::DiffMinifiedCommand;
+use anyhow::Result;
+use std::{sync::Arc, time::Instant};
 use structopt::StructOpt;
+use swc_common::SourceMap;
 
 mod minified;
 
@@ -8,6 +11,30 @@ enum Cmd {
     Minified(DiffMinifiedCommand),
 }
 
-fn main() {
-    println!("Hello, world!");
+struct Track {
+    start: Instant,
+}
+
+impl Drop for Track {
+    fn drop(&mut self) {
+        eprintln!("Done in {:?}", self.start.elapsed());
+    }
+}
+
+fn main() -> Result<()> {
+    let cm = Arc::new(SourceMap::default());
+
+    let _track = Track {
+        start: Instant::now(),
+    };
+
+    let cmd = Cmd::from_args();
+
+    match cmd {
+        Cmd::Minified(cmd) => {
+            cmd.run()?;
+        }
+    }
+
+    Ok(())
 }
--- crates/esdiff/src/minified.rs
+++ crates/esdiff/src/minified.rs
@@ -1,7 +1,17 @@
+use anyhow::Result;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 pub(crate) struct DiffMinifiedCommand {
     input: PathBuf,
+
+    #[structopt(long)]
+    no_mangler: bool,
+}
+
+impl DiffMinifiedCommand {
+    pub fn run(self) -> Result<()> {
+        todo!()
+    }
 }
