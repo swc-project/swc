192e2822886374cd24a3c3dbf079320bea31d09a
Donny <kdy1997.dev@gmail.com>
2022-01-02 18:00:26 +0900

    Configure more

--- crates/esdiff/src/main.rs
+++ crates/esdiff/src/main.rs
@@ -1,11 +1,12 @@
 use crate::minified::DiffMinifiedCommand;
 use anyhow::Result;
-use std::{sync::Arc, time::Instant};
+use std::{env, str::FromStr, sync::Arc, time::Instant};
 use structopt::StructOpt;
 use swc_common::{
     errors::{ColorConfig, Handler, HANDLER},
     SourceMap, GLOBALS,
 };
+use tracing_subscriber::EnvFilter;
 
 mod minified;
 
@@ -28,6 +29,21 @@ fn main() -> Result<()> {
     let globals = swc_common::Globals::default();
     let cm = Arc::new(SourceMap::default());
     let handler = Handler::with_tty_emitter(ColorConfig::Always, true, false, Some(cm.clone()));
+
+    let _logger = {
+        let log_env = env::var("RUST_LOG").unwrap_or_else(|_| "debug".to_string());
+
+        let logger = tracing_subscriber::FmtSubscriber::builder()
+            .without_time()
+            .with_target(false)
+            .with_ansi(true)
+            .with_env_filter(EnvFilter::from_str(&log_env).unwrap())
+            .with_test_writer()
+            .pretty()
+            .finish();
+
+        tracing::subscriber::set_default(logger)
+    };
 
     let _track = Track {
         start: Instant::now(),
