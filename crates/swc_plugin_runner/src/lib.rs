6916f40bf7616a14e9f281b15e4add6df51d6d4c
OJ Kwon <kwon.ohjoong@gmail.com>
2022-01-03 01:07:42 +0000

    refactor(swc/plugins): deprecate plugin runner abi_stable

--- crates/swc_plugin_runner/src/lib.rs
+++ crates/swc_plugin_runner/src/lib.rs
@@ -1,41 +1,19 @@
-use abi_stable::{
-    library::RootModule,
-    std_types::{RResult, RStr},
-};
-use anyhow::{anyhow, Context, Error};
-use rplugin::StableAst;
+use anyhow::{Context, Error};
 use std::path::Path;
 use swc_ecma_ast::Program;
-use swc_plugin_js_api::SwcJsPluginRef;
 
 pub mod resolve;
 
 pub fn apply_js_plugin(
     plugin_name: &str,
     path: &Path,
-    config_json: &str,
+    _config_json: &str,
     program: Program,
 ) -> Result<Program, Error> {
     (|| -> Result<_, Error> {
-        let plugin_rt = swc_common::plugin::get_runtime_for_plugin(plugin_name.to_string());
-
-        let plugin = SwcJsPluginRef::load_from_file(path).context("failed to load plugin")?;
-
-        let plugin_ast = StableAst::from_unstable(program);
-
-        let plugin_fn = plugin
-            .process_js()
-            .ok_or_else(|| anyhow!("the plugin does not support transforming js"))?;
-
-        let new_ast = plugin_fn(plugin_rt, RStr::from(config_json), plugin_ast);
-
-        let new = match new_ast {
-            RResult::ROk(v) => v,
-            RResult::RErr(err) => return Err(anyhow!("plugin returned an error\n{}", err)),
-        };
-        let new: Program = new.into_unstable();
-
-        Ok(new)
+        let _plugin_rt = swc_common::plugin::get_runtime_for_plugin(plugin_name.to_string());
+        //TODO: https://github.com/swc-project/swc/issues/3167
+        Ok(program)
     })()
     .with_context(|| {
         format!(
