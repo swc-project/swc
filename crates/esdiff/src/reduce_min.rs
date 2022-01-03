83c29b647030651e17f935a7379846a70c63c985
Donny <kdy1997.dev@gmail.com>
2022-01-03 19:25:16 +0900

    More work for reducer

--- crates/esdiff/src/reduce_min.rs
+++ crates/esdiff/src/reduce_min.rs
@@ -1,7 +1,12 @@
-use anyhow::Result;
-use std::{path::PathBuf, sync::Arc};
+use anyhow::{bail, Context, Result};
+use std::{
+    path::PathBuf,
+    process::{Command, Stdio},
+    sync::Arc,
+};
 use structopt::StructOpt;
 use swc_common::SourceMap;
+use tracing::info;
 
 /// This tool repeat replacing one file with a minified form at a time.
 #[derive(Debug, StructOpt)]
@@ -22,7 +27,15 @@ pub(crate) struct ReduceMinCommand {
 
 impl ReduceMinCommand {
     pub(crate) fn run(self, cm: Arc<SourceMap>) -> Result<()> {
-        let mut runner = Runner { cm };
+        let mut runner = Runner {
+            cm,
+            working_dir: self.working_dir,
+            build_command: self.build_command,
+            test_command: self.test_command,
+            expected: Default::default(),
+        };
+
+        runner.expected = runner.check().context("initial check failed")?;
 
         todo!()
     }
@@ -30,4 +43,45 @@ impl ReduceMinCommand {
 
 struct Runner {
     cm: Arc<SourceMap>,
+
+    working_dir: PathBuf,
+    build_command: String,
+    test_command: String,
+
+    expected: String,
+}
+
+impl Runner {
+    /// Build, test, and grab the console output.
+    fn check(&mut self) -> Result<String> {
+        {
+            info!("Building app");
+
+            let mut cmd = Command::new("bash");
+            let status = cmd
+                .current_dir(&self.working_dir)
+                .arg("-c")
+                .arg(&self.build_command)
+                .status()
+                .context("failed to run build command")?;
+
+            if !status.success() {
+                bail!("build failed");
+            }
+        }
+
+        info!("Testing app");
+
+        let mut cmd = Command::new("bash");
+
+        let output = cmd
+            .current_dir(&self.working_dir)
+            .arg("-c")
+            .arg(&self.test_command)
+            .stderr(Stdio::inherit())
+            .output()
+            .context("failed to get test output")?;
+
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    }
 }
