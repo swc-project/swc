8731368093da0b6ce872f4638398395463f85724
OJ Kwon <kwon.ohjoong@gmail.com>
2022-01-03 00:49:38 -0800

    refactor(swc/plugin): avoid read plugins multiple time

--- crates/swc_plugin_runner/src/lib.rs
+++ crates/swc_plugin_runner/src/lib.rs
@@ -1,6 +1,5 @@
 use anyhow::{Context, Error};
 use resolve::PluginCache;
-use std::path::Path;
 use swc_ecma_ast::Program;
 use wasmer::{imports, Instance, Module, Store};
 use wasmer_cache::{Cache, Hash};
@@ -10,9 +9,8 @@ pub mod resolve;
 /// Load plugin from specified path.
 /// If cache is provided, it'll try to load from cache first to avoid
 /// compilation.
-fn load_plugin(plugin_path: &Path, cache: &mut Option<PluginCache>) -> Result<Instance, Error> {
+fn load_plugin(module_bytes: &Vec<u8>, cache: &mut Option<PluginCache>) -> Result<Instance, Error> {
     let wasmer_store = Store::default();
-    let module_bytes = std::fs::read(plugin_path).context("Cannot read plugin from specified path");
 
     let load_from_cache = |c: &mut PluginCache, hash: Hash| match c {
         PluginCache::File(filesystem_cache) => unsafe {
@@ -24,69 +22,58 @@ fn load_plugin(plugin_path: &Path, cache: &mut Option<PluginCache>) -> Result<In
         PluginCache::File(filesystem_cache) => filesystem_cache.store(hash, module),
     };
 
-    match module_bytes {
-        Ok(bytes) => {
-            let hash = Hash::generate(&bytes);
+    let hash = Hash::generate(module_bytes);
 
-            let load_cold_wasm_bytes =
-                || Module::new(&wasmer_store, bytes).context("Cannot compile plugin");
+    let load_cold_wasm_bytes =
+        || Module::new(&wasmer_store, module_bytes).context("Cannot compile plugin");
 
-            let module = if let Some(cache) = cache {
-                let cached_module =
-                    load_from_cache(cache, hash).context("Failed to load plugin from cache");
+    let module = if let Some(cache) = cache {
+        let cached_module =
+            load_from_cache(cache, hash).context("Failed to load plugin from cache");
 
-                match cached_module {
-                    Ok(module) => Ok(module),
-                    Err(err) => {
-                        let loaded_module = load_cold_wasm_bytes().map_err(|_| err);
-                        match &loaded_module {
-                            Ok(module) => {
-                                if let Err(err) = store_into_cache(cache, hash, &module) {
-                                    loaded_module
-                                        .map_err(|_| err)
-                                        .context("Failed to store compiled plugin into cache")
-                                } else {
-                                    loaded_module
-                                }
-                            }
-                            Err(..) => loaded_module,
+        match cached_module {
+            Ok(module) => Ok(module),
+            Err(err) => {
+                let loaded_module = load_cold_wasm_bytes().map_err(|_| err);
+                match &loaded_module {
+                    Ok(module) => {
+                        if let Err(err) = store_into_cache(cache, hash, &module) {
+                            loaded_module
+                                .map_err(|_| err)
+                                .context("Failed to store compiled plugin into cache")
+                        } else {
+                            loaded_module
                         }
                     }
+                    Err(..) => loaded_module,
                 }
-            } else {
-                load_cold_wasm_bytes()
-            };
-
-            return match module {
-                Ok(module) => {
-                    let import_object = imports! {};
-
-                    Instance::new(&module, &import_object)
-                        .context("Failed to create plugin instance")
-                }
-                Err(err) => Err(err.into()),
-            };
+            }
         }
-        Err(err) => Err(err),
-    }
+    } else {
+        load_cold_wasm_bytes()
+    };
+
+    return match module {
+        Ok(module) => {
+            let import_object = imports! {};
+
+            Instance::new(&module, &import_object).context("Failed to create plugin instance")
+        }
+        Err(err) => Err(err.into()),
+    };
 }
 
 pub fn apply_js_plugin(
-    _plugin_name: &str,
-    path: &Path,
+    plugin_name: &str,
+    module_bytes: &Vec<u8>,
     cache: &mut Option<PluginCache>,
     _config_json: &str,
     program: Program,
 ) -> Result<Program, Error> {
     (|| -> Result<_, Error> {
-        let _instance = load_plugin(path, cache)?;
+        let _instance = load_plugin(module_bytes, cache)?;
         // TODO: actually apply transform from plugin
         Ok(program)
     })()
-    .with_context(|| {
-        format!(
-            "failed to invoke `{}` as js transform plugin",
-            path.display()
-        )
-    })
+    .with_context(|| format!("failed to invoke `{}` as js transform plugin", plugin_name))
 }
