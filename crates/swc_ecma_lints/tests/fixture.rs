e1883cbbdd250f1607edf3ae9335063b29f10b8e
Donny <kdy1997.dev@gmail.com>
2022-01-05 07:34:49 +0900

    Test

---
+++ crates/swc_ecma_lints/tests/fixture.rs
@@ -0,0 +1,51 @@
+use std::path::PathBuf;
+
+use swc_common::input::SourceFileInput;
+use swc_ecma_ast::EsVersion;
+use swc_ecma_lints::rules::all;
+use swc_ecma_parser::{lexer::Lexer, Parser, Syntax};
+
+#[testing::fixture("tests/pass/**/input.js")]
+#[testing::fixture("tests/pass/**/input.ts")]
+fn pass(input: PathBuf) {
+    testing::run_test(false, |cm, handler| {
+        let fm = cm.load_file(&input).unwrap();
+
+        let lexer = Lexer::new(
+            if input.extension().unwrap() == "ts" {
+                Syntax::Typescript(swc_ecma_parser::TsConfig {
+                    ..Default::default()
+                })
+            } else if input.extension().unwrap() == "tsx" {
+                Syntax::Typescript(swc_ecma_parser::TsConfig {
+                    tsx: true,
+                    ..Default::default()
+                })
+            } else {
+                Syntax::Es(swc_ecma_parser::EsConfig {
+                    ..Default::default()
+                })
+            },
+            EsVersion::latest(),
+            SourceFileInput::from(&*fm),
+            None,
+        );
+
+        let mut parser = Parser::new_from(lexer);
+
+        let m = parser.parse_module().unwrap();
+
+        let rules = all();
+
+        for mut rule in rules {
+            rule.lint_module(&m);
+        }
+
+        if handler.has_errors() {
+            return Err(());
+        }
+
+        Ok(())
+    })
+    .unwrap();
+}
