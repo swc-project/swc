b6aaa842df6e171eb5a8efe66b7c9ed95260317c
Donny <kdy1997.dev@gmail.com>
2022-01-04 21:26:25 +0900

    auto install creduce

--- crates/swc_ecma_minifier/scripts/reduce.sh
+++ crates/swc_ecma_minifier/scripts/reduce.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
+if ! command -v creduce &> /dev/null
+then
+    echo "creduce not found. Installing"
+    brew install creduce
+    exit
+fi
+
 cp $1 input.js
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
