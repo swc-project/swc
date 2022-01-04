2bf293dbf88ca731d695cc8c1424a2cdbd6958bf
Donny <kdy1997.dev@gmail.com>
2022-01-04 18:16:00 +0900

    Rename

--- crates/esdiff/scripts/try-next.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-./scripts/install.sh
-
-esdiff reduce-min --build 'rm -rf .next && npx next build' --test 'esdiff grab-console http://localhost:3000 --script test.js --start "npm start"' $@
