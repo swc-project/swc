19aa1218a11eb2c407311e831bcb5b45191e8871
Donny <kdy1997.dev@gmail.com>
2022-01-05 01:29:37 +0900

    Print code

--- crates/swc_ecma_minifier/scripts/_/reduce/compare.js
+++ crates/swc_ecma_minifier/scripts/_/reduce/compare.js
@@ -7,6 +7,9 @@ const { notDeepEqual } = require('assert');
 let [ast1, ast2] =
     process.argv.slice(2) // skip node executable and script filename
         .map(file => readFileSync(file, 'utf-8')) // read given files as strings
-        .map(code => parse(code)); // parse into ASTs
+        .map(code => {
+            console.log(code);
+            return parse(code)
+        }); // parse into ASTs
 
 notDeepEqual(ast1, ast2); // ensure they're still not equal
