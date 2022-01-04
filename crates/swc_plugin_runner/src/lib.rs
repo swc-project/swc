e573be2e9233fd5a477012ae6495dcf893c14b26
OJ Kwon <kwon.ohjoong@gmail.com>
2022-01-04 06:12:21 +0000

    refactor(swc/plugin): avoid redundant filesystem read

--- crates/swc_plugin_runner/src/lib.rs
+++ crates/swc_plugin_runner/src/lib.rs
@@ -1,5 +1,13 @@
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+
 use anyhow::{Context, Error};
+use once_cell::sync::Lazy;
+use parking_lot::Mutex;
 use resolve::PluginCache;
+use swc_common::collections::AHashMap;
 use swc_ecma_ast::Program;
 use wasmer::{imports, Instance, Module, Store};
 use wasmer_cache::{Cache, Hash};
@@ -9,7 +17,40 @@ pub mod resolve;
 /// Load plugin from specified path.
 /// If cache is provided, it'll try to load from cache first to avoid
 /// compilation.
-fn load_plugin(module_bytes: &Vec<u8>, cache: &mut Option<PluginCache>) -> Result<Instance, Error> {
+///
+/// Since plugin will be initialized per-file transform, this function tries to
+/// avoid reading filesystem per each initialization via naive in-memory map
+/// which stores raw bytecodes from file. Unlike compiled bytecode cache for the
+/// wasm, this is volatile.
+///
+/// ### Notes
+/// [This code](https://github.com/swc-project/swc/blob/fc4c6708f24cda39640fbbfe56123f2f6eeb2474/crates/swc/src/plugin.rs#L19-L44)
+/// includes previous incorrect attempt to workaround file read issues.
+/// In actual transform, `plugins` is also being called per each transform.
+fn load_plugin(plugin_path: &Path, cache: &mut Option<PluginCache>) -> Result<Instance, Error> {
+    static BYTE_CACHE: Lazy<Mutex<AHashMap<PathBuf, Arc<Vec<u8>>>>> =
+        Lazy::new(|| Default::default());
+
+    // TODO: This caching streategy does not consider few edge cases.
+    // 1. If process is long-running (devServer) binary change in the middle of
+    // process won't be reflected.
+    // 2. If reading binary fails somehow it won't bail out but keep retry.
+    let module_bytes_key = plugin_path.to_path_buf();
+    let module_bytes = if let Some(cached_bytes) = BYTE_CACHE.lock().get(&module_bytes_key).cloned()
+    {
+        cached_bytes
+    } else {
+        let fresh_module_bytes = std::fs::read(plugin_path)
+            .map(Arc::new)
+            .context("Cannot read plugin from specified path")?;
+        BYTE_CACHE
+            .lock()
+            .insert(module_bytes_key, fresh_module_bytes.clone());
+
+        fresh_module_bytes
+    };
+
+    // TODO: can we share store instances across each plugin binaries?
     let wasmer_store = Store::default();
 
     let load_from_cache = |c: &mut PluginCache, hash: Hash| match c {
@@ -22,10 +63,10 @@ fn load_plugin(module_bytes: &Vec<u8>, cache: &mut Option<PluginCache>) -> Resul
         PluginCache::File(filesystem_cache) => filesystem_cache.store(hash, module),
     };
 
-    let hash = Hash::generate(module_bytes);
+    let hash = Hash::generate(&module_bytes);
 
     let load_cold_wasm_bytes =
-        || Module::new(&wasmer_store, module_bytes).context("Cannot compile plugin");
+        || Module::new(&wasmer_store, module_bytes.as_ref()).context("Cannot compile plugin");
 
     let module = if let Some(cache) = cache {
         let cached_module =
@@ -64,16 +105,21 @@ fn load_plugin(module_bytes: &Vec<u8>, cache: &mut Option<PluginCache>) -> Resul
 }
 
 pub fn apply_js_plugin(
-    plugin_name: &str,
-    module_bytes: &Vec<u8>,
+    _plugin_name: &str,
+    path: &Path,
     cache: &mut Option<PluginCache>,
     _config_json: &str,
     program: Program,
 ) -> Result<Program, Error> {
     (|| -> Result<_, Error> {
-        let _instance = load_plugin(module_bytes, cache)?;
+        let _instance = load_plugin(path, cache)?;
         // TODO: actually apply transform from plugin
         Ok(program)
     })()
-    .with_context(|| format!("failed to invoke `{}` as js transform plugin", plugin_name))
+    .with_context(|| {
+        format!(
+            "failed to invoke `{}` as js transform plugin",
+            path.display()
+        )
+    })
 }
