4d38407b135aa9b5d2c336e5d2f8afc1dc416764
Donny <kdy1997.dev@gmail.com>
2022-01-05 04:52:01 +0900

    Update test refs

--- crates/swc/tests/fixture/issue-2717/output/index.ts
+++ crates/swc/tests/fixture/issue-2717/output/index.ts
@@ -2,14 +2,15 @@ import a from "regenerator-runtime";
 import { takeLatest as b, select as c } from "redux-saga/effects";
 import { CHANGE_THEME as d, CHANGE_THEME_CUSTOM_PALETTE as e, CHANGE_THEME_SWITCH_MODE as f } from "core/actions/changeTheme";
 var g = a.mark(saveTheme);
+export var selectThemeObject = function(a) {
+    return a.theme;
+};
 export function saveTheme(b) {
     var d, e, h;
     return a.wrap(function(a) {
         for(;;)switch(a.prev = a.next){
             case 0:
-                return d = b.type, a.next = 3, c(function(a) {
-                    return a.theme;
-                });
+                return d = b.type, a.next = 3, c(selectThemeObject);
             case 3:
                 e = a.sent, window.localStorage.setItem("theme", JSON.stringify({
                     theme: e.theme,
