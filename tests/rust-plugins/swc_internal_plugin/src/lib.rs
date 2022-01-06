3e196f442ea057a6508ca342555ecf4684274360
OJ Kwon <kwon.ohjoong@gmail.com>
2022-01-06 00:13:05 -0800

    test(swc/plugin_runner): update test plugin

--- tests/rust-plugins/swc_internal_plugin/src/lib.rs
+++ tests/rust-plugins/swc_internal_plugin/src/lib.rs
@@ -1,16 +1,28 @@
-use serde::Deserialize;
-use swc_ecmascript::visit::Fold;
-use swc_plugin::define_js_plugin;
+use rkyv::{AlignedVec, Deserialize};
+use swc_plugin::{swc_plugin, ModuleItem, Program, VisitMut};
 
-define_js_plugin!(internal_test);
+struct Dummy;
 
-fn internal_test(_: Config) -> impl Fold {
-    InternalTest
+impl VisitMut for Dummy {
+    fn visit_mut_module_item(&mut self, _module: &mut ModuleItem) {
+        // noop
+    }
 }
 
-#[derive(Debug, Deserialize)]
-struct Config {}
+#[no_mangle]
+// TODO:
+// - this is not complete signature, need to take config_json
+// - swc_plugin macro to support ergonomic interfaces
+// - better developer experience: println / dbg!() doesn't emit anything
+// - typed json config instead of str, which requires additional deserialization
+pub fn process(ast_ptr: *mut u8, len: u32) -> i32 {
+    let mut vec = AlignedVec::with_capacity(len.try_into().unwrap());
+    let v = unsafe { std::slice::from_raw_parts(ast_ptr, len.try_into().unwrap()) };
+    vec.extend_from_slice(v);
 
-struct InternalTest;
+    // TODO: trait bound complaining in deserialize_for_plugin<T>
+    let archived = unsafe { rkyv::archived_root::<Program>(&vec[..]) };
+    let _v: Program = archived.deserialize(&mut rkyv::Infallible).unwrap();
 
-impl Fold for InternalTest {}
+    0
+}
