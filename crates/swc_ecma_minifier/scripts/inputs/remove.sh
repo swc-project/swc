167d7b99159154303ebb6ca18a2a632c93310f58
Donny <kdy1997.dev@gmail.com>
2022-01-08 14:56:49 +0900

    more ext

--- crates/swc_ecma_minifier/scripts/inputs/remove.sh
+++ crates/swc_ecma_minifier/scripts/inputs/remove.sh
@@ -3,6 +3,19 @@ set -eu
 
 
 find ./inputs/ -type f \
+    -name "*.html" \
+    -name "*.xml" \
+    -name "*.aac" \
+    -name "*.avif" \
+    -name "*.ogg" \
+    -name "*.mp3" \
+    -name "*.mp4" \
+    -name "*.wav" \
+    -name "*.bat" \
+    -name "*.cur" \
+    -name "*.db" \
+    -name "*.diff" \
+    -name "*.donotoptimizepng" \
     -name "*.gif" \
     -name "*.svg" \
     -name "*.css" \
@@ -10,11 +23,41 @@ find ./inputs/ -type f \
     -name "*.json" \
     -name "*.woff" \
     -name "*.woff2" \
+    -name "*.otf" \
     -name "*.ttf" \
     -name "*.eot" \
+    -name "*.flow" \
+    -name "*.md" \
+    -name "*.less" \
+    -name "*.sass" \
+    -name "*.scss" \
+    -name "*.ico" \
+    -name "*.sh" \
     -name "*.jpg" \
+    -name "*.webp" \
     -name "*.jpeg" \
-    -name "*.map" \
+    -name "*.gz" \
+    -name "*.gzip" \
+    -name "*.zip" \
+    -name "*.woff" \
+    -name "*.wasm" \
+    -name "*.wast" \
+    -name "*.tsbuildinfo" \
     -print0 -delete
+
+# hpb
+# jsonp
+# lang
+# php
+# properties
+# smd
+# styl
+# svg alias
+# swf
+# ts
+# txt
+# vtt
+# vue
+# xap
 
 find ./inputs -type d -empty -delete
