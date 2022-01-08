cc9650dcd44aba2c931fb6c34525d3021364107e
Donny <kdy1997.dev@gmail.com>
2022-01-08 15:59:16 +0900

    Remove empty dirs

--- crates/swc_ecma_minifier/scripts/reduce-all.sh
+++ crates/swc_ecma_minifier/scripts/reduce-all.sh
@@ -6,3 +6,8 @@ dir="${1-"./inputs/"}"
 find $dir -type f -name '*.js' -print0 | xargs -0 -I {} sh -c '\
     echo "Processing {}"
     ./scripts/reduce.sh {}'
+
+
+echo "Removing empty directories in $dir"
+
+find $dir -type d -empty -delete
