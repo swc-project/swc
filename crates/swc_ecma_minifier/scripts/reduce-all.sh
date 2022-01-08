1a4eb194e0d82a7b32f1fd0a1a72aaa38a32e11f
Donny <kdy1997.dev@gmail.com>
2022-01-08 16:11:19 +0900

    || true

--- crates/swc_ecma_minifier/scripts/reduce-all.sh
+++ crates/swc_ecma_minifier/scripts/reduce-all.sh
@@ -5,8 +5,7 @@ dir="${1-"./inputs/"}"
 
 find $dir -type f -name '*.js' -print0 | xargs -0 -I {} sh -c '\
     echo "Processing {}"
-    ./scripts/reduce.sh {}'
-
+    ./scripts/reduce.sh {}' || true
 
 echo "Removing empty directories in $dir"
 
