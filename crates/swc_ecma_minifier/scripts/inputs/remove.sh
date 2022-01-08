fde5307b826d82d8c12729423428a6b85863fcd5
Donny <kdy1997.dev@gmail.com>
2022-01-08 14:39:46 +0900

    fixup

--- crates/swc_ecma_minifier/scripts/inputs/remove.sh
+++ crates/swc_ecma_minifier/scripts/inputs/remove.sh
@@ -2,6 +2,6 @@
 set -eu
 
 
-find ./inputs/ -type f -name "*.gif" -name "*.svg" -name "*.css" -name "*.png" -name "*.json" -print0 -delete
+find ./inputs/ -type f -name "*.gif" -name "*.svg" -name "*.css" -name "*.png" -name "*.json" -name "*.woff" -name "*.woff2" -name "*.ttf" -name "*.eot" -name "*.jpg" -name "*.jpeg" -print0 -delete
 
 find ./inputs -type d -empty -delete
