4962dfecd69a04b44716818eae7b5170c221043d
Donny <kdy1997.dev@gmail.com>
2022-01-13 17:21:16 +0900

    Harden a test

--- crates/swc_ecma_transforms_compat/tests/fixture/async-to-generator/issue-3135/2/exec.js
+++ crates/swc_ecma_transforms_compat/tests/fixture/async-to-generator/issue-3135/2/exec.js
@@ -1,6 +1,11 @@
+const expect = require('expect');
+
 const foo = async (x, y, ...z) => {
     return this;
 };
 
+const _this = (() => this)()
+
 console.log(foo.length)
-console.log(foo(1, 2, 3))
+
+expect(typeof foo(1, 2, 3)).toEqual('object')
