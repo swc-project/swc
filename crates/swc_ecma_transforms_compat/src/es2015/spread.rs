ee5931cf0042e3b7077a4077f4ab482bc3a9aa7b
Donny <kdy1997.dev@gmail.com>
2022-01-13 21:46:14 +0900

    More fix

--- crates/swc_ecma_transforms_compat/src/es2015/spread.rs
+++ crates/swc_ecma_transforms_compat/src/es2015/spread.rs
@@ -353,21 +353,12 @@ impl Spread {
                 .make_member(Ident::new(js_word!("concat"), DUMMY_SP))
                 .as_callee();
 
-            if buf[0].spread.is_none() {
-                return Expr::Call(CallExpr {
-                    span,
-                    callee,
-                    args: buf,
-                    type_args: Default::default(),
-                });
-            } else {
-                return Expr::Call(CallExpr {
-                    span,
-                    callee,
-                    args: buf,
-                    type_args: Default::default(),
-                });
-            }
+            return Expr::Call(CallExpr {
+                span,
+                callee,
+                args: buf,
+                type_args: Default::default(),
+            });
         }
 
         Expr::Call(CallExpr {
--- crates/swc_ecma_transforms_module/src/amd.rs
+++ crates/swc_ecma_transforms_module/src/amd.rs
@@ -331,7 +331,6 @@ where
                             let imported = export.src.clone().map(|src| {
                                 scope.import_to_export(&src, !export.specifiers.is_empty())
                             });
-                            drop(scope);
                             drop(scope_ref_mut);
 
                             stmts.reserve(export.specifiers.len());
@@ -393,7 +392,6 @@ where
                                             .or_insert(false);
                                     }
                                 }
-                                drop(scope);
                                 drop(scope_ref_mut);
                                 let value = match imported {
                                     Some(ref imported) => Box::new(
@@ -403,10 +401,7 @@ where
                                 };
 
                                 // True if we are exporting our own stuff.
-                                let is_value_ident = match *value {
-                                    Expr::Ident(..) => true,
-                                    _ => false,
-                                };
+                                let is_value_ident = matches!(*value, Expr::Ident(..));
 
                                 if is_value_ident {
                                     let exported_symbol = exported
