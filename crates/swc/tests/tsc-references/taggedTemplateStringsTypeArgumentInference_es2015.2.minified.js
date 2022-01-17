6b3db76e298c31e33d15fa5bdb15e80a04f767cb
Donny <kdy1997.dev@gmail.com>
2022-01-17 14:39:42 +0900

    Update test refs

--- crates/swc/tests/tsc-references/taggedTemplateStringsTypeArgumentInference_es2015.2.minified.js
+++ crates/swc/tests/tsc-references/taggedTemplateStringsTypeArgumentInference_es2015.2.minified.js
@@ -9,13 +9,13 @@ function someGenerics9(strs, a, b, c) {
 }
 (function(n) {})``, (function(n) {})``, (function(n, m) {})`${3}`, (function(n, m) {})`${3}`, (function(strs, n) {})`${(n)=>n
 }`, (function(strs, n) {})`${(n, x)=>n
-}`, someGenerics3`${()=>''
-}`, someGenerics3`${()=>undefined
+}`, someGenerics3`${()=>""
+}`, someGenerics3`${()=>void 0
 }`, someGenerics3`${()=>3
 }`, someGenerics4`${4}${()=>null
-}`, someGenerics4`${''}${()=>3
+}`, someGenerics4`${""}${()=>3
 }`, someGenerics4`${null}${null}`, someGenerics5`${4} ${()=>null
-}`, someGenerics5`${''}${()=>3
+}`, someGenerics5`${""}${()=>3
 }`, someGenerics5`${null}${null}`, someGenerics6`${(n)=>n
 }${(n)=>n
 }${(n)=>n
@@ -36,16 +36,16 @@ function someGenerics9(strs, a, b, c) {
 }${(n)=>n
 }`, (function(strs, n) {
     return n;
-})`${someGenerics7}``${null}${null}${null}`, someGenerics9`${''}${0}${[]}`, someGenerics9`${undefined}${{
+})`${someGenerics7}``${null}${null}${null}`, someGenerics9`${""}${0}${[]}`, someGenerics9`${void 0}${{
     x: 6,
     z: new Date()
 }}${{
     x: 6,
-    y: ''
+    y: ""
 }}`, someGenerics9`${{
     x: 3
 }}${{
     x: 6
 }}${{
     x: 6
-}}`, someGenerics9`${7}${anyVar}${4}`, someGenerics9`${[]}${null}${undefined}`;
+}}`, someGenerics9`${7}${anyVar}${4}`, someGenerics9`${[]}${null}${void 0}`;
