90c2baa2a85d36750461ba79370e2ff8d5d5b63a
Artur Aralin <artur.aralin97@gmail.com>
2022-01-18 17:37:05 +0300

    rework rule with SyntaxContext

--- crates/swc_ecma_lints/src/rules/no_console.rs
+++ crates/swc_ecma_lints/src/rules/no_console.rs
@@ -1,22 +1,28 @@
 use crate::rule::{visitor_rule, Rule};
-use swc_common::errors::HANDLER;
+use swc_common::{errors::HANDLER, SyntaxContext};
 use swc_ecma_ast::*;
 use swc_ecma_visit::{noop_visit_type, Visit};
 
 const MESSAGE: &'static str = "Unexpected console statement";
 
-pub fn no_console() -> Box<dyn Rule> {
-    visitor_rule(NoConsole::default())
+pub fn no_console(top_level_ctxt: SyntaxContext) -> Box<dyn Rule> {
+    visitor_rule(NoConsole::new(top_level_ctxt))
 }
 
 #[derive(Debug, Default)]
-struct NoConsole;
+struct NoConsole {
+    top_level_ctxt: SyntaxContext,
+}
 
 impl NoConsole {
+    fn new(top_level_ctxt: SyntaxContext) -> Self {
+        Self { top_level_ctxt }
+    }
+
     fn check(&mut self, id: &Ident) {
         if id.sym.eq("console") {
-            if id.span.ctxt.outer().as_u32() == 1 {
-                HANDLER.with(|handler| handler.struct_span_err(id.span, MESSAGE).emit());
+            if id.span.ctxt == self.top_level_ctxt {
+                HANDLER.with(|handler| handler.struct_span_warn(id.span, MESSAGE).emit());
             }
         }
     }
