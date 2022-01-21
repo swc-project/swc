bcc569fcb1c40b3efb6ad42a03cec1a69440d39e
evilebottnawi <sheo13666q@gmail.com>
2022-01-21 18:52:13 +0300

    refactor: move simple block to style_rule

--- crates/swc_css_ast/src/style_rule.rs
+++ crates/swc_css_ast/src/style_rule.rs
@@ -1,6 +1,15 @@
 use crate::{AtRule, Ident, SelectorList, Tokens, Value};
 use swc_common::{ast_node, Span};
 
+#[ast_node("SimpleBlock")]
+pub struct SimpleBlock {
+    pub span: Span,
+    // TODO Create a simple block with its associated token set to the current input token and with
+    // its value initially set to an empty list.
+    pub name: char,
+    pub value: Vec<Value>,
+}
+
 #[ast_node("QualifiedRule")]
 pub struct QualifiedRule {
     pub span: Span,
@@ -31,11 +40,4 @@ pub struct Declaration {
     pub value: Vec<Value>,
     /// The span includes `!`
     pub important: Option<Span>,
-}
-
-#[ast_node("SimpleBlock")]
-pub struct SimpleBlock {
-    pub span: Span,
-    pub name: char,
-    pub value: Vec<Value>,
 }
