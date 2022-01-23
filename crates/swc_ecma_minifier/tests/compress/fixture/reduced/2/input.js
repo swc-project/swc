286d05cd55adc4eedd60fd1bf9ab2d3db75e4fd7
Donny <kdy1997.dev@gmail.com>
2022-01-23 22:53:33 +0900

    Add a reduced test

---
+++ crates/swc_ecma_minifier/tests/compress/fixture/reduced/2/input.js
@@ -0,0 +1,14 @@
+export function validateSchemaDeps(cxt, schemaDeps = cxt.schema) {
+    const { gen, data, keyword, it } = cxt;
+    const valid = gen.name("valid");
+    for (const prop in schemaDeps) {
+        if ((0, util_1.alwaysValidSchema)(it, schemaDeps[prop]))
+            continue;
+        gen.if((0, code_1.propertyInData)(gen, data, prop, it.opts.ownProperties), () => {
+            const schCxt = cxt.subschema({ keyword, schemaProp: prop }, valid);
+            cxt.mergeValidEvaluated(schCxt, valid);
+        }, () => gen.var(valid, true) // TODO var
+        );
+        cxt.ok(valid);
+    }
+}
