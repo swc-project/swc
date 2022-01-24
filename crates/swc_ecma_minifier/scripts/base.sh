6d5bb2e8d2f970c6d70efbd8576916026daa4d7a
Donny <kdy1997.dev@gmail.com>
2022-01-24 18:00:32 +0900

    Make it faster

--- crates/swc_ecma_minifier/scripts/base.sh
+++ crates/swc_ecma_minifier/scripts/base.sh
@@ -4,4 +4,4 @@ set -eux
 export RUST_LOG=swc_ecma_minifier=trace
 
 # To prevent regression, we run base test before real tests.
-cargo test --test compress -q --all-features ${1-base_exec}
+cargo test --test compress -q ${1-base_exec}
