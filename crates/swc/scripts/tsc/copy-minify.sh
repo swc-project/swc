c9ce5378671593cc95ef344cbc547f546c0072f6
Donny <kdy1997.dev@gmail.com>
2022-01-25 20:42:03 +0900

    This is useless

--- crates/swc/scripts/tsc/copy-minify.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-git diff --name-only | xargs -L 1 -I {} $SCRIPT_DIR/copy-minify-file.sh {} \;
