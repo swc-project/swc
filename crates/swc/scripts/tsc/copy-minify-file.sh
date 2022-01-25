dbce2fcff1df806b52518370aee92927416af754
Donny <kdy1997.dev@gmail.com>
2022-01-25 19:19:22 +0900

    Copy

--- crates/swc/scripts/tsc/copy-minify-file.sh
+++ crates/swc/scripts/tsc/copy-minify-file.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-echo $1
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+name="$(basename $1)"
+
+echo "Copying $name"
+
+
+normal_file_path="tests/tsc-references/$(sed 's/2\.minified/1\.normal/' <<< $name)"
+echo "Normal file: $normal_file_path"
+
+cp $normal_file_path "$SCRIPT_DIR/../swc_ecma_minifier/tests/compress/fixture/tsc-spec/$name/input.js"
+
+echo
+
