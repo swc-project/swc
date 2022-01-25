9f7017e5efa3edc249aee6180373f779dd5f39ac
Donny <kdy1997.dev@gmail.com>
2022-01-25 13:30:24 +0900

    Add a test for #3347

---
+++ crates/swc/tests/vercel/loader-only/issue-3347/1/input/index.js
@@ -0,0 +1,18 @@
+const myFunction = () => {
+    for (let j = 0; j <= 3; j++) {
+        for (const _ of []) {
+        }
+
+        if (j === 1) {
+            console.log("before set timeout, j is:", j)
+
+            setTimeout(() => {
+                console.log("in timeout: j is", j)
+            }, 50)
+        }
+    }
+
+    return null
+}
+
+myFunction()
