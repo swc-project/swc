e3b4ed8ca6f02e5a57552f6dde584736632cfbd4
Donny <kdy1997.dev@gmail.com>
2022-01-26 20:30:35 +0900

    Update test refs

--- crates/swc/tests/tsc-references/exportAsNamespace3_es5.2.minified.js
+++ crates/swc/tests/tsc-references/exportAsNamespace3_es5.2.minified.js
@@ -3,4 +3,9 @@ import * as foo from "./1";
 export var a = 1;
 export var b = 2;
 export { _ns as ns };
+ns.a, ns.b;
+var ns = {
+    a: 1,
+    b: 2
+};
 foo.ns.a, foo.ns.b;
--- crates/swc/tests/tsc-references/exportsAndImportsWithUnderscores3_es5.2.minified.js
+++ crates/swc/tests/tsc-references/exportsAndImportsWithUnderscores3_es5.2.minified.js
@@ -1,5 +1,7 @@
-export default {
+var R;
+export default R = {
     "___": 30,
     "___hello": 21,
     "_hi": 40
 };
+R.___, R.___hello, R._hi;
