17dfb9e13ce4abed29145dd7a18adedf96ded76a
Donny <kdy1997.dev@gmail.com>
2022-01-26 21:09:43 +0900

    Update test refs

--- crates/swc/tests/tsc-references/protectedStaticClassPropertyAccessibleWithinSubclass_es2015.2.minified.js
+++ crates/swc/tests/tsc-references/protectedStaticClassPropertyAccessibleWithinSubclass_es2015.2.minified.js
@@ -1,12 +1,21 @@
 class Base {
-    static staticMethod() {}
+    static staticMethod() {
+        Base.x, Derived1.x, Derived2.x, Derived3.x;
+    }
 }
 class Derived1 extends Base {
-    static staticMethod1() {}
+    static staticMethod1() {
+        Base.x, Derived1.x, Derived2.x, Derived3.x;
+    }
 }
 class Derived2 extends Base {
-    static staticMethod2() {}
+    static staticMethod2() {
+        Base.x, Derived1.x, Derived2.x, Derived3.x;
+    }
 }
 class Derived3 extends Derived1 {
-    static staticMethod3() {}
+    static staticMethod3() {
+        Base.x, Derived1.x, Derived2.x, Derived3.x;
+    }
 }
+Base.x, Derived1.x, Derived2.x, Derived3.x;
