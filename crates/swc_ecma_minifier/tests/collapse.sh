bdb4219c1118d81c7e4e1026d9211ce1d547be6b
Donny <kdy1997.dev@gmail.com>
2022-01-27 16:01:54 +0900

    Collapse

---
+++ crates/swc_ecma_minifier/tests/collapse.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+
+find ./tests/exec -name 'input.js' | while read -r input; do
+  dir="$(dirname $input)"
+  conf_json="$(dirname $input)/config.json"
+  test_name="${dir/\.\//}";
+  test_name="${test_name//\//_}";
+  test_name="${test_name//\-/_}";
+  echo "
+#[test]
+fn $test_name() {
+    let src = r###\"$(cat $input)\"###;
+    let config = r###\"$(cat $conf_json)\"###;
+
+
+    run_exec_test(src, config);
+}"
+done
