0995afa5879412a2fb2ad874d4bb5313c0caa2c5
Donny <kdy1997.dev@gmail.com>
2022-01-28 14:17:05 +0900

    lint

--- crates/swc_ecma_minifier/src/compress/optimize/unused.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/unused.rs
@@ -7,7 +7,6 @@ use swc_atoms::js_word;
 use swc_common::{util::take::Take, Span, DUMMY_SP};
 use swc_ecma_ast::*;
 use swc_ecma_utils::{contains_ident_ref, ident::IdentLike};
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 
 /// Methods related to the option `unused`.
 impl<M> Optimizer<'_, M>
