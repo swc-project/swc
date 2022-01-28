365b29ec7e60e14eccf0d13bbf8be13f0eaa56fa
Donny <kdy1997.dev@gmail.com>
2022-01-28 18:25:16 +0900

    Remove scripts

--- crates/swc_ecma_parser/flatten.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# Remove empty directories
-find . -type d -empty -delete
-
-find ./tests/tsc -type f \( -iname 'input.ts.json' -o -iname 'input.tsx.json' \) -delete
-
-find ./tests/tsc -type f \( -iname 'input.ts' -o -iname 'input.tsx' \) | xargs -L 1 ./flatten-one-tsc-test.sh
