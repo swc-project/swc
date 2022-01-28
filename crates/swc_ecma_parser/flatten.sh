9b472b58815c73ed4b945dfd8d4d2e5e2cbd85e1
Donny <kdy1997.dev@gmail.com>
2022-01-28 17:57:00 +0900

    flatten

--- crates/swc_ecma_parser/flatten.sh
+++ crates/swc_ecma_parser/flatten.sh
@@ -4,4 +4,6 @@ set -eu
 # Remove empty directories
 find . -type d -empty -delete
 
+find ./tests/tsc -type f \( -iname 'input.ts.json' -o -iname 'input.tsx.json' \) -delete
+
 find ./tests/tsc -type f \( -iname 'input.ts' -o -iname 'input.tsx' \) | xargs -L 1 ./flatten-one-tsc-test.sh
