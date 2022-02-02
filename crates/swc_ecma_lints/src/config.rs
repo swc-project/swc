fed0a4734989aaa4e49f099b56c0f36fc7484875
Artur Aralin <artur.aralin97@gmail.com>
2022-02-02 12:13:20 +0300

    feat(es/lint): fix feature gate issues

--- crates/swc_ecma_lints/src/config.rs
+++ crates/swc_ecma_lints/src/config.rs
@@ -1,61 +1,51 @@
 #[cfg(feature = "non_critical_lints")]
-#[path = ""]
-mod non_critical_lints {
-    use crate::rules::non_critical_lints::no_console::NoConsoleConfig;
-    use serde::{Deserialize, Serialize};
-    use std::fmt::Debug;
+use crate::rules::non_critical_lints::no_console::NoConsoleConfig;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
 
-    #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-    #[serde(rename_all = "lowercase")]
-    pub enum LintRuleReaction {
-        Off,
-        Warning,
-        Error,
-    }
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum LintRuleReaction {
+    Off,
+    Warning,
+    Error,
+}
 
-    impl Default for LintRuleReaction {
-        fn default() -> Self {
-            Self::Off
-        }
-    }
-
-    #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-    pub struct RuleConfig<T: Debug + Clone + Serialize + Default>(
-        #[serde(default)] LintRuleReaction,
-        #[serde(default)] T,
-    );
-
-    impl<T: Debug + Clone + Serialize + Default> RuleConfig<T> {
-        pub(crate) fn get_rule_reaction(&self) -> &LintRuleReaction {
-            &self.0
-        }
-
-        pub(crate) fn get_rule_config(&self) -> &T {
-            &self.1
-        }
-    }
-
-    #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-    #[non_exhaustive]
-    #[serde(rename_all = "camelCase")]
-    pub struct LintConfig {
-        #[serde(default)]
-        pub no_console: RuleConfig<NoConsoleConfig>,
-
-        #[serde(default)]
-        pub no_alert: RuleConfig<()>,
-
-        #[serde(default)]
-        pub no_debugger: RuleConfig<()>,
+impl Default for LintRuleReaction {
+    fn default() -> Self {
+        Self::Off
     }
 }
 
-#[cfg(not(feature = "non_critical_lints"))]
-mod non_critical_lints {
-    use serde::{Deserialize, Serialize};
-    use std::fmt::Debug;
-    #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-    pub struct LintConfig {}
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct RuleConfig<T: Debug + Clone + Serialize + Default>(
+    #[serde(default)] LintRuleReaction,
+    #[serde(default)] T,
+);
+
+impl<T: Debug + Clone + Serialize + Default> RuleConfig<T> {
+    pub(crate) fn get_rule_reaction(&self) -> &LintRuleReaction {
+        &self.0
+    }
+
+    pub(crate) fn get_rule_config(&self) -> &T {
+        &self.1
+    }
 }
 
-pub use non_critical_lints::*;
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[non_exhaustive]
+#[serde(rename_all = "camelCase")]
+pub struct LintConfig {
+    #[cfg(feature = "non_critical_lints")]
+    #[serde(default)]
+    pub no_console: RuleConfig<NoConsoleConfig>,
+
+    #[cfg(feature = "non_critical_lints")]
+    #[serde(default)]
+    pub no_alert: RuleConfig<()>,
+
+    #[cfg(feature = "non_critical_lints")]
+    #[serde(default)]
+    pub no_debugger: RuleConfig<()>,
+}
