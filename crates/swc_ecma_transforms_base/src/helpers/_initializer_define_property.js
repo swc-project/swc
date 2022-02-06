46d4cd8566c104575323418ef160e76863ff79d6
William Tetlow <william@getshogun.com>
2022-02-06 13:28:58 +0000

    better replicate original fix for 2127

--- crates/swc_ecma_transforms_base/src/helpers/_initializer_define_property.js
+++ crates/swc_ecma_transforms_base/src/helpers/_initializer_define_property.js
@@ -1,15 +1,24 @@
 function _initializerDefineProperty(target, property, descriptor, context) {
   if (!descriptor) return;
-  var currentVal = target[property];
+
+  var own =
+    Object.getOwnPropertyDescriptor(target, property) ||
+    Object.getOwnPropertyDescriptor(
+      Object.getPrototypeOf(target),
+      property
+    );
+
+  // prevent overriding obj accessors
+  if (own && (own.get || own.set)) return;
 
   Object.defineProperty(target, property, {
     enumerable: descriptor.enumerable,
     configurable: descriptor.configurable,
     writable: descriptor.writable,
     value: descriptor.initializer
-        ? descriptor.initializer.call(context)
-        : currentVal !== void 0
-        ? currentVal
-        : void 0,
+      ? descriptor.initializer.call(context)
+      : own && own.value !== void 0
+      ? own.value
+      : void 0,
   });
 }
