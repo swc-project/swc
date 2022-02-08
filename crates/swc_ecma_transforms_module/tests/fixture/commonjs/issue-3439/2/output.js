7c1877b5aa447d4084adc82ad3f6edabac950bf4
magic-akari <akari.ccino@gmail.com>
2022-02-08 15:33:26 +0800

    chore: add test cases

---
+++ crates/swc_ecma_transforms_module/tests/fixture/commonjs/issue-3439/2/output.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = exports.bar2 = exports.bar = exports.foo = exports.foo1 = void 0;
+const foo = 1;
+exports.foo1 = foo;
+exports.foo = foo;
+let bar = 2;
+exports.bar = bar;
+exports.bar2 = bar;
+var _default = bar;
+exports.default = _default;
+exports.bar2 = exports.bar = bar = 3;
