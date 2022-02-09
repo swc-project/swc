edc46ccf939f8fd3ac49300085daf3b42d4ce841
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-09 20:35:36 +0900

    chore(crates): Use bot account for commiitter

--- scripts/cargo/bump.sh
+++ scripts/cargo/bump.sh
@@ -11,6 +11,9 @@ cargo metadata --offline --format-version 1 > /dev/null
 # Ensure that dependencies are all verified
 ./scripts/crev/verify.sh
 
+export GIT_COMMITTER_NAME="SWC Bot"
+export GIT_COMMITTER_EMAIL="bot@swc.rs"
+
 git add -A
 git commit --author="SWC Bot <bot@swc.rs>" -m 'chore: Publish crates'
 git push --no-verify
