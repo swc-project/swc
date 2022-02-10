4063b88ce26b23430802d2a809aa3fb5db6ccf6c
evilebottnawi <sheo13666q@gmail.com>
2022-02-10 20:13:23 +0300

    feat: minifier

--- crates/swc_css_minifier/src/lib.rs
+++ crates/swc_css_minifier/src/lib.rs
@@ -1,24 +1,73 @@
 use swc_css_ast::*;
 use swc_css_visit::{VisitMut, VisitMutWith};
 
-pub fn minify(ss: &mut Stylesheet) {
-    ss.visit_mut_with(&mut minifier());
+pub fn minify(stylesheet: &mut Stylesheet) {
+    stylesheet.visit_mut_with(&mut compress_empty_qualified_rule());
+    stylesheet.visit_mut_with(&mut compress_time());
 }
 
-fn minifier() -> impl VisitMut {
-    Minifier {}
+fn compress_empty_qualified_rule() -> impl VisitMut {
+    CompressEmptyQualifiedRule {}
 }
 
-struct Minifier {}
+struct CompressEmptyQualifiedRule {}
 
-impl VisitMut for Minifier {
-    fn visit_mut_tokens(&mut self, tokens: &mut Tokens) {
-        for tok in tokens.tokens.iter_mut() {
-            if let Token::WhiteSpace { value, .. } = &mut tok.token {
-                if value.len() >= 2 {
-                    *value = " ".into()
+impl VisitMut for CompressEmptyQualifiedRule {
+    fn visit_mut_stylesheet(&mut self, stylesheet: &mut Stylesheet) {
+        stylesheet.visit_mut_children_with(self);
+
+        stylesheet.rules.retain(|rule| {
+            if let Rule::QualifiedRule(QualifiedRule { block, .. }) = rule {
+                if block.value.len() == 0 {
+                    return false;
                 }
             }
+
+            true
+        });
+    }
+}
+
+fn compress_time() -> impl VisitMut {
+    CompressTime {}
+}
+
+struct CompressTime {}
+
+impl VisitMut for CompressTime {
+    fn visit_mut_time(&mut self, time: &mut Time) {
+        time.visit_mut_children_with(self);
+
+        match &*time.unit.value {
+            "ms" if time.value.value == 0.0 || time.value.value >= 100.0 => {
+                let new_value = time.value.value / 1000.0;
+
+                time.value = Number {
+                    span: time.value.span,
+                    value: new_value,
+                    raw: new_value.to_string().into(),
+                };
+                time.unit = Ident {
+                    span: time.unit.span,
+                    value: "s".into(),
+                    raw: "s".into(),
+                };
+            }
+            "s" if time.value.value > 0.0 && time.value.value < 0.1 => {
+                let new_value = time.value.value * 1000.0;
+
+                time.value = Number {
+                    value: new_value,
+                    raw: new_value.to_string().into(),
+                    span: time.span,
+                };
+                time.unit = Ident {
+                    span: time.unit.span,
+                    value: "ms".into(),
+                    raw: "ms".into(),
+                };
+            }
+            _ => {}
         }
     }
 }
