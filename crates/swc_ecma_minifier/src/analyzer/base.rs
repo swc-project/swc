5d44d9aef52a306c9aedc11b82a008b71616d3d7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-11 19:42:39 +0900

    Improve detection of alias

--- crates/swc_ecma_minifier/src/analyzer/base.rs
+++ crates/swc_ecma_minifier/src/analyzer/base.rs
@@ -5,12 +5,10 @@
 //!
 //!  - Infection analysis
 
-use swc_common::collections::AHashMap;
+use swc_common::collections::{AHashMap, AHashSet};
 use swc_ecma_ast::*;
 use swc_ecma_utils::ident::IdentLike;
-use swc_ecma_visit::{Visit, VisitWith};
-
-use crate::util::idents_used_by_ignoring_nested;
+use swc_ecma_visit::{noop_visit_type, visit_obj_and_computed, Visit, VisitWith};
 
 #[derive(Debug, Default)]
 pub(crate) struct BaseData {
@@ -27,7 +25,11 @@ impl Visit for BaseAnalyzer<'_> {
 
         for decl in &n.decls {
             if let (Pat::Ident(var), Some(init)) = (&decl.name, decl.init.as_deref()) {
-                let used_idents = idents_used_by_ignoring_nested(init);
+                let used_idents = {
+                    let mut v = AliasCollector::default();
+                    init.visit_with(&mut v);
+                    v.ids
+                };
 
                 for id in used_idents {
                     {
@@ -45,5 +47,60 @@ impl Visit for BaseAnalyzer<'_> {
                 }
             }
         }
+    }
+}
+
+#[derive(Default)]
+struct AliasCollector {
+    ids: AHashSet<Id>,
+}
+
+impl Visit for AliasCollector {
+    noop_visit_type!();
+
+    visit_obj_and_computed!();
+
+    fn visit_bin_expr(&mut self, n: &BinExpr) {
+        if matches!(
+            n.op,
+            op!(bin, "+")
+                | op!(bin, "-")
+                | op!("*")
+                | op!("/")
+                | op!("%")
+                | op!("**")
+                | op!("<<")
+                | op!(">>")
+                | op!(">>>")
+        ) {
+            return;
+        }
+
+        n.visit_children_with(self);
+    }
+
+    fn visit_block_stmt_or_expr(&mut self, _: &BlockStmtOrExpr) {}
+
+    fn visit_constructor(&mut self, _: &Constructor) {}
+
+    fn visit_function(&mut self, _: &Function) {}
+
+    fn visit_ident(&mut self, n: &Ident) {
+        self.ids.insert(n.to_id());
+    }
+
+    fn visit_prop_name(&mut self, n: &PropName) {
+        if let PropName::Computed(..) = n {
+            n.visit_children_with(self);
+        }
+    }
+
+    fn visit_unary_expr(&mut self, n: &UnaryExpr) {
+        // Modifying argument does not affect original value
+        if matches!(n.op, op!("typeof") | op!(unary, "+") | op!(unary, "-")) {
+            return;
+        }
+
+        n.visit_children_with(self);
     }
 }
