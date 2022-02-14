817717e8a446c27a2619ec75d84ad8c24dd337a9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-14 20:21:34 +0900

    matrix

--- scripts/github/create-matrix.sh
+++ scripts/github/create-matrix.sh
@@ -9,7 +9,25 @@ $SCRIPT_DIR/sort.sh $SCRIPT_DIR/list-windows.txt
 
 function prepend() { while read line; do echo "${1}${line}"; done; }
 
-cargo metadata --format-version 1 \
- | jq -r '.workspace_members[]' \
- | cut -f1 -d" " \
- | sort
+crates=$(\
+    cargo metadata --format-version 1 \
+        | jq -r '.workspace_members[]' \
+        | cut -f1 -d" " \
+        | sort \
+)
+
+for crate in $crates
+do
+    echo "- crate: $crate"
+    echo "  os: ubuntu-latest"
+
+    if grep -q "^$crate\$" "$SCRIPT_DIR/list-macosx.txt"; then
+        echo "- crate: $crate"
+        echo "  os: macosx-latest"
+    fi
+
+    if grep -q "^$crate\$" "$SCRIPT_DIR/list-windows.txt"; then
+        echo "- crate: $crate"
+        echo "  os: windows-latest"
+    fi
+done
