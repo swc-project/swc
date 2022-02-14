51a1d4ce60ce33fa696db1613fccad1d58d6dd16
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-14 19:47:51 +0900

    Done?

--- scripts/bot/src/auto-rebase.ts
+++ scripts/bot/src/auto-rebase.ts
@@ -6,20 +6,42 @@ import { octokit } from "./util/octokit";
 // - test(*)
 // - chore:
 
+const owner = 'swc-project';
+const repo = 'swc';
 
 (async () => {
     const latestCommitMessage = await getLatestCommitMesssage();
 
     console.log(`Latest commit message: ${latestCommitMessage}`);
 
-    if (!latestCommitMessage.startsWith("test(") && !latestCommitMessage.startsWith("chore:")) {
-        console.log(`Auto rebase script cannot work because the latest commit may require a version bump`);
+    // TODO: Enable this before merging.
+    // if (!latestCommitMessage.startsWith("test(") && !latestCommitMessage.startsWith("chore:")) {
+    //     console.log(`Auto rebase script cannot work because the latest commit may require a version bump`);
+    //     return;
+    // }
+
+    const allPrs = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        state: 'open',
+        sort: 'long-running'
+    });
+
+    const autoMergePrs = allPrs.data.filter(pr => !!pr.auto_merge);
+
+    if (autoMergePrs.length === 0) {
+        console.log(`No PRs with auto-merge enabled`);
         return;
     }
 
-    // octokit.rest.repos.mergeUpstream({
-    //     owner,
-    //     repo,
-    //     branch,
-    // });
+    const pr = autoMergePrs[0];
+
+    await octokit.rest.repos.mergeUpstream({
+        owner: pr.head.repo.owner.login,
+        repo: pr.head.repo.name,
+        branch: pr.head.ref,
+    });
+
+    console.log(`Updated PR ${pr.number} to merge upstream`);
+
 })()
