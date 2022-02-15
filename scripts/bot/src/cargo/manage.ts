4fee96ff35a186e46c263c205b73c8fec04c3e43
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-15 18:35:58 +0900

    feat(bot): Actions (#3573)

--- scripts/bot/src/cargo/manage.ts
+++ scripts/bot/src/cargo/manage.ts
@@ -6,18 +6,20 @@ import { parsePrComments } from "./comment-parser";
     const latestCommitMessage = await getLatestCommitMesssage();
     console.log('Latest commit message:', latestCommitMessage);
 
-    const lParenIndex = latestCommitMessage.lastIndexOf('(');
+    const lParenIndex = latestCommitMessage.lastIndexOf('(#');
+
+    console.log(lParenIndex)
 
     if (!latestCommitMessage.endsWith(')') || lParenIndex === -1) {
         console.log(`This commit does not seems like a PR merge`)
         return;
     }
 
-    const prNumber = parseInt(latestCommitMessage.substring(lParenIndex + 1, latestCommitMessage.length - 1));
+    const prNumber = parseInt(latestCommitMessage.substring(lParenIndex + 2, latestCommitMessage.length - 1));
 
     const actions = await parsePrComments(prNumber);
 
-    if (actions.length === 0) {
-        throw new Error('PR does not have a review comment to parse');
+    for (const action of actions) {
+        console.log(action);
     }
 })()
