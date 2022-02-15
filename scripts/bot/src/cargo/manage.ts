195b02a09d7dc7e3c68e60255677e974dd3f5c74
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-15 18:33:57 +0900

    feat(bot): Test (#3573)

--- scripts/bot/src/cargo/manage.ts
+++ scripts/bot/src/cargo/manage.ts
@@ -1,2 +1,23 @@
-import { octokit } from "../util/octokit";
+import { getLatestCommitMesssage } from "../util/git";
+import { parsePrComments } from "./comment-parser";
 
+
+(async () => {
+    const latestCommitMessage = await getLatestCommitMesssage();
+    console.log('Latest commit message:', latestCommitMessage);
+
+    const lParenIndex = latestCommitMessage.lastIndexOf('(');
+
+    if (!latestCommitMessage.endsWith(')') || lParenIndex === -1) {
+        console.log(`This commit does not seems like a PR merge`)
+        return;
+    }
+
+    const prNumber = parseInt(latestCommitMessage.substring(lParenIndex + 1, latestCommitMessage.length - 1));
+
+    const actions = await parsePrComments(prNumber);
+
+    if (actions.length === 0) {
+        throw new Error('PR does not have a review comment to parse');
+    }
+})()
