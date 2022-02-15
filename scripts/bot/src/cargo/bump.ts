a1e45c4b863cd9119fd8ca8cd24439ec1bff1bfc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-15 19:35:59 +0900

    feat(bot): Add auto-bump script (#3573)

---
+++ scripts/bot/src/cargo/bump.ts
@@ -0,0 +1,32 @@
+import { exec } from "child_process";
+import { getLatestCommitMesssage } from "../util/git";
+import { parsePrComments } from "./comment-parser";
+
+
+(async () => {
+    const latestCommitMessage = await getLatestCommitMesssage();
+    console.log('Latest commit message:', latestCommitMessage);
+
+    const lParenIndex = latestCommitMessage.lastIndexOf('(#');
+
+    console.log(lParenIndex)
+
+    if (!latestCommitMessage.endsWith(')') || lParenIndex === -1) {
+        console.log(`This commit does not seems like a PR merge`)
+        return;
+    }
+
+    const prNumber = parseInt(latestCommitMessage.substring(lParenIndex + 2, latestCommitMessage.length - 1));
+
+    const actions = await parsePrComments(prNumber);
+
+    for (const action of actions) {
+        console.log(action);
+
+        if (action.breaking) {
+            await exec(`cargo mono bump ${action.crate} --breaking`)
+        } else {
+            await exec(`cargo mono bump ${action.crate}`)
+        }
+    }
+})()
