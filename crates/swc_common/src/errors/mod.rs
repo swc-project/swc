4f37eb15b1f2299430476371930f9477c968062d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-17 16:03:29 +0900

    Use it

--- crates/swc_common/src/errors/mod.rs
+++ crates/swc_common/src/errors/mod.rs
@@ -17,7 +17,6 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering::SeqCst},
 };
 
-use scoped_tls::scoped_thread_local;
 #[cfg(feature = "tty-emitter")]
 use termcolor::{Color, ColorSpec};
 
@@ -899,7 +898,7 @@ impl Level {
     }
 }
 
-scoped_thread_local!(
+better_scoped_tls::scoped_tls_with_good_error!(
     /// Used for error reporting in transform.
     ///
     /// This should be only used for errors from the api which does not returning errors.
