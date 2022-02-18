8e06b2a42c7ba349ddbf957a6e62916c82f94b1a
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-02-18 07:55:24 +0300

    feat(css/minifier): Implement the rule for easing functions (#3615)

--- crates/swc_css_minifier/src/lib.rs
+++ crates/swc_css_minifier/src/lib.rs
@@ -2,8 +2,8 @@ use swc_css_ast::*;
 use swc_css_visit::VisitMutWith;
 
 use self::compress::{
-    angle::compress_angle, empty::compress_empty, keyframes::compress_keyframes,
-    time::compress_time,
+    angle::compress_angle, easing_function::compress_easing_function, empty::compress_empty,
+    keyframes::compress_keyframes, time::compress_time,
 };
 
 mod compress;
@@ -11,6 +11,7 @@ mod compress;
 pub fn minify(stylesheet: &mut Stylesheet) {
     stylesheet.visit_mut_with(&mut compress_empty());
     stylesheet.visit_mut_with(&mut compress_keyframes());
+    stylesheet.visit_mut_with(&mut compress_easing_function());
     stylesheet.visit_mut_with(&mut compress_time());
     stylesheet.visit_mut_with(&mut compress_angle());
 }
