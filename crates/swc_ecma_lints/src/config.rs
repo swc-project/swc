631158b812dd072a93aa463179692a6053e819a1
Artur Aralin <artur.aralin97@gmail.com>
2022-02-19 14:38:39 +0300

    feat(es/lint): resolve conflicts

--- crates/swc_ecma_lints/src/config.rs
+++ crates/swc_ecma_lints/src/config.rs
@@ -5,8 +5,8 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "non_critical_lints")]
 use crate::rules::non_critical_lints::{
     dot_notation::DotNotationConfig, eqeqeq::EqeqeqConfig, no_console::NoConsoleConfig,
-    no_empty_function::NoEmptyFunctionConfig, no_use_before_define::NoUseBeforeDefineConfig,
-    no_restricted_syntax::NoRestrictedSyntaxConfig, no_use_before_define::NoUseBeforeDefineConfig,
+    no_empty_function::NoEmptyFunctionConfig, no_restricted_syntax::NoRestrictedSyntaxConfig,
+    no_use_before_define::NoUseBeforeDefineConfig,
     prefer_regex_literals::PreferRegexLiteralsConfig, quotes::QuotesConfig,
 };
 
