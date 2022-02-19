63ccc034e3369f6c05d0b8fa36bc02c6b310dcb8
Artur Aralin <artur.aralin97@gmail.com>
2022-02-19 15:41:17 +0300

    feat(es/lint): add tests

---
+++ crates/swc/tests/errors/lints/no-bitwise/default/input.js
@@ -0,0 +1,33 @@
+var x = y | z;
+
+var x = y & z;
+
+var x = y ^ z;
+
+var x = ~ z;
+
+var x = y << z;
+
+var x = y >> z;
+
+var x = y >>> z;
+
+x |= y;
+
+x &= y;
+
+x ^= y;
+
+x <<= y;
+
+x >>= y;
+
+x >>>= y;
+
+if (a ^ b) {}
+
+var a = x ^ y;
+
+var {
+  a = x ^ y
+} = obj;
