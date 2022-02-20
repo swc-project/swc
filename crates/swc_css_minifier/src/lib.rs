6424821f22f31dd830fe7c7743d64188991ce450
evilebottnawi <sheo13666q@gmail.com>
2022-02-20 17:19:13 +0300

    refactor: rebase

--- crates/swc_css_minifier/src/lib.rs
+++ crates/swc_css_minifier/src/lib.rs
@@ -2,11 +2,9 @@ use swc_css_ast::*;
 use swc_css_visit::VisitMutWith;
 
 use self::compress::{
-    angle::compress_angle, easing_function::compress_easing_function, empty::compress_empty,
-    keyframes::compress_keyframes, selector::compress_selector, time::compress_time,
     angle::compress_angle, declaration::compress_declaration,
     easing_function::compress_easing_function, empty::compress_empty,
-    keyframes::compress_keyframes, time::compress_time,
+    keyframes::compress_keyframes, selector::compress_selector, time::compress_time,
 };
 
 mod compress;
