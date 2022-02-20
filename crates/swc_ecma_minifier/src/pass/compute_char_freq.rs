b506a13f7dc095877360a10446c576390bb64dc3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-20 18:16:31 +0900

    Merge code

--- crates/swc_ecma_minifier/src/pass/compute_char_freq.rs
+++ crates/swc_ecma_minifier/src/pass/compute_char_freq.rs
@@ -4,6 +4,8 @@ use swc_common::collections::AHashMap;
 use swc_ecma_ast::*;
 use swc_ecma_visit::{noop_visit_type, Visit, VisitWith};
 
+use crate::util::base54::{incr_base54, BASE54_DEFAULT_CHARS};
+
 /// TODO: Use [u8; 64] instead
 type CharFreq = AHashMap<u8, f32>;
 
@@ -14,12 +16,10 @@ pub(crate) struct CharFreqInfo {
     chars: [u8; 64],
 }
 
-static CHARS: &[u8; 64] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$_0123456789";
-
 pub(crate) fn compute_char_freq(m: &Module) -> CharFreqInfo {
     let mut base54 = CharFreqInfo {
         frequency: AHashMap::with_capacity_and_hasher(64, Default::default()),
-        chars: *CHARS,
+        chars: *BASE54_DEFAULT_CHARS,
     };
 
     m.visit_with(&mut base54);
@@ -33,7 +33,7 @@ impl CharFreqInfo {
         for c in s.chars().rev().filter_map(|c| {
             if c.len_utf8() == 1 {
                 let b = c as u8;
-                if CHARS.contains(&b) {
+                if BASE54_DEFAULT_CHARS.contains(&b) {
                     return Some(b);
                 }
 
@@ -61,38 +61,7 @@ impl CharFreqInfo {
 
     /// returns `(used_value, identifier_symbol)`
     pub fn incr_base54(&self, init: &mut usize) -> (usize, String) {
-        let mut n = *init;
-
-        *init += 1;
-
-        let mut ret = String::new();
-        let mut base = 54;
-
-        n += 1;
-
-        while n > 0 {
-            n -= 1;
-
-            let c = self.chars[n % base] as char;
-
-            if ret.is_empty() && c.is_digit(10) {
-                return self.incr_base54(init);
-            }
-
-            ret.push(c);
-
-            n /= base;
-            base = 64;
-        }
-
-        if ret.is_reserved()
-            || ret.is_reserved_in_strict_bind()
-            || ret.is_reserved_in_strict_mode(true)
-        {
-            return self.incr_base54(init);
-        }
-
-        (*init - 1, ret)
+        incr_base54(init, &self.chars)
     }
 }
 
