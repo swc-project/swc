61e711c912bc31453a24dce2d1e1d98044aab3df
Pig Fang <g-plane@hotmail.com>
2022-02-20 23:51:31 +0800

    feat(es/ast): Support TypeScript Instantiation Expression (#3642)

--- crates/swc_ecma_parser/src/parser/util.rs
+++ crates/swc_ecma_parser/src/parser/util.rs
@@ -297,7 +297,8 @@ pub(super) trait ExprExt {
             // typescript
             Expr::TsNonNull(TsNonNullExpr { ref expr, .. })
             | Expr::TsTypeAssertion(TsTypeAssertion { ref expr, .. })
-            | Expr::TsAs(TsAsExpr { ref expr, .. }) => {
+            | Expr::TsAs(TsAsExpr { ref expr, .. })
+            | Expr::TsInstantiation(TsExprWithTypeArgs { ref expr, .. }) => {
                 expr.is_valid_simple_assignment_target(strict)
             }
 
