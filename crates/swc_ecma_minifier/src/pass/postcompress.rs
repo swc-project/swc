302f4909da2da692bec84478915aaf15d557f1bd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-21 15:12:50 +0900

    export

--- crates/swc_ecma_minifier/src/pass/postcompress.rs
+++ crates/swc_ecma_minifier/src/pass/postcompress.rs
@@ -62,6 +62,18 @@ impl PostcompressOptimizer<'_> {
 impl VisitMut for PostcompressOptimizer<'_> {
     noop_visit_mut_type!();
 
+    fn visit_mut_export_decl(&mut self, export: &mut ExportDecl) {
+        match &mut export.decl {
+            Decl::Var(decl) => {
+                // Don't change constness of exported variables.
+                decl.visit_mut_children_with(self);
+            }
+            _ => {
+                export.decl.visit_mut_with(self);
+            }
+        }
+    }
+
     fn visit_mut_var_decl(&mut self, v: &mut VarDecl) {
         v.visit_mut_children_with(self);
 
