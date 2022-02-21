20a7f2b60c948b71f2416a6488de273699d9f282
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-21 14:12:20 +0900

    Update test refs

--- crates/swc/tests/tsc-references/throwInEnclosingStatements_es2015.2.minified.js
+++ crates/swc/tests/tsc-references/throwInEnclosingStatements_es2015.2.minified.js
@@ -1,10 +1,11 @@
+var y;
 switch(y){
     case "a":
         throw y;
     default:
         throw y;
 }
-for(var y;;)throw 0;
+for(;;)throw 0;
 for(;;)throw 0;
 for(var idx in {})throw idx;
 for(;;)throw null;
