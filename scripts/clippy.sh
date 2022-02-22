1b6c22445065b996c740167f6e6b1a7a8ba4bf25
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-23 06:49:38 +0900

    More

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo clippy --all -- --deny explicit_deref_methods --deny explicit_into_iter_loop
+cargo clippy --all -- --deny explicit_deref_methods --deny explicit_into_iter_loop --deny implicit_clone --deny inefficient_to_string
