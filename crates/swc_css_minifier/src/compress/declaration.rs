280bc562f3b22bd73a9cb5c6664cddb4f42515db
evilebottnawi <sheo13666q@gmail.com>
2022-02-23 19:02:57 +0300

    feat: compress display

--- crates/swc_css_minifier/src/compress/declaration.rs
+++ crates/swc_css_minifier/src/compress/declaration.rs
@@ -46,6 +46,198 @@ impl VisitMut for CompressDeclaration {
                 "padding" | "margin" | "inset" | "scroll-margin" | "scroll-padding"
                     if declaration.value.len() > 1 =>
                 {
+                "display" if declaration.value.len() > 1 => {
+                    let mut outside = None;
+                    let mut inside = None;
+                    let mut list_item = None;
+
+                    for value in declaration.value.iter() {
+                        match value {
+                            outside_node @ Value::Ident(Ident { value, .. })
+                                if matches!(
+                                    &*value.to_lowercase(),
+                                    "block" | "inline" | "run-in"
+                                ) =>
+                            {
+                                outside = Some(outside_node);
+                            }
+                            inside_node @ Value::Ident(Ident { value, .. })
+                                if matches!(
+                                    &*value.to_lowercase(),
+                                    "flow" | "flow-root" | "table" | "flex" | "grid" | "ruby"
+                                ) =>
+                            {
+                                inside = Some(inside_node);
+                            }
+                            list_item_node @ Value::Ident(Ident { value, .. })
+                                if &*value.to_lowercase() == "list-item" =>
+                            {
+                                if let Some(Value::Ident(Ident { value, .. })) = inside {
+                                    if !matches!(&*value.to_lowercase(), "flow" | "flow-root") {
+                                        continue;
+                                    }
+                                }
+
+                                list_item = Some(list_item_node)
+                            }
+                            _ => {}
+                        }
+                    }
+
+                    match (outside, inside, list_item) {
+                        // `block flow` -> `block`
+                        // `inline flow` -> `inline`
+                        // `run-in flow` -> `run-in`
+                        (
+                            Some(outside),
+                            Some(Value::Ident(Ident {
+                                value: inside_value,
+                                ..
+                            })),
+                            None,
+                        ) if &*inside_value.to_lowercase() == "flow" => {
+                            declaration.value = vec![outside.clone()];
+                        }
+                        // `block flow-root` -> `flow-root`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                ..
+                            })),
+                            Some(
+                                inside @ Value::Ident(Ident {
+                                    value: inside_value,
+                                    ..
+                                }),
+                            ),
+                            None,
+                        ) if &*outside_value.to_lowercase() == "block"
+                            && &*inside_value.to_lowercase() == "flow-root" =>
+                        {
+                            declaration.value = vec![inside.clone()];
+                        }
+                        // `inline flow-root` -> `inline-block`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                span,
+                                ..
+                            })),
+                            Some(Value::Ident(Ident {
+                                value: inside_value,
+                                ..
+                            })),
+                            None,
+                        ) if &*outside_value.to_lowercase() == "inline"
+                            && &*inside_value.to_lowercase() == "flow-root" =>
+                        {
+                            declaration.value = vec![Value::Ident(Ident {
+                                span: *span,
+                                value: "inline-block".into(),
+                                raw: "inline-block".into(),
+                            })];
+                        }
+                        // `block flow list-item` -> `list-item`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                ..
+                            })),
+                            Some(Value::Ident(Ident {
+                                value: inside_value,
+                                ..
+                            })),
+                            Some(list_item),
+                        ) if &*outside_value.to_lowercase() == "block"
+                            && &*inside_value.to_lowercase() == "flow" =>
+                        {
+                            declaration.value = vec![list_item.clone()];
+                        }
+                        // `block list-item` -> `list-item`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                ..
+                            })),
+                            None,
+                            Some(list_item),
+                        ) if &*outside_value.to_lowercase() == "block" => {
+                            declaration.value = vec![list_item.clone()];
+                        }
+                        // `flow list-item` -> `list-item`
+                        (
+                            None,
+                            Some(Value::Ident(Ident {
+                                value: inside_value,
+                                ..
+                            })),
+                            Some(list_item),
+                        ) if &*inside_value.to_lowercase() == "flow" => {
+                            declaration.value = vec![list_item.clone()];
+                        }
+                        // `inline flow list-item` -> `inline list-item`
+                        (
+                            Some(
+                                outside @ Value::Ident(Ident {
+                                    value: outside_value,
+                                    ..
+                                }),
+                            ),
+                            Some(Value::Ident(Ident {
+                                value: inside_value,
+                                ..
+                            })),
+                            Some(list_item),
+                        ) if &*outside_value.to_lowercase() == "inline"
+                            && &*inside_value.to_lowercase() == "flow" =>
+                        {
+                            declaration.value = vec![outside.clone(), list_item.clone()];
+                        }
+                        // `block flex` -> `flex`
+                        // `block grid` -> `grid`
+                        // `block table` -> `table`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                ..
+                            })),
+                            Some(
+                                inside @ Value::Ident(Ident {
+                                    value: inside_value,
+                                    ..
+                                }),
+                            ),
+                            None,
+                        ) if &*outside_value.to_lowercase() == "block"
+                            && matches!(
+                                &*inside_value.to_lowercase(),
+                                "flex" | "grid" | "table"
+                            ) =>
+                        {
+                            declaration.value = vec![inside.clone()];
+                        }
+                        // `inline ruby` -> `ruby`
+                        (
+                            Some(Value::Ident(Ident {
+                                value: outside_value,
+                                ..
+                            })),
+                            Some(
+                                inside @ Value::Ident(Ident {
+                                    value: inside_value,
+                                    ..
+                                }),
+                            ),
+                            None,
+                        ) if &*outside_value.to_lowercase() == "inline"
+                            && inside_value.to_lowercase() == "ruby" =>
+                        {
+                            declaration.value = vec![inside.clone()];
+                        }
+                        _ => {}
+                    }
+                }
+                "padding" | "margin" | "inset" if declaration.value.len() > 1 => {
                     let top = declaration.value.get(0);
                     let right = declaration
                         .value
