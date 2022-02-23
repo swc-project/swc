b1da08c67c6c8de803c0e047d9e9028505196cf2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-23 14:20:40 +0900

    Fix more

--- crates/swc_ecma_codegen/src/lib.rs
+++ crates/swc_ecma_codegen/src/lib.rs
@@ -1,4 +1,5 @@
 #![recursion_limit = "1024"]
+#![deny(clippy::all)]
 #![allow(clippy::match_like_matches_macro)]
 #![allow(clippy::nonminimal_bool)]
 #![allow(unused_variables)]
@@ -1123,7 +1124,7 @@ where
     fn emit_class_method(&mut self, n: &ClassMethod) -> Result {
         self.emit_leading_comments_of_span(n.span(), false)?;
 
-        self.emit_accesibility(n.accessibility)?;
+        self.emit_accessibility(n.accessibility)?;
 
         if n.is_static {
             keyword!("static");
@@ -1216,7 +1217,7 @@ where
 
         self.emit_list(n.span, Some(&n.decorators), ListFormat::Decorators)?;
 
-        self.emit_accesibility(n.accessibility)?;
+        self.emit_accessibility(n.accessibility)?;
 
         if n.is_static {
             keyword!("static");
@@ -1257,7 +1258,7 @@ where
         self.emit_leading_comments_of_span(n.span(), false)?;
 
         if n.accessibility != Some(Accessibility::Public) {
-            self.emit_accesibility(n.accessibility)?;
+            self.emit_accessibility(n.accessibility)?;
         }
 
         if n.is_static {
@@ -1295,7 +1296,7 @@ where
         semi!();
     }
 
-    fn emit_accesibility(&mut self, n: Option<Accessibility>) -> Result {
+    fn emit_accessibility(&mut self, n: Option<Accessibility>) -> Result {
         if let Some(a) = n {
             match a {
                 Accessibility::Public => keyword!(self, "public"),
@@ -1312,7 +1313,7 @@ where
     fn emit_class_constructor(&mut self, n: &Constructor) -> Result {
         self.emit_leading_comments_of_span(n.span(), false)?;
 
-        self.emit_accesibility(n.accessibility)?;
+        self.emit_accessibility(n.accessibility)?;
 
         keyword!("constructor");
         punct!("(");
@@ -2844,7 +2845,7 @@ where
     pub fn emit_module_export_name(&mut self, node: &ModuleExportName) -> Result {
         match *node {
             ModuleExportName::Ident(ref ident) => emit!(ident),
-            ModuleExportName::Str(ref str) => emit!(str),
+            ModuleExportName::Str(ref s) => emit!(s),
         }
     }
 }
