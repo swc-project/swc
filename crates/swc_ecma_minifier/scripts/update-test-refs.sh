db01560a6c4848827cf6bf4aa26b40c82e872d24
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-24 16:16:36 +0900

    set -eu

--- crates/swc_ecma_minifier/scripts/update-test-refs.sh
+++ crates/swc_ecma_minifier/scripts/update-test-refs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+set -eu
 
 export DIFF=0
 export UPDATE=1
 
-cargo test -p swc_ecma_minifier -p swc --no-fail-fast
+cargo test -p swc_ecma_minifier -p swc --no-fail-fast || true
 
 # find ../swc/tests/ -type f -empty -delete
