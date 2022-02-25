1e0a2256eefdb604679f81583773ba674c64dc32
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-25 11:58:25 +0900

    Update test refs

--- crates/swc/tests/tsc-references/symbolType1_es5.2.minified.js
+++ crates/swc/tests/tsc-references/symbolType1_es5.2.minified.js
@@ -1,4 +1,4 @@
 function _instanceof(left, right) {
-    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? right[Symbol.hasInstance](left) : left instanceof right;
+    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? !!right[Symbol.hasInstance](left) : left instanceof right;
 }
 _instanceof(Symbol(), Symbol), _instanceof(Symbol, Symbol()), _instanceof(Symbol() || {}, Object), _instanceof(Symbol, Symbol() || {});
