72c9e6ca2b8186c4b297658bec1ce37912898d9f
Bryan Lee <doslby@gmail.com>
2022-02-25 11:22:40 +0800

    fix(es/helpers): Cast the result of the `instanceof` helper to boolean (#3728)
    Co-authored-by: Donny/강동윤 <kdy1997.dev@gmail.com>

--- crates/swc/tests/tsc-references/instanceofOperatorWithAny_es5.2.minified.js
+++ crates/swc/tests/tsc-references/instanceofOperatorWithAny_es5.2.minified.js
@@ -1,4 +1,4 @@
 var a;
 !function(left, right) {
-    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? right[Symbol.hasInstance](left) : left instanceof right;
+    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? !!right[Symbol.hasInstance](left) : left instanceof right;
 }(a, a);
--- crates/swc/tests/tsc-references/instanceofOperatorWithRHSIsSubtypeOfFunction_es5.2.minified.js
+++ crates/swc/tests/tsc-references/instanceofOperatorWithRHSIsSubtypeOfFunction_es5.2.minified.js
@@ -1,5 +1,5 @@
 var x, f1, f3, f4;
 function _instanceof(left, right) {
-    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? right[Symbol.hasInstance](left) : left instanceof right;
+    return null != right && "undefined" != typeof Symbol && right[Symbol.hasInstance] ? !!right[Symbol.hasInstance](left) : left instanceof right;
 }
 _instanceof(x, f1), _instanceof(x, void 0), _instanceof(x, f3), _instanceof(x, f4), _instanceof(x, null), _instanceof(x, void 0);
