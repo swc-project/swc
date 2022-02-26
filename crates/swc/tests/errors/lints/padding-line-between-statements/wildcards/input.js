1f8560fa2a0a79b71b35c0fe38d0af207995302b
Artur Aralin <artur.aralin97@gmail.com>
2022-02-26 12:59:43 +0300

    feat(es/lint): add wildcard tests

---
+++ crates/swc/tests/errors/lints/padding-line-between-statements/wildcards/input.js
@@ -0,0 +1,21 @@
+function f1() {
+    if (cond) {}
+
+    return;
+}
+
+function f2() {
+    if (cond) {}
+    const x = 10;
+}
+
+function f3() {
+    if (cond) {}
+    return;
+}
+
+function f4() {
+    if (cond) {}
+
+    const x = 10;
+}
