7b7454dd77ad8eed50676cfcbe46734c319f747c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 20:32:04 +0900

    Pat

--- crates/swc_ecma_quote_macros/src/ast/pat.rs
+++ crates/swc_ecma_quote_macros/src/ast/pat.rs
@@ -1,28 +1,8 @@
-use pmutil::q;
-use swc_ecma_ast::{ArrayPat, AssignPat, BindingIdent, ObjectPat, RestPat};
+use swc_ecma_ast::*;
 
-use super::ToCode;
-use crate::ctxt::Ctx;
-
-impl ToCode for BindingIdent {
-    fn to_code(&self, cx: &Ctx) -> syn::Expr {
-        q!(
-            Vars {
-                id_val: self.id.to_code(cx),
-                type_ann_val: self.type_ann.to_code(cx),
-            },
-            {
-                swc_ecma_ast::BindingIdent {
-                    ident: id_val,
-                    type_ann: type_ann_val,
-                }
-            }
-        )
-        .parse()
-    }
-}
-
-fail_todo!(ArrayPat);
-fail_todo!(ObjectPat);
-fail_todo!(RestPat);
-fail_todo!(AssignPat);
+impl_struct!(BindingIdent, [id, type_ann]);
+impl_struct!(ArrayPat, [span, elems, optional, type_ann]);
+impl_struct!(ObjectPat, [span, props, optional, type_ann]);
+impl_struct!(RestPat, [span, dot3_token, arg, type_ann]);
+impl_struct!(AssignPat, [span, left, right, type_ann]);
+impl_struct!(Param, [span, decorators, pat]);
