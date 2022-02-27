32c9bdb8c4d20a82f7b9c0a7af1b894a14b15786
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 18:18:13 +0900

    Parse

--- crates/swc_ecma_quote_macros/src/input.rs
+++ crates/swc_ecma_quote_macros/src/input.rs
@@ -1,4 +1,8 @@
-use syn::{parse::Parse, punctuated::Punctuated, Token};
+use syn::{
+    parse::{Parse, ParseStream},
+    punctuated::Punctuated,
+    Token,
+};
 
 pub(super) struct QuoteInput {
     pub src: syn::LitStr,
@@ -16,6 +20,30 @@ pub(super) struct QuoteVar {
     pub value: syn::Expr,
 }
 
-impl Parse for QuoteInput {}
+impl Parse for QuoteInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let src = input.parse()?;
+        let as_token = input.parse()?;
+        let output_type = input.parse()?;
+        let comma_for_vars = input.parse()?;
+        let vars = Punctuated::parse_terminated(input)?;
 
-impl Parse for QuoteVar {}
+        Ok(Self {
+            src,
+            as_token,
+            output_type,
+            comma_for_vars,
+            vars,
+        })
+    }
+}
+
+impl Parse for QuoteVar {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            name: input.parse()?,
+            eq_token: input.parse()?,
+            value: input.parse()?,
+        })
+    }
+}
