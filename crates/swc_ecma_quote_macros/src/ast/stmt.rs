a9c76c15c8e68c5c0739853970c4b9cd7848d598
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 18:56:21 +0900

    Stmt

--- crates/swc_ecma_quote_macros/src/ast/stmt.rs
+++ crates/swc_ecma_quote_macros/src/ast/stmt.rs
@@ -5,6 +5,20 @@ use super::ToCode;
 use crate::ctxt::Ctx;
 
 fail_todo!(BlockStmt);
+fail_todo!(DebuggerStmt);
+fail_todo!(WithStmt);
+fail_todo!(LabeledStmt);
+fail_todo!(BreakStmt);
+fail_todo!(ContinueStmt);
+fail_todo!(IfStmt);
+fail_todo!(SwitchStmt);
+fail_todo!(ThrowStmt);
+fail_todo!(TryStmt);
+fail_todo!(WhileStmt);
+fail_todo!(DoWhileStmt);
+fail_todo!(ForStmt);
+fail_todo!(ForInStmt);
+fail_todo!(ForOfStmt);
 
 impl ToCode for EmptyStmt {
     fn to_code(&self, cx: &Ctx) -> syn::Expr {
@@ -13,6 +27,40 @@ impl ToCode for EmptyStmt {
             ({
                 swc_ecma_ast::EmptyStmt {
                     span: swc_common::DUMMY_SP,
+                }
+            })
+        )
+        .parse()
+    }
+}
+
+impl ToCode for ReturnStmt {
+    fn to_code(&self, cx: &Ctx) -> syn::Expr {
+        q!(
+            Vars {
+                arg_value: self.arg.to_code(cx),
+            },
+            ({
+                swc_ecma_ast::ReturnStmt {
+                    span: swc_common::DUMMY_SP,
+                    arg: arg_value,
+                }
+            })
+        )
+        .parse()
+    }
+}
+
+impl ToCode for ExprStmt {
+    fn to_code(&self, cx: &Ctx) -> syn::Expr {
+        q!(
+            Vars {
+                expr_value: self.expr.to_code(cx),
+            },
+            ({
+                swc_ecma_ast::ExprStmt {
+                    span: swc_common::DUMMY_SP,
+                    expr: expr_value,
                 }
             })
         )
