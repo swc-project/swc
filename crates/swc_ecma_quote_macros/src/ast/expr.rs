e9c1ad1e0dde1aded2247e0474f5ba1692a817ba
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 20:29:54 +0900

    More

--- crates/swc_ecma_quote_macros/src/ast/expr.rs
+++ crates/swc_ecma_quote_macros/src/ast/expr.rs
@@ -19,11 +19,11 @@ impl_struct!(CallExpr, [span, callee, args, type_args]);
 impl_struct!(ExprOrSpread, [spread, expr]);
 impl_struct!(Super, [span]);
 impl_struct!(Import, [span]);
+impl_struct!(NewExpr, [span, callee, args, type_args]);
+impl_struct!(SeqExpr, [span, exprs]);
 
-fail_todo!(NewExpr);
-fail_todo!(SeqExpr);
 fail_todo!(TaggedTpl);
-fail_todo!(YieldExpr);
+impl_struct!(YieldExpr, [span, arg, delegate]);
 fail_todo!(MetaPropExpr);
 fail_todo!(AwaitExpr);
 fail_todo!(JSXMemberExpr);
@@ -34,3 +34,29 @@ fail_todo!(JSXFragment);
 fail_todo!(OptChainExpr);
 
 impl_struct!(ParenExpr, [span, expr]);
+impl_struct!(
+    Function,
+    [
+        params,
+        decorators,
+        span,
+        body,
+        is_generator,
+        is_async,
+        type_params,
+        return_type
+    ]
+);
+impl_struct!(
+    Class,
+    [
+        span,
+        decorators,
+        body,
+        super_class,
+        is_abstract,
+        type_params,
+        super_type_params,
+        implements
+    ]
+);
