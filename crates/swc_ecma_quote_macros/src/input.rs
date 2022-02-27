314c7dd1fd1e388d1cc6c7f8d5418d41c29e11a5
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 18:11:26 +0900

    Input types

--- crates/swc_ecma_quote_macros/src/input.rs
+++ crates/swc_ecma_quote_macros/src/input.rs
@@ -1,4 +1,14 @@
-pub struct Input {
-    src: Str,
-    as_token: Token![as],
+use syn::Token;
+
+pub(super) struct Input {
+    pub src: syn::LitStr,
+    pub as_token: Token![as],
+    pub output_type: syn::Type,
+    pub vars: Vec<InputVar>,
+}
+
+pub(super) struct InputVar {
+    pub name: syn::Ident,
+    pub eq_token: Token![=],
+    pub value: syn::Expr,
 }
