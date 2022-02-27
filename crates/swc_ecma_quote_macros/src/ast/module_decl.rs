4ff11b02f435f941670401331702ea31e757419b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 18:45:02 +0900

    todo

---
+++ crates/swc_ecma_quote_macros/src/ast/module_decl.rs
@@ -0,0 +1,8 @@
+use swc_ecma_ast::*;
+
+fail_todo!(ImportDecl);
+fail_todo!(ExportDecl);
+fail_todo!(ExportDefaultDecl);
+fail_todo!(ExportDefaultExpr);
+fail_todo!(ExportAll);
+fail_todo!(NamedExport);
--- crates/swc_ecma_quote_macros/src/ast/typescript.rs
+++ crates/swc_ecma_quote_macros/src/ast/typescript.rs
@@ -4,3 +4,6 @@ fail_todo!(TsInterfaceDecl);
 fail_todo!(TsTypeAliasDecl);
 fail_todo!(TsEnumDecl);
 fail_todo!(TsModuleDecl);
+fail_todo!(TsImportEqualsDecl);
+fail_todo!(TsExportAssignment);
+fail_todo!(TsNamespaceExportDecl);
