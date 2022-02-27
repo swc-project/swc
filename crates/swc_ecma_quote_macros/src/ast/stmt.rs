1fe5a67f82afcdd1fff5cd9ab38e5c195e3286c3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-02-27 20:58:32 +0900

    More

--- crates/swc_ecma_quote_macros/src/ast/stmt.rs
+++ crates/swc_ecma_quote_macros/src/ast/stmt.rs
@@ -4,66 +4,21 @@ use swc_ecma_ast::*;
 use super::ToCode;
 use crate::ctxt::Ctx;
 
-impl_struct!(BlockStmt);
-impl_struct!(DebuggerStmt);
-impl_struct!(WithStmt);
-impl_struct!(LabeledStmt);
-impl_struct!(BreakStmt);
-impl_struct!(ContinueStmt);
-impl_struct!(IfStmt);
-impl_struct!(SwitchStmt);
-impl_struct!(ThrowStmt);
-impl_struct!(TryStmt);
-impl_struct!(WhileStmt);
-impl_struct!(DoWhileStmt);
-impl_struct!(ForStmt);
-impl_struct!(ForInStmt);
-impl_struct!(ForOfStmt);
-
-impl ToCode for EmptyStmt {
-    fn to_code(&self, cx: &Ctx) -> syn::Expr {
-        q!(
-            Vars {},
-            ({
-                swc_ecma_ast::EmptyStmt {
-                    span: swc_common::DUMMY_SP,
-                }
-            })
-        )
-        .parse()
-    }
-}
-
-impl ToCode for ReturnStmt {
-    fn to_code(&self, cx: &Ctx) -> syn::Expr {
-        q!(
-            Vars {
-                arg_value: self.arg.to_code(cx),
-            },
-            ({
-                swc_ecma_ast::ReturnStmt {
-                    span: swc_common::DUMMY_SP,
-                    arg: arg_value,
-                }
-            })
-        )
-        .parse()
-    }
-}
-
-impl ToCode for ExprStmt {
-    fn to_code(&self, cx: &Ctx) -> syn::Expr {
-        q!(
-            Vars {
-                expr_value: self.expr.to_code(cx),
-            },
-            ({
-                swc_ecma_ast::ExprStmt {
-                    span: swc_common::DUMMY_SP,
-                    expr: expr_value,
-                }
-            })
-        )
-        .parse()
-    }
-}
+impl_struct!(EmptyStmt, [span]);
+impl_struct!(BlockStmt, [span, stmts]);
+impl_struct!(DebuggerStmt, [span]);
+impl_struct!(WithStmt, [span, obj, body]);
+impl_struct!(LabeledStmt, [span, label, body]);
+impl_struct!(BreakStmt, [span, label]);
+impl_struct!(ContinueStmt, [span, label]);
+impl_struct!(IfStmt, [span, test, cons, alt]);
+impl_struct!(SwitchStmt, [span, discriminant, cases]);
+impl_struct!(ThrowStmt, [span, arg]);
+impl_struct!(TryStmt, [span, block, handler, finalizer]);
+impl_struct!(WhileStmt, [span, test, body]);
+impl_struct!(DoWhileStmt, [span, test, body]);
+impl_struct!(ForStmt, [span, init, test, update, body]);
+impl_struct!(ForInStmt, [span, left, right, body]);
+impl_struct!(ForOfStmt, [span, await_token, left, right, body]);
+impl_struct!(ReturnStmt, [span, arg]);
+impl_struct!(ExprStmt, [span, expr]);
