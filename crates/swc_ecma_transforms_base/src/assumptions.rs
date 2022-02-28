a6c2b70f169fb5493d3974208cf9213898f0f424
austaras <austaras@outlook.com>
2022-02-28 23:56:54 +0800

    add default for assumptions

--- crates/swc_ecma_transforms_base/src/assumptions.rs
+++ crates/swc_ecma_transforms_base/src/assumptions.rs
@@ -10,67 +10,90 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct Assumptions {
     /// https://babeljs.io/docs/en/assumptions#arraylikeisiterable
+    #[serde(default)]
     pub array_like_is_iterable: bool,
 
     /// https://babeljs.io/docs/en/assumptions#constantreexports
+    #[serde(default)]
     pub constant_reexports: bool,
 
     /// https://babeljs.io/docs/en/assumptions#constantsuper
+    #[serde(default)]
     pub constant_super: bool,
 
     /// https://babeljs.io/docs/en/assumptions#enumerablemodulemeta
+    #[serde(default)]
     pub enumerable_module_meta: bool,
 
     /// https://babeljs.io/docs/en/assumptions#ignorefunctionlength
+    #[serde(default)]
     pub ignore_function_length: bool,
 
     /// https://babeljs.io/docs/en/assumptions#ignoretoprimitivehint
+    #[serde(default)]
     pub ignore_to_primitive_hint: bool,
 
     /// https://babeljs.io/docs/en/assumptions#iterableisarray
+    #[serde(default)]
     pub iterable_is_array: bool,
 
     /// https://babeljs.io/docs/en/assumptions#mutabletemplateobject
+    #[serde(default)]
     pub mutable_template_object: bool,
 
     /// https://babeljs.io/docs/en/assumptions#noclasscalls
+    #[serde(default)]
     pub no_class_calls: bool,
 
     /// https://babeljs.io/docs/en/assumptions#nodocumentall
+    #[serde(default)]
     pub no_document_all: bool,
 
     /// https://babeljs.io/docs/en/assumptions#noincompletensimportdetection
+    #[serde(default)]
     pub no_incomplete_ns_import_detection: bool,
 
     /// https://babeljs.io/docs/en/assumptions#nonewarrows
+    #[serde(default)]
     pub no_new_arrows: bool,
+
     /// https://babeljs.io/docs/en/assumptions#objectrestnosymbols
+    #[serde(default)]
     pub object_rest_no_symbols: bool,
 
     /// https://babeljs.io/docs/en/assumptions#privatefieldsasproperties
+    #[serde(default)]
     pub private_fields_as_properties: bool,
 
     /// https://babeljs.io/docs/en/assumptions#puregetters
+    #[serde(default)]
     pub pure_getters: bool,
 
     /// https://babeljs.io/docs/en/assumptions#setclassmethods
+    #[serde(default)]
     pub set_class_methods: bool,
 
     /// https://babeljs.io/docs/en/assumptions#setcomputedproperties
+    #[serde(default)]
     pub set_computed_properties: bool,
 
     /// https://babeljs.io/docs/en/assumptions#setpublicclassfields
+    #[serde(default)]
     pub set_public_class_fields: bool,
 
     /// https://babeljs.io/docs/en/assumptions#setspreadproperties
+    #[serde(default)]
     pub set_spread_properties: bool,
 
     /// https://babeljs.io/docs/en/assumptions#skipforofiteratorclosing
+    #[serde(default)]
     pub skip_for_of_iterator_closing: bool,
 
     /// https://babeljs.io/docs/en/assumptions#superiscallableconstructor
+    #[serde(default)]
     pub super_is_callable_constructor: bool,
 
+    #[serde(default)]
     pub ts_enum_is_readonly: bool,
 }
 
