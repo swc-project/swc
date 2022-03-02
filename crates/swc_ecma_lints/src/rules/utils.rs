1f034ff5f230b99b9c1ab02848ebdb358d6a06f2
Artur Aralin <artur.aralin97@gmail.com>
2022-03-02 11:15:09 +0300

    feat(es/lint): extract tagged call as string only for String.raw

--- crates/swc_ecma_lints/src/rules/utils.rs
+++ crates/swc_ecma_lints/src/rules/utils.rs
@@ -2,8 +2,11 @@ use std::{fmt::Debug, sync::Arc};
 
 use serde::{Deserialize, Serialize};
 use swc_atoms::JsWord;
-use swc_common::{SourceMap, Span};
-use swc_ecma_ast::{Expr, Lit, Number, ParenExpr, Regex, Str, TaggedTpl, Tpl};
+use swc_common::{collections::AHashSet, SourceMap, Span, SyntaxContext};
+use swc_ecma_ast::{
+    Expr, Id, Lit, MemberExpr, MemberProp, Number, ParenExpr, Regex, Str, TaggedTpl, Tpl,
+};
+use swc_ecma_utils::ident::IdentLike;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -52,7 +55,13 @@ pub enum ArgValue {
     Other,
 }
 
-pub fn extract_arg_val(source_map: &Arc<SourceMap>, expr: &Expr, check_parens: bool) -> ArgValue {
+pub fn extract_arg_val(
+    source_map: &Arc<SourceMap>,
+    top_level_ctxt: &SyntaxContext,
+    top_level_declared_vars: &AHashSet<Id>,
+    expr: &Expr,
+    check_parens: bool,
+) -> ArgValue {
     fn extract_value_from_tpl(source_map: &Arc<SourceMap>, span: &Span) -> JsWord {
         let source = source_map.lookup_byte_offset(span.lo);
         let lo = (span.lo.0 as usize) + 1;
@@ -65,7 +74,13 @@ pub fn extract_arg_val(source_map: &Arc<SourceMap>, expr: &Expr, check_parens: b
         Expr::Ident(_) => ArgValue::Ident,
         Expr::Paren(ParenExpr { expr, .. }) => {
             if check_parens {
-                extract_arg_val(source_map, expr.as_ref(), check_parens)
+                extract_arg_val(
+                    source_map,
+                    top_level_ctxt,
+                    top_level_declared_vars,
+                    expr.as_ref(),
+                    check_parens,
+                )
             } else {
                 ArgValue::Other
             }
@@ -84,14 +99,35 @@ pub fn extract_arg_val(source_map: &Arc<SourceMap>, expr: &Expr, check_parens: b
             }
         }
         Expr::TaggedTpl(TaggedTpl {
+            tag,
             tpl: Tpl { span, exprs, .. },
             ..
         }) => {
             if exprs.is_empty() {
-                ArgValue::Str(extract_value_from_tpl(source_map, span))
-            } else {
-                ArgValue::Other
+                if let Expr::Member(MemberExpr { obj, prop, .. }) = tag.as_ref() {
+                    if let (Expr::Ident(obj), MemberProp::Ident(prop)) = (obj.as_ref(), prop) {
+                        if &*obj.sym != "String" {
+                            return ArgValue::Other;
+                        }
+
+                        if obj.span.ctxt != *top_level_ctxt {
+                            return ArgValue::Other;
+                        }
+
+                        if top_level_declared_vars.contains(&obj.to_id()) {
+                            return ArgValue::Other;
+                        }
+
+                        if &prop.sym != "raw" {
+                            return ArgValue::Other;
+                        }
+
+                        return ArgValue::Str(extract_value_from_tpl(source_map, span));
+                    }
+                }
             }
+
+            ArgValue::Other
         }
         _ => ArgValue::Other,
     }
