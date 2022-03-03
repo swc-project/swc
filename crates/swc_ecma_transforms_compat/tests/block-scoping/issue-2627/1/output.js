6659157e7a9416bfdcb97e6dcd0dd2fad28d3ca8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-03 16:46:36 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_compat/tests/block-scoping/issue-2627/1/output.js
@@ -0,0 +1,7 @@
+{
+    var _a = 1;
+    var _b = 1;
+    var c = 1;
+}console.log(a);
+console.log(b);
+console.log(c);
---
+++ crates/swc_ecma_transforms_compat/tests/block-scoping/issue-3235/1/output.js
@@ -0,0 +1,16 @@
+var res = [];
+var a = 2;
+res.push(a === 2);
+{
+    var _b = 1;
+}res.push(typeof b === "undefined");
+if (true) {
+    var _b1 = 0;
+}
+res.push(typeof b === "undefined");
+for(var _b2 = 0; _b2 < 10; _b2++){}
+res.push(typeof b === "undefined");
+function test() {
+    var _b = 7;
+}
+res.push(typeof b === "undefined");
