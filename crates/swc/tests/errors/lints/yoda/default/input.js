d63a08f64880301bf052a050e9ee8ec59f91abc0
Artur Aralin <artur.aralin97@gmail.com>
2022-03-03 10:18:19 +0300

    feat(es/lint): add tests for default settings

---
+++ crates/swc/tests/errors/lints/yoda/default/input.js
@@ -0,0 +1,21 @@
+if (10 === a) {}
+
+if ('a' === a)
+
+if (`abc` === a) {}
+
+if (-10 === a) {}
+
+if (10 < a && a > 30) {}
+
+if (c && (10 < a && a > 30)) {}
+
+// wait for rule "radix" merge
+if (String.raw`abc` === a) {}
+
+// Valid cases
+if (a === 10) {}
+
+if (`${y}` === a) {}
+
+if (f() === a) {}
