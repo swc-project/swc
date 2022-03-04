080295067e55791de0d47508d6f0ddc7f5b29eb1
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-04 12:13:26 -0800

    refactor(swc/cli): setup subcommands for the features

--- crates/swc_cli/src/main.rs
+++ crates/swc_cli/src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use commands::{Command, CommandRunner, PluginSubcommand, SwcCliOptions};
 
 mod commands;
+mod util;
 
 fn main() -> anyhow::Result<()> {
     let command = SwcCliOptions::parse().command;
@@ -9,5 +10,8 @@ fn main() -> anyhow::Result<()> {
     match &command {
         Command::Plugin(PluginSubcommand::New(options)) => options.execute(),
         Command::Compile(options) => options.execute(),
+        Command::Minify(options) => options.execute(),
+        Command::Bundle(options) => options.execute(),
+        Command::Lint(options) => options.execute(),
     }
 }
