550de92e084bda5e60a68f85d077dd09bf280515
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-04 06:31:02 +0000

    feat(trace): add trace spans for es2017-19 transforms

--- crates/swc_ecma_transforms_compat/src/es2017/mod.rs
+++ crates/swc_ecma_transforms_compat/src/es2017/mod.rs
@@ -4,6 +4,7 @@ pub use self::async_to_generator::async_to_generator;
 
 mod async_to_generator;
 
+#[tracing::instrument(level = "trace", skip_all)]
 pub fn es2017() -> impl Fold {
     async_to_generator()
 }
