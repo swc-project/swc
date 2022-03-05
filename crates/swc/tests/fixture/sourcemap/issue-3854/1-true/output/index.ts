63c14863bc02e601567afde77dcfb2323a122809
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-05 15:54:36 +0900

    It's working

---
+++ crates/swc/tests/fixture/sourcemap/issue-3854/1-true/output/index.ts
@@ -0,0 +1,329 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var _exportNames = {
+    node: true,
+    node: true
+};
+exports.seq = exports.sources = exports.source = exports.header = exports.stage = exports.opener = exports.trustBox = exports.create = exports.node = void 0;
+var swcHelpers = require("@swc/helpers");
+var _abstractBuilders = require("./AbstractBuilders");
+var _builderUtils = require("./Builder.utils");
+var _elementBuilder = require("./ElementBuilder");
+var _node = swcHelpers.interopRequireWildcard(require("./BodyNodesBuilder"));
+Object.keys(_node).forEach(function(key) {
+    if (key === "default" || key === "__esModule") return;
+    if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+    if (key in exports && exports[key] === _node[key]) return;
+    Object.defineProperty(exports, key, {
+        enumerable: true,
+        get: function() {
+            return _node[key];
+        }
+    });
+});
+exports.node = _node;
+const create = ()=>new BodyBuilder()
+;
+exports.create = create;
+const trustBox = ()=>new TrustBoxBuilder()
+;
+exports.trustBox = trustBox;
+const opener = ()=>new OpenerBuilder()
+;
+exports.opener = opener;
+const stage = ()=>new BodyStageBuilder()
+;
+exports.stage = stage;
+const header = ()=>new BodyHeaderBuilder()
+;
+exports.header = header;
+const source = (nodes = [])=>new ArticleSourceBuilder(...nodes)
+;
+exports.source = source;
+const sources = ()=>new ArticleSourcesBuilder()
+;
+exports.sources = sources;
+const seq = {
+    stage: ()=>new BodyStageSeqBuilder()
+    ,
+    source: ()=>new ArticleSourceSeqBuilder()
+};
+exports.seq = seq;
+var _stages = new WeakMap(), _trustBox = new WeakMap(), _disclaimer = new WeakMap(), _articleSources = new WeakMap();
+class BodyBuilder extends _abstractBuilders.AbstractBuilder {
+    stages(...stages) {
+        swcHelpers.classPrivateFieldSet(this, _stages, stages.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    trustBox(trustBox1) {
+        swcHelpers.classPrivateFieldSet(this, _trustBox, (0, _builderUtils).mapBuildArg(trustBox1));
+        return this;
+    }
+    disclaimer(disclaimer) {
+        swcHelpers.classPrivateFieldSet(this, _disclaimer, disclaimer?.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    articleSources(articleSources) {
+        swcHelpers.classPrivateFieldSet(this, _articleSources, (0, _builderUtils).mapBuildArg(articleSources));
+        return this;
+    }
+    build() {
+        return {
+            stages: swcHelpers.classPrivateFieldGet(this, _stages),
+            trustBox: swcHelpers.classPrivateFieldGet(this, _trustBox),
+            disclaimer: swcHelpers.classPrivateFieldGet(this, _disclaimer),
+            articleSources: swcHelpers.classPrivateFieldGet(this, _articleSources)
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _stages, {
+            writable: true,
+            value: []
+        });
+        swcHelpers.classPrivateFieldInit(this, _trustBox, {
+            writable: true,
+            value: undefined
+        });
+        swcHelpers.classPrivateFieldInit(this, _disclaimer, {
+            writable: true,
+            value: undefined
+        });
+        swcHelpers.classPrivateFieldInit(this, _articleSources, {
+            writable: true,
+            value: undefined
+        });
+    }
+}
+var _nodes = new WeakMap(), _hidden = new WeakMap();
+class TrustBoxBuilder extends _abstractBuilders.AbstractBuilder {
+    nodes(nodes) {
+        swcHelpers.classPrivateFieldSet(this, _nodes, nodes.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    hidden(hidden) {
+        swcHelpers.classPrivateFieldSet(this, _hidden, hidden.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    build() {
+        return {
+            nodes: swcHelpers.classPrivateFieldGet(this, _nodes),
+            hidden: swcHelpers.classPrivateFieldGet(this, _hidden)
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _nodes, {
+            writable: true,
+            value: []
+        });
+        swcHelpers.classPrivateFieldInit(this, _hidden, {
+            writable: true,
+            value: []
+        });
+    }
+}
+var _element = new WeakMap();
+class OpenerBuilder extends _abstractBuilders.AbstractBuilder {
+    element(element) {
+        swcHelpers.classPrivateFieldSet(this, _element, (0, _builderUtils).mapBuildArg(element));
+        return this;
+    }
+    build() {
+        return {
+            element: swcHelpers.classPrivateFieldGet(this, _element)
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _element, {
+            writable: true,
+            value: (0, _elementBuilder).image().build()
+        });
+    }
+}
+var _nodes1 = new WeakMap(), _header = new WeakMap(), _companions = new WeakMap(), _commercialsEndOfStage = new WeakMap();
+class BodyStageSeqBuilder extends _abstractBuilders.AbstractSeqBuilder {
+    nodes(nodes) {
+        swcHelpers.classPrivateFieldSet(this, _nodes1, nodes.map(_builderUtils.mapBuildArgs));
+        return this;
+    }
+    header(header1) {
+        swcHelpers.classPrivateFieldSet(this, _header, (0, _builderUtils).mapBuildArgs(header1 ?? []));
+        return this;
+    }
+    companions(companions) {
+        swcHelpers.classPrivateFieldSet(this, _companions, companions.map(_builderUtils.mapBuildArgs));
+        return this;
+    }
+    commercialsEndOfStage(commercialsEndOfStage) {
+        swcHelpers.classPrivateFieldSet(this, _commercialsEndOfStage, commercialsEndOfStage.map(_builderUtils.mapBuildArgs));
+        return this;
+    }
+    buildListItem(seqNextElement) {
+        return {
+            id: (0, _builderUtils).hash('bodyStage', swcHelpers.classPrivateFieldGet(this, _nodes1), swcHelpers.classPrivateFieldGet(this, _companions), swcHelpers.classPrivateFieldGet(this, _commercialsEndOfStage), swcHelpers.classPrivateFieldGet(this, _header)),
+            nodes: seqNextElement.array(swcHelpers.classPrivateFieldGet(this, _nodes1)),
+            header: seqNextElement.maybe(swcHelpers.classPrivateFieldGet(this, _header)),
+            companions: seqNextElement.array(swcHelpers.classPrivateFieldGet(this, _companions)),
+            commercialsEndOfStage: seqNextElement.array(swcHelpers.classPrivateFieldGet(this, _commercialsEndOfStage))
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _nodes1, {
+            writable: true,
+            value: []
+        });
+        swcHelpers.classPrivateFieldInit(this, _header, {
+            writable: true,
+            value: undefined
+        });
+        swcHelpers.classPrivateFieldInit(this, _companions, {
+            writable: true,
+            value: []
+        });
+        swcHelpers.classPrivateFieldInit(this, _commercialsEndOfStage, {
+            writable: true,
+            value: []
+        });
+    }
+}
+var _seqBuilder = new WeakMap();
+class BodyStageBuilder extends _abstractBuilders.AbstractBuilder {
+    nodes(nodes) {
+        swcHelpers.classPrivateFieldGet(this, _seqBuilder).nodes([
+            nodes
+        ]);
+        return this;
+    }
+    header(header2) {
+        if (header2) {
+            swcHelpers.classPrivateFieldGet(this, _seqBuilder).header([
+                header2
+            ]);
+        }
+        return this;
+    }
+    companions(companions) {
+        swcHelpers.classPrivateFieldGet(this, _seqBuilder).companions([
+            companions
+        ]);
+        return this;
+    }
+    commercialsEndOfStage(commercialsEndOfStage) {
+        swcHelpers.classPrivateFieldGet(this, _seqBuilder).commercialsEndOfStage([
+            commercialsEndOfStage
+        ]);
+        return this;
+    }
+    build() {
+        return swcHelpers.classPrivateFieldGet(this, _seqBuilder).build();
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _seqBuilder, {
+            writable: true,
+            value: new BodyStageSeqBuilder()
+        });
+    }
+}
+var _variant = new WeakMap(), _opener = new WeakMap();
+class BodyHeaderBuilder extends _abstractBuilders.AbstractBuilder {
+    variant(variant) {
+        swcHelpers.classPrivateFieldSet(this, _variant, variant);
+        return this;
+    }
+    opener(opener1) {
+        swcHelpers.classPrivateFieldSet(this, _opener, (0, _builderUtils).mapBuildArg(opener1));
+        return this;
+    }
+    build() {
+        return {
+            variant: swcHelpers.classPrivateFieldGet(this, _variant),
+            opener: swcHelpers.classPrivateFieldGet(this, _opener)
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _variant, {
+            writable: true,
+            value: 'full'
+        });
+        swcHelpers.classPrivateFieldInit(this, _opener, {
+            writable: true,
+            value: undefined
+        });
+    }
+}
+var _nodes2 = new WeakMap();
+class ArticleSourceSeqBuilder extends _abstractBuilders.AbstractSeqBuilder {
+    nodes(nodes) {
+        swcHelpers.classPrivateFieldSet(this, _nodes2, nodes.map(_builderUtils.mapBuildArgs));
+        return this;
+    }
+    buildListItem(seqNextElement) {
+        const id = (0, _builderUtils).hash('article-source', swcHelpers.classPrivateFieldGet(this, _nodes2));
+        return {
+            id,
+            nodes: seqNextElement.array(swcHelpers.classPrivateFieldGet(this, _nodes2))
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _nodes2, {
+            writable: true,
+            value: []
+        });
+    }
+}
+var _seqBuilder1 = new WeakMap();
+class ArticleSourceBuilder extends _abstractBuilders.AbstractBuilder {
+    nodes(...nodes) {
+        swcHelpers.classPrivateFieldGet(this, _seqBuilder1).nodes([
+            nodes
+        ]);
+        return this;
+    }
+    build() {
+        return swcHelpers.classPrivateFieldGet(this, _seqBuilder1).build();
+    }
+    constructor(...nodes){
+        super();
+        swcHelpers.classPrivateFieldInit(this, _seqBuilder1, {
+            writable: true,
+            value: new ArticleSourceSeqBuilder()
+        });
+        this.nodes(...nodes);
+    }
+}
+var _nodes3 = new WeakMap(), _hidden1 = new WeakMap();
+class ArticleSourcesBuilder extends _abstractBuilders.AbstractBuilder {
+    nodes(...nodes) {
+        swcHelpers.classPrivateFieldSet(this, _nodes3, nodes.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    hidden(...hidden) {
+        swcHelpers.classPrivateFieldSet(this, _hidden1, hidden.map(_builderUtils.mapBuildArg));
+        return this;
+    }
+    build() {
+        return {
+            nodes: swcHelpers.classPrivateFieldGet(this, _nodes3),
+            hidden: swcHelpers.classPrivateFieldGet(this, _hidden1)
+        };
+    }
+    constructor(...args){
+        super(...args);
+        swcHelpers.classPrivateFieldInit(this, _nodes3, {
+            writable: true,
+            value: []
+        });
+        swcHelpers.classPrivateFieldInit(this, _hidden1, {
+            writable: true,
+            value: []
+        });
+    }
+}
