2605e132e683ad0b649621a321be31347e0ac5e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-06 06:56:34 +0900

    test(es): Add tests for fixed issues (#3874)

---
+++ crates/swc/tests/fixture/issue-2636/output/index.ts
@@ -0,0 +1,5 @@
+export var RuleInterpreterHelper;
+(function(RuleInterpreterHelper1) {
+    var fieldNameMap = RuleInterpreterHelper1.fieldNameMap = [];
+    var fieldNameHashMap = RuleInterpreterHelper1.fieldNameHashMap = new Map(fieldNameMap);
+})(RuleInterpreterHelper || (RuleInterpreterHelper = {}));
