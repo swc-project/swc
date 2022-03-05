1d9b0e6a4718e5a4f94e88dd5f3c497df565c414
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-06 05:43:29 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issue-2336/output/input.tsx
@@ -0,0 +1,5 @@
+import * as swcHelpers from "@swc/helpers";
+var ref = swcHelpers.slicedToArray(createQuery(), 2);
+export var useQuery = ref[0], query = ref[1].query;
+var ref1 = swcHelpers.slicedToArray(createMutation(), 2);
+export var useMutation = ref1[0], mutate = ref1[1].mutate;
