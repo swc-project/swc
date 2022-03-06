653f79b728d3d8088f7e6a383fd33f626cf6d993
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-06 10:21:04 +0900

    Update test refs

--- crates/swc/tests/fixture/paths/issue-2844/output/src/utils/index.ts
+++ crates/swc/tests/fixture/paths/issue-2844/output/src/utils/index.ts
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.utilFn = utilFn;
-var _utilFile = require("./utilFile");
+var _utilFile = require("../core/utils/utilFile");
 (0, _utilFile).test();
 function utilFn() {}
