5e34e5fd07d073721837100f3256bdb661f0c6cd
Artur Aralin <artur.aralin97@gmail.com>
2022-03-06 20:40:38 +0300

    feat(es/lint): add tests

---
+++ crates/swc/tests/errors/lints/max-length/ignore-trailing-comment/input.js
@@ -0,0 +1,22 @@
+() => {
+    const x = 'lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.lineeeee.';
+}
+
+
+`// not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment`;
+
+'// not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment';
+
+"// not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment";
+
+`// not a comment with escape \` -_-  not a comment not a comment not a comment not a comment not a comment not a comment not a comment not a comment`;
+
+// comment comment comment comment comment comment comment comment comment comment comment comment comment comment comment comment
+
+/**
+ * comment comment comment comment comment comment comment comment comment comment comment comment comment comment comment comment
+ * comment comment comment comment
+*/
+
+const a = 10; // tailing comment tailing comment tailing comment tailing comment tailing comment tailing comment tailing comment
+
