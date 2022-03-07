dcaaa9240c864821fabd79ec9e068b50346acee4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-07 21:35:41 +0900

    adjust more

--- crates/swc_ecma_parser/scripts/instrument.sh
+++ crates/swc_ecma_parser/scripts/instrument.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo profile instruments --release -t time --features tracing/release_max_level_info --example perf
+cargo profile instruments --release -t time --features tracing/release_max_level_info --features swc_common/concurrent --features swc_common/parking_lot --example perf
