f4f8580c4179dba3a32a259ec838576c731ecae3
magic-akari <akari.ccino@gmail.com>
2022-03-09 01:19:01 +0800

    fix(es/module): use `IndexMap` for stable order

--- crates/swc_ecma_transforms_module/tests/fixture/commonjs/export-update/1/output.js
+++ crates/swc_ecma_transforms_module/tests/fixture/commonjs/export-update/1/output.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.foobar = exports.bazbar = exports.baz = exports.bar = exports.foo = void 0;
+exports.bazbar = exports.foobar = exports.baz = exports.bar = exports.foo = void 0;
 let foo = 1n;
 exports.foo = foo;
 foo++, exports.foo = foo;
@@ -11,5 +11,5 @@ exports.bar = bar;
 let baz = (bar--, exports.bar = bar);
 exports.baz = baz;
 exports.bar = --bar;
-exports.bazbar = baz;
 exports.foobar = foo;
+exports.bazbar = baz;
