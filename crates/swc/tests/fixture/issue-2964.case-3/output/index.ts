a78cdca517dbb2d78e76d26b15948ebd008facc6
David <david.campion@walmartlabs.com>
2022-03-08 10:49:46 -0800

    update test fixtures for swc helper integration

--- crates/swc/tests/fixture/issue-2964.case-3/output/index.ts
+++ crates/swc/tests/fixture/issue-2964.case-3/output/index.ts
@@ -1,34 +1,8 @@
 "use strict";
-function _objectWithoutProperties(source, excluded) {
-    if (source == null) return {};
-    var target = _objectWithoutPropertiesLoose(source, excluded);
-    var key, i;
-    if (Object.getOwnPropertySymbols) {
-        var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
-        for(i = 0; i < sourceSymbolKeys.length; i++){
-            key = sourceSymbolKeys[i];
-            if (excluded.indexOf(key) >= 0) continue;
-            if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
-            target[key] = source[key];
-        }
-    }
-    return target;
-}
-function _objectWithoutPropertiesLoose(source, excluded) {
-    if (source == null) return {};
-    var target = {};
-    var sourceKeys = Object.keys(source);
-    var key, i;
-    for(i = 0; i < sourceKeys.length; i++){
-        key = sourceKeys[i];
-        if (excluded.indexOf(key) >= 0) continue;
-        target[key] = source[key];
-    }
-    return target;
-}
+var swcHelpers = require("@swc/helpers");
 // single line comment
 const x = (_param)=>/*todo: refactor any type*/ {
-    var { y  } = _param, rest = _objectWithoutProperties(_param, [
+    var { y  } = _param, rest = swcHelpers.objectWithoutProperties(_param, [
         "y"
     ]);
     return {
