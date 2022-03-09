abfc00ea41a57b8f81f584947f13724639fab2e5
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-09 20:39:51 +0900

    Reduce

--- crates/swc_ecma_parser/tests/typescript/issue-3241/3/input.tsx
+++ crates/swc_ecma_parser/tests/typescript/issue-3241/3/input.tsx
@@ -1,16 +1,3 @@
-export default function SingleProjectPage({
-    frontmatter,
-    code,
-}: SingleProjectPageProps) {
-    const Component = useMemo(() => getMDXComponent(code), [code]);
-
+export default function SingleProjectPage({}: SingleProjectPageProps) {
     return <Component components={...MDXComponents as any}></Component>;
 }
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const slug = params!["slug"] as string;
-    const { code, frontmatter } = await getFileBySlugAndType(slug, "projects");
-    return {
-        props: { frontmatter: frontmatter as ProjectFrontMatter, code },
-    };
-};
