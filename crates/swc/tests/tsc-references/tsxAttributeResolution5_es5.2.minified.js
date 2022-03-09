0f994f67dd453c9901ff58e16d27241718ad70f0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-09 20:57:19 +0900

    More

---
+++ crates/swc/tests/tsc-references/tsxAttributeResolution5_es5.2.minified.js
@@ -0,0 +1,2 @@
+import * as swcHelpers from "@swc/helpers";
+React.createElement("test1", swcHelpers.extends({}, {})), React.createElement("test2", swcHelpers.extends({}, {}));
---
+++ crates/swc/tests/tsc-references/tsxTypeArgumentsJsxPreserveOutput_es5.2.minified.js
@@ -0,0 +1,6 @@
+var React = require("react");
+function Foo() {
+    return null;
+}
+React.createElement(React.Fragment, null, React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null), React.createElement(Foo, null));
+export { };
