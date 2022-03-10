a5930fb648b089cfc10d1a4fb972fc3ba028d594
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-10 19:52:22 +0900

    More work

--- crates/swc_reporter/src/lib.rs
+++ crates/swc_reporter/src/lib.rs
@@ -1,12 +1,115 @@
-use std::fmt::Write;
+use std::fmt::{self, Write};
 
-use miette::GraphicalReportHandler;
-use swc_common::{sync::Lrc, SourceMap};
+use miette::{GraphicalReportHandler, MietteError, SourceCode, SpanContents};
+use swc_common::{
+    errors::{DiagnosticBuilder, Emitter},
+    sync::Lrc,
+    SourceMap,
+};
 
 pub struct PrettyEmitter {
     cm: Lrc<SourceMap>,
 
-    wr: Box<dyn Write + Send + Sync>,
+    wr: WriterWrapper,
 
     reporter: GraphicalReportHandler,
+}
+
+struct WriterWrapper(Box<dyn Write + Send + Sync>);
+
+impl Write for WriterWrapper {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.0.write_str(s)
+    }
+
+    fn write_char(&mut self, c: char) -> fmt::Result {
+        self.0.write_char(c)
+    }
+
+    fn write_fmt(mut self: &mut Self, args: fmt::Arguments<'_>) -> fmt::Result {
+        self.0.write_fmt(args)
+    }
+}
+
+struct MietteSourceCode<'a>(&'a SourceMap);
+
+impl SourceCode for MietteSourceCode<'_> {
+    fn read_span<'a>(
+        &'a self,
+        span: &miette::SourceSpan,
+        context_lines_before: usize,
+        context_lines_after: usize,
+    ) -> Result<Box<dyn SpanContents<'a> + 'a>, MietteError> {
+        todo!()
+    }
+}
+
+impl Emitter for PrettyEmitter {
+    fn emit(&mut self, db: &DiagnosticBuilder) {
+        let source_code = MietteSourceCode(&self.cm);
+        let diagnostic = MietteDiagnostic {
+            source_code,
+            d: &**db,
+        };
+
+        self.reporter
+            .render_report(&mut self.wr, &diagnostic)
+            .unwrap();
+    }
+}
+
+struct MietteDiagnostic<'a> {
+    source_code: MietteSourceCode<'a>,
+    d: &'a swc_common::errors::Diagnostic,
+}
+
+impl miette::Diagnostic for MietteDiagnostic<'_> {
+    fn code<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
+        self.d
+            .code
+            .as_ref()
+            .map(|v| match v {
+                swc_common::errors::DiagnosticId::Error(v) => v.clone(),
+                swc_common::errors::DiagnosticId::Lint(v) => v.clone(),
+            })
+            .map(|code| Box::new(code) as Box<dyn fmt::Display>)
+    }
+
+    fn severity(&self) -> Option<miette::Severity> {
+        match self.d.level {}
+    }
+
+    fn help<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
+        None
+    }
+
+    fn url<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
+        None
+    }
+
+    fn source_code(&self) -> Option<&dyn SourceCode> {
+        None
+    }
+
+    fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
+        None
+    }
+
+    fn related<'a>(&'a self) -> Option<Box<dyn Iterator<Item = &'a dyn miette::Diagnostic> + 'a>> {
+        None
+    }
+}
+
+impl std::error::Error for MietteDiagnostic<'_> {}
+
+impl fmt::Debug for MietteDiagnostic<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        todo!()
+    }
+}
+
+impl fmt::Display for MietteDiagnostic<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        todo!()
+    }
 }
