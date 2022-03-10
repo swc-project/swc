62ab17999eb98312bb03f3ad2f436fbac6ae4acc
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-03-10 07:23:34 +0300

    feat(css/prefixer): Implement more rules (#3933)

--- crates/swc_css_utils/src/lib.rs
+++ crates/swc_css_utils/src/lib.rs
@@ -12,7 +12,7 @@ impl VisitMut for IdentReplacer<'_> {
     fn visit_mut_ident(&mut self, n: &mut Ident) {
         n.visit_mut_children_with(self);
 
-        if &*n.value == self.from {
+        if &*n.value.to_lowercase() == self.from {
             n.value = self.to.into();
             n.raw = self.to.into();
         }
@@ -24,4 +24,27 @@ where
     N: for<'aa> VisitMutWith<IdentReplacer<'aa>>,
 {
     node.visit_mut_with(&mut IdentReplacer { from, to });
+}
+
+pub struct FunctionNameReplacer<'a> {
+    from: &'a str,
+    to: &'a str,
+}
+
+impl VisitMut for FunctionNameReplacer<'_> {
+    fn visit_mut_function(&mut self, n: &mut Function) {
+        n.visit_mut_children_with(self);
+
+        if &*n.name.value.to_lowercase() == self.from {
+            n.name.value = self.to.into();
+            n.name.raw = self.to.into();
+        }
+    }
+}
+
+pub fn replace_function_name<N>(node: &mut N, from: &str, to: &str)
+where
+    N: for<'aa> VisitMutWith<FunctionNameReplacer<'aa>>,
+{
+    node.visit_mut_with(&mut FunctionNameReplacer { from, to });
 }
