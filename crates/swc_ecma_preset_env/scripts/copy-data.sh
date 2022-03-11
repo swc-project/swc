e76e95d0b004dcad53eae69011f4751044da005a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-11 23:15:02 +0900

    Script

--- crates/swc_ecma_preset_env/scripts/copy-data.sh
+++ crates/swc_ecma_preset_env/scripts/copy-data.sh
@@ -6,9 +6,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NM_DIR="$SCRIPT_DIR/../../../node_modules"
 
 function data {
-    mkdir -p "$(dirname $SCRIPT_DIR/../src/data/$1)"
+    mkdir -p "$(dirname $SCRIPT_DIR/../data/$1)"
 
-    cp "$NM_DIR/$1" "$SCRIPT_DIR/../src/data/$1" 
+    cp "$NM_DIR/$1" "$SCRIPT_DIR/../data/$1" 
 }
 
 data "@babel/compat-data/data/plugins.json"
+data "@babel/compat-data/data/plugin-bugfixes.json"
+data "core-js-compat/data.json"
+data "core-js-compat/entries.json"
+data "core-js-compat/modules-by-versions.json"
