ddfa490899209a6acf99989b3b2958e1c185fab2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-12 10:00:23 +0900

    chore: Publish crates

--- scripts/cargo/bump.sh
+++ scripts/cargo/bump.sh
@@ -11,10 +11,7 @@ cargo metadata --offline --format-version 1 > /dev/null
 # Ensure that dependencies are all verified
 ./scripts/crev/verify.sh
 
-export GIT_COMMITTER_NAME="SWC Bot"
-export GIT_COMMITTER_EMAIL="bot@swc.rs"
-
 git add -A
-git commit --author="SWC Bot <bot@swc.rs>" -m 'chore: Publish crates'
+git commit -m 'chore: Publish crates'
 git push --no-verify
 cargo mono publish --no-verify
