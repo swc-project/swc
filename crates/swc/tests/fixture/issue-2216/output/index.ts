18b381c58cd7ae205c1f2b6ee051af1390025cea
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-12 17:49:29 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issue-2216/output/index.ts
@@ -0,0 +1,7 @@
+export var ATestForSwc = /*#__PURE__*/ function() {
+    "use strict";
+    function ATestForSwc() {}
+    var _proto = ATestForSwc.prototype;
+    _proto.testMethod = function testMethod() {};
+    return ATestForSwc;
+}();
