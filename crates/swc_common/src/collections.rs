adbb49dbac55862380d503f3e22bd9103126f3e0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-12 15:42:52 +0900

    Impl

--- crates/swc_common/src/collections.rs
+++ crates/swc_common/src/collections.rs
@@ -1,5 +1,27 @@
-use std::collections::{HashMap, HashSet};
+#[cfg(not(feature = "fasthash"))]
+use self::ahash::*;
+#[cfg(feature = "fasthash")]
+pub use self::fxhash::*;
 
-pub type AHashMap<K, V> = HashMap<K, V, ahash::RandomState>;
+#[cfg(feature = "fasthash")]
+mod fxhash {
+    use std::{
+        collections::{HashMap, HashSet},
+        hash::BuildHasherDefault,
+    };
 
-pub type AHashSet<V> = HashSet<V, ahash::RandomState>;
+    use rustc_hash::FxHasher;
+
+    pub type AHashMap<K, V> = HashMap<K, V, BuildHasherDefault<FxHasher>>;
+
+    pub type AHashSet<V> = HashSet<V, BuildHasherDefault<FxHasher>>;
+}
+
+#[cfg(not(feature = "fasthash"))]
+mod ahash {
+    use std::collections::{HashMap, HashSet};
+
+    pub type AHashMap<K, V> = HashMap<K, V, ahash::RandomState>;
+
+    pub type AHashSet<V> = HashSet<V, ahash::RandomState>;
+}
