f3c8149e23c20b2c0003d84981240c698f66f6f5
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-14 05:43:03 +0900

    Document more

--- crates/swc/examples/minify.rs
+++ crates/swc/examples/minify.rs
@@ -1,1 +1,46 @@
+use std::{path::Path, sync::Arc};
 
+use anyhow::Context;
+use swc::{self, config::JsMinifyOptions, try_with_handler, HandlerOpts};
+use swc_common::SourceMap;
+
+fn main() {
+    let cm = Arc::<SourceMap>::default();
+
+    let c = swc::Compiler::new(cm.clone());
+
+    let output = try_with_handler(
+        cm.clone(),
+        HandlerOpts {
+            ..Default::default()
+        },
+        |handler| {
+            let fm = cm
+                .load_file(Path::new("examples/minify-input.js"))
+                .expect("failed to load file");
+
+            c.minify(
+                fm,
+                handler,
+                &JsMinifyOptions {
+                    compress: true.into(),
+                    mangle: true.into(),
+                    format: Default::default(),
+                    ecma: Default::default(),
+                    keep_classnames: Default::default(),
+                    keep_fnames: Default::default(),
+                    module: Default::default(),
+                    safari10: Default::default(),
+                    toplevel: Default::default(),
+                    source_map: Default::default(),
+                    output_path: Default::default(),
+                    inline_sources_content: Default::default(),
+                },
+            )
+            .context("failed to minify")
+        },
+    )
+    .unwrap();
+
+    println!("{}", output.code);
+}
--- crates/swc/examples/transform.rs
+++ crates/swc/examples/transform.rs
@@ -1,7 +1,7 @@
 use std::{path::Path, sync::Arc};
 
 use anyhow::Context;
-use swc::{self, config::Options, try_with_handler};
+use swc::{self, config::Options, try_with_handler, HandlerOpts};
 use swc_common::SourceMap;
 
 fn main() {
@@ -9,20 +9,26 @@ fn main() {
 
     let c = swc::Compiler::new(cm.clone());
 
-    let output = try_with_handler(cm.clone(), false, |handler| {
-        let fm = cm
-            .load_file(Path::new("examples/transform-input.js"))
-            .expect("failed to load file");
+    let output = try_with_handler(
+        cm.clone(),
+        HandlerOpts {
+            ..Default::default()
+        },
+        |handler| {
+            let fm = cm
+                .load_file(Path::new("examples/transform-input.js"))
+                .expect("failed to load file");
 
-        c.process_js_file(
-            fm,
-            handler,
-            &Options {
-                ..Default::default()
-            },
-        )
-        .context("failed to process file")
-    })
+            c.process_js_file(
+                fm,
+                handler,
+                &Options {
+                    ..Default::default()
+                },
+            )
+            .context("failed to process file")
+        },
+    )
     .unwrap();
 
     println!("{}", output.code);
