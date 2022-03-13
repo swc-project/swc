dac84b98e38bd546cb9c9df72e3ad8d08b9550d9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-14 07:07:34 +0900

    doc(api/rust): Add examples (#4002)

---
+++ crates/swc/examples/transform_error.rs
@@ -0,0 +1,36 @@
+use std::sync::Arc;
+
+use anyhow::Context;
+use swc::{self, config::Options, try_with_handler};
+use swc_common::{errors::ColorConfig, FileName, SourceMap};
+
+fn main() {
+    let cm = Arc::<SourceMap>::default();
+
+    let c = swc::Compiler::new(cm.clone());
+
+    let output = try_with_handler(
+        cm.clone(),
+        swc::HandlerOpts {
+            // Auto is default, but for it's an example.
+            // You can use the env var named `NO_COLOR` to control this.
+            color: ColorConfig::Auto,
+            skip_filename: false,
+        },
+        |handler| {
+            let fm = cm.new_source_file(FileName::Custom("foo.js".into()), "this ?= foo".into());
+
+            c.process_js_file(
+                fm,
+                handler,
+                &Options {
+                    ..Default::default()
+                },
+            )
+            .context("failed to process file")
+        },
+    )
+    .expect("anyhow will give you good error message");
+
+    println!("{}", output.code);
+}
