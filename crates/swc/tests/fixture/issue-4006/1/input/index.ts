e67afd9a1fb266df0bbd0cfbf97c7735fb6e1e04
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-14 18:01:26 +0900

    Add a test case

---
+++ crates/swc/tests/fixture/issue-4006/1/input/index.ts
@@ -0,0 +1,21 @@
+import { html, css, LitElement } from 'lit';
+
+export class App extends LitElement {
+    name: string;
+
+    static styles = css`p { color: blue }`;
+
+    static properties = {
+        name: { type: String },
+    };
+
+    constructor() {
+        super();
+        this.name = 'Somebody';
+    }
+
+    render() {
+        return html`<p>Hello, ${this.name}!</p>`;
+    }
+}
+customElements.define('app', App);
