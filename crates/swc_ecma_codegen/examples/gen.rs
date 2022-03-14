0cb22e850716e3835ea6dac540b03fba9974fcb8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-14 12:21:57 +0900

    Remove loop

--- crates/swc_ecma_codegen/examples/gen.rs
+++ crates/swc_ecma_codegen/examples/gen.rs
@@ -23,28 +23,27 @@ fn parse_and_gen(entry: &Path) {
             .parse_module()
             .expect("failed to parse input as a module");
 
-        for _ in 0..100 {
-            let code = {
-                let mut buf = vec![];
+        let code = {
+            let mut buf = vec![];
+            let mut srcmap = vec![];
 
-                {
-                    let mut emitter = Emitter {
-                        cfg: swc_ecma_codegen::Config {
-                            ..Default::default()
-                        },
-                        cm: cm.clone(),
-                        comments: None,
-                        wr: JsWriter::new(cm.clone(), "\n", &mut buf, None),
-                    };
+            {
+                let mut emitter = Emitter {
+                    cfg: swc_ecma_codegen::Config {
+                        ..Default::default()
+                    },
+                    cm: cm.clone(),
+                    comments: None,
+                    wr: JsWriter::new(cm, "\n", &mut buf, Some(&mut srcmap)),
+                };
 
-                    emitter.emit_module(&m).unwrap();
-                }
+                emitter.emit_module(&m).unwrap();
+            }
 
-                String::from_utf8_lossy(&buf).to_string()
-            };
+            String::from_utf8_lossy(&buf).to_string()
+        };
 
-            fs::write("output.js", &code).unwrap();
-        }
+        fs::write("output.js", &code).unwrap();
 
         Ok(())
     })
