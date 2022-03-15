9817773a1f01430dde2fd258a921db55c05001e3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-15 22:44:16 +0900

    Fix

--- crates/swc_common/src/syntax_pos/hygiene.rs
+++ crates/swc_common/src/syntax_pos/hygiene.rs
@@ -72,9 +72,9 @@ struct MarkData {
 )]
 pub struct MutableMarkContext(pub u32, pub u32, pub u32);
 
-/// List of proxy calls injected by the host in the plugin's runtime context.
-/// When related calls being executed inside of the plugin, it'll call these
-/// proxies instead which'll call actual host fn.
+// List of proxy calls injected by the host in the plugin's runtime context.
+// When related calls being executed inside of the plugin, it'll call these
+// proxies instead which'll call actual host fn.
 extern "C" {
     // Instead of trying to copy-serialize `Mark`, this fn directly consume
     // inner raw value as well as fn and let each context constrcuts struct
