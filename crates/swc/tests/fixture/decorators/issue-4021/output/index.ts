0c8c3d7d51e49ad9050224a8647ebc55a0262b35
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-15 18:14:47 +0900

    fix(es/decorator): Remove `noop_fold_type` (#4022)

---
+++ crates/swc/tests/fixture/decorators/issue-4021/output/index.ts
@@ -0,0 +1,8 @@
+var _class, _class1;
+var N;
+(function(N) {
+    let C1 = _class = foo((_class = class C1 {
+    }) || _class) || _class;
+})(N || (N = {}));
+let C2 = _class1 = foo((_class1 = class C2 {
+}) || _class1) || _class1;
