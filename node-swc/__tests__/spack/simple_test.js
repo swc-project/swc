8fd8fc89f0e68a3a22dd6e444d9152bcce1c5599
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-15 22:47:50 -0700

    dummy

--- node-swc/__tests__/spack/simple_test.js
+++ node-swc/__tests__/spack/simple_test.js
@@ -1,6 +1,30 @@
 const swc = require("../../../");
 const path = require('path');
 
+it('dummy', () => {
+    const { transformSync } = swc;
+
+    const input = `
+    export * from './createColors'
+    export * from './createColorsFromMap'
+    export * from './interfaces'
+    export * from './rgbHex'
+    export * from './rgbaString'
+    `;
+
+    let option = {
+        sourceMaps: true,
+        filename: 'C:\\github\\color-map\\ts\\index.ts',
+    }
+    const pass = transformSync(input, option);
+    const fail = transformSync(input, {
+        ...option,
+        module: { type: 'commonjs' },
+    });
+
+    expect(JSON.parse(pass.map)).toEqual(JSON.parse(fail.map));
+});
+
 
 it('should handle a simple case', async () => {
     const result = await swc.bundle({
