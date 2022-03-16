782e22b09adb308f49d35de8104e79ef2d2013c4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-16 15:41:16 +0900

    Add a test

--- node-swc/__tests__/transform/sourcemap_test.js
+++ node-swc/__tests__/transform/sourcemap_test.js
@@ -129,5 +129,30 @@ describe('soruceMaps: true in .swcrc', () => {
         expect(out.map).toBeUndefined();
     })
 
-})
+});
 
+describe('BytePos(0)', () => {
+    it('should work correctly', () => {
+        const { transformSync } = swc;
+
+        const input = `
+        export * from './createColors'
+        export * from './createColorsFromMap'
+        export * from './interfaces'
+        export * from './rgbHex'
+        export * from './rgbaString'
+        `;
+
+        let option = {
+            sourceMaps: true,
+            filename: 'C:\\github\\color-map\\ts\\index.ts',
+        }
+        const pass = transformSync(input, option);
+        const fail = transformSync(input, {
+            ...option,
+            module: { type: 'commonjs' },
+        });
+
+        expect(JSON.parse(pass.map)).toEqual(JSON.parse(fail.map));
+    });
+});
