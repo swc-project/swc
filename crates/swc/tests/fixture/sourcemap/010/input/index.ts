26f5e5af1ae58d1e60ed99030aa73219ff44c792
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-17 16:23:52 +0900

    Add a test

---
+++ crates/swc/tests/fixture/sourcemap/010/input/index.ts
@@ -0,0 +1,5 @@
+export * from './createColors'
+export * from './createColorsFromMap'
+export type { Alpha, ColorMap, RGB, RGBA } from './interfaces'
+export * from './rgbHex'
+export * from './rgbaString'
