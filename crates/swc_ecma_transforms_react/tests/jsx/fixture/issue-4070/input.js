c0aedb81948b9736e975add6e10441296a27f87b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-18 06:16:47 +0900

    Reduce

--- crates/swc_ecma_transforms_react/tests/jsx/fixture/issue-4070/input.js
+++ crates/swc_ecma_transforms_react/tests/jsx/fixture/issue-4070/input.js
@@ -1,12 +1,3 @@
-import React from "react";
-import { render } from "react-dom";
-
-const array = ["one", "two", "three", "four"];
-
-const WrapperWhereMagicHappens = (props) => {
-    console.log("wrapper props", props);
-    return <div>{props.children}</div>;
-};
 const ChildrenFail = (props) => {
     return array.map((label) => (
         <WrapperWhereMagicHappens {...props} key={label}>
@@ -15,45 +6,3 @@ const ChildrenFail = (props) => {
         </WrapperWhereMagicHappens>
     ));
 };
-
-const ChildrenOK = (props) => {
-    return array.map((label) => (
-        <WrapperWhereMagicHappens key={label} {...props}>
-            <h2>{label}</h2>
-            {/*<div>{console.log(props.children) || props.children}</div>*/}
-        </WrapperWhereMagicHappens>
-    ));
-};
-
-// const ChildrenButNotInAnArray = (props) => {
-//     return (
-//         <>
-//             <WrapperWhereMagicHappens {...props} key="foo">
-//                 <h2>Foo</h2>
-//                 <div>
-//                     {console.log("no array", props.children) || props.children}
-//                 </div>
-//             </WrapperWhereMagicHappens>
-//         </>
-//     );
-// };
-
-const App = () => {
-    return (
-        <>
-            <ChildrenFail foo={1} bar={2}>
-                <p>I'm a child</p>
-            </ChildrenFail>
-            <ChildrenOK foo={1} bar={2}>
-                <p>I'm a child</p>
-            </ChildrenOK>
-        </>
-    );
-};
-
-const main = () => {
-    const root = document.getElementById("root");
-    render(<App />, root);
-};
-
-main();
