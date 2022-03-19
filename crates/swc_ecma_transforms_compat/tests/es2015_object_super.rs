32c9112cb6698ce160c951c286725ce6b299061c
evilebottnawi <sheo13666q@gmail.com>
2022-03-19 21:55:19 +0300

    test: fix

--- crates/swc_ecma_transforms_compat/tests/es2015_object_super.rs
+++ crates/swc_ecma_transforms_compat/tests/es2015_object_super.rs
@@ -227,14 +227,14 @@ f0.prototype = {
 var _obj;
 function f0() {}
 f0.prototype = _obj = {
-    name: "Nicholas",
+    name: 'Nicholas',
     age: 29,
-    job: "Software Engineer",
+    job: 'Software Engineer',
     sayName: function sayName() {
         v0[args](1, {
             v9: (v7)=>_get(_getPrototypeOf(_obj), "v3", this).call(this, v27),
             foo: a,
-            done: "a"
+            done: 'a'
         });
     }
 };    
