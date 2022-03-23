03c7396c67123d26d135fdce7a38dbdeb0e8b400
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-03-23 20:48:30 +0900

    Remove unused

--- crates/swc/tests/fixture/paths/.issue-2050/output/B.ts
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.B = void 0;
-const B = 400;
-exports.B = B;
--- crates/swc/tests/fixture/paths/issue-2844/output/src/index.ts
+++ crates/swc/tests/fixture/paths/issue-2844/output/src/index.ts
@@ -1,6 +1,6 @@
 "use strict";
 require("./core/module/moduleFile");
-require("./core/utils/index");
+require("./core/utils");
 require("./core/utilFile");
 require("./utils");
 console.log('SUCCESS');
