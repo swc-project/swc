a65804a023e98795cceddcf73942bfd481cbde57
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-30 17:18:12 -0700

    docs(swc/plugin_runner): short doc for imported_fn

--- crates/swc_plugin_runner/src/imported_fn/mod.rs
+++ crates/swc_plugin_runner/src/imported_fn/mod.rs
@@ -1,6 +1,45 @@
 //! Functions for syntax_pos::hygiene imported into the guests (plugin) runtime
 //! allows interop between host's state to plugin. When guest calls these fn,
 //! it'll be executed in host's memory space.
+/*
+* Below diagram shows one reference example how guest does trampoline between
+* host's memory space.
+* ┌───────────────────────────────────────┐    ┌─────────────────────────────────────────────┐
+* │Host (SWC/core)                        │    │Plugin (wasm)                                │
+* │                                       │    │  ┌──────────────────────────────────────┐   │
+* │                                       │    │  │PluginCommentsProxy                   │   │
+* │  ┌────────────────────────────────┐   │    │  │                                      │   │
+* │  │get_leading_comments_proxy()    │   │    │  │ ┌────────────────────────────────┐   │   │
+* │  │                                ◀───┼────┼──┼─┤get_leading()                   │   │   │
+* │  │                                │   │    │  │ │                                │   │   │
+* │  │                                │   │    │  │ │ ┌──────────────────────────┐   │   │   │
+* │  └────────────────▲───────────────┴───┼──┬─┼──┼─┼─▶CommentsVecPtr(ptr, len)  │   │   │   │
+* │                   │                   │  │ │  │ │ │                          │   │   │   │
+* │                   │                   │  │ │  │ │ └─────────────┬────────────┘   │   │   │
+* │  ┌────────────────────────────────┐   │  │ │  │ │               │                │   │   │
+* │  │COMMENTS                        │   │  │ │  │ │ ┌─────────────▼────────────┐   │   │   │
+* │  │                                │   │  │ │  │ │ │Vec<Comments>             │   │   │   │
+* │  │                                │   │  └─┼──┼─┼─▶                          │   │   │   │
+* │  └────────────────────────────────┘   │    │  │ │ └──────────────────────────┘   │   │   │
+* │                                       │    │  │ │                                │   │   │
+* │                                       │    │  │ └────────────────────────────────┘   │   │
+* │                                       │    │  └──────────────────────────────────────┘   │
+* └───────────────────────────────────────┘    └─────────────────────────────────────────────┘
+
+* 1. Plugin calls `PluginCommentsProxy::get_leading()`. PluginCommentsProxy is
+* a struct constructed in plugin's memory space.
+* 2. `get_leading()` internally calls `__get_leading_comments_proxy`, which is
+* imported fn `get_leading_comments_proxy` exists in the host.
+* 3. Host access necessary values in its memory space (COMMENTS)
+* 4. Host copies value to be returned into plguin's memory space. Memory
+* allocation for the value should be manually performed.
+* 5. Host completes imported fn, `PluginCommentsProxy::get_leading()` now can
+* read, deserialize memory host wrote.
+* - In case of `get_leading`, returned value is non-deterministic vec
+* (`Vec<Comments>`) guest cannot preallocate with specific length. Instead,
+* guest passes a fixed size struct (CommentsVecPtr), once host allocates
+* actual vec into guest it'll write pointer to the vec into the struct.
+*/
 
 use std::sync::Arc;
 
