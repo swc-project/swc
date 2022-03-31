eecda21d9a36fc2c4b98c2cae4933adb300cdaa0
OJ Kwon <kwon.ohjoong@gmail.com>
2022-03-31 00:11:31 -0700

    fix(node-swc): Initialize custom trace subscriber only once (#4209)

--- crates/node/src/util.rs
+++ crates/node/src/util.rs
@@ -7,7 +7,11 @@ use anyhow::{anyhow, Context, Error};
 use napi::{Env, Status};
 use serde::de::DeserializeOwned;
 use swc::try_with_handler;
-use swc_common::{errors::Handler, sync::Lrc, SourceMap};
+use swc_common::{
+    errors::Handler,
+    sync::{Lrc, OnceCell},
+    SourceMap,
+};
 use tracing::instrument;
 use tracing_chrome::ChromeLayerBuilder;
 use tracing_subscriber::{
@@ -15,6 +19,7 @@ use tracing_subscriber::{
 };
 
 static TARGET_TRIPLE: &str = include_str!(concat!(env!("OUT_DIR"), "/triple.txt"));
+static CUSTOM_TRACE_SUBSCRIBER: OnceCell<bool> = OnceCell::new();
 
 #[napi]
 pub fn get_target_triple() -> napi::Result<String> {
@@ -26,23 +31,28 @@ pub fn init_custom_trace_subscriber(
     mut env: Env,
     trace_out_file_path: Option<String>,
 ) -> napi::Result<()> {
-    let mut layer = ChromeLayerBuilder::new().include_args(true);
-    if let Some(trace_out_file) = trace_out_file_path {
-        layer = layer.file(trace_out_file);
-    }
+    CUSTOM_TRACE_SUBSCRIBER.get_or_init(|| {
+        let mut layer = ChromeLayerBuilder::new().include_args(true);
+        if let Some(trace_out_file) = trace_out_file_path {
+            layer = layer.file(trace_out_file);
+        }
 
-    let (chrome_layer, guard) = layer.build();
-    tracing_subscriber::registry()
-        .with(chrome_layer.with_filter(filter::filter_fn(|metadata| {
-            !metadata.target().contains("cranelift") && !metadata.name().contains("log ")
-        })))
-        .try_init()
-        .expect("Failed to register tracing subscriber");
+        let (chrome_layer, guard) = layer.build();
+        tracing_subscriber::registry()
+            .with(chrome_layer.with_filter(filter::filter_fn(|metadata| {
+                !metadata.target().contains("cranelift") && !metadata.name().contains("log ")
+            })))
+            .try_init()
+            .expect("Failed to register tracing subscriber");
 
-    env.add_env_cleanup_hook(guard, |flush_guard| {
-        flush_guard.flush();
-        drop(flush_guard);
-    })?;
+        env.add_env_cleanup_hook(guard, |flush_guard| {
+            flush_guard.flush();
+            drop(flush_guard);
+        })
+        .expect("Should able to initialize cleanup for custom trace subscriber");
+
+        true
+    });
 
     Ok(())
 }
