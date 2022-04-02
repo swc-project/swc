c017e93ef96585d0a2134bb494ca2384b8e0e9cf
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-02 15:11:15 +0900

    Add a test

---
+++ crates/swc/tests/fixture/issues-4xxx/issue-4224/3/input/index.js
@@ -0,0 +1,12 @@
+class A {
+    method() {
+        this.foo = async () => {
+            this.x();
+        };
+        this.bar = async () => {
+            this.x();
+        };
+    }
+}
+
+console.log(A); 
