073953a4a59c47977088799b79941e26f98a55a9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-03 21:35:25 +0900

    Update test refs

--- crates/swc/tests/tsc-references/controlFlowIIFE_es2015.2.minified.js
+++ crates/swc/tests/tsc-references/controlFlowIIFE_es2015.2.minified.js
@@ -1,6 +1,6 @@
 import * as swcHelpers from "@swc/helpers";
 let maybeNumber;
-maybeNumber = 1, maybeNumber++, void 0 !== maybeNumber && maybeNumber++;
+maybeNumber = 1, ++maybeNumber, maybeNumber++;
 let test;
 if (!test) throw new Error('Test is not defined');
 test.slice(1);
