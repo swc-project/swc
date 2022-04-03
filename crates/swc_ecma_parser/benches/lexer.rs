69c9d47dea1313dce4ef2f5baf59729ce9d6a73c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-03 23:42:50 +0900

    One more

--- crates/swc_ecma_parser/benches/lexer.rs
+++ crates/swc_ecma_parser/benches/lexer.rs
@@ -1,92 +1,10 @@
-#![feature(test)]
-#![feature(bench_black_box)]
-
 extern crate swc_node_base;
-extern crate test;
 
-use std::hint::black_box;
-
+use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
 use swc_common::FileName;
 use swc_ecma_parser::{lexer::Lexer, StringInput, Syntax};
-use test::Bencher;
-
-#[bench]
-fn colors(b: &mut Bencher) {
-    // Copied from ratel-rust
-    bench_module(b, Default::default(), include_str!("../colors.js"))
-}
-
-#[bench]
-fn angular(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/angular-1.2.5.js"),
-    )
-}
-
-#[bench]
-fn backbone(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/backbone-1.1.0.js"),
-    )
-}
-
-#[bench]
-fn jquery(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/jquery-1.9.1.js"),
-    )
-}
-
-#[bench]
-fn jquery_mobile(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/jquery.mobile-1.4.2.js"),
-    )
-}
-
-#[bench]
-fn mootools(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/mootools-1.4.5.js"),
-    )
-}
-
-#[bench]
-fn underscore(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/underscore-1.5.2.js"),
-    )
-}
-
-#[bench]
-fn three(b: &mut Bencher) {
-    bench_module(
-        b,
-        Default::default(),
-        include_str!("./files/three-0.138.3.js"),
-    )
-}
-
-#[bench]
-fn yui(b: &mut Bencher) {
-    bench_module(b, Default::default(), include_str!("./files/yui-3.12.0.js"))
-}
 
 fn bench_module(b: &mut Bencher, syntax: Syntax, src: &'static str) {
-    b.bytes = src.len() as _;
-
     let _ = ::testing::run_test(false, |cm, _| {
         let fm = cm.new_source_file(FileName::Anon, src.into());
 
@@ -99,3 +17,72 @@ fn bench_module(b: &mut Bencher, syntax: Syntax, src: &'static str) {
         Ok(())
     });
 }
+
+fn bench_files(c: &mut Criterion) {
+    c.bench_function("colors", |b| {
+        // Copied from ratel-rust
+        bench_module(b, Default::default(), include_str!("../colors.js"))
+    });
+
+    c.bench_function("angular", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/angular-1.2.5.js"),
+        )
+    });
+
+    c.bench_function("backbone", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/backbone-1.1.0.js"),
+        )
+    });
+
+    c.bench_function("jqeury", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/jquery-1.9.1.js"),
+        )
+    });
+
+    c.bench_function("jquery mobile", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/jquery.mobile-1.4.2.js"),
+        )
+    });
+    c.bench_function("mootools", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/mootools-1.4.5.js"),
+        )
+    });
+
+    c.bench_function("underscore", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/underscore-1.5.2.js"),
+        )
+    });
+
+    c.bench_function("three", |b| {
+        bench_module(
+            b,
+            Default::default(),
+            include_str!("./files/three-0.138.3.js"),
+        )
+    });
+
+    c.bench_function("yui", |b| {
+        bench_module(b, Default::default(), include_str!("./files/yui-3.12.0.js"))
+    });
+}
+
+criterion_group!(benches, bench_files);
+criterion_main!(benches);
