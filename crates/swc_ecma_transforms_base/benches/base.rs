3eeef15d50c3dcd1121607a26a2b23e5b1a881fb
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-03 23:49:18 +0900

    One more

--- crates/swc_ecma_transforms_base/benches/base.rs
+++ crates/swc_ecma_transforms_base/benches/base.rs
@@ -1,25 +1,19 @@
-#![feature(test)]
-
-extern crate test;
-
 #[global_allocator]
 static GLOBAL: System = System;
 
 use std::alloc::System;
 
+use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
 use swc_common::{chain, FileName};
 use swc_ecma_parser::{Parser, StringInput, Syntax};
 use swc_ecma_transforms_base::helpers;
 use swc_ecma_visit::FoldWith;
-use test::Bencher;
 
 static SOURCE: &str = include_str!("assets/AjaxObservable.ts");
 
 /// Benchmark a folder
 macro_rules! tr {
     ($b:expr, $tr:expr) => {
-        $b.bytes = SOURCE.len() as _;
-
         let _ = ::testing::run_test(false, |cm, _| {
             let fm = cm.new_source_file(FileName::Anon, SOURCE.into());
 
@@ -34,7 +28,7 @@ macro_rules! tr {
 
                 $b.iter(|| {
                     let module = module.clone();
-                    test::black_box(module.fold_with(&mut tr))
+                    black_box(module.fold_with(&mut tr))
                 });
                 Ok(())
             })
@@ -42,25 +36,31 @@ macro_rules! tr {
     };
 }
 
-#[bench]
 fn resolver(b: &mut Bencher) {
     tr!(b, swc_ecma_transforms_base::resolver::resolver);
 }
 
-#[bench]
 fn fixer(b: &mut Bencher) {
     tr!(b, || swc_ecma_transforms_base::fixer::fixer(None));
 }
 
-#[bench]
 fn hygiene(b: &mut Bencher) {
     tr!(b, swc_ecma_transforms_base::hygiene::hygiene);
 }
 
-#[bench]
 fn resolver_with_hygiene(b: &mut Bencher) {
     tr!(b, || chain!(
         swc_ecma_transforms_base::resolver::resolver(),
         swc_ecma_transforms_base::hygiene::hygiene()
     ));
 }
+
+fn bench_cases(c: &mut Criterion) {
+    c.bench_function("resolver", resolver);
+    c.bench_function("fixer", fixer);
+    c.bench_function("hygiene", hygiene);
+    c.bench_function("resolver_with_hygiene", resolver_with_hygiene);
+}
+
+criterion_group!(benches, bench_cases);
+criterion_main!(benches);
