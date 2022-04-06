872c94f275f530f36e015baa242b11d2178ca936
alexander.akait <sheo13666q@gmail.com>
2022-04-06 22:43:38 +0300

    fix: bug

--- crates/swc_html_parser/src/lexer/mod.rs
+++ crates/swc_html_parser/src/lexer/mod.rs
@@ -4871,6 +4871,64 @@ where
                             // TODO handle exception above
 
                             let mut temporary_buffer = String::new();
+                            let cr = match cr {
+                                // 0x80	0x20AC	EURO SIGN (€)
+                                0x80 => 0x20ac,
+                                // 0x82	0x201A	SINGLE LOW-9 QUOTATION MARK (‚)
+                                0x82 => 0x201a,
+                                // 0x83	0x0192	LATIN SMALL LETTER F WITH HOOK (ƒ)
+                                0x83 => 0x0192,
+                                // 0x84	0x201E	DOUBLE LOW-9 QUOTATION MARK („)
+                                0x84 => 0x201e,
+                                // 0x85	0x2026	HORIZONTAL ELLIPSIS (…)
+                                0x85 => 0x2026,
+                                // 0x86	0x2020	DAGGER (†)
+                                0x86 => 0x2020,
+                                // 0x87	0x2021	DOUBLE DAGGER (‡)
+                                0x87 => 0x2021,
+                                // 0x88	0x02C6	MODIFIER LETTER CIRCUMFLEX ACCENT (ˆ)
+                                0x88 => 0x02c6,
+                                // 0x89	0x2030	PER MILLE SIGN (‰)
+                                0x89 => 0x2030,
+                                // 0x8A	0x0160	LATIN CAPITAL LETTER S WITH CARON (Š)
+                                0x8a => 0x0160,
+                                // 0x8B	0x2039	SINGLE LEFT-POINTING ANGLE QUOTATION MARK (‹)
+                                0x8b => 0x2039,
+                                // 0x8C	0x0152	LATIN CAPITAL LIGATURE OE (Œ)
+                                0x8c => 0x0152,
+                                // 0x8E	0x017D	LATIN CAPITAL LETTER Z WITH CARON (Ž)
+                                0x8e => 0x017d,
+                                // 0x91	0x2018	LEFT SINGLE QUOTATION MARK (‘)
+                                0x91 => 0x2018,
+                                // 0x92	0x2018	RIGHT SINGLE QUOTATION MARK (’)
+                                0x92 => 0x2019,
+                                // 0x93	0x201C	LEFT DOUBLE QUOTATION MARK (“)
+                                0x93 => 0x201c,
+                                // 0x94	0x201D	RIGHT DOUBLE QUOTATION MARK (”)
+                                0x94 => 0x201d,
+                                // 0x95	0x2022	BULLET (•)
+                                0x95 => 0x2022,
+                                // 0x96	0x2013	EN DASH (–)
+                                0x96 => 0x2013,
+                                // 0x97	0x2014	EM DASH (—)
+                                0x97 => 0x2014,
+                                // 0x98	0x02DC	SMALL TILDE (˜)
+                                0x98 => 0x02dc,
+                                // 0x99	0x2122	TRADE MARK SIGN (™)
+                                0x99 => 0x2122,
+                                // 0x9A	0x0161	LATIN SMALL LETTER S WITH CARON (š)
+                                0x9a => 0x0161,
+                                // 0x9B	0x203A	SINGLE RIGHT-POINTING ANGLE QUOTATION MARK (›)
+                                0x9b => 0x203a,
+                                // 0x9C	0x0153	LATIN SMALL LIGATURE OE (œ)
+                                0x9c => 0x0153,
+                                // 0x9E	0x017E	LATIN SMALL LETTER Z WITH CARON (ž)
+                                0x9e => 0x017e,
+                                // 0x9F	0x0178	LATIN CAPITAL LETTER Y WITH DIAERESIS (Ÿ)
+                                0x9f => 0x0178,
+                                _ => cr,
+                            };
+
                             let c = match char::from_u32(cr) {
                                 Some(c) => c,
                                 _ => {
