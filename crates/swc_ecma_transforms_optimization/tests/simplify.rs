4bc8760398b01d9c45a16eda362f5ae1fc23d02d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-08 22:04:24 +0900

    Add a test

--- crates/swc_ecma_transforms_optimization/tests/simplify.rs
+++ crates/swc_ecma_transforms_optimization/tests/simplify.rs
@@ -633,6 +633,21 @@ test!(
 
 test!(
     Syntax::default(),
+    |_| dead_branch_remover(),
+    issue_4272,
+    "
+    function oe() {
+        var e, t;
+        return t !== i && (e, t = t), e = t;
+    }
+    ",
+    "
+    
+    "
+);
+
+test!(
+    Syntax::default(),
     |_| chain!(
         resolver(),
         Repeat::new(chain!(inlining(Default::default()), dead_branch_remover()))
