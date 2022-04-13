a032425b74bc42b1ed2f45caae7148a3c80aea21
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-13 21:02:17 +0900

    Rename

--- crates/swc_ecma_minifier/src/macros.rs
+++ crates/swc_ecma_minifier/src/macros.rs
@@ -1,8 +1,21 @@
-macro_rules! log_change {
+/// Used when something is modified.
+macro_rules! report {
     ($($tt:tt)+) => {{
         tracing::debug!(
             kind = "change",
             $($tt)*
         );
+    }};
+}
+
+/// Used when a function decided to give up.
+macro_rules! log_abort {
+    ($($tt:tt)+) => {{
+        if cfg!(feature = "debug") {
+            tracing::trace!(
+                kind = "abort",
+                $($tt)*
+            );
+        }
     }};
 }
