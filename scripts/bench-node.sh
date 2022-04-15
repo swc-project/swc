3050c392bb246086eac010958940e4e19ee88790
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-16 06:01:32 +0900

    antd

--- scripts/bench-node.sh
+++ scripts/bench-node.sh
@@ -5,3 +5,7 @@ set -eu
 
 echo "Binary load time"
 hyperfine --warmup 10 --runs 5 'node ./node-swc/benches/load.mjs'
+
+echo "Load + minify antd"
+hyperfine --warmup 5 --runs 5 "RAYON_NUM_THREADS=2 node ./node-swc/benches/minify.mjs $PWD/crates/swc_ecma_minifier/benches/full/antd.js"
+
