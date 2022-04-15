a1a28990c0ee11896a06b930ba919b0a3fddb591
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-15 19:23:04 +0900

    Inline before cloning

--- crates/swc_ecma_minifier/src/compress/optimize/inline.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -435,6 +435,8 @@ where
             // Inline very simple functions.
             match decl {
                 Decl::Fn(f) if self.options.inline >= 2 && f.ident.sym != *"arguments" => {
+                    self.vars.inline_with_multi_replacer(&mut f.function.body);
+
                     match &f.function.body {
                         Some(body) => {
                             if !UsageFinder::find(&i, body)
