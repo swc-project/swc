f82076044a3afa6f07cf3198006c14ffe7687ace
alexander.akait <sheo13666q@gmail.com>
2022-04-15 16:00:11 +0300

    feat: add AST nodes

--- crates/swc_html_ast/src/base.rs
+++ crates/swc_html_ast/src/base.rs
@@ -1,38 +1,55 @@
+use is_macro::Is;
 use swc_atoms::JsWord;
-use swc_common::{ast_node, Span};
-
-use crate::TokenAndSpan;
+use swc_common::{ast_node, EqIgnoreSpan, Span};
 
 #[ast_node("Document")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Document {
     pub span: Span,
-    pub children: Vec<TokenAndSpan>,
+    pub children: Vec<Node>,
+}
+
+#[ast_node]
+#[derive(Eq, Hash, Is, EqIgnoreSpan)]
+pub enum Node {
+    #[tag("DocumentType")]
+    DocumentType(DocumentType),
+    #[tag("Element")]
+    Element(Element),
+    #[tag("Text")]
+    Text(Text),
+    #[tag("Comment")]
+    Comment(Comment),
 }
 
 #[ast_node("DocumentType")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct DocumentType {
     pub span: Span,
-    pub name: JsWord,
-    pub public_id: JsWord,
-    pub system_id: JsWord,
+    pub name: Option<JsWord>,
+    pub public_id: Option<JsWord>,
+    pub system_id: Option<JsWord>,
 }
 
 #[ast_node("Element")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Element {
     pub span: Span,
     pub tag_name: JsWord,
     pub start_tag: Option<Tag>,
-    pub children: Vec<Element>,
+    pub children: Vec<Node>,
     pub end_tag: Option<Tag>,
 }
 
 #[ast_node("Tag")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Tag {
     pub span: Span,
     pub attributes: Vec<Attribute>,
 }
 
 #[ast_node("Attribute")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Attribute {
     pub span: Span,
     pub name: JsWord,
@@ -40,13 +57,15 @@ pub struct Attribute {
 }
 
 #[ast_node("Text")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Text {
     pub span: Span,
     pub value: JsWord,
 }
 
 #[ast_node("Comment")]
+#[derive(Eq, Hash, EqIgnoreSpan)]
 pub struct Comment {
     pub span: Span,
-    pub value: JsWord,
+    pub data: JsWord,
 }
--- crates/swc_html_visit/src/lib.rs
+++ crates/swc_html_visit/src/lib.rs
@@ -1,22 +1,66 @@
 #![deny(clippy::all)]
 #![allow(clippy::ptr_arg)]
 
+use swc_atoms::JsWord;
 use swc_common::Span;
-use swc_html_ast::*;
+use swc_html_ast::{Node as AstNode, *};
 use swc_visit::define;
 
 /// Visitable nodes.
 pub trait Node {}
 
 define!({
+    pub struct Document {
+        pub span: Span,
+        pub children: Vec<AstNode>,
+    }
+
+    pub enum AstNode {
+        DocumentType(DocumentType),
+        Element(Element),
+        Text(Text),
+        Comment(Comment),
+    }
+
+    pub struct DocumentType {
+        pub span: Span,
+        pub name: Option<JsWord>,
+        pub public_id: Option<JsWord>,
+        pub system_id: Option<JsWord>,
+    }
+
+    pub struct Element {
+        pub span: Span,
+        pub tag_name: JsWord,
+        pub start_tag: Option<Tag>,
+        pub children: Vec<AstNode>,
+        pub end_tag: Option<Tag>,
+    }
+
+    pub struct Tag {
+        pub span: Span,
+        pub attributes: Vec<Attribute>,
+    }
+
+    pub struct Attribute {
+        pub span: Span,
+        pub name: JsWord,
+        pub value: JsWord,
+    }
+
+    pub struct Text {
+        pub span: Span,
+        pub value: JsWord,
+    }
+
+    pub struct Comment {
+        pub span: Span,
+        pub data: JsWord,
+    }
+
     pub struct TokenAndSpan {
         pub span: Span,
         pub token: Token,
-    }
-
-    pub struct Document {
-        pub span: Span,
-        pub children: Vec<TokenAndSpan>,
     }
 });
 
