94e89f78b152ccbad57ef78504bcde621f3ed0f4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-15 19:23:29 +0900

    Update test refs

---
+++ crates/swc_ecma_minifier/tests/compress/fixture/next/36127/2/2/output.js
@@ -0,0 +1,6 @@
+function regexCheck(regex) {
+    return function(code) {
+        return null !== code && regex.test(String.fromCharCode(code));
+    };
+}
+console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo')), console.log(regexCheck('Foo'));
