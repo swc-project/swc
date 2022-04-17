8113bdc025da986b4d38e02c412ab36d62147816
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-18 06:48:56 +0900

    Add `eval_tpl_as_str`

--- crates/swc_ecma_minifier/src/compress/pure/evaluate.rs
+++ crates/swc_ecma_minifier/src/compress/pure/evaluate.rs
@@ -582,6 +582,13 @@ fn to_trivial_exprs(e: &mut Expr) -> Vec<&mut Expr> {
 
 /// Evaluation of strings.
 impl Pure<'_> {
+    pub(super) fn eval_tpl_as_str(&mut self, e: &mut Expr) {
+        let tpl = match e {
+            Expr::Tpl(e) => e,
+            _ => return,
+        };
+    }
+
     /// Handle calls on string literals, like `'foo'.toUpperCase()`.
     pub(super) fn eval_str_method_call(&mut self, e: &mut Expr) {
         if !self.options.evaluate {
