14c74f50c0b21d65d9604b58a2ce348baeb1baa3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-17 19:16:40 +0900

    chore(es/minifier): Organize scripts (#4347)

--- crates/swc_ecma_minifier/scripts/cdnjs/download-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-# Work in .data directory
-cd .data
-
-git clone https://github.com/cdnjs/cdnjs.git --depth 1 || true
-
-cd cdnjs
--- crates/swc_ecma_minifier/scripts/deopt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -u
-
-UPDATE=1 cargo test --test compress deopt -- --nocapture
-ls -al ./tests/full/private/deopt/output.js
-
