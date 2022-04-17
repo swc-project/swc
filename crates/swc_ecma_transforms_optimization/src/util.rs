da6fa602310c0a56825723d83585fb258b88c931
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-18 01:24:00 +0900

    perf(es): Disable wrong parallelization (#4354)

--- crates/swc_ecma_transforms_optimization/src/util.rs
+++ crates/swc_ecma_transforms_optimization/src/util.rs
@@ -1,5 +1,24 @@
+use std::ops::Deref;
+
 #[cfg(feature = "concurrent")]
 pub(crate) type Readonly<T> = std::sync::Arc<T>;
 
 #[cfg(not(feature = "concurrent"))]
-pub(crate) type Readonly<T> = T;
+#[derive(Default)]
+pub(crate) struct Readonly<T>(T);
+
+#[cfg(not(feature = "concurrent"))]
+impl<T> Deref for Readonly<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(not(feature = "concurrent"))]
+impl<T> From<T> for Readonly<T> {
+    fn from(v: T) -> Self {
+        Self(v)
+    }
+}
