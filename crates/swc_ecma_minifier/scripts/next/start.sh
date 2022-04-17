b56c4ef89a5ec2120883f7da77c32b8c8568b11f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-17 19:47:54 +0900

    script

--- crates/swc_ecma_minifier/scripts/next/start.sh
+++ crates/swc_ecma_minifier/scripts/next/start.sh
@@ -13,3 +13,22 @@
 #  - remove some dependencies (`next`, `react`, `react-dom`)
 #  - yarn next build examples/foo
 #  - yarn next start examples/foo
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+dir="$1"
+
+# Ensure that next-swc is up to date
+(cd ./packages/next-swc && yarn build-native)
+
+# Install dependencies
+(cd $dir && yarn)
+
+# Remove some packages
+(cd $dir && rm -rf node_modules/react)
+(cd $dir && rm -rf node_modules/next)
+(cd $dir && rm -rf node_modules/react-dom)
+
+# Build and start
+yarn next build $dir
+yarn next start $dir
