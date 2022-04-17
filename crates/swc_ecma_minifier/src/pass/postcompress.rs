1584b186b9e78ab1be6b7209062ac78c51ca8848
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-18 00:21:50 +0900

    Impl

--- crates/swc_ecma_minifier/src/pass/postcompress.rs
+++ crates/swc_ecma_minifier/src/pass/postcompress.rs
@@ -8,55 +8,7 @@ pub fn postcompress_optimizer(options: &CompressOptions) -> impl '_ + VisitMut {
 }
 
 struct PostcompressOptimizer<'a> {
-    #[allow(unused)]
     options: &'a CompressOptions,
-}
-
-impl PostcompressOptimizer<'_> {
-    fn optimize_in_bool_ctx(&mut self, e: &mut Expr) {
-        if !self.options.bools {
-            return;
-        }
-        // This is buggy
-        if DISABLE_BUGGY_PASSES {
-            return;
-        }
-
-        // Note: `||` is not handled because of precedence.
-        if let Expr::Bin(BinExpr {
-            op: op @ op!("&&"),
-            right,
-            left,
-            ..
-        }) = e
-        {
-            if let Expr::Bin(BinExpr { op: op!("&&"), .. }) = &**left {
-                return;
-            }
-
-            match &mut **right {
-                Expr::Unary(UnaryExpr {
-                    op: op!("!"), arg, ..
-                }) if arg.is_ident() => {
-                    let new_op = if *op == op!("&&") {
-                        op!("||")
-                    } else {
-                        op!("&&")
-                    };
-
-                    report_change!(
-                        "bools: `(a {} !b)` => `(a {} b)` (in bool context)",
-                        *op,
-                        new_op
-                    );
-                    *op = new_op;
-                    *right = arg.take();
-                }
-
-                _ => {}
-            }
-        }
-    }
 }
 
 impl VisitMut for PostcompressOptimizer<'_> {
@@ -77,8 +29,10 @@ impl VisitMut for PostcompressOptimizer<'_> {
     fn visit_mut_var_decl(&mut self, v: &mut VarDecl) {
         v.visit_mut_children_with(self);
 
-        if let VarDeclKind::Const = v.kind {
-            v.kind = VarDeclKind::Let;
+        if self.options.const_to_let {
+            if let VarDeclKind::Const = v.kind {
+                v.kind = VarDeclKind::Let;
+            }
         }
     }
 }
