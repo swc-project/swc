bf8d6c4b33c5c45b3d6d3a0121ad6e3e6af7b0e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-18 13:41:16 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-4xxx/issue-4127/1/output/index.ts
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = void 0;
+var _class;
+function test(constructor) {
+    console.log(constructor);
+}
+let _class1 = _class = test((_class = class {
+}) || _class) || _class;
+exports.default = _class1;
---
+++ crates/swc/tests/fixture/issues-4xxx/issue-4127/2/output/index.ts
@@ -0,0 +1,7 @@
+var _class;
+function test(constructor) {
+    console.log(constructor);
+}
+let _class1 = _class = test((_class = class {
+}) || _class) || _class;
+export { _class1 as default };
