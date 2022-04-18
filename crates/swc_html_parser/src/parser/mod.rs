d751fa65377302e4deb31d64b3e5ef51b112ee57
alexander.akait <sheo13666q@gmail.com>
2022-04-18 05:39:32 +0300

    feat: add doctype

--- crates/swc_html_parser/src/parser/mod.rs
+++ crates/swc_html_parser/src/parser/mod.rs
@@ -59,21 +59,92 @@ enum Target<'a, T, U> {
     Element(&'a mut U),
 }
 
-struct OpenElementsStack {
-    items: Vec<Element>,
-    template_element_count: usize,
-}
+static LIMITED_QUIRKY_PUBLIC_PREFIXES: &'static [&'static str] = &[
+    "-//w3c//dtd xhtml 1.0 frameset//",
+    "-//w3c//dtd xhtml 1.0 transitional//",
+];
 
-const IMPLICIT_END_TAG_REQUIRED: &[&str] = &[
+static HTML4_PUBLIC_PREFIXES: &'static [&'static str] = &[
+    "-//w3c//dtd html 4.01 frameset//",
+    "-//w3c//dtd html 4.01 transitional//",
+];
+
+static QUIRKY_PUBLIC_PREFIXES: &'static [&'static str] = &[
+    "-//advasoft ltd//dtd html 3.0 aswedit + extensions//",
+    "-//as//dtd html 3.0 aswedit + extensions//",
+    "-//ietf//dtd html 2.0 level 1//",
+    "-//ietf//dtd html 2.0 level 2//",
+    "-//ietf//dtd html 2.0 strict level 1//",
+    "-//ietf//dtd html 2.0 strict level 2//",
+    "-//ietf//dtd html 2.0 strict//",
+    "-//ietf//dtd html 2.0//",
+    "-//ietf//dtd html 2.1e//",
+    "-//ietf//dtd html 3.0//",
+    "-//ietf//dtd html 3.2 final//",
+    "-//ietf//dtd html 3.2//",
+    "-//ietf//dtd html 3//",
+    "-//ietf//dtd html level 0//",
+    "-//ietf//dtd html level 1//",
+    "-//ietf//dtd html level 2//",
+    "-//ietf//dtd html level 3//",
+    "-//ietf//dtd html strict level 0//",
+    "-//ietf//dtd html strict level 1//",
+    "-//ietf//dtd html strict level 2//",
+    "-//ietf//dtd html strict level 3//",
+    "-//ietf//dtd html strict//",
+    "-//ietf//dtd html//",
+    "-//metrius//dtd metrius presentational//",
+    "-//microsoft//dtd internet explorer 2.0 html strict//",
+    "-//microsoft//dtd internet explorer 2.0 html//",
+    "-//microsoft//dtd internet explorer 2.0 tables//",
+    "-//microsoft//dtd internet explorer 3.0 html strict//",
+    "-//microsoft//dtd internet explorer 3.0 html//",
+    "-//microsoft//dtd internet explorer 3.0 tables//",
+    "-//netscape comm. corp.//dtd html//",
+    "-//netscape comm. corp.//dtd strict html//",
+    "-//o'reilly and associates//dtd html 2.0//",
+    "-//o'reilly and associates//dtd html extended 1.0//",
+    "-//o'reilly and associates//dtd html extended relaxed 1.0//",
+    "-//softquad software//dtd hotmetal pro 6.0::19990601::extensions to html 4.0//",
+    "-//softquad//dtd hotmetal pro 4.0::19971010::extensions to html 4.0//",
+    "-//spyglass//dtd html 2.0 extended//",
+    "-//sq//dtd html 2.0 hotmetal + extensions//",
+    "-//sun microsystems corp.//dtd hotjava html//",
+    "-//sun microsystems corp.//dtd hotjava strict html//",
+    "-//w3c//dtd html 3 1995-03-24//",
+    "-//w3c//dtd html 3.2 draft//",
+    "-//w3c//dtd html 3.2 final//",
+    "-//w3c//dtd html 3.2//",
+    "-//w3c//dtd html 3.2s draft//",
+    "-//w3c//dtd html 4.0 frameset//",
+    "-//w3c//dtd html 4.0 transitional//",
+    "-//w3c//dtd html experimental 19960712//",
+    "-//w3c//dtd html experimental 970421//",
+    "-//w3c//dtd w3 html//",
+    "-//w3o//dtd w3 html 3.0//",
+    "-//webtechs//dtd mozilla html 2.0//",
+    "-//webtechs//dtd mozilla html//",
+];
+
+static QUIRKY_PUBLIC_MATCHES: &'static [&'static str] = &[
+    "-//w3o//dtd w3 html strict 3.0//en//",
+    "-/w3c/dtd html 4.0 transitional/en",
+    "html",
+];
+
+static QUIRKY_SYSTEM_MATCHES: &'static [&'static str] =
+    &["http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"];
+
+static IMPLICIT_END_TAG_REQUIRED: &'static [&'static str] = &[
     "dd", "dt", "li", "optgroup", "option", "p", "rb", "rp", "rt", "rtc",
 ];
 
-const IMPLICIT_END_TAG_REQUIRED_THOROUGHLY: &[&str] = &[
+static IMPLICIT_END_TAG_REQUIRED_THOROUGHLY: &'static [&'static str] = &[
     "caption", "colgroup", "dd", "dt", "li", "optgroup", "option", "p", "rb", "rp", "rt", "rtc",
     "tbody", "td", "tfoot", "th", "thead", "tr",
 ];
 
-const SPECIFIC_SCOPE: &[(&str, Namespace)] = &[
+static SPECIFIC_SCOPE: &'static [(&'static str, Namespace)] = &[
     ("applet", Namespace::HTML),
     ("caption", Namespace::HTML),
     ("html", Namespace::HTML),
@@ -94,7 +165,7 @@ const SPECIFIC_SCOPE: &[(&str, Namespace)] = &[
     ("title", Namespace::SVG),
 ];
 
-const LIST_ITEM_SCOPE: &[(&str, Namespace)] = &[
+static LIST_ITEM_SCOPE: &'static [(&'static str, Namespace)] = &[
     ("applet", Namespace::HTML),
     ("caption", Namespace::HTML),
     ("html", Namespace::HTML),
@@ -116,7 +187,8 @@ const LIST_ITEM_SCOPE: &[(&str, Namespace)] = &[
     ("ol", Namespace::HTML),
     ("ul", Namespace::HTML),
 ];
-const BUTTON_SCOPE: &[(&str, Namespace)] = &[
+
+static BUTTON_SCOPE: &'static [(&'static str, Namespace)] = &[
     ("applet", Namespace::HTML),
     ("caption", Namespace::HTML),
     ("html", Namespace::HTML),
@@ -137,13 +209,20 @@ const BUTTON_SCOPE: &[(&str, Namespace)] = &[
     ("title", Namespace::SVG),
     ("button", Namespace::HTML),
 ];
-const TABLE_SCOPE: &[(&str, Namespace)] = &[
+
+static TABLE_SCOPE: &'static [(&'static str, Namespace)] = &[
     ("html", Namespace::HTML),
     ("table", Namespace::HTML),
     ("template", Namespace::HTML),
 ];
-const SELECT_SCOPE: &[(&str, Namespace)] =
+
+static SELECT_SCOPE: &'static [(&'static str, Namespace)] =
     &[("optgroup", Namespace::HTML), ("option", Namespace::HTML)];
+
+struct OpenElementsStack {
+    items: Vec<Element>,
+    template_element_count: usize,
+}
 
 impl OpenElementsStack {
     pub fn new() -> Self {
