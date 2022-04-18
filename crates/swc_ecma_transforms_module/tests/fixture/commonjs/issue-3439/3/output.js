9708cdbb02b02eb75e8da407240871090a727d7a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-18 18:43:49 +0900

    Update test refs

--- crates/swc_ecma_transforms_module/tests/fixture/commonjs/issue-3439/3/output.js
+++ crates/swc_ecma_transforms_module/tests/fixture/commonjs/issue-3439/3/output.js
@@ -4,10 +4,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = exports.bar2 = exports.bar = exports.foo = exports.foo1 = void 0;
 const foo = 1;
+let bar = 2;
+exports.default = exports.bar2 = exports.bar = bar = 3;
 exports.foo1 = foo;
 exports.foo = foo;
-let bar = 2;
 exports.bar = bar;
 exports.bar2 = bar;
 exports.default = bar;
-exports.default = exports.bar2 = exports.bar = bar = 3;
