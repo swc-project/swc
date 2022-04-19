3d6ac9d62285542e8646ff5e5f7f330b6e9d084b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-20 00:55:19 +0900

    Simple change

--- crates/swc/benches/minify.rs
+++ crates/swc/benches/minify.rs
@@ -16,46 +16,44 @@ fn mk() -> swc::Compiler {
 fn bench_minify(b: &mut Bencher, filename: &str) {
     let c = mk();
 
-    c.run(|| {
-        b.iter(|| {
-            let fm = black_box(
-                c.cm.load_file(
-                    &PathBuf::from("..")
-                        .join("swc_ecma_minifier")
-                        .join("benches")
-                        .join("full")
-                        .join(filename),
-                )
-                .unwrap(),
-            );
+    b.iter(|| {
+        let fm = black_box(
+            c.cm.load_file(
+                &PathBuf::from("..")
+                    .join("swc_ecma_minifier")
+                    .join("benches")
+                    .join("full")
+                    .join(filename),
+            )
+            .unwrap(),
+        );
 
-            let res = try_with_handler(c.cm.clone(), Default::default(), |handler| {
-                c.minify(
-                    fm,
-                    handler,
-                    &JsMinifyOptions {
-                        compress: true.into(),
-                        mangle: true.into(),
-                        format: Default::default(),
-                        ecma: Default::default(),
-                        keep_classnames: Default::default(),
-                        keep_fnames: Default::default(),
-                        module: Default::default(),
-                        safari10: Default::default(),
-                        toplevel: Default::default(),
-                        source_map: Default::default(),
-                        output_path: Default::default(),
-                        inline_sources_content: Default::default(),
-                    },
-                )
-            })
-            .unwrap();
-
-            let res = black_box(res);
-
-            assert_eq!(res.map, None);
+        let res = try_with_handler(c.cm.clone(), Default::default(), |handler| {
+            c.minify(
+                fm,
+                handler,
+                &JsMinifyOptions {
+                    compress: true.into(),
+                    mangle: true.into(),
+                    format: Default::default(),
+                    ecma: Default::default(),
+                    keep_classnames: Default::default(),
+                    keep_fnames: Default::default(),
+                    module: Default::default(),
+                    safari10: Default::default(),
+                    toplevel: Default::default(),
+                    source_map: Default::default(),
+                    output_path: Default::default(),
+                    inline_sources_content: Default::default(),
+                },
+            )
         })
-    });
+        .unwrap();
+
+        let res = black_box(res);
+
+        assert_eq!(res.map, None);
+    })
 }
 
 fn files_group(c: &mut Criterion) {
