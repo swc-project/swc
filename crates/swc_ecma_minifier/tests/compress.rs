ef2b21b41ba3c90fed233722bb670e1b8eb8cdef
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-20 06:32:07 +0900

    Fix test

--- crates/swc_ecma_minifier/tests/compress.rs
+++ crates/swc_ecma_minifier/tests/compress.rs
@@ -108,6 +108,9 @@ impl TestMangleOptions {
 struct TestOptions {
     #[serde(default)]
     defaults: bool,
+
+    #[serde(default)]
+    passes: usize,
 }
 
 fn parse_compressor_config(cm: Lrc<SourceMap>, s: &str) -> (bool, CompressOptions) {
@@ -117,7 +120,8 @@ fn parse_compressor_config(cm: Lrc<SourceMap>, s: &str) -> (bool, CompressOption
         serde_json::from_str(s).expect("failed to deserialize value into a compressor config");
 
     c.defaults = opts.defaults;
-    c.const_to_let = Some(false);
+    c.defaults = opts.defaults;
+    c.passes = opts.passes;
 
     (c.module, c.into_config(cm))
 }
