2a9660db692981a153bf740f13a4e7a49f954ed7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-19 16:16:18 +0900

    Log

--- crates/swc_ecma_minifier/tests/exec.rs
+++ crates/swc_ecma_minifier/tests/exec.rs
@@ -26,6 +26,7 @@ use swc_ecma_parser::{parse_file_as_module, EsConfig, Syntax};
 use swc_ecma_transforms_base::{fixer::fixer, hygiene::hygiene, resolver::resolver_with_mark};
 use swc_ecma_visit::{FoldWith, VisitMutWith};
 use testing::DebugUsingDisplay;
+use tracing::info;
 
 #[derive(Debug, Clone, Deserialize)]
 struct TestOptions {
@@ -59,7 +60,11 @@ fn stdout_of(code: &str) -> Result<String, Error> {
         )
     }
 
-    Ok(String::from_utf8_lossy(&actual_output.stdout).to_string())
+    let stdout = String::from_utf8_lossy(&actual_output.stdout).to_string();
+
+    info!("Stdout: {}", stdout);
+
+    Ok(stdout)
 }
 
 fn print<N: swc_ecma_codegen::Node>(
