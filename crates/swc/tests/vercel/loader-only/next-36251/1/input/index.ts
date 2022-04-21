b1f629fd4fd8efda26db30c28b1d8388653422c8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-21 20:50:21 +0900

    fixup

--- crates/swc/tests/vercel/loader-only/next-36251/1/input/index.ts
+++ crates/swc/tests/vercel/loader-only/next-36251/1/input/index.ts
@@ -2,3 +2,11 @@ type ü = {
     name: string;
     value: string;
 };
+
+export const SomeComponent = ({ name, value }: ü) => {
+    return (
+        <div>
+        { name } { value }
+    </div>
+    );
+  };
