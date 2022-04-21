f3b2fee7a8aec422c654b8a5688da031d2b35511
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-21 15:34:04 +0900

    Add a test

---
+++ crates/swc/tests/fixture/issues-4xxx/issue-4296/1/input/index.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface ListProps {
+    callback: () => void
+}
+
+const SomeList = ({ callback }: ListProps) => {
+    callback()
+
+    return <div />
+}
+
+const list = [0]
+
+const MyComponent = (): JSX.Element => {
+    return <div>
+        {
+            list.map(() => (
+                true ? <SomeList callback= { (): void => console.log('do something')} /> : <div / >
+      ))
+    }
+</div>
+}
