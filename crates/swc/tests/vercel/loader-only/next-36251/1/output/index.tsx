951c209afbccff91c5c25c1b95cea66a10a79d93
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-21 21:47:37 +0900

    Update test refs

---
+++ crates/swc/tests/vercel/loader-only/next-36251/1/output/index.tsx
@@ -0,0 +1,11 @@
+import { jsxs as _jsxs } from "react/jsx-runtime";
+export var SomeComponent = function(param) {
+    var name = param.name, value = param.value;
+    return /*#__PURE__*/ _jsxs("div", {
+        children: [
+            name,
+            " ",
+            value
+        ]
+    });
+};
