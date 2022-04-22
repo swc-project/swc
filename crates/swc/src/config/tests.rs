c0683627306f1b34fad44b474f1653ef2b1c1215
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-22 18:04:19 +0900

    Add a test

--- crates/swc/src/config/tests.rs
+++ crates/swc/src/config/tests.rs
@@ -13,6 +13,11 @@ fn array() {
 }
 
 #[test]
+fn issue_4390() {
+    let _ = parse_swcrc(include_str!("issue-4390.json")).expect("failed to parse");
+}
+
+#[test]
 fn issue_1532() {
     let res = serde_json::from_str::<Options>(include_str!("issue-1532.json"));
 
