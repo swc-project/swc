9e522b78e560a3b8971995b57af479eee5a50723
alexander.akait <sheo13666q@gmail.com>
2022-04-22 13:42:27 +0300

    refactor: extract node

--- crates/swc_html_parser/src/parser/mod.rs
+++ crates/swc_html_parser/src/parser/mod.rs
@@ -1,10 +1,6 @@
-use std::{
-    cell::{Cell, RefCell},
-    mem,
-    mem::take,
-    rc::{Rc, Weak},
-};
+use std::{mem::take, rc::Rc};
 
+use node::{Data, Node, RcNode};
 use swc_atoms::JsWord;
 use swc_common::Span;
 use swc_html_ast::*;
@@ -19,6 +15,7 @@ use crate::{
 #[macro_use]
 mod macros;
 pub mod input;
+mod node;
 
 pub type PResult<T> = Result<T, Error>;
 
@@ -66,54 +63,6 @@ impl Default for InsertionMode {
         InsertionMode::Initial
     }
 }
-
-#[derive(Debug, Clone)]
-pub enum Data {
-    Document(Document),
-    DocumentType(DocumentType),
-    Element(Element),
-    Text(Text),
-    Comment(Comment),
-}
-
-struct Node {
-    pub parent: Cell<Option<WeakNode>>,
-    pub children: RefCell<Vec<RcNode>>,
-    pub data: Data,
-}
-
-impl Node {
-    /// Create a new node from its contents
-    pub fn new(data: Data) -> Rc<Self> {
-        Rc::new(Node {
-            parent: Cell::new(None),
-            children: RefCell::new(Vec::new()),
-            data,
-        })
-    }
-}
-
-impl Drop for Node {
-    fn drop(&mut self) {
-        let mut nodes = mem::take(&mut *self.children.borrow_mut());
-
-        while let Some(node) = nodes.pop() {
-            let children = mem::take(&mut *node.children.borrow_mut());
-
-            nodes.extend(children.into_iter());
-
-            // TODO fix me
-            // if let Element { ref children, .. } = node.data {
-            //     if let Some(template_contents) = children.borrow_mut().take()
-            //     {     nodes.push(template_contents);
-            //     }
-            // }
-        }
-    }
-}
-
-type RcNode = Rc<Node>;
-type WeakNode = Weak<Node>;
 
 #[allow(dead_code)]
 enum InsertionPosition {
