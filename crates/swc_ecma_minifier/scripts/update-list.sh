2f7c8095235c6727a3e6bedd87c0816d29a0d489
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-23 21:27:26 +0900

    fixup

--- crates/swc_ecma_minifier/scripts/update-list.sh
+++ crates/swc_ecma_minifier/scripts/update-list.sh
@@ -15,7 +15,7 @@ unset SWC_RUN
 unset SWC_CHECK
 
 # Update golden.txt
-cargo test --test compress --all-features fixture_tests__terser__compress__ \
+cargo test --test compress fixture_tests__terser__compress__ \
   | grep 'fixture_tests__terser__compress__' \
   | grep 'js .\.\. ok$' \
   | sed -e 's!test fixture_tests__terser__compress__!!' \
@@ -34,4 +34,4 @@ cargo test --test compress 'fixture_tests__terser__compress__' \
   | sed -e 's!_js!.js!' \
   >> tests/TODO.txt
 
-./scrtips/sort.sh
+./scripts/sort.sh
