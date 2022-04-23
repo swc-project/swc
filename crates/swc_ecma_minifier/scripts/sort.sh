5e5dd6659bafaf1ef81b22e58361c5aa9e2d02e7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-23 21:23:48 +0900

    sort

--- crates/swc_ecma_minifier/scripts/sort.sh
+++ crates/swc_ecma_minifier/scripts/sort.sh
@@ -4,11 +4,14 @@ set -eu
 # Fix sorting differences between Linux and Mac
 export LC_ALL=C
 
-cat tests/golden.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/golden_sorted.txt
-mv tests/golden_sorted.txt tests/golden.txt
+function sortOne() {
+    cat $file | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/sorted.txt
+    mv tests/sorted.txt $file
+}
 
-cat tests/TODO.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/ignored_sorted.txt
-mv tests/ignored_sorted.txt tests/TODO.txt
+sortOne tests/golden.txt
+sortOne tests/TODO.txt
+sortOne tests/postponed.txt
 
 # Don't mark ignored test as golden
 comm -23 tests/golden.txt tests/TODO.txt > tests/nodup.txt
