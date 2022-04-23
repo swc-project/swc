a5dedd01207facda765ee48261bf2fad59a0fc8a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-23 21:26:41 +0900

    Merge more

--- crates/swc_ecma_minifier/scripts/update-list.sh
+++ crates/swc_ecma_minifier/scripts/update-list.sh
@@ -14,7 +14,15 @@ echo '' > tests/TODO.txt
 unset SWC_RUN
 unset SWC_CHECK
 
-scripts/add-golden.sh
+# Update golden.txt
+cargo test --test compress --all-features fixture_tests__terser__compress__ \
+  | grep 'fixture_tests__terser__compress__' \
+  | grep 'js .\.\. ok$' \
+  | sed -e 's!test fixture_tests__terser__compress__!!' \
+  | sed -e 's! ... ok!!' \
+  | sed -e 's!__!/!g' \
+  | sed -e 's!_js!.js!' \
+  >> tests/golden.txt
 
 # Update TODO.txt
 cargo test --test compress 'fixture_tests__terser__compress__' \
