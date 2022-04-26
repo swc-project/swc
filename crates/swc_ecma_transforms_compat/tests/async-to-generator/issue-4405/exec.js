629ad552ffac6c0c1f8a8e6dea9afbba513df9e0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-27 01:29:28 +0900

    Add a test for #4406

---
+++ crates/swc_ecma_transforms_compat/tests/async-to-generator/issue-4405/exec.js
@@ -0,0 +1,18 @@
+async function* asyncGenerator(
+) {
+    for (let i = 5; i < 10; i++) {
+        yield new Promise((res) => setTimeout(() => res({ success: true }), 5));
+    }
+    yield { success: true };
+}
+
+async function* execute(
+) {
+    yield* asyncGenerator();
+}
+
+(async () => {
+    for await (const p of execute()) {
+        console.log(await p);
+    }
+})();
