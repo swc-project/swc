ee8ba3af73bead22eb52a77d0c69d29e39f7b815
Pig Fang <g-plane@hotmail.com>
2022-04-26 22:24:53 +0800

    refactor: simplify

--- crates/swc_html_minifier/src/steps/remove_default_attrs.rs
+++ crates/swc_html_minifier/src/steps/remove_default_attrs.rs
@@ -93,15 +93,12 @@ fn is_default_attribute_value(
 }
 
 pub fn remove_default_attributes(element: &mut Element) {
-    element
-        .attributes
-        .retain(|attribute| match &attribute.value {
-            Some(value) => !is_default_attribute_value(
-                element.namespace,
-                &element.tag_name,
-                &attribute.name,
-                value,
-            ),
-            None => true,
-        });
+    element.attributes.retain(|attribute| {
+        !matches!(&attribute.value, Some(value) if is_default_attribute_value(
+            element.namespace,
+            &element.tag_name,
+            &attribute.name,
+            value,
+        ))
+    });
 }
