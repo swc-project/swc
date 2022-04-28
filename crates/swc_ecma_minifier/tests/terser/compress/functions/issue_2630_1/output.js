c0c91e327a334f7df4b29a204d2cf78c3c1d1d7e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-28 23:45:39 +0900

    Revert

--- crates/swc_ecma_minifier/tests/terser/compress/functions/issue_2630_1/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/functions/issue_2630_1/output.js
@@ -1,7 +1,5 @@
 var c = 0;
-(function() {
-    while(f());
-    function f() {
-        c = 1 + ++c;
-    }
-})(), console.log(c);
+(function () {
+    while (void (c = 1 + ++c));
+})(),
+    console.log(c);
--- crates/swc_ecma_minifier/tests/terser/compress/functions/issue_2630_4/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/functions/issue_2630_4/output.js
@@ -1,8 +1,5 @@
 var x = 3, a = 1, b = 2;
 (function() {
-    while(--x >= 0 && f2());
-    function f2() {
-        a++, b += a;
-    }
+    while(--x >= 0 && void (a++, b += a));
 })();
 console.log(a);
