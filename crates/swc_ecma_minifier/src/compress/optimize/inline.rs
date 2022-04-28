540b931c59bba0916bdabfea2e4af2229c145445
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-04-28 23:20:28 +0900

    fixup

--- crates/swc_ecma_minifier/src/compress/optimize/inline.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -293,7 +293,7 @@ where
     }
 
     /// Check if the body of a function is simple enough to inline.
-    fn is_fn_body_simple_enough_to_inline(&self, body: &BlockStmt) -> bool {
+    fn is_fn_body_simple_enough_to_inline(&self, body: &BlockStmt, param_count: usize) -> bool {
         fn expr_cost(e: &Expr) -> Option<usize> {
             match e {
                 // TODO?
@@ -366,10 +366,6 @@ where
                             return true;
                         }
                     }
-                }
-
-                Stmt::Try(TryStmt { block, .. }) => {
-                    return self.is_fn_body_simple_enough_to_inline(block)
                 }
 
                 _ => {}
@@ -478,7 +474,10 @@ where
                     match &f.function.body {
                         Some(body) => {
                             if !UsageFinder::find(&i, body)
-                                && self.is_fn_body_simple_enough_to_inline(body)
+                                && self.is_fn_body_simple_enough_to_inline(
+                                    body,
+                                    f.function.params.len(),
+                                )
                             {
                                 trace_op!(
                                     "inline: Decided to inline function '{}{:?}' as it's very \
