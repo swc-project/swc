0bd5f6bb6ef57225233ea6bc4a8ba2fa10a82b82
magic-akari <akari.ccino@gmail.com>
2022-05-01 21:50:49 +0800

    fix: typo

--- packages/swc-helpers/src/_class_private_field_update.js
+++ packages/swc-helpers/src/_class_private_field_update.js
@@ -1,7 +1,7 @@
 import classExtractFieldDescriptor from "./_class_extract_field_descriptor";
-import _classApplyDescriptorUpdate from "./_class_apply_descriptor_update";
+import classApplyDescriptorUpdate from "./_class_apply_descriptor_update";
 
 export default function _classPrivateFieldUpdate(receiver, privateMap) {
     var descriptor = classExtractFieldDescriptor(receiver, privateMap, "update");
-    return _classApplyDescriptorUpdate(receiver, descriptor);
+    return classApplyDescriptorUpdate(receiver, descriptor);
 }
