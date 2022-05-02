4f748f07e966c6f4f0a29c32015073495fa239f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-02 16:39:11 +0900

    base

--- crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
+++ crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
@@ -1,5 +1,26 @@
+use swc_ecma_ast::*;
+use swc_ecma_utils::StmtLike;
 use swc_ecma_visit::VisitMut;
 
-pub struct TscDecorator {}
+pub struct TscDecorator {
+    extra_stmts: Vec<Stmt>,
+}
 
-impl VisitMut for TscDecorator {}
+impl TscDecorator {
+    fn visit_mut_stmt_likes<T>(&mut self, stmts: &mut Vec<T>)
+    where
+        T: StmtLike,
+    {
+        for s in stmts.iter_mut() {}
+    }
+}
+
+impl VisitMut for TscDecorator {
+    fn visit_mut_module_items(&mut self, s: &mut Vec<ModuleItem>) {
+        self.visit_mut_stmt_likes(s)
+    }
+
+    fn visit_mut_stmts(&mut self, s: &mut Vec<Stmt>) {
+        self.visit_mut_stmt_likes(s)
+    }
+}
