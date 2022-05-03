fc7eb62662daec4ed1366b082e240cd1c8c4da73
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 15:04:50 +0900

    Migrate some tests

---
+++ crates/swc_ecma_transforms/tests/fixture/ts-decorator/metadata/generics/base/input.ts
@@ -0,0 +1,13 @@
+@Decorate
+class MyClass {
+    constructor(
+        private generic: Generic<A>,
+        generic2: Generic<A, B>
+    ) { }
+
+    @Run
+    method(
+        generic: Inter<A>,
+        @Arg() generic2: InterGen<A, B>
+    ) { }
+}
