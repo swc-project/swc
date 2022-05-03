8f38a05fb3c7b7032891cfb0e25ed76e938b0609
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 10:44:03 +0900

    More

--- crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
+++ crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
@@ -1,6 +1,7 @@
 use swc_common::{util::take::Take, DUMMY_SP};
 use swc_ecma_ast::*;
-use swc_ecma_utils::StmtLike;
+use swc_ecma_transforms_base::helper;
+use swc_ecma_utils::{ExprFactory, StmtLike};
 use swc_ecma_visit::{VisitMut, VisitMutWith};
 
 pub struct TscDecorator {
@@ -41,6 +42,32 @@ impl TscDecorator {
 
         self.appended_exprs = old_appended_exprs;
     }
+
+    /// Creates `__decorator` calls.
+    fn add_decorate_call(
+        &mut self,
+        decorators: impl IntoIterator<Item = Box<Expr>>,
+        target: ExprOrSpread,
+        key: ExprOrSpread,
+        desc: ExprOrSpread,
+    ) {
+        let decorators = ArrayLit {
+            span: DUMMY_SP,
+            elems: decorators
+                .into_iter()
+                .map(|v| v.as_arg())
+                .map(Some)
+                .collect(),
+        }
+        .as_arg();
+
+        self.appended_exprs.push(Box::new(Expr::Call(CallExpr {
+            span: DUMMY_SP,
+            callee: helper!(decorate, "__decorate"),
+            args: vec![decorators, target, key, desc],
+            type_args: Default::default(),
+        })));
+    }
 }
 
 impl VisitMut for TscDecorator {
@@ -50,5 +77,11 @@ impl VisitMut for TscDecorator {
 
     fn visit_mut_stmts(&mut self, s: &mut Vec<Stmt>) {
         self.visit_mut_stmt_likes(s)
+    }
+
+    fn visit_mut_class_prop(&mut self, c: &mut ClassProp) {
+        c.visit_mut_children_with(self);
+
+        if !c.decorators.is_empty() {}
     }
 }
