8c024e9c9cff9986fe7a43efe5b02658fc32201f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 13:12:47 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-4xxx/4127/1/output/index.ts
+++ crates/swc/tests/fixture/issues-4xxx/4127/1/output/index.ts
@@ -3,10 +3,13 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = void 0;
+var swcHelpers = require("@swc/helpers");
+class _class {
+}
+exports.default = _class;
 function test(constructor) {
     console.log(constructor);
 }
-@test
-class _default {
-}
-exports.default = _default;
+swcHelpers.__decorate([
+    test
+], _class);
--- crates/swc/tests/fixture/issues-4xxx/4127/2/output/index.ts
+++ crates/swc/tests/fixture/issues-4xxx/4127/2/output/index.ts
@@ -1,6 +1,9 @@
+import * as swcHelpers from "@swc/helpers";
 function test(constructor) {
     console.log(constructor);
 }
-export default @test
-class {
+export default class _class {
 };
+swcHelpers.__decorate([
+    test
+], _class);
