5871d0a93fd4eddcea6a9c66debda7aa9c5178b0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 15:13:45 +0900

    fixup

---
+++ crates/swc_ecma_transforms/tests/fixture/legacy-only/decl-to-expression/class-decorators/output.js
@@ -0,0 +1,5 @@
+var _class, _class1;
+export default _class = dec((_class = class A {
+}) || _class) || _class;
+let B = _class1 = dec((_class1 = class B {
+}) || _class1) || _class1;
---
+++ crates/swc_ecma_transforms/tests/fixture/legacy-only/regression-10264/output.js
@@ -0,0 +1,5 @@
+var _class;
+function myDecorator(decoratee) {}
+let _class1 = _class = myDecorator((_class = class {
+}) || _class) || _class;
+export { _class1 as default };
