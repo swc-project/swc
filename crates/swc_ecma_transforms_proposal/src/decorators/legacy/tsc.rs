9e42f8f2b2249471e38b78f868be2867c772f783
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 10:51:33 +0900

    properties

--- crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
+++ crates/swc_ecma_transforms_proposal/src/decorators/legacy/tsc.rs
@@ -1,11 +1,13 @@
 use swc_common::{util::take::Take, DUMMY_SP};
 use swc_ecma_ast::*;
 use swc_ecma_transforms_base::helper;
-use swc_ecma_utils::{ExprFactory, StmtLike};
+use swc_ecma_utils::{prop_name_to_expr_value, quote_ident, undefined, ExprFactory, StmtLike};
 use swc_ecma_visit::{VisitMut, VisitMutWith};
 
 pub struct TscDecorator {
     appended_exprs: Vec<Box<Expr>>,
+
+    class_name: Option<Ident>,
 }
 
 impl TscDecorator {
@@ -71,17 +73,48 @@ impl TscDecorator {
 }
 
 impl VisitMut for TscDecorator {
+    fn visit_mut_class_decl(&mut self, n: &mut ClassDecl) {
+        let old = self.class_name.take();
+        self.class_name = Some(n.ident.clone());
+
+        n.visit_mut_children_with(self);
+
+        self.class_name = old;
+    }
+
+    fn visit_mut_class_expr(&mut self, n: &mut ClassExpr) {
+        let old = self.class_name.take();
+        if let Some(ident) = &n.ident {
+            self.class_name = Some(ident.clone());
+        }
+
+        n.visit_mut_children_with(self);
+
+        self.class_name = old;
+    }
+
+    fn visit_mut_class_prop(&mut self, c: &mut ClassProp) {
+        c.visit_mut_children_with(self);
+
+        if let Some(class_name) = self.class_name.clone() {
+            if !c.decorators.is_empty() {
+                let key = prop_name_to_expr_value(c.key.clone());
+
+                self.add_decorate_call(
+                    c.decorators.drain(..).map(|d| d.expr),
+                    class_name.make_member(quote_ident!("prototype")).as_arg(),
+                    key.as_arg(),
+                    undefined(DUMMY_SP).as_arg(),
+                );
+            }
+        }
+    }
+
     fn visit_mut_module_items(&mut self, s: &mut Vec<ModuleItem>) {
         self.visit_mut_stmt_likes(s)
     }
 
     fn visit_mut_stmts(&mut self, s: &mut Vec<Stmt>) {
         self.visit_mut_stmt_likes(s)
-    }
-
-    fn visit_mut_class_prop(&mut self, c: &mut ClassProp) {
-        c.visit_mut_children_with(self);
-
-        if !c.decorators.is_empty() {}
     }
 }
