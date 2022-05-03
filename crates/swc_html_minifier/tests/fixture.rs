fca84eb0f27cec0821b1dd63a331de9b7d1118b4
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-05-03 05:49:06 +0300

    fix(html/parser): Fix bugs (#4502)

--- crates/swc_html_minifier/tests/fixture.rs
+++ crates/swc_html_minifier/tests/fixture.rs
@@ -54,3 +54,54 @@ fn minify_fixtures(input: PathBuf) {
     })
     .unwrap();
 }
+
+#[testing::fixture("tests/recovery/**/input.html")]
+fn minify_recovery(input: PathBuf) {
+    let dir = input.parent().unwrap();
+    let output = dir.join(format!(
+        "output.min.{}",
+        input.extension().unwrap().to_string_lossy()
+    ));
+    let mut recovered = false;
+
+    testing::run_test(false, |cm, handler| {
+        let fm = cm.load_file(&input).unwrap();
+
+        let mut errors = vec![];
+        let res: Result<Document, _> = parse_file(&fm, Default::default(), &mut errors);
+
+        for err in errors {
+            err.to_diagnostics(handler).emit();
+        }
+
+        if handler.has_errors() {
+            recovered = true;
+        }
+
+        let mut ss = res.unwrap();
+
+        // Apply transforms
+        minify(&mut ss);
+
+        let mut html_str = String::new();
+        {
+            let wr = BasicHtmlWriter::new(&mut html_str, None, BasicHtmlWriterConfig::default());
+            let mut gen = CodeGenerator::new(wr, CodegenConfig { minify: true });
+
+            gen.emit(&ss).unwrap();
+        }
+
+        NormalizedOutput::from(html_str)
+            .compare_to_file(&output)
+            .unwrap();
+
+        Ok(())
+    })
+    .unwrap();
+
+    if !recovered {
+        panic!(
+            "Parser should emit errors (recover mode), but parser parsed everything successfully"
+        );
+    }
+}
