811393c559f943e21e31345fe4226e9180686edf
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-03 14:37:16 +0900

    Update test refs

--- crates/swc_ecma_transforms_optimization/tests/simplify_dce.rs
+++ crates/swc_ecma_transforms_optimization/tests/simplify_dce.rs
@@ -440,26 +440,20 @@ test!(
 }
 ",
     "
-var _class, _descriptor;
-let X = ((_class = class X {
-    x() {
-        const localVar = aFunctionSomewhere();
-        return localVar;
+
+    export default class X {
+        constructor(){
+            this.anything = 0;
+        }
+        x() {
+            const localVar = aFunctionSomewhere();
+            return localVar;
+        }
     }
-    constructor(){
-        _initializerDefineProperty(this, \"anything\", _descriptor, this);
-    }
-}) || _class, _descriptor = _applyDecoratedDescriptor(_class.prototype, \"anything\", [
-    whatever
-], {
-    configurable: true,
-    enumerable: true,
-    writable: true,
-    initializer: function() {
-        return 0;
-    }
-}), _class);
-export { X as default };"
+    __decorate([
+        whatever
+    ], X.prototype, \"anything\", void 0);
+    "
 );
 
 test!(
