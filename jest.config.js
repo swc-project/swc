dee0e7fdcd40509deb1aae5af74b3b15fc01fb74
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-04 22:13:57 +0900

    fmt

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/__tests__/**/*.m[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    transform: {
-    },
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/__tests__/**/*.m[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)",
+    ],
+    transform: {},
     // transform: {
     //     "^.+\\.jsx?$": "babel-jest",
     //     "^.+\\.mjs$": "babel-jest",
     // },
     testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
-    moduleFileExtensions: ["js", "jsx", "mjs"]
-}
+    moduleFileExtensions: ["js", "jsx", "mjs"],
+};
