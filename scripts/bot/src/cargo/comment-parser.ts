9c316909962acd392d78806b5b9c1a510c27e3c2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-04 22:15:18 +0900

    fmt

--- scripts/bot/src/cargo/comment-parser.ts
+++ scripts/bot/src/cargo/comment-parser.ts
@@ -1,55 +1,57 @@
 import { octokit } from "../util/octokit";
-import YAML from 'yaml'
+import YAML from "yaml";
 
-const owner = 'swc-project';
-const repo = 'swc';
-const maintainer = 'kdy1';
+const owner = "swc-project";
+const repo = "swc";
+const maintainer = "kdy1";
 
 export interface Action {
-    crate: string
-    breaking: boolean
+    crate: string;
+    breaking: boolean;
 }
 
 export async function parsePrComments(prNumber: number): Promise<Action[]> {
     const comments = await octokit.pulls.listReviews({
         owner,
         repo,
-        pull_number: prNumber
+        pull_number: prNumber,
     });
 
     return comments.data
         .filter((c) => c.user && c.user.login === maintainer)
-        .map(c => {
-            const idx = c.body.indexOf('swc-bump:');
+        .map((c) => {
+            const idx = c.body.indexOf("swc-bump:");
             if (idx === -1) {
                 return undefined;
             }
-            return c.body.substring(idx)
+            return c.body.substring(idx);
         })
-        .filter(text => !!text)
-        .map(text => text!)
-        .map(text => YAML.parse(text))
-        .map(data => data['swc-bump'])
-        .flatMap(data => data)
+        .filter((text) => !!text)
+        .map((text) => text!)
+        .map((text) => YAML.parse(text))
+        .map((data) => data["swc-bump"])
+        .flatMap((data) => data)
         .map((line) => {
-            if (typeof line !== 'string') {
-                throw new Error(`Non-string data: ${line}`)
+            if (typeof line !== "string") {
+                throw new Error(`Non-string data: ${line}`);
             }
             line = line.trim();
 
             console.log(`Comment line: '${line}'`);
 
-
-            if (line.endsWith(' --breaking')) {
+            if (line.endsWith(" --breaking")) {
                 return {
-                    crate: line.substring(0, line.length - ' --breaking'.length),
-                    breaking: true
-                }
+                    crate: line.substring(
+                        0,
+                        line.length - " --breaking".length
+                    ),
+                    breaking: true,
+                };
             }
 
             return {
                 crate: line,
-                breaking: false
-            }
-        })
+                breaking: false,
+            };
+        });
 }
