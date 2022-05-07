d4573eae639f940369b9f2f5f3b3e6e6b3ea4dac
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-07 20:58:55 +0900

    Hmm

--- crates/swc_ecma_minifier_internal_gcc/src/control_flow/analysis.rs
+++ crates/swc_ecma_minifier_internal_gcc/src/control_flow/analysis.rs
@@ -1,9 +1,28 @@
 use super::ControlFlowGraph;
-use crate::node::Node;
+use crate::{graph::DiGraphNode, node::Node};
 
 impl ControlFlowGraph<Node> {
     /// Ported from https://github.com/google/closure-compiler/blob/e60b2d7bb0977893e113f673fc0e78e7c485ec2f/src/com/google/javascript/jscomp/ControlFlowAnalysis.java
     pub fn analyze(root: &Node) -> Self {
+        todo!()
+    }
+}
+
+type N = DiGraphNode<Node>;
+
+struct Analyzer<'a> {
+    cfg: &'a mut ControlFlowGraph<Node>,
+    root: N,
+}
+
+impl Analyzer<'_> {
+    fn process(&mut self) {
+        let root = self.root.clone();
+        let cfg_entry = self.compute_fall_through(&root);
+        self.cfg.entry = cfg_entry;
+    }
+
+    fn compute_fall_through(&mut self, node: &N) -> N {
         todo!()
     }
 }
