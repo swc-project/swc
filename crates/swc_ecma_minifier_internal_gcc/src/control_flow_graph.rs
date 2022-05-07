a2fe6ec3a9e146199c7dee1b50b99ec75ac96d66
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-07 17:08:14 +0900

    Doc

--- crates/swc_ecma_minifier_internal_gcc/src/control_flow_graph.rs
+++ crates/swc_ecma_minifier_internal_gcc/src/control_flow_graph.rs
@@ -27,6 +27,7 @@ impl<N> Deref for ControlFlowGraph<N> {
 }
 
 impl ControlFlowGraph<Node> {
+    /// Ported from https://github.com/google/closure-compiler/blob/e60b2d7bb0977893e113f673fc0e78e7c485ec2f/src/com/google/javascript/jscomp/ControlFlowAnalysis.java
     pub fn analyze(root: &Node) -> Self {
         todo!()
     }
