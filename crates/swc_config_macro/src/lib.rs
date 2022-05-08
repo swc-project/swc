78c4cec038a3b53f9f0a4f72ee3df99097df2631
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-08 20:53:07 +0900

    fixup

--- crates/swc_config_macro/src/lib.rs
+++ crates/swc_config_macro/src/lib.rs
@@ -1,4 +1,4 @@
 extern crate proc_macro;
 
 #[proc_macro_derive(Merge)]
-pub fn derive_spanned(input: proc_macro::TokenStream) -> proc_macro::TokenStream {}
+pub fn derive_merge(input: proc_macro::TokenStream) -> proc_macro::TokenStream {}
