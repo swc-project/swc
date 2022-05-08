518d1a09969fd5275664002f52623eb54fae532c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-08 21:49:42 +0900

    `BoolConfig`

---
+++ crates/swc_config/src/config_types.rs
@@ -0,0 +1,42 @@
+use crate::merge::Merge;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct BoolConfig<const DEFAULT: bool>(Option<bool>);
+
+impl<const DEFAULT: bool> BoolConfig<DEFAULT> {
+    #[inline]
+    pub fn new(value: Option<bool>) -> Self {
+        Self(value)
+    }
+}
+
+impl<const DEFAULT: bool> From<BoolConfig<DEFAULT>> for bool {
+    #[inline]
+    fn from(v: BoolConfig<DEFAULT>) -> Self {
+        match v.0 {
+            Some(v) => v,
+            _ => DEFAULT,
+        }
+    }
+}
+
+impl<const DEFAULT: bool> From<Option<bool>> for BoolConfig<DEFAULT> {
+    #[inline]
+    fn from(v: Option<bool>) -> Self {
+        Self(v)
+    }
+}
+
+impl<const DEFAULT: bool> From<bool> for BoolConfig<DEFAULT> {
+    #[inline]
+    fn from(v: bool) -> Self {
+        Self(Some(v))
+    }
+}
+
+impl<const DEFAULT: bool> Merge for BoolConfig<DEFAULT> {
+    #[inline]
+    fn merge(&mut self, other: Self) {
+        self.0.merge(other.0);
+    }
+}
--- crates/swc_config/src/lib.rs
+++ crates/swc_config/src/lib.rs
@@ -1,3 +1,4 @@
 #[macro_use]
 mod macros;
+pub mod config_types;
 pub mod merge;
