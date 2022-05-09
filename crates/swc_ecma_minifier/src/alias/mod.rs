aee3d404ee256f34afeff156774a1fe238ddddd7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-09 23:39:03 +0900

    Add `AliasCollector`

--- crates/swc_ecma_minifier/src/alias/mod.rs
+++ crates/swc_ecma_minifier/src/alias/mod.rs
@@ -1,8 +1,34 @@
 use rustc_hash::FxHashSet;
+use swc_common::SyntaxContext;
 use swc_ecma_ast::Id;
+use swc_ecma_visit::{noop_visit_type, Visit, VisitWith};
+
+pub(crate) struct AliasConfig {
+    pub unresolved_ctxt: SyntaxContext,
+}
+
+pub(crate) fn collect_aliases<N>(node: &N, config: AliasConfig) -> FxHashSet<Id>
+where
+    N: VisitWith<AliasCollector>,
+{
+    let mut visitor = AliasCollector {
+        config,
+        aliases: FxHashSet::default(),
+    };
+
+    node.visit_with(&mut visitor);
+
+    visitor.aliases
+}
 
 pub(crate) struct AliasCollector {
+    config: AliasConfig,
+
     aliases: FxHashSet<Id>,
 }
 
 impl AliasCollector {}
+
+impl Visit for AliasCollector {
+    noop_visit_type!();
+}
