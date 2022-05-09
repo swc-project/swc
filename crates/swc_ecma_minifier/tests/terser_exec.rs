a58ef73a00ed56376e23a9ee4009e62c6582cf47
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-09 16:20:37 +0900

    fmt

--- crates/swc_ecma_minifier/tests/terser_exec.rs
+++ crates/swc_ecma_minifier/tests/terser_exec.rs
@@ -53,10 +53,8 @@ use testing::assert_eq;
         "properties/issue_3188_3/",
         "rename/function_catch_catch/",
         "yield/issue_2689/",
-
         // We don't care about ie8
         "ie8",
-
         // tests with infinite loops
         "reduce_vars/toplevel_off_loops_2",
         "reduce_vars/toplevel_on_loops_2",
