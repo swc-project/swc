9717754b120148210b5158b05a5e93fcac03c8fb
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-10 01:07:23 +0900

    Globals

--- crates/swc_ecma_minifier/src/alias/mod.rs
+++ crates/swc_ecma_minifier/src/alias/mod.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::needless_update)]
 
 use rustc_hash::FxHashSet;
-use swc_common::collections::AHashSet;
+use swc_atoms::js_word;
+use swc_common::{collections::AHashSet, SyntaxContext};
 use swc_ecma_ast::*;
 use swc_ecma_utils::{collect_decls, ident::IdentLike, BindingCollector};
 use swc_ecma_visit::{noop_visit_type, Visit, VisitWith};
@@ -21,13 +22,16 @@ where
     N: for<'aa> VisitWith<InfectionCollector<'aa>>,
     N: VisitWith<BindingCollector<Id>>,
 {
+    let unresolved_ctxt = SyntaxContext::empty().apply_mark(config.marks.unresolved_mark);
     let decls = collect_decls(node);
 
     let mut visitor = InfectionCollector {
         config,
+        unresolved_ctxt,
+
         exclude: &decls,
-        aliases: FxHashSet::default(),
         ctx: Default::default(),
+        aliases: FxHashSet::default(),
     };
 
     node.visit_with(&mut visitor);
@@ -38,6 +42,7 @@ where
 pub(crate) struct InfectionCollector<'a> {
     #[allow(unused)]
     config: AliasConfig,
+    unresolved_ctxt: SyntaxContext,
 
     exclude: &'a AHashSet<Id>,
 
@@ -50,6 +55,18 @@ impl InfectionCollector<'_> {
     fn add_id(&mut self, e: &Id) {
         if self.exclude.contains(e) {
             return;
+        }
+
+        if self.unresolved_ctxt == e.1 {
+            match e.0 {
+                js_word!("String")
+                | js_word!("Object")
+                | js_word!("Number")
+                | js_word!("BigInt")
+                | js_word!("Boolean")
+                | js_word!("Math") => return,
+                _ => {}
+            }
         }
 
         self.aliases.insert(e.clone());
