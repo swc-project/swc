c12a960b2ba5ddc7a961e901e5d20323a0586ca7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-10 01:40:13 +0900

    Start

--- crates/swc_ecma_minifier/tests/terser_exec.rs
+++ crates/swc_ecma_minifier/tests/terser_exec.rs
@@ -34,7 +34,6 @@ use testing::assert_eq;
 #[testing::fixture(
     "tests/terser/compress/**/input.js",
     exclude(
-        "evaluate/issue_1760_1/",
         "functions/issue_2620_4/",
         "issue_1105/assorted_Infinity_NaN_undefined_in_with_scope/",
         "issue_1105/assorted_Infinity_NaN_undefined_in_with_scope_keep_infinity/",
