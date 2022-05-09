e843aa333a6c1193409688af5bc69fc1c51de410
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-10 05:13:45 +0900

    Update test refs

---
+++ crates/swc/tests/tsc-references/stringLiteralsWithSwitchStatements03_es5.2.minified.js
@@ -0,0 +1,11 @@
+var x, z;
+switch(x){
+    case randBool() ? "foo" : "baz":
+    case randBool(), "bar":
+    case "bar":
+    case "baz":
+    case "foo":
+    case "bar":
+    case z || "baz":
+    case "baz":
+}
