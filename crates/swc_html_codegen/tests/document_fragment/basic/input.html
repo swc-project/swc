24c843531906dfee9a5765c4762eaa32e237ad89
alexander.akait <sheo13666q@gmail.com>
2022-05-11 00:30:04 +0300

    feat: improve parse API

---
+++ crates/swc_html_codegen/tests/document_fragment/basic/input.html
@@ -0,0 +1,4 @@
+<div>
+    <h1>Test</h1>
+    <p>Test</p>
+</div>
--- crates/swc_html_codegen/tests/fixture.rs
+++ crates/swc_html_codegen/tests/fixture.rs
@@ -11,11 +11,13 @@ use swc_html_codegen::{
     writer::basic::{BasicHtmlWriter, BasicHtmlWriterConfig, IndentType, LineFeed},
     CodeGenerator, CodegenConfig, Emit,
 };
-use swc_html_parser::{parse_file, parser::ParserConfig};
+use swc_html_parser::{
+    parse_file_as_document, parse_file_as_document_fragment, parser::ParserConfig,
+};
 use swc_html_visit::{VisitMut, VisitMutWith};
 use testing::{assert_eq, run_test2, NormalizedOutput};
 
-fn print(
+fn print_document(
     input: &Path,
     parser_config: Option<ParserConfig>,
     writer_config: Option<BasicHtmlWriterConfig>,
@@ -46,7 +48,8 @@ fn print(
     run_test2(false, |cm, handler| {
         let fm = cm.load_file(input).unwrap();
         let mut errors = vec![];
-        let mut document: Document = parse_file(&fm, parser_config, &mut errors).unwrap();
+        let mut document: Document =
+            parse_file_as_document(&fm, parser_config, &mut errors).unwrap();
 
         for err in take(&mut errors) {
             err.to_diagnostics(&handler).emit();
@@ -65,8 +68,8 @@ fn print(
             .unwrap();
 
         let mut errors = vec![];
-        let mut document_output: Document = parse_file(&fm_output, parser_config, &mut errors)
-            .map_err(|err| {
+        let mut document_parsed_again =
+            parse_file_as_document(&fm_output, parser_config, &mut errors).map_err(|err| {
                 err.to_diagnostics(&handler).emit();
             })?;
 
@@ -75,16 +78,97 @@ fn print(
         }
 
         document.visit_mut_with(&mut NormalizeTest);
-        document_output.visit_mut_with(&mut NormalizeTest);
+        document_parsed_again.visit_mut_with(&mut NormalizeTest);
 
-        assert_eq!(document, document_output);
+        assert_eq!(document, document_parsed_again);
 
         Ok(())
     })
     .unwrap();
 }
 
-fn verify(
+fn print_document_fragment(
+    input: &Path,
+    context_element: Element,
+    parser_config: Option<ParserConfig>,
+    writer_config: Option<BasicHtmlWriterConfig>,
+    minify: bool,
+) {
+    let dir = input.parent().unwrap();
+    let output = if minify {
+        dir.join(format!(
+            "output.min.{}",
+            input.extension().unwrap().to_string_lossy()
+        ))
+    } else {
+        dir.join(format!(
+            "output.{}",
+            input.extension().unwrap().to_string_lossy()
+        ))
+    };
+
+    let parser_config = match parser_config {
+        Some(parser_config) => parser_config,
+        _ => ParserConfig::default(),
+    };
+    let writer_config = match writer_config {
+        Some(writer_config) => writer_config,
+        _ => BasicHtmlWriterConfig::default(),
+    };
+
+    run_test2(false, |cm, handler| {
+        let fm = cm.load_file(input).unwrap();
+        let mut errors = vec![];
+        let mut document_fragment = parse_file_as_document_fragment(
+            &fm,
+            context_element.clone(),
+            parser_config,
+            &mut errors,
+        )
+        .unwrap();
+
+        for err in take(&mut errors) {
+            err.to_diagnostics(&handler).emit();
+        }
+
+        let mut html_str = String::new();
+        let wr = BasicHtmlWriter::new(&mut html_str, None, writer_config);
+        let mut gen = CodeGenerator::new(wr, CodegenConfig { minify });
+
+        gen.emit(&document_fragment).unwrap();
+
+        let fm_output = cm.load_file(&output).unwrap();
+
+        NormalizedOutput::from(html_str)
+            .compare_to_file(output)
+            .unwrap();
+
+        let mut errors = vec![];
+        let mut document_fragment_parsed_again = parse_file_as_document_fragment(
+            &fm_output,
+            context_element.clone(),
+            parser_config,
+            &mut errors,
+        )
+        .map_err(|err| {
+            err.to_diagnostics(&handler).emit();
+        })?;
+
+        for error in take(&mut errors) {
+            error.to_diagnostics(&handler).emit();
+        }
+
+        document_fragment.visit_mut_with(&mut NormalizeTest);
+        document_fragment_parsed_again.visit_mut_with(&mut NormalizeTest);
+
+        assert_eq!(document_fragment, document_fragment_parsed_again);
+
+        Ok(())
+    })
+    .unwrap();
+}
+
+fn verify_document(
     input: &Path,
     parser_config: Option<ParserConfig>,
     writer_config: Option<BasicHtmlWriterConfig>,
@@ -102,8 +186,9 @@ fn verify(
     testing::run_test2(false, |cm, handler| {
         let fm = cm.load_file(&input).unwrap();
         let mut errors = vec![];
-        let mut document: Document =
-            parse_file(&fm, parser_config, &mut errors).map_err(|err| {
+
+        let mut document =
+            parse_file_as_document(&fm, parser_config, &mut errors).map_err(|err| {
                 err.to_diagnostics(&handler).emit();
             })?;
 
@@ -121,8 +206,8 @@ fn verify(
 
         let new_fm = cm.new_source_file(FileName::Anon, html_str);
         let mut parsed_errors = vec![];
-        let mut parsed: Document =
-            parse_file(&new_fm, parser_config, &mut parsed_errors).map_err(|err| {
+        let mut document_parsed_again =
+            parse_file_as_document(&new_fm, parser_config, &mut parsed_errors).map_err(|err| {
                 err.to_diagnostics(&handler).emit();
             })?;
 
@@ -133,9 +218,79 @@ fn verify(
         }
 
         document.visit_mut_with(&mut DropSpan);
-        parsed.visit_mut_with(&mut DropSpan);
+        document_parsed_again.visit_mut_with(&mut DropSpan);
 
-        assert_eq!(document, parsed);
+        assert_eq!(document, document_parsed_again);
+
+        Ok(())
+    })
+    .unwrap();
+}
+
+fn verify_document_fragment(
+    input: &Path,
+    context_element: Element,
+    parser_config: Option<ParserConfig>,
+    writer_config: Option<BasicHtmlWriterConfig>,
+    ignore_errors: bool,
+) {
+    let parser_config = match parser_config {
+        Some(parser_config) => parser_config,
+        _ => ParserConfig::default(),
+    };
+    let writer_config = match writer_config {
+        Some(writer_config) => writer_config,
+        _ => BasicHtmlWriterConfig::default(),
+    };
+
+    testing::run_test2(false, |cm, handler| {
+        let fm = cm.load_file(&input).unwrap();
+        let mut errors = vec![];
+
+        let mut document_fragment = parse_file_as_document_fragment(
+            &fm,
+            context_element.clone(),
+            parser_config,
+            &mut errors,
+        )
+        .map_err(|err| {
+            err.to_diagnostics(&handler).emit();
+        })?;
+
+        if !ignore_errors {
+            for err in take(&mut errors) {
+                err.to_diagnostics(&handler).emit();
+            }
+        }
+
+        let mut html_str = String::new();
+        let wr = BasicHtmlWriter::new(&mut html_str, None, writer_config);
+        let mut gen = CodeGenerator::new(wr, CodegenConfig { minify: false });
+
+        gen.emit(&document_fragment).unwrap();
+
+        let new_fm = cm.new_source_file(FileName::Anon, html_str);
+        let mut parsed_errors = vec![];
+        let mut document_fragment_parsed_again = parse_file_as_document_fragment(
+            &new_fm,
+            context_element.clone(),
+            parser_config,
+            &mut parsed_errors,
+        )
+        .map_err(|err| {
+            err.to_diagnostics(&handler).emit();
+        })?;
+
+        if !ignore_errors {
+            for err in parsed_errors {
+                err.to_diagnostics(&handler).emit();
+            }
+        }
+
+        document_fragment.visit_mut_with(&mut DropSpan);
+        document_fragment_parsed_again.visit_mut_with(&mut DropSpan);
+
+        assert_eq!(document_fragment, document_fragment_parsed_again);
 
         Ok(())
     })
@@ -178,14 +333,46 @@ impl VisitMut for NormalizeTest {
 }
 
 #[testing::fixture("tests/fixture/**/input.html")]
-fn test_fixture(input: PathBuf) {
-    print(&input, None, None, false);
-    print(&input, None, None, true);
+fn test_document(input: PathBuf) {
+    print_document(&input, None, None, false);
+    print_document(&input, None, None, true);
+}
+
+#[testing::fixture("tests/document_fragment/**/input.html")]
+fn test_document_fragment(input: PathBuf) {
+    print_document_fragment(
+        &input,
+        Element {
+            span: Default::default(),
+            tag_name: "template".into(),
+            namespace: Namespace::HTML,
+            attributes: vec![],
+            children: vec![],
+            content: None,
+        },
+        None,
+        None,
+        false,
+    );
+    print_document_fragment(
+        &input,
+        Element {
+            span: Default::default(),
+            tag_name: "template".into(),
+            namespace: Namespace::HTML,
+            attributes: vec![],
+            children: vec![],
+            content: None,
+        },
+        None,
+        None,
+        true,
+    );
 }
 
 #[testing::fixture("tests/options/indent_type/**/input.html")]
 fn test_indent_type_option(input: PathBuf) {
-    print(
+    print_document(
         &input,
         None,
         Some(BasicHtmlWriterConfig {
@@ -199,15 +386,59 @@ fn test_indent_type_option(input: PathBuf) {
 
 #[testing::fixture("../swc_html_parser/tests/fixture/**/*.html")]
 fn parser_verify(input: PathBuf) {
-    verify(&input, None, None, false);
+    verify_document(&input, None, None, false);
 }
 
 #[testing::fixture("../swc_html_parser/tests/recovery/**/*.html")]
 fn parser_recovery_verify(input: PathBuf) {
-    verify(&input, None, None, true);
+    verify_document(&input, None, None, true);
 }
 
 #[testing::fixture("../swc_html_parser/tests/html5lib-tests-fixture/**/*.html")]
 fn html5lib_tests_verify(input: PathBuf) {
-    verify(&input, None, None, true);
+    let file_stem = input.file_stem().unwrap().to_str().unwrap().to_owned();
+    let scripting_enabled = file_stem.contains("script_on");
+    let parser_config = ParserConfig { scripting_enabled };
+
+    if file_stem.contains("fragment") {
+        let mut context_element_namespace = Namespace::HTML;
+        let mut context_element_tag_name = "";
+        let context_element = file_stem
+            .split('.')
+            .last()
+            .expect("failed to get context element from filename");
+
+        if context_element.contains('_') {
+            let mut splited = context_element.split('_');
+
+            if let Some(namespace) = splited.next() {
+                context_element_namespace = match namespace {
+                    "math" => Namespace::MATHML,
+                    "svg" => Namespace::SVG,
+                    _ => {
+                        unreachable!();
+                    }
+                };
+            }
+
+            if let Some(tag_name) = splited.next() {
+                context_element_tag_name = tag_name;
+            }
+        } else {
+            context_element_tag_name = context_element;
+        }
+
+        let context_element = Element {
+            span: Default::default(),
+            namespace: context_element_namespace,
+            tag_name: context_element_tag_name.into(),
+            attributes: vec![],
+            children: vec![],
+            content: None,
+        };
+
+        verify_document_fragment(&input, context_element, Some(parser_config), None, true);
+    } else {
+        verify_document(&input, Some(parser_config), None, true);
+    }
 }
