4afac305cb57c311a1b039469f24fe03559f1798
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-10 16:35:34 +0900

    Fix

--- crates/binding_core_node/build.rs
+++ crates/binding_core_node/build.rs
@@ -10,7 +10,7 @@ extern crate napi_build;
 #[cfg(all(not(feature = "swc_v1"), not(feature = "swc_v2")))]
 compile_error!("Please enable swc_v1 or swc_v2 feature");
 
-#[cfg(all(feature = "swc_v1", feature = "swc_v2"))]
+#[cfg(all(feature = "swc_v1", feature = "swc_v2", not(debug_assertions)))]
 compile_error!("Features swc_v1 and swc_v2 are incompatible");
 
 fn main() {
