2276687981de7e1dcfb118265cf6f81943eb88dd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-11 15:28:52 +0900

    cspell

--- crates/swc_common/src/input.rs
+++ crates/swc_common/src/input.rs
@@ -288,8 +288,8 @@ mod tests {
 
     #[test]
     fn src_input_reset_to_1() {
-        let _ = with_test_sess("foad", |mut i| {
-            assert_eq!(i.slice(BytePos(1), BytePos(3)), "fo");
+        let _ = with_test_sess("load", |mut i| {
+            assert_eq!(i.slice(BytePos(1), BytePos(3)), "lo");
             assert_eq!(i.last_pos, BytePos(3));
             assert_eq!(i.start_pos, BytePos(3));
             assert_eq!(i.cur(), Some('a'));
