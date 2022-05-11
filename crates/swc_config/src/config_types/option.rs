aa269a630a1538e4105a03735a2951545b40cad9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-11 13:32:17 +0900

    Fix `MergingOption`

--- crates/swc_config/src/config_types/option.rs
+++ crates/swc_config/src/config_types/option.rs
@@ -1,9 +1,33 @@
 use crate::merge::Merge;
 
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct MergingOption<T>(Option<T>)
 where
-    T: Merge;
+    T: Merge + Default;
 
-impl<T> MergingOption<T> where T: Merge {}
+impl<T> MergingOption<T>
+where
+    T: Merge + Default,
+{
+    pub fn unwrap(self) -> Option<T> {
+        self.0
+    }
+}
 
-impl<T> Merge for MergingOption<T> where T: Merge {}
+impl<T> Merge for MergingOption<T>
+where
+    T: Merge + Default,
+{
+    fn merge(&mut self, other: Self) {
+        match other.0 {
+            Some(other) => {
+                if self.0.is_none() {
+                    *self = Default::default();
+                }
+
+                self.0.as_mut().unwrap().merge(other);
+            }
+            None => {}
+        }
+    }
+}
