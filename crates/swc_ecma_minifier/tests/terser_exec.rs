6679a91e95c4e6eec41017cd7e9906cd94e3a9f0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-12 17:27:35 +0900

    Let's fix codegen first

--- crates/swc_ecma_minifier/tests/terser_exec.rs
+++ crates/swc_ecma_minifier/tests/terser_exec.rs
@@ -35,6 +35,7 @@ use testing::assert_eq;
     "tests/terser/compress/**/input.js",
     exclude(
         "expansions/avoid_spread_getset_object/",
+        "expansions/avoid_spread_hole/",
         "functions/avoid_generating_duplicate_functions_compared_together_3/",
         // We don't care about ie8
         "ie8",
