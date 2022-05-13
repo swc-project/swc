8d9ace12a60a42dabce9f6046f82e20febaba7d6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-13 15:56:52 +0900

    Minify

--- crates/dbg-swc/src/minify.rs
+++ crates/dbg-swc/src/minify.rs
@@ -6,9 +6,14 @@ use std::{
 
 use anyhow::{bail, Context, Result};
 use clap::{Args, Subcommand};
+use rayon::prelude::*;
 use swc_common::SourceMap;
+use swc_ecma_minifier::option::MinifyOptions;
+use swc_ecma_transforms_base::fixer::fixer;
+use swc_ecma_visit::VisitMutWith;
+use swc_timer::timer;
 
-use crate::util::{all_js_files, wrap_task};
+use crate::util::{all_js_files, parse_js, print_js, wrap_task};
 
 /// Minify a javascript file.
 #[derive(Debug, Subcommand)]
@@ -41,12 +46,69 @@ pub struct EnsureSize {
 
 impl EnsureSize {
     pub fn run(self, cm: Arc<SourceMap>) -> Result<()> {
-        let all_files = all_js_files(&self.path);
+        let all_files = all_js_files(&self.path)?;
 
         dbg!(&all_files);
 
-        Ok(())
+        let results = all_files
+            .par_iter()
+            .map(|js_file| self.check_file(cm.clone(), &js_file))
+            .map(|v| v.transpose())
+            .flatten()
+            .collect::<Result<Vec<_>>>()?;
+
+        if results.is_empty() {
+            return Ok(());
+        }
+        for report in &results {
+            dbg!(&report);
+        }
+
+        bail!("found some issues")
     }
+
+    fn check_file(&self, cm: Arc<SourceMap>, js_file: &Path) -> Result<Option<SizeIssue>> {
+        wrap_task(|| {
+            let fm = cm.load_file(js_file).context("failed to load file")?;
+            let module = parse_js(fm)?;
+
+            let mut minified = {
+                let _timer = timer!("minify");
+                swc_ecma_minifier::optimize(
+                    module.module,
+                    cm.clone(),
+                    None,
+                    None,
+                    &MinifyOptions {
+                        compress: Some(Default::default()),
+                        mangle: Some(Default::default()),
+                        ..Default::default()
+                    },
+                    &swc_ecma_minifier::option::ExtraOptions {
+                        unresolved_mark: module.unresolved_mark,
+                        top_level_mark: module.top_level_mark,
+                    },
+                )
+            };
+
+            minified.visit_mut_with(&mut fixer(None));
+
+            let code = print_js(cm, &minified, true).context("failed to convert ast to code")?;
+
+            eprintln!("The output size of swc minifier: {}", code.len());
+
+            Ok(None)
+        })
+        .with_context(|| format!("failed to check file: {}", js_file.display()))
+    }
+}
+
+#[derive(Debug)]
+struct SizeIssue {
+    filename: PathBuf,
+
+    /// `true` if the output is gathered with mangler enabled.
+    mangler_enabled: bool,
 }
 
 fn get_terser_output(file: &Path, compress: bool, mangle: bool) -> Result<String> {
