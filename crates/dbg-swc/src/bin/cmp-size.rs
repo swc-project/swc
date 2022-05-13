37c70c6b4debc002dbe3bb6a91456a4e57a617e8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-13 21:25:52 +0900

    Add a bin

---
+++ crates/dbg-swc/src/bin/cmp-size.rs
@@ -0,0 +1,5 @@
+//! https://embed.cs.utah.edu/creduce/using/
+//!
+//! This binary is used to check if `input.js` is interesting,
+//! i.e. if terser or esbuild generates smaller output for input.js,
+//! it's interesting (in creduce terms) and this script returns 0
