77cd06e12337c4d18d029fdee2fb96ad1d4fa099
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-13 15:15:15 +0900

    Hmm

--- crates/dbg-swc/src/minify.rs
+++ crates/dbg-swc/src/minify.rs
@@ -1,5 +1,34 @@
-use clap::Subcommand;
+use anyhow::Result;
+use clap::{Args, Subcommand};
 
 /// Minify a javascript file.
 #[derive(Debug, Subcommand)]
-pub enum MinifyCommand {}
+pub enum MinifyCommand {
+    EnsureSize(EnsureSize),
+}
+
+impl MinifyCommand {
+    pub fn run(self) -> Result<()> {
+        match self {
+            MinifyCommand::EnsureSize(cmd) => cmd.run(),
+        }
+    }
+}
+
+/// Ensure that we are performing better than other minification tools.
+#[derive(Debug, Args)]
+pub struct EnsureSize {
+    #[clap(long)]
+    pub no_terser: bool,
+
+    #[clap(long)]
+    pub no_esbuild: bool,
+}
+
+impl EnsureSize {
+    pub fn run(self) -> Result<()> {}
+}
+
+fn get_terser_output(code: &str, mangle: bool) -> Result<String> {}
+
+fn get_esbuild_output(code: &str, mangle: bool) -> Result<String> {}
