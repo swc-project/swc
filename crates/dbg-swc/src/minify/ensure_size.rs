1ecc88b628c0c26e9cc811e81cc38bdd11fc1a15
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-13 16:32:11 +0900

    compare command

--- crates/dbg-swc/src/minify/ensure_size.rs
+++ crates/dbg-swc/src/minify/ensure_size.rs
@@ -1,6 +1,5 @@
 use std::{
     path::{Path, PathBuf},
-    process::{Command, Stdio},
     sync::Arc,
 };
 
@@ -13,7 +12,10 @@ use swc_ecma_transforms_base::fixer::fixer;
 use swc_ecma_visit::VisitMutWith;
 use tracing::info;
 
-use crate::util::{all_js_files, parse_js, print_js, wrap_task};
+use crate::{
+    minify::{get_esbuild_output, get_terser_output},
+    util::{all_js_files, parse_js, print_js, wrap_task},
+};
 
 /// Ensure that we are performing better than other minification tools.
 #[derive(Debug, Args)]
@@ -186,53 +188,4 @@ struct SizeIssue {
 struct MinifierOutput {
     mangled_size: usize,
     no_mangle_size: usize,
-}
-
-fn get_terser_output(file: &Path, compress: bool, mangle: bool) -> Result<String> {
-    wrap_task(|| {
-        let mut cmd = Command::new("terser");
-        cmd.stderr(Stdio::inherit());
-
-        if compress {
-            cmd.arg("--compress");
-        }
-        if mangle {
-            cmd.arg("--mangle");
-        }
-        cmd.arg("--");
-        cmd.arg(file);
-
-        let output = cmd.output().context("failed to get output")?;
-
-        if !output.status.success() {
-            bail!("failed to run terser");
-        }
-
-        String::from_utf8(output.stdout).context("terser emitted non-utf8 string")
-    })
-    .with_context(|| format!("failed to get output of {} from terser", file.display()))
-}
-
-fn get_esbuild_output(file: &Path, mangle: bool) -> Result<String> {
-    wrap_task(|| {
-        let mut cmd = Command::new("esbuild");
-        cmd.stderr(Stdio::inherit());
-
-        cmd.arg(file);
-
-        if mangle {
-            cmd.arg("--minify");
-        } else {
-            cmd.arg("--minify-syntax").arg("--minify-whitespace");
-        }
-
-        let output = cmd.output().context("failed to get output")?;
-
-        if !output.status.success() {
-            bail!("failed to run esbuild");
-        }
-
-        String::from_utf8(output.stdout).context("esbuild emitted non-utf8 string")
-    })
-    .with_context(|| format!("failed to get output of {} from esbuild", file.display()))
 }
