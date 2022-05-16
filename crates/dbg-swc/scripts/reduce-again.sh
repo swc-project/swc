dcbcf38162d7406d307fe709f75ad3431bd90997
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-16 12:40:37 +0900

    Parallel

--- crates/dbg-swc/scripts/reduce-again.sh
+++ crates/dbg-swc/scripts/reduce-again.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-find .input -type f | xargs -L 1 -I {} cargo run --release -- minify reduce --mode size '{}' && rm '{}'
+cargo run --release -- minify reduce --mode size ./input
 # mv data/*.js ./.input/
