bfa5ea7bd93ecf2adbda8bf8520b50f36bb9bbf2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-20 12:54:26 +0900

    Add a test

---
+++ crates/swc_ecma_minifier/tests/single-pass/6/input.js
@@ -0,0 +1,33 @@
+const loadScript = (options) => {
+    const { id, onLoad = () => null, ...rest } = options;
+
+    return new Promise((resolve) => {
+        let scriptEl = document.getElementById(id);
+
+        const isMounted = !!scriptEl;
+
+        if (!scriptEl) {
+            scriptEl = document.createElement("script");
+            Object.keys(rest).forEach((key) => (scriptEl[key] = rest[key]));
+            scriptEl.id = id;
+            scriptEl.async = true;
+            scriptEl.type = "text/javascript";
+        }
+        if (rest.src) {
+            scriptEl.addEventListener("load", () => {
+                onLoad();
+                return resolve();
+            });
+        }
+
+        if (!isMounted) {
+            // when enabling swcMinify, this section won't be executed
+            document.getElementsByTagName("head")[0].appendChild(scriptEl);
+        }
+
+        if (!rest.src) {
+            onLoad();
+            return resolve();
+        }
+    });
+};
