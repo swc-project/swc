132296dca27b747237a43d6869e7b7f6688ca1c6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-21 23:07:21 +0900

    Add a test for #4734

---
+++ node-swc/__tests__/transform/issue_4734_test.mjs
@@ -0,0 +1,30 @@
+import swc from "../../..";
+import { dirname, join } from "path";
+import { platform } from "os";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+it("should transpile decorators", async () => {
+    const filename = join(
+        __dirname,
+        "..",
+        "..",
+        "tests",
+        "issue-4734",
+        "1",
+        "index.tsx"
+    );
+    console.log(filename);
+    const { code } = await swc.transformFile(filename, {
+    });
+    expect(code).toMatchInlineSnapshot(`
+"import { jsx as _jsx } from \\"react/jsx-runtime\\";
+export default function foo() {
+    return /*#__PURE__*/ _jsx(\\"div\\", {
+        children: \\"Hello\\"
+    });
+};
+"
+`);
+});
