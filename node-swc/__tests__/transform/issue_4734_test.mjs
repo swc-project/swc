0e0709e0bbc6890dd014146269bf52a7d2913c3c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-21 23:11:35 +0900

    Update test refs

--- node-swc/__tests__/transform/issue_4734_test.mjs
+++ node-swc/__tests__/transform/issue_4734_test.mjs
@@ -16,15 +16,23 @@ it("should transpile decorators", async () => {
         "index.ts"
     );
     console.log(filename);
-    const { code } = await swc.transformFile(filename, {
-    });
+    const { code } = await swc.transformFile(filename, {});
     expect(code).toMatchInlineSnapshot(`
-"import { jsx as _jsx } from \\"react/jsx-runtime\\";
-export default function foo() {
-    return /*#__PURE__*/ _jsx(\\"div\\", {
-        children: \\"Hello\\"
-    });
-};
-"
-`);
+        "var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+            var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+            if (typeof Reflect === \\"object\\" && typeof Reflect.decorate === \\"function\\") r = Reflect.decorate(decorators, target, key, desc);
+            else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+            return c > 3 && r && Object.defineProperty(target, key, r), r;
+        };
+        class TestClass {
+            abc() {}
+        }
+        __decorate([
+            foo
+        ], TestClass.prototype, \\"abc\\", null);
+        function foo(target, propertyKey) {
+            console.log(\`Decorating \${target}.\${String(propertyKey)}\`);
+        }
+        "
+    `);
 });
