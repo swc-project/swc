341d47efef510c8b68a20af86941755e1813b26c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-22 23:06:21 +0900

    Update test refs

--- node-swc/__tests__/transform/issue_4730_test.mjs
+++ node-swc/__tests__/transform/issue_4730_test.mjs
@@ -33,7 +33,55 @@ it("should work", async () => {
         },
     });
     expect(code).toMatchInlineSnapshot(`
-        "\\"use strict\\";
-        "
-    `);
+"\\"use strict\\";
+var _b = require(\\"@print/b\\");
+function _getRequireWildcardCache() {
+    if (typeof WeakMap !== \\"function\\") return null;
+    var cache = new WeakMap();
+    _getRequireWildcardCache = function() {
+        return cache;
+    };
+    return cache;
+}
+function _interopRequireWildcard(obj) {
+    if (obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || typeof obj !== \\"object\\" && typeof obj !== \\"function\\") {
+        return {
+            default: obj
+        };
+    }
+    var cache = _getRequireWildcardCache();
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {};
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+async function display() {
+    const displayA = await Promise.resolve().then(function() {
+        return _interopRequireWildcard(require(\\"@print/a\\"));
+    }).then((c)=>c.displayA);
+    console.log(displayA());
+    console.log((0, _b).displayB());
+}
+display();
+"
+`);
 });
