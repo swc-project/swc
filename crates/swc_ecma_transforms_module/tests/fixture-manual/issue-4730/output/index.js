735b9695058fe447d2321a66ad44eca5ed77b8ae
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-23 01:54:03 +0900

    Fix test input

--- crates/swc_ecma_transforms_module/tests/fixture-manual/issue-4730/output/index.js
+++
@@ -1,7 +0,0 @@
-import { displayB } from "@print/b";
-async function display() {
-    const displayA = await import("@print/a").then((c)=>c.displayA);
-    console.log(displayA());
-    console.log(displayB());
-}
-display();
