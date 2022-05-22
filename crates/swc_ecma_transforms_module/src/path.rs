417b2182cead074c9ebf9358d53f353157637a8d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-23 05:12:22 +0900

    fix(es/loader): Fix `jsc.paths` on windows (#4739)

--- crates/swc_ecma_transforms_module/src/path.rs
+++ crates/swc_ecma_transforms_module/src/path.rs
@@ -249,17 +249,8 @@ where
             }
         }
 
-        debug_assert!(
-            !rel_path.is_absolute(),
-            "Resolved path should not be absolute (in swc repository) but found {}\nbase: \
-             {}\ntarget: {}",
-            rel_path.display(),
-            base.display(),
-            target.display(),
-        );
-
         let s = rel_path.to_string_lossy();
-        let s = if s.starts_with('.') || s.starts_with('/') {
+        let s = if s.starts_with('.') || s.starts_with('/') || rel_path.is_absolute() {
             s
         } else {
             Cow::Owned(format!("./{}", s))
