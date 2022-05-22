a498157321cf1400efa9e3f6c50fa9c1f65c1053
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-22 22:53:14 +0900

    Add a test

--- node-swc/__tests__/transform/issue_4730_test.mjs
+++ node-swc/__tests__/transform/issue_4730_test.mjs
@@ -17,41 +17,23 @@ it("should work", async () => {
     );
     console.log(filename);
     const { code } = await swc.transformFile(filename, {
-        "jsc": {
-            "parser": {
-                "syntax": "typescript",
-                "dynamicImport": true
+        jsc: {
+            parser: {
+                syntax: "typescript",
+                dynamicImport: true,
             },
-            "target": "es2020",
-            "paths": {
-                "@print/a": [
-                    join(process.cwd(), "./packages/a/src/index.ts")
-                ],
-                "@print/b": [
-                    join(process.cwd(), "./packages/b/src/index.ts")
-                ]
-            }
+            target: "es2020",
+            paths: {
+                "@print/a": [join(process.cwd(), "./packages/a/src/index.ts")],
+                "@print/b": [join(process.cwd(), "./packages/b/src/index.ts")],
+            },
         },
-        "module": {
-            "type": "commonjs"
-        }
+        module: {
+            type: "commonjs",
+        },
     });
     expect(code).toMatchInlineSnapshot(`
-        "var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
-            var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-            if (typeof Reflect === \\"object\\" && typeof Reflect.decorate === \\"function\\") r = Reflect.decorate(decorators, target, key, desc);
-            else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-            return c > 3 && r && Object.defineProperty(target, key, r), r;
-        };
-        class TestClass {
-            abc() {}
-        }
-        __decorate([
-            foo
-        ], TestClass.prototype, \\"abc\\", null);
-        function foo(target, propertyKey) {
-            console.log(\`Decorating \${target}.\${String(propertyKey)}\`);
-        }
+        "\\"use strict\\";
         "
     `);
 });
