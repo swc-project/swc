f64b92ec15a328e8b40f3fe0444312c046c79693
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-23 00:16:49 +0900

    external helpers

--- node-swc/__tests__/transform/issue_4730_test.mjs
+++ node-swc/__tests__/transform/issue_4730_test.mjs
@@ -6,17 +6,8 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 it("should work", async () => {
-    const dir = path.join(__dirname,
-        "..",
-        "..",
-        "tests",
-        "issue-4730",
-    );
-    const filename = join(
-        dir,
-        "src",
-        "index.ts"
-    );
+    const dir = join(__dirname, "..", "..", "tests", "issue-4730");
+    const filename = join(dir, "src", "index.ts");
     console.log(filename);
     const { code } = await swc.transformFile(filename, {
         jsc: {
@@ -29,61 +20,62 @@ it("should work", async () => {
                 "@print/a": [join(dir, "./packages/a/src/index.ts")],
                 "@print/b": [join(dir, "./packages/b/src/index.ts")],
             },
+            externalHelpers: true
         },
         module: {
             type: "commonjs",
         },
     });
     expect(code).toMatchInlineSnapshot(`
-"\\"use strict\\";
-var _b = require(\\"@print/b\\");
-function _getRequireWildcardCache() {
-    if (typeof WeakMap !== \\"function\\") return null;
-    var cache = new WeakMap();
-    _getRequireWildcardCache = function() {
-        return cache;
-    };
-    return cache;
-}
-function _interopRequireWildcard(obj) {
-    if (obj && obj.__esModule) {
-        return obj;
-    }
-    if (obj === null || typeof obj !== \\"object\\" && typeof obj !== \\"function\\") {
-        return {
-            default: obj
-        };
-    }
-    var cache = _getRequireWildcardCache();
-    if (cache && cache.has(obj)) {
-        return cache.get(obj);
-    }
-    var newObj = {};
-    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
-    for(var key in obj){
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
-            if (desc && (desc.get || desc.set)) {
-                Object.defineProperty(newObj, key, desc);
-            } else {
-                newObj[key] = obj[key];
-            }
+        "\\"use strict\\";
+        var _b = require(\\"../packages/b/src/index\\");
+        function _getRequireWildcardCache() {
+            if (typeof WeakMap !== \\"function\\") return null;
+            var cache = new WeakMap();
+            _getRequireWildcardCache = function() {
+                return cache;
+            };
+            return cache;
         }
-    }
-    newObj.default = obj;
-    if (cache) {
-        cache.set(obj, newObj);
-    }
-    return newObj;
-}
-async function display() {
-    const displayA = await Promise.resolve().then(function() {
-        return _interopRequireWildcard(require(\\"@print/a\\"));
-    }).then((c)=>c.displayA);
-    console.log(displayA());
-    console.log((0, _b).displayB());
-}
-display();
-"
-`);
+        function _interopRequireWildcard(obj) {
+            if (obj && obj.__esModule) {
+                return obj;
+            }
+            if (obj === null || typeof obj !== \\"object\\" && typeof obj !== \\"function\\") {
+                return {
+                    default: obj
+                };
+            }
+            var cache = _getRequireWildcardCache();
+            if (cache && cache.has(obj)) {
+                return cache.get(obj);
+            }
+            var newObj = {};
+            var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+            for(var key in obj){
+                if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                    var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+                    if (desc && (desc.get || desc.set)) {
+                        Object.defineProperty(newObj, key, desc);
+                    } else {
+                        newObj[key] = obj[key];
+                    }
+                }
+            }
+            newObj.default = obj;
+            if (cache) {
+                cache.set(obj, newObj);
+            }
+            return newObj;
+        }
+        async function display() {
+            const displayA = await Promise.resolve().then(function() {
+                return _interopRequireWildcard(require(\\"../packages/a/src/index\\"));
+            }).then((c)=>c.displayA);
+            console.log(displayA());
+            console.log((0, _b).displayB());
+        }
+        display();
+        "
+    `);
 });
