18d23b591b69edfe67b50bf86d9bac56b677f6a6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-23 14:47:32 +0900

    Doc

--- crates/swc_ecma_minifier_base/src/lib.rs
+++ crates/swc_ecma_minifier_base/src/lib.rs
@@ -1,3 +1,11 @@
+//! Internal module of `swc_ecma_minifier`.
+//!
+//! This logics are splitted as a separate crate, because a crate is the unit of
+//! `cargo build`.
+//!
+//! This crate **will** make breaking changes without proper version bump.
+//! You should not use this crate directly.
+
 #![deny(warnings)]
 
 pub mod debug;
