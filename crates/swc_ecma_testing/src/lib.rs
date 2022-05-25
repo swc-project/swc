4a3af6eb1017b233f13872bab5ab3a77aa0e84c8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-25 14:24:57 +0900

    Method

--- crates/swc_ecma_testing/src/lib.rs
+++ crates/swc_ecma_testing/src/lib.rs
@@ -1,1 +1,23 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum JsRuntime {
+    Node,
+    Deno,
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct JsExecOptions {
+    pub runtime: JsRuntime,
+
+    /// Cache the result of the execution.
+    ///
+    /// If `true`, the result of the execution will be cached.
+    /// Cache is not removed and it will be reused if the source code is
+    /// identical.
+    ///
+    /// Note that this cache is stored in cargo target directory and will be
+    /// removed by `cargo clean`.
+    pub cache: bool,
+}
+
+/// Executes `js_code` and capture thw output.
+pub fn exec_js(js_code: &str, opts: JsExecOptions) {}
