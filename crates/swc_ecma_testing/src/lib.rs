9921ac6bff1fd26fee2d16c88f746a415ace340c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-25 14:25:22 +0900

    API

--- crates/swc_ecma_testing/src/lib.rs
+++ crates/swc_ecma_testing/src/lib.rs
@@ -1,13 +1,5 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum JsRuntime {
-    Node,
-    Deno,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct JsExecOptions {
-    pub runtime: JsRuntime,
-
     /// Cache the result of the execution.
     ///
     /// If `true`, the result of the execution will be cached.
@@ -20,4 +12,4 @@ pub struct JsExecOptions {
 }
 
 /// Executes `js_code` and capture thw output.
-pub fn exec_js(js_code: &str, opts: JsExecOptions) {}
+pub fn exec_node_js(js_code: &str, opts: JsExecOptions) {}
