6f04e846394df4bcf1e82748cd5df739b7df7a96
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-25 17:10:44 +0900

    perf(common): Use `jemalloc` for linux (#4791)

--- crates/swc_node_base/src/lib.rs
+++ crates/swc_node_base/src/lib.rs
@@ -6,10 +6,6 @@
 #[global_allocator]
 static GLOBAL: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
 
-#[cfg(all(
-    target_os = "linux",
-    target_env = "gnu",
-    all(target_arch = "x86_64", target_arch = "aarch64")
-))]
+#[cfg(target_os = "linux")]
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
