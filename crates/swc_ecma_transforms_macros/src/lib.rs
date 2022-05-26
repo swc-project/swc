0a1cad266c15301bde9934e4738dbec773c1e231
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-26 22:51:31 +0900

    Remove macro

--- crates/swc_ecma_transforms_macros/src/lib.rs
+++ crates/swc_ecma_transforms_macros/src/lib.rs
@@ -7,7 +7,6 @@ use swc_macros_common::print;
 
 mod common;
 mod fast;
-mod parallel;
 
 /// This macro adds fast-path to the `swc_ecma_visit::Fold` and
 /// `swc_ecma_visit::Visit`.
@@ -28,23 +27,4 @@ pub fn fast_path(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item = syn::parse(item).expect("failed to parse input as an item");
     let expanded = fast::expand(attr.into(), item);
     print("fast_path", expanded.dump())
-}
-
-///
-/// # Input
-///
-/// Basically, input for each types are wrapped in the suffix of the visitor
-/// method for the type.
-///
-/// ## `#[threashold]`
-///
-/// ```ignore,
-/// #[parallel(module_items(threshold = "4"))]
-/// impl VisitMut for Pass {}
-/// ```
-#[proc_macro_attribute]
-pub fn parallel(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item = syn::parse(item).expect("failed to parse input as an item");
-    let expanded = parallel::expand(attr.into(), item);
-    print("parallel", expanded.dump())
 }
