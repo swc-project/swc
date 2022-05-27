5738e10c11770fb839cec14cae7624cece47101b
magic-akari <akari.ccino@gmail.com>
2022-05-28 02:27:33 +0800

    rewrite ModuleDeclStrip

--- crates/swc_ecma_transforms_module/src/module_decl_strip.rs
+++ crates/swc_ecma_transforms_module/src/module_decl_strip.rs
@@ -1,24 +1,109 @@
-use std::mem::swap;
-
-use swc_common::{
-    collections::{AHashMap, AHashSet},
-    util::take::Take,
-    DUMMY_SP,
-};
+use indexmap::IndexMap;
+use swc_atoms::{js_word, JsWord};
+use swc_common::{collections::AHashSet, util::take::Take, Span, DUMMY_SP};
 use swc_ecma_ast::*;
-use swc_ecma_utils::{find_pat_ids, private_ident, quote_ident};
+use swc_ecma_utils::{find_pat_ids, private_ident};
 use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 
 #[derive(Default)]
 pub struct ModuleDeclStrip {
-    stmt: Option<Stmt>,
-    /// Map<src, ImportSpecifier[]>
-    pub import: AHashMap<Str, Vec<ImportSpecifier>>,
-    /// Map<exported, orig>
-    pub export: AHashMap<ModuleExportName, ModuleExportName>,
-    pub export_all: AHashSet<Str>,
+    /// all import/export ordered by path
+    pub link: IndexMap<JsWord, AHashSet<Specifier>>,
 
-    export_src: Option<Str>,
+    /// local exported binding
+    ///
+    /// `export { foo as "1", bar }`
+    /// -> Map("1" => foo, bar => bar)
+    pub export: IndexMap<(JsWord, Span), Ident>,
+
+    stmt: Option<Stmt>,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+pub enum Specifier {
+    /// ```javascript
+    /// import { imported as local, local } from "mod";
+    /// import { "imported" as local } from "mod";
+    /// ```
+    ImportNamed {
+        imported: Option<(JsWord, Span)>,
+        local: Ident,
+    },
+    /// ```javascript
+    /// import foo from "mod";
+    /// ```
+    ImportDefault(Ident),
+    /// ```javascript
+    /// import * as foo from "mod";
+    /// ```
+    ImportStarAs(Ident),
+    /// ```javascript
+    /// export { orig, orig as exported } from "mod";
+    /// export { "orig", "orig" as "exported" } from "mod";
+    /// ```
+    ExportNamed {
+        orig: (JsWord, Span),
+        exported: Option<(JsWord, Span)>,
+    },
+    /// ```javascript
+    /// export * as foo from "mod";
+    /// export * as "bar" from "mod";
+    /// ```
+    ExportStarAs(JsWord, Span),
+    /// ```javascript
+    /// export * from "mod";
+    /// ```
+    ExportStar,
+}
+
+impl From<ImportSpecifier> for Specifier {
+    fn from(i: ImportSpecifier) -> Self {
+        match i {
+            ImportSpecifier::Named(ImportNamedSpecifier {
+                local, imported, ..
+            }) => {
+                let imported = imported.map(|e| match e {
+                    ModuleExportName::Ident(Ident { span, sym, .. }) => (sym, span),
+                    ModuleExportName::Str(Str { span, value, .. }) => (value, span),
+                });
+
+                Self::ImportNamed { local, imported }
+            }
+            ImportSpecifier::Default(ImportDefaultSpecifier { local, .. }) => {
+                Self::ImportDefault(local)
+            }
+            ImportSpecifier::Namespace(ImportStarAsSpecifier { local, .. }) => {
+                Self::ImportStarAs(local)
+            }
+        }
+    }
+}
+
+impl From<ExportSpecifier> for Specifier {
+    fn from(e: ExportSpecifier) -> Self {
+        match e {
+            ExportSpecifier::Namespace(ExportNamespaceSpecifier { name, .. }) => match name {
+                ModuleExportName::Ident(Ident { span, sym, .. }) => Self::ExportStarAs(sym, span),
+                ModuleExportName::Str(Str { span, value, .. }) => Self::ExportStarAs(value, span),
+            },
+            ExportSpecifier::Default(_) => {
+                unreachable!("`export default` does not support re-export")
+            }
+            ExportSpecifier::Named(ExportNamedSpecifier { orig, exported, .. }) => {
+                let orig = match orig {
+                    ModuleExportName::Ident(Ident { span, sym, .. }) => (sym, span),
+                    ModuleExportName::Str(Str { span, value, .. }) => (value, span),
+                };
+
+                let exported = exported.map(|exported| match exported {
+                    ModuleExportName::Ident(Ident { span, sym, .. }) => (sym, span),
+                    ModuleExportName::Str(Str { span, value, .. }) => (value, span),
+                });
+
+                Self::ExportNamed { orig, exported }
+            }
+        }
+    }
 }
 
 impl ModuleDeclStrip {
@@ -62,7 +147,10 @@ impl VisitMut for ModuleDeclStrip {
             specifiers, src, ..
         } = n.take();
 
-        self.import.entry(src).or_default().extend(specifiers);
+        self.link
+            .entry(src.value)
+            .or_default()
+            .extend(specifiers.into_iter().map(Into::into));
     }
 
     /// ```javascript
@@ -81,62 +169,56 @@ impl VisitMut for ModuleDeclStrip {
     fn visit_mut_export_decl(&mut self, n: &mut ExportDecl) {
         self.export
             .extend(find_pat_ids::<_, Ident>(&n.decl).into_iter().map(|id| {
-                (
-                    ModuleExportName::Ident(id.clone()),
-                    ModuleExportName::Ident(id),
-                )
+                let ident = id.clone();
+                ((id.sym, id.span), ident)
             }));
 
         self.set_stmt(n.decl.take());
     }
 
+    /// ```javascript
+    /// export { foo, foo as bar, foo as "baz" };
+    /// export { "foo", foo as bar, "foo" as "baz" } from "mod";
+    /// export * as foo from "mod";
+    /// export * as "bar" from "mod";
+    /// ```
     fn visit_mut_named_export(&mut self, n: &mut NamedExport) {
-        swap(&mut self.export_src, &mut n.src);
-        n.visit_mut_children_with(self);
-        swap(&mut self.export_src, &mut n.src);
-    }
+        let NamedExport {
+            specifiers, src, ..
+        } = n.take();
 
-    /// ```javascript
-    /// export { orig };
-    /// export { orig as exported };
-    /// ```
-    ///
-    /// split re-export
-    /// ```javascript
-    /// export { foo as bae } from "x"
-    /// export { "1" as "2" } from "y";
-    /// ```
-    /// ->
-    /// ```javascript
-    /// import { foo as _x } from "x";
-    /// import { "1" as _y } from "y";
-    /// export { _x as bar, _y as "2" };
-    /// ```
-    ///
-    /// `export { "1" as "2" }` without src is invalid.
-    fn visit_mut_export_named_specifier(&mut self, n: &mut ExportNamedSpecifier) {
-        if let Some(src) = &self.export_src {
-            let bridge = private_ident!("_bridge");
-
-            self.import.entry(src.clone()).or_default().push(
-                ImportNamedSpecifier {
-                    span: DUMMY_SP,
-                    local: bridge.clone(),
-                    imported: None,
-                    is_type_only: false,
-                }
-                .into(),
-            );
-
-            self.export.insert(
-                n.exported.clone().unwrap_or_else(|| n.orig.clone()),
-                bridge.into(),
-            );
+        if let Some(src) = src {
+            self.link
+                .entry(src.value)
+                .or_default()
+                .extend(specifiers.into_iter().map(Into::into));
         } else {
-            self.export.insert(
-                n.exported.clone().unwrap_or_else(|| n.orig.clone()),
-                n.orig.clone(),
-            );
+            self.export.extend(specifiers.into_iter().map(|e| match e {
+                ExportSpecifier::Namespace(..) => unreachable!("`export * as` without src"),
+                ExportSpecifier::Default(ExportDefaultSpecifier { exported }) => {
+                    ((js_word!("default"), DUMMY_SP), exported)
+                }
+                ExportSpecifier::Named(ExportNamedSpecifier { orig, exported, .. }) => {
+                    let orig = match orig {
+                        ModuleExportName::Ident(id) => id,
+                        ModuleExportName::Str(_) => {
+                            unreachable!(r#"`export {{ "foo" }}` is not valid"#)
+                        }
+                    };
+
+                    if let Some(exported) = exported {
+                        let exported = match exported {
+                            ModuleExportName::Ident(Ident { span, sym, .. }) => (sym, span),
+                            ModuleExportName::Str(Str { span, value, .. }) => (value, span),
+                        };
+
+                        (exported, orig)
+                    } else {
+                        let exported = orig.sym.clone();
+                        ((exported, orig.span), orig)
+                    }
+                }
+            }))
         }
     }
 
@@ -154,7 +236,7 @@ impl VisitMut for ModuleDeclStrip {
         let ident = ident.unwrap_or_else(|| private_ident!("_default"));
 
         self.export
-            .insert(quote_ident!("default").into(), ident.clone().into());
+            .insert((js_word!("default"), DUMMY_SP), ident.clone());
 
         self.set_stmt(Stmt::Decl(
             ClassDecl {
@@ -180,7 +262,7 @@ impl VisitMut for ModuleDeclStrip {
         let ident = ident.unwrap_or_else(|| private_ident!("_default"));
 
         self.export
-            .insert(quote_ident!("default").into(), ident.clone().into());
+            .insert((js_word!("default"), DUMMY_SP), ident.clone());
 
         self.set_stmt(Stmt::Decl(
             FnDecl {
@@ -205,7 +287,7 @@ impl VisitMut for ModuleDeclStrip {
         let ident = private_ident!("_default");
 
         self.export
-            .insert(quote_ident!("default").into(), ident.clone().into());
+            .insert((js_word!("default"), DUMMY_SP), ident.clone());
 
         let var_declarator = VarDeclarator {
             span: DUMMY_SP,
@@ -225,6 +307,9 @@ impl VisitMut for ModuleDeclStrip {
     }
 
     fn visit_mut_export_all(&mut self, n: &mut ExportAll) {
-        self.export_all.insert(n.src.take());
+        self.link
+            .entry(n.take().src.value)
+            .or_default()
+            .insert(Specifier::ExportStar);
     }
 }
