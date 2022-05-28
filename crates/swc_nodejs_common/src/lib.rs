8f8e1483dd18a58f98483ff8b690de3af6af9b56
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-29 03:10:34 +0900

    Fix logger

--- crates/swc_nodejs_common/src/lib.rs
+++ crates/swc_nodejs_common/src/lib.rs
@@ -17,9 +17,9 @@ pub fn init_default_trace_subscriber() {
     let _unused = tracing_subscriber::FmtSubscriber::builder()
         .without_time()
         .with_target(false)
+        .with_writer(std::io::stderr)
         .with_ansi(true)
         .with_env_filter(EnvFilter::from_env("SWC_LOG"))
-        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::ERROR.into()))
         .try_init();
 }
 
