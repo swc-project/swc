3882a6dd7e418bc86370d2387fa47b7a71dc3c2e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-29 10:23:23 +0900

    Typo

--- crates/swc_js_interop/src/impls/mod.rs
+++ crates/swc_js_interop/src/impls/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(target_arch = "wasm32")]
+pub use self::wasm::*;
+
 mod napi;
 mod subprocess;
 #[cfg(target_arch = "wasm32")]
--- crates/swc_js_interop/src/impls/wasm.rs
+++ crates/swc_js_interop/src/impls/wasm.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module = "/wasm-rt.js")]
+#[wasm_bindgen(module = "./wasm-rt.js")]
 extern "C" {
     fn run(src: &str);
 }
