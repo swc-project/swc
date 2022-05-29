2d1017cc88632399e41ffbefd36485e62e24014c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-05-29 10:39:51 +0900

    Expose

--- crates/swc_js_interop/src/lib.rs
+++ crates/swc_js_interop/src/lib.rs
@@ -2,4 +2,11 @@
 //!
 //! This is js executer runtime for swc plugin host. (`@swc/core` or `next-swc`)
 
+use anyhow::Result;
+
 mod impls;
+
+/// `fn_src`: The function source code, which will be cached.
+pub async fn exec_js(fn_src: &str, data: Vec<u8>) -> Result<Vec<u8>> {
+    impls::exec_js(fn_src, data).await
+}
