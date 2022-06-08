05ed6277fb27ffe2ababde3d9303ecbbc1abd8e0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-08 12:51:01 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-4xxx/4899/output/index.ts
@@ -0,0 +1,24 @@
+define([
+    "require",
+    "@swc/helpers/lib/_ts_decorate.js"
+], function(require, _ts_decorate) {
+    "use strict";
+    function es5ClassCompat(target) {
+        function _() {
+            return Reflect.construct(target, arguments, this.constructor);
+        }
+        Object.defineProperty(_, "name", Object.getOwnPropertyDescriptor(target, "name"));
+        Object.setPrototypeOf(_, target);
+        Object.setPrototypeOf(_.prototype, target.prototype);
+        return _;
+    }
+    let Foo = class Foo1 {
+        static create() {
+            return new Foo();
+        }
+        constructor(){}
+    };
+    Foo = _ts_decorate([
+        es5ClassCompat
+    ], Foo);
+});
