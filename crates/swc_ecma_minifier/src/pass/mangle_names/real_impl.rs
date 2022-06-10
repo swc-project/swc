b3eace1a6079f5cb1020443f48186a0e257d64b8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-10 17:16:31 +0900

    Use custom decl collector

--- crates/swc_ecma_minifier/src/pass/mangle_names/real_impl.rs
+++ crates/swc_ecma_minifier/src/pass/mangle_names/real_impl.rs
@@ -1,10 +1,14 @@
+use std::hash::Hash;
+
 use rustc_hash::FxHashSet;
 use swc_atoms::JsWord;
 use swc_common::{collections::AHashMap, SyntaxContext};
 use swc_ecma_ast::*;
 use swc_ecma_transforms_base::hygiene::rename;
-use swc_ecma_utils::{collect_decls, BindingCollector};
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith, VisitWith};
+use swc_ecma_utils::ident::IdentLike;
+use swc_ecma_visit::{
+    noop_visit_mut_type, noop_visit_type, Visit, VisitMut, VisitMutWith, VisitWith,
+};
 use tracing::info;
 
 use super::{analyzer::Analyzer, preserver::idents_to_preserve};
@@ -36,7 +40,9 @@ struct Mangler {
 impl Mangler {
     fn get_map<N>(&self, node: &N, skip_one: bool) -> AHashMap<Id, JsWord>
     where
-        N: VisitWith<Analyzer> + VisitWith<IdentUsageCollector> + VisitWith<BindingCollector<Id>>,
+        N: VisitWith<Analyzer>
+            + VisitWith<IdentUsageCollector>
+            + VisitWith<CustomBindingCollector<Id>>,
     {
         let mut analyzer = Analyzer {
             scope: Default::default(),
@@ -150,4 +156,143 @@ impl VisitMut for Mangler {
 
         s.visit_mut_with(&mut rename(&map));
     }
+}
+
+struct CustomBindingCollector<I>
+where
+    I: IdentLike + Eq + Hash + Send + Sync,
+{
+    only: Option<SyntaxContext>,
+    bindings: FxHashSet<I>,
+    is_pat_decl: bool,
+}
+
+impl<I> CustomBindingCollector<I>
+where
+    I: IdentLike + Eq + Hash + Send + Sync,
+{
+    fn add(&mut self, i: &Ident) {
+        if let Some(only) = self.only {
+            if only != i.span.ctxt {
+                return;
+            }
+        }
+
+        self.bindings.insert(I::from_ident(i));
+    }
+}
+
+impl<I> Visit for CustomBindingCollector<I>
+where
+    I: IdentLike + Eq + Hash + Send + Sync,
+{
+    noop_visit_type!();
+
+    fn visit_arrow_expr(&mut self, n: &ArrowExpr) {
+        let old = self.is_pat_decl;
+
+        for p in &n.params {
+            self.is_pat_decl = true;
+            p.visit_with(self);
+        }
+
+        n.body.visit_with(self);
+        self.is_pat_decl = old;
+    }
+
+    fn visit_assign_pat_prop(&mut self, node: &AssignPatProp) {
+        node.value.visit_with(self);
+
+        if self.is_pat_decl {
+            self.add(&node.key);
+        }
+    }
+
+    fn visit_class_decl(&mut self, node: &ClassDecl) {
+        node.visit_children_with(self);
+
+        self.add(&node.ident);
+    }
+
+    fn visit_class_expr(&mut self, node: &ClassExpr) {
+        node.visit_children_with(self);
+
+        if let Some(id) = &node.ident {
+            self.add(&id);
+        }
+    }
+
+    fn visit_expr(&mut self, node: &Expr) {
+        let old = self.is_pat_decl;
+        self.is_pat_decl = false;
+        node.visit_children_with(self);
+        self.is_pat_decl = old;
+    }
+
+    fn visit_fn_decl(&mut self, node: &FnDecl) {
+        node.visit_children_with(self);
+
+        self.add(&node.ident);
+    }
+
+    fn visit_fn_expr(&mut self, node: &FnExpr) {
+        node.visit_children_with(self);
+
+        if let Some(id) = &node.ident {
+            self.add(&id);
+        }
+    }
+
+    fn visit_import_default_specifier(&mut self, node: &ImportDefaultSpecifier) {
+        self.add(&node.local);
+    }
+
+    fn visit_import_named_specifier(&mut self, node: &ImportNamedSpecifier) {
+        self.add(&node.local);
+    }
+
+    fn visit_import_star_as_specifier(&mut self, node: &ImportStarAsSpecifier) {
+        self.add(&node.local);
+    }
+
+    fn visit_param(&mut self, node: &Param) {
+        let old = self.is_pat_decl;
+        self.is_pat_decl = true;
+        node.visit_children_with(self);
+        self.is_pat_decl = old;
+    }
+
+    fn visit_pat(&mut self, node: &Pat) {
+        node.visit_children_with(self);
+
+        if self.is_pat_decl {
+            if let Pat::Ident(i) = node {
+                self.add(&i.id)
+            }
+        }
+    }
+
+    fn visit_var_declarator(&mut self, node: &VarDeclarator) {
+        let old = self.is_pat_decl;
+        self.is_pat_decl = true;
+        node.name.visit_with(self);
+
+        self.is_pat_decl = false;
+        node.init.visit_with(self);
+        self.is_pat_decl = old;
+    }
+}
+
+fn collect_decls<I, N>(n: &N) -> FxHashSet<I>
+where
+    I: IdentLike + Eq + Hash + Send + Sync,
+    N: VisitWith<CustomBindingCollector<I>>,
+{
+    let mut v = CustomBindingCollector {
+        only: None,
+        bindings: Default::default(),
+        is_pat_decl: false,
+    };
+    n.visit_with(&mut v);
+    v.bindings
 }
