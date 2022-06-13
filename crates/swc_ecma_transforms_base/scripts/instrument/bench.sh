548b083d9e8c711d829741c3ba6bd017e1309ab8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-13 22:34:17 +0900

    Bnechmark

---
+++ crates/swc_ecma_transforms_base/scripts/instrument/bench.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# This instruments a full benchmark suite.
+#
+# Usage:
+#
+#   ./scripts/instrument/bench.sh typescript
+
+
+export RUST_LOG=off
+cargo profile instruments -t time --bench base --features concurrent --release -- $@
