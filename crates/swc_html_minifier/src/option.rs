f2690cce08bf2e922b7eb9146288ca78ad0eeed4
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-06-13 16:55:03 +0300

    feat(html/minifier): Compress whitespaces (#4916)

---
+++ crates/swc_html_minifier/src/option.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct MinifyOptions {
+    #[serde(default)]
+    pub collapse_whitespaces: Option<CollapseWhitespaces>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+#[serde(rename_all = "kebab-case")]
+pub enum CollapseWhitespaces {
+    All,
+    Smart,
+    Conservative,
+}
