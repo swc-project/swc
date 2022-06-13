445d58518dc1ea5ab670f4e3d9e8b833108e1cf7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-13 17:10:09 +0900

    perf(es/transforms): Make rename operator parallel (#4951)

--- crates/swc_ecma_transforms_base/src/hygiene/mod.rs
+++ crates/swc_ecma_transforms_base/src/hygiene/mod.rs
@@ -126,7 +126,11 @@ pub struct Config {
 }
 
 pub fn rename(map: &AHashMap<Id, JsWord>) -> impl '_ + Fold + VisitMut {
-    as_folder(Operator(map, Default::default()))
+    as_folder(Operator {
+        rename: map,
+        config: Default::default(),
+        extra: Default::default(),
+    })
 }
 
 /// See [hygiene_with_config] for doc. Creates a `hygiene` pass with default
@@ -202,7 +206,11 @@ impl Hygiene {
         }
 
         let ops = data.ops.into_inner();
-        n.visit_mut_with(&mut Operator(&ops.rename, self.config.clone()));
+        n.visit_mut_with(&mut Operator {
+            rename: &ops.rename,
+            config: self.config.clone(),
+            extra: Default::default(),
+        });
     }
 }
 
--- crates/swc_ecma_transforms_base/src/hygiene/unique_scope.rs
+++ crates/swc_ecma_transforms_base/src/hygiene/unique_scope.rs
@@ -2,16 +2,27 @@ use swc_common::{Mark, Span};
 use swc_ecma_ast::*;
 use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 
+use crate::perf::{cpu_count, ParVisitMut, Parallel};
+
 pub(super) fn unique_scope() -> impl VisitMut {
     UniqueScope
 }
 
+#[derive(Debug, Clone, Copy)]
 struct UniqueScope;
 
 impl UniqueScope {
     pub fn mark(&self, s: &mut Span) {
         *s = s.apply_mark(Mark::fresh(Mark::root()));
     }
+}
+
+impl Parallel for UniqueScope {
+    fn create(&self) -> Self {
+        *self
+    }
+
+    fn merge(&mut self, _: Self) {}
 }
 
 impl VisitMut for UniqueScope {
@@ -51,5 +62,13 @@ impl VisitMut for UniqueScope {
         n.visit_mut_children_with(self);
 
         self.mark(&mut n.function.span);
+    }
+
+    fn visit_mut_module_items(&mut self, n: &mut Vec<ModuleItem>) {
+        self.visit_mut_par(cpu_count() * 64, n)
+    }
+
+    fn visit_mut_stmts(&mut self, n: &mut Vec<Stmt>) {
+        self.visit_mut_par(cpu_count() * 64, n)
     }
 }
