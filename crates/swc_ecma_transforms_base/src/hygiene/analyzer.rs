272e614a04ba37f3cbcdc8b3fcb60d67492a1057
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-15 14:14:04 +0900

    Some work

--- crates/swc_ecma_transforms_base/src/hygiene/analyzer.rs
+++ crates/swc_ecma_transforms_base/src/hygiene/analyzer.rs
@@ -1,15 +1,80 @@
+use rustc_hash::FxHashSet;
+use swc_atoms::js_word;
+use swc_ecma_ast::Id;
 use swc_ecma_visit::{noop_visit_type, Visit};
 
+#[derive(Debug, Default)]
 pub(super) struct Analyzer {
     pub scope: Scope,
 
     pub is_pat_decl: bool,
 }
 
-pub(super) struct Scope {
-    children: Vec<Scope>,
+impl Analyzer {
+    fn add_decl(&mut self, id: Id) {
+        self.scope.add_decl(&id);
+    }
+
+    fn add_usage(&mut self, id: Id) {
+        self.scope.add_usage(&id);
+    }
+
+    fn with_scope<F>(&mut self, op: F)
+    where
+        F: FnOnce(&mut Analyzer),
+    {
+        {
+            let mut v = Analyzer {
+                scope: Scope {
+                    ..Default::default()
+                },
+                is_pat_decl: self.is_pat_decl,
+            };
+
+            op(&mut v);
+
+            self.scope.children.push(v.scope);
+        }
+    }
 }
 
 impl Visit for Analyzer {
     noop_visit_type!();
+}
+
+#[derive(Debug, Default)]
+pub(super) struct Scope {
+    pub data: ScopeData,
+
+    children: Vec<Scope>,
+}
+
+#[derive(Debug, Default)]
+pub struct ScopeData {
+    /// This is add-only.
+    ///
+    /// If the add-only contraint is violated, it is very likely to be a bug,
+    /// because we merge every items in children to current scope.
+    all: FxHashSet<Id>,
+
+    queue: Vec<Id>,
+}
+
+impl Scope {
+    pub(super) fn add_decl(&mut self, id: &Id) {
+        if id.0 == js_word!("arguments") {
+            return;
+        }
+
+        self.data.all.insert(id.clone());
+        self.data.queue.push(id.clone());
+    }
+
+    pub(super) fn add_usage(&mut self, id: &Id) {
+        if id.0 == js_word!("arguments") {
+            return;
+        }
+
+        self.data.all.insert(id.clone());
+    }
 }
