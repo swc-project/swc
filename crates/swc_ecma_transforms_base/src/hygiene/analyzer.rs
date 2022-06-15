180abbca38af124ad72cd698cec02c2921825241
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-15 14:11:19 +0900

    analyzer

--- crates/swc_ecma_transforms_base/src/hygiene/analyzer.rs
+++ crates/swc_ecma_transforms_base/src/hygiene/analyzer.rs
@@ -1,1 +1,15 @@
+use swc_ecma_visit::{noop_visit_type, Visit};
 
+pub(super) struct Analyzer {
+    pub scope: Scope,
+
+    pub is_pat_decl: bool,
+}
+
+pub(super) struct Scope {
+    children: Vec<Scope>,
+}
+
+impl Visit for Analyzer {
+    noop_visit_type!();
+}
