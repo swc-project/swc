43ef21f206fd455f89bce375fc97b52de3572811
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-15 19:52:56 +0900

    Improve eval finder

--- crates/swc_ecma_transforms_base/src/rename/eval.rs
+++ crates/swc_ecma_transforms_base/src/rename/eval.rs
@@ -25,9 +25,29 @@ impl Visit for EvalFinder {
 
     visit_obj_and_computed!();
 
+    fn visit_export_default_specifier(&mut self, _: &ExportDefaultSpecifier) {}
+
+    fn visit_export_named_specifier(&mut self, _: &ExportNamedSpecifier) {}
+
+    fn visit_export_namespace_specifier(&mut self, _: &ExportNamespaceSpecifier) {}
+
     fn visit_ident(&mut self, i: &Ident) {
         if i.sym == js_word!("eval") {
             self.found = true;
+        }
+    }
+
+    fn visit_named_export(&mut self, e: &NamedExport) {
+        if e.src.is_some() {
+            return;
+        }
+
+        e.visit_children_with(self);
+    }
+
+    fn visit_prop_name(&mut self, p: &PropName) {
+        if let PropName::Computed(n) = p {
+            n.visit_with(self);
         }
     }
 
