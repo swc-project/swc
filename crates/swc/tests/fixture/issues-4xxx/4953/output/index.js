8747ca6a23901bdd9675a39463ce5743edadb574
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-15 23:09:50 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-4xxx/4953/output/index.js
+++ crates/swc/tests/fixture/issues-4xxx/4953/output/index.js
@@ -3,15 +3,15 @@ Object.defineProperty(exports, "__esModule", {
     value: !0
 }), exports.default = function(a, b) {
     "use strict";
-    !function(c, a) {
-        a(b);
+    !function(a, c) {
+        c(b);
     }(void 0, function(a) {
         "use strict";
-        a.vr = function(b, c, d) {
-            function a() {
+        a.vr = function(a, b, c) {
+            function d() {
                 return '123';
             }
-            return a;
+            return d;
         };
     });
 };
