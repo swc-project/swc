e2180741f70d117104f4595ef4784fa844b8ba78
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-17 01:16:00 +0900

    dbg

--- crates/swc_css_modules/src/sel.rs
+++ crates/swc_css_modules/src/sel.rs
@@ -1,5 +1,12 @@
-use swc_css_visit::VisitMut;
+use swc_css_ast::*;
+use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub struct SelVisitor {}
 
-impl VisitMut for SelVisitor {}
+impl VisitMut for SelVisitor {
+    fn visit_mut_rule(&mut self, rule: &mut Rule) {
+        rule.visit_mut_children_with(self);
+
+        dbg!(&*rule);
+    }
+}
