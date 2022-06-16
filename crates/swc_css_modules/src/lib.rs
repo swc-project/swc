1e5d37aea4a7ba52cb7c127b7a03ead69e313d6b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-17 00:56:17 +0900

    dbg

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -8,7 +8,10 @@ use swc_css_visit::{FoldWith, VisitMutWith};
 use crate::css_modules::CssModuleComponent;
 
 pub fn transform_with_css_modules(mut stylesheet: Stylesheet, filepath: &str) -> Stylesheet {
+    dbg!(&stylesheet);
     stylesheet.visit_mut_with(&mut SelVisitor {});
+    dbg!(&stylesheet);
+
     let mut visitor = CssModuleComponent::new(filepath);
     stylesheet.fold_with(&mut visitor)
 }
