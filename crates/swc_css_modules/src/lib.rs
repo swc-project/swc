38b4abacd4238749150f619cb8d2878fd513df83
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-17 00:54:25 +0900

    sel visitor

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -1,11 +1,14 @@
 pub mod css_modules;
+mod sel;
 
+use sel::SelVisitor;
 use swc_css_ast::Stylesheet;
-use swc_css_visit::FoldWith;
+use swc_css_visit::{FoldWith, VisitMutWith};
 
 use crate::css_modules::CssModuleComponent;
 
-pub fn transform_with_css_modules(stylesheet: Stylesheet, filepath: &str) -> Stylesheet {
+pub fn transform_with_css_modules(mut stylesheet: Stylesheet, filepath: &str) -> Stylesheet {
+    stylesheet.visit_mut_with(&mut SelVisitor {});
     let mut visitor = CssModuleComponent::new(filepath);
     stylesheet.fold_with(&mut visitor)
 }
---
+++ crates/swc_css_modules/src/sel.rs
@@ -0,0 +1,5 @@
+use swc_css_visit::VisitMut;
+
+pub struct SelVisitor {}
+
+impl VisitMut for SelVisitor {}
