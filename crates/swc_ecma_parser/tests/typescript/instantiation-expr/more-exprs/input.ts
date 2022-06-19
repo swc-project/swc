a62b2b32446613fca0a6193c93c19f1e0b86c3b0
Pig Fang <g-plane@hotmail.com>
2022-06-20 01:52:05 +0800

    fix(es/parser): Allow instantiation expression followed by a line break or a binary operator (#5000)

--- crates/swc_ecma_parser/tests/typescript/instantiation-expr/more-exprs/input.ts
+++ crates/swc_ecma_parser/tests/typescript/instantiation-expr/more-exprs/input.ts
@@ -1,4 +1,14 @@
 f<x>, g<y>;
 [f<x>];
 f<x> ? g<y> : h<z>;
+f<x> ^ g<y>;
+f<x> & g<y>;
+f<x> | g<y>;
+f<x> && g<y>;
+f<x> || g<y>;
 { f<x> }
+f<x> ?? g<y>;
+f<x> == g<y>;
+f<x> === g<y>;
+f<x> != g<y>;
+f<x> !== g<y>;
