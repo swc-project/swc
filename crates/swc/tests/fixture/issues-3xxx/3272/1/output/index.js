c203123a16d9595ccc52cdb40f9deaad3bb4ab9f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-20 21:56:19 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-3xxx/3272/1/output/index.js
+++ crates/swc/tests/fixture/issues-3xxx/3272/1/output/index.js
@@ -1,4 +1,4 @@
 "use strict";
-var _interop_require_default = require("@swc/helpers/lib/_interop_require_default.js").default;
+var _interop_require_default = require("../../../../../../../../packages/swc-helpers/src/_interop_require_default.mjs").default;
 var _handlebars = _interop_require_default(require("handlebars"));
 console.log(_handlebars.default);
