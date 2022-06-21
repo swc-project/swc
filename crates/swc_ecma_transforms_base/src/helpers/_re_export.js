6ab7edabfcd753b115190c4669451784aefc5ca2
magic-akari <akari.ccino@gmail.com>
2022-06-21 18:31:58 +0800

    fix: re_export helper

--- crates/swc_ecma_transforms_base/src/helpers/_re_export.js
+++ crates/swc_ecma_transforms_base/src/helpers/_re_export.js
@@ -1,6 +1,6 @@
 function _reExport(target, mod) {
   Object.keys(mod).forEach(function (k) {
-    if (k !== 'default' && !exports.hasOwnProperty(k)) Object.defineProperty(target, k, {
+    if (k !== 'default' && !target.hasOwnProperty(k)) Object.defineProperty(target, k, {
       enumerable: true,
       get: function () { return x[k]; }
     });
