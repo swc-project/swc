da3f8a053e6d90bdafe2cf80d9833ec9ec1dca34
magic-akari <akari.ccino@gmail.com>
2022-06-27 01:33:18 +0800

    fix: update test cases

--- crates/swc/tests/fixture/deno/paths/issue-2724/output/src/index.ts
+++ crates/swc/tests/fixture/deno/paths/issue-2724/output/src/index.ts
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-const _interopRequireWildcard = require("@swc/helpers/lib/_interop_require_wildcard.js").default;
+const _interopRequireWildcard = require("../../../../../../../../../packages/swc-helpers/src/_interop_require_wildcard.mjs").default;
 (async function() {
     const { displayA  } = await Promise.resolve("../packages/a/src/index").then((p)=>_interopRequireWildcard(require(p)));
     console.log(displayA());
--- crates/swc/tests/fixture/issues-2xxx/2050/output/index.ts
+++ crates/swc/tests/fixture/issues-2xxx/2050/output/index.ts
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-const _interopRequireDefault = require("@swc/helpers/lib/_interop_require_default.js").default;
+const _interopRequireDefault = require("../../../../../../../packages/swc-helpers/src/_interop_require_default.mjs").default;
 const _a = _interopRequireDefault(require("./subfolder/A"));
 console.log(_a.default);
