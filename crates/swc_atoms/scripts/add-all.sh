f2e3506566a25b6f75c7276e8fb6155cf3a57ef9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-29 20:30:26 +0900

    Fix script

--- crates/swc_atoms/scripts/add-all.sh
+++ crates/swc_atoms/scripts/add-all.sh
@@ -5,6 +5,6 @@ cargo build -p dbg-swc
 BINARY="$(cargo metadata --format-version 1 | jq -r '.target_directory')/debug/dbg-swc"
 echo "Binary:$BINARY"
 # TypeScript libraries has lots of commonly used identifiers.
-ls ../../node_modules/typescript/lib/lib.*.d.ts | xargs -L 1 -I {} $BINARY x print-atoms {} | sed -r '/^.{,3}$/d' >> words.txt
+ls ../../node_modules/typescript/lib/lib.*.d.ts | xargs -L 1 -I {} $BINARY x print-atoms {} | sed -r '/^.{,3}$/d' >> words.txt || true
 
 ./scripts/sort.sh
