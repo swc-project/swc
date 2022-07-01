6ff834c8837be8f71fb268141827aa0ab5c5e3ec
magic-akari <akari.ccino@gmail.com>
2022-07-01 16:54:37 +0800

    chore: update test case

---
+++ crates/swc/tests/fixture/issues-2xxx/2964/case-5/output/index.ts
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        get: all[name],
+        enumerable: true
+    });
+}
+_export(exports, {
+    foo: ()=>foo,
+    a: ()=>a,
+    b: ()=>b
+});
+const _extends = require("@swc/helpers/lib/_extends.js").default;
+const a = 1;
+const b = 2;
+var foo = _extends({}, {
+    a: 1,
+    b: 2
+});
