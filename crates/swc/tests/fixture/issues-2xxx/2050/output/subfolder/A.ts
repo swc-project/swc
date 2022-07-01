2e5def69c859d30d1f949ea1d3387c31a657f0fb
magic-akari <akari.ccino@gmail.com>
2022-07-01 14:30:09 +0800

    chore: update test case

--- crates/swc/tests/fixture/issues-2xxx/2050/output/subfolder/A.ts
+++ crates/swc/tests/fixture/issues-2xxx/2050/output/subfolder/A.ts
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 Object.defineProperty(exports, "A", {
-    get: ()=>A,
-    enumerable: true
+    enumerable: true,
+    get: ()=>A
 });
 const _b = require("./B");
 console.log(_b.B);
