9addef6fc8672854b52a1b71668795474a6f3fec
magic-akari <akari.ccino@gmail.com>
2022-07-02 12:17:44 +0800

    feat(es/modules): Support `allowTopLevelThis` (#5086)

--- crates/swc_ecma_transforms_module/src/umd.rs
+++ crates/swc_ecma_transforms_module/src/umd.rs
@@ -143,6 +143,7 @@ impl VisitMut for Umd {
         stmts.visit_mut_children_with(&mut ModuleRefRewriter {
             import_map,
             lazy_record: Default::default(),
+            allow_top_level_this: self.config.config.allow_top_level_this,
             is_global_this: true,
         });
 
