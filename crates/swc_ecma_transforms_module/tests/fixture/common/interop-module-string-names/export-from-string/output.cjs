52bfb18113895d20bc952e84302e8591c1fdf407
magic-akari <akari.ccino@gmail.com>
2022-07-03 12:38:48 +0800

    test(es/modules): Add test for string-names (#5088)

---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/interop-module-string-names/export-from-string/output.cjs
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "some exports", {
+    enumerable: true,
+    get: ()=>_foo["some exports"]
+});
+const _foo = require("foo");
---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/interop-module-string-names/import-named/output.cjs
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _foo = require("foo");
+_foo["default imports"];
