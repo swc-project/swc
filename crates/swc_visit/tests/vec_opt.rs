6bdee0e2e35d7de95962fb19c9b0aa2e128eff56
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-04 13:19:53 +0900

    Fix

--- crates/swc_visit/tests/vec_opt.rs
+++ crates/swc_visit/tests/vec_opt.rs
@@ -4,15 +4,12 @@ use std::any::Any;
 
 use swc_visit::define;
 
-/// Visitable nodes.
-pub trait Node: Any {}
-
-impl<T: ?Sized> Node for T where T: Any {}
-
+#[derive(Debug, PartialEq)]
 pub struct Item {
     pub vec_opt1: Vec<Option<Item>>,
     pub vec_opt2: Vec<Option<Enum>>,
 }
+#[derive(Debug, PartialEq)]
 pub enum Enum {
     Item(Item),
 }
