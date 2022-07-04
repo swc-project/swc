ca1b4d05ea7d89d622bb4892e729d7c33f1e38fc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-04 13:21:13 +0900

    More fix

--- crates/swc_visit/tests/fold.rs
+++ crates/swc_visit/tests/fold.rs
@@ -9,12 +9,14 @@ pub trait Node: Any {}
 
 impl<T: ?Sized> Node for T where T: Any {}
 
+#[derive(Debug, PartialEq)]
 pub struct Item {
     // pub field: usize,
     // pub inner: Option<Box<Item>>,
     pub opt_vec: Option<Vec<Item>>,
     pub vec_opt: Vec<Option<Item>>,
 }
+#[derive(Debug, PartialEq)]
 pub enum Enum {
     Item(Item),
 }
