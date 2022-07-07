cba00951f723564a606694f99a222190e9f16ed3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-07 23:14:52 +0900

    Change

--- crates/swc_visit_macros/src/lib.rs
+++ crates/swc_visit_macros/src/lib.rs
@@ -97,7 +97,7 @@ pub fn define(tts: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut q = Quote::new_call_site();
     q.push_tokens(&q!({
         pub type AstKindPath = swc_visit::AstKindPath<AstKind>;
-        pub type AstNodePath<'ast> = swc_visit::AstNodePath<AstNodeRef<'ast>>;
+        pub type AstNodePath<'ast> = swc_visit::AstNodePath<AstKind, AstNodeRef<'ast>>;
     }));
 
     {
