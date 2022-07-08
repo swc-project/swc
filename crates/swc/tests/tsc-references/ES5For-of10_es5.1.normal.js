93513031b51e6b0b8ac21982ed97bb12e5dec14f
magic-akari <akari.ccino@gmail.com>
2022-07-08 08:54:53 +0800

    fix(es/compat): Optimize `for-of` loops for array literals (#5141)

--- crates/swc/tests/tsc-references/ES5For-of10_es5.1.normal.js
+++ crates/swc/tests/tsc-references/ES5For-of10_es5.1.normal.js
@@ -3,42 +3,10 @@ function foo() {
         x: 0
     };
 }
-var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-try {
-    for(var _iterator = [][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true){
-        foo().x = _step.value;
-        var _iteratorNormalCompletion1 = true, _didIteratorError1 = false, _iteratorError1 = undefined;
-        try {
-            for(var _iterator1 = [][Symbol.iterator](), _step1; !(_iteratorNormalCompletion1 = (_step1 = _iterator1.next()).done); _iteratorNormalCompletion1 = true){
-                foo().x = _step1.value;
-                var p = foo().x;
-            }
-        } catch (err) {
-            _didIteratorError1 = true;
-            _iteratorError1 = err;
-        } finally{
-            try {
-                if (!_iteratorNormalCompletion1 && _iterator1.return != null) {
-                    _iterator1.return();
-                }
-            } finally{
-                if (_didIteratorError1) {
-                    throw _iteratorError1;
-                }
-            }
-        }
-    }
-} catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-} finally{
-    try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-            _iterator.return();
-        }
-    } finally{
-        if (_didIteratorError) {
-            throw _iteratorError;
-        }
+for(var _i = 0, _iter = []; _i < _iter.length; _i++){
+    foo().x = _iter[_i];
+    for(var _i1 = 0, _iter1 = []; _i1 < _iter1.length; _i1++){
+        foo().x = _iter1[_i1];
+        var p = foo().x;
     }
 }
--- crates/swc/tests/tsc-references/for-of6_es5.1.normal.js
+++ crates/swc/tests/tsc-references/for-of6_es5.1.normal.js
@@ -1,23 +1,7 @@
-var _iteratorNormalCompletion = true, _didIteratorError = false, _iteratorError = undefined;
-try {
-    //@target: ES6
-    for(var _iterator = [
-        0
-    ][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true){
-        v = _step.value;
-        var _$v = void 0;
-    }
-} catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-} finally{
-    try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-            _iterator.return();
-        }
-    } finally{
-        if (_didIteratorError) {
-            throw _iteratorError;
-        }
-    }
+//@target: ES6
+for(var _i = 0, _iter = [
+    0
+]; _i < _iter.length; _i++){
+    v = _iter[_i];
+    var _$v = void 0;
 }
