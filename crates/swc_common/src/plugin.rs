08f5f8afd136b9c156e6fb494e81ce638ebe5546
OJ Kwon <kwon.ohjoong@gmail.com>
2022-07-12 11:09:43 -0700

    refactor(cargo): align feature naming

--- crates/swc_common/src/plugin.rs
+++ crates/swc_common/src/plugin.rs
@@ -27,7 +27,7 @@ use crate::{syntax_pos::Mark, SyntaxContext};
  *
  * - When removing, or changing existing properties in the AST struct: TBD
  */
-#[cfg(feature = "plugin-transform-schema-v1")]
+#[cfg(feature = "plugin_transform_schema_v1")]
 pub const PLUGIN_TRANSFORM_AST_SCHEMA_VERSION: u32 = 1;
 
 // Reserved for the testing purpose.
