57dbdbb1a56514ddb638003328285bb4f240c817
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-14 12:37:39 +0900

    Fix

--- crates/swc_ecmascript/src/lib.rs
+++ crates/swc_ecmascript/src/lib.rs
@@ -2,30 +2,30 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub use swc_ecma_ast as ast;
-#[cfg(any(docsrs, feature = "codegen"))]
+#[cfg(feature = "codegen")]
 #[cfg_attr(docsrs, doc(cfg(feature = "codegen")))]
 pub use swc_ecma_codegen as codegen;
-#[cfg(any(docsrs, feature = "dep_graph"))]
+#[cfg(feature = "dep_graph")]
 #[cfg_attr(docsrs, doc(cfg(feature = "dep_graph")))]
 pub use swc_ecma_dep_graph as dep_graph;
-#[cfg(any(docsrs, feature = "minifier"))]
+#[cfg(feature = "minifier")]
 #[cfg_attr(docsrs, doc(cfg(feature = "minifier")))]
 pub use swc_ecma_minifier as minifier;
-#[cfg(any(docsrs, feature = "parser"))]
+#[cfg(feature = "parser")]
 #[cfg_attr(docsrs, doc(cfg(feature = "parser")))]
 pub use swc_ecma_parser as parser;
-#[cfg(any(docsrs, feature = "preset_env"))]
+#[cfg(feature = "preset_env")]
 #[cfg_attr(docsrs, doc(cfg(feature = "preset_env")))]
 pub use swc_ecma_preset_env as preset_env;
-#[cfg(any(docsrs, feature = "swc_ecma_quote"))]
+#[cfg(feature = "swc_ecma_quote")]
 #[cfg_attr(docsrs, doc(cfg(feature = "parser")))]
 pub use swc_ecma_quote as quote;
-#[cfg(any(docsrs, feature = "transforms"))]
+#[cfg(feature = "transforms")]
 #[cfg_attr(docsrs, doc(cfg(feature = "transforms")))]
 pub use swc_ecma_transforms as transforms;
-#[cfg(any(docsrs, feature = "utils"))]
+#[cfg(feature = "utils")]
 #[cfg_attr(docsrs, doc(cfg(feature = "utils")))]
 pub use swc_ecma_utils as utils;
-#[cfg(any(docsrs, feature = "visit"))]
+#[cfg(feature = "visit")]
 #[cfg_attr(docsrs, doc(cfg(feature = "visit")))]
 pub use swc_ecma_visit as visit;
