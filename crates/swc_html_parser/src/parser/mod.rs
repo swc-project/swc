45a7fd5b78ad22ccda93089a9e592033a3de6534
alexander.akait <sheo13666q@gmail.com>
2022-07-14 20:01:37 +0300

    fix(html/parser): fix span

--- crates/swc_html_parser/src/parser/mod.rs
+++ crates/swc_html_parser/src/parser/mod.rs
@@ -6235,6 +6235,11 @@ where
                     //
                     // Stop parsing.
                     Token::Eof => {
+                        self.update_end_tag_span(
+                            self.open_elements_stack.items.last(),
+                            token_and_info.span,
+                        );
+
                         match self.open_elements_stack.items.last() {
                             Some(node) if !is_html_element!(node, "html") => {
                                 self.errors.push(Error::new(
