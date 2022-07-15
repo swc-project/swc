cd94f0a2e9a7331073d20638031ae86d3308760e
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-07-14 19:10:13 -0700

    test(es/plugin): Build plugins for e2e testing (#5210)

--- node-swc/e2e/plugins/plugins.schema.test.js
+++ node-swc/e2e/plugins/plugins.schema.test.js
@@ -1,5 +1,24 @@
+/// <reference types="jest" />
 const { getPkgRoot } = require("../utils");
 const { spawn } = require("child_process");
+const path = require("path");
+
+const waitProcessAsync = async (proc) =>
+    new Promise((resolve, reject) => {
+        proc.on("exit", (code) => {
+            if (code === 0) {
+                resolve(code);
+            } else {
+                reject(code);
+            }
+        });
+    });
+
+const getPluginAbsolutePath = (feature) =>
+    path.join(
+        getPkgRoot(),
+        `node-swc/e2e/fixtures/${feature}/target/wasm32-wasi/debug/${feature}.wasm`
+    );
 
 /**
  * Build host bindings with specific schema version flag.
@@ -17,21 +36,32 @@ const buildHost = async (feature) => {
     ];
 
     const proc = spawn("napi", args, { cwd: getPkgRoot(), stdio: "inherit" });
-    await new Promise((resolve, reject) => {
-        proc.on("exit", (code) => {
-            if (code === 0) {
-                resolve(code);
-            } else {
-                reject(code);
-            }
-        });
-    });
+    await waitProcessAsync(proc);
+};
+
+const buildPlugin = async (feature) => {
+    const args = [
+        "build",
+        "--manifest-path",
+        `./node-swc/e2e/fixtures/${feature}/Cargo.toml`,
+        "--target",
+        "wasm32-wasi",
+    ];
+
+    const proc = spawn("cargo", args, { cwd: getPkgRoot(), stdio: "inherit" });
+    await waitProcessAsync(proc);
 };
 
 describe("Plugins", () => {
     describe("Transform AST schema versions", () => {
         const versionMatrix = [
-            ["plugin_transform_schema_v1", "plugin_transform_schema_v1"],
+            {
+                host: "plugin_transform_schema_vtest",
+                plugin: [
+                    "plugin_transform_schema_v1",
+                    "plugin_transform_schema_vtest",
+                ],
+            },
             /* TODO
             ["v1", "vtest"],
             ["vtest", "v1"],
@@ -39,16 +69,46 @@ describe("Plugins", () => {
             */
         ];
 
-        // Arbitrary large number to ensure test doesn't timeout due to native binaries build time.
-        jest.setTimeout(10000000);
-
         describe.each(versionMatrix)(
-            "Host schema version '%s'",
-            (hostVersion, pluginVersion) => {
-                beforeAll(() => buildHost(hostVersion));
+            "Host schema version '$host'",
+            ({ host, plugin: pluginVersions }) => {
+                // Arbitrary large number to ensure test doesn't timeout due to native binaries build time.
+                beforeAll(async () => {
+                    await buildHost(host);
+                    await Promise.all(
+                        pluginVersions.map((p) => buildPlugin(p))
+                    );
+                }, 10000000);
 
-                test.todo(
-                    `Should work with plugin schema version '${pluginVersion}'`
+                const transform = (code, feature) => {
+                    const { transformSync } = require(path.resolve(
+                        getPkgRoot(),
+                        `swc_host_${host}.node`
+                    ));
+
+                    const options = {
+                        jsc: {
+                            experimental: {
+                                plugins: [[getPluginAbsolutePath(feature), {}]],
+                            },
+                        },
+                    };
+
+                    return transformSync(
+                        code,
+                        false,
+                        Buffer.from(JSON.stringify(options))
+                    );
+                };
+
+                it.each(pluginVersions)(
+                    `Should work with plugin schema version %s`,
+                    (pluginVersion) => {
+                        const result = transform(
+                            `console.log('boo')`,
+                            pluginVersion
+                        );
+                    }
                 );
             }
         );
