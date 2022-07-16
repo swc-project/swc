0ff5e9f04ce58cfd18f9f739437e852a0301f31c
Calvin Huang <calvin@scale.com>
2022-07-16 07:41:57 -0700

    more fix test

--- crates/swc/tests/fixture/issues-3xxx/3272/1/output/index.js
+++ crates/swc/tests/fixture/issues-3xxx/3272/1/output/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-const _interopRequireDefault = require("@swc/helpers/lib/_interop_require_default.js").default;
+var _interopRequireDefault = require("@swc/helpers/lib/_interop_require_default.js").default;
 var _handlebars = /*#__PURE__*/ _interopRequireDefault(require("handlebars"));
 console.log(_handlebars.default);
