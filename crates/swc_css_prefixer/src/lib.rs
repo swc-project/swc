bc7c48b2ebf948198355e4544c92446244957900
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-18 09:53:01 +0900

    api

--- crates/swc_css_prefixer/src/lib.rs
+++ crates/swc_css_prefixer/src/lib.rs
@@ -1,3 +1,12 @@
+use preset_env_base::Versions;
+use swc_css_visit::VisitMut;
+
 pub use self::prefixer::prefixer;
 
 mod prefixer;
+
+/// Prefixer for CSS.
+///
+///
+/// You can get `versions` from [`preset_env_base::query::targets_to_versions`].
+pub fn preset_env(versions: Versions) -> impl VisitMut {}
