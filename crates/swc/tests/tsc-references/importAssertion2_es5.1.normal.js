bee4d0961b443df45a6faed10be228ada7801ff5
magic-akari <akari.ccino@gmail.com>
2022-07-19 13:26:44 +0800

    chore: update test cases

--- crates/swc/tests/tsc-references/importAssertion2_es5.1.normal.js
+++ crates/swc/tests/tsc-references/importAssertion2_es5.1.normal.js
@@ -5,15 +5,9 @@
 export var a = 1;
 export var b = 2;
 export { a, b } from "./0";
-import "./0";
-export { };
 export * from "./0";
 import * as _ns from "./0";
 export { _ns as ns };
 // @filename: 2.ts
 export { a, b } from "./0";
-import "./0";
-export { };
 export { a as c, b as d } from "./0";
-import "./0";
-export { };
--- crates/swc/tests/tsc-references/importAssertion2_es5.2.minified.js
+++ crates/swc/tests/tsc-references/importAssertion2_es5.2.minified.js
@@ -1,11 +1,8 @@
 export var a = 1;
 export var b = 2;
 export { a, b } from "./0";
-import "./0";
 export * from "./0";
 import * as _ns from "./0";
 export { a, b } from "./0";
-import "./0";
 export { a as c, b as d } from "./0";
-import "./0";
 export { _ns as ns };
