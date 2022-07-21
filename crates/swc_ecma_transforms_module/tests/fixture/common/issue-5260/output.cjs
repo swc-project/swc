c0ace4cb2c63b81dc08e35d6a59f9a9bcd961985
magic-akari <akari.ccino@gmail.com>
2022-07-21 14:04:06 +0800

    fix(es/modules): Use a private context for `export from` (#5262)

---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/issue-5260/output.cjs
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: all[name]
+    });
+}
+_export(exports, {
+    a: ()=>_a.X,
+    b: ()=>_b.X,
+    c: ()=>_c.default,
+    d: ()=>_d.default,
+    e: ()=>_e,
+    f: ()=>_f
+});
+const _a = require("a");
+const _b = require("b");
+const _c = /*#__PURE__*/ _interopRequireDefault(require("c"));
+const _d = /*#__PURE__*/ _interopRequireDefault(require("d"));
+const _e = /*#__PURE__*/ _interopRequireWildcard(require("e"));
+const _f = /*#__PURE__*/ _interopRequireWildcard(require("f"));
+// unresolved
+const x = X;
+const _a1 = a;
+const _c1 = c;
+const _e1 = e;
+// top level
+const b = 1, d = 2, f = 3;
+console.log(b, d, f);
