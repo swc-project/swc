0f254d5ce5c23fa3ec06c074aa1b8acc5d1dbddf
Austaras <austaras@outlook.com>
2022-07-26 11:35:49 +0800

    feat(es/codegen): Remove extra space before a private method (#5295)

--- crates/swc/tests/fixture/issues-2xxx/2086/output/index.ts
+++ crates/swc/tests/fixture/issues-2xxx/2086/output/index.ts
@@ -1,1 +1,1 @@
-export default class a{#a;constructor(a){this.#a=a} #b(){setTimeout(()=>{this.#a.textContent="TESTED"},1e3)}run(){this.#b()}}
+export default class a{#a;constructor(a){this.#a=a}#b(){setTimeout(()=>{this.#a.textContent="TESTED"},1e3)}run(){this.#b()}}
