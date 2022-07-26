4d7316199500e4a4f5eb0ebe4c50b18ae68e564c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-26 10:58:34 +0900

    Patch update script

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -4,13 +4,21 @@ set -u
 export UPDATE=1
 export DIFF=0
 
-function up(){
-    cargo test -p swc -p swc_ecma_codegen -p swc_ecma_parser -p swc_bundler -p swc_node_bundler --no-fail-fast
+function crate() {
+    until cargo test -p $1 --no-fail-fast
+    do
+        git add -A
+        git commit -m 'Update test refs'
+    done
+    
+    git add -A
+    git commit -m 'Update test refs'
 }
 
-up
+crate swc
+crate swc_ecma_codegen
+crate swc_ecma_parser
+crate swc_bundler
+crate swc_node_bundler
 
-
-git add -A
-git commit -m 'Update test refs'
 git push -u origin "$(git rev-parse --abbrev-ref HEAD)" --no-verify
