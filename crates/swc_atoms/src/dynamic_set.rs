656adc0e69ee29ccef34450f5ec5eab57d39c2a8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-28 20:03:00 +0900

    set

--- crates/swc_atoms/src/dynamic_set.rs
+++ crates/swc_atoms/src/dynamic_set.rs
@@ -1,1 +1,20 @@
+use std::cell::RefCell;
 
+use rustc_hash::FxHashSet;
+
+thread_local!(static LOCAL_SET: RefCell<Set> = Default::default());
+
+pub(super) struct Set {
+    /// hashbrown has better logic for collision resolution.
+    inner: FxHashSet<Entry>,
+}
+
+impl Default for Set {
+    fn default() -> Self {
+        Self {
+            inner: FxHashSet::with_capacity_and_hasher(256, Default::default()),
+        }
+    }
+}
+
+struct Entry {}
