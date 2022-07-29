b557a9538ddd002f3777eacbb4edf62474471a43
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-29 20:31:24 +0900

    JsWordList

--- crates/swc_ecma_transforms_base/src/rename/analyzer/fast.rs
+++ crates/swc_ecma_transforms_base/src/rename/analyzer/fast.rs
@@ -1,1 +1,17 @@
+use swc_atoms::JsWord;
 
+pub struct JsWordList {
+    words: Vec<JsWord>,
+}
+
+impl JsWordList {
+    pub fn get(&mut self, w: &JsWord) -> usize {
+        if let Some(i) = self.words.iter().position(|x| x == w) {
+            i
+        } else {
+            let i = self.words.len();
+            self.words.push(w.clone());
+            i
+        }
+    }
+}
