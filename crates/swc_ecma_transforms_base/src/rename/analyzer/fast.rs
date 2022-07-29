ab6d14d668b1dc96a49e0d7488384c2556f138ad
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-29 20:32:29 +0900

    More

--- crates/swc_ecma_transforms_base/src/rename/analyzer/fast.rs
+++ crates/swc_ecma_transforms_base/src/rename/analyzer/fast.rs
@@ -1,17 +1,19 @@
 use swc_atoms::JsWord;
 
-pub struct JsWordList {
+pub(super) struct JsWordList {
     words: Vec<JsWord>,
 }
 
+pub(super) struct JsWordIndex(usize);
+
 impl JsWordList {
-    pub fn get(&mut self, w: &JsWord) -> usize {
-        if let Some(i) = self.words.iter().position(|x| x == w) {
+    pub fn get(&mut self, w: &JsWord) -> JsWordIndex {
+        JsWordIndex(if let Some(i) = self.words.iter().position(|x| x == w) {
             i
         } else {
             let i = self.words.len();
             self.words.push(w.clone());
             i
-        }
+        })
     }
 }
