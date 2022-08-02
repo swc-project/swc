7e0898a19f518205003712a3539db5f5f9e72905
magic-akari <akari.ccino@gmail.com>
2022-08-02 11:18:06 +0800

    chore: update test cases

--- crates/swc/tests/tsc-references/tsxPreserveEmit1_es5.1.normal.js
+++ crates/swc/tests/tsc-references/tsxPreserveEmit1_es5.1.normal.js
@@ -2,22 +2,6 @@
 //@jsx: preserve
 //@target: ES5
 //@Filename: react.d.ts
-//!
-//!  x multiple `export =` found
-//!    ,-[4:5]
-//!  4 | export = x;
-//!    : ^^^^^|^^^^^
-//!    :      `-- previous `export =` declared here
-//!  5 | }
-//!  6 | 
-//!  7 | declare module ReactRouter {
-//!  8 | 	var Route: any;
-//!  9 | 	interface Thing { }
-//! 10 | }
-//! 11 | declare module 'react-router' {
-//! 12 | 	export = ReactRouter;
-//!    :  ^^^^^^^^^^^^^^^^^^^^^
-//!    `----
 //@Filename: test.tsx
 // Should emit 'react-router' in the AMD dependency list
 var React = require("react");
