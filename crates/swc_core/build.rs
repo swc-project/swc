2b0c7a36369d321ac40faf48a25223d23f1dd935
OJ Kwon <kwon.ohjoong@gmail.com>
2022-08-02 11:49:09 -0700

    feat(swc/core): expose toggle perf feature

--- crates/swc_core/build.rs
+++ crates/swc_core/build.rs
@@ -1,6 +1,3 @@
-/*#[cfg(all(not(feature = "swc_v1"), not(feature = "swc_v2")))]
-compile_error!("Please enable swc_v1 or swc_v2 feature");*/
-
 // Validate conflict between host / plugin features
 #[cfg(all(
     feature = "plugin_transform",
@@ -13,6 +10,13 @@ compile_error!(
     "'plugin_transform' and 'plugin_transform_host*' features are mutually exclusive. If you're \
      writing a plugin, use 'plugin_transform' feature. If you're writing a custom SWC binary to \
      run plugin, use 'plugin_transform_host_*' instead."
+);
+
+// TODO: need exhaustive list of features support __common
+#[cfg(all(feature = "perf", not(feature = "__common")))]
+compile_error!(
+    "'perf' feature is only available when enabling 'common' feature. Enabling \
+     'plugin_transform*' will enable common features internally."
 );
 
 fn main() {
