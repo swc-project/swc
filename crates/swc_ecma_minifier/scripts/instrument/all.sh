6552d1a6f77e5a69cf3c21de9606653b1dcc285a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-03 15:20:04 +0900

    perf(es/minifier): Make postcompress pass parallel (#5374)

--- crates/swc_ecma_minifier/scripts/instrument/all.sh
+++ crates/swc_ecma_minifier/scripts/instrument/all.sh
@@ -8,4 +8,4 @@
 
 
 export RUST_LOG=off
-cargo profile instruments -t time --example minify-all --features swc_common/perf --release -- $@
+cargo profile instruments -t time --example minify-all --features swc_common/perf --features concurrent --release -- $@
