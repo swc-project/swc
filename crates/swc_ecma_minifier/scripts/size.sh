a98202c13fea3ecc112c5b08491bdacca6558f25
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-04 16:40:05 +0900

    fixup

--- crates/swc_ecma_minifier/scripts/size.sh
+++ crates/swc_ecma_minifier/scripts/size.sh
@@ -4,6 +4,6 @@ set -eu
 export UPDATE=1
 export DIFF=0
 export RUST_LOG=off
-cargo test --test compress --features concurrent full_libs
+cargo test --test compress --features concurrent full_libs || true
 
 for f in ./tests/lib-output/*.js; do echo "$f:" && gzip -9 -c $f | wc -c; done
