4cccc1f390e26535027b4232fbc72bcb67d6e41b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-04 16:39:01 +0900

    size.sh

--- crates/swc_ecma_minifier/scripts/size.sh
+++ crates/swc_ecma_minifier/scripts/size.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-for f in ./benches/full/*.js; do RUST_LOG=off cargo run --example minifier -- $f && gzip -9 -c output.js | wc -c; done
+export UPDATE=1
+export DIFF=0
+export RUST_LOG=off
+cargo test --test compress --features concurrent full_libs
+
+for f in ./tests/lib-output/*.js; do echo "$f:" && gzip -9 -c $f | wc -c; done
