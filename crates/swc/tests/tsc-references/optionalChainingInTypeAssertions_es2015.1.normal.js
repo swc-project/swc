0bc38f32fbd82c8e3541bfa2627590ffebf41e80
magic-akari <akari.ccino@gmail.com>
2022-08-04 11:20:43 +0800

    chore: update test case

--- crates/swc/tests/tsc-references/optionalChainingInTypeAssertions_es2015.1.normal.js
+++ crates/swc/tests/tsc-references/optionalChainingInTypeAssertions_es2015.1.normal.js
@@ -8,3 +8,8 @@ const foo = new Foo();
 (ref1 = foo.m) === null || ref1 === void 0 ? void 0 : ref1();
 (ref2 = (/*a2*/ foo.m)) === null || ref2 === void 0 ? void 0 : ref2();
 (ref3 = (foo.m /*b3*/ )) === null || ref3 === void 0 ? void 0 : ref3();
+// https://github.com/microsoft/TypeScript/issues/50148
+(foo === null || foo === void 0 ? void 0 : foo.m).length;
+(foo === null || foo === void 0 ? void 0 : foo.m).length;
+(foo === null || foo === void 0 ? void 0 : foo["m"]).length;
+(foo === null || foo === void 0 ? void 0 : foo["m"]).length;
