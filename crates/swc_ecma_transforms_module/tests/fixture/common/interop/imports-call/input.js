cdb6164937b285aa294a8d68459a2c39862d941f
magic-akari <akari.ccino@gmail.com>
2022-08-04 12:59:42 +0800

    fix(es/modules): Use an indirect call for a tagged template (#5382)

---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/interop/imports-call/input.js
@@ -0,0 +1,8 @@
+import foo from "foo";
+import { bar } from "bar";
+
+foo("foo");
+foo`foo`;
+
+bar("bar");
+bar`bar`;
---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/interop/imports-call/output.cjs
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _foo = /*#__PURE__*/ _interopRequireDefault(require("foo"));
+const _bar = require("bar");
+(0, _foo.default)("foo");
+(0, _foo.default)`foo`;
+(0, _bar.bar)("bar");
+(0, _bar.bar)`bar`;
