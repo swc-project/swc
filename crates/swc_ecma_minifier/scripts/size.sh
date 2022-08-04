9e077fdc2e61f58735462159f5b159e9042bfb0e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-04 15:23:34 +0900

    -9

--- crates/swc_ecma_minifier/scripts/size.sh
+++ crates/swc_ecma_minifier/scripts/size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-for f in ./benches/full/*.js; do RUST_LOG=off cargo run --example minifier -- $f && gzip -c output.js | wc -c; done
+for f in ./benches/full/*.js; do RUST_LOG=off cargo run --example minifier -- $f && gzip -9 -c output.js | wc -c; done
