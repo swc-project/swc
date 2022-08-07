27e7e0b69b15a215a887b2a3f75a598ac01252b6
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-08-06 20:33:18 -0700

    fix(swc_core): Update references for `quote!` (#5408)

--- crates/swc_ecma_quote_macros/src/ast/enums.rs
+++ crates/swc_ecma_quote_macros/src/ast/enums.rs
@@ -9,7 +9,7 @@ macro_rules! impl_simple_enum {
                     $(
                         $E::$v => q!(
                             Vars {},
-                            { swc_ecma_ast::$E::$v }
+                            { swc_core::ast::$E::$v }
                         )
                         .parse(),
                     )*
