0121cd0be0af6eac2513dff005bc43435e534f4c
OJ Kwon <kwon.ohjoong@gmail.com>
2022-08-15 22:56:50 -0700

    feat(swc/plugin_runner): bump up wasmer@3

--- crates/swc_plugin_runner/src/imported_fn/handler.rs
+++ crates/swc_plugin_runner/src/imported_fn/handler.rs
@@ -2,15 +2,21 @@ use swc_common::{
     errors::{Diagnostic, HANDLER},
     plugin::serialized::PluginSerializedBytes,
 };
+use wasmer::FunctionEnvMut;
 
 use crate::{host_environment::BaseHostEnvironment, memory_interop::copy_bytes_into_host};
 
 #[tracing::instrument(level = "info", skip_all)]
-pub fn emit_diagnostics(env: &BaseHostEnvironment, bytes_ptr: i32, bytes_ptr_len: i32) {
-    if let Some(memory) = env.memory_ref() {
+pub fn emit_diagnostics(
+    env: FunctionEnvMut<BaseHostEnvironment>,
+    bytes_ptr: i32,
+    bytes_ptr_len: i32,
+) {
+    if let Some(memory) = env.data().memory.as_ref() {
         if HANDLER.is_set() {
             HANDLER.with(|handler| {
-                let diagnostics_bytes = copy_bytes_into_host(memory, bytes_ptr, bytes_ptr_len);
+                let diagnostics_bytes =
+                    copy_bytes_into_host(&memory.view(&env), bytes_ptr, bytes_ptr_len);
                 let serialized = PluginSerializedBytes::from_slice(&diagnostics_bytes[..]);
                 let diagnostic = PluginSerializedBytes::deserialize::<Diagnostic>(&serialized)
                     .expect("Should able to be deserialized into diagnostic");
