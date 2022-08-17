7394deef428e15bb9b03e0113e72b8b6b2ae07bc
magic-akari <akari.ccino@gmail.com>
2022-08-18 02:28:37 +0800

    fix(es/fixer): Fix handling of `yield` and `await` (#5533)

---
+++ crates/swc/tests/fixture/issues-5xxx/5530/input/index.js
@@ -0,0 +1,22 @@
+async function* level1() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+async function* level2() {
+    yield* (0, level1());
+}
+
+async function* foo() {
+    yield (foo, bar);
+    await (foo, bar);
+    yield* (0, level1());
+    await (0, level1());
+    yield (foo ? bar : baz);
+    await (foo ? bar : baz);
+    yield (++foo);
+    await (++foo);
+    yield (!foo);
+    await (!foo);
+}
