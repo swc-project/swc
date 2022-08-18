f1a42639541ef5a356079b7fb177a621ddd5a2c0
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-08-17 21:29:19 -0700

    feat(swc_core): Implement engine diagnostics (#5515)

---
+++ crates/swc_core/src/__diagnostics.rs
@@ -0,0 +1,7 @@
+// This is auto-generated when publish.
+
+// A fallback semver version set via cargo build script.
+pub(crate) static PKG_SEMVER_FALLBACK: &str =
+    include_str!(concat!(env!("OUT_DIR"), "/core_pkg_version.txt"));
+
+pub(crate) static GIT_SHA: &str = "dev";
