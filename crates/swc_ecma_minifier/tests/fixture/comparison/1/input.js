678a2db5f8bbf4deefead3a10d88853be873728b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-19 12:01:30 +0900

    fmt input

--- crates/swc_ecma_minifier/tests/fixture/comparison/1/input.js
+++ crates/swc_ecma_minifier/tests/fixture/comparison/1/input.js
@@ -1,16 +1,33 @@
 self.push({
-    9111: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+        9111: (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-        /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-        /* harmony export */   "F": function () { return /* binding */ composeRefs; },
-        /* harmony export */   "e": function () { return /* binding */ useComposedRefs; }
-        /* harmony export */
-});
-        /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(27378);
-        function composeRefs(...o) { return e => o.forEach((o => function (o, e) { "function" == typeof o ? o(e) : null != o && (o.current = e) }(o, e))) } function useComposedRefs(...e) { return react__WEBPACK_IMPORTED_MODULE_0__.useCallback(composeRefs(...e), e) }
-        //# sourceMappingURL=index.module.js.map
+                /* harmony export */
+                __webpack_require__.d(__webpack_exports__, {
+                        /* harmony export */
+                        "F": function () {
+                                return /* binding */ composeRefs;
+                        },
+                        /* harmony export */
+                        "e": function () {
+                                return /* binding */ useComposedRefs;
+                        }
+                        /* harmony export */
+                });
+                /* harmony import */
+                var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(27378);
+
+                function composeRefs(...o) {
+                        return e => o.forEach((o => function (o, e) {
+                                "function" == typeof o ? o(e) : null != o && (o.current = e)
+                        }(o, e)))
+                }
+
+                function useComposedRefs(...e) {
+                        return react__WEBPACK_IMPORTED_MODULE_0__.useCallback(composeRefs(...e), e)
+                }
+                //# sourceMappingURL=index.module.js.map
 
 
-        /***/
-})
+                /***/
+        })
 })
