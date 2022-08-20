82cc3ca7a4f27f932ed4e8b4e620d1d8b5f41bdc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-20 19:48:00 +0900

    fix(es/quote): Allow cloning (#5571)

--- crates/swc_ecma_quote/src/clone.rs
+++ crates/swc_ecma_quote/src/clone.rs
@@ -1,35 +1,10 @@
 //! Module for implicitly copyable types.
 
-use swc_atoms::JsWord;
-use swc_ecma_ast::*;
-
-use self::private::Sealed;
-
-/// Not a public API.
-///
-/// Implemented for types which will be implicitly cloned when used as a
-/// variable in [crate::quote] macro calls.
-pub trait ImplicitClone: Clone + Sealed {
+/// Noop
+pub trait ImplicitClone: Clone {
     fn clone_quote_var(&self) -> Self {
         self.clone()
     }
 }
 
-macro_rules! impl_for {
-    ($T:ty) => {
-        impl ImplicitClone for $T {}
-        impl Sealed for $T {}
-    };
-}
-
-impl_for!(Id);
-impl_for!(Ident);
-impl_for!(JsWord);
-
-impl_for!(Str);
-impl_for!(Number);
-impl_for!(Bool);
-
-mod private {
-    pub trait Sealed {}
-}
+impl<T: Clone> ImplicitClone for T {}
