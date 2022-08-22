1f8e78f2747a8bc6234f9b55ac0a5c2ca3770f74
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-22 15:53:17 +0900

    Remove do expr

--- crates/swc/tests/babel-exec/packages/babel-plugin-proposal-async-do-expressions/test/fixtures/async-do-expressions/throw-statement/exec.js
+++
@@ -1,5 +0,0 @@
-const x = async do { throw new Error("sync") };
-
-expect(x).toBeInstanceOf(Promise);
-x.then(() => { throw new Error("expected an error: sync is thrown.") })
-.catch(err => { expect(err.message).toEqual("sync") });
