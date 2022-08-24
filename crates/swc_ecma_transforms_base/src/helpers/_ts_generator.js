c86513256f8bdc3cb4779c865c564221206d2f47
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-24 14:11:29 +0900

    Fix ts generator

--- crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
+++ crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
@@ -1,5 +1,5 @@
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    var f, y, t, g, _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] };
     return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
