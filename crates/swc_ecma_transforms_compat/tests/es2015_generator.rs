b8527e612237534efc05f82d51d7e7b99c32d15a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-25 12:30:00 +0900

    Update test refs

--- crates/swc_ecma_transforms_compat/tests/es2015_generator.rs
+++ crates/swc_ecma_transforms_compat/tests/es2015_generator.rs
@@ -1187,24 +1187,15 @@ function* foo() {
 }
 "#,
     r#"
-var regeneratorRuntime = require("regenerator-runtime");
-var _marked = regeneratorRuntime.mark(foo);
-function foo() {
-  var bar;
-
-  return regeneratorRuntime.wrap(function foo$(_ctx) {
-    while (1)
-      switch (_ctx.prev = _ctx.next) {
-        case 0:
-          bar = function _bar() {};
-
-          return _ctx.abrupt("return", bar);
-        case 3:
-        case "end":
-          return _ctx.stop();
-      }
-  }, _marked);
-}
+    function foo() {
+        function bar() {}
+        return __generator(this, function(_state) {
+            return [
+                2,
+                bar
+            ];
+        });
+    }
 "#
 );
 
@@ -1469,77 +1460,59 @@ async function init() {
 }
 "#,
     r#"
-var regeneratorRuntime = require("regenerator-runtime");
-var fib = function fib() {
-    return 42;
-};
-function init() {
-    return _init.apply(this, arguments);
-}
-function _init() {
-    _init = _asyncToGenerator(
-        regeneratorRuntime.mark(function _callee() {
-            var fib, _fib;
-            return regeneratorRuntime.wrap(function _callee$(_ctx) {
-                while (1)
-                    switch (_ctx.prev = _ctx.next) {
-                        case 0:
-                            fib = function _fib1(n) {
-                                return _fib.apply(this, arguments);
-                            };
-                            _fib = function __fib() {
-                                _fib = _asyncToGenerator(
-                                    regeneratorRuntime.mark(function _callee(
+    var fib = function fib() {
+        return 42;
+    };
+    function init() {
+        return _init.apply(this, arguments);
+    }
+    function _init() {
+        _init = _asyncToGenerator(function() {
+            function fib(n) {
+                return _fib.apply(this, arguments);
+            }
+            function _fib() {
+                _fib = _asyncToGenerator(function(n) {
+                    var x, y;
+                    return __generator(this, function(_state) {
+                        switch(_state.label){
+                            case 0:
+                                if (n <= 1) {
+                                    return [
+                                        2,
                                         n
-                                    ) {
-                                        var x, y;
-                                        return regeneratorRuntime.wrap(
-                                            function _callee$(_ctx) {
-                                                while (1)
-                                                    switch (_ctx.prev = _ctx.next) {
-                                                        case 0:
-                                                            if (!(n <= 1)) {
-                                                                _ctx.next = 2;
-                                                                break;
-                                                            }
-                                                            return _ctx.abrupt(
-                                                                "return",
-                                                                n
-                                                            );
-                                                        case 2:
-                                                            _ctx.next = 4;
-                                                            return fib(n - 1);
-                                                        case 4:
-                                                            x = _ctx.sent;
-                                                            _ctx.next = 7;
-                                                            return fib(n - 2);
-                                                        case 7:
-                                                            y = _ctx.sent;
-                                                            return _ctx.abrupt(
-                                                                "return",
-                                                                x + y
-                                                            );
-                                                        case 9:
-                                                        case "end":
-                                                            return _ctx.stop();
-                                                    }
-                                            },
-                                            _callee
-                                        );
-                                    })
-                                );
-                                return _fib.apply(this, arguments);
-                            };
-                            return _ctx.abrupt("return", fib);
-                        case 5:
-                        case "end":
-                            return _ctx.stop();
-                    }
-            }, _callee);
-        })
-    );
-    return _init.apply(this, arguments);
-}
+                                    ];
+                                }
+                                return [
+                                    4,
+                                    fib(n - 1)
+                                ];
+                            case 1:
+                                x = _state.sent();
+                                return [
+                                    4,
+                                    fib(n - 2)
+                                ];
+                            case 2:
+                                y = _state.sent();
+                                return [
+                                    2,
+                                    x + y
+                                ];
+                        }
+                    });
+                });
+                return _fib.apply(this, arguments);
+            }
+            return __generator(this, function(_state) {
+                return [
+                    2,
+                    fib
+                ];
+            });
+        });
+        return _init.apply(this, arguments);
+    }
 "#
 );
 
