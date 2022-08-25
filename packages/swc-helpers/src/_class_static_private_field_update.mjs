946f4000872c32195f76fd650cdeef938a38188f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-25 14:11:44 +0900

    Fix

--- packages/swc-helpers/src/_class_static_private_field_update.mjs
+++ packages/swc-helpers/src/_class_static_private_field_update.mjs
@@ -1,5 +1,5 @@
 import classCheckPrivateStaticAccess from './_class_check_private_static_access.mjs';
-import classCheckPrivateStaticFieldDescriptor from './_class_check_private_static_access.mjs';
+import classCheckPrivateStaticFieldDescriptor from './_class_check_private_static_field_descriptor.mjs';
 import classApplyDescriptorUpdate from './_class_apply_descriptor_update.mjs';
 
 export default function _classStaticPrivateFieldUpdate(receiver, classConstructor, descriptor) {
