838438dc9d090402756ecc038b5b091f51dada66
magic-akari <akari.ccino@gmail.com>
2022-08-26 22:13:38 +0800

    fix(es/parser): Handle more `TS2406` errors (#5640)

--- crates/swc/tests/tsc-references/parserGreaterThanTokenAmbiguity11_es2015.1.normal.js
+++ crates/swc/tests/tsc-references/parserGreaterThanTokenAmbiguity11_es2015.1.normal.js
@@ -1,1 +1,12 @@
-1 >>= 2;
+//!
+//!  x The left-hand side of an assignment expression must be a variable or a property access.
+//!   ,----
+//! 1 | 1 >>= 2;
+//!   : ^
+//!   `----
+//!
+//!
+//!Caused by:
+//!    0: failed to process input file
+//!    1: error was recoverable, but proceeding would result in wrong codegen
+//!    2: Syntax Error
