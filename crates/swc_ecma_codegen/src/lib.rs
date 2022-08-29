6fdfa3a7991774a143f7d6c0c67a22e5a928a4bc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-08-29 20:06:23 +0900

    Span of empty statement, 674

--- crates/swc_ecma_codegen/src/lib.rs
+++ crates/swc_ecma_codegen/src/lib.rs
@@ -2644,7 +2644,7 @@ where
     fn emit_empty_stmt(&mut self, node: &EmptyStmt) -> Result {
         self.emit_leading_comments_of_span(node.span(), false)?;
 
-        self.wr.write_punct(None, ";")?;
+        self.wr.write_punct(Some(node.span), ";")?;
     }
 
     #[emitter]
