f53c162cedf24c59812c815953652b60262b4f24
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-01 23:54:30 +0900

    Fix dbg-swc

--- crates/dbg-swc/src/minify/ensure_size.rs
+++ crates/dbg-swc/src/minify/ensure_size.rs
@@ -8,7 +8,10 @@ use anyhow::{Context, Result};
 use clap::Args;
 use flate2::{write::ZlibEncoder, Compression};
 use rayon::prelude::*;
-use swc_common::{SourceFile, SourceMap, GLOBALS};
+use swc_common::{
+    errors::{ColorConfig, Handler, HANDLER},
+    SourceFile, SourceMap, GLOBALS,
+};
 use tracing::info;
 
 use crate::util::{
@@ -148,61 +151,64 @@ impl EnsureSize {
             info!("Checking {}", js_file.display());
 
             let fm = cm.load_file(js_file).context("failed to load file")?;
+            let handler =
+                Handler::with_tty_emitter(ColorConfig::Never, true, false, Some(cm.clone()));
+            HANDLER.set(&handler, || {
+                let code_mangled = {
+                    let minified_mangled = get_minified(cm.clone(), js_file, true, true)?;
 
-            let code_mangled = {
-                let minified_mangled = get_minified(cm.clone(), js_file, true, true)?;
+                    print_js(cm.clone(), &minified_mangled.module, true)
+                        .context("failed to convert ast to code")?
+                };
 
-                print_js(cm.clone(), &minified_mangled.module, true)
-                    .context("failed to convert ast to code")?
-            };
+                let swc_no_mangle = {
+                    let minified_no_mangled = get_minified(cm.clone(), js_file, true, false)?;
 
-            let swc_no_mangle = {
-                let minified_no_mangled = get_minified(cm.clone(), js_file, true, false)?;
+                    print_js(cm, &minified_no_mangled.module, true)
+                        .context("failed to convert ast to code")?
+                };
 
-                print_js(cm, &minified_no_mangled.module, true)
-                    .context("failed to convert ast to code")?
-            };
+                // eprintln!("The output size of swc minifier: {}", code_mangled.len());
 
-            // eprintln!("The output size of swc minifier: {}", code_mangled.len());
+                let mut file_size = FileSize {
+                    fm,
+                    swc: MinifierOutput {
+                        mangled_size: code_mangled.len(),
+                        no_mangle_size: swc_no_mangle.len(),
+                        gzipped_size: gzipped_size(&code_mangled),
+                    },
+                    terser: Default::default(),
+                    esbuild: Default::default(),
+                };
 
-            let mut file_size = FileSize {
-                fm,
-                swc: MinifierOutput {
-                    mangled_size: code_mangled.len(),
-                    no_mangle_size: swc_no_mangle.len(),
-                    gzipped_size: gzipped_size(&code_mangled),
-                },
-                terser: Default::default(),
-                esbuild: Default::default(),
-            };
+                if !self.no_terser {
+                    let terser_mangled = get_terser_output(js_file, true, true)?;
+                    let terser_no_mangle = get_terser_output(js_file, true, false)?;
 
-            if !self.no_terser {
-                let terser_mangled = get_terser_output(js_file, true, true)?;
-                let terser_no_mangle = get_terser_output(js_file, true, false)?;
+                    file_size.terser = Some(MinifierOutput {
+                        mangled_size: terser_mangled.len(),
+                        no_mangle_size: terser_no_mangle.len(),
+                        gzipped_size: gzipped_size(&terser_mangled),
+                    });
+                }
 
-                file_size.terser = Some(MinifierOutput {
-                    mangled_size: terser_mangled.len(),
-                    no_mangle_size: terser_no_mangle.len(),
-                    gzipped_size: gzipped_size(&terser_mangled),
-                });
-            }
+                if !self.no_esbuild {
+                    let esbuild_mangled = get_esbuild_output(js_file, true)?;
+                    let esbuild_no_mangle = get_esbuild_output(js_file, false)?;
 
-            if !self.no_esbuild {
-                let esbuild_mangled = get_esbuild_output(js_file, true)?;
-                let esbuild_no_mangle = get_esbuild_output(js_file, false)?;
+                    file_size.esbuild = Some(MinifierOutput {
+                        mangled_size: esbuild_mangled.len(),
+                        no_mangle_size: esbuild_no_mangle.len(),
+                        gzipped_size: gzipped_size(&esbuild_mangled),
+                    });
+                }
 
-                file_size.esbuild = Some(MinifierOutput {
-                    mangled_size: esbuild_mangled.len(),
-                    no_mangle_size: esbuild_no_mangle.len(),
-                    gzipped_size: gzipped_size(&esbuild_mangled),
-                });
-            }
+                if file_size.terser.is_none() && file_size.esbuild.is_none() {
+                    return Ok(None);
+                }
 
-            if file_size.terser.is_none() && file_size.esbuild.is_none() {
-                return Ok(None);
-            }
-
-            Ok(Some(file_size))
+                Ok(Some(file_size))
+            })
         })
         .with_context(|| format!("failed to check file: {}", js_file.display()))
     }
