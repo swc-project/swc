da761108a07d4ce2f289e83ee425311d0700c562
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-01 19:47:16 +0900

    --release

--- crates/swc_ecma_minifier/scripts/test.sh
+++ crates/swc_ecma_minifier/scripts/test.sh
@@ -6,6 +6,6 @@ export UPDATE=1
 export SWC_CHECK=0
 export SWC_RUN=0
 
-cargo test -p swc_ecma_minifier -p swc --no-fail-fast --features concurrent
+cargo test -p swc_ecma_minifier --release -p swc --no-fail-fast --features concurrent
 
 # find ../swc/tests/ -type f -empty -delete
