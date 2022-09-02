d95ec3c171df375c859f162f6e7096235da2a2b9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-02 15:38:16 +0900

    Update test refs

--- crates/swc/tests/tsc-references/assignParameterPropertyToPropertyDeclarationESNext.2.minified.js
+++ crates/swc/tests/tsc-references/assignParameterPropertyToPropertyDeclarationESNext.2.minified.js
@@ -5,12 +5,3 @@ class F {
     };
     p1 = 0;
 }
-class G {
-    Inner;
-    constructor(p1){
-        this.p1 = p1, this.Inner = class extends G {
-            p2 = this.p1;
-        };
-    }
-    p1;
-}
