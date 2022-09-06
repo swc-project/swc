a0c835b23afe1c1060aa9acc93c7e763c6b7a12e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-06 17:40:32 +0900

    WIP

---
+++ crates/swc_ecma_minifier/scripts/mangler/terser-one.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Generates reference output for the mangler, using terser
+# 
+set -eu
+
+
+
+npx terser --mangle --output {}-terser.js
--- crates/swc_ecma_minifier/scripts/mangler/terser.sh
+++ crates/swc_ecma_minifier/scripts/mangler/terser.sh
@@ -3,3 +3,7 @@
 # Generates reference output for the mangler, using terser
 # 
 set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+find ./tests/terser -name 'input.js' -exec $SCRIPT_DIR/terser-one.sh {} \;
