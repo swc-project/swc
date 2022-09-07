68885eb74fba6758ba1ac355b0c2ce80475490da
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-07 13:28:19 +0900

    fixup

--- crates/swc_ecma_parser/examples/parse-all.rs
+++ crates/swc_ecma_parser/examples/parse-all.rs
@@ -1,20 +1,12 @@
+#![feature(bench_black_box)]
 #![deny(warnings)]
 
 extern crate swc_node_base;
 
-use std::{env, fs, path::PathBuf, time::Instant};
+use std::{env, hint::black_box, path::PathBuf, time::Instant};
 
-use anyhow::Result;
-use rayon::prelude::*;
-use swc_common::{errors::HANDLER, sync::Lrc, Mark, SourceMap, GLOBALS};
-use swc_ecma_codegen::text_writer::JsWriter;
-use swc_ecma_minifier::{
-    optimize,
-    option::{ExtraOptions, MangleOptions, MinifyOptions},
-};
+use swc_common::{errors::HANDLER, GLOBALS};
 use swc_ecma_parser::parse_file_as_module;
-use swc_ecma_transforms_base::{fixer::fixer, resolver};
-use swc_ecma_visit::FoldWith;
 use walkdir::WalkDir;
 
 fn main() {
@@ -28,12 +20,9 @@ fn main() {
             HANDLER.set(&handler, || {
                 let _ = files
                     .into_iter()
-                    .map(|path| -> Result<_> {
+                    .map(|path| -> Result<(), ()> {
                         GLOBALS.set(globals, || {
                             let fm = cm.load_file(&path).expect("failed to load file");
-
-                            let unresolved_mark = Mark::new();
-                            let top_level_mark = Mark::new();
 
                             let program = parse_file_as_module(
                                 &fm,
@@ -45,41 +34,9 @@ fn main() {
                             .map_err(|err| {
                                 err.into_diagnostic(&handler).emit();
                             })
-                            .map(|module| {
-                                module.fold_with(&mut resolver(
-                                    unresolved_mark,
-                                    top_level_mark,
-                                    false,
-                                ))
-                            })
                             .unwrap();
 
-                            let output = optimize(
-                                program.into(),
-                                cm.clone(),
-                                None,
-                                None,
-                                &MinifyOptions {
-                                    compress: Some(Default::default()),
-                                    mangle: Some(MangleOptions {
-                                        top_level: true,
-                                        ..Default::default()
-                                    }),
-                                    ..Default::default()
-                                },
-                                &ExtraOptions {
-                                    unresolved_mark,
-                                    top_level_mark,
-                                },
-                            )
-                            .expect_module();
-
-                            let output = output.fold_with(&mut fixer(None));
-
-                            let code = print(cm.clone(), &[output], true);
-
-                            fs::write("output.js", code.as_bytes())
-                                .expect("failed to write output");
+                            black_box(program);
 
                             Ok(())
                         })
@@ -97,7 +54,7 @@ fn main() {
 
 /// Return the whole input files as abolute path.
 fn expand_dirs(dirs: Vec<String>) -> Vec<PathBuf> {
-    dirs.into_par_iter()
+    dirs.into_iter()
         .map(PathBuf::from)
         .map(|dir| dir.canonicalize().unwrap())
         .flat_map(|dir| {
@@ -115,26 +72,4 @@ fn expand_dirs(dirs: Vec<String>) -> Vec<PathBuf> {
                 .collect::<Vec<_>>()
         })
         .collect()
-}
-
-fn print<N: swc_ecma_codegen::Node>(cm: Lrc<SourceMap>, nodes: &[N], minify: bool) -> String {
-    let mut buf = vec![];
-
-    {
-        let mut emitter = swc_ecma_codegen::Emitter {
-            cfg: swc_ecma_codegen::Config {
-                minify,
-                ..Default::default()
-            },
-            cm: cm.clone(),
-            comments: None,
-            wr: Box::new(JsWriter::new(cm, "\n", &mut buf, None)),
-        };
-
-        for n in nodes {
-            n.emit_with(&mut emitter).unwrap();
-        }
-    }
-
-    String::from_utf8(buf).unwrap()
 }
