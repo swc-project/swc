eba95a2a2f39fc50536b8eadac2c64caa33f2539
magic-akari <akari.ccino@gmail.com>
2022-09-10 01:20:07 +0800

    fix test case

--- crates/swc/tests/exec/next/40399/1/exec.js
+++ crates/swc/tests/exec/next/40399/1/exec.js
@@ -1,5 +1,5 @@
-async () => {
+(async () => {
     const blob = new Blob();
     new Uint8Array(await blob.arrayBuffer());
     console.log("Success");
-};
+})();
--- crates/swc/tests/exec/next/40399/2/exec.js
+++ crates/swc/tests/exec/next/40399/2/exec.js
@@ -1,6 +1,6 @@
-async () => {
+(async () => {
     const a = await 1;
     expect(a).toBe(1);
     const b = ((x) => x + x)(await 2);
     expect(b).toBe(4);
-};
+})();
