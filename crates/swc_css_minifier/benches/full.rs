d6bf7458c30efdd11c9e8eb21f5203d9e61bc22f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-10 12:23:58 +0900

    Bench

--- crates/swc_css_minifier/benches/full.rs
+++ crates/swc_css_minifier/benches/full.rs
@@ -3,8 +3,13 @@ extern crate swc_node_base;
 use std::{fs::read_to_string, path::Path};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use swc_common::{errors::HANDLER, sync::Lrc, FileName, Mark, SourceMap};
+use swc_common::{errors::HANDLER, FileName};
 use swc_css_ast::Stylesheet;
+use swc_css_codegen::{
+    writer::basic::{BasicCssWriter, BasicCssWriterConfig},
+    Emit,
+};
+use swc_css_minifier::minify;
 use swc_css_parser::{parse_file, parser::ParserConfig};
 
 pub fn bench_files(c: &mut Criterion) {
@@ -37,83 +42,37 @@ fn run(src: &str) {
             let fm = cm.new_source_file(FileName::Anon, src.into());
 
             let mut errors = vec![];
-            let ss: Stylesheet = parse_file(&fm, ParserConfig {}, &mut errors).unwrap();
-
-            let unresolved_mark = Mark::new();
-            let top_level_mark = Mark::new();
-
-            let program = parse_file_as_module(
+            let mut ss: Stylesheet = parse_file(
                 &fm,
-                Default::default(),
-                Default::default(),
-                None,
-                &mut vec![],
+                ParserConfig {
+                    ..Default::default()
+                },
+                &mut errors,
             )
-            .map_err(|err| {
-                err.into_diagnostic(&handler).emit();
-            })
-            .map(|module| module.fold_with(&mut resolver(unresolved_mark, top_level_mark, false)))
             .unwrap();
 
-            let output = optimize(
-                program.into(),
-                cm.clone(),
-                None,
-                None,
-                &MinifyOptions {
-                    rename: false,
-                    compress: Some(CompressOptions {
+            minify(&mut ss, Default::default());
+
+            let mut buf = String::new();
+            {
+                let wr = BasicCssWriter::new(
+                    &mut buf,
+                    None,
+                    BasicCssWriterConfig {
                         ..Default::default()
-                    }),
-                    mangle: Some(MangleOptions {
-                        props: None,
-                        top_level: true,
-                        keep_class_names: false,
-                        keep_fn_names: false,
-                        keep_private_props: false,
-                        ie8: false,
-                        safari10: false,
-                        reserved: Default::default(),
-                    }),
-                    wrap: false,
-                    enclose: false,
-                },
-                &ExtraOptions {
-                    unresolved_mark,
-                    top_level_mark,
-                },
-            )
-            .expect_module();
+                    },
+                );
+                let mut generator = swc_css_codegen::CodeGenerator::new(
+                    wr,
+                    swc_css_codegen::CodegenConfig { minify: true },
+                );
 
-            let output = output.fold_with(&mut fixer(None));
+                generator.emit(&ss).unwrap();
+            }
 
-            let code = print(cm, &[output], true);
-
-            black_box(code);
+            black_box(buf);
             Ok(())
         })
     })
     .unwrap();
-}
-
-fn print<N: swc_ecma_codegen::Node>(cm: Lrc<SourceMap>, nodes: &[N], minify: bool) -> String {
-    let mut buf = vec![];
-
-    {
-        let mut emitter = swc_ecma_codegen::Emitter {
-            cfg: swc_ecma_codegen::Config {
-                minify,
-                ..Default::default()
-            },
-            cm: cm.clone(),
-            comments: None,
-            wr: Box::new(JsWriter::new(cm, "\n", &mut buf, None)),
-        };
-
-        for n in nodes {
-            n.emit_with(&mut emitter).unwrap();
-        }
-    }
-
-    String::from_utf8(buf).unwrap()
 }
