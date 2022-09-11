b8aceae3fa05bd0622ca0679363bf07b98aeee11
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-11 14:06:24 +0900

    Move url logic

--- crates/swc_css_minifier/src/compressor/mod.rs
+++ crates/swc_css_minifier/src/compressor/mod.rs
@@ -1,7 +1,14 @@
-use swc_css_visit::VisitMut;
+use swc_css_ast::*;
+use swc_css_visit::{VisitMut, VisitMutWith};
 
 mod url;
 
 struct Compressor {}
 
-impl VisitMut for Compressor {}
+impl VisitMut for Compressor {
+    fn visit_mut_url(&mut self, n: &mut Url) {
+        n.visit_mut_children_with(self);
+
+        self.compress_url(n);
+    }
+}
--- crates/swc_css_minifier/src/compressor/url.rs
+++ crates/swc_css_minifier/src/compressor/url.rs
@@ -1,3 +1,55 @@
+use swc_atoms::js_word;
+use swc_css_ast::*;
+
 use super::Compressor;
 
-impl Compressor {}
+impl Compressor {
+    pub(super) fn compress_url(&self, url: &mut Url) {
+        if url.name.value.to_ascii_lowercase() != js_word!("url") {
+            return;
+        }
+
+        if let Some(modifiers) = &url.modifiers {
+            if !modifiers.is_empty() {
+                return;
+            }
+        }
+
+        if let Some(UrlValue::Str(Str { value, span, .. })) = &url.value {
+            let mut escaped = String::new();
+            let mut has_escaped = false;
+
+            for c in value.chars() {
+                match c {
+                    '(' | ')' | '"' | '\'' => {
+                        if has_escaped {
+                            return;
+                        }
+
+                        has_escaped = true;
+                        escaped.push('\\');
+                        escaped.push(c)
+                    }
+                    _ if c.is_whitespace() => {
+                        if has_escaped {
+                            return;
+                        }
+
+                        has_escaped = true;
+                        escaped.push('\\');
+                        escaped.push(c)
+                    }
+                    _ => escaped.push(c),
+                }
+            }
+
+            url.value = Some(UrlValue::Raw(UrlValueRaw {
+                span: *span,
+                value: escaped.clone().into(),
+                before: None,
+                raw: None,
+                after: None,
+            }));
+        }
+    }
+}
