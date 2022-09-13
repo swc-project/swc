0284c1bf40de7d199733ee8f93c189d9658fe6b0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-13 19:32:00 +0900

    test(es/transforms): Enable tests for fixed issues (#5845)

--- crates/swc_ecma_minifier/tests/terser/compress/harmony/array_literal_with_spread_4a/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/harmony/array_literal_with_spread_4a/output.js
@@ -2,15 +2,15 @@ function t(x) {
     console.log("(" + x + ")");
     return 10 * x;
 }
-console.log((t(2), t(1)));
+console.log([t(1), t(2)][0]);
 console.log((t(1), t(2)));
-console.log((t(1), t(2), void 0));
-console.log((t(2), t(1)));
+console.log([t(1), t(2)][2]);
+console.log([t(1), t(2)][0]);
 console.log((t(1), t(2)));
-console.log((t(1), t(2), void 0));
-console.log((t(2), t(1)));
+console.log([t(1), t(2)][2]);
+console.log([t(1), t(2)][0]);
 console.log((t(1), t(2)));
-console.log((t(1), t(2), void 0));
-console.log((t(2), t(1)));
+console.log([t(1), t(2)][2]);
+console.log([t(1), t(2)][0]);
 console.log((t(1), t(2)));
-console.log((t(1), t(2), void 0));
+console.log([t(1), t(2)][2]);
