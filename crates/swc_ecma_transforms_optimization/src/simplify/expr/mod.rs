75e077cc0893bf82cb0fb8da7f97c020c009a3e7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-13 12:44:37 +0900

    Parallel

--- crates/swc_ecma_transforms_optimization/src/simplify/expr/mod.rs
+++ crates/swc_ecma_transforms_optimization/src/simplify/expr/mod.rs
@@ -1562,7 +1562,9 @@ impl VisitMut for SimplifyExpr {
             in_callee: Default::default(),
         };
 
-        n.visit_mut_children_with(&mut child);
+        child.maybe_par(cpu_count() * 8, n, |v, n| {
+            n.visit_mut_with(v);
+        });
         self.changed |= child.changed;
     }
 
