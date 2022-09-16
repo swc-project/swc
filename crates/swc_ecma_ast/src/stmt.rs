f3f3e0175d58b4550a35f5f99c8cf9c23065e6f0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-16 14:47:12 +0900

    More ast work

--- crates/swc_ecma_ast/src/stmt.rs
+++ crates/swc_ecma_ast/src/stmt.rs
@@ -402,6 +402,9 @@ pub enum VarDeclOrExpr {
     Expr(Box<Expr>),
 }
 
+bridge_from!(VarDeclOrExpr, Box<VarDecl>, VarDecl);
+bridge_from!(VarDeclOrExpr, Box<Expr>, Expr);
+
 impl Take for VarDeclOrExpr {
     fn dummy() -> Self {
         VarDeclOrExpr::Expr(Take::dummy())
