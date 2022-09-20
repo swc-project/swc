6bbd3f4c14eb243b697dc12593ada1b196a0cad8
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-20 13:29:08 +0900

    perf(es/minifier): Make more passes parallel (#5906)
    **Description:**
    
    We only unwrap parenthesis in precompress optimizer, so I refactored it. Also, as it's embarrassingly parallel, I parallelized it.

--- crates/swc_ecma_minifier/src/pass/precompress.rs
+++ crates/swc_ecma_minifier/src/pass/precompress.rs
@@ -1,85 +1,30 @@
-use swc_atoms::js_word;
 use swc_common::util::take::Take;
 use swc_ecma_ast::*;
-use swc_ecma_transforms_base::{
-    helpers::{Helpers, HELPERS},
-    perf::Parallel,
-};
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith, VisitWith};
+use swc_ecma_transforms_base::perf::{Parallel, ParallelExt};
+use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
 
-use crate::{
-    analyzer::{ModuleInfo, UsageAnalyzer},
-    marks::Marks,
-    option::CompressOptions,
-    util::ModuleItemExt,
-};
+use crate::HEAVY_TASK_PARALLELS;
 
 /// Optimizer invoked before invoking compressor.
 ///
 /// - Remove parens.
-pub(crate) fn precompress_optimizer<'a>(
-    module_info: &'a ModuleInfo,
-    options: &'a CompressOptions,
-    marks: Marks,
-) -> impl 'a + VisitMut {
-    PrecompressOptimizer {
-        options,
-        module_info,
-        marks,
-        ctx: Default::default(),
-    }
+pub(crate) fn precompress_optimizer<'a>() -> impl 'a + VisitMut {
+    PrecompressOptimizer {}
 }
 
 #[derive(Debug)]
-pub(crate) struct PrecompressOptimizer<'a> {
-    options: &'a CompressOptions,
-    module_info: &'a ModuleInfo,
+pub(crate) struct PrecompressOptimizer {}
 
-    marks: Marks,
-
-    ctx: Ctx,
-}
-
-impl Parallel for PrecompressOptimizer<'_> {
+impl Parallel for PrecompressOptimizer {
     fn create(&self) -> Self {
-        Self { ..*self }
+        Self {}
     }
 
     fn merge(&mut self, _: Self) {}
 }
 
-#[derive(Debug, Default, Clone, Copy)]
-struct Ctx {
-    in_var_pat: bool,
-}
-
-impl PrecompressOptimizer<'_> {
-    fn handle_stmts<T>(&mut self, stmts: &mut Vec<T>)
-    where
-        T: for<'aa> VisitMutWith<PrecompressOptimizer<'aa>> + ModuleItemExt,
-        Vec<T>: for<'aa> VisitMutWith<PrecompressOptimizer<'aa>> + VisitWith<UsageAnalyzer>,
-    {
-        stmts.visit_mut_children_with(&mut PrecompressOptimizer {
-            options: self.options,
-            module_info: self.module_info,
-            marks: self.marks,
-            ctx: self.ctx,
-        });
-    }
-}
-
-impl VisitMut for PrecompressOptimizer<'_> {
+impl VisitMut for PrecompressOptimizer {
     noop_visit_mut_type!();
-
-    fn visit_mut_decl(&mut self, n: &mut Decl) {
-        n.visit_mut_children_with(self);
-
-        if let Decl::Fn(FnDecl { ident, .. }) = n {
-            if ident.sym == js_word!("") {
-                n.take();
-            }
-        }
-    }
 
     fn visit_mut_expr(&mut self, e: &mut Expr) {
         e.visit_mut_children_with(self);
@@ -89,53 +34,39 @@ impl VisitMut for PrecompressOptimizer<'_> {
         }
     }
 
-    fn visit_mut_module_item(&mut self, n: &mut ModuleItem) {
-        n.visit_mut_children_with(self);
-
-        match n {
-            ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ExportDecl {
-                decl: Decl::Var(var),
-                ..
-            })) if var.decls.is_empty() => {
-                n.take();
-            }
-            _ => {}
-        }
+    fn visit_mut_stmts(&mut self, n: &mut Vec<Stmt>) {
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
+        });
     }
 
     fn visit_mut_module_items(&mut self, n: &mut Vec<ModuleItem>) {
-        HELPERS.set(&Helpers::new(true), || {
-            self.handle_stmts(n);
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
         });
-
-        n.retain(|s| !matches!(s, ModuleItem::Stmt(Stmt::Empty(..))));
     }
 
-    fn visit_mut_stmt(&mut self, n: &mut Stmt) {
-        n.visit_mut_children_with(self);
-
-        match n {
-            Stmt::Decl(Decl::Var(var)) if var.decls.is_empty() => {
-                n.take();
-            }
-            _ => {}
-        }
+    fn visit_mut_exprs(&mut self, n: &mut Vec<Box<Expr>>) {
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
+        });
     }
 
-    fn visit_mut_stmts(&mut self, n: &mut Vec<Stmt>) {
-        self.handle_stmts(n);
-
-        n.retain(|s| !matches!(s, Stmt::Empty(..)));
+    fn visit_mut_opt_vec_expr_or_spreads(&mut self, n: &mut Vec<Option<ExprOrSpread>>) {
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
+        });
     }
 
-    fn visit_mut_var_declarator(&mut self, n: &mut VarDeclarator) {
-        let old = self.ctx;
-        self.ctx.in_var_pat = true;
-        n.name.visit_mut_with(self);
+    fn visit_mut_expr_or_spreads(&mut self, n: &mut Vec<ExprOrSpread>) {
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
+        });
+    }
 
-        self.ctx.in_var_pat = false;
-        n.init.visit_mut_with(self);
-
-        self.ctx = old;
+    fn visit_mut_var_declarators(&mut self, n: &mut Vec<VarDeclarator>) {
+        self.maybe_par(*HEAVY_TASK_PARALLELS, n, |v, n| {
+            n.visit_mut_with(v);
+        });
     }
 }
