74f25c1ded54a07df16ee106c7c1c44ddcad6b54
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-20 16:34:57 +0900

    Revert

--- crates/swc_ecma_minifier/src/lib.rs
+++ crates/swc_ecma_minifier/src/lib.rs
@@ -23,7 +23,12 @@
 #![allow(clippy::match_like_matches_macro)]
 
 use once_cell::sync::Lazy;
-use swc_common::{comments::Comments, pass::Repeated, sync::Lrc, SourceMap, SyntaxContext};
+use swc_common::{
+    comments::Comments,
+    pass::{Repeat, Repeated},
+    sync::Lrc,
+    SourceMap, SyntaxContext,
+};
 use swc_ecma_ast::*;
 use swc_ecma_transforms_optimization::debug_assert_valid;
 use swc_ecma_visit::VisitMutWith;
@@ -240,31 +245,17 @@ pub fn optimize(
         let _timer = timer!("postcompress");
 
         m.visit_mut_with(&mut postcompress_optimizer(options));
-
-        let mut pass = 0;
-        loop {
-            pass += 1;
-
-            if pass > 3 {
-                break;
-            }
-
-            let mut v = pure_optimizer(
-                options,
-                None,
-                marks,
-                PureOptimizerConfig {
-                    force_str_for_tpl: Minification::force_str_for_tpl(),
-                    enable_join_vars: true,
-                    #[cfg(feature = "debug")]
-                    debug_infinite_loop: false,
-                },
-            );
-            m.visit_mut_with(&mut v);
-            if !v.changed() {
-                break;
-            }
-        }
+        m.visit_mut_with(&mut Repeat::new(pure_optimizer(
+            options,
+            None,
+            marks,
+            PureOptimizerConfig {
+                force_str_for_tpl: Minification::force_str_for_tpl(),
+                enable_join_vars: true,
+                #[cfg(feature = "debug")]
+                debug_infinite_loop: false,
+            },
+        )));
     }
 
     if let Some(ref mut _t) = timings {
