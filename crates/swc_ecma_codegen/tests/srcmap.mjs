1ab06280d51151ccffe02dbc8edc576e33505836
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-21 16:20:43 +0900

    Ignore syntax error

--- crates/swc_ecma_codegen/tests/srcmap.mjs
+++ crates/swc_ecma_codegen/tests/srcmap.mjs
@@ -1,10 +1,22 @@
 import { minify } from "terser";
 
-const code = process.argv[1];
+try {
+    const code = process.argv[1];
 
-const output = await minify(code, { sourceMap: true, });
+    const output = await minify(code, { sourceMap: true, });
 
-console.log(JSON.stringify({
-    code: output.code,
-    map: output.map,
-}))
+    console.log(JSON.stringify({
+        code: output.code,
+        map: output.map,
+    }))
+} catch (e) {
+    // Ignore syntax error
+    if (e instanceof SyntaxError) {
+        console.log(JSON.stringify({
+            code: '',
+            map: {},
+        }));
+    } else {
+        throw e;
+    }
+}
