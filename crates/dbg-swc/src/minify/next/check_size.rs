d174c14bcca4c2aa228720dd55fc424658515e0a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-24 12:26:35 +0900

    hmm

--- crates/dbg-swc/src/minify/next/check_size.rs
+++ crates/dbg-swc/src/minify/next/check_size.rs
@@ -10,13 +10,21 @@ pub struct CheckSizeCommand {
     #[clap(long, short = 'w', default_value = ".swc/dbg-swc")]
     workspace: PathBuf,
 
-    /// Rerun `next build` even if `workspace` is not empty.
+    /// Rerun `npm run build` even if `workspace` is not empty.
     #[clap(long)]
     ensure_fresh: bool,
 }
 
 impl CheckSizeCommand {
     pub fn run(self, cm: Arc<SourceMap>) -> Result<()> {
+        Ok(())
+    }
+
+    /// Invokes `npm run build` with appropriate environment variables, and
+    /// store the result in `self.workspace`.
+    fn extract_inputs(&self) -> Result<()> {
+        if !self.ensure_fresh {}
+
         Ok(())
     }
 }
