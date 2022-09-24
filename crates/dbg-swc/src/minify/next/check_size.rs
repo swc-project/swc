aefee896d215af973a145953f2b36526f0e4b119
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-24 12:21:16 +0900

    workspace

--- crates/dbg-swc/src/minify/next/check_size.rs
+++ crates/dbg-swc/src/minify/next/check_size.rs
@@ -1,11 +1,14 @@
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
 use anyhow::Result;
 use clap::Args;
 use swc_common::SourceMap;
 
 #[derive(Debug, Args)]
-pub struct CheckSizeCommand {}
+pub struct CheckSizeCommand {
+    #[clap(long, default_value = ".swc/dbg-swc")]
+    workspace: PathBuf,
+}
 
 impl CheckSizeCommand {
     pub fn run(self, cm: Arc<SourceMap>) -> Result<()> {
