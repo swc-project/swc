af10c297ed5d930ac892b3cf9b018f5e5278d84a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-24 11:55:23 +0900

    Fix

--- crates/swc_ecma_utils/src/lib.rs
+++ crates/swc_ecma_utils/src/lib.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::boxed_local)]
 #![allow(clippy::match_like_matches_macro)]
 #![allow(clippy::vec_box)]
-#![cfg_attr(not(feature = "concurrent", allow(unused)))]
+#![cfg_attr(not(feature = "concurrent"), allow(unused))]
 
 #[doc(hidden)]
 pub extern crate swc_ecma_ast;
