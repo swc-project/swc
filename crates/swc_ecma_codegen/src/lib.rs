910f5b6e9e8b96767f7fd521f1a8be6b42e2b48e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-25 23:01:26 +0900

    Patch

--- crates/swc_ecma_codegen/src/lib.rs
+++ crates/swc_ecma_codegen/src/lib.rs
@@ -2639,8 +2639,8 @@ where
 
         self.emit_leading_comments_of_span(node.span(), true)?;
 
+        srcmap!(node, false, true);
         punct!("}");
-        srcmap!(node, false);
     }
 
     #[emitter]
