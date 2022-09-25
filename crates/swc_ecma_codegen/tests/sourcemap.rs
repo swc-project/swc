c4b4091f795b8d8a61b9119d51e9343ff6d247ee
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-25 23:44:12 +0900

    Ignore

--- crates/swc_ecma_codegen/tests/sourcemap.rs
+++ crates/swc_ecma_codegen/tests/sourcemap.rs
@@ -67,6 +67,7 @@ const IGNORED_PASS_TESTS: &[&str] = &[
     "fe2d3b945530c806.js",
     "fa5b398eeef697a6.js",
     "fd5ea844fcc07d3d.js",
+    "fbde237f11796df9.js",
 ];
 
 #[testing::fixture("../swc_ecma_parser/tests/test262-parser/pass/*.js")]
