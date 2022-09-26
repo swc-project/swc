e7e8f1f018f10bbbe056c2c29296ecf5bc4e1ec2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-26 17:14:19 +0900

    Next

--- crates/swc_ecma_codegen/tests/sourcemap.rs
+++ crates/swc_ecma_codegen/tests/sourcemap.rs
@@ -248,9 +248,9 @@ static IGNORED_PASS_TESTS: &[&str] = &[
     "6c5f0dd83c417a5a.js",
     "78eb22badc114b6f.js",
     "7afd38d79e6795a8.js",
+    "80950061e291542b.js",
     // TODO(kdy1): Fix
-    // "80950061e291542b.js",
-    "8a0fc8ea31727188.module.js",
+    // "8a0fc8ea31727188.module.js",
     "af97a3752e579223.js",
     "bbffb851469a3f0e.js",
     "bc302492d441d561.js",
