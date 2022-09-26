089ad1ae776d81a930b1ecda05e4fcbf43813653
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-26 22:36:10 +0900

    Fix lints

--- crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
@@ -1025,7 +1025,7 @@ where
                     return false;
                 }
 
-                if !self.is_skippable_for_seq(a, &obj) {
+                if !self.is_skippable_for_seq(a, obj) {
                     return false;
                 }
 
@@ -1035,27 +1035,27 @@ where
                     }
                 }
 
-                return true;
+                true
             }
 
-            Expr::Lit(..) => return true,
+            Expr::Lit(..) => true,
 
-            Expr::Yield(..) | Expr::Await(..) => return false,
+            Expr::Yield(..) | Expr::Await(..) => false,
 
             Expr::Unary(UnaryExpr {
                 op: op!("!") | op!("void") | op!("typeof") | op!(unary, "-") | op!(unary, "+"),
                 arg,
                 ..
-            }) => return self.is_skippable_for_seq(a, arg),
+            }) => self.is_skippable_for_seq(a, arg),
 
             Expr::Bin(BinExpr { left, right, .. }) => {
-                return self.is_skippable_for_seq(a, left) && self.is_skippable_for_seq(a, right)
+                self.is_skippable_for_seq(a, left) && self.is_skippable_for_seq(a, right)
             }
 
             Expr::Cond(CondExpr {
                 test, cons, alt, ..
             }) => {
-                return self.is_skippable_for_seq(a, test)
+                self.is_skippable_for_seq(a, test)
                     && self.is_skippable_for_seq(a, cons)
                     && self.is_skippable_for_seq(a, alt)
             }
@@ -1110,7 +1110,7 @@ where
                     return false;
                 }
 
-                return self.is_skippable_for_seq(a, &e.right);
+                self.is_skippable_for_seq(a, &e.right)
             }
 
             Expr::Object(e) => {
@@ -1152,7 +1152,7 @@ where
 
                 // TODO: Check for side effects in object properties.
 
-                return true;
+                true
             }
 
             Expr::Array(e) => {
@@ -1163,7 +1163,7 @@ where
                     }
                 }
 
-                return true;
+                true
             }
 
             Expr::Call(e) => {
@@ -1193,7 +1193,7 @@ where
                     }
                 }
 
-                return true;
+                true
             }
 
             Expr::New(e) => {
@@ -1209,39 +1209,37 @@ where
                     }
                 }
 
-                return true;
+                true
             }
 
             Expr::Seq(SeqExpr { exprs, .. }) => {
-                return exprs.iter().all(|e| self.is_skippable_for_seq(a, e));
+                exprs.iter().all(|e| self.is_skippable_for_seq(a, e))
             }
 
             Expr::TaggedTpl(..) => {
                 // TODO(kdy1): We can optimize some known calls.
 
-                return false;
+                false
             }
 
-            Expr::Tpl(Tpl { exprs, .. }) => {
-                return exprs.iter().all(|e| self.is_skippable_for_seq(a, e));
-            }
+            Expr::Tpl(Tpl { exprs, .. }) => exprs.iter().all(|e| self.is_skippable_for_seq(a, e)),
 
             // Expressions without any effects
             Expr::This(_)
             | Expr::Fn(_)
             | Expr::MetaProp(_)
             | Expr::Arrow(_)
-            | Expr::PrivateName(_) => return true,
+            | Expr::PrivateName(_) => true,
 
-            Expr::Update(e) => return self.is_skippable_for_seq(a, &e.arg),
+            Expr::Update(e) => self.is_skippable_for_seq(a, &e.arg),
             Expr::SuperProp(e) => match &e.prop {
-                SuperProp::Ident(_) => return true,
-                SuperProp::Computed(p) => return self.is_skippable_for_seq(a, &p.expr),
+                SuperProp::Ident(_) => true,
+                SuperProp::Computed(p) => self.is_skippable_for_seq(a, &p.expr),
             },
-            Expr::Class(_) => return e.may_have_side_effects(&self.expr_ctx),
+            Expr::Class(_) => e.may_have_side_effects(&self.expr_ctx),
 
-            Expr::Paren(e) => return self.is_skippable_for_seq(a, &e.expr),
-            Expr::Unary(e) => return self.is_skippable_for_seq(a, &e.arg),
+            Expr::Paren(e) => self.is_skippable_for_seq(a, &e.expr),
+            Expr::Unary(e) => self.is_skippable_for_seq(a, &e.arg),
 
             Expr::OptChain(OptChainExpr { base, .. }) => match base {
                 OptChainBase::Member(e) => {
@@ -1265,7 +1263,7 @@ where
                         }
                     }
 
-                    return true;
+                    true
                 }
                 OptChainBase::Call(e) => {
                     if !self.is_skippable_for_seq(a, &e.callee) {
@@ -1278,11 +1276,11 @@ where
                         }
                     }
 
-                    return true;
+                    true
                 }
             },
 
-            Expr::Invalid(_) => return true,
+            Expr::Invalid(_) => true,
 
             Expr::JSXMember(_)
             | Expr::JSXNamespacedName(_)
@@ -1296,10 +1294,6 @@ where
             | Expr::TsInstantiation(_)
             | Expr::TsSatisfaction(_) => unreachable!("jsx/ts is not supported"),
         }
-
-        log_abort!("sequences: skip: Unknown expr: {}", dump(e, true));
-
-        false
     }
 
     /// Returns true if something is modified.
