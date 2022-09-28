dfdad69c03d13d8950e89358f6587f2bc66d5912
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-09-28 05:55:17 +0300

    feat(css/minifier): Remove duplicate `@counter-style` at-rules (#5970)

--- crates/swc_css_minifier/src/compressor/mod.rs
+++ crates/swc_css_minifier/src/compressor/mod.rs
@@ -41,13 +41,13 @@ impl VisitMut for Compressor {
     fn visit_mut_stylesheet(&mut self, n: &mut Stylesheet) {
         n.visit_mut_children_with(self);
 
-        self.compresss_empty_stylesheet(n);
+        self.compress_empty_stylesheet(n);
     }
 
     fn visit_mut_simple_block(&mut self, n: &mut SimpleBlock) {
         n.visit_mut_children_with(self);
 
-        self.compresss_empty_simple_block(n);
+        self.compress_empty_simple_block(n);
     }
 
     fn visit_mut_time(&mut self, n: &mut Time) {
