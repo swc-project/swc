2a42952802631f6bf4ae50ac03c6335121a7b28d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-29 08:22:05 +0900

    Fix

--- crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
@@ -1284,7 +1284,7 @@ where
             | Expr::TsNonNull(_)
             | Expr::TsAs(_)
             | Expr::TsInstantiation(_)
-            | Expr::TsSatisfaction(_) => {}
+            | Expr::TsSatisfaction(_) => false,
         }
     }
 
