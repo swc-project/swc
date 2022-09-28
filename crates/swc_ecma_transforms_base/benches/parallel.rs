f1d5ec1dfec4e055947e87e7b6f2bc20273901af
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-09-28 13:26:23 +0900

    Parallel bench

---
+++ crates/swc_ecma_transforms_base/benches/parallel.rs
@@ -0,0 +1,70 @@
+extern crate swc_node_base;
+
+use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
+use rayon::prelude::*;
+use swc_common::{chain, errors::HANDLER, FileName, Mark};
+use swc_ecma_parser::{Parser, StringInput, Syntax};
+use swc_ecma_transforms_base::helpers;
+use swc_ecma_visit::FoldWith;
+
+static SOURCE: &str = include_str!("../../swc_ecma_minifier/benches/full/typescript.js");
+
+/// Benchmark a folder
+macro_rules! tr {
+    ($b:expr, $tr:expr) => {
+        let _ = ::testing::run_test(false, |cm, handler| {
+            HANDLER.set(&handler, || {
+                let fm = cm.new_source_file(FileName::Anon, SOURCE.into());
+
+                let mut parser = Parser::new(
+                    Syntax::Typescript(Default::default()),
+                    StringInput::from(&*fm),
+                    None,
+                );
+                let module = parser.parse_module().map_err(|_| ()).unwrap();
+                helpers::HELPERS.set(&Default::default(), || {
+                    $b.iter(|| {
+                        (0..50).into_par_iter().for_each(|_| {
+                            let mut tr = $tr();
+
+                            let module = module.clone();
+                            black_box(module.fold_with(&mut tr));
+                        })
+                    });
+                    Ok(())
+                })
+            })
+        });
+    };
+}
+
+fn resolver(b: &mut Bencher) {
+    tr!(b, || swc_ecma_transforms_base::resolver(
+        Mark::new(),
+        Mark::new(),
+        false
+    ));
+}
+
+fn hygiene(b: &mut Bencher) {
+    tr!(b, swc_ecma_transforms_base::hygiene::hygiene);
+}
+
+fn resolver_with_hygiene(b: &mut Bencher) {
+    tr!(b, || chain!(
+        swc_ecma_transforms_base::resolver(Mark::new(), Mark::new(), false),
+        swc_ecma_transforms_base::hygiene::hygiene()
+    ));
+}
+
+fn bench_cases(c: &mut Criterion) {
+    c.bench_function("es/parallel/resolver/typescript", resolver);
+    c.bench_function("es/parallel/hygiene/typescript", hygiene);
+    c.bench_function(
+        "es/parallel/resolver_with_hygiene/typescript",
+        resolver_with_hygiene,
+    );
+}
+
+criterion_group!(benches, bench_cases);
+criterion_main!(benches);
