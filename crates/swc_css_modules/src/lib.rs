bd754891a08d13c5dfa0142d5ebd5620478ca32e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-02 11:46:38 +0900

    Config type

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -1,1 +1,7 @@
 pub mod analyzer;
+
+/// Various configurations for the css modules.
+///
+/// This is a trait rather than a struct because api like `fn() -> String` is
+/// too restricted and `Box<Fn() -> String` is (needlessly) slow.
+pub trait Config {}
