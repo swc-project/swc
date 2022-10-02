38605daa3b56009288627a26b1df5c06cc92cb25
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-02 15:18:21 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/terser/compress/issue_1639/issue_1639_2/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/issue_1639/issue_1639_2/output.js
@@ -1,6 +1,5 @@
-var a = 100,
-    b = 10;
+var a = 100, b = 10;
 function f19() {
-    ++a, 0;
+    ++a;
 }
 f19(), console.log(a, b);
--- crates/swc_ecma_minifier/tests/terser/compress/issue_1733/function_catch_catch/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/issue_1733/function_catch_catch/output.js
@@ -2,14 +2,14 @@ var o = 0;
 function f() {
     try {
         throw 1;
-    } catch (c) {
+    } catch (t) {
         try {
             throw 2;
-        } catch (o) {
-            var o = 3;
-            console.log(o);
+        } catch (c) {
+            var c = 3;
+            console.log(c);
         }
     }
-    console.log(o);
+    console.log(c);
 }
 f();
