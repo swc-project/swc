5990f05757d02ea71a2d67a467fd556e3c8c3638
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-03 23:49:51 +0900

    compose

--- crates/swc_css_modules/src/analyzer.rs
+++ crates/swc_css_modules/src/analyzer.rs
@@ -1,7 +1,7 @@
 //! Import/export analyzer
 
 use swc_atoms::JsWord;
-use swc_css_ast::{Declaration, Stylesheet};
+use swc_css_ast::{Declaration, DeclarationName, Stylesheet};
 use swc_css_visit::{Visit, VisitWith};
 
 pub fn analyze_imports(ss: &Stylesheet) -> Vec<JsWord> {
@@ -15,5 +15,9 @@ struct Analyzer {
 impl Visit for Analyzer {
     fn visit_declaration(&mut self, d: &Declaration) {
         d.visit_children_with(self);
+
+        if let DeclarationName::Ident(name) = &d.name {
+            if &*name.value == "compose" {}
+        }
     }
 }
