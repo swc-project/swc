954aefdc6754710d8f8d7f9abf5d5bd8a996455b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-03 22:12:08 +0900

    visit

--- crates/swc_css_modules/src/analyzer.rs
+++ crates/swc_css_modules/src/analyzer.rs
@@ -2,7 +2,14 @@
 
 use swc_atoms::JsWord;
 use swc_css_ast::Stylesheet;
+use swc_css_visit::Visit;
 
 pub fn analyze_imports(ss: &Stylesheet) -> Vec<JsWord> {
     todo!()
 }
+
+struct Analyzer {
+    imports: Vec<JsWord>,
+}
+
+impl Visit for Analyzer {}
