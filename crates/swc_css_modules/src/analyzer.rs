45693b6b68956d7515d672bd64a85c2029c852f1
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-03 22:36:54 +0900

    WIP

--- crates/swc_css_modules/src/analyzer.rs
+++ crates/swc_css_modules/src/analyzer.rs
@@ -1,8 +1,8 @@
 //! Import/export analyzer
 
 use swc_atoms::JsWord;
-use swc_css_ast::Stylesheet;
-use swc_css_visit::Visit;
+use swc_css_ast::{Declaration, Stylesheet};
+use swc_css_visit::{Visit, VisitWith};
 
 pub fn analyze_imports(ss: &Stylesheet) -> Vec<JsWord> {
     todo!()
@@ -12,4 +12,8 @@ struct Analyzer {
     imports: Vec<JsWord>,
 }
 
-impl Visit for Analyzer {}
+impl Visit for Analyzer {
+    fn visit_declaration(&mut self, d: &Declaration) {
+        d.visit_children_with(self);
+    }
+}
