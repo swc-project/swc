021d4d041ee27e11fdece2856f501161aef51c9e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-04 18:00:09 +0900

    swc_css

--- crates/swc_css/src/lib.rs
+++ crates/swc_css/src/lib.rs
@@ -3,6 +3,9 @@ pub extern crate swc_css_codegen as codegen;
 #[cfg(feature = "swc_css_minifier")]
 #[cfg_attr(docsrs, doc(cfg(feature = "minifier")))]
 pub extern crate swc_css_minifier as minifier;
+#[cfg(feature = "swc_css_modules")]
+#[cfg_attr(docsrs, doc(cfg(feature = "modules")))]
+pub extern crate swc_css_modules as modules;
 pub extern crate swc_css_parser as parser;
 pub extern crate swc_css_utils as utils;
 pub extern crate swc_css_visit as visit;
