d75553e541f93fd8e732d7438805035486d607b7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-04 11:51:51 +0900

    compiler

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -1,3 +1,6 @@
+use swc_css_ast::Stylesheet;
+use swc_css_visit::VisitMut;
+
 pub mod imports;
 
 /// Various configurations for the css modules.
@@ -5,3 +8,14 @@ pub mod imports;
 /// This is a trait rather than a struct because api like `fn() -> String` is
 /// too restricted and `Box<Fn() -> String` is (needlessly) slow.
 pub trait Config {}
+
+pub fn compile(ss: &mut Stylesheet, config: impl Config) {}
+
+struct Compiler<C>
+where
+    C: Config,
+{
+    config: C,
+}
+
+impl<C> VisitMut for Compiler<C> where C: Config {}
