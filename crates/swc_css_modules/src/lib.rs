3b0909ef195041c150f9531d5769737a7d2705de
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-04 13:54:39 +0900

    to_tokens

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -1,4 +1,10 @@
-use swc_css_ast::{Declaration, PseudoClassSelector, SelectorList, Stylesheet};
+use swc_common::{input::StringInput, Spanned};
+use swc_css_ast::{Declaration, PseudoClassSelector, SelectorList, Stylesheet, TokenAndSpan};
+use swc_css_codegen::{
+    writer::basic::{BasicCssWriter, BasicCssWriterConfig, IndentType},
+    CodeGenerator, CodegenConfig, Emit,
+};
+use swc_css_parser::{lexer::Lexer, parser::ParserConfig};
 use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub mod imports;
@@ -40,9 +46,8 @@ where
         match &*n.name.value {
             "local" => {
                 if let Some(children) = &mut n.children {
-                    for s in children {
-                        match s {}
-                    }
+                    let tokens = children.to_tokens();
+                    for s in children {}
                 }
             }
             "global" => {}
@@ -50,4 +55,41 @@ where
             _ => {}
         }
     }
+}
+
+fn to_tokens<N>(n: &N) -> Vec<TokenAndSpan>
+where
+    N: Spanned,
+    for<'aa, 'ab> CodeGenerator<BasicCssWriter<'aa, &'ab mut String>>: swc_css_codegen::Emit<N>,
+{
+    let span = n.span();
+
+    let mut buf = String::new();
+    {
+        let mut wr = BasicCssWriter::new(
+            &mut buf,
+            None,
+            BasicCssWriterConfig {
+                indent_type: IndentType::Tab,
+                ..Default::default()
+            },
+        );
+        let mut g = CodeGenerator::new(
+            wr,
+            CodegenConfig {
+                ..Default::default()
+            },
+        );
+
+        g.emit(n).unwrap();
+    }
+
+    let mut lexer = Lexer::new(
+        StringInput::new(&buf, span.lo, span.hi),
+        ParserConfig {
+            allow_wrong_line_comments: true,
+        },
+    );
+
+    lexer.collect()
 }
