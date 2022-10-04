2ac59507a05e6c6776a55f24ec29bdbac8d06678
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-04 13:28:54 +0900

    Test

--- crates/swc_css_modules/tests/fixture.rs
+++ crates/swc_css_modules/tests/fixture.rs
@@ -1,5 +1,9 @@
 use std::path::PathBuf;
 
+use swc_css_codegen::{
+    writer::basic::{BasicCssWriter, BasicCssWriterConfig, IndentType},
+    CodeGenerator, CodegenConfig, Emit,
+};
 use testing::NormalizedOutput;
 
 #[testing::fixture("tests/fixture/**/*.css")]
@@ -26,3 +30,53 @@ fn imports(input: PathBuf) {
     })
     .unwrap();
 }
+
+#[testing::fixture("tests/fixture/**/*.css")]
+fn compile(input: PathBuf) {
+    testing::run_test(false, |cm, handler| {
+        let fm = cm.load_file(&input).unwrap();
+        let mut errors = vec![];
+        let mut ss = swc_css_parser::parse_file(&fm, Default::default(), &mut errors).unwrap();
+        let result = swc_css_modules::imports::analyze_imports(&ss);
+
+        if result.is_empty() {
+            return Ok(());
+        }
+
+        swc_css_modules::compile(&mut ss, TestConfig {});
+
+        let mut buf = String::new();
+        {
+            let mut wr = BasicCssWriter::new(
+                &mut buf,
+                None,
+                BasicCssWriterConfig {
+                    indent_type: IndentType::Tab,
+                    ..Default::default()
+                },
+            );
+            let mut g = CodeGenerator::new(
+                wr,
+                CodegenConfig {
+                    ..Default::default()
+                },
+            );
+
+            g.emit(&ss).unwrap();
+        }
+
+        NormalizedOutput::from(buf)
+            .compare_to_file(input.with_file_name(format!(
+                "{}.compiled.css",
+                input.file_stem().unwrap().to_string_lossy()
+            )))
+            .unwrap();
+
+        Ok(())
+    })
+    .unwrap();
+}
+
+struct TestConfig {}
+
+impl swc_css_modules::Config for TestConfig {}
