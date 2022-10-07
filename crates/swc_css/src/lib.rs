347d4b7602d434608efb8d6fdb6874cd25aaa437
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-07 14:18:14 +0900

    feat(css/compat): Implement transform for nested css (#6077)

--- crates/swc_css/src/lib.rs
+++ crates/swc_css/src/lib.rs
@@ -1,5 +1,8 @@
 pub extern crate swc_css_ast as ast;
 pub extern crate swc_css_codegen as codegen;
+#[cfg(feature = "swc_css_compat")]
+#[cfg_attr(docsrs, doc(cfg(feature = "compat")))]
+pub extern crate swc_css_compat as compat;
 #[cfg(feature = "swc_css_minifier")]
 #[cfg_attr(docsrs, doc(cfg(feature = "minifier")))]
 pub extern crate swc_css_minifier as minifier;
@@ -7,5 +10,8 @@ pub extern crate swc_css_minifier as minifier;
 #[cfg_attr(docsrs, doc(cfg(feature = "modules")))]
 pub extern crate swc_css_modules as modules;
 pub extern crate swc_css_parser as parser;
+#[cfg(feature = "swc_css_prefixer")]
+#[cfg_attr(docsrs, doc(cfg(feature = "prefixer")))]
+pub extern crate swc_css_prefixer as prefixer;
 pub extern crate swc_css_utils as utils;
 pub extern crate swc_css_visit as visit;
---
+++ crates/swc_css_compat/tests/nesting.rs
@@ -0,0 +1,65 @@
+//! Tests ported from https://github.com/thysultan/stylis.js
+//!
+//! License is MIT, which is original license at the time of copying.
+//! Original test authors have copyright for their work.
+#![deny(warnings)]
+#![allow(clippy::needless_update)]
+
+use std::path::PathBuf;
+
+use swc_css_ast::Stylesheet;
+use swc_css_codegen::{
+    writer::basic::{BasicCssWriter, BasicCssWriterConfig},
+    CodegenConfig, Emit,
+};
+use swc_css_compat::nesting::nesting;
+use swc_css_parser::{parse_file, parser::ParserConfig};
+use swc_css_visit::VisitMutWith;
+use testing::NormalizedOutput;
+
+fn test_nesting(input: PathBuf, suffix: Option<&str>) {
+    let parent = input.parent().unwrap();
+    let output = match suffix {
+        Some(suffix) => parent.join("output.".to_owned() + suffix + ".css"),
+        _ => parent.join("output.css"),
+    };
+
+    testing::run_test2(false, |cm, handler| {
+        //
+        let fm = cm.load_file(&input).unwrap();
+        let mut errors = vec![];
+        let mut ss: Stylesheet = parse_file(
+            &fm,
+            ParserConfig {
+                allow_wrong_line_comments: true,
+                ..Default::default()
+            },
+            &mut errors,
+        )
+        .unwrap();
+        for err in errors {
+            err.to_diagnostics(&handler).emit();
+        }
+
+        ss.visit_mut_with(&mut nesting());
+
+        let mut s = String::new();
+        {
+            let mut wr = BasicCssWriter::new(&mut s, None, BasicCssWriterConfig::default());
+            let mut gen =
+                swc_css_codegen::CodeGenerator::new(&mut wr, CodegenConfig { minify: false });
+
+            gen.emit(&ss).unwrap();
+        }
+
+        NormalizedOutput::from(s).compare_to_file(&output).unwrap();
+
+        Ok(())
+    })
+    .unwrap();
+}
+
+#[testing::fixture("tests/nesting/**/input.css")]
+fn test_without_env(input: PathBuf) {
+    test_nesting(input, None)
+}
