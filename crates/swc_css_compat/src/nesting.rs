a5ebe01ca980c27a355bc9e880900e8c8c340554
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-07 11:15:54 +0900

    structure

--- crates/swc_css_compat/src/nesting.rs
+++ crates/swc_css_compat/src/nesting.rs
@@ -1,3 +1,5 @@
+use swc_common::util::take::Take;
+use swc_css_ast::{QualifiedRule, Rule};
 use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub fn nesting() -> impl VisitMut {
@@ -6,4 +8,29 @@ pub fn nesting() -> impl VisitMut {
 
 struct NestingHandler {}
 
-impl VisitMut for NestingHandler {}
+impl NestingHandler {
+    fn extract_nested_rules(&mut self, n: &mut QualifiedRule) -> Vec<Box<QualifiedRule>> {
+        vec![]
+    }
+}
+
+impl VisitMut for NestingHandler {
+    fn visit_mut_rules(&mut self, n: &mut Vec<Rule>) {
+        n.visit_mut_children_with(self);
+
+        let mut new = vec![];
+        for n in n.take() {
+            match n {
+                Rule::QualifiedRule(mut n) => {
+                    let rules = self.extract_nested_rules(&mut n);
+                    new.push(Rule::QualifiedRule(n));
+                    new.extend(rules.into_iter().map(Rule::QualifiedRule));
+                }
+                _ => {
+                    new.push(n);
+                }
+            }
+        }
+        *n = new;
+    }
+}
