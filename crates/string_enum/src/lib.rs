4c103074f625f0912039e27394ac3c3b9e62e2ff
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-10 21:21:24 +0900

    Eat it

--- crates/string_enum/src/lib.rs
+++ crates/string_enum/src/lib.rs
@@ -63,7 +63,7 @@ use syn::{self, *};
 ///
 ///
 /// All formatting flags are handled correctly.
-#[proc_macro_derive(StringEnum)]
+#[proc_macro_derive(StringEnum, attributes(string_enum))]
 pub fn derive_string_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse::<syn::DeriveInput>(input)
         .map(From::from)
