3799197a6fd33b36072b559f1be44928bcf50a2f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-11 15:22:12 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/terser/compress/issue_640/cond_5/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/issue_640/cond_5/output.js
@@ -1,2 +1,2 @@
 some_condition() && some_other_condition() ? do_something() : alternate();
-if (some_condition() && some_other_condition()) do_something();
+some_condition() && some_other_condition() && do_something();
--- crates/swc_ecma_minifier/tests/terser/compress/issue_640/iife/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/issue_640/iife/output.js
@@ -1,8 +1,1 @@
-(x = 42),
-    (function a() {})(),
-    (function b() {})(),
-    (function c() {})(),
-    (function d() {})(),
-    (function e() {})(),
-    (function f() {})(),
-    typeof (function g() {})();
+x = 42;
