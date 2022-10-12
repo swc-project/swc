0e12cf6e977dc504589dc5661408a49a53f3b32b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-12 15:55:53 +0900

    Rename

--- crates/swc_ecma_minifier/tests/compress.rs
+++ crates/swc_ecma_minifier/tests/compress.rs
@@ -293,7 +293,7 @@ fn find_config(dir: &Path) -> String {
 #[testing::fixture("tests/fixture/**/input.js")]
 #[testing::fixture("tests/pass-1/**/input.js")]
 #[testing::fixture("tests/single-pass/**/input.js")]
-#[testing::fixture("tests/default-option/**/input.js")]
+#[testing::fixture("tests/pass-default/**/input.js")]
 fn custom_fixture(input: PathBuf) {
     let dir = input.parent().unwrap();
     let config = find_config(dir);
