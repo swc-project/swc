7b9d085965f81e058d30e364cace2a94d3a9c596
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-12 14:41:20 +0900

    Inline more

--- crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
@@ -1512,6 +1512,13 @@ where
                 };
 
                 if !self.is_skippable_for_seq(Some(a), &Expr::Ident(b_left.clone())) {
+                    if let Some(a_id) = a.id() {
+                        if a_id == b_left.to_id() {
+                            if self.replace_seq_assignment(a, b)? {
+                                return Ok(true);
+                            }
+                        }
+                    }
                     return Ok(false);
                 }
 
