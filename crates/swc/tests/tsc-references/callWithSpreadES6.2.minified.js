1221f243b3643c845eea6ef935d6d2d0ba5d746c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-12 11:50:55 +0900

    Revert

--- crates/swc/tests/tsc-references/callWithSpreadES6.2.minified.js
+++ crates/swc/tests/tsc-references/callWithSpreadES6.2.minified.js
@@ -1,6 +1,7 @@
 //// [callWithSpreadES6.ts]
 var a, obj, xa;
-obj.foo(1, 2, "abc"), obj.foo(1, 2, ...a), obj.foo(1, 2, ...a, "abc"), obj.foo(1, 2, "abc"), obj.foo(1, 2, ...a), obj.foo(1, 2, ...a, "abc"), xa[1].foo(1, 2, "abc"), xa[1].foo(1, 2, ...a), xa[1].foo(1, 2, ...a, "abc"), xa[1].foo(...[
+function foo(x, y, ...z) {}
+foo(1, 2, "abc"), foo(1, 2, ...a), foo(1, 2, ...a, "abc"), obj.foo(1, 2, "abc"), obj.foo(1, 2, ...a), obj.foo(1, 2, ...a, "abc"), obj.foo(1, 2, "abc"), obj.foo(1, 2, ...a), obj.foo(1, 2, ...a, "abc"), xa[1].foo(1, 2, "abc"), xa[1].foo(1, 2, ...a), xa[1].foo(1, 2, ...a, "abc"), xa[1].foo(...[
     1,
     2,
     "abc"
