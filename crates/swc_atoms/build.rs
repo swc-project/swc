e31673151e44a4ffd568726055867c2c9e5693d9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-13 14:01:07 +0900

    Fix doc

--- crates/swc_atoms/build.rs
+++ crates/swc_atoms/build.rs
@@ -16,15 +16,15 @@ fn gen(mac_name: &str, type_name: &str, atoms: &[&str]) {
         .atoms(atoms)
         .with_atom_doc(
             "
-//! [JsWord] is an interned string.
-//!
-//! This type should be used instead of [String] for values, because lots of
-//! values are duplicated. For example, if an identifier is named `myVariable`,
-//! there will be lots of identifier usages with the value `myVariable`.
-//!
-//! This type
-//!  - makes equality comparison faster.
-//!  - reduces memory usage.
+[JsWord] is an interned string.
+
+This type should be used instead of [String] for values, because lots of
+values are duplicated. For example, if an identifier is named `myVariable`,
+there will be lots of identifier usages with the value `myVariable`.
+
+This type
+  - makes equality comparison faster.
+  - reduces memory usage.
             ",
         )
         .write_to_file(&Path::new(&env::var("OUT_DIR").unwrap()).join(format!("{}.rs", mac_name)))
