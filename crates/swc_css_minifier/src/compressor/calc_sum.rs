26aeb18c0cbb3299951be7b7a9398705d33374cd
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-10-14 17:46:36 +0300

    fix(css/parser): Improve handling of math functions in at-rules (#6140)

--- crates/swc_css_minifier/src/compressor/calc_sum.rs
+++ crates/swc_css_minifier/src/compressor/calc_sum.rs
@@ -1,15 +1,10 @@
 use std::collections::HashMap;
 
-use swc_atoms::{js_word, JsWord};
+use swc_atoms::JsWord;
 use swc_css_ast::*;
 
 use super::{unit::*, Compressor};
-
-fn is_calc_function_name(ident: &Ident) -> bool {
-    ident.value.to_ascii_lowercase() == js_word!("calc")
-        || ident.value.to_ascii_lowercase() == js_word!("-webkit-calc")
-        || ident.value.to_ascii_lowercase() == js_word!("-moz-calc")
-}
+use crate::compressor::math::{is_calc_function_name, transform_calc_value_into_component_value};
 
 // transform "(simple calc-value)" into "simple calc-value"
 fn remove_unnecessary_nesting_from_calc_sum(calc_sum: &mut CalcSum) {
@@ -48,76 +43,6 @@ fn try_to_extract_into_calc_value(calc_sum: &CalcSum) -> Option<CalcValue> {
     }
 
     None
-}
-
-fn transform_calc_value_into_component_value(calc_value: &CalcValue) -> ComponentValue {
-    match &calc_value {
-        CalcValue::Number(n) => ComponentValue::Number(n.clone()),
-        CalcValue::Dimension(Dimension::Length(l)) => {
-            ComponentValue::Dimension(Dimension::Length(Length {
-                span: l.span,
-                value: l.value.clone(),
-                unit: l.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::Angle(a)) => {
-            ComponentValue::Dimension(Dimension::Angle(Angle {
-                span: a.span,
-                value: a.value.clone(),
-                unit: a.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::Time(t)) => {
-            ComponentValue::Dimension(Dimension::Time(Time {
-                span: t.span,
-                value: t.value.clone(),
-                unit: t.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::Frequency(f)) => {
-            ComponentValue::Dimension(Dimension::Frequency(Frequency {
-                span: f.span,
-                value: f.value.clone(),
-                unit: f.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::Resolution(r)) => {
-            ComponentValue::Dimension(Dimension::Resolution(Resolution {
-                span: r.span,
-                value: r.value.clone(),
-                unit: r.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::Flex(f)) => {
-            ComponentValue::Dimension(Dimension::Flex(Flex {
-                span: f.span,
-                value: f.value.clone(),
-                unit: f.unit.clone(),
-            }))
-        }
-        CalcValue::Dimension(Dimension::UnknownDimension(u)) => {
-            ComponentValue::Dimension(Dimension::UnknownDimension(UnknownDimension {
-                span: u.span,
-                value: u.value.clone(),
-                unit: u.unit.clone(),
-            }))
-        }
-        CalcValue::Percentage(p) => ComponentValue::Percentage(Percentage {
-            span: p.span,
-            value: p.value.clone(),
-        }),
-        CalcValue::Function(f) => ComponentValue::Function(Function {
-            span: f.span,
-            name: f.name.clone(),
-            value: f.value.to_vec(),
-        }),
-        CalcValue::Constant(_) => {
-            unreachable!("CalcValue::Constant cannot be transformed into a ComponentValue per spec")
-        }
-        CalcValue::Sum(_) => {
-            unreachable!("CalcValue::Sum cannot be transformed into a ComponentValue")
-        }
-    }
 }
 
 // We want to track the position of data (dimension, percentage, operator...) in
@@ -1064,7 +989,11 @@ impl Compressor {
                                         // they’re treated like any other
                                         // keyword"
                                     }
-                                    CalcValueOrOperator::Value(calc_value) => {
+                                    // `calc` and other math functions can be used in `@supports` to
+                                    // check availability, we should leave them as is
+                                    CalcValueOrOperator::Value(calc_value)
+                                        if !self.in_supports_conidition =>
+                                    {
                                         *component_value =
                                             transform_calc_value_into_component_value(calc_value);
                                     }
