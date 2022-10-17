fee99e4d6c65d3c259119e852343dbd76ff88db5
alexander.akait <sheo13666q@gmail.com>
2022-10-18 01:41:38 +0300

    fix(html/minifier): compress `exportparts`

--- crates/swc_html_minifier/src/lib.rs
+++ crates/swc_html_minifier/src/lib.rs
@@ -445,6 +445,7 @@ impl Minifier<'_> {
                 {
                     true
                 }
+                _ if attribute_name == "exportparts" => true,
                 _ => COMMA_SEPARATED_HTML_ATTRIBUTES.contains(&(&element.tag_name, attribute_name)),
             },
             Namespace::SVG => {
@@ -2355,6 +2356,8 @@ impl VisitMut for Minifier<'_> {
                                     }
                                 } else if matches!(n.name, js_word!("points")) {
                                     self.collapse_whitespace(value.trim())
+                                } else if matches!(n.name, js_word!("exportparts")) {
+                                    value.chars().filter(|c| !c.is_whitespace()).collect()
                                 } else {
                                     value.trim().to_string()
                                 }
