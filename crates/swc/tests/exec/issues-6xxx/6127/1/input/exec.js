8ca1c15e2e35bae1f6596527183b8c9bc19fefe9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-20 22:33:37 +0900

    Add a test

---
+++ crates/swc/tests/exec/issues-6xxx/6127/1/input/exec.js
@@ -0,0 +1,13 @@
+const a = 0;
+let b;
+for (let i = 0; i < 2; ++i) {
+    if (i === 0) {
+        b = 1;
+    } else {
+        b = 2;
+    }
+    console.log(a, b);
+}
+
+let c = 1, d;
+while (c--) { }
