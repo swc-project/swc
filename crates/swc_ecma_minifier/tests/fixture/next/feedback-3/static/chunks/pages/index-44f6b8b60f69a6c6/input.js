e1b47a39a967ad867380e2c239de8e7bbdf289f5
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-20 12:55:25 +0900

    Add tests

---
+++ crates/swc_ecma_minifier/tests/fixture/next/feedback-3/static/chunks/pages/index-44f6b8b60f69a6c6/input.js
@@ -0,0 +1,54 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[405],{
+
+/***/ 5557:
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/",
+      function () {
+        return __webpack_require__(3678);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 3678:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Home; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var next_test_pkg_reproduce__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(7029);
+/* harmony import */ var next_test_pkg_reproduce__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_test_pkg_reproduce__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+
+
+console.log((next_test_pkg_reproduce__WEBPACK_IMPORTED_MODULE_1___default()));
+
+function Home() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+            href: "/test",
+            children: "Test"
+        })
+    });
+}
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, [579,774,888,179], function() { return __webpack_exec__(5557); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
