efa8a02c7468704a531745c4166f467d3f1b42ba
alexander.akait <sheo13666q@gmail.com>
2022-10-20 21:39:50 +0300

    test: more

--- crates/swc_css_minifier/src/compressor/rules.rs
+++ crates/swc_css_minifier/src/compressor/rules.rs
@@ -253,7 +253,7 @@ impl Compressor {
         };
 
         match name.to_ascii_lowercase() {
-            js_word!("media") | js_word!("container") => true,
+            js_word!("media") | js_word!("container") | js_word!("nest") => true,
             _ => false,
         }
     }
