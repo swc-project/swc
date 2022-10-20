75f8dbb64603511cb436204cf8503439c0b0b5bc
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-20 13:01:40 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/next/feedback-3/static/vEESw4AJQZmfZsjR7Pp26/_buildManifest/input.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002F579-dcac359116b2707c.js","static\u002Fchunks\u002Fpages\u002Findex-44f6b8b60f69a6c6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2077dbd4a2768e48.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
