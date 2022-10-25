b0206cb855e018e9e5e9c19aaf79adda2d32123e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-25 13:22:24 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/terser/compress/collapse_vars/var_defs/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/collapse_vars/var_defs/output.js
@@ -1,7 +1,5 @@
-var f1 = function (x, y) {
-    var r = x + y,
-        a = r * r - r,
-        b = 7;
-    console.log(a + b);
+var f1 = function(x, y) {
+    var r = x + y;
+    console.log(r * r - r + 7);
 };
 f1("1", 0);
