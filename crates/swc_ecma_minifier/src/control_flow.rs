bcb067e8bf4d2d113e748fd16dedf1abfef9d490
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-27 14:55:30 +0900

    Type

--- crates/swc_ecma_minifier/src/control_flow.rs
+++ crates/swc_ecma_minifier/src/control_flow.rs
@@ -1,1 +1,19 @@
+use swc_ecma_ast::{Expr, Stmt};
+use swc_fast_graph::digraph::FastDiGraphMap;
 
+/// The id of a basic block
+pub(crate) type BlockId = u32;
+
+pub(crate) enum NodeRef<'a> {
+    Expr(&'a Expr),
+    Stmt(&'a Stmt),
+}
+
+pub(crate) struct BasicBlock<'a> {
+    pub(crate) id: BlockId,
+    pub(crate) exprs: Vec<NodeRef<'a>>,
+}
+
+pub struct ControlFlowGraph<'a> {
+    graph: FastDiGraphMap<u32, BasicBlock<'a>>,
+}
