fe9eaf897be171f5c697b158812ab584f3ff5f36
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-28 14:11:36 +0900

    Only reduced

--- crates/swc_ecma_minifier/tests/fixture/next/41992/2-reduced/input.js
+++
@@ -1,5 +0,0 @@
-export const
-    N = (0, p.default)(e = (0, ft.default)((0, p.default)(r).call(r, ((e, t) => {
-        const r = t.get("in");
-        return e[r] ?? (e[r] = []), e[r].push(t), e
-    }), {}))).call(e, ((e, t) => (0, h.default)(e).call(e, t)), []);
--- crates/swc_ecma_minifier/tests/fixture/next/41992/2-reduced/output.js
+++
@@ -1,4 +0,0 @@
-export const N = (0, p.default)(e = (0, ft.default)((0, p.default)(r).call(r, (e1, t)=>{
-    const r1 = t.get("in");
-    return e1[r1] ?? (e1[r1] = []), e1[r1].push(t), e1;
-}, {}))).call(e, (e1, t)=>(0, h.default)(e1).call(e1, t), []);
