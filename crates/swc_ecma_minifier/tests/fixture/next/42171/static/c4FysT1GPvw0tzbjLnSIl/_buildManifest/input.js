9bb261616bc4ba45e30ecb2c06823cded5df08bb
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-01 10:57:12 +0900

    Remove unrelated

--- crates/swc_ecma_minifier/tests/fixture/next/42171/static/c4FysT1GPvw0tzbjLnSIl/_buildManifest/input.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002F584-c58203e505b85ffc.js","static\u002Fchunks\u002Fpages\u002Findex-800f3285a2da07a5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c8b0663085900c58.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
