5044bf32f922253a881a70a8f05eced6f5770527
austaras <austaras@outlook.com>
2022-11-05 23:21:02 +0800

    fix(es/lints): Disallow assign to class

---
+++ crates/swc/tests/errors/lints/const-assign/3/input.js
@@ -0,0 +1,1 @@
+class X { static y = X = 0 }
--- crates/swc/tests/tsc-references/assignments.2.minified.js
+++ crates/swc/tests/tsc-references/assignments.2.minified.js
@@ -1,5 +1,10 @@
 //// [assignments.ts]
-var E;
-M = null, function(E) {
-    E[E.A = 0] = "A";
-}(E || (E = {})), (E = null).A = null, I = null;
+//! 
+//!   x cannot reassign to a class
+//!     ,-[13:1]
+//!  13 | class C { }
+//!     :       |
+//!     :       `-- class name
+//!  14 | C = null; // Error
+//!     : ^
+//!     `----
