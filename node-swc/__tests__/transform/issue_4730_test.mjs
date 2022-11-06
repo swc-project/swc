76c0c1d1c72fc4d451fe6559555c863c4c896bfd
nissy-dev <nd.12021218@gmail.com>
2022-11-06 23:05:28 +0900

    fix: update snapshot

--- node-swc/__tests__/transform/issue_4730_test.mjs
+++ node-swc/__tests__/transform/issue_4730_test.mjs
@@ -37,9 +37,9 @@ Object.defineProperty(exports, \\"__esModule\\", {
     value: true
 });
 const _interopRequireWildcard = require(\\"@swc/helpers/lib/_interop_require_wildcard.js\\").default;
-const _b = require(\\"../packages/b/src/index\\");
+const _b = require(\\"../packages/b/src/index.ts\\");
 async function display() {
-    const displayA = await Promise.resolve().then(()=>/*#__PURE__*/ _interopRequireWildcard(require(\\"../packages/a/src/index\\"))).then((c)=>c.displayA);
+    const displayA = await Promise.resolve().then(()=>/*#__PURE__*/ _interopRequireWildcard(require(\\"../packages/a/src/index.ts\\"))).then((c)=>c.displayA);
     console.log(displayA());
     console.log((0, _b.displayB)());
 }
