61eab5fa6d1b5f39b5a1f20ac17e262b268f56db
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-14 11:03:10 +0900

    Add a single-pass test

--- crates/swc_ecma_minifier/tests/exec.rs
+++ crates/swc_ecma_minifier/tests/exec.rs
@@ -10427,3 +10427,73 @@ fn issue_6419_1() {
         false,
     );
 }
+
+#[test]
+fn issue_6419_single_pass() {
+    run_exec_test(
+        r###"
+        (function () {
+            var first = state();
+            var on = true;
+            var obj = {
+                state: state
+            };
+            console.log(first)
+            console.log(obj.state());
+            function state() {
+                return on ? "ON" : "OFF";
+            }
+        })();
+        "###,
+        r###"
+        {
+            "passes": 1,
+            "arguments": false,
+            "arrows": true,
+            "booleans": true,
+            "booleans_as_integers": false,
+            "collapse_vars": true,
+            "comparisons": true,
+            "computed_props": true,
+            "conditionals": true,
+            "dead_code": true,
+            "directives": true,
+            "drop_console": false,
+            "drop_debugger": true,
+            "evaluate": true,
+            "expression": false,
+            "hoist_funs": false,
+            "hoist_props": true,
+            "hoist_vars": false,
+            "if_return": true,
+            "join_vars": true,
+            "keep_classnames": false,
+            "keep_fargs": true,
+            "keep_fnames": false,
+            "keep_infinity": false,
+            "loops": true,
+            "negate_iife": true,
+            "properties": true,
+            "reduce_funcs": false,
+            "reduce_vars": false,
+            "side_effects": true,
+            "switches": true,
+            "typeofs": true,
+            "unsafe": false,
+            "unsafe_arrows": false,
+            "unsafe_comps": false,
+            "unsafe_Function": false,
+            "unsafe_math": false,
+            "unsafe_symbols": false,
+            "unsafe_methods": false,
+            "unsafe_proto": false,
+            "unsafe_regexp": false,
+            "unsafe_undefined": false,
+            "unused": true,
+            "const_to_let": true,
+            "pristine_globals": true
+        }
+        "###,
+        false,
+    );
+}
