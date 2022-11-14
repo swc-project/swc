00d76b7944aeac2885e69b0bfea806ceae72cd20
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-14 13:31:03 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-6xxx/6345/1/output/index.js
@@ -0,0 +1,23 @@
+"use strict";
+function n() {}
+var r = function() {
+    var n = function n(n) {};
+    var r = null;
+    return {
+        init: function t(t) {
+            return r = new n(t);
+        }
+    };
+}();
+var t = function() {
+    var r = function r() {
+        n();
+    };
+    ;
+    var t = null;
+    return {
+        init: function n(n) {
+            return t;
+        }
+    };
+}();
