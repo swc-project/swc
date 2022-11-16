55e27c5342121041faec786bc8ec38a0150d330e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-16 13:16:29 +0900

    Update test refs

--- crates/swc_ecma_transforms_module/tests/fixture-manual/issue-4730/output/index.js
+++ crates/swc_ecma_transforms_module/tests/fixture-manual/issue-4730/output/index.js
@@ -1,6 +1,6 @@
-import { displayB } from "../packages/b/src/index.ts";
+import { displayB } from "../packages/b/src/index";
 async function display() {
-    const displayA = await import("../packages/a/src/index.ts").then((c)=>c.displayA);
+    const displayA = await import("../packages/a/src/index").then((c)=>c.displayA);
     console.log(displayA());
     console.log(displayB());
 }
