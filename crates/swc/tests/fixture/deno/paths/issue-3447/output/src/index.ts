4ea3dc8950a783cd88304ebb0f924d6ef2b89176
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-16 13:13:33 +0900

    Update test refs

--- crates/swc/tests/fixture/deno/paths/issue-3447/output/src/index.ts
+++ crates/swc/tests/fixture/deno/paths/issue-3447/output/src/index.ts
@@ -1,3 +1,3 @@
-import { sum } from "./modules/01-moduleA/utils.ts";
+import { sum } from "./modules/01-moduleA/utils";
 export { multiply } from "./modules/03-moduleB/utils";
 console.log(sum(1, 2));
