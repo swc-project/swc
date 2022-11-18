15ad2c256880bea01931203ec7adfa61c9ca13be
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-18 16:53:13 +0900

    fix(es/minifier): Abort inliner on fn declarations used multiple time (#6473)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/6463.

--- crates/swc_ecma_minifier/src/compress/optimize/inline.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -184,7 +184,19 @@ where
                         .data
                         .vars
                         .get(&id.to_id())
-                        .filter(|a| !a.reassigned() && a.declared)
+                        .filter(|a| {
+                            !a.reassigned() && a.declared && {
+                                // Function declarations are hoisted
+                                //
+                                // As we copy expressions, this can cause a problem.
+                                // See https://github.com/swc-project/swc/issues/6463
+                                //
+                                // We check callee_count of `usage` because we copy simple functions
+                                !a.used_above_decl
+                                    || !a.declared_as_fn_decl
+                                    || usage.callee_count == 0
+                            }
+                        })
                         .is_some(),
 
                     Expr::Lit(lit) => match lit {
