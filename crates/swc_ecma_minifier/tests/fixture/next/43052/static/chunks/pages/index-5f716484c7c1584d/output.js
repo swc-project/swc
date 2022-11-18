bfe8bc422db5dfecebf3e6f98e8a6bf7fc8e11d4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-19 05:34:59 +0900

    Store correct output

---
+++ crates/swc_ecma_minifier/tests/fixture/next/43052/static/chunks/pages/index-5f716484c7c1584d/output.js
@@ -0,0 +1,49 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([
+    [
+        405
+    ],
+    {
+        5557: function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+            (window.__NEXT_P = window.__NEXT_P || []).push([
+                "/",
+                function() {
+                    return __webpack_require__(4369);
+                }
+            ]);
+            if (false) ;
+        },
+        4369: function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+            "use strict";
+            __webpack_require__.r(__webpack_exports__);
+            __webpack_require__.d(__webpack_exports__, {
+                default: function() {
+                    return Home;
+                }
+            });
+            var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+            var _w3ui_react_uploader__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1522);
+            var _w3ui_react_uploader__WEBPACK_IMPORTED_MODULE_1___default = __webpack_require__.n(_w3ui_react_uploader__WEBPACK_IMPORTED_MODULE_1__);
+            function Home() {
+                return (0, react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_w3ui_react_uploader__WEBPACK_IMPORTED_MODULE_1__.UploaderProvider, {
+                    children: "test"
+                });
+            }
+        }
+    },
+    function(__webpack_require__) {
+        var __webpack_exec__ = function(moduleId) {
+            return __webpack_require__(__webpack_require__.s = moduleId);
+        };
+        __webpack_require__.O(0, [
+            610,
+            321,
+            774,
+            888,
+            179
+        ], function() {
+            return __webpack_exec__(5557);
+        });
+        var __webpack_exports__ = __webpack_require__.O();
+        _N_E = __webpack_exports__;
+    }
+]);
