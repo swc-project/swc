f74de71faea9175d27ab332f71970647c0fc5fe9
austaras <austaras@outlook.com>
2022-11-18 09:26:00 +0800

    fix CI

--- crates/swc/tests/fixture/issues-6xxx/6373/output/input.js
+++ crates/swc/tests/fixture/issues-6xxx/6373/output/input.js
@@ -1,10 +1,9 @@
 import _class_call_check from "@swc/helpers/src/_class_call_check.mjs";
-var _brand_check_y = new WeakSet(), _tmp;
 var x = function x() {
     "use strict";
     _class_call_check(this, x);
 };
 var _y = {
     writable: true,
-    value: (_tmp = _brand_check_y.has(x), _brand_check_y.add(x), _tmp)
+    value: x === x
 };
