f023c88595edca58fa66b5b6e5d241739d3be866
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-18 10:42:11 +0900

    fixup

--- crates/swc_ecma_transforms_module/src/path.rs
+++ crates/swc_ecma_transforms_module/src/path.rs
@@ -121,7 +121,7 @@ where
 
             if let Some(orig_ext) = orig_ext {
                 let use_orig = if let Some(ext) = p.extension() {
-                    (ext == "ts" || ext == "tsx")
+                    ext == "ts" || ext == "tsx"
                 } else {
                     false
                 };
