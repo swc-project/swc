fff7d97d7fd5ee3e5961fd6600a66c4dcf837bb2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-20 15:57:53 +0900

    Fix

--- scripts/cargo/patch-section.sh
+++ scripts/cargo/patch-section.sh
@@ -5,11 +5,8 @@ set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function toLine {
-    # >&2 echo "toLine: $@"
-    arr=(${1// / })
-    # >&2 echo "arr: ${arr[0]} ${arr[1]}"
-    dir="$(dirname ${arr[1]})"
-    echo "${arr[0]} = { path = '$dir' }"
+    dir="$(dirname $2)"
+    echo "$1 = { path = '$dir' }"
 }
 
 export -f toLine
