75c4d22bb63f50e4bf6bc1020121921c939a5a1c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-23 10:04:58 +0900

    chore: Publish crates

--- scripts/cargo/cli.sh
+++ scripts/cargo/cli.sh
@@ -4,9 +4,9 @@ set -eu
 git pull || true
 
 yarn changelog
-(cd ./bindings && cargo mono bump -i)
+(cd ./bindings && cargo mono bump swc_cli)
 # Ensure that Cargo.lock is up-to-date
-cargo metadata --format-version 1 > /dev/null
+(cd ./bindings && cargo metadata --format-version 1 > /dev/null)
 
 # Ensure that dependencies are all verified
 ./scripts/crev/verify.sh
@@ -14,4 +14,4 @@ cargo metadata --format-version 1 > /dev/null
 git add -A
 git commit -m 'chore: Publish crates'
 git push --no-verify
-cargo mono publish --no-verify
+(cd ./bindings && cargo mono publish --no-verify)
