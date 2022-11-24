d9aff2a37de956a96dbf086635c3bea4d5e5864d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-24 23:02:37 +0900

    Add tests

---
+++ crates/swc_ecma_minifier/tests/fixture/next/react-joyride/static/UuArcfoI4tK5u5juVy4X2/_buildManifest/input.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002F511-0c0a027c6638cdca.js","static\u002Fchunks\u002Fpages\u002Findex-e0f6e6942d76fb50.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c8b0663085900c58.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
