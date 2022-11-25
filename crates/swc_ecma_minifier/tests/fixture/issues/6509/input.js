b28f16fed93ebc0c0ba7dd8c40ea7dbc02fafc13
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 13:22:55 +0900

    REduce

--- crates/swc_ecma_minifier/tests/fixture/issues/6509/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/6509/input.js
@@ -103,55 +103,10 @@ function offset(data, _ref) {
 
 
 export var modifiers = {
-
-  /**
-   * The `offset` modifier can shift your popper on both its axis.
-   *
-   * It accepts the following units:
-   * - `px` or unit-less, interpreted as pixels
-   * - `%` or `%r`, percentage relative to the length of the reference element
-   * - `%p`, percentage relative to the length of the popper element
-   * - `vw`, CSS viewport width unit
-   * - `vh`, CSS viewport height unit
-   *
-   * For length is intended the main axis relative to the placement of the popper.<br />
-   * This means that if the placement is `top` or `bottom`, the length will be the
-   * `width`. In case of `left` or `right`, it will be the `height`.
-   *
-   * You can provide a single value (as `Number` or `String`), or a pair of values
-   * as `String` divided by a comma or one (or more) white spaces.<br />
-   * The latter is a deprecated method because it leads to confusion and will be
-   * removed in v2.<br />
-   * Additionally, it accepts additions and subtractions between different units.
-   * Note that multiplications and divisions aren't supported.
-   *
-   * Valid examples are:
-   * ```
-   * 10
-   * '10%'
-   * '10, 10'
-   * '10%, 10'
-   * '10 + 10%'
-   * '10 - 5vh + 3%'
-   * '-10px + 5vh, 5px - 6%'
-   * ```
-   * > **NB**: If you desire to apply offsets to your poppers in a way that may make them overlap
-   * > with their reference element, unfortunately, you will have to disable the `flip` modifier.
-   * > You can read more on this at this [issue](https://github.com/FezVrasta/popper.js/issues/373).
-   *
-   * @memberof modifiers
-   * @inner
-   */
   offset: {
-    /** @prop {number} order=200 - Index used to define the order of execution */
     order: 200,
-    /** @prop {Boolean} enabled=true - Whether the modifier is enabled or not */
     enabled: true,
-    /** @prop {ModifierFn} */
     fn: offset,
-    /** @prop {Number|String} offset=0
-     * The offset value as described in the modifier description
-     */
     offset: 0
   },
 };
