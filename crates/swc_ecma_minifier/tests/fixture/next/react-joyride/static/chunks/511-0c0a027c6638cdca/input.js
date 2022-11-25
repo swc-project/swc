aaff63374a0d867b4b2d2a005ee32f3a0318bda7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 13:19:28 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/next/react-joyride/static/chunks/511-0c0a027c6638cdca/input.js
+++ crates/swc_ecma_minifier/tests/fixture/next/react-joyride/static/chunks/511-0c0a027c6638cdca/input.js
@@ -2022,8 +2022,6 @@
        */
       var placements = ['auto-start', 'auto', 'auto-end', 'top-start', 'top', 'top-end', 'right-start', 'right', 'right-end', 'bottom-end', 'bottom', 'bottom-start', 'left-end', 'left', 'left-start'];
 
-      // Get rid of `auto` `auto-start` and `auto-end`
-      var validPlacements = placements.slice(3);
 
 
 
