cdc1e1a5a3d1fbf6acd6beac17f6c197c00d3d33
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-11-26 09:42:32 -0800

    ci(publish): trying to fix upload cli binaries

--- scripts/cli_upload_gh_release.sh
+++ scripts/cli_upload_gh_release.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # Naive substitution to napi artifacts for the cli binary.
-for filename in artifacts/*/*.node
+for filename in artifacts_cli/*/*
 do
+  echo "Trying to upload $filename"
+
   BINDING_NAME=${filename#*.}
   BINDING_ABI=${BINDING_NAME%%.*}
   CLI_BINARY_PATH=${filename%%.*}
 
   if [ -f "$CLI_BINARY_PATH" ]; then
       chmod +x $CLI_BINARY_PATH
-      gh release upload $RELEASE_VERSION ./scripts/npm/$BINDING_ABI/$CLI_BINARY_PATH
+      gh release upload $RELEASE_VERSION $CLI_BINARY_PATH
   elif [ -f "$CLI_BINARY_PATH.exe" ]; then
-      gh release upload $RELEASE_VERSION ./scripts/npm/$BINDING_ABI/$CLI_BINARY_PATH/$CLI_BINARY_PATH.exe
+      gh release upload $RELEASE_VERSION $CLI_BINARY_PATH.exe
   fi
 done
