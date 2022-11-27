e04a6ad1e498d68604572c30977f9b5edecad1f9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-27 22:45:00 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-6xxx/6508/2/output/index.js
@@ -0,0 +1,16 @@
+"use client";
+import _class_call_check from "@swc/helpers/src/_class_call_check.mjs";
+import _create_class from "@swc/helpers/src/_create_class.mjs";
+export var Foo = /*#__PURE__*/ function() {
+    "use strict";
+    function Foo() {
+        _class_call_check(this, Foo);
+    }
+    _create_class(Foo, [
+        {
+            key: "foo",
+            value: function foo() {}
+        }
+    ]);
+    return Foo;
+}();
