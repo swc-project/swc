5098fc91d93108f167fe91a935625d6d8554699d
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-28 12:02:17 +0900

    Update test refs

---
+++ crates/swc_ecma_minifier/tests/pass-1/joda/2/output.js
@@ -0,0 +1,17 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [
+        715
+    ],
+    {
+        3266: function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+            __webpack_require__.d(__webpack_exports__, {
+                h: function() {
+                    return LocalDate;
+                }
+            });
+            var isInit = !1;
+            isInit = !0;
+        }
+    }
+]);
