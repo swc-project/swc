5cdf685c687c8cf9aece9fa622db87e8517df764
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-28 11:44:02 +0900

    Add tests

---
+++ crates/swc_ecma_minifier/tests/fixture/next/joda/static/chunks/pages/index-6950d80ca82b03bf/input.js
@@ -0,0 +1,47 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[405],{
+
+/***/ 8312:
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/",
+      function () {
+        return __webpack_require__(4186);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ }),
+
+/***/ 4186:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Home; }
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var _js_joda_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3266);
+
+
+function Home() {
+    const localDate = _js_joda_core__WEBPACK_IMPORTED_MODULE_1__/* .LocalDate.now */ .h.now();
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
+        children: localDate.toString()
+    });
+}
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, [715,774,888,179], function() { return __webpack_exec__(8312); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
