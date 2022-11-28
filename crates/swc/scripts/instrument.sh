0e3eb907b5bf76e5b04a38401a1323684ce4951b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-28 16:05:07 +0900

    instrument.sh

--- crates/swc/scripts/instrument.sh
+++ crates/swc/scripts/instrument.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo profile instruments --release -t time --bench typescript --features tracing/release_max_level_info -- --bench $@
+export RUST_LOG=off
+
+cargo profile instruments --release -t time --bench minify --features tracing/release_max_level_info -- --bench $@
 # MINIFY=1 cargo profile instruments --release -t time --bench typescript --features concurrent,tracing/release_max_level_info $@
