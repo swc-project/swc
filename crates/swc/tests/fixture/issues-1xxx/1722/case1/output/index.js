c0ac3beccc3272187bd46440315719b698b21250
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-30 15:36:50 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-1xxx/1722/case1/output/index.js
+++ crates/swc/tests/fixture/issues-1xxx/1722/case1/output/index.js
@@ -1,16 +1,3 @@
-import _async_to_generator from "@swc/helpers/src/_async_to_generator.mjs";
-import _ts_generator from "@swc/helpers/src/_ts_generator.mjs";
-(function() {
-    var _main = _async_to_generator(function() {
-        return _ts_generator(this, function(_state) {
-            console.log(1);
-            return [
-                2
-            ];
-        });
-    });
-    function main() {
-        return _main.apply(this, arguments);
-    }
-    return main;
-})()();
+(async function main() {
+    console.log(1);
+})();
