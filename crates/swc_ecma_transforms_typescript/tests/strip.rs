3592e55139d3c4c9e4197573e09bf3f8a9fd30c3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-30 14:45:42 +0900

    Migrate

--- crates/swc_ecma_transforms_typescript/tests/strip.rs
+++ crates/swc_ecma_transforms_typescript/tests/strip.rs
@@ -96,53 +96,6 @@ test!(
 // TODO: Test function / variable hoisting
 
 to!(
-    issue_236,
-    "function foo(this: any, $scope: angular.IScope){}",
-    "function foo($scope){}"
-);
-
-to!(
-    issue_357,
-    "export function addProp<T, K extends string, V>(
-  obj: T,
-  prop: K,
-  value: V
-): T & { [x in K]: V };
-export function addProp<T, K extends string, V>(
-  prop: K,
-  value: V
-): (obj: T) => T & { [x in K]: V };
-
-export function addProp(arg1: any, arg2: any, arg3?: any): any {
-  if (arguments.length === 2) {
-    return (object: any) => _addProp(object, arg1, arg2);
-  }
-  return _addProp(arg1, arg2, arg3);
-}
-
-function _addProp(obj: any, prop: string, value: any) {
-  return {
-    ...obj,
-    [prop]: value,
-  };
-}",
-    "export function addProp(arg1, arg2, arg3) {
-  if (arguments.length === 2) {
-    return (object) => _addProp(object, arg1, arg2);
-  }
-  return _addProp(arg1, arg2, arg3);
-}
-
-function _addProp(obj, prop, value) {
-  return {
-    ...obj,
-    [prop]: value,
-  };
-}
-"
-);
-
-to!(
     issue_366_01,
     "
 class App {
