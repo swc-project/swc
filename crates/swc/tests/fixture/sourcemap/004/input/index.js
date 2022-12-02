d02f8a7d2ccb0df07fa8d28bfc0da32d41ba99d3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-02 12:35:49 +0900

    input.js

--- crates/swc/tests/fixture/sourcemap/004/input/index.js
+++ crates/swc/tests/fixture/sourcemap/004/input/index.js
@@ -1,51 +1,42 @@
-(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
     [
         158
     ],
     {
-        /***/ 2943: /***/ function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+        2943: function(n, t, u) {
             "use strict";
-            var StaticPage = function StaticPage(_ref) {
-                var data = _ref.data;
-                return /*#__PURE__*/ (0, react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
-                    children: data.foo
+            var r = function(n) {
+                var t = n.data;
+                return (0, _.jsx)("div", {
+                    children: t.foo
                 });
             };
-            __webpack_require__.r(__webpack_exports__);
-            /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-                /* harmony export */ __N_SSG: function __N_SSG1() {
-                    return /* binding */ __N_SSG;
+            u.r(t), u.d(t, {
+                __N_SSG: function() {
+                    return i;
                 },
-                /* harmony export */ default: function _default() {
-                    return /* binding */ StaticPage;
+                default: function() {
+                    return r;
                 }
             });
-            /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4512);
-            var __N_SSG = true;
-        /***/ },
-        /***/ 7139: /***/ function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+            var _ = u(4512), i = !0;
+        },
+        7139: function(n, t, u) {
             (window.__NEXT_P = window.__NEXT_P || []).push([
                 "/static",
                 function() {
-                    return __webpack_require__(2943);
+                    return u(2943);
                 }
             ]);
-            if (false) {}
-        /***/ }
+        }
     },
-    /******/ function(__webpack_require__) {
-        // webpackRuntimeModules
-        /******/ var __webpack_exec__ = function __webpack_exec__(moduleId) {
-            return __webpack_require__(__webpack_require__.s = moduleId);
-        };
-        /******/ __webpack_require__.O(0, [
+    function(n) {
+        n.O(0, [
             774,
             888,
             179
         ], function() {
-            return __webpack_exec__(7139);
-        });
-        /******/ var __webpack_exports__ = __webpack_require__.O();
-        /******/ _N_E = __webpack_exports__;
-    /******/ }
+            return n(n.s = 7139);
+        }), _N_E = n.O();
+    }
 ]);
