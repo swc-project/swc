d8adc580110e0764686f5aea848425905605daa0
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-06 21:24:55 +0900

    chore(ci): Fix benchmark action (#6582)

--- crates/swc_estree_compat/benches/babelify.rs
+++ crates/swc_estree_compat/benches/babelify.rs
@@ -2,7 +2,9 @@ use std::{io::stderr, sync::Arc};
 
 use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
 use swc::config::IsModule;
-use swc_common::{errors::Handler, FileName, FilePathMapping, Mark, SourceFile, SourceMap};
+use swc_common::{
+    errors::Handler, FileName, FilePathMapping, Mark, SourceFile, SourceMap, GLOBALS,
+};
 use swc_ecma_ast::{EsVersion, Program};
 use swc_ecma_parser::Syntax;
 use swc_ecma_transforms::{compat::es2020, resolver, typescript};
@@ -41,47 +43,51 @@ fn parse(c: &swc::Compiler, src: &str) -> (Arc<SourceFile>, Program) {
 }
 
 fn babelify_only(b: &mut Bencher) {
-    let c = mk();
-    let (fm, module) = parse(&c, SOURCE);
-    let handler = Handler::with_emitter_writer(Box::new(stderr()), Some(c.cm.clone()));
+    GLOBALS.set(&Default::default(), || {
+        let c = mk();
+        let (fm, module) = parse(&c, SOURCE);
+        let handler = Handler::with_emitter_writer(Box::new(stderr()), Some(c.cm.clone()));
 
-    let module = c.run_transform(&handler, false, || {
-        let unresolved_mark = Mark::new();
-        let top_level_mark = Mark::new();
+        let module = c.run_transform(&handler, false, || {
+            let unresolved_mark = Mark::new();
+            let top_level_mark = Mark::new();
 
-        module
-            .fold_with(&mut resolver(unresolved_mark, top_level_mark, true))
-            .fold_with(&mut typescript::strip(top_level_mark))
-            .fold_with(&mut es2020(Default::default()))
-    });
+            module
+                .fold_with(&mut resolver(unresolved_mark, top_level_mark, true))
+                .fold_with(&mut typescript::strip(top_level_mark))
+                .fold_with(&mut es2020(Default::default()))
+        });
 
-    b.iter(|| {
-        let program = module.clone();
-        let ctx = Context {
-            fm: fm.clone(),
-            cm: c.cm.clone(),
-            comments: c.comments().clone(),
-        };
+        b.iter(|| {
+            let program = module.clone();
+            let ctx = Context {
+                fm: fm.clone(),
+                cm: c.cm.clone(),
+                comments: c.comments().clone(),
+            };
 
-        let babel_ast = program.babelify(&ctx);
-        black_box(babel_ast)
-    });
+            let babel_ast = program.babelify(&ctx);
+            black_box(babel_ast)
+        });
+    })
 }
 
 fn parse_and_babelify(b: &mut Bencher, _name: &str, src: &str) {
-    let c = mk();
+    GLOBALS.set(&Default::default(), || {
+        let c = mk();
 
-    b.iter(|| {
-        let (fm, program) = parse(&c, src);
-        let ctx = Context {
-            fm,
-            cm: c.cm.clone(),
-            comments: c.comments().clone(),
-        };
+        b.iter(|| {
+            let (fm, program) = parse(&c, src);
+            let ctx = Context {
+                fm,
+                cm: c.cm.clone(),
+                comments: c.comments().clone(),
+            };
 
-        let babel_ast = program.babelify(&ctx);
-        black_box(babel_ast);
-    });
+            let babel_ast = program.babelify(&ctx);
+            black_box(babel_ast);
+        });
+    })
 }
 
 fn bench_cases(c: &mut Criterion) {
