ad95ead291640b1ada65860af1adf5e8185c80a4
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2022-12-06 03:50:26 +0300

    fix(es/parser): Fix parsing of JSX with ASI (#6577)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/6522.

---
+++ crates/swc_ecma_parser/tests/jsx/basic/issue-6522/input.js
@@ -0,0 +1,32 @@
+let x
+<Comp></Comp>
+
+let x
+
+<Comp></Comp>
+
+let x;
+<Comp></Comp>
+
+var x
+<Comp></Comp>
+
+var x;
+<Comp></Comp>
+
+function x() {
+    let x
+    <div />
+}
+
+{ foo: 'test' }
+<Comp></Comp>
+
+function test1() {}
+<Comp></Comp>
+
+class Foo {}
+<>
+<Comp></Comp>
+<Comp></Comp>
+</>
