84ce4a5e03d3aa17221d50d2c0ad3d5630dd1ed7
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-09 13:37:21 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/next/43052/input.js
+++ crates/swc_ecma_minifier/tests/fixture/next/43052/input.js
@@ -24,16 +24,6 @@
 					var index_production = {
 						exports: {}
 					};
-					/**
-					 * uploader-core
-					 *
-					 * Copyright (c) Web3.Storage
-					 *
-					 * This source code is licensed under Apache-2.0 OR MIT license found in the
-					 * LICENSE.md file in the root directory of this source tree.
-					 *
-					 * @license Apache-2.0 OR MIT
-					 */
 					(function (module, exports) {
 						var t;
 						t = function (exports) {
@@ -45,30 +35,13 @@
 								return null
 							}
 
-							async function* chunkBlocks(e, t = {}) { }
-
-							async function uploadCarChunks(e, t, r = {}) { }
-							async function createUpload(e, t, r) { }
-							async function uploadCarBytes(e, t, r = {}) {
-
-							}
-							exports.chunkBlocks = chunkBlocks, exports.createUpload = createUpload, exports.encodeDirectory = encodeDirectory, exports.encodeFile = encodeFile, exports.uploadCarBytes = uploadCarBytes, exports.uploadCarChunks = uploadCarChunks, Object.defineProperty(exports, "__esModule", {
+							Object.defineProperty(exports, "__esModule", {
 								value: !0
 							})
 						}, t(exports)
 					})(index_production, index_production.exports);
-					const UploaderContext = React.createContext([{
-						uploadedCarChunks: []
-					}, {}]);
 
-					function UploaderProvider({ }) {
-
-					}
-
-					function useUploader() {
-						return React.useContext(UploaderContext)
-					}
-					exports.UploaderProvider = UploaderProvider, exports.chunkBlocks = index_production.exports.chunkBlocks, exports.encodeDirectory = index_production.exports.encodeDirectory, exports.encodeFile = index_production.exports.encodeFile, exports.useUploader = useUploader, Object.defineProperty(exports, "__esModule", {
+					exports.chunkBlocks = index_production.exports.chunkBlocks, exports.encodeDirectory = index_production.exports.encodeDirectory, exports.encodeFile = index_production.exports.encodeFile, Object.defineProperty(exports, "__esModule", {
 						value: !0
 					})
 				}));
