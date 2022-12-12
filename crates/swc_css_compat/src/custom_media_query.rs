1f8033ca64ebd67aa67aab7c4f3a3e345ac3a354
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 12:20:13 +0900

    custom media

--- crates/swc_css_compat/src/custom_media_query.rs
+++ crates/swc_css_compat/src/custom_media_query.rs
@@ -1,4 +1,5 @@
-use swc_css_visit::VisitMut;
+use swc_css_ast::AtRule;
+use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub fn custom_media_query() -> impl VisitMut {
     CustomMediaQuery {}
@@ -6,4 +7,12 @@ pub fn custom_media_query() -> impl VisitMut {
 
 struct CustomMediaQuery {}
 
-impl VisitMut for CustomMediaQuery {}
+impl VisitMut for CustomMediaQuery {
+    fn visit_mut_at_rule(&mut self, n: &mut AtRule) {
+        n.visit_mut_children_with(self);
+
+        if n.name == *"custom-media" {
+            dbg!(n);
+        }
+    }
+}
