b643a9e54181ce48a5e33ce3d6be3655981b2b90
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 12:48:22 +0900

    take

--- crates/swc_css_compat/src/custom_media_query.rs
+++ crates/swc_css_compat/src/custom_media_query.rs
@@ -1,4 +1,5 @@
-use swc_css_ast::{AtRule, AtRulePrelude, CustomMediaQuery};
+use swc_common::util::take::Take;
+use swc_css_ast::{AtRule, AtRulePrelude, CustomMediaQuery, Rule};
 use swc_css_visit::{VisitMut, VisitMutWith};
 
 pub fn custom_media_query() -> impl VisitMut {
@@ -15,9 +16,18 @@ impl VisitMut for CustomMediaQueryTransform {
         n.visit_mut_children_with(self);
 
         if n.name == *"custom-media" {
-            if let Some(box AtRulePrelude::CustomMediaPrelude(prelude)) = &n.prelude {
-                self.medias.push(prelude.clone());
+            if let Some(box AtRulePrelude::CustomMediaPrelude(prelude)) = &mut n.prelude {
+                self.medias.push(prelude.take());
             }
         }
+    }
+
+    fn visit_mut_rules(&mut self, n: &mut Vec<Rule>) {
+        n.visit_mut_children_with(self);
+
+        n.retain(|n| match n {
+            Rule::AtRule(n) => n.name != *"custom-media",
+            _ => true,
+        });
     }
 }
