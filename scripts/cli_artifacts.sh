81b352d9667c8874b1d9d87a92cca5b8d4c9fd41
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-12-11 19:50:09 -0800

    chore(ci): Adjust upload of cli binaries (#6624)

--- scripts/cli_artifacts.sh
+++ scripts/cli_artifacts.sh
@@ -8,12 +8,17 @@ do
   BINDING_ABI=${BINDING_NAME%%.*}
   CLI_BINARY_PATH=${filename%%.*}
 
+  echo "Preparing build artifacts:"
+  echo "Binding name $BINDING_NAME"
+  echo "Filename $filename"
+  echo "Cli binary path $CLI_BINARY_PATH"
+
   if [ -f "$CLI_BINARY_PATH" ]; then
       chmod +x $CLI_BINARY_PATH
-      cp -v $CLI_BINARY_PATH ./artifacts_cli/$BINDING_ABI
+      cp -v $CLI_BINARY_PATH ./artifacts_cli/swc-$BINDING_ABI
       mv $CLI_BINARY_PATH ./scripts/npm/$BINDING_ABI
   elif [ -f "$CLI_BINARY_PATH.exe" ]; then
-      cp -v $CLI_BINARY_PATH.exe ./artifacts_cli/$BINDING_ABI
+      cp -v $CLI_BINARY_PATH.exe ./artifacts_cli/swc-$BINDING_ABI
       mv $CLI_BINARY_PATH.exe ./scripts/npm/$BINDING_ABI
   fi
 done
