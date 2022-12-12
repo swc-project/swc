ae7e1f664109f44b8e39b949be0a7ab75c592a9e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 16:08:34 +0900

    Try: Pass down `to`

--- crates/swc_css_compat/src/compiler/custom_media.rs
+++ crates/swc_css_compat/src/compiler/custom_media.rs
@@ -32,32 +32,49 @@ impl CustomMediaHandler {
 
     fn process_media_query(&mut self, to: &mut Vec<MediaQuery>, q: &mut MediaQuery) {
         if let Some(cond) = &mut q.condition {
-            self.process_media_condition_type(cond);
+            self.process_media_condition_type(to, cond);
         }
 
         to.push(q.take());
     }
 
-    fn process_media_condition_type(&mut self, cond: &mut MediaConditionType) {
+    fn process_media_condition_type(
+        &mut self,
+        to: &mut Vec<MediaQuery>,
+        cond: &mut MediaConditionType,
+    ) {
         match cond {
-            MediaConditionType::All(cond) => self.process_media_condition(cond),
-            MediaConditionType::WithoutOr(cond) => self.process_media_condition_without_or(cond),
+            MediaConditionType::All(cond) => self.process_media_condition(to, cond),
+            MediaConditionType::WithoutOr(cond) => {
+                self.process_media_condition_without_or(to, cond)
+            }
         }
     }
 
-    fn process_media_condition(&mut self, cond: &mut MediaCondition) {
+    fn process_media_condition(&mut self, to: &mut Vec<MediaQuery>, cond: &mut MediaCondition) {
         for cond in cond.conditions.iter_mut() {
-            self.process_media_condition_all_type(cond);
+            if let Some(added) = self.process_media_condition_all_type(cond) {
+                to.extend(added.queries);
+            }
         }
     }
 
-    fn process_media_condition_without_or(&mut self, cond: &mut MediaConditionWithoutOr) {
+    fn process_media_condition_without_or(
+        &mut self,
+        to: &mut Vec<MediaQuery>,
+        cond: &mut MediaConditionWithoutOr,
+    ) {
         for cond in cond.conditions.iter_mut() {
-            self.process_media_condition_without_or_type(cond);
+            if let Some(added) = self.process_media_condition_without_or_type(cond) {
+                to.extend(added.queries);
+            }
         }
     }
 
-    fn process_media_condition_all_type(&mut self, cond: &mut MediaConditionAllType) {
+    fn process_media_condition_all_type(
+        &mut self,
+        cond: &mut MediaConditionAllType,
+    ) -> Option<MediaQueryList> {
         match cond {
             MediaConditionAllType::Not(cond) => self.process_media_not(cond),
             MediaConditionAllType::And(cond) => self.process_media_and(cond),
@@ -66,7 +83,10 @@ impl CustomMediaHandler {
         }
     }
 
-    fn process_media_condition_without_or_type(&mut self, cond: &mut MediaConditionWithoutOrType) {
+    fn process_media_condition_without_or_type(
+        &mut self,
+        cond: &mut MediaConditionWithoutOrType,
+    ) -> Option<MediaQueryList> {
         match cond {
             MediaConditionWithoutOrType::Not(cond) => self.process_media_not(cond),
             MediaConditionWithoutOrType::And(cond) => self.process_media_and(cond),
@@ -74,13 +94,25 @@ impl CustomMediaHandler {
         }
     }
 
-    fn process_media_not(&mut self, n: &mut MediaNot) {}
+    fn process_media_not(&mut self, n: &mut MediaNot) -> Option<MediaQueryList> {
+        dbg!(&*n);
 
-    fn process_media_and(&mut self, n: &mut MediaAnd) {}
+        None
+    }
 
-    fn process_media_or(&mut self, n: &mut MediaOr) {}
+    fn process_media_and(&mut self, n: &mut MediaAnd) -> Option<MediaQueryList> {
+        dbg!(&*n);
 
-    fn process_media_in_parens(&mut self, n: &mut MediaInParens) {
+        None
+    }
+
+    fn process_media_or(&mut self, n: &mut MediaOr) -> Option<MediaQueryList> {
+        dbg!(&*n);
+
+        None
+    }
+
+    fn process_media_in_parens(&mut self, n: &mut MediaInParens) -> Option<MediaQueryList> {
         if let MediaInParens::Feature(box MediaFeature::Boolean(MediaFeatureBoolean {
             name: MediaFeatureName::Ident(name),
             ..
@@ -93,11 +125,11 @@ impl CustomMediaHandler {
                     CustomMediaQueryMediaType::Ident(media) => {
                         *name = media.clone();
                     }
-                    CustomMediaQueryMediaType::MediaQueryList(_) => {
-                        dbg!(&*n);
-                    }
+                    CustomMediaQueryMediaType::MediaQueryList(q) => return Some(q.clone()),
                 }
             }
         }
+
+        None
     }
 }
