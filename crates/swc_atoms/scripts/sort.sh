4d86d21ebdac285c14ccfe77c82ef2295befb1ac
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 21:44:27 +0900

    Use sory.py

--- crates/swc_atoms/scripts/sort.sh
+++ crates/swc_atoms/scripts/sort.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu
 
-cat words.txt | awk '{$1=$1};1' | uniq | sort > words_sorted.txt
+cat words.txt | awk '{$1=$1};1' | uniq | ./scripts/_/sort.py > words_sorted.txt
 mv words_sorted.txt words.txt
