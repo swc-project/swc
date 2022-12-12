11be78f4aa1e89f82d4d3891c3468bb433f1a119
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 15:49:37 +0900

    More

--- crates/swc_css_compat/src/compiler/custom_media.rs
+++ crates/swc_css_compat/src/compiler/custom_media.rs
@@ -1,5 +1,9 @@
 use swc_common::util::take::Take;
-use swc_css_ast::{AtRule, AtRulePrelude, CustomMediaQuery, MediaQuery, MediaQueryList};
+use swc_css_ast::{
+    AtRule, AtRulePrelude, CustomMediaQuery, MediaCondition, MediaConditionAllType,
+    MediaConditionType, MediaConditionWithoutOr, MediaConditionWithoutOrType, MediaQuery,
+    MediaQueryList,
+};
 
 #[derive(Debug, Default)]
 pub(super) struct CustomMediaHandler {
@@ -25,5 +29,46 @@ impl CustomMediaHandler {
         n.queries = new;
     }
 
-    fn process_media_query(&mut self, to: &mut Vec<MediaQuery>, n: &mut MediaQuery) {}
+    fn process_media_query(&mut self, to: &mut Vec<MediaQuery>, q: &mut MediaQuery) {
+        if let Some(cond) = &mut q.condition {
+            self.process_media_condition_type(cond);
+        }
+    }
+
+    fn process_media_condition_type(&mut self, cond: &mut MediaConditionType) {
+        dbg!(&*cond);
+        match cond {
+            MediaConditionType::All(cond) => self.process_media_condition(cond),
+            MediaConditionType::WithoutOr(cond) => self.process_media_condition_without_or(cond),
+        }
+    }
+
+    fn process_media_condition(&mut self, cond: &mut MediaCondition) {
+        for cond in cond.conditions.iter_mut() {
+            self.process_media_condition_all_type(cond);
+        }
+    }
+
+    fn process_media_condition_without_or(&mut self, cond: &mut MediaConditionWithoutOr) {
+        for cond in cond.conditions.iter_mut() {
+            self.process_media_condition_without_or_type(cond);
+        }
+    }
+
+    fn process_media_condition_all_type(&mut self, cond: &mut MediaConditionAllType) {
+        match cond {
+            MediaConditionAllType::Not(cond) => {}
+            MediaConditionAllType::And(cond) => {}
+            MediaConditionAllType::Or(cond) => {}
+            MediaConditionAllType::MediaInParens(cond) => {}
+        }
+    }
+
+    fn process_media_condition_without_or_type(&mut self, cond: &mut MediaConditionWithoutOrType) {
+        match cond {
+            MediaConditionWithoutOrType::Not(cond) => {}
+            MediaConditionWithoutOrType::And(cond) => {}
+            MediaConditionWithoutOrType::MediaInParens(cond) => {}
+        }
+    }
 }
