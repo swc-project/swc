6e79037bb32dccf6254948022d0d9d3680b5940c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-12 13:03:47 +0900

    Remove useless

--- crates/swc_css_compat/tests/custom-media-query/_require.cjs
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-const plugin = require('postcss-custom-media');
-plugin();
-
-assert.ok(plugin.postcss, 'should have "postcss flag"');
-assert.equal(typeof plugin, 'function', 'should return a function');
--- crates/swc_css_compat/tests/custom-media-query/import-media.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	customMedia: {
-		'--mq-a': '(max-width: 30em), (max-height: 30em)',
-		'--not-mq-a': 'not all and (--mq-a)'
-	}
-}
