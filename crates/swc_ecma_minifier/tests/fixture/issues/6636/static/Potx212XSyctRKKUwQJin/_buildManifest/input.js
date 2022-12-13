ec56c8be6bcd462659ca0bd42cb21836f43e9541
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-13 19:51:15 +0900

    Remove

--- crates/swc_ecma_minifier/tests/fixture/issues/6636/static/Potx212XSyctRKKUwQJin/_buildManifest/input.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002F94-cfb59e87e57b535d.js","static\u002Fchunks\u002Fpages\u002Findex-ddba972a3aa5ce15.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1c679e8edaa37665.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
