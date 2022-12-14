5bd5fa0f36227b7bce38f6adfca0f0e96952795c
alexander.akait <sheo13666q@gmail.com>
2022-12-14 21:16:20 +0300

    fix: some bugs

--- crates/swc_css_compat/src/compiler/color_alpha_parameter.rs
+++ crates/swc_css_compat/src/compiler/color_alpha_parameter.rs
@@ -6,9 +6,11 @@ use crate::compiler::{utils::round_alpha, Compiler};
 impl Compiler {
     pub(crate) fn process_color_alpha_parameter(&mut self, n: &mut AbsoluteColorBase) {
         if let AbsoluteColorBase::Function(function) = n {
-            if let Some(ComponentValue::AlphaValue(box alpha_value)) = function.value.last_mut() {
-                let name = function.name.value.to_ascii_lowercase();
+            let name = function.name.value.to_ascii_lowercase();
 
+            if let Some(ComponentValue::AlphaValue(_) | ComponentValue::Function(_)) =
+                function.value.last()
+            {
                 if !matches!(
                     name,
                     js_word!("rgb") | js_word!("rgba") | js_word!("hsl") | js_word!("hsla")
@@ -27,18 +29,17 @@ impl Compiler {
                     }
                     _ => {}
                 }
-
-                if let AlphaValue::Percentage(Percentage {
-                    span,
-                    value: Number { value: a, .. },
-                    ..
-                }) = alpha_value
-                {
-                    *alpha_value = AlphaValue::Number(Number {
-                        span: *span,
-                        value: round_alpha(*a / 100.0),
-                        raw: None,
-                    });
+            } else {
+                match name {
+                    js_word!("rgba") => {
+                        function.name.value = js_word!("rgb");
+                        function.name.raw = None;
+                    }
+                    js_word!("hsla") => {
+                        function.name.value = js_word!("hsl");
+                        function.name.raw = None;
+                    }
+                    _ => {}
                 }
             }
         }
