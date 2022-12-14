8ea3df9f6a4e5c8841293c30ff5f33b0f5b47597
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-14 13:47:55 +0900

    Features::None

--- crates/swc_css_compat/src/feature.rs
+++ crates/swc_css_compat/src/feature.rs
@@ -2,6 +2,7 @@ use bitflags::bitflags;
 
 bitflags! {
     pub struct Features: u64 {
+        const NONE = 0;
         const NESTING = 1 << 0;
         const CUSTOM_MEDIA = 1 << 1;
         const MEDIA_QUERY_RANGES = 1 << 2;
