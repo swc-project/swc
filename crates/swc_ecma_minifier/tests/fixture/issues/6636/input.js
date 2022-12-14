26c0862a7a5a831073c6c25fb15288d53e153b77
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-14 10:48:00 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/issues/6636/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/6636/input.js
@@ -1,23 +1,8 @@
-export function memo(getDeps, fn, opts) {
-  let deps = [];
+export function memo(fn, opts) {
   let result;
   return () => {
-    let depTime;
-    if (opts.key && opts.debug) depTime = Date.now();
-    const newDeps = getDeps();
-    const depsChanged = newDeps.length !== deps.length || newDeps.some((dep, index) => deps[index] !== dep);
-    if (!depsChanged) {
-      return result;
-    }
-    deps = newDeps;
-    let resultTime;
-    if (opts.key && opts.debug) resultTime = Date.now();
     result = fn(...newDeps);
     opts?.onChange?.(result);
-    if (opts.key && opts.debug) {
-      if (opts?.debug()) {
-      }
-    }
     return result;
   };
 }
