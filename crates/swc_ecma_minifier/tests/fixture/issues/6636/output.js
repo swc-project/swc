cef1be23ef7198fbdffbb015abfb8eefe9a0ea88
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-14 10:48:14 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/fixture/issues/6636/output.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/6636/output.js
@@ -1,8 +1,4 @@
-export function memo(getDeps, fn, opts) {
-    let result, deps = [];
-    return ()=>{
-        opts.key && opts.debug && Date.now();
-        const newDeps = getDeps(), depsChanged = newDeps.length !== deps.length || newDeps.some((dep, index)=>deps[index] !== dep);
-        return depsChanged && (deps = newDeps, opts.key && opts.debug && Date.now(), result = fn(...newDeps), opts?.onChange?.(result), opts.key && opts.debug && opts?.debug()), result;
-    };
+export function memo(fn, opts) {
+    let result;
+    return ()=>(result = fn(...newDeps), opts?.onChange?.(result), result);
 }
