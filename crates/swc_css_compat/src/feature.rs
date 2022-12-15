f7bb812616445855b75b795ca1e22203e23cec69
alexander.akait <sheo13666q@gmail.com>
2022-12-15 18:34:43 +0300

    fix: features

--- crates/swc_css_compat/src/feature.rs
+++ crates/swc_css_compat/src/feature.rs
@@ -8,6 +8,6 @@ bitflags! {
         const COLOR_HEX_ALPHA = 1 << 3;
         const COLOR_ALPHA_PARAMETER = 1 << 4;
         const COLOR_SPACE_SEPARATED_PARAMETERS = 1 << 5;
-        const COLOR_LEGACY_RGB_AND_HSL = 1 << 5;
+        const COLOR_LEGACY_RGB_AND_HSL = 1 << 6;
     }
 }
