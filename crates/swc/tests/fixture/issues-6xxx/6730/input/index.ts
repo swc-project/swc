f79b04b50dd0dba7c2cb07416cf029b5c8d3ea75
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-03 13:22:21 +0900

    Reduce input

--- crates/swc/tests/fixture/issues-6xxx/6730/input/index.ts
+++ crates/swc/tests/fixture/issues-6xxx/6730/input/index.ts
@@ -1,12 +1,9 @@
-import { OnResolveArgs, PartialMessage, Plugin, PluginBuild } from 'esbuild';
-import fs from 'fs';
-import path from 'path';
+import { Plugin, PluginBuild } from 'esbuild';
 
 export const styleLoader = (): Plugin => {
     return {
-        name: 'style-loader',
         setup(build: PluginBuild) {
-            build.onLoad({ filter: /.*/, namespace: 'less' }, async (args) => {
+            build.onLoad(async (args) => {
 
             });
         },
