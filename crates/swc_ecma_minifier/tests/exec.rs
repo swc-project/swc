88d40e8ab899976c123b5b5f74f0ad02ec9d7868
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-04 13:31:12 +0900

    fix(es/minifier): Fix usage counter to fix infinite loop (#6744)
    **Description:**
    
    We skip non-computed property names while checking if we can inline an expression.
    
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/6729.

--- crates/swc_ecma_minifier/tests/exec.rs
+++ crates/swc_ecma_minifier/tests/exec.rs
@@ -10407,3 +10407,25 @@ fn issue_6641() {
         "###,
     )
 }
+
+#[test]
+fn issue_6728() {
+    run_default_exec_test(
+        r###"
+        async function foo() {
+            if (undefined_var_1) {
+              let replace;
+            
+              if (undefined_var_2) {
+                replace = 1;
+              } else {
+                replace = 2;
+              }
+            
+              await a({ replace })
+            }
+        }
+        console.log('PASS')
+        "###,
+    )
+}
