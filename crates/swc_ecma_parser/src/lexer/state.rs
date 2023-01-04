65a0d3a3109f3d32818f7bf2c8ebbeb863c62060
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-04 16:27:08 +0900

    fix(es/parser): Fix priority of `>>>` (#6748)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/6739.

--- crates/swc_ecma_parser/src/lexer/state.rs
+++ crates/swc_ecma_parser/src/lexer/state.rs
@@ -329,7 +329,7 @@ impl<'a, I: Input> Iterator for Lexer<'a, I> {
                 if c == '<' {
                     self.input.bump();
                     return Ok(Some(tok!('<')));
-                } else if c == '>' {
+                } else if c == '>' && !self.ctx.prefer_bin_op_over_type_arg_closing {
                     self.input.bump();
                     return Ok(Some(tok!('>')));
                 }
