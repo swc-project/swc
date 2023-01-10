d15c922f90373abbefb44d2198f88791ba9a0fa2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-10 16:05:19 +0900

    Update test refs

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/4386/1/output.js
@@ -0,0 +1,23 @@
+var application;
+(()=>{
+    var __webpack_require__ = {};
+    __webpack_require__.d = (exports, definition)=>{
+        for(var key in definition)__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key) && Object.defineProperty(exports, key, {
+            enumerable: !0,
+            get: definition[key]
+        });
+    }, __webpack_require__.o = (obj, prop)=>Object.prototype.hasOwnProperty.call(obj, prop), __webpack_require__.r = (exports)=>{
+        'undefined' != typeof Symbol && Symbol.toStringTag && Object.defineProperty(exports, Symbol.toStringTag, {
+            value: 'Module'
+        }), Object.defineProperty(exports, '__esModule', {
+            value: !0
+        });
+    };
+    var __webpack_exports__ = {};
+    function bootstrap() {
+        alert();
+    }
+    __webpack_require__.r(__webpack_exports__), __webpack_require__.d(__webpack_exports__, {
+        bootstrap: ()=>bootstrap
+    }), application = __webpack_exports__;
+})();
