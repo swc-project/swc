ed25fe636355228836334d54dd271931d09f7ca7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-11 14:37:32 +0900

    Reduce input

--- crates/swc_ecma_minifier/tests/fixture/issues/4386/1/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/4386/1/input.js
@@ -1,40 +1,24 @@
 var application;
-(()=>{
-    var __webpack_require__ = {};
-    (()=>{
-        __webpack_require__.d = (exports, definition)=>{
-            for(var key in definition){
-                if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-                    Object.defineProperty(exports, key, {
-                        enumerable: true,
-                        get: definition[key]
-                    });
-                }
-            }
-        };
-    })();
-    (()=>{
-        __webpack_require__.o = (obj, prop)=>(Object.prototype.hasOwnProperty.call(obj, prop));
-    })();
-    (()=>{
-        __webpack_require__.r = (exports)=>{
-            if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-                Object.defineProperty(exports, Symbol.toStringTag, {
-                    value: 'Module'
-                });
-            }
-            Object.defineProperty(exports, '__esModule', {
-                value: true
-            });
-        };
-    })();
-    var __webpack_exports__ = {};
-    __webpack_require__.r(__webpack_exports__);
-    __webpack_require__.d(__webpack_exports__, {
-        "bootstrap": ()=>(bootstrap)
-    });
-    function bootstrap() {
-        alert();
-    }
-    application = __webpack_exports__;
+(() => {
+        var __webpack_require__ = {};
+        (() => {
+                __webpack_require__.d = (exports, definition) => {
+                };
+        })();
+        (() => {
+                __webpack_require__.o = (obj, prop) => { };
+        })();
+        (() => {
+                __webpack_require__.r = (exports) => {
+                };
+        })();
+        var __webpack_exports__ = {};
+        __webpack_require__.r(__webpack_exports__);
+        __webpack_require__.d(__webpack_exports__, {
+                "bootstrap": () => (bootstrap)
+        });
+        function bootstrap() {
+                alert();
+        }
+        application = __webpack_exports__;
 })();
--- crates/swc_ecma_minifier/tests/fixture/issues/4386/2/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/4386/2/input.js
@@ -3,29 +3,13 @@ var application;
         var __webpack_require__ = {};
         (() => {
                 __webpack_require__.d = (exports, definition) => {
-                        for (var key in definition) {
-                                if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-                                        Object.defineProperty(exports, key, {
-                                                enumerable: true,
-                                                get: definition[key]
-                                        });
-                                }
-                        }
                 };
         })();
         (() => {
-                __webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop));
+                __webpack_require__.o = (obj, prop) => { };
         })();
         (() => {
                 __webpack_require__.r = (exports) => {
-                        if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-                                Object.defineProperty(exports, Symbol.toStringTag, {
-                                        value: 'Module'
-                                });
-                        }
-                        Object.defineProperty(exports, '__esModule', {
-                                value: true
-                        });
                 };
         })();
         var __webpack_exports__ = {};
