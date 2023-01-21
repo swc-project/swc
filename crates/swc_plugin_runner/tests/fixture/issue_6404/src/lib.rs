c192acb32fc5d2bc8d5e245737bb7a306872206e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-21 16:23:11 +0900

    fixup for fixture

--- crates/swc_plugin_runner/tests/fixture/issue_6404/src/lib.rs
+++ crates/swc_plugin_runner/tests/fixture/issue_6404/src/lib.rs
@@ -1,23 +1,15 @@
 use swc_core::{
-    common::DUMMY_SP,
-    ecma::{ast::*, atoms::*, visit::*},
-    plugin::{
-        errors::HANDLER,
-        metadata::{TransformPluginMetadataContextKind, TransformPluginProgramMetadata},
-        plugin_transform,
-    },
-    quote,
+    common::{BytePos, SourceMapper, Span, SyntaxContext},
+    ecma::ast::*,
+    plugin::{metadata::TransformPluginProgramMetadata, plugin_transform},
 };
 
 #[plugin_transform]
-pub fn process_transform(
-    mut program: Program,
-    metadata: TransformPluginProgramMetadata,
-) -> Program {
+pub fn process_transform(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
     for i in 1..50000 {
         let j: u32 = i;
         println!("i {} j {}", i, j);
-        metadata.source_map.span_to_snippet(Span::new(
+        let _ = metadata.source_map.span_to_snippet(Span::new(
             BytePos(j),
             BytePos(j + 10),
             SyntaxContext::empty(),
