b3928957eac507b09160dc3585f2b8e7771b032c
austaras <austaras@outlook.com>
2023-01-27 20:20:25 +0800

    fix(es/minifier): Track reassign in parent scope

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/6864/output.js
@@ -0,0 +1,22 @@
+export function removeFromMatrix(matrix, id) {
+    var newMatrix, indexOfIdToRemove, row = _.find(matrix, (entry, index)=>{
+        if (_.includes(entry, id)) return indexOfIdToRemove = index, entry;
+    });
+    if (!row) return matrix;
+    if (1 === row.length) {
+        if (2 === (newMatrix = _.without(matrix, row))[0].length) {
+            const remainingEntry = newMatrix[0];
+            newMatrix = [
+                [
+                    remainingEntry[0]
+                ],
+                [
+                    remainingEntry[1]
+                ]
+            ];
+        }
+    } else (newMatrix = [
+        ...matrix
+    ])[indexOfIdToRemove] = _.without(row, id);
+    return newMatrix || matrix;
+}
