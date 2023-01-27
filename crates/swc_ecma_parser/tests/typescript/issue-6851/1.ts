5a1a465190f140f056f6ad5e10adf0a9d47283ae
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-27 10:27:56 +0900

    Reduce

--- crates/swc_ecma_parser/tests/typescript/issue-6851/1.ts
+++ crates/swc_ecma_parser/tests/typescript/issue-6851/1.ts
@@ -1,20 +1,3 @@
-interface AccountType {
-    accType: string
-}
-
-interface Account<T> {
-    accountType: T
-}
-
-interface WithAccountHelpers<T> {
-    accout: T;
-    helpers: string;
-}
-
-const useAccount = <T>(): T => {
-    return {} as T;
-}
-
 export const useProductAccount = useAccount<
     WithAccountHelpers<Account<AccountType>>
 >
