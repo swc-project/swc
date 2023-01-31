0b4c165218400bafe21f018622cdf732c769132f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-31 12:27:23 +0900

    Revert

--- crates/swc_ecma_transforms_compat/src/es2020/opt_chaining.rs
+++ crates/swc_ecma_transforms_compat/src/es2020/opt_chaining.rs
@@ -27,7 +27,6 @@ pub fn optional_chaining(c: Config) -> impl Fold + VisitMut {
 struct OptChaining {
     vars_without_init: Vec<VarDeclarator>,
     vars_with_init: Vec<VarDeclarator>,
-    in_opt_chain: bool,
     c: Config,
 }
 
@@ -149,6 +148,7 @@ impl OptChaining {
     }
 }
 
+#[swc_trace]
 impl OptChaining {
     /// Only called from [VisitMut].
     fn handle_unary(&mut self, e: &mut UnaryExpr) -> Expr {
@@ -243,15 +243,6 @@ impl OptChaining {
     }
 
     fn handle_member(&mut self, e: &mut MemberExpr) -> Result<CondExpr, Expr> {
-        if self.in_opt_chain {
-            let mut opt = OptChainExpr {
-                span: e.span,
-                question_dot_token: DUMMY_SP,
-                base: OptChainBase::Member(e.take()),
-            };
-            return Ok(self.unwrap(&mut opt));
-        }
-
         let obj = match &mut *e.obj {
             Expr::Member(obj) => {
                 let obj = self.handle_member(obj).map(Expr::Cond);
@@ -557,10 +548,7 @@ impl OptChaining {
             }
         };
 
-        let old = self.in_opt_chain;
-        self.in_opt_chain = true;
         base.visit_mut_with(self);
-        self.in_opt_chain = old;
 
         base
     }
