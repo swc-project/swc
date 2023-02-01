1e7e5b8049955194cc68d622dc8344fced4987f7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 13:37:29 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/issue-5622/2/output.ts
@@ -0,0 +1,36 @@
+function left__1(a__2, b__2 = a__2, c__2 = b__2) {
+    a__2;
+    b__2;
+}
+function right__1(a__3 = b__3, b__3 = a__3) {
+    a__3;
+    b__3;
+}
+function right2__1(a__4 = b__4, b__4 = c__4, c__4 = a__4) {
+    a__4;
+    b__4;
+    c__4;
+}
+function inside__1(a__5 = b) {
+    var b__5;
+}
+function outside__1() {
+    var b__7;
+    function inside__7(a__8 = b__7) {
+        var b__8;
+    }
+}
+function defaultArgFunction__1(a__9 = function() {
+    return b__9;
+}, b__9 = 1) {}
+function defaultArgArrow__1(a__10 = ()=>()=>b__10, b__10 = 3) {}
+class C__1 {
+    constructor(a__11 = b, b__11 = 1){}
+    method(a__12 = b__12, b__12 = 1) {}
+}
+var x__1 = (a__13 = b, b__13 = c, c__13 = d)=>{
+    var d__13;
+};
+function f__1(a__14, b__14 = function() {
+    return c__14;
+}, c__14 = b__14()) {}
