922cb65f69fbf67a96c4b61c6be758698125d7fb
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-05 11:53:52 +0900

    Add a test

--- crates/swc_ecma_minifier/tests/exec.rs
+++ crates/swc_ecma_minifier/tests/exec.rs
@@ -10677,3 +10677,47 @@ fn issue_6899_3() {
         "###,
     );
 }
+
+#[test]
+fn issue_6899_4() {
+    run_exec_test(
+        r###"
+        console.log("The result should be 0. And it is:", function(n, e) {
+            var r = Math.abs(n - e);
+        
+            function t(e) {
+                return e < n
+            }
+        
+            function u(n) {
+                return n > e
+            }
+        
+            function c(n) {
+                return t(n) || u(n)
+            }
+            return {
+                length: r,
+                max: e,
+                min: n,
+                constrain: function(r) {
+                    return c(r) ? t(r) ? n : e : r
+                },
+                reachedAny: c,
+                reachedMax: u,
+                reachedMin: t,
+                removeOffset: function(n) {
+                    return r ? n - r * Math.ceil((n - e) / r) : n
+                }
+            }
+        }(0, 3).constrain(-1));
+
+        "###,
+        r###"
+        {
+            "defaults": true
+        }
+        "###,
+        false,
+    );
+}
