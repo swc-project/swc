f30d7c70460ca756f2509de1c48055093076d841
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-07 13:06:52 +0900

    Fix test

--- crates/swc_ecma_parser/tests/js.rs
+++ crates/swc_ecma_parser/tests/js.rs
@@ -146,32 +146,3 @@ where
         res
     })
 }
-
-#[testing::fixture("tests/typescript-errors/**/*.ts")]
-#[testing::fixture("tests/typescript-errors/**/*.tsx")]
-fn errors(file: PathBuf) {
-    let file_name = file.display().to_string();
-
-    {
-        let input = {
-            let mut buf = String::new();
-            File::open(&file).unwrap().read_to_string(&mut buf).unwrap();
-            buf
-        };
-
-        eprintln!(
-            "\n\n========== Running reference test {}\nSource:\n{}\n",
-            file_name, input
-        );
-    }
-
-    let module = with_parser(false, &file, false, |p, _| p.parse_typescript_module());
-
-    let err = module.expect_err("should fail, but parsed as");
-    if err
-        .compare_to_file(format!("{}.swc-stderr", file.display()))
-        .is_err()
-    {
-        panic!()
-    }
-}
