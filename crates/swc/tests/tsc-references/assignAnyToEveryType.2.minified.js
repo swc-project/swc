cb79ee9af5e9ec74db3b52ef34278da4942cb5cc
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-13 13:22:42 +0900

    Update test refs

--- crates/swc/tests/tsc-references/assignAnyToEveryType.2.minified.js
+++ crates/swc/tests/tsc-references/assignAnyToEveryType.2.minified.js
@@ -1,6 +1,9 @@
 //// [assignAnyToEveryType.ts]
 var x, E, M;
 import "@swc/helpers/_/_class_call_check";
+import "@swc/helpers/src/_class_call_check.mjs";
+var E, M;
+import _class_call_check from "@swc/helpers/src/_class_call_check.mjs";
 !function(E) {
     E[E.A = 0] = "A";
-}(E || (E = {})), E.A, (M || (M = {})).foo = 1, M = x;
+}(E || (E = {})), E.A, (M || (M = {})).foo = 1;
--- crates/swc/tests/tsc-references/assignmentCompatWithCallSignatures6.2.minified.js
+++ crates/swc/tests/tsc-references/assignmentCompatWithCallSignatures6.2.minified.js
@@ -4,3 +4,7 @@ import { _ as _class_call_check } from "@swc/helpers/_/_class_call_check";
 import { _ as _inherits } from "@swc/helpers/_/_inherits";
 import { _ as _create_super } from "@swc/helpers/_/_create_super";
 x.a = b, b = x.a, x.a2 = b2, b2 = x.a2, x.a3 = b3, b3 = x.a3, x.a4 = b4, b4 = x.a4, x.a5 = b5, b5 = x.a5, x.a11 = b11, b11 = x.a11, x.a16 = b16, b16 = x.a16;
+import _class_call_check from "@swc/helpers/src/_class_call_check.mjs";
+import _inherits from "@swc/helpers/src/_inherits.mjs";
+import _create_super from "@swc/helpers/src/_create_super.mjs";
+x.a = b, b = x.a, x.a2 = b2, b2 = x.a2, x.a3 = b3, b3 = x.a3, x.a4 = b4, b4 = x.a4, x.a5 = b5, b5 = x.a5, x.a11 = b11, b11 = x.a11, x.a16 = b16, x.a16;
