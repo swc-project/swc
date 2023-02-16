703c325379bb360f5a42f18ca1529084d4a54e6b
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-16 14:08:14 +0900

    WIP: fixup

--- crates/swc_ecma_transforms_proposal/tests/decorators.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorators.rs
@@ -1,10 +1,11 @@
 use std::path::PathBuf;
 
-use swc_common::chain;
+use swc_common::{chain, Mark};
 use swc_ecma_parser::{EsConfig, Syntax, TsConfig};
+use swc_ecma_transforms_base::resolver;
 use swc_ecma_transforms_compat::es2020::export_namespace_from;
 use swc_ecma_transforms_proposal::export_default_from;
-use swc_ecma_transforms_testing::test;
+use swc_ecma_transforms_testing::{test, test_fixture};
 use swc_ecma_visit::Fold;
 
 fn syntax_default() -> Syntax {
@@ -14,12 +15,14 @@ fn syntax_default() -> Syntax {
     })
 }
 
-#[testing::fixture("tests/fixture/decorator/**/exec.ts")]
+#[testing::fixture("tests/decorators/**/exec.js")]
 fn exec(input: PathBuf) {
     exec_inner(input)
 }
 
 fn exec_inner(input: PathBuf) {
+    let code = std::fs::read_to_string(&input).unwrap();
+
     swc_ecma_transforms_testing::exec_tr(
         "decorator",
         Syntax::Typescript(TsConfig {
@@ -32,44 +35,34 @@ fn exec_inner(input: PathBuf) {
 
             chain!(
                 resolver(unresolved_mark, top_level_mark, true),
-                decorators(Config {
-                    legacy: true,
-                    emit_metadata: true,
-                    use_define_for_class_fields: false,
-                }),
-                strip(top_level_mark),
+                decorator_2022_03()
             )
         },
         &code,
     );
 }
 
-#[testing::fixture("tests/fixture/decorator/**/exec.ts")]
+#[testing::fixture("tests/decorators/**/input.js")]
 fn fixture(input: PathBuf) {
     fixture_inner(input)
 }
 
 fn fixture_inner(input: PathBuf) {
-    swc_ecma_transforms_testing::exec_tr(
-        "decorator",
-        Syntax::Typescript(TsConfig {
-            decorators: true,
-            ..Default::default()
-        }),
-        |_t| {
+    let output = input.with_file_name("output.js");
+
+    test_fixture(
+        syntax_default(),
+        &|t| {
             let unresolved_mark = Mark::new();
             let top_level_mark = Mark::new();
 
             chain!(
-                resolver(unresolved_mark, top_level_mark, true),
-                decorators(Config {
-                    legacy: true,
-                    emit_metadata: true,
-                    use_define_for_class_fields: false,
-                }),
-                strip(top_level_mark),
+                resolver(unresolved_mark, top_level_mark, false),
+                decorator_2022_03()
             )
         },
-        &code,
+        &input,
+        &output,
+        Default::default(),
     );
 }
