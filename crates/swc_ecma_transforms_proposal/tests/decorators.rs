1defb8a17959373c4f93e423ac3381d88ea187b5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-16 15:24:28 +0900

    `create_pass`

--- crates/swc_ecma_transforms_proposal/tests/decorators.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorators.rs
@@ -1,10 +1,12 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
+use serde::Deserialize;
 use swc_common::{chain, Mark};
 use swc_ecma_parser::{EsConfig, Syntax, TsConfig};
 use swc_ecma_transforms_base::resolver;
 use swc_ecma_transforms_proposal::decorator_2022_03::decorator_2022_03;
 use swc_ecma_transforms_testing::test_fixture;
+use swc_ecma_visit::Fold;
 
 fn syntax_default() -> Syntax {
     Syntax::Es(EsConfig {
@@ -27,15 +29,7 @@ fn exec_inner(input: PathBuf) {
             decorators: true,
             ..Default::default()
         }),
-        |_t| {
-            let unresolved_mark = Mark::new();
-            let top_level_mark = Mark::new();
-
-            chain!(
-                resolver(unresolved_mark, top_level_mark, true),
-                decorator_2022_03()
-            )
-        },
+        |_t| create_pass(&input),
         &code,
     );
 }
@@ -50,17 +44,40 @@ fn fixture_inner(input: PathBuf) {
 
     test_fixture(
         syntax_default(),
-        &|_| {
-            let unresolved_mark = Mark::new();
-            let top_level_mark = Mark::new();
-
-            chain!(
-                resolver(unresolved_mark, top_level_mark, false),
-                decorator_2022_03()
-            )
-        },
+        &|_| create_pass(&input),
         &input,
         &output,
         Default::default(),
     );
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+struct Options {}
+
+fn read_options_json(input: &Path) -> Options {
+    let mut options_path = input.to_path_buf();
+    options_path.set_file_name("options.json");
+
+    if !options_path.exists() {
+        // Look for parent directory
+
+        if !options_path.is_absolute() {
+            return read_options_json(&options_path.parent().unwrap());
+        }
+    }
+
+    serde_json::from_str(&std::fs::read_to_string(&options_path).unwrap()).unwrap()
+}
+
+fn create_pass(input: &PathBuf) -> Box<dyn Fold> {
+    let options_json = read_options_json(&input);
+
+    let unresolved_mark = Mark::new();
+    let top_level_mark = Mark::new();
+
+    Box::new(chain!(
+        resolver(unresolved_mark, top_level_mark, false),
+        decorator_2022_03()
+    ))
 }
