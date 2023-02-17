0c7526588929247025b65b1baf8ce3edc4234e1f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-17 14:28:12 +0900

    Add a visitor

--- crates/swc_ecma_transforms_optimization/src/expr_sweeper.rs
+++ crates/swc_ecma_transforms_optimization/src/expr_sweeper.rs
@@ -1,1 +1,13 @@
+use swc_ecma_visit::{noop_visit_mut_type, VisitMut};
 
+/// DCE, but for unused expressions.
+pub fn expr_sweeper() -> impl VisitMut {
+    ExprSweeper::default()
+}
+
+#[derive(Debug, Default)]
+struct ExprSweeper {}
+
+impl VisitMut for ExprSweeper {
+    noop_visit_mut_type!();
+}
