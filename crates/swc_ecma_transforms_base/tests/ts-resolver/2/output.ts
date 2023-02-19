a9899440ddf1069575428deeec611a470efa08bf
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-19 22:36:49 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/2/output.ts
@@ -0,0 +1,13 @@
+interface Foo__1 {
+    (a: string) : void;
+    b__0: string;
+}
+interface Foo__1 {
+    (a: number) : number;
+    c__0: boolean;
+    d__0: {
+        x__0: number;
+        y__0: number;
+    };
+}
+export = Foo__1
