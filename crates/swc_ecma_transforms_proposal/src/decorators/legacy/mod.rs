809c9ed37923e623375a3c59b91f92f4c6b2a39a
magic-akari <akari.ccino@gmail.com>
2023-02-22 02:38:42 +0800

    fix(es/decorator): Preserve class prop order

--- crates/swc_ecma_transforms_proposal/src/decorators/legacy/mod.rs
+++ crates/swc_ecma_transforms_proposal/src/decorators/legacy/mod.rs
@@ -36,6 +36,8 @@ pub(super) fn new(metadata: bool, use_define_for_class_fields: bool) -> TscDecor
 
 pub(super) struct TscDecorator {
     metadata: bool,
+
+    #[allow(dead_code)]
     use_define_for_class_fields: bool,
 
     enums: AHashMap<JsWord, EnumKind>,
@@ -352,28 +354,6 @@ impl VisitMut for TscDecorator {
                     key.as_arg(),
                     undefined(DUMMY_SP).as_arg(),
                 );
-
-                if !self.use_define_for_class_fields && !c.is_static {
-                    if let Some(init) = c.value.take() {
-                        self.constructor_exprs
-                            .push(Box::new(Expr::Assign(AssignExpr {
-                                span: c.span,
-                                op: op!("="),
-                                left: PatOrExpr::Expr(Box::new(Expr::Member(MemberExpr {
-                                    span: DUMMY_SP,
-                                    obj: Box::new(Expr::This(ThisExpr { span: DUMMY_SP })),
-                                    prop: match &c.key {
-                                        PropName::Ident(i) => MemberProp::Ident(i.clone()),
-                                        _ => MemberProp::Computed(ComputedPropName {
-                                            span: DUMMY_SP,
-                                            expr: Box::new(prop_name_to_expr_value(c.key.clone())),
-                                        }),
-                                    },
-                                }))),
-                                right: init,
-                            })));
-                    }
-                }
             }
         }
     }
