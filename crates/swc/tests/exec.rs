003e00b2fd3fe2bf45efac3735a271c3d9de8625
magic-akari <akari.ccino@gmail.com>
2023-02-22 15:51:23 +0800

    chore: update test config

--- crates/swc/tests/exec.rs
+++ crates/swc/tests/exec.rs
@@ -113,6 +113,18 @@ fn init_helpers() -> Arc<PathBuf> {
 }
 
 fn create_matrix(entry: &Path) -> Vec<Options> {
+    // use_define_for_class_fields: false
+    // force to use [[Set]] instead of [[Define]]
+    // EsVersion should be lower than EsVersion::Es2022
+    let force_set_class_field = entry
+        .parent()
+        .map(|parent| parent.join(".swcrc"))
+        .and_then(|path| fs::read_to_string(path).ok())
+        .and_then(|content| serde_json::from_value::<Config>(content.into()).ok())
+        .and_then(|config| config.jsc.transform.into_inner())
+        .map(|c| c.use_define_for_class_fields == false.into())
+        .unwrap_or(false);
+
     [
         EsVersion::Es2022,
         EsVersion::Es2021,
@@ -125,6 +137,7 @@ fn create_matrix(entry: &Path) -> Vec<Options> {
         EsVersion::Es5,
     ]
     .into_iter()
+    .filter(|e| !force_set_class_field || e < &EsVersion::Es2022)
     .matrix(|| {
         let default_es = Syntax::Es(EsConfig {
             ..Default::default()
