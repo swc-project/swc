ed03bd372871ef1743d69cfd9603b04db88c8ecb
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-23 14:21:56 +0900

    More fix

--- crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
+++ crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
@@ -713,6 +713,13 @@ impl VisitMut for TreeShaker {
                                     .as_deref()
                                     .map_or(false, |e| e.may_have_side_effects(&self.expr_ctx))
                         }
+                        ClassMember::AutoAccessor(m) => {
+                            !matches!(m.key, PropName::Computed(..))
+                                && !m
+                                    .value
+                                    .as_deref()
+                                    .map_or(false, |e| e.may_have_side_effects(&self.expr_ctx))
+                        }
 
                         ClassMember::PrivateProp(m) => !m
                             .value
