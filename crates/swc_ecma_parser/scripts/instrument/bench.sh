482866121f6fa6872d6478db5d338f7134be9421
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-02 15:13:17 +0900

    Revert "simd?"
    This reverts commit 380a66dff8fdee6ac6baa2d7f605c4e3ca2f7969.

--- crates/swc_ecma_parser/scripts/instrument/bench.sh
+++ crates/swc_ecma_parser/scripts/instrument/bench.sh
@@ -3,6 +3,5 @@ set -eu
 
 export RUST_LOG=off
 export MIMALLOC_SHOW_STATS=1
-export RUSTFLAGS="-C target-cpu=native"
 
 cargo profile instruments --release -t time --features tracing/release_max_level_info --features swc_common/concurrent --features swc_common/parking_lot --bench parser -- --bench --color $@
