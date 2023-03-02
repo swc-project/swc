5a90fd77a229cd6602f83c17144afe5afd312c34
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-02 15:09:35 +0900

    simd?

--- crates/swc_ecma_parser/scripts/instrument/bench.sh
+++ crates/swc_ecma_parser/scripts/instrument/bench.sh
@@ -3,5 +3,6 @@ set -eu
 
 export RUST_LOG=off
 export MIMALLOC_SHOW_STATS=1
+export RUSTFLAGS="-C target-cpu=native"
 
 cargo profile instruments --release -t time --features tracing/release_max_level_info --features swc_common/concurrent --features swc_common/parking_lot --bench parser -- --bench --color $@
