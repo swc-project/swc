54724078c3421c45f38abb6008c3d90a456e1bc4
nissy-dev <nd.12021218@gmail.com>
2023-03-05 15:36:19 +0900

    fix: update test

---
+++ crates/swc_ecma_preset_env/tests/fixtures/corejs3/usage-array-find-last/input.mjs
@@ -0,0 +1,3 @@
+const array = [1, 2, 3];
+array.findLastIndex();
+array.findLast();
---
+++ crates/swc_ecma_preset_env/tests/fixtures/corejs3/usage-array-find-last/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.array.find-last-index.js";
+import "core-js/modules/es.array.find-last.js";
+var array = [
+    1,
+    2,
+    3
+];
+array.findLastIndex();
+array.findLast();
