4fb147be687cfe5d14e457448bd159924e104c90
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-09 13:32:41 +0900

    More fix

--- crates/swc_node_bundler/src/loaders/json.rs
+++ crates/swc_node_bundler/src/loaders/json.rs
@@ -9,7 +9,7 @@ pub(super) fn load_json_as_module(fm: &Arc<SourceFile>) -> Result<Module, Error>
     let expr = parse_file_as_expr(fm, Syntax::default(), EsVersion::Es2020, None, &mut vec![])
         .map_err(|err| anyhow!("failed parse json as javascript object: {:#?}", err))?;
 
-    let export = ModuleItem::Stmt(Stmt::Expr(ExprStmt {
+    let export = ModuleItem::Stmt(Box::new(Stmt::Expr(ExprStmt {
         span: DUMMY_SP,
         expr: Box::new(Expr::Assign(AssignExpr {
             span: DUMMY_SP,
@@ -21,7 +21,7 @@ pub(super) fn load_json_as_module(fm: &Arc<SourceFile>) -> Result<Module, Error>
             }))),
             right: expr,
         })),
-    }));
+    })));
 
     Ok(Module {
         span: DUMMY_SP,
