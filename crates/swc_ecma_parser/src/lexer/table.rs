528344e734f917b0cdb92ade486c43c6bdbb86f8
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-11 19:24:26 +0900

    More

--- crates/swc_ecma_parser/src/lexer/table.rs
+++ crates/swc_ecma_parser/src/lexer/table.rs
@@ -4,6 +4,8 @@
 //!
 //! https://github.com/ratel-rust/ratel-core/blob/e55a1310ba69a3f5ce2a9a6eef643feced02ac08/ratel/src/lexer/mod.rs#L665
 
+use either::Either;
+
 use super::{LexResult, Lexer};
 use crate::token::Token;
 
@@ -15,7 +17,7 @@ static BYTE_HANDLERS: [ByteHandler; 256] = [
     EOF, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, // 0
     ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, // 1
     ___, EXL, QOT, ERR, IDT, PRC, AMP, QOT, PNO, PNC, ATR, PLS, COM, MIN, PRD, SLH, // 2
-    ZER, DIG, DIG, DIG, DIG, DIG, DIG, DIG, DIG, DIG, COL, SEM, LSS, EQL, MOR, QST, // 3
+    ZER, DI0, DIG, DIG, DIG, DIG, DIG, DIG, DIG, DIG, COL, SEM, LSS, EQL, MOR, QST, // 3
     ERR, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, // 4
     IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, IDT, BTO, IDT, BTC, CRT, IDT, // 5
     TPL, IDT, L_B, L_C, L_D, L_E, L_F, IDT, IDT, L_I, IDT, IDT, L_L, IDT, L_N, IDT, // 6
@@ -38,4 +40,22 @@ const EOF: ByteHandler = Some(|lexer| {
     Ok(None)
 });
 
+/// Identifier
 const IDT: ByteHandler = Some(|lexer| lexer.read_ident_or_keyword().map(Some));
+
+/// `0`
+const DI0: ByteHandler = Some(|lexer| lexer.read_token_zero().map(Some));
+
+/// Numbers
+const DIG: ByteHandler = Some(|lexer| {
+    lexer
+        .read_number(false)
+        .map(|v| match v {
+            Either::Left((value, raw)) => Token::Num { value, raw },
+            Either::Right((value, raw)) => Token::BigInt { value, raw },
+        })
+        .map(Some)
+});
+
+/// String literals with `'` or `"`
+const QOT: ByteHandler = Some(|lexer| self.read_str_lit().map(Some));
