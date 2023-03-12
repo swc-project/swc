66fb01b83fe31329971bc22ef84d27970f40e45b
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-12 12:47:14 +0900

    is_await

--- crates/swc_ecma_quote_macros/src/ast/stmt.rs
+++ crates/swc_ecma_quote_macros/src/ast/stmt.rs
@@ -23,7 +23,7 @@ impl_struct!(WhileStmt, [span, test, body]);
 impl_struct!(DoWhileStmt, [span, test, body]);
 impl_struct!(ForStmt, [span, init, test, update, body]);
 impl_struct!(ForInStmt, [span, left, right, body]);
-impl_struct!(ForOfStmt, [span, await_token, left, right, body]);
+impl_struct!(ForOfStmt, [span, is_await, left, right, body]);
 impl_struct!(ReturnStmt, [span, arg]);
 impl_struct!(ExprStmt, [span, expr]);
 
