f4a3a513f48ac157a3483fb90dc159faebffd196
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-13 11:25:12 +0900

    Fix

--- crates/string_enum/src/lib.rs
+++ crates/string_enum/src/lib.rs
@@ -75,8 +75,12 @@ pub fn derive_string_enum(input: proc_macro::TokenStream) -> proc_macro::TokenSt
     make_serialize(&input).to_tokens(&mut tts);
     make_deserialize(&input).to_tokens(&mut tts);
 
-    derive_fmt(&input, quote_spanned!(call_site() => std::fmt::Debug)).to_tokens(&mut tts);
-    derive_fmt(&input, quote_spanned!(call_site() => std::fmt::Display)).to_tokens(&mut tts);
+    derive_fmt(&input, quote_spanned!(Span::call_site() => std::fmt::Debug)).to_tokens(&mut tts);
+    derive_fmt(
+        &input,
+        quote_spanned!(Span::call_site() => std::fmt::Display),
+    )
+    .to_tokens(&mut tts);
 
     print("derive(StringEnum)", tts)
 }
