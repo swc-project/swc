d9a46402d617b79b1eb00da43215f7963eac1f7c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-15 11:01:55 +0900

    Inject init vars

--- crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
+++ crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
@@ -1,4 +1,6 @@
+use swc_common::{util::take::Take, DUMMY_SP};
 use swc_ecma_ast::*;
+use swc_ecma_utils::private_ident;
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 pub fn decorator_2022_03() -> impl VisitMut + Fold {
@@ -7,7 +9,9 @@ pub fn decorator_2022_03() -> impl VisitMut + Fold {
 
 #[derive(Debug, Default)]
 struct Decorator202203 {
-    stmts_for_constuctor: Vec<Stmt>,
+    /// Variables without initializer.
+    extra_vars: Vec<VarDeclarator>,
+    extra_stmts: Vec<Stmt>,
 }
 
 impl VisitMut for Decorator202203 {
@@ -17,11 +21,52 @@ impl VisitMut for Decorator202203 {
         n.visit_mut_children_with(self);
 
         match n {
-            ClassMember::PrivateProp(p) => {}
+            ClassMember::PrivateProp(p) => {
+                let init = private_ident!(format!("init_{}", p.key.id.sym));
+
+                self.extra_vars.push(VarDeclarator {
+                    span: p.span,
+                    name: Pat::Ident(init.into()),
+                    init: None,
+                    definite: false,
+                });
+            }
 
             ClassMember::PrivateMethod(m) => {}
 
             _ => {}
+        }
+    }
+
+    fn visit_mut_module_items(&mut self, n: &mut Vec<ModuleItem>) {
+        n.visit_mut_children_with(self);
+
+        if !self.extra_vars.is_empty() {
+            self.extra_stmts.push(
+                VarDecl {
+                    span: DUMMY_SP,
+                    kind: VarDeclKind::Var,
+                    decls: self.extra_vars.take(),
+                    declare: false,
+                }
+                .into(),
+            );
+        }
+    }
+
+    fn visit_mut_stmts(&mut self, n: &mut Vec<Stmt>) {
+        n.visit_mut_children_with(self);
+
+        if !self.extra_vars.is_empty() {
+            self.extra_stmts.push(
+                VarDecl {
+                    span: DUMMY_SP,
+                    kind: VarDeclKind::Var,
+                    decls: self.extra_vars.take(),
+                    declare: false,
+                }
+                .into(),
+            );
         }
     }
 }
