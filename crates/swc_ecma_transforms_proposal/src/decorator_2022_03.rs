4ff3af0f32544b697389ffe694c9bfea423554f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-15 10:57:19 +0900

    Some work

--- crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
+++ crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
@@ -1,9 +1,27 @@
-use swc_ecma_visit::{as_folder, Fold, VisitMut};
+use swc_ecma_ast::*;
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 pub fn decorator_2022_03() -> impl VisitMut + Fold {
-    as_folder(Decorator202203 {})
+    as_folder(Decorator202203::default())
 }
 
-struct Decorator202203 {}
+#[derive(Debug, Default)]
+struct Decorator202203 {
+    stmts_for_constuctor: Vec<Stmt>,
+}
 
-impl VisitMut for Decorator202203 {}
+impl VisitMut for Decorator202203 {
+    noop_visit_mut_type!();
+
+    fn visit_mut_class_member(&mut self, n: &mut ClassMember) {
+        n.visit_mut_children_with(self);
+
+        match n {
+            ClassMember::PrivateProp(p) => {}
+
+            ClassMember::PrivateMethod(m) => {}
+
+            _ => {}
+        }
+    }
+}
