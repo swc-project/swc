c3d4ee75d2351252d7abd9e107a9e66f2e0f825f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-22 11:24:47 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-6xxx/6871/output/index.js
@@ -0,0 +1,16 @@
+import _async_to_generator from "@swc/helpers/src/_async_to_generator.mjs";
+function fromTokenizer() {
+    return _fromTokenizer1.apply(this, arguments);
+}
+function _fromTokenizer1() {
+    _fromTokenizer1 = _async_to_generator(function*() {});
+    return _fromTokenizer1.apply(this, arguments);
+}
+function _fromTokenizer() {
+    return __fromTokenizer.apply(this, arguments);
+}
+function __fromTokenizer() {
+    __fromTokenizer = _async_to_generator(function*() {});
+    return __fromTokenizer.apply(this, arguments);
+}
+eval();
