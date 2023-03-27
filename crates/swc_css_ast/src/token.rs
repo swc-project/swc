bc52e3ba23558cb02368bcafc836a2992b601c19
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-27 11:10:42 +0900

    Fix more

--- crates/swc_css_ast/src/token.rs
+++ crates/swc_css_ast/src/token.rs
@@ -45,17 +45,19 @@ pub enum NumberType {
     Number,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, EqIgnoreSpan)]
+#[derive(Debug, Clone, PartialEq, EqIgnoreSpan)]
 #[cfg_attr(
     feature = "rkyv",
     derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)
 )]
+#[cfg_attr(feat = "serde-impl", derive(Serialize, Deserialize))]
 pub struct DimensionToken {
     pub value: f64,
     pub raw_value: Atom,
     #[cfg_attr(feature = "rkyv", with(swc_atoms::EncodeJsWord))]
     pub unit: JsWord,
-    #[serde(rename = "type")]
+
+    #[cfg_attr(feat = "serde-impl", serde(rename = "type"))]
     pub type_flag: NumberType,
     pub raw_unit: Atom,
 }
