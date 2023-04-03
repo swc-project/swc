bfbec2bd75b9f9ba796f3c7d6e54b2a7b67bffe4
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-03 10:01:24 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-setters/public/output.js
+++ crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-setters/public/output.js
@@ -1,18 +1,31 @@
 var _computedKey, _initProto;
-const dec = () => { };
+const dec = ()=>{};
 _computedKey = 'b';
 class Foo {
-  static {
-    [_initProto] = _applyDecs2203R(this, [[dec, 4, "a"], [dec, 4, _computedKey]], []).e;
-  }
-  constructor(...args) {
-    _initProto(this);
-  }
-  value = 1;
-  set a(v) {
-    return this.value = v;
-  }
-  set [_computedKey](v) {
-    return this.value = v;
-  }
+    static{
+        [_initProto] = _applyDecs2203R(this, [
+            [
+                [
+                    dec,
+                    4,
+                    "a"
+                ],
+                [
+                    dec,
+                    4,
+                    _computedKey
+                ]
+            ]
+        ], []).e;
+    }
+    constructor(){
+        _initProto(this);
+    }
+    value = 1;
+    set a(v) {
+        return this.value = v;
+    }
+    set [_computedKey](v) {
+        return this.value = v;
+    }
 }
