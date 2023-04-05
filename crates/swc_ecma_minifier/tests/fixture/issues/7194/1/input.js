73bc29eeb0e0a758daff2ec9567d8949fb48fa50
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-05 13:35:37 +0900

    fix(es/minifier): Don't remove used var decl (#7200)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/7194.

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7194/1/input.js
@@ -0,0 +1,11 @@
+function example() {
+    var MyEnum = /*#__PURE__*/function (MyEnumInner) {
+        MyEnumInner["First"] = "first";
+        MyEnumInner["Second"] = "second";
+        return MyEnumInner;
+    }(MyEnum || {});
+
+    return MyEnum
+}
+
+example()
