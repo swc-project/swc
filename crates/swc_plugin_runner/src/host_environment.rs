56bdacc72df967bc613c5b067cf773f39c4bce00
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-04-09 20:40:25 -0700

    feat(plugin/runner): Update `wasmer` to `v3` (#7197)
    **Description:**
    
    This PR reattempts https://github.com/swc-project/swc/pull/5456.
    
    Most of the changes are for the breaking changes of wasmer@3, as well as enabling rkyv's strict mode (https://github.com/swc-project/swc/pull/6922). This could not be seperated since wasmer@3 enables strict mode by default without a way to turn it off.
    
    There are a couple of changes worth noting:
    
    - Disabling in-memory module lookup: https://github.com/swc-project/swc/pull/7197/files#diff-3bda5def6ce2b7553c3b3a5ad241c0bdb7021e67b7de1e594df4cd5a54d403b3R154-R159
    - Disabling plugin_runner in bindings_wasm: https://github.com/swc-project/swc/pull/7197/files#diff-dc3ded556a1fd709a129acd588e5eda651b842c6acc3f5340d40088a1f927facR310-R312
    - Skipping plugin compat test: https://github.com/swc-project/swc/pull/7197/files#diff-531197dfcefba05faca53f0cf442ecc2dc6b59d5ead01979f5ffb912aa36249aR64-R66

--- crates/swc_plugin_runner/src/host_environment.rs
+++ crates/swc_plugin_runner/src/host_environment.rs
@@ -1,25 +1,22 @@
-use wasmer::{LazyInit, Memory};
+use wasmer::Memory;
 
 /// An external environment state imported (declared in host, injected into
 /// guest) fn can access. This'll allow host to read from updated state from
 /// guest.
 ///
-/// This is `base` environment exposes guest's memory space only. For other
+/// This is `base` environment exposes nothing. For other
 /// calls requires additional data to be set in the host, separate
 /// hostenvironments are declared. Refer `CommentsHostEnvironment` for an
 /// example.
 ///
 /// ref: https://docs.wasmer.io/integrations/examples/host-functions#declaring-the-data
-#[derive(wasmer::WasmerEnv, Clone)]
+#[derive(Clone)]
 pub struct BaseHostEnvironment {
-    #[wasmer(export)]
-    pub memory: wasmer::LazyInit<Memory>,
+    pub memory: Option<Memory>,
 }
 
 impl BaseHostEnvironment {
     pub fn new() -> BaseHostEnvironment {
-        BaseHostEnvironment {
-            memory: LazyInit::new(),
-        }
+        BaseHostEnvironment { memory: None }
     }
 }
