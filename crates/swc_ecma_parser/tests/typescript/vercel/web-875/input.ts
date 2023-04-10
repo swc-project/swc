68f178b15271458666d6b913fee38cd866845b4e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-10 14:51:03 +0900

    Reduce

--- crates/swc_ecma_parser/tests/typescript/vercel/web-875/input.ts
+++ crates/swc_ecma_parser/tests/typescript/vercel/web-875/input.ts
@@ -17,39 +17,11 @@ export async function postProcessHTML(
         process.env.NEXT_RUNTIME !== 'edge' && renderOpts.optimizeFonts
             ? async (html: string) => {
                 const getFontDefinition = (url: string): string => {
-                    if (renderOpts.fontManifest) {
-                        const { getFontDefinitionFromManifest } =
-                            require('./font-utils') as typeof import('./font-utils')
-                        return getFontDefinitionFromManifest!(
-                            url,
-                            renderOpts.fontManifest
-                        )
-                    }
-                    return ''
                 }
-                return await processHTML(
-                    html,
-                    { getFontDefinition },
-                    {
-                        optimizeFonts: renderOpts.optimizeFonts,
-                    }
-                )
             }
             : null,
         process.env.NEXT_RUNTIME !== 'edge' && renderOpts.optimizeCss
             ? async (html: string) => {
-                // eslint-disable-next-line import/no-extraneous-dependencies
-                const Critters = require('critters')
-                const cssOptimizer = new Critters({
-                    ssrMode: true,
-                    reduceInlineStyles: false,
-                    path: renderOpts.distDir,
-                    publicPath: `${renderOpts.assetPrefix}/_next/`,
-                    preload: 'media',
-                    fonts: false,
-                    ...renderOpts.optimizeCss,
-                })
-                return await cssOptimizer.process(html)
             }
             : null,
         inAmpMode || hybridAmp
