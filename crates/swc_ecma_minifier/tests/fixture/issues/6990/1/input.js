dde7637b887599a3cc302de58179b2a5f20e9e6d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-17 13:13:15 +0900

    Add a test

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/6990/1/input.js
@@ -0,0 +1,11 @@
+(() => {
+    function $parcel$interopDefault(a) {
+        return a && a.__esModule ? a.default : a;
+    }
+    var $074842700de94141$exports = {};
+    $074842700de94141$exports = "some unused data";
+
+
+    let $f61716f0e972bb72$var$value = (0, (/*@__PURE__*/$parcel$interopDefault($074842700de94141$exports)));
+
+})();
