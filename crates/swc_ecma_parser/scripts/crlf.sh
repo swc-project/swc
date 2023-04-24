4d87c2fe4524355faf440fe1ae455c4349096b12
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-24 17:44:33 +0900

    crlf.sh

--- crates/swc_ecma_parser/scripts/crlf.sh
+++ crates/swc_ecma_parser/scripts/crlf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 find tests/tsc -type f -exec ./scripts/dos2unix.sh {} \;
-find tests/typescrit -type f -exec ./scripts/dos2unix.sh {} \;
-find tests/typescrit-errors -type f -exec ./scripts/dos2unix.sh {} \;
+find tests/typescript -type f -exec ./scripts/dos2unix.sh {} \;
+find tests/typescript-errors -type f -exec ./scripts/dos2unix.sh {} \;
 
