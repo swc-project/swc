0ca05d8b69b6301e2346e5de6c45a77c863676e9
David Sherret <dsherret@users.noreply.github.com>
2023-04-24 00:15:08 -0400

    fix(es/codegen): Fix codegen of `TsModuleDecl` (#7319)

---
+++ crates/swc_ecma_codegen/tests/fixture/typescript/module/input.js
@@ -0,0 +1,23 @@
+module Test {
+  1;
+}
+
+export module Test.Test {
+  1;
+}
+
+declare module Test.Test2 {
+  1;
+}
+
+declare global {
+  1;
+}
+
+declare namespace Test1.Test2 {
+  1;
+}
+
+export namespace Test1.Test2.Test3 {
+  2;
+}
