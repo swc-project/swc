1612b483a05148598b988c58a137aa50b3c7998e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-25 11:16:37 +0900

    test config

--- crates/swc_ecma_parser/tests/errors.rs
+++ crates/swc_ecma_parser/tests/errors.rs
@@ -47,6 +47,7 @@ where
     } else {
         ::swc_ecma_parser::Syntax::Es(::swc_ecma_parser::EsConfig {
             jsx: is_jsx,
+            using_decl: true,
             ..Default::default()
         })
     };
