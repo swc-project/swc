246300ae25be0cfdbbb266e02f80d06013a96d85
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-29 22:19:00 +0900

    feat(es/minifier): Drop expressions using sequential inliner (#6936)

--- crates/swc/tests/tsc-references/invalidUndefinedAssignments.2.minified.js
+++ crates/swc/tests/tsc-references/invalidUndefinedAssignments.2.minified.js
@@ -3,4 +3,4 @@ var x, E, M;
 import "@swc/helpers/_/_class_call_check";
 !function(E) {
     E[E.A = 0] = "A";
-}(E || (E = {})), (E = x).A = x, I = x, (M || (M = {})).x = 1, M = x;
+}(E || (E = {})), (E = x).A = x, I = x, (M || (M = {})).x = 1;
--- crates/swc/tests/tsc-references/literalTypes2.2.minified.js
+++ crates/swc/tests/tsc-references/literalTypes2.2.minified.js
@@ -14,11 +14,8 @@ g1(1), g2(1, 1), g2(1, 2), g3(1, "two"), g4(1), g5(1, 2), g6([
     return x;
 }), g8(1, function(x) {
     return x + 1;
-});
-var aa = [
+}), function(a, x) {
+    a.slice().push(1);
+}([
     0
-];
-aa = function(a, x) {
-    var result = a.slice();
-    return result.push(1), result;
-}(aa, 0);
+], 0);
