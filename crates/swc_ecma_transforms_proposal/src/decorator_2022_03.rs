947d1a8d3b48f0896c3dc0e7aaf0149e5bf5382e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-08 17:58:48 +0900

    init name

--- crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
+++ crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
@@ -787,7 +787,7 @@ impl VisitMut for Decorator202203 {
                                 }
                             }
                             Key::Public(k) => {
-                                (name, init) = self.initializer_name(k, "__");
+                                (name, init) = self.initializer_name(k, "init");
 
                                 PrivateName {
                                     span: init.span.with_ctxt(SyntaxContext::empty()),
