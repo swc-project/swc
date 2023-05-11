dffab9f52207d8977a98b58fa83d941c4f8bd493
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-11 12:10:46 +0900

    test_fixture

--- crates/swc_ecma_transforms_proposal/tests/explicit_resource_management.rs
+++ crates/swc_ecma_transforms_proposal/tests/explicit_resource_management.rs
@@ -1,8 +1,37 @@
 use std::path::PathBuf;
 
+use swc_common::{chain, Mark};
+use swc_ecma_parser::{EsConfig, Syntax};
+use swc_ecma_transforms_base::resolver;
+use swc_ecma_transforms_proposal::explicit_resource_management::explicit_resource_management;
+use swc_ecma_transforms_testing::{test_fixture, FixtureTestConfig};
+
 #[testing::fixture("tests/explicit-resource_management/**/input.js")]
 fn fixture(input: PathBuf) {
     run(input);
 }
 
-fn run(input: PathBuf) {}
+fn run(input: PathBuf) {
+    let output = input.with_extension("output.js");
+
+    test_fixture(
+        Syntax::Es(EsConfig {
+            using_decl: true,
+            ..Default::default()
+        }),
+        &|_t| {
+            let unresolved_mark = Mark::new();
+            let top_level_mark = Mark::new();
+
+            chain!(
+                resolver(unresolved_mark, top_level_mark, false),
+                explicit_resource_management()
+            )
+        },
+        &input,
+        &output,
+        FixtureTestConfig {
+            ..Default::default()
+        },
+    );
+}
