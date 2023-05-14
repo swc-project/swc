ec12dc3386471a4e0a089ecd30269320b053b9f7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-14 17:48:35 +0900

    initClass

--- crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
+++ crates/swc_ecma_transforms_proposal/src/decorator_2022_03.rs
@@ -481,54 +481,60 @@ impl Decorator202203 {
 
                 replace_ident(&mut c.class, c.ident.to_id(), &preserved_class_name);
 
-                if let Some(last) = last_static_block {
-                    let constructor = self.ensure_constructor(&mut c.class);
+                let constructor = self.ensure_constructor(&mut c.class);
 
-                    let super_call = CallExpr {
-                        span: DUMMY_SP,
-                        callee: Callee::Super(Super { span: DUMMY_SP }),
-                        args: vec![c.ident.clone().as_arg()],
-                        type_args: Default::default(),
-                    }
-                    .into();
-
-                    let static_call = CallExpr {
-                        span: DUMMY_SP,
-                        callee: ArrowExpr {
-                            span: DUMMY_SP,
-                            params: vec![],
-                            body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
-                                span: DUMMY_SP,
-                                stmts: last,
-                            })),
-                            is_async: false,
-                            is_generator: false,
-                            type_params: Default::default(),
-                            return_type: Default::default(),
-                        }
-                        .as_callee(),
-                        args: vec![],
-                        type_args: Default::default(),
-                    }
-                    .into();
-
-                    let init_class_call = CallExpr {
-                        span: DUMMY_SP,
-                        callee: init_class.clone().as_callee(),
-                        args: Vec::new(),
-                        type_args: Default::default(),
-                    }
-                    .into();
-
-                    constructor.body.as_mut().unwrap().stmts.insert(
-                        0,
-                        SeqExpr {
-                            span: DUMMY_SP,
-                            exprs: vec![super_call, static_call, init_class_call],
-                        }
-                        .into_stmt(),
-                    );
+                let super_call = CallExpr {
+                    span: DUMMY_SP,
+                    callee: Callee::Super(Super { span: DUMMY_SP }),
+                    args: vec![c.ident.clone().as_arg()],
+                    type_args: Default::default(),
                 }
+                .into();
+                let static_call = if let Some(last) = last_static_block {
+                    Some(
+                        CallExpr {
+                            span: DUMMY_SP,
+                            callee: ArrowExpr {
+                                span: DUMMY_SP,
+                                params: vec![],
+                                body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
+                                    span: DUMMY_SP,
+                                    stmts: last,
+                                })),
+                                is_async: false,
+                                is_generator: false,
+                                type_params: Default::default(),
+                                return_type: Default::default(),
+                            }
+                            .as_callee(),
+                            args: vec![],
+                            type_args: Default::default(),
+                        }
+                        .into(),
+                    )
+                } else {
+                    None
+                };
+
+                let init_class_call = CallExpr {
+                    span: DUMMY_SP,
+                    callee: init_class.clone().as_callee(),
+                    args: Vec::new(),
+                    type_args: Default::default(),
+                }
+                .into();
+
+                constructor.body.as_mut().unwrap().stmts.insert(
+                    0,
+                    SeqExpr {
+                        span: DUMMY_SP,
+                        exprs: once(super_call)
+                            .chain(static_call)
+                            .chain(once(init_class_call))
+                            .collect(),
+                    }
+                    .into_stmt(),
+                );
 
                 let class = Box::new(Class {
                     span: DUMMY_SP,
