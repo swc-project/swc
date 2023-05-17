3c8ee2a09bde4c46eec652c3009aadd141a8df29
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-17 14:21:42 +0900

    Reduce

--- crates/swc_ecma_parser/tests/typescript/issue-7403/1/input.ts
+++ crates/swc_ecma_parser/tests/typescript/issue-7403/1/input.ts
@@ -1,7 +1,4 @@
-declare let fn: any, x: any, y: any
-declare function z<A, B>(_: any): any
 fn(z<Number, Object>(y))
-fn(z<Number, Object> = y)
 fn(x < y, x > y)
 fn(x < y, x >= y)
 fn(x < y, x >> y)
