2a8b379dda65006b68a5ad380ec8df58b1a3a2d4
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-05-21 21:39:12 -0700

    fix(core): correctly expose plugin with host

--- crates/swc_core/src/plugin.rs
+++ crates/swc_core/src/plugin.rs
@@ -1,7 +1,32 @@
 // #[plugin_transform] macro
-#[cfg(any(docsrs, feature = "__common_plugin_transform"))]
-#[cfg_attr(docsrs, doc(cfg(feature = "__common_plugin_transform")))]
-pub use swc_plugin_macro::{css_plugin_transform, plugin_transform};
+#[cfg(any(
+    docsrs,
+    feature = "__common_plugin_transform",
+    feature = "__css_plugin_transform",
+))]
+#[cfg_attr(
+    docsrs,
+    doc(cfg(any(
+        feature = "__common_plugin_transform",
+        feature = "__css_plugin_transform"
+    )))
+)]
+pub use swc_plugin_macro::css_plugin_transform;
+#[cfg(any(
+    docsrs,
+    feature = "__common_plugin_transform",
+    feature = "__css_plugin_transform",
+    feature = "__ecma_plugin_transform"
+))]
+#[cfg_attr(
+    docsrs,
+    doc(cfg(any(
+        feature = "__common_plugin_transform",
+        feature = "__css_plugin_transform",
+        feature = "__ecma_plugin_transform"
+    )))
+)]
+pub use swc_plugin_macro::plugin_transform;
 
 /// exported __alloc / __free fn for the guest (plugin)
 /// allows to allocate memory from the host side.
@@ -13,8 +38,20 @@ pub mod memory {
 
 /// Global HANDLER implementation for the plugin
 /// for error reporting.
-#[cfg(any(docsrs, feature = "__common_plugin_transform"))]
-#[cfg_attr(docsrs, doc(cfg(feature = "__common_plugin_transform")))]
+#[cfg(any(
+    docsrs,
+    feature = "__common_plugin_transform",
+    feature = "__css_plugin_transform",
+    feature = "__ecma_plugin_transform"
+))]
+#[cfg_attr(
+    docsrs,
+    doc(cfg(any(
+        feature = "__common_plugin_transform",
+        feature = "__css_plugin_transform",
+        feature = "__ecma_plugin_transform"
+    )))
+)]
 pub mod errors {
     /// global context HANDLER in plugin's transform function.
     pub static HANDLER: swc_plugin::pseudo_scoped_key::PseudoScopedKey<
@@ -25,14 +62,27 @@ pub mod errors {
 }
 
 /// Plugin's environment metadata context.
-#[cfg(any(docsrs, feature = "__common_plugin_transform"))]
-#[cfg_attr(docsrs, doc(cfg(feature = "__common_plugin_transform")))]
+#[cfg(any(
+    docsrs,
+    feature = "__common_plugin_transform",
+    feature = "__css_plugin_transform",
+    feature = "__ecma_plugin_transform"
+))]
+#[cfg_attr(
+    docsrs,
+    doc(cfg(any(
+        feature = "__common_plugin_transform",
+        feature = "__css_plugin_transform",
+        feature = "__ecma_plugin_transform"
+    )))
+)]
 pub mod metadata {
     pub use swc_common::plugin::metadata::TransformPluginMetadataContextKind;
     pub use swc_plugin_proxy::TransformPluginProgramMetadata;
 }
 
 /// Proxy to the host's data not attached to the AST, like sourcemap / comments.
+/// Or interfaces to setup the plugin's environment from the host.
 #[cfg(any(
     docsrs,
     feature = "__common_plugin_transform",
