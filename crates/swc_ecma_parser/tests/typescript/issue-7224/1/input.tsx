e38a6a5342e2a2312239ca9049aaa4c58482e232
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-24 13:33:41 +0900

    Reduce

--- crates/swc_ecma_parser/tests/typescript/issue-7224/1/input.tsx
+++ crates/swc_ecma_parser/tests/typescript/issue-7224/1/input.tsx
@@ -1,19 +1,8 @@
-import React from 'react';
-
-const StyledComponent = ({ className }) => {
-  const style = className({ theme: { blue: '#0000ff' } });
-  return <div style={style}>Hello</div>;
-};
-
 const getStatusLabel = () => {
-  const disabled = false;
-
   return (
     <div>
       {disabled ? <StyledComponent className={({ theme }): { [key: string]: any } => ({ color: theme.blue })} /> : null}
     </div>
   );
 };
-const Temp = () => <div>{getStatusLabel()}</div>;
-
-export default Temp;
+export default getStatusLabel;
