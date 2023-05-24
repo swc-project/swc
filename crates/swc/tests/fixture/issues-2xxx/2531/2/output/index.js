4125309c341a53b4af165b88a9bf3e18df56b851
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-24 13:23:35 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-2xxx/2531/2/output/index.js
+++ crates/swc/tests/fixture/issues-2xxx/2531/2/output/index.js
@@ -1,7 +1,7 @@
 import { _ as _object_without_properties } from "@swc/helpers/_/_object_without_properties";
 const items = [];
 for (const _ref of items){
-    var item = _ref;
+    const item = _ref;
     const { name  } = item, rest = _object_without_properties(item, [
         "name"
     ]);
