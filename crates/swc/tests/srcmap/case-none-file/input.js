0c92e534a5e3ccc74077ac03c473ad2c12fd5349
limerick <guods2015@gmail.com>
2023-05-25 10:15:21 +0800

    fix(es): Ignore input sourcemap error (#7446)

---
+++ crates/swc/tests/srcmap/case-none-file/input.js
@@ -0,0 +1,43 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [
+        158
+    ],
+    {
+        2943: function(n, t, u) {
+            "use strict";
+            var r = function(n) {
+                var t = n.data;
+                return (0, _.jsx)("div", {
+                    children: t.foo
+                });
+            };
+            u.r(t), u.d(t, {
+                __N_SSG: function() {
+                    return i;
+                },
+                default: function() {
+                    return r;
+                }
+            });
+            var _ = u(4512), i = !0;
+        },
+        7139: function(n, t, u) {
+            (window.__NEXT_P = window.__NEXT_P || []).push([
+                "/static",
+                function() {
+                    return u(2943);
+                }
+            ]);
+        }
+    },
+    function(n) {
+        n.O(0, [
+            774,
+            888,
+            179
+        ], function() {
+            return n(n.s = 7139);
+        }), _N_E = n.O();
+    }
+]);
+//# sourceMappingURL=none.js.map
