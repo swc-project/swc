13b5be28b650a4b242cde22586df9fbe9807af97
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-25 13:59:36 +0900

    fix estree

--- crates/swc_estree_compat/src/swcify/expr.rs
+++ crates/swc_estree_compat/src/swcify/expr.rs
@@ -774,7 +774,7 @@ impl Swcify for OptionalMemberExpression {
         OptChainExpr {
             span: ctx.span(&self.base),
             // TODO: Use correct span.
-            question_dot_token: DUMMY_SP,
+            optional: self.optional,
             base: Box::new(OptChainBase::Member(MemberExpr {
                 span: ctx.span(&self.base),
                 obj: self.object.swcify(ctx),
@@ -811,8 +811,7 @@ impl Swcify for OptionalCallExpression {
     fn swcify(self, ctx: &Context) -> Self::Output {
         OptChainExpr {
             span: ctx.span(&self.base),
-            // TODO: Use correct span.
-            question_dot_token: DUMMY_SP,
+            optional: self.optional,
             base: Box::new(OptChainBase::Call(OptCall {
                 span: ctx.span(&self.base),
                 callee: self.callee.swcify(ctx),
