bb9b0c3c3d37057d9ed9c2aedfdf7e5b9d79083d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-07 12:42:48 +0900

    fixup

--- crates/swc_ecma_parser/tests/typescript/issue-7493/1/input.ts
+++ crates/swc_ecma_parser/tests/typescript/issue-7493/1/input.ts
@@ -1,8 +1,2 @@
 const onChange = useCallback<<T extends keyof typeof initialState>(key: T, value: typeof initialState[T]) => void>(
-  (key, value) => {
-    setState((preState) => {
-      preState[key] = value;
-    });
-  },
-  [setState],
-);
+  );
