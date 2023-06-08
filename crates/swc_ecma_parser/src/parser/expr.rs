9877306068cceb7a87d16767ca82a3ab0e379d42
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-08 18:12:15 +0900

    More opt call

--- crates/swc_ecma_parser/src/parser/expr.rs
+++ crates/swc_ecma_parser/src/parser/expr.rs
@@ -1391,10 +1391,11 @@ impl<I: Tokens> Parser<I> {
             };
             let args = self.parse_args(obj.is_import())?;
             let span = span!(self, start);
-            return if match &obj {
-                Callee::Expr(obj) => obj.is_opt_chain(),
-                _ => false,
-            } {
+            return if question_dot_token.is_some()
+                || match &obj {
+                    Callee::Expr(obj) => obj.is_opt_chain(),
+                    _ => false,
+                } {
                 match obj {
                     Callee::Super(_) | Callee::Import(_) => {
                         syntax_error!(self, self.input.cur_span(), SyntaxError::SuperCallOptional)
