e97979ebafe638cd54391b2057390af76f0bacd0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-12 11:52:59 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining/issue-732/1/output.js
@@ -0,0 +1,2 @@
+var _test_a_b_c;
+(_test_a_b_c = test.a) === null || _test_a_b_c === void 0 ? void 0 : _test_a_b_c.b.c.d;
---
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining/issue-732/2/output.js
@@ -0,0 +1,2 @@
+var _test_a_b;
+(_test_a_b = test.a) === null || _test_a_b === void 0 ? void 0 : _test_a_b.b.c;
