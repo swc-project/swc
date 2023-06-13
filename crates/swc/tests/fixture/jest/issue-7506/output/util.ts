00f6e71070e40ff2394e4bfa44475697e5545099
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-13 14:27:01 +0900

    Update test refs using old version

--- crates/swc/tests/fixture/jest/issue-7506/output/util.ts
+++ crates/swc/tests/fixture/jest/issue-7506/output/util.ts
@@ -1,6 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-export function getStackTrace() {
-    var rawStack = new Error().stack;
-    return rawStack.split("\n");
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "getStackTrace", {
+    enumerable: true,
+    get: function () {
+        return getStackTrace;
+    }
+});
+function getStackTrace() {
+    const rawStack = new Error().stack;
+    return rawStack.split('\n');
 }
