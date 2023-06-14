1c02cd3247f782af190734f0a7c7c77301ce5170
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-14 13:08:51 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining/swc-node-95-3/output.js
@@ -0,0 +1,2 @@
+var _obj_a_b_c, _this, _obj_a, _obj;
+expect((_this = (_obj_a = (_obj = obj) === null || _obj === void 0 ? void 0 : _obj.a) === null || _obj_a === void 0 ? void 0 : _obj_a.b) === null || _this === void 0 ? void 0 : (_obj_a_b_c = _this.c) === null || _obj_a_b_c === void 0 ? void 0 : _obj_a_b_c.call(_this)).toBe(2);
