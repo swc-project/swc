9e7cf61bbef4fd03bf23a854f7e112f4591d04b1
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-15 20:52:10 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-7xxx/7315/1/output/index.js
@@ -0,0 +1,15 @@
+// input.ts
+"use client";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "ReactClientComponent", {
+    enumerable: true,
+    get: function() {
+        return ReactClientComponent;
+    }
+});
+"use client";
+function ReactClientComponent() {
+    return "Hello world";
+}
