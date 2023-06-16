be069401f59779c69e0515afa7298bb62a39889d
Justin Ridgewell <justin@ridgewell.name>
2023-06-16 17:03:26 -0400

    Comments

--- crates/swc_ecma_transforms_compat/src/es2020/optional_chaining.rs
+++ crates/swc_ecma_transforms_compat/src/es2020/optional_chaining.rs
@@ -44,6 +44,9 @@ impl VisitMut for OptChaining {
             };
             stmt.visit_mut_with(self);
 
+            // If there are optional chains in this expression, then the visitor will have
+            // injected an VarDecl statement and we need to transform into a
+            // block. If not, then we can keep the expression.
             match &mut stmt.stmts[..] {
                 [Stmt::Return(ReturnStmt { arg: Some(e), .. })] => {
                     *expr = BlockStmtOrExpr::Expr(e.take())
@@ -55,7 +58,7 @@ impl VisitMut for OptChaining {
 
     fn visit_mut_expr(&mut self, e: &mut Expr) {
         match e {
-            // foo?.bar -> foo == null ? undefined : foo.bar
+            // foo?.bar -> foo == null ? void 0 : foo.bar
             Expr::OptChain(v) => {
                 let data = self.gather(v.take(), vec![]);
                 *e = self.construct(data, false, self.c.no_document_all);
@@ -76,47 +79,54 @@ impl VisitMut for OptChaining {
     }
 
     fn visit_mut_pat(&mut self, n: &mut Pat) {
-        if let Pat::Assign(a) = n {
-            let uninit = self.vars.take();
-            a.right.visit_mut_with(self);
-            if !self.vars.is_empty() {
-                let stmts = vec![
-                    Stmt::Decl(Decl::Var(Box::new(VarDecl {
-                        span: DUMMY_SP,
-                        declare: false,
-                        kind: VarDeclKind::Var,
-                        decls: mem::take(&mut self.vars),
-                    }))),
-                    Stmt::Return(ReturnStmt {
-                        span: DUMMY_SP,
-                        arg: Some(a.right.take()),
-                    }),
-                ];
-                a.right = Box::new(Expr::Call(CallExpr {
-                    span: DUMMY_SP,
-                    callee: Expr::Arrow(ArrowExpr {
-                        span: DUMMY_SP,
-                        params: vec![],
-                        body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
-                            span: DUMMY_SP,
-                            stmts,
-                        })),
-                        is_async: false,
-                        is_generator: false,
-                        type_params: Default::default(),
-                        return_type: Default::default(),
-                    })
-                    .as_callee(),
-                    args: vec![],
-                    type_args: Default::default(),
-                }));
-            }
-
-            self.vars = uninit;
-            a.left.visit_mut_with(self);
-        } else {
+        // The default initializer of an assignment pattern must not leak the memo
+        // variable into the enclosing scope.
+        // function(a, b = a?.b) {} -> function(a, b = (() => var _a; …)()) {}
+        let Pat::Assign(a) = n else {
             n.visit_mut_children_with(self);
+            return;
+        };
+
+        let uninit = self.vars.take();
+        a.right.visit_mut_with(self);
+
+        // If we found an optional chain, we need to transform into an arrow IIFE to
+        // capture the memo variable.
+        if !self.vars.is_empty() {
+            let stmts = vec![
+                Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                    span: DUMMY_SP,
+                    declare: false,
+                    kind: VarDeclKind::Var,
+                    decls: mem::take(&mut self.vars),
+                }))),
+                Stmt::Return(ReturnStmt {
+                    span: DUMMY_SP,
+                    arg: Some(a.right.take()),
+                }),
+            ];
+            a.right = Box::new(Expr::Call(CallExpr {
+                span: DUMMY_SP,
+                callee: Expr::Arrow(ArrowExpr {
+                    span: DUMMY_SP,
+                    params: vec![],
+                    body: Box::new(BlockStmtOrExpr::BlockStmt(BlockStmt {
+                        span: DUMMY_SP,
+                        stmts,
+                    })),
+                    is_async: false,
+                    is_generator: false,
+                    type_params: Default::default(),
+                    return_type: Default::default(),
+                })
+                .as_callee(),
+                args: vec![],
+                type_args: Default::default(),
+            }));
         }
+
+        self.vars = uninit;
+        a.left.visit_mut_with(self);
     }
 
     fn visit_mut_module_items(&mut self, n: &mut Vec<ModuleItem>) {
@@ -137,6 +147,11 @@ enum Gathering {
 }
 
 impl OptChaining {
+    /// Transforms the left-nested structure into a flat vec. The obj/callee
+    /// of every node in the chain will be Invalid, to be replaced with a
+    /// constructed node in the construct step.
+    /// The top member/call will be first, and the deepest obj/callee will be
+    /// last.
     fn gather(
         &mut self,
         v: OptChainExpr,
@@ -148,9 +163,11 @@ impl OptChaining {
             let OptChainExpr {
                 optional, mut base, ..
             } = current;
+
             if optional {
                 count += 1;
             }
+
             let next;
             match &mut *base {
                 OptChainBase::Member(m) => {
@@ -162,6 +179,7 @@ impl OptChaining {
                         Gathering::Member(m.take())
                     });
                 }
+
                 OptChainBase::Call(c) => {
                     next = c.callee.take();
                     c.args.visit_mut_with(self);
@@ -173,6 +191,7 @@ impl OptChaining {
                     });
                 }
             }
+
             match *next {
                 Expr::OptChain(next) => {
                     current = next;
@@ -182,6 +201,8 @@ impl OptChaining {
         }
     }
 
+    /// Constructs a rightward nested conditional expression out of our
+    /// flattened chain.
     fn construct(
         &mut self,
         data: (Expr, usize, Vec<Gathering>),
@@ -189,9 +210,26 @@ impl OptChaining {
         no_document_all: bool,
     ) -> Expr {
         let (mut current, count, chain) = data;
+
+        // Stores partially constructed CondExprs for us to assemble later on.
         let mut committed_cond = Vec::with_capacity(count);
+
+        // Stores the memo used to construct an optional chain, so that it can be used
+        // as the this context of an optional call:
+        // foo?.bar?.() ->
+        // (_foo = foo) == null
+        //   ? void 0
+        //   : (_foo_bar = _foo.bar) == null
+        //     ?  void 0 : _foo_bar.call(_foo)
         let mut ctx = None;
 
+        // In the first pass, we construct a "current" node and several committed
+        // CondExprs. The conditionals will have an invalid alt, waiting for the
+        // second pass to properly construct them.
+        // We reverse iterate so that we can construct a rightward conditional
+        // `(_a = a) == null ? void 0 : (_a_b = _a.b) == null ? void 0 : _a_b.c`
+        // instead of a leftward one
+        // `(_a_b = (_a = a) == null ? void 0 : _a.b) == null ? void 0 : _a_b.c`
         for v in chain.into_iter().rev() {
             current = match v {
                 Gathering::Call(mut c) => {
@@ -206,6 +244,8 @@ impl OptChaining {
                 }
                 Gathering::OptCall(mut c, memo) => {
                     let mut call = false;
+
+                    // foo.bar?.() -> (_foo_bar == null) ? void 0 : _foo_bar.call(foo)
                     match &mut current {
                         Expr::Member(m) => {
                             call = true;
@@ -227,6 +267,7 @@ impl OptChaining {
                         }
                         _ => {}
                     }
+
                     committed_cond.push(CondExpr {
                         span: DUMMY_SP,
                         test: init_and_eq_null_or_undefined(&memo, current, no_document_all),
@@ -260,8 +301,10 @@ impl OptChaining {
                     m.obj = memo.into();
                     Expr::Member(m)
                 }
-            }
+            };
         }
+
+        // At this point, `current` is the right-most expression `_a_b.c` in `a?.b?.c`
         if is_delete {
             current = Expr::Unary(UnaryExpr {
                 span: DUMMY_SP,
@@ -269,6 +312,8 @@ impl OptChaining {
                 arg: Box::new(current),
             });
         }
+
+        // We now need to reverse iterate the conditionals to construct out tree.
         for mut cond in committed_cond.into_iter().rev() {
             cond.alt = Box::new(current);
             current = Expr::Cond(cond)
