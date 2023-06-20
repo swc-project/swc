1d9f972fb44e0722fce8a83090cbada81decf577
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-20 14:03:01 +0900

    fix(es/resolver): Resolve the super class before registering a class name (#7550)
    **Related issue:**
    
     - Closes #7546.

---
+++ crates/swc_ecma_transforms_base/tests/resolver/issue-7546/output.js
@@ -0,0 +1,8 @@
+import { ClassName__2 } from './some-file';
+export default {
+    field: class ClassName__3 extends ClassName__2 {
+        constructor(){
+            super();
+        }
+    }
+};
