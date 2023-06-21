c98264b630f4df3d7527f852eb0d403be84252ef
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-21 23:29:06 +0900

    Make it faster

--- crates/swc_plugin_runner/tests/fixture/issue_6404/src/lib.rs
+++ crates/swc_plugin_runner/tests/fixture/issue_6404/src/lib.rs
@@ -8,7 +8,7 @@ use swc_core::{
 pub fn process_transform(program: Program, metadata: TransformPluginProgramMetadata) -> Program {
     for i in 1..50000 {
         let j: u32 = i;
-        println!("i {} j {}", i, j);
+        // println!("i {} j {}", i, j);
         let res = metadata.source_map.span_to_snippet(Span::new(
             BytePos(j),
             BytePos(j + 1000),
