d8a35b728081ca88e6ad9d39995657bc3500ecaf
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-22 10:50:34 +0900

    Fix

--- crates/swc_ecma_minifier/src/compress/optimize/unused.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/unused.rs
@@ -857,7 +857,12 @@ impl Optimizer<'_> {
             return None;
         }
 
-        let should_preserve_property = |sym: &JsWord| !usage.accessed_props.contains_key(sym);
+        let should_preserve_property = |sym: &JsWord| {
+            if let "toString" = &**sym {
+                return true;
+            }
+            !usage.accessed_props.contains_key(sym)
+        };
         let should_preserve = |key: &PropName| match key {
             PropName::Ident(k) => should_preserve_property(&k.sym),
             PropName::Str(k) => should_preserve_property(&k.value),
