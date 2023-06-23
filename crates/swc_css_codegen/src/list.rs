95ac74e6e494afc90f32c8fc9add2b1824f25db5
Kieran Siek <github@siek.dev>
2023-06-22 19:26:22 -0700

    refactor(common): Remove `add_bitflags` and update `bitflags` (#7571)

--- crates/swc_css_codegen/src/list.rs
+++ crates/swc_css_codegen/src/list.rs
@@ -1,40 +1,30 @@
 #![allow(non_upper_case_globals)]
 use bitflags::bitflags;
-use swc_common::add_bitflags;
 
 bitflags! {
     #[derive(PartialEq, Eq, Copy, Clone)]
     pub struct ListFormat: u16 {
         const None = 0;
+
+        // Line separators
+        /// Prints the list on a single line (default).
+        const SingleLine = 0;
+        /// Prints the list on multiple lines.
+        const MultiLine = 1 << 0;
+        /// Prints the list using line preservation if possible.
+        const PreserveLines = 1 << 1;
+        const LinesMask = Self::MultiLine.bits() | Self::PreserveLines.bits();
+
+        // Delimiters
+        const NotDelimited = 0;
+        const SpaceDelimited = 1 << 2;
+        /// There is no delimiter between list items (default).
+        const SemiDelimited = 1 << 3;
+        const CommaDelimited = 1 << 4;
+        const DotDelimited = 1 << 5;
+        const DelimitersMask = Self::SpaceDelimited.bits()
+            | Self::SemiDelimited.bits()
+            | Self::CommaDelimited.bits()
+            | Self::DotDelimited.bits();
     }
 }
-
-add_bitflags!(
-    ListFormat,
-    // Handled by bitflags! macro.
-    // Values { None: 0 },
-    /// Line separators
-    Values {
-        /// Prints the list on a single line (default).
-        SingleLine: 0,
-        /// Prints the list on multiple lines.
-        MultiLine: 1 << 0,
-        /// Prints the list using line preservation if possible.
-        PreserveLines: 1 << 1,
-        LinesMask: SingleLine | MultiLine | PreserveLines,
-    },
-    /// Delimiters
-    Values {
-        NotDelimited: 0,
-        SpaceDelimited: 1 << 2,
-        /// There is no delimiter between list items (default).
-        SemiDelimited: 1 << 3,
-        CommaDelimited: 1 << 4,
-        DotDelimited: 1 << 5,
-        DelimitersMask: SpaceDelimited | SemiDelimited | CommaDelimited | DotDelimited,
-    },
-    Values {
-        /// Write a trailing comma (",") if present.
-        AllowTrailingComma: 1 << 6,
-    },
-);
