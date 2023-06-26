d1df1c858bfb5eacbb088b6ddefb21e75d901849
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-26 11:06:35 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/issues/7568/1/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/7568/1/input.js
@@ -1,10 +1,10 @@
 var specific_microfront;
-/******/ (function () { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+(function () { // webpackBootstrap
+    "use strict";
+    var __webpack_modules__ = ({
 
-/***/ "webpack/container/entry/specific_page":
-/***/ (function (__unused_webpack_module, exports, __webpack_require__) {
+        "webpack/container/entry/specific_page":
+            (function (__unused_webpack_module, exports, __webpack_require__) {
 
                 var moduleMap = {
                     ".": function () {
@@ -38,87 +38,87 @@ var specific_microfront;
                     init: function () { return init; }
                 });
 
-                /***/
-}),
 
-/***/ "react":
-/***/ (function (module) {
+            }),
+
+        "react":
+            (function (module) {
 
                 module.exports = React;
 
-                /***/
-}),
 
-/***/ "jquery":
-/***/ (function (module) {
+            }),
+
+        "jquery":
+            (function (module) {
 
                 module.exports = jQuery;
 
-                /***/
-})
 
-        /******/
-});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-            /******/
-}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
-/******/ 			loaded: false,
-/******/ 			exports: {}
-            /******/
-};
-/******/
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-        /******/
-}
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = __webpack_modules__;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = __webpack_module_cache__;
-/******/
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function () {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function (module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function () { return module['default']; } :
-/******/ 				function () { return module; };
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-            /******/
-};
-        /******/
-}();
-/******/
-/************************************************************************/
-/******/
-/******/ 	// module cache are used so entry inlining is disabled
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	var __webpack_exports__ = __webpack_require__("webpack/container/entry/specific_page");
-/******/ 	specific_microfront = __webpack_exports__;
-    /******/
-    /******/
+            })
+
+
+    });
+    /************************************************************************/
+    // The module cache
+    var __webpack_module_cache__ = {};
+
+    // The require function
+    function __webpack_require__(moduleId) {
+        // Check if module is in cache
+        var cachedModule = __webpack_module_cache__[moduleId];
+        if (cachedModule !== undefined) {
+            return cachedModule.exports;
+
+        }
+        // Create a new module (and put it into the cache)
+        var module = __webpack_module_cache__[moduleId] = {
+            id: moduleId,
+            loaded: false,
+            exports: {}
+
+        };
+
+        // Execute the module function
+        __webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+        // Flag the module as loaded
+        module.loaded = true;
+
+        // Return the exports of the module
+        return module.exports;
+
+    }
+
+    // expose the modules object (__webpack_modules__)
+    __webpack_require__.m = __webpack_modules__;
+
+    // expose the module cache
+    __webpack_require__.c = __webpack_module_cache__;
+
+    /************************************************************************/
+    /* webpack/runtime/compat get default export */
+    !function () {
+        // getDefaultExport function for compatibility with non-harmony modules
+        __webpack_require__.n = function (module) {
+            var getter = module && module.__esModule ?
+                function () { return module['default']; } :
+                function () { return module; };
+            __webpack_require__.d(getter, { a: getter });
+            return getter;
+
+        };
+
+    }();
+
+    /************************************************************************/
+
+    // module cache are used so entry inlining is disabled
+    // startup
+    // Load entry module and return exports
+    var __webpack_exports__ = __webpack_require__("webpack/container/entry/specific_page");
+    specific_microfront = __webpack_exports__;
+
+
 })()
     ;
