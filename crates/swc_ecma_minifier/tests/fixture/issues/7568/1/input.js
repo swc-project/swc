968dbab80fc38a7d57c5624e8b79d10d8a975d01
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-27 09:55:21 +0900

    Reduce

--- crates/swc_ecma_minifier/tests/fixture/issues/7568/1/input.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/7568/1/input.js
@@ -6,19 +6,7 @@ var specific_microfront;
 
 
     });
-    /************************************************************************/
-    // The module cache
-    var __webpack_module_cache__ = {};
-
-    // The require function
     function __webpack_require__(moduleId) {
-        // Check if module is in cache
-        var cachedModule = __webpack_module_cache__[moduleId];
-        if (cachedModule !== undefined) {
-            return cachedModule.exports;
-
-        }
-        // Create a new module (and put it into the cache)
         var module = __webpack_module_cache__[moduleId] = {
             id: moduleId,
             loaded: false,
@@ -26,43 +14,12 @@ var specific_microfront;
 
         };
 
-        // Execute the module function
         __webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
-        // Flag the module as loaded
         module.loaded = true;
-
-        // Return the exports of the module
         return module.exports;
 
     }
 
-    // expose the modules object (__webpack_modules__)
-    __webpack_require__.m = __webpack_modules__;
-
-    // expose the module cache
-    __webpack_require__.c = __webpack_module_cache__;
-
-    /************************************************************************/
-    /* webpack/runtime/compat get default export */
-    !function () {
-        // getDefaultExport function for compatibility with non-harmony modules
-        __webpack_require__.n = function (module) {
-            var getter = module && module.__esModule ?
-                function () { return module['default']; } :
-                function () { return module; };
-            __webpack_require__.d(getter, { a: getter });
-            return getter;
-
-        };
-
-    }();
-
-    /************************************************************************/
-
-    // module cache are used so entry inlining is disabled
-    // startup
-    // Load entry module and return exports
     var __webpack_exports__ = __webpack_require__("webpack/container/entry/specific_page");
     specific_microfront = __webpack_exports__;
 
