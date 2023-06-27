028c53789500f6d8d57339982361da2789696d37
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-27 10:53:57 +0900

    `visit_mut_stmt_likes`

--- crates/swc_ecma_transforms_proposal/src/explicit_resource_management.rs
+++ crates/swc_ecma_transforms_proposal/src/explicit_resource_management.rs
@@ -1,11 +1,40 @@
+use swc_ecma_ast::*;
+use swc_ecma_utils::StmtLike;
 use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 
 pub fn explicit_resource_management() -> impl Fold + VisitMut {
-    as_folder(ExplicitResourceManagement)
+    as_folder(ExplicitResourceManagement::default())
 }
 
-struct ExplicitResourceManagement;
+#[derive(Default)]
+struct ExplicitResourceManagement {}
+
+impl ExplicitResourceManagement {
+    fn visit_mut_stmt_likes<T>(&mut self, stmts: &mut Vec<T>)
+    where
+        T: StmtLike,
+    {
+        for stmt in stmts.iter_mut() {
+            match stmt.as_stmt() {
+                Some(stmt) => match stmt {
+                    Stmt::Decl(Decl::Using(using)) => {}
+
+                    _ => {}
+                },
+                None => {}
+            }
+        }
+    }
+}
 
 impl VisitMut for ExplicitResourceManagement {
     noop_visit_mut_type!();
+
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        self.visit_mut_stmt_likes(stmts)
+    }
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        self.visit_mut_stmt_likes(stmts)
+    }
 }
