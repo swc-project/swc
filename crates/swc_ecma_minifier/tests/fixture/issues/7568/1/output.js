6e0409ae866c735f6c8c0949a09fb6864a6c82f7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-27 10:11:53 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/fixture/issues/7568/1/output.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/7568/1/output.js
@@ -1,25 +1,13 @@
 var specific_microfront;
 !function() {
     "use strict";
-    var __webpack_modules__ = {}, __webpack_module_cache__ = {};
-    function __webpack_require__(moduleId) {
-        var cachedModule = __webpack_module_cache__[moduleId];
-        if (void 0 !== cachedModule) return cachedModule.exports;
+    var __webpack_modules__ = {};
+    specific_microfront = function __webpack_require__(moduleId) {
         var module = __webpack_module_cache__[moduleId] = {
             id: moduleId,
             loaded: !1,
             exports: {}
         };
         return __webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__), module.loaded = !0, module.exports;
-    }
-    __webpack_require__.m = __webpack_modules__, __webpack_require__.c = __webpack_module_cache__, __webpack_require__.n = function(module) {
-        var getter = module && module.__esModule ? function() {
-            return module.default;
-        } : function() {
-            return module;
-        };
-        return __webpack_require__.d(getter, {
-            a: getter
-        }), getter;
-    }, specific_microfront = __webpack_require__("webpack/container/entry/specific_page");
+    }("webpack/container/entry/specific_page");
 }();
