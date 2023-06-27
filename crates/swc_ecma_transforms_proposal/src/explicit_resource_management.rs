0bffa0d25bd59e60efca6f499c3a4ee530e29afc
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-27 11:36:21 +0900

    More

--- crates/swc_ecma_transforms_proposal/src/explicit_resource_management.rs
+++ crates/swc_ecma_transforms_proposal/src/explicit_resource_management.rs
@@ -1,29 +1,96 @@
+use swc_common::DUMMY_SP;
 use swc_ecma_ast::*;
-use swc_ecma_utils::StmtLike;
-use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
+use swc_ecma_transforms_base::helper;
+use swc_ecma_utils::{ExprFactory, StmtLike};
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 pub fn explicit_resource_management() -> impl Fold + VisitMut {
     as_folder(ExplicitResourceManagement::default())
 }
 
 #[derive(Default)]
-struct ExplicitResourceManagement {}
+struct ExplicitResourceManagement {
+    state: Option<State>,
+}
+
+struct State {
+    stack: Ident,
+    has_error: Ident,
+    error_var: Ident,
+    catch_var: Ident,
+}
 
 impl ExplicitResourceManagement {
     fn visit_mut_stmt_likes<T>(&mut self, stmts: &mut Vec<T>)
     where
         T: StmtLike,
+        Vec<T>: VisitMutWith<Self>,
     {
-        for stmt in stmts.iter_mut() {
-            match stmt.as_stmt() {
-                Some(stmt) => match stmt {
-                    Stmt::Decl(Decl::Using(using)) => {}
+        let old_state = self.state.take();
 
-                    _ => {}
+        stmts.visit_mut_with(self);
+
+        if let Some(state) = self.state.take() {
+            // var has_error = true
+            let has_error_true = Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                span: DUMMY_SP,
+                kind: VarDeclKind::Var,
+                declare: false,
+                decls: vec![VarDeclarator {
+                    span: DUMMY_SP,
+                    name: state.has_error.into(),
+                    init: Some(true.into()),
+                    definite: false,
+                }],
+            })));
+            // var error = $catch_var
+            let error_catch_var = Stmt::Decl(Decl::Var(Box::new(VarDecl {
+                span: DUMMY_SP,
+                kind: VarDeclKind::Var,
+                declare: false,
+                decls: vec![VarDeclarator {
+                    span: DUMMY_SP,
+                    name: state.error_var.into(),
+                    init: Some(state.catch_var.clone().into()),
+                    definite: false,
+                }],
+            })));
+
+            let dispose_stmt = CallExpr {
+                span: DUMMY_SP,
+                callee: helper!(dispose),
+                args: vec![
+                    state.stack.as_arg(),
+                    state.error_var.as_arg(),
+                    state.has_error.as_arg(),
+                ],
+                type_args: Default::default(),
+            };
+
+            let try_stmt = TryStmt {
+                span: DUMMY_SP,
+                block: BlockStmt {
+                    span: DUMMY_SP,
+                    stmts: (),
                 },
-                None => {}
-            }
+                handler: Some(CatchClause {
+                    span: DUMMY_SP,
+                    param: Some(state.catch_var.into()),
+                    body: BlockStmt {
+                        span: DUMMY_SP,
+                        stmts: vec![has_error_true, error_catch_var],
+                    },
+                }),
+                finalizer: Some(BlockStmt {
+                    span: DUMMY_SP,
+                    stmts: vec![dispose_stmt],
+                }),
+            };
+
+            *stmts = T::from_stmt(Stmt::Try(try_stmt));
         }
+
+        self.state = old_state;
     }
 }
 
