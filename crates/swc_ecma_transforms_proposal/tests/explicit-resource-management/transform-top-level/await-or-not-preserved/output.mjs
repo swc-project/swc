12a393aaaa64186039c4b871104b075f4c0c043e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 12:56:47 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-top-level/await-or-not-preserved/output.mjs
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-top-level/await-or-not-preserved/output.mjs
@@ -1,11 +1,11 @@
 export { x, y };
 try {
-  var _stack = [];
-  var x = babelHelpers.using(_stack, A);
-  var y = babelHelpers.using(_stack, B, true);
+    var _stack = [];
+    var x = _using(_stack, A);
+    var y = _using(_stack, B, true);
 } catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  await babelHelpers.dispose(_stack, _error, _hasError);
+    var _error = _;
+    var _hasError = true;
+} finally{
+    await _dispose(_stack, _error, _hasError);
 }
