059a37e71dbf1b22fb07f022d410c62d253fce28
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 13:13:22 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/for-await-head/output.mjs
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/for-await-head/output.mjs
@@ -1,10 +1,13 @@
-for await (const _x of y) try {
-  var _stack = [];
-  const x = babelHelpers.using(_stack, _x);
-  doSomethingWith(x);
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  babelHelpers.dispose(_stack, _error, _hasError);
+for await (const x of y){
+    try {
+        var _stack = [];
+        {
+            doSomethingWith(x);
+        }
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        _dispose(_stack, _error, _hasError);
+    }
 }
