fa0c57a987ef90363ac7ad78af409d049892f67d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 12:44:30 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-await/mixed/output.mjs
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-await/mixed/output.mjs
@@ -1,11 +1,13 @@
-try {
-  var _stack = [];
-  const a = babelHelpers.using(_stack, 1);
-  const b = babelHelpers.using(_stack, 2, true);
-  const c = babelHelpers.using(_stack, 3);
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  await babelHelpers.dispose(_stack, _error, _hasError);
+{
+    try {
+        var _stack = [];
+        const a = _using(_stack, 1);
+        const b = _using(_stack, 2, true);
+        const c = _using(_stack, 3);
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        await _dispose(_stack, _error, _hasError);
+    }
 }
--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-await/only-using-await/output.mjs
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-await/only-using-await/output.mjs
@@ -1,13 +1,14 @@
-try {
-  var _stack = [];
-  const x = babelHelpers.using(_stack, obj, true);
-  stmt;
-  const y = babelHelpers.using(_stack, obj, true),
-    z = babelHelpers.using(_stack, obj, true);
-  doSomethingWith(x, y);
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  await babelHelpers.dispose(_stack, _error, _hasError);
+{
+    try {
+        var _stack = [];
+        const x = _using(_stack, obj, true);
+        stmt;
+        const y = _using(_stack, obj, true), z = _using(_stack, obj, true);
+        doSomethingWith(x, y);
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        await _dispose(_stack, _error, _hasError);
+    }
 }
