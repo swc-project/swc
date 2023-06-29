16a7411ddc1eb30f9c78bc5a337fd21961f7dad9
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 12:46:03 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/bare-block/output.js
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/bare-block/output.js
@@ -1,10 +1,12 @@
-try {
-  var _stack = [];
-  const x = babelHelpers.using(_stack, obj);
-  doSomethingWith(x);
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  babelHelpers.dispose(_stack, _error, _hasError);
+{
+    try {
+        var _stack = [];
+        const x = _using(_stack, obj);
+        doSomethingWith(x);
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        _dispose(_stack, _error, _hasError);
+    }
 }
--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/function-body/output.js
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/function-body/output.js
@@ -1,12 +1,12 @@
 function fn() {
-  try {
-    var _stack = [];
-    const x = babelHelpers.using(_stack, obj);
-    return doSomethingWith(x);
-  } catch (_) {
-    var _error = _;
-    var _hasError = true;
-  } finally {
-    babelHelpers.dispose(_stack, _error, _hasError);
-  }
+    try {
+        var _stack = [];
+        const x = _using(_stack, obj);
+        return doSomethingWith(x);
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        _dispose(_stack, _error, _hasError);
+    }
 }
