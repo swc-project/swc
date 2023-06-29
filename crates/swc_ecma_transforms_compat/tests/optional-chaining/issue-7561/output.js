9482b41de7559334c568a38296fc4dedaedb9aeb
Felipe Armoni <felipe.armoni@gmail.com>
2023-06-29 15:02:55 -0300

    Added tests

---
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining/issue-7561/output.js
@@ -0,0 +1,10 @@
+// …
+var _x = new WeakMap();
+class Foo {
+    test() {
+        (_this = this) === null || _this === void 0
+          ? void 0
+          : _class_private_field_get(_this.y , _x);
+    }
+    // …
+}
