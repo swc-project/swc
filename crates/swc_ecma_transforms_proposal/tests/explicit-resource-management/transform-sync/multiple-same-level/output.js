0e068e108d303883241a57d933073c894410dfde
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 12:47:42 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/multiple-same-level/output.js
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/multiple-same-level/output.js
@@ -1,16 +1,17 @@
-try {
-  var _stack = [];
-  stmt;
-  const x = babelHelpers.using(_stack, obj);
-  stmt;
-  const y = babelHelpers.using(_stack, obj),
-    z = babelHelpers.using(_stack, obj);
-  stmt;
-  const w = babelHelpers.using(_stack, obj);
-  doSomethingWith(x, z);
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  babelHelpers.dispose(_stack, _error, _hasError);
+{
+    try {
+        var _stack = [];
+        stmt;
+        const x = _using(_stack, obj);
+        stmt;
+        const y = _using(_stack, obj), z = _using(_stack, obj);
+        stmt;
+        const w = _using(_stack, obj);
+        doSomethingWith(x, z);
+    } catch (_) {
+        var _error = _;
+        var _hasError = true;
+    } finally{
+        _dispose(_stack, _error, _hasError);
+    }
 }
