f5761becb2c08109ff194eb08eecfbe004d4dba3
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-06-29 12:49:29 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/multiple-nested/output.js
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/multiple-nested/output.js
@@ -1,31 +1,35 @@
-try {
-  var _stack = [];
-  const x = babelHelpers.using(_stack, obj);
-  try {
-    var _stack2 = [];
-    const y = babelHelpers.using(_stack2, call(() => {
-      try {
-        var _stack3 = [];
-        const z = babelHelpers.using(_stack3, obj);
-        return z;
-      } catch (_) {
-        var _error3 = _;
-        var _hasError3 = true;
-      } finally {
-        babelHelpers.dispose(_stack3, _error3, _hasError3);
-      }
-    }));
-    stmt;
-  } catch (_) {
-    var _error2 = _;
-    var _hasError2 = true;
-  } finally {
-    babelHelpers.dispose(_stack2, _error2, _hasError2);
-  }
-  stmt;
-} catch (_) {
-  var _error = _;
-  var _hasError = true;
-} finally {
-  babelHelpers.dispose(_stack, _error, _hasError);
+{
+    try {
+        var _stack = [];
+        const x = _using(_stack, obj);
+        {
+            try {
+                var _stack1 = [];
+                const y = _using(_stack1, call(()=>{
+                    try {
+                        var _stack = [];
+                        const z = _using(_stack, obj);
+                        return z;
+                    } catch (_) {
+                        var _error = _;
+                        var _hasError = true;
+                    } finally{
+                        _dispose(_stack, _error, _hasError);
+                    }
+                }));
+                stmt;
+            } catch (_) {
+                var _error = _;
+                var _hasError = true;
+            } finally{
+                _dispose(_stack1, _error, _hasError);
+            }
+        }
+        stmt;
+    } catch (_) {
+        var _error1 = _;
+        var _hasError1 = true;
+    } finally{
+        _dispose(_stack, _error1, _hasError1);
+    }
 }
