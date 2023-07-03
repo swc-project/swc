4d1bec7f7af101a3a345ee93bfc131518ee674fe
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-03 11:11:10 +0900

    Add a test

---
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining/issue-7612/1/input.js
@@ -0,0 +1,27 @@
+const ModalInner = ({ data }: { data: GetReturnOrderDetails.Result }) => {
+    return (
+        <div className={styles.qcWrapper} ref={wrapperRef} id="qcDataScroll">
+            {data!.qcDataList.map(qcData => (
+                <div className={styles.desc} key={qcData.flawDesc}>
+                    <div className={styles.descTitle}>{qcData?.flawDesc}</div>
+                    {qcData?.flawImageList?.length === 0 ? (
+                        <Empty />
+                    ) : (
+                        <Album
+                            data={(qcData?.flawImageList || [])?.map(src => ({
+                                url: src,
+                                describe: qcData?.flawDesc
+                            }))}
+                            showIndicators
+                            useSafeAreaBottom
+                            showDescription
+                            current={current}
+                        >
+                            test
+                        </Album>
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+}
