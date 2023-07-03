cc45fe747344a6a07c8da977bbd4decb4ff2139c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-03 11:39:06 +0900

    Reduce

--- crates/swc/tests/fixture/issues-7xxx/7612/input/1.js
+++ crates/swc/tests/fixture/issues-7xxx/7612/input/1.js
@@ -1,27 +1,16 @@
 const ModalInner = ({ data }) => {
     return (
-        <div className={styles.qcWrapper} ref={wrapperRef} id="qcDataScroll">
-            {data.qcDataList.map(qcData => (
-                <div className={styles.desc} key={qcData.flawDesc}>
-                    <div className={styles.descTitle}>{qcData?.flawDesc}</div>
-                    {qcData?.flawImageList?.length === 0 ? (
-                        <Empty />
-                    ) : (
-                        <Album
-                            data={(qcData?.flawImageList || [])?.map(src => ({
-                                url: src,
-                                describe: qcData?.flawDesc
-                            }))}
-                            showIndicators
-                            useSafeAreaBottom
-                            showDescription
-                            current={current}
-                        >
-                            test
-                        </Album>
-                    )}
-                </div>
-            ))}
-        </div>
+        <Album
+            data={(qcData?.flawImageList || [])?.map(src => ({
+                url: src,
+                describe: qcData?.flawDesc
+            }))}
+            showIndicators
+            useSafeAreaBottom
+            showDescription
+            current={current}
+        >
+            test
+        </Album>
     )
 }
