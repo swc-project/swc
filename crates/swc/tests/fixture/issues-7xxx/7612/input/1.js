ec8e8dcd3ca1dc5ab268ff30e5137749c5c8f773
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-04 13:41:10 +0900

    Reduce

--- crates/swc/tests/fixture/issues-7xxx/7612/input/1.js
+++ crates/swc/tests/fixture/issues-7xxx/7612/input/1.js
@@ -1,16 +1,1 @@
-const ModalInner = ({ data }) => {
-    return (
-        <Album
-            data={(qcData?.flawImageList || [])?.map(src => ({
-                url: src,
-                describe: qcData?.flawDesc
-            }))}
-            showIndicators
-            useSafeAreaBottom
-            showDescription
-            current={current}
-        >
-            test
-        </Album>
-    )
-}
+(qcData?.flawImageList || [])?.map()
