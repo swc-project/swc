eeecf4b178c805e3d9b82df255f1410aee17e485
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-04 13:46:14 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-7xxx/7612/output/1.js
+++ crates/swc/tests/fixture/issues-7xxx/7612/output/1.js
@@ -1,17 +1,2 @@
-var ModalInner = function(param) {
-    var data = param.data;
-    var _this, _qcData;
-    return React.createElement(Album, {
-        data: (_this = ((_qcData = qcData) === null || _qcData === void 0 ? void 0 : _qcData.flawImageList) || []) === null || _this === void 0 ? void 0 : _this.map(function(src) {
-            var _qcData;
-            return {
-                url: src,
-                describe: (_qcData = qcData) === null || _qcData === void 0 ? void 0 : _qcData.flawDesc
-            };
-        }),
-        showIndicators: true,
-        useSafeAreaBottom: true,
-        showDescription: true,
-        current: current
-    }, "test");
-};
+var _this, _qcData;
+(_this = ((_qcData = qcData) === null || _qcData === void 0 ? void 0 : _qcData.flawImageList) || []) === null || _this === void 0 ? void 0 : _this.map();
