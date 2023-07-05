fc042a9b0b598af702d9e70eb25459a43efc2d65
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-05 16:02:43 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-7xxx/7629/output/1.js
@@ -0,0 +1,5 @@
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.symbol.async-dispose.js";
+import "core-js/modules/es.symbol.description.js";
+import "core-js/modules/es.symbol.js";
+Symbol.asyncDispose;
