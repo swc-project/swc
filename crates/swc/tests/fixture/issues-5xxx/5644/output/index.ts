9012362e4e4948eb79e071067e15f34c6615e9d9
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-13 13:15:04 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-5xxx/5644/output/index.ts
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _export_star = require("@swc/helpers/_/_export_star");
+_export_star._(require("./authentication"), exports);
+_export_star._(require("./persistence"), exports);
+_export_star._(require("./logging"), exports);
+_export_star._(require("./infrastructure"), exports);
+_export_star._(require("./domain"), exports);
+_export_star._(require("./values"), exports);
+_export_star._(require("./domain/rules"), exports);
+_export_star._(require("./command"), exports);
+_export_star._(require("./query"), exports);
+_export_star._(require("./migration"), exports);
+_export_star._(require("./amqp"), exports);
+_export_star._(require("./event-logging"), exports);
+_export_star._(require("./utils"), exports);
+_export_star._(require("./cache-driver"), exports);
+_export_star._(require("./container"), exports);
+_export_star._(require("./common-storage"), exports);
+_export_star._(require("./dto"), exports);
+_export_star._(require("./search-builder"), exports);
+_export_star._(require("./api-response"), exports);
+_export_star._(require("./model-context-factory"), exports);
+_export_star._(require("./web"), exports);
+_export_star._(require("./translation"), exports);
+_export_star._(require("./config"), exports);
