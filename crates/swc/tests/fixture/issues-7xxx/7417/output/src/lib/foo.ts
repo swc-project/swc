34d55ba940ad0cddebada10be4953fc37ca43fa3
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-13 12:55:29 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-7xxx/7417/output/src/lib/foo.ts
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return bar;
+    }
+});
+const _interop_require_default = require("@swc/helpers/_/_interop_require_default");
+const _ = /*#__PURE__*/ _interop_require_default._(require("./index"));
+function bar() {
+    console.log(_.default);
+}
