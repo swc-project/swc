04b0f6d8234bdcc34815a558c4c9eecf24c8e4e7
Austaras <austaras@outlook.com>
2023-07-15 04:54:34 +0800

    feat(es/minifier): Drop recursively used var declaration (#7649)

--- crates/swc/tests/tsc-references/objectLiteralShorthandProperties.2.minified.js
+++ crates/swc/tests/tsc-references/objectLiteralShorthandProperties.2.minified.js
@@ -1,9 +1,1 @@
 //// [objectLiteralShorthandProperties.ts]
-var x3 = {
-    a: 0,
-    b: void 0,
-    c: void 0,
-    d: function() {},
-    x3: x3,
-    parent: x3
-};
--- crates/swc/tests/tsc-references/objectLiteralShorthandPropertiesES6.2.minified.js
+++ crates/swc/tests/tsc-references/objectLiteralShorthandPropertiesES6.2.minified.js
@@ -1,9 +1,1 @@
 //// [objectLiteralShorthandPropertiesES6.ts]
-var x3 = {
-    a: 0,
-    b: void 0,
-    c: void 0,
-    d () {},
-    x3,
-    parent: x3
-};
