9e7fdf21a6c9491463714c13ac8573f1a5d97812
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-18 09:28:26 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/fixture/next/outstatic/1/output.js
+++ crates/swc_ecma_minifier/tests/fixture/next/outstatic/1/output.js
@@ -8,49 +8,34 @@
         },
         1985: function(o, f, i) {
             "use strict";
-            var g = 0, v = (o)=>"checkbox" === o.type;
+            var k = 0, v = (o)=>"checkbox" === o.type;
             var b = (o, f)=>{
                 var i;
                 return o.has((i = f, i.substring(0, i.search(/\.\d+(\.|$)/)) || i));
-            }, k = ()=>{};
+            }, g = ()=>{};
             function C(o, f, i) {}
             var m = (o)=>"radio" === o.type;
-            function B(o = {}) {
+            function w(o = {}) {
                 let f, i = {
                     ...o
-                }, g = {}, C = 0, B = {}, w = {};
-                const N = 0, U = async (o)=>{
+                }, k = {}, v = 0, b = {}, g = {};
+                const C = 0, m = async (o)=>{
                     const i = o.target;
-                    let g = i.name;
+                    let k = i.name;
                     const v = 0;
                     if (v) {
                         let i, b;
-                        const k = 0;
-                        k ? (v._f.onBlur && v._f.onBlur(o), f && f(0)) : v._f.onChange && v._f.onChange(o);
-                        const C = 0;
-                        v._f.deps && N(g, b, i, C);
+                        const g = 0;
+                        g ? (v._f.onBlur && v._f.onBlur(o), f && f(0)) : v._f.onChange && v._f.onChange(o);
+                        const m = 0;
+                        v._f.deps && C(k, b, i, m);
                     }
-                }, x = (o, f = {})=>{
-                    let C = 0;
-                    const N = 0;
-                    return C && N && f.disabled, {
-                        onChange: U,
-                        onBlur: U,
-                        ref: (N)=>{
-                            if (N) {
-                                x(o, f), C = k(g, o);
-                                const i = 0;
-                                var U;
-                                U = i, m(U) || v(U), C._f.refs;
-                            } else (C = k(g, o, {}))._f && (C._f.mount = !1), (i.shouldUnregister || f.shouldUnregister) && (!b(w.array, o) || !B.action) && w.unMount.add(o);
-                        }
-                    };
                 };
             }
             f.useForm = function(o = {}) {
-                const f = g.default.useRef();
+                const f = k.default.useRef();
                 f.current ? f.current.control._options = o : f.current = {
-                    ...B(o)
+                    ...w(o)
                 };
             };
         }
