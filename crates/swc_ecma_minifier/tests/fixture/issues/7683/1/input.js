241c04ab4a2e7e7f34563fdc8a355f82c6ba03e8
magic-akari <akari.ccino@gmail.com>
2023-07-21 09:57:39 +0800

    fix(es/minifier): Only cast global `Infinity`/`undefined`/`NaN` (#7684)
    **Related issue:**
    
     - Closes #7683.

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7683/1/input.js
@@ -0,0 +1,23 @@
+{
+    let Infinity = 3;
+    console.log(Infinity);
+    if (Infinity > 4) console.log("Infinity");
+}
+
+{
+    let NaN = 3;
+    console.log(NaN);
+    if (NaN < 4) console.log("NaN");
+}
+
+{
+    let undefined = 3;
+    console.log(undefined);
+    if (undefined < 4) console.log("undefined");
+}
+
+{
+    let x = 3;
+    console.log(x);
+    if (x < 4) console.log("undefined");
+}
