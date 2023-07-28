6bb3e876b9d7f8019261157db645a48844e88f44
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-28 14:26:04 +0900

    fix typing

--- crates/swc_ecma_preset_env/src/util.rs
+++ crates/swc_ecma_preset_env/src/util.rs
@@ -3,7 +3,7 @@ use rustc_hash::FxHashMap;
 pub(crate) type ObjectMap<T> = FxHashMap<&'static str, T>;
 pub(crate) type ObjectMap2<V> = ObjectMap<ObjectMap<V>>;
 
-pub(crate) fn init_map<T>(s: &[&'static str]) -> ObjectMap<T> {}
+pub(crate) fn init_map<T>(s: &[(&'static str, T)]) -> ObjectMap<T> {}
 
 pub(crate) fn descriptor(
     pure: Option<&'static str>,
