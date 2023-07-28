faee0224b43b2d1a25b34092c9171950622eb0a1
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-07-28 13:11:05 +0900

    Add tests

--- crates/swc/tests/fixture/issues-7xxx/7710/output/7.js
+++
@@ -1,6 +0,0 @@
-let obj = {
-    foo: 1,
-    bar: 2
-};
-console.log(obj.bar);
-export default obj ?? {};
--- crates/swc/tests/fixture/issues-7xxx/7710/output/8.js
+++
@@ -1,8 +0,0 @@
-let obj = {
-    foo: 1,
-    bar: 2
-};
-console.log(obj.bar);
-export default class {
-    [obj ?? 1] = 1;
-}
