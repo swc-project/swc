da67174cfe4c18449378631923227c47884531d2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-08 05:38:26 +0900

    Add a tets

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7770/input.js
@@ -0,0 +1,73 @@
+(self["webpackChunkabc_orchestra"] = self["webpackChunkabc_orchestra"] || []).push([
+    [118], {
+
+        /***/
+        "./node_modules/path/to/abc.js":
+            /***/
+            (function (__unused_webpack_module, exports, __webpack_require__) {
+
+                "use strict";
+
+                Object.defineProperty(exports, "__esModule", ({
+                    value: true
+                }));
+                exports.MainCSS = exports.sHeight = void 0;
+                exports.sHeight = "asdasd";
+                const sWidth = "asdasd";
+                const sAsideWidth = "weqweqw";
+                const sHeight = "asdsads";
+
+                function absolute(abeetew = {}) {
+                    return `
+        abc: cde;
+        def: ${abeetew.def || 0};
+        qwert: ${abeetew.qwert || 0};
+        asda: ${abeetew.asda || 0};
+        left: ${abeetew.abc || 0};
+    `;
+                }
+
+                function flex() {
+                    return `
+        abc: ab;
+        cdef: de;
+        ghn: fg;
+        sadasn: hi;
+    `;
+                }
+                exports.MainCSS = `
+    .ThisshouldOnlyBeonTop {
+        ${absolute()}
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ${exports.sHeight};
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        ${flex()}
+        ${absolute()}
+        
+        asdasd-adsdas: asdasd-asdas;
+        xzczxc: zxczxcc;
+    }
+    .asdasd {
+        ${absolute({ abc: sAsideWidth + "!important", def: exports.sHeight + "!important", qwert: "0 !important" })}
+        sdsd: auto !important;
+        xcxcx: auto !important;
+    }
+    .aasdasdasd .asdsada {
+        we: ${sWidth} !important;
+    }
+    .asdasdasd:not(.adsadsad) .asdasdasd {
+        ds: ${parseFloat(exports.sHeight) + parseFloat(sHeight)}rem !important;
+    }
+    `;
+                //# sourceMappingURL=abc.js.map
+
+                /***/
+            })
+
+
+    }
+]);
