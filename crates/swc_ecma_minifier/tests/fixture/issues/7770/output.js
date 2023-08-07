ea492fe8df783a4d18834331f5847dda45af269e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-08 05:47:15 +0900

    Update test refs

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7770/output.js
@@ -0,0 +1,58 @@
+(self.webpackChunkabc_orchestra = self.webpackChunkabc_orchestra || []).push([
+    [
+        118
+    ],
+    {
+        "./node_modules/path/to/abc.js": function(__unused_webpack_module, exports, __webpack_require__) {
+            "use strict";
+            function absolute(abeetew = {}) {
+                return `
+        abc: cde;
+        def: ${abeetew.def || 0};
+        qwert: ${abeetew.qwert || 0};
+        asda: ${abeetew.asda || 0};
+        left: ${abeetew.abc || 0};
+    `;
+            }
+            Object.defineProperty(exports, "__esModule", {
+                value: !0
+            }), exports.MainCSS = exports.sHeight = void 0, exports.sHeight = "asdasd", exports.MainCSS = `
+    .ThisshouldOnlyBeonTop {
+        ${absolute()}
+        asda-asdasda: asdad-asdsa;
+    }
+    .abcBlablaOneQSR {
+        line-height: ${exports.sHeight};
+        text-shadow: none;
+    }
+    .abcBlablaOne .asdsad {
+        
+        abc: ab;
+        cdef: de;
+        ghn: fg;
+        sadasn: hi;
+    
+        ${absolute()}
+        
+        asdasd-adsdas: asdasd-asdas;
+        xzczxc: zxczxcc;
+    }
+    .asdasd {
+        ${absolute({
+                abc: "weqweqw!important",
+                def: exports.sHeight + "!important",
+                qwert: "0 !important"
+            })}
+        sdsd: auto !important;
+        xcxcx: auto !important;
+    }
+    .aasdasdasd .asdsada {
+        we: asdasd !important;
+    }
+    .asdasdasd:not(.adsadsad) .asdasdasd {
+        ds: ${parseFloat(exports.sHeight) + parseFloat("asdsads")}rem !important;
+    }
+    `;
+        }
+    }
+]);
