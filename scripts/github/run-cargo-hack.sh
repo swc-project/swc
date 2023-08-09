ab39f1c3c83c7156e6e9979773bf542a59d9b4f4
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-09 17:35:36 +0900

    chore(ci): Cleanup CI (#7787)
    **Description**:
     - Disable `crev` action.
     - Cleanup compilation check matrix.
     - Update `cargo-hack`.

---
+++ scripts/github/run-cargo-hack.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+crate=$1
+
+echo "Running cargo hack for crate $crate"
+
+# yq query syntax is weird, so we have to use jq
+json_str="$(yq -o=json $SCRIPT_DIR/tests.yml)"
+
+
+if echo $json_str | jq -e ".check.\"$crate\"" > /dev/null; then
+    check_commands=$(echo $json_str | jq -e -r ".check.\"$crate\" | .[]")
+
+    while IFS= read -r line; do
+        echo "    Running '$line'"
+        (cd "crates/$crate" && $line)
+    done <<< "$check_commands"
+fi
