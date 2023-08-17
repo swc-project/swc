5f579500446824ece9c955c2e0be537fd4a2f2dd
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-17 10:32:17 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-7xxx/7821/output/1.js
@@ -0,0 +1,18 @@
+var Blocks = {
+    Block1: function() {
+        return React.createElement(React.Fragment, null, "'Block1xx'");
+    },
+    Block2: function() {
+        return React.createElement(React.Fragment, null, "'Block2xx'");
+    },
+    Layout1: function() {
+        return [
+            "Block1"
+        ].map(function(e) {
+            return Blocks[e];
+        });
+    }
+};
+export function render() {
+    return React.createElement(Blocks.Layout1, null);
+}
