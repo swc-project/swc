c952f94f52b8ebf1a8a015d578fb16c4095f0ef3
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-17 12:07:02 +0900

    Update test refs

--- crates/swc_ecma_transforms_compat/tests/es2018_object_rest_spread.rs
+++ crates/swc_ecma_transforms_compat/tests/es2018_object_rest_spread.rs
@@ -103,7 +103,7 @@ function foo([{...bar}]) {
 "#,
     r#"
 function foo([_param]) {
-  var bar = _extends({}, _object_destructuring_empty(_param));
+  var bar = _extends({}, _object_destructuring_empty(_param[0]));
 }
 
 "#
