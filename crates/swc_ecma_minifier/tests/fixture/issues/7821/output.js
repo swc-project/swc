a02fe3eec39ff29cc48b767a8034998a48b4e7bf
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-17 10:09:13 +0900

    Update test refs

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7821/output.js
@@ -0,0 +1,21 @@
+var Blocks = {
+    Block1: function() {
+        return React.createElement(React.Fragment, null, "'Block1xx'");
+    },
+    Block2: function() {
+        return React.createElement(React.Fragment, null, "'Block2xx'");
+    },
+    Layout1: function() {
+        return RenderLayout(Blocks, [
+            "Block1"
+        ]);
+    }
+};
+function RenderLayout(Comps, items) {
+    return items.map(function(item) {
+        return Comps[item];
+    });
+}
+export function render() {
+    return React.createElement(Blocks.Layout1, null);
+}
