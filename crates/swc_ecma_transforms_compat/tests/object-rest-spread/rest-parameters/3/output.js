3766a7c776b63e159be3f11f5f931c5e5f968cdb
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-17 13:01:30 +0900

    fix(es/compat): Remove wrong logic for object patterns in `object_rest` (#7788)
    **Related issue:**
    
     - Closes #7776

---
+++ crates/swc_ecma_transforms_compat/tests/object-rest-spread/rest-parameters/3/output.js
@@ -0,0 +1,6 @@
+function a3(_param) {
+    var { a2, b2 } = _param, c2 = _object_without_properties(_param, [
+        "a2",
+        "b2"
+    ]);
+}
---
+++ crates/swc_ecma_transforms_compat/tests/object-rest-spread/rest-parameters/6/output.js
@@ -0,0 +1,5 @@
+function a6(_param) {
+    var { a3, b2: { ba1 } } = _param, ba2 = _object_without_properties(_param.b2, [
+        "ba1"
+    ]);
+}
