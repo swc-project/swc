86f3dece67625d9820b3fdbc7a8e3f1f041b45fd
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-19 15:07:11 +0900

    fixup

--- crates/swc_ecma_minifier/tests/fixture/next/54192/input.js
+++ crates/swc_ecma_minifier/tests/fixture/next/54192/input.js
@@ -1,16 +1,5 @@
 (self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([
     [931], {
-
-        /***/
-        8835:
-            /***/
-            (function (__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
-
-                Promise.resolve( /* import() eager */).then(__webpack_require__.bind(__webpack_require__, 821))
-
-                /***/
-            }),
-
         /***/
         821:
             /***/
@@ -75,75 +64,8 @@
                 /***/
             }),
 
-        /***/
-        622:
-            /***/
-            (function (__unused_webpack_module, exports, __webpack_require__) {
-
-                "use strict";
-                /**
-                 * @license React
-                 * react-jsx-runtime.production.min.js
-                 *
-                 * Copyright (c) Meta Platforms, Inc. and affiliates.
-                 *
-                 * This source code is licensed under the MIT license found in the
-                 * LICENSE file in the root directory of this source tree.
-                 */
-                var f = __webpack_require__(2265),
-                    k = Symbol.for("react.element"),
-                    l = Symbol.for("react.fragment"),
-                    m = Object.prototype.hasOwnProperty,
-                    n = f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,
-                    p = {
-                        key: !0,
-                        ref: !0,
-                        __self: !0,
-                        __source: !0
-                    };
-
-                function q(c, a, g) {
-                    var b, d = {},
-                        e = null,
-                        h = null;
-                    void 0 !== g && (e = "" + g);
-                    void 0 !== a.key && (e = "" + a.key);
-                    void 0 !== a.ref && (h = a.ref);
-                    for (b in a) m.call(a, b) && !p.hasOwnProperty(b) && (d[b] = a[b]);
-                    if (c && c.defaultProps)
-                        for (b in a = c.defaultProps, a) void 0 === d[b] && (d[b] = a[b]);
-                    return {
-                        $$typeof: k,
-                        type: c,
-                        key: e,
-                        ref: h,
-                        props: d,
-                        _owner: n.current
-                    }
-                }
-                exports.Fragment = l;
-                exports.jsx = q;
-                exports.jsxs = q;
 
 
-                /***/
-            }),
-
-        /***/
-        7437:
-            /***/
-            (function (module, __unused_webpack_exports, __webpack_require__) {
-
-                "use strict";
-
-
-                if (true) {
-                    module.exports = __webpack_require__(622);
-                } else { }
-
-
-                /***/
-            })
 
     },
     /******/
