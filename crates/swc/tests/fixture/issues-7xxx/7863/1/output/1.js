2dfa13f87d0eb78ab24ddc2704f86250c132d7b8
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-25 21:31:55 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-7xxx/7863/1/output/1.js
@@ -0,0 +1,7 @@
+import { _ as _class_call_check } from "@swc/helpers/_/_class_call_check";
+import test from "./src/util/test";
+var X = function X() {
+    "use strict";
+    _class_call_check(this, X);
+};
+console.log(test, X);
