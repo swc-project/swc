417aebb566dd1f621a012ade3c07b4de74a556af
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-29 09:07:12 +0900

    DeclExt

--- crates/swc_ecma_ast/src/ext.rs
+++ crates/swc_ecma_ast/src/ext.rs
@@ -2,6 +2,8 @@ use std::fmt::Debug;
 
 pub trait AstNodeExt: 'static + Send + Sync + Debug {}
 
+pub trait DeclExt: AstNodeExt {}
+
 pub trait PatExt: AstNodeExt {}
 
 pub trait ExprExt: AstNodeExt {}
