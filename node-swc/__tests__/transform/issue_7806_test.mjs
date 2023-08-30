3a62256c9142cb23edb79b84956b418576e6dba1
강동윤 <kdy1997.dev@gmail.com>
2023-08-30 15:22:50 +0900

    fixup

--- node-swc/__tests__/transform/issue_7806_test.mjs
+++ node-swc/__tests__/transform/issue_7806_test.mjs
@@ -1,19 +1,29 @@
 import path from "path";
 import swc from "../../..";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
 
 describe('jsc.paths', () => {
 
     it("should work with process.cwd()", async () => {
-        expect((await swc.transformFile(path.join(
+        const f = path.join(
 
 
-            "tests", "swc-path-bug-1", "src", "index.ts"
-        ), {
+            __filename, '..', '..', '..', "tests", "swc-path-bug-1", "src", "index.ts"
+        )
+        console.log(f)
+        expect((await swc.transformFile(f, {
             jsc: {
                 parser: {
                     syntax: 'typescript',
                 },
                 baseUrl: process.cwd(),
+                paths: {
+                    "@utils/*": [
+                        "src/utils/*"
+                    ],
+                }
             }
         })).code).toMatchInlineSnapshot(`
             "\\"\\".concat(100, \\"testing\\");
