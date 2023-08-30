782e02c7d1f8d10d601390c2724699f1fe6ee466
강동윤 <kdy1997.dev@gmail.com>
2023-08-30 15:23:26 +0900

    fixup

--- node-swc/__tests__/transform/issue_7806_test.mjs
+++ node-swc/__tests__/transform/issue_7806_test.mjs
@@ -1,33 +1,40 @@
 import path from "path";
 import swc from "../../..";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
-describe('jsc.paths', () => {
-
+describe("jsc.paths", () => {
     it("should work with process.cwd()", async () => {
         const f = path.join(
-
-
-            __filename, '..', '..', '..', "tests", "swc-path-bug-1", "src", "index.ts"
-        )
-        console.log(f)
-        expect((await swc.transformFile(f, {
-            jsc: {
-                parser: {
-                    syntax: 'typescript',
-                },
-                baseUrl: process.cwd(),
-                paths: {
-                    "@utils/*": [
-                        "src/utils/*"
-                    ],
-                }
-            }
-        })).code).toMatchInlineSnapshot(`
-            "\\"\\".concat(100, \\"testing\\");
+            __filename,
+            "..",
+            "..",
+            "..",
+            "tests",
+            "swc-path-bug-1",
+            "src",
+            "index.ts"
+        );
+        console.log(f);
+        expect(
+            (
+                await swc.transformFile(f, {
+                    jsc: {
+                        parser: {
+                            syntax: "typescript",
+                        },
+                        baseUrl: process.cwd(),
+                        paths: {
+                            "@utils/*": ["src/utils/*"],
+                        },
+                    },
+                })
+            ).code
+        ).toMatchInlineSnapshot(`
+            "import { helloWorld } from \\"src/utils/hello-world.utils.js\\";
+            console.log(helloWorld());
             "
         `);
     });
-})
+});
