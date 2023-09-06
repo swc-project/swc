a86e9f3bb5bd490ebf0b18fe7349a2b0fbc0c45f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-07 02:29:47 +0900

    fix(es/module): Fix `jsc.paths` for projects using pnpm (#7918)
    **Related issue:**
    
    - x-ref: https://vercel.slack.com/archives/C04DUD7EB1B/p1693911653100299?thread_ts=1693819683.070959&cid=C04DUD7EB1B

---
+++ crates/swc/tests/fixture/jsc-paths/vercel-site/1/input/node_modules/.pnpm/nanoid@4.0.2/node_modules/nanoid/non-secure/index.js
@@ -0,0 +1,20 @@
+let urlAlphabet =
+  'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
+export let customAlphabet = (alphabet, defaultSize = 21) => {
+  return (size = defaultSize) => {
+    let id = ''
+    let i = size
+    while (i--) {
+      id += alphabet[(Math.random() * alphabet.length) | 0]
+    }
+    return id
+  }
+}
+export let nanoid = (size = 21) => {
+  let id = ''
+  let i = size
+  while (i--) {
+    id += urlAlphabet[(Math.random() * 64) | 0]
+  }
+  return id
+}
