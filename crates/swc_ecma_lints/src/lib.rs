f7b5e16aef968c9c9f38f40962edf334cc3983e6
magic-akari <akari.ccino@gmail.com>
2023-09-18 22:39:53 +0800

    refactor(es/lint): Remove usage of `box_patterns` (#7966)
    **Related issue:**
     
     - Closes #7964

--- crates/swc_ecma_lints/src/lib.rs
+++ crates/swc_ecma_lints/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(feature = "non_critical_lints", deny(unused))]
 #![cfg_attr(feature = "non_critical_lints", deny(clippy::all))]
-#![feature(box_patterns)]
 
 pub mod config;
 pub mod rule;
