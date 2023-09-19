fa1665109503a2ed685fb938a6ec9a986e200627
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-09-19 10:59:57 +0900

    Add a test

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/7969/input.js
@@ -0,0 +1,14 @@
+let a = 0;
+function f(arr) {
+    let b = a;
+    return `${arr.map((child) => {
+        a = b + "str";
+    })}`;
+}
+
+function g(arr) {
+    return f(arr);
+}
+globalThis.g = g;
+g([1, 2, 3])
+console.log(a);
