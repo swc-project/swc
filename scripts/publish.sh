37fbc5fc0ac8442ff45c00e16dfb040ecfeaba9f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-01 20:40:27 +0900

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,8 +6,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 version="$1"
 
+# Reset other changes
 git commit $SCRIPT_DIR/publish.sh -m 'Update publish script' || true
 git reset --hard
 
+# Update version
+
 npm version "$version" --no-git-tag-version
 (cd ./bindings && cargo set-version $version -p binding_core_wasm)
+(cd ./bindings && cargo set-version --bump swc_cli)
+
+# Update swc_core
