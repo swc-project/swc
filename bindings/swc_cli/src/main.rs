f717cf21cc1cea5e30e87c4d08861daadb25ab14
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-10-03 00:46:14 +0900

    refactor(cli): Make CLI testable/managable with `swc-bump` (#8045)
    **Description:**
    
    It was hard to maintain CLI because actual logic was in `./bindings`.

--- bindings/swc_cli/src/main.rs
+++ bindings/swc_cli/src/main.rs
@@ -1,17 +1,3 @@
-use clap::Parser;
-use commands::{Command, CommandRunner, PluginSubcommand, SwcCliOptions};
-
-mod commands;
-mod util;
-
 fn main() -> anyhow::Result<()> {
-    let command = SwcCliOptions::parse().command;
-
-    match &command {
-        Command::Plugin(PluginSubcommand::New(options)) => options.execute(),
-        Command::Compile(options) => options.execute(),
-        Command::Minify(options) => options.execute(),
-        Command::Bundle(options) => options.execute(),
-        Command::Lint(options) => options.execute(),
-    }
+    swc_cli_impl::run()
 }
