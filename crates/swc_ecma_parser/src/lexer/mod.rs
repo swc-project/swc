7a975fbec9cccf836be4211892c3eae868c3c822
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-06 08:30:09 +0900

    more

--- crates/swc_ecma_parser/src/lexer/mod.rs
+++ crates/swc_ecma_parser/src/lexer/mod.rs
@@ -348,8 +348,8 @@ impl<'a> Lexer<'a> {
         // '|=', '&='
         if self.input.eat_byte(b'=') {
             return Ok(Token::AssignOp(match token {
-                BinOptoken::BitAnd => AssignOp::BitAndAssign,
-                BinOptoken::BitOr => AssignOp::BitOrAssign,
+                BinOpToken::BitAnd => AssignOp::BitAndAssign,
+                BinOpToken::BitOr => AssignOp::BitOrAssign,
                 _ => unreachable!(),
             }));
         }
