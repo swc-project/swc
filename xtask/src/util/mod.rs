d6c2d9d203e4bba53168c65958ffb11b1cd9e138
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-06 22:35:43 +0900

    Rename

--- xtask/src/util/mod.rs
+++ xtask/src/util/mod.rs
@@ -5,6 +5,8 @@ use std::{
 
 use anyhow::{Context, Result};
 
+pub mod cargo_bench;
+
 pub fn repository_root() -> Result<PathBuf> {
     let dir = env::var("CARGO_MANIFEST_DIR").context("failed to get manifest dir")?;
     Ok(Path::new(&*dir).parent().unwrap().to_path_buf())
