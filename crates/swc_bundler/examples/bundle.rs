8a97c066f582417b2c5fcbfdf9c29dbbee4e41fe
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-07 13:07:41 +0900

    Replace

--- crates/swc_bundler/examples/bundle.rs
+++ crates/swc_bundler/examples/bundle.rs
@@ -191,7 +191,7 @@ impl swc_bundler::Hook for Hook {
 
         Ok(vec![
             KeyValueProp {
-                key: PropName::Ident(Ident::new(js_word!("url"), span)),
+                key: PropName::Ident(Ident::new("url", span)),
                 value: Box::new(Expr::Lit(Lit::Str(Str {
                     span,
                     raw: None,
@@ -199,7 +199,7 @@ impl swc_bundler::Hook for Hook {
                 }))),
             },
             KeyValueProp {
-                key: PropName::Ident(Ident::new(js_word!("main"), span)),
+                key: PropName::Ident(Ident::new("main", span)),
                 value: Box::new(if module_record.is_entry {
                     Expr::Member(MemberExpr {
                         span,
@@ -207,7 +207,7 @@ impl swc_bundler::Hook for Hook {
                             span,
                             kind: MetaPropKind::ImportMeta,
                         })),
-                        prop: MemberProp::Ident(Ident::new(js_word!("main"), span)),
+                        prop: MemberProp::Ident(Ident::new("main", span)),
                     })
                 } else {
                     Expr::Lit(Lit::Bool(Bool { span, value: false }))
