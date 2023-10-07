a611accc7243ac7da5725aff5711b4920bf50f4a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-07 13:07:47 +0900

    Replace

--- crates/swc_bundler/tests/deno.rs
+++ crates/swc_bundler/tests/deno.rs
@@ -1113,7 +1113,7 @@ impl swc_bundler::Hook for Hook {
 
         Ok(vec![
             KeyValueProp {
-                key: PropName::Ident(Ident::new(js_word!("url"), span)),
+                key: PropName::Ident(Ident::new("url", span)),
                 value: Box::new(Expr::Lit(Lit::Str(Str {
                     span,
                     raw: None,
@@ -1121,7 +1121,7 @@ impl swc_bundler::Hook for Hook {
                 }))),
             },
             KeyValueProp {
-                key: PropName::Ident(Ident::new(js_word!("main"), span)),
+                key: PropName::Ident(Ident::new("main", span)),
                 value: Box::new(if module_record.is_entry {
                     Expr::Member(MemberExpr {
                         span,
@@ -1129,7 +1129,7 @@ impl swc_bundler::Hook for Hook {
                             span,
                             kind: MetaPropKind::ImportMeta,
                         })),
-                        prop: MemberProp::Ident(Ident::new(js_word!("main"), span)),
+                        prop: MemberProp::Ident(Ident::new("main", span)),
                     })
                 } else {
                     Expr::Lit(Lit::Bool(Bool { span, value: false }))
