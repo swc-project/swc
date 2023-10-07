47031dca95a0266e9cbcb70dfa954cf4ade00771
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-07 13:08:48 +0900

    Replace

--- crates/swc_ecma_transforms_proposal/src/decorators/legacy/metadata.rs
+++ crates/swc_ecma_transforms_proposal/src/decorators/legacy/metadata.rs
@@ -324,11 +324,7 @@ fn serialize_type(class_name: Option<&Ident>, param: Option<&TsTypeAnn>) -> Expr
             let item = serialize_type_node(class_name, ty);
 
             // One of the individual is global object, return immediately
-            if let Expr::Ident(Ident {
-                sym: js_word!("Object"),
-                ..
-            }) = item
-            {
+            if let Expr::Ident(Ident { sym: "Object", .. }) = item {
                 return item;
             }
 
