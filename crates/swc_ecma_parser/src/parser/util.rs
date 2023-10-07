a6fcee6d2f2865c499874e80f5b66325b8171391
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-07 14:34:17 +0900

    fix

--- crates/swc_ecma_parser/src/parser/util.rs
+++ crates/swc_ecma_parser/src/parser/util.rs
@@ -64,8 +64,8 @@ impl Context {
         }
     }
 
-    pub fn is_reserved_word(self, word: &JsWord) -> bool {
-        match *word {
+    pub fn is_reserved_word(self, word: &str) -> bool {
+        match word {
             "let" => self.strict,
             // SyntaxError in the module only, not in the strict.
             // ```JavaScript
