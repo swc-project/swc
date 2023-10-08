d933d92ade7a99f15a519ed6fddd3d21624f1917
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-08 16:02:52 +0900

    Use compact string

--- crates/swc_atoms/src/lib.rs
+++ crates/swc_atoms/src/lib.rs
@@ -14,6 +14,7 @@ use std::{
     rc::Rc,
 };
 
+use compact_str::CompactString;
 use once_cell::sync::Lazy;
 use serde::Serializer;
 
@@ -26,7 +27,7 @@ pub use self::{atom as js_word, Atom as JsWord};
 #[derive(Clone, Default)]
 #[cfg_attr(feature = "rkyv-impl", derive(rkyv::bytecheck::CheckBytes))]
 #[cfg_attr(feature = "rkyv-impl", repr(C))]
-pub struct Atom(string_cache::Atom<InternalWordStaticSet>);
+pub struct Atom(CompactString);
 
 /// Safety: We do not perform slicing of single [Atom] from multiple threads.
 /// In other words, typically [Atom] is created in a single thread (and in the
@@ -48,7 +49,7 @@ impl Atom {
 
     #[inline]
     pub fn to_ascii_lowercase(&self) -> Self {
-        Self(self.0.to_ascii_lowercase())
+        Self(self.0.to_ascii_lowercase().into())
     }
 }
 
@@ -235,8 +236,6 @@ where
 
 #[doc(hidden)]
 pub type CahcedAtom = Lazy<Atom>;
-
-include!(concat!(env!("OUT_DIR"), "/internal_word.rs"));
 
 /// This should be used as a key for hash maps and hash sets.
 ///
