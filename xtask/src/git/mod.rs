61a9fd863e6846cb25bd6cd9530d86fca62f2e99
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-10 12:09:11 +0900

    git subcommand

--- xtask/src/git/mod.rs
+++ xtask/src/git/mod.rs
@@ -1,9 +1,17 @@
 use anyhow::Result;
-use clap::Args;
+use clap::{Args, Subcommand};
 
 #[derive(Debug, Args)]
-pub(super) struct GitCmd {}
+pub(super) struct GitCmd {
+    #[clap(subcommand)]
+    cmd: Inner,
+}
+
+#[derive(Debug, Subcommand)]
+pub(super) enum Inner {}
 
 impl GitCmd {
-    pub fn run(self) -> Result<()> {}
+    pub fn run(self) -> Result<()> {
+        match self.cmd {}
+    }
 }
