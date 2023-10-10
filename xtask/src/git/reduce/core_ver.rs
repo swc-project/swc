736ae996b4f29e182426af4268a2f2ec705ed2e2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-10 12:12:53 +0900

    git reduce

--- xtask/src/git/reduce/core_ver.rs
+++ xtask/src/git/reduce/core_ver.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Args;
 
+/// Reduce the difference of the versions of `swc_core`s to the list of commits
+/// and pull requests.
 #[derive(Debug, Args)]
 pub(super) struct CoreVerCmd {}
 
