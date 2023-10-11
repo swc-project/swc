c5d517dc307483271b066cced91941fda631e22d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-11 13:48:04 +0900

    Update test refs

--- crates/swc_ecma_transforms_compat/tests/optional-chaining-loose/issue-7559/output.js
+++ crates/swc_ecma_transforms_compat/tests/optional-chaining-loose/issue-7559/output.js
@@ -1,7 +1,14 @@
+var _x = /*#__PURE__*/ new WeakMap();
 class Foo {
-    #x;
     test() {
         var _this;
-        (_this = this) == null ? void 0 : _this.y.#x;
+        var _this_y, _this_y1;
+        _this_y === null || _this_y === void 0 ? void 0 : _class_private_field_get(_this_y1 = (_this = this) == null ? void 0 : _this.y, _x);
+    }
+    constructor(){
+        _class_private_field_init(this, _x, {
+            writable: true,
+            value: void 0
+        });
     }
 }
