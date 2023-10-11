82f01739ab81dabb3390d67e5d1479f8dafa5f6c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-11 13:46:33 +0900

    Update test refs

---
+++ crates/swc_ecma_transforms_compat/tests/class-properties/issue-7561/output.js
@@ -0,0 +1,13 @@
+var _x = /*#__PURE__*/ new WeakMap();
+class Foo {
+    test() {
+        var _this_y, _this_y1;
+        _this_y === null || _this_y === void 0 ? void 0 : _class_private_field_get(_this_y1 = this?.y, _x);
+    }
+    constructor(){
+        _class_private_field_init(this, _x, {
+            writable: true,
+            value: void 0
+        });
+    }
+}
