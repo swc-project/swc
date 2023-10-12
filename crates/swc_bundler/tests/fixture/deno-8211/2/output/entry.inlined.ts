fe0bf5746fb40ec66719858f149a4e5c45f10749
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-10-12 12:26:04 +0900

    chore(ci): Fix CI action (#8107)

--- crates/swc_bundler/tests/fixture/deno-8211/2/output/entry.inlined.ts
+++ crates/swc_bundler/tests/fixture/deno-8211/2/output/entry.inlined.ts
@@ -1,10 +1,13 @@
-class Zone {
-}
-class FixedOffsetZone extends Zone {
-}
-class Info {
+var Zone;
+Zone = class Zone {
+};
+var FixedOffsetZone;
+FixedOffsetZone = class FixedOffsetZone extends Zone {
+};
+var Info;
+Info = class Info {
     use() {
         console.log(FixedOffsetZone);
     }
-}
+};
 export { Zone as Zone, Info as Info, FixedOffsetZone as FixedOffsetZone };
