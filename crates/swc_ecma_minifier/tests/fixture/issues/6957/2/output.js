7da3f52485062eb23c719e1236b2fee90c228bdd
magic-akari <akari.ccino@gmail.com>
2023-10-13 01:07:00 -0500

    feat(es/minifier): Evaluate more `toFixed` expressions (#8109)

--- crates/swc_ecma_minifier/tests/fixture/issues/6957/2/output.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/6957/2/output.js
@@ -1,2 +1,2 @@
-assertEquals("1", 0.5.toFixed(0), "0.5.toFixed(0)");
-assertEquals("-1", (-0.5).toFixed(0), "(-0.5).toFixed(0)");
+assertEquals("1", "1", "0.5.toFixed(0)");
+assertEquals("-1", "-1", "(-0.5).toFixed(0)");
