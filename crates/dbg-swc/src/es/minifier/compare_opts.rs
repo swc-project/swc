ad67debe70fafacd7617d0ea929f840da1427516
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-19 09:38:20 -0700

    sum

--- crates/dbg-swc/src/es/minifier/compare_opts.rs
+++ crates/dbg-swc/src/es/minifier/compare_opts.rs
@@ -6,6 +6,7 @@ use swc_common::SourceMap;
 use swc_ecma_minifier::option::{CompressOptions, MangleOptions};
 
 use crate::util::{
+    all_js_files,
     minifier::{get_minified, get_minified_with_opts},
     print_js,
 };
@@ -19,30 +20,42 @@ pub struct CompareOptsCommand {
 
 impl CompareOptsCommand {
     pub fn run(self, cm: Arc<SourceMap>) -> Result<()> {
-        let default_record = get_minified(cm.clone(), &self.path, true, true)?;
-        let default_code = print_js(cm.clone(), &default_record.module, true)
-            .context("failed to convert ast to code")?;
+        let files = all_js_files(&self.path)?;
 
-        eprintln!("default: {} bytes", default_code.as_bytes().len());
+        let mut default_sum = 0;
+        let mut new_sum = 0;
 
-        let new_record = get_minified_with_opts(
-            cm.clone(),
-            &self.path,
-            Some(CompressOptions {
-                keep_classnames: true,
-                keep_fnames: true,
-                ..Default::default()
-            }),
-            Some(MangleOptions {
-                keep_class_names: true,
-                keep_fn_names: true,
-                ..Default::default()
-            }),
-        )?;
-        let new_code =
-            print_js(cm, &new_record.module, true).context("failed to convert ast to code")?;
+        for file in files {
+            let default_record = get_minified(cm.clone(), &file, true, true)?;
+            let default_code = print_js(cm.clone(), &default_record.module, true)
+                .context("failed to convert ast to code")?;
 
-        eprintln!("new: {} bytes", new_code.as_bytes().len());
+            eprintln!("default: {} bytes", default_code.as_bytes().len());
+            default_sum += default_code.as_bytes().len();
+
+            let new_record = get_minified_with_opts(
+                cm.clone(),
+                &self.path,
+                Some(CompressOptions {
+                    keep_classnames: true,
+                    keep_fnames: true,
+                    ..Default::default()
+                }),
+                Some(MangleOptions {
+                    keep_class_names: true,
+                    keep_fn_names: true,
+                    ..Default::default()
+                }),
+            )?;
+            let new_code = print_js(cm.clone(), &new_record.module, true)
+                .context("failed to convert ast to code")?;
+
+            eprintln!("new: {} bytes", new_code.as_bytes().len());
+            new_sum += new_code.as_bytes().len();
+        }
+
+        eprintln!("default (total): {} bytes", default_sum);
+        eprintln!("new (total): {} bytes", new_sum);
 
         Ok(())
     }
