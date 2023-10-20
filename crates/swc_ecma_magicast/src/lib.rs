75edcdbda1046f8e1191f9f69eecfb8ebb28226e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-20 16:12:22 -0700

    more

--- crates/swc_ecma_magicast/src/lib.rs
+++ crates/swc_ecma_magicast/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut};
+
 pub struct ModuleNode<'a> {
     pub exports: ModuleExports<'a>,
 }
@@ -6,8 +8,20 @@ pub struct ModuleExports<'a> {
     pub default: OptionalNode<ExportItemNode<'a>>,
 }
 
+impl<'a> Index<String> for ModuleExports<'a> {
+    type Output = ExportItemNode<'a>;
+}
+
+impl<'a> IndexMut<String> for ModuleExports<'a> {}
+
 pub struct OptionalNode<T> {
     node: Option<T>,
+}
+
+impl<N> OptionalNode<N> {
+    pub fn ensure(&mut self) -> &mut N {
+        self.node.get_or_insert_with(|| panic!("unreachable"))
+    }
 }
 
 pub struct ExportItemNode<'a> {}
