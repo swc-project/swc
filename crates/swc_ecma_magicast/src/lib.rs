2ae2992bab79b32886d510ed02f75f8edc4b3acf
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-20 17:21:48 -0700

    new_proxied

--- crates/swc_ecma_magicast/src/lib.rs
+++ crates/swc_ecma_magicast/src/lib.rs
@@ -72,6 +72,10 @@ pub struct BindingRef {}
 
 pub trait Proxy {
     type Item;
+
+    /// Creates a new proxied data using self as the context, and new_data as
+    /// the data
+    fn new_proxied(&self, new_data: Self::Item) -> Result<Box<Self>, ()>;
 }
 
 pub trait Ensurable: Proxy {
