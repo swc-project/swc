117c579278e323933fae66d152d4702bf8507440
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-22 00:40:50 -0700

    Data<T>

--- crates/swc_ecma_magicast/src/node.rs
+++ crates/swc_ecma_magicast/src/node.rs
@@ -2,8 +2,18 @@ use std::{cell::RefCell, rc::Rc};
 
 use swc_ecma_ast::Program;
 
-pub(crate) struct Data<T>(Rc<RefCell<Program>>, Box<dyn Fn(RefCell<Program>) -> T>);
+pub(crate) struct Data<T>(
+    Rc<RefCell<Program>>,
+    Box<dyn Fn(&Program) -> &T>,
+    Box<dyn Fn(&mut Program) -> &mut T>,
+);
 
 impl<T> Data<T> {
-    pub fn new_root(root: Program) -> Data<Program> {}
+    pub fn new_root(root: Program) -> Data<Program> {
+        Data(
+            Rc::new(RefCell::new(root)),
+            Box::new(|root| root),
+            Box::new(|root| root),
+        )
+    }
 }
