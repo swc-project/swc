b801aa121e191d5f563dd679d0b6724af2fcede1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 05:34:32 +0900

    Update test refs

--- crates/swc_ecma_transforms_react/tests/jsx/fixture/react-automatic/should-disallow-spread-children/output.mjs
+++ crates/swc_ecma_transforms_react/tests/jsx/fixture/react-automatic/should-disallow-spread-children/output.mjs
@@ -1,1 +1,6 @@
-/*#__PURE__*/ React.createElement("div", null, ...children);
+/*#__PURE__*/ import { jsx as _jsx } from "react/jsx-runtime";
+_jsx("div", {
+    children: [
+        ...children
+    ]
+});
