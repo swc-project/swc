936466ef2e57fc0eb8cd9f8d19ae52198c178c06
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 06:18:27 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-accessors--to-es2015/undecorated-static-public/output.js
+++ crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-accessors--to-es2015/undecorated-static-public/output.js
@@ -1,3 +1,40 @@
+function _class_apply_descriptor_get(receiver, descriptor) {
+    if (descriptor.get) {
+        return descriptor.get.call(receiver);
+    }
+    return descriptor.value;
+}
+function _class_apply_descriptor_set(receiver, descriptor, value) {
+    if (descriptor.set) {
+        descriptor.set.call(receiver, value);
+    } else {
+        if (!descriptor.writable) {
+            throw new TypeError("attempted to set read only private field");
+        }
+        descriptor.value = value;
+    }
+}
+function _class_check_private_static_field_descriptor(descriptor, action) {
+    if (descriptor === undefined) {
+        throw new TypeError("attempted to " + action + " private static field before its declaration");
+    }
+}
+function _class_static_private_field_spec_get(receiver, classConstructor, descriptor) {
+    _class_check_private_static_access(receiver, classConstructor);
+    _class_check_private_static_field_descriptor(descriptor, "get");
+    return _class_apply_descriptor_get(receiver, descriptor);
+}
+function _class_static_private_field_spec_set(receiver, classConstructor, descriptor, value) {
+    _class_check_private_static_access(receiver, classConstructor);
+    _class_check_private_static_field_descriptor(descriptor, "set");
+    _class_apply_descriptor_set(receiver, descriptor, value);
+    return value;
+}
+function _class_check_private_static_access(receiver, classConstructor) {
+    if (receiver !== classConstructor) {
+        throw new TypeError("Private static access of wrong provenance");
+    }
+}
 var _computedKey;
 const dec = ()=>{};
 _computedKey = 'c';
