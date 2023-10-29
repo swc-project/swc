983eede39fb2777aac85fb8c9405ac5a9990aa47
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 04:30:13 +0900

    Migrate decorators test

--- crates/swc_ecma_transforms_proposal/tests/decorators.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorators.rs
@@ -10,7 +10,10 @@ use swc_common::{chain, comments::SingleThreadedComments, Mark};
 use swc_ecma_parser::{EsConfig, Syntax, TsConfig};
 use swc_ecma_transforms_base::{assumptions::Assumptions, resolver};
 use swc_ecma_transforms_proposal::decorator_2022_03::decorator_2022_03;
-use swc_ecma_transforms_testing::{test_fixture, FixtureTestConfig};
+use swc_ecma_transforms_testing::{
+    babel_like::{BabelLikeFixtureTest, PassContext},
+    test_fixture, FixtureTestConfig,
+};
 use swc_ecma_visit::Fold;
 
 fn syntax_default() -> Syntax {
@@ -29,17 +32,15 @@ fn exec(input: PathBuf) {
 }
 
 fn exec_inner(input: PathBuf) {
-    let code = std::fs::read_to_string(&input).unwrap();
+    let static_block_mark = Mark::new();
 
-    swc_ecma_transforms_testing::exec_tr(
-        "decorator",
-        Syntax::Typescript(TsConfig {
+    BabelLikeFixtureTest::new(&input)
+        .syntax(Syntax::Typescript(TsConfig {
             decorators: true,
             ..Default::default()
-        }),
-        |t| create_pass(t.comments.clone(), &input),
-        &code,
-    );
+        }))
+        .add_factory(|c, name, opts| create_pass(c, name, opts, static_block_mark))
+        .execute();
 }
 
 #[testing::fixture("tests/decorators/**/input.js")]
@@ -56,118 +57,46 @@ fn fixture_inner(input: PathBuf) {
         input.extension().unwrap().to_string_lossy()
     ));
 
-    test_fixture(
-        syntax_default(),
-        &|t| create_pass(t.comments.clone(), &input),
-        &input,
-        &output,
-        FixtureTestConfig {
-            allow_error: true,
-            ..Default::default()
-        },
-    );
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(deny_unknown_fields, rename_all = "camelCase")]
-struct BabelTestOptions {
-    #[serde(default)]
-    assumptions: Assumptions,
-
-    #[serde(default)]
-    plugins: Vec<BabelPluginEntry>,
-
-    #[serde(default)]
-    min_node_version: String,
-
-    #[serde(default)]
-    throws: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(deny_unknown_fields, rename_all = "camelCase", untagged)]
-enum BabelPluginEntry {
-    NameOnly(String),
-    WithConfig(String, BabelPluginOption),
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(deny_unknown_fields, untagged, rename_all = "camelCase")]
-enum BabelPluginOption {
-    Decorator { version: String },
-}
-
-fn read_options_json(input: &Path) -> BabelTestOptions {
-    let mut options_path = input.to_path_buf();
-    options_path.set_file_name("options.json");
-
-    if options_path.exists() {
-        let s = std::fs::read_to_string(&options_path).unwrap();
-        println!("Options: {}", s);
-        return serde_json::from_str(&s).expect("failed to read options.json");
-    }
-
-    println!("Reading options from {:?}", options_path);
-
-    // Look for parent directory
-
-    read_options_json(options_path.parent().unwrap())
-}
-
-fn create_pass(comments: Rc<SingleThreadedComments>, input: &Path) -> Box<dyn Fold> {
-    let options_json = read_options_json(input);
-
-    let unresolved_mark = Mark::new();
-    let top_level_mark = Mark::new();
-
-    let mut pass: Box<dyn Fold> = Box::new(chain!(
-        resolver(unresolved_mark, top_level_mark, false),
-        decorator_2022_03()
-    ));
-
-    macro_rules! add {
-        ($e:expr) => {{
-            pass = Box::new(chain!(pass, $e));
-        }};
-    }
-
     let static_block_mark = Mark::new();
 
-    for plugin in &options_json.plugins {
-        match plugin {
-            BabelPluginEntry::NameOnly(name) => match &**name {
-                "proposal-class-properties" => {
-                    add!(swc_ecma_transforms_compat::es2022::static_blocks(
-                        static_block_mark
-                    ));
-                    add!(swc_ecma_transforms_compat::es2022::class_properties(
-                        Some(comments.clone()),
-                        Default::default()
-                    ));
-                    continue;
-                }
+    BabelLikeFixtureTest::new(&input)
+        .syntax(Syntax::Es(EsConfig {
+            decorators: true,
+            auto_accessors: true,
+            allow_super_outside_method: true,
+            decorators_before_export: true,
+            ..Default::default()
+        }))
+        .add_factory(|c, name, opts| create_pass(c, name, opts, static_block_mark))
+        .allow_error()
+        .fixture(&output);
+}
 
-                "proposal-private-methods" => {
-                    add!(swc_ecma_transforms_compat::es2022::class_properties(
-                        Some(comments.clone()),
-                        Default::default()
-                    ));
-                    continue;
-                }
+fn create_pass(
+    c: &PassContext,
+    name: &str,
+    options: Option<serde_json::Value>,
+    static_block_mark: Mark,
+) -> Option<Box<dyn Fold>> {
+    match name {
+        "proposal-class-properties" => Some(Box::new(chain!(
+            swc_ecma_transforms_compat::es2022::static_blocks(static_block_mark),
+            swc_ecma_transforms_compat::es2022::class_properties(
+                Some(c.comments.clone()),
+                Default::default()
+            )
+        ))),
 
-                "proposal-class-static-block" => {
-                    add!(swc_ecma_transforms_compat::es2022::static_blocks(
-                        static_block_mark
-                    ));
-                    continue;
-                }
-                _ => {}
-            },
-            BabelPluginEntry::WithConfig(name, config) => {}
-        }
+        "proposal-private-methods" => Some(Box::new(
+            swc_ecma_transforms_compat::es2022::class_properties(
+                Some(c.comments.clone()),
+                Default::default(),
+            ),
+        )),
 
-        dbg!(&plugin);
+        "proposal-class-static-block" => Some(Box::new(
+            swc_ecma_transforms_compat::es2022::static_blocks(static_block_mark),
+        )),
+        _ => None,
     }
-
-    pass
 }
