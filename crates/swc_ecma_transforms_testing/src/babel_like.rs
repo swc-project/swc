e478b761e146ecc972e4ec1c8e3576eea8213ef4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 03:22:10 +0900

    more

--- crates/swc_ecma_transforms_testing/src/babel_like.rs
+++ crates/swc_ecma_transforms_testing/src/babel_like.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 
+use serde::Deserialize;
 use swc_common::{comments::SingleThreadedComments, Mark};
+use swc_ecma_transforms_base::assumptions::Assumptions;
 
 /// These tests use `options.json`.
 ///
@@ -27,8 +29,17 @@ impl<'a> BabelLikeFixtureTest<'a> {
     }
 }
 
+fn read_options_json(input: &Path) -> BabelOptions {}
+
+#[derive(Deserialize)]
+struct BabelOptions {
+    #[serde(default)]
+    pub assumptions: Assumptions,
+}
+
 #[derive(Clone)]
 pub struct BabelPassBuilder {
+    pub assumptions: Assumptions,
     pub unresolved_mark: Mark,
     pub top_level_mark: Mark,
 
