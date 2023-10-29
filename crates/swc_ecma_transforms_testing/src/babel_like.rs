312d9ab81906b0bf270c5c84b0bdacf527ac34a0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 04:01:48 +0900

    parse

--- crates/swc_ecma_transforms_testing/src/babel_like.rs
+++ crates/swc_ecma_transforms_testing/src/babel_like.rs
@@ -3,7 +3,8 @@ use std::path::Path;
 use serde::Deserialize;
 use serde_json::Value;
 use swc_common::{chain, comments::SingleThreadedComments, Mark};
-use swc_ecma_parser::Syntax;
+use swc_ecma_ast::EsVersion;
+use swc_ecma_parser::{parse_file_as_program, Syntax};
 use swc_ecma_transforms_base::{
     assumptions::Assumptions, fixer::fixer, hygiene::hygiene, resolver,
 };
@@ -57,48 +58,83 @@ impl<'a> BabelLikeFixtureTest<'a> {
     }
 
     fn run(self) -> TestOutput {
-        let options = parse_options::<BabelOptions>(self.input.parent().unwrap());
+        let output = testing::run_test(false, |cm, handler| {
+            let options = parse_options::<BabelOptions>(self.input.parent().unwrap());
 
-        let comments = SingleThreadedComments::default();
-        let mut builder = BabelPassBuilder {
-            assumptions: options.assumptions,
-            unresolved_mark: Mark::new(),
-            top_level_mark: Mark::new(),
-            comments: comments.clone(),
-        };
-
-        let mut pass: Box<dyn 'a + Fold> = Box::new(resolver(
-            builder.unresolved_mark,
-            builder.top_level_mark,
-            self.syntax.typescript(),
-        ));
-
-        // Build pass using babel options
-
-        //
-        for plugin in options.plugins {
-            let (name, options) = match plugin {
-                BabelPluginEntry::NameOnly(name) => (name, None),
-                BabelPluginEntry::WithConfig(name, options) => (name, Some(options)),
+            let comments = SingleThreadedComments::default();
+            let mut builder = BabelPassBuilder {
+                assumptions: options.assumptions,
+                unresolved_mark: Mark::new(),
+                top_level_mark: Mark::new(),
+                comments: comments.clone(),
             };
 
-            let mut done = false;
-            for factory in &mut self.factories {
-                if let Some(built) = factory(name.clone(), options.clone()) {
-                    pass = Box::new(chain!(pass, built));
-                    done = true;
-                    break;
+            let mut pass: Box<dyn 'a + Fold> = Box::new(resolver(
+                builder.unresolved_mark,
+                builder.top_level_mark,
+                self.syntax.typescript(),
+            ));
+
+            // Build pass using babel options
+
+            //
+            for plugin in options.plugins {
+                let (name, options) = match plugin {
+                    BabelPluginEntry::NameOnly(name) => (name, None),
+                    BabelPluginEntry::WithConfig(name, options) => (name, Some(options)),
+                };
+
+                let mut done = false;
+                for factory in &mut self.factories {
+                    if let Some(built) = factory(name.clone(), options.clone()) {
+                        pass = Box::new(chain!(pass, built));
+                        done = true;
+                        break;
+                    }
+                }
+
+                if !done {
+                    panic!("Unknown plugin: {}", name);
                 }
             }
 
-            if !done {
-                panic!("Unknown plugin: {}", name);
+            pass = Box::new(chain!(pass, hygiene(), fixer(Some(&comments.clone()))));
+
+            // Run pass
+
+            let fm = cm.load_file(self.input).expect("failed to load file");
+
+            let mut errors = vec![];
+            let input_program = parse_file_as_program(
+                &fm,
+                self.syntax,
+                EsVersion::latest(),
+                Some(&comments),
+                &mut errors,
+            );
+
+            let errored = !errors.is_empty();
+
+            for e in errors {
+                e.into_diagnostic(&handler).emit();
             }
-        }
 
-        pass = Box::new(chain!(pass, hygiene(), fixer(Some(&comments.clone()))));
+            let input_program = match input_program {
+                Ok(v) => v,
+                Err(err) => {
+                    err.into_diagnostic(&handler).emit();
+                    panic!("failed to parse file");
+                }
+            };
 
-        // Run pass
+            if errored {
+                panic!("failed to parse file");
+            }
+
+            Ok(())
+        });
+
+        TestOutput { code: () }
     }
 
     /// Execute ussing node.js
