419cf09709aec8f2798c97a0079ee9ea2b6df167
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 02:32:02 +0900

    Update test refs

--- crates/swc_ecma_transforms_compat/tests/classes/create-super-1/output.js
+++ crates/swc_ecma_transforms_compat/tests/classes/create-super-1/output.js
@@ -1,6 +1,6 @@
-let Foo = /*#__PURE__*/ function(UnknownNativeClass) {
+let Foo = /*#__PURE__*/ function(UnknownNativeClass1) {
     "use strict";
-    _inherits(Foo, UnknownNativeClass);
+    _inherits(Foo, UnknownNativeClass1);
     var _super = _create_super(Foo);
     function Foo() {
         _class_call_check(this, Foo);
