3dc4e1e02d3e594da6301ca4c80aaee582642fd7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-10-30 17:46:24 +0900

    feat(es/preset-env): Update compat data (#8194)
    **Related issue:**
    
     - Closes #8193.

--- crates/swc_ecma_transforms_compat/tests/__swc_snapshots__/tests/es2015_destructuring.rs/destructuring_for_in_loose.js
+++ crates/swc_ecma_transforms_compat/tests/__swc_snapshots__/tests/es2015_destructuring.rs/destructuring_for_in_loose.js
@@ -1,9 +1,9 @@
 for(var _$ref in obj){
-    var name = _$ref[0], value = _$ref[1];
-    print("Name: " + name + " Value: " + value);
+    var _$name = _$ref[0], _$value = _$ref[1];
+    print("Name: " + _$name + " Value: " + _$value);
 }
 var _$ref1;
 for(ref in obj){
-    _$ref1 = ref, name = _$ref1[0], value = _$ref1[1], _$ref1;
-    print("Name: " + name + " Value: " + value);
+    _$ref1 = ref, _$name = _$ref1[0], _$value = _$ref1[1], _$ref1;
+    print("Name: " + _$name + " Value: " + _$value);
 }
