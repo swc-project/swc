2dcef1d6c04bc939cb367ef2a276dc742ec480b4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-30 13:31:03 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-8xxx/8148/output/input.js
+++ crates/swc/tests/fixture/issues-8xxx/8148/output/input.js
@@ -2,7 +2,7 @@ function _toBeMocked() {
     return "I am the original function";
 }
 export var toBeMocked = _toBeMocked;
-export var _$mock = {
+var _$mock = {
     get toBeMocked () {
         return toBeMocked;
     },
@@ -10,3 +10,4 @@ export var _$mock = {
         toBeMocked = mock;
     }
 };
+export { _$mock as mock };
