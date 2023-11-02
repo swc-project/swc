8ca9a282dc8629924f49c74f223bd4693ef74c7d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-03 07:12:13 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-8xxx/8210/output/1.js
@@ -0,0 +1,15 @@
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+const Component = ()=>{
+    return /*#__PURE__*/ _jsxDEV("p", {
+        thing: /*#__PURE__*/ _jsxDEV("a", {}, void 0, false, {
+            fileName: "$DIR/tests/fixture/issues-8xxx/8210/input/1.js",
+            lineNumber: 2,
+            columnNumber: 23
+        }, void 0),
+        children: "Hello"
+    }, void 0, false, {
+        fileName: "$DIR/tests/fixture/issues-8xxx/8210/input/1.js",
+        lineNumber: 2,
+        columnNumber: 13
+    }, this);
+};
