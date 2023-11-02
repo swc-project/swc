44d37bb7567df3deef4ee1b4817a5c14f9ecabcf
magic-akari <akari.ccino@gmail.com>
2023-11-02 11:58:07 +0800

    chore: update test cases

--- crates/swc/tests/tsc-references/exportsAndImports1-es6.2.minified.js
+++ crates/swc/tests/tsc-references/exportsAndImports1-es6.2.minified.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", {
         return C;
     },
     D: function() {
-        return D;
+        return D1;
     },
     E: function() {
         return E1;
@@ -30,11 +30,11 @@ Object.defineProperty(exports, "__esModule", {
         return v;
     }
 });
-var E, E1, D, M, v = 1;
+var E, D, E1, D1, M, v = 1;
 function f() {}
 class C {
 }
-(E = E1 || (E1 = {}))[E.A = 0] = "A", E[E.B = 1] = "B", E[E.C = 2] = "C", M || (M = {});
+(E = E1 || (E1 = {}))[E.A = 0] = "A", E[E.B = 1] = "B", E[E.C = 2] = "C", (D = D1 || (D1 = {}))[D.A = 0] = "A", D[D.B = 1] = "B", D[D.C = 2] = "C", M || (M = {});
 const a = M.x;
 //// [t2.ts]
 Object.defineProperty(exports, "__esModule", {
--- crates/swc/tests/tsc-references/exportsAndImports1.2.minified.js
+++ crates/swc/tests/tsc-references/exportsAndImports1.2.minified.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", {
         return C;
     },
     D: function() {
-        return D;
+        return D1;
     },
     E: function() {
         return E1;
@@ -30,12 +30,12 @@ Object.defineProperty(exports, "__esModule", {
         return v;
     }
 });
-var E, E1, D, M, _class_call_check = require("@swc/helpers/_/_class_call_check"), v = 1;
+var E, D, E1, D1, M, _class_call_check = require("@swc/helpers/_/_class_call_check"), v = 1;
 function f() {}
 var C = function C() {
     _class_call_check._(this, C);
 };
-(E = E1 || (E1 = {}))[E.A = 0] = "A", E[E.B = 1] = "B", E[E.C = 2] = "C", M || (M = {});
+(E = E1 || (E1 = {}))[E.A = 0] = "A", E[E.B = 1] = "B", E[E.C = 2] = "C", (D = D1 || (D1 = {}))[D.A = 0] = "A", D[D.B = 1] = "B", D[D.C = 2] = "C", M || (M = {});
 var a = M.x;
 //// [t2.ts]
 Object.defineProperty(exports, "__esModule", {
