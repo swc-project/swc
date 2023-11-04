8a220758800d9d84ae31fa6ac3e51082b327332c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-05 06:11:18 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_3146_4/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_3146_4/output.js
@@ -1,7 +1,7 @@
 var g = "PASS";
-(function(f1) {
-    var g1 = "FAIL";
-    f1("console.log(g)", g1[g1]);
-})(function(a) {
+(function (f) {
+    var g = "FAIL";
+    f("console.log(g)", g[g]);
+})(function (a) {
     eval(a);
 });
