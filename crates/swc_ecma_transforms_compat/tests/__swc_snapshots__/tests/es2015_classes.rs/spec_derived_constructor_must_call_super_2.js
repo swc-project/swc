11382dbbca834a312cfe31b0f440ea06b432390d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-05 06:09:07 +0900

    Update test refs

--- crates/swc_ecma_transforms_compat/tests/__swc_snapshots__/tests/es2015_classes.rs/spec_derived_constructor_must_call_super_2.js
+++ crates/swc_ecma_transforms_compat/tests/__swc_snapshots__/tests/es2015_classes.rs/spec_derived_constructor_must_call_super_2.js
@@ -1,6 +1,6 @@
-var Foo = /*#__PURE__*/ function(Bar) {
+var Foo = /*#__PURE__*/ function(Bar1) {
     "use strict";
-    _inherits(Foo, Bar);
+    _inherits(Foo, Bar1);
     var _super = _create_super(Foo);
     function Foo() {
         _class_call_check(this, Foo);
