b620898a0b0358233853e9bd341bac7b97ce2cd9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-10 20:43:51 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-8xxx/8199/output/1.js
+++ crates/swc/tests/fixture/issues-8xxx/8199/output/1.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 export const IconSpecHotkey = (param)=>{
     let { icon } = param;
-    return /*#__PURE__*/ _jsx("div", {
+    return _jsx("div", {
         children: icon
     });
 };
