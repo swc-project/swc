9412c050d4f1c6d16b820645ced6815f239af2be
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-16 12:21:41 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-8xxx/8265/1/output/src/modules/moduleB/index.ts
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "moduleB", {
+    enumerable: true,
+    get: function() {
+        return moduleB;
+    }
+});
+const moduleB = ()=>console.log("This is module B!");
