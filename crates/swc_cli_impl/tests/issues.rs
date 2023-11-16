189b4d28ed4481125f152c19923aaf370e0d1ea6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-16 11:54:07 +0900

    Add a test

--- crates/swc_cli_impl/tests/issues.rs
+++ crates/swc_cli_impl/tests/issues.rs
@@ -1,5 +1,6 @@
 use std::{
-    fs::{self, create_dir},
+    fs::{self, create_dir, create_dir_all, soft_link},
+    path::Path,
     process::Command,
 };
 
@@ -12,19 +13,49 @@ fn cli() -> Result<Command> {
 }
 
 #[test]
-fn issue_8139_1() -> Result<()> {
-    let cwd = TempDir::new()?;
+fn issue_8265_1() -> Result<()> {
+    let pwd = Path::new("tests/fixture-manual/8265");
+    let tmp = TempDir::new()?;
 
-    create_dir(cwd.path().join("src"))?;
-    fs::write(
-        cwd.join("src").join("main.ts"),
-        "console.log('Hello, world!');",
-    )?;
+    create_dir_all(tmp.path().join("src/modules/moduleA"))?;
+    create_dir_all(tmp.path().join("src/modules/moduleB"))?;
+
+    symlink(&pwd.join(".swcrc"), &tmp.path().join(".swcrc"));
+    symlink(&pwd.join("src/index.ts"), &tmp.path().join("src/index.ts"));
+    symlink(&pwd.join("src/index.ts"), &tmp.path().join("src/index.ts"));
+    symlink(
+        &pwd.join("src/modules/moduleA/index.ts"),
+        &tmp.path().join("src/modules/moduleA/index.ts"),
+    );
+    symlink(
+        &pwd.join("src/modules/moduleB/index.ts"),
+        &tmp.path().join("src/modules/moduleB/index.ts"),
+    );
 
     let mut cmd = cli()?;
-    cmd.current_dir(&cwd).arg(".").arg("-d").arg("dist");
+    cmd.current_dir(&tmp)
+        .arg("compile")
+        .arg("--source-maps")
+        .arg("false")
+        .arg("--config-file")
+        .arg(".swcrc")
+        .arg("--out-file")
+        .arg("src/index.js");
 
     cmd.assert().success();
 
     Ok(())
+}
+
+/// ln -s $a $b
+fn symlink(a: &Path, b: &Path) {
+    #[cfg(unix)]
+    {
+        std::os::unix::fs::symlink(a, b).unwrap();
+    }
+
+    #[cfg(windows)]
+    {
+        std::os::windows::fs::symlink_file(a, b).unwrap();
+    }
 }
