5444743d15ab21a752449aa8d967f1ff2aacf8fb
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-16 11:42:17 +0900

    cmd

--- crates/swc_cli_impl/tests/issues.rs
+++ crates/swc_cli_impl/tests/issues.rs
@@ -1,11 +1,26 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use assert_cmd::prelude::*;
+use assert_fs::TempDir;
+
+fn cli() -> Result<Command> {
+    Command::cargo_bin("swc").context("Failed to get swc binary")
+}
 
 #[test]
 fn issue_8139_1() -> Result<()> {
-    Command::cargo_bin("swc")?;
+    let cwd = TempDir::new()?;
+
+    fs::write(
+        cwd.join("src").join("main.ts"),
+        "console.log('Hello, world!');",
+    )?;
+
+    let mut cmd = cli()?;
+    cmd.current_dir(&cwd).arg(".").arg("-d").arg("dist");
+
+    cmd.assert().success();
 
     Ok(())
 }
