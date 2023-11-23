01e2c7fc5ab71d55c522e48eae9e3e08d8bf418d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-11-23 09:54:57 +0900

    fix(es/minifier): Fix `if_return` bug related to `await` and `yield` (#8328)
    **Related issue:**
    
     - Closes #8324

---
+++ crates/swc/tests/fixture/issues-8xxx/8324/output/1.js
@@ -0,0 +1,19 @@
+function Deferred() {
+    const deferred = this;
+    deferred.promise = new Promise(function(resolve, reject) {
+        deferred.resolve = resolve, deferred.reject = reject;
+    });
+}
+export async function bug() {
+    const s = "next";
+    if (!window[s]) for(window[s] = new Deferred();;)if (window.current) await window.current.promise;
+    else {
+        window.current = window[s];
+        try {
+            return await window[s].promise;
+        } finally{
+            delete window.current;
+        }
+    }
+    return await window[s].promise;
+}
