88d94c7d54e1a28bf63374539aa01fa88cde7d17
magic-akari <akari.ccino@gmail.com>
2023-12-05 01:49:15 +0800

    refactor(html/minifier): Support stable rust

--- crates/swc_html_minifier/src/lib.rs
+++ crates/swc_html_minifier/src/lib.rs
@@ -2271,32 +2271,29 @@ impl Minifier<'_> {
                 let swc_css_ast::Stylesheet { rules, .. } = &stylesheet;
 
                 // Because CSS is grammar free, protect for fails
-                if let Some(swc_css_ast::Rule::QualifiedRule(box swc_css_ast::QualifiedRule {
-                    block,
-                    ..
-                })) = rules.first()
-                {
-                    swc_css_codegen::Emit::emit(&mut gen, &block).unwrap();
-
-                    minified = minified[1..minified.len() - 1].to_string();
-                } else {
+                let Some(swc_css_ast::Rule::QualifiedRule(qualified_rule)) = rules.first() else {
                     return None;
-                }
+                };
+
+                let swc_css_ast::QualifiedRule { block, .. } = &**qualified_rule;
+
+                swc_css_codegen::Emit::emit(&mut gen, &block).unwrap();
+
+                minified = minified[1..minified.len() - 1].to_string();
             }
             CssMinificationMode::MediaQueryList => {
                 let swc_css_ast::Stylesheet { rules, .. } = &stylesheet;
 
                 // Because CSS is grammar free, protect for fails
-                if let Some(swc_css_ast::Rule::AtRule(box swc_css_ast::AtRule {
-                    prelude, ..
-                })) = rules.first()
-                {
-                    swc_css_codegen::Emit::emit(&mut gen, &prelude).unwrap();
-
-                    minified = minified.trim().to_string();
-                } else {
+                let Some(swc_css_ast::Rule::AtRule(at_rule)) = rules.first() else {
                     return None;
-                }
+                };
+
+                let swc_css_ast::AtRule { prelude, .. } = &**at_rule;
+
+                swc_css_codegen::Emit::emit(&mut gen, &prelude).unwrap();
+
+                minified = minified.trim().to_string();
             }
         }
 
