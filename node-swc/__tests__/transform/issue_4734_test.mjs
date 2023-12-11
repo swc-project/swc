4b8184ca99e146eed1dd09eb8f9059a77ef38aeb
LongYinan <lynweklm@gmail.com>
2023-12-11 12:45:04 +0800

    chore(ci): Add linux arm build back (#8406)

--- node-swc/__tests__/transform/issue_4734_test.mjs
+++ node-swc/__tests__/transform/issue_4734_test.mjs
@@ -20,7 +20,7 @@ it("should transpile decorators", async () => {
     expect(code).toMatchInlineSnapshot(`
         "function _ts_decorate(decorators, target, key, desc) {
             var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-            if (typeof Reflect === \\"object\\" && typeof Reflect.decorate === \\"function\\") r = Reflect.decorate(decorators, target, key, desc);
+            if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
             else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
             return c > 3 && r && Object.defineProperty(target, key, r), r;
         }
@@ -29,7 +29,7 @@ it("should transpile decorators", async () => {
         }
         _ts_decorate([
             foo
-        ], TestClass.prototype, \\"abc\\", null);
+        ], TestClass.prototype, "abc", null);
         function foo(target, propertyKey) {
             console.log(\`Decorating \${target}.\${String(propertyKey)}\`);
         }
