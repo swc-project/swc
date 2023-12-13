b63ada0a73af998c9f10f9dc9491689d2e3e4b53
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-13 15:03:44 +0900

    fix using rule

--- crates/swc_ecma_compat_bugfixes/src/template_literal_caching.rs
+++ crates/swc_ecma_compat_bugfixes/src/template_literal_caching.rs
@@ -108,7 +108,7 @@ impl Fold for TemplateLiteralCaching {
                     right: Box::new(Expr::Assign(AssignExpr {
                         span: DUMMY_SP,
                         op: op!("="),
-                        left: AssignTarget::Pat(t.into()),
+                        left: t.into(),
                         right: Box::new(Expr::TaggedTpl(template)),
                     })),
                 });
