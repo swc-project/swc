2fa2efcb3c30768db7e3531310fa7fd555879026
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-13 15:05:19 +0900

    fix using rule

--- crates/swc_ecma_transforms_module/src/system_js.rs
+++ crates/swc_ecma_transforms_module/src/system_js.rs
@@ -420,7 +420,7 @@ impl SystemJs {
                 exprs.push(Box::new(Expr::Assign(AssignExpr {
                     span: DUMMY_SP,
                     op: op!("="),
-                    left: AssignTarget::Pat(Box::new(var_declarator.name)),
+                    left: Box::new(var_declarator.name).into(),
                     right: init,
                 })));
             }
