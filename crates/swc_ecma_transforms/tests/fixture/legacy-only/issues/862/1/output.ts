8f757e6b671f2967ab6fc6e4c300ce40188b0de5
David Sherret <dsherret@gmail.com>
2023-12-19 18:59:03 -0500

    Run --all-features

--- crates/swc_ecma_transforms/tests/fixture/legacy-only/issues/862/1/output.ts
+++ crates/swc_ecma_transforms/tests/fixture/legacy-only/issues/862/1/output.ts
@@ -1,10 +1,10 @@
 export class Product extends TimestampedEntity {
-    id!: string;
-    price!: number;
-    type!: ProductType;
-    productEntityId!: string;
-    /* ANCHOR: Relations ------------------------------------------------------ */ orders!: Order[];
-    discounts!: Discount[];
+    public id!: string;
+    public price!: number;
+    public type!: ProductType;
+    public productEntityId!: string;
+    /* ANCHOR: Relations ------------------------------------------------------ */ public orders!: Order[];
+    public discounts!: Discount[];
 }
 _ts_decorate([
     PrimaryGeneratedColumn("uuid")
