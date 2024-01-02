0cf3a9ff4365ccf15d5507f9258e9f8929a97543
David Sherret <dsherret@gmail.com>
2024-01-02 16:03:14 -0500

    Update test.

--- crates/swc_ecma_transforms_base/tests/ts-resolver/stc/1/output.ts
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/stc/1/output.ts
@@ -2,7 +2,7 @@ interface Mixin1__2 {
     mixinMethod__0(): void;
 }
 class AbstractBase__2 {
-    abstractBaseMethod(): void;
+    abstract abstractBaseMethod(): void;
 }
 function Mixin2__2<TBase__4 extends abstract new(...args: any[]) => any>(baseClass__4: TBase__4) {
     class MixinClass__4 extends baseClass__4 implements Mixin1__2 {
