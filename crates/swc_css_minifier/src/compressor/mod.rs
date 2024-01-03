85be8a4de1d8407421aadeb5769d414b9938f693
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-01-04 05:40:51 +0900

    refactor(css/parser): Remove value normalization (#8434)
    **Related issue :**
    
     - https://linear.app/vercel/issue/PACK-2154

--- crates/swc_css_minifier/src/compressor/mod.rs
+++ crates/swc_css_minifier/src/compressor/mod.rs
@@ -54,46 +54,111 @@ impl VisitMut for Compressor {
         self.compress_alpha_value(n);
     }
 
-    fn visit_mut_stylesheet(&mut self, n: &mut Stylesheet) {
+    fn visit_mut_an_plus_b(&mut self, n: &mut AnPlusB) {
+        if let AnPlusB::Ident(n) = n {
+            n.value = n.value.to_ascii_lowercase();
+            n.raw = None;
+        }
+
         n.visit_mut_children_with(self);
 
-        self.compress_stylesheet(n);
+        self.compress_an_plus_b(n);
+    }
 
-        if !self.need_utf8_at_rule
-            && n.rules
-                .first()
-                .and_then(|rule| rule.as_at_rule())
-                .and_then(|at_rule| at_rule.prelude.as_ref())
-                .and_then(|prelude| prelude.as_charset_prelude())
-                .filter(|x| x.value.eq_ignore_ascii_case("utf-8"))
-                .is_some()
-        {
-            n.rules.remove(0);
+    fn visit_mut_angle(&mut self, n: &mut Angle) {
+        n.visit_mut_children_with(self);
+
+        self.compress_angle(n);
+    }
+
+    fn visit_mut_at_rule(&mut self, n: &mut AtRule) {
+        n.visit_mut_children_with(self);
+
+        self.compress_keyframes_at_rule(n);
+    }
+
+    fn visit_mut_at_rule_name(&mut self, n: &mut AtRuleName) {
+        if let AtRuleName::Ident(n) = n {
+            n.value = n.value.to_ascii_lowercase();
+            n.raw = None;
+        }
+
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_attribute_selector(&mut self, n: &mut AttributeSelector) {
+        n.visit_mut_children_with(self);
+
+        self.compress_attribute_selector(n);
+    }
+
+    fn visit_mut_calc_sum(&mut self, n: &mut CalcSum) {
+        n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
+            in_math_function: true,
+            ..self.ctx
+        }));
+
+        // Don't touch `@supports`, it can be used to check a browser's support for one
+        // or more specific CSS features
+        if !self.in_supports_condition {
+            self.compress_calc_sum(n);
         }
     }
 
-    fn visit_mut_simple_block(&mut self, n: &mut SimpleBlock) {
+    fn visit_mut_color(&mut self, n: &mut Color) {
         n.visit_mut_children_with(self);
 
-        self.compress_simple_block(n);
+        self.compress_color(n);
     }
 
-    fn visit_mut_time(&mut self, n: &mut Time) {
+    fn visit_mut_component_value(&mut self, n: &mut ComponentValue) {
         n.visit_mut_children_with(self);
 
-        self.compress_time(n);
+        if self.in_supports_condition {
+            return;
+        }
+
+        self.compress_calc_sum_in_component_value(n);
+
+        self.compress_alpha_value_in_component_value(n);
+
+        self.compress_component_value_for_length(n);
+
+        self.compress_easing_function(n);
+
+        self.compress_transform_function(n);
+
+        self.compress_angle_in_component_value(n);
     }
 
-    fn visit_mut_unicode_range(&mut self, n: &mut UnicodeRange) {
+    fn visit_mut_compound_selector(&mut self, n: &mut CompoundSelector) {
         n.visit_mut_children_with(self);
 
-        self.compress_unicode_range(n);
+        self.compress_compound_selector(n);
     }
 
-    fn visit_mut_url(&mut self, n: &mut Url) {
-        n.visit_mut_children_with(self);
+    fn visit_mut_custom_ident(&mut self, custom_ident: &mut CustomIdent) {
+        custom_ident.visit_mut_children_with(self);
 
-        self.compress_url(n);
+        if !self.need_utf8_at_rule {
+            self.need_utf8_at_rule = !contains_only_ascii_characters(&custom_ident.value);
+        }
+    }
+
+    fn visit_mut_custom_property_name(&mut self, custom_property_name: &mut CustomPropertyName) {
+        custom_property_name.visit_mut_children_with(self);
+
+        if !self.need_utf8_at_rule {
+            self.need_utf8_at_rule = !contains_only_ascii_characters(&custom_property_name.value);
+        }
+    }
+
+    fn visit_mut_dashed_ident(&mut self, dashed_ident: &mut DashedIdent) {
+        dashed_ident.visit_mut_children_with(self);
+
+        if !self.need_utf8_at_rule {
+            self.need_utf8_at_rule = !contains_only_ascii_characters(&dashed_ident.value);
+        }
     }
 
     fn visit_mut_declaration(&mut self, n: &mut Declaration) {
@@ -125,22 +190,64 @@ impl VisitMut for Compressor {
         }
     }
 
-    fn visit_mut_color(&mut self, n: &mut Color) {
+    fn visit_mut_forgiving_relative_selector_list(
+        &mut self,
+        n: &mut ForgivingRelativeSelectorList,
+    ) {
         n.visit_mut_children_with(self);
 
-        self.compress_color(n);
+        self.compress_forgiving_relative_selector_list(n);
+    }
+
+    fn visit_mut_forgiving_selector_list(&mut self, n: &mut ForgivingSelectorList) {
+        n.visit_mut_children_with(self);
+
+        self.compress_forgiving_selector_list(n);
     }
 
     fn visit_mut_frequency(&mut self, n: &mut Frequency) {
+        n.unit.value = n.unit.value.to_ascii_lowercase();
+        n.unit.raw = None;
+
         n.visit_mut_children_with(self);
 
         self.compress_frequency(n);
     }
 
-    fn visit_mut_at_rule(&mut self, n: &mut AtRule) {
-        n.visit_mut_children_with(self);
+    fn visit_mut_function(&mut self, n: &mut Function) {
+        if let FunctionName::Ident(n) = &mut n.name {
+            n.value = n.value.to_ascii_lowercase();
+            n.raw = None;
+        }
 
-        self.compress_keyframes_at_rule(n);
+        if matches_eq!(
+            n.name, "rotate", "skew", "skewx", "skewy", "rotate3d", "rotatex", "rotatey", "rotatez"
+        ) {
+            n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
+                in_transform_function: true,
+                ..self.ctx
+            }));
+        } else {
+            n.visit_mut_children_with(self);
+        }
+    }
+
+    fn visit_mut_hex_color(&mut self, n: &mut HexColor) {
+        let new = n.value.to_ascii_lowercase();
+        if new != n.value {
+            n.value = new;
+            n.raw = None;
+        }
+
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_ident(&mut self, ident: &mut Ident) {
+        ident.visit_mut_children_with(self);
+
+        if !self.need_utf8_at_rule {
+            self.need_utf8_at_rule = !contains_only_ascii_characters(&ident.value);
+        }
     }
 
     fn visit_mut_import_href(&mut self, n: &mut ImportHref) {
@@ -149,10 +256,26 @@ impl VisitMut for Compressor {
         self.compress_import_href(n);
     }
 
-    fn visit_mut_media_query_list(&mut self, n: &mut MediaQueryList) {
+    fn visit_mut_keyframe_block(&mut self, n: &mut KeyframeBlock) {
+        n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
+            in_keyframe_block: true,
+            ..self.ctx
+        }));
+    }
+
+    fn visit_mut_keyframe_selector(&mut self, n: &mut KeyframeSelector) {
         n.visit_mut_children_with(self);
 
-        self.compress_media_query_list(n);
+        self.compress_keyframe_selector(n);
+    }
+
+    fn visit_mut_length(&mut self, n: &mut Length) {
+        n.unit.value = n.unit.value.to_ascii_lowercase();
+        n.unit.raw = None;
+
+        n.visit_mut_children_with(self);
+
+        self.compress_length(n);
     }
 
     fn visit_mut_media_condition(&mut self, n: &mut MediaCondition) {
@@ -167,12 +290,6 @@ impl VisitMut for Compressor {
         self.compress_media_condition_without_or(n);
     }
 
-    fn visit_mut_media_in_parens(&mut self, n: &mut MediaInParens) {
-        n.visit_mut_children_with(self);
-
-        self.compress_media_in_parens(n);
-    }
-
     fn visit_mut_media_feature(&mut self, n: &mut MediaFeature) {
         n.visit_mut_children_with(self);
 
@@ -184,6 +301,114 @@ impl VisitMut for Compressor {
 
         self.compress_calc_sum_in_media_feature_value(n);
         self.compress_media_feature_value_length(n);
+    }
+
+    fn visit_mut_media_in_parens(&mut self, n: &mut MediaInParens) {
+        n.visit_mut_children_with(self);
+
+        self.compress_media_in_parens(n);
+    }
+
+    fn visit_mut_media_query_list(&mut self, n: &mut MediaQueryList) {
+        n.visit_mut_children_with(self);
+
+        self.compress_media_query_list(n);
+    }
+
+    fn visit_mut_pseudo_class_selector(&mut self, n: &mut PseudoClassSelector) {
+        match &mut n.name {
+            Ident { value, .. }
+                if matches_eq_ignore_ascii_case!(
+                    &**value,
+                    "not",
+                    "is",
+                    "where",
+                    "matches",
+                    "-moz-any",
+                    "-webkit-any"
+                ) =>
+            {
+                n.name.value = n.name.value.to_ascii_lowercase();
+                n.name.raw = None;
+
+                n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
+                    in_logic_combinator_selector: true,
+                    ..self.ctx
+                }));
+            }
+            _ => {
+                n.visit_mut_children_with(self);
+            }
+        }
+    }
+
+    fn visit_mut_pseudo_element_selector(&mut self, n: &mut PseudoElementSelector) {
+        n.name.value = n.name.value.to_ascii_lowercase();
+        n.name.raw = None;
+
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_relative_selector_list(&mut self, n: &mut RelativeSelectorList) {
+        n.visit_mut_children_with(self);
+
+        self.compress_relative_selector_list(n);
+    }
+
+    fn visit_mut_selector_list(&mut self, n: &mut SelectorList) {
+        n.visit_mut_children_with(self);
+
+        self.compress_selector_list(n);
+    }
+
+    fn visit_mut_simple_block(&mut self, n: &mut SimpleBlock) {
+        n.visit_mut_children_with(self);
+
+        self.compress_simple_block(n);
+    }
+
+    fn visit_mut_size_feature_value(&mut self, n: &mut SizeFeatureValue) {
+        n.visit_mut_children_with(self);
+
+        self.compress_calc_sum_in_size_feature_value(n);
+        self.compress_size_feature_value_length(n);
+    }
+
+    fn visit_mut_str(&mut self, string: &mut Str) {
+        string.visit_mut_children_with(self);
+
+        if !self.need_utf8_at_rule {
+            self.need_utf8_at_rule = !contains_only_ascii_characters(&string.value);
+        }
+    }
+
+    fn visit_mut_stylesheet(&mut self, n: &mut Stylesheet) {
+        n.visit_mut_children_with(self);
+
+        self.compress_stylesheet(n);
+
+        if !self.need_utf8_at_rule
+            && n.rules
+                .first()
+                .and_then(|rule| rule.as_at_rule())
+                .and_then(|at_rule| at_rule.prelude.as_ref())
+                .and_then(|prelude| prelude.as_charset_prelude())
+                .filter(|x| x.value.eq_ignore_ascii_case("utf-8"))
+                .is_some()
+        {
+            n.rules.remove(0);
+        }
+    }
+
+    fn visit_mut_subclass_selector(&mut self, n: &mut SubclassSelector) {
+        if let SubclassSelector::PseudoClass(PseudoClassSelector { name, .. }) = n {
+            name.value = name.value.to_ascii_lowercase();
+            name.raw = None;
+        }
+
+        n.visit_mut_children_with(self);
+
+        self.compress_subclass_selector(n);
     }
 
     fn visit_mut_supports_condition(&mut self, n: &mut SupportsCondition) {
@@ -204,192 +429,17 @@ impl VisitMut for Compressor {
         self.compress_supports_in_parens(n);
     }
 
-    fn visit_mut_size_feature_value(&mut self, n: &mut SizeFeatureValue) {
+    fn visit_mut_tag_name_selector(&mut self, n: &mut TagNameSelector) {
+        n.name.value.value = n.name.value.value.to_ascii_lowercase();
+        n.name.value.raw = None;
+
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_time(&mut self, n: &mut Time) {
         n.visit_mut_children_with(self);
 
-        self.compress_calc_sum_in_size_feature_value(n);
-        self.compress_size_feature_value_length(n);
-    }
-
-    fn visit_mut_keyframe_selector(&mut self, n: &mut KeyframeSelector) {
-        n.visit_mut_children_with(self);
-
-        self.compress_keyframe_selector(n);
-    }
-
-    fn visit_mut_calc_sum(&mut self, n: &mut CalcSum) {
-        n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
-            in_math_function: true,
-            ..self.ctx
-        }));
-
-        // Don't touch `@supports`, it can be used to check a browser's support for one
-        // or more specific CSS features
-        if !self.in_supports_condition {
-            self.compress_calc_sum(n);
-        }
-    }
-
-    fn visit_mut_component_value(&mut self, n: &mut ComponentValue) {
-        n.visit_mut_children_with(self);
-
-        if self.in_supports_condition {
-            return;
-        }
-
-        self.compress_calc_sum_in_component_value(n);
-
-        self.compress_alpha_value_in_component_value(n);
-
-        self.compress_component_value_for_length(n);
-
-        self.compress_easing_function(n);
-
-        self.compress_transform_function(n);
-
-        self.compress_angle_in_component_value(n);
-    }
-
-    fn visit_mut_length(&mut self, n: &mut Length) {
-        n.visit_mut_children_with(self);
-
-        self.compress_length(n);
-    }
-
-    fn visit_mut_pseudo_class_selector(&mut self, n: &mut PseudoClassSelector) {
-        match &n.name {
-            Ident { value, .. }
-                if matches!(
-                    &**value,
-                    "not" | "is" | "where" | "matches" | "-moz-any" | "-webkit-any"
-                ) =>
-            {
-                n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
-                    in_logic_combinator_selector: true,
-                    ..self.ctx
-                }));
-            }
-            _ => {
-                n.visit_mut_children_with(self);
-            }
-        }
-    }
-
-    fn visit_mut_selector_list(&mut self, n: &mut SelectorList) {
-        n.visit_mut_children_with(self);
-
-        self.compress_selector_list(n);
-    }
-
-    fn visit_mut_forgiving_selector_list(&mut self, n: &mut ForgivingSelectorList) {
-        n.visit_mut_children_with(self);
-
-        self.compress_forgiving_selector_list(n);
-    }
-
-    fn visit_mut_relative_selector_list(&mut self, n: &mut RelativeSelectorList) {
-        n.visit_mut_children_with(self);
-
-        self.compress_relative_selector_list(n);
-    }
-
-    fn visit_mut_forgiving_relative_selector_list(
-        &mut self,
-        n: &mut ForgivingRelativeSelectorList,
-    ) {
-        n.visit_mut_children_with(self);
-
-        self.compress_forgiving_relative_selector_list(n);
-    }
-
-    fn visit_mut_an_plus_b(&mut self, n: &mut AnPlusB) {
-        n.visit_mut_children_with(self);
-
-        self.compress_an_plus_b(n);
-    }
-
-    fn visit_mut_subclass_selector(&mut self, n: &mut SubclassSelector) {
-        n.visit_mut_children_with(self);
-
-        self.compress_subclass_selector(n);
-    }
-
-    fn visit_mut_compound_selector(&mut self, n: &mut CompoundSelector) {
-        n.visit_mut_children_with(self);
-
-        self.compress_compound_selector(n);
-    }
-
-    fn visit_mut_attribute_selector(&mut self, n: &mut AttributeSelector) {
-        n.visit_mut_children_with(self);
-
-        self.compress_attribute_selector(n);
-    }
-
-    fn visit_mut_keyframe_block(&mut self, n: &mut KeyframeBlock) {
-        n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
-            in_keyframe_block: true,
-            ..self.ctx
-        }));
-    }
-
-    fn visit_mut_function(&mut self, n: &mut Function) {
-        if matches_eq!(
-            n.name, "rotate", "skew", "skewx", "skewy", "rotate3d", "rotatex", "rotatey", "rotatez"
-        ) {
-            n.visit_mut_children_with(&mut *self.with_ctx(Ctx {
-                in_transform_function: true,
-                ..self.ctx
-            }));
-        } else {
-            n.visit_mut_children_with(self);
-        }
-    }
-
-    fn visit_mut_angle(&mut self, n: &mut Angle) {
-        n.visit_mut_children_with(self);
-
-        self.compress_angle(n);
-    }
-
-    fn visit_mut_ident(&mut self, ident: &mut Ident) {
-        ident.visit_mut_children_with(self);
-
-        if !self.need_utf8_at_rule {
-            self.need_utf8_at_rule = !contains_only_ascii_characters(&ident.value);
-        }
-    }
-
-    fn visit_mut_custom_ident(&mut self, custom_ident: &mut CustomIdent) {
-        custom_ident.visit_mut_children_with(self);
-
-        if !self.need_utf8_at_rule {
-            self.need_utf8_at_rule = !contains_only_ascii_characters(&custom_ident.value);
-        }
-    }
-
-    fn visit_mut_dashed_ident(&mut self, dashed_ident: &mut DashedIdent) {
-        dashed_ident.visit_mut_children_with(self);
-
-        if !self.need_utf8_at_rule {
-            self.need_utf8_at_rule = !contains_only_ascii_characters(&dashed_ident.value);
-        }
-    }
-
-    fn visit_mut_str(&mut self, string: &mut Str) {
-        string.visit_mut_children_with(self);
-
-        if !self.need_utf8_at_rule {
-            self.need_utf8_at_rule = !contains_only_ascii_characters(&string.value);
-        }
-    }
-
-    fn visit_mut_custom_property_name(&mut self, custom_property_name: &mut CustomPropertyName) {
-        custom_property_name.visit_mut_children_with(self);
-
-        if !self.need_utf8_at_rule {
-            self.need_utf8_at_rule = !contains_only_ascii_characters(&custom_property_name.value);
-        }
+        self.compress_time(n);
     }
 
     fn visit_mut_token_and_span(&mut self, token_and_span: &mut TokenAndSpan) {
@@ -420,6 +470,21 @@ impl VisitMut for Compressor {
                 _ => {}
             }
         }
+    }
+
+    fn visit_mut_unicode_range(&mut self, n: &mut UnicodeRange) {
+        n.visit_mut_children_with(self);
+
+        self.compress_unicode_range(n);
+    }
+
+    fn visit_mut_url(&mut self, n: &mut Url) {
+        n.name.value = n.name.value.to_ascii_lowercase();
+        n.name.raw = None;
+
+        n.visit_mut_children_with(self);
+
+        self.compress_url(n);
     }
 }
 
