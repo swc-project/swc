a5a3099470d1f216e19009847b2080e5603ca9c2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-03 14:25:38 +0900

    Remove

--- crates/swc_ecma_transforms_module/tests/paths/pack-1682/2/input/utils/test.js
+++
@@ -1,9 +0,0 @@
-import PrismaClientProvider from "@/db/PrismaClientProvider";
-
-export default function setupTests() {
-  const context = {};
-
-  beforeEach(() => {
-    context.prisma = PrismaClientProvider.prisma;
-  });
-}
