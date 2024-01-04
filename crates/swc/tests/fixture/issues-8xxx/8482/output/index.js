3ab6536feecd6df8dfb78ffdd9faa4f90ebab120
magic-akari <akari.ccino@gmail.com>
2024-01-04 19:58:26 +0800

    chore: update test cases

---
+++ crates/swc/tests/fixture/issues-8xxx/8482/output/index.js
@@ -0,0 +1,19 @@
+(function(global, factory) {
+    if (typeof module === "object" && typeof module.exports === "object") factory(exports);
+    else if (typeof define === "function" && define.amd) define([
+        "exports"
+    ], factory);
+    else if (global = typeof globalThis !== "undefined" ? globalThis : global || self) factory(global.index = {});
+})(this, function(exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    Object.defineProperty(exports, "a", {
+        enumerable: true,
+        get: function() {
+            return a;
+        }
+    });
+    let a = "";
+});
