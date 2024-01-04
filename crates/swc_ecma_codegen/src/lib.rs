e0bdc0f7c210c73f0291ab72e380743fe5f03b72
David Sherret <dsherret@users.noreply.github.com>
2024-01-03 23:48:07 -0500

    fix(es/codegen): Emit `?` for an optional computed property (#8481)
    **Description:**
    
    Emits optional `?` for a computed property.

--- crates/swc_ecma_codegen/src/lib.rs
+++ crates/swc_ecma_codegen/src/lib.rs
@@ -1566,10 +1566,12 @@ where
         }
 
         emit!(n.key);
+
+        if n.is_optional {
+            punct!("?");
+        }
+
         if let Some(type_ann) = &n.type_ann {
-            if n.is_optional {
-                punct!("?");
-            }
             if n.definite {
                 punct!("!");
             }
@@ -1634,6 +1636,12 @@ where
         }
 
         emit!(n.key);
+
+        // emit for a computed property, but not an identifier already marked as
+        // optional
+        if n.is_optional && !n.key.as_ident().map(|i| i.optional).unwrap_or(false) {
+            punct!("?");
+        }
 
         if let Some(ty) = &n.type_ann {
             if n.definite {
