fa4481f86e90ea6ebbebf1c1debbca4f8c681b93
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-01-14 18:13:27 +0900

    Discard changes to crates/swc/tests/tsc-references/ES5SymbolProperty2.2.minified.js

--- crates/swc/tests/tsc-references/ES5SymbolProperty2.2.minified.js
+++ crates/swc/tests/tsc-references/ES5SymbolProperty2.2.minified.js
@@ -1,9 +1,9 @@
 //// [ES5SymbolProperty2.ts]
-var M1, _$Symbol, C;
+var M, M1, _$Symbol, C;
 import { _ as _class_call_check } from "@swc/helpers/_/_class_call_check";
-C = function() {
+M1 = M || (M = {}), C = function() {
     function C() {
         _class_call_check(this, C);
     }
     return C.prototype[_$Symbol.iterator] = function() {}, C;
-}(), M.C = C, (new C)[_$Symbol.iterator], (new M1.C)[Symbol.iterator];
+}(), M1.C = C, (new C)[_$Symbol.iterator], (new M.C)[Symbol.iterator];
