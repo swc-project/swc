25131321cb736b9cd158d6c974b566c439760636
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-15 11:02:34 +0900

    #?

--- crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/sequences.rs
@@ -1715,7 +1715,7 @@ impl Optimizer<'_> {
                                         *b = *err.take();
                                         return Ok(true);
                                     }
-                                    Err(err) => unreachable!("{err:?}"),
+                                    Err(err) => unreachable!("{err:#?}"),
                                 };
                                 if res? {
                                     return Ok(true);
