8faa1eaddb5c3dd5347933213bd8f6d1a0b46b1f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-17 01:24:06 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/terser/compress/template_string/template_string_evaluate_with_many_segments/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/template_string/template_string_evaluate_with_many_segments/output.js
@@ -1,4 +1,4 @@
-var foo = `Hello ${guest()}, welcome to ${location()}"."`;
+var foo = `Hello ${guest()}, welcome to ${location()}.`;
 var bar = "1234567890";
 var baz = `${foobar()}${foobar()}${foobar()}${foobar()}`;
 var buzz = `${foobar()}${foobar()}${foobar()}`;
