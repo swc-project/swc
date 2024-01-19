29146071e641c51b5017dd6dd4d125a28e36c049
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-19 11:33:06 +0900

    Update test refs

--- node-swc/__tests__/transform/issue_8491_test.mjs
+++ node-swc/__tests__/transform/issue_8491_test.mjs
@@ -5,26 +5,32 @@ it("should transpile decorators", async () => {
 
     console.log(a)`;
 
-    expect(swc.transformSync(source, {
-        "jsc": {
-            "parser": {
-                "syntax": "ecmascript",
-                "jsx": false
+    expect(
+        swc.transformSync(source, {
+            jsc: {
+                parser: {
+                    syntax: "ecmascript",
+                    jsx: false,
+                },
+                target: "es5",
+                loose: false,
+                minify: {
+                    compress: true,
+                    format: {
+                        asciiOnly: true,
+                    },
+                },
             },
-            "target": "es5",
-            "loose": false,
-            "minify": {
-                "compress": true,
-                "format": {
-                    "asciiOnly": true
-                }
-            }
-        },
-        "module": {
-            "type": "es6"
-        },
-        "minify": false,
-        "isModule": true
-    }).code).toMatchInlineSnapshot(`
+            module: {
+                type: "es6",
+            },
+            minify: false,
+            isModule: true,
+        }).code
+    ).toMatchInlineSnapshot(`
+        "console.log({
+            "a\\xf3b": "\\xf3"
+        });
+        "
     `);
 });
