c42e4974a5398c6dd4448e88b86f50f202a85984
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-20 18:43:50 +0900

    Add a test

---
+++ crates/swc/tests/fixture/issues-8xxx/8184/1/input/src/a/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const a = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+a.propTypes = {
+  
+};
+
+export default a;
---
+++ crates/swc/tests/fixture/issues-8xxx/8184/1/input/src/d/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IAppProps {
+}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
+  return <div>123</div>;
+};
+
+export default App;
