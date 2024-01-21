2d8bd9ed894cbc2e0e9063f6a1ab723c3d3bfdc6
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-01-22 00:28:38 +0900

    fix(es/module): Fix handling of `*` in `jsc.paths` (#8535)
    **Related issue:**
    
     - Closes #8375

--- crates/swc/tests/fixture/issues-8xxx/8375/1/output/index.ts
+++ crates/swc/tests/fixture/issues-8xxx/8375/1/output/index.ts
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-const _interop_require_wildcard = require("src/@swc/helpers/_/_interop_require_wildcard");
+const _interop_require_wildcard = require("@swc/helpers/_/_interop_require_wildcard");
 const _hello = require("./src/hello");
-const _node = _interop_require_wildcard._(require("src/@sentry/node"));
+const _node = _interop_require_wildcard._(require("@sentry/node"));
 _node;
 (0, _hello.helloWorld)("SWC"); /*#__PURE__*/ 
