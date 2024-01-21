c16c69978a0144a582280b96f4ba386164fadda4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-21 21:45:46 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-8xxx/8375/1/output/src/hello/index.ts
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "helloWorld", {
+    enumerable: true,
+    get: function() {
+        return helloWorld;
+    }
+});
+function helloWorld(name) {
+    console.log(`Hello, ${name}`);
+}
