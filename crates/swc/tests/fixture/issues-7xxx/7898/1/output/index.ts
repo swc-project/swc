576bda6620e36293d64b91bec1a1896a3951a5b9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-21 15:05:09 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-7xxx/7898/1/output/index.ts
+++ crates/swc/tests/fixture/issues-7xxx/7898/1/output/index.ts
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-const _a = require("./packages/a/src/index.js");
+const _a = require("./packages/a/src/index.ts");
 console.log(`${(0, _a.displayA)()}`);
