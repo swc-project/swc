01b7a032b8baea71a562f5ff8695e13bc0eca28d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-23 23:44:59 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/fixture/next/outstatic/1/output.js
+++ crates/swc_ecma_minifier/tests/fixture/next/outstatic/1/output.js
@@ -6,28 +6,28 @@
         1973: function(module) {
             eval(module.code);
         },
-        1985: function(n, t, e) {
+        1985: function(t, e, n) {
             "use strict";
-            var c = 0, r = (n)=>"checkbox" === n.type;
-            var u = (n, t)=>{
-                var e;
-                return n.has((e = t).substring(0, e.search(/\.\d+(\.|$)/)) || e);
+            var c = 0, r = (t)=>"checkbox" === t.type;
+            var u = (t, e)=>{
+                let n;
+                return t.has((n = e).substring(0, n.search(/\.\d+(\.|$)/)) || n);
             }, o = ()=>{};
-            function s(n, t, e) {}
-            var a = (n)=>"radio" === n.type;
-            function f(n = {}) {
-                let t, e = {
-                    ...n
+            function s(t, e, n) {}
+            var a = (t)=>"radio" === t.type;
+            function f(t = {}) {
+                let e, n = {
+                    ...t
                 }, c = {}, r = 0, u = {}, o = {};
-                const s = 0, a = async (n)=>{
-                    let t = n.target.name;
-                    const e = 0;
+                const s = 0, a = async (t)=>{
+                    let e = t.target.name;
+                    const n = 0;
                 };
             }
-            t.useForm = function(n = {}) {
-                const t = c.default.useRef();
-                t.current ? t.current.control._options = n : t.current = {
-                    ...f(n)
+            e.useForm = function(t = {}) {
+                const e = c.default.useRef();
+                e.current ? e.current.control._options = t : e.current = {
+                    ...f(t)
                 };
             };
         }
