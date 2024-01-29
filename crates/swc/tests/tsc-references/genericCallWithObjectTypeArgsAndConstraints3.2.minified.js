cddf8d47bdb06669554e0db5b15d8b9334c4f467
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-29 23:56:53 +0900

    Update test refs (verified)

--- crates/swc/tests/tsc-references/genericCallWithObjectTypeArgsAndConstraints3.2.minified.js
+++ crates/swc/tests/tsc-references/genericCallWithObjectTypeArgsAndConstraints3.2.minified.js
@@ -2,7 +2,7 @@
 import { _ as _class_call_check } from "@swc/helpers/_/_class_call_check";
 import { _ as _inherits } from "@swc/helpers/_/_inherits";
 import { _ as _create_super } from "@swc/helpers/_/_create_super";
-var Base = function Base() {
+var y, y1, Base = function Base() {
     _class_call_check(this, Base);
 }, Derived = function(Base) {
     _inherits(Derived, Base);
@@ -19,4 +19,8 @@ var Base = function Base() {
     }
     return Derived2;
 }(Base);
-new Derived(), new Derived2(), new Derived(), new Derived2(), new Derived(), new Derived2(), new Base(), new Derived();
+new Derived(), new Derived2(), new Derived(), new Derived2(), new Derived(), new Derived2(), y = function(x) {
+    return x;
+}, new Base(), y(null), y1 = function(x) {
+    return x;
+}, new Derived(), y1(null);
