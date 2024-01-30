916df5beae072b634081b4157d65139fde2bf264
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-30 12:35:37 +0900

    Update test refs (better than terser)

--- crates/swc_ecma_minifier/tests/terser/compress/reduce_vars/regex_loop/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/reduce_vars/regex_loop/output.js
@@ -1,6 +1,6 @@
 var a = /ab*/g;
-(function (x) {
-    for (var r, s = "acdabcdeabbb"; (r = x().exec(s)); ) console.log(r[0]);
-})(function () {
+(function(x) {
+    for(var r; r = x().exec("acdabcdeabbb");)console.log(r[0]);
+})(function() {
     return a;
 });
