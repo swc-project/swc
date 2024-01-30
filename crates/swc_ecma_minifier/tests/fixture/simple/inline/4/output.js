81b58bafa706e7ac177237bdc013617d90d5fe60
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-30 10:40:08 +0900

    Update test refs (verified)

--- crates/swc_ecma_minifier/tests/fixture/simple/inline/4/output.js
+++ crates/swc_ecma_minifier/tests/fixture/simple/inline/4/output.js
@@ -1,8 +1,9 @@
-module.exports.A = function() {
+var a, c, a1, c1, a2, c2;
+a = module.exports, c = function() {
     return A;
-}, module.exports.B = function() {
+}, a.A = c, a1 = module.exports, c1 = function() {
     return B;
-}, module.exports.C = function() {
+}, a1.B = c1, a2 = module.exports, c2 = function() {
     return C;
-};
+}, a2.C = c2;
 const A = "A", B = "B", C = "C";
