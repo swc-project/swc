d82d3b84517048ba626df10a0dfada0b00fb72d2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-01 13:33:20 +0900

    Update test refs (verified)

--- crates/swc_ecma_minifier/tests/fixture/projects/wmr/archive-1/chunks/index.5a544c41/output.js
+++ crates/swc_ecma_minifier/tests/fixture/projects/wmr/archive-1/chunks/index.5a544c41/output.js
@@ -1,6 +1,5 @@
 import { m } from "../index.f66dda46.js";
 const jpg = "/assets/img.2dae108d.jpg";
-function Files() {
+export default function() {
     return m`<div style="padding: 2rem;"><h1>Files</h1><p> jpg: ${jpg}<br/><img src=${jpg} alt="" height="320"/></p></div>`;
 }
-export default Files;
--- crates/swc_ecma_minifier/tests/fixture/projects/wmr/archive-1/chunks/index.ddc4110d/output.js
+++ crates/swc_ecma_minifier/tests/fixture/projects/wmr/archive-1/chunks/index.ddc4110d/output.js
@@ -1,10 +1,6 @@
-import { s as style, y, m } from "../index.f66dda46.js";
-const styles = {
-    about: "about_migxty"
-};
-function About({ query, title }) {
+import { y, m } from "../index.f66dda46.js";
+export default function({ query, title }) {
     return y(()=>(console.log("Mounted About: ", title), ()=>{
             console.log("Unmounting About: ", title);
         }), []), m`<section class=${"about_migxty"}><h1>${title || "About"}</h1><p>My name is Jason.</p><pre>${JSON.stringify(query)}</pre></section>`;
 }
-export default About;
