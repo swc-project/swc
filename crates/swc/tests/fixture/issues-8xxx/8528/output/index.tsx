e42786e9240cceca8a5f96261f2192d89108cf6e
magic-akari <akari.ccino@gmail.com>
2024-02-04 13:36:45 +0800

    chore: update test cases

---
+++ crates/swc/tests/fixture/issues-8xxx/8528/output/index.tsx
@@ -0,0 +1,15 @@
+import { _ as _apply_decs_2203_r } from "@swc/helpers/_/_apply_decs_2203_r";
+var _initClass, _React_Component;
+import { observer } from "mobx";
+import * as React from "react";
+import * as S from "./BetButton.styled";
+let _BetButtonWrapper;
+class BetButtonWrapper extends (_React_Component = React.Component) {
+    static #_ = { c: [_BetButtonWrapper, _initClass] } = _apply_decs_2203_r(this, [], [
+        observer
+    ], _React_Component);
+    render() {
+        return /*#__PURE__*/ React.createElement(S.BetButtonWrapper, null);
+    }
+    static #_2 = _initClass();
+}
