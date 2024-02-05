0336779204de0c14d7bec5383008cac46779f2d7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-05 20:00:01 +0900

    Update test refs (verified)

--- crates/swc_ecma_minifier/tests/terser/compress/hoist_props/name_collision_1/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/hoist_props/name_collision_1/output.js
@@ -1,11 +1,4 @@
-var obj_foo = 1;
 var obj_bar = 2;
-function f() {
-    var obj_foo$0 = 3,
-        obj_bar$0 = 4,
-        obj_b_r = 5,
-        obj_b_r$0 = 6,
-        obj_b_r$1 = 7;
-    console.log(obj_foo, obj_foo$0, obj_bar$0, obj_b_r, obj_b_r$0, obj_b_r$1);
-}
-f();
+(function() {
+    console.log(1, 3, 4, 5, 6, 7);
+})();
--- crates/swc_ecma_minifier/tests/terser/compress/hoist_props/name_collision_2/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/hoist_props/name_collision_2/output.js
@@ -1,10 +1,6 @@
-var o_p = 1,
-    o__ = function (x) {
-        return x;
-    },
-    o__$2 = function (x) {
-        return x + 1;
-    },
-    o__$0 = 2,
-    o__$1 = 3;
-console.log(o_p === o_p, o__(4), o__$2(5), o__$0, o__$1);
+var o_p = 1;
+console.log(true, function(x) {
+    return 4;
+}(0), function(x) {
+    return 6;
+}(0), 2, 3);
