b44631887136944fe3841b7bf46241a808c37e4c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-06 19:40:08 +0900

    fixup

--- crates/swc_ecma_minifier/src/eval.rs
+++ crates/swc_ecma_minifier/src/eval.rs
@@ -73,7 +73,7 @@ impl Mode for Eval {
 }
 
 impl Evaluator {
-    #[tracing::instrument(level = "debug", skip_all)]
+    #[tracing::instrument(name = "Evaluator::run", level = "debug", skip_all)]
     fn run(&mut self) {
         if !self.done {
             self.done = true;
