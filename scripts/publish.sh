83dfc26dac7966aa93176bce6a5ca8c5098b46c7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-13 14:51:57 +0900

    chore: Publish 1.4.1 with swc_core v0.90.8

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 version="$1"
+swc_core_version="$(cargo tree -i -p swc_core --depth 0 | awk '{print $2}')"
 
-# Reset other changes
-git commit $SCRIPT_DIR/publish.sh -m 'Update publish script' || true
-git reset --hard
+git pull || true
+yarn
+
+echo "Publishing $version with swc_core $swc_core_version"
 
 # Update swc_core
 (cd ./bindings && cargo upgrade -p swc_core --recursive false)
@@ -19,3 +21,20 @@ npm version "$version" --no-git-tag-version --allow-same-version || true
 (cd ./packages/minifier && npm version "$version" --no-git-tag-version --allow-same-version || true)
 (cd ./bindings && cargo set-version $version -p binding_core_wasm -p binding_minifier_wasm)
 (cd ./bindings && cargo set-version --bump patch -p swc_cli)
+
+
+# Commmit and tag
+git add -A
+git commit -am "chore: Publish $version with swc_core $swc_core_version"
+git tag -a -m "swc_core $swc_core_version" "v$version"
+
+
+# Update changelog
+yarn changelog
+git add -A
+git commit -m 'chore: Update changelog'
+
+# Publish packages
+git push
+git push --tags
+(cd ./bindings && cargo mono publish --no-verify)
