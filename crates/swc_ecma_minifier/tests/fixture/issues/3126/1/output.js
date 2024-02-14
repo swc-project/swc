dee2814ce5b9790657d1843ffc8e2253c58d4930
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-14 10:30:00 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/fixture/issues/3126/1/output.js
+++ crates/swc_ecma_minifier/tests/fixture/issues/3126/1/output.js
@@ -1,12 +1,12 @@
 (()=>{
     var __webpack_modules__ = {
-        746: (__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__1)=>{
+        746: ()=>{
             Object.prototype.hasOwnProperty;
         }
     };
     __webpack_require__.m = __webpack_modules__;
     (()=>{
-        __webpack_require__.O = (result, chunkIds, fn, priority)=>{
+        __webpack_require__.O = (result, chunkIds)=>{
             for(var j = 0; j < chunkIds.length; j++)Object.keys(__webpack_require__.O).every((key)=>__webpack_require__.O[key](chunkIds[j]));
         };
     })();
