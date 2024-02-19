8b20f6c473a66a2df4c41ad8b69adaa1714b6f0b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-19 14:07:11 +0900

    Add a test

---
+++ crates/swc/tests/fixture/issues-8xxx/8300/2/input/1.js
@@ -0,0 +1,10 @@
+class Foo extends Bar {
+    constructor() {
+        for (const i of [1]) {
+            setTimeout(() => {
+                console.log(this)
+            });
+        }
+        super();
+    }
+}
