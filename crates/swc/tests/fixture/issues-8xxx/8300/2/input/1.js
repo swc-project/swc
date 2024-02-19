fe954f6ede788d46233708da0b1660c5d468f827
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-19 14:59:25 +0900

    Harden

--- crates/swc/tests/fixture/issues-8xxx/8300/2/input/1.js
+++ crates/swc/tests/fixture/issues-8xxx/8300/2/input/1.js
@@ -2,7 +2,7 @@ class Foo extends Bar {
     constructor() {
         for (const i of [1]) {
             setTimeout(() => {
-                console.log(this)
+                console.log(this, i)
             });
         }
         super();
