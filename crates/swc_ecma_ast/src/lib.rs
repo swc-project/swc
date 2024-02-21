59261d0cb1f071da49deb43a15f262f40e0c194f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-21 13:33:10 +0900

    fix

--- crates/swc_ecma_ast/src/lib.rs
+++ crates/swc_ecma_ast/src/lib.rs
@@ -108,28 +108,28 @@ impl Take for Invalid {
 #[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum EsVersion {
-    #[cfg_attr(feature = "serde", serde(rename = "es3", alias = "Es3"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es3", alias = "ES3"))]
     Es3,
-    #[cfg_attr(feature = "serde", serde(rename = "es5", alias = "Es5"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es5", alias = "ES5"))]
     Es5,
     #[cfg_attr(
         feature = "serde",
-        serde(rename = "es2015", alias = "Es2015", alias = "Es6", alias = "es6")
+        serde(rename = "es2015", alias = "ES2015", alias = "ES6", alias = "es6")
     )]
     Es2015,
-    #[cfg_attr(feature = "serde", serde(rename = "es2016", alias = "Es2016"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2016", alias = "ES2016"))]
     Es2016,
-    #[cfg_attr(feature = "serde", serde(rename = "es2017", alias = "Es2017"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2017", alias = "ES2017"))]
     Es2017,
-    #[cfg_attr(feature = "serde", serde(rename = "es2018", alias = "Es2018"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2018", alias = "ES2018"))]
     Es2018,
-    #[cfg_attr(feature = "serde", serde(rename = "es2019", alias = "Es2019"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2019", alias = "ES2019"))]
     Es2019,
-    #[cfg_attr(feature = "serde", serde(rename = "es2020", alias = "Es2020"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2020", alias = "ES2020"))]
     Es2020,
-    #[cfg_attr(feature = "serde", serde(rename = "es2021", alias = "Es2021"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2021", alias = "ES2021"))]
     Es2021,
-    #[cfg_attr(feature = "serde", serde(rename = "es2022", alias = "Es2022"))]
+    #[cfg_attr(feature = "serde", serde(rename = "es2022", alias = "ES2022"))]
     Es2022,
     #[cfg_attr(feature = "serde", serde(rename = "esnext", alias = "EsNext"))]
     EsNext,
