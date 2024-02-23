66bf8e907d09349d847a99036747edb724c4a20a
David Sherret <dsherret@users.noreply.github.com>
2024-02-22 19:14:29 -0500

    fix(es/codegen): Emit computed getter and setter signatures (#8656)
    **Description:**
    
    Emits computed getter/setter signatures.

---
+++ crates/swc_ecma_codegen/tests/fixture/typescript/interface/input.ts
@@ -0,0 +1,14 @@
+const symbol1 = Symbol();
+const symbol2 = Symbol();
+const symbol3 = Symbol();
+
+export interface Test {
+  [symbol1]: string;
+  [symbol2](): string;
+  get [symbol3](): string;
+  set [symbol3](value: string);
+  prop1: string;
+  method1(value: string, param2: number): string;
+  get method2(): string;
+  set method2(value: string);
+}
