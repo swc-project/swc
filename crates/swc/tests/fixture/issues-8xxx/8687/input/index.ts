a508caca3b796d858d1588031139f2c3e1db7547
magic-akari <akari.ccino@gmail.com>
2024-03-04 12:05:28 +0800

    fix(es/typescript): Handle accessibility in private method

---
+++ crates/swc/tests/fixture/issues-8xxx/8687/input/index.ts
@@ -0,0 +1,3 @@
+class Oops {
+    private #uhOh() { }
+}
---
+++ crates/swc/tests/fixture/issues-8xxx/8687/output/index.ts
@@ -0,0 +1,9 @@
+import { _ as _class_call_check } from "@swc/helpers/_/_class_call_check";
+import { _ as _class_private_method_init } from "@swc/helpers/_/_class_private_method_init";
+var _uhOh = new WeakSet();
+var Oops = function Oops() {
+    "use strict";
+    _class_call_check(this, Oops);
+    _class_private_method_init(this, _uhOh);
+};
+function uhOh() {}
