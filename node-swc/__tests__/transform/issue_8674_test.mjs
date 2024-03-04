d60dbac2949cf10b259f139d3d7fd824a74caeab
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-04 11:23:10 +0900

    Add a node test

---
+++ node-swc/__tests__/transform/issue_8674_test.mjs
@@ -0,0 +1,28 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import swc from "../../..";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+it("should transpile import path correctly", async () => {
+    const baseUrl = path.resolve(__dirname, "../../tests/issue-8674");
+    console.log("baseUrl", baseUrl);
+
+    const { code } = await swc.transform(
+        `
+        import { foo } from "src/foo"
+        console.log(foo)
+    `,
+        {
+            jsc: {
+                baseUrl,
+            },
+        }
+    );
+
+    expect(code).toMatchInlineSnapshot(`
+        "import { foo } from "./src/foo";
+        console.log(foo);
+        "
+    `);
+});
