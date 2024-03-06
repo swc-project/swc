929a20a3c5e6f44361b1b2d74a9bb4e326c164ed
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-06 16:30:49 +0900

    Update test refs

--- node-swc/__tests__/transform/issue_8701_test.mjs
+++ node-swc/__tests__/transform/issue_8701_test.mjs
@@ -24,5 +24,28 @@ it("should transpile import path correctly", async () => {
     });
 
     expect(code).toMatchInlineSnapshot(`
+        "function _ts_decorate(decorators, target, key, desc) {
+            var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+            if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+            else for(var i = decorators.length - 1; i >= 0; i--)if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+            return c > 3 && r && Object.defineProperty(target, key, r), r;
+        }
+        import { Module } from "@nestjs/common";
+        import { AppController } from "./app.controller";
+        import { AppService } from "./app.service";
+        export class AppModule {
+        }
+        AppModule = _ts_decorate([
+            Module({
+                imports: [],
+                controllers: [
+                    AppController
+                ],
+                providers: [
+                    AppService
+                ]
+            })
+        ], AppModule);
+        "
     `);
 });
