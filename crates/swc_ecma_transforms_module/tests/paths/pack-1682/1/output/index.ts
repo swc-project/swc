20abf96cb0f9f029b2f3636c67d1294e3b46d032
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-06 16:43:26 +0900

    Update test refs

--- crates/swc_ecma_transforms_module/tests/paths/pack-1682/1/output/index.ts
+++ crates/swc_ecma_transforms_module/tests/paths/pack-1682/1/output/index.ts
@@ -1,4 +1,4 @@
-import PrismaClientProvider from "db/PrismaClientProvider";
+import PrismaClientProvider from "../db/PrismaClientProvider.js";
 export default function setupTests() {
     const context = {};
     beforeEach(()=>{
