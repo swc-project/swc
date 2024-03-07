606921700ee9fdb3747a8b54bb851e1d0df27484
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-03-07 16:18:26 +0900

    fix(es/minifier): Do not drop used properties (#8703)
    **Related issue:**
    
     - Closes #8692

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/8692/input.js
@@ -0,0 +1,10 @@
+const props = {
+    neededProp: 1,
+    nestedProp: {
+        getProps: () => props,
+        getProp() {
+            return this.getProps().neededProp;
+        },
+    },
+};
+console.log(props.nestedProp.getProp()); // should log: 1
