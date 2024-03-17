054420863bcdaadead023e0c436bea3bc35b193a
Levi <github.annotate017@passmail.net>
2024-03-17 15:05:47 +1300

    Fix broken index logic

--- crates/swc_ecma_transforms_optimization/src/simplify/expr/mod.rs
+++ crates/swc_ecma_transforms_optimization/src/simplify/expr/mod.rs
@@ -139,13 +139,16 @@ impl SimplifyExpr {
                 if self.in_callee {
                     return;
                 }
-                
-                // JavaScript cast index expression to a number
-                let Known(index) = expr.as_pure_number(&self.expr_ctx) else {
+
+                if let Expr::Lit(Lit::Num(Number { value, .. })) = &**expr {
+                    // x[5]
+                    KnownOp::Index(*value)
+                } else if let Known(s) = expr.as_pure_string(&self.expr_ctx) {
+                    // x[''] or x[...] where ... is an expression like [], ie x[[]]
+                    KnownOp::IndexStr(JsWord::from(s))
+                } else {
                     return;
-                };
-                
-                KnownOp::Index(index)
+                }
             }
             _ => return,
         };
@@ -172,7 +175,7 @@ impl SimplifyExpr {
                         self.changed = true;
                 KnownOp::Index(idx) => {
                     self.changed = true;
-                    
+
                     if idx.fract() != 0.0 || idx < 0.0 || idx as usize >= value.len() {
                         *expr = *undefined(*span)
                     } else {
@@ -184,13 +187,20 @@ impl SimplifyExpr {
                             span: *span,
                         }))
                     };
+                },
+                
+                // 'foo'['']
+                KnownOp::IndexStr(_) => {
+                    self.changed = true;
+                    *expr = *undefined(*span);
                 }
-                _ => {}
             },
 
             // [1, 2, 3].length
             //
             // [1, 2, 3][0]
+            //
+            // [1, 2, 3]['']
             Expr::Array(ArrayLit { elems, span }) => {
                 // do nothing if spread exists
                 let has_spread = elems.iter().any(|elem| {
@@ -287,6 +297,10 @@ impl SimplifyExpr {
                     exprs.push(val);
 
                     *expr = Expr::Seq(SeqExpr { span: *span, exprs });
+                } else if matches!(op, KnownOp::IndexStr(..)) {
+                    self.changed = true;
+                    *expr = *undefined(*span);
+                    return;
                 }
             }
 
