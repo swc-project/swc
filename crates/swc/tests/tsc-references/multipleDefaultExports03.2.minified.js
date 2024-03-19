c9c971a79236f0e5c713f4341ea71591e979ea21
pshu <stormslowly@gmail.com>
2024-03-19 13:39:06 +0800

    feat(es/lints): Detect duplicate bindings in export defaults (#8760)
    **Description:**
    add DefaultDecl's FnExpr and ClassExpr's ident to bindings. 
    because all of them should be treated as hoisted Fn/Class Declare
    
    ```txt
      × the name `x` is defined multiple times
       ╭─[examples/all.js:1:1]
     1 │ export default class x{}
       ·                      ┬
       ·                      ╰── previous definition of `x` here
     2 │
     3 │ let x = 1;
       ·     ┬
       ·     ╰── `x` redefined here
     4 │ let t = function x(){};
       ╰────
    ```
    
    
    
    **Related issue:**
    
     - Closes #8755

--- crates/swc/tests/tsc-references/multipleDefaultExports03.2.minified.js
+++ crates/swc/tests/tsc-references/multipleDefaultExports03.2.minified.js
@@ -1,5 +1,19 @@
 //// [multipleDefaultExports03.ts]
 //! 
+//!   x the name `C` is defined multiple times
+//!    ,-[1:1]
+//!  1 | 
+//!  2 | export default class C {
+//!    :                      |
+//!    :                      `-- previous definition of `C` here
+//!  3 | }
+//!  4 | 
+//!  5 | export default class C {
+//!    :                      |
+//!    :                      `-- `C` redefined here
+//!  6 | }
+//!    `----
+//! 
 //!   x the name `default` is exported multiple times
 //!    ,-[1:1]
 //!  1 |     
