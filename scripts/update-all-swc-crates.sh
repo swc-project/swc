2b8aa63bd0a3aa8e976ce12ca44b972a5c76cff5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-26 14:46:44 +0900

    chore: Fix update script

--- scripts/update-all-swc-crates.sh
+++ scripts/update-all-swc-crates.sh
@@ -6,6 +6,14 @@ echo "Listing all swc crates"
 
 crates=$(cargo metadata --format-version=1 | jq '.packages .[] | select(.repository == "https://github.com/swc-project/swc.git" or .repository == "https://github.com/swc-project/plugins.git") | .name')
 
+command="cargo update"
+for crate in $crates; do
+  command="$command -p $crate"
+done
+
+echo "Running: $command"
+eval $command
+
 command="cargo upgrade --incompatible --recursive false"
 for crate in $crates; do
   command="$command -p $crate"
