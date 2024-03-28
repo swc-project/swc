ecd878c53134fc9f24644a1f0aad6c8f735160c0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-03-28 12:47:59 +0900

    chore: Fix crate update script

--- scripts/update-all-swc-crates.sh
+++ scripts/update-all-swc-crates.sh
@@ -4,18 +4,22 @@ set -eu
 
 echo "Listing all swc crates"
 
-crates=$(cargo metadata --format-version=1 | jq '.packages .[] | select(.repository == "https://github.com/swc-project/swc.git" or .repository == "https://github.com/swc-project/plugins.git") | .name')
+swc_crates=$(cargo metadata --format-version=1 | jq '.packages .[] | select(.repository == "https://github.com/swc-project/swc.git" or .repository == "https://github.com/swc-project/plugins.git") | .name' -r)
 
 command="cargo update"
-for crate in $crates; do
+for crate in $swc_crates; do
   command="$command -p $crate"
 done
 
 echo "Running: $command"
 eval $command
 
+all_direct_deps=$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.source == null) | .dependencies .[] .name' -r)
+
+direct_swc_deps=$(comm -12 <(echo "$swc_crates" | sort) <(echo "$all_direct_deps" | sort))
+
 command="cargo upgrade --incompatible --recursive false"
-for crate in $crates; do
+for crate in $direct_swc_deps; do
   command="$command -p $crate"
 done
 
