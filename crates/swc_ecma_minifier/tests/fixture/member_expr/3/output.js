f59430d58c25201a9d226dc394a9a30fe687272f
Levi <github.annotate017@passmail.net>
2024-04-03 07:06:30 +1300

    Add unit test files

---
+++ crates/swc_ecma_minifier/tests/fixture/member_expr/3/output.js
@@ -0,0 +1,13 @@
+// Object symbols
+[].constructor;
+[].__proto__;
+[].__defineGetter__;
+[].__defineSetter__;
+[].__lookupGetter__;
+[].__lookupSetter__;
+[].hasOwnProperty;
+[].isPrototypeOf;
+[].propertyIsEnumerable;
+[].toLocaleString;
+[].toString;
+[].valueOf;
