13c0eed8595a64b100d6e17aff64b6cae67be482
levi <github.annotate017@passmail.net>
2024-04-03 07:42:50 +1300

    Update test files

--- crates/swc_ecma_minifier/tests/fixture/member_expr/3/output.js
+++ crates/swc_ecma_minifier/tests/fixture/member_expr/3/output.js
@@ -1,13 +1,1 @@
-// Object symbols
-[].constructor;
-[].__proto__;
-[].__defineGetter__;
-[].__defineSetter__;
-[].__lookupGetter__;
-[].__lookupSetter__;
-[].hasOwnProperty;
-[].isPrototypeOf;
-[].propertyIsEnumerable;
-[].toLocaleString;
-[].toString;
-[].valueOf;
+[].constructor, [].__proto__, [].__defineGetter__, [].__defineSetter__, [].__lookupGetter__, [].__lookupSetter__, [].hasOwnProperty, [].isPrototypeOf, [].propertyIsEnumerable, [].toLocaleString, [].toString, [].valueOf;
