af09c0190bfcecc62026070f0ec98f18fe1ced16
Levi <github.annotate017@passmail.net>
2024-04-04 04:50:37 +1300

    Add array side effect unit test

---
+++ crates/swc_ecma_minifier/tests/fixture/member_expr/array_side_effects/output.js
@@ -0,0 +1,7 @@
+f(void 0), f(void 0), f(void 0), f(void 0), f((x(), void y())), f((x(), void y())), f(void 0), f(void 0), f((x(), void y())), f((x(), void y())), f([].push), f([].push), f([
+    x(),
+    y()
+].push), f([
+    x(),
+    y()
+].push);
