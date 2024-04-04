47714c52ce08b0e94617d17c11c8e8994c62dbaa
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-04-04 15:56:29 +0900

    feat(es/minifier): Evaluate spread of arrays (#8811)
    **Related issue:**
    
     - Closes #8808

--- crates/swc/tests/tsc-references/callWithSpread4.2.minified.js
+++ crates/swc/tests/tsc-references/callWithSpread4.2.minified.js
@@ -1,6 +1,2 @@
 //// [callWithSpread4.ts]
-pli(reads, ...gun, tr, fun, ...gz, writes), test(...anys), pli(...[
-    reads,
-    writes,
-    writes
-]);
+pli(reads, ...gun, tr, fun, ...gz, writes), test(...anys), pli(reads, writes, writes);
