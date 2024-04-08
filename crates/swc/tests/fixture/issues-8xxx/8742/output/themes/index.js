12ae9e574844b751a159836a6fcbd1c5ae05439b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-08 11:31:40 +0900

    Fix input

--- crates/swc/tests/fixture/issues-8xxx/8742/output/themes/index.js
+++ crates/swc/tests/fixture/issues-8xxx/8742/output/themes/index.js
@@ -1,8 +1,1 @@
-// ======
-// SOURCE
-// ======
-export { teamsDarkTheme, teamsHighContrastTheme, teamsLightTheme, webDarkTheme, webLightTheme } from "./themes"; // 🚨 invalid directory path - wont work within browser
-export { createDarkTheme, createHighContrastTheme, createLightTheme, createTeamsDarkTheme } from "./utils"; // 🚨 invalid directory path - wont work within browser
-export { themeToTokensObject } from "./themeToTokensObject";
-export { tokens } from "./tokens";
-export { typographyStyles } from "./global";
+export { };
