f78d1b13a8ed9f5df1bdca20529f1a666ac72902
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-08 11:48:08 +0900

    --fix

--- crates/swc_html_codegen/tests/fixture.rs
+++ crates/swc_html_codegen/tests/fixture.rs
@@ -24,18 +24,9 @@ fn print_document(
     codegen_config: Option<CodegenConfig>,
 ) {
     let dir = input.parent().unwrap();
-    let parser_config = match parser_config {
-        Some(parser_config) => parser_config,
-        _ => ParserConfig::default(),
-    };
-    let writer_config = match writer_config {
-        Some(writer_config) => writer_config,
-        _ => BasicHtmlWriterConfig::default(),
-    };
-    let codegen_config = match codegen_config {
-        Some(codegen_config) => codegen_config,
-        _ => CodegenConfig::default(),
-    };
+    let parser_config = parser_config.unwrap_or_default();
+    let writer_config = writer_config.unwrap_or_default();
+    let codegen_config = codegen_config.unwrap_or_default();
     let output = if codegen_config.minify {
         dir.join(format!(
             "output.min.{}",
@@ -98,18 +89,9 @@ fn print_document_fragment(
     codegen_config: Option<CodegenConfig>,
 ) {
     let dir = input.parent().unwrap();
-    let parser_config = match parser_config {
-        Some(parser_config) => parser_config,
-        _ => ParserConfig::default(),
-    };
-    let writer_config = match writer_config {
-        Some(writer_config) => writer_config,
-        _ => BasicHtmlWriterConfig::default(),
-    };
-    let codegen_config = match codegen_config {
-        Some(codegen_config) => codegen_config,
-        _ => CodegenConfig::default(),
-    };
+    let parser_config = parser_config.unwrap_or_default();
+    let writer_config = writer_config.unwrap_or_default();
+    let codegen_config = codegen_config.unwrap_or_default();
     let output = if codegen_config.minify {
         dir.join(format!(
             "output.min.{}",
@@ -185,18 +167,9 @@ fn verify_document(
     codegen_config: Option<CodegenConfig>,
     ignore_errors: bool,
 ) {
-    let parser_config = match parser_config {
-        Some(parser_config) => parser_config,
-        _ => ParserConfig::default(),
-    };
-    let writer_config = match writer_config {
-        Some(writer_config) => writer_config,
-        _ => BasicHtmlWriterConfig::default(),
-    };
-    let codegen_config = match codegen_config {
-        Some(codegen_config) => codegen_config,
-        _ => CodegenConfig::default(),
-    };
+    let parser_config = parser_config.unwrap_or_default();
+    let writer_config = writer_config.unwrap_or_default();
+    let codegen_config = codegen_config.unwrap_or_default();
 
     testing::run_test2(false, |cm, handler| {
         let fm = cm.load_file(input).unwrap();
@@ -250,18 +223,9 @@ fn verify_document_fragment(
     codegen_config: Option<CodegenConfig>,
     ignore_errors: bool,
 ) {
-    let parser_config = match parser_config {
-        Some(parser_config) => parser_config,
-        _ => ParserConfig::default(),
-    };
-    let writer_config = match writer_config {
-        Some(writer_config) => writer_config,
-        _ => BasicHtmlWriterConfig::default(),
-    };
-    let mut codegen_config = match codegen_config {
-        Some(codegen_config) => codegen_config,
-        _ => CodegenConfig::default(),
-    };
+    let parser_config = parser_config.unwrap_or_default();
+    let writer_config = writer_config.unwrap_or_default();
+    let mut codegen_config = codegen_config.unwrap_or_default();
 
     codegen_config.context_element = Some(&context_element);
 
