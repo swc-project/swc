5629f24f0fa513c578df89d7763d943616953d2b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-15 10:55:42 +0900

    Add a test

---
+++ crates/swc/tests/minify/next-62593/input.js
@@ -0,0 +1,290 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[931], {
+
+    /***/ 1478:
+    /***/ (function (__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+            Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 6428));
+
+
+            /***/
+}),
+
+    /***/ 6428:
+    /***/ (function (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+            "use strict";
+
+            // EXPORTS
+            __webpack_require__.d(__webpack_exports__, {
+                "default": function () { return /* binding */ Counter; }
+            });
+
+            // UNUSED EXPORTS: dynamic, revalidate
+
+            // EXTERNAL MODULE: ./node_modules/next/dist/compiled/react/jsx-runtime.js
+            var jsx_runtime = __webpack_require__(7437);
+            // EXTERNAL MODULE: ./node_modules/next/dist/compiled/react/index.js
+            var react = __webpack_require__(2265);
+            // EXTERNAL MODULE: ./node_modules/use-sync-external-store/shim/with-selector.js
+            var with_selector = __webpack_require__(5006);
+            ;// CONCATENATED MODULE: ./node_modules/zustand/esm/index.mjs
+
+
+
+
+
+            const { useDebugValue } = react;
+            const { useSyncExternalStoreWithSelector } = with_selector;
+            let didWarnAboutEqualityFn = false;
+            const identity = (arg) => arg;
+            function useStore(api, selector = identity, equalityFn) {
+                if ((false ? 0 : void 0) !== "production" && equalityFn && !didWarnAboutEqualityFn) {
+                    console.warn(
+                        "[DEPRECATED] Use `createWithEqualityFn` instead of `create` or use `useStoreWithEqualityFn` instead of `useStore`. They can be imported from 'zustand/traditional'. https://github.com/pmndrs/zustand/discussions/1937"
+                    );
+                    didWarnAboutEqualityFn = true;
+                }
+                const slice = useSyncExternalStoreWithSelector(
+                    api.subscribe,
+                    api.getState,
+                    api.getServerState || api.getInitialState,
+                    selector,
+                    equalityFn
+                );
+                useDebugValue(slice);
+                return slice;
+            }
+            const createImpl = (createState) => {
+                if ((false ? 0 : void 0) !== "production" && typeof createState !== "function") {
+                    console.warn(
+                        "[DEPRECATED] Passing a vanilla store will be unsupported in a future version. Instead use `import { useStore } from 'zustand'`."
+                    );
+                }
+                const api = typeof createState === "function" ? createStore(createState) : createState;
+                const useBoundStore = (selector, equalityFn) => useStore(api, selector, equalityFn);
+                Object.assign(useBoundStore, api);
+                return useBoundStore;
+            };
+            const create = (createState) => createState ? createImpl(createState) : createImpl;
+            var esm_react = (createState) => {
+                if ((false ? 0 : void 0) !== "production") {
+                    console.warn(
+                        "[DEPRECATED] Default export is deprecated. Instead use `import { create } from 'zustand'`."
+                    );
+                }
+                return create(createState);
+            };
+
+
+
+            ;// CONCATENATED MODULE: ./node_modules/zustand/esm/vanilla.mjs
+            const createStoreImpl = (createState) => {
+                let state;
+                const listeners = /* @__PURE__ */ new Set();
+                const setState = (partial, replace) => {
+                    const nextState = typeof partial === "function" ? partial(state) : partial;
+                    if (!Object.is(nextState, state)) {
+                        const previousState = state;
+                        state = (replace != null ? replace : typeof nextState !== "object" || nextState === null) ? nextState : Object.assign({}, state, nextState);
+                        listeners.forEach((listener) => listener(state, previousState));
+                    }
+                };
+                const getState = () => state;
+                const getInitialState = () => initialState;
+                const subscribe = (listener) => {
+                    listeners.add(listener);
+                    return () => listeners.delete(listener);
+                };
+                const destroy = () => {
+                    if ((false ? 0 : void 0) !== "production") {
+                        console.warn(
+                            "[DEPRECATED] The `destroy` method will be unsupported in a future version. Instead use unsubscribe function returned by subscribe. Everything will be garbage-collected if store is garbage-collected."
+                        );
+                    }
+                    listeners.clear();
+                };
+                const api = { setState, getState, getInitialState, subscribe, destroy };
+                const initialState = state = createState(setState, getState, api);
+                return api;
+            };
+            const vanilla_createStore = (createState) => createState ? createStoreImpl(createState) : createStoreImpl;
+            var vanilla = (createState) => {
+                if ((false ? 0 : void 0) !== "production") {
+                    console.warn(
+                        "[DEPRECATED] Default export is deprecated. Instead use import { createStore } from 'zustand/vanilla'."
+                    );
+                }
+                return vanilla_createStore(createState);
+            };
+
+
+
+            ;// CONCATENATED MODULE: ./lib/counterSlice.ts
+    /* __next_internal_client_entry_do_not_use__ createCounterSlice auto */ const createCounterSlice = (a, initialValues) => {
+                const createCounterSliceInner = (set, get) => {
+                    var _initialValues_count;
+                    return {
+                        count: (_initialValues_count = initialValues.count) !== null && _initialValues_count !== void 0 ? _initialValues_count : 0,
+                        incrBy: (by) => set({
+                            count: get().count + by
+                        })
+                    };
+                };
+                return createCounterSliceInner(...a);
+            };
+
+            ;// CONCATENATED MODULE: ./lib/store.tsx
+            /* __next_internal_client_entry_do_not_use__ StoreContext,StoreProvider,useBoundStore auto */
+
+
+
+            const StoreContext = /*#__PURE__*/ (0, react.createContext)(null);
+            function StoreProvider(param) {
+                let { children, ...props } = param;
+                const initialProps = (0, react.useRef)(props);
+                const storeRef = (0, react.useRef)();
+                if (!storeRef.current) {
+                    storeRef.current = createBoundStore(props);
+                } else if (props !== initialProps.current) {
+                    // If the props change, we need to update the store
+                    initialProps.current = props;
+                    storeRef.current.setState(props);
+                }
+                return /*#__PURE__*/ (0, jsx_runtime.jsx)(StoreContext.Provider, {
+                    value: storeRef.current,
+                    children: children
+                });
+            }
+            function useBoundStore(selector) {
+                var _useContext;
+                const store = (_useContext = (0, react.useContext)(StoreContext)) !== null && _useContext !== void 0 ? _useContext : (() => {
+                    throw new Error("Store not found");
+                })();
+                return useStore(store, selector);
+            }
+            const createBoundStore = (initialState) => {
+                const boundStore = vanilla_createStore()(function () {
+                    for (var _len = arguments.length, a = new Array(_len), _key = 0; _key < _len; _key++) {
+                        a[_key] = arguments[_key];
+                    }
+                    return {
+                        ...createCounterSlice(a, initialState)
+                    };
+                });
+                boundStore.setState(initialState);
+                return boundStore;
+            };
+
+            ;// CONCATENATED MODULE: ./components/counter.tsx
+            /* __next_internal_client_entry_do_not_use__ dynamic,revalidate,default auto */
+            const dynamic = "force-dynamic";
+            const revalidate = 0;
+
+            function Counter() {
+                return /*#__PURE__*/ (0, jsx_runtime.jsx)(StoreProvider, {
+                    count: 0,
+                    children: /*#__PURE__*/ (0, jsx_runtime.jsx)(CounterButton, {})
+                });
+            }
+            function CounterButton() {
+                const { count, incrBy } = useBoundStore((state) => state);
+                return /*#__PURE__*/ (0, jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+                    children: [
+                /*#__PURE__*/ (0, jsx_runtime.jsxs)("p", {
+                        children: [
+                            "Count: ",
+                            count
+                        ]
+                    }),
+                /*#__PURE__*/ (0, jsx_runtime.jsx)("button", {
+                        onClick: () => incrBy(1),
+                        children: "Increment"
+                    })
+                    ]
+                });
+            }
+
+
+            /***/
+}),
+
+    /***/ 4492:
+    /***/ (function (__unused_webpack_module, exports, __webpack_require__) {
+
+            "use strict";
+            /**
+             * @license React
+             * use-sync-external-store-shim.production.min.js
+             *
+             * Copyright (c) Facebook, Inc. and its affiliates.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */
+            var e = __webpack_require__(2265); function h(a, b) { return a === b && (0 !== a || 1 / a === 1 / b) || a !== a && b !== b } var k = "function" === typeof Object.is ? Object.is : h, l = e.useState, m = e.useEffect, n = e.useLayoutEffect, p = e.useDebugValue; function q(a, b) { var d = b(), f = l({ inst: { value: d, getSnapshot: b } }), c = f[0].inst, g = f[1]; n(function () { c.value = d; c.getSnapshot = b; r(c) && g({ inst: c }) }, [a, d, b]); m(function () { r(c) && g({ inst: c }); return a(function () { r(c) && g({ inst: c }) }) }, [a]); p(d); return d }
+            function r(a) { var b = a.getSnapshot; a = a.value; try { var d = b(); return !k(a, d) } catch (f) { return !0 } } function t(a, b) { return b() } var u = "undefined" === typeof window || "undefined" === typeof window.document || "undefined" === typeof window.document.createElement ? t : q; exports.useSyncExternalStore = void 0 !== e.useSyncExternalStore ? e.useSyncExternalStore : u;
+
+
+            /***/
+}),
+
+    /***/ 5107:
+    /***/ (function (__unused_webpack_module, exports, __webpack_require__) {
+
+            "use strict";
+            /**
+             * @license React
+             * use-sync-external-store-shim/with-selector.production.min.js
+             *
+             * Copyright (c) Facebook, Inc. and its affiliates.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */
+            var h = __webpack_require__(2265), n = __webpack_require__(554); function p(a, b) { return a === b && (0 !== a || 1 / a === 1 / b) || a !== a && b !== b } var q = "function" === typeof Object.is ? Object.is : p, r = n.useSyncExternalStore, t = h.useRef, u = h.useEffect, v = h.useMemo, w = h.useDebugValue;
+            exports.useSyncExternalStoreWithSelector = function (a, b, e, l, g) {
+                var c = t(null); if (null === c.current) { var f = { hasValue: !1, value: null }; c.current = f } else f = c.current; c = v(function () { function a(a) { if (!c) { c = !0; d = a; a = l(a); if (void 0 !== g && f.hasValue) { var b = f.value; if (g(b, a)) return k = b } return k = a } b = k; if (q(d, a)) return b; var e = l(a); if (void 0 !== g && g(b, e)) return b; d = a; return k = e } var c = !1, d, k, m = void 0 === e ? null : e; return [function () { return a(b()) }, null === m ? void 0 : function () { return a(m()) }] }, [b, e, l, g]); var d = r(a, c[0], c[1]);
+                u(function () { f.hasValue = !0; f.value = d }, [d]); w(d); return d
+            };
+
+
+            /***/
+}),
+
+    /***/ 554:
+    /***/ (function (module, __unused_webpack_exports, __webpack_require__) {
+
+            "use strict";
+
+
+            if (true) {
+                module.exports = __webpack_require__(4492);
+            } else { }
+
+
+            /***/
+}),
+
+    /***/ 5006:
+    /***/ (function (module, __unused_webpack_exports, __webpack_require__) {
+
+            "use strict";
+
+
+            if (true) {
+                module.exports = __webpack_require__(5107);
+            } else { }
+
+
+            /***/
+})
+
+},
+    /******/ function (__webpack_require__) { // webpackRuntimeModules
+    /******/ var __webpack_exec__ = function (moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+    /******/ __webpack_require__.O(0, [971, 23, 744], function () { return __webpack_exec__(1478); });
+    /******/ var __webpack_exports__ = __webpack_require__.O();
+    /******/ _N_E = __webpack_exports__;
+    /******/
+}
+]);
