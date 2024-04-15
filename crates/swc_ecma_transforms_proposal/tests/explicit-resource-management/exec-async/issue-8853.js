c3e61b5945e0af2aa2bdd23c42479c6b1961bd09
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-15 13:11:09 +0900

    Add a test

---
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/exec-async/issue-8853.js
@@ -0,0 +1,40 @@
+import { deepStrictEqual } from 'node:assert'
+import process from 'node:process'
+
+let i = 0
+let err
+try {
+    await using _x1 = {
+        async [Symbol.asyncDispose]() {
+            throw [1, ++i]
+        }
+    }
+
+    await using _x2 = {
+        async [Symbol.asyncDispose]() {
+            throw [2, ++i]
+        }
+    }
+
+    await using _x3 = {
+        async [Symbol.asyncDispose]() {
+            throw [3, ++i]
+        }
+    }
+
+    await using _x4 = {
+        async [Symbol.asyncDispose]() {
+            throw [4, ++i]
+        }
+    }
+
+    throw [5, ++i]
+} catch (e) {
+    err = e
+}
+
+deepStrictEqual(err.error, 1)
+deepStrictEqual(err.suppressed.error, 2)
+deepStrictEqual(err.suppressed.suppressed.error, 3)
+deepStrictEqual(err.suppressed.suppressed.suppressed.error, 4)
+deepStrictEqual(err.suppressed.suppressed.suppressed.suppressed, 5)
