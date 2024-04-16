83a4fd9961f9592506d35b28f5d2a027eeaf89ec
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-16 14:28:30 +0900

    Add `maybe_grow` to `swc_ecma_utils`

---
+++ crates/swc_ecma_utils/src/stack_size.rs
@@ -0,0 +1,20 @@
+#[inline(always)]
+#[cfg(any(
+    target_arch = "wasm32",
+    target_arch = "arm",
+    not(feature = "stacker"),
+    // miri does not work with stacker
+    miri
+))]
+pub fn maybe_grow<R, F: FnOnce() -> R>(_red_zone: usize, _stack_size: usize, callback: F) -> R {
+    callback()
+}
+
+#[inline(always)]
+#[cfg(all(
+    not(any(target_arch = "wasm32", target_arch = "arm", miri)),
+    feature = "stacker"
+))]
+pub fn maybe_grow<R, F: FnOnce() -> R>(red_zone: usize, stack_size: usize, callback: F) -> R {
+    stacker::maybe_grow(red_zone, stack_size, callback)
+}
