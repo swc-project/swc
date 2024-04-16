1dac26f5744f7e084841e7c533ec92d65ecb0f0a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-16 15:36:56 +0900

    stack size

--- crates/swc_ecma_parser/src/parser/stmt.rs
+++ crates/swc_ecma_parser/src/parser/stmt.rs
@@ -519,7 +519,7 @@ impl<'a, I: Tokens> Parser<I> {
 
         let cons = {
             // Prevent stack overflow
-            crate::maybe_grow(4 * 1024, 16 * 1024, || {
+            crate::maybe_grow(4 * 1024, 64 * 1024, || {
                 // Annex B
                 if !self.ctx().strict && is!(self, "function") {
                     // TODO: report error?
@@ -552,7 +552,7 @@ impl<'a, I: Tokens> Parser<I> {
 
                 if !is!(self, "if") {
                     // As we eat `else` above, we need to parse statement once.
-                    let last = crate::maybe_grow(4 * 1024, 16 * 1024, || {
+                    let last = crate::maybe_grow(4 * 1024, 64 * 1024, || {
                         let ctx = Context {
                             ignore_else_clause: false,
                             ..self.ctx()
