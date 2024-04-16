e231a53152ef502aa7af153b8562b38aae4b59e5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-16 15:04:58 +0900

    fix

--- crates/swc_ecma_transforms_base/src/hygiene/mod.rs
+++ crates/swc_ecma_transforms_base/src/hygiene/mod.rs
@@ -1,6 +1,7 @@
 use swc_common::{chain, Mark};
 use swc_ecma_ast::*;
-use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
+use swc_ecma_utils::stack_size::maybe_grow_default;
+use swc_ecma_visit::{as_folder, noop_visit_mut_type, Fold, VisitMut, VisitMutWith};
 
 pub use crate::rename::rename;
 use crate::rename::{renamer, Renamer};
@@ -82,6 +83,10 @@ struct HygieneRemover;
 
 impl VisitMut for HygieneRemover {
     noop_visit_mut_type!();
+
+    fn visit_mut_expr(&mut self, n: &mut Expr) {
+        maybe_grow_default(|| n.visit_mut_children_with(self));
+    }
 
     fn visit_mut_ident(&mut self, i: &mut Ident) {
         i.span.ctxt = Default::default();
