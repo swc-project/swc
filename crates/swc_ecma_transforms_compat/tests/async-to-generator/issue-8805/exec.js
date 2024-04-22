d2ccc5b169d9bca3cf554994ce72d7a72e4b499f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-22 14:35:28 +0900

    Add a test

---
+++ crates/swc_ecma_transforms_compat/tests/async-to-generator/issue-8805/exec.js
@@ -0,0 +1,24 @@
+async function* foo() {
+    yield 1
+}
+
+async function* bar(inputs, returnValues) {
+    for await (const input of inputs) {
+        if (!returnValues) {
+            return
+        }
+        yield input
+    }
+}
+
+async function run() {
+    for await (const number of bar(foo(), true)) {
+        console.log(number)
+    }
+
+    for await (const value of bar(foo(), false)) {
+        console.log(value)
+    }
+}
+
+run()
