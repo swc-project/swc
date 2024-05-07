501654bc7b383b8cb8cd13d2bfc87f77c135610e
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-07 11:05:47 +0900

    Update test refs

--- crates/swc/tests/fixture/deno/paths/issue-2724/output/src/index.ts
+++ crates/swc/tests/fixture/deno/paths/issue-2724/output/src/index.ts
@@ -1,4 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _interop_require_wildcard = require("@swc/helpers/_/_interop_require_wildcard");
 (async function() {
-    const { displayA } = await import("@print/a");
+    const { displayA } = await Promise.resolve().then(()=>/*#__PURE__*/ _interop_require_wildcard._(require("../packages/a/src")));
     console.log(displayA());
 })();
