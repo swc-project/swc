2405dc6ba0aa227df81fda3db303fc6f523972db
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-05-07 17:43:41 +0900

    fix(css/modules): Fix `:global` selectors without preceding whitespace (#8926)
    **Related issue:**
    
     - Closes #8461

--- crates/swc_css_modules/src/lib.rs
+++ crates/swc_css_modules/src/lib.rs
@@ -704,9 +704,8 @@ fn prepend_left_subclass_selectors(
     {
         c.subclass_selectors.splice(0..0, sels.drain(..sel_index));
 
-        if sels.len() > sel_index {
-            c.subclass_selectors
-                .extend(sels[..sel_index + 1].iter().cloned());
+        if !sels.is_empty() {
+            c.subclass_selectors.extend(sels[..].iter().cloned());
         }
 
         sel_index = 0;
