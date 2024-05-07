6651fe4338d15cc3193d0ec1715cb82aef637ebf
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-07 22:37:10 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/issues-8xxx/8931/output/1.js
@@ -0,0 +1,1 @@
+export function _jsx(t,s){return s}export const a=_jsx("math","P \\vdash q");export const b=_jsx("math","P \\vdash q");
--- crates/swc_ecma_minifier/tests/terser/compress/template_string/special_chars_in_string/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/template_string/special_chars_in_string/output.js
@@ -1,3 +1,3 @@
-var str = "foo `;\n`${any} bar";
+var str = "foo `;\n\\`${any} bar";
 var concat = `foo ${any} bar\`;\n\`\${any}`;
 var template = `foo \`;\n\`\${any} ${any} bar`;
