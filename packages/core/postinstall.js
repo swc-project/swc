3e2d9f7c0344e702d672caa5f131c6043595c55a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-16 20:24:09 +0900

    yarn

--- packages/core/postinstall.js
+++ packages/core/postinstall.js
@@ -133,7 +133,7 @@ const validateBinary = () =>
         // For now, it is considered as deliberate decision.
         let binding;
         try {
-            binding = require("./binding");
+            binding = require("./src/binding");
             // Check if binding binary actually works.
             // For the latest version, checks target triple. If it's old version doesn't have target triple, use parseSync instead.
             const triple = binding.getTargetTriple
