039316a5450fc2b74ea5ab22cba21aa3da54a6f2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-22 18:25:30 +0900

    code

--- crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
@@ -1,1 +1,21 @@
+use std::{fs::read_to_string, process::Command};
 
+use testing::find_executable;
+
+#[test]
+fn execute() {
+    let node = find_executable("node").expect("node not found");
+
+    let code = read_to_string("tests/decorator-tests/decorator-tests.js")
+        .expect("failed to read test input file");
+
+    // Transpile with swc
+
+    let status = Command::new(node)
+        .arg("-e")
+        .arg(&code)
+        .status()
+        .expect("failed to execute process");
+
+    assert!(status.success());
+}
