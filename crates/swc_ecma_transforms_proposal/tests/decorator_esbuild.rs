efdcffa52a280e8175daa1633b3bb4903f4e4f2f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-23 13:52:44 +0900

    program

--- crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
@@ -1,8 +1,12 @@
 use std::{fs::read_to_string, process::Command};
 
+use swc_common::Mark;
 use swc_ecma_ast::EsVersion;
 use swc_ecma_codegen::to_code;
 use swc_ecma_parser::parse_file_as_program;
+use swc_ecma_transforms_base::{fixer::fixer, helpers::inject_helpers, hygiene::hygiene, resolver};
+use swc_ecma_transforms_proposal::decorator_2022_03::decorator_2022_03;
+use swc_ecma_visit::VisitMutWith;
 use testing::find_executable;
 
 #[test]
@@ -11,13 +15,13 @@ fn execute() {
         let node = find_executable("node").expect("node not found");
 
         let fm = cm
-            .load_file("tests/decorator-tests/decorator-tests.js")
+            .load_file("tests/decorator-tests/decorator-tests.js".as_ref())
             .expect("failed to load file");
 
         let code = {
             // Transpile with swc
 
-            let program = parse_file_as_program(
+            let mut program = parse_file_as_program(
                 &fm,
                 swc_ecma_parser::Syntax::Es(swc_ecma_parser::EsConfig {
                     decorators: true,
@@ -29,7 +33,17 @@ fn execute() {
             )
             .expect("failed to parse file");
 
-            to_code(None, &program)
+            let unresolved_mark = Mark::new();
+            let top_level_mark = Mark::new();
+            program.visit_mut_with(&mut resolver(unresolved_mark, top_level_mark, false));
+
+            program.visit_mut_with(&mut decorator_2022_03());
+
+            program.visit_mut_with(&mut inject_helpers(unresolved_mark));
+            program.visit_mut_with(&mut hygiene());
+            program.visit_mut_with(&mut fixer(None));
+
+            to_code(&program)
         };
 
         let status = Command::new(node)
@@ -39,6 +53,8 @@ fn execute() {
             .expect("failed to execute process");
 
         assert!(status.success());
+
+        Ok(())
     })
     .unwrap()
 }
