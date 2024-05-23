56fbb2479ec2466ade71100c0f8761d615782015
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-23 22:23:13 +0900

    split

--- crates/swc_ecma_minifier/tests/exec.rs
+++ crates/swc_ecma_minifier/tests/exec.rs
@@ -11295,11 +11295,19 @@ fn issue_8964() {
 }
 
 #[test]
-fn issue_8982() {
+fn issue_8982_1() {
+    run_default_exec_test(
+        "
+        console.log(Math.max(0, -0));
+        ",
+    );
+}
+
+#[test]
+fn issue_8982_2() {
     run_default_exec_test(
         "
         console.log(Math.min(0, -0));
-        console.log(Math.max(0, -0));
         ",
     );
 }
