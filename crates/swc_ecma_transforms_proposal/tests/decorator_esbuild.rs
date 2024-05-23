bb19588e213fb4f603b3ce0923498ede6a380473
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-23 11:23:57 +0900

    쨔ㅖ

--- crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorator_esbuild.rs
@@ -1,21 +1,43 @@
 use std::{fs::read_to_string, process::Command};
 
+use swc_ecma_ast::EsVersion;
+use swc_ecma_parser::parse_file_as_program;
 use testing::find_executable;
 
 #[test]
 fn execute() {
-    let node = find_executable("node").expect("node not found");
+    testing::run_test(false, |cm, handler| {
+        let node = find_executable("node").expect("node not found");
 
-    let code = read_to_string("tests/decorator-tests/decorator-tests.js")
-        .expect("failed to read test input file");
+        let fm = cm
+            .load_file("tests/decorator-tests/decorator-tests.js")
+            .expect("failed to load file");
 
-    // Transpile with swc
+        let code = {
+            // Transpile with swc
 
-    let status = Command::new(node)
-        .arg("-e")
-        .arg(&code)
-        .status()
-        .expect("failed to execute process");
+            let program = parse_file_as_program(
+                &fm,
+                swc_ecma_parser::Syntax::Es(swc_ecma_parser::EsConfig {
+                    decorators: true,
+                    ..Default::default()
+                }),
+                EsVersion::EsNext,
+                None,
+                &mut vec![],
+            )
+            .expect("failed to parse file");
 
-    assert!(status.success());
+            code
+        };
+
+        let status = Command::new(node)
+            .arg("-e")
+            .arg(&code)
+            .status()
+            .expect("failed to execute process");
+
+        assert!(status.success());
+    })
+    .unwrap()
 }
