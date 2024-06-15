233ae462251b6142694e178ffd0f8a308fcbb65f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-15 17:10:35 +0900

    Repr

--- crates/swc_common/src/source_slice.rs
+++ crates/swc_common/src/source_slice.rs
@@ -3,9 +3,21 @@ use crate::sync::Lrc;
 #[derive(Clone)]
 pub struct SourceSlice(Repr);
 
+impl SourceSlice {
+    pub fn new(src: Lrc<String>, start: u32, end: u32) -> Self {
+        SourceSlice(Repr::Pointer { src, start, end })
+    }
+
+    pub fn new_owned(value: Lrc<String>) -> Self {
+        SourceSlice(Repr::Owned { value })
+    }
+}
+
 #[derive(Clone)]
 enum Repr {
-    Owned(Lrc<String>),
+    Owned {
+        value: Lrc<String>,
+    },
     Pointer {
         src: Lrc<String>,
         start: u32,
