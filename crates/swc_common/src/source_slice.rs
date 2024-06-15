851ca5a4a97c6c42bde2bb60c6b161b9586d4298
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-15 17:18:24 +0900

    Deref

--- crates/swc_common/src/source_slice.rs
+++ crates/swc_common/src/source_slice.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use crate::sync::Lrc;
 
 #[derive(Clone)]
@@ -17,4 +19,15 @@ impl SourceSlice {
 enum Repr {
     Owned { value: Lrc<str> },
     Pointer { src: Lrc<str>, start: u32, end: u32 },
+}
+
+impl Deref for SourceSlice {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        match &self.0 {
+            Repr::Owned { value } => value,
+            Repr::Pointer { src, start, end } => &src[*start as usize..*end as usize],
+        }
+    }
 }
