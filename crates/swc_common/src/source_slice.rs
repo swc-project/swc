3d670a7fcb4131ec2b8d21050a89152e8e08fcf3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-15 17:09:30 +0900

    Repr

--- crates/swc_common/src/source_slice.rs
+++ crates/swc_common/src/source_slice.rs
@@ -1,1 +1,14 @@
-pub struct SourceSlice {}
+use crate::sync::Lrc;
+
+#[derive(Clone)]
+pub struct SourceSlice(Repr);
+
+#[derive(Clone)]
+enum Repr {
+    Owned(Lrc<String>),
+    Pointer {
+        src: Lrc<String>,
+        start: u32,
+        end: u32,
+    },
+}
