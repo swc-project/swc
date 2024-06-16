cdd20cd16370342f645a181198432a3dc3b6c7ab
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-06-16 14:30:01 +0900

    fix(common): Revert `Use SourceMap::adjust_mappings` (#9058)
    **Description:**
    
    https://github.com/getsentry/rust-sourcemap/pull/91 should fix this
    issue, but let's revert #9052 for now.
    
    # Context
    
    `swc_core` regressed.
    
    Caught by https://github.com/vercel/next.js/pull/66902
    
    ```
     ⚠ Linting is disabled.
      ▲ Next.js 15.0.0-canary.34
    
     ✓ Checking validity of types
       Creating an optimized production build ...
    Panic: PanicInfo { payload: Any { .. }, message: Some(attempt to add with overflow), location: Location { file: "/Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/sourcemap-8.0.1/src/encoder.rs", line: 89, col: 13 }, can_unwind: true, force_no_backtrace: false }
    Backtrace:    0: backtrace::backtrace::libunwind::trace
                 at /Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.68/src/backtrace/libunwind.rs:93:5
          backtrace::backtrace::trace_unsynchronized::<<backtrace::capture::Backtrace>::create::{closure#0}>
                 at /Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.68/src/backtrace/mod.rs:66:5
          backtrace::backtrace::trace::<<backtrace::capture::Backtrace>::create::{closure#0}>
                 at /Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.68/src/backtrace/mod.rs:53:14
          <backtrace::capture::Backtrace>::create
                 at /Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.68/src/capture.rs:176:9
          <backtrace::capture::Backtrace>::new
                 at /Users/kdy1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/backtrace-0.3.68/src/capture.rs:140:22
       1: next_swc_napi::init::{closure#0}
                 at packages/next-swc/crates/napi/src/lib.rs:85:29
       2: <alloc::boxed::Box<F,A> as core::ops::function::Fn<Args>>::call
                 at /rustc/6f3df08aadf71e8d4bf7e49f5dc10dfa6f254cb4/library/alloc/src/boxed.rs:2077:9
          std::panicking::rust_panic_with_hook
                 at /rustc/6f3df08aadf71e8d4bf7e49f5dc10dfa6f254cb4/library/std/src/panicking.rs:799:13
       3: std::panicking::begin_panic_handler::{{closure}}
                 at /rustc/6f3df08aadf71e8d4bf7e49f5dc10dfa6f254cb4/library/std/src/panicking.rs
    ```

--- crates/swc_common/src/source_map.rs
+++ crates/swc_common/src/source_map.rs
@@ -1212,12 +1212,21 @@ impl SourceMap {
     pub fn build_source_map_with_config(
         &self,
         mappings: &[(BytePos, LineCol)],
-        mut orig: Option<sourcemap::SourceMap>,
+        orig: Option<sourcemap::SourceMap>,
         config: impl SourceMapGenConfig,
     ) -> sourcemap::SourceMap {
         let mut builder = SourceMapBuilder::new(None);
 
         let mut src_id = 0u32;
+
+        if let Some(orig) = &orig {
+            for src in orig.sources() {
+                let id = builder.add_source(src);
+                src_id = id;
+
+                builder.set_source_contents(id, orig.get_source_contents(id));
+            }
+        }
 
         // // This method is optimized based on the fact that mapping is sorted.
         // mappings.sort_by_key(|v| v.0);
@@ -1226,6 +1235,7 @@ impl SourceMap {
 
         let mut prev_dst_line = u32::MAX;
 
+        let mut inline_sources_content = false;
         let mut ch_state = ByteToCharPosState::default();
         let mut line_state = ByteToCharPosState::default();
 
@@ -1257,11 +1267,13 @@ impl SourceMap {
                     if config.skip(&f.name) {
                         continue;
                     }
-                    src_id = builder.add_source(&config.file_name_to_source(&f.name));
+                    if orig.is_none() {
+                        src_id = builder.add_source(&config.file_name_to_source(&f.name));
 
-                    let inline_sources_content = config.inline_sources_content(&f.name);
-                    if inline_sources_content {
-                        builder.set_source_contents(src_id, Some(&f.src));
+                        inline_sources_content = config.inline_sources_content(&f.name);
+                        if inline_sources_content && orig.is_none() {
+                            builder.set_source_contents(src_id, Some(&f.src));
+                        }
                     }
 
                     ch_state = ByteToCharPosState::default();
@@ -1281,7 +1293,7 @@ impl SourceMap {
                 continue;
             }
 
-            let line = match f.lookup_line(pos) {
+            let mut line = match f.lookup_line(pos) {
                 Some(line) => line as u32,
                 None => continue,
             };
@@ -1307,8 +1319,31 @@ impl SourceMap {
                 linechpos,
             );
 
-            let col = chpos - linechpos;
-            let name = None;
+            let mut col = chpos - linechpos;
+            let mut name = None;
+            if let Some(orig) = &orig {
+                if let Some(token) = orig
+                    .lookup_token(line, col)
+                    .filter(|t| t.get_dst_line() == line)
+                {
+                    line = token.get_src_line();
+                    col = token.get_src_col();
+                    if token.has_name() {
+                        name = token.get_name();
+                    }
+                    if token.get_source().is_some() {
+                        // When we have the original source map, we use the source ids from it
+                        src_id = token.get_raw_token().src_id;
+                        if inline_sources_content && !builder.has_source_contents(src_id) {
+                            if let Some(contents) = token.get_source_view() {
+                                builder.set_source_contents(src_id, Some(contents.source()));
+                            }
+                        }
+                    }
+                } else {
+                    continue;
+                }
+            }
 
             let name_idx = name
                 .or_else(|| config.name_for_bytepos(pos))
@@ -1318,17 +1353,7 @@ impl SourceMap {
             prev_dst_line = lc.line;
         }
 
-        let mut map = builder.into_sourcemap();
-
-        if let Some(o) = &mut orig {
-            o.adjust_mappings(&map);
-        }
-
-        if let Some(orig) = orig {
-            map = orig;
-        }
-
-        map
+        builder.into_sourcemap()
     }
 }
 
