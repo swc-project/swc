b4d21e910ba852ef769c8db3ee3803a321aea8c3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-17 00:57:33 +0900

    Impl list-tests

--- .github/swc-ecosystem-ci/run-all.ts
+++ .github/swc-ecosystem-ci/run-all.ts
@@ -1,25 +1,10 @@
-import { Octokit } from "octokit";
 import { getSuitesToRun, setupEnvironment } from "./utils";
 
 async function runAll() {
   const { root } = await setupEnvironment();
   const suitesToRun = getSuitesToRun([], root);
 
-  const octokit = new Octokit({ auth: process.env.BOT_GH_TOKEN! });
-
-  console.log(await octokit.rest.users.getAuthenticated());
-
-  await octokit.rest.actions.createWorkflowDispatch({
-    owner: "swc-project",
-    repo: "swc-ecosystem-ci",
-    workflow_id: "72388640",
-    ref: "main",
-    inputs: {
-      suites: JSON.stringify(suitesToRun),
-      mode: process.env.CI_MODE!,
-      version: process.env.SWC_VERSION!,
-    },
-  });
+  console.log(`::set-output name=suites::${JSON.stringify(suitesToRun)}`);
 }
 
 runAll();
