ce774683477c6d08dead4295b7fe87df72bd7e07
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-17 09:55:23 +0900

    Exclude xtask

--- scripts/cargo/get-crates-as-json.sh
+++ scripts/cargo/get-crates-as-json.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo metadata --format-version 1 | jq '[.packages[] | select(.source == null) | .name]'
+cargo metadata --format-version 1 | jq '[.packages[] | select(.source == null and .name != "xtask") | .name]'
