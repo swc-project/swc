d20c12cdfae6fd8a29736b3416ec6df8753b2c81
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-21 22:56:07 +0900

    Remove

--- crates/swc/tests/fixture/ts-isolated-declaration/error/input/1.ts
+++
@@ -1,9 +0,0 @@
-function A() {
-    return () => {
-        return C;
-    }
-}
-
-const B = () => { return B };
-
-const C = function () { }
--- crates/swc/tests/fixture/ts-isolated-declaration/error/output/1.ts
+++
@@ -1,9 +0,0 @@
-function A() {
-    return function() {
-        return C;
-    };
-}
-var B = function() {
-    return B;
-};
-var C = function C() {};
