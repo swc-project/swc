2133d3c13c9cfaafe276f8315fd47e0a77f2cfe8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-06-30 09:56:56 +0900

    is_class_property

--- crates/swc_ecma_parser/src/parser/class_and_fn.rs
+++ crates/swc_ecma_parser/src/parser/class_and_fn.rs
@@ -1170,6 +1170,7 @@ impl<I: Tokens> Parser<I> {
 
     fn is_class_property(&mut self, asi: bool) -> bool {
         (self.input.syntax().typescript() && is_one_of!(self, '!', ':'))
+            || (self.input.syntax().flow() && is!(self, ':'))
             || is_one_of!(self, '=', '}')
             || if asi {
                 is!(self, ';')
