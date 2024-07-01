00e1d81f015eb5f8be864dc4596b734d1103c266
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-01 13:36:22 +0900

    Update test refs (usage data)

--- crates/swc/tests/fixture/issues-8xxx/8528/output/index.tsx
+++ crates/swc/tests/fixture/issues-8xxx/8528/output/index.tsx
@@ -5,11 +5,15 @@ import * as React from "react";
 import * as S from "./BetButton.styled";
 let _BetButtonWrapper;
 class BetButtonWrapper extends (_React_Component = React.Component) {
-    static #_ = { c: [_BetButtonWrapper, _initClass] } = _apply_decs_2203_r(this, [], [
-        observer
-    ], _React_Component);
+    static{
+        ({ c: [_BetButtonWrapper, _initClass] } = _apply_decs_2203_r(this, [], [
+            observer
+        ], _React_Component));
+    }
     render() {
         return /*#__PURE__*/ React.createElement(S.BetButtonWrapper, null);
     }
-    static #_2 = _initClass();
+    static{
+        _initClass();
+    }
 }
