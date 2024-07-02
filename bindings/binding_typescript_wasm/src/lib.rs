fc76627de7c8d09bfab1c46da3b27e37fefd0272
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-03 08:29:33 +0900

    Some work

--- bindings/binding_typescript_wasm/src/lib.rs
+++ bindings/binding_typescript_wasm/src/lib.rs
@@ -1,13 +1,29 @@
 use swc_core::binding_macros::{build_minify, build_minify_sync};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::{
+    future_to_promise,
+    js_sys::{JsString, Promise},
+};
 
 /// Custom interface definitions for the @swc/wasm's public interface instead of
 /// auto generated one, which is not reflecting most of types in detail.
 #[wasm_bindgen(typescript_custom_section)]
 const INTERFACE_DEFINITIONS: &'static str = r#"
-export function minify(src: string, opts?: JsMinifyOptions): Promise<Output>;
-export function minifySync(code: string, opts?: JsMinifyOptions): Output;
+export function transform(src: string, opts?: Options): Promise<TransformOutput>;
+export function transformSync(src: string, opts?: Options): TransformOutput;
 "#;
 
-build_minify_sync!(#[wasm_bindgen(js_name = "minifySync", typescript_type = "minifySync",skip_typescript)]);
-build_minify!(#[wasm_bindgen(js_name = "minify", typescript_type = "minify",skip_typescript)]);
+pub struct Options {}
+
+pub struct TransformOutput {
+    code: String,
+    map: Option<String>,
+}
+
+#[wasm_bindgen]
+pub fn transform(input: JsString, options: JsValue) -> Promise {
+    future_to_promise(async move { transform_sync(input, options) })
+}
+
+#[wasm_bindgen]
+pub fn transform_sync(input: JsString, options: JsValue) -> Result<JsValue, JsValue> {}
