3137f57bb9a032ae6c2ca81a81ae94cec5492cbc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-03 09:13:52 +0900

    more work

--- bindings/binding_typescript_wasm/src/lib.rs
+++ bindings/binding_typescript_wasm/src/lib.rs
@@ -1,7 +1,24 @@
+use anyhow::Error;
 use serde::{Deserialize, Serialize};
 use swc_core::{
-    base::HandlerOpts,
-    common::{errors::ColorConfig, sync::Lrc, SourceMap, GLOBALS},
+    base::{config::ErrorFormat, HandlerOpts},
+    common::{
+        comments::SingleThreadedComments, errors::ColorConfig, sync::Lrc, FileName, Mark,
+        SourceMap, GLOBALS,
+    },
+    ecma::{
+        ast::{EsVersion, Program},
+        parser::{
+            parse_file_as_module, parse_file_as_program, parse_file_as_script, Syntax, TsSyntax,
+        },
+        transforms::base::{
+            fixer::fixer,
+            helpers::{inject_helpers, Helpers, HELPERS},
+            hygiene::hygiene,
+            resolver,
+        },
+        visit::VisitMutWith,
+    },
 };
 use swc_error_reporters::handler::try_with_handler;
 use wasm_bindgen::prelude::*;
@@ -19,7 +36,25 @@ export function transformSync(src: string, opts?: Options): TransformOutput;
 "#;
 
 #[derive(Deserialize)]
-pub struct Options {}
+#[serde(rename_all = "camelCase")]
+pub struct Options {
+    #[serde(default)]
+    pub module: Option<bool>,
+    #[serde(default)]
+    pub filename: Option<String>,
+
+    #[serde(default)]
+    pub parser: TsSyntax,
+
+    #[serde(default)]
+    pub external_helpers: bool,
+
+    #[serde(default)]
+    pub transform: swc_core::ecma::transforms::typescript::Config,
+
+    #[serde(default)]
+    pub source_maps: bool,
+}
 
 #[derive(Serialize)]
 pub struct TransformOutput {
@@ -36,15 +71,109 @@ pub fn transform(input: JsString, options: JsValue) -> Promise {
 pub fn transform_sync(input: JsString, options: JsValue) -> Result<JsValue, JsValue> {
     let options: Options = serde_wasm_bindgen::from_value(options)?;
 
+    let input = input.as_string().unwrap().into();
+
+    let result = GLOBALS.set(&Default::default(), || operate(input, options));
+
+    Ok(())
+}
+
+fn operate(input: String, options: Options) -> Result<TransformOutput, Error> {
     let cm = Lrc::new(SourceMap::default());
-    GLOBALS.set(&Default::default(), || {
-        try_with_handler(
-            cm.clone(),
-            HandlerOpts {
-                color: ColorConfig::Never,
-                skip_filename: true,
-            },
-            |handler| {},
-        )
-    })
+
+    try_with_handler(
+        cm.clone(),
+        HandlerOpts {
+            color: ColorConfig::Never,
+            skip_filename: true,
+        },
+        |handler| {
+            let fileame = options
+                .filename
+                .map_or(FileName::Anon, |f| FileName::Real(f.into()));
+
+            let fm = cm.new_source_file(filename, input);
+
+            let syntax = Syntax::Typescript(options.parser);
+            let target = EsVersion::latest();
+
+            let comments = SingleThreadedComments::default();
+            let mut errors = vec![];
+
+            let mut program = match options.module {
+                Some(true) => {
+                    parse_file_as_module(&fm, syntax, target, Some(&comments), &mut errors)
+                        .map(Program::Module)
+                }
+                Some(false) => {
+                    parse_file_as_script(&fm, syntax, target, Some(&comments), &mut errors)
+                        .map(Program::Script)
+                }
+                None => parse_file_as_program(&fm, syntax, target, Some(&comments), &mut errors),
+            };
+
+            let mut program = match program {
+                Ok(program) => program,
+                Err(err) => {
+                    err.into_diagnostic(&handler).emit();
+
+                    for e in errors {
+                        e.into_diagnostic(&handler).emit();
+                    }
+
+                    return Err(anyhow::anyhow!("failed to parse"));
+                }
+            };
+
+            if !errors.is_empty() {
+                for e in errors {
+                    e.into_diagnostic(&handler).emit();
+                }
+
+                return Err(anyhow::anyhow!("failed to parse"));
+            }
+
+            let mut unresolved_mark = Mark::new();
+            let top_level_mark = Mark::new();
+
+            HELPERS.set(&Helpers::new(options.external_helpers), || {
+                // Apply resolver
+
+                program.visit_mut_with(&mut resolver(unresolved_mark, top_level_mark, true));
+
+                // Strip typescript types
+
+                program.visit_mut_with(&mut swc_core::ecma::transforms::typescript::typescript(
+                    options.transform,
+                    top_level_mark,
+                ));
+
+                // Apply external helpers
+
+                program.visit_mut_with(&mut inject_helpers(unresolved_mark));
+
+                // Apply hygiene
+
+                program.visit_mut_with(&mut hygiene());
+
+                // Apply fixer
+
+                program.visit_mut_with(&mut fixer(Some(&comments)));
+            });
+
+            // Generate code
+
+            Ok(TransformOutput {})
+        },
+    )
+}
+
+pub fn convert_err(
+    err: Error,
+    error_format: Option<ErrorFormat>,
+) -> wasm_bindgen::prelude::JsValue {
+    error_format
+        .unwrap_or(ErrorFormat::Normal)
+        .format(&err)
+        .into()
 }
