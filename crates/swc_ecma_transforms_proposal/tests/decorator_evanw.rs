aa2afb90b83604d8e1b5293ff31eb5f068e69a26
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-04 14:56:50 +0900

    test

--- crates/swc_ecma_transforms_proposal/tests/decorator_evanw.rs
+++ crates/swc_ecma_transforms_proposal/tests/decorator_evanw.rs
@@ -1,80 +1,22 @@
-use std::{fs, process::Command};
+use std::{fs, path::PathBuf};
 
-use swc_common::Mark;
-use swc_ecma_ast::EsVersion;
-use swc_ecma_codegen::to_code;
-use swc_ecma_parser::parse_file_as_program;
-use swc_ecma_transforms_base::{
-    fixer::fixer,
-    helpers::{inject_helpers, Helpers, HELPERS},
-    hygiene::hygiene,
-    resolver,
-};
+use swc_ecma_parser::{EsSyntax, Syntax};
 use swc_ecma_transforms_proposal::decorator_2023_11::decorator_2023_11;
-use swc_ecma_visit::VisitMutWith;
-use testing::find_executable;
+use swc_ecma_transforms_testing::exec_tr;
+use swc_ecma_visit::as_folder;
 
-#[test]
-fn execute() {
-    testing::run_test(false, |cm, handler| {
-        let node = find_executable("node").expect("node not found");
+#[testing::fixture("tests/decorator-evanw-split/*.js")]
+fn fixture(input: PathBuf) {
+    let input_code = fs::read_to_string(&input).unwrap();
 
-        let fm = cm
-            .load_file("tests/decorator-tests/decorator-tests.js".as_ref())
-            .expect("failed to load file");
-
-        let code = {
-            // Transpile with swc
-            let mut errors = vec![];
-
-            let program = parse_file_as_program(
-                &fm,
-                swc_ecma_parser::Syntax::Es(swc_ecma_parser::EsSyntax {
-                    decorators: true,
-                    auto_accessors: true,
-                    ..Default::default()
-                }),
-                EsVersion::EsNext,
-                None,
-                &mut errors,
-            );
-
-            let mut program = match program {
-                Ok(v) => v,
-                Err(e) => {
-                    e.into_diagnostic(handler).emit();
-                    return Err(());
-                }
-            };
-
-            for e in errors {
-                e.into_diagnostic(handler).emit();
-            }
-            HELPERS.set(&Helpers::new(false), || {
-                let unresolved_mark = Mark::new();
-                let top_level_mark = Mark::new();
-                program.visit_mut_with(&mut resolver(unresolved_mark, top_level_mark, false));
-
-                program.visit_mut_with(&mut decorator_2023_11());
-
-                program.visit_mut_with(&mut inject_helpers(unresolved_mark));
-                program.visit_mut_with(&mut hygiene());
-                program.visit_mut_with(&mut fixer(None));
-            });
-
-            to_code(&program)
-        };
-
-        fs::write("tests/built-decorator-tests.js", code).expect("failed to write file");
-
-        let status = Command::new(node)
-            .arg("tests/built-decorator-tests.js")
-            .status()
-            .expect("failed to execute process");
-
-        assert!(status.success());
-
-        Ok(())
-    })
-    .unwrap()
+    exec_tr(
+        &input.file_name().unwrap().to_string_lossy(),
+        Syntax::Es(EsSyntax {
+            decorators: true,
+            auto_accessors: true,
+            ..Default::default()
+        }),
+        |_| as_folder(decorator_2023_11()),
+        &input_code,
+    );
 }
