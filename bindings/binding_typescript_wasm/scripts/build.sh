dca05bb8dc0e6a35a65463891a4b32aa043a56ed
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-04 18:22:57 +0900

    build.sh

--- bindings/binding_typescript_wasm/scripts/build.sh
+++ bindings/binding_typescript_wasm/scripts/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 
+export CARGO_PROFILE_RELEASE_LTO="fat"
+export CARGO_PROFILE_RELEASE_OPT_LEVEL="z"
 wasm-pack build --out-name wasm --release --scope=swc --target nodejs
 ls -al ./pkg
+
 node ./scripts/patch.mjs
