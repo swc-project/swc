98df35b805a8b0005d84909288682dad820c30f8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-05 13:30:57 +0900

    Add helpers

---
+++ crates/swc_ecma_transforms_base/src/helpers/_set_function_name.js
@@ -0,0 +1,15 @@
+export function setFunctionName(fn, name, prefix) {
+    if (typeof name === "symbol") {
+        name = name.description;
+        name = name ? "[" + name + "]" : "";
+    }
+    try {
+        Object.defineProperty(fn, "name", {
+            configurable: true,
+            value: prefix ? prefix + " " + name : name
+        });
+    } catch (_) { }
+    return fn;
+}
+
+export { setFunctionName as _ }
---
+++ packages/helpers/esm/_set_function_name.js
@@ -0,0 +1,13 @@
+export default function setFunctionName(fn, name, prefix) {
+    if (typeof name === "symbol") {
+        name = name.description;
+        name = name ? "[" + name + "]" : "";
+    }
+    try {
+        Object.defineProperty(fn, "name", {
+            configurable: true,
+            value: prefix ? prefix + " " + name : name
+        });
+    } catch (_) { }
+    return fn;
+}
