3b714b46c4311667ce2cc512ad9e86ac17032ed8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-05 12:07:07 +0900

    Harden

--- crates/swc/tests/fixture/retain-lines/strip-only/input/4.ts
+++ crates/swc/tests/fixture/retain-lines/strip-only/input/4.ts
@@ -2,8 +2,15 @@
 
 export const mySet = new Set<string>();
 
+const myMap = new Map<string, number>();
 
 export class Foo { }
 
+class Bar {
+
+}
 
 export function create() { }
+
+
+function create2() { }
