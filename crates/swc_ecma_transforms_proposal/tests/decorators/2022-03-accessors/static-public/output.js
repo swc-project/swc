2bd8ad5b70987f53b48a5bae3e4cf45d6f946614
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-05 14:16:41 +0900

    Update test refs

--- crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-accessors/static-public/output.js
+++ crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-accessors/static-public/output.js
@@ -3,7 +3,7 @@ const dec = ()=>{};
 _computedKey = 'c';
 class Foo {
     static{
-        ({ e: [_init_a, _init_b, _init_computedKey, _initStatic]  } = _apply_decs_2203_r(this, [
+        ({ e: [_init_a, _init_b, _init_computedKey, _initStatic] } = _apply_decs_2203_r(this, [
             [
                 dec,
                 6,
@@ -22,25 +22,25 @@ class Foo {
         ], []));
         _initStatic(this);
     }
-    static #___private_a = _init_a(this);
+    static #___private_a_1 = _init_a(this);
     static get a() {
-        return this.#___private_a;
+        return this.#___private_a_1;
     }
     static set a(_v) {
-        this.#___private_a = _v;
+        this.#___private_a_1 = _v;
     }
-    static #___private_b = _init_b(this, 123);
+    static #___private_b_2 = _init_b(this, 123);
     static get b() {
-        return this.#___private_b;
+        return this.#___private_b_2;
     }
     static set b(_v) {
-        this.#___private_b = _v;
+        this.#___private_b_2 = _v;
     }
-    static #___private_computedKey = _init_computedKey(this, 456);
+    static #___private_computedKey_3 = _init_computedKey(this, 456);
     static get [_computedKey]() {
-        return this.#___private_computedKey;
+        return this.#___private_computedKey_3;
     }
     static set [_computedKey](_v) {
-        this.#___private_computedKey = _v;
+        this.#___private_computedKey_3 = _v;
     }
 }
