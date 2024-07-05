f22b24558f3d5aade07d7f4958aee249c6a41b95
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-05 12:39:59 +0900

    Update test refs

---
+++ crates/swc/tests/fixture/retain-lines/strip-only/output/4.ts
@@ -0,0 +1,19 @@
+
+
+export const mySet = new Set        ();
+
+
+export const call2 = call        ();
+
+const myMap = new Map                ();
+
+export class Foo {
+}
+class Bar {
+}
+
+
+export function create() {}
+
+
+function create2() {}
