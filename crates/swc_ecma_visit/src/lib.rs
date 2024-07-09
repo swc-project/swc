31ea7982403ab53253a070d4e3519ab698667ee4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-09 14:02:41 +0900

    Box more

--- crates/swc_ecma_visit/src/lib.rs
+++ crates/swc_ecma_visit/src/lib.rs
@@ -1746,7 +1746,7 @@ define!({
     }
     pub enum TsParamPropParam {
         Ident(BindingIdent),
-        Assign(AssignPat),
+        Assign(Box<AssignPat>),
     }
     pub struct TsQualifiedName {
         pub span: Span,
