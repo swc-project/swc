fbf6bc7876c41ca78b9715965f038ca20800ced1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-09 13:32:22 +0900

    Fix visitor

--- crates/swc_ecma_visit/src/lib.rs
+++ crates/swc_ecma_visit/src/lib.rs
@@ -726,7 +726,7 @@ define!({
     }
     pub enum PropOrSpread {
         Spread(SpreadElement),
-        Prop(Box<Prop>),
+        Prop(Prop),
     }
     pub struct SpreadElement {
         pub dot3_token: Span,
@@ -1259,7 +1259,7 @@ define!({
         Ident(BindingIdent),
         Array(Box<ArrayPat>),
         Rest(Box<RestPat>),
-        Object( Box<ObjectPat>),
+        Object(Box<ObjectPat>),
         Assign(Box<AssignPat>),
         Invalid(Invalid),
         Expr(Expr),
@@ -1305,7 +1305,7 @@ define!({
         Shorthand(Ident),
         KeyValue(Box<KeyValueProp>),
         Assign(Box<AssignProp>),
-        Getter( Box<GetterProp>),
+        Getter(Box<GetterProp>),
         Setter(Box<SetterProp>),
         Method(Box<MethodProp>),
     }
@@ -1893,11 +1893,6 @@ define!({
         pub span: Span,
         pub expr: Expr,
         pub type_ann: Box<TsType>,
-    }
-
-    pub struct ReservedUnused {
-        pub span: Span,
-        pub body: Option<Vec<ModuleItem>>,
     }
 
     pub struct AutoAccessor {
