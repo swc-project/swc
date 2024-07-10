4f29d2828cd6dc3226fb757b5592282c7a4c303f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-10 13:29:14 +0900

    Fix boxing

--- crates/swc_ecma_visit/src/lib.rs
+++ crates/swc_ecma_visit/src/lib.rs
@@ -1010,8 +1010,6 @@ define!({
     }
     pub enum SuperProp {
         Ident(IdentName),
-        Computed(ComputedPropName),
-        Ident(Ident),
         Computed(Box<ComputedPropName>),
     }
     pub struct CondExpr {
@@ -1248,8 +1246,6 @@ define!({
     }
     pub enum JSXAttrName {
         Ident(IdentName),
-        JSXNamespacedName(JSXNamespacedName),
-        Ident(Ident),
         JSXNamespacedName(Box<JSXNamespacedName>),
     }
     pub enum JSXAttrValue {
