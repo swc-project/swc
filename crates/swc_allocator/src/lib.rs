be99ce0a570c41b3fd471c5d609bd63c25740b36
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-10 20:27:34 +0900

    doc(allocator): Mention `oxc_allocator`

--- crates/swc_allocator/src/lib.rs
+++ crates/swc_allocator/src/lib.rs
@@ -1,3 +1,7 @@
+//! Allocator for swc.
+//!
+//! API designed after [`oxc_allocator`](https://github.com/oxc-project/oxc/tree/725571aad193ec6ba779c820baeb4a7774533ed7/crates/oxc_allocator/src).
+
 use std::ops::{Deref, DerefMut};
 
 use bumpalo::Bump;
