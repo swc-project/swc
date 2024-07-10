00a74dcfc535e7172c3b1e72399ffd7f449a859c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-10 15:03:44 +0900

    Fix visitor

--- crates/swc_ecma_visit/src/lib.rs
+++ crates/swc_ecma_visit/src/lib.rs
@@ -1359,6 +1359,7 @@ define!({
         pub stmts: Vec<Stmt>,
     }
     pub enum Stmt {
+        Directive(Directive),
         Block(BlockStmt),
         Empty(EmptyStmt),
         Debugger(DebuggerStmt),
@@ -1378,6 +1379,10 @@ define!({
         ForOf(Box<ForOfStmt>),
         Decl(Decl),
         Expr(ExprStmt),
+    }
+    pub struct Directive {
+        pub span: Span,
+        pub value: Atom,
     }
     pub struct ExprStmt {
         pub span: Span,
