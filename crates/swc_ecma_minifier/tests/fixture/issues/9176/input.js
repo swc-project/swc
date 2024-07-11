f81fa6e06335745ff6ab5f7956cecc38116b1343
CPunisher <1343316114@qq.com>
2024-07-11 22:29:51 +0800

    fix(es/minifier): Fix case matching (#9208)
    **Description:**
    
    Caused by https://github.com/swc-project/swc/pull/7112
    Since swc minifier evaluates expressions in a separate pass, I make a temporary patch here.
    
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/9176

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/9176/input.js
@@ -0,0 +1,10 @@
+"use strict";
+const k = (function () {
+    switch (-0) {
+        case 0:
+            console.log("hi");
+            break;
+        default:
+            throw 0;
+    }
+})();

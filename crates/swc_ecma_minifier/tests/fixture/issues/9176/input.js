swc-project/swc

commit 6a570a334c787358d67ae6c590a7fd0e916f35e8

fix(es/ast): Fix `EqIgnoreSpan` impl of `Number` (#7112)
**Related issue:**

 - Closes https://github.com/swc-project/swc/issues/7111.

crates/swc_ecma_minifier/tests/fixture/issues/7111/input.js
@@ -1,0 +1,1 @@
+console.log(Math.random() > 0.5 ? -0 : 0)

crates/swc_ecma_minifier/tests/fixture/issues/7111/output.js
@@ -1,0 +1,1 @@
+console.log(Math.random() > 0.5 ? -0 : 0);

crates/swc_ecma_minifier/tests/fixture/next/wrap-contracts/output.js
@@ -7207,7 +7207,7 @@
                             if (m.c ? !m.c[0] : !m.s) return new BigNumber(NaN);
                             (isModExp = !nIsNeg && x.isInteger() && m.isInteger()) && (x = x.mod(m));
                         } else {
-                            if (n.e > 9 && (x.e > 0 || x.e < -1 || (0 == x.e ? x.c[0] > 1 || nIsBig && x.c[1] >= 24e7 : x.c[0] < 8e13 || nIsBig && x.c[0] <= 9999975e7))) return x.s < 0 && isOdd(n), k = -0, x.e > -1 && (k = 1 / k), new BigNumber(nIsNeg ? 1 / k : k);
+                            if (n.e > 9 && (x.e > 0 || x.e < -1 || (0 == x.e ? x.c[0] > 1 || nIsBig && x.c[1] >= 24e7 : x.c[0] < 8e13 || nIsBig && x.c[0] <= 9999975e7))) return k = x.s < 0 && isOdd(n) ? -0 : 0, x.e > -1 && (k = 1 / k), new BigNumber(nIsNeg ? 1 / k : k);
                             POW_PRECISION && (k = mathceil(POW_PRECISION / LOG_BASE + 2));
                         }
                         for(nIsBig ? (half = new BigNumber(0.5), nIsNeg && (n.s = 1), nIsOdd = isOdd(n)) : nIsOdd = (i = Math.abs(+valueOf(n))) % 2, y = new BigNumber(ONE);;){
@@ -7258,7 +7258,7 @@
                         var xe = x.e / LOG_BASE, ye = y.e / LOG_BASE, xc = x.c, yc = y.c;
                         if (!xe || !ye) {
                             if (!xc || !yc) return xc ? (y.s = -b, y) : new BigNumber(yc ? x : NaN);
-                            if (!xc[0] || !yc[0]) return yc[0] ? (y.s = -b, y) : new BigNumber(xc[0] ? x : -0);
+                            if (!xc[0] || !yc[0]) return yc[0] ? (y.s = -b, y) : new BigNumber(xc[0] ? x : 3 == ROUNDING_MODE ? -0 : 0);
                         }
                         if (xe = bitFloor(xe), ye = bitFloor(ye), xc = xc.slice(), a = xe - ye) {
                             for((xLTy = a < 0) ? (a = -a, t = xc) : (ye = xe, t = yc), t.reverse(), b = a; b--; t.push(0));

crates/swc_ecma_ast/src/lit.rs
@@ -350,7 +350,7 @@ impl Eq for Number {}
 
 impl EqIgnoreSpan for Number {
     fn eq_ignore_span(&self, other: &Self) -> bool {
-        self.value == other.value
+        self.value == other.value && self.value.is_sign_positive() == other.value.is_sign_positive()
     }
 }
 

commit f81fa6e06335745ff6ab5f7956cecc38116b1343

fix(es/minifier): Fix case matching (#9208)
**Description:**

Caused by https://github.com/swc-project/swc/pull/7112
Since swc minifier evaluates expressions in a separate pass, I make a temporary patch here.

**Related issue:**

 - Closes https://github.com/swc-project/swc/issues/9176

crates/swc_ecma_minifier/src/compress/optimize/switches.rs
@@ -44,7 +44,12 @@ impl Optimizer<'_> {
         for (idx, case) in stmt.cases.iter_mut().enumerate() {
             if let Some(test) = case.test.as_ref() {
                 if let Some(e) = is_primitive(&self.expr_ctx, tail_expr(test)) {
-                    if e.eq_ignore_span(tail) {
+                    if match (e, tail) {
+                        (Expr::Lit(Lit::Num(e)), Expr::Lit(Lit::Num(tail))) => {
+                            e.value == tail.value
+                        }
+                        _ => e.eq_ignore_span(tail),
+                    } {
                         cases.push(case.take());
 
                         exact = Some(idx);

crates/swc_ecma_minifier/tests/fixture/issues/9176/input.js
@@ -1,0 +1,10 @@
+"use strict";
+const k = (function () {
+    switch (-0) {
+        case 0:
+            console.log("hi");
+            break;
+        default:
+            throw 0;
+    }
+})();

crates/swc_ecma_minifier/tests/fixture/issues/9176/output.js
@@ -1,0 +1,2 @@
+"use strict";
+console.log("hi");