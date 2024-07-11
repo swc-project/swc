fe25324a51f9b13bcc078d1db4aa691fe13e0632
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-11 16:00:17 +0900

    More from impl

--- crates/swc_ecma_ast/src/macros.rs
+++ crates/swc_ecma_ast/src/macros.rs
@@ -242,6 +242,8 @@ macro_rules! bridge_decl_from {
     ($bridge:ty, $src:ty) => {
         bridge_from!(crate::Decl, $bridge, $src);
         bridge_from!(crate::Stmt, crate::Decl, $src);
+        bridge_from!(crate::Stmt, crate::Decl, $bridge);
         bridge_from!(crate::ModuleItem, crate::Stmt, $src);
+        bridge_from!(crate::ModuleItem, crate::Stmt, $bridge);
     };
 }
