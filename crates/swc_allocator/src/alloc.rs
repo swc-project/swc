565012e1aa44e765d9124dc58d3fa9fde668f2fe
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 04:54:17 +0900

    `with_allocator`

--- crates/swc_allocator/src/alloc.rs
+++ crates/swc_allocator/src/alloc.rs
@@ -4,6 +4,17 @@ use crate::Allocator;
 
 scoped_thread_local!(pub(crate) static ALLOC: Allocator);
 
+fn with_allocator(f: impl FnOnce(&dyn allocator_api2::alloc::Allocator)) {
+    if ALLOC.is_set() {
+        ALLOC.with(|a| {
+            //
+            f(&&**a as &dyn allocator_api2::alloc::Allocator)
+        });
+    } else {
+        f(&allocator_api2::alloc::Global);
+    }
+}
+
 pub(crate) struct SwcAlloc;
 
 impl allocator_api2::alloc::Allocator for SwcAlloc {}
