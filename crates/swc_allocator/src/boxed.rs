580d0b15b5f5ace81438052018ed484397ee45d1
강동윤/Donny <kdy1997.dev@gmail.com>
2024-07-12 21:20:08 +0900

    doc

--- crates/swc_allocator/src/boxed.rs
+++ crates/swc_allocator/src/boxed.rs
@@ -1,8 +1,12 @@
-use std::{marker::PhantomData, ptr::NonNull};
+use std::ptr::NonNull;
 
 /// A special `Box` which has size of [`std::boxed::Box`] but **may** be
 /// allocated with a custom allocator.
+///
+///
+/// # Representation
+///
+/// The last bit is 1 if the box is allocated with a custom allocator.
 pub struct Box<T: ?Sized> {
     data: NonNull<T>,
-    marker: PhantomData<&mut T>,
 }
