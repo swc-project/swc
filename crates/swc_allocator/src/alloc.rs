074b4708a156483421cb5e1966db178af8296905
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 05:05:35 +0900

    `SwcAlloc`

--- crates/swc_allocator/src/alloc.rs
+++ crates/swc_allocator/src/alloc.rs
@@ -6,34 +6,38 @@ use crate::Allocator;
 
 scoped_thread_local!(pub(crate) static ALLOC: Allocator);
 
-/// `true` is passed to `f` if the box is allocated with a custom allocator.
-fn with_allocator<T>(f: impl FnOnce(&dyn allocator_api2::alloc::Allocator, bool) -> T) -> T {
-    if ALLOC.is_set() {
-        ALLOC.with(|a| {
-            //
-            f(&&**a as &dyn allocator_api2::alloc::Allocator, true)
-        })
-    } else {
-        f(&allocator_api2::alloc::Global, false)
+pub(crate) struct SwcAlloc {
+    pub is_custom: bool,
+}
+
+impl SwcAlloc {
+    /// `true` is passed to `f` if the box is allocated with a custom allocator.
+    fn with_allocator<T>(&self, f: impl FnOnce(&dyn allocator_api2::alloc::Allocator) -> T) -> T {
+        if self.is_custom {
+            ALLOC.with(|a| {
+                //
+                f(&&**a as &dyn allocator_api2::alloc::Allocator)
+            })
+        } else {
+            f(&allocator_api2::alloc::Global)
+        }
     }
 }
 
-pub(crate) struct SwcAlloc;
-
 unsafe impl allocator_api2::alloc::Allocator for SwcAlloc {
     fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, allocator_api2::alloc::AllocError> {
-        with_allocator(|a, _custom| a.allocate(layout))
+        self.with_allocator(|a| a.allocate(layout))
     }
 
     fn allocate_zeroed(
         &self,
         layout: Layout,
     ) -> Result<NonNull<[u8]>, allocator_api2::alloc::AllocError> {
-        with_allocator(|a, _custom| a.allocate_zeroed(layout))
+        self.with_allocator(|a| a.allocate_zeroed(layout))
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        with_allocator(|a, _custom| a.deallocate(ptr, layout))
+        self.with_allocator(|a| a.deallocate(ptr, layout))
     }
 
     unsafe fn grow(
@@ -42,7 +46,7 @@ unsafe impl allocator_api2::alloc::Allocator for SwcAlloc {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, allocator_api2::alloc::AllocError> {
-        with_allocator(|a, _custom| a.grow(ptr, old_layout, new_layout))
+        self.with_allocator(|a| a.grow(ptr, old_layout, new_layout))
     }
 
     unsafe fn grow_zeroed(
@@ -51,7 +55,7 @@ unsafe impl allocator_api2::alloc::Allocator for SwcAlloc {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, allocator_api2::alloc::AllocError> {
-        with_allocator(|a, _custom| a.grow_zeroed(ptr, old_layout, new_layout))
+        self.with_allocator(|a| a.grow_zeroed(ptr, old_layout, new_layout))
     }
 
     unsafe fn shrink(
@@ -60,7 +64,7 @@ unsafe impl allocator_api2::alloc::Allocator for SwcAlloc {
         old_layout: Layout,
         new_layout: Layout,
     ) -> Result<NonNull<[u8]>, allocator_api2::alloc::AllocError> {
-        with_allocator(|a, _custom| a.shrink(ptr, old_layout, new_layout))
+        self.with_allocator(|a| a.shrink(ptr, old_layout, new_layout))
     }
 
     fn by_ref(&self) -> &Self
