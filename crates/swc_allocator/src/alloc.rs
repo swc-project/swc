e4f31409f730bd13c6384d2d0a0a04254d0f9ba0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 04:50:57 +0900

    traits

--- crates/swc_allocator/src/alloc.rs
+++ crates/swc_allocator/src/alloc.rs
@@ -1,1 +1,9 @@
+use scoped_tls::scoped_thread_local;
+
+use crate::Allocator;
+
+scoped_thread_local!(pub(crate) static ALLOC: Allocator);
+
 pub(crate) struct SwcAlloc;
+
+impl allocator_api2::alloc::Allocator for SwcAlloc {}
