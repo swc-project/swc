6ac00236b7011c6c8a941700eb86ff890544efd0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 06:07:39 +0900

    scope

--- crates/swc_allocator/src/lib.rs
+++ crates/swc_allocator/src/lib.rs
@@ -2,6 +2,7 @@
 //!
 //! API designed after [`oxc_allocator`](https://github.com/oxc-project/oxc/tree/725571aad193ec6ba779c820baeb4a7774533ed7/crates/oxc_allocator/src).
 
+use alloc::ALLOC;
 use std::ops::{Deref, DerefMut};
 
 use bumpalo::Bump;
@@ -12,6 +13,17 @@ pub mod boxed;
 #[derive(Default)]
 pub struct Allocator {
     alloc: Bump,
+}
+
+impl Allocator {
+    /// Invokes `f` in a scope where the allocations are done in this allocator.
+    #[inline(always)]
+    pub fn scope<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        ALLOC.set(self, f)
+    }
 }
 
 impl From<Bump> for Allocator {
