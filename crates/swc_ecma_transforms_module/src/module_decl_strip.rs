f64f8d674d906473ee48a33f66cc3b812db4cee8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-12 12:43:48 +0900

    chore: Fix for rebase

--- crates/swc_ecma_transforms_module/src/module_decl_strip.rs
+++ crates/swc_ecma_transforms_module/src/module_decl_strip.rs
@@ -6,10 +6,8 @@ use swc_common::{
     Mark, Span, SyntaxContext,
 };
 use swc_ecma_ast::*;
-use swc_ecma_utils::{find_pat_ids, ident::IdentLike, private_ident, quote_ident, ExprFactory};
-use swc_ecma_visit::{standard_only_visit_mut, VisitMut, VisitMutWith};
 use swc_ecma_utils::{find_pat_ids, private_ident, quote_ident, ExprFactory};
-use swc_ecma_visit::{noop_visit_mut_type, VisitMut, VisitMutWith};
+use swc_ecma_visit::{standard_only_visit_mut, VisitMut, VisitMutWith};
 
 use crate::{module_ref_rewriter::ImportMap, SpanCtx};
 
