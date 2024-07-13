a29beda6100798d04f5a71f40273b6b2a3b520a3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 15:08:43 +0900

    WIP

--- crates/swc_allocator/src/boxed/rkyv.rs
+++ crates/swc_allocator/src/boxed/rkyv.rs
@@ -1,6 +1,9 @@
+use std::{alloc, cmp};
+
 use rkyv::{
     boxed::{ArchivedBox, BoxResolver},
-    Archive, ArchiveUnsized, Fallible, Serialize, SerializeUnsized,
+    Archive, ArchivePointee, ArchiveUnsized, Deserialize, DeserializeUnsized, Fallible, Serialize,
+    SerializeUnsized,
 };
 
 use super::Box;
@@ -19,5 +22,38 @@ impl<T: SerializeUnsized<S> + ?Sized, S: Fallible + ?Sized> Serialize<S> for Box
     #[inline]
     fn serialize(&self, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
         ArchivedBox::serialize_from_ref(self.as_ref(), serializer)
+    }
+}
+
+impl<T, D> Deserialize<Box<T>, D> for ArchivedBox<T::Archived>
+where
+    T: ArchiveUnsized + ?Sized,
+    T::Archived: DeserializeUnsized<T, D>,
+    D: Fallible + ?Sized,
+{
+    #[inline]
+    fn deserialize(&self, deserializer: &mut D) -> Result<Box<T>, D::Error> {
+        unsafe {
+            let data_address = self
+                .get()
+                .deserialize_unsized(deserializer, |layout| alloc::alloc(layout))?;
+            let metadata = self.get().deserialize_metadata(deserializer)?;
+            let ptr = ptr_meta::from_raw_parts_mut(data_address, metadata);
+            Ok(Box::from_raw(ptr))
+        }
+    }
+}
+
+impl<T: ArchivePointee + PartialEq<U> + ?Sized, U: ?Sized> PartialEq<Box<U>> for ArchivedBox<T> {
+    #[inline]
+    fn eq(&self, other: &Box<U>) -> bool {
+        self.get().eq(other.as_ref())
+    }
+}
+
+impl<T: ArchivePointee + PartialOrd<U> + ?Sized, U: ?Sized> PartialOrd<Box<U>> for ArchivedBox<T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Box<U>) -> Option<cmp::Ordering> {
+        self.get().partial_cmp(other.as_ref())
     }
 }
