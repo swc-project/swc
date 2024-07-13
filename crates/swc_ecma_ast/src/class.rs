d8f4880da43a08181eaf578275717e2c02447f7a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-13 17:32:07 +0900

    Update import

--- crates/swc_ecma_ast/src/class.rs
+++ crates/swc_ecma_ast/src/class.rs
@@ -1,5 +1,5 @@
 use is_macro::Is;
-use swc_allocator::{boxed::Box, Vec};
+use swc_allocator::{boxed::Box, vec::Vec};
 use swc_common::{ast_node, util::take::Take, EqIgnoreSpan, Span, SyntaxContext, DUMMY_SP};
 
 use crate::{
