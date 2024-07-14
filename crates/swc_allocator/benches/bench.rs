ac33e713a5ea71c67ec43fc840faaf5bfd65c6d9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-14 13:39:16 +0900

    direct alloc

--- crates/swc_allocator/benches/bench.rs
+++ crates/swc_allocator/benches/bench.rs
@@ -1,1 +1,21 @@
+extern crate swc_malloc;
 
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Bencher, Criterion};
+
+fn bench_alloc(c: &mut Criterion) {
+    fn direct_alloc(b: &mut Bencher, times: usize) {
+        b.iter(|| {
+            for _ in 0..times {
+                let _: swc_allocator::boxed::Box<usize> =
+                    black_box(swc_allocator::boxed::Box::new(black_box(1234)));
+            }
+        })
+    }
+
+    c.bench_function("common/allocator/alloc/1000", |b| {
+        direct_alloc(b, 1000 * 1000 * 1000)
+    });
+}
+
+criterion_group!(benches, bench_alloc);
+criterion_main!(benches);
