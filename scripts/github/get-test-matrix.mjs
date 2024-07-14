7c08be733c44be29751edf186cf0b7949d815485
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-14 12:59:19 +0900

    matrix.mjs

--- scripts/github/get-test-matrix.mjs
+++ scripts/github/get-test-matrix.mjs
@@ -1,10 +1,12 @@
 #!/usr/bin/env zx
 import * as path from 'node:path';
 import * as fs from 'node:fs/promises';
+import { parse, } from 'yaml'
 
 const scriptDir = __dirname;
 const repoRootDir = path.resolve(scriptDir, '../../');
 const testsYmlPath = path.resolve(repoRootDir, 'tests.yml');
+const testsYml = parse(await fs.readFile(testsYmlPath, 'utf8'));
 
 console.log("Script dir:", scriptDir);
 console.log(`Using tests.yml at ${testsYmlPath}`);
@@ -14,3 +16,34 @@ const metadata = JSON.parse(rawMetadata.stdout);
 const packages = metadata.packages.map(p => p.name).filter(name => name !== 'xtask');
 
 console.log('Crates: ', packages)
+console.log('Test config: ', testsYml)
+
+const settings = [];
+
+for (const pkg of packages) {
+    settings.push({
+        crate: pkg,
+        runner: 'ubunut-latest',
+        os: 'ubuntu-latest',
+    });
+
+    if (testsYml.os?.windows?.includes(pkg)) {
+        settings.push({
+            crate: pkg,
+            runner: 'windows-latest',
+            os: 'windows-latest',
+        });
+    }
+
+    if (testsYml.os?.macos?.includes(pkg)) {
+        settings.push({
+            crate: pkg,
+            runner: 'macos-latest',
+            os: 'macos-latest',
+        });
+    }
+}
+
+const output = JSON.stringify(settings)
+console.log(output)
+await `echo ${output} >> $GITHUB_OUTPUT`;
