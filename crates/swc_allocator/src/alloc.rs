0aa845d23de19e10932d7b1c1dd9b734b8c6dcd2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-15 14:22:53 +0900

    fix lint

--- crates/swc_allocator/src/alloc.rs
+++ crates/swc_allocator/src/alloc.rs
@@ -1,11 +1,12 @@
-use std::{alloc::Layout, mem::transmute, ptr::NonNull};
+use std::{alloc::Layout, cell::Cell, mem::transmute, ptr::NonNull};
 
 use allocator_api2::alloc::Global;
-use scoped_tls::scoped_thread_local;
 
 use crate::{FastAlloc, MemorySpace};
 
-scoped_thread_local!(pub(crate) static ALLOC: &'static SwcAllocator);
+thread_local! {
+  static ALLOC: Cell<Option<&'static SwcAllocator>> = const { Cell::new(None) };
+}
 
 #[derive(Default)]
 pub struct SwcAllocator(MemorySpace);
@@ -22,15 +23,18 @@ impl SwcAllocator {
             transmute::<&'a SwcAllocator, &'static SwcAllocator>(self)
         };
 
-        ALLOC.set(&s, f)
+        ALLOC.set(Some(s));
+        let ret = f();
+        ALLOC.set(None);
+        ret
     }
 }
 
 impl Default for FastAlloc {
     fn default() -> Self {
         Self {
-            alloc: if ALLOC.is_set() {
-                Some(ALLOC.with(|v| *v))
+            alloc: if let Some(v) = ALLOC.get() {
+                Some(v)
             } else {
                 None
             },
@@ -87,7 +91,7 @@ unsafe impl allocator_api2::alloc::Allocator for FastAlloc {
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
         if self.alloc.is_some() {
             debug_assert!(
-                ALLOC.is_set(),
+                ALLOC.get().is_some(),
                 "Deallocating a pointer allocated with arena mode with a non-arena mode allocator"
             );
 
