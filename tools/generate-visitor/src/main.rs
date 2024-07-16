2dc66fc7f1c993a5f2f4aff0db1d780640bf3e65
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-16 14:08:53 +0900

    collect inputs

--- tools/generate-visitor/src/main.rs
+++ tools/generate-visitor/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Result};
 use clap::Parser;
@@ -14,9 +14,22 @@ fn main() -> Result<()> {
 
     input_dir = input_dir
         .canonicalize()
-        .context("faield to canonicalize input directory")?;
+        .context("faield to canonicalize input directory")?
+        .join("src");
 
     eprintln!("Generating visitor for crate in directory: {:?}", input_dir);
 
+    let input_files = collect_input_files(&input_dir)?;
+    eprintln!("Found {} input files", input_files.len());
+
     Ok(())
+}
+
+fn collect_input_files(input_dir: &Path) -> Result<Vec<PathBuf>> {
+    Ok(walkdir::WalkDir::new(input_dir)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_type().is_file())
+        .map(|entry| entry.path().to_path_buf())
+        .collect())
 }
