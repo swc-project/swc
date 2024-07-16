6e67b0949c24d8801f7469f983f9a55f2fbbf227
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-16 14:01:26 +0900

    cli args

--- tools/generate-visitor/src/main.rs
+++ tools/generate-visitor/src/main.rs
@@ -1,3 +1,12 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct CliArgs {
+    input_dir: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
 }
