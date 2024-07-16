03215575c859241a8fc0d73a71cf8d7a315655b4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-16 14:06:52 +0900

    main

--- tools/generate-visitor/src/main.rs
+++ tools/generate-visitor/src/main.rs
@@ -1,12 +1,22 @@
 use std::path::PathBuf;
 
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct CliArgs {
+    /// The directory containing the crate to generate the visitor for.
     input_dir: PathBuf,
 }
 
-fn main() {
-    let args = CliArgs::parse();
+fn main() -> Result<()> {
+    let CliArgs { mut input_dir } = CliArgs::parse();
+
+    input_dir = input_dir
+        .canonicalize()
+        .context("faield to canonicalize input directory")?;
+
+    eprintln!("Generating visitor for crate in directory: {:?}", input_dir);
+
+    Ok(())
 }
