f9f64d5021b1ec4655e00b1695b0ed0b82031b39
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-16 14:12:09 +0900

    more main

--- tools/generate-visitor/src/main.rs
+++ tools/generate-visitor/src/main.rs
@@ -6,11 +6,19 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 struct CliArgs {
     /// The directory containing the crate to generate the visitor for.
+    #[clap(short = 'i', long)]
     input_dir: PathBuf,
+
+    /// The file for the generated visitor code.
+    #[clap(short = 'o', long)]
+    output: PathBuf,
 }
 
 fn main() -> Result<()> {
-    let CliArgs { mut input_dir } = CliArgs::parse();
+    let CliArgs {
+        mut input_dir,
+        output,
+    } = CliArgs::parse();
 
     input_dir = input_dir
         .canonicalize()
@@ -18,9 +26,10 @@ fn main() -> Result<()> {
         .join("src");
 
     eprintln!("Generating visitor for crate in directory: {:?}", input_dir);
-
     let input_files = collect_input_files(&input_dir)?;
     eprintln!("Found {} input files", input_files.len());
+
+    eprintln!("Generating visitor in directory: {:?}", output);
 
     Ok(())
 }
