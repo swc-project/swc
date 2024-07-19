096399ca5aae03ea3c6fff1db57497663e659d1a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-20 08:53:01 +0900

    Doc

--- crates/swc_ecma_usage_analyzer/src/lib.rs
+++ crates/swc_ecma_usage_analyzer/src/lib.rs
@@ -1,3 +1,7 @@
+//! This crate is an internal crate that is extracted just to reduce compile
+//! time. Do not use this crate directly, and this package does not follow
+//! semver.
+
 #![allow(clippy::mutable_key_type)]
 
 pub mod alias;
