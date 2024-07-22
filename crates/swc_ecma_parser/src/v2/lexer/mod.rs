884bb21e5c1c01075004422939fd4fecc10f0644
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-22 15:30:40 +0900

    `cold_branch`

--- crates/swc_ecma_parser/src/v2/lexer/mod.rs
+++ crates/swc_ecma_parser/src/v2/lexer/mod.rs
@@ -3,3 +3,11 @@ pub use self::{kind::Kind, token::Token};
 mod byte_handlers;
 mod kind;
 mod token;
+
+/// Call a closure while hinting to compiler that this branch is rarely taken.
+/// "Cold trampoline function", suggested in:
+/// <https://users.rust-lang.org/t/is-cold-the-only-reliable-way-to-hint-to-branch-predictor/106509/2>
+#[cold]
+pub fn cold_branch<F: FnOnce() -> T, T>(f: F) -> T {
+    f()
+}
