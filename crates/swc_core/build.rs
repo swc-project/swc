43bb3f9e6657eb1cf07b1e7300dd2a850da46458
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-23 13:37:07 +0900

    fi

--- crates/swc_core/build.rs
+++ crates/swc_core/build.rs
@@ -9,7 +9,7 @@ use vergen::EmitBuilder;
 
 // Validate conflict between host / plugin features
 #[cfg(all(
-    feature = "plugin_transform",
+    feature = "ecma_plugin_transform",
     any(
         feature = "plugin_transform_host_native",
         feature = "plugin_transform_host_js"
@@ -19,15 +19,6 @@ compile_error!(
     "'plugin_transform' and 'plugin_transform_host*' features are mutually exclusive. If you're \
      writing a plugin, use 'plugin_transform' feature. If you're writing a custom SWC binary to \
      run plugin, use 'plugin_transform_host_*' instead."
-);
-
-#[cfg(all(feature = "__plugin_transform", feature = "common_concurrent"))]
-compile_error!("plugin transform cannot enable concurrent mode.");
-
-#[cfg(all(feature = "transforms", feature = "transforms_concurrent"))]
-compile_error!(
-    "'transforms' and 'transforms_concurrent' features are mutually exclusive. Please choose only \
-     one feature."
 );
 
 fn main() {
