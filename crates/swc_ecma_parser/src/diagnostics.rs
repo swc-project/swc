5251e67f0f62398e60d2489a958d0ed0002f3087
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-24 12:30:16 +0900

    daig

--- crates/swc_ecma_parser/src/diagnostics.rs
+++ crates/swc_ecma_parser/src/diagnostics.rs
@@ -1,5 +1,19 @@
+#![allow(unused)]
+
+use swc_common::Span;
+
 #[derive(Debug)]
 pub struct Diagnotic(Box<DiagnoticKind>);
 
 #[derive(Debug)]
 pub enum DiagnoticKind {}
+
+#[cold]
+pub(crate) fn invalid_character(c: char, span: Span) -> Diagnotic {
+    todo!("invalid_character")
+}
+
+#[cold]
+pub(crate) fn unterminated_multi_line_comment(span: Span) -> Diagnotic {
+    todo!("unterminated_multi_line_comment")
+}
