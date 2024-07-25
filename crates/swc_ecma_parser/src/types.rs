a3dada98f09def86f97b3d68f3f29d9e65cb0359
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-25 14:17:22 +0900

    More types

--- crates/swc_ecma_parser/src/types.rs
+++ crates/swc_ecma_parser/src/types.rs
@@ -6,3 +6,15 @@ pub enum MethodDefinitionKind {
     Get,
     Set,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ClassType {
+    ClassDeclaration,
+    ClassExpression,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum MethodDefinitionType {
+    MethodDefinition,
+    TSAbstractMethodDefinition,
+}
