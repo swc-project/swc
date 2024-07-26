ca3137ea07e28d2f0c161baa65022a21574de404
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-26 09:35:53 +0900

    More

--- crates/swc_ecma_parser/src/v2/diagnostics.rs
+++ crates/swc_ecma_parser/src/v2/diagnostics.rs
@@ -28,6 +28,11 @@ pub(crate) fn modifier_cannot_be_used_here(modifier: &Modifier) -> Diagnostic {
 }
 
 #[cold]
+pub(crate) fn cannot_appear_on_a_parameter(modifier: &Modifier) -> Diagnostic {
+    todo!("cannot_appear_on_a_parameter")
+}
+
+#[cold]
 pub(crate) fn invalid_character(c: char, span: Span) -> Diagnostic {
     todo!("invalid_character")
 }
