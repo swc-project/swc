2ecd681a7733931f865d641dd7ed14b9fdff2dd1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-29 11:36:26 +0900

    Add a test

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/9148/input.js
@@ -0,0 +1,15 @@
+function foo() {
+    const obj = {
+        clear: function () {
+            console.log('clear')
+        },
+        start: function () {
+            setTimeout(function () {
+                this.clear();
+            });
+        }
+    };
+    return () => obj.start()
+};
+
+foo()
