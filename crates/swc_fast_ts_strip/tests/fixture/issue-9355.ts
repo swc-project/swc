3ee82e223fe0ce54ad892cc5b009f573d997c60e
magic-akari <akari.ccino@gmail.com>
2024-07-31 21:33:33 +0800

    fix(es/typescript): Fix ASI in expression for fast strip (#9358)
    - Closes #9355

---
+++ crates/swc_fast_ts_strip/tests/fixture/issue-9355.ts
@@ -0,0 +1,20 @@
+const x1 = 10 as any
+(1)
+
+const x2 = (10)as any
+(1)
+
+const x3 = 10 satisfies any
+(1)
+
+const x4 = (10)satisfies any
+(1)
+
+const y = 10 as any
+    + 1
+
+const z = 10 as any
+    / 1
+
+const w = 10 as any
+[1];
