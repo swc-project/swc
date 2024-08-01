a4d19ea4f887195a26b5814d2d1b89de20951028
magic-akari <akari.ccino@gmail.com>
2024-08-02 00:09:10 +0800

    chore: avoid clone

--- crates/swc_ecma_transforms_typescript/src/strip_import_export.rs
+++ crates/swc_ecma_transforms_typescript/src/strip_import_export.rs
@@ -121,10 +121,10 @@ impl UsageCollect {
 
         let mut new_usage = AHashSet::default();
         for id in &self.id_usage {
-            let mut id = id.clone();
-            while let Some(next) = self.import_chain.remove(&id) {
-                new_usage.insert(next.clone());
-                id = next;
+            let mut entry = self.import_chain.remove_entry(id);
+            while let Some((id, next)) = entry {
+                new_usage.insert(next);
+                entry = self.import_chain.remove_entry(&id);
             }
             if self.import_chain.is_empty() {
                 break;
