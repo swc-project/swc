868be56a2515b96a7720bbe4620d14e07da16d65
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-08-20 09:49:19 +0900

    chore(ci): Configure publish pipeline for html binding (#9458)
    **Related issue:**
    
     - https://github.com/swc-project/swc/issues/9457

---
+++ packages/html/index.ts
@@ -0,0 +1,74 @@
+import * as binding from "./binding";
+
+export type MinifierType = "js-module" | "js-script" | "json" | "css" | "html";
+
+export type Options = {
+    filename?: string;
+    iframeSrcdoc?: boolean;
+    scriptingEnabled?: boolean;
+    forceSetHtml5Doctype?: boolean;
+    collapseWhitespaces?:
+        | "none"
+        | "all"
+        | "smart"
+        | "conservative"
+        | "advanced-conservative"
+        | "only-metadata";
+    removeEmptyMetadataElements?: boolean;
+    removeComments?: boolean;
+    preserveComments: string[];
+    minifyConditionalComments?: boolean;
+    removeEmptyAttributes?: boolean;
+    removeRedundantAttributes?: boolean;
+    collapseBooleanAttributes?: boolean;
+    normalizeAttributes?: boolean;
+    minifyJson?: boolean | { pretty?: boolean };
+    // TODO improve me after typing `@swc/css`
+    minifyJs?: boolean | { parser?: any; minifier?: any; codegen?: any };
+    minifyCss?: boolean | { parser?: any; minifier?: any; codegen?: any };
+    minifyAdditionalScriptsContent?: [string, MinifierType][];
+    minifyAdditionalAttributes?: [string, MinifierType][];
+    sortSpaceSeparatedAttributeValues?: boolean;
+    sortAttributes?: boolean;
+    tagOmission?: boolean;
+    selfClosingVoidElements?: boolean;
+    quotes?: boolean;
+};
+
+export type FragmentOptions = Options & {
+    mode?: "no-quirks" | "limited-quirks" | "quirks";
+    context_element?: binding.Element;
+    form_element?: binding.Element;
+};
+
+export async function minify(
+    content: Buffer,
+    options?: Options
+): Promise<binding.TransformOutput> {
+    return binding.minify(content, toBuffer(options ?? {}));
+}
+
+export async function minifyFragment(
+    content: Buffer,
+    options?: FragmentOptions
+): Promise<binding.TransformOutput> {
+    return binding.minifyFragment(content, toBuffer(options ?? {}));
+}
+
+export function minifySync(
+    content: Buffer,
+    options?: Options
+): binding.TransformOutput {
+    return binding.minifySync(content, toBuffer(options ?? {}));
+}
+
+export async function minifyFragmentSync(
+    content: Buffer,
+    options?: FragmentOptions
+): Promise<binding.TransformOutput> {
+    return binding.minifyFragmentSync(content, toBuffer(options ?? {}));
+}
+
+function toBuffer(t: any): Buffer {
+    return Buffer.from(JSON.stringify(t));
+}
---
+++ packages/html/scripts/npm/linux-arm64-gnu/README.md
@@ -0,0 +1,3 @@
+# `@swc/html-linux-arm64-gnu`
+
+This is the **aarch64-unknown-linux-gnu** binary for `@swc/html`
