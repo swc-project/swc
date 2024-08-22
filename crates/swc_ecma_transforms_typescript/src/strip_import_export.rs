65a24a52b983da041fed51e47ce55bd664c065db
magic-akari <akari.ccino@gmail.com>
2024-08-22 22:20:54 +0800

    chore: clean code

--- crates/swc_ecma_transforms_typescript/src/strip_import_export.rs
+++ crates/swc_ecma_transforms_typescript/src/strip_import_export.rs
@@ -121,10 +121,10 @@ impl UsageCollect {
 
         let mut new_usage = AHashSet::default();
         for id in &self.id_usage {
-            let mut entry = self.import_chain.remove_entry(id);
-            while let Some((_, next)) = entry {
-                entry = self.import_chain.remove_entry(&next);
-                new_usage.insert(next);
+            let mut next = self.import_chain.remove(id);
+            while let Some(id) = next {
+                next = self.import_chain.remove(&id);
+                new_usage.insert(id);
             }
             if self.import_chain.is_empty() {
                 break;
