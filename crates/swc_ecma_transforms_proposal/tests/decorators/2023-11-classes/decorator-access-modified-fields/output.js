6d32baacd77f3457efdffb0c804247396d6844b5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-23 11:37:51 +0900

    Add tests

---
+++ crates/swc_ecma_transforms_proposal/tests/decorators/2023-11-classes/decorator-access-modified-fields/output.js
@@ -0,0 +1,28 @@
+var _initClass, _init_m, _init_extra_m;
+var value;
+const classDec = (Class)=>{
+    value = (new Class).p;
+    return Class;
+};
+const memberDec = ()=>()=>42;
+let _C;
+class C {
+    static{
+        ({ e: [_init_m, _init_extra_m], c: [_C, _initClass] } = _apply_decs_2311(this, [
+            classDec
+        ], [
+            [
+                memberDec,
+                0,
+                "m"
+            ]
+        ]));
+    }
+    constructor(){
+        _init_extra_m(this);
+    }
+    m = _init_m(this);
+    static{
+        _initClass();
+    }
+}
