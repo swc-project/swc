e976808b0db992fe70b078a0a118e11d120efb28
CPunisher <1343316114@qq.com>
2024-09-03 11:42:14 +0800

    Add more class cases

--- crates/swc_ecma_minifier/src/compress/optimize/inline.rs
+++ crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -156,7 +156,7 @@ impl Optimizer<'_> {
 
             if !usage.reassigned {
                 match init {
-                    Expr::Fn(..) | Expr::Arrow(..) => {
+                    Expr::Fn(..) | Expr::Arrow(..) | Expr::Class(..) => {
                         self.typeofs.insert(ident.to_id(), "function".into());
                     }
                     Expr::Array(..) | Expr::Object(..) => {
