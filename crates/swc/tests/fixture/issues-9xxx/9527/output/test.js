00cffa100f87675e670023e774fb6357d4f3d569
magic-akari <akari.ccino@gmail.com>
2024-09-03 17:19:56 +0800

    chore: update test cases

---
+++ crates/swc/tests/fixture/issues-9xxx/9527/output/test.js
@@ -0,0 +1,54 @@
+var _call_super = require("@swc/helpers/_/_call_super");
+var _class_call_check = require("@swc/helpers/_/_class_call_check");
+var _inherits = require("@swc/helpers/_/_inherits");
+var Base = function Base() {
+    "use strict";
+    _class_call_check._(this, Base);
+};
+;
+new /*#__PURE__*/ (function(Base) {
+    "use strict";
+    _inherits._(_class, Base);
+    function _class() {
+        _class_call_check._(this, _class);
+        var _this;
+        (_this = _call_super._(this, _class)) || null;
+        return _this;
+    }
+    return _class;
+}(Base));
+new /*#__PURE__*/ (function(Base) {
+    "use strict";
+    _inherits._(_class, Base);
+    function _class() {
+        _class_call_check._(this, _class);
+        var _this;
+        var _this1;
+        (_this1 = _this = _call_super._(this, _class)) === null || _this1 === void 0 ? void 0 : _this1();
+        return _this;
+    }
+    return _class;
+}(Base));
+new /*#__PURE__*/ (function(Base) {
+    "use strict";
+    _inherits._(_class, Base);
+    function _class() {
+        _class_call_check._(this, _class);
+        var _this;
+        var _this1;
+        (_this1 = _this = _call_super._(this, _class)) === null || _this1 === void 0 ? void 0 : _this1[null];
+        return _this;
+    }
+    return _class;
+}(Base));
+new /*#__PURE__*/ (function(Base) {
+    "use strict";
+    _inherits._(_class, Base);
+    function _class() {
+        _class_call_check._(this, _class);
+        var _this;
+        1 + (_this = _call_super._(this, _class));
+        return _this;
+    }
+    return _class;
+}(Base));
