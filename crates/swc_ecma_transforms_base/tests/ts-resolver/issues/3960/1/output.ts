785664226ae986b6ffff36176d822b38405e9799
magic-akari <akari.ccino@gmail.com>
2024-09-11 00:47:56 +0800

    chore: update test cases

--- crates/swc_ecma_transforms_base/tests/ts-resolver/issues/3960/1/output.ts
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/issues/3960/1/output.ts
@@ -1,6 +1,8 @@
 import { field__2 } from "../validation/decorators";
 class C__2 {
-    constructor(@field__2("a")
-    readonly field__3: string, @field__2("b")
+    constructor(
+    @field__2("a")
+    readonly field__3: string, 
+    @field__2("b")
     readonly b__3: string){}
 }
