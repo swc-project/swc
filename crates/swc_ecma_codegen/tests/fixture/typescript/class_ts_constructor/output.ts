64b0effbfa291095fc0a8876bd476c9a34fc1c9c
magic-akari <akari.ccino@gmail.com>
2024-09-11 00:30:35 +0800

    chore: update test cases

---
+++ crates/swc_ecma_codegen/tests/fixture/typescript/class_ts_constructor/output.ts
@@ -0,0 +1,7 @@
+export class A {
+    constructor(
+    @IInstantiationService
+    protected readonly _instantiationService: IInstantiationService, 
+    @IContainerService
+    private readonly _containerService: IContainerService){}
+}
