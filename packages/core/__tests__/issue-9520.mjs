ca76b71e34cb5afb7d9db50df4ea090b35c991e2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-11 20:24:53 +0900

    Add tests

---
+++ packages/core/__tests__/issue-9520.mjs
@@ -0,0 +1,21 @@
+import swc from "..";
+
+it("should transform synchronously", () => {
+    const { code } = swc.transformSync(`export function foo() {
+	if (false) {
+	}
+	return 'foo';
+};`);
+
+    expect(code).toMatchInlineSnapshot();
+});
+
+it("should transform asynchronously", async () => {
+    const { code } = await swc.transform(`export function foo() {
+	if (false) {
+	}
+	return 'foo';
+};`);
+    expect(code).toMatchInlineSnapshot();
+});
+
