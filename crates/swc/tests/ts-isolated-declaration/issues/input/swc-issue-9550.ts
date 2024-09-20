64a7065bd4baa7bdc448ec11f0dcf65ef7774d4a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-20 14:10:13 +0900

    Add a test

---
+++ crates/swc/tests/ts-isolated-declaration/issues/input/swc-issue-9550.ts
@@ -0,0 +1,9 @@
+/**
+ * Sample JSDoc that I wish was in the swc.transform output
+ * @param a - string param
+ * @param b - number param
+ * @returns - object with a and b
+ */
+function sampleFunc(a: string, b: number) {
+    return { a, b };
+}
