479e433f8861cb9a3764333148fb7ea4b7811f95
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-27 20:11:03 +0900

    Sync test suite

--- crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/if-body/output.js
+++ crates/swc_ecma_transforms_proposal/tests/explicit-resource-management/transform-sync/if-body/output.js
@@ -1,11 +1,9 @@
-if (test) {
-    try {
-        var _usingCtx = _using_ctx();
-        const x = _usingCtx.u(obj);
-        doSomethingWith(x);
-    } catch (_) {
-        _usingCtx.e = _;
-    } finally{
-        _usingCtx.d();
-    }
+if (test) try {
+  var _usingCtx = babelHelpers.usingCtx();
+  const x = _usingCtx.u(obj);
+  doSomethingWith(x);
+} catch (_) {
+  _usingCtx.e = _;
+} finally {
+  _usingCtx.d();
 }
