74e3d0466abcd7422620623d8adcceac04ce26c9
Victor Freitas <git.main@victorfreitas.tech>
2024-09-30 23:12:26 -0300

    fix(es/module): Fix `jsc.paths` using absolute paths with dots in a filename for an alias (#9595)
    **Description:**
    
    - Resolves `jsc.paths` using an absolute filename with dots
    
    **Related issue:**
    
    - Closes https://github.com/swc-project/swc/issues/9591

--- crates/swc_ecma_loader/src/resolvers/tsc.rs
+++ crates/swc_ecma_loader/src/resolvers/tsc.rs
@@ -302,7 +302,7 @@ where
                         .split([std::path::MAIN_SEPARATOR, '/'])
                         .last()
                         .filter(|&slug| slug != "index.ts" && slug != "index.tsx")
-                        .map(|v| v.split_once('.').map(|v| v.0).unwrap_or(v))
+                        .map(|v| v.rsplit_once('.').map(|v| v.0).unwrap_or(v))
                         .map(From::from);
 
                     if tp.is_absolute() {
