70aff8b684ac5d8714d807e9ce60032c141a6c03
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-02 09:39:04 +0900

    types

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -349,7 +349,7 @@ export interface TerserMangleOptions {
     reserved?: string[];
 }
 
-export interface TerserManglePropertiesOptions {}
+export interface TerserManglePropertiesOptions { }
 
 /**
  * Programmatic options.
@@ -671,6 +671,11 @@ export interface JscConfig {
          * Emit isolated dts files for each module.
          */
         emitIsolatedDts?: boolean;
+
+        /**
+         * Disable all lint rules.
+         */
+        disableAllLints?: boolean;
     };
 
     baseUrl?: string;
@@ -1154,7 +1159,7 @@ export interface Output {
     map?: string;
 }
 
-export interface MatchPattern {}
+export interface MatchPattern { }
 
 // -------------------------------
 // ---------- Ast nodes ----------
@@ -1386,7 +1391,7 @@ export type Expression =
     | OptionalChainingExpression
     | Invalid;
 
-interface ExpressionBase extends Node, HasSpan {}
+interface ExpressionBase extends Node, HasSpan { }
 
 export interface Identifier extends ExpressionBase {
     type: "Identifier";
