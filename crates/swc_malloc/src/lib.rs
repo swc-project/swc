f63905406d45aaefe2f5bc11bea135351aa3495a
CPunisher <1343316114@qq.com>
2024-10-12 23:00:17 +0800

    allocator miri

--- crates/swc_malloc/src/lib.rs
+++ crates/swc_malloc/src/lib.rs
@@ -2,14 +2,15 @@
 //!
 //! The swc crates related to the  node binding should depend on this crate.
 
-#[cfg(not(target_os = "linux"))]
+#[cfg(all(not(target_os = "linux"), not(miri)))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[cfg(all(
     target_os = "linux",
     target_env = "gnu",
-    any(target_arch = "x86_64", target_arch = "aarch64")
+    any(target_arch = "x86_64", target_arch = "aarch64"),
+    not(miri)
 ))]
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
