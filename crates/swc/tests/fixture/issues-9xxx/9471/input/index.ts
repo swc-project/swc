9ca4e45d44aa7b53fe46420437c4ae3d824a69a0
magic-akari <akari.ccino@gmail.com>
2024-10-17 03:35:53 +0800

    chore: update test cases

---
+++ crates/swc/tests/fixture/issues-9xxx/9471/input/index.ts
@@ -0,0 +1,7 @@
+const x0 = testNum as number > 0;
+const x1 = testNum as number >> 0;
+const x2 = testNum as number >= 0;
+const x3 = testNum as number >>> 0;
+const x4 = testNum as number < 0;
+const x5 = testNum as number << 0;
+const x6 = testNum as number <= 0;
