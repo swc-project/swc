a1c6d13fe1d5881edc78e01d0882e25ba7764686
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-17 12:03:32 +0900

    Improve helper

--- crates/swc_ecma_transforms_base/src/helpers/_ts_add_disposable_resource.js
+++ crates/swc_ecma_transforms_base/src/helpers/_ts_add_disposable_resource.js
@@ -1,7 +1,7 @@
 function _ts_add_disposable_resource(env, value, async) {
     if (value !== null && value !== void 0) {
         if (typeof value !== "object" && typeof value !== "function") throw new TypeError("Object expected.");
-        var dispose, inner;
+        var dispose;
         if (async) {
             if (!Symbol.asyncDispose) throw new TypeError("Symbol.asyncDispose is not defined.");
             dispose = value[Symbol.asyncDispose];
@@ -9,14 +9,12 @@ function _ts_add_disposable_resource(env, value, async) {
         if (dispose === void 0) {
             if (!Symbol.dispose) throw new TypeError("Symbol.dispose is not defined.");
             dispose = value[Symbol.dispose];
-            if (async) inner = dispose;
         }
         if (typeof dispose !== "function") throw new TypeError("Object not disposable.");
-        if (inner) dispose = function () { try { inner.call(this); } catch (e) { return Promise.reject(e); } };
         env.stack.push({ value: value, dispose: dispose, async: async });
     }
     else if (async) {
         env.stack.push({ async: true });
     }
     return value;
-};
+}
