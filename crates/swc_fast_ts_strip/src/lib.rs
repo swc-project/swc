a59e3543509673de64d929bcef305e5bbe044f4c
Benny Powers - עם ישראל חי! <bennypowers@users.noreply.github.com>
2024-10-20 12:10:01 +0300

    fix: correct option types of wasm-typescript functions
    without this fix:
    
    ```
    Property 'TransformConfig' is missing in type '{ module: true; filename: any; sourceMap:   • 
     true; mode: "transform"; transform: { verbatimModuleSyntax: boolean; nativeClassProperties:       
     boolean; importNotUsedAsValues: string; importExportAssignConfig: string; }; }' but required in   
     type 'Options'. [2345]  
    ```

--- crates/swc_fast_ts_strip/src/lib.rs
+++ crates/swc_fast_ts_strip/src/lib.rs
@@ -64,7 +64,7 @@ interface Options {
     module?: boolean;
     filename?: string;
     mode?: Mode;
-    transform?; TransformConfig;
+    transform?: TransformConfig;
     sourceMap?: boolean;
 }
 
