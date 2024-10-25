e466be380a458437ff05366d9d419acb5c14bd35
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-25 22:30:56 +0900

    More work

--- crates/swc_ecma_minifier/examples/compress.rs
+++ crates/swc_ecma_minifier/examples/compress.rs
@@ -9,6 +9,7 @@ extern crate swc_malloc;
 use std::{env::args, fs, path::Path};
 
 use swc_common::{sync::Lrc, Mark, SourceMap};
+use swc_ecma_ast::Program;
 use swc_ecma_codegen::text_writer::JsWriter;
 use swc_ecma_minifier::{
     optimize,
@@ -42,8 +43,9 @@ fn main() {
         .map_err(|err| {
             err.into_diagnostic(&handler).emit();
         })
-        .map(|module| module.fold_with(&mut resolver(unresolved_mark, top_level_mark, false)))
-        .map(|module| module.fold_with(&mut paren_remover(None)))
+        .map(Program::Module)
+        .map(|module| module.apply(resolver(unresolved_mark, top_level_mark, false)))
+        .map(|module| module.apply(paren_remover(None)))
         .unwrap();
 
         let output = optimize(
