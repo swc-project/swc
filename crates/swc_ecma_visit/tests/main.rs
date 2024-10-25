c68cad74df382b876d97b9f937644e728e393f21
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-25 15:27:05 +0900

    more

--- crates/swc_ecma_visit/tests/main.rs
+++ crates/swc_ecma_visit/tests/main.rs
@@ -1,34 +1,6 @@
 use swc_common::DUMMY_SP;
 use swc_ecma_ast::*;
-use swc_ecma_visit::{NodeRef, Visit, VisitWith};
-
-#[test]
-fn should_visit_program() {
-    struct Pass1<'a>(&'a mut usize);
-    struct Pass2;
-
-    impl Visit for Pass1<'_> {
-        fn visit_program(&mut self, _program: &Program) {
-            *self.0 += 1;
-        }
-    }
-
-    impl Visit for Pass2 {}
-
-    let n = Program::Module(Module {
-        span: DUMMY_SP,
-        body: Vec::new(),
-        shebang: None,
-    });
-
-    let mut counter = 0;
-    let p1 = Pass1(&mut counter);
-    let p2 = Pass2;
-    let mut pass = chain!(p1, p2);
-    n.visit_with(&mut pass);
-
-    assert_eq!(counter, 1);
-}
+use swc_ecma_visit::NodeRef;
 
 #[test]
 fn traverse_lookup() {
