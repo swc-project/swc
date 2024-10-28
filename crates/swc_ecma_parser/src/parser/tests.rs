7ac68227e5da949d1d87c3cb3ca1d8573ef9a94f
David Sherret <dsherret@gmail.com>
2024-10-28 18:41:41 -0400

    format

--- crates/swc_ecma_parser/src/parser/tests.rs
+++ crates/swc_ecma_parser/src/parser/tests.rs
@@ -350,26 +350,22 @@ fn parse_non_strict_for_loop() {
 
 #[test]
 fn parse_program_take_script_module_errors() {
-    test_parser(
-        r#"077;"#,
-        Default::default(),
-        |p| {
-            let program = p.parse_program()?;
+    test_parser(r#"077;"#, Default::default(), |p| {
+        let program = p.parse_program()?;
 
-            assert_eq!(p.take_errors(), vec![]);
-            // will contain the script's potential module errors
-            assert_eq!(
-                p.take_script_module_errors(),
-                vec![Error::new(
-                    Span {
-                        lo: BytePos(1),
-                        hi: BytePos(4),
-                    },
-                    crate::parser::SyntaxError::LegacyOctal
-                )]
-            );
+        assert_eq!(p.take_errors(), vec![]);
+        // will contain the script's potential module errors
+        assert_eq!(
+            p.take_script_module_errors(),
+            vec![Error::new(
+                Span {
+                    lo: BytePos(1),
+                    hi: BytePos(4),
+                },
+                crate::parser::SyntaxError::LegacyOctal
+            )]
+        );
 
-            Ok(program)
-        },
-    );
+        Ok(program)
+    });
 }
