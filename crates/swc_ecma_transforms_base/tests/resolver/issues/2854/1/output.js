6ea7616b5f8655dda913309aa135a017711b0ec5
magic-akari <akari.ccino@gmail.com>
2024-10-29 22:46:00 +0800

    chore: update test cases

--- crates/swc_ecma_transforms_base/tests/resolver/issues/2854/1/output.js
+++ crates/swc_ecma_transforms_base/tests/resolver/issues/2854/1/output.js
@@ -2,7 +2,7 @@ export function App__2() {
     return <Form__2/>;
 }
 export function Form__2({ onChange__4 = function() {} }) {
-    return <input onChange={function() {
+    return <input__0 onChange={function() {
         onChange__4();
     }}/>;
 }
