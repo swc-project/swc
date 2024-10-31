bcf05de2ebe755a54ec8a6b93311b1686494c578
magic-akari <akari.ccino@gmail.com>
2024-10-31 09:32:27 +0800

    fix(es/parser): Parse `await using()` call (#9693)
    **Related issue:**
    
    - Closes #9688

---
+++ crates/swc/tests/fixture/issues-9xxx/9688/input/index.js
@@ -0,0 +1,4 @@
+async function using(x) { /* do something... */ }
+async function test() {
+    await using();
+}
---
+++ crates/swc/tests/fixture/issues-9xxx/9688/output/index.js
@@ -0,0 +1,4 @@
+async function using(x) {}
+async function test() {
+    await using();
+}
