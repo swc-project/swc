bec9c0e27a8f1f096282ba607a36335737ffc6a1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-01 14:12:20 +0900

    CahceCell

--- crates/swc_common/src/cache.rs
+++ crates/swc_common/src/cache.rs
@@ -1,12 +1,58 @@
 use once_cell::sync::OnceCell;
-#[cfg(feature = "rkyv-impl")]
-use rkyv::Archive;
 
 /// Wrapper for [OnceCell] with support for [rkyv].
 #[derive(Clone, Debug)]
-#[cfg_attr(feature = "rkyv-impl", derive(Archive))]
-#[cfg_attr(
-    feature = "rkyv-impl",
-    archive(check_bytes, bound(archive = "T: rkyv::Archive"))
-)]
-pub struct CacheCell<T>(#[cfg_attr(feature = "rkyv-impl", archive(omit_bounds))] OnceCell<T>);
+pub struct CacheCell<T>(OnceCell<T>);
+
+#[cfg(feature = "rkyv-impl")]
+mod rkyv_impl {
+    use std::{hint::unreachable_unchecked, ptr};
+
+    use rkyv::{out_field, Archive, Archived, Resolver};
+
+    use super::*;
+
+    #[allow(dead_code)]
+    #[repr(u8)]
+    enum ArchivedOptionTag {
+        None,
+        Some,
+    }
+
+    #[repr(C)]
+    struct ArchivedOptionVariantNone(ArchivedOptionTag);
+
+    #[repr(C)]
+    struct ArchivedOptionVariantSome<T>(ArchivedOptionTag, T);
+
+    impl<T> Archive for CacheCell<T>
+    where
+        T: Archive,
+    {
+        type Archived = Archived<Option<T>>;
+        type Resolver = Resolver<Option<T>>;
+
+        unsafe fn resolve(&self, pos: usize, resolver: Self::Resolver, out: *mut Self::Archived) {
+            match resolver {
+                None => {
+                    let out = out.cast::<ArchivedOptionVariantNone>();
+                    ptr::addr_of_mut!((*out).0).write(ArchivedOptionTag::None);
+                }
+                Some(resolver) => {
+                    let out = out.cast::<ArchivedOptionVariantSome<T::Archived>>();
+                    ptr::addr_of_mut!((*out).0).write(ArchivedOptionTag::Some);
+
+                    let v = self.0.get();
+                    let value = if let Some(value) = v.as_ref() {
+                        value
+                    } else {
+                        unreachable_unchecked();
+                    };
+
+                    let (fp, fo) = out_field!(out.1);
+                    value.resolve(pos + fp, resolver, fo);
+                }
+            }
+        }
+    }
+}
