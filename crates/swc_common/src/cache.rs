2357fd707f0ee439c0fe0e835317521c45a809f8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-01 14:03:43 +0900

    rkyv

--- crates/swc_common/src/cache.rs
+++ crates/swc_common/src/cache.rs
@@ -5,4 +5,8 @@ use rkyv::Archive;
 /// Wrapper for [OnceCell] with support for [rkyv].
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "rkyv-impl", derive(Archive))]
+#[cfg_attr(
+    feature = "rkyv-impl",
+    archive(check_bytes, bound(archive = "T: rkyv::Archive"))
+)]
 pub struct CacheCell<T>(#[cfg_attr(feature = "rkyv-impl", archive(omit_bounds))] OnceCell<T>);
