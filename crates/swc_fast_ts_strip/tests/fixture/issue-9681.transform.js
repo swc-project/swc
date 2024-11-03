844ba71cce7b6510606f1f81a1ef711e0a53be6e
magic-akari <akari.ccino@gmail.com>
2024-11-03 21:00:51 +0800

    fix(es/typescript): Handle ASI hazards in fast type strip

---
+++ crates/swc_fast_ts_strip/tests/fixture/issue-9681.transform.js
@@ -0,0 +1,33 @@
+class A {
+    foo = 1;
+    ["bar"] = 2;
+    x = 1;
+    ["y"]() {}
+    z = 1;
+    static ["w"]() {}
+}
+class Test {
+    x = 1;
+    *f() {}
+}
+class Test2 {
+    out = 'out';
+    in;
+    name;
+}
+class Test3 {
+    out = 'out';
+    in;
+    name;
+    x = 'x';
+    instanceof;
+    String;
+}
+class Test4 {
+    set;
+    a(x) {}
+    get;
+    b() {}
+    static;
+    c(x) {}
+}
