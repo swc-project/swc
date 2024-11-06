b49317a40344c2c153044095f49d0a9e8a1ef3f3
CPunisher <1343316114@qq.com>
2024-11-06 10:14:33 +0800

    fix(es/minifier): Avoid generating reserved mangling names (#9710)
    **Description:**
    
    Copy user defined `reserved` to `preserved_symbols`, so the generated name will be skipped by
    https://github.com/swc-project/swc/blob/56e83e2392c8ae89ce3854b36c64fed5e90f14c4/crates/swc_ecma_transforms_base/src/rename/analyzer/scope.rs#L296
    
    
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/9650

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/9650/output.js
@@ -0,0 +1,3 @@
+export function logVariables(o, l, e, n, a, c, g, i, r, s, t, b, f, p, u, x, V, d, h, j, k, m, q, v, w, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, W, X, Y, Z, _, oo, ol, oe, on, oa, oc, og) {
+    console.log(o, l, e, n, a, c, g, i, r, s, t, b, f, p, u, x, V, d, h, j, k, m, q, v, w, y, z, A, B, C);
+}
