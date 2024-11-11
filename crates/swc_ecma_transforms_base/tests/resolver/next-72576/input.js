0cf3605cf45ec62c00c9efb554fb6bfb3864cf46
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-11 21:54:56 +0900

    Add  a test

---
+++ crates/swc_ecma_transforms_base/tests/resolver/next-72576/input.js
@@ -0,0 +1,9 @@
+declare const MY_MAGIC_VARIABLE: string;
+
+export default function Page() {
+    return (
+        <ul>
+            <li>Server value: {true ? MY_MAGIC_VARIABLE : "not set"}</li>
+        </ul>
+    );
+}
