1dcd370e2f1e55b36a1d828b27779d611766db9d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-11 21:56:36 +0900

    test

--- crates/swc_ecma_transforms_base/tests/resolver/next-72576/input.ts
+++ crates/swc_ecma_transforms_base/tests/resolver/next-72576/input.ts
@@ -3,7 +3,7 @@ declare const MY_MAGIC_VARIABLE: string;
 export default function Page() {
     return (
         <ul>
-            <li>Server value: {true ? MY_MAGIC_VARIABLE : "not set"}</li>
-        </ul>
+        <li>Server value: { true ? MY_MAGIC_VARIABLE : "not set" } </li>
+            </ul>
     );
 }
