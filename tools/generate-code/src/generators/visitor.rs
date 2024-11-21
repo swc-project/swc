34444cc8eecdad595c60ca25d2250401d6d73521
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-21 20:14:15 +0900

    derive more

--- tools/generate-code/src/generators/visitor.rs
+++ tools/generate-code/src/generators/visitor.rs
@@ -1530,6 +1530,7 @@ fn define_fields(crate_name: &Ident, node_types: &[&Item]) -> Vec<Item> {
 
             defs.push(parse_quote!(
                 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+                #[cfg_attr(feature = "serde-impl", derive(serde::Serialize, serde::Deserialize))]
                 pub enum #fields_enum_name {
                     #(#variants),*
                 }
