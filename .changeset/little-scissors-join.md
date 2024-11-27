b83c44f4ad604edc30ec157aa8fb0d8755adb389
magic-akari <akari.ccino@gmail.com>
2024-11-27 11:37:10 +0800

    fix(es/codegen): Use raw value for emitting JSX text (#9762)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/9758

---
+++ .changeset/little-scissors-join.md
@@ -0,0 +1,6 @@
+---
+swc_ecma_codegen: patch
+swc_core: patch
+---
+
+fix(es/codegen): use raw value for emitting JSX text
---
+++ crates/swc_ecma_codegen/tests/fixture/issue-9758/input.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Test() {
+  return (
+    <div className={test}>
+      <span>&gt; 右上角设置</span>
+      <span>&gt; 保证金管理</span>
+    </div>
+  );
+}
