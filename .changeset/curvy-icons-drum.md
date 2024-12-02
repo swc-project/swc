0f12bbdcaeae3538cabe04db125ac5824da42bd5
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-12-02 10:38:51 +0900

    refactor(estree/compat): Do not use nightly features (#9772)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/9770

---
+++ .changeset/curvy-icons-drum.md
@@ -0,0 +1,6 @@
+---
+swc_estree_compat: patch
+swc_core: patch
+---
+
+refactor(estree/compat): Do not use nightly features
--- crates/swc_estree_compat/src/swcify/expr.rs
+++ crates/swc_estree_compat/src/swcify/expr.rs
@@ -27,7 +27,7 @@ use swc_estree_ast::{
 };
 
 use super::Context;
-use crate::swcify::Swcify;
+use crate::{swcify::Swcify, Never};
 
 impl Swcify for Expression {
     type Output = Box<Expr>;
@@ -819,7 +819,7 @@ impl Swcify for OptionalCallExpression {
 }
 
 impl Swcify for TypeCastExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         unimplemented!("flow type cast")
@@ -1083,7 +1083,7 @@ impl Swcify for swc_estree_ast::JSXClosingFragment {
 }
 
 impl Swcify for BindExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support bind expressions")
@@ -1091,7 +1091,7 @@ impl Swcify for BindExpression {
 }
 
 impl Swcify for DoExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support do expressions")
@@ -1099,7 +1099,7 @@ impl Swcify for DoExpression {
 }
 
 impl Swcify for PipelinePrimaryTopicReference {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support `PipelinePrimaryTopicReference`")
@@ -1107,7 +1107,7 @@ impl Swcify for PipelinePrimaryTopicReference {
 }
 
 impl Swcify for RecordExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support record expressions")
@@ -1115,7 +1115,7 @@ impl Swcify for RecordExpression {
 }
 
 impl Swcify for TupleExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support tuple expressions")
@@ -1123,7 +1123,7 @@ impl Swcify for TupleExpression {
 }
 
 impl Swcify for ModuleExpression {
-    type Output = !;
+    type Output = Never;
 
     fn swcify(self, _: &Context) -> Self::Output {
         panic!("swc does not support module expressions")
