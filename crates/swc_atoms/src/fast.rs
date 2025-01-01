f5cedaa741ecb052835b85783342913e490bc734
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-01 10:08:29 +0900

    FastAtom

--- crates/swc_atoms/src/fast.rs
+++ crates/swc_atoms/src/fast.rs
@@ -1,4 +1,7 @@
-use std::mem::ManuallyDrop;
+use std::{
+    mem::{transmute_copy, ManuallyDrop},
+    ops::Deref,
+};
 
 use crate::Atom;
 
@@ -7,3 +10,20 @@ use crate::Atom;
 ///
 /// Do not use this unless you know what you are doing.
 pub struct FastAtom(ManuallyDrop<Atom>);
+
+impl FastAtom {
+    /// # Safety
+    ///
+    ///  - You should ensure that the passed `atom` is not freed.
+    pub unsafe fn new(atom: &Atom) -> Self {
+        Self(ManuallyDrop::new(transmute_copy(atom)))
+    }
+}
+
+impl Deref for FastAtom {
+    type Target = Atom;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
