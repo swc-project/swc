663950dd3060ed6f7623c877115904a1fec55c9c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-06 15:54:08 +0900

    `join`

--- crates/swc_parallel/src/lib.rs
+++ crates/swc_parallel/src/lib.rs
@@ -1,20 +1,10 @@
-use once_cell::sync::Lazy;
-
-static GLOBAL_SCOPE: Lazy<GlobalScope> = Lazy::new(|| GlobalScope(chili::Scope::global()));
+use std::{cell::RefCell, mem::transmute};
 
 #[cfg(not(feature = "parallel"))]
 pub struct Scope<'a>(std::marker::PhantomData<&'a ()>);
 
 #[cfg(feature = "parallel")]
-pub struct Scope<'a>(chili::Scope<'a>);
-
-#[cfg(feature = "parallel")]
-struct GlobalScope(chili::Scope<'static>);
-
-#[cfg(feature = "parallel")]
-unsafe impl Sync for GlobalScope {}
-#[cfg(feature = "parallel")]
-unsafe impl Send for GlobalScope {}
+pub struct Scope<'a>(&'a mut chili::Scope<'a>);
 
 pub fn join<A, B, RA, RB>(oper_a: A, oper_b: B) -> (RA, RB)
 where
@@ -23,5 +13,34 @@ where
     RA: Send,
     RB: Send,
 {
-    let (ra, rb) = GLOBAL_SCOPE.0.join(|scope| {}, |scope| {});
+    thread_local! {
+        static SCOPE: RefCell<Option<Scope<'static>>> = const { RefCell::new(None) };
+    }
+
+    let mut global_scope;
+    let scope = SCOPE.try_with(|s| s.borrow_mut().take()).unwrap();
+
+    let scope = match scope {
+        Some(scope) => scope,
+        None => {
+            global_scope = chili::Scope::global();
+
+            Scope(unsafe { transmute::<&mut chili::Scope, &mut chili::Scope>(&mut global_scope) })
+        }
+    };
+
+    let (ra, rb) = scope.0.join(
+        |scope| {
+            let scope = Scope(unsafe { transmute::<&mut chili::Scope, &mut chili::Scope>(scope) });
+            SCOPE.set(Some(scope));
+            oper_a()
+        },
+        |scope| {
+            let scope = Scope(unsafe { transmute::<&mut chili::Scope, &mut chili::Scope>(scope) });
+            SCOPE.set(Some(scope));
+            oper_b()
+        },
+    );
+
+    (ra, rb)
 }
