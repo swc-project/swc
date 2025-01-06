66a4d9cbb64c825993d83a48745ff0c013298f1b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-06 20:08:57 +0900

    More maybe_par

--- crates/swc_ecma_lints/src/rules/no_dupe_args.rs
+++ crates/swc_ecma_lints/src/rules/no_dupe_args.rs
@@ -105,30 +105,6 @@ macro_rules! check {
 impl Visit for NoDupeArgs {
     noop_visit_type!();
 
-    fn visit_exprs(&mut self, exprs: &[Box<Expr>]) {
-        self.maybe_par(cpu_count() * 8, exprs, |v, expr| {
-            expr.visit_with(v);
-        });
-    }
-
-    fn visit_module_items(&mut self, items: &[ModuleItem]) {
-        self.maybe_par(cpu_count() * 8, items, |v, item| {
-            item.visit_with(v);
-        });
-    }
-
-    fn visit_stmts(&mut self, stmts: &[Stmt]) {
-        self.maybe_par(cpu_count() * 8, stmts, |v, stmt| {
-            stmt.visit_with(v);
-        });
-    }
-
-    fn visit_function(&mut self, f: &Function) {
-        check!(&f.params);
-
-        f.visit_children_with(self);
-    }
-
     fn visit_arrow_expr(&mut self, f: &ArrowExpr) {
         check!(&f.params);
 
@@ -139,5 +115,47 @@ impl Visit for NoDupeArgs {
         check!(&f.params);
 
         f.visit_children_with(self);
+    }
+
+    fn visit_expr_or_spreads(&mut self, n: &[ExprOrSpread]) {
+        self.maybe_par(cpu_count() * 8, n, |v, n| {
+            n.visit_with(v);
+        });
+    }
+
+    fn visit_exprs(&mut self, exprs: &[Box<Expr>]) {
+        self.maybe_par(cpu_count() * 8, exprs, |v, expr| {
+            expr.visit_with(v);
+        });
+    }
+
+    fn visit_function(&mut self, f: &Function) {
+        check!(&f.params);
+
+        f.visit_children_with(self);
+    }
+
+    fn visit_module_items(&mut self, items: &[ModuleItem]) {
+        self.maybe_par(cpu_count() * 8, items, |v, item| {
+            item.visit_with(v);
+        });
+    }
+
+    fn visit_opt_vec_expr_or_spreads(&mut self, n: &[Option<ExprOrSpread>]) {
+        self.maybe_par(cpu_count() * 8, n, |v, n| {
+            n.visit_with(v);
+        });
+    }
+
+    fn visit_prop_or_spreads(&mut self, n: &[PropOrSpread]) {
+        self.maybe_par(cpu_count() * 8, n, |v, n| {
+            n.visit_with(v);
+        });
+    }
+
+    fn visit_stmts(&mut self, stmts: &[Stmt]) {
+        self.maybe_par(cpu_count() * 8, stmts, |v, stmt| {
+            stmt.visit_with(v);
+        });
     }
 }
