fe74453627f0fe4b9d423603b608752d0ab41431
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-08 16:07:43 +0900

    Error object

--- bindings/binding_typescript_wasm/src/lib.rs
+++ bindings/binding_typescript_wasm/src/lib.rs
@@ -1,7 +1,8 @@
 use anyhow::Error;
+use serde::Serialize;
 use swc_common::{errors::ColorConfig, sync::Lrc, SourceMap, GLOBALS};
 use swc_error_reporters::handler::{try_with_handler, HandlerOpts};
-use swc_fast_ts_strip::{Options, TransformOutput, TsError};
+use swc_fast_ts_strip::{ErrorCode, Options, TransformOutput, TsError};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::{
     future_to_promise,
@@ -54,9 +55,19 @@ fn operate(input: String, options: Options) -> Result<TransformOutput, Error> {
     )
 }
 
+#[derive(Debug, Serialize)]
+struct ErrorObject {
+    code: ErrorCode,
+    message: String,
+}
+
 pub fn convert_err(err: Error) -> wasm_bindgen::prelude::JsValue {
     if let Some(ts_error) = err.downcast_ref::<TsError>() {
-        return Err(anyhow::Error::new(ts_error));
+        return serde_wasm_bindgen::to_value(&ErrorObject {
+            code: ts_error.code,
+            message: err.to_string(),
+        })
+        .unwrap();
     }
 
     format!("{}", err).into()
