72eac34ffe194dce98aa41ad093f3b3ae40d094c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-10 13:14:35 +0900

    skip

--- scripts/bench/build-all-crates.sh
+++ scripts/bench/build-all-crates.sh
@@ -16,12 +16,19 @@ for crate in $CRATE_NAMES; do
     fi
     
     crate_info=$(echo "$WS_CRATES" | jq -r 'select(.name == "'$crate'")')
-    echo $crate_info
+    bench_targets=$(echo $crate_info | jq -r '.targets[] | select(.kind | contains(["bench"]))')
+
+
+    # If crate has no benchmark target, skip it
+    if [[ -z $bench_targets ]]; then
+        echo "Skipping $crate because it has no benchmark target"
+        continue
+    fi
 
     # If crate has feature 'concurrent', build it with feature
     if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
         echo "Building $crate with feature 'concurrent'"
-        cargo codspeed build -p $crate --features "$crate/concurrent"
+        cargo codspeed build -p $crate --features concurrent
     else
         echo "Building $crate"
         cargo codspeed build -p $crate
