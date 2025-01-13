15c96c2ac2578504960697f3c959d861ae023d84
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-13 14:51:18 +0900

    `Edges`

--- crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
+++ crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
@@ -120,7 +120,7 @@ impl CompilerPass for TreeShaker {
 struct Data {
     used_names: FxHashMap<Id, VarInfo>,
 
-    edges: IndexMap<(Id, Id), VarInfo, RandomState>,
+    edges: Edges,
     /// Entrypoints.
     entry_ids: FxHashSet<Id>,
 
@@ -128,10 +128,13 @@ struct Data {
     graph_ix: IndexSet<Id, BuildHasherDefault<FxHasher>>,
 }
 
+#[derive(Default)]
+struct Edges(IndexMap<(Id, Id), VarInfo, RandomState>);
+
 impl Data {
     /// Add an edge to dependency graph
     fn add_dep_edge(&mut self, from: Id, to: Id, assign: bool) {
-        match self.edges.entry((from, to)) {
+        match self.edges.0.entry((from, to)) {
             indexmap::map::Entry::Occupied(mut info) => {
                 if assign {
                     info.get_mut().assign += 1;
@@ -169,7 +172,7 @@ impl Data {
             .map(|id| get_node(id.clone()))
             .collect::<IndexSet<_, RandomState>>();
 
-        for ((src, dst), info) in edges {
+        for ((src, dst), info) in edges.0 {
             let src = get_node(src);
             let dst = get_node(dst);
 
@@ -1288,7 +1291,7 @@ impl Merge for Data {
     fn merge(&mut self, other: Self) {
         self.used_names.merge(other.used_names);
         self.entry_ids.extend(other.entry_ids);
-        self.edges.merge(other.edges);
+        self.edges.0.merge(other.edges.0);
     }
 }
 
