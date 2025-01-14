3ceeaad519ac3af67a66c6ea51253948c77cb8e2
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-01-15 03:05:12 +0900

    Update list-crates-with-bench.sh

--- scripts/bench/list-crates-with-bench.sh
+++ scripts/bench/list-crates-with-bench.sh
@@ -3,4 +3,4 @@
 set -eu
 
 WS_CRATES=$(./scripts/cargo/get-workspace-crates-json.sh)
-echo "$WS_CRATES" | jq -r -c '[.[] | select(.targets[] | .name != "swc_plugin_runner" and .targets[] | .kind | contains(["bench"])) | .name] | unique'
+echo "$WS_CRATES" | jq -r -c '[.[] | select(.targets[] | (.kind | contains(["bench"])) and .name != "swc_plugin_runner") | .name] | unique'
