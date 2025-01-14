806fbd1885ab42a63d2db82897f1b73d81aa8aa4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-15 00:10:28 +0900

    concurrent build

--- scripts/bench/build-all-crates.sh
+++ scripts/bench/build-all-crates.sh
@@ -28,9 +28,11 @@ for crate in $CRATE_NAMES; do
     # If crate has feature 'concurrent', build it with feature
     if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
         echo "Building $crate with feature 'concurrent'"
-        cargo codspeed build -p $crate --features concurrent
+        cargo codspeed build -p $crate --features concurrent &
     else
         echo "Building $crate"
-        cargo codspeed build -p $crate
+        cargo codspeed build -p $crate &
     fi
 done
+
+wait
