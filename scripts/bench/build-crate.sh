e5b0f2a9d38fa95320bf7f4fa4138d963fa3c4f3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-15 00:45:07 +0900

    Remove `&`

--- scripts/bench/build-crate.sh
+++ scripts/bench/build-crate.sh
@@ -7,8 +7,8 @@ crate=$1
 # If crate has feature 'concurrent', build it with feature
 if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
     echo "Building $crate with feature 'concurrent'"
-    cargo codspeed build -p $crate --features concurrent &
+    cargo codspeed build -p $crate --features concurrent
 else
     echo "Building $crate"
-    cargo codspeed build -p $crate &
+    cargo codspeed build -p $crate
 fi
