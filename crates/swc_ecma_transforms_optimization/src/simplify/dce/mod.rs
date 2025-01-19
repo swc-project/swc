0e535ecd53bb1be111bfe55121f46f346ac37fdf
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-19 16:38:57 +0900

    Disable cycle

--- crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
+++ crates/swc_ecma_transforms_optimization/src/simplify/dce/mod.rs
@@ -154,6 +154,7 @@ impl Data {
     }
 
     /// Traverse the graph and subtract usages from `used_names`.
+    #[allow(unused)]
     fn subtract_cycles(&mut self) {
         let edges = take(&mut self.edges);
 
@@ -1317,7 +1318,7 @@ fn merge_data(data: Arc<ThreadLocal<RefCell<Data>>>) -> Data {
         merged.merge(data);
     }
 
-    merged.subtract_cycles();
+    // merged.subtract_cycles();
 
     merged
 }
@@ -1381,7 +1382,7 @@ impl Merge for Data {
     fn merge(&mut self, other: Self) {
         self.used_names.merge(other.used_names);
         self.entry_ids.extend(other.entry_ids);
-        self.edges.0.merge(other.edges.0);
+        // self.edges.0.merge(other.edges.0);
     }
 }
 
