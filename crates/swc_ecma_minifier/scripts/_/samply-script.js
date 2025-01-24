5c8eaee6e1ab0b2da4d2461b7ce9921aaea0e9e5
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-01-24 19:20:04 +0900

    chore: Update the script for samply (#9940)

--- crates/swc_ecma_minifier/scripts/_/samply-script.js
+++ crates/swc_ecma_minifier/scripts/_/samply-script.js
@@ -1,72 +1,81 @@
-
 (async () => {
-    const treeView = document.querySelector(".treeViewBodyInner.treeViewBodyInner1");
+    const treeView = document.querySelector(
+        ".treeViewBodyInner.treeViewBodyInner1"
+    );
 
     function sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
     function triggerMouseEvent(node, eventType) {
-        var clickEvent = new PointerEvent(eventType, { button: 0, bubbles: true, cancelable: true });
+        var clickEvent = new PointerEvent(eventType, {
+            button: 0,
+            bubbles: true,
+            cancelable: true,
+        });
         node.dispatchEvent(clickEvent);
     }
 
     async function sendKey(node, key, keyCode, code) {
-        node.dispatchEvent(new KeyboardEvent("keydown", {
-            key: key,
-            keyCode: keyCode,
-            code: code,
-            which: keyCode,
-            shiftKey: false,
-            ctrlKey: false,
-            metaKey: false,
-            bubbles: true,
-            cancelable: true,
-        }));
-        node.dispatchEvent(new KeyboardEvent("keyup", {
-            key: key,
-            keyCode: keyCode,
-            code: code,
-            which: keyCode,
-            shiftKey: false,
-            ctrlKey: false,
-            metaKey: false,
-            bubbles: true,
-            cancelable: true,
-        }));
-
+        node.dispatchEvent(
+            new KeyboardEvent("keydown", {
+                key: key,
+                keyCode: keyCode,
+                code: code,
+                which: keyCode,
+                shiftKey: false,
+                ctrlKey: false,
+                metaKey: false,
+                bubbles: true,
+                cancelable: true,
+            })
+        );
+        node.dispatchEvent(
+            new KeyboardEvent("keyup", {
+                key: key,
+                keyCode: keyCode,
+                code: code,
+                which: keyCode,
+                shiftKey: false,
+                ctrlKey: false,
+                metaKey: false,
+                bubbles: true,
+                cancelable: true,
+            })
+        );
     }
 
     async function handleRow(row) {
-        const lib = row.querySelector('.lib');
+        const lib = row.querySelector(".lib");
         const libText = lib.textContent.trim();
-        if (libText.endsWith("/swc_ecma_visit/src/generated.rs") ||
+        if (
+            libText.endsWith("/swc_ecma_visit/src/") ||
             libText.endsWith("/swc_ecma_utils/src/parallel.rs") ||
-            libText.includes('/.rustup/') ||
-            libText.includes('chili-0.2.0/') ||
-            libText.includes('scoped-tls-1.0.1/') ||
-            libText.includes('indexmap-2.7.1/') ||
-            libText.includes('hashbrown-0.14.5/') ||
-            libText.includes('/rust/deps/') ||
-            libText.includes('/better_scoped_tls/') ||
-            libText.includes('/swc_parallel/')) {
+            libText.includes("/.rustup/") ||
+            libText.includes("/std/") ||
+            libText.includes("chili-") ||
+            libText.includes("scoped-tls-") ||
+            libText.includes("indexmap-") ||
+            libText.includes("hashbrown-") ||
+            libText.includes("/rust/deps/") ||
+            libText.includes("/better_scoped_tls/") ||
+            libText.includes("/swc_parallel/")
+        ) {
             triggerMouseEvent(row, "mouseover");
             triggerMouseEvent(row, "mousedown");
             triggerMouseEvent(row, "mouseup");
             triggerMouseEvent(row, "click");
             await sleep(30);
 
-            await sendKey(row, 'm', 77, 'KeyM');
+            await sendKey(row, "m", 77, "KeyM");
             await sleep(100);
             return true;
         }
     }
 
-
     while (true) {
-        const rows = treeView.querySelectorAll('.treeViewRow');
+        const rows = treeView.querySelectorAll(".treeViewRow");
         console.log(`Checking ${rows.length} rows`);
-
 
         let didWork = false;
         for (const row of rows) {
@@ -81,5 +90,4 @@
             break;
         }
     }
-
 })();
