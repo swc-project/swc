56b20667687d9f52f3a4297520e264ce8b4565c3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-29 08:07:50 +0900

    Remove wrong strictness check

--- crates/swc_ecma_parser/src/parser/stmt.rs
+++ crates/swc_ecma_parser/src/parser/stmt.rs
@@ -1006,8 +1006,7 @@ impl<'a, I: Tokens> Parser<I> {
                 // typescript allows `declare` vars not to have initializers.
                 if self.ctx().in_declare {
                     None
-                } else if kind == VarDeclKind::Const && self.ctx().strict && !self.ctx().in_declare
-                {
+                } else if kind == VarDeclKind::Const && !self.ctx().in_declare {
                     self.emit_err(
                         span!(self, start),
                         SyntaxError::ConstDeclarationsRequireInitialization,
