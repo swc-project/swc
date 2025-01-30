7fcd401c209dd7794dd8cfebd841c26c9518b8d9
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-30 11:09:23 +0900

    option

--- packages/js-interop/scripts/bench.js
+++ packages/js-interop/scripts/bench.js
@@ -23,27 +23,50 @@ const SOURCE = fs.readFileSync('./scripts/input.js.txt', 'utf8');
 
 var suite = new Benchmark.Suite;
 
+const firstSwcOption = {
+    jsc: {
+        parser: {
+            syntax: 'ecmascript',
+            jsx: true
+        },
+        target: 'esnext'
+    }
+};
+
+const secondSwcOption = {
+    jsc: {
+        target: 'es2019'
+    }
+};
+
 // add tests
 suite
     .add('swc (code) -> babel (code) -> swc', {
         defer: true,
         fn: async function (deferred) {
-            await lib.transform3Times(SOURCE, {
-            }, createBabelTransform({}), {});
+            await lib.transform3Times(SOURCE, firstSwcOption, createBabelTransform({}), secondSwcOption);
             deferred.resolve();
         },
     })
     .add('swc (code) -> babel', {
         defer: true,
         fn: async function (deferred) {
-            await lib.transform2Times(SOURCE, {}, createBabelTransform({}));
+            await lib.transform2Times(SOURCE, firstSwcOption, createBabelTransform({}));
             deferred.resolve();
         },
     })
     .add('swc', {
         defer: true,
         fn: async function (deferred) {
-            await lib.transformOnce(SOURCE, {});
+            await lib.transformOnce(SOURCE, {
+                jsc: {
+                    parser: {
+                        syntax: 'ecmascript',
+                        jsx: true
+                    },
+                    target: 'es2019'
+                }
+            });
             deferred.resolve();
         },
     })
