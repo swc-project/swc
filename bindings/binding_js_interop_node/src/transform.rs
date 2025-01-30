c4380c9c5915b6378925d4393fed2d2dcba37201
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-30 09:57:35 +0900

    apply 2 or 3

--- bindings/binding_js_interop_node/src/transform.rs
+++ bindings/binding_js_interop_node/src/transform.rs
@@ -1,90 +1,46 @@
-use std::{
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::path::Path;
 
-use anyhow::Context as _;
-use napi::{
-    bindgen_prelude::{AbortSignal, AsyncTask},
-    Env, JsBuffer, JsBufferValue, JsFunction, Ref, Task,
-};
-use swc_core::{
-    base::{config::Options, Compiler, TransformOutput},
-    common::FileName,
-    node::MapErr,
-};
-use swc_interop_babel::transform::JsTrasnform;
+use napi::{bindgen_prelude::AbortSignal, Env, JsBuffer, JsBufferValue, JsFunction, JsObject, Ref};
+use swc_core::{base::config::Options, common::FileName, node::MapErr};
+use swc_interop_babel::transform::{JsTrasnform, TransformOutput};
 use tracing::instrument;
 
 use crate::{get_compiler, util::try_with};
 
-/// Input to transform
-#[derive(Debug)]
-pub enum Input {
-    /// Raw source code.
-    Source { src: String },
-    /// File
-    File(PathBuf),
-}
+fn apply_swc(input: TransformOutput, options: Ref<JsBufferValue>) -> napi::Result<TransformOutput> {
+    let c = get_compiler();
+    let mut options: Options = serde_json::from_slice(options.as_ref())?;
+    if !options.filename.is_empty() {
+        options.config.adjust(Path::new(&options.filename));
+    }
 
-pub struct TransformTask {
-    pub c: Arc<Compiler>,
-    pub input: Input,
-    pub options: Ref<JsBufferValue>,
-    pub babel_transform: Option<JsTrasnform>,
-}
+    let error_format = options.experimental.error_format.unwrap_or_default();
 
-#[napi]
-impl Task for TransformTask {
-    type JsValue = TransformOutput;
-    type Output = TransformOutput;
+    try_with(
+        c.cm.clone(),
+        !options.config.error.filename.into_bool(),
+        error_format,
+        |handler| {
+            c.run(|| {
+                let fm = c.cm.new_source_file(
+                    if options.filename.is_empty() {
+                        FileName::Anon.into()
+                    } else {
+                        FileName::Real(options.filename.clone().into()).into()
+                    },
+                    input.code,
+                );
 
-    #[instrument(level = "trace", skip_all)]
-    fn compute(&mut self) -> napi::Result<Self::Output> {
-        let mut options: Options = serde_json::from_slice(self.options.as_ref())?;
-        if !options.filename.is_empty() {
-            options.config.adjust(Path::new(&options.filename));
-        }
+                let output = c.process_js_file(fm, handler, &options)?;
 
-        let error_format = options.experimental.error_format.unwrap_or_default();
-
-        try_with(
-            self.c.cm.clone(),
-            !options.config.error.filename.into_bool(),
-            error_format,
-            |handler| {
-                self.c.run(|| match &self.input {
-                    Input::File(ref path) => {
-                        let fm = self.c.cm.load_file(path).context("failed to load file")?;
-                        self.c.process_js_file(fm, handler, &options)
-                    }
-
-                    Input::Source { src } => {
-                        let fm = self.c.cm.new_source_file(
-                            if options.filename.is_empty() {
-                                FileName::Anon.into()
-                            } else {
-                                FileName::Real(options.filename.clone().into()).into()
-                            },
-                            src.to_string(),
-                        );
-
-                        self.c.process_js_file(fm, handler, &options)
-                    }
+                Ok(TransformOutput {
+                    code: output.code,
+                    map: output.map,
                 })
-            },
-        )
-        .convert_err()
-    }
-
-    fn resolve(&mut self, _env: Env, result: Self::Output) -> napi::Result<Self::JsValue> {
-        Ok(result)
-    }
-
-    fn finally(&mut self, env: Env) -> napi::Result<()> {
-        self.options.unref(env)?;
-        Ok(())
-    }
+            })
+        },
+    )
+    .convert_err()
 }
 
 #[napi]
@@ -92,22 +48,47 @@ impl Task for TransformTask {
 pub fn transform_3_times(
     env: Env,
     src: String,
-    options: JsBuffer,
-    babel_transform: Option<JsFunction>,
-    signal: Option<AbortSignal>,
-) -> napi::Result<AsyncTask<TransformTask>> {
-    let c = get_compiler();
+    first_options: JsBuffer,
+    babel_transform: JsFunction,
+    third_options: JsBuffer,
+) -> napi::Result<JsObject> {
+    let first_options = first_options.into_ref()?;
+    let third_options = third_options.into_ref()?;
+    let babel_hook = JsTrasnform::new(&env, &babel_transform)?;
 
-    let input = Input::Source { src };
+    env.spawn_future(async move {
+        let input = TransformOutput {
+            code: src,
+            map: None,
+        };
 
-    let task = TransformTask {
-        c,
-        input,
-        options: options.into_ref()?,
-        babel_transform: match babel_transform {
-            Some(f) => Some(JsTrasnform::new(&env, &f)?),
-            None => None,
-        },
-    };
-    Ok(AsyncTask::with_optional_signal(task, signal))
+        let output = apply_swc(input, first_options)?;
+
+        let output = babel_hook.transform(output).await?;
+
+        apply_swc(output, third_options)
+    })
+}
+
+#[napi]
+#[instrument(level = "trace", skip_all)]
+pub fn transform_2_times(
+    env: Env,
+    src: String,
+    first_options: JsBuffer,
+    babel_transform: JsFunction,
+) -> napi::Result<JsObject> {
+    let first_options = first_options.into_ref()?;
+    let babel_hook = JsTrasnform::new(&env, &babel_transform)?;
+
+    env.spawn_future(async move {
+        let input = TransformOutput {
+            code: src,
+            map: None,
+        };
+
+        let output = apply_swc(input, first_options)?;
+
+        babel_hook.transform(output).await
+    })
 }
