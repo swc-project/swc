3fed0e747edfa1e7785241543db77b71c6ff5f98
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-30 09:46:24 +0900

    index

--- packages/js-interop/index.ts
+++ packages/js-interop/index.ts
@@ -1,75 +1,18 @@
 import * as binding from "./binding";
+import { TransformOutput } from "./binding";
 
-export type MinifierType = "js-module" | "js-script" | "json" | "css" | "html";
+interface Options {
 
-export type Options = {
-    filename?: string;
-    iframeSrcdoc?: boolean;
-    scriptingEnabled?: boolean;
-    forceSetHtml5Doctype?: boolean;
-    collapseWhitespaces?:
-        | "none"
-        | "all"
-        | "smart"
-        | "conservative"
-        | "advanced-conservative"
-        | "only-metadata";
-    removeEmptyMetadataElements?: boolean;
-    removeComments?: boolean;
-    preserveComments?: string[];
-    minifyConditionalComments?: boolean;
-    removeEmptyAttributes?: boolean;
-    removeRedundantAttributes?: "none" | "all" | "smart";
-    collapseBooleanAttributes?: boolean;
-    normalizeAttributes?: boolean;
-    minifyJson?: boolean | { pretty?: boolean };
-    // TODO improve me after typing `@swc/css`
-    minifyJs?: boolean | { parser?: any; minifier?: any; codegen?: any };
-    minifyCss?:
-        | boolean
-        | { lib: "lightningcss" }
-        | { lib: "swc"; parser?: any; minifier?: any; codegen?: any };
-    minifyAdditionalScriptsContent?: [string, MinifierType][];
-    minifyAdditionalAttributes?: [string, MinifierType][];
-    sortSpaceSeparatedAttributeValues?: boolean;
-    sortAttributes?: boolean;
-    tagOmission?: boolean;
-    selfClosingVoidElements?: boolean;
-    quotes?: boolean;
-};
-
-export type FragmentOptions = Options & {
-    mode?: "no-quirks" | "limited-quirks" | "quirks";
-    context_element?: binding.Element;
-    form_element?: binding.Element;
-};
-
-export async function minify(
-    content: string | Buffer,
-    options?: Options
-): Promise<binding.TransformOutput> {
-    return binding.minify(content, toBuffer(options ?? {}));
 }
 
-export async function minifyFragment(
-    content: string | Buffer,
-    options?: FragmentOptions
-): Promise<binding.TransformOutput> {
-    return binding.minifyFragment(content, toBuffer(options ?? {}));
-}
+type BabelTransform = (input: TransformOutput) => Promise<TransformOutput>;
 
-export function minifySync(
-    content: string | Buffer,
-    options?: Options
-): binding.TransformOutput {
-    return binding.minifySync(content, toBuffer(options ?? {}));
-}
-
-export async function minifyFragmentSync(
-    content: string | Buffer,
-    options?: FragmentOptions
+export async function transform3Times(
+    content: string,
+    options?: Options,
+    babel_transform?: BabelTransform
 ): Promise<binding.TransformOutput> {
-    return binding.minifyFragmentSync(content, toBuffer(options ?? {}));
+    return binding.transform3Times(content, toBuffer(options ?? {}), babel_transform);
 }
 
 function toBuffer(t: any): Buffer {
