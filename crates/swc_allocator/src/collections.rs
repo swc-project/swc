eaf25f97224874030304902b980d36ca1722a8d7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-01 10:01:09 +0900

    Patch `swc_allocator`

--- crates/swc_allocator/src/collections.rs
+++ crates/swc_allocator/src/collections.rs
@@ -1,9 +1,5 @@
 //! Various collections.
 
-use std::hash::BuildHasherDefault;
-
-use rustc_hash::FxHasher;
-
 #[cfg(feature = "nightly")]
 use crate::FastAlloc;
 
@@ -22,15 +18,6 @@ pub type HashMap<K, V, S> = hashbrown::HashMap<K, V, S>;
 #[cfg(not(feature = "nightly"))]
 /// See [std::collections::HashSet].
 pub type HashSet<T, S> = hashbrown::HashSet<T, S>;
-
-/// Used for `FxHashMap` and `FxHashSet`.
-pub type FxBuildHasher = BuildHasherDefault<FxHasher>;
-
-/// Faster `HashMap` which uses `FxHasher`.
-pub type FxHashMap<K, V> = HashMap<K, V, FxBuildHasher>;
-
-/// Faster `HashSet` which uses `FxHasher`.
-pub type FxHashSet<T> = HashSet<T, FxBuildHasher>;
 
 /// Re-export for convenience.
 pub use hashbrown::hash_map;
