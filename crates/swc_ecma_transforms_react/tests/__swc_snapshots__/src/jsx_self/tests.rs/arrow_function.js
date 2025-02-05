83f24afad9114801c897d04bfa7a1525c92686c1
CPunisher <1343316114@qq.com>
2025-02-05 11:02:23 +0800

    fix(es/react): Avoid adding `__self` in constructors of derived class in the `jsx_self` (#9987)
    **Description:**
    
    1. port babel test:
    https://github.com/babel/babel/tree/main/packages/babel-plugin-transform-react-jsx-self/test/fixtures/react-source
    2. check if the jsx is in a constructor of a derived class:
    https://github.com/babel/babel/blob/main/packages/babel-plugin-transform-react-jsx-self/src/index.ts#L77
    
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/9904

---
+++ crates/swc_ecma_transforms_react/tests/__swc_snapshots__/src/jsx_self/tests.rs/arrow_function.js
@@ -0,0 +1,12 @@
+var _this = this;
+<div __self={this}/>;
+(function() {
+    return <div __self={_this}/>;
+});
+function fn() {
+    var _this = this;
+    <div __self={this}/>;
+    (function() {
+        return <div __self={_this}/>;
+    });
+}
