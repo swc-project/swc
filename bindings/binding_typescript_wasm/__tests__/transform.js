37672e024e340b1509f4d8f70414bc132a8337bf
magic-akari <akari.ccino@gmail.com>
2025-02-17 15:09:56 +0800

    test(ts/fast-strip): Add tests for `declare module` error cases (#10040)
    **Related issue:**
    - https://github.com/nodejs/amaro/pull/174

--- bindings/binding_typescript_wasm/__tests__/transform.js
+++ bindings/binding_typescript_wasm/__tests__/transform.js
@@ -132,6 +132,15 @@ describe("transform", () => {
             ).rejects.toMatchSnapshot();
         });
 
+        it("should throw an error when it encounters a module", async () => {
+            await expect(
+                swc.transform("declare module foo { }", {
+                    mode: "strip-only",
+                    deprecatedTsModuleAsError: true,
+                }),
+            ).rejects.toMatchSnapshot();
+        });
+
         it("should not emit 'Caused by: failed to parse'", async () => {
             await expect(
                 swc.transform("function foo() { await Promise.resolve(1); }", {
@@ -166,6 +175,15 @@ describe("transform", () => {
         it("should throw an error when it encounters a module", async () => {
             await expect(
                 swc.transform("module foo { }", {
+                    mode: "transform",
+                    deprecatedTsModuleAsError: true,
+                }),
+            ).rejects.toMatchSnapshot();
+        });
+
+        it("should throw an error when it encounters a declared module", async () => {
+            await expect(
+                swc.transform("declare module foo { }", {
                     mode: "transform",
                     deprecatedTsModuleAsError: true,
                 }),
