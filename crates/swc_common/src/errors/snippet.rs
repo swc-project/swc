1d6c0d7aca68d5e60079b92df1ba5677d67e973a
GiveMe-A-Name <a455300764b@hotmail.com>
2025-02-17 16:51:01 +0800

    fix: reset code

--- crates/swc_common/src/errors/snippet.rs
+++ crates/swc_common/src/errors/snippet.rs
@@ -177,12 +177,12 @@ pub struct StyledString {
 #[allow(clippy::enum_variant_names)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(
-    any(feature = "rkyv-impl"),
-    derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)
-)]
-#[cfg_attr(
     feature = "diagnostic-serde",
     derive(serde::Serialize, serde::Deserialize)
+)]
+#[cfg_attr(
+    any(feature = "rkyv-impl"),
+    derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)
 )]
 #[cfg_attr(feature = "rkyv-impl", derive(bytecheck::CheckBytes))]
 #[cfg_attr(feature = "rkyv-impl", repr(u32))]
