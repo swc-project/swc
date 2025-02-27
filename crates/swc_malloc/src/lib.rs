e2f0e997da46c053a25bb04482337a27e92707a1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-27 14:59:40 +0900

    global alloc

--- crates/swc_malloc/src/lib.rs
+++ crates/swc_malloc/src/lib.rs
@@ -2,14 +2,6 @@
 //!
 //! The swc crates related to the  node binding should depend on this crate.
 
-#[cfg(not(target_os = "linux"))]
+#[cfg(not(any(target_family = "wasm", target_env = "musl")))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
-#[cfg(all(
-    target_os = "linux",
-    target_env = "gnu",
-    any(target_arch = "x86_64", target_arch = "aarch64")
-))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
