61d7b7faeda8705039da85354eeedcd4e78a1ef3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-27 20:02:17 +0900

    Update test refs

--- crates/swc/tests/tsc-references/multipleDefaultExports04.1.normal.js
+++ crates/swc/tests/tsc-references/multipleDefaultExports04.1.normal.js
@@ -1,17 +1,4 @@
 //// [multipleDefaultExports04.ts]
-//!   x the name `f` is defined multiple times
-//!    ,-[2:1]
-//!  1 | 
-//!  2 | export default function f() {
-//!    :                         |
-//!    :                         `-- previous definition of `f` here
-//!  3 | }
-//!  4 | 
-//!  5 | export default function f() {
-//!    :                         |
-//!    :                         `-- `f` redefined here
-//!  6 | }
-//!    `----
 //!   x the name `default` is exported multiple times
 //!    ,-[2:1]
 //!  1 |     
@@ -25,3 +12,16 @@
 //!    `----
 //! 
 //! Advice:   > Exported identifiers must be unique
+//!   x the name `f` is defined multiple times
+//!    ,-[2:1]
+//!  1 | 
+//!  2 | export default function f() {
+//!    :                         |
+//!    :                         `-- previous definition of `f` here
+//!  3 | }
+//!  4 | 
+//!  5 | export default function f() {
+//!    :                         |
+//!    :                         `-- `f` redefined here
+//!  6 | }
+//!    `----
