9db2fac7ec01a7277cfe5a7d1394cdcda16beb72
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-27 14:51:31 +0900

    Update test refs

--- crates/swc/tests/tsc-references/noPropertyAccessFromIndexSignature1.2.minified.js
+++ crates/swc/tests/tsc-references/noPropertyAccessFromIndexSignature1.2.minified.js
@@ -1,3 +1,2 @@
 //// [noPropertyAccessFromIndexSignature1.ts]
-var _d, _d1, _d2, _d3;
-a.foo, a.foo, b.foo, b.foo, c.foo, c.foo, c.bar, c.bar, null === (_d = d) || void 0 === _d || _d.foo, null === (_d1 = d) || void 0 === _d1 || _d1.foo, null === (_d2 = d) || void 0 === _d2 || _d2.bar, null === (_d3 = d) || void 0 === _d3 || _d3.bar;
+a.foo, a.foo, b.foo, b.foo, c.foo, c.foo, c.bar, c.bar, null == d || d.foo, null == d || d.foo, null == d || d.bar, null == d || d.bar;
--- crates/swc/tests/tsc-references/optionalChainingInference.2.minified.js
+++ crates/swc/tests/tsc-references/optionalChainingInference.2.minified.js
@@ -1,19 +1,18 @@
 //// [optionalChainingInference.ts]
-var _su, _su1, _su2, _fnu, _su3, _osu, _osu1, _ofnu;
 unbox({
-    value: null === (_su = su) || void 0 === _su ? void 0 : _su.length
+    value: null == su ? void 0 : su.length
 }), unbox({
-    value: null === (_su1 = su) || void 0 === _su1 ? void 0 : _su1.length
+    value: null == su ? void 0 : su.length
 }), unbox({
-    value: null === (_su2 = su) || void 0 === _su2 ? void 0 : _su2.length
+    value: null == su ? void 0 : su.length
 }), unbox({
-    value: null === (_fnu = fnu) || void 0 === _fnu ? void 0 : _fnu()
+    value: null == fnu ? void 0 : fnu()
 }), unbox({
-    value: null === (_su3 = su) || void 0 === _su3 ? void 0 : _su3.length
+    value: null == su ? void 0 : su.length
 }), unbox({
-    value: null === (_osu = osu) || void 0 === _osu ? void 0 : _osu.prop.length
+    value: null == osu ? void 0 : osu.prop.length
 }), unbox({
-    value: null === (_osu1 = osu) || void 0 === _osu1 ? void 0 : _osu1.prop.length
+    value: null == osu ? void 0 : osu.prop.length
 }), unbox({
-    value: null === (_ofnu = ofnu) || void 0 === _ofnu ? void 0 : _ofnu.prop()
+    value: null == ofnu ? void 0 : ofnu.prop()
 });
