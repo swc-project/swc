9fce3a63418a81c2c9bec39c6710bdc291e749cf
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-02-27 16:09:29 +0900

    jemalloc

--- crates/swc_malloc/src/lib.rs
+++ crates/swc_malloc/src/lib.rs
@@ -2,6 +2,17 @@
 //!
 //! The swc crates related to the  node binding should depend on this crate.
 
-#[cfg(not(any(target_family = "wasm", target_env = "musl")))]
+#[cfg(not(any(
+    target_family = "wasm",
+    target_env = "musl",
+    all(target_os = "linux", target_env = "gnu", target_arch = "aarch64")
+)))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+// On linux aarch64, mimalloc fails to build.
+// So we use tikv-jemallocator instead.
+
+#[cfg(all(target_os = "linux", target_env = "gnu", target_arch = "aarch64"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
