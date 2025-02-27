207a13f2ddc12503174a71c5cf0fc65fdf96c906
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-02-27 16:46:14 +0900

    fix(swc_malloc): Fix build issue due to malloc, really (#10117)
    **Description:**
    
    - `cargo tree --target armv7-unknown-linux-gnueabihf -p swc_malloc -e features`:
    
    ```
    swc_malloc v1.2.0 (/Users/kdy1/projects/s/minifier/crates/swc_malloc)
    ├── tikv-jemallocator feature "default"
    │   ├── tikv-jemallocator v0.5.4
    │   │   ├── libc v0.2.162
    │   │   └── tikv-jemalloc-sys v0.5.4+5.3.0-patched
    │   │       └── libc v0.2.162
    │   │       [build-dependencies]
    │   │       └── cc feature "default"
    │   │           └── cc v1.2.13
    │   │               └── shlex feature "default"
    │   │                   ├── shlex v1.3.0
    │   │                   └── shlex feature "std"
    │   │                       └── shlex v1.3.0
    │   └── tikv-jemallocator feature "background_threads_runtime_support"
    │       ├── tikv-jemallocator v0.5.4 (*)
    │       └── tikv-jemalloc-sys feature "background_threads_runtime_support"
    │           └── tikv-jemalloc-sys v0.5.4+5.3.0-patched (*)
    └── tikv-jemallocator feature "disable_initial_exec_tls"
        ├── tikv-jemallocator v0.5.4 (*)
        └── tikv-jemalloc-sys feature "disable_initial_exec_tls"
            └── tikv-jemalloc-sys v0.5.4+5.3.0-patched (*)
    ```
    
    - `cargo tree --target aarch64-unknown-linux-gnu -p swc_malloc -e features`:
    
    ```
    swc_malloc v1.2.0 (/Users/kdy1/projects/s/minifier/crates/swc_malloc)
    ├── tikv-jemallocator feature "default"
    │   ├── tikv-jemallocator v0.5.4
    │   │   ├── libc v0.2.162
    │   │   └── tikv-jemalloc-sys v0.5.4+5.3.0-patched
    │   │       └── libc v0.2.162
    │   │       [build-dependencies]
    │   │       └── cc feature "default"
    │   │           └── cc v1.2.13
    │   │               └── shlex feature "default"
    │   │                   ├── shlex v1.3.0
    │   │                   └── shlex feature "std"
    │   │                       └── shlex v1.3.0
    │   └── tikv-jemallocator feature "background_threads_runtime_support"
    │       ├── tikv-jemallocator v0.5.4 (*)
    │       └── tikv-jemalloc-sys feature "background_threads_runtime_support"
    │           └── tikv-jemalloc-sys v0.5.4+5.3.0-patched (*)
    └── tikv-jemallocator feature "disable_initial_exec_tls"
        ├── tikv-jemallocator v0.5.4 (*)
        └── tikv-jemalloc-sys feature "disable_initial_exec_tls"
            └── tikv-jemalloc-sys v0.5.4+5.3.0-patched (*)
    ```

--- crates/swc_malloc/src/lib.rs
+++ crates/swc_malloc/src/lib.rs
@@ -2,17 +2,28 @@
 //!
 //! The swc crates related to the  node binding should depend on this crate.
 
-#[cfg(not(any(
-    target_family = "wasm",
-    target_env = "musl",
-    all(target_os = "linux", target_env = "gnu", target_arch = "aarch64")
-)))]
+#[cfg(any(
+    not(any(
+        target_os = "linux",
+        target_family = "wasm",
+        target_env = "musl",
+        all(target_os = "linux", any(target_arch = "aarch64", target_arch = "arm"))
+    )),
+    all(
+        target_os = "linux",
+        not(any(
+            target_family = "wasm",
+            target_env = "musl",
+            all(target_os = "linux", any(target_arch = "aarch64", target_arch = "arm"))
+        ))
+    )
+))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 // On linux aarch64, mimalloc fails to build.
 // So we use tikv-jemallocator instead.
 
-#[cfg(all(target_os = "linux", target_env = "gnu", target_arch = "aarch64"))]
+#[cfg(all(target_os = "linux", any(target_arch = "aarch64", target_arch = "arm")))]
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
