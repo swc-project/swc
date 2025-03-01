a61f806f85aec5c42eb84828d64904e8763567d6
CPunisher <1343316114@qq.com>
2025-03-01 14:20:06 +0800

    Test

--- crates/swc_ecma_transforms_typescript/tests/__swc_snapshots__/tests/strip.rs/ts_enum_is_mutable_true.js
+++ crates/swc_ecma_transforms_typescript/tests/__swc_snapshots__/tests/strip.rs/ts_enum_is_mutable_true.js
@@ -5,9 +5,7 @@ var D = /*#__PURE__*/ function(D) {
 }(D || {});
 D.A = 5;
 console.log(D.A);
-;
 console.log(1);
-;
 var G = /*#__PURE__*/ function(G) {
     G[G["A"] = 2] = "A";
     return G;
@@ -17,5 +15,4 @@ var H = /*#__PURE__*/ function(H) {
     H[H["A"] = 2] = "A";
     return H;
 }(H || {});
-;
 console.log(2);
---
+++ crates/swc_ecma_transforms_typescript/tests/fixture/issue-10097/output.js
@@ -0,0 +1,14 @@
+export var B = /*#__PURE__*/ function(B) {
+    B[B["B"] = 0] = "B";
+    return B;
+}({});
+{}label: ;
+if (Math.random()) ;
+else ;
+while(Math.random());
+do ;
+while (Math.random())
+for (const _ of Math.random());
+switch(Math.random()){
+    case 1:
+}
