d745678652905be1574b79b441bac7525cf64419
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-04 17:34:49 +0900

    WIP

--- crates/swc_error_reporters/src/json_emitter.rs
+++ crates/swc_error_reporters/src/json_emitter.rs
@@ -1,14 +1,18 @@
-use miette::GraphicalReportHandler;
-use swc_common::{sync::Lrc, SourceMap};
+use std::{f128::consts::E, fmt::Write};
 
-use crate::WriterWrapper;
+use serde_derive::Serialize;
+use swc_common::{
+    errors::{DiagnosticBuilder, Emitter, Level},
+    sync::Lrc,
+    SourceMap,
+};
+
+use crate::{MietteDiagnostic, MietteSourceCode, MietteSubdiagnostic, WriterWrapper};
 
 pub struct JsonEmitter {
     cm: Lrc<SourceMap>,
 
     wr: WriterWrapper,
-
-    reporter: GraphicalReportHandler,
 
     config: JsonEmitterConfig,
 
@@ -18,4 +22,71 @@ pub struct JsonEmitter {
 #[derive(Debug, Clone, Default)]
 pub struct JsonEmitterConfig {
     pub skip_filename: bool,
+}
+
+impl Emitter for JsonEmitter {
+    fn emit(&mut self, db: &DiagnosticBuilder) {
+        let d = &**db;
+
+        let children = d
+            .children
+            .iter()
+            .map(|d| JsonSubdiagnostic {
+                code: &d.code,
+                message: &d.message,
+                snippet: d.snippet,
+                filename: d.filename(),
+                line: d.line(),
+            })
+            .collect::<Vec<_>>();
+
+        let error = JsonDiagnostic {
+            code: &d.code,
+            message: &d.message,
+            filename: d.filename(),
+            line: d.line(),
+            column: d.column(),
+            children,
+        };
+
+        let result = serde_json::to_string(&mut error).unwrap();
+
+        self.wr.write_str(&result).unwrap();
+
+        self.diagnostics.push(result);
+    }
+
+    fn take_diagnostics(&mut self) -> Vec<String> {
+        std::mem::take(&mut self.diagnostics)
+    }
+}
+
+#[derive(Serialize)]
+struct JsonDiagnostic<'a> {
+    /// Error code
+    code: &'a str,
+    message: &'a str,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    snippet: Option<&'a str>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    filename: Option<&'a str>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    line: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    column: Option<usize>,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    children: Vec<JsonSubdiagnostic<'a>>,
+}
+
+#[derive(Serialize)]
+struct JsonSubdiagnostic<'a> {
+    code: &'a str,
+    message: &'a str,
+    snippet: &'a str,
+    filename: &'a str,
+    line: usize,
 }
