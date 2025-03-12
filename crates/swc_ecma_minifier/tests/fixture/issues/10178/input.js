caf84604d709651ce63ee8174988e54310d47e5b
austaras <austaras@outlook.com>
2025-03-12 16:20:45 +0800

    fix(es/minifier): Check array inline for indexed with dynamic key

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/10178/input.js
@@ -0,0 +1,6 @@
+//// [indexerWithTuple.ts]
+var strNumTuple = ["foo", 10],
+    numTupleTuple = [10, ["bar", 20]],
+    unionTuple1 = [10, "foo"],
+    unionTuple2 = [!0, "foo"];
+strNumTuple[0], strNumTuple["0"];
