4348ddf74ad3ff0f9fb689c43a37baa63ecfcf46
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-13 12:12:24 +0900

    Init

--- crates/swc_ecma_fast_parser/src/parser/mod.rs
+++ crates/swc_ecma_fast_parser/src/parser/mod.rs
@@ -1,1 +1,264 @@
+//! High-performance parser for ECMAScript/TypeScript
+//!
+//! This parser is designed for maximum performance and memory efficiency,
+//! operating at the token level for optimal throughput.
 
+#![allow(clippy::redundant_closure_call)]
+
+mod expr;
+mod pat;
+mod stmt;
+mod util;
+
+use std::rc::Rc;
+
+use swc_atoms::Atom;
+use swc_common::{BytePos, Span, DUMMY_SP};
+use swc_ecma_ast::{
+    ArrayLit, ArrayPat, ArrowExpr, AssignExpr, AssignOp, AssignPat, AssignPatProp, AssignProp,
+    AssignTarget, AwaitExpr, BinExpr, BindingIdent, BlockStmt, BlockStmtOrExpr, Bool, CallExpr,
+    Callee, Class, ClassDecl, ClassExpr, ClassMember, ComputedPropName, CondExpr, Constructor,
+    Decl, DoWhileStmt, ExportAll, ExportDecl, ExportDefaultDecl, ExportDefaultExpr,
+    ExportDefaultSpecifier, ExportNamedSpecifier, ExportNamespaceSpecifier, ExportSpecifier, Expr,
+    ExprOrSpread, ExprStmt, FnDecl, FnExpr, ForHead, ForInStmt, ForOfStmt, ForStmt, Function,
+    GetterProp, Id, Ident, IfStmt, Import, ImportDecl, ImportDefaultSpecifier,
+    ImportNamedSpecifier, ImportPhase, ImportSpecifier, ImportStarAsSpecifier, Invalid, JSXAttr,
+    JSXAttrName, JSXAttrOrSpread, JSXAttrValue, JSXClosingElement, JSXClosingFragment, JSXElement,
+    JSXElementChild, JSXElementName, JSXEmptyExpr, JSXExpr, JSXExprContainer, JSXFragment,
+    JSXMemberExpr, JSXNamespacedName, JSXObject, JSXOpeningElement, JSXOpeningFragment,
+    JSXSpreadChild, JSXText, KeyValuePatProp, KeyValueProp, Lit, MemberExpr, MemberProp,
+    MetaPropExpr, MetaPropKind, MethodKind, MethodProp, Module, ModuleDecl, ModuleExportName,
+    ModuleItem, NamedExport, NewExpr, Null, Number, ObjectLit, ObjectPat, ObjectPatProp, OptCall,
+    OptChainBase, OptChainExpr, Param, ParenExpr, Pat, PrivateMethod, PrivateName, PrivateProp,
+    Program, Prop, PropName, PropOrSpread, RestPat, ReturnStmt, Script, SeqExpr, SetterProp,
+    SpreadElement, Stmt, Str, Super, SuperProp, SuperPropExpr, SwitchCase, SwitchStmt, TaggedTpl,
+    ThisExpr, ThrowStmt, Tpl, TplElement, TryStmt, UnaryExpr, UnaryOp, UpdateExpr, UpdateOp,
+    VarDecl, VarDeclKind, VarDeclOrExpr, VarDeclarator, WhileStmt, WithStmt, YieldExpr,
+};
+
+use crate::{
+    error::{Error, ErrorKind, Result},
+    lexer::Lexer,
+    token::{Token, TokenType, TokenValue},
+    util::{likely, unlikely},
+    JscTarget, SingleThreadedComments, Syntax,
+};
+
+/// High-performance parser for ECMAScript/TypeScript
+///
+/// This parser processes tokens from the lexer to build an AST.
+#[repr(C)] // Ensure predictable memory layout
+pub struct Parser<'a> {
+    /// Lexer instance
+    lexer: Lexer<'a>,
+
+    /// Syntax configuration
+    pub syntax: Syntax,
+
+    /// Target ECMAScript version
+    pub target: JscTarget,
+
+    /// Whether the parser is in strict mode
+    pub strict_mode: bool,
+
+    /// Whether the parser is in module mode
+    pub is_module: bool,
+
+    /// Whether the parser is in a function context
+    pub in_function: bool,
+
+    /// Whether the parser is in a generator function
+    pub in_generator: bool,
+
+    /// Whether the parser is in an async function
+    pub in_async: bool,
+
+    /// Whether the parser is in a loop
+    pub in_loop: bool,
+
+    /// Whether the parser is in a switch statement
+    pub in_switch: bool,
+
+    /// Whether the parser is in a class context
+    pub in_class: bool,
+
+    /// Whether the parser is in a static block
+    pub in_static_block: bool,
+
+    /// Label set for break/continue statements
+    pub labels: Vec<Atom>,
+
+    /// Comments storage
+    pub comments: Option<Rc<SingleThreadedComments>>,
+}
+
+impl<'a> Parser<'a> {
+    /// Create a new parser from a string input
+    #[inline]
+    pub fn new(
+        input: &'a str,
+        target: JscTarget,
+        syntax: Syntax,
+        is_module: bool,
+        comments: Option<Rc<SingleThreadedComments>>,
+    ) -> Self {
+        let lexer = Lexer::new(input, target, syntax, comments.clone());
+
+        Self {
+            lexer,
+            syntax,
+            target,
+            strict_mode: is_module, // Modules are always in strict mode
+            is_module,
+            in_function: false,
+            in_generator: false,
+            in_async: false,
+            in_loop: false,
+            in_switch: false,
+            in_class: false,
+            in_static_block: false,
+            labels: Vec::new(),
+            comments,
+        }
+    }
+
+    /// Parse a complete program (script or module)
+    pub fn parse_program(&mut self) -> Result<Program> {
+        if self.is_module {
+            let module = self.parse_module()?;
+            Ok(Program::Module(module))
+        } else {
+            let script = self.parse_script()?;
+            Ok(Program::Script(script))
+        }
+    }
+
+    /// Parse a script
+    fn parse_script(&mut self) -> Result<Script> {
+        let span_start = self.current_span().lo;
+        let body = self.parse_statements(false)?;
+        let span = Span::new(span_start, self.current_span().hi);
+
+        Ok(Script {
+            span,
+            body,
+            shebang: None, // TODO: Handle shebang
+        })
+    }
+
+    /// Parse a module
+    fn parse_module(&mut self) -> Result<Module> {
+        let span_start = self.current_span().lo;
+        let body = self.parse_module_items()?;
+        let span = Span::new(span_start, self.current_span().hi);
+
+        Ok(Module {
+            span,
+            body,
+            shebang: None, // TODO: Handle shebang
+        })
+    }
+
+    /// Parse statements until end of input or closing brace
+    fn parse_statements(&mut self, is_top_level: bool) -> Result<Vec<Stmt>> {
+        let mut stmts = Vec::new();
+
+        while !self.is(TokenType::EOF) && !self.is(TokenType::RBrace) {
+            let stmt = self.parse_stmt()?;
+            stmts.push(stmt);
+        }
+
+        Ok(stmts)
+    }
+
+    /// Parse module items until end of input
+    fn parse_module_items(&mut self) -> Result<Vec<ModuleItem>> {
+        let mut items = Vec::new();
+
+        while !self.is(TokenType::EOF) {
+            // For now, just parse statements as module items
+            // TODO: Add support for import/export declarations
+            let stmt = self.parse_stmt()?;
+            items.push(ModuleItem::Stmt(stmt));
+        }
+
+        Ok(items)
+    }
+
+    /// Get the current token
+    #[inline(always)]
+    pub fn current(&self) -> &Token {
+        &self.lexer.current
+    }
+
+    /// Get the current token type
+    #[inline(always)]
+    pub fn current_token_type(&self) -> TokenType {
+        self.lexer.current.token_type
+    }
+
+    /// Get the current token span
+    #[inline(always)]
+    pub fn current_span(&self) -> Span {
+        self.lexer.current.span
+    }
+
+    /// Check if the current token is of the specified type
+    #[inline(always)]
+    pub fn is(&self, token_type: TokenType) -> bool {
+        self.current_token_type() == token_type
+    }
+
+    /// Check if the current token is one of the specified types
+    #[inline(always)]
+    pub fn is_one_of(&self, token_types: &[TokenType]) -> bool {
+        token_types.contains(&self.current_token_type())
+    }
+
+    /// Advance to the next token
+    #[inline(always)]
+    pub fn next(&mut self) -> Result<()> {
+        self.lexer.next_token()?;
+        Ok(())
+    }
+
+    /// Expect a specific token type and advance to the next token
+    #[inline]
+    pub fn expect(&mut self, token_type: TokenType) -> Result<()> {
+        if self.is(token_type) {
+            self.next()?;
+            Ok(())
+        } else {
+            let span = self.current_span();
+            Err(Error {
+                kind: ErrorKind::UnexpectedToken {
+                    expected: Some(token_type.as_str()),
+                    got: self.current_token_type().as_str().to_string(),
+                },
+                span,
+            })
+        }
+    }
+
+    /// Consume the current token if it matches the specified type
+    #[inline]
+    pub fn eat(&mut self, token_type: TokenType) -> Result<bool> {
+        if self.is(token_type) {
+            self.next()?;
+            Ok(true)
+        } else {
+            Ok(false)
+        }
+    }
+
+    /// Create an invalid node with the current span
+    #[inline]
+    fn error_node<T>(&self) -> T
+    where
+        T: From<Invalid>,
+    {
+        T::from(Invalid {
+            span: self.current_span(),
+        })
+    }
+}
---
+++ crates/swc_ecma_fast_parser/src/parser/util.rs
@@ -0,0 +1,221 @@
+//! Utility functions for the parser
+
+use swc_atoms::Atom;
+use swc_common::{BytePos, Span, SyntaxContext, DUMMY_SP};
+use swc_ecma_ast::{
+    BindingIdent, Bool, Expr, Ident, IdentName, Lit, Number, Pat, Str, ThrowStmt, TryStmt,
+};
+
+use crate::{
+    error::{Error, ErrorKind},
+    token::{Token, TokenType, TokenValue},
+};
+
+/// Trait for getting spans from AST nodes
+pub trait GetSpan {
+    fn span(&self) -> Span;
+}
+
+// Implement GetSpan for Box<Expr>
+impl GetSpan for Box<Expr> {
+    fn span(&self) -> Span {
+        match &**self {
+            Expr::Array(e) => e.span,
+            Expr::Arrow(e) => e.span,
+            Expr::Assign(e) => e.span,
+            Expr::Await(e) => e.span,
+            Expr::Bin(e) => e.span,
+            Expr::Call(e) => e.span,
+            Expr::Class(e) => {
+                if let Some(ident) = &e.ident {
+                    ident.span
+                } else {
+                    e.class.span
+                }
+            }
+            Expr::Cond(e) => e.span,
+            Expr::Fn(e) => {
+                if let Some(ident) = &e.ident {
+                    ident.span
+                } else {
+                    e.function.span
+                }
+            }
+            Expr::Ident(e) => e.span,
+            Expr::Lit(e) => match e {
+                swc_ecma_ast::Lit::Str(s) => s.span,
+                swc_ecma_ast::Lit::Bool(b) => b.span,
+                swc_ecma_ast::Lit::Null(n) => n.span,
+                swc_ecma_ast::Lit::Num(n) => n.span,
+                swc_ecma_ast::Lit::BigInt(b) => b.span,
+                swc_ecma_ast::Lit::Regex(r) => r.span,
+                swc_ecma_ast::Lit::JSXText(j) => j.span,
+            },
+            Expr::Member(e) => e.span,
+            Expr::MetaProp(e) => e.span,
+            Expr::New(e) => e.span,
+            Expr::Object(e) => e.span,
+            Expr::OptChain(e) => e.span,
+            Expr::Paren(e) => e.span,
+            Expr::PrivateName(e) => e.span,
+            Expr::Seq(e) => e.span,
+            Expr::SuperProp(e) => e.span,
+            Expr::TaggedTpl(e) => e.span,
+            Expr::This(e) => e.span,
+            Expr::Tpl(e) => e.span,
+            Expr::Unary(e) => e.span,
+            Expr::Update(e) => e.span,
+            Expr::Yield(e) => e.span,
+            Expr::JSXMember(e) => e.span,
+            Expr::JSXNamespacedName(e) => e.span,
+            Expr::JSXEmpty(e) => e.span,
+            Expr::JSXElement(e) => e.span,
+            Expr::JSXFragment(e) => e.span,
+            Expr::TsTypeAssertion(e) => e.span,
+            Expr::TsConstAssertion(e) => e.span,
+            Expr::TsNonNull(e) => e.span,
+            Expr::TsAs(e) => e.span,
+            Expr::TsInstantiation(e) => e.span,
+            Expr::TsSatisfies(e) => e.span,
+            Expr::Invalid(e) => e.span,
+        }
+    }
+}
+
+// Implement GetSpan for Pat
+impl GetSpan for Pat {
+    fn span(&self) -> Span {
+        match self {
+            Pat::Ident(i) => i.id.span,
+            Pat::Array(a) => a.span,
+            Pat::Rest(r) => r.span,
+            Pat::Object(o) => o.span,
+            Pat::Assign(a) => a.span,
+            Pat::Invalid(i) => i.span,
+            Pat::Expr(e) => e.span(),
+        }
+    }
+}
+
+// Implement GetSpan for BindingIdent
+impl GetSpan for BindingIdent {
+    fn span(&self) -> Span {
+        self.id.span
+    }
+}
+
+/// Convert a token value to an identifier
+pub fn token_value_to_ident(token: &Token, span: Span) -> Ident {
+    if let TokenValue::Str { value, .. } = &token.value {
+        Ident {
+            span,
+            sym: value.clone(),
+            optional: false,
+            ctxt: SyntaxContext::empty(),
+        }
+    } else {
+        // Default to empty identifier if token is not a string
+        Ident {
+            span,
+            sym: Atom::from(""),
+            optional: false,
+            ctxt: SyntaxContext::empty(),
+        }
+    }
+}
+
+/// Convert a token value to a binding identifier
+pub fn token_value_to_binding_ident(token: &Token, span: Span) -> BindingIdent {
+    BindingIdent {
+        id: token_value_to_ident(token, span),
+        type_ann: None,
+    }
+}
+
+/// Convert a token value to a string literal
+pub fn token_value_to_str(token: &Token, span: Span) -> Str {
+    if let TokenValue::Str { value, raw } = &token.value {
+        Str {
+            span,
+            value: value.clone(),
+            raw: Some(raw.clone()),
+        }
+    } else {
+        // Default to empty string if token is not a string
+        Str {
+            span,
+            value: Atom::from(""),
+            raw: None,
+        }
+    }
+}
+
+/// Convert a token value to a number literal
+pub fn token_value_to_number(token: &Token, span: Span) -> Number {
+    if let TokenValue::Num { value, raw } = &token.value {
+        Number {
+            span,
+            value: *value,
+            raw: Some(raw.clone()),
+        }
+    } else {
+        // Default to 0 if token is not a number
+        Number {
+            span,
+            value: 0.0,
+            raw: None,
+        }
+    }
+}
+
+/// Check if a token type is a binary operator
+pub fn is_binary_operator(token_type: TokenType) -> bool {
+    matches!(
+        token_type,
+        TokenType::EqEq
+            | TokenType::NotEq
+            | TokenType::EqEqEq
+            | TokenType::NotEqEq
+            | TokenType::Lt
+            | TokenType::LtEq
+            | TokenType::Gt
+            | TokenType::GtEq
+            | TokenType::LShift
+            | TokenType::RShift
+            | TokenType::ZeroFillRShift
+            | TokenType::Plus
+            | TokenType::Minus
+            | TokenType::Asterisk
+            | TokenType::Slash
+            | TokenType::Percent
+            | TokenType::Pipe
+            | TokenType::Ampersand
+            | TokenType::Caret
+            | TokenType::Exp
+            | TokenType::LogicalOr
+            | TokenType::LogicalAnd
+            | TokenType::NullishCoalescing
+            | TokenType::In
+            | TokenType::InstanceOf
+    )
+}
+
+/// Create an error for invalid await usage
+pub fn invalid_await_error(span: Span) -> Error {
+    Error {
+        kind: ErrorKind::General {
+            message: "await is only valid in async functions".to_string(),
+        },
+        span,
+    }
+}
+
+/// Create an error for invalid yield usage
+pub fn invalid_yield_error(span: Span) -> Error {
+    Error {
+        kind: ErrorKind::General {
+            message: "yield is only valid in generator functions".to_string(),
+        },
+        span,
+    }
+}
