ae435f8fe589e75022ab3a669e2efd94ebbdd6ea
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-17 14:06:04 +0900

    size in release build

--- crates/swc_ecma_minifier/scripts/test.sh
+++ crates/swc_ecma_minifier/scripts/test.sh
@@ -8,6 +8,7 @@ export SWC_RUN=0
 
 touch tests/compress.rs
 
-cargo test -p swc_ecma_minifier -p swc --no-fail-fast --test size --test projects --test tsc --test compress --test mangle --features concurrent $@
+cargo test -p swc_ecma_minifier -p swc --no-fail-fast --test projects --test tsc --test compress --test mangle --features concurrent $@
+cargo test -p swc_ecma_minifier --release --test size
 
 # find ../swc/tests/ -type f -empty -delete
