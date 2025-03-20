b660f4de34fb28498924cbab16bfcc485c62bf73
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-20 22:32:47 +0900

    build-crate.sh

--- scripts/bench/build-crate.sh
+++ scripts/bench/build-crate.sh
@@ -6,8 +6,13 @@ crate=$1
 
 # If crate has feature 'concurrent', build it with feature
 if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
-    echo "Building $crate with feature 'concurrent'"
-    cargo codspeed build -p $crate --features concurrent
+    if [[ $(./scripts/cargo/list-dependencies.sh $crate) == *"swc_parallel"* ]]; then
+        echo "Building $crate with feature 'chili'"
+        cargo codspeed build -p $crate --features concurrent,swc_parallel/chili
+    else
+        echo "Building $crate with feature 'concurrent'"
+        cargo codspeed build -p $crate --features concurrent
+    fi
 else
     echo "Building $crate"
     cargo codspeed build -p $crate
