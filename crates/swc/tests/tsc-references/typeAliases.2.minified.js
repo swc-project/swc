ff83cc416502816f8c1a66addd3682effca57790
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-20 20:46:14 +0900

    Update test refs

--- crates/swc/tests/tsc-references/typeAliases.2.minified.js
+++ crates/swc/tests/tsc-references/typeAliases.2.minified.js
@@ -2,7 +2,4 @@
 import "@swc/helpers/_/_class_call_check";
 foo13(x13_1, x13_2), foo13(x13_2, x13_1);
 var E, x13_1, x13_2, x, E1 = ((E = E1 || {})[E.x = 10] = "x", E);
-f15(10).toLowerCase(), f16(x), [
-    "1",
-    !1
-][0].toLowerCase();
+f15(10).toLowerCase(), f16(x);
