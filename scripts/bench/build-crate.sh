228e93e3d27e900e3aeb18c18f54d74ec9c08d40
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-20 22:50:37 +0900

    restore scripts

--- scripts/bench/build-crate.sh
+++ scripts/bench/build-crate.sh
@@ -6,13 +6,8 @@ crate=$1
 
 # If crate has feature 'concurrent', build it with feature
 if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
-    if [[ $(./scripts/cargo/list-dependencies.sh $crate) == *"swc_parallel"* ]]; then
-        echo "Building $crate with feature 'chili'"
-        cargo codspeed build -p $crate --features concurrent,swc_parallel/chili
-    else
-        echo "Building $crate with feature 'concurrent'"
-        cargo codspeed build -p $crate --features concurrent
-    fi
+    echo "Building $crate with feature 'concurrent'"
+    cargo codspeed build -p $crate --features concurrent
 else
     echo "Building $crate"
     cargo codspeed build -p $crate
--- scripts/github/test-concurrent.sh
+++ scripts/github/test-concurrent.sh
@@ -2,9 +2,7 @@
 
 set -eu
 
-crate=$1
-
-echo "Checking if '$crate' has feature 'concurrent'"
+echo "Checking if '$1' has feature 'concurrent'"
 
 # Get crates with feature named `concurrent`
 CRATES=$(./scripts/cargo/list-crates.sh | \
@@ -15,10 +13,6 @@ if [[ "swc" == "$1" ]]; then
     exit 0
 fi
 
-if [[ $(./scripts/cargo/list-features.sh $crate) == *"concurrent"* ]]; then
-    if [[ $(./scripts/cargo/list-dependencies.sh $crate) == *"swc_parallel"* ]]; then
-        cargo test --color always -p $crate --all-targets --features concurrent --features swc_parallel/chili
-    else
-        cargo test --color always -p $crate --all-targets --features concurrent
-    fi
+if [[ $CRATES == *"$1"* ]]; then
+    cargo test --color always -p $1 --all-targets --features "$1/concurrent"
 fi
