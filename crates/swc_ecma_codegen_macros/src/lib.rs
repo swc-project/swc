65e657a54fd580e5181f99b2597892abc1c3d328
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-25 14:00:29 +0900

    more macro wokr

--- crates/swc_ecma_codegen_macros/src/lib.rs
+++ crates/swc_ecma_codegen_macros/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro2::TokenStream;
 use swc_macros_common::prelude::*;
-use syn::{fold::Fold, *};
+use syn::*;
 
 mod fold;
 
@@ -18,15 +18,7 @@ pub fn node_impl(
     for i in item.items {
         match i {
             ImplItem::Fn(i) => {
-                let (node_type, mtd_for_emitter, mtd_for_adjuster) = expand_method(i);
-
-                let item: ItemImpl = parse_quote! {
-                    impl crate::Node for #node_type {
-                        #mtd_for_emitter
-
-                        #mtd_for_adjuster
-                    }
-                };
+                let item = expand_method(i);
 
                 output.extend(item.to_token_stream());
             }
@@ -41,76 +33,25 @@ pub fn node_impl(
 }
 
 /// Returns `(emitter_method, adjuster_method)`
-fn expand_method(src: ImplItemFn) -> (Type, ImplItemFn, ImplItemFn) {
+fn expand_method(src: ImplItemFn) -> ItemImpl {
     let method_name = src.sig.ident.clone();
     let block = src.block;
-}
 
-fn expand(i: ImplItemFn) -> ImplItemFn {
-    let mtd_name = i.sig.ident.clone();
-    assert!(
-        format!("{}", i.sig.ident).starts_with("emit_"),
-        "#[emitter] methods should start with `emit_`"
-    );
-    let block = {
-        let node_type = {
-            i.sig
-                .inputs
-                .clone()
-                .into_iter()
-                .nth(1)
-                .and_then(|arg| match arg {
-                    FnArg::Typed(ty) => Some(ty.ty),
-                    _ => None,
-                })
-                .map(|ty| {
-                    // &Ident -> Ident
-                    match *ty {
-                        Type::Reference(TypeReference { elem, .. }) => *elem,
-                        _ => panic!(
-                            "Type of node parameter should be reference but got {}",
-                            ty.into_token_stream()
-                        ),
-                    }
-                })
-                .expect(
-                    "#[emitter] methods should have signature of
-fn (&mut self, node: Node) -> Result;
-    ",
-                )
-        };
-
-        let block = &i.block;
-
-        parse_quote!({
-            impl crate::Node for #node_type {
-                fn emit_with<W, S>(&self, e: &mut crate::Emitter<'_, W, S>) -> Result
-                where
-                    W: crate::text_writer::WriteJs,
-                    S: swc_common::SourceMapper + swc_ecma_ast::SourceMapperExt
-                {
-                    e.#mtd_name(self)
-                }
-
-                fn adjust_span<W, S>(&mut self, wr: &mut crate::SpanWriter<'_, W, S>) -> Result
-                where
-                    W: crate::text_writer::SpannedWriteJs,
-                    S: swc_common::SourceMapper + swc_ecma_ast::SourceMapperExt
-                {
-                    wr.#mtd_name(self)
-                }
-            }
-
-            #block
-
-            // Emitter methods return Result<_, _>
-            // We inject this to avoid writing Ok(()) every time.
-            #[allow(unreachable_code)]
+    parse_quote!(
+        impl crate::Node for #node_type {
+            fn emit_with<W, S>(&self, e: &mut crate::Emitter<'_, W, S>) -> Result
+            where
+                W: crate::text_writer::WriteJs,
+                S: swc_common::SourceMapper + swc_ecma_ast::SourceMapperExt,
             {
-                return Ok(());
             }
-        })
-    };
 
-    ImplItemFn { block, ..i }
+            fn adjust_span<W, S>(&mut self, wr: &mut crate::SpanWriter<'_, W, S>) -> Result
+            where
+                W: crate::text_writer::SpannedWriteJs,
+                S: swc_common::SourceMapper + swc_ecma_ast::SourceMapperExt,
+            {
+            }
+        }
+    )
 }
