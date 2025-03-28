6e3787566eb0926b57c39d16d45db910b261ce3f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-28 16:11:04 +0900

    analyze

--- crates/swc/src/wasm_analysis.rs
+++ crates/swc/src/wasm_analysis.rs
@@ -1,10 +1,14 @@
 use std::sync::Arc;
 
-use anyhow::Error;
-use common::{comments::SingleThreadedComments, errors::Handler, SourceFile};
+use anyhow::{Context, Error};
+use common::{
+    comments::SingleThreadedComments, errors::Handler, plugin::serialized::PluginSerializedBytes,
+    SourceFile,
+};
+use par_iter::iter::{IntoParallelRefIterator, ParallelIterator};
 use serde::Deserialize;
 use swc_config::IsModule;
-use swc_ecma_ast::{EsVersion, Program};
+use swc_ecma_ast::EsVersion;
 use swc_ecma_parser::Syntax;
 
 use crate::{plugin::PluginConfig, Compiler};
@@ -27,6 +31,63 @@ impl Compiler {
                 opts.module,
                 Some(&comments),
             )?;
+
+            let serialized = {
+                let _span = tracing::span!(tracing::Level::INFO, "serialize_program").entered();
+                let program = swc_common::plugin::serialized::VersionedSerializable::new(n);
+                let mut serialized = PluginSerializedBytes::try_serialize(&program)?;
+            };
+
+            let mut result = vec![];
+
+            opts.plugins.par_iter().map(|p| {
+                let plugin_module_bytes = crate::config::PLUGIN_MODULE_CACHE
+                    .inner
+                    .get()
+                    .unwrap()
+                    .lock()
+                    .get(&p.0)
+                    .expect("plugin module should be loaded");
+
+                let plugin_name = plugin_module_bytes.get_module_name().to_string();
+                let runtime = swc_plugin_runner::wasix_runtime::build_wasi_runtime(
+                    crate::config::PLUGIN_MODULE_CACHE
+                        .inner
+                        .get()
+                        .unwrap()
+                        .lock()
+                        .get_fs_cache_root()
+                        .map(|v| std::path::PathBuf::from(v)),
+                );
+                let mut transform_plugin_executor =
+                    swc_plugin_runner::create_plugin_transform_executor(
+                        &self.source_map,
+                        &self.unresolved_mark,
+                        &self.metadata_context,
+                        plugin_module_bytes,
+                        Some(p.1),
+                        runtime,
+                    );
+
+                let span = tracing::span!(
+                    tracing::Level::INFO,
+                    "execute_plugin_runner",
+                    plugin_module = p.0.as_str()
+                )
+                .entered();
+
+                transform_plugin_executor
+                    .transform(&serialized, Some(should_enable_comments_proxy))
+                    .with_context(|| {
+                        format!(
+                            "failed to invoke `{}` as js analysis plugin at {}",
+                            &p.0, plugin_name
+                        )
+                    })?;
+                drop(span);
+
+                Ok(())
+            });
 
             Ok(vec![])
         })
