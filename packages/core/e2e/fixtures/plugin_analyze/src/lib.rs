6eaf168746bcd7ba88271534de3e19b7d025022d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-31 13:27:40 +0900

    plugin

--- packages/core/e2e/fixtures/plugin_analyze/src/lib.rs
+++ packages/core/e2e/fixtures/plugin_analyze/src/lib.rs
@@ -1,35 +1,11 @@
-use swc_plugin::{
-    ast::*, metadata::TransformPluginProgramMetadata, plugin_transform, syntax_pos::DUMMY_SP,
+use swc_core::{
+    ecma::ast::Program,
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata},
 };
-
-struct ConsoleOutputReplacer;
-
-impl VisitMut for ConsoleOutputReplacer {
-    fn visit_mut_call_expr(&mut self, call: &mut CallExpr) {
-        if let Callee::Expr(expr) = &call.callee {
-            if let Expr::Member(MemberExpr { obj, .. }) = &**expr {
-                if let Expr::Ident(ident) = &**obj {
-                    if ident.sym == *"console" {
-                        call.args[0].expr = Box::new(Expr::Lit(Lit::Str(Str {
-                            span: DUMMY_SP,
-                            value: Atom::from("plugin_transform_schema_v1"),
-                            raw: Some(Atom::new("\"plugin_transform_schema_v1\"")),
-                        })));
-                    }
-                }
-            }
-        }
-    }
-}
 
 #[plugin_transform]
 pub fn process(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
-    // Ensure this plugin uses v1 AST struct schema, by compile-time validating
-    // it doesn't have new enum for the testing purpose.
-    match &program {
-        Program::Script(_script) => {}
-        Program::Module(_module) => {}
-    }
+    experimental_emit("test".into(), "test".into());
 
-    program.fold_with(&mut visit_mut_pass(ConsoleOutputReplacer))
+    program
 }
