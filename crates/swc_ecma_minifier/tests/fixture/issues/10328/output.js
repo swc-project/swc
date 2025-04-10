a4ac3b7188c595aa76d0f28c75d302da7fe25ccc
Austaras <austaras@outlook.com>
2025-04-10 09:58:00 +0800

    fix(es/minifier): Preserve block with block scoped declaration (#10335)
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/10328

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/10328/output.js
@@ -0,0 +1,12 @@
+function g(x, v) {
+    if ("a" === x) {
+        const h = i({
+            onCancel: ()=>h()
+        });
+    } else {
+        class A {
+        }
+        console.log(A, A);
+    }
+}
+g("a"), g("b");
