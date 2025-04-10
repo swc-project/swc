451649b79627d1b523872d1cdffb272eacfbb949
magic-akari <akari.ccino@gmail.com>
2025-04-11 01:05:13 +0800

    chore: update test cases

---
+++ crates/swc_ecma_transforms_base/tests/resolver/issue-10331/output.js
@@ -0,0 +1,13 @@
+using data__2 = create();
+console.log(data__2);
+function test__2() {
+    function test2__3() {
+        return data__3;
+    }
+    using data__3 = create();
+    console.log(data__3);
+}
+async function test3__2() {
+    await using data__5 = create();
+    console.log(data__5);
+}
