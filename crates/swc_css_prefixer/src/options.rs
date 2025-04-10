41a5fbb69abc0a7d8dfd484962a29778328b7585
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-10 18:07:47 +0900

    fix defaults

--- crates/swc_css_prefixer/src/options.rs
+++ crates/swc_css_prefixer/src/options.rs
@@ -2,7 +2,7 @@ use preset_env_base::query::Query;
 pub use preset_env_base::query::Targets;
 use serde::Deserialize;
 
-#[derive(Debug, Default, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
 pub struct Options {
@@ -12,4 +12,12 @@ pub struct Options {
 
 fn default_targets() -> Option<Targets> {
     Some(Targets::Query(Query::Single("".into())))
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            env: default_targets(),
+        }
+    }
 }
