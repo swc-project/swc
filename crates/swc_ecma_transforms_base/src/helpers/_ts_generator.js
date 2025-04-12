27001b3224724415e87ce7694926c668c02c0845
magic-akari <akari.ccino@gmail.com>
2025-04-12 19:58:21 +0800

    fix: Use global Iterator.prototype for down-level generators
    - microsoft/TypeScript#59514

--- crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
+++ crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
@@ -1,6 +1,6 @@
 function _ts_generator(thisArg, body) {
-    var f, y, t, g, _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] };
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
+    var f, y, t, _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
