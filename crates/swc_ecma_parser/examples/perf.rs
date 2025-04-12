51a74d011c0a52025001d4a23347730d0365fcde
CPunisher <1343316114@qq.com>
2025-04-12 21:07:06 +0800

    Revert

--- crates/swc_ecma_parser/examples/perf.rs
+++ crates/swc_ecma_parser/examples/perf.rs
@@ -22,30 +22,28 @@ fn main() {
 
         let fm = cm.load_file(entry.path()).unwrap();
 
-        for _ in 0..1000 {
-            let lexer = Lexer::new(
-                Syntax::Typescript(TsSyntax {
-                    no_early_errors: true,
-                    tsx: entry.path().to_string_lossy().ends_with(".tsx"),
-                    ..Default::default()
-                }),
-                Default::default(),
-                StringInput::from(&*fm),
-                None,
-            );
+        let lexer = Lexer::new(
+            Syntax::Typescript(TsSyntax {
+                no_early_errors: true,
+                tsx: entry.path().to_string_lossy().ends_with(".tsx"),
+                ..Default::default()
+            }),
+            Default::default(),
+            StringInput::from(&*fm),
+            None,
+        );
 
-            let mut parser = Parser::new_from(lexer);
+        let mut parser = Parser::new_from(lexer);
 
-            let module = parser.parse_typescript_module();
+        let module = parser.parse_typescript_module();
 
-            if let Ok(module) = &module {
-                module.hash(&mut hasher);
-            }
-
-            let _ = black_box(module);
-
-            cnt += 1;
+        if let Ok(module) = &module {
+            module.hash(&mut hasher);
         }
+
+        let _ = black_box(module);
+
+        cnt += 1;
     }
 
     eprintln!("Parsed {} files", cnt);
