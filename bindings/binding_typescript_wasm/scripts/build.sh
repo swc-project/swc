ca0eb389eb5d46b23bcd1685578e8e762c1a171f
GiveMe-A-Name <a455300764b@hotmail.com>
2025-04-12 15:14:49 +0800

    chore: revert build.sh

--- bindings/binding_typescript_wasm/scripts/build.sh
+++ bindings/binding_typescript_wasm/scripts/build.sh
@@ -3,7 +3,7 @@ set -eux
 
 export CARGO_PROFILE_RELEASE_LTO="fat"
 export CARGO_PROFILE_RELEASE_OPT_LEVEL="z"
-wasm-pack build --out-name wasm  --scope=swc --target nodejs
+wasm-pack build --out-name wasm --release --scope=swc --target nodejs
 ls -al ./pkg
 
 node ./scripts/patch.mjs
