1dcf7270aa50d813918d45b79b255ce5b122ba70
bohan <bohan-zhang@foxmail.com>
2025-04-23 14:44:27 +0800

    chore: common lexer::{Char, CharIter, CharExt}

--- crates/swc_ecma_parser/src/lexer/util.rs
+++ crates/swc_ecma_parser/src/lexer/util.rs
@@ -9,14 +9,13 @@ use swc_common::{
     input::Input,
     BytePos, Span,
 };
-use swc_ecma_ast::Ident;
 use swc_ecma_lexer::common::lexer::{
     comments_buffer::{BufferedComment, BufferedCommentKind},
     whitespace::SkipWhitespace,
 };
 use tracing::warn;
 
-use super::{Char, LexResult, Lexer};
+use super::{LexResult, Lexer};
 use crate::{
     error::{Error, SyntaxError},
     parser::input::Tokens,
@@ -379,66 +378,5 @@ impl Lexer<'_> {
                 });
             }
         }
-    }
-}
-
-/// Implemented for `char`.
-pub trait CharExt: Copy {
-    fn to_char(self) -> Option<char>;
-
-    /// Test whether a given character code starts an identifier.
-    ///
-    /// https://tc39.github.io/ecma262/#prod-IdentifierStart
-    #[inline]
-    fn is_ident_start(self) -> bool {
-        let c = match self.to_char() {
-            Some(c) => c,
-            None => return false,
-        };
-        Ident::is_valid_start(c)
-    }
-
-    /// Test whether a given character is part of an identifier.
-    #[inline]
-    fn is_ident_part(self) -> bool {
-        let c = match self.to_char() {
-            Some(c) => c,
-            None => return false,
-        };
-        Ident::is_valid_continue(c)
-    }
-
-    /// See https://tc39.github.io/ecma262/#sec-line-terminators
-    #[inline]
-    fn is_line_terminator(self) -> bool {
-        let c = match self.to_char() {
-            Some(c) => c,
-            None => return false,
-        };
-        matches!(c, '\r' | '\n' | '\u{2028}' | '\u{2029}')
-    }
-
-    /// See https://tc39.github.io/ecma262/#sec-literals-string-literals
-    #[inline]
-    fn is_line_break(self) -> bool {
-        let c = match self.to_char() {
-            Some(c) => c,
-            None => return false,
-        };
-        matches!(c, '\r' | '\n')
-    }
-}
-
-impl CharExt for Char {
-    #[inline(always)]
-    fn to_char(self) -> Option<char> {
-        char::from_u32(self.0)
-    }
-}
-
-impl CharExt for char {
-    #[inline(always)]
-    fn to_char(self) -> Option<char> {
-        Some(self)
     }
 }
