053a6efa451610dbbcbae20fe1ef0c1d4d7ed0c2
magic-akari <akari.ccino@gmail.com>
2025-04-29 06:31:01 +0800

    chore: update test cases

--- crates/swc_ecma_transforms_typescript/tests/__swc_snapshots__/tests/strip.rs/issue_1235_1.js
+++ crates/swc_ecma_transforms_typescript/tests/__swc_snapshots__/tests/strip.rs/issue_1235_1.js
@@ -1,10 +1,10 @@
 class Service {
     is(a) {
-        return /*#__PURE__*/ _async_to_generator(function*() {
+        return _async_to_generator(function*() {
             return a.toUpperCase() === a;
         })();
     }
 }
-(()=>/*#__PURE__*/ _async_to_generator(function*() {
+(()=>_async_to_generator(function*() {
         yield new Service().is('ABC');
     })())();
