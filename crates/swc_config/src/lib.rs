6b3adc12f80f6c63977409e31c2d829ccd7e899d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-08 12:33:26 -0700

    fix docsrs

--- crates/swc_config/src/lib.rs
+++ crates/swc_config/src/lib.rs
@@ -2,18 +2,18 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[cfg_attr(docsrs, doc(cfg(feature = "file_pattern")))]
-#[cfg(any(feature = "file_pattern", docsrs))]
+#[cfg(feature = "file_pattern")]
 pub mod file_pattern;
 #[cfg_attr(docsrs, doc(cfg(feature = "glob")))]
-#[cfg(any(feature = "glob", docsrs))]
+#[cfg(feature = "glob")]
 pub mod glob;
 pub mod is_module;
 pub mod merge;
 #[cfg_attr(docsrs, doc(cfg(feature = "regex")))]
-#[cfg(any(feature = "regex", docsrs))]
+#[cfg(feature = "regex")]
 pub mod regex;
 
 #[cfg_attr(docsrs, doc(cfg(feature = "sourcemap")))]
-#[cfg(any(feature = "sourcemap", docsrs))]
+#[cfg(feature = "sourcemap")]
 pub mod source_map;
 pub mod types;
