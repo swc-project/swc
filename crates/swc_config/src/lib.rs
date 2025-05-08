ac170dd31e01fe61f5057ab3acbc75aea724c8b1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-08 10:00:14 -0700

    Merge

--- crates/swc_config/src/lib.rs
+++ crates/swc_config/src/lib.rs
@@ -1,13 +1,14 @@
 //! Configuration for swc
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
-pub mod config_types;
+#[doc(cfg(feature = "is_module"))]
+#[cfg(any(feature = "is_module", docsrs))]
+pub mod is_module;
 pub mod merge;
-mod module;
-#[cfg(feature = "sourcemap")]
-mod source_map;
-
-pub use swc_cached::{regex::CachedRegex, Error};
-
-pub use crate::module::IsModule;
-#[cfg(feature = "sourcemap")]
+#[doc(cfg(feature = "regex"))]
+#[cfg(any(feature = "regex", docsrs))]
+pub mod regex;
+pub mod types;
+#[doc(cfg(feature = "sourcemap"))]
+#[cfg(any(feature = "sourcemap", docsrs))]
 pub use crate::source_map::*;
