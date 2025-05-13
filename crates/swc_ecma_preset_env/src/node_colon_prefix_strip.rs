098d1113ba68606e5322029e2733a9d15fa86c7d
magic-akari <akari.ccino@gmail.com>
2025-05-13 21:12:23 +0800

    fix: only strip known builtin modules

--- crates/swc_ecma_preset_env/src/node_colon_prefix_strip.rs
+++ crates/swc_ecma_preset_env/src/node_colon_prefix_strip.rs
@@ -16,7 +16,7 @@ impl VisitMut for NodeColonPrefixStrip {
     noop_visit_mut_type!();
 
     fn visit_mut_import_decl(&mut self, node: &mut ImportDecl) {
-        if let Some(value) = node.src.value.strip_prefix("node:") {
+        if let Some(value) = Self::strip_node_colon(&node.src.value) {
             node.src.value = value.into();
             node.src.raw = None;
         }
@@ -26,14 +26,14 @@ impl VisitMut for NodeColonPrefixStrip {
         let Some(src) = &mut node.src else {
             return;
         };
-        if let Some(value) = src.value.strip_prefix("node:") {
+        if let Some(value) = Self::strip_node_colon(&src.value) {
             src.value = value.into();
             src.raw = None;
         }
     }
 
     fn visit_mut_export_all(&mut self, node: &mut ExportAll) {
-        if let Some(value) = node.src.value.strip_prefix("node:") {
+        if let Some(value) = Self::strip_node_colon(&node.src.value) {
             node.src.value = value.into();
             node.src.raw = None;
         }
@@ -57,10 +57,96 @@ impl VisitMut for NodeColonPrefixStrip {
             .and_then(|arg| arg.expr.as_mut_lit())
             .and_then(|lit| lit.as_mut_str())
         {
-            if let Some(value) = source.value.strip_prefix("node:") {
+            if let Some(value) = Self::strip_node_colon(&source.value) {
                 source.value = value.into();
                 source.raw = None;
             }
         }
+    }
+}
+
+impl NodeColonPrefixStrip {
+    /// Only Strip known `node:` prefixes.
+    /// There are some modules only available in the `node:` namespace which we
+    /// should not strip.
+    ///
+    /// You can use the following code to generate this list:
+    /// ```JavaScript
+    /// require("node:module").builtinModules.filter((m) => !m.startsWith("node:"))
+    /// ```
+    fn strip_node_colon(value: &str) -> Option<&str> {
+        value.strip_prefix("node:").filter(|value| {
+            matches!(
+                *value,
+                "_http_agent"
+                    | "_http_client"
+                    | "_http_common"
+                    | "_http_incoming"
+                    | "_http_outgoing"
+                    | "_http_server"
+                    | "_stream_duplex"
+                    | "_stream_passthrough"
+                    | "_stream_readable"
+                    | "_stream_transform"
+                    | "_stream_wrap"
+                    | "_stream_writable"
+                    | "_tls_common"
+                    | "_tls_wrap"
+                    | "assert"
+                    | "assert/strict"
+                    | "async_hooks"
+                    | "buffer"
+                    | "child_process"
+                    | "cluster"
+                    | "console"
+                    | "constants"
+                    | "crypto"
+                    | "dgram"
+                    | "diagnostics_channel"
+                    | "dns"
+                    | "dns/promises"
+                    | "domain"
+                    | "events"
+                    | "fs"
+                    | "fs/promises"
+                    | "http"
+                    | "http2"
+                    | "https"
+                    | "inspector"
+                    | "inspector/promises"
+                    | "module"
+                    | "net"
+                    | "os"
+                    | "path"
+                    | "path/posix"
+                    | "path/win32"
+                    | "perf_hooks"
+                    | "process"
+                    | "punycode"
+                    | "querystring"
+                    | "readline"
+                    | "readline/promises"
+                    | "repl"
+                    | "stream"
+                    | "stream/consumers"
+                    | "stream/promises"
+                    | "stream/web"
+                    | "string_decoder"
+                    | "sys"
+                    | "timers"
+                    | "timers/promises"
+                    | "tls"
+                    | "trace_events"
+                    | "tty"
+                    | "url"
+                    | "util"
+                    | "util/types"
+                    | "v8"
+                    | "vm"
+                    | "wasi"
+                    | "worker_threads"
+                    | "zlib"
+            )
+        })
     }
 }
