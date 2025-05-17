6afa900e68b37f87ec7852c214b64bb0be2af175
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-16 19:53:55 -0700

    isReactCompilerRequired

---
+++ bindings/binding_react_compiler_node/src/support.rs
@@ -0,0 +1,61 @@
+use napi::bindgen_prelude::*;
+use swc_core::{
+    common::{sync::Lrc, FileName, SourceMap},
+    ecma::ast::EsVersion,
+};
+
+struct IsReactCompilerRequiredTask {
+    code: String,
+}
+
+#[napi]
+impl Task for IsReactCompilerRequiredTask {
+    type JsValue = bool;
+    type Output = bool;
+
+    fn compute(&mut self) -> napi::Result<Self::Output> {
+        Ok(is_react_compiler_required_inner(&self.code))
+    }
+
+    fn resolve(&mut self, _env: napi::Env, output: Self::Output) -> napi::Result<Self::JsValue> {
+        Ok(output)
+    }
+}
+
+fn is_react_compiler_required_inner(code: &str) -> bool {
+    let cm = Lrc::new(SourceMap::default());
+    let fm = cm.new_source_file(FileName::Anon.into(), code.to_string());
+
+    let program = swc_core::ecma::parser::parse_file_as_program(
+        &fm,
+        Default::default(),
+        EsVersion::latest(),
+        None,
+        &mut vec![],
+    );
+
+    let Ok(program) = program else {
+        return false;
+    };
+
+    swc_ecma_react_compiler::fast_check::is_required(&program)
+}
+
+#[napi]
+fn is_react_compiler_required(
+    code: Buffer,
+    signal: Option<AbortSignal>,
+) -> AsyncTask<IsReactCompilerRequiredTask> {
+    let code = String::from_utf8_lossy(code.as_ref()).to_string();
+
+    let task = IsReactCompilerRequiredTask { code };
+
+    AsyncTask::with_optional_signal(task, signal)
+}
+
+#[napi]
+pub fn is_react_compiler_required_sync(code: Buffer) -> napi::Result<bool> {
+    let code = String::from_utf8_lossy(code.as_ref()).to_string();
+
+    Ok(is_react_compiler_required_inner(&code))
+}
