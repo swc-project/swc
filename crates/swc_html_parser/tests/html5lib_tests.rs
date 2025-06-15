3973427e8b297308cec0ed72e2d7020183dcdb4c
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-14 21:57:00 -0700

    fix a missing use

--- crates/swc_html_parser/tests/html5lib_tests.rs
+++ crates/swc_html_parser/tests/html5lib_tests.rs
@@ -6,7 +6,7 @@ use std::{fs, mem::take, path::PathBuf};
 use common::{document_span_visualizer, DomVisualizer};
 use rustc_hash::FxHashSet;
 use serde_json::Value;
-use swc_atoms::Atom;
+use swc_atoms::{atom, Atom};
 use swc_common::{
     input::{SourceFileInput, StringInput},
     BytePos,
