d4bd65ceb99c7ceb26fb9dc66677946f36d51e39
magic-akari <akari.ccino@gmail.com>
2025-06-18 17:07:41 +0800

    chore: update test cases

--- crates/swc_typescript/tests/fixture/class-constructor-overload.ts
+++ crates/swc_typescript/tests/fixture/class-constructor-overload.ts
@@ -1,14 +1,24 @@
 // @isolatedDeclarations: true
-export class Foo {
+export class C1 {
     constructor(private prop: string) {}
 }
 
-export class Bar {
+export class C2 {
     constructor(prop: string);
     constructor(private prop: any) {}
 }
 
-export class Baz {
+export class C3 {
     constructor(prop: string);
     constructor(readonly prop: any) {}
+}
+
+export class C4 {
+    constructor(prop: string);
+    constructor(public readonly prop: any) {}
+}
+
+export class C5 {
+    constructor(prop: string);
+    constructor(private readonly prop: any) {}
 }
