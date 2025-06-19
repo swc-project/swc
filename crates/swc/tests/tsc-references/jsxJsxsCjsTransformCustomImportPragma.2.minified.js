c252c84b810a3b786809dde4327b16c094406884
magic-akari <akari.ccino@gmail.com>
2025-06-19 13:22:16 +0800

    refactor(es/react): Split jsx into automatic/classic (#10616)

--- crates/swc/tests/tsc-references/jsxJsxsCjsTransformCustomImportPragma.2.minified.js
+++ crates/swc/tests/tsc-references/jsxJsxsCjsTransformCustomImportPragma.2.minified.js
@@ -1,10 +1,10 @@
 //// [preact.tsx]
 Object.defineProperty(exports, "__esModule", {
     value: !0
-}), require("preact/jsx-runtime").Fragment;
+}), require("react/jsx-dev-runtime").Fragment;
 //// [react.tsx]
 Object.defineProperty(exports, "__esModule", {
     value: !0
 });
-var _jsxruntime = require("react/jsx-runtime");
-require("./preact"), _jsxruntime.Fragment;
+var _jsxdevruntime = require("react/jsx-dev-runtime");
+require("./preact"), _jsxdevruntime.Fragment;
--- crates/swc/tests/tsc-references/tsxSpreadChildrenInvalidType(target=es2015).1.normal.js
+++ crates/swc/tests/tsc-references/tsxSpreadChildrenInvalidType(target=es2015).1.normal.js
@@ -1,19 +1,29 @@
 //// [tsxSpreadChildrenInvalidType.tsx]
+import { _ as _object_spread } from "@swc/helpers/_/_object_spread";
+import { jsx as _jsx } from "react/jsx-runtime";
 function Todo(prop) {
-    return /*#__PURE__*/ React.createElement("div", null, prop.key.toString() + prop.todo);
+    return /*#__PURE__*/ _jsx("div", {
+        children: prop.key.toString() + prop.todo
+    });
 }
 function TodoList({ todos }) {
-    return /*#__PURE__*/ React.createElement("div", null, .../*#__PURE__*/ React.createElement(Todo, {
-        key: todos[0].id,
-        todo: todos[0].todo
-    }));
+    return /*#__PURE__*/ _jsx("div", {
+        children: [
+            .../*#__PURE__*/ _jsx(Todo, {
+                todo: todos[0].todo
+            }, todos[0].id)
+        ]
+    });
 }
 function TodoListNoError({ todos }) {
     // any is not checked
-    return /*#__PURE__*/ React.createElement("div", null, .../*#__PURE__*/ React.createElement(Todo, {
-        key: todos[0].id,
-        todo: todos[0].todo
-    }));
+    return /*#__PURE__*/ _jsx("div", {
+        children: [
+            .../*#__PURE__*/ _jsx(Todo, {
+                todo: todos[0].todo
+            }, todos[0].id)
+        ]
+    });
 }
 let x;
-/*#__PURE__*/ React.createElement(TodoList, x);
+/*#__PURE__*/ _jsx(TodoList, _object_spread({}, x));
