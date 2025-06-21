954d20dd47177591c089ad6bbccbdb520fd822bd
magic-akari <akari.ccino@gmail.com>
2025-06-21 13:22:45 +0800

    fix

--- crates/swc_ecma_lexer/src/common/lexer/mod.rs
+++ crates/swc_ecma_lexer/src/common/lexer/mod.rs
@@ -594,51 +594,53 @@ pub trait Lexer<'a, TokenAndSpan>: Tokens<TokenAndSpan> + Sized {
         );
         let start = self.cur_pos();
 
-        // Fast path: try to parse using optimized integer arithmetic
-        let start_pos = self.cur_pos();
-        let mut digit_count = 0;
-
         let mut non_octal = false;
 
-        while let Some(c) = self.input().cur() {
-            if c == '_' {
-                self.bump();
-                continue;
+        // Fast path: try to parse using optimized integer arithmetic
+        let start_pos = self.cur_pos();
+        'fast_result: {
+            let mut digit_count = 0;
+
+            while let Some(c) = self.input().cur() {
+                if c == '_' {
+                    self.bump();
+                    continue;
+                }
+                if c == '8' || c == '9' {
+                    non_octal = true;
+                }
+                if c.is_digit(RADIX as u32) {
+                    digit_count += 1;
+                    self.bump();
+                } else {
+                    break;
+                }
             }
-            if c == '8' || c == '9' {
-                non_octal = true;
+
+            if digit_count == 0 {
+                break 'fast_result;
             }
-            if c.is_digit(RADIX as u32) {
-                digit_count += 1;
-                self.bump();
-            } else {
-                break;
+
+            let end_pos = self.cur_pos();
+            let raw_str = unsafe {
+                // Safety: We got both start and end position from `self.input`
+                self.input_slice(start_pos, end_pos)
+            };
+
+            // Try fast parsing, but skip octal fast path if non_octal digits found
+            let fast_value = match RADIX {
+                10 => fast_number::parse_decimal_fast(raw_str),
+                2 => fast_number::parse_binary_fast(raw_str),
+                8 => fast_number::parse_octal_fast(raw_str),
+                16 => fast_number::parse_hex_fast(raw_str),
+                _ => None,
+            };
+
+            if let Some(value) = fast_value {
+                let raw_number_str = raw_str.replace('_', "");
+
+                return Ok((value, LazyBigInt::new(raw_number_str), non_octal));
             }
-        }
-
-        if digit_count == 0 {
-            self.error(start, SyntaxError::ExpectedDigit { radix: RADIX })?;
-        }
-
-        let end_pos = self.cur_pos();
-        let raw_str = unsafe {
-            // Safety: We got both start and end position from `self.input`
-            self.input_slice(start_pos, end_pos)
-        };
-
-        // Try fast parsing, but skip octal fast path if non_octal digits found
-        let fast_value = match RADIX {
-            10 => fast_number::parse_decimal_fast(raw_str),
-            2 => fast_number::parse_binary_fast(raw_str),
-            8 => fast_number::parse_octal_fast(raw_str),
-            16 => fast_number::parse_hex_fast(raw_str),
-            _ => None,
-        };
-
-        if let Some(value) = fast_value {
-            let raw_number_str = raw_str.replace('_', "");
-
-            return Ok((value, LazyBigInt::new(raw_number_str), non_octal));
         }
 
         // Slow path: reset and use original method
