d9d0bdf57417e88a4ca59f1946513b59a54998da
CPunisher <1343316114@qq.com>
2025-06-25 10:27:59 +0800

    Add override keyword

--- crates/swc_ecma_parser/src/lexer/table.rs
+++ crates/swc_ecma_parser/src/lexer/table.rs
@@ -191,6 +191,7 @@ const L_O: ByteHandler = Some(|lexer| {
     lexer.read_word_with(&|s| match s {
         "of" => Some(Token::Of),
         "object" => Some(Token::Object),
+        "override" => Some(Token::Override),
         _ => None,
     })
 });
