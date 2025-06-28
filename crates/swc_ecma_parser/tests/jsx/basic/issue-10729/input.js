04ef20ad9b7e43dc70666258cb2c996a1a5e4074
magic-akari <akari.ccino@gmail.com>
2025-06-28 09:23:52 +0800

    fix(es/lexer): Allow keywords as jsx attribute names (#10730)
    **Related issue:**
    
    - Closes https://github.com/swc-project/swc/issues/10729

---
+++ crates/swc_ecma_parser/tests/jsx/basic/issue-10729/input.js
@@ -0,0 +1,3 @@
+<widget.component.type if="aaa" />;
+<widget.component.type if={true} for="loop" while="waiting" null={null} />;
+<widget.component.type if={true} null={null} for="loop" while="waiting" />;
