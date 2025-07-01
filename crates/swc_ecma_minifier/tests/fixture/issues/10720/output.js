87bc69883daae48b633556b55caeb480b7ba2d97
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-07-01 13:48:16 +0900

    fix(es/minifier): Fix termination detection  (#10741)
    **Description:**
    
    `terminates()` of `swc_ecma_utils` was wrong.
    
    **Related issue:**
    
     - Closes https://github.com/swc-project/swc/issues/10720

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/10720/output.js
@@ -0,0 +1,12 @@
+export function someFn({ someVal, shouldBreak }) {
+    switch(someVal){
+        case 'one':
+            // if there is a certain condition, we exit
+            if ('break' === shouldBreak) break;
+            return 1;
+        default:
+            return 0;
+    }
+    // this code gets hit if we break
+    return 11;
+}
