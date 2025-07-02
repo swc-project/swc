4976b12f9398c471c0f77e69ad141e0172927a2b
bohan <bohan-zhang@foxmail.com>
2025-07-02 15:00:37 +0800

    perf(es/parser): Reduce value set operations for context (#10751)

--- crates/swc_ecma_lexer/src/common/parser/ctx.rs
+++ crates/swc_ecma_lexer/src/common/parser/ctx.rs
@@ -3,10 +3,31 @@ use std::ops::{Deref, DerefMut};
 use crate::common::context::Context;
 
 pub struct WithCtx<'a, 'w, Parser: super::Parser<'a>> {
-    pub(super) inner: &'w mut Parser,
-    pub(super) orig_ctx: Context,
-    pub(super) marker: std::marker::PhantomData<&'a ()>,
+    inner: &'w mut Parser,
+    orig_ctx: Option<Context>,
+    marker: std::marker::PhantomData<&'a ()>,
 }
+
+impl<'a, 'w, Parser: super::Parser<'a>> WithCtx<'a, 'w, Parser> {
+    pub(super) fn new(inner: &'w mut Parser, ctx: Context) -> Self {
+        let orig_ctx = inner.ctx();
+        inner.set_ctx(ctx);
+        WithCtx {
+            inner,
+            orig_ctx: Some(orig_ctx),
+            marker: std::marker::PhantomData,
+        }
+    }
+
+    pub(super) fn new_without_ctx(inner: &'w mut Parser) -> Self {
+        WithCtx {
+            inner,
+            orig_ctx: None,
+            marker: std::marker::PhantomData,
+        }
+    }
+}
+
 impl<'a, Parser: super::Parser<'a>> Deref for WithCtx<'a, '_, Parser> {
     type Target = Parser;
 
@@ -14,6 +35,7 @@ impl<'a, Parser: super::Parser<'a>> Deref for WithCtx<'a, '_, Parser> {
         self.inner
     }
 }
+
 impl<'a, Parser: super::Parser<'a>> DerefMut for WithCtx<'a, '_, Parser> {
     fn deref_mut(&mut self) -> &mut Parser {
         self.inner
@@ -22,6 +44,8 @@ impl<'a, Parser: super::Parser<'a>> DerefMut for WithCtx<'a, '_, Parser> {
 
 impl<'a, Parser: super::Parser<'a>> Drop for WithCtx<'a, '_, Parser> {
     fn drop(&mut self) {
-        self.inner.set_ctx(self.orig_ctx);
+        if let Some(orig_ctx) = self.orig_ctx.take() {
+            self.inner.set_ctx(orig_ctx);
+        }
     }
 }
