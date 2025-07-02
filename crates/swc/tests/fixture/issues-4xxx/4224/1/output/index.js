a750c312b7b6f96890783b56d8548ad2447fbd99
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-07-02 19:18:05 +0900

    Update test refs

--- crates/swc/tests/fixture/issues-4xxx/4224/1/output/index.js
+++ crates/swc/tests/fixture/issues-4xxx/4224/1/output/index.js
@@ -1,29 +1,11 @@
-var _async_to_generator = require("@swc/helpers/_/_async_to_generator");
-var _class_call_check = require("@swc/helpers/_/_class_call_check");
-var _ts_generator = require("@swc/helpers/_/_ts_generator");
-var A = function A() {
-    "use strict";
-    var _this = this;
-    _class_call_check._(this, A);
-    this.foo = function() {
-        return _async_to_generator._(function() {
-            return _ts_generator._(this, function(_state) {
-                this.x();
-                return [
-                    2
-                ];
-            });
-        }).call(_this);
-    };
-    this.bar = function() {
-        return _async_to_generator._(function() {
-            return _ts_generator._(this, function(_state) {
-                this.x();
-                return [
-                    2
-                ];
-            });
-        }).call(_this);
-    };
-};
+class A {
+    constructor(){
+        this.foo = async ()=>{
+            this.x();
+        };
+        this.bar = async ()=>{
+            this.x();
+        };
+    }
+}
 console.log(A);
