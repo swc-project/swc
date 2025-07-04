19b38056aeb8decedee87efb0bb960d39237b8dc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-07-04 10:35:48 +0900

    remove unused

--- crates/swc_ecma_lints/src/rules/mod.rs
+++ crates/swc_ecma_lints/src/rules/mod.rs
@@ -5,11 +5,7 @@ use swc_ecma_ast::*;
 
 use crate::{config::LintConfig, rule::Rule};
 
-mod const_assign;
 mod critical_rules;
-mod duplicate_bindings;
-mod duplicate_exports;
-mod no_dupe_args;
 mod utils;
 
 #[cfg(feature = "non_critical_lints")]
