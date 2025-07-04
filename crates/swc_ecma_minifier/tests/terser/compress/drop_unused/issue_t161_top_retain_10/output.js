8aa888bc2a0fc0ed84d189725af917ae2be9f905
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-07-05 08:15:42 +0900

    test(es/minifier): Update the passing test list (#10782)

--- crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_t161_top_retain_10/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_t161_top_retain_10/output.js
@@ -1,6 +1,5 @@
 function f() {
     return x;
 }
-var x = 2,
-    y = 3;
-console.log(x, y, 4, x * y, 4 * x, 4 * y, f(), y, 4);
+var x = 2, y = 3;
+console.log(2, y, 4, 2 * y, 8, 4 * y, f(), y, 4);
--- crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_t161_top_retain_12/output.js
+++ crates/swc_ecma_minifier/tests/terser/compress/drop_unused/issue_t161_top_retain_12/output.js
@@ -4,6 +4,5 @@ function g() {
 function h() {
     return z;
 }
-var y = 3,
-    z = 4;
-console.log(2, y, z, 2 * y, 2 * z, y * z, 2, g(), h());
+var y = 3, z = 4;
+console.log(2, 3, 4, 6, 8, 12, 2, g(), h());
