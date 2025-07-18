4743ba03616fe9ec03b3e4ea1fde809b1549c991
CPunisher <1343316114@qq.com>
2025-07-18 17:49:57 +0800

    update test case

--- crates/swc/tests/tsc-references/nullishCoalescingOperator12.2.minified.js
+++ crates/swc/tests/tsc-references/nullishCoalescingOperator12.2.minified.js
@@ -1,3 +1,6 @@
 //// [nullishCoalescingOperator12.ts]
 var _obj_arr;
-for (const i of null != (_obj_arr = null == obj ? void 0 : []) ? _obj_arr : []);
+const obj = {
+    arr: []
+};
+for (const i of null != (_obj_arr = null == obj ? void 0 : obj.arr) ? _obj_arr : []);
