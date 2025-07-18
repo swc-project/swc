f0af185bc2573187ef94bdadd5e7eaa9eded4470
magic-akari <akari.ccino@gmail.com>
2025-07-18 15:31:24 +0800

    chore: update test cases

---
+++ crates/swc/tests/fixture/issues-10xxx/10890/output/index.ts
@@ -0,0 +1,15 @@
+var _decorate = require("@swc/helpers/_/_decorate");
+let Foo = _decorate._([
+    foo
+], function(_initialize) {
+    "use strict";
+    class Foo {
+        constructor(){
+            _initialize(this);
+        }
+    }
+    return {
+        F: Foo,
+        d: []
+    };
+});
