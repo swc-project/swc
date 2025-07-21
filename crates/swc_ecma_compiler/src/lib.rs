0b3af8220837c5f52dce07d965b13737dd2d46b5
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-07-21 13:59:04 +0900

    features

--- crates/swc_ecma_compiler/src/lib.rs
+++ crates/swc_ecma_compiler/src/lib.rs
@@ -1,7 +1,28 @@
 use swc_ecma_ast::Pass;
 
+pub use crate::features::Features;
+
+mod features;
+
 #[derive(Debug)]
-pub struct Compiler {}
+pub struct Compiler {
+    #[allow(unused)]
+    config: Config,
+}
+
+impl Compiler {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    /// Always compile these syntaxes.
+    pub includes: Features,
+    /// Always preserve these syntaxes.
+    pub excludes: Features,
+}
 
 impl Pass for Compiler {
     fn process(&mut self, _program: &mut swc_ecma_ast::Program) {}
