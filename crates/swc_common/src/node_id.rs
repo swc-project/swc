6dd6f9c0f68ac25ad743d477c8ee916cedb2964a
bohan <bohan-zhang@foxmail.com>
2025-08-04 21:36:44 +0800

    feat(es/ast): Add node id to `Ident` (#10973)

---
+++ crates/swc_common/src/node_id.rs
@@ -0,0 +1,27 @@
+use crate::util::take::Take;
+
+#[derive(Eq, Hash, Debug, PartialEq, Clone, Copy, serde::Serialize, serde::Deserialize)]
+#[cfg_attr(
+    any(feature = "rkyv-impl"),
+    derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)
+)]
+#[cfg_attr(feature = "rkyv-impl", derive(bytecheck::CheckBytes))]
+#[cfg_attr(feature = "rkyv-impl", repr(C))]
+#[cfg_attr(feature = "shrink-to-fit", derive(shrink_to_fit::ShrinkToFit))]
+pub struct NodeId(#[cfg_attr(feature = "__rkyv", rkyv(omit_bounds))] u32);
+
+impl NodeId {
+    pub const DUMMY: NodeId = NodeId(u32::MAX);
+}
+
+impl Default for NodeId {
+    fn default() -> Self {
+        NodeId::DUMMY
+    }
+}
+
+impl Take for NodeId {
+    fn dummy() -> Self {
+        NodeId::DUMMY
+    }
+}
