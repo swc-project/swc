7a85d63a444c40461dece8fbeea75f9555bee5cf
CPunisher <1343316114@qq.com>
2025-08-13 14:11:28 +0800

    Revert "Remove slow benches 1"
    This reverts commit dab35a0194cbcd3cadc928b86c0fc0010270e3bc.

---
+++ crates/swc_ecma_transforms_typescript/benches/compat.rs
@@ -0,0 +1,344 @@
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Bencher, Criterion};
+use swc_common::{comments::SingleThreadedComments, sync::Lrc, FileName, Mark, SourceMap};
+use swc_ecma_ast::{Module, Pass, Program};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+use swc_ecma_transforms_base::{helpers, resolver};
+use swc_ecma_transforms_typescript::strip;
+use swc_ecma_visit::{fold_pass, Fold};
+
+static SOURCE: &str = include_str!("assets/AjaxObservable.ts");
+
+fn module(cm: Lrc<SourceMap>) -> Program {
+    let fm = cm.new_source_file(FileName::Anon.into(), SOURCE);
+    let lexer = Lexer::new(
+        Syntax::Typescript(Default::default()),
+        Default::default(),
+        StringInput::from(&*fm),
+        None,
+    );
+    let mut parser = Parser::new_from(lexer);
+
+    parser
+        .parse_module()
+        .map(Program::Module)
+        .map_err(|_| ())
+        .unwrap()
+}
+
+fn run<V>(b: &mut Bencher, tr: impl Fn(Mark) -> V)
+where
+    V: Pass,
+{
+    let _ = ::testing::run_test(false, |cm, _| {
+        let module = module(cm);
+        let unresolved_mark = Mark::fresh(Mark::root());
+        let top_level_mark = Mark::fresh(Mark::root());
+        let module = module
+            .apply(resolver(unresolved_mark, top_level_mark, true))
+            .apply(strip(unresolved_mark, top_level_mark));
+
+        b.iter(|| {
+            let module = module.clone();
+
+            helpers::HELPERS.set(&Default::default(), || {
+                let tr = tr(unresolved_mark);
+
+                black_box(module.apply(tr));
+            });
+        });
+
+        Ok(())
+    });
+}
+
+fn baseline_group(c: &mut Criterion) {
+    c.bench_function("es/transform/baseline/base", base);
+    c.bench_function(
+        "es/transform/baseline/common_reserved_word",
+        common_reserved_word,
+    );
+    c.bench_function("es/transform/baseline/common_typescript", common_typescript);
+}
+
+fn base(b: &mut Bencher) {
+    struct Noop;
+    impl Fold for Noop {
+        #[inline]
+        fn fold_module(&mut self, m: Module) -> Module {
+            m
+        }
+    }
+    run(b, |_| fold_pass(Noop));
+}
+
+fn common_typescript(b: &mut Bencher) {
+    let _ = ::testing::run_test(false, |cm, _| {
+        let module = module(cm);
+        let unresolved_mark = Mark::fresh(Mark::root());
+        let top_level_mark = Mark::fresh(Mark::root());
+        let module = module
+            .apply(resolver(unresolved_mark, top_level_mark, true))
+            .apply(strip(unresolved_mark, top_level_mark));
+
+        b.iter(|| {
+            let module = module.clone();
+
+            helpers::HELPERS.set(&Default::default(), || {
+                black_box(module.apply(strip(unresolved_mark, top_level_mark)));
+            });
+        });
+
+        Ok(())
+    });
+}
+
+fn common_reserved_word(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::reserved_words::reserved_words()
+    });
+}
+
+fn version_group(c: &mut Criterion) {
+    c.bench_function("es/target/es3", es3);
+    c.bench_function("es/target/es2015", es2015);
+    c.bench_function("es/target/es2016", es2016);
+    c.bench_function("es/target/es2017", es2017);
+    c.bench_function("es/target/es2018", es2018);
+    c.bench_function("es/target/es2020", es2020);
+}
+
+fn single_tr_group(c: &mut Criterion) {
+    c.bench_function("es2020_nullish_coalescing", es2020_nullish_coalescing);
+    c.bench_function("es2020_optional_chaining", es2020_optional_chaining);
+    c.bench_function("es2022_class_properties", es2022_class_properties);
+    c.bench_function("es2018_object_rest_spread", es2018_object_rest_spread);
+    c.bench_function(
+        "es2019_optional_catch_binding",
+        es2019_optional_catch_binding,
+    );
+    c.bench_function("es2017_async_to_generator", es2017_async_to_generator);
+    c.bench_function("es2016_exponentiation", es2016_exponentiation);
+    c.bench_function("es2015_arrow", es2015_arrow);
+    c.bench_function("es2015_block_scoped_fn", es2015_block_scoped_fn);
+    c.bench_function("es2015_block_scoping", es2015_block_scoping);
+    c.bench_function("es2015_classes", es2015_classes);
+    c.bench_function("es2015_computed_props", es2015_computed_props);
+    c.bench_function("es2015_destructuring", es2015_destructuring);
+    c.bench_function("es2015_duplicate_keys", es2015_duplicate_keys);
+    c.bench_function("es2015_parameters", es2015_parameters);
+    c.bench_function("es2015_fn_name", es2015_fn_name);
+    c.bench_function("es2015_for_of", es2015_for_of);
+    c.bench_function("es2015_instanceof", es2015_instanceof);
+    c.bench_function("es2015_shorthand_property", es2015_shorthand_property);
+    c.bench_function("es2015_spread", es2015_spread);
+    c.bench_function("es2015_sticky_regex", es2015_sticky_regex);
+    c.bench_function("es2015_typeof_symbol", es2015_typeof_symbol);
+}
+
+fn es2020(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        swc_ecma_transforms_compat::es2022(Default::default(), unresolved_mark)
+    });
+}
+
+fn es2020_nullish_coalescing(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2020::nullish_coalescing(
+            swc_ecma_transforms_compat::es2020::nullish_coalescing::Config {
+                no_document_all: false,
+            },
+        )
+    });
+}
+
+fn es2020_optional_chaining(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2020::optional_chaining(Default::default(), Mark::new())
+    });
+}
+
+fn es2022_class_properties(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        swc_ecma_transforms_compat::es2022::class_properties(Default::default(), unresolved_mark)
+    });
+}
+
+fn es2018(b: &mut Bencher) {
+    run(
+        b,
+        |_| swc_ecma_transforms_compat::es2018(Default::default()),
+    );
+}
+
+fn es2018_object_rest_spread(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2018::object_rest_spread(Default::default())
+    });
+}
+
+fn es2019_optional_catch_binding(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2019::optional_catch_binding()
+    });
+}
+
+fn es2017(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2017(Default::default(), Mark::new())
+    });
+}
+
+fn es2017_async_to_generator(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2017::async_to_generator(Default::default(), Mark::new())
+    });
+}
+
+fn es2016(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2016());
+}
+
+fn es2016_exponentiation(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2016::exponentiation());
+}
+
+fn es2015(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        swc_ecma_transforms_compat::es2015(
+            unresolved_mark,
+            Some(SingleThreadedComments::default()),
+            Default::default(),
+        )
+    });
+}
+
+fn es2015_arrow(b: &mut Bencher) {
+    run(
+        b,
+        |_| swc_ecma_transforms_compat::es2015::arrow(Mark::new()),
+    );
+}
+
+fn es2015_block_scoped_fn(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::block_scoped_functions()
+    });
+}
+
+fn es2015_block_scoping(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::block_scoping(Mark::new())
+    });
+}
+
+fn es2015_classes(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::classes(Default::default())
+    });
+}
+
+fn es2015_computed_props(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::computed_properties(Default::default())
+    });
+}
+
+fn es2015_destructuring(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::destructuring(Default::default())
+    });
+}
+
+fn es2015_duplicate_keys(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::duplicate_keys());
+}
+
+fn es2015_parameters(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::parameters(Default::default(), Mark::new())
+    });
+}
+
+fn es2015_fn_name(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::function_name());
+}
+
+fn es2015_for_of(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::for_of(Default::default())
+    });
+}
+
+fn es2015_instanceof(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::instance_of());
+}
+
+fn es2015_shorthand_property(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::shorthand());
+}
+
+fn es2015_spread(b: &mut Bencher) {
+    run(b, |_| {
+        swc_ecma_transforms_compat::es2015::spread(Default::default())
+    });
+}
+
+fn es2015_sticky_regex(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::sticky_regex());
+}
+
+fn es2015_typeof_symbol(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es2015::typeof_symbol());
+}
+
+fn es3(b: &mut Bencher) {
+    run(b, |_| swc_ecma_transforms_compat::es3(Default::default()));
+}
+
+fn full_es2016(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        (
+            swc_ecma_transforms_compat::es2022(Default::default(), unresolved_mark),
+            swc_ecma_transforms_compat::es2019(),
+            swc_ecma_transforms_compat::es2018(Default::default()),
+            swc_ecma_transforms_compat::es2017(Default::default(), Mark::new()),
+            swc_ecma_transforms_compat::es2016(),
+        )
+    });
+}
+
+fn full_es2017(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        (
+            swc_ecma_transforms_compat::es2022(Default::default(), unresolved_mark),
+            swc_ecma_transforms_compat::es2019(),
+            swc_ecma_transforms_compat::es2018(Default::default()),
+            swc_ecma_transforms_compat::es2017(Default::default(), Mark::new()),
+        )
+    });
+}
+
+fn full_es2018(b: &mut Bencher) {
+    run(b, |unresolved_mark| {
+        (
+            swc_ecma_transforms_compat::es2022(Default::default(), unresolved_mark),
+            swc_ecma_transforms_compat::es2019(),
+            swc_ecma_transforms_compat::es2018(Default::default()),
+        )
+    });
+}
+
+fn full_group(c: &mut Criterion) {
+    c.bench_function("es/full-target/es2016", full_es2016);
+    c.bench_function("es/full-target/es2017", full_es2017);
+    c.bench_function("es/full-target/es2018", full_es2018);
+}
+
+criterion_group!(
+    benches,
+    full_group,
+    single_tr_group,
+    baseline_group,
+    version_group
+);
+criterion_main!(benches);
---
+++ crates/swc_html_minifier/benches/full.rs
@@ -0,0 +1,164 @@
+extern crate swc_malloc;
+
+use std::{fs::read_to_string, path::Path};
+
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+use swc_common::{errors::HANDLER, FileName};
+use swc_html_ast::{Document, DocumentFragment, DocumentMode, Element, Namespace};
+use swc_html_codegen::{writer::basic::BasicHtmlWriter, Emit};
+use swc_html_minifier::{minify_document, minify_document_fragment, option::MinifyOptions};
+use swc_html_parser::{parse_file_as_document, parse_file_as_document_fragment};
+
+pub fn bench_files_document(c: &mut Criterion) {
+    let mut group = c.benchmark_group("html/minify/document");
+    group.sample_size(10);
+
+    let mut bench_file = |name: &str, path: &Path| {
+        let src = read_to_string(path).unwrap();
+
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                // We benchmark full time, including time for creating cm, handler
+                run_document(&src)
+            })
+        });
+    };
+
+    bench_file(
+        "css_spec",
+        Path::new("../swc_html_parser/benches/files/css_2021_spec.html"),
+    );
+    bench_file(
+        "github",
+        Path::new("../swc_html_parser/benches/files/github_com_17_05_2022.html"),
+    );
+    bench_file(
+        "stackoverflow",
+        Path::new("../swc_html_parser/benches/files/stackoverflow_com_17_05_2022.html"),
+    );
+}
+
+pub fn bench_files_document_fragment(c: &mut Criterion) {
+    let mut group = c.benchmark_group("html/minify/document_fragment");
+    group.sample_size(10);
+
+    let mut bench_file = |name: &str, path: &Path| {
+        let src = read_to_string(path).unwrap();
+
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                // We benchmark full time, including time for creating cm, handler
+                run_document_fragment(&src)
+            })
+        });
+    };
+
+    bench_file(
+        "css_spec",
+        Path::new("../swc_html_parser/benches/files/css_2021_spec.html"),
+    );
+    bench_file(
+        "github",
+        Path::new("../swc_html_parser/benches/files/github_com_17_05_2022.html"),
+    );
+    bench_file(
+        "stackoverflow",
+        Path::new("../swc_html_parser/benches/files/stackoverflow_com_17_05_2022.html"),
+    );
+}
+
+criterion_group!(files_document, bench_files_document);
+criterion_group!(files_document_fragment, bench_files_document_fragment);
+criterion_main!(files_document, files_document_fragment);
+
+fn run_document(src: &str) {
+    testing::run_test2(false, |cm, handler| {
+        HANDLER.set(&handler, || {
+            let fm = cm.new_source_file(FileName::Anon.into(), src.to_string());
+
+            let mut errors = Vec::new();
+            let mut document: Document =
+                parse_file_as_document(&fm, Default::default(), &mut errors).unwrap();
+
+            for err in errors {
+                err.to_diagnostics(&handler).emit();
+            }
+
+            minify_document(&mut document, &MinifyOptions::default());
+
+            let mut buf = String::new();
+            {
+                let wr = BasicHtmlWriter::new(&mut buf, None, Default::default());
+                let mut generator = swc_html_codegen::CodeGenerator::new(
+                    wr,
+                    swc_html_codegen::CodegenConfig {
+                        minify: true,
+                        ..Default::default()
+                    },
+                );
+
+                generator.emit(&document).unwrap();
+            }
+
+            black_box(buf);
+
+            Ok(())
+        })
+    })
+    .unwrap();
+}
+
+fn run_document_fragment(src: &str) {
+    testing::run_test2(false, |cm, handler| {
+        HANDLER.set(&handler, || {
+            let fm = cm.new_source_file(FileName::Anon.into(), src.to_string());
+
+            let mut errors = Vec::new();
+            let context_element_namespace = Namespace::HTML;
+            let context_element_tag_name = "template";
+            let context_element = Element {
+                span: Default::default(),
+                namespace: context_element_namespace,
+                tag_name: context_element_tag_name.into(),
+                attributes: Vec::new(),
+                is_self_closing: false,
+                children: Vec::new(),
+                content: None,
+            };
+            let mut document: DocumentFragment = parse_file_as_document_fragment(
+                &fm,
+                &context_element,
+                DocumentMode::NoQuirks,
+                None,
+                Default::default(),
+                &mut errors,
+            )
+            .unwrap();
+
+            for err in errors {
+                err.to_diagnostics(&handler).emit();
+            }
+
+            minify_document_fragment(&mut document, &context_element, &MinifyOptions::default());
+
+            let mut buf = String::new();
+            {
+                let wr = BasicHtmlWriter::new(&mut buf, None, Default::default());
+                let mut generator = swc_html_codegen::CodeGenerator::new(
+                    wr,
+                    swc_html_codegen::CodegenConfig {
+                        minify: true,
+                        ..Default::default()
+                    },
+                );
+
+                generator.emit(&document).unwrap();
+            }
+
+            black_box(buf);
+
+            Ok(())
+        })
+    })
+    .unwrap();
+}
