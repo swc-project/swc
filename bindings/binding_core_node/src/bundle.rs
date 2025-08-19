770086ed12c898a6f4cd3a69b8da9beffbba1ff6
Hana <andywangsy@gmail.com>
2025-08-19 15:26:22 +0800

    fix: remove lone surrogate from bindings as it's not upgraded

--- bindings/binding_core_node/src/bundle.rs
+++ bindings/binding_core_node/src/bundle.rs
@@ -281,7 +281,6 @@ impl swc_core::bundler::Hook for Hook {
                     span,
                     raw: None,
                     value: file_name.into(),
-                    lone_surrogates: false,
                 }))),
             },
             KeyValueProp {
