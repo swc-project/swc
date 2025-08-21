448fc99767f39d27ef3d69fdbe95703ae9e76f30
magic-akari <akari.ccino@gmail.com>
2025-08-21 17:44:43 +0800

    chore: update test cases

--- crates/swc/tests/fixture/issues-10xxx/10796/output/1.js
+++ crates/swc/tests/fixture/issues-10xxx/10796/output/1.js
@@ -1,2 +1,4 @@
+require("core-js/modules/es.object.to-string.js");
+require("core-js/modules/es.promise.js");
 require("core-js/modules/es.promise.with-resolvers.js");
 console.log(Promise.withResolvers());
