88f2460bf66c1cc4933d41c18c5ad5a7db22a9b5
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-09-06 14:19:36 +0900

    restore

--- crates/swc_ecma_parser/src/parser/jsx/mod.rs
+++ crates/swc_ecma_parser/src/parser/jsx/mod.rs
@@ -506,8 +506,6 @@ mod tests {
                             value: atom!("w < w"),
                             raw: Some(atom!("\"w &lt; w\"")),
                         })),
-                            lone_surrogates: false,
-                        }))),
                     })],
                     name: JSXElementName::Ident(Ident::new_no_ctxt(atom!("div"), span)),
                     self_closing: true,
