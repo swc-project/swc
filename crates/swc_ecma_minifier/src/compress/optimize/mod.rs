swc-project/swc

commit 5ab92f792fbb763f7fd9e7fd6eba97c6f541f876

fix(es/minifier): Avoid `const_to_let` if reassigned (#11035)
**Description:**

I move the process of `const_to_let` from `postcompress` to `Optimizer`
because varinfo is need to check if the variable is reassigned.

I'm also curious why `const_to_let` is disabled in
https://github.com/swc-project/swc/pull/11035/files#diff-dd713fdbfb1b9c77d20d446319255c3d41a169213b8825f43aacc0ee04f4c90f

**Related issue:**

 - Closes https://github.com/swc-project/swc/issues/11034

crates/swc_ecma_minifier/src/compress/optimize/mod.rs
@@ -27,7 +27,7 @@ use super::util::{drop_invalid_stmts, is_fine_for_if_cons};
 #[cfg(feature = "debug")]
 use crate::debug::dump;
 use crate::{
-    compress::util::is_pure_undefined,
+    compress::{optimize::util::get_ids_of_pat, util::is_pure_undefined},
     debug::AssertValid,
     maybe_par,
     mode::Mode,
@@ -80,6 +80,7 @@ pub(super) fn optimizer<'a>(
     Optimizer {
         marks,
         changed: false,
+        is_module: false,
         options,
         mangle_options,
         prepend_stmts: Default::default(),
@@ -218,6 +219,7 @@ struct Optimizer<'a> {
     marks: Marks,
 
     changed: bool,
+    is_module: bool,
     options: &'a CompressOptions,
     mangle_options: Option<&'a MangleOptions>,
     /// Statements prepended to the current statement.
@@ -2258,6 +2260,11 @@ impl VisitMut for Optimizer<'_> {
         }
     }
 
+    fn visit_mut_module(&mut self, m: &mut Module) {
+        self.is_module = true;
+        m.visit_mut_children_with(self);
+    }
+
     #[cfg_attr(feature = "debug", tracing::instrument(level = "debug", skip_all))]
     fn visit_mut_module_item(&mut self, s: &mut ModuleItem) {
         s.visit_mut_children_with(self);
@@ -2821,6 +2828,31 @@ impl VisitMut for Optimizer<'_> {
                     }
                 }
             });
+        }
+
+        if self.options.const_to_let
+            // Don't change constness of exported variables.
+            && !self.ctx.bit_ctx.contains(BitCtx::IsExported)
+            && (self.is_module || !self.ctx.in_top_level())
+        {
+            if n.kind == VarDeclKind::Const {
+                // If a const variable is reassigned, we should not convert it to `let`
+                let no_reassignment = n.decls.iter().all(|var| {
+                    let ids = get_ids_of_pat(&var.name);
+                    for id in ids {
+                        if let Some(usage) = self.data.vars.get(&id) {
+                            if usage.flags.contains(VarUsageInfoFlags::REASSIGNED) {
+                                return false;
+                            }
+                        }
+                    }
+                    true
+                });
+
+                if no_reassignment {
+                    n.kind = VarDeclKind::Let;
+                }
+            }
         }
     }
 

crates/swc_html_minifier/tests/fixture/element/script/output.min.html
@@ -27,7 +27,7 @@
 
         var   q    =   "WRONG"   ;
 
-        </script><script>let foo="testtest";function test(){alert("1 2")}alert("<!--"),alert("<!-- foo -->"),alert("-->"),alert("testtest")</script><h2>Party coffee cake recipe</h2>
+        </script><script>const foo="testtest";function test(){alert("1 2")}alert("<!--"),alert("<!-- foo -->"),alert("-->"),alert("testtest")</script><h2>Party coffee cake recipe</h2>
     <p>
         <i>by Mary Stone, 2018-03-10</i>
     </p>

crates/swc/tests/minify/issue-7475/issue-8372/1/output.js
@@ -1,2 +1,2 @@
-let a={aób:"\xf3"};console.log(a);
+const a={aób:"\xf3"};console.log(a);
 //# sourceMappingURL=input.map

crates/swc/tests/minify/issue-7475/issue-8372/2/output.js
@@ -1,1 +1,1 @@
-let a={aób:"\xf3"};console.log(a);
+const a={aób:"\xf3"};console.log(a);

crates/swc_ecma_minifier/tests/fixture/issues/10328/output.js
@@ -1,6 +1,7 @@
 function g(x, v) {
     if ("a" === x) {
-        const h = i({
+        let h;
+        h = i({
             onCancel: ()=>h()
         });
     } else {

commit 79dc8d4f70b1cee2558f002e75a10ef2fa069d7e

fix(es/minifier): Make `const_to_let` work in arrows again (#11062)
**Description:**

I was seeing this snapshot test failure when trying to upgrade swc in
Turbopack:
```js
--- expected
+++ actual
@@ -1,3 +1,3 @@
-module.exports=["index.js",(t,e,o)=>{let s="Hello";console.log("Hello, world!",3,s),console.log(s)}];
+module.exports=["index.js",(o,s,t)=>{const e="Hello";console.log("Hello, world!",3,e),console.log(e)}];
```

The problem was that `is_top_level` was true here, even though the code
is inside an arrow function:

https://github.com/swc-project/swc/blob/0557609d6f862f7632a67ce91163571e5284163f/crates/swc_ecma_minifier/src/compress/optimize/mod.rs#L2836

Probably caused by https://github.com/swc-project/swc/pull/11035

crates/swc_ecma_minifier/src/compress/optimize/mod.rs
@@ -1471,7 +1471,18 @@ impl VisitMut for Optimizer<'_> {
             n.params.visit_mut_with(&mut *self.with_ctx(ctx));
         }
 
-        n.body.visit_mut_with(self);
+        {
+            let ctx = Ctx {
+                bit_ctx: self
+                    .ctx
+                    .bit_ctx
+                    .with(BitCtx::InFnLike, true)
+                    .with(BitCtx::TopLevel, false),
+                scope: n.ctxt,
+                ..self.ctx.clone()
+            };
+            n.body.visit_mut_with(&mut *self.with_ctx(ctx));
+        }
 
         if !self.prepend_stmts.is_empty() {
             let mut stmts = self.prepend_stmts.take().take_stmts();

crates/swc_ecma_minifier/tests/fixture/issues/8737/output.js
@@ -1,6 +1,4 @@
 d(()=>{
-    var obj = {
-        key: "some string"
-    }, b = ()=>(a, obj.key);
+    var b = ()=>(a, "some string");
     return ()=>b;
 });

crates/swc_ecma_minifier/tests/fixture/issues/8813/output.js
@@ -1,13 +1,11 @@
-const x = 'asdf';
-let y = "PASS 1";
+let x = 'asdf', y = "PASS 1";
 switch(x){
     case x:
     default:
     case y = "FAIL":
 }
 console.log(y);
-const x1 = 'asdf';
-let y1 = "PASS 2";
+let x1 = 'asdf', y1 = "PASS 2";
 switch(x1){
     case x1:
     case y1 = "FAIL":

crates/swc_ecma_minifier/tests/fixture/issues/firebase-firestore/1/output.js
@@ -8145,7 +8145,7 @@
                     // operations. Meant to speed up recovery when we regain file system access
                     // after page comes into foreground.
                     this.Rc = ()=>{
-                        const t = Jr();
+                        let t = Jr();
                         t && $("AsyncQueue", "Visibility state changed to " + t.visibilityState), this.ar.tr();
                     };
                     const t = Jr();

crates/swc_ecma_minifier/tests/fixture/next/target-es2015/static/chunks/main-04b5934c26266542/output.js
@@ -2448,10 +2448,9 @@ You should only use "next/router" on the client side of your app.
                 constructor(pathname1, query1, as1, { initialProps, pageLoader, App, wrapApp, Component, err, subscription, isFallback, locale, locales, defaultLocale, domainLocales, isPreview }){
                     // Server Data Cache
                     this.sdc = {}, this.isFirstPopStateEvent = !0, this._key = createKey(), this.onPopState = (e)=>{
-                        let forcedScroll;
-                        const { isFirstPopStateEvent } = this;
+                        let forcedScroll, { isFirstPopStateEvent } = this;
                         this.isFirstPopStateEvent = !1;
-                        const state = e.state;
+                        let state = e.state;
                         if (!state) {
                             // We get state as undefined for two reasons.
                             //  1. With older safari (< 8) and older chrome (< 34)
@@ -2462,7 +2461,7 @@ You should only use "next/router" on the client side of your app.
                             // But we can simply replace the state with the new changes.
                             // Actually, for (1) we don't need to nothing. But it's hard to detect that event.
                             // So, doing the following for (1) does no harm.
-                            const { pathname, query } = this;
+                            let { pathname, query } = this;
                             this.changeState("replaceState", _formatUrl.formatWithValidation({
                                 pathname: _addBasePath.addBasePath(pathname),
                                 query
@@ -2472,9 +2471,9 @@ You should only use "next/router" on the client side of your app.
                         // __NA is used to identify if the history entry can be handled by the app-router.
                         if (state.__NA) return void window.location.reload();
                         if (!state.__N || isFirstPopStateEvent && this.locale === state.options.locale && state.as === this.asPath) return;
-                        const { url, as, options, key } = state;
+                        let { url, as, options, key } = state;
                         this._key = key;
-                        const { pathname: pathname1 } = _parseRelativeUrl.parseRelativeUrl(url);
+                        let { pathname: pathname1 } = _parseRelativeUrl.parseRelativeUrl(url);
                         // Make sure we don't re-render on initial load,
                         // can be caused by navigating back from an external site
                         this.isSsr && as === _addBasePath.addBasePath(this.asPath) && pathname1 === _addBasePath.addBasePath(this.pathname) || (!this._bps || this._bps(state)) && this.change("replaceState", url, as, Object.assign({}, options, {

crates/swc_ecma_minifier/tests/fixture/next/wrap-contracts/output.js
@@ -26666,13 +26666,13 @@ class Zip {
                     const mem = ()=>new DataView(this._inst.exports.memory.buffer), setInt64 = (addr, v)=>{
                         mem().setUint32(addr + 0, v, !0), mem().setUint32(addr + 4, Math.floor(v / 4294967296), !0);
                     }, loadValue = (addr)=>{
-                        const f = mem().getFloat64(addr, !0);
+                        let f = mem().getFloat64(addr, !0);
                         if (0 === f) return;
                         if (!isNaN(f)) return f;
-                        const id = mem().getUint32(addr, !0);
+                        let id = mem().getUint32(addr, !0);
                         return this._values[id];
                     }, storeValue = (addr, v)=>{
-                        const nanHead = 0x7ff80000;
+                        let nanHead = 0x7ff80000;
                         if ('number' == typeof v) {
                             if (isNaN(v)) {
                                 mem().setUint32(addr + 4, nanHead, !0), mem().setUint32(addr, 0, !0);
@@ -26714,7 +26714,7 @@ class Zip {
                         }
                         mem().setUint32(addr + 4, nanHead | typeFlag, !0), mem().setUint32(addr, id, !0);
                     }, loadSlice = (array, len, cap = null)=>new Uint8Array(this._inst.exports.memory.buffer, array, len), loadSliceOfValues = (array, len, cap)=>{
-                        const a = Array(len);
+                        let a = Array(len);
                         for(let i = 0; i < len; i++)a[i] = loadValue(array + 8 * i);
                         return a;
                     }, loadString = (ptr, len)=>decoder.decode(new DataView(this._inst.exports.memory.buffer, ptr, len)), timeOrigin = Date.now() - performance.now();
@@ -26765,9 +26765,9 @@ class Zip {
                             'syscall/js.finalizeRef': (v_addr)=>{
                                 // Note: TinyGo does not support finalizers so this is only called
                                 // for one specific case, by js.go:jsString.
-                                const id = mem().getUint32(v_addr, !0);
+                                let id = mem().getUint32(v_addr, !0);
                                 if (this._goRefCounts[id]--, 0 === this._goRefCounts[id]) {
-                                    const v = this._values[id];
+                                    let v = this._values[id];
                                     this._values[id] = null, this._ids.delete(v), this._idPool.push(id);
                                 }
                             },
@@ -26798,9 +26798,9 @@ class Zip {
                             },
                             // func valueCall(v ref, m string, args []ref) (ref, bool)
                             'syscall/js.valueCall': (ret_addr, v_addr, m_ptr, m_len, args_ptr, args_len, args_cap)=>{
-                                const v = loadValue(v_addr), name = loadString(m_ptr, m_len), args = loadSliceOfValues(args_ptr, args_len, args_cap);
+                                let v = loadValue(v_addr), name = loadString(m_ptr, m_len), args = loadSliceOfValues(args_ptr, args_len, args_cap);
                                 try {
-                                    const m = Reflect.get(v, name);
+                                    let m = Reflect.get(v, name);
                                     storeValue(ret_addr, Reflect.apply(m, v, args)), mem().setUint8(ret_addr + 8, 1);
                                 } catch (err) {
                                     storeValue(ret_addr, err), mem().setUint8(ret_addr + 8, 0);
@@ -26809,7 +26809,7 @@ class Zip {
                             // func valueInvoke(v ref, args []ref) (ref, bool)
                             'syscall/js.valueInvoke': (ret_addr, v_addr, args_ptr, args_len, args_cap)=>{
                                 try {
-                                    const v = loadValue(v_addr), args = loadSliceOfValues(args_ptr, args_len, args_cap);
+                                    let v = loadValue(v_addr), args = loadSliceOfValues(args_ptr, args_len, args_cap);
                                     storeValue(ret_addr, Reflect.apply(v, void 0, args)), mem().setUint8(ret_addr + 8, 1);
                                 } catch (err) {
                                     storeValue(ret_addr, err), mem().setUint8(ret_addr + 8, 0);
@@ -26817,7 +26817,7 @@ class Zip {
                             },
                             // func valueNew(v ref, args []ref) (ref, bool)
                             'syscall/js.valueNew': (ret_addr, v_addr, args_ptr, args_len, args_cap)=>{
-                                const v = loadValue(v_addr), args = loadSliceOfValues(args_ptr, args_len, args_cap);
+                                let v = loadValue(v_addr), args = loadSliceOfValues(args_ptr, args_len, args_cap);
                                 try {
                                     storeValue(ret_addr, Reflect.construct(v, args)), mem().setUint8(ret_addr + 8, 1);
                                 } catch (err) {
@@ -26828,32 +26828,30 @@ class Zip {
                             'syscall/js.valueLength': (v_addr)=>loadValue(v_addr).length,
                             // valuePrepareString(v ref) (ref, int)
                             'syscall/js.valuePrepareString': (ret_addr, v_addr)=>{
-                                const s = String(loadValue(v_addr)), str = encoder.encode(s);
+                                let s = String(loadValue(v_addr)), str = encoder.encode(s);
                                 storeValue(ret_addr, str), setInt64(ret_addr + 8, str.length);
                             },
                             // valueLoadString(v ref, b []byte)
                             'syscall/js.valueLoadString': (v_addr, slice_ptr, slice_len, slice_cap)=>{
-                                const str = loadValue(v_addr);
+                                let str = loadValue(v_addr);
                                 loadSlice(slice_ptr, slice_len, slice_cap).set(str);
                             },
                             // func valueInstanceOf(v ref, t ref) bool
                             'syscall/js.valueInstanceOf': (v_addr, t_addr)=>loadValue(v_addr) instanceof loadValue(t_addr),
                             // func copyBytesToGo(dst []byte, src ref) (int, bool)
                             'syscall/js.copyBytesToGo': (ret_addr, dest_addr, dest_len, dest_cap, source_addr)=>{
-                                let num_bytes_copied_addr = ret_addr, returned_status_addr = ret_addr + 4;
-                                const dst = loadSlice(dest_addr, dest_len), src = loadValue(source_addr);
+                                let num_bytes_copied_addr = ret_addr, returned_status_addr = ret_addr + 4, dst = loadSlice(dest_addr, dest_len), src = loadValue(source_addr);
                                 if (!(src instanceof Uint8Array)) return void mem().setUint8(returned_status_addr, 0);
-                                const toCopy = src.subarray(0, dst.length);
+                                let toCopy = src.subarray(0, dst.length);
                                 dst.set(toCopy), setInt64(num_bytes_copied_addr, toCopy.length), mem().setUint8(returned_status_addr, 1);
                             },
                             // copyBytesToJS(dst ref, src []byte) (int, bool)
                             // Originally copied from upstream Go project, then modified:
                             //   https://github.com/golang/go/blob/3f995c3f3b43033013013e6c7ccc93a9b1411ca9/misc/wasm/wasm_exec.js#L404-L416
                             'syscall/js.copyBytesToJS': (ret_addr, dest_addr, source_addr, source_len, source_cap)=>{
-                                let num_bytes_copied_addr = ret_addr, returned_status_addr = ret_addr + 4;
-                                const dst = loadValue(dest_addr), src = loadSlice(source_addr, source_len);
+                                let num_bytes_copied_addr = ret_addr, returned_status_addr = ret_addr + 4, dst = loadValue(dest_addr), src = loadSlice(source_addr, source_len);
                                 if (!(dst instanceof Uint8Array)) return void mem().setUint8(returned_status_addr, 0);
-                                const toCopy = src.subarray(0, dst.length);
+                                let toCopy = src.subarray(0, dst.length);
                                 dst.set(toCopy), setInt64(num_bytes_copied_addr, toCopy.length), mem().setUint8(returned_status_addr, 1);
                             }
                         }

crates/swc_html_minifier/tests/fixture/element/script/output.min.html
@@ -42,7 +42,7 @@
     <script>alert(1)</script>
 </svg>
     <svg viewBox="0 0 10 10">
-        <script>window.addEventListener("DOMContentLoaded",()=>{document.querySelector("circle").addEventListener("click",t=>{t.target.style.fill=function(){let t=Math.round(255*Math.random()).toString(16).padStart(2,"0"),n=Math.round(255*Math.random()).toString(16).padStart(2,"0"),r=Math.round(255*Math.random()).toString(16).padStart(2,"0");return`#${t}${n}${r}`}()})})</script>
+        <script>window.addEventListener("DOMContentLoaded",()=>{document.querySelector("circle").addEventListener("click",t=>{let a,r,d;t.target.style.fill=(a=Math.round(255*Math.random()).toString(16).padStart(2,"0"),r=Math.round(255*Math.random()).toString(16).padStart(2,"0"),d=Math.round(255*Math.random()).toString(16).padStart(2,"0"),`#${a}${r}${d}`)})})</script>
 
         <circle cx=5 cy=5 r=4 />
     </svg>