0e93b5dcd721108069c4b651948ee087eaf0d213
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-09-07 18:38:26 +0900

    size

--- crates/swc_ecma_ast/src/compat.rs
+++ crates/swc_ecma_ast/src/compat.rs
@@ -1,20 +1,4 @@
-pub const KIND_EXPR: usize = 1;
-
-pub const KIND_STMT: usize = 2;
-
-pub const KIND_PAT: usize = 3;
-
-pub const KIND_PROP: usize = 4;
-
-pub const KIND_DECL: usize = 5;
-
-pub const KIND_TYPE: usize = 6;
-
-pub const KIND_MODULE: usize = 7;
-
-pub const KIND_MODULE_DECL: usize = 8;
-
-pub const KIND_MODULE_ITEM: usize = 9;
+pub const KIND_CLASS: usize = 1;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct UnknownBytes<const KIND: usize, const LEN: usize> {
