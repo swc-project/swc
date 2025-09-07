d1a98825afbca3b954f257d3ef1ed09e18bfe004
CPunisher <1343316114@qq.com>
2025-09-07 10:18:38 +0800

    Revert

--- crates/swc/examples/transform.rs
+++ crates/swc/examples/transform.rs
@@ -6,25 +6,17 @@ use swc_common::{SourceMap, GLOBALS};
 
 fn main() {
     let cm = Arc::<SourceMap>::default();
-    let path = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| "examples/transform-input.js".to_string());
 
     let c = swc::Compiler::new(cm.clone());
     let output = GLOBALS
         .set(&Default::default(), || {
             try_with_handler(cm.clone(), Default::default(), |handler| {
-                let fm = cm.load_file(Path::new(&path)).expect("failed to load file");
+                let fm = cm
+                    .load_file(Path::new("examples/transform-input.js"))
+                    .expect("failed to load file");
 
-                c.process_js_file(
-                    fm,
-                    handler,
-                    &swc::config::Options {
-                        swcrc: true,
-                        ..Default::default()
-                    },
-                )
-                .context("failed to process file")
+                c.process_js_file(fm, handler, &Default::default())
+                    .context("failed to process file")
             })
         })
         .unwrap();
