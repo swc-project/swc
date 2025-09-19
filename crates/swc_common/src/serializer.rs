2c069539fd3a6b4dd7abe5683eee0eaea257c94c
quininer <quininer@live.com>
2025-09-19 18:52:23 +0800

    make html_ast

--- crates/swc_common/src/serializer.rs
+++ crates/swc_common/src/serializer.rs
@@ -1,5 +1,5 @@
 #![allow(deprecated)]
-#![deprecated = "Not used by swc, and this will be removed with next breaking change"]
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -15,4 +15,32 @@ pub struct Node<T> {
 pub struct Type {
     #[serde(rename = "type")]
     pub ty: String,
+}
+
+#[cfg(feature = "encoding-impl")]
+pub struct WithChar<T>(pub T);
+
+#[cfg(feature = "encoding-impl")]
+impl cbor4ii::core::enc::Encode for WithChar<&char> {
+    fn encode<W: cbor4ii::core::enc::Write>(
+        &self,
+        writer: &mut W,
+    ) -> Result<(), cbor4ii::core::enc::Error<W::Error>> {
+        u32::from(*self.0).encode(writer)
+    }
+}
+
+#[cfg(feature = "encoding-impl")]
+impl<'de> cbor4ii::core::dec::Decode<'de> for WithChar<char> {
+    fn decode<R: cbor4ii::core::dec::Read<'de>>(
+        reader: &mut R,
+    ) -> Result<Self, cbor4ii::core::dec::Error<R::Error>> {
+        let n = u32::decode(reader)?;
+        let value =
+            char::from_u32(n).ok_or_else(|| cbor4ii::core::dec::Error::Mismatch {
+                name: &"Token::Delim",
+                found: 0,
+            })?;
+        Ok(WithChar(value))
+    }
 }
--- crates/swc_css_ast/src/token.rs
+++ crates/swc_css_ast/src/token.rs
@@ -96,6 +96,10 @@ pub struct DimensionToken {
         __S::Error: rkyv::rancor::Source))
 )]
 #[cfg_attr(feature = "serde-impl", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "encoding-impl",
+    derive(::swc_common::Encode, ::swc_common::Decode)
+)]
 pub enum Token {
     Ident {
         value: Atom,
@@ -134,6 +138,10 @@ pub enum Token {
         raw: Atom,
     },
     Delim {
+        #[cfg_attr(
+            feature = "encoding-impl",
+            encoding(with = "::swc_common::serializer::WithChar")
+        )]
         value: char,
     },
     Number {
@@ -146,7 +154,9 @@ pub enum Token {
         value: f64,
         raw: Atom,
     },
-    Dimension(Box<DimensionToken>),
+    Dimension {
+        dimension: Box<DimensionToken>
+    },
     /// One or more whitespace.
     WhiteSpace {
         value: Atom,
@@ -247,7 +257,7 @@ impl Hash for Token {
                 integer_decode(*value).hash(state);
                 raw.hash(state);
             }
-            Token::Dimension(dimension) => {
+            Token::Dimension { dimension } => {
                 integer_decode(dimension.value).hash(state);
                 dimension.unit.hash(state);
                 dimension.type_flag.hash(state);
@@ -273,220 +283,3 @@ impl Hash for Token {
 }
 
 impl Eq for Token {}
-
-#[cfg(feature = "encoding-impl")]
-impl cbor4ii::core::enc::Encode for Token {
-    fn encode<W: cbor4ii::core::enc::Write>(
-        &self,
-        writer: &mut W,
-    ) -> Result<(), cbor4ii::core::enc::Error<W::Error>> {
-        <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-
-        match self {
-            Token::Ident { value, raw } => {
-                1u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::Function { value, raw } => {
-                2u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::AtKeyword { value, raw } => {
-                3u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::Hash { value, raw, is_id } => {
-                4u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(3, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-                is_id.encode(writer)?;
-            }
-            Token::String { value, raw } => {
-                5u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::BadString { raw } => {
-                6u32.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::Url { value, raw } => {
-                7u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::BadUrl { raw } => {
-                8u32.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::Delim { value } => {
-                9u32.encode(writer)?;
-                let n = u32::from(*value);
-                n.encode(writer)?;
-            }
-            Token::Number {
-                value,
-                raw,
-                type_flag,
-            } => {
-                10u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(3, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-                type_flag.encode(writer)?;
-            }
-            Token::Percentage { value, raw } => {
-                11u32.encode(writer)?;
-                <cbor4ii::core::types::Array<()>>::bounded(2, writer)?;
-                value.encode(writer)?;
-                raw.encode(writer)?;
-            }
-            Token::Dimension(dimension) => {
-                12u32.encode(writer)?;
-                dimension.encode(writer)?;
-            }
-            Token::WhiteSpace { value } => {
-                13u32.encode(writer)?;
-                value.encode(writer)?;
-            }
-            Token::CDO => 14u32.encode(writer)?,
-            Token::CDC => 15u32.encode(writer)?,
-            Token::Colon => 16u32.encode(writer)?,
-            Token::Semi => 17u32.encode(writer)?,
-            Token::Comma => 18u32.encode(writer)?,
-            Token::LBracket => 19u32.encode(writer)?,
-            Token::RBracket => 20u32.encode(writer)?,
-            Token::LParen => 21u32.encode(writer)?,
-            Token::RParen => 22u32.encode(writer)?,
-            Token::LBrace => 23u32.encode(writer)?,
-            Token::RBrace => 24u32.encode(writer)?,
-        }
-
-        Ok(())
-    }
-}
-
-#[cfg(feature = "encoding-impl")]
-impl<'de> cbor4ii::core::dec::Decode<'de> for Token {
-    fn decode<R: cbor4ii::core::dec::Read<'de>>(
-        reader: &mut R,
-    ) -> Result<Self, cbor4ii::core::dec::Error<R::Error>> {
-        let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-        debug_assert_eq!(len, Some(2));
-        let tag = u32::decode(reader)?;
-
-        match tag {
-            1 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = Atom::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                Ok(Token::Ident { value, raw })
-            }
-            2 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = Atom::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                Ok(Token::Function { value, raw })
-            }
-            3 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = Atom::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                Ok(Token::AtKeyword { value, raw })
-            }
-            4 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(3));
-                let value = Atom::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                let is_id = bool::decode(reader)?;
-                Ok(Token::Hash { is_id, value, raw })
-            }
-            5 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = Atom::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                Ok(Token::String { value, raw })
-            }
-            6 => {
-                let raw = Atom::decode(reader)?;
-                Ok(Token::BadString { raw })
-            }
-            7 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = Atom::decode(reader)?;
-                let raw = <Box<UrlKeyValue>>::decode(reader)?;
-                Ok(Token::Url { value, raw })
-            }
-            8 => {
-                let raw = Atom::decode(reader)?;
-                Ok(Token::BadUrl { raw })
-            }
-            9 => {
-                let n = u32::decode(reader)?;
-                let value =
-                    char::from_u32(n).ok_or_else(|| cbor4ii::core::dec::Error::Mismatch {
-                        name: &"Token::Delim",
-                        found: 0,
-                    })?;
-                Ok(Token::Delim { value })
-            }
-            10 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(3));
-                let value = f64::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                let type_flag = NumberType::decode(reader)?;
-                Ok(Token::Number {
-                    value,
-                    raw,
-                    type_flag,
-                })
-            }
-            11 => {
-                let len = <cbor4ii::core::types::Array<()>>::len(reader)?;
-                debug_assert_eq!(len, Some(2));
-                let value = f64::decode(reader)?;
-                let raw = Atom::decode(reader)?;
-                Ok(Token::Percentage { value, raw })
-            }
-            12 => {
-                let value = <Box<DimensionToken>>::decode(reader)?;
-                Ok(Token::Dimension(value))
-            }
-            13 => {
-                let value = Atom::decode(reader)?;
-                Ok(Token::WhiteSpace { value })
-            }
-            14 => Ok(Token::CDO),
-            15 => Ok(Token::CDC),
-            16 => Ok(Token::Colon),
-            17 => Ok(Token::Semi),
-            18 => Ok(Token::Comma),
-            19 => Ok(Token::LBracket),
-            20 => Ok(Token::RBracket),
-            21 => Ok(Token::LParen),
-            22 => Ok(Token::RParen),
-            23 => Ok(Token::LBrace),
-            24 => Ok(Token::RBrace),
-            _ => Err(cbor4ii::core::dec::Error::Mismatch {
-                name: &"Token",
-                found: 0,
-            }),
-        }
-    }
-}
