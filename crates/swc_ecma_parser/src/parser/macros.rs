1318f2aa5fbd2c01869794d9a027675cdc5939c7
CPunisher <1343316114@qq.com>
2025-10-11 17:06:42 +0800

    Fix parser/typescript.rs

--- crates/swc_ecma_parser/src/parser/macros.rs
+++ crates/swc_ecma_parser/src/parser/macros.rs
@@ -7,13 +7,16 @@ macro_rules! trace_cur {
 }
 
 macro_rules! syntax_error {
+    ($p:expr, $err:expr) => {
+        syntax_error!($p, $p.input().cur_span(), $err)
+    };
     ($p:expr, $span:expr, $err:expr) => {{
         let err = $crate::error::Error::new($span, $err);
         {
             let cur = $p.input().cur();
-            if cur == &Token::Error {
-                let err = $p.input_mut().expect_error_token_and_bump();
-                $p.emit_error(err);
+            if cur.is_error() {
+                let error = $p.input_mut().expect_error_token_and_bump();
+                $p.emit_error(error);
             }
         }
         if cfg!(feature = "debug") {
@@ -29,16 +32,74 @@ macro_rules! syntax_error {
     }};
 }
 
+macro_rules! expect {
+    ($p:expr, $t:expr) => {{
+        if !$p.input_mut().eat($t) {
+            let span = $p.input().cur_span();
+            let cur = $p.input_mut().dump_cur();
+            syntax_error!(
+                $p,
+                span,
+                $crate::error::SyntaxError::Expected(format!("{:?}", $t), cur)
+            )
+        }
+    }};
+}
+
 macro_rules! unexpected {
     ($p:expr, $expected:literal) => {{
         let got = $p.input_mut().dump_cur();
         syntax_error!(
             $p,
             $p.input().cur_span(),
-            SyntaxError::Unexpected {
+            $crate::error::SyntaxError::Unexpected {
                 got,
                 expected: $expected
             }
         )
+    }};
+}
+
+macro_rules! debug_tracing {
+    ($p:expr, $name:tt) => {{
+        #[cfg(feature = "debug")]
+        {
+            tracing::span!(
+                tracing::Level::ERROR,
+                $name,
+                cur = tracing::field::debug(&$p.input.cur())
+            )
+            .entered()
+        }
+    }};
+}
+
+macro_rules! peek {
+    ($p:expr) => {{
+        debug_assert!(
+            !$p.input().cur().is_eof(),
+            "parser should not call peek() without knowing current token.
+Current token is {:?}",
+            $p.input().cur(),
+        );
+        $p.input_mut().peek()
+    }};
+}
+
+macro_rules! return_if_arrow {
+    ($p:expr, $expr:expr) => {{
+        // FIXME:
+        //
+        //
+
+        // let is_cur = match $p.state.potential_arrow_start {
+        //     Some(start) => $expr.span.lo() == start,
+        //     None => false
+        // };
+        // if is_cur {
+        if let Expr::Arrow { .. } = *$expr {
+            return Ok($expr);
+        }
+        // }
     }};
 }
