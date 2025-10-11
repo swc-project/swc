8f52e7ac82c3328a4eb47a99f6c7525ec12a7f22
CPunisher <1343316114@qq.com>
2025-10-11 15:17:43 +0800

    Fix lexer/mod.rs

---
+++ crates/swc_ecma_parser/src/lexer/char_ext.rs
@@ -0,0 +1,179 @@
+use std::iter::FusedIterator;
+
+use arrayvec::ArrayVec;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Char(u32);
+
+impl From<char> for Char {
+    fn from(c: char) -> Self {
+        Char(c as u32)
+    }
+}
+
+impl From<u32> for Char {
+    fn from(c: u32) -> Self {
+        Char(c)
+    }
+}
+
+pub struct CharIter(ArrayVec<char, 12>);
+
+/// Ported from https://github.com/web-infra-dev/oxc/blob/99a4816ce7b6132b2667257984f9d92ae3768f03/crates/oxc_parser/src/lexer/mod.rs#L1349-L1374
+impl IntoIterator for Char {
+    type IntoIter = CharIter;
+    type Item = char;
+
+    #[allow(unsafe_code)]
+    fn into_iter(self) -> Self::IntoIter {
+        //        // TODO: Check if this is correct
+        //        fn to_char(v: u8) -> char {
+        //            char::from_digit(v as _, 16).unwrap_or('0')
+        //        }
+
+        CharIter(match char::from_u32(self.0) {
+            Some(c) => {
+                let mut buf = ArrayVec::new();
+                // Safety: we can make sure that `buf` has enough capacity
+                unsafe {
+                    buf.push_unchecked(c);
+                }
+                buf
+            }
+            None => {
+                let mut buf = ArrayVec::new();
+
+                let high = self.0 & 0xffff0000 >> 16;
+
+                let low = self.0 & 0x0000ffff;
+
+                // The second code unit of a surrogate pair is always in the range from 0xDC00
+                // to 0xDFFF, and is called a low surrogate or a trail surrogate.
+                if !(0xdc00..=0xdfff).contains(&low) {
+                    // Safety: we can make sure that `buf` has enough capacity
+                    unsafe {
+                        buf.push_unchecked('\\');
+                        buf.push_unchecked('u');
+                        for c in format!("{high:x}").chars() {
+                            buf.push_unchecked(c);
+                        }
+                        buf.push_unchecked('\\');
+                        buf.push_unchecked('u');
+                        for c in format!("{low:x}").chars() {
+                            buf.push_unchecked(c);
+                        }
+                    }
+                } else {
+                    // `https://tc39.es/ecma262/#sec-utf16decodesurrogatepair`
+                    let astral_code_point = (high - 0xd800) * 0x400 + low - 0xdc00 + 0x10000;
+
+                    // Safety: we can make sure that `buf` has enough capacity
+                    unsafe {
+                        buf.push_unchecked('\\');
+                        buf.push_unchecked('u');
+                        for c in format!("{astral_code_point:x}").chars() {
+                            buf.push_unchecked(c);
+                        }
+                    }
+                }
+
+                buf
+            }
+        })
+    }
+}
+
+impl Iterator for CharIter {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.is_empty() {
+            None
+        } else {
+            Some(self.0.remove(0))
+        }
+    }
+}
+
+impl FusedIterator for CharIter {}
+
+/// Implemented for `char`.
+pub trait CharExt: Copy {
+    fn to_char(self) -> Option<char>;
+
+    /// Test whether a given character code starts an identifier.
+    ///
+    /// https://tc39.github.io/ecma262/#prod-IdentifierStart
+    #[inline]
+    fn is_ident_start(self) -> bool {
+        let c = match self.to_char() {
+            Some(c) => c,
+            None => return false,
+        };
+        swc_ecma_ast::Ident::is_valid_start(c)
+    }
+
+    /// Test whether a given character is part of an identifier.
+    #[inline]
+    fn is_ident_part(self) -> bool {
+        let c = match self.to_char() {
+            Some(c) => c,
+            None => return false,
+        };
+        swc_ecma_ast::Ident::is_valid_continue(c)
+    }
+
+    /// See https://tc39.github.io/ecma262/#sec-line-terminators
+    #[inline]
+    fn is_line_terminator(self) -> bool {
+        let c = match self.to_char() {
+            Some(c) => c,
+            None => return false,
+        };
+        matches!(c, '\r' | '\n' | '\u{2028}' | '\u{2029}')
+    }
+
+    /// See https://tc39.github.io/ecma262/#sec-literals-string-literals
+    #[inline]
+    fn is_line_break(self) -> bool {
+        let c = match self.to_char() {
+            Some(c) => c,
+            None => return false,
+        };
+        matches!(c, '\r' | '\n')
+    }
+
+    /// See https://tc39.github.io/ecma262/#sec-white-space
+    #[inline]
+    fn is_ws(self) -> bool {
+        let c = match self.to_char() {
+            Some(c) => c,
+            None => return false,
+        };
+        match c {
+            '\u{0009}' | '\u{000b}' | '\u{000c}' | '\u{0020}' | '\u{00a0}' | '\u{feff}' => true,
+            _ => {
+                if self.is_line_terminator() {
+                    // NOTE: Line terminator is not whitespace.
+                    false
+                } else {
+                    c.is_whitespace()
+                }
+            }
+        }
+    }
+}
+
+impl CharExt for Char {
+    #[inline(always)]
+    fn to_char(self) -> Option<char> {
+        char::from_u32(self.0)
+    }
+}
+
+impl CharExt for char {
+    #[inline(always)]
+    fn to_char(self) -> Option<char> {
+        Some(self)
+    }
+}
