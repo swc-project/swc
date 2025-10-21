swc-project/swc

commit 4a55bea8543ee0e3247da4095ec6420611d55c38

feat(es/minifier): Inline arrow with call expr (#11021)
**Related issue:**

 - Closes #11007

crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use rustc_hash::{FxHashMap, FxHashSet};
 use swc_atoms::atom;
 use swc_common::{util::take::Take, EqIgnoreSpan, Mark};
@@ -989,6 +991,16 @@ fn is_arrow_body_simple_enough_for_copy(e: &Expr) -> Option<u8> {
         Expr::Ident(..) | Expr::Lit(..) => return Some(1),
         Expr::Member(MemberExpr { obj, prop, .. }) if !prop.is_computed() => {
             return Some(is_arrow_body_simple_enough_for_copy(obj)? + 2)
+        }
+        Expr::Call(c) => {
+            let mut cost = is_arrow_body_simple_enough_for_copy(c.callee.as_expr()?.deref())?;
+            for arg in &c.args {
+                let arg_cost = is_arrow_body_simple_enough_for_copy(&arg.expr)?;
+                cost += arg_cost;
+                cost += if arg.spread.is_some() { 3 } else { 0 };
+            }
+
+            return Some(cost + 2);
         }
         Expr::Unary(u) => return Some(is_arrow_body_simple_enough_for_copy(&u.arg)? + 1),
 

crates/swc_ecma_minifier/tests/fixture/issues/11007/input.js
@@ -1,0 +1,10 @@
+const profile = (_s, fn) => {
+    return fn();
+};
+
+profile("trace1", () => {
+    someFunction({
+        args1: profile("trace2", () => JSON.stringify(someObj)), // if this line removed, the issue will not reproduce
+        args2: JSON.stringify(someObj),
+    });
+});

crates/swc_ecma_minifier/tests/fixture/issues/11007/output.js
@@ -1,0 +1,4 @@
+someFunction({
+    args1: JSON.stringify(someObj),
+    args2: JSON.stringify(someObj)
+});

crates/swc/tests/tsc-references/destructuringEvaluationOrder(target=es2015).2.minified.js
@@ -3,9 +3,9 @@ import { _ as _extends } from "@swc/helpers/_/_extends";
 import { _ as _object_destructuring_empty } from "@swc/helpers/_/_object_destructuring_empty";
 import { _ as _object_without_properties } from "@swc/helpers/_/_object_without_properties";
 import { _ as _to_property_key } from "@swc/helpers/_/_to_property_key";
-let trace = [], order = (n)=>trace.push(n), [{ [order(1)]: x } = order(0)] = [], [{ [order(1)]: y } = order(0)] = [
+let trace = [], [{ [trace.push(1)]: x } = trace.push(0)] = [], [{ [trace.push(1)]: y } = trace.push(0)] = [
     {}
-], _ref = {}, _order = order(0), _order1 = order(2), { [_order]: { [_order1]: z } = order(1) } = _ref;
+], _ref = {}, _order = trace.push(0), _order1 = trace.push(2), { [_order]: { [_order1]: z } = trace.push(1) } = _ref;
 _object_without_properties(_ref, [
     _order
 ].map(_to_property_key));

crates/swc_ecma_minifier/tests/benches-full/terser.js
@@ -155,7 +155,7 @@
     }
     // Subset of regexps that is not going to cause regexp based DDOS
     // https://owasp.org/www-community/attacks/Regular_expression_Denial_of_Service_-_ReDoS
-    const re_safe_regexp = /^[\\/|\0\s\w^$.[\]()]*$/, regexp_is_safe = (source)=>re_safe_regexp.test(source);
+    const re_safe_regexp = /^[\\/|\0\s\w^$.[\]()]*$/;
     function has_annotation(node, annotation) {
         return node._annotations & annotation;
     }
@@ -2093,8 +2093,8 @@
         }
         // Return a string summary of the token for node.js console.log
         [Symbol.for("nodejs.util.inspect.custom")](_depth, options) {
-            const special = (str)=>options.stylize(str, "special"), quote = "string" == typeof this.value && this.value.includes("`") ? "'" : "`", value = `${quote}${this.value}${quote}`;
-            return `${special("[AST_Token")} ${value} at ${this.line}:${this.col}${special("]")}`;
+            const quote = "string" == typeof this.value && this.value.includes("`") ? "'" : "`", value = `${quote}${this.value}${quote}`;
+            return `${options.stylize("[AST_Token", "special")} ${value} at ${this.line}:${this.col}${options.stylize("]", "special")}`;
         }
         get nlb() {
             return !!(0b0001 & this.flags);
@@ -7465,8 +7465,8 @@
     def_eval(AST_BigInt, function() {
         return supports_bigint ? BigInt(this.value) : this;
     }), def_eval(AST_RegExp, function(compressor) {
-        let evaluated = compressor.evaluated_regexps.get(this.value);
-        if (void 0 === evaluated && regexp_is_safe(this.value.source)) {
+        let source, evaluated = compressor.evaluated_regexps.get(this.value);
+        if (void 0 === evaluated && (source = this.value.source, re_safe_regexp.test(source))) {
             try {
                 const { source, flags } = this.value;
                 evaluated = new RegExp(source, flags);
@@ -10126,11 +10126,12 @@
                     }).optimize(compressor);
                     break;
                 case "RegExp":
+                    let source;
                     var params = [];
                     if (self1.args.length >= 1 && self1.args.length <= 2 && self1.args.every((arg)=>{
                         var value = arg.evaluate(compressor);
                         return params.push(value), arg !== value;
-                    }) && regexp_is_safe(params[0])) {
+                    }) && (source = params[0], re_safe_regexp.test(source))) {
                         let [source, flags] = params;
                         const rx = make_node(AST_RegExp, self1, {
                             value: {

commit bd55d30811d5f421b43dc70fd9c05d2f2b56a049

fix(es/minifier): fix inlining of hoisted functions (#11159)
**Description:**

A regression from https://github.com/swc-project/swc/pull/11021

**Related issue:**

Closes https://github.com/swc-project/swc/issues/11158

crates/swc_ecma_minifier/src/compress/optimize/inline.rs
@@ -317,7 +317,32 @@ impl Optimizer<'_> {
                     ..
                 } = **usage;
                 let mut inc_usage = || {
-                    if let Expr::Ident(i) = &*init {
+                    if let Some(i) = match &*init {
+                        // The condition above filters `init` for literals, idents, and arrow
+                        // functions.
+                        Expr::Ident(i) => Some(i),
+                        Expr::Arrow(arr) => match &*arr.body {
+                            BlockStmtOrExpr::Expr(e) => match &**e {
+                                Expr::Call(i) => i.callee.as_expr().and_then(|e| e.as_ident()),
+                                _ => None,
+                            },
+                            BlockStmtOrExpr::BlockStmt(BlockStmt { stmts, .. }) => {
+                                if let &[Stmt::Return(ReturnStmt { arg: Some(arg), .. })] =
+                                    &&**stmts
+                                {
+                                    match &**arg {
+                                        Expr::Call(i) => {
+                                            i.callee.as_expr().and_then(|e| e.as_ident())
+                                        }
+                                        _ => None,
+                                    }
+                                } else {
+                                    None
+                                }
+                            }
+                        },
+                        _ => None,
+                    } {
                         if let Some(u) = self.data.vars.get_mut(&i.to_id()) {
                             u.flags |= flags & VarUsageInfoFlags::USED_AS_ARG;
                             u.flags |= flags & VarUsageInfoFlags::USED_AS_REF;

crates/swc_ecma_minifier/tests/fixture/issues/11158/input.js
@@ -1,0 +1,9 @@
+(() => {
+    const gen = () => foo(12);
+    function foo(length) {
+        return length;
+    }
+    const a = `tmp-${gen()}-a`,
+        b = `tmp-${gen()}-b`;
+    console.log(a, b);
+})();

crates/swc_ecma_minifier/tests/fixture/issues/11158/output.js
@@ -1,0 +1,1 @@
+console.log("tmp-12-a", "tmp-12-b");