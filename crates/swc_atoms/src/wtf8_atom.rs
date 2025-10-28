swc-project/swc

commit 845512c67819cd37bb25601d34bd5b1ac79afca3

fix(es/ast): Fix unicode unpaired surrogates handling (#11144)
**Description:**

This PR reapplied https://github.com/swc-project/swc/pull/10987 and
replaced `lone_surrogates: bool` mark with `Wtf8Atom`. `lone_surrogates`
introduced in https://github.com/swc-project/swc/pull/10987 is more of
an implicit mark, whereas the use of `Wtf8Atom` makes it more explicit
where users need to explicitly call `to_string_lossy` to get the lossy
UTF-8 result, making a huge difference from the original implementation
where users need to convert `\uFFFDxxx` into the correct unicode.

**BREAKING CHANGE:**

Both `cooked` in `TemplateElement` and `value` in `StringLiteral` are
replaced with `Wtf8Atom` introduced in
https://github.com/swc-project/swc/pull/11104.

`Wtf8Atom` does not expose a `to_string` method like the old `Atom`
does. Internally, it stores the code points of the characters. You can
call `code_points()` to get an iterator of the code points or call
`to_string_lossy()` to get an lossy string in which all unpaired
surrogates are replaced with `U+FFFD`(Replacement Character).

crates/swc_atoms/src/wtf8_atom.rs
@@ -1,0 +1,237 @@
+use std::{
+    borrow::Cow,
+    fmt::{self, Formatter},
+    ops::Deref,
+};
+
+use hstr::wtf8::{Wtf8, Wtf8Buf};
+use serde::Serializer;
+
+use crate::Atom;
+
+/// Clone-on-write WTF-8 string.
+///
+///
+/// See [tendril] for more details.
+#[derive(Clone, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "rkyv-impl", derive(bytecheck::CheckBytes))]
+#[repr(transparent)]
+pub struct Wtf8Atom(pub(super) hstr::Wtf8Atom);
+
+#[cfg(feature = "arbitrary")]
+#[cfg_attr(docsrs, doc(cfg(feature = "arbitrary")))]
+impl<'a> arbitrary::Arbitrary<'a> for Wtf8Atom {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        let sym = u.arbitrary::<Vec<u8>>()?;
+        if sym.is_empty() {
+            return Err(arbitrary::Error::NotEnoughData);
+        }
+        Ok(Self(hstr::Wtf8Atom::from(unsafe {
+            Wtf8Buf::from_bytes_unchecked(sym)
+        })))
+    }
+}
+
+fn _asserts() {
+    // let _static_assert_size_eq = std::mem::transmute::<Atom, [usize; 1]>;
+
+    fn _assert_send<T: Send>() {}
+    fn _assert_sync<T: Sync>() {}
+
+    _assert_sync::<Wtf8Atom>();
+    _assert_send::<Wtf8Atom>();
+}
+
+impl Wtf8Atom {
+    /// Creates a new [Wtf8Atom] from a string.
+    #[inline(always)]
+    pub fn new<S>(s: S) -> Self
+    where
+        hstr::Wtf8Atom: From<S>,
+    {
+        Wtf8Atom(hstr::Wtf8Atom::from(s))
+    }
+
+    pub fn as_wtf8(&self) -> &Wtf8 {
+        &self.0
+    }
+
+    pub fn as_atom(&self) -> Option<&Atom> {
+        if self.as_str().is_some() {
+            Some(unsafe { &*(self as *const Wtf8Atom as *const Atom) })
+        } else {
+            None
+        }
+    }
+
+    /// Returns the UTF-8 [`Atom`] representation, borrowing when possible.
+    pub fn to_atom_lossy(&self) -> Cow<'_, Atom> {
+        if let Some(atom) = self.as_atom() {
+            return Cow::Borrowed(atom);
+        }
+        Cow::Owned(Atom::new(self.to_string_lossy()))
+    }
+
+    /// Try to convert this to a UTF-8 [Atom].
+    ///
+    /// Returns [Atom] if the string is valid UTF-8, otherwise returns
+    /// the original [Wtf8Atom].
+    pub fn try_into_atom(self) -> Result<Atom, Wtf8Atom> {
+        self.0.try_into_atom().map(Atom).map_err(Wtf8Atom)
+    }
+}
+
+impl Deref for Wtf8Atom {
+    type Target = Wtf8;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl fmt::Debug for Wtf8Atom {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&**self, f)
+    }
+}
+
+impl PartialOrd for Wtf8Atom {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Wtf8Atom {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.as_wtf8().cmp(other.as_wtf8())
+    }
+}
+
+impl<T> PartialEq<T> for Wtf8Atom
+where
+    hstr::Wtf8Atom: PartialEq<T>,
+    T: ?Sized,
+{
+    fn eq(&self, other: &T) -> bool {
+        self.0.eq(other)
+    }
+}
+
+impl<T> From<T> for Wtf8Atom
+where
+    hstr::Wtf8Atom: From<T>,
+{
+    fn from(s: T) -> Self {
+        Wtf8Atom::new(s)
+    }
+}
+
+impl From<&Wtf8Atom> for Wtf8Buf {
+    fn from(s: &Wtf8Atom) -> Self {
+        // SAFETY: `Wtf8Atom` is guaranteed to be valid WTF-8 byte sequence.
+        unsafe { Wtf8Buf::from_bytes_unchecked(s.as_bytes().to_vec()) }
+    }
+}
+
+impl serde::ser::Serialize for Wtf8Atom {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de> serde::de::Deserialize<'de> for Wtf8Atom {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        hstr::Wtf8Atom::deserialize(deserializer).map(Wtf8Atom)
+    }
+}
+
+/// NOT A PUBLIC API
+#[cfg(feature = "rkyv-impl")]
+impl rkyv::Archive for Wtf8Atom {
+    type Archived = rkyv::vec::ArchivedVec<u8>;
+    type Resolver = rkyv::vec::VecResolver;
+
+    #[allow(clippy::unit_arg)]
+    fn resolve(&self, resolver: Self::Resolver, out: rkyv::Place<Self::Archived>) {
+        rkyv::vec::ArchivedVec::<u8>::resolve_from_slice(self.as_bytes(), resolver, out)
+    }
+}
+
+/// NOT A PUBLIC API
+#[cfg(feature = "rkyv-impl")]
+impl<S: rancor::Fallible + rkyv::ser::Writer + rkyv::ser::Allocator + ?Sized> rkyv::Serialize<S>
+    for Wtf8Atom
+where
+    <S as rancor::Fallible>::Error: rancor::Source,
+{
+    fn serialize(&self, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
+        rkyv::vec::ArchivedVec::<u8>::serialize_from_slice(self.as_bytes(), serializer)
+    }
+}
+
+/// NOT A PUBLIC API
+#[cfg(feature = "rkyv-impl")]
+impl<D> rkyv::Deserialize<Wtf8Atom, D> for rkyv::vec::ArchivedVec<u8>
+where
+    D: ?Sized + rancor::Fallible,
+    <D as rancor::Fallible>::Error: rancor::Source,
+{
+    fn deserialize(
+        &self,
+        deserializer: &mut D,
+    ) -> Result<Wtf8Atom, <D as rancor::Fallible>::Error> {
+        let s: Vec<u8> = self.deserialize(deserializer)?;
+
+        Ok(Wtf8Atom::new(
+            // SAFETY: `ArchivedVec<u8>` is guaranteed to be serialized with `Wtf8Atom` byte
+            // sequence.  `Wtf8Atom` byte sequence is identical to `Wtf8` byte sequence.
+            unsafe { Wtf8::from_bytes_unchecked(&s) },
+        ))
+    }
+}
+
+/// noop
+#[cfg(feature = "shrink-to-fit")]
+impl shrink_to_fit::ShrinkToFit for Wtf8Atom {
+    #[inline(always)]
+    fn shrink_to_fit(&mut self) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_atom_lossy_returns_borrowed_for_utf8() {
+        let atom = Atom::from("swc");
+        let wtf = Wtf8Atom::from(atom.clone());
+
+        match wtf.to_atom_lossy() {
+            Cow::Borrowed(borrowed) => assert_eq!(borrowed, &atom),
+            Cow::Owned(_) => panic!("expected a borrowed Atom for valid UTF-8 input"),
+        }
+    }
+
+    #[test]
+    fn to_atom_lossy_returns_owned_for_invalid_utf8() {
+        let invalid_bytes = vec![0xed, 0xa0, 0x80];
+        let invalid = unsafe { Wtf8Buf::from_bytes_unchecked(invalid_bytes.clone()) };
+        let wtf = Wtf8Atom::new(invalid);
+
+        let lossy = wtf.to_string_lossy();
+
+        match wtf.to_atom_lossy() {
+            Cow::Borrowed(_) => panic!("expected an owned Atom for invalid UTF-8 input"),
+            Cow::Owned(atom) => {
+                assert_eq!(atom.as_ref(), lossy);
+            }
+        }
+    }
+}

crates/swc_ecma_quote_macros/src/ast/lit.rs
@@ -1,7 +1,7 @@
 use proc_macro2::Span;
-use swc_atoms::Atom;
+use swc_atoms::{Atom, Wtf8Atom};
 use swc_ecma_ast::*;
-use syn::{parse_quote, ExprLit, LitBool, LitFloat};
+use syn::{parse_quote, ExprLit, LitBool, LitByteStr, LitFloat};
 
 use super::ToCode;
 use crate::{builder::Builder, ctxt::Ctx};
@@ -11,9 +11,11 @@ fail_todo!(JSXText);
 
 impl ToCode for Str {
     fn to_code(&self, cx: &crate::ctxt::Ctx) -> syn::Expr {
-        if let Some(var_name) = self.value.strip_prefix('$') {
-            if let Some(var) = cx.var(crate::ctxt::VarPos::Str, var_name) {
-                return var.get_expr();
+        if let Some(var_name) = self.value.as_str() {
+            if let Some(var_name) = var_name.strip_prefix('$') {
+                if let Some(var) = cx.var(crate::ctxt::VarPos::Str, var_name) {
+                    return var.get_expr();
+                }
             }
         }
 
@@ -33,6 +35,15 @@ impl ToCode for Atom {
     fn to_code(&self, _: &Ctx) -> syn::Expr {
         let val = &**self;
         parse_quote!(swc_core::atoms::atom!(#val))
+    }
+}
+
+impl ToCode for Wtf8Atom {
+    fn to_code(&self, _: &Ctx) -> syn::Expr {
+        let bytes_literal = LitByteStr::new(self.as_bytes(), Span::call_site());
+        parse_quote!(swc_atoms::wtf8::Wtf8Atom::from(unsafe {
+            swc_atoms::wtf8::Wtf8::from_bytes_unchecked(#bytes_literal)
+        }))
     }
 }
 

crates/hstr/src/wtf8_atom.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::Debug,
     hash::Hash,
-    mem::{forget, transmute},
+    mem::{forget, transmute, ManuallyDrop},
     ops::Deref,
 };
 
@@ -10,14 +10,15 @@ use debug_unreachable::debug_unreachable;
 use crate::{
     macros::{get_hash, impl_from_alias, partial_eq},
     tagged_value::TaggedValue,
-    wtf8::Wtf8,
-    DYNAMIC_TAG, INLINE_TAG, LEN_MASK, LEN_OFFSET, TAG_MASK,
+    wtf8::{CodePoint, Wtf8, Wtf8Buf},
+    Atom, DYNAMIC_TAG, INLINE_TAG, LEN_MASK, LEN_OFFSET, TAG_MASK,
 };
 
 /// A WTF-8 encoded atom. This is like [Atom], but can contain unpaired
 /// surrogates.
 ///
 /// [Atom]: crate::Atom
+#[repr(transparent)]
 pub struct Wtf8Atom {
     pub(crate) unsafe_data: TaggedValue,
 }
@@ -29,6 +30,21 @@ impl Wtf8Atom {
         Self: From<S>,
     {
         Self::from(s)
+    }
+
+    /// Try to convert this to a UTF-8 [Atom].
+    ///
+    /// Returns [Atom] if the string is valid UTF-8, otherwise returns
+    /// the original [Wtf8Atom].
+    pub fn try_into_atom(self) -> Result<Atom, Wtf8Atom> {
+        if self.as_str().is_some() {
+            let atom = ManuallyDrop::new(self);
+            Ok(Atom {
+                unsafe_data: atom.unsafe_data,
+            })
+        } else {
+            Err(self)
+        }
     }
 
     #[inline(always)]
@@ -59,7 +75,7 @@ unsafe impl Sync for Wtf8Atom {}
 impl Debug for Wtf8Atom {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(&self.to_string_lossy(), f)
+        Debug::fmt(&**self, f)
     }
 }
 
@@ -69,7 +85,37 @@ impl serde::ser::Serialize for Wtf8Atom {
     where
         S: serde::ser::Serializer,
     {
-        serializer.serialize_bytes(self.as_bytes())
+        fn convert_wtf8_to_raw(s: &Wtf8) -> String {
+            let mut result = String::new();
+            let mut iter = s.code_points().peekable();
+
+            while let Some(code_point) = iter.next() {
+                if let Some(c) = code_point.to_char() {
+                    // Escape literal '\u' sequences to avoid ambiguity with surrogate encoding.
+                    // Without this escaping, we couldn't distinguish between:
+                    // - JavaScript's "\uD800" (actual unpaired surrogate)
+                    // - JavaScript's "\\uD800" (literal text '\uD800')
+                    //
+                    // By escaping literal '\u' to '\\u', we ensure:
+                    // - Unpaired surrogates serialize as '\uXXXX'
+                    // - Literal '\u' text serializes as '\\uXXXX'
+                    if c == '\\' && iter.peek().map(|cp| cp.to_u32()) == Some('u' as u32) {
+                        iter.next(); // skip 'u'
+                        result.push_str("\\\\u");
+                    } else {
+                        result.push(c)
+                    }
+                } else {
+                    // Unpaired surrogates can't be represented in valid UTF-8,
+                    // so encode them as '\uXXXX' for JavaScript compatibility
+                    result.push_str(format!("\\u{:04X}", code_point.to_u32()).as_str());
+                }
+            }
+
+            result
+        }
+
+        serializer.serialize_str(&convert_wtf8_to_raw(self))
     }
 }
 
@@ -79,7 +125,79 @@ impl<'de> serde::de::Deserialize<'de> for Wtf8Atom {
     where
         D: serde::Deserializer<'de>,
     {
-        String::deserialize(deserializer).map(Self::new)
+        fn convert_wtf8_string_to_wtf8(s: String) -> Wtf8Buf {
+            let mut iter = s.chars().peekable();
+            let mut result = Wtf8Buf::with_capacity(s.len());
+
+            // This function reverses the encoding done in serialize.
+            // It handles two cases:
+            // 1. '\uXXXX' - Decode as an unpaired surrogate code point
+            // 2. '\\uXXXX' - Treat as literal text '\uXXXX'
+            while let Some(c) = iter.next() {
+                if c == '\\' {
+                    if iter.peek() == Some(&'u') {
+                        // Found '\u' - might be a surrogate encoding
+                        let _ = iter.next(); // skip 'u'
+
+                        // Try to read 4 hex digits
+                        let d1 = iter.next();
+                        let d2 = iter.next();
+                        let d3 = iter.next();
+                        let d4 = iter.next();
+
+                        if d1.is_some() && d2.is_some() && d3.is_some() && d4.is_some() {
+                            let hex = format!(
+                                "{}{}{}{}",
+                                d1.unwrap(),
+                                d2.unwrap(),
+                                d3.unwrap(),
+                                d4.unwrap()
+                            );
+                            if let Ok(code_point) = u16::from_str_radix(&hex, 16) {
+                                result.push(unsafe {
+                                    CodePoint::from_u32_unchecked(code_point as u32)
+                                });
+                                continue;
+                            }
+                        }
+
+                        result.push_char('\\');
+                        result.push_char('u');
+
+                        macro_rules! push_if_some {
+                            ($expr:expr) => {
+                                if let Some(c) = $expr {
+                                    result.push_char(c);
+                                }
+                            };
+                        }
+
+                        push_if_some!(d1);
+                        push_if_some!(d2);
+                        push_if_some!(d3);
+                        push_if_some!(d4);
+                    } else if iter.peek() == Some(&'\\') {
+                        // Found '\\' - this is an escaped backslash
+                        // '\\u' should become literal '\u' text
+                        let _ = iter.next(); // skip the second '\'
+                        if iter.peek() == Some(&'u') {
+                            let _ = iter.next(); // skip 'u'
+                            result.push_char('\\');
+                            result.push_char('u');
+                        } else {
+                            result.push_str("\\\\");
+                        }
+                    } else {
+                        result.push_char(c);
+                    }
+                } else {
+                    result.push_char(c);
+                }
+            }
+            result
+        }
+
+        String::deserialize(deserializer).map(|v| convert_wtf8_string_to_wtf8(v).into())
     }
 }
 
@@ -163,6 +281,20 @@ impl PartialEq<Wtf8Atom> for Wtf8 {
     }
 }
 
+impl PartialEq<str> for Wtf8Atom {
+    #[inline]
+    fn eq(&self, other: &str) -> bool {
+        matches!(self.as_str(), Some(s) if s == other)
+    }
+}
+
+impl PartialEq<&str> for Wtf8Atom {
+    #[inline]
+    fn eq(&self, other: &&str) -> bool {
+        matches!(self.as_str(), Some(s) if s == *other)
+    }
+}
+
 impl Wtf8Atom {
     pub(super) fn get_hash(&self) -> u64 {
         get_hash!(self)
@@ -197,5 +329,226 @@ impl Wtf8Atom {
             }
             _ => 1,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::wtf8::{CodePoint, Wtf8Buf};
+
+    #[test]
+    fn test_serialize_normal_utf8() {
+        let atom = Wtf8Atom::new("Hello, world!");
+        let serialized = serde_json::to_string(&atom).unwrap();
+        assert_eq!(serialized, "\"Hello, world!\"");
+    }
+
+    #[test]
+    fn test_deserialize_normal_utf8() {
+        let json = "\"Hello, world!\"";
+        let atom: Wtf8Atom = serde_json::from_str(json).unwrap();
+        assert_eq!(atom.as_str(), Some("Hello, world!"));
+    }
+
+    #[test]
+    fn test_serialize_unpaired_high_surrogate() {
+        // Create a WTF-8 string with an unpaired high surrogate (U+D800)
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) });
+        let atom = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&atom).unwrap();
+        // The serialized output will have double escaping due to serde_json
+        assert_eq!(serialized, "\"\\\\uD800\"");
+    }
+
+    #[test]
+    fn test_serialize_unpaired_low_surrogate() {
+        // Create a WTF-8 string with an unpaired low surrogate (U+DC00)
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xdc00) });
+        let atom = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&atom).unwrap();
+        // The serialized output will have double escaping due to serde_json
+        assert_eq!(serialized, "\"\\\\uDC00\"");
+    }
+
+    #[test]
+    fn test_serialize_multiple_surrogates() {
+        // Create a WTF-8 string with multiple unpaired surrogates
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push_str("Hello ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) });
+        wtf8.push_str(" World ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xdc00) });
+        let atom = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&atom).unwrap();
+        // The serialized output will have double escaping due to serde_json
+        assert_eq!(serialized, "\"Hello \\\\uD800 World \\\\uDC00\"");
+    }
+
+    #[test]
+    fn test_serialize_literal_backslash_u() {
+        // Test that literal "\u" in the string gets escaped properly
+        let atom = Wtf8Atom::new("\\u0041");
+        let serialized = serde_json::to_string(&atom).unwrap();
+        // serde_json escapes the backslash, resulting in 4 backslashes
+        assert_eq!(serialized, "\"\\\\\\\\u0041\"");
+    }
+
+    #[test]
+    fn test_deserialize_escaped_backslash_u() {
+        // Test deserializing the escaped format for unpaired surrogates
+        let json = "\"\\\\uD800\"";
+        let atom: Wtf8Atom = serde_json::from_str(json).unwrap();
+        // This should be parsed as an unpaired surrogate
+        assert_eq!(atom.as_str(), None);
+        assert_eq!(atom.to_string_lossy(), "\u{FFFD}");
+    }
+
+    #[test]
+    fn test_deserialize_unpaired_surrogates() {
+        let json = "\"\\\\uD800\""; // Use escaped format that matches serialization
+        let atom: Wtf8Atom = serde_json::from_str(json).unwrap();
+        // Should contain an unpaired surrogate, so as_str() returns None
+        assert_eq!(atom.as_str(), None);
+        // But to_string_lossy should work
+        assert_eq!(atom.to_string_lossy(), "\u{FFFD}");
+    }
+
+    #[test]
+    fn test_round_trip_normal_string() {
+        let original = Wtf8Atom::new("Hello, 世界! 🌍");
+        let serialized = serde_json::to_string(&original).unwrap();
+        let deserialized: Wtf8Atom = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(original.as_str(), deserialized.as_str());
+    }
+
+    #[test]
+    fn test_round_trip_unpaired_surrogates() {
+        // Create a string with unpaired surrogates
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push_str("Before ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) });
+        wtf8.push_str(" Middle ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xdc00) });
+        wtf8.push_str(" After");
+        let original = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&original).unwrap();
+        let deserialized: Wtf8Atom = serde_json::from_str(&serialized).unwrap();
+
+        // Both should be equal when compared as WTF-8
+        assert_eq!(original, deserialized);
+
+        // Both should produce the same lossy string
+        assert_eq!(original.to_string_lossy(), deserialized.to_string_lossy());
+    }
+
+    #[test]
+    fn test_round_trip_mixed_content() {
+        // Create a complex string with normal text, emojis, and unpaired surrogates
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push_str("Hello 世界 🌍 ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd83d) }); // Unpaired high
+        wtf8.push_str(" test ");
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xdca9) }); // Unpaired low
+        let original = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&original).unwrap();
+        let deserialized: Wtf8Atom = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(original, deserialized);
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let atom = Wtf8Atom::new("");
+        let serialized = serde_json::to_string(&atom).unwrap();
+        assert_eq!(serialized, "\"\"");
+
+        let deserialized: Wtf8Atom = serde_json::from_str("\"\"").unwrap();
+        assert_eq!(deserialized.as_str(), Some(""));
+    }
+
+    #[test]
+    fn test_special_characters() {
+        let test_cases = vec![
+            ("\"", "\"\\\"\""),
+            ("\n\r\t", "\"\\n\\r\\t\""), // serde_json escapes control characters
+            ("\\", "\"\\\\\""),
+            ("/", "\"/\""),
+        ];
+
+        for (input, expected) in test_cases {
+            let atom = Wtf8Atom::new(input);
+            let serialized = serde_json::to_string(&atom).unwrap();
+            assert_eq!(serialized, expected, "Failed for input: {input:?}");
+
+            let deserialized: Wtf8Atom = serde_json::from_str(&serialized).unwrap();
+            assert_eq!(deserialized.as_str(), Some(input));
+        }
+    }
+
+    #[test]
+    fn test_consecutive_surrogates_not_paired() {
+        // Test that consecutive surrogates that don't form a valid pair
+        // are handled correctly
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) }); // High surrogate
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) }); // Another high surrogate
+        let atom = Wtf8Atom::from(wtf8);
+
+        let serialized = serde_json::to_string(&atom).unwrap();
+        // The serialized output will have double escaping due to serde_json
+        assert_eq!(serialized, "\"\\\\uD800\\\\uD800\"");
+
+        let deserialized: Wtf8Atom = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(atom, deserialized);
+    }
+
+    #[test]
+    fn test_deserialize_incomplete_escape() {
+        // Test handling of incomplete escape sequences from our custom format
+        let json = "\"\\\\\\\\u123\""; // Escaped backslash + incomplete sequence
+        let atom: Wtf8Atom = serde_json::from_str(json).unwrap();
+        // JSON decodes \\\\u123 to \\u123, then our deserializer sees \u123 and treats
+        // it as literal
+        assert_eq!(atom.as_str(), Some("\\u123"));
+    }
+
+    #[test]
+    fn test_deserialize_invalid_hex() {
+        // Test handling of invalid hex in escape sequences from our custom format
+        let json = "\"\\\\\\\\uGGGG\""; // Escaped backslash + invalid hex
+        let atom: Wtf8Atom = serde_json::from_str(json).unwrap();
+        // JSON decodes \\\\uGGGG to \\uGGGG, then our deserializer sees \uGGGG and
+        // treats it as literal
+        assert_eq!(atom.as_str(), Some("\\uGGGG"));
+    }
+
+    #[test]
+    fn test_try_into_atom_valid_utf8() {
+        let wtf8_atom = Wtf8Atom::new("Valid UTF-8 string");
+        let result = wtf8_atom.try_into_atom();
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().as_str(), "Valid UTF-8 string");
+    }
+
+    #[test]
+    fn test_try_into_atom_invalid_utf8() {
+        // Create an atom with unpaired surrogates
+        let mut wtf8 = Wtf8Buf::new();
+        wtf8.push(unsafe { CodePoint::from_u32_unchecked(0xd800) });
+        let wtf8_atom = Wtf8Atom::from(wtf8);
+
+        let result = wtf8_atom.try_into_atom();
+        assert!(result.is_err());
+        // Should return the original Wtf8Atom
+        let err_atom = result.unwrap_err();
+        assert_eq!(err_atom.to_string_lossy(), "\u{FFFD}");
     }
 }

crates/swc_ecma_ast/src/lit.rs
@@ -6,7 +6,7 @@ use std::{
 
 use is_macro::Is;
 use num_bigint::BigInt as BigIntValue;
-use swc_atoms::{atom, Atom};
+use swc_atoms::{Atom, Wtf8Atom};
 use swc_common::{ast_node, util::take::Take, EqIgnoreSpan, Span, DUMMY_SP};
 
 use crate::jsx::JSXText;
@@ -55,6 +55,7 @@ bridge_expr_from!(Lit, JSXText);
 
 bridge_lit_from!(Str, &'_ str);
 bridge_lit_from!(Str, Atom);
+bridge_lit_from!(Str, Wtf8Atom);
 bridge_lit_from!(Str, Cow<'_, str>);
 bridge_lit_from!(Str, String);
 bridge_lit_from!(Bool, bool);
@@ -185,7 +186,7 @@ impl From<BigIntValue> for BigInt {
 pub struct Str {
     pub span: Span,
 
-    pub value: Atom,
+    pub value: Wtf8Atom,
 
     /// Use `None` value only for transformations to avoid recalculate escaped
     /// characters in strings
@@ -196,7 +197,7 @@ impl Take for Str {
     fn dummy() -> Self {
         Str {
             span: DUMMY_SP,
-            value: atom!(""),
+            value: Wtf8Atom::default(),
             raw: None,
         }
     }
@@ -207,7 +208,7 @@ impl Take for Str {
 impl<'a> arbitrary::Arbitrary<'a> for Str {
     fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
         let span = u.arbitrary()?;
-        let value = u.arbitrary::<String>()?.into();
+        let value = u.arbitrary::<Wtf8Atom>()?.into();
         let raw = Some(u.arbitrary::<String>()?.into());
 
         Ok(Self { span, value, raw })
@@ -278,6 +279,17 @@ impl EqIgnoreSpan for Str {
 impl From<Atom> for Str {
     #[inline]
     fn from(value: Atom) -> Self {
+        Str {
+            span: DUMMY_SP,
+            value: value.into(),
+            raw: None,
+        }
+    }
+}
+
+impl From<Wtf8Atom> for Str {
+    #[inline]
+    fn from(value: Wtf8Atom) -> Self {
         Str {
             span: DUMMY_SP,
             value,
@@ -368,6 +380,8 @@ impl Take for Regex {
 #[cfg_attr(docsrs, doc(cfg(feature = "arbitrary")))]
 impl<'a> arbitrary::Arbitrary<'a> for Regex {
     fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        use swc_atoms::atom;
+
         let span = u.arbitrary()?;
         let exp = u.arbitrary::<String>()?.into();
         let flags = atom!(""); // TODO

commit 3485179196c056b913cdc7507ed5f3bb282623ee

fix(atoms): Fix broken quote macro (#11195)
**Description:**

This PR fixes a regression of `quote!` macro caused by https://github.com/swc-project/swc/pull/11144

crates/swc_atoms/src/wtf8_atom.rs
@@ -79,6 +79,20 @@ impl Wtf8Atom {
     pub fn try_into_atom(self) -> Result<Atom, Wtf8Atom> {
         self.0.try_into_atom().map(Atom).map_err(Wtf8Atom)
     }
+
+    /// Creates a new [Wtf8Atom] from a byte slice.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that `bytes` is a well-formed WTF-8 byte
+    /// sequence.
+    ///
+    /// See [hstr::wtf8::Wtf8::from_bytes_unchecked] for more details.
+    pub unsafe fn from_bytes_unchecked(bytes: &[u8]) -> Self {
+        Wtf8Atom(hstr::Wtf8Atom::from(
+            hstr::wtf8::Wtf8::from_bytes_unchecked(bytes),
+        ))
+    }
 }
 
 impl Deref for Wtf8Atom {

crates/swc_ecma_quote_macros/src/ast/lit.rs
@@ -41,9 +41,7 @@ impl ToCode for Atom {
 impl ToCode for Wtf8Atom {
     fn to_code(&self, _: &Ctx) -> syn::Expr {
         let bytes_literal = LitByteStr::new(self.as_bytes(), Span::call_site());
-        parse_quote!(swc_atoms::wtf8::Wtf8Atom::from(unsafe {
-            swc_atoms::wtf8::Wtf8::from_bytes_unchecked(#bytes_literal)
-        }))
+        parse_quote!(unsafe { swc_atoms::Wtf8Atom::from_bytes_unchecked(#bytes_literal) })
     }
 }
 