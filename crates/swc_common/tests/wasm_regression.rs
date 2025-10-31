6c3a23d39836c8d8ee68a2b453cf3cc61381af3d
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-10-31 14:57:40 +0900

    types

--- crates/swc_common/tests/wasm_regression.rs
+++ crates/swc_common/tests/wasm_regression.rs
@@ -1,1 +1,49 @@
+use ast_node::ast_node;
 
+#[ast_node]
+pub enum ExprV1 {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Null,
+    Undefined,
+    Object(ObjectV1),
+}
+
+#[ast_node("Object")]
+pub struct ObjectV1 {
+    properties: Vec<PropertyV1>,
+}
+
+#[ast_node("Property")]
+pub struct PropertyV1 {
+    key: String,
+    value: Box<ExprV1>,
+}
+
+#[ast_node]
+pub enum ExprV2 {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Null,
+    Undefined,
+    Object(ObjectV2),
+    Array(Array),
+}
+
+#[ast_node("Object")]
+pub struct ObjectV2 {
+    properties: Vec<PropertyV2>,
+}
+
+#[ast_node("Property")]
+pub struct PropertyV2 {
+    key: String,
+    value: Box<ExprV2>,
+}
+
+#[ast_node("Array")]
+pub struct Array {
+    elements: Vec<ExprV2>,
+}
