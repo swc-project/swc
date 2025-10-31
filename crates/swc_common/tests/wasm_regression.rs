8c81bf21a17089e936696553520929a7626c1aec
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-10-31 15:02:25 +0900

    Add a test

--- crates/swc_common/tests/wasm_regression.rs
+++ crates/swc_common/tests/wasm_regression.rs
@@ -1,4 +1,5 @@
 use ast_node::ast_node;
+use swc_common::plugin::serialized::{PluginSerializedBytes, VersionedSerializable};
 
 #[ast_node]
 pub enum ExprV1 {
@@ -46,4 +47,33 @@ pub struct PropertyV2 {
 #[ast_node("Array")]
 pub struct Array {
     elements: Vec<ExprV2>,
+}
+
+#[test]
+fn bakward_compatible() {
+    let v2_data = {
+        let s1 = String::from("test-1");
+        let s2 = String::from("test-2");
+        let arr = Array {
+            elements: vec![ExprV2::String(s1), ExprV2::String(s2)],
+        };
+
+        VersionedSerializable::new(ExprV2::Object(ObjectV2 {
+            properties: vec![
+                PropertyV2 {
+                    key: String::from("string"),
+                    value: Box::new(ExprV2::String(s)),
+                },
+                PropertyV2 {
+                    key: String::from("array"),
+                    value: Box::new(ExprV2::Array(arr)),
+                },
+            ],
+        }))
+    };
+
+    let v2_bytes = PluginSerializedBytes::try_serialize(&v2_data).unwrap();
+    v2_data
+        .deserialize::<ExprV1>()
+        .expect("failed to deserialize v2 data into v1 data");
 }
