38045d35df7aef03d07151d8e06d44a7db25069d
Donny/강동윤 <kdy.1997.dev@gmail.com>
2025-11-07 00:52:16 +0900

    feat(visit): Add hook APIs for visitors (#11242)
    **Description:**
    
    ### VisitMutHook 
    
    This does not traverse the node, and have `enter_xxx` and `exit_xxx`
    
    ### CompositeHook<A, B>: VisitMutHook
    
     - enter: a.enter() -> b.enter()
     - exit: b.exit() -> a.exit()
    
    ### VisitMutWithHook<H: VisitMutHook>
    
    Terms:
    
     - visitor = VisitMutWithHook
     - hook = H (implements VisitMutHook)
    
    Traverse order:
     - a.enter() -> b.enter() -> recursive visit -> b.enter() -> a.enter()
    
    
    ---
    
    
    ```
    enter_expr: name=hook1, depth=0, count=0 # symmetric with bottom-most item
    enter_expr: name=hook2, depth=0, count=0
    
    enter_span: name=hook1, depth=1, count=0
    enter_span: name=hook2, depth=1, count=0
    exit_span: name=hook2, depth=1, count=1
    exit_span: name=hook1, depth=1, count=1
    
    enter_expr: name=hook1, depth=1, count=1
    enter_expr: name=hook2, depth=1, count=1
    
    enter_span: name=hook1, depth=2, count=1
    enter_span: name=hook2, depth=2, count=1
    exit_span: name=hook2, depth=2, count=2
    exit_span: name=hook1, depth=2, count=2
    enter_expr: name=hook1, depth=2, count=2
    enter_expr: name=hook2, depth=2, count=2
    enter_span: name=hook1, depth=3, count=2
    enter_span: name=hook2, depth=3, count=2
    exit_span: name=hook2, depth=3, count=3
    exit_span: name=hook1, depth=3, count=3
    exit_expr: name=hook2, depth=2, count=3
    exit_expr: name=hook1, depth=2, count=3
    
    exit_expr: name=hook2, depth=1, count=3
    exit_expr: name=hook1, depth=1, count=3
    
    exit_expr: name=hook2, depth=0, count=3
    exit_expr: name=hook1, depth=0, count=3
    ```

---
+++ crates/swc_ecma_visit/tests/compose.rs
@@ -0,0 +1,84 @@
+use swc_atoms::atom;
+use swc_common::{Span, DUMMY_SP};
+use swc_ecma_ast::*;
+use swc_ecma_visit::{CompositeHook, VisitMutHook, VisitMutWith, VisitMutWithHook};
+
+#[derive(Debug, Default)]
+struct Hook {
+    name: String,
+    depth: usize,
+    span_count: usize,
+}
+
+impl VisitMutHook for Hook {
+    fn enter_span(&mut self, _span: &mut Span) {
+        println!(
+            "enter_span: name={}, depth={}, count={}",
+            self.name, self.depth, self.span_count
+        );
+        self.depth += 1;
+        self.span_count += 1;
+    }
+
+    fn exit_span(&mut self, _span: &mut Span) {
+        self.depth -= 1;
+        println!(
+            "exit_span: name={}, depth={}, count={}",
+            self.name, self.depth, self.span_count
+        );
+    }
+
+    fn enter_expr(&mut self, _expr: &mut Expr) {
+        println!(
+            "enter_expr: name={}, depth={}, count={}",
+            self.name, self.depth, self.span_count
+        );
+        self.depth += 1;
+    }
+
+    fn exit_expr(&mut self, _expr: &mut Expr) {
+        self.depth -= 1;
+        println!(
+            "exit_expr: name={}, depth={}, count={}",
+            self.name, self.depth, self.span_count
+        );
+    }
+}
+
+#[test]
+fn compose_visit() {
+    let hook1 = Hook {
+        name: "hook1".to_string(),
+        ..Default::default()
+    };
+    let hook2 = Hook {
+        name: "hook2".to_string(),
+        ..Default::default()
+    };
+
+    let hook = CompositeHook {
+        first: hook1,
+        second: hook2,
+    };
+
+    let mut node = Expr::Call(CallExpr {
+        span: DUMMY_SP,
+        callee: Callee::Expr(
+            AwaitExpr {
+                span: DUMMY_SP,
+                arg: Ident::new_no_ctxt(atom!("foo"), DUMMY_SP).into(),
+            }
+            .into(),
+        ),
+        ..Default::default()
+    });
+
+    let mut visitor = VisitMutWithHook { hook };
+
+    node.visit_mut_with(&mut visitor);
+
+    assert_eq!(visitor.hook.first.depth, 0);
+    assert_eq!(visitor.hook.first.span_count, 3);
+    assert_eq!(visitor.hook.second.depth, 0);
+    assert_eq!(visitor.hook.second.span_count, 3);
+}
