d8f468a07571fbf4fe66dfacb33088ff6f38125c
CPunisher <1343316114@qq.com>
2025-11-14 14:31:04 +0800

    Update

--- crates/swc_atoms/src/hash.rs
+++ crates/swc_atoms/src/hash.rs
@@ -11,22 +11,32 @@ pub type AtomMap<V> = HashMap<Atom, V, BuildHasherDefault<IdentityHasher>>;
 
 pub type AtomSet = HashSet<Atom, BuildHasherDefault<IdentityHasher>>;
 
+#[cfg(target_pointer_width = "64")]
+const K: usize = 0xf1357aea2e62a9c5;
+#[cfg(target_pointer_width = "32")]
+const K: usize = 0x93d765dd;
+
 #[derive(Default)]
 pub struct IdentityHasher {
-    hash: u64,
+    hash: usize,
 }
 
 impl Hasher for IdentityHasher {
     #[inline]
     fn write(&mut self, bytes: &[u8]) {
         if bytes.len() == 8 {
-            self.hash = NativeEndian::read_u64(bytes);
+            self.hash = (NativeEndian::read_u64(bytes) as usize).wrapping_mul(K);
         }
     }
 
     #[inline]
     fn finish(&self) -> u64 {
-        self.hash
+        #[cfg(target_pointer_width = "64")]
+        const ROTATE: u32 = 26;
+        #[cfg(target_pointer_width = "32")]
+        const ROTATE: u32 = 15;
+
+        self.hash.rotate_left(ROTATE) as u64
     }
 }
 
