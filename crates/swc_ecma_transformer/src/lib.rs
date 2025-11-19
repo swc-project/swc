5a8f618be040475da8a1c93e325360d94419acae
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-19 09:57:12 -0500

    hook

--- crates/swc_ecma_transformer/src/lib.rs
+++ crates/swc_ecma_transformer/src/lib.rs
@@ -1,30 +1,11 @@
 use swc_ecma_ast::*;
-use swc_ecma_hooks::VisitMutHook;
-use swc_ecma_visit::{visit_mut_pass, VisitMut, VisitMutWith};
+use swc_ecma_hooks::{VisitMutHook, VisitMutWithHook};
+use swc_ecma_visit::visit_mut_pass;
 
 pub struct TraverseCtx {}
 
 pub fn transform_pass<H: VisitMutHook<TraverseCtx>>(hook: H) -> impl Pass {
     let ctx = TraverseCtx {};
 
-    visit_mut_pass(Transformer { hook, context: ctx })
-}
-
-struct Transformer<H>
-where
-    H: VisitMutHook<TraverseCtx>,
-{
-    hook: H,
-    context: TraverseCtx,
-}
-
-impl<H> VisitMut for Transformer<H>
-where
-    H: VisitMutHook<TraverseCtx>,
-{
-    fn visit_mut_program(&mut self, program: &mut Program) {
-        self.hook.enter_program(program, &mut self.context);
-        program.visit_mut_children_with(self);
-        self.hook.exit_program(program, &mut self.context);
-    }
+    visit_mut_pass(VisitMutWithHook { hook, context: ctx })
 }
