6fc25e7014fc134af1de896e20cb0e02c8e795e8
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-19 09:58:26 -0500

    HookBuilder

--- crates/swc_ecma_transformer/src/lib.rs
+++ crates/swc_ecma_transformer/src/lib.rs
@@ -4,6 +4,26 @@ use swc_ecma_visit::visit_mut_pass;
 
 pub struct TraverseCtx {}
 
+pub struct HookBuilder<H>
+where
+    H: VisitMutHook<TraverseCtx>,
+{
+    hook: H,
+}
+
+impl<H> HookBuilder<H>
+where
+    H: VisitMutHook<TraverseCtx>,
+{
+    pub fn new(hook: H) -> Self {
+        Self { hook }
+    }
+
+    pub fn build(self) -> impl VisitMutHook<TraverseCtx> {
+        self.hook
+    }
+}
+
 pub fn transform_pass<H: VisitMutHook<TraverseCtx>>(hook: H) -> impl Pass {
     let ctx = TraverseCtx {};
 
