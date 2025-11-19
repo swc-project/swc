cd32da33e0ab655151b25dc0cb363e2475c36a5c
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-19 09:52:41 -0500

    impl Pass for Transformer

--- crates/swc_ecma_transformer/src/lib.rs
+++ crates/swc_ecma_transformer/src/lib.rs
@@ -1,9 +1,17 @@
-use swc_ecma_hooks::VisitMutHook;
+use swc_ecma_ast::*;
+use swc_ecma_visit::VisitMutWith;
 
 pub struct TraverseCtx {}
 
 pub struct Transformer {}
 
-struct TransformerImpl<H: VisitMutHook<TraverseCtx>> {
-    hook: H,
+impl Pass for Transformer {
+    fn process(&mut self, program: &mut Program) {
+        let hook = {};
+        let ctx = TraverseCtx {};
+
+        let mut visitor = swc_ecma_hooks::VisitMutWithHook { hook, context: ctx };
+
+        program.visit_mut_with(&mut visitor);
+    }
 }
