571b1658bd90c5c219af71b13ccf55d409b713dc
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-20 06:12:14 -0500

    cmt

--- crates/swc_ecma_transformer/src/common/statement_injector.rs
+++ crates/swc_ecma_transformer/src/common/statement_injector.rs
@@ -1,3 +1,20 @@
+//! Utility transform to add new statements before or after the specified
+//! statement.
+//!
+//! `StatementInjectorStore` contains a `FxHashMap<Address,
+//! Vec<AdjacentStatement>>`. It is stored on `TransformCtx`.
+//!
+//! `StatementInjector` transform inserts new statements before or after a
+//! statement which is determined by the address of the statement.
+//!
+//! Other transforms can add statements to the store with following methods:
+//!
+//! ```rs
+//! self.ctx.statement_injector.insert_before(address, statement);
+//! self.ctx.statement_injector.insert_after(address, statement);
+//! self.ctx.statement_injector.insert_many_after(address, statements);
+//! ```
+
 use std::cell::RefCell;
 
 use rustc_hash::FxHashMap;
@@ -8,7 +25,7 @@ use swc_ecma_visit::AstKindPath;
 use crate::TraverseCtx;
 
 #[derive(Debug, Default)]
-pub struct StatementInjector {}
+pub struct StmtInjector {}
 
 #[derive(Debug)]
 enum Direction {
@@ -17,15 +34,15 @@ enum Direction {
 }
 
 #[derive(Debug)]
-struct AdjacentStatement {
+struct AdjacentStmt {
     stmt: Stmt,
     direction: Direction,
 }
 
-/// Store for statements to be added to the statements.
+/// Store for Stmts to be added to the Stmts.
 #[derive(Default)]
-pub struct StatementInjectorStore {
-    insertions: RefCell<FxHashMap<AstKindPath, Vec<AdjacentStatement>>>,
+pub struct StmtInjectorStore {
+    insertions: RefCell<FxHashMap<AstKindPath, Vec<AdjacentStmt>>>,
 }
 
-impl VisitMutHook<TraverseCtx> for StatementInjector {}
+impl VisitMutHook<TraverseCtx> for StmtInjector {}
