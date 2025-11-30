e660a9d4895b33b3f27a854fda5584d77a890462
magic-akari <akari.ccino@gmail.com>
2025-11-30 17:47:40 +0800

    fix(es/compat): Fix generator transform for compound assignments, for-in, and labeled break

--- crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
+++ crates/swc_ecma_transforms_base/src/helpers/_ts_generator.js
@@ -1,6 +1,6 @@
 function _ts_generator(thisArg, body) {
-    var f, y, t, _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
+    var f, y, t, _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype), d = Object.defineProperty;
+    return d(g, "next", { value: verb(0) }), d(g, "throw", { value: verb(1) }), d(g, "return", { value: verb(2) }), typeof Symbol === "function" && d(g, Symbol.iterator, { value: function () { return this; } }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
