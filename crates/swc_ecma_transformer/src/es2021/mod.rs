7a3634912e4439eb4b28be285fc6f3efe301c4a9
DongYun Kang <kdy.1997.dev@gmail.com>
2025-12-04 16:16:26 +0900

    Add modules

--- crates/swc_ecma_transformer/src/es2021/mod.rs
+++ crates/swc_ecma_transformer/src/es2021/mod.rs
@@ -1,10 +1,14 @@
+mod logical_assignment_operators;
+
 use swc_ecma_hooks::VisitMutHook;
 
 use crate::TraverseCtx;
 
 #[derive(Debug, Default)]
 #[non_exhaustive]
-pub struct Es2021Options {}
+pub struct Es2021Options {
+    pub logical_assignment_operators: bool,
+}
 
 pub fn hook(options: Es2021Options) -> impl VisitMutHook<TraverseCtx> {
     Es2021Pass { options }
--- crates/swc_ecma_transformer/src/es2022/mod.rs
+++ crates/swc_ecma_transformer/src/es2022/mod.rs
@@ -2,9 +2,16 @@ use swc_ecma_hooks::VisitMutHook;
 
 use crate::TraverseCtx;
 
+mod class_static_block;
+mod private_property_in_object;
+
 #[derive(Debug, Default)]
 #[non_exhaustive]
-pub struct Es2022Options {}
+pub struct Es2022Options {
+    pub class_static_block: bool,
+
+    pub private_property_in_object: bool,
+}
 
 pub fn hook(options: Es2022Options) -> impl VisitMutHook<TraverseCtx> {
     Es2022Pass { options }
