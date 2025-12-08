d1975d44c9a580978672f2829e5e9831d7751e78
DongYun Kang <kdy.1997.dev@gmail.com>
2025-12-08 14:22:03 +0900

    hook_utils

--- crates/swc_ecma_transformer/src/hook_utils.rs
+++ crates/swc_ecma_transformer/src/hook_utils.rs
@@ -52,6 +52,42 @@ where
     optional_method!(enter_program, exit_program, Program);
 
     optional_method!(enter_catch_clause, exit_catch_clause, CatchClause);
+
+    optional_method!(enter_for_in_stmt, exit_for_in_stmt, ForInStmt);
+
+    optional_method!(enter_for_of_stmt, exit_for_of_stmt, ForOfStmt);
+
+    optional_method!(enter_for_stmt, exit_for_stmt, ForStmt);
+
+    optional_method!(enter_while_stmt, exit_while_stmt, WhileStmt);
+
+    optional_method!(enter_do_while_stmt, exit_do_while_stmt, DoWhileStmt);
+
+    optional_method!(enter_switch_stmt, exit_switch_stmt, SwitchStmt);
+
+    optional_method!(enter_labeled_stmt, exit_labeled_stmt, LabeledStmt);
+
+    optional_method!(enter_break_stmt, exit_break_stmt, BreakStmt);
+
+    optional_method!(enter_continue_stmt, exit_continue_stmt, ContinueStmt);
+
+    optional_method!(enter_return_stmt, exit_return_stmt, ReturnStmt);
+
+    optional_method!(enter_throw_stmt, exit_throw_stmt, ThrowStmt);
+
+    optional_method!(enter_try_stmt, exit_try_stmt, TryStmt);
+
+    optional_method!(enter_with_stmt, exit_with_stmt, WithStmt);
+
+    optional_method!(enter_debugger_stmt, exit_debugger_stmt, DebuggerStmt);
+
+    optional_method!(enter_function, exit_function, Function);
+
+    optional_method!(enter_arrow_expr, exit_arrow_expr, ArrowExpr);
+
+    optional_method!(enter_var_decl, exit_var_decl, VarDecl);
+
+    optional_method!(enter_var_declarator, exit_var_declarator, VarDeclarator);
 }
 
 pub(crate) struct NoopHook;
