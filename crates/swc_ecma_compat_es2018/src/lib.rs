c4a5e7989bf0bb943051c56d03f8121d921c9f13
Donny/강동윤 <kdy.1997.dev@gmail.com>
2025-12-12 17:03:13 +0900

    perf(es/compat): Use merged transformer (#11366)

--- crates/swc_ecma_compat_es2018/src/lib.rs
+++ crates/swc_ecma_compat_es2018/src/lib.rs
@@ -3,26 +3,29 @@
 
 use serde::Deserialize;
 use swc_ecma_ast::Pass;
-use swc_ecma_compat_common::regexp::{self, regexp};
 
 pub use self::object_rest_spread::object_rest_spread;
 
 pub mod object_rest_spread;
 
-pub fn es2018(c: Config) -> impl Pass {
-    (
-        regexp(regexp::Config {
-            dot_all_regex: true,
-            has_indices: false,
-            lookbehind_assertion: true,
-            named_capturing_groups_regex: true,
-            sticky_regex: false,
-            unicode_property_regex: true,
-            unicode_regex: false,
-            unicode_sets_regex: false,
-        }),
-        object_rest_spread(c.object_rest_spread),
-    )
+pub fn es2018(config: Config) -> impl Pass {
+    let mut options = swc_ecma_transformer::Options::default();
+
+    {
+        options.env.regexp.dot_all_regex = true;
+        options.env.regexp.lookbehind_assertion = true;
+        options.env.regexp.named_capturing_groups_regex = true;
+        options.env.regexp.unicode_property_regex = true;
+    }
+
+    {
+        options.env.es2018.object_rest_spread = true;
+        options.assumptions.object_rest_no_symbols = config.object_rest_spread.no_symbol;
+        options.assumptions.set_spread_properties = config.object_rest_spread.set_property;
+        options.assumptions.pure_getters = config.object_rest_spread.pure_getters;
+    }
+
+    options.into_pass()
 }
 
 #[derive(Debug, Clone, Copy, Default, Deserialize)]
