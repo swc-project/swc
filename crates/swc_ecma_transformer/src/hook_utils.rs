316d189305f021f43c382d6049e205ee940ec192
DongYun Kang <kdy.1997.dev@gmail.com>
2025-12-13 13:58:49 +0900

    hook order

--- crates/swc_ecma_transformer/src/hook_utils.rs
+++ crates/swc_ecma_transformer/src/hook_utils.rs
@@ -74,6 +74,76 @@ where
     optional_method!(enter_for_of_stmt, exit_for_of_stmt, ForOfStmt);
 }
 
+macro_rules! chained_method {
+    ($enter_name:ident, $exit_name:ident, $T:ty) => {
+        fn $enter_name(&mut self, node: &mut $T, ctx: &mut TraverseCtx) {
+            self.0.$enter_name(node, ctx);
+            self.1.$enter_name(node, ctx);
+        }
+
+        fn $exit_name(&mut self, node: &mut $T, ctx: &mut TraverseCtx) {
+            self.0.$exit_name(node, ctx);
+            self.1.$exit_name(node, ctx);
+        }
+    };
+}
+
+/// Execution order: A => B (both for enter and exit)
+pub(crate) struct OrderedChain<A, B>(pub A, pub B)
+where
+    A: VisitMutHook<TraverseCtx>,
+    B: VisitMutHook<TraverseCtx>;
+
+impl<A, B> VisitMutHook<TraverseCtx> for OrderedChain<A, B>
+where
+    A: VisitMutHook<TraverseCtx>,
+    B: VisitMutHook<TraverseCtx>,
+{
+    chained_method!(enter_expr, exit_expr, Expr);
+
+    chained_method!(enter_expr_stmt, exit_expr_stmt, ExprStmt);
+
+    chained_method!(enter_pat, exit_pat, Pat);
+
+    chained_method!(enter_stmt, exit_stmt, Stmt);
+
+    chained_method!(enter_stmts, exit_stmts, Vec<Stmt>);
+
+    chained_method!(enter_block_stmt, exit_block_stmt, BlockStmt);
+
+    chained_method!(enter_module_item, exit_module_item, ModuleItem);
+
+    chained_method!(enter_module_items, exit_module_items, Vec<ModuleItem>);
+
+    chained_method!(enter_module, exit_module, Module);
+
+    chained_method!(enter_script, exit_script, Script);
+
+    chained_method!(enter_program, exit_program, Program);
+
+    chained_method!(enter_catch_clause, exit_catch_clause, CatchClause);
+
+    chained_method!(enter_function, exit_function, Function);
+
+    chained_method!(enter_arrow_expr, exit_arrow_expr, ArrowExpr);
+
+    chained_method!(enter_class, exit_class, Class);
+
+    chained_method!(enter_constructor, exit_constructor, Constructor);
+
+    chained_method!(enter_getter_prop, exit_getter_prop, GetterProp);
+
+    chained_method!(enter_setter_prop, exit_setter_prop, SetterProp);
+
+    chained_method!(enter_super, exit_super, Super);
+
+    chained_method!(enter_var_decl, exit_var_decl, VarDecl);
+
+    chained_method!(enter_for_in_stmt, exit_for_in_stmt, ForInStmt);
+
+    chained_method!(enter_for_of_stmt, exit_for_of_stmt, ForOfStmt);
+}
+
 pub(crate) struct NoopHook;
 
 impl VisitMutHook<TraverseCtx> for NoopHook {}
@@ -98,10 +168,7 @@ where
         B: VisitMutHook<TraverseCtx>,
     {
         HookBuilder {
-            hook: CompositeHook {
-                first: hook,
-                second: self.hook,
-            },
+            hook: OrderedChain(self.hook, hook),
         }
     }
 
