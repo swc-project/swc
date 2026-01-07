8fc9b74e7e7f65b93d7a53080439b8ddf7af67e2
DongYun Kang <kdy.1997.dev@gmail.com>
2026-01-07 14:44:24 +0900

    vis

--- crates/swc_ecma_transforms_typescript/src/lib.rs
+++ crates/swc_ecma_transforms_typescript/src/lib.rs
@@ -5,8 +5,8 @@
 pub use self::typescript::*;
 mod config;
 mod macros;
-pub mod strip_import_export;
-pub mod strip_type;
+mod strip_import_export;
+mod strip_type;
 mod transform;
 mod ts_enum;
 pub mod typescript;
