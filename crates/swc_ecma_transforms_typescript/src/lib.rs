688a5fb8195a8cc69e4f02a05c496fdbde787cde
DongYun Kang <kdy.1997.dev@gmail.com>
2026-01-07 13:21:42 +0900

    revert

--- crates/swc_ecma_transforms_typescript/src/lib.rs
+++ crates/swc_ecma_transforms_typescript/src/lib.rs
@@ -2,11 +2,11 @@
 #![allow(clippy::vec_box)]
 #![allow(clippy::mutable_key_type)]
 
-pub use self::typescript::*;
+pub use self::{strip_type::*, typescript::*};
 mod config;
 mod macros;
-pub mod strip_import_export;
-pub mod strip_type;
+mod strip_import_export;
+mod strip_type;
 mod transform;
 mod ts_enum;
 pub mod typescript;
