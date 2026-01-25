050b8011b4e584a79933aae9cc90dd79747fd903
DongYun Kang <kdy.1997.dev@gmail.com>
2026-01-26 08:25:50 +0900

    use swc_ecma_transfomer

--- crates/swc_ecma_compat_es2015/src/instanceof.rs
+++ crates/swc_ecma_compat_es2015/src/instanceof.rs
@@ -31,44 +31,7 @@ use swc_trace_macro::swc_trace;
 /// _instanceof(foo, Bar);
 /// ```
 pub fn instance_of() -> impl Pass {
-    visit_mut_pass(InstanceOf)
-}
-struct InstanceOf;
-
-impl Parallel for InstanceOf {
-    fn merge(&mut self, _: Self) {}
-
-    fn create(&self) -> Self {
-        InstanceOf
-    }
-}
-
-#[swc_trace]
-impl VisitMut for InstanceOf {
-    noop_visit_mut_type!(fail);
-
-    fn visit_mut_expr(&mut self, expr: &mut Expr) {
-        expr.visit_mut_children_with(self);
-
-        if let Expr::Bin(BinExpr {
-            span,
-            left,
-            op: op!("instanceof"),
-            right,
-        }) = expr
-        {
-            let instanceof_span = Span {
-                lo: left.span_hi(),
-                hi: right.span_lo(),
-            };
-
-            *expr = CallExpr {
-                span: *span,
-                callee: helper!(instanceof_span, instanceof),
-                args: vec![left.take().as_arg(), right.take().as_arg()],
-                ..Default::default()
-            }
-            .into();
-        }
-    }
+    let mut options = swc_ecma_transformer::Options::default();
+    options.env.es2015.instanceof = true;
+    options.into_pass()
 }
