c4604e58f05c5bbf2d8f3f11fb9bb1c158b9e3c6
Donny/강동윤 <kdy.1997.dev@gmail.com>
2026-01-26 10:19:08 +0900

    Discard changes to crates/swc/tests/fixture/issues-10xxx/10729/output/index.jsx

--- crates/swc/tests/fixture/issues-10xxx/10729/output/index.jsx
+++ crates/swc/tests/fixture/issues-10xxx/10729/output/index.jsx
@@ -1,5 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-var type = function type() {
+var type = function() {
     return null;
 };
 var widget = {
@@ -7,7 +7,7 @@ var widget = {
         type: type
     }
 };
-var render = function render() {
+var render = function() {
     return /*#__PURE__*/ _jsx(widget.component.type, {
         if: "aaa"
     });
