d0458f64b7099a915c1458a6732fdeffe8ad52bd
DongYun Kang <kdy.1997.dev@gmail.com>
2026-02-18 20:37:24 -0800

    test(react): expand issue-11550 multiline attr fixtures

---
+++ crates/swc_ecma_transforms_react/tests/jsx/fixture/issue-11550/automatic/output.mjs
@@ -0,0 +1,28 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export const lf = /*#__PURE__*/ _jsx("div", {
+    "data-a": "bruh\nbruh"
+});
+export const lf_with_indent = /*#__PURE__*/ _jsx("div", {
+    "data-b": "bruh\n    bruh"
+});
+export const lf_with_leading_space = /*#__PURE__*/ _jsx("div", {
+    "data-c": "bruh\n bruh"
+});
+export const double_lf = /*#__PURE__*/ _jsx("div", {
+    "data-d": "bruh\n\nbruh"
+});
+export const three_lines = /*#__PURE__*/ _jsx("div", {
+    "data-e": "bruh\nbruh\nbruh"
+});
+export const entity_lf = /*#__PURE__*/ _jsx("div", {
+    "data-f": "bruh\nbruh"
+});
+export const entity_cr = /*#__PURE__*/ _jsx("div", {
+    "data-g": "bruh\rbruh"
+});
+export const entity_tab = /*#__PURE__*/ _jsx("div", {
+    "data-h": "bruh	bruh"
+});
+export const lf_then_entity_tab = /*#__PURE__*/ _jsx("div", {
+    "data-i": "bruh\n	bruh"
+});
--- crates/swc_ecma_transforms_react/tests/jsx/fixture/issue-11550/output.mjs
+++ crates/swc_ecma_transforms_react/tests/jsx/fixture/issue-11550/output.mjs
@@ -1,6 +1,27 @@
-"use client";
-export default function Hello({}) {
-    return /*#__PURE__*/ React.createElement("div", {
-        "data-anything": "bruh\nbruh"
-    }, "hello");
-}
+export const lf = /*#__PURE__*/ React.createElement("div", {
+    "data-a": "bruh\nbruh"
+});
+export const lf_with_indent = /*#__PURE__*/ React.createElement("div", {
+    "data-b": "bruh\n    bruh"
+});
+export const lf_with_leading_space = /*#__PURE__*/ React.createElement("div", {
+    "data-c": "bruh\n bruh"
+});
+export const double_lf = /*#__PURE__*/ React.createElement("div", {
+    "data-d": "bruh\n\nbruh"
+});
+export const three_lines = /*#__PURE__*/ React.createElement("div", {
+    "data-e": "bruh\nbruh\nbruh"
+});
+export const entity_lf = /*#__PURE__*/ React.createElement("div", {
+    "data-f": "bruh\nbruh"
+});
+export const entity_cr = /*#__PURE__*/ React.createElement("div", {
+    "data-g": "bruh\rbruh"
+});
+export const entity_tab = /*#__PURE__*/ React.createElement("div", {
+    "data-h": "bruh	bruh"
+});
+export const lf_then_entity_tab = /*#__PURE__*/ React.createElement("div", {
+    "data-i": "bruh\n	bruh"
+});
