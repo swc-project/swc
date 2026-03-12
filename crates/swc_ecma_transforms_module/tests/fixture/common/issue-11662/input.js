ec1a253a351e30dd02d5b79b77a053b0cf50f8d9
DongYun Kang <kdy.1997.dev@gmail.com>
2026-03-12 09:35:29 +0900

    perf(swc_ecma_transforms_module): avoid export sort key clones

---
+++ crates/swc_ecma_transforms_module/tests/fixture/common/issue-11662/input.js
@@ -0,0 +1,8 @@
+export { zed as m, alpha as z } from "dep-a";
+
+const localC = 3;
+const localA = 1;
+
+export { localC as c, localA as a };
+export { default as b } from "dep-b";
+export * as d from "dep-c";
