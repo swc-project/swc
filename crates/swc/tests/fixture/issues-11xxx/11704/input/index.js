8b4f05da4436ae984e8363d6fe3f07c84fe4d7cf
magic-akari <akari.ccino@gmail.com>
2026-04-23 20:46:22 +0800

    chore: update test fixture

---
+++ crates/swc/tests/fixture/issues-11xxx/11704/input/index.js
@@ -0,0 +1,11 @@
+16777215; // same length after minification
+16777216; // bigger after minification
+
+268435455; // same length after minification
+268435456; // bigger after minification
+
+4294967295; // same length after minification
+4294967296; // bigger after minification
+
+68719476735; // same length after minification
+68719476736; // bigger after minification
---
+++ crates/swc/tests/fixture/issues-11xxx/11704/output/index.js
@@ -0,0 +1,8 @@
+0xffffff;// same length after minification
+16777216;// bigger after minification
+0xfffffff;// same length after minification
+268435456;// bigger after minification
+0xffffffff;// same length after minification
+4294967296;// bigger after minification
+0xfffffffff;// same length after minification
+68719476736;// bigger after minification
