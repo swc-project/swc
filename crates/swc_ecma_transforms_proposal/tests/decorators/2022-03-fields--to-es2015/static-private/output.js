3273a083a77c745aadbba2a0926bd6e2d60251fb
DongYun Kang <kdy.1997.dev@gmail.com>
2026-05-27 15:31:18 +0900

    revert(es/decorators): revert decorator initializer ordering

--- crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-fields--to-es2015/static-private/output.js
+++ crates/swc_ecma_transforms_proposal/tests/decorators/2022-03-fields--to-es2015/static-private/output.js
@@ -1,44 +1,39 @@
-var _init_a, _init_extra_a, _init_b, _init_extra_b;
+var _init_a, _init_b, _initStatic;
 const dec = ()=>{};
 class Foo {
 }
-({ e: [_init_a, _init_extra_a, _init_b, _init_extra_b] } = _apply_decs_2203_r(Foo, [
-    [
-        dec,
-        5,
-        "a",
-        function() {
-            return _class_static_private_field_spec_get(this, Foo, _a);
-        },
-        function(value) {
-            _class_static_private_field_spec_set(this, Foo, _a, value);
-        }
-    ],
-    [
-        dec,
-        5,
-        "b",
-        function() {
-            return _class_static_private_field_spec_get(this, Foo, _b);
-        },
-        function(value) {
-            _class_static_private_field_spec_set(this, Foo, _b, value);
-        }
-    ]
-], []));
+(()=>{
+    ({ e: [_init_a, _init_b, _initStatic] } = _apply_decs_2203_r(Foo, [
+        [
+            dec,
+            5,
+            "a",
+            function() {
+                return _class_static_private_field_spec_get(this, Foo, _a);
+            },
+            function(value) {
+                _class_static_private_field_spec_set(this, Foo, _a, value);
+            }
+        ],
+        [
+            dec,
+            5,
+            "b",
+            function() {
+                return _class_static_private_field_spec_get(this, Foo, _b);
+            },
+            function(value) {
+                _class_static_private_field_spec_set(this, Foo, _b, value);
+            }
+        ]
+    ], []));
+    _initStatic(Foo);
+})();
 var _a = {
     writable: true,
-    value: (()=>{
-        const _value = _init_a(Foo);
-        _init_extra_a(Foo);
-        return _value;
-    })()
+    value: _init_a(Foo)
 };
 var _b = {
     writable: true,
-    value: (()=>{
-        const _value = _init_b(Foo, 123);
-        _init_extra_b(Foo);
-        return _value;
-    })()
+    value: _init_b(Foo, 123)
 };
