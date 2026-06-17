22dca84bb7f42c95df7743673cdc1e6d37e08f58
magic-akari <akari.ccino@gmail.com>
2026-06-18 00:30:47 +0800

    test: Add more test cases

---
+++ crates/swc_ecma_transforms_base/tests/ts-resolver/namespace_reopen_nested_type_only_export/output.ts
@@ -0,0 +1,12 @@
+namespace Outer__2 {
+    export interface Inner__3 {}
+    export namespace Inner__4 {
+        export const a__5 = 1;
+    }
+}
+namespace Outer__2 {
+    export interface Inner__3 {}
+    export namespace Inner__6 {
+        export const b__7 = a__5;
+    }
+}
