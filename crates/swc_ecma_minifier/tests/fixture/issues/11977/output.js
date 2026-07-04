1ef9efe9c2abe4d0f15341f5b7a32697522cbbaf
austaras <austaras@outlook.com>
2026-07-04 17:28:50 +0800

    fix(es/minifier): Disable IIFE invoke when there's eval

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/11977/output.js
@@ -0,0 +1,12 @@
+var Subscription = function() {
+    (function(listeners, listener) {}).prototype.add = function(subscription) {
+        this.unsubscribed;
+    };
+}();
+try {
+    serverOnlyRequire = eval("require");
+} catch (err) {}
+var __require = /* @__PURE__ */ ((x)=>(0, __turbopack_context__.z))(function(x) {})(ErrorType || {}), isRequestError = (error)=>{
+    i.getKey;
+}, x = class extends d {
+};
