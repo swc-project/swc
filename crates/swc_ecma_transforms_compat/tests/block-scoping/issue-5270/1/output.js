176dea1ff9f10c72bf973ad77355583075015d87
austaras <austaras@outlook.com>
2026-07-05 18:17:14 +0800

    update test

--- crates/swc_ecma_transforms_compat/tests/block-scoping/issue-5270/1/output.js
+++ crates/swc_ecma_transforms_compat/tests/block-scoping/issue-5270/1/output.js
@@ -1,5 +1,5 @@
-var _loop__5 = function(i__3) {
+var _loop__6 = function(i__3) {
     var j__4 = Math.random();
     setTimeout(()=>console.log(j__4), 0);
 };
-for(var i__3 = 0; i__3 < 5; i__3++)_loop__5(i__3);
+for(var i__3 = 0; i__3 < 5; i__3++)_loop__6(i__3);
