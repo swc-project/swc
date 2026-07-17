3a96049245db5bbd6bf15fbd139ccddacfae94a8
DongYun Kang <kdy.1997.dev@gmail.com>
2026-07-17 14:32:10 +0900

    test(es/minifier): cover dynamic new argument safety

---
+++ crates/swc_ecma_minifier/tests/fixture/issues/11684/decorated-class/input.js
@@ -0,0 +1,18 @@
+@replace
+class DecoratedDeclaration {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+out.DecoratedDeclaration = DecoratedDeclaration;
+out.decoratedDeclaration = new DecoratedDeclaration(1, 2, 3);
+
+const DecoratedExpression = @replace class {
+    constructor(value) {
+        this.value = value;
+    }
+};
+
+out.DecoratedExpression = DecoratedExpression;
+out.decoratedExpression = new DecoratedExpression(1, 2, 3);
