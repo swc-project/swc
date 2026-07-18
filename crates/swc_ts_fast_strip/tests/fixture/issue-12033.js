8a3130537bf919bdd53ee91b3563c592061267c4
magic-akari <akari.ccino@gmail.com>
2026-07-18 22:00:43 +0800

    chore: update test cases

--- crates/swc_ts_fast_strip/tests/fixture/issue-12033.js
+++ crates/swc_ts_fast_strip/tests/fixture/issue-12033.js
@@ -1,7 +1,21 @@
+// keep as-is
+const assigned =    
+(value   ) => value;
+
 const asyncArrow = async(  
  value   ) => value;
 
 function returnsOne() {
   return (  
+   value   ) => value, 1;
+}
+
+function throwOne() {
+  throw (  
+   value   ) => value, 1;
+}
+
+function *yieldOne() {
+  yield (  
    value   ) => value, 1;
 }
--- crates/swc_ts_fast_strip/tests/fixture/issue-12033.ts
+++ crates/swc_ts_fast_strip/tests/fixture/issue-12033.ts
@@ -1,7 +1,21 @@
+// keep as-is
+const assigned = <T>
+(value: T) => value;
+
 const asyncArrow = async<T>
 (value: T) => value;
 
 function returnsOne() {
   return <T>
+  (value: T) => value, 1;
+}
+
+function throwOne() {
+  throw <T>
+  (value: T) => value, 1;
+}
+
+function *yieldOne() {
+  yield <T>
   (value: T) => value, 1;
 }
