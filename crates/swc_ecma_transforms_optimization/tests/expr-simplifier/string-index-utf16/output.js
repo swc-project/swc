a0d57eec6cf2f66df02a01870b647ddfbf804f03
magic-akari <akari.ccino@gmail.com>
2026-07-21 13:33:15 +0800

    fix(es/minifier): index strings by UTF-16 code unit

---
+++ crates/swc_ecma_transforms_optimization/tests/expr-simplifier/string-index-utf16/output.js
@@ -0,0 +1,7 @@
+const bmpStringIndices = ["é", "\u00e9"["1"]];
+const astralStringIndices = [
+    "\uD83D",
+    "\uDE00",
+    "\ud83d\ude00"["2"],
+];
+const loneSurrogateIndices = ["\uD800", "\ud800"["1"]];
